ed4da0a708a00778bbc9e87eaf73c4c80362d2c7
JaredMathis <jared.mathis@native.consulting>
2023-05-27 17:49:56 -0400

    progress

--- src/string/sub/start.mjs
+++ src/string/sub/start.mjs
@@ -1,4 +1,5 @@
-import { string_length } from "../string_length.mjs";
+import { string_length } from "../length.mjs";
+import { string_sub_from } from "./from.mjs";
 
 export function string_sub_start(input, start) {
     let length = string_length(input);
