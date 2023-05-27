34ddde339c1e067cce68a934aed03462d8be38fd
JaredMathis <jared.mathis@native.consulting>
2023-05-27 19:01:08 -0400

    progress

--- src/string/function/tests/name/is.mjs
+++ src/string/function/tests/name/is.mjs
@@ -2,5 +2,5 @@ import { string_includes } from "../../../includes.mjs";
 import { string_function_tests_sub } from "../sub.mjs";
 
 export function string_function_tests_name_is(a) {
-    return string_includes(a, string_function_tests_sub(fns));
+    return string_includes(a, string_function_tests_sub());
 }
--- src/string/function/tests/sub.mjs
+++ src/string/function/tests/sub.mjs
@@ -1,6 +1,6 @@
 import { function_name_separator } from "../../../function/name/separator.mjs";
 
-export function string_function_tests_sub(fns) {
+export function string_function_tests_sub() {
     let fns = function_name_separator();
     return `${fns}test${fns}`;
 }
