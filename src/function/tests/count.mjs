00565f1ab6ff12dc05534af68cdb4b1fd66de24d
JaredMathis <jared.mathis@native.consulting>
2023-05-27 18:15:03 -0400

    rename

--- src/function/tests/count.mjs
+++ src/function/tests/count.mjs
@@ -3,7 +3,7 @@ import { directory_read } from "../../directory/read.mjs";
 import { path_join } from "../../path/join.mjs";
 import { function_name_to_path } from "../name/to/path.mjs";
 
-export async function function_test_count(function_name) {
+export async function function_tests_count(function_name) {
     let function_path = function_name_to_path(function_name);
     let test_path = path_join([function_path, 'test']);
     if (!await directory_exists(test_path)) {
--- untested.mjs
+++ untested.mjs
@@ -1,8 +1,8 @@
 import { function_name_all } from "./src/function/name/all.mjs";
-import { function_test_count } from "./src/function/test/count.mjs";
+import { function_tests_count } from "./src/function/tests/count.mjs";
 
 for (let function_name of await function_name_all()) {
-    if (function_test_count(function_name) === 0) {
+    if (function_tests_count(function_name) === 0) {
         log(function_name);
     }
 }
