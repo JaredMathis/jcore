32969028f76b019824bcce8989e151193fdef824
JaredMathis <jared.mathis@native.consulting>
2023-05-27 18:19:56 -0400

    refactor

--- src/string/function/test/name/is.mjs
+++ src/string/function/test/name/is.mjs
@@ -2,7 +2,11 @@ import { function_name_separator } from "../../../../function/name/separator.mjs
 import { string_includes } from "../../../includes.mjs";
 
 export function string_function_tests_name_is(a) {
+    return string_includes(a, string_function_tests_sub(fns));
+}
+
+function string_function_tests_sub(fns) {
     let fns = function_name_separator();
-    return string_includes(a, `${fns}test${fns}`);
+    return `${fns}test${fns}`;
 }
 
