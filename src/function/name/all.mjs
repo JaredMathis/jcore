de994da8f5de383d137363b59c8b10931b672628
JaredMathis <jared.mathis@native.consulting>
2023-05-27 18:16:37 -0400

    progress

--- src/function/name/all.mjs
+++ src/function/name/all.mjs
@@ -3,10 +3,10 @@ import { string_function_test_name_is } from "../../string/function/test/name/is
 import { file_js_all } from "../../file/js/all.mjs";
 
 export async function function_name_all() {
-    let mapped4 = await file_js_all();
-    let filtered2 =  array_filter(
-        mapped4, 
+    let all = await file_js_all();
+    let filtered =  array_filter(
+        all, 
         a => !string_function_test_name_is(a));
-    return filtered2;
+    return filtered;
 }
 
