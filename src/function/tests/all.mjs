19a74840c480554099a1920a79d347287212479f
JaredMathis <jared.mathis@native.consulting>
2023-05-27 19:07:59 -0400

    progress

--- src/function/tests/all.mjs
+++ src/function/tests/all.mjs
@@ -1,6 +1,6 @@
 import { array_filter } from "../../array/filter.mjs";
 import { file_js_all } from "../../file/js/all.mjs";
-import { string_function_tests_name_is } from "../../string/function/test/name/is.mjs";
+import { string_function_tests_name_is } from "../../string/function/tests/name/is.mjs";
 import { string_starts_with } from "../../string/starts/with.mjs";
 import { function_name_separator } from "../name/separator.mjs";
 
@@ -11,6 +11,6 @@ export async function function_tests_all(function_name) {
         a => string_starts_with(a, function_name + function_name_separator()));
     let filtered2 = array_filter(
         filtered, 
-        a => !string_function_tests_name_is(a));
+        a => string_function_tests_name_is(a));
     return filtered2;
 }
--- src/function/tests/count.mjs
+++ src/function/tests/count.mjs
@@ -2,12 +2,11 @@ import { directory_exists } from "../../directory/exists.mjs";
 import { directory_read } from "../../directory/read.mjs";
 import { path_join } from "../../path/join.mjs";
 import { function_name_to_path } from "../name/to/path.mjs";
+import { function_tests_all } from "./all.mjs";
 
 export async function function_tests_count(function_name) {
     let function_path = function_name_to_path(function_name);
-    let test_path = path_join([function_path, 'test']);
-    if (!await directory_exists(test_path)) {
-        return 0;
-    }
-    await directory_read(test_path);
+    let tests = await function_tests_all(function_name);
+
+    console.log({tests})
 }
