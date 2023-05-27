45122d2d07e3e060184ff44b4b461b4e3d226130
JaredMathis <jared.mathis@native.consulting>
2023-05-27 13:01:21 -0400

    comment

--- src/number/is.mjs
+++ src/number/is.mjs
@@ -1,6 +1,6 @@
 import { todo } from "../todo.mjs";
 
 export function number_is(candidate) {
-    todo(`Maybe revisit after learning about JS number nuance`);
+    todo(`Maybe revisit after learning about JS number nuance - what about Infinity, NaN, etc.`);
     return Number.isFinite(candidate);
 }
