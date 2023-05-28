d68a0b2e91b4b9226b0083be26bb6d518598b3d8
JaredMathis <jared.mathis@native.consulting>
2023-05-28 15:25:01 -0400

    #27

--- src/js/parse.mjs
+++ src/js/parse.mjs
@@ -1,6 +1,9 @@
 
 import {Parser} from "acorn";
 export function js_parse(unparsed) {
-    let parsed = Parser.parse(unparsed, {sourceType: 'module'});
+    let parsed = Parser.parse(unparsed, {
+        sourceType: 'module',
+        ecmaVersion:'2020'
+    });
     return parsed;
 }
--- src/refactor/import/missing.mjs
+++ src/refactor/import/missing.mjs
@@ -1,4 +1,5 @@
 import { file_js_parse } from "../../file/js/parse.mjs";
+import { js_identifiers } from "../../js/identifiers.mjs";
 
 export async function refactor_import_missing(file_path) {
     let parsed = await file_js_parse(file_path);
