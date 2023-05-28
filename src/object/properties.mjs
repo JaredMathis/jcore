6e345b3a2d307267dd4786e6ee6035ea1b0597ab
JaredMathis <jared.mathis@native.consulting>
2023-05-28 16:02:22 -0400

    #27

--- src/object/properties.mjs
+++ src/object/properties.mjs
@@ -3,6 +3,10 @@ import {object_property_get} from '../object/property/get.mjs'
 export function object_properties(object) {
     let property_name = 'getOwnPropertyNames';
     let default_value = [];
+    return object_property_invoke(default_value, object, property_name);
+}
+
+function object_property_invoke(default_value, object, property_name) {
     let result = default_value;
     if (object_property_exists(object, property_name)) {
         result = object_property_get(object, property_name)();
