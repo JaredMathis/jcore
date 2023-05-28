8ebe2109093ee4b2e8711f46abe2ff00c967db0f
JaredMathis <jared.mathis@native.consulting>
2023-05-28 15:11:47 -0400

    #21 refactor

--- src/file/js/parse.mjs
+++ src/file/js/parse.mjs
@@ -1,11 +1,11 @@
 import { file_read } from "../read.mjs";
-const {Parser} = require("acorn")
 
 export function file_js_parse(file_path) {
     let unparsed = await file_read(file_path);
     return js_parse(unparsed);
 }
 
+const {Parser} = require("acorn")
 function js_parse(unparsed) {
     let parsed = Parser.parse(unparsed);
     return parsed;
