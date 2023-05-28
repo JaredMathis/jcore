bbe127cb78cc1468a9226ca8bbd0bf16b4e9fcb6
JaredMathis <jared.mathis@native.consulting>
2023-05-28 14:59:45 -0400

    change

--- src/command/line.mjs
+++ src/command/line.mjs
@@ -2,12 +2,13 @@ import {exec} from 'child_process';
 export function command_line(command) {
     return new Promise(function(resolve, reject) {
         exec(command, (error, stdout, stderr) => {
+            let result = {error, stdout, stderr}
             if (error) {
-                reject(error);
+                reject(result);
                 return;
             }
 
-            resolve(stdout.trim());
+            resolve(result);
         });
     });
 }
