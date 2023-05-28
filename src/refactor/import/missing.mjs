55009d48d9f91ca9eee450f34aa9b82e552a951b
JaredMathis <jared.mathis@native.consulting>
2023-05-28 17:15:50 -0400

    #27

--- src/refactor/import/missing.mjs
+++ src/refactor/import/missing.mjs
@@ -2,6 +2,8 @@ import { file_js_parse } from "../../file/js/parse.mjs";
 import { js_identifiers } from "../../js/identifiers.mjs";
 import { js_import_all } from "../../js/import/all.mjs";
 import { function_name_all } from "../../function/name/all.mjs";
+import { object_property_get } from "../../object/property/get.mjs";
+import { js_node_is_type } from "../../js/node/is/type.mjs";
 
 export async function refactor_import_missing(file_path) {
     let parsed = await file_js_parse(file_path);
@@ -10,8 +12,13 @@ export async function refactor_import_missing(file_path) {
     let function_names = await function_name_all();
 
     for (let i of import_all) {
+        if (js_node_is_import_specifier(i)) {
 
-        console.log(i.specifiers)
+        }
     }
 
+}
+
+function js_node_is_import_specifier(i) {
+    return js_node_is_type(i, 'ImportSpecifier');
 }
