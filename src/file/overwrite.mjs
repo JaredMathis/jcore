def8e938d4f04c3727207bc89c897fed300062a7
JaredMathis <jared.mathis@native.consulting>
2023-05-28 14:37:40 -0400

    refactor

--- src/file/overwrite.mjs
+++ src/file/overwrite.mjs
@@ -5,9 +5,14 @@ import { path_join } from '../path/join.mjs';
 import { array_last_remove } from '../array/last/remove.mjs';
 
 export async function file_overwrite(file_path, contents) {
-    let split = file_path_split(file_path);
-    array_last_remove(split);
-    let directory_path = path_join(split)
+    let directory_path = path_parent(file_path);
     await directory_exists_ensure(directory_path);
     await fs.promises.writeFile(file_path, contents);
+}
+
+function path_parent(file_path) {
+    let split = file_path_split(file_path);
+    array_last_remove(split);
+    let directory_path = path_join(split);
+    return directory_path;
 }
