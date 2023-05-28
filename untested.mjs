bc33b31b53d61c2ec9b9f1a1bceb9b64150d5963
JaredMathis <jared.mathis@native.consulting>
2023-05-27 23:43:05 -0400

    refactor

--- untested.mjs
+++ untested.mjs
@@ -2,7 +2,8 @@ import { function_name_all } from "./src/function/name/all.mjs";
 import { function_tests_count } from "./src/function/tests/count.mjs";
 import { log } from "./src/log.mjs";
 
-for (let function_name of await function_name_all()) {
+const function_names = await function_name_all();
+for (let function_name of function_names) {
     if (await function_tests_count(function_name) === 0) {
         log(function_name);
     }
