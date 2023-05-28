bb2e2a950097038f6bc5895dc7553685f7295c30
JaredMathis <jared.mathis@native.consulting>
2023-05-27 23:52:31 -0400

    progress

--- untested.mjs
+++ untested.mjs
@@ -4,11 +4,12 @@ import { function_name_all } from "./src/function/name/all.mjs";
 import { function_tests_count } from "./src/function/tests/count.mjs";
 import { log } from "./src/log.mjs";
 
+let maximum_untested_display = 10;
 const function_names = await function_name_all();
 let filtered = await array_filter_async(
     function_names, 
     async f =>  await function_tests_count(f) === 0);
-let limited = array_take(filtered, 10);
+let limited = array_take(filtered, maximum_untested_display);
 for (let function_name of limited) {
     log(function_name);
 }
