951715d753812163e81d01e6e0d1cb86b302b8b5
JaredMathis <jared.mathis@native.consulting>
2023-05-28 11:43:24 -0400

    refactor

--- run.mjs
+++ run.mjs
@@ -1,6 +1,7 @@
 import { array_first } from "./src/array/first.mjs";
 import { array_skip } from "./src/array/skip.mjs";
 import { array_skip_first } from "./src/array/skip/first.mjs";
+import { directory_separator } from "./src/directory/separator.mjs";
 import { function_name_to_path } from "./src/function/name/to/path.mjs";
 import { log } from "./src/log.mjs"
 
@@ -11,7 +12,8 @@ let function_name = array_first(skipped);
 let remaining = array_skip_first(skipped);
 
 let function_path = function_name_to_path(function_name);
-let imported = await import(function_path);
+let replaced = string_replace(function_path, directory_separator(), '/');
+let imported = await import(replaced);
 let imported_function = imported[function_name];
 let result = await imported_function(...remaining);
 
---
+++ src/string/replace.mjs
@@ -0,0 +1,3 @@
+export function string_replace(input, from, to) {
+    return input.replaceAll(from, to);
+}
