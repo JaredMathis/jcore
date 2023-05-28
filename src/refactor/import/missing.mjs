4eb854f0d368abfca29c02a77a7abc337c44154a
JaredMathis <jared.mathis@native.consulting>
2023-05-28 15:21:52 -0400

    #27

--- src/refactor/import/missing.mjs
+++ src/refactor/import/missing.mjs
@@ -1,6 +1,6 @@
 import { file_js_parse } from "../../file/js/parse.mjs";
 
-export function refactor_import_missing(file_path) {
+export async function refactor_import_missing(file_path) {
     let parsed = await file_js_parse(file_path);
     let identifiers = js_identifiers(parsed);
     console.log(identifiers)
