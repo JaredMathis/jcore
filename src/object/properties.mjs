0f6d98e2522c665d0fcfbd80b042402a70fa4ac4
JaredMathis <jared.mathis@native.consulting>
2023-05-28 15:52:51 -0400

    #27

--- src/object/properties.mjs
+++ src/object/properties.mjs
@@ -1,9 +1,10 @@
 import {array_add} from '../array/add.mjs'
+import {object_property_get} from '../object/property/get.mjs'
 export function object_properties(object) {
     let result = [];
     for (let property in object) {
         let r = object_property_get(object, property);
         array_add(result, r);
     }
-    return r;
+    return result;
 }
