e966640b3232dfaa3d4bd76e6dc79c8ab7c38fe1
JaredMathis <jared.mathis@native.consulting>
2023-05-28 10:39:01 -0400

    #20

---
+++ run.mjs
@@ -0,0 +1,8 @@
+import { array_skip } from "./src/array/skip.mjs";
+import { array_take } from "./src/array/take.mjs"
+import { log } from "./src/log.mjs"
+
+let args = Array.from(process.argv);
+let skipped = array_skip(args, 2);
+
+log(skipped) 
