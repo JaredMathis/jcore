311b5a76cb5f03a44500ebda4741b095b4c32bb1
JaredMathis <jared.mathis@native.consulting>
2023-05-28 18:57:44 -0400

    #32

--- src/function/run.mjs
+++ src/function/run.mjs
@@ -8,12 +8,12 @@ import { log } from "./src/log.mjs"
 import { directory_current } from "./src/directory/current.mjs";
 import { js_directory_separator } from "./src/js/directory/separator.mjs";
 
-export async function function_run(function_name, remaining) {
+export async function function_run(function_name, args) {
     let function_path = function_name_to_path(function_name);
     let concated = directory_current() + directory_separator() + function_path;
     let replaced = string_replace(concated, directory_separator(), js_directory_separator());
     let imported = await import(replaced);
     let imported_function = imported[function_name];
-    let result = await imported_function(...remaining);
+    let result = await imported_function(...args);
     return result;
 }
