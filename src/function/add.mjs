1a8432cf33779ded9cd6bc10878d1548d1f7b52b
JaredMathis <jared.mathis@native.consulting>
2023-05-28 11:20:46 -0400

    #4

--- src/function/add.mjs
+++ src/function/add.mjs
@@ -1,6 +1,9 @@
+import { file_write } from "../file/write.mjs";
 import { function_name_to_path } from "./name/to/path.mjs";
 
 export function function_add(function_name) {
-    
     let function_path = function_name_to_path(function_name);
+    file_write(function_path, `export function ${function_name}() {
+    
+}`);
 }
