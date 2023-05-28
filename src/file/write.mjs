a1ede8074cbcd385fb1f35bb18135f3121f9b8f1
JaredMathis <jared.mathis@native.consulting>
2023-05-28 10:35:39 -0400

    #3 #4

---
+++ src/file/write.mjs
@@ -0,0 +1,9 @@
+import directory_exists from '../directory/exists.mjs'
+import { error } from '../error.mjs'
+import { file_overwrite } from './overwrite.mjs';
+export function file_write(file_path, contents) {
+    if (await directory_exists(file_path)) {
+        error('file already exists: ' + file_path);
+    }
+    await file_overwrite(file_path, contents);
+}
