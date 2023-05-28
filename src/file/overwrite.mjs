6f2997439acad861f8a048e5e912b3314058a936
JaredMathis <jared.mathis@native.consulting>
2023-05-28 14:27:08 -0400

    #23

--- src/file/overwrite.mjs
+++ src/file/overwrite.mjs
@@ -1,8 +1,12 @@
 import fs from 'fs';
 import { directory_exists_ensure } from '../directory/exists/ensure.mjs';
+import { file_path_split } from '../file/path/split.mjs';
+import { path_join } from '../path_join.mjs';
 
 export async function file_overwrite(file_path, contents) {
-    
+    let split = file_path_split(file_path);
+    array_last_remove(split);
+    let directory_path = path_join(split)
     await directory_exists_ensure(directory_path);
     await fs.promises.writeFile(file_path, contents);
 }
--- src/function/name/to/path.mjs
+++ src/function/name/to/path.mjs
@@ -5,7 +5,7 @@ import { directory_source } from "../../../directory/source.mjs";
 import { string_split } from "../../../string/split.mjs";
 import { function_extension } from "../../extension.mjs";
 import { directory_current } from "../../../directory/current.mjs";
-import { directory_separator } from "../../../directory/separator.mjs";
+import { path_join } from "../../../path/join.mjs";
 
 export function function_name_to_path(function_name) {
     let directory_source_result = directory_source();
@@ -13,6 +13,6 @@ export function function_name_to_path(function_name) {
     array_add_beginning(split, directory_source_result);
     array_add_beginning(split, directory_current());
     array_last_map(split, last => last + function_extension());
-    let joined = array_join(split, directory_separator());
+    let joined = path_join(split);
     return joined
 }
