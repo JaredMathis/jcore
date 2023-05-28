98247fec2c09e3a641ea800710dc140ebedb2e84
JaredMathis <jared.mathis@native.consulting>
2023-05-28 15:58:46 -0400

    #27 refactor

--- src/object/property/get.mjs
+++ src/object/property/get.mjs
@@ -1,3 +1,7 @@
+import { assert } from "../../assert.mjs";
+import { object_property_exists } from "./exists.mjs";
+
 export function object_property_get(object, property_name) {
+    assert(object_property_exists(object, property_name))
     return object[property_name];
 }
