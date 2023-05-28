9c0099f02385ebd0d2d1e354476ba228f7ea3653
JaredMathis <jared.mathis@native.consulting>
2023-05-28 14:40:20 -0400

    #25

--- src/directory/add.mjs
+++ src/directory/add.mjs
@@ -1,4 +1,7 @@
 import fs from 'fs'
+import { directory_exists_ensure } from './exists/ensure.mjs';
 export async function directory_add(directory_path) {
+    let parent = path_parent(directory_path);
+    await directory_exists_ensure(parent);
     await fs.promises.mkdir(directory_path);
 }
