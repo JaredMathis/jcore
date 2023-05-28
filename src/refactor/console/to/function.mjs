fd1555dcfc0e6c37708554ffbc2357ef1f65969f
JaredMathis <jared.mathis@native.consulting>
2023-05-28 15:14:08 -0400

    #21 refactor

--- src/refactor/console/to/function.mjs
+++ src/refactor/console/to/function.mjs
@@ -1,3 +1,4 @@
+import file_js_parse from '../../../file/js/parse.mjs';
 export async function refactor_console_to_function(file_path) {
-    console.log(contents);
+    let parsed = file_js_parse(file_path);
 }
