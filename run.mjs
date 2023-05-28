60b43173aab6da9449d1fba6a53a827b0a31d143
JaredMathis <jared.mathis@native.consulting>
2023-05-28 10:43:44 -0400

    #20

--- run.mjs
+++ run.mjs
@@ -1,11 +1,12 @@
 import { array_first } from "./src/array/first.mjs";
 import { array_skip } from "./src/array/skip.mjs";
+import { array_skip_first } from "./src/array/skip/first.mjs";
 import { log } from "./src/log.mjs"
 
 let args = Array.from(process.argv);
 let skipped = array_skip(args, 2);
 
 let function_name = array_first(skipped);
-
+let remaining = array_skip_first(skipped);
 
 log(skipped) 
