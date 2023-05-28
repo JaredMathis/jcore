62c72677be5677155fd15b3f81df6f0da4871a13
JaredMathis <jared.mathis@native.consulting>
2023-05-28 18:55:27 -0400

    #32

--- src/function/run.mjs
+++ src/function/run.mjs
@@ -7,20 +7,13 @@ import { function_name_to_path } from "./src/function/name/to/path.mjs";
 import { log } from "./src/log.mjs"
 import { directory_current } from "./src/directory/current.mjs";
 import { js_directory_separator } from "./src/js/directory/separator.mjs";
-import { function_run } from "./src/function/run.mjs";
 
-async function run() {
-    let args_command_line = Array.from(process.argv);
-    let skipped = array_skip(args_command_line, 2);
-    
-    let function_name = array_first(skipped);
-    let remaining = array_skip_first(skipped);
-    
-    let result = await function_run(function_name, remaining);
-
-    if (false)array_contains;
-    
-    log(`Run result: ${result}`);
+export async function function_run(function_name, remaining) {
+    let function_path = function_name_to_path(function_name);
+    let concated = directory_current() + directory_separator() + function_path;
+    let replaced = string_replace(concated, directory_separator(), js_directory_separator());
+    let imported = await import(replaced);
+    let imported_function = imported[function_name];
+    let result = await imported_function(...remaining);
+    return result;
 }
-
-run();
