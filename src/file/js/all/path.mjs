216a903bbb8325865a46c3f71e3c3afd369f107e
JaredMathis <jared.mathis@native.consulting>
2023-05-28 19:33:00 -0400

    #34

--- src/file/js/all/path.mjs
+++ src/file/js/all/path.mjs
@@ -15,25 +15,11 @@ import { array_skip_first } from "../../array/skip/first.mjs";
 import { directory_source } from "../../directory/source.mjs";
 import { directory_current } from "../../directory/current.mjs";
 import { file_path_split } from "../path/split.mjs";
-import { file_js_all_path } from "./all/path.mjs";
 
-export async function file_js_all() {
-    let directory_source_result = directory_source();
-    let filtered = await file_js_all_path(directory_source_result);
-    let mapped = array_map(filtered, a => file_path_split(a));
-    let mapped2 = array_map(mapped, a => {
-        assert(array_first(a) === directory_source_result);
-        return array_skip_first(a);
-    });
-    let fns = function_name_separator();
-    let mapped3 = array_map(
-        mapped2,
-        a => array_join(a, fns));
-    let mapped4 = array_map(
-        mapped3,
-        a => string_without_suffix(a, function_extension())
-    );
-    return mapped4;
+export async function file_js_all_path(directory_source_result) {
+    let result = await directory_read(`${directory_current()}${directory_separator()}${directory_source_result}`);
+    let filtered = array_filter(
+        result,
+        a => string_ends_with(a, function_extension()));
+    return filtered;
 }
-
-
