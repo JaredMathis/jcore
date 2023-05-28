8aa05fb8c986a350ae8443ee5b025366a849d155
JaredMathis <jared.mathis@native.consulting>
2023-05-28 18:13:29 -0400

    #27

--- src/js/identifiers.mjs
+++ src/js/identifiers.mjs
@@ -1,6 +1,7 @@
 import { object_properties } from '../object/properties.mjs';
 import { visit_filter } from '../visit/filter.mjs';
 import { js_node_is_identifier } from './node/is/identifier.mjs';
+import { js_node_is } from './node/is.mjs';
 import { array_add } from '../array/add.mjs';
 import { array_is } from '../array/is.mjs';
 import { object_property_get } from '../object/property/get.mjs';
--- src/js/node/is.mjs
+++ src/js/node/is.mjs
@@ -1,5 +1,8 @@
 import { object_property_exists } from "../../object/property/exists.mjs";
 
 export function js_node_is(candidate) {
+    if (!candidate) {
+        return false;
+    }
     return object_property_exists(candidate, 'type')
 }
