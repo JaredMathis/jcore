88e1edff0f42c94d81a0b3fcff2775116b883781
JaredMathis <jared.mathis@native.consulting>
2023-05-28 15:59:10 -0400

    #27 refactor

--- src/object/properties.mjs
+++ src/object/properties.mjs
@@ -2,8 +2,9 @@ import {object_property_exists} from '../object/property/exists.mjs'
 import {object_property_get} from '../object/property/get.mjs'
 export function object_properties(object) {
     let property_name = 'getOwnPropertyNames';
+    let default_value = []
     if (!object_property_exists(object, property_name)) {
-        return [];
+        return default_value;
     }
     return object_property_get(object, property_name)();
 }
