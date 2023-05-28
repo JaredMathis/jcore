58cb47b72e327a4e0e82dc2deab3b904f55bcf89
JaredMathis <jared.mathis@native.consulting>
2023-05-28 16:05:48 -0400

    #27

--- src/object/properties.mjs
+++ src/object/properties.mjs
@@ -1,6 +1,3 @@
-import {object_property_invoke} from '../object/property/invoke.mjs'
 export function object_properties(object) {
-    let property_name = 'getOwnPropertyNames';
-    let default_value = [];
-    return object_property_invoke(default_value, object, property_name);
+    return Object.values(object)
 }
--- src/refactor/import/missing.mjs
+++ src/refactor/import/missing.mjs
@@ -1,10 +1,11 @@
 import { file_js_parse } from "../../file/js/parse.mjs";
 import { js_identifiers } from "../../js/identifiers.mjs";
+import { js_import_all } from "../../js/import/all.mjs";
 
 export async function refactor_import_missing(file_path) {
     let parsed = await file_js_parse(file_path);
     let identifiers = js_identifiers(parsed);
     let import_all = js_import_all(parsed);
-    
+
     console.log(identifiers)
 }
