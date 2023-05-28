b50a9a5d7e015a869f8e1139353132449a59cb26
JaredMathis <jared.mathis@native.consulting>
2023-05-28 16:24:58 -0400

    #27

--- src/visit.mjs
+++ src/visit.mjs
@@ -1,8 +1,5 @@
 export function visit(root, children_get, lambda) {
-    let success = lambda(root);
-    if (!success) {
-        return;
-    }
+    lambda(root);
     let children = children_get(root);
     for (let c of children) {
         visit(c, children_get, lambda);
