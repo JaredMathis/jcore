46f19593603358ff1a686b439dfac65b11f15640
JaredMathis <jared.mathis@native.consulting>
2023-05-29 11:45:44 -0400

    #38

--- src/data/task/finish.mjs
+++ src/data/task/finish.mjs
@@ -1,5 +1,5 @@
 import { file_js_all_map } from '../../file/js/all/map.mjs';
 import { refactor_import_path_fix } from '../../refactor/import/path/fix.mjs';
-export function data_task_finish() {
+export async function data_task_finish() {
     await file_js_all_map(refactor_import_path_fix.name);
 }
