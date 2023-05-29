f348e16443fc7ca3a4f682b9a10bd5c50e5d9f2e
JaredMathis <jared.mathis@native.consulting>
2023-05-29 16:36:09 -0400

    #44

--- src/js/identifier/rename.mjs
+++ src/js/identifier/rename.mjs
@@ -1,11 +1,12 @@
 import { object_property_get } from '../../object/property/get.mjs';
+import { object_property_set } from '../../object/property/set.mjs';
 import { js_visit_nodes_identifier } from '../visit/nodes/identifier.mjs';
 export function js_identifier_rename(args, identifier_name_old, identifier_name_new) {
     let parsed = object_property_get(args, 'parsed');
     js_visit_nodes_identifier(parsed, node => {
         let n = object_property_get(node, 'name');
         if (n === identifier_name_old) {
-            console.log({n})
+            object_property_set(node, 'name', identifier_name_new)
         }
     });
 }
