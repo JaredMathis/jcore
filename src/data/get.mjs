c3f3a80ccba8c427021712f7832c34eea2f2f37e
JaredMathis <jared.mathis@native.consulting>
2023-05-28 21:24:25 -0400

    #35

--- src/data/get.mjs
+++ src/data/get.mjs
@@ -1,8 +1,9 @@
-import { directory_exists } from '../directory/exists.mjs';
+import { json_from } from './json_from.mjs';
+import { path_exists_not } from './path_exists_not.mjs';
 import { file_read } from './file_read.mjs';
 export async function data_get() {
     const file_path = './data.json';
-    if (await directory_exists(file_path)) {
+    if (await path_exists_not(file_path)) {
         
     }
     let json = await file_read(file_path);
