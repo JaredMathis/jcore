a52ee2acb525b58d70d18bdd1ac9d70ea2fd6c6f
JaredMathis <jared.mathis@native.consulting>
2023-05-29 00:59:59 -0400

    #28

--- src/object/each.mjs
+++ src/object/each.mjs
@@ -1,3 +1,8 @@
-export function object_each() {
-    
+import { object_property_get } from "./property/get.mjs";
+
+export function object_each(object, lambda) {
+    for (let key in object) {
+        let value = object_property_get(object, key);
+        lambda(value, key);
+    }
 }
