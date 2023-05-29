ace9dbf9fed60f55cb72a535735367d50a8b25bb
JaredMathis <jared.mathis@native.consulting>
2023-05-29 14:40:13 -0400

    #43

--- src/file/js/path/to/args.mjs
+++ src/file/js/path/to/args.mjs
@@ -1,3 +1,10 @@
-export function file_js_path_to_args() {
-    
+import { file_js_parse } from "../../parse.mjs";
+
+export async function file_js_path_to_args(file_path) {
+    let parsed = await file_js_parse(file_path);
+    let args = {
+        parsed,
+        file_path
+    };
+    return { args, parsed };
 }
