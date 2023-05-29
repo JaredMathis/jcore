9aac673d7b92d2526137d4badd47cf78406b7f15
JaredMathis <jared.mathis@native.consulting>
2023-05-29 16:13:40 -0400

    #44

--- src/file/js/identifier/rename.mjs
+++ src/file/js/identifier/rename.mjs
@@ -1,4 +1,5 @@
 import { file_js_map } from '../map.mjs';
+import { file_js_map_args } from '../map/args.mjs';
 export async function file_js_identifier_rename(file_path, identifier_name_old, identifier_name_new) {
-    await file_js_map(js_identifier_rename.name, file_path);
+    await file_js_map_args(file_path, args => js_identifier_rename(args, identifier_name_old, identifier_name_new));
 }
