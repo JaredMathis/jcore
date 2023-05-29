5aa8b171d9dbf33fbe404f6046f43bc7f6f19a6c
JaredMathis <jared.mathis@native.consulting>
2023-05-29 00:52:53 -0400

    #28

--- src/refactor/import/extra.mjs
+++ src/refactor/import/extra.mjs
@@ -1,9 +1,9 @@
 import { error } from '../../error.mjs';
-import { js_identifiers } from '../../js/identifiers.mjs';
+import { js_identifier_counts } from '../../js/identifier/counts.mjs';
 import { js_import_all_to_function_name } from '../../js/import/all/to/function/name.mjs';
 export async function refactor_import_extra(parsed) {
     let import_name_all = js_import_all_to_function_name(parsed);
-    let identifiers = js_identifiers(parsed);
-    console.log({import_name_all,parsed})
+    let counts = js_identifier_counts(parsed);
+    console.log({import_name_all,counts})
     error();
 }
