c14a4c312fdf7a0200fa9818a55b0c7a6bce9d62
JaredMathis <jared.mathis@native.consulting>
2023-05-29 12:29:14 -0400

    #38

--- src/data/git/commit/message/get.mjs
+++ src/data/git/commit/message/get.mjs
@@ -1,3 +1,8 @@
+import { object_property_get } from "../../../../object/property/get.mjs";
+import { data_get } from "../../../get.mjs";
+import { data_task_id } from "../../../task/id.mjs";
+
 export function data_git_commit_message_get() {
-    
+    let data = await data_get();
+    return object_property_get(data, data_task_id());
 }
