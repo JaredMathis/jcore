9184f2d600eb68d5feffaed00b0b7c1a2c9ddf66
JaredMathis <jared.mathis@native.consulting>
2023-05-29 10:46:56 -0400

    #38

--- src/refactor/import/path/fix.mjs
+++ src/refactor/import/path/fix.mjs
@@ -4,6 +4,7 @@ import { object_property_get } from '../../../object/property/get.mjs';
 import { path_relative_file } from '../../../path/relative/file.mjs';
 import { path_parent } from '../../../path/parent.mjs';
 import { js_node_is_literal } from '../../../js/node/is/literal.mjs';
+import { js_import_path_normalize } from '../../../js/import/path/normalize.mjs';
 export function refactor_import_path_fix(args) {
     let {parsed, file_path} = args;
     let imports = js_import_all_with_function_names(parsed);
@@ -11,6 +12,7 @@ export function refactor_import_path_fix(args) {
         let function_name = object_property_get(iw, 'name');
         let function_path = function_name_to_path(function_name);
         let relative = path_relative_file(file_path, function_path);
+        let normalized = js_import_path_normalize(relative);
         let i = object_property_get(iw, 'import');
         let source = object_property_get(i, 'source');
         if (!js_node_is_literal(source)) {
