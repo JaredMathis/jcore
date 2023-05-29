17f0d021571f9dcec2ee5ea855fb52cfd95e8a6f
JaredMathis <jared.mathis@native.consulting>
2023-05-29 12:52:53 -0400

    no task

--- src/data/git/commit/message/set.mjs
+++ src/data/git/commit/message/set.mjs
@@ -1,5 +1,4 @@
-import { data_git_commit_message_initial } from './initial.mjs';
 import { data_key_value_set } from '../../../key/value/set.mjs';
 export async function data_git_commit_message_set(message) {
-    await data_key_value_set(data_git_commit_message_initial(), message);
+    await data_key_value_set(data_commit_message(), message);
 }
