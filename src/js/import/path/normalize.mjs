d50b3cfe5b741a1a7675750eab93a36e82e9ba0a
JaredMathis <jared.mathis@native.consulting>
2023-05-29 11:11:48 -0400

    #38

--- src/js/import/path/normalize.mjs
+++ src/js/import/path/normalize.mjs
@@ -1,6 +1,7 @@
 import { comment } from "../../../comment.mjs";
 import { directory_separator } from "../../../directory/separator.mjs";
 import { string_replace } from "../../../string/replace.mjs";
+import { string_starts_with } from "../../../string/starts/with.mjs";
 import { js_directory_separator } from "../../directory/separator.mjs";
 
 comment(`path.normalize slashes one way. However import statements in mjs require slashes another way. This function makes imports suitable for import usage in mjs.`)
@@ -9,5 +10,8 @@ export function js_import_path_normalize(import_path) {
         import_path, 
         directory_separator(), 
         js_directory_separator());
+    if (!string_starts_with(result, '.')) {
+        return './' + result;
+    }
     return result;
 }
