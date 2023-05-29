5171db21d1461b67514c375793e9626615de9ff0
JaredMathis <jared.mathis@native.consulting>
2023-05-29 00:48:39 -0400

    #28

--- src/js/identifier/counts.mjs
+++ src/js/identifier/counts.mjs
@@ -7,11 +7,13 @@ import { js_node_is } from "../node/is.mjs";
 import { js_node_is_identifier } from "../node/is/identifier.mjs";
 
 export function js_identifier_counts(parsed) {
-    let result = [];
+    let result = {};
     let body = js_body_get(parsed);
     visit_filter(body, node => object_properties(node), node => js_node_is(node) && js_node_is_identifier(node), node => {
         let r = object_property_get(node, 'name');
-        array_add(result, r);
+        object_property_initialize(result, r, 0);
+        let previous = object_property_get(result, r);
+        object_property_set(result, r, add_1(previous));
     });
     return result;
 }
