267eea52a8884c982c1628bbf47fc35ad12a7a57
JaredMathis <jared.mathis@native.consulting>
2023-05-28 23:48:50 -0400

    #35

--- src/function/run/arguments.mjs
+++ src/function/run/arguments.mjs
@@ -1,10 +1,12 @@
 import { array_first } from "../../array/first.mjs";
 import { array_skip_first } from "../../array/skip/first.mjs";
+import { array_to } from "../../array/to.mjs";
 import { function_run } from "../run.mjs";
 
-export async function function_run_arguments(skipped) {
-    let function_name = array_first(skipped);
-    let remaining = array_skip_first(skipped);
+export async function function_run_arguments(input) {
+    let args = array_to(input);
+    let function_name = array_first(args);
+    let remaining = array_skip_first(args);
 
     let result = await function_run(function_name, remaining);
     return result;
--- src/git.mjs
+++ src/git.mjs
@@ -2,7 +2,6 @@ import { function_run_arguments } from "./function/run/arguments.mjs";
 import { git_acp } from "./git/acp.mjs";
 
 export async function git() {
-    console.log({a:arguments})
     await function_run_arguments(arguments);
     await git_acp();
 }
