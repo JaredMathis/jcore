5d0c5a132303b8ad63091ecfe9cbd1af74b1b533
JaredMathis <jared.mathis@native.consulting>
2023-05-29 09:46:37 -0400

    #38

--- src/refactor/import/path/fix.mjs
+++ src/refactor/import/path/fix.mjs
@@ -1,7 +1,8 @@
+import { function_name_to_path } from '../../../function/name/to/path.mjs';
 import { js_import_all_with_function_names } from '../../../js/import/all/with/function/names.mjs';
 export function refactor_import_path_fix() {
     let imports = js_import_all_with_function_names(parsed);
     for (let i of imports) {
-        
+        let function_path = function_name_to_path(function_name)
     }
 }
