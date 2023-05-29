dbf469f8ca3fd75fa2dfc5885216c2f3e79d7e60
JaredMathis <jared.mathis@native.consulting>
2023-05-29 15:48:55 -0400

    #44

--- src/file/js/all/identifier/exists.mjs
+++ src/file/js/all/identifier/exists.mjs
@@ -1,6 +1,7 @@
 import { array_contains } from "../../../../array/contains.mjs";
 import { error } from "../../../../error.mjs";
 import { js_identifiers } from "../../../../js/identifiers.mjs";
+import { file_js_parse } from "../../parse.mjs";
 import { file_js_all_path } from "../path.mjs";
 
 export async function file_js_all_identifier_exists(identifier_name) {
@@ -8,7 +9,8 @@ export async function file_js_all_identifier_exists(identifier_name) {
 
     let result = [];
     for (let a of all) {
-        let identifiers = await js_identifiers(a);
+        let parsed = await file_js_parse(a);
+        let identifiers = js_identifiers(parsed);
         for (let i of identifiers) {
             array_add_if_not_exists(result, i);
         }
