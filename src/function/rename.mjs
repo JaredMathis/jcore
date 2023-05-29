55c43b06ba23e786536a7f541bc043994082308d
JaredMathis <jared.mathis@native.consulting>
2023-05-29 15:20:16 -0400

    #44

--- src/function/rename.mjs
+++ src/function/rename.mjs
@@ -1,2 +1,7 @@
+import { assert } from "../assert.mjs";
+import { function_exists } from "./exists.mjs";
+
 export function function_rename(function_name_old, function_name_new) {
+    assert(await function_exists(function_name_old));
+    assert(!await function_exists(function_name_new));
 }
