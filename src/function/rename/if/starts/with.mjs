a7986e0b2f08274b56d9aa739d0858c6fb5cab42
JaredMathis <jared.mathis@native.consulting>
2023-05-29 19:49:01 -0400

    #13

--- src/function/rename/if/starts/with.mjs
+++ src/function/rename/if/starts/with.mjs
@@ -1,12 +1,11 @@
+import { file_js_all_identifier_all_rename } from '../../../../file/js/all/identifier/all/rename.mjs';
 import { refactor_import_fix } from '../../../../refactor/import/fix.mjs';
 import { file_js_all_map } from '../../../../file/js/all/map.mjs';
-import { file_js_all_identifier_rename } from '../../../../file/js/all/identifier/rename.mjs';
 import { error } from '../../../../error.mjs';
 import { function_rename_without_all_refactor } from '../../without/all/refactor.mjs';
 import { string_prefix_replace } from '../../../../string/prefix/replace.mjs';
 import { string_starts_with } from '../../../../string/starts/with.mjs';
 import { function_name_all } from '../../../name/all.mjs';
-import { object_property_set } from '../../../../object/property/set.mjs';
 import { object_property_initialize } from '../../../../object/property/initialize.mjs';
 export async function function_rename_if_starts_with(prefix_old, prefix_new) {
     let names = await function_name_all();
@@ -17,8 +16,8 @@ export async function function_rename_if_starts_with(prefix_old, prefix_new) {
             await function_rename_without_all_refactor(n_old, n_new);
             object_property_initialize(dictionary, n_old, n_new);
             error();
-            await file_js_all_identifier_rename(n_old, n_new);
         }
     }
+    await file_js_all_identifier_all_rename(dictionary);
     await file_js_all_map(refactor_import_fix.name);
 }
