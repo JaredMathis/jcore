a27a37c337dc253d53f53c91de5d01042208f0f2
JaredMathis <jared.mathis@native.consulting>
2023-05-29 12:34:40 -0400

    #38

--- src/object/property/get/initialize.mjs
+++ src/object/property/get/initialize.mjs
@@ -1,6 +1,6 @@
 import { object_property_get } from '../get.mjs';
 import { object_property_initialize } from '../initialize.mjs';
-export function object_property_get_initialize(data, property_name) {
+export function object_property_get_initialize(data, property_name, intial_value) {
     object_property_initialize(data, property_name, '');
     return object_property_get(data, property_name);
 }
