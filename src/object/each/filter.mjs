89ffe112787b0752b94b1fa70e9a6b9557d1f42c
JaredMathis <jared.mathis@native.consulting>
2023-05-29 01:01:08 -0400

    #28

--- src/object/each/filter.mjs
+++ src/object/each/filter.mjs
@@ -1,3 +1,11 @@
-export function object_each_filter() {
-    
+import {object_each} from '../each.mjs';
+export function object_each_filter(object, predicate) {
+    let result = [];
+    let lambda = (value, key) => {
+        if (predicate(value)) {
+            array_add(result, value);
+        }
+    }
+    object_each(object, lambda);
+    return result;
 }
--- src/refactor/import/extra.mjs
+++ src/refactor/import/extra.mjs
@@ -2,6 +2,7 @@ import { array_add } from '../../array/add.mjs';
 import { error } from '../../error.mjs';
 import { js_identifier_counts } from '../../js/identifier/counts.mjs';
 import { js_import_all_to_function_name } from '../../js/import/all/to/function/name.mjs';
+import { object_each_filter } from '../../object/each/filter.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 export async function refactor_import_extra(parsed) {
     let import_name_all = js_import_all_to_function_name(parsed);
@@ -17,15 +18,5 @@ export async function refactor_import_extra(parsed) {
     error();
 }
 
-function object_each_filter(object, predicate) {
-    let result = [];
-    let lambda = (value, key) => {
-        if (predicate(value)) {
-            array_add(result, value);
-        }
-    }
-    object_each(object, lambda);
-    return result;
-}
 
 
