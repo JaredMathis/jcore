4ccb6c506b45c74227c78ff1053f242e5c0b659b
JaredMathis <jared.mathis@native.consulting>
2023-05-29 00:27:34 -0400

    #28

--- src/function/map.mjs
+++ src/function/map.mjs
@@ -1,4 +1,5 @@
 import {file_js_map} from '../file/js/map.mjs'
 export async function function_map(function_name_mapper, function_name) {
+    let file_path = function_name_to_path(function_name);
     await file_js_map(function_name_mapper, file_path);
 }
