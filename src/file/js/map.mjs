e94aff9b7781f758ae24e01d0856feb89cb217e0
JaredMathis <jared.mathis@native.consulting>
2023-05-29 16:04:26 -0400

    #44

--- src/file/js/map.mjs
+++ src/file/js/map.mjs
@@ -3,6 +3,7 @@ import { js_unparse } from '../../js/unparse.mjs';
 import { function_run } from '../../function/run.mjs';
 import { file_js_path_to_args } from './path/to/args.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
+import { file_js_overwrite } from './overwrite.mjs';
 export async function file_js_map(function_name_mapper, file_path) {
     let args = await file_js_path_to_args(file_path);
     await function_run(function_name_mapper, [args]);
--- src/file/js/overwrite.mjs
+++ src/file/js/overwrite.mjs
@@ -1,3 +1,7 @@
+import { js_unparse } from "../../js/unparse.mjs";
+import { object_property_get } from "../../object/property/get.mjs";
+import { file_overwrite } from "../overwrite.mjs";
+
 export async function file_js_overwrite(args) {
     let file_path = object_property_get(args, 'file_path');
     let parsed = object_property_get(args, 'parsed');
