076cff0146e3b7fe0db8b8fe7d395faf77919e73
JaredMathis <jared.mathis@native.consulting>
2023-05-29 15:27:38 -0400

    #44

--- src/function/rename.mjs
+++ src/function/rename.mjs
@@ -1,4 +1,6 @@
 import { assert } from "../assert.mjs";
+import { file_js_all_map } from "../file/js/all/map.mjs";
+import { refactor_import_fix } from "../refactor/import/fix.mjs";
 import { function_exists } from "./exists.mjs";
 import { function_name_to_path } from "./name/to/path.mjs";
 
@@ -8,5 +10,6 @@ export async function function_rename(function_name_old, function_name_new) {
     assert(!await file_js_all_identifier_exists(function_name_new));
     let file_path_old = function_name_to_path(function_name_old);
     let file_path_new = function_name_to_path(function_name_new);
-    await file_rename(file_path_old, file_path_new)
+    await file_rename(file_path_old, file_path_new);
+    await file_js_all_map(refactor_import_fix.name);
 }
