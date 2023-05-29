e308743288744080d23cb0eae2239917daf54758
JaredMathis <jared.mathis@native.consulting>
2023-05-29 01:02:43 -0400

    #28

--- src/array/intersection.mjs
+++ src/array/intersection.mjs
@@ -1,3 +1,6 @@
-export function array_intersection() {
-    
+import { array_contains } from "./contains.mjs";
+import { array_filter } from "./filter.mjs";
+
+export function array_intersection(left, right) {
+    return array_filter(left, l => array_contains(right, l));
 }
