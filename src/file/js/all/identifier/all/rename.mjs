7d74593ac783da90811992e31a69f77363f98c32
JaredMathis <jared.mathis@native.consulting>
2023-05-29 19:00:38 -0400

    #13

--- src/file/js/all/identifier/all/rename.mjs
+++ src/file/js/all/identifier/all/rename.mjs
@@ -1,6 +1,7 @@
 import { object_each } from '../../../../../object/each.mjs';
 export function file_js_all_identifier_all_rename(dictionary) {
-    
+    let keys = object_keys(dictionary);
+    let values = object_values(dictionary);
 
     object_each(dictionary, (to, from) => {
 
