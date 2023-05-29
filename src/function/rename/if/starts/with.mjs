143864621d1afbf99b5cb7ac4556251254c0f243
JaredMathis <jared.mathis@native.consulting>
2023-05-29 18:31:30 -0400

    #13

--- src/function/rename/if/starts/with.mjs
+++ src/function/rename/if/starts/with.mjs
@@ -5,7 +5,7 @@ export function function_rename_if_starts_with(prefix_old, prefix_new) {
     let names = await function_name_all();
     for (let n of names) {
         if (string_starts_with(n, prefix_old)) {
-            
+            let n_new = string_prefix_replace(n, prefix_old, prefix_new);
         }
     }
 }
