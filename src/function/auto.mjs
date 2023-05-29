eb75d192724907726a0a627ee3513f5b6cc1e5ce
JaredMathis <jared.mathis@native.consulting>
2023-05-29 14:40:33 -0400

    #43

--- src/function/auto.mjs
+++ src/function/auto.mjs
@@ -1,5 +1,5 @@
 import { refactor_import_fix } from "../refactor/import/fix.mjs";
 
-export function function_auto(function_name) {
+export async function function_auto(function_name) {
     await refactor_import_fix(args);
 }
