96d83a9c5e8fd7b53a2a04a606d19acc0693938d
JaredMathis <jared.mathis@native.consulting>
2023-05-29 16:25:13 -0400

    #44

--- src/js/identifier/rename.mjs
+++ src/js/identifier/rename.mjs
@@ -1,3 +1,4 @@
+import { object_property_get } from '../../object/property/get.mjs';
 export function js_identifier_rename(args, identifier_name_old, identifier_name_new) {
     let parsed = object_property_get(args, 'parsed');
 }
