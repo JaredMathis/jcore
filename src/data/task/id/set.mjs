bad78274e0eb687a6084a6e42badce682ec402ca
JaredMathis <jared.mathis@native.consulting>
2023-05-28 21:37:58 -0400

    #35

---
+++ src/data/task/id/set.mjs
@@ -0,0 +1,7 @@
+import { data_get } from "../../get.mjs";
+import { data_task_id } from "../id.mjs";
+
+export async function data_task_id_set(task_id) {
+    let data = await data_get();
+    object_property_set(data, data_task_id(), task_id);
+}
---
+++ src/object/property/set.mjs
@@ -0,0 +1,3 @@
+export function object_property_set(object, property_name, value) {
+    object[property_name] = value;
+}
