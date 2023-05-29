9ea51de18b4e18aa22d2b89365dddfe479929644
JaredMathis <jared.mathis@native.consulting>
2023-05-29 16:03:13 -0400

    #44

--- src/file/js/map.mjs
+++ src/file/js/map.mjs
@@ -9,9 +9,4 @@ export async function file_js_map(function_name_mapper, file_path) {
     await file_js_overwrite(args);
 }
 
-async function file_js_overwrite(args) {
-    let file_path = object_property_get(args, 'file_path');
-    let parsed = object_property_get(args, 'parsed');
-    let unparsed = js_unparse(parsed);
-    await file_overwrite(file_path, unparsed);
-}
+
--- src/file/js/overwrite.mjs
+++ src/file/js/overwrite.mjs
@@ -1,2 +1,6 @@
-export function file_js_overwrite() {
+export async function file_js_overwrite(args) {
+    let file_path = object_property_get(args, 'file_path');
+    let parsed = object_property_get(args, 'parsed');
+    let unparsed = js_unparse(parsed);
+    await file_overwrite(file_path, unparsed);
 }
