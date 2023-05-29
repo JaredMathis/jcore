2ca88ba003e844ed2d8ac4acac22567941d5a3de
JaredMathis <jared.mathis@native.consulting>
2023-05-29 00:46:18 -0400

    #28

--- src/js/identifier/counts.mjs
+++ src/js/identifier/counts.mjs
@@ -1,3 +1,9 @@
-export function js_identifier_counts() {
-    
+export function js_identifier_counts(parsed) {
+    let result = [];
+    let body = js_body_get(parsed);
+    visit_filter(body, node => object_properties(node), node => js_node_is(node) && js_node_is_identifier(node), node => {
+        let r = object_property_get(node, 'name');
+        array_add(result, r);
+    });
+    return result;
 }
