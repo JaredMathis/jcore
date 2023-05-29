85af30541c87439fd8aebc039bda0068b8b49de5
JaredMathis <jared.mathis@native.consulting>
2023-05-29 14:58:44 -0400

    #43

--- src/function/auto.mjs
+++ src/function/auto.mjs
@@ -1,10 +1,11 @@
+import { function_exists } from './exists.mjs';
 import { function_add } from './add.mjs';
 import { function_map } from './map.mjs';
 import { refactor_import_fix } from '../refactor/import/fix.mjs';
 export async function function_auto(function_name) {
     if (false) {
         if (!await function_exists(function_name)) {
-            function_add(function_name);
+            await function_add(function_name);
         }
     }
     await function_map(refactor_import_fix.name, function_name);
