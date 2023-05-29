fb773443bedce708c4b649e9c95b7fa3114a9d34
JaredMathis <jared.mathis@native.consulting>
2023-05-29 18:29:15 -0400

    #13

--- src/function/rename/if/starts/with.mjs
+++ src/function/rename/if/starts/with.mjs
@@ -1,8 +1,11 @@
+import { string_starts_with } from "../../../../string/starts/with.mjs";
 import { function_name_all } from "../../../name/all.mjs";
 
 export function function_rename_if_starts_with(prefix_old, prefix_new) {
     let names = await function_name_all();
     for (let n of names) {
-        
+        if (string_starts_with(n, prefix_old)) {
+            
+        }
     }
 }
