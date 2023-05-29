e1bf98246e2d4c4708c1808a723b78922daad350
JaredMathis <jared.mathis@native.consulting>
2023-05-29 15:49:37 -0400

    #44

--- src/array/add/if/not/exists.mjs
+++ src/array/add/if/not/exists.mjs
@@ -1,2 +1,8 @@
-export function array_add_if_not_exists() {
+import { array_add } from '../../../add.mjs';
+import { array_contains } from '../../../contains.mjs';
+export function array_add_if_not_exists(list, element) {
+    if (array_contains(list, element)) {
+        return;
+    }
+    array_add(list, element);
 }
