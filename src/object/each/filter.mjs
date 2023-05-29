e614fde336a8b74fa665ef91010e09537460eb0f
JaredMathis <jared.mathis@native.consulting>
2023-05-29 01:05:42 -0400

    #28

--- src/object/each/filter.mjs
+++ src/object/each/filter.mjs
@@ -4,7 +4,7 @@ export function object_each_filter(object, predicate) {
     let result = [];
     let lambda = (value, key) => {
         if (predicate(value, key)) {
-            array_add(result, value);
+            array_add(result, key);
         }
     }
     object_each(object, lambda);
