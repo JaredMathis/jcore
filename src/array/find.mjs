bdbbd5ee60b2cabf8a64ec63fbff15f5c95bb262
JaredMathis <jared.mathis@native.consulting>
2023-05-29 08:31:08 -0400

    #28

--- src/array/find.mjs
+++ src/array/find.mjs
@@ -1,3 +1,6 @@
+import { array_filter } from "./filter.mjs";
+
 export function array_find(list, predicate) {
-    
+    let filtered = array_filter(list, predicate);
+    return array_single(filtered);
 }
