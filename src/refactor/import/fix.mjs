25713b7b3577770816e386e4af59f6ec5e80ae95
JaredMathis <jared.mathis@native.consulting>
2023-05-29 11:06:05 -0400

    #38

--- src/refactor/import/fix.mjs
+++ src/refactor/import/fix.mjs
@@ -1,4 +1,11 @@
+import { refactor_import_path_fix } from "./path/fix.mjs";
+
 export function refactor_import_fix(args) {
     let {parsed, file_path} = args;
-    
+
+    let refactors = [
+        refactor_import_extra,
+        refactor_import_missing,
+        refactor_import_path_fix,
+    ]
 }
