3b2fdced8d41af13cc288b462787432ea5ebb36f
JaredMathis <jared.mathis@native.consulting>
2023-05-29 12:35:43 -0400

    #38

--- src/data/git/commit/message/get.mjs
+++ src/data/git/commit/message/get.mjs
@@ -4,6 +4,10 @@ import { data_get } from '../../../get.mjs';
 export async function data_git_commit_message_get() {
     let data = await data_get();
     const property_name = data_commit_message();
-    const intial_value = '';
+    const intial_value = data_git_commit_message_initial();
     return object_property_get_initialize(data, property_name, intial_value);
+}
+
+function data_git_commit_message_initial() {
+    return '';
 }
