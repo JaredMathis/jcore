0d455f7aeb8ee590fab2be240f006dbcd175228e
JaredMathis <jared.mathis@native.consulting>
2023-05-29 08:48:43 -0400

    #28

--- src/array/remove/at.mjs
+++ src/array/remove/at.mjs
@@ -1,3 +1,6 @@
-export function array_remove_at() {
-    
+import { assert } from "../../assert.mjs";
+
+export function array_remove_at(list, index) {
+    assert(array_index_valid(list, index));
+    list.splice(index, 1);
 }
