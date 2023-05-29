255074ea0a4a466520ff2b951a551a93cf2498c2
JaredMathis <jared.mathis@native.consulting>
2023-05-29 14:48:31 -0400

    #43

--- src/data/task/id/remove.mjs
+++ src/data/task/id/remove.mjs
@@ -1,3 +1,6 @@
+import { data_task_id } from '../id.mjs';
+import { object_property_get } from '../../../object/property/get.mjs';
+import { data_get } from '../../get.mjs';
 export async function data_task_id_remove() {
     let data = await data_get();
     return object_property_get(data, data_task_id());
--- src/function/auto.mjs
+++ src/function/auto.mjs
@@ -1,10 +1,11 @@
+import { file_js_all_map } from "../file/js/all/map.mjs";
 import { file_js_path_to_args } from "../file/js/path/to/args.mjs";
 import { refactor_import_fix } from "../refactor/import/fix.mjs";
 import { function_name_to_path } from "./name/to/path.mjs";
 
 export async function function_auto(function_name) {
-    console.log({function_name})
     let function_path = function_name_to_path(function_name);
     let args = await file_js_path_to_args(function_path);
     await refactor_import_fix(args);
+    await function_map(refactor_import_fix.name, )
 }
