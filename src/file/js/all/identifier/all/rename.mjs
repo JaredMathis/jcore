eb00c3ffb7af29220aa7ac8955ecb283b237cc6e
JaredMathis <jared.mathis@native.consulting>
2023-05-29 19:02:46 -0400

    #13

--- src/file/js/all/identifier/all/rename.mjs
+++ src/file/js/all/identifier/all/rename.mjs
@@ -1,7 +1,12 @@
+import { assert } from '../../../../../assert.mjs';
 import { object_each } from '../../../../../object/each.mjs';
 export function file_js_all_identifier_all_rename(dictionary) {
     let keys = object_keys(dictionary);
     let values = object_values(dictionary);
+
+    for (let list of [keys, values]) {
+        assert(array_unique_is(list))
+    }
 
     object_each(dictionary, (to, from) => {
 
