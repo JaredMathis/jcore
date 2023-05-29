a065512455b4a691620812c07b735e2064c26383
JaredMathis <jared.mathis@native.consulting>
2023-05-29 16:27:56 -0400

    #44

--- src/js/visit.mjs
+++ src/js/visit.mjs
@@ -1,5 +1,7 @@
+import { object_properties } from '../object/properties.mjs';
+import { visit_filter } from '../visit/filter.mjs';
+import { js_body_get } from './body/get.mjs';
 export function js_visit(parsed) {
     let body = js_body_get(parsed);
-    visit_filter(
-        body, node => object_properties(node), filter, lambda);
+    visit_filter(body, node => object_properties(node), filter, lambda);
 }
