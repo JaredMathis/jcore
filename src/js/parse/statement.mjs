1ff19e2d6f1ac9bbccee8fddf692e8f666ff4ffe
JaredMathis <jared.mathis@native.consulting>
2023-05-29 08:33:20 -0400

    #28

--- src/js/parse/statement.mjs
+++ src/js/parse/statement.mjs
@@ -1,4 +1,4 @@
-import { array_first } from '../../array/first.mjs';
+import { array_single } from '../../array/single.mjs';
 import { array_length_is_1 } from '../../array/length/is/1.mjs';
 import { assert } from '../../assert.mjs';
 import { js_body_get } from '../body/get.mjs';
@@ -6,7 +6,6 @@ import { js_parse } from '../parse.mjs';
 export function js_parse_statement(statement_code) {
     let parsed = js_parse(statement_code);
     let body = js_body_get(parsed);
-    assert(array_length_is_1(body, 1));
-    let statement = array_first(body);
+    let statement = array_single(body);
     return statement;
 }
