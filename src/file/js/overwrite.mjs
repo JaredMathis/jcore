6f16aeaca2553db904bf77416182ff610f336fc2
JaredMathis <jared.mathis@native.consulting>
2023-05-29 16:24:34 -0400

    #44

--- src/file/js/overwrite.mjs
+++ src/file/js/overwrite.mjs
@@ -5,6 +5,5 @@ export async function file_js_overwrite(args) {
     let file_path = object_property_get(args, 'file_path');
     let parsed = object_property_get(args, 'parsed');
     let unparsed = js_unparse(parsed);
-    console.log(unparsed)
     await file_overwrite(file_path, unparsed);
 }
