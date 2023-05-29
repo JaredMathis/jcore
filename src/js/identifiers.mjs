c27ed8ad8f8dfde10cc7647f29636eb758978426
JaredMathis <jared.mathis@native.consulting>
2023-05-29 09:37:04 -0400

    #38

--- src/js/identifiers.mjs
+++ src/js/identifiers.mjs
@@ -1,4 +1,5 @@
 import { js_identifier_counts } from './identifier/counts.mjs';
+import { object_keys } from '../object/keys.mjs';
 export function js_identifiers(parsed) {
     let counts = js_identifier_counts(parsed);
     let keys = object_keys(counts);
