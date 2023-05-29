1fd7434f700efa6ec7a72d538c9073d840c72893
JaredMathis <jared.mathis@native.consulting>
2023-05-29 17:14:16 -0400

    #44

--- src/file/rename.mjs
+++ src/file/rename.mjs
@@ -4,6 +4,6 @@ import fs from 'fs';
 export async function file_rename(file_path_old, file_path_new) {
     assert(await directory_exists(file_path_old));
     assert(!await directory_exists(file_path_new));
-   await path_parent_exists_ensure(file_path_new)
+   if (false)await path_parent_exists_ensure(file_path_new)
     await fs.promises.rename(file_path_old, file_path_new);
 }
