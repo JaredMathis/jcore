fff556f4eff45d789d1627c08bfb85c0dc687177
JaredMathis <jared.mathis@native.consulting>
2023-05-29 08:45:38 -0400

    #28

--- src/array/remove.mjs
+++ src/array/remove.mjs
@@ -1,3 +1,6 @@
-export function array_remove() {
-    
+export function array_remove(list, element) {
+    let index = array_index_of(list, element);
+    if (index >= 0) {
+        array_remove_at(list, index);
+    }
 }
