94285adf0316717003f21c52967efb1454d35daa
JaredMathis <jared.mathis@native.consulting>
2023-05-29 12:34:21 -0400

    #38

--- src/git/acp.mjs
+++ src/git/acp.mjs
@@ -5,11 +5,9 @@ import { data_get } from '../data/get.mjs';
 import { data_task_id } from '../data/task/id.mjs';
 export async function git_acp() {
     let data = await data_get();
-    let commit_message;
+    let commit_message = data_git_commit_message_get();
     let task_id = object_property_get(data, data_task_id());
     commit_message = task_id;
-    if (false)
-        data_git_commit_message_get();
     let commands = [
         `git add *`,
         `git commit -m "${ commit_message }"`,
