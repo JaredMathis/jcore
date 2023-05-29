b24c6a9a915e2ca38ca784ac2fd9d1df5b6d9cbb
JaredMathis <jared.mathis@native.consulting>
2023-05-29 11:49:17 -0400

    #38

--- src/data/task/next.mjs
+++ src/data/task/next.mjs
@@ -1,5 +1,5 @@
-import { data_task_finish } from "./finish.mjs";
-
+import { data_task_id_set_hash } from './id/set/hash.mjs';
+import { data_task_finish } from './finish.mjs';
 export async function data_task_next(task_id) {
     await data_task_finish();
     await data_task_id_set_hash(task_id);
