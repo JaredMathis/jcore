b6f50a9be8b8da07c151af49cadfdca87814a3f3
JaredMathis <jared.mathis@native.consulting>
2023-05-29 19:53:08 -0400

    #13

--- src/file/js/all/identifier/all/rename.mjs
+++ src/file/js/all/identifier/all/rename.mjs
@@ -5,7 +5,7 @@ import { object_keys } from '../../../../../object/keys.mjs';
 import { assert } from '../../../../../assert.mjs';
 import { object_each } from '../../../../../object/each.mjs';
 import { file_js_map_args } from '../../../map/args.mjs';
-export function file_js_all_identifier_all_rename(dictionary) {
+export async function file_js_all_identifier_all_rename(dictionary) {
     let keys = object_keys(dictionary);
     let values = object_properties(dictionary);
     const concats = [
@@ -14,7 +14,9 @@ export function file_js_all_identifier_all_rename(dictionary) {
     ];
     let concat = array_all_combine(concats);
     assert(array_unique_is(concat));
-    file_js_map_args()
-    object_each(dictionary, (to, from) => {
+    await file_js_all_map_args(function mapper(args) {
+        object_each(dictionary, (to, from) => {
+            js_identifier_rename(args, from, to);
+        });
     });
 }
