91d970ee01e74396518fe5072459c18c8fb1b2b5
JaredMathis <jared.mathis@native.consulting>
2023-05-29 10:03:42 -0400

    #38

--- src/file/js/map.mjs
+++ src/file/js/map.mjs
@@ -4,7 +4,7 @@ import { js_unparse } from '../../js/unparse.mjs';
 import { function_run } from '../../function/run.mjs';
 export async function file_js_map(function_name_mapper, file_path) {
     let parsed = await file_js_parse(file_path);
-    let args = {parsed};
+    let args = {parsed, file_path};
     await function_run(function_name_mapper, [args]);
     let unparsed = js_unparse(parsed);
     file_overwrite(file_path, unparsed);
