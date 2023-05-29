58587b9dae82416a579e36e6bbea2a29d7b13b87
JaredMathis <jared.mathis@native.consulting>
2023-05-29 18:33:54 -0400

    #13

--- src/string/prefix/replace.mjs
+++ src/string/prefix/replace.mjs
@@ -1,8 +1,9 @@
+import { string_prefix_without } from './without.mjs';
 import { assert } from '../../assert.mjs';
 import { todo } from '../../todo.mjs';
 import { string_starts_with } from '../starts/with.mjs';
-export function string_prefix_replace(n, prefix_old, prefix_new) {
-    assert(string_starts_with(n, prefix_old));
-    let prefix_without = string_prefix_without(n, prefix_old);
+export function string_prefix_replace(input, prefix_old, prefix_new) {
+    assert(string_starts_with(input, prefix_old));
+    let prefix_without = string_prefix_without(input, prefix_old);
     todo();
 }
