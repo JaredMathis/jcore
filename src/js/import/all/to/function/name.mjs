eec0aec862467e474b9d0882f7cb5cb41fd36906
JaredMathis <jared.mathis@native.consulting>
2023-05-29 08:18:14 -0400

    #28

--- src/js/import/all/to/function/name.mjs
+++ src/js/import/all/to/function/name.mjs
@@ -11,45 +11,10 @@ import { js_node_is_identifier } from "../../../../node/is/identifier.mjs";
 import { js_node_is_import_specifier } from "../../../../node/is/import/specifier.mjs";
 import { js_node_is_literal } from "../../../../node/is/literal.mjs";
 import { js_import_all } from "../../../all.mjs";
+import { js_import_all_with_function_names } from "../../with/function/names.mjs";
 
 export function js_import_all_to_function_name(parsed) {
-    let import_all = js_import_all(parsed);
-    let import_name_all = [];
-    for (let i of import_all) {
-        let source = object_property_get(i, 'source');
-        if (!js_node_is_literal(source)) {
-            continue;
-        }
-        let source_value = object_property_get(source, 'value');
-        const prefix = directory_current();
-        if (!string_starts_with(source_value, prefix)) {
-            continue;
-        }
-        let specifiers = object_property_get(i, 'specifiers');
-        if (!array_length_is_1(specifiers)) {
-            continue;
-        }
-        let specifier = array_first(specifiers);
-        if (!js_node_is_import_specifier(specifier)) {
-            continue;
-        }
-        let properties = [
-            'imported',
-            'local'
-        ];
-        let values = array_map(
-            properties, p => object_property_get(specifier, p));
-        if (array_any(values, v => !js_node_is_identifier(v))) {
-            continue;
-        }
-        let first = array_first(values);
-        let first_name = object_property_get(first, 'name');
-        if (array_any(values, v => object_property_get(v, 'name') !== first_name)) {
-            continue;
-        }
-        array_add(import_name_all, first_name);
-    }
-    return import_name_all;
+    return js_import_all_with_function_names(parsed);
 }
 
 
