2c693604cd72cca421344129a0b0e73f30113419
JaredMathis <jared.mathis@native.consulting>
2023-05-29 13:29:28 -0400

    #undefined

--- src/data/task/id/set.mjs
+++ src/data/task/id/set.mjs
@@ -1,5 +1,8 @@
+import { arguments_assert } from '../../../arguments/assert.mjs';
+import { string_is } from '../../../string/is.mjs';
 import { data_key_value_set } from '../../key/value/set.mjs';
 import { data_task_id } from '../id.mjs';
 export async function data_task_id_set(task_id) {
+    arguments_assert(arguments, [string_is]);
     await data_key_value_set(data_task_id(), task_id);
 }
