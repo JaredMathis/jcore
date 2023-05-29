1b3172c86322d31023b8e8ed08e4d9153cd118aa
JaredMathis <jared.mathis@native.consulting>
2023-05-29 08:49:20 -0400

    #28

--- src/array/remove.mjs
+++ src/array/remove.mjs
@@ -1,4 +1,5 @@
 import { array_index_of } from "./index/of.mjs";
+import { array_remove_at } from "./remove/at.mjs";
 
 export function array_remove(list, element) {
     let index = array_index_of(list, element);
