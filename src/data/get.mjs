d88ba0472a5cb02c132c8a375eb5757c0247a506
JaredMathis <jared.mathis@native.consulting>
2023-05-28 21:19:54 -0400

    #35

--- src/data/get.mjs
+++ src/data/get.mjs
@@ -1,5 +1,6 @@
 import { file_read } from './file_read.mjs';
 export async function data_get() {
     const file_path = './data.json';
-    await file_read(file_path);
+    let json = await file_read(file_path);
+    let data = json_from(json);
 }
