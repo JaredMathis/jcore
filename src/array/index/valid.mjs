2460f246169b05ba1b4f976a2e706804a046de3c
JaredMathis <jared.mathis@native.consulting>
2023-05-29 08:50:57 -0400

    #28

--- src/array/index/valid.mjs
+++ src/array/index/valid.mjs
@@ -1,3 +1,5 @@
-export function array_index_valid() {
-    
+import { array_last_index } from "../last/index.mjs";
+
+export function array_index_valid(list ,index) {
+    return index >= 0 && index <= array_last_index(list);
 }
