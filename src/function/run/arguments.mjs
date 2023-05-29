172d95b48240093bdc484db2c2dc356423175a91
JaredMathis <jared.mathis@native.consulting>
2023-05-28 23:48:59 -0400

    #35

--- src/function/run/arguments.mjs
+++ src/function/run/arguments.mjs
@@ -7,7 +7,6 @@ export async function function_run_arguments(input) {
     let args = array_to(input);
     let function_name = array_first(args);
     let remaining = array_skip_first(args);
-
     let result = await function_run(function_name, remaining);
     return result;
 }
