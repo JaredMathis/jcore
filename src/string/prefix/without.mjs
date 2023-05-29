8c1efda85e8a3fbcfb3b864a50d8b6e45299060f
JaredMathis <jared.mathis@native.consulting>
2023-05-29 18:35:27 -0400

    #13

--- src/string/prefix/without.mjs
+++ src/string/prefix/without.mjs
@@ -1,5 +1,5 @@
 import { string_starts_with } from '../starts/with.mjs';
 import { assert } from '../../assert.mjs';
-export function string_prefix_without(input, prefix_old) {
-    assert(string_starts_with(input, prefix_old));
+export function string_prefix_without(input, prefix) {
+    assert(string_starts_with(input, prefix));
 }
