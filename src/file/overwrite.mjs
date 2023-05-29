cd5dbb31f38d184598860f0fb2034bf3ae31f965
JaredMathis <jared.mathis@native.consulting>
2023-05-29 17:13:24 -0400

    #44

--- src/file/overwrite.mjs
+++ src/file/overwrite.mjs
@@ -1,12 +1,10 @@
 import fs from 'fs';
 import { directory_exists_ensure } from '../directory/exists/ensure.mjs';
 import { path_parent } from '../path/parent.mjs';
+import { path_parent_exists_ensure } from '../path/parent/exists/ensure.mjs';
 export async function file_overwrite(file_path, contents) {
     await path_parent_exists_ensure(file_path);
     await fs.promises.writeFile(file_path, contents);
 }
 
-async function path_parent_exists_ensure(file_path) {
-    let directory_path = path_parent(file_path);
-    await directory_exists_ensure(directory_path);
-}
+
