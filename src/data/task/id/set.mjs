e6f68ff0b1130ffb1f226f7b40497f5e603c87a1
JaredMathis <jared.mathis@native.consulting>
2023-05-29 12:13:37 -0400

    #38

--- src/data/task/id/set.mjs
+++ src/data/task/id/set.mjs
@@ -1,14 +1,9 @@
-import { data_get } from '../../get.mjs';
+import { data_map } from '../../map.mjs';
 import { data_task_id } from '../id.mjs';
 import { object_property_set } from '../../../object/property/set.mjs';
-import { data_path } from '../../path.mjs';
-import { file_json_overwrite } from '../../../file/json/overwrite.mjs';
 export async function data_task_id_set(task_id) {
     await data_map(map);
-
     function map(data) {
         object_property_set(data, data_task_id(), task_id);
     }
 }
-
-
