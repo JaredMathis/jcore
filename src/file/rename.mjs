a3aabe732c103a8e85b0b4770f2ca939b1308642
JaredMathis <jared.mathis@native.consulting>
2023-05-29 15:54:38 -0400

    #44

--- src/file/rename.mjs
+++ src/file/rename.mjs
@@ -1,3 +1,7 @@
+import { assert } from "../assert.mjs";
+import { directory_exists } from "../directory/exists.mjs";
+
 export function file_rename(file_path_old, file_path_new) {
-    
+    assert(await directory_exists(file_path_old));
+    assert(!await directory_exists(file_path_new));
 }
