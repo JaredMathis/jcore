25eb78aeae326a589d54832dc101a0f3c409c05f
JaredMathis <jared.mathis@native.consulting>
2023-05-29 16:04:40 -0400

    #44

--- src/file/js/overwrite.mjs
+++ src/file/js/overwrite.mjs
@@ -1,7 +1,6 @@
-import { js_unparse } from "../../js/unparse.mjs";
-import { object_property_get } from "../../object/property/get.mjs";
-import { file_overwrite } from "../overwrite.mjs";
-
+import { js_unparse } from '../../js/unparse.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
+import { file_overwrite } from '../overwrite.mjs';
 export async function file_js_overwrite(args) {
     let file_path = object_property_get(args, 'file_path');
     let parsed = object_property_get(args, 'parsed');
