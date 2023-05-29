359c2268690a994b4157042516e5df1682791961
JaredMathis <jared.mathis@native.consulting>
2023-05-29 09:12:08 -0400

    #28

--- src/js/identifier/counts.mjs
+++ src/js/identifier/counts.mjs
@@ -2,6 +2,7 @@ import { object_properties } from '../../object/properties.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 import { object_property_initialize } from '../../object/property/initialize.mjs';
 import { object_property_set } from '../../object/property/set.mjs';
+import { array_contains } from '../../array/contains.mjs';
 import { visit_filter } from '../../visit/filter.mjs';
 import { js_body_get } from '../body/get.mjs';
 import { js_node_is } from '../node/is.mjs';
@@ -15,6 +16,9 @@ export function js_identifier_counts(parsed) {
     visit_filter(body, node => object_properties(node), node => js_node_is(node) && js_node_is_identifier(node), node => {
         let invalid_names = ['hasOwnProperty']
         let r = object_property_get(node, 'name');
+        if (array_contains(invalid_names, r)) {
+            
+        }
         object_property_initialize(result, r, 0);
         let previous = object_property_get(result, r);
         object_property_set(result, r, add_1(previous));
