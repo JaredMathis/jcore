2c61d3727f0d3dba10ed86817dfb06ce6442762d
JaredMathis <jared.mathis@native.consulting>
2023-05-29 00:28:00 -0400

    #28

--- src/refactor/import/extra.mjs
+++ src/refactor/import/extra.mjs
@@ -1,3 +1,4 @@
+import { js_import_all_to_function_name } from './js_import_all_to_function_name.mjs';
 export async function refactor_import_extra(parsed) {
     let import_name_all = js_import_all_to_function_name(parsed);
 }
