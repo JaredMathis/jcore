ac8178fa3785fa674969937dd92c06e64af055e0
JaredMathis <jared.mathis@native.consulting>
2023-05-29 00:57:03 -0400

    #28

--- src/refactor/import/extra.mjs
+++ src/refactor/import/extra.mjs
@@ -1,9 +1,24 @@
+import { array_add } from '../../array/add.mjs';
 import { error } from '../../error.mjs';
 import { js_identifier_counts } from '../../js/identifier/counts.mjs';
 import { js_import_all_to_function_name } from '../../js/import/all/to/function/name.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
 export async function refactor_import_extra(parsed) {
     let import_name_all = js_import_all_to_function_name(parsed);
     let counts = js_identifier_counts(parsed);
+    let twices = [];
+    function is_two(value) {
+        return value === 2;
+    }
+    let predicate = is_two;
+    let result = [];
+    for (let i in counts) {
+        let value = object_property_get(counts, i);
+        if (predicate(value)) {
+            array_add(result, value);
+        }
+    }
+    let intersection = array_intersection(import_name_all)
     console.log({import_name_all,counts})
     error();
 }
