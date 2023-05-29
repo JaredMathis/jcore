d4f5ad321190d6c205c4150eec8ecde15524e018
JaredMathis <jared.mathis@native.consulting>
2023-05-29 17:09:51 -0400

    #44

--- src/file/js/identifier/rename.mjs
+++ src/file/js/identifier/rename.mjs
@@ -1,8 +1,5 @@
 import { js_identifier_rename } from '../../../js/identifier/rename.mjs';
-import { file_js_all_path } from '../all/path.mjs';
 import { file_js_map_args } from '../map/args.mjs';
 export async function file_js_identifier_rename(file_path, identifier_name_old, identifier_name_new) {
-    for (let file_path of await file_js_all_path()) {
-        await file_js_map_args(file_path, args => js_identifier_rename(args, identifier_name_old, identifier_name_new));
-    }
+    await file_js_map_args(file_path, args => js_identifier_rename(args, identifier_name_old, identifier_name_new));
 }
--- src/function/rename.mjs
+++ src/function/rename.mjs
@@ -6,6 +6,7 @@ import { function_exists } from './exists.mjs';
 import { function_name_to_path } from './name/to/path.mjs';
 import { file_rename } from '../file/rename.mjs';
 import { file_js_identifier_rename } from '../file/js/identifier/rename.mjs';
+import { file_js_all_identifier_rename } from '../file/js/all/identifier/rename.mjs';
 export async function function_rename(function_name_old, function_name_new) {
     assert(await function_exists(function_name_old));
     assert(!await function_exists(function_name_new));
@@ -13,6 +14,6 @@ export async function function_rename(function_name_old, function_name_new) {
     let file_path_old = function_name_to_path(function_name_old);
     let file_path_new = function_name_to_path(function_name_new);
     await file_rename(file_path_old, file_path_new);
-    await file_js_identifier_rename(file_path_new, function_name_old, function_name_new);
+    await file_js_all_identifier_rename(function_name_old, function_name_new);
     await file_js_all_map(refactor_import_fix.name);
 }
