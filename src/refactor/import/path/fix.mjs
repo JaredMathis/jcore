3b48c7e0ccb85c72982e15317702d54e13863906
JaredMathis <jared.mathis@native.consulting>
2023-05-29 10:57:24 -0400

    #38

--- src/refactor/import/path/fix.mjs
+++ src/refactor/import/path/fix.mjs
@@ -1,10 +1,12 @@
 import { function_name_to_path } from '../../../function/name/to/path.mjs';
 import { js_import_all_with_function_names } from '../../../js/import/all/with/function/names.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
+import { string_includes } from '../../../string/includes.mjs';
 import { path_relative_file } from '../../../path/relative/file.mjs';
 import { path_parent } from '../../../path/parent.mjs';
 import { js_node_is_literal } from '../../../js/node/is/literal.mjs';
 import { js_import_path_normalize } from '../../../js/import/path/normalize.mjs';
+import { assert } from '../../../assert.mjs';
 export function refactor_import_path_fix(args) {
     let {parsed, file_path} = args;
     let imports = js_import_all_with_function_names(parsed);
@@ -15,6 +17,10 @@ export function refactor_import_path_fix(args) {
         let normalized = js_import_path_normalize(relative);
         let i = object_property_get(iw, 'import');
         let source = object_property_get(i, 'source');
+        object_property_set(source, 'value', normalized);
+        assert(!string_includes(normalized, "'"))
+        let raw = `'${normalized}'`;
+        object_property_set(source, 'raw', raw);
         if (!js_node_is_literal(source)) {
             continue;
         }
