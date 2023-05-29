02038b239c61d13d3887f3a7ccdfd533983c52c9
JaredMathis <jared.mathis@native.consulting>
2023-05-29 08:11:18 -0400

    #28

--- src/js/exported/function/names.mjs
+++ src/js/exported/function/names.mjs
@@ -1,3 +1,26 @@
-export function js_exported_function_names() {
-    
+import { array_filter } from "../../../array/filter.mjs";
+import { array_map } from "../../../array/map.mjs";
+import { assert } from "../../../assert.mjs";
+import { object_property_get } from "../../../object/property/get.mjs";
+import { js_body_get } from "../../body/get.mjs";
+import { js_node_is_export_named_declaration } from "../../node/is/export/named/declaration.mjs";
+import { js_node_is_function_declaration } from "../../node/is/function/declaration.mjs";
+import { js_node_is_identifier } from "../../node/is/identifier.mjs";
+
+export function js_exported_function_names(parsed) {
+    let body = js_body_get(parsed);
+    let exports = array_filter(
+        body, b => js_node_is_export_named_declaration(b));
+    let declarations = array_map(exports, e => {
+        let d = object_property_get(e, 'declaration');
+        return d;
+    });
+    let filtered = array_filter(declarations, d => js_node_is_function_declaration(d));
+    let mapped = array_map(filtered, f => {
+        let id = object_property_get(f, 'id');
+        assert(js_node_is_identifier(id));
+        let name = object_property_get(id, 'name');
+        return name;
+    });
+    return mapped;
 }
--- src/refactor/import/missing.mjs
+++ src/refactor/import/missing.mjs
@@ -45,20 +45,3 @@ export async function refactor_import_missing(parsed) {
     array_add_beginning_all(body, import_new_all);
 }
 
-function js_exported_function_names(parsed) {
-    let body = js_body_get(parsed);
-    let exports = array_filter(
-        body, b => js_node_is_export_named_declaration(b));
-    let declarations = array_map(exports, e => {
-        let d = object_property_get(e, 'declaration');
-        return d;
-    });
-    let filtered = array_filter(declarations, d => js_node_is_function_declaration(d));
-    let mapped = array_map(filtered, f => {
-        let id = object_property_get(f, 'id');
-        assert(js_node_is_identifier(id));
-        let name = object_property_get(id, 'name');
-        return name;
-    });
-    return mapped;
-}
