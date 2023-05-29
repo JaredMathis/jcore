c049062517bac28a9c648b65201ddf1c04f71af8
JaredMathis <jared.mathis@native.consulting>
2023-05-29 18:35:57 -0400

    #13

--- src/string/prefix/without.mjs
+++ src/string/prefix/without.mjs
@@ -1,5 +1,7 @@
 import { string_starts_with } from '../starts/with.mjs';
 import { assert } from '../../assert.mjs';
+import { string_length } from '../length.mjs';
 export function string_prefix_without(input, prefix) {
     assert(string_starts_with(input, prefix));
+    let length = string_length(input);
 }
