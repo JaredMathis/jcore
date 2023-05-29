e774ed6acf5f0c94051842484aebda69c5aad9b0
JaredMathis <jared.mathis@native.consulting>
2023-05-29 11:44:02 -0400

    #38

--- src/data/task/finish.mjs
+++ src/data/task/finish.mjs
@@ -1,3 +1,4 @@
+import { file_js_all_map } from '../../file/js/all/map.mjs';
 export function data_task_finish() {
     file_js_all_map();
 }
--- src/refactor/import/fix.mjs
+++ src/refactor/import/fix.mjs
@@ -1,14 +1,13 @@
 import { refactor_import_missing } from './missing.mjs';
 import { refactor_import_extra } from './extra.mjs';
 import { refactor_import_path_fix } from './path/fix.mjs';
-export function refactor_import_fix(args) {
+export async function refactor_import_fix(args) {
     let refactors = [
         refactor_import_extra,
         refactor_import_missing,
         refactor_import_path_fix
     ];
     for (let r of refactors) {
-        console.log(r.name, {args})
-        r(args);
+        await r(args);
     }
 }
