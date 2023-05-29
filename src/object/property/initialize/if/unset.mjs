15352fc6a3892ff51ec9afdb07f60fa47a35e689
JaredMathis <jared.mathis@native.consulting>
2023-05-29 19:44:13 -0400

    #13

--- src/object/property/initialize/if/unset.mjs
+++ src/object/property/initialize/if/unset.mjs
@@ -1,8 +1,10 @@
+import { assert } from '../../../../assert.mjs';
 import { object_property_exists } from '../../exists.mjs';
 import { object_property_set } from '../../set.mjs';
 export function object_property_initialize_if_unset(object, property_name, initial_value) {
     if (object_property_exists(object, property_name)) {
         return;
     }
+    assert(object_property_exists(object, property_name));
     object_property_set(object, property_name, initial_value);
 }
