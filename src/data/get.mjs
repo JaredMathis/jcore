b2906695d2f58e0e059d55b3b6dcf90574c9466d
JaredMathis <jared.mathis@native.consulting>
2023-05-29 14:04:05 -0400

    no task

--- src/data/get.mjs
+++ src/data/get.mjs
@@ -12,10 +12,6 @@ export async function data_get() {
         await file_write(file_path, default_json);
     }
     let json = await file_read(file_path);
-    console.log({
-        json,
-        file_path
-    });
     let data = json_from(json);
     return data;
 }
