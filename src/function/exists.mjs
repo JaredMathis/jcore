3100a5e08af76d67ed0ec8024714a98f27e093db
JaredMathis <jared.mathis@native.consulting>
2023-05-29 15:00:03 -0400

    #43

--- src/function/exists.mjs
+++ src/function/exists.mjs
@@ -2,5 +2,5 @@ import { function_name_to_path } from './name/to/path.mjs';
 import { directory_exists } from '../directory/exists.mjs';
 export async function function_exists(function_name) {
     let function_path = function_name_to_path(function_name);
-    await directory_exists(function_path);
+    return await directory_exists(function_path);
 }
