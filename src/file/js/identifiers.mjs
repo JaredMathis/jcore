6cdb8c26bbaad9474ab00e490e45f57ed857490d
JaredMathis <jared.mathis@native.consulting>
2023-05-29 15:40:05 -0400

    #44

--- src/file/js/identifiers.mjs
+++ src/file/js/identifiers.mjs
@@ -1,2 +1,5 @@
-export function file_js_identifiers(file_path) {
+import { file_js_parse } from "./parse.mjs";
+
+export async function file_js_identifiers(file_path) {
+    let parsed = await file_js_parse(file_path)
 }
