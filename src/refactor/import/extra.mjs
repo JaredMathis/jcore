cf22bda24a7a4ce18f6393042843b5f936d3767f
JaredMathis <jared.mathis@native.consulting>
2023-05-29 08:40:04 -0400

    #28

--- src/refactor/import/extra.mjs
+++ src/refactor/import/extra.mjs
@@ -13,7 +13,7 @@ import { js_exported_function_names } from '../../js/exported/function/names.mjs
 import { js_import_all_with_function_names } from '../../js/import/all/with/function/names.mjs';
 import { array_find } from '../../array/find.mjs';
 export async function refactor_import_extra(parsed) {
-    let imports = js_import_all_with_function_names(parsed);
+    let with_function_names = js_import_all_with_function_names(parsed);
     let import_name_all = array_map(
         with_function_names, w => object_property_get(w, 'name'));
     let counts = js_identifier_counts(parsed);
