5bc30da77cdc87732ad204dfb384a6a5846d6a4d
JaredMathis <jared.mathis@native.consulting>
2023-05-29 15:37:58 -0400

    #44

--- src/file/js/all/identifier/exists.mjs
+++ src/file/js/all/identifier/exists.mjs
@@ -1,8 +1,8 @@
 import { error } from "../../../../error.mjs";
-import { file_js_all } from "../../all.mjs";
+import { file_js_all_path } from "../path.mjs";
 
 export async function file_js_all_identifier_exists(identifier_name) {
-    let all = await file_js_all();
+    let all = await file_js_all_path();
 
     for (let a of all) {
         let identifiers = await file_js_identifiers(a);
