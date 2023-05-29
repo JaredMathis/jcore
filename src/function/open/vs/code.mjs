2386f08bf84967e7a8bd6ec3f10f58986e41daf2
JaredMathis <jared.mathis@native.consulting>
2023-05-29 17:35:46 -0400

    #45

--- src/function/open/vs/code.mjs
+++ src/function/open/vs/code.mjs
@@ -1,2 +1,6 @@
-export function function_open_vs_code() {
+import { file_open_vs_code } from '../../../file/open/vs/code.mjs';
+import { function_name_to_path } from '../../name/to/path.mjs';
+export async function function_open_vs_code(function_name) {
+    let function_path = function_name_to_path(function_name);
+    await file_open_vs_code(function_path);
 }
