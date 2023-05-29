3f8047cec990202e524cd0212f634d86770602e1
JaredMathis <jared.mathis@native.consulting>
2023-05-29 19:39:05 -0400

    #13

--- src/function/rename/if/starts/with.mjs
+++ src/function/rename/if/starts/with.mjs
@@ -1,13 +1,20 @@
-import { log } from '../../../../log.mjs';
+import { refactor_import_fix } from '../../../../refactor/import/fix.mjs';
+import { file_js_all_map } from '../../../../file/js/all/map.mjs';
+import { file_js_all_identifier_rename } from '../../../../file/js/all/identifier/rename.mjs';
+import { error } from '../../../../error.mjs';
+import { function_rename_without_all_refactor } from '../../without/all/refactor.mjs';
 import { string_prefix_replace } from '../../../../string/prefix/replace.mjs';
 import { string_starts_with } from '../../../../string/starts/with.mjs';
 import { function_name_all } from '../../../name/all.mjs';
 export async function function_rename_if_starts_with(prefix_old, prefix_new) {
     let names = await function_name_all();
-    for (let n of names) {
-        if (string_starts_with(n, prefix_old)) {
-            let n_new = string_prefix_replace(n, prefix_old, prefix_new);
-            console.log(n_new);
+    for (let n_old of names) {
+        if (string_starts_with(n_old, prefix_old)) {
+            let n_new = string_prefix_replace(n_old, prefix_old, prefix_new);
+            await function_rename_without_all_refactor(n_old, n_new);
+            error();
+            await file_js_all_identifier_rename(n_old, n_new);
+            await file_js_all_map(refactor_import_fix.name);
         }
     }
 }
