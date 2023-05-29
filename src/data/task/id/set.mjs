60916352284806454eb028e2237eab861bd92223
JaredMathis <jared.mathis@native.consulting>
2023-05-29 12:14:42 -0400

    #38

--- src/data/task/id/set.mjs
+++ src/data/task/id/set.mjs
@@ -2,9 +2,9 @@ import { data_map } from '../../map.mjs';
 import { data_task_id } from '../id.mjs';
 import { object_property_set } from '../../../object/property/set.mjs';
 export async function data_task_id_set(task_id) {
-    await data_map(map);
     const key = data_task_id();
     const value = task_id;
+    await data_map(map);
     function map(data) {
         object_property_set(data, key, value);
     }
