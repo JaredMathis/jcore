c47edfa7333b6f1d5a5e3924198eafefe1185b44
JaredMathis <jared.mathis@native.consulting>
2023-05-29 20:31:10 -0400

    #21

--- untested.mjs
+++ untested.mjs
@@ -1,15 +1,15 @@
-import { array_filter_async } from "./src/array/filter/async.mjs";
-import { array_take } from "./src/array/take.mjs";
+import { list_filter_async } from "./src/list/filter/async.mjs";
+import { list_take } from "./src/list/take.mjs";
 import { function_name_all } from "./src/function/name/all.mjs";
 import { function_tests_count } from "./src/function/tests/count.mjs";
 import { log } from "./src/log.mjs";
 
 let maximum_untested_display = 10;
 const function_names = await function_name_all();
-let filtered = await array_filter_async(
+let filtered = await list_filter_async(
     function_names, 
     async f =>  await function_tests_count(f) === 0);
-let limited = array_take(filtered, maximum_untested_display);
+let limited = list_take(filtered, maximum_untested_display);
 for (let function_name of limited) {
     log(function_name);
 }
