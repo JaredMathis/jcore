7de2d07af0afde7efc1641acebe0fbdf53d77cb1
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:04:24 -0400

    #10 git_pacp

--- src/function/add/with/declaration.mjs
+++ src/function/add/with/declaration.mjs
@@ -4,10 +4,13 @@ import { function_write } from '../../write.mjs';
 import { error } from '../../../error.mjs';
 import { js_parse } from '../../../js/parse.mjs';
 import { js_export_declaration } from '../../../js/export/declaration.mjs';
+import { assert } from '../../../assert.mjs';
+import { js_node_is_function_declaration } from '../../../js/node/is/function/declaration.mjs';
 export async function function_add_with_declaration(function_name, fd) {
     let parsed = js_parse(js_function_name_to_export(function_name));
     let export_single = js_export_single(parsed);
     let declaration = js_export_declaration(export_single);
+    assert(js_node_is_function_declaration(declaration));
     error('todo: function_add_with_declaration');
     return await function_write(function_name, parsed);
 }
