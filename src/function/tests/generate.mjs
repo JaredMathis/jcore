6fb68434c76854f340af97f53476d116cf3e912d
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:22:50 -0400

    #10 git_pacp

--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -5,7 +5,7 @@ import { function_all_tests } from '../all/tests.mjs';
 export async function function_tests_generate() {
     let test_names = await function_all_tests();
 
-    let mapped = list_map(test_names, n => `await ${n}();`)
+    let mapped = list_map(test_names, n => `await ${n}();${string_new_line()}`)
     let calls = list_join()
     
     console.log(test_names);
