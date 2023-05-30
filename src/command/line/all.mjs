5297f73a7e04359829fd19aa3efa289b7e086c9a
JaredMathis <jared.mathis@native.consulting>
2023-05-29 21:52:35 -0400

    #40

--- src/command/line/all.mjs
+++ src/command/line/all.mjs
@@ -1,4 +1,17 @@
-import { error } from '../../error.mjs';
-export function command_line_all() {
-    error('todo: command_line_all');
+import { result_unsuccess } from '../../result/unsuccess.mjs';
+import { log } from '../../log.mjs';
+import { command_line_check } from './check.mjs';
+import { result_empty } from '../../result/empty.mjs';
+export async function command_line_all() {
+    let result = result_empty();
+    for (let c of commands) {
+        let c_result = await command_line_check(c);
+        if (!c_result.success) {
+            log(`Command failed: ${ c }`);
+            log(c_result.stdout);
+            result_unsuccess(result);
+            return result;
+        }
+    }
+    return result;
 }
