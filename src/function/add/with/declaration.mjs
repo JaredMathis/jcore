23eeddc985aed73957f70e49f651819d445a3fff
JaredMathis <jared.mathis@native.consulting>
2023-05-30 11:52:58 -0400

    #10 function_auto function_add_with_declaration

--- src/function/add/with/declaration.mjs
+++ src/function/add/with/declaration.mjs
@@ -1,4 +1,6 @@
+import { function_write } from '../../write.mjs';
 import { error } from '../../../error.mjs';
-export function function_add_with_declaration() {
+export async function function_add_with_declaration(function_name, fd) {
     error('todo: function_add_with_declaration');
+    return await function_write(function_name, parsed);
 }
