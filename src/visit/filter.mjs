e7212f4c159cdef2c25e66300e65cabdff0acd77
JaredMathis <jared.mathis@native.consulting>
2023-05-30 16:15:36 -0400

    #51 git_pacp

--- src/visit/filter.mjs
+++ src/visit/filter.mjs
@@ -1,8 +1,9 @@
 import { visit_recursive } from './recursive.mjs';
 export function visit_filter(root, children_get, filter, lambda) {
-    visit_recursive(root, children_get, function lambda_local(node) {
+    visit_recursive(root, children_get, lambda_local, []);
+    function lambda_local(node) {
         if (filter(node)) {
             lambda(node);
         }
-    }, []);
+    }
 }
