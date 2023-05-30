f2c3c1eb9bc9987589110fb33909a2f0e05e8460
JaredMathis <jared.mathis@native.consulting>
2023-05-30 19:22:56 -0400

    #1 git_pacp

--- src/js/identifier/rename.mjs
+++ src/js/identifier/rename.mjs
@@ -4,7 +4,7 @@ import { js_visit_nodes_identifier } from '../visit/nodes/identifier.mjs';
 export function js_identifier_rename(args, identifier_name_old, identifier_name_new) {
     let changed = false;
     let parsed = object_property_get(args, 'parsed');
-    js_visit_nodes_identifier(parsed, nodve => {
+    js_visit_nodes_identifier(parsed, v => {
         let node = object_property_get(v, 'node');
         let n = object_property_get(node, 'name');
         if (n === identifier_name_old) {
--- src/tests/add.mjs
+++ src/tests/add.mjs
@@ -1,17 +1,13 @@
-import { string_length } from '../string/length.mjs';
-import { string_skip } from '../string/skip.mjs';
+import { string_prefix_without } from '../string/prefix/without.mjs';
 import { function_tests_all } from '../function/tests/all.mjs';
 import { list_map } from '../list/map.mjs';
 import { log } from '../log.mjs';
 import { string_function_tests_sub } from '../string/function/tests/sub.mjs';
-import { assert } from '../assert.mjs';
-import { string_starts_with } from '../string/starts/with.mjs';
 export async function tests_add(function_name) {
     let tests_all = await function_tests_all(function_name);
     let tests_ids_all = list_map(tests_all, t => {
         let function_tests_prefix = `${ function_name }${ string_function_tests_sub() }`;
-        assert(string_starts_with(t, function_tests_prefix));
-        return string_skip(t, string_length(function_tests_prefix));
+        return string_prefix_without(t, function_tests_prefix);
     });
     log(tests_ids_all);
 }
