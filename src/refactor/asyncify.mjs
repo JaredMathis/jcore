19b5685424cc25c06437838158206cb138397859
JaredMathis <jared.mathis@native.consulting>
2023-05-30 14:19:26 -0400

    #10 git_pacp

--- src/refactor/asyncify.mjs
+++ src/refactor/asyncify.mjs
@@ -4,5 +4,9 @@ import { js_keyword_async } from '../js/keyword/async.mjs';
 export function refactor_asyncify(args) {
     let {parsed} = args;
     let fd = js_export_function_single(parsed);
+    js_function_declaration_asyncify(fd);
+}
+
+function js_function_declaration_asyncify(fd) {
     object_property_change(fd, js_keyword_async(), true);
 }
