d1f809262fd1725029f78217ed0f8680881f69dc
JaredMathis <jared.mathis@native.consulting>
2023-05-29 23:10:40 -0400

    closes #10 data_task_next 10

--- src/data/task/finish.mjs
+++ src/data/task/finish.mjs
@@ -1,3 +1,5 @@
+import { data_get } from '../get.mjs';
+import { log } from '../../log.mjs';
 import { data_task_id_remove } from './id/remove.mjs';
 import { data_task_id_get } from './id/get.mjs';
 import { todo } from '../../todo.mjs';
@@ -11,5 +13,7 @@ export async function data_task_finish() {
     await file_js_all_map(refactor_import_fix.name);
     let result = await git_pacp_with_message(`closes ${ task_id }`);
     assert(result.success);
+    console.log(await data_get());
     await data_task_id_remove();
+    console.log(await data_get());
 }
