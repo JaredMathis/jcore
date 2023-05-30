6169b2e711acc8f585d6dd6e3443af2dc3a4549d
JaredMathis <jared.mathis@native.consulting>
2023-05-30 17:17:05 -0400

    #51 function_auto list_last

--- src/list/last.mjs
+++ src/list/last.mjs
@@ -1,4 +1,5 @@
-import { error } from '../error.mjs';
-export function list_last() {
-    error('todo: list_last');
+import { list_last_index } from './last/index.mjs';
+import { list_index_at } from './index/at.mjs';
+export function list_last(list) {
+    return list_index_at(list, list_last_index(list));
 }
