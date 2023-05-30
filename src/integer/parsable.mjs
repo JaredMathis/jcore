4dfe680dc90b197e7bf9ab05fd6bdc7690e217b2
JaredMathis <jared.mathis@native.consulting>
2023-05-30 19:47:50 -0400

    #1 function_auto integer_parsable

--- src/integer/parsable.mjs
+++ src/integer/parsable.mjs
@@ -1,4 +1,5 @@
+import { number_nan_is_not } from '../number/nan/is/not.mjs';
 import { integer_parse } from './parse.mjs';
 export function integer_parsable(input) {
-    return !isNaN(integer_parse(input));
+    return number_nan_is_not(integer_parse(input));
 }
