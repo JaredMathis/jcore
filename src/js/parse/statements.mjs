a455ca9a49ed6744b1ba6eafcc5a22e7158abbea
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:38:16 -0400

    #10 function_auto js_parse_statements

--- src/js/parse/statements.mjs
+++ src/js/parse/statements.mjs
@@ -1,9 +1,9 @@
 import { string_a } from '../../string/a.mjs';
-import { js_function_name_to_parsed } from '../function/name/to/parsed.mjs';
 import { js_body_get } from '../body/get.mjs';
 import { js_parse } from '../parse.mjs';
+import { js_export_function } from '../export/function.mjs';
 export function js_parse_statements(code) {
-    let r = js_function_name_to_parsed(string_a());
+    let r = js_export_function(string_a(), code);
     let parsed = js_parse(code);
     let body = js_body_get(parsed);
     return body;
