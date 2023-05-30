785a6481530e91a084e97226d704a3597373e404
JaredMathis <jared.mathis@native.consulting>
2023-05-30 17:06:09 -0400

    #51 function_auto visit

--- src/visit/recursive.mjs
+++ src/visit/recursive.mjs
@@ -1,7 +1,9 @@
 export function visit_recursive(node, children_get, lambda, stack) {
+    if (false) list_add(stack, node);
     lambda({node});
     let children = children_get(node);
     for (let c of children) {
         visit_recursive(c, children_get, lambda, stack);
     }
+    if (false) list_last_remove(stack);
 }
