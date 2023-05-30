1e6ca3790de75773c9d4a64d0b5d850f5f165fdf
JaredMathis <jared.mathis@native.consulting>
2023-05-30 15:39:31 -0400

    #50 git_pacp

--- src/refactor/member/expression/to/function/call.mjs
+++ src/refactor/member/expression/to/function/call.mjs
@@ -1,7 +1,7 @@
-import { tautology } from '../../../../../tautology.mjs';
+import { js_node_is_type_member_expression } from '../../../../../js/node/is/type/member/expression.mjs';
 import { js_visit } from '../../../../../js/visit.mjs';
 import { log } from '../../../../../log.mjs';
 export function refactor_member_expression_to_function_call(args) {
     let {parsed} = args;
-    js_visit(parsed, tautology, n => log(n));
+    js_visit(parsed, n => js_node_is_type_member_expression(n), n => log(n));
 }
