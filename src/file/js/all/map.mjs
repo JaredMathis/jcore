011be08b09f20d37dbe7928125cce89e82b284a3
JaredMathis <jared.mathis@native.consulting>
2023-05-30 15:04:40 -0400

    #49 function_auto file_js_all_map

--- src/file/js/all/map.mjs
+++ src/file/js/all/map.mjs
@@ -1,10 +1,6 @@
+import { file_js_filered_map } from '../filered/map.mjs';
 import { tautology } from '../../../tautology.mjs';
-import { list_filter } from '../../../list/filter.mjs';
-import { file_js_multiple_map } from '../multiple/map.mjs';
-import { file_js_all_path } from './path.mjs';
 export async function file_js_all_map(function_name_mapper) {
     let filter = tautology;
-    let all = await file_js_all_path();
-    let filtered = list_filter(all, filter);
-    await file_js_multiple_map(function_name_mapper, filtered);
+    await file_js_filered_map(filter, function_name_mapper);
 }
---
+++ src/file/js/filered/map.mjs
@@ -0,0 +1,8 @@
+import { file_js_multiple_map } from '../multiple/map.mjs';
+import { list_filter } from '../../../list/filter.mjs';
+import { file_js_all_path } from '../all/path.mjs';
+export async function file_js_filered_map(filter, function_name_mapper) {
+    let all = await file_js_all_path();
+    let filtered = list_filter(all, filter);
+    await file_js_multiple_map(function_name_mapper, filtered);
+}
