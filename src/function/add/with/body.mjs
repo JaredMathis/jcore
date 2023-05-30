ae852ed86264b1dc95f685dc90ee738f7ea8c267
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:55:16 -0400

    #10 git_pacp

--- src/function/add/with/body.mjs
+++ src/function/add/with/body.mjs
@@ -3,10 +3,10 @@ import { js_function_declaration_statements_initialize } from '../../../js/funct
 import { js_exported_function_declaration_single } from '../../../js/exported/function/declaration/single.mjs';
 import { js_function_name_to_export } from '../../../js/function/name/to/export.mjs';
 import { js_parse } from '../../../js/parse.mjs';
-export async function function_add_with_body(function_name, body) {
+export async function function_add_with_body(function_name, statements) {
     const code = js_function_name_to_export(function_name);
     let parsed = js_parse(code);
     let fd = js_exported_function_declaration_single(parsed);
-    js_function_declaration_statements_initialize(fd, body);
+    js_function_declaration_statements_initialize(fd, statements);
     return await function_write(function_name, parsed);
 }
--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -7,12 +7,16 @@ import { list_map } from '../../list/map.mjs';
 import { function_all_tests } from '../all/tests.mjs';
 import { function_name_to_path } from '../name/to/path.mjs';
 import { js_add_function_with_statements } from '../../js/add/function/with/statements.mjs';
+import { function_add_with_body } from '../add/with/body.mjs';
 export async function function_tests_generate() {
     let function_name = 'tests';
     let file_path = function_name_to_path(function_name);
     let test_names = await function_all_tests();
     let mapped = list_map(test_names, n => `await ${ n }();`);
     let tests = list_join(mapped, string_new_line());
+
+    await function_add_with_body(function_name, statements)
+
     js_add_function_with_statements(parsed, function_name, statements);
     await file_js_map(refactor_console_to_function.name, file_path);
 }
