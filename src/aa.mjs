a735f377437a65b8936849d4bee7f53632e35668
JaredMathis <jared.mathis@native.consulting>
2023-05-30 10:34:13 -0400

    #48 git_pacp

--- src/aa.mjs
+++ src/aa.mjs
@@ -1,9 +1,14 @@
 import { error } from './error.mjs';
 export function aa() {
     error('todo: aa');
+    bb();
 }
 function bb() {
+    dd();
     cc();
 }
 function cc() {
+    cc();
+}
+function dd() {
 }
