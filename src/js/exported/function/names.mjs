a663a6ecd5acbe384b5f2146fd970e335c906b59
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:17:55 -0400

    #48 function_auto js_exported_function_names

--- src/js/exported/function/names.mjs
+++ src/js/exported/function/names.mjs
@@ -1,17 +1,8 @@
 import { js_function_declaration_to_name } from '../../function/declaration/to/name.mjs';
 import { js_exported_function_declarations } from './declarations.mjs';
 import { list_map } from '../../../list/map.mjs';
-import { assert } from '../../../assert.mjs';
-import { object_property_get } from '../../../object/property/get.mjs';
-import { js_node_is_identifier } from '../../node/is/identifier.mjs';
 export function js_exported_function_names(parsed) {
     let filtered = js_exported_function_declarations(parsed);
     let mapped = list_map(filtered, js_function_declaration_to_name);
     return mapped;
-}
-function js_function_declaration_to_name(f) {
-    let id = object_property_get(f, 'id');
-    assert(js_node_is_identifier(id));
-    let name = object_property_get(id, 'name');
-    return name;
 }
