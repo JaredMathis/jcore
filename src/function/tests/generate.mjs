6b79db74631334a23331921322bce17c24e3d750
JaredMathis <jared.mathis@native.consulting>
2023-05-30 14:26:57 -0400

    #10 function_auto function_tests_generate

--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -1,3 +1,4 @@
+import { function_add_with_statements_synchronized } from '../add/with/statements/synchronized.mjs';
 import { file_delete } from '../../file/delete.mjs';
 import { file_exists } from '../../file/exists.mjs';
 import { js_parse_statements } from '../../js/parse/statements.mjs';
@@ -8,7 +9,6 @@ import { list_join } from '../../list/join.mjs';
 import { list_map } from '../../list/map.mjs';
 import { function_all_tests } from '../all/tests.mjs';
 import { function_name_to_path } from '../name/to/path.mjs';
-import { function_add_with_statements } from '../add/with/statements.mjs';
 export async function function_tests_generate() {
     let function_name = 'tests';
     let file_path = function_name_to_path(function_name);
