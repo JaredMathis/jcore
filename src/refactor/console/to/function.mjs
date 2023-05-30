0630247ccfdcea3eae2fecd03e17f45d32dc3e70
JaredMathis <jared.mathis@native.consulting>
2023-05-30 08:31:07 -0400

    #47 function_auto refactor_console_to_function

--- src/refactor/console/to/function.mjs
+++ src/refactor/console/to/function.mjs
@@ -1,3 +1,4 @@
+import { list_add_all } from '../../../list/add/all.mjs';
 import { js_node_is_block_statement } from '../../../js/node/is/block/statement.mjs';
 import { js_exported_function_declaration_single } from '../../../js/exported/function/declaration/single.mjs';
 import { log } from '../../../log.mjs';
@@ -34,6 +35,8 @@ export async function refactor_console_to_function(file_path) {
     let function_body = object_property_get(fd, 'body');
     assert(js_node_is_block_statement(function_body));
     let function_body_statements_old = object_property_get(function_body, 'body');
+    assert(list_length_is_0(function_body_statements_old));
+    list_add_all(function_body_statements_old, function_body_statements_new);
     console.log({ function_body });
     error();
 }
