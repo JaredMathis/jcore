b2245ce1119a2e8ce7dcf47cf5a8e359b5108379
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:35:51 -0400

    #48 function_auto js_exported_function_declaration_single_statements_set

---
+++ src/js/exported/function/declaration/single/statements/set.mjs
@@ -0,0 +1,4 @@
+import { error } from '../../../../../../error.mjs';
+export function js_exported_function_declaration_single_statements_set() {
+    error('todo: js_exported_function_declaration_single_statements_set');
+}
