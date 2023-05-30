50d608be2acbe172618b57539622d770961e725b
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:57:02 -0400

    #10 function_auto js_export_function

--- src/js/export/function.mjs
+++ src/js/export/function.mjs
@@ -1,13 +1,8 @@
-import { js_keyword_async } from '../keyword/async.mjs';
+import { js_export_function_synchronized } from './function/synchronized.mjs';
 import { assert } from '../../assert.mjs';
-import { string_empty } from '../../string/empty.mjs';
 import { string_length } from '../../string/length.mjs';
 export function js_export_function(function_name, function_statements_code) {
     assert(string_length(function_name) >= 1);
     let is_async = false;
-    let async_code = string_empty();
-    if (is_async) {
-        async_code = js_keyword_async();
-    }
-    return `export ${ async_code } function ${ function_name }() { ${ function_statements_code } }`;
+    return js_export_function_synchronized(is_async, function_name, function_statements_code);
 }
---
+++ src/js/export/function/synchronized.mjs
@@ -0,0 +1,9 @@
+import { js_keyword_async } from '../../keyword/async.mjs';
+import { string_empty } from '../../../string/empty.mjs';
+export function js_export_function_synchronized(is_async, function_name, function_statements_code) {
+    let async_code = string_empty();
+    if (is_async) {
+        async_code = js_keyword_async();
+    }
+    return `export ${ async_code } function ${ function_name }() { ${ function_statements_code } }`;
+}
