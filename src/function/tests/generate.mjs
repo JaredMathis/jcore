11a497d6c0a120bdd8cbfe977088a536a09637a4
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:28:34 -0400

    #10 git_pacp

--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -3,10 +3,12 @@ import { list_map } from '../../list/map.mjs';
 import { log } from '../../log.mjs';
 import { function_all_tests } from '../all/tests.mjs';
 export async function function_tests_generate() {
+    let file_path = './src/tests.mjs'
     let test_names = await function_all_tests();
 
-    let mapped = list_map(test_names, n => `await ${n}();${string_new_line()}`)
-    let calls = list_join()
+    let mapped = list_map(test_names, n => `await ${n}();`)
+    let calls = list_join(mapped, string_new_line())
+    file_js_overwrite()
     
     console.log(test_names);
 }
