d5b913865a4937c633d998d39c92867a944ce102
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:07:11 -0400

    #10 function_auto js_export_declaration_get

--- src/js/export/declaration/get.mjs
+++ src/js/export/declaration/get.mjs
@@ -1,5 +1,6 @@
+import { js_node_property_declaration } from '../../node/property/declaration.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
 export function js_export_declaration_get(e) {
-    let d = object_property_get(e, 'declaration');
+    let d = object_property_get(e, js_node_property_declaration());
     return d;
 }
