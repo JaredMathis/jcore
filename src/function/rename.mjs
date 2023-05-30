00944f0c7cda6490badc407e7570aae89b9bf994
JaredMathis <jared.mathis@native.consulting>
2023-05-30 15:10:08 -0400

    #49 git_pacp

--- src/function/rename.mjs
+++ src/function/rename.mjs
@@ -6,6 +6,5 @@ import { function_rename_without_all_refactor } from './rename/without/all/refac
 export async function function_rename(function_name_old, function_name_new) {
     await function_rename_without_all_refactor(function_name_old, function_name_new);
     let file_paths_changed = await file_js_all_identifier_rename(function_name_old, function_name_new);
-    error();
     await file_js_multiple_map(refactor_import_fix.name, file_paths_changed);
 }
