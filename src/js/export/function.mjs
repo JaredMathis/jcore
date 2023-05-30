6da686180af4db6b736db2351183e88990ea0e65
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:19:13 -0400

    #10 git_pacp

--- src/js/export/function.mjs
+++ src/js/export/function.mjs
@@ -1,3 +1,7 @@
+import { assert } from "../../assert.mjs";
+import { string_length } from "../../string/length.mjs";
+
 export function js_export_function(function_name, function_statements_code) {
+    assert(string_length(function_name) >= 1);
     return `export function ${ function_name }() { ${ function_statements_code } }`;
 }
