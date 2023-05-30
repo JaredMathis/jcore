1f93b1c2201d7eff75ab809794aceac06633b721
JaredMathis <jared.mathis@native.consulting>
2023-05-29 20:43:17 -0400

    #21

--- src/refactor/console/to/function.mjs
+++ src/refactor/console/to/function.mjs
@@ -1,3 +1,4 @@
+import { list_remove_all } from '../../../list/remove/all.mjs';
 import { list_without_all } from '../../../list/without/all.mjs';
 import { js_import_all } from '../../../js/import/all.mjs';
 import { file_js_parse } from '../../../file/js/parse.mjs';
@@ -6,5 +7,6 @@ export async function refactor_console_to_function(file_path) {
     let parsed = file_js_parse(file_path);
     let imports = js_import_all(parsed);
     let body = js_body_get(parsed);
-    list_without_all(body, imports);
+    let imports_missing = list_without_all(body, imports);
+    list_remove_all();
 }
