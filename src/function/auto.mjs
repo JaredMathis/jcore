43ae57c2b4808277df98ba8c73e1e9ce0949e892
JaredMathis <jared.mathis@native.consulting>
2023-05-30 18:08:03 -0400

    #50 function_auto function_auto

--- src/function/auto.mjs
+++ src/function/auto.mjs
@@ -1,8 +1,6 @@
-import { refactor_functions_to_files } from '../refactor/functions/to/files.mjs';
+import { function_auto_after } from './auto/after.mjs';
 import { function_exists } from './exists.mjs';
 import { function_add } from './add.mjs';
-import { function_map } from './map.mjs';
-import { refactor_import_fix } from '../refactor/import/fix.mjs';
 import { function_open_vs_code } from './open/vs/code.mjs';
 export async function function_auto(function_name) {
     if (!await function_exists(function_name)) {
@@ -10,14 +8,4 @@ export async function function_auto(function_name) {
     }
     await function_auto_after(function_name);
     await function_open_vs_code(function_name);
-}
-
-async function function_auto_after(function_name) {
-    let mappers = [
-        refactor_import_fix,
-        refactor_functions_to_files
-    ];
-    for (let m of mappers) {
-        await function_map(m.name, function_name);
-    }
 }
--- src/function/auto/after.mjs
+++ src/function/auto/after.mjs
@@ -1,4 +1,12 @@
-import { error } from '../../error.mjs';
-export function function_auto_after() {
-    error('todo: function_auto_after');
+import { function_map } from '../map.mjs';
+import { refactor_functions_to_files } from '../../refactor/functions/to/files.mjs';
+import { refactor_import_fix } from '../../refactor/import/fix.mjs';
+export async function function_auto_after(function_name) {
+    let mappers = [
+        refactor_import_fix,
+        refactor_functions_to_files
+    ];
+    for (let m of mappers) {
+        await function_map(m.name, function_name);
+    }
 }
