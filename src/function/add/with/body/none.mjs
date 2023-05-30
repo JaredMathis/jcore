bdc15f28098acba5bf831118bf2248d5fcaa76af
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:28:15 -0400

    #48 function_auto function_add_with_body_none

--- src/function/add/with/body/none.mjs
+++ src/function/add/with/body/none.mjs
@@ -2,6 +2,9 @@ import { file_write } from '../../../../file/write.mjs';
 import { function_name_to_path } from '../../../name/to/path.mjs';
 import { error } from '../../../../error.mjs';
 export async function function_add_with_body_none(function_name) {
+    return await function_add_with_body(function_name);
+}
+async function function_add_with_body(function_name) {
     let function_path = function_name_to_path(function_name);
     await file_write(function_path, `export function ${ function_name }() {
     ${ error.name }('todo: ${ function_name }');
