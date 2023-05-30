2fcce0338c98c26c2f9165e8bb266622a1368c6f
JaredMathis <jared.mathis@native.consulting>
2023-05-29 21:54:55 -0400

    #40

--- src/git/pacp/with/message.mjs
+++ src/git/pacp/with/message.mjs
@@ -1,9 +1,10 @@
 import { command_line_all } from '../../../command/line/all.mjs';
 export async function git_pacp_with_message(commit_message) {
+    const command_commit = `git commit -m "${ commit_message }"`;
     let commands = [
         `git pull`,
         `git add *`,
-        `git commit -m "${ commit_message }"`,
+        command_commit,
         `git push`
     ];
     return command_line_all(commands);
