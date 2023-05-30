d793c4b9f05762d3183d57c66858de9f47e26524
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:31:59 -0400

    #10 function_auto js_unparse

--- src/js/unparse.mjs
+++ src/js/unparse.mjs
@@ -2,7 +2,8 @@ import { js_parse } from './parse.mjs';
 import { try_catch } from '../try/catch.mjs';
 import escodegen from 'escodegen';
 export function js_unparse(parsed) {
-    if (false) js_from(parsed)
+    if (false)
+        js_from(parsed);
     return try_catch(js_parse, lambda);
     function lambda() {
         return escodegen.generate(parsed);
