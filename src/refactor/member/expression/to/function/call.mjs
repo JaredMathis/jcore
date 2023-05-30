b2c9dd9b72ff2d2f49cd3c8cb23f8f911a029b66
JaredMathis <jared.mathis@native.consulting>
2023-05-30 15:35:36 -0400

    #50 function_rename refactor_property_get_to_function_call refactor_member_expression_to_function_call

--- src/refactor/member/expression/to/function/call.mjs
+++ src/refactor/member/expression/to/function/call.mjs
@@ -1,8 +1,7 @@
-import { noop } from '../../../../../noop.mjs';
 import { tautology } from '../../../../../tautology.mjs';
 import { js_visit } from '../../../../../js/visit.mjs';
 import { log } from '../../../../../log.mjs';
-export function refactor_property_get_to_function_call(args) {
+export function refactor_member_expression_to_function_call(args) {
     let {parsed} = args;
     js_visit(parsed, tautology, n => log(n));
 }
