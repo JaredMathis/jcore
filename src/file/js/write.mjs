b844dbacb126bbeb490aa368f1cf4c87ac1589ad
JaredMathis <jared.mathis@native.consulting>
2023-05-30 11:44:59 -0400

    #10 git_pacp

--- src/file/js/write.mjs
+++ src/file/js/write.mjs
@@ -1,6 +1,6 @@
 import { file_write } from '../write.mjs';
 import { js_unparse } from '../../js/unparse.mjs';
-export async function file_js_write(parsed, function_path) {
+export async function file_js_write(function_path, parsed) {
     let unparsed = js_unparse(parsed);
     await file_write(function_path, unparsed);
 }
--- src/function/add/with/declaration.mjs
+++ src/function/add/with/declaration.mjs
@@ -2,6 +2,6 @@ import { file_js_write } from '../../../file/js/write.mjs';
 import { function_name_to_path } from '../../name/to/path.mjs';
 export async function function_add_with_declaration(function_name, parsed) {
     let function_path = function_name_to_path(function_name);
-    await file_js_write(parsed, function_path);
+    await file_js_write(function_path, parsed);
     return function_path;
 }
