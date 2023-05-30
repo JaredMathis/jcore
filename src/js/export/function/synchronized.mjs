0c1b48e450151f17934dbd7d00b32187f8d6c0b7
JaredMathis <jared.mathis@native.consulting>
2023-05-30 14:15:54 -0400

    #10 git_pacp

--- src/js/export/function/synchronized.mjs
+++ src/js/export/function/synchronized.mjs
@@ -2,11 +2,14 @@ import { string_length } from '../../../string/length.mjs';
 import { assert } from '../../../assert.mjs';
 import { js_keyword_async } from '../../keyword/async.mjs';
 import { string_empty } from '../../../string/empty.mjs';
+import { log } from '../../../log.mjs';
 export function js_export_function_synchronized(function_name, is_async, function_statements_code) {
     assert(string_length(function_name) >= 1);
     let async_code = string_empty();
     if (is_async) {
         async_code = js_keyword_async();
     }
-    return `export ${ async_code } function ${ function_name }() { ${ function_statements_code } }`;
+    let result = `export ${ async_code } function ${ function_name }() { ${ function_statements_code } }`;
+    log(result);
+    return result;
 }
