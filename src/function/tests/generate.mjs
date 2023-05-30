dde69349efc02a0443caf62ef930e473c675859f
JaredMathis <jared.mathis@native.consulting>
2023-05-30 14:39:34 -0400

    #10 git_pacp

--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -21,6 +21,6 @@ export async function function_tests_generate() {
     }
     let is_async = true;
     await function_add_with_statements_synchronized(function_name, statements, is_async);
-    const mappers = [refactor_console_to_function.name];
+    const mappers = [refactor_console_to_function.name, refactor_imports_fix.name];
     await file_js_map_multiple(mappers, file_path);
 }
