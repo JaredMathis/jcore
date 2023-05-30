c8bc243bef80e201cf46767bc61abfd696ffa282
JaredMathis <jared.mathis@native.consulting>
2023-05-30 08:17:22 -0400

    #47 function_auto js_exported_function_names

--- src/js/exported/function/names.mjs
+++ src/js/exported/function/names.mjs
@@ -1,11 +1,8 @@
 import { js_exported_function_declarations } from './declarations.mjs';
-import { list_filter } from '../../../list/filter.mjs';
 import { list_map } from '../../../list/map.mjs';
 import { assert } from '../../../assert.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
-import { js_node_is_function_declaration } from '../../node/is/function/declaration.mjs';
 import { js_node_is_identifier } from '../../node/is/identifier.mjs';
-import { js_exports } from '../../exports.mjs';
 export function js_exported_function_names(parsed) {
     let filtered = js_exported_function_declarations(parsed);
     let mapped = list_map(filtered, f => {
@@ -15,13 +12,4 @@ export function js_exported_function_names(parsed) {
         return name;
     });
     return mapped;
-}
-function js_exported_function_declarations(parsed) {
-    let exports = js_exports(parsed);
-    let declarations = list_map(exports, e => {
-        let d = object_property_get(e, 'declaration');
-        return d;
-    });
-    let filtered = list_filter(declarations, d => js_node_is_function_declaration(d));
-    return filtered;
 }
