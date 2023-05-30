6c1eb4d04f12069f0d96990ac79ffcce841db9ea
JaredMathis <jared.mathis@native.consulting>
2023-05-29 22:01:08 -0400

    #40

--- src/data/task/finish.mjs
+++ src/data/task/finish.mjs
@@ -4,10 +4,12 @@ import { todo } from '../../todo.mjs';
 import { file_js_all_map } from '../../file/js/all/map.mjs';
 import { refactor_import_fix } from '../../refactor/import/fix.mjs';
 import { git_pacp_with_message } from '../../git/pacp/with/message.mjs';
+import { assert } from '../../assert.mjs';
 export async function data_task_finish() {
     todo(`Validate the task id is valid i.e. #123 not asdf`);
     let task_id = await data_task_id_get();
     await file_js_all_map(refactor_import_fix.name);
-    await git_pacp_with_message(`closes ${ task_id }`);
+    let result = await git_pacp_with_message(`closes ${ task_id }`);
+    assert(result.success);
     await data_task_id_remove();
 }
