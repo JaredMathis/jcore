063dde559243a61eab4c64aa42f926058c6d2a07
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:09:32 -0400

    #10 function_auto function_add_with_declaration

--- src/function/add/with/declaration.mjs
+++ src/function/add/with/declaration.mjs
@@ -3,12 +3,10 @@ import { object_property_change } from '../../../object/property/change.mjs';
 import { js_export_single } from '../../../js/export/single.mjs';
 import { js_function_name_to_export } from '../../../js/function/name/to/export.mjs';
 import { function_write } from '../../write.mjs';
-import { error } from '../../../error.mjs';
 import { js_parse } from '../../../js/parse.mjs';
 export async function function_add_with_declaration(function_name, fd) {
     let parsed = js_parse(js_function_name_to_export(function_name));
     let export_single = js_export_single(parsed);
     object_property_change(export_single, js_node_property_declaration(), fd);
-    error('todo: function_add_with_declaration');
     return await function_write(function_name, parsed);
 }
