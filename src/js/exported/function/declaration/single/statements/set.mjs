b96a8766a6a04b11ec1739129dba01f720ae339c
JaredMathis <jared.mathis@native.consulting>
2023-05-30 11:35:21 -0400

    #10 git_pacp

--- src/js/exported/function/declaration/single/statements/set.mjs
+++ src/js/exported/function/declaration/single/statements/set.mjs
@@ -5,6 +5,10 @@ import { assert } from '../../../../../../assert.mjs';
 import { js_exported_function_declaration_single } from '../../single.mjs';
 export function js_exported_function_declaration_single_statements_set(parsed, function_body_statements_new) {
     let fd = js_exported_function_declaration_single(parsed);
+    js_function_declaration_statements_initialize(fd, function_body_statements_new);
+}
+
+function js_function_declaration_statements_initialize(fd, function_body_statements_new) {
     let function_body_statements_old = js_function_delcaration_to_statements(fd);
     assert(list_length_is_0(function_body_statements_old));
     list_add_all(function_body_statements_old, function_body_statements_new);
