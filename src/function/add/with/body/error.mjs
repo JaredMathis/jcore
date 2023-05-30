fae42675e70e3587f6fa4ddac9602b1139b8ae44
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:45:33 -0400

    #48 function_auto function_add_with_body_error

--- src/function/add/with/body/error.mjs
+++ src/function/add/with/body/error.mjs
@@ -1,6 +1,8 @@
+import { js_parse_statement } from '../../../../js/parse/statement.mjs';
 import { error } from '../../../../error.mjs';
 import { function_add_with_body } from '../body.mjs';
 export async function function_add_with_body_error(function_name) {
-    let error_statement = `${ error.name }('todo: ${ function_name }');`;
+    let error_statement_code = `${ error.name }('todo: ${ function_name }');`;
+    let error_statement = js_parse_statement(error_statement_code);
     return await function_add_with_body(function_name, []);
 }
