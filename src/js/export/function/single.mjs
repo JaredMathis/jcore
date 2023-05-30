142ae8bb5efd9e63f9789280ddd590e6f3572c9e
JaredMathis <jared.mathis@native.consulting>
2023-05-30 08:09:02 -0400

    #47 git_pacp

--- src/js/export/function/single.mjs
+++ src/js/export/function/single.mjs
@@ -1,5 +1,8 @@
 import { error } from '../../../error.mjs';
+import { list_single } from '../../../list/single.mjs';
 import { js_body_get } from '../../body/get.mjs';
 export function js_export_function_single(parsed) {
     let body = js_body_get(parsed);
+    let exports_existing = js_exports(parsed);
+    let export_single = list_single(exports_existing);
 }
