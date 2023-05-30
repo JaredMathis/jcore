692afd6d4b9c54ddc56c217eaf1162a680f00fda
JaredMathis <jared.mathis@native.consulting>
2023-05-30 15:39:56 -0400

    #50 git_pacp

--- src/js/visit/nodes/identifier.mjs
+++ src/js/visit/nodes/identifier.mjs
@@ -3,5 +3,9 @@ import { js_node_is } from '../../node/is.mjs';
 import { js_visit } from '../../visit.mjs';
 export function js_visit_nodes_identifier(parsed, lambda) {
     let predicate = node => js_node_is_identifier(node)
+    js_visit_nodes(parsed, predicate, lambda);
+}
+
+function js_visit_nodes(parsed, predicate, lambda) {
     js_visit(parsed, node => js_node_is(node) && predicate, lambda);
 }
