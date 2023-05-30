10df3f01894463050f77d832153948ef38bab9f2
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:01:32 -0400

    #48 function_auto refactor_console_to_function

--- src/refactor/console/to/function.mjs
+++ src/refactor/console/to/function.mjs
@@ -1,11 +1,10 @@
+import { js_without_imports } from '../../../js/without/imports.mjs';
 import { list_add_all } from '../../../list/add/all.mjs';
 import { js_node_is_block_statement } from '../../../js/node/is/block/statement.mjs';
 import { js_exported_function_declaration_single } from '../../../js/exported/function/declaration/single.mjs';
 import { file_js_path_to_name } from '../../../file/js/path/to/name.mjs';
 import { list_length_is_0 } from '../../../list/length/is/0.mjs';
 import { list_remove_all } from '../../../list/remove/all.mjs';
-import { list_without_all } from '../../../list/without/all.mjs';
-import { js_import_all } from '../../../js/import/all.mjs';
 import { js_body_get } from '../../../js/body/get.mjs';
 import { js_exports } from '../../../js/exports.mjs';
 import { assert } from '../../../assert.mjs';
@@ -35,11 +34,4 @@ export async function refactor_console_to_function(args) {
     let function_body_statements_old = object_property_get(function_body, 'body');
     assert(list_length_is_0(function_body_statements_old));
     list_add_all(function_body_statements_old, function_body_statements_new);
-}
-
-function js_without_imports(parsed) {
-    let imports = js_import_all(parsed);
-    let body = js_body_get(parsed);
-    let function_body_statements_new = list_without_all(body, imports);
-    return function_body_statements_new;
 }
