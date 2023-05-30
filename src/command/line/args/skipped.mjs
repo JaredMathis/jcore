280f25ce8bbc9cb4c75cda4087964ae7fcb42931
JaredMathis <jared.mathis@native.consulting>
2023-05-29 22:08:53 -0400

    #41:

--- src/command/line/args/skipped.mjs
+++ src/command/line/args/skipped.mjs
@@ -1,4 +1,7 @@
-import { error } from '../../../error.mjs';
+import { list_skip } from '../../../list/skip.mjs';
+import { list_to } from '../../../list/to.mjs';
 export function command_line_args_skipped() {
-    error('todo: command_line_args_skipped');
+    let args_command_line = list_to(process.argv);
+    let skipped = list_skip(args_command_line, 2);
+    return skipped;
 }
