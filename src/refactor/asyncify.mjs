3d50accb9dff32626d7588ded3e892ba560e8989
JaredMathis <jared.mathis@native.consulting>
2023-05-30 11:11:54 -0400

    #10 function_auto refactor_asyncify

--- src/refactor/asyncify.mjs
+++ src/refactor/asyncify.mjs
@@ -1,5 +1,7 @@
+import { js_export_function_single } from '../js/export/function/single.mjs';
 import { error } from '../error.mjs';
 export function refactor_asyncify(args) {
     let {parsed} = args;
+    let f = js_export_function_single(parsed);
     error('todo: refactor_asyncify');
 }
