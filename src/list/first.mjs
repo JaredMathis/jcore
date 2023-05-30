60e8374df33f9205b540b4d6f7ce7a84cb4d5002
JaredMathis <jared.mathis@native.consulting>
2023-05-29 22:17:40 -0400

    #41: git function_auto list_first

--- src/list/first.mjs
+++ src/list/first.mjs
@@ -3,6 +3,3 @@ import { list_index_at } from './index/at.mjs';
 export function list_first(list) {
     return list_index_at(list, list_first_index());
 }
-function list_first_index() {
-    return 0;
-}
