81e6389db55e2144018bad98cb1ebf125511e721
JaredMathis <jared.mathis@native.consulting>
2023-05-30 10:08:56 -0400

    #48 function_auto file_js_all_map

--- src/file/js/all/map.mjs
+++ src/file/js/all/map.mjs
@@ -1,8 +1,6 @@
-import { file_js_map } from '../map.mjs';
+import { file_js_map_multiple } from '../map/multiple.mjs';
 import { file_js_all_path } from './path.mjs';
 export async function file_js_all_map(function_name_mapper) {
     let all = await file_js_all_path();
     await file_js_map_multiple(all, function_name_mapper);
 }
-
-
