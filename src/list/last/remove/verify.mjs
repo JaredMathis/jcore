ce64b2ab27e55667ffd32efab803ab45d224650f
JaredMathis <jared.mathis@native.consulting>
2023-05-30 17:08:56 -0400

    #51 git_pacp

--- src/list/last/remove/verify.mjs
+++ src/list/last/remove/verify.mjs
@@ -1,7 +1,7 @@
 import { equal } from '../../../equal.mjs';
 import { assert } from '../../../assert.mjs';
 import { list_last_remove } from '../remove.mjs';
-export function list_last_remove_verify(stack, node) {
-    let last = list_last_remove(stack);
-    assert(equal(node, last));
+export function list_last_remove_verify(list, last_expected) {
+    let last_actual = list_last_remove(list);
+    assert(equal(last_expected, last_actual));
 }
