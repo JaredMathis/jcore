9e2706a49da3b81e14c2263f3614be74e52c24f9
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:04:03 -0400

    #10 git_pacp

--- src/function/add/with/declaration.mjs
+++ src/function/add/with/declaration.mjs
@@ -3,9 +3,11 @@ import { js_function_name_to_export } from '../../../js/function/name/to/export.
 import { function_write } from '../../write.mjs';
 import { error } from '../../../error.mjs';
 import { js_parse } from '../../../js/parse.mjs';
+import { js_export_declaration } from '../../../js/export/declaration.mjs';
 export async function function_add_with_declaration(function_name, fd) {
     let parsed = js_parse(js_function_name_to_export(function_name));
     let export_single = js_export_single(parsed);
+    let declaration = js_export_declaration(export_single);
     error('todo: function_add_with_declaration');
     return await function_write(function_name, parsed);
 }
--- src/js/export/declaration.mjs
+++ src/js/export/declaration.mjs
@@ -1,5 +1,5 @@
 import { object_property_get } from '../../object/property/get.mjs';
-export function js_export_declaration() {
+export function js_export_declaration(e) {
     let d = object_property_get(e, 'declaration');
     return d;
 }
