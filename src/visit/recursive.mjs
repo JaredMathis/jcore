46720312ac6828dcf5c71b3a01dc0d8c021dcf74
JaredMathis <jared.mathis@native.consulting>
2023-05-30 17:14:12 -0400

    #51 function_auto visit_recursive

--- src/visit/recursive.mjs
+++ src/visit/recursive.mjs
@@ -1,6 +1,12 @@
+import { list_length } from '../list/length.mjs';
 import { list_add_exists_not } from '../list/add/exists/not.mjs';
 import { list_last_remove_verify } from '../list/last/remove/verify.mjs';
 export function visit_recursive(node, children_get, lambda, stack) {
+    let parent = null;
+    if (false)
+        if (list_length(stack) >= 1) {
+            parent = list_last(stack);
+        }
     list_add_exists_not(stack, node);
     lambda({
         node,
