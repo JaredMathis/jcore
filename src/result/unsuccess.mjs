90e4df041b766759653f906612bfdcbb0d64bd5d
JaredMathis <jared.mathis@native.consulting>
2023-05-29 21:48:42 -0400

    #40

--- src/result/unsuccess.mjs
+++ src/result/unsuccess.mjs
@@ -1,3 +1,4 @@
+import { result_success } from './success.mjs';
 import { object_property_set } from '../object/property/set.mjs';
 export function result_unsuccess(result) {
     object_property_set(result, result_success(), false);
