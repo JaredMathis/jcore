e2be925f7a7022c2bae45b9dbd53a4113b1f670c
JaredMathis <jared.mathis@native.consulting>
2023-05-29 23:34:22 -0400

    #47 git_pacp

--- src/list/length/is/1.mjs
+++ src/list/length/is/1.mjs
@@ -1,4 +1,5 @@
 import { list_length } from '../../length.mjs';
 export function list_length_is_1(specifiers) {
-    return list_length(specifiers) === 1;
+    const value = 1;
+    return list_length(specifiers) === value;
 }
