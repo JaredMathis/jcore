274ce8085e6e73d37011609a98659544ad96480b
JaredMathis <jared.mathis@native.consulting>
2023-05-30 14:08:58 -0400

    #10 function_auto function_delete

--- src/function/delete.mjs
+++ src/function/delete.mjs
@@ -1,5 +1,7 @@
 import { function_exists } from './exists.mjs';
 import { assert } from '../assert.mjs';
+import { function_name_to_path } from './name/to/path.mjs';
 export async function function_delete(function_name) {
     assert(await function_exists(function_name));
+    let file_path = function_name_to_path(function_name);
 }
