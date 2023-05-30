79ebc13082ecb49c61d7e9a98b06cba6440944c3
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:52:14 -0400

    #10 git_pacp

--- src/js/function/name/to/import.mjs
+++ src/js/function/name/to/import.mjs
@@ -2,9 +2,13 @@ import { function_extension } from '../../../../function/extension.mjs';
 import { list_single } from '../../../../list/single.mjs';
 import { js_body_get } from '../../../body/get.mjs';
 import { js_parse } from '../../../parse.mjs';
-import { js_parse_statement } from '../../../parse/statement.mjs';
 export function js_function_name_to_import(m) {
     const code = `import {${ m }} from './${ m }${ function_extension() }'`;
+    let statement = js_parse_statement_module(code);
+    return statement;
+}
+
+function js_parse_statement_module(code) {
     let parsed = js_parse(code);
     let statements = js_body_get(parsed);
     let statement = list_single(statements);
