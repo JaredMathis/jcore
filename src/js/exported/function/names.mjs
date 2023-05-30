dcb3d13ce3b1ba6d944c11536e140ec24294359d
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:20:18 -0400

    #48 function_auto js_exported_function_names

--- src/js/exported/function/names.mjs
+++ src/js/exported/function/names.mjs
@@ -1,3 +1,4 @@
+import { js_function_declarations_to_names } from '../../function/declarations/to/names.mjs';
 import { js_function_declaration_to_name } from '../../function/declaration/to/name.mjs';
 import { js_exported_function_declarations } from './declarations.mjs';
 import { list_map } from '../../../list/map.mjs';
@@ -6,7 +7,6 @@ export function js_exported_function_names(parsed) {
     let mapped = js_function_declarations_to_names(filtered);
     return mapped;
 }
-
 function js_function_declarations_to_names(filtered) {
     return list_map(filtered, js_function_declaration_to_name);
 }
