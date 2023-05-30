f3b850ccde86b0e7c5d51ae3624d2e2da68d6878
JaredMathis <jared.mathis@native.consulting>
2023-05-30 15:28:52 -0400

    #50 function_auto refactor_property_get_to_function_call

--- src/refactor/property/get/to/function/call.mjs
+++ src/refactor/property/get/to/function/call.mjs
@@ -1,4 +1,7 @@
-import { error } from '../../../../../error.mjs';
+import { noop } from '../../../../../noop.mjs';
+import { tautology } from '../../../../../tautology.mjs';
+import { js_visit } from '../../../../../js/visit.mjs';
 export function refactor_property_get_to_function_call() {
-    error('todo: refactor_property_get_to_function_call');
+    let {parsed} = args;
+    js_visit(parsed, tautology, noop);
 }
