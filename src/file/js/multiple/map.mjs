2e6150db6a420687f6c96b62634837874ea41fd4
JaredMathis <jared.mathis@native.consulting>
2023-05-30 15:13:33 -0400

    #49 git_pacp

--- src/file/js/multiple/map.mjs
+++ src/file/js/multiple/map.mjs
@@ -1,5 +1,6 @@
 import { file_js_map } from '../map.mjs';
 export async function file_js_multiple_map(function_name_mapper, multiple) {
+    console.log({multiple})
     for (let m of multiple) {
         await file_js_map(function_name_mapper, m);
     }
