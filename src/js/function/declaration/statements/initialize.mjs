4e3c0c8f7e58dbb75783af1f13543d3d8ee780e3
JaredMathis <jared.mathis@native.consulting>
2023-05-30 11:42:02 -0400

    #10 function_auto function_add_with_declaration

--- src/js/function/declaration/statements/initialize.mjs
+++ src/js/function/declaration/statements/initialize.mjs
@@ -2,7 +2,7 @@ import { list_add_all } from '../../../../list/add/all.mjs';
 import { list_length_is_0 } from '../../../../list/length/is/0.mjs';
 import { assert } from '../../../../assert.mjs';
 import { js_function_delcaration_to_statements } from '../../delcaration/to/statements.mjs';
-export function js_function_declaration_statements_initialize() {
+export function js_function_declaration_statements_initialize(fd, function_body_statements_new) {
     let function_body_statements_old = js_function_delcaration_to_statements(fd);
     assert(list_length_is_0(function_body_statements_old));
     list_add_all(function_body_statements_old, function_body_statements_new);
