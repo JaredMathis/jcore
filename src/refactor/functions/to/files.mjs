9602f590d0545d3b47a4965b120492df55c23d75
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:01:52 -0400

    #48 function_auto refactor_functions_to_files

--- src/refactor/functions/to/files.mjs
+++ src/refactor/functions/to/files.mjs
@@ -1,5 +1,5 @@
+import { js_without_imports } from '../../../js/without/imports.mjs';
 export function refactor_functions_to_files(args) {
     let {parsed, file_path} = args;
-
-    let nodes = 
+    let without_imports = js_without_imports(parsed);
 }
