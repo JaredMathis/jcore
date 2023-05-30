290c4e820f866ebd89eb403f8e4d2324724a08ae
JaredMathis <jared.mathis@native.consulting>
2023-05-30 10:57:49 -0400

    #10 git_pacp

--- src/function/all/tests.mjs
+++ src/function/all/tests.mjs
@@ -1,4 +1,5 @@
 import { error } from '../../error.mjs';
 export function function_all_tests() {
-    error('todo: function_all_tests');
+    let all = await file_js_all();
+    let filtered2 = list_filter(filtered, string_function_tests_name_is);
 }
