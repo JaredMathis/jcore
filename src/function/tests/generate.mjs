b9e2eb3734a588cacf398e3c73b3e4cfbde9be44
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:32:01 -0400

    #10 function_auto function_tests_generate

--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -1,14 +1,14 @@
+import { file_overwrite } from '../../file/overwrite.mjs';
+import { string_new_line } from '../../string/new/line.mjs';
 import { list_join } from '../../list/join.mjs';
 import { list_map } from '../../list/map.mjs';
 import { log } from '../../log.mjs';
 import { function_all_tests } from '../all/tests.mjs';
 export async function function_tests_generate() {
-    let file_path = './src/tests.mjs'
+    let file_path = './src/tests.mjs';
     let test_names = await function_all_tests();
-
-    let mapped = list_map(test_names, n => `await ${n}();`)
-    let code = list_join(mapped, string_new_line())
+    let mapped = list_map(test_names, n => `await ${ n }();`);
+    let code = list_join(mapped, string_new_line());
     await file_overwrite(file_path, code);
-    
     console.log(test_names);
 }
