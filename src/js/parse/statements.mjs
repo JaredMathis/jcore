11ff9773ea15fd1ff5e27a05eeaf1e1fbd423da5
JaredMathis <jared.mathis@native.consulting>
2023-05-30 14:04:48 -0400

    #10 function_auto js_parse_statements

--- src/js/parse/statements.mjs
+++ src/js/parse/statements.mjs
@@ -1,16 +1,16 @@
-import { log } from '../../log.mjs';
+import { js_node_property_declaration } from '../node/property/declaration.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
 import { js_export_function_synchronized } from '../export/function/synchronized.mjs';
 import { js_export_single } from '../export/single.mjs';
 import { string_a } from '../../string/a.mjs';
 import { js_parse } from '../parse.mjs';
 import { js_function_delcaration_to_statements } from '../function/delcaration/to/statements.mjs';
-import { json_to } from '../../json/to.mjs';
 export function js_parse_statements(code) {
     let is_async = true;
     let unparsed = js_export_function_synchronized(string_a(), is_async, code);
     let parsed = js_parse(unparsed);
     let export_single = js_export_single(parsed);
-    log(json_to(export_single));
-    let statements = js_function_delcaration_to_statements(export_single);
+    let fd = object_property_get(export_single, js_node_property_declaration());
+    let statements = js_function_delcaration_to_statements(fd);
     return statements;
 }
