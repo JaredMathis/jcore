ca4d3414b57081a972b05457cda32e00d963bad1
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:47:34 -0400

    #10 function_auto js_export_function

--- src/js/export/function.mjs
+++ src/js/export/function.mjs
@@ -1,13 +1,14 @@
-import { assert } from "../../assert.mjs";
-import { string_empty } from "../../string/empty.mjs";
-import { string_length } from "../../string/length.mjs";
-
+import { assert } from '../../assert.mjs';
+import { string_empty } from '../../string/empty.mjs';
+import { string_length } from '../../string/length.mjs';
 export function js_export_function(function_name, function_statements_code) {
     assert(string_length(function_name) >= 1);
-    let is_async = false;
-    let async_code = string_empty();
-    if (is_async) {
-        async_code = 'async';
+    if (false) {
+        let is_async = false;
+        let async_code = string_empty();
+        if (is_async) {
+            async_code = 'async';
+        }
     }
     return `export function ${ function_name }() { ${ function_statements_code } }`;
 }
