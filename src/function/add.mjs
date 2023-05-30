19a51c3baacbbcb65a4dd905e3ff01f60d6ebd24
JaredMathis <jared.mathis@native.consulting>
2023-05-29 20:06:07 -0400

    #13

--- src/function/add.mjs
+++ src/function/add.mjs
@@ -4,7 +4,7 @@ import { file_open_vs_code } from '../file/open/vs/code.mjs';
 export async function function_add(function_name) {
     let function_path = function_name_to_path(function_name);
     await file_write(function_path, `export function ${ function_name }() {
-    
+    todo('${function_name}');
 }`);
     await file_open_vs_code(function_path);
 }
