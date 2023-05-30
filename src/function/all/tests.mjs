b127251a5a90e21d22d13b1704ad99f7c81ef181
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:17:20 -0400

    #10 git_pacp

--- src/function/all/tests.mjs
+++ src/function/all/tests.mjs
@@ -3,5 +3,5 @@ import { list_filter } from '../../list/filter.mjs';
 import { file_js_all } from '../../file/js/all.mjs';
 export async function function_all_tests() {
     let all = await file_js_all();
-    let filtered2 = list_filter(filtered, string_function_tests_name_is);
+    return list_filter(all, string_function_tests_name_is);
 }
