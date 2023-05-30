423653da6c5e9a8f91a3fc26a93f33102953dca5
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:44:43 -0400

    #48 function_auto function_add_with_body_error

--- src/function/add/with/body/error.mjs
+++ src/function/add/with/body/error.mjs
@@ -1,3 +1,4 @@
+import { error } from '../../../../error.mjs';
 import { function_add_with_body } from '../body.mjs';
 export async function function_add_with_body_error(function_name) {
     let error_statement = `${ error.name }('todo: ${ function_name }');`;
