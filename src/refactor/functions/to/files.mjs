fac9b7400ccee3e74f232aaf9044e5d83b796d10
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:21:06 -0400

    #48 function_auto refactor_functions_to_files

--- src/refactor/functions/to/files.mjs
+++ src/refactor/functions/to/files.mjs
@@ -1,3 +1,5 @@
+import { log } from '../../../log.mjs';
+import { js_function_declarations_to_names } from '../../../js/function/declarations/to/names.mjs';
 import { js_node_is_function_declaration } from '../../../js/node/is/function/declaration.mjs';
 import { js_without_imports } from '../../../js/without/imports.mjs';
 import { list_filter } from '../../../list/filter.mjs';
@@ -5,8 +7,6 @@ export function refactor_functions_to_files(args) {
     let {parsed} = args;
     let without_imports = js_without_imports(parsed);
     let functions_to_export = list_filter(without_imports, js_node_is_function_declaration);
-
-    let function_names_new = list_map(functions_to_export, js_function_declaration_to_name);
-
-    console.log({functions_to_export})
+    let function_names_new = js_function_declarations_to_names(functions_to_export);
+    console.log({ functions_to_export });
 }
