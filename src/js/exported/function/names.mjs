4e9b6dd7980b3d15e40a7be52a09e29af35ff3bd
JaredMathis <jared.mathis@native.consulting>
2023-05-29 23:33:06 -0400

    #47 git_pacp

--- src/js/exported/function/names.mjs
+++ src/js/exported/function/names.mjs
@@ -6,6 +6,7 @@ import { js_body_get } from '../../body/get.mjs';
 import { js_node_is_export_named_declaration } from '../../node/is/export/named/declaration.mjs';
 import { js_node_is_function_declaration } from '../../node/is/function/declaration.mjs';
 import { js_node_is_identifier } from '../../node/is/identifier.mjs';
+import { js_exports } from '../../exports.mjs';
 export function js_exported_function_names(parsed) {
     let exports = js_exports(parsed);
     let declarations = list_map(exports, e => {
@@ -20,10 +21,4 @@ export function js_exported_function_names(parsed) {
         return name;
     });
     return mapped;
-}
-
-function js_exports(parsed) {
-    let body = js_body_get(parsed);
-    let exports = list_filter(body, b => js_node_is_export_named_declaration(b));
-    return exports;
 }
