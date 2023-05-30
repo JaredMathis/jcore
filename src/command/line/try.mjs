28b881b46f43bf39e7fe5e37efcc209a68bec578
JaredMathis <jared.mathis@native.consulting>
2023-05-29 21:28:38 -0400

    #40

--- src/command/line/try.mjs
+++ src/command/line/try.mjs
@@ -1,10 +1,4 @@
-import { log } from '../../log.mjs';
 import { command_line } from '../line.mjs';
 export async function command_line_try(command_text) {
-    try {
-        await command_line(command_text);
-    } catch (e) {
-        log(e.stdout);
-        throw e;
-    }
+    await command_line(command_text);
 }
