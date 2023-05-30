bca45e6cd095ed7744be31262aac191e6bdf4cb4
JaredMathis <jared.mathis@native.consulting>
2023-05-30 19:07:54 -0400

    #1 function_auto tests_add

--- src/tests/add.mjs
+++ src/tests/add.mjs
@@ -1,5 +1,7 @@
-import { function_tests_all } from '../function/tests/all.mjs';
 import { tests } from '../tests.mjs';
+import { function_tests_all } from '../function/tests/all.mjs';
+import { log } from '../log.mjs';
 export async function tests_add(function_name) {
     let tests = await function_tests_all(function_name);
+    log(tests);
 }
