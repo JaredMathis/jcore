e44aa39d63492177bb223b317179adc593384ed2
JaredMathis <jared.mathis@native.consulting>
2023-05-30 11:39:02 -0400

    #10 function_auto function_add_with_body

--- src/function/add/with/body.mjs
+++ src/function/add/with/body.mjs
@@ -1,13 +1,15 @@
+import { js_function_declaration_statements_initialize } from '../../../js/function/declaration/statements/initialize.mjs';
+import { js_exported_function_declaration_single } from '../../../js/exported/function/declaration/single.mjs';
 import { file_js_write } from '../../../file/js/write.mjs';
 import { js_function_name_to_export } from '../../../js/function/name/to/export.mjs';
-import { js_exported_function_declaration_single_statements_initialize } from '../../../js/exported/function/declaration/single/statements/initialize.mjs';
 import { function_name_to_path } from '../../name/to/path.mjs';
 import { js_parse } from '../../../js/parse.mjs';
 export async function function_add_with_body(function_name, body) {
     let function_path = function_name_to_path(function_name);
     const code = js_function_name_to_export(function_name);
     let parsed = js_parse(code);
-    js_exported_function_declaration_single_statements_initialize(parsed, body);
+    let fd = js_exported_function_declaration_single(parsed);
+    js_function_declaration_statements_initialize(fd, function_body_statements_new);
     await file_js_write(parsed, function_path);
     return function_path;
 }
