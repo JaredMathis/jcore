6abbeaf70d19e800c3b1519f896cfd1ba3e231a9
JaredMathis <jared.mathis@native.consulting>
2023-05-30 17:18:03 -0400

    #51 function_auto visit_recursive

---
+++ src/list/last/or/null.mjs
@@ -0,0 +1,9 @@
+import { list_last } from '../../last.mjs';
+import { list_length } from '../../length.mjs';
+export function list_last_or_null(stack) {
+    let parent = null;
+    if (list_length(stack) >= 1) {
+        parent = list_last(stack);
+    }
+    return parent;
+}
--- src/visit/recursive.mjs
+++ src/visit/recursive.mjs
@@ -1,12 +1,8 @@
-import { list_last } from '../list/last.mjs';
-import { list_length } from '../list/length.mjs';
+import { list_last_or_null } from '../list/last/or/null.mjs';
 import { list_add_exists_not } from '../list/add/exists/not.mjs';
 import { list_last_remove_verify } from '../list/last/remove/verify.mjs';
 export function visit_recursive(node, children_get, lambda, stack) {
-    let parent = null;
-    if (list_length(stack) >= 1) {
-        parent = list_last(stack);
-    }
+    let parent = list_last_or_null(stack);
     list_add_exists_not(stack, node);
     lambda({
         node,
