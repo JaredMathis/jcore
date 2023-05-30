b278040e98c64d5eea7af0399d9e21c9e5aef7b1
JaredMathis <jared.mathis@native.consulting>
2023-05-30 17:36:38 -0400

    #50 function_auto refactor_member_expression_to_function_call

--- src/refactor/member/expression/to/function/call.mjs
+++ src/refactor/member/expression/to/function/call.mjs
@@ -1,3 +1,5 @@
+import { js_parse_statement } from '../../../../../js/parse/statement.mjs';
+import { log } from '../../../../../log.mjs';
 import { object_property_get } from '../../../../../object/property/get.mjs';
 import { js_visit_nodes } from '../../../../../js/visit/nodes.mjs';
 import { js_node_is_type_member_expression } from '../../../../../js/node/is/type/member/expression.mjs';
@@ -7,10 +9,10 @@ export function refactor_member_expression_to_function_call(args) {
         let node = object_property_get(v, 'node');
         let parent = object_property_get(v, 'parent');
         let property = object_property_get(node, 'property');
-        let property_name =  object_property_get(property, 'name');
-        console.log({property_name})
-        if (property_name=== 'name') {
-            console.log(parent)
+        let property_name = object_property_get(property, 'name');
+        if (property_name === 'name') {
+            console.log(parent);
+            js_parse_statement();
         }
     });
 }
