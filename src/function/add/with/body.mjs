153a64a520c2c4bbbabf414e20fc0b7a412b879e
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:30:30 -0400

    #48 function_auto function_add_with_body

--- src/function/add/with/body.mjs
+++ src/function/add/with/body.mjs
@@ -1,11 +1,13 @@
 import { file_write } from '../../../file/write.mjs';
 import { function_name_to_path } from '../../name/to/path.mjs';
 import { error } from '../../../error.mjs';
+import { js_parse } from '../../../js/parse.mjs';
 export async function function_add_with_body(function_name, body) {
     let function_path = function_name_to_path(function_name);
     const code = `export function ${ function_name }() {
     ${ error.name }('todo: ${ function_name }');
 }`;
+    let parsed = js_parse(code);
     await file_write(function_path, code);
     return function_path;
 }
