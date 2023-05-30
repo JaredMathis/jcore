3cc816353bb7a89d0acf920acc2a5635366079c9
JaredMathis <jared.mathis@native.consulting>
2023-05-30 15:51:44 -0400

    #50 git_pacp

--- src/refactor/member/expression/to/function/call.mjs
+++ src/refactor/member/expression/to/function/call.mjs
@@ -1,7 +1,11 @@
+import { js_visit_nodes } from '../../../../../js/visit/nodes.mjs';
 import { js_node_is_type_member_expression } from '../../../../../js/node/is/type/member/expression.mjs';
-import { js_visit } from '../../../../../js/visit.mjs';
 import { log } from '../../../../../log.mjs';
 export function refactor_member_expression_to_function_call(args) {
     let {parsed} = args;
-    js_visit_nodes(parsed, js_node_is_type_member_expression, n => log(n));
+    js_visit_nodes(parsed, js_node_is_type_member_expression, n => {
+        if (object_property_get(w, 'name') === 'name') {
+            
+        }
+    });
 }
