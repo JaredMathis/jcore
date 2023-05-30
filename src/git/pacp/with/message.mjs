97b2fc033dd7b83b7ecb11a2f54f8131ff983bab
JaredMathis <jared.mathis@native.consulting>
2023-05-29 22:14:43 -0400

    #41: git function_auto git_pacp_with_message

--- src/git/pacp/with/message.mjs
+++ src/git/pacp/with/message.mjs
@@ -1,9 +1,16 @@
+import { git } from '../../../git.mjs';
 import { list_join } from '../../../list/join.mjs';
 import { command_line_args_skipped } from '../../../command/line/args/skipped.mjs';
 import { command_line_all } from '../../../command/line/all.mjs';
 import { result_empty } from '../../../result/empty.mjs';
+import { list_first } from '../../../list/first.mjs';
 export async function git_pacp_with_message(commit_message) {
     let args = command_line_args_skipped();
+    if (false) {
+        while (list_first(args, a => a === git.name)) {
+            list_first_remove(args);
+        }
+    }
     let args_message = list_join(args, ' ');
     const command_commit = `git commit -m "${ commit_message }: ${ args_message }"`;
     let commands = [
