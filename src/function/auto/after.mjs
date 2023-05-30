b1192e87e253fff87fa324c2236329b1643ad590
JaredMathis <jared.mathis@native.consulting>
2023-05-30 18:10:10 -0400

    #50 git_pacp

--- src/function/auto/after.mjs
+++ src/function/auto/after.mjs
@@ -2,11 +2,11 @@ import { function_map } from '../map.mjs';
 import { refactor_functions_to_files } from '../../refactor/functions/to/files.mjs';
 import { refactor_import_fix } from '../../refactor/import/fix.mjs';
 export async function function_auto_after(function_name) {
-    let mappers = [
+    let refactors = [
         refactor_import_fix,
         refactor_functions_to_files
     ];
-    for (let m of mappers) {
+    for (let m of refactors) {
         await function_map(m.name, function_name);
     }
 }
