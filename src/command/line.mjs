99339984975b4659588a38e5bf2b2a211a2a2e1a
JaredMathis <jared.mathis@native.consulting>
2023-05-29 21:25:34 -0400

    #40

--- src/command/line.mjs
+++ src/command/line.mjs
@@ -3,12 +3,14 @@ import { exec } from 'child_process';
 export function command_line(command) {
     return new Promise(function (resolve, reject) {
         exec(command, (error, stdout, stderr) => {
+            let success = true;
             let result = {
                 error,
                 stdout,
                 stderr
             };
             if (error) {
+                success = false;
                 reject(result);
                 return;
             }
