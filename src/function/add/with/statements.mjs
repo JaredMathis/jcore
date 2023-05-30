7a3740bc2c8aedf0649e3814e311e655c6474935
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:36:43 -0400

    #10 git_pacp

--- src/function/add/with/statements.mjs
+++ src/function/add/with/statements.mjs
@@ -3,10 +3,13 @@ import { js_function_declaration_statements_initialize } from '../../../js/funct
 import { js_exported_function_declaration_single } from '../../../js/exported/function/declaration/single.mjs';
 import { js_function_name_to_export } from '../../../js/function/name/to/export.mjs';
 import { js_parse } from '../../../js/parse.mjs';
+import { error } from '../../../error.mjs';
 export async function function_add_with_statements(function_name, statements) {
     const code = js_function_name_to_export(function_name);
     let parsed = js_parse(code);
     let fd = js_exported_function_declaration_single(parsed);
     js_function_declaration_statements_initialize(fd, statements);
+    console.log(statements);
+    error()
     return await function_write(function_name, parsed);
 }
