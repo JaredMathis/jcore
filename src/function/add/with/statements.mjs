057fe7ea8f8b34793c1d98f3cb6debff1d45e17e
JaredMathis <jared.mathis@native.consulting>
2023-05-30 14:24:11 -0400

    #10 git_pacp

--- src/function/add/with/statements.mjs
+++ src/function/add/with/statements.mjs
@@ -1,5 +1,5 @@
 import { function_add_with_statements_synchronized } from './statements/synchronized.mjs';
 export async function function_add_with_statements(function_name, statements) {
     let is_async = false;
-    return function_add_with_statements_synchronized(function_name, statements, is_async);
+    return await function_add_with_statements_synchronized(function_name, statements, is_async);
 }
