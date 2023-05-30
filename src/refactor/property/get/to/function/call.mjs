4b75cc73e395911b9ef76184c57d177ffea0bcca
JaredMathis <jared.mathis@native.consulting>
2023-05-30 15:33:55 -0400

    #50 git_pacp

--- src/refactor/property/get/to/function/call.mjs
+++ src/refactor/property/get/to/function/call.mjs
@@ -1,7 +1,8 @@
 import { noop } from '../../../../../noop.mjs';
 import { tautology } from '../../../../../tautology.mjs';
 import { js_visit } from '../../../../../js/visit.mjs';
+import { log } from '../../../../../log.mjs';
 export function refactor_property_get_to_function_call() {
     let {parsed} = args;
-    js_visit(parsed, tautology, noop);
+    js_visit(parsed, tautology, n => log(n));
 }
