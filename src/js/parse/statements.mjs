47182eac7ab01a55cb86aec6d977ec6e38eedab4
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:40:57 -0400

    #10 function_auto js_parse_statements

--- src/js/parse/statements.mjs
+++ src/js/parse/statements.mjs
@@ -1,13 +1,12 @@
 import { js_export_single } from '../export/single.mjs';
 import { string_a } from '../../string/a.mjs';
-import { js_body_get } from '../body/get.mjs';
 import { js_parse } from '../parse.mjs';
 import { js_export_function } from '../export/function.mjs';
+import { js_function_delcaration_to_statements } from '../function/delcaration/to/statements.mjs';
 export function js_parse_statements(code) {
-    let r = js_export_function(string_a(), code);
-    let parsed = js_parse(code);
-    if (false)
-        js_export_single;
-    let body = js_body_get(parsed);
-    return body;
+    let unparsed = js_export_function(string_a(), code);
+    let parsed = js_parse(unparsed);
+    let export_single = js_export_single(parsed);
+    let statements = js_function_delcaration_to_statements(export_single);
+    return statements;
 }
