c9b3f4303ea0d024305c69b5139cdc86f21adb3e
JaredMathis <jared.mathis@native.consulting>
2023-05-30 11:32:00 -0400

    #10 function_auto function_add_with_body

--- src/function/add/with/body.mjs
+++ src/function/add/with/body.mjs
@@ -1,7 +1,6 @@
+import { file_js_write } from '../../../file/js/write.mjs';
 import { js_function_name_to_export } from '../../../js/function/name/to/export.mjs';
 import { js_exported_function_declaration_single_statements_set } from '../../../js/exported/function/declaration/single/statements/set.mjs';
-import { js_unparse } from '../../../js/unparse.mjs';
-import { file_write } from '../../../file/write.mjs';
 import { function_name_to_path } from '../../name/to/path.mjs';
 import { js_parse } from '../../../js/parse.mjs';
 export async function function_add_with_body(function_name, body) {
@@ -12,5 +11,3 @@ export async function function_add_with_body(function_name, body) {
     await file_js_write(parsed, function_path);
     return function_path;
 }
-
-
