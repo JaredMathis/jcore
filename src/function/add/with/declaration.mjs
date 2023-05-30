704e0a677b4da47cba3129cbc5f5c207e2b5561b
JaredMathis <jared.mathis@native.consulting>
2023-05-30 11:58:56 -0400

    #10 function_auto function_add_with_declaration

--- src/function/add/with/declaration.mjs
+++ src/function/add/with/declaration.mjs
@@ -1,13 +1,11 @@
-import { list_single } from '../../../list/single.mjs';
-import { js_exports } from '../../../js/exports.mjs';
+import { js_export_single } from '../../../js/export/single.mjs';
 import { js_function_name_to_export } from '../../../js/function/name/to/export.mjs';
 import { function_write } from '../../write.mjs';
 import { error } from '../../../error.mjs';
 import { js_parse } from '../../../js/parse.mjs';
 export async function function_add_with_declaration(function_name, fd) {
     let parsed = js_parse(js_function_name_to_export(function_name));
-    let exports = js_exports(parsed);
-    let export_single = list_single(exports);
+    js_export_single(parsed);
     error('todo: function_add_with_declaration');
     return await function_write(function_name, parsed);
 }
---
+++ src/js/export/single.mjs
@@ -0,0 +1,6 @@
+import { list_single } from '../../list/single.mjs';
+import { js_exports } from '../exports.mjs';
+export function js_export_single() {
+    let exports = js_exports(parsed);
+    let export_single = list_single(exports);
+}
