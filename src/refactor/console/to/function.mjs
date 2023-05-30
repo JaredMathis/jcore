a0e2d5bbb80927f31e1071a56c59235810a6302c
JaredMathis <jared.mathis@native.consulting>
2023-05-29 23:39:00 -0400

    #47 git_pacp

--- src/refactor/console/to/function.mjs
+++ src/refactor/console/to/function.mjs
@@ -8,8 +8,8 @@ import { js_exports } from '../../../js/exports.mjs';
 import { assert } from '../../../assert.mjs';
 export async function refactor_console_to_function(file_path) {
     let parsed = await file_js_parse(file_path);
-    let exports = js_exports(parsed);
-    assert(list_length_is_0(exports));
+    let exports_existing = js_exports(parsed);
+    assert(list_length_is_0(exports_existing));
     let imports = js_import_all(parsed);
     let body = js_body_get(parsed);
     let function_body = list_without_all(body, imports);
