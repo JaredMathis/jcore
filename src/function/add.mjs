54df4f718a9cc2854eced40b5ab0ad7ae980fdb7
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:27:40 -0400

    #48 function_auto function_add

--- src/function/add.mjs
+++ src/function/add.mjs
@@ -1,3 +1,4 @@
+import { function_add_with_body_none } from './add/with/body/none.mjs';
 import { file_write } from '../file/write.mjs';
 import { function_name_to_path } from './name/to/path.mjs';
 import { file_open_vs_code } from '../file/open/vs/code.mjs';
@@ -6,11 +7,10 @@ export async function function_add(function_name) {
     let function_path = await function_add_with_body_none(function_name);
     await file_open_vs_code(function_path);
 }
-
 async function function_add_with_body_none(function_name) {
     let function_path = function_name_to_path(function_name);
-    await file_write(function_path, `export function ${function_name}() {
-    ${error.name}('todo: ${function_name}');
+    await file_write(function_path, `export function ${ function_name }() {
+    ${ error.name }('todo: ${ function_name }');
 }`);
     return function_path;
 }
