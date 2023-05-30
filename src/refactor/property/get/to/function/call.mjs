16e6587557706a0c962ccfa7c1893a0798964ec9
JaredMathis <jared.mathis@native.consulting>
2023-05-30 15:38:02 -0400

    #50 function_delete refactor_property_get_to_function_call

--- src/refactor/property/get/to/function/call.mjs
+++
@@ -1,7 +0,0 @@
-import { js_node_is_type_member_expression } from '../../../../../js/node/is/type/member/expression.mjs';
-import { js_visit } from '../../../../../js/visit.mjs';
-import { log } from '../../../../../log.mjs';
-export function refactor_property_get_to_function_call(args) {
-    let {parsed} = args;
-    js_visit(parsed, n => js_node_is_type_member_expression(n), n => log(n));
-}
