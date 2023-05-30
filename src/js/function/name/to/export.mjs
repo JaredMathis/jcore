418195f65dcdca61770891f130dc34f7abcaba9d
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:17:17 -0400

    #10 git_pacp

--- src/js/function/name/to/export.mjs
+++ src/js/function/name/to/export.mjs
@@ -1,4 +1,8 @@
 export function js_function_name_to_export(function_name) {
     let function_statements_code = ''
-    return `export function ${ function_name }() { ${function_statements_code} }`;
+    return js_export_function(function_name, function_statements_code);
+}
+
+function js_export_function(function_name, function_statements_code) {
+    return `export function ${function_name}() { ${function_statements_code} }`;
 }
