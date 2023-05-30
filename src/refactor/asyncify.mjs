86e50d460bdbb94c2f3e8321e8f39e2ee21a9e02
JaredMathis <jared.mathis@native.consulting>
2023-05-30 14:17:50 -0400

    #10 git_pacp

--- src/refactor/asyncify.mjs
+++ src/refactor/asyncify.mjs
@@ -1,7 +1,8 @@
 import { object_property_change } from '../object/property/change.mjs';
 import { js_export_function_single } from '../js/export/function/single.mjs';
+import { js_keyword_async } from '../js/keyword/async.mjs';
 export function refactor_asyncify(args) {
     let {parsed} = args;
     let f = js_export_function_single(parsed);
-    object_property_change(f, 'async', true);
+    object_property_change(f, js_keyword_async(), true);
 }
