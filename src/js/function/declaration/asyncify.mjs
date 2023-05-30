697d5054e855a5613c080cda5cc5535e773c872b
JaredMathis <jared.mathis@native.consulting>
2023-05-30 14:19:41 -0400

    #10 function_auto refactor_asyncify

---
+++ src/js/function/declaration/asyncify.mjs
@@ -0,0 +1,5 @@
+import { js_keyword_async } from '../../keyword/async.mjs';
+import { object_property_change } from '../../../object/property/change.mjs';
+export function js_function_declaration_asyncify(fd) {
+    object_property_change(fd, js_keyword_async(), true);
+}
--- src/refactor/asyncify.mjs
+++ src/refactor/asyncify.mjs
@@ -1,12 +1,7 @@
-import { object_property_change } from '../object/property/change.mjs';
+import { js_function_declaration_asyncify } from '../js/function/declaration/asyncify.mjs';
 import { js_export_function_single } from '../js/export/function/single.mjs';
-import { js_keyword_async } from '../js/keyword/async.mjs';
 export function refactor_asyncify(args) {
     let {parsed} = args;
     let fd = js_export_function_single(parsed);
     js_function_declaration_asyncify(fd);
-}
-
-function js_function_declaration_asyncify(fd) {
-    object_property_change(fd, js_keyword_async(), true);
 }
