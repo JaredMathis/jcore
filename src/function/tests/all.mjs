7d444b77b8387f64aacbde4f012458d23908f438
JaredMathis <jared.mathis@native.consulting>
2023-05-30 10:58:15 -0400

    #10 git_pacp

--- src/function/tests/all.mjs
+++ src/function/tests/all.mjs
@@ -5,7 +5,11 @@ import { string_starts_with } from '../../string/starts/with.mjs';
 import { function_name_separator } from '../name/separator.mjs';
 export async function function_tests_all(function_name) {
     let all = await file_js_all();
-    let filtered = list_filter(all, a => string_starts_with(a, function_name + function_name_separator()));
+    let filtered = filter(all);
     let filtered2 = list_filter(filtered, string_function_tests_name_is);
     return filtered2;
+
+    function filter(all) {
+        return list_filter(all, a => string_starts_with(a, function_name + function_name_separator()));
+    }
 }
