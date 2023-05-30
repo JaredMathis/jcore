06e736e48240eca4331b4085da4fb76d6d1b1212
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:48:23 -0400

    #48 git_pacp

--- src/function/add/with/body.mjs
+++ src/function/add/with/body.mjs
@@ -5,10 +5,14 @@ import { function_name_to_path } from '../../name/to/path.mjs';
 import { js_parse } from '../../../js/parse.mjs';
 export async function function_add_with_body(function_name, body) {
     let function_path = function_name_to_path(function_name);
-    const code = `export function ${ function_name }() {}`;
+    const code = js_function_name_to_export(function_name);
     let parsed = js_parse(code);
     js_exported_function_declaration_single_statements_set(parsed, body);
     let unparsed = js_unparse(parsed);
     await file_write(function_path, unparsed);
     return function_path;
+}
+
+function js_function_name_to_export(function_name) {
+    return `export function ${function_name}() {}`;
 }
