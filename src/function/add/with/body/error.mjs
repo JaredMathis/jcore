31e7e535132e338c28376c3c6a1776f831105e40
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:44:27 -0400

    #48 git_pacp

--- src/function/add/with/body/error.mjs
+++ src/function/add/with/body/error.mjs
@@ -1,4 +1,5 @@
 import { function_add_with_body } from '../body.mjs';
 export async function function_add_with_body_error(function_name) {
+    let error_statement = `${ error.name }('todo: ${ function_name }');`;
     return await function_add_with_body(function_name, []);
 }
