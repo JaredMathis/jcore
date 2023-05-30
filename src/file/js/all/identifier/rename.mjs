56210d23f025445aa6d321c5da1a2fc1fce58b78
JaredMathis <jared.mathis@native.consulting>
2023-05-30 15:01:31 -0400

    #49 git_pacp

--- src/file/js/all/identifier/rename.mjs
+++ src/file/js/all/identifier/rename.mjs
@@ -2,10 +2,13 @@ import { js_identifier_rename } from '../../../../js/identifier/rename.mjs';
 import { list_add_all } from '../../../../list/add/all.mjs';
 import { file_js_all_map_args } from '../map/args.mjs';
 export async function file_js_all_identifier_rename(identifier_name_old, identifier_name_new) {
-    let file_paths_all_changed = [];
+    let file_paths_changed = [];
     await file_js_all_map_args(function mapper(args) {
-        let file_paths_changed = js_identifier_rename(args, identifier_name_old, identifier_name_new);
-        list_add_all(file_paths_all_changed, file_paths_changed)
+        let changed = js_identifier_rename(args, identifier_name_old, identifier_name_new);
+        if (changed) {
+            let file_path = object_property_get(args, 'file_path');
+            list_add_all(file_paths_changed, file_path)
+        }
     });
-    return file_paths_all_changed;
+    return file_paths_changed;
 }
