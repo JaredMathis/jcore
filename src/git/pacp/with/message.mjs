1810e97da48988b9cbde9e9126c5fdc20c862f24
JaredMathis <jared.mathis@native.consulting>
2023-05-29 22:21:36 -0400

    #41: function_auto git_pacp_with_message

--- src/git/pacp/with/message.mjs
+++ src/git/pacp/with/message.mjs
@@ -1,14 +1,12 @@
+import { list_remove_all_first_equals } from '../../../list/remove/all/first/equals.mjs';
 import { git } from '../../../git.mjs';
 import { list_join } from '../../../list/join.mjs';
 import { command_line_args_skipped } from '../../../command/line/args/skipped.mjs';
 import { command_line_all } from '../../../command/line/all.mjs';
 import { result_empty } from '../../../result/empty.mjs';
-import { list_first } from '../../../list/first.mjs';
 export async function git_pacp_with_message(commit_message) {
     let args = command_line_args_skipped();
-    if (false) {
-        list_remove_all_first_equals(args, git.name);
-    }
+    list_remove_all_first_equals(args, git.name);
     let args_message = list_join(args, ' ');
     const command_commit = `git commit -m "${ commit_message }: ${ args_message }"`;
     let commands = [
@@ -28,5 +26,3 @@ export async function git_pacp_with_message(commit_message) {
     }
     return c_result;
 }
-
-
--- src/list/remove/all/first/equals.mjs
+++ src/list/remove/all/first/equals.mjs
@@ -1,7 +1,7 @@
 import { list_first_remove } from '../../../first/remove.mjs';
 import { list_first } from '../../../first.mjs';
 export function list_remove_all_first_equals(args, value) {
-    while (list_first(args, a => a === value)) {
+    while (list_first(args) === value) {
         list_first_remove(args);
     }
 }
