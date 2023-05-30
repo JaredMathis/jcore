8fa4c11dd27c35e56adf3c7c2bd1d4f580c0b649
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:18:04 -0400

    #10 function_auto js_function_name_to_export

--- src/js/function/name/to/export.mjs
+++ src/js/function/name/to/export.mjs
@@ -1,5 +1,9 @@
+import { string_empty } from '../../../../string/empty.mjs';
 import { js_export_function } from '../../../export/function.mjs';
 export function js_function_name_to_export(function_name) {
     let function_statements_code = '';
+    if (false) {
+        string_empty();
+    }
     return js_export_function(function_name, function_statements_code);
 }
