8050ccbe6ffc9a941a386db7758ac83352718ef5
JaredMathis <jared.mathis@native.consulting>
2023-05-29 22:17:59 -0400

    #41: git function_auto list_first

--- src/list/first/remove.mjs
+++ src/list/first/remove.mjs
@@ -1,5 +1,6 @@
 import { error } from '../../error.mjs';
 import { list_remove_at } from '../remove/at.mjs';
+import { list_first_index } from './index.mjs';
 export function list_first_remove(list) {
-    list_remove_at(list, 0);
+    list_remove_at(list, list_first_index());
 }
