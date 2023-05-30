80f86dc4310108330a0d2bac6281747c69898b07
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:02:55 -0400

    #10 function_auto js_exported_function_declarations

---
+++ src/js/export/declaration.mjs
@@ -0,0 +1,5 @@
+import { object_property_get } from '../../object/property/get.mjs';
+export function js_export_declaration() {
+    let d = object_property_get(e, 'declaration');
+    return d;
+}
--- src/js/exported/function/declarations.mjs
+++ src/js/exported/function/declarations.mjs
@@ -1,6 +1,6 @@
+import { js_export_declaration } from '../../export/declaration.mjs';
 import { js_node_is_function_declaration } from '../../node/is/function/declaration.mjs';
 import { list_filter } from '../../../list/filter.mjs';
-import { object_property_get } from '../../../object/property/get.mjs';
 import { list_map } from '../../../list/map.mjs';
 import { js_exports } from '../../exports.mjs';
 export function js_exported_function_declarations(parsed) {
@@ -8,7 +8,4 @@ export function js_exported_function_declarations(parsed) {
     let declarations = list_map(exports, js_export_declaration);
     let filtered = list_filter(declarations, d => js_node_is_function_declaration(d));
     return filtered;
-}function js_export_declaration(e) {
-    let d = object_property_get(e, 'declaration');
-    return d;
 }
