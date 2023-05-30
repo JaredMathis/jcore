c12eb273e406d7af2af8781233f82dd4362cb9f3
JaredMathis <jared.mathis@native.consulting>
2023-05-30 09:00:26 -0400

    #48 git_pacp

--- src/js/without/imports.mjs
+++ src/js/without/imports.mjs
@@ -1,4 +1,7 @@
 import { error } from '../../error.mjs';
-export function js_without_imports() {
-    error('todo: js_without_imports');
+export function js_without_imports(parsed, body) {
+    let imports = js_import_all(parsed);
+    let body = js_body_get(parsed);
+    let function_body_statements_new = list_without_all(body, imports);
+    return function_body_statements_new;
 }
