e68e1805ecce82b6811048163fa9b6d1b8488e41
JaredMathis <jared.mathis@native.consulting>
2023-05-30 11:04:50 -0400

    #10 git_pacp

--- src/function/tests/all.mjs
+++ src/function/tests/all.mjs
@@ -5,12 +5,10 @@ import { string_starts_with } from '../../string/starts/with.mjs';
 import { function_name_separator } from '../name/separator.mjs';
 export async function function_tests_all(function_name) {
     return await function_tests_filtered(filter);
-
     function filter(all) {
         return list_filter(all, a => string_starts_with(a, function_name + function_name_separator()));
     }
 }
-
 async function function_tests_filtered(filter) {
     let all = await file_js_all();
     let filtered = filter(all);
--- src/js/parse.mjs
+++ src/js/parse.mjs
@@ -1,8 +1,12 @@
 import { Parser } from 'acorn';
 export function js_parse(unparsed) {
-    let parsed = Parser.parse(unparsed, {
-        sourceType: 'module',
-        ecmaVersion: '2020'
-    });
-    return parsed;
+    try {
+        let parsed = Parser.parse(unparsed, {
+            sourceType: 'module',
+            ecmaVersion: '2020'
+        });
+        return parsed;
+    } catch (e) {
+        throw e;
+    }
 }
