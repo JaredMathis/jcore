6722a2d34d13e7079ddd3b4ab4c8efae8835fbc2
JaredMathis <jared.mathis@native.consulting>
2023-05-30 08:21:02 -0400

    #47 function_auto js_exported_function_declaration_single

--- src/js/exported/function/declaration/single.mjs
+++ src/js/exported/function/declaration/single.mjs
@@ -1,4 +1,7 @@
-import { error } from '../../../../error.mjs';
-export function js_exported_function_declaration_single() {
-    error('todo: js_exported_function_declaration_single');
+import { list_single } from '../../../../list/single.mjs';
+import { js_exported_function_declarations } from '../declarations.mjs';
+export function js_exported_function_declaration_single(parsed) {
+    let declarations = js_exported_function_declarations(parsed);
+    let result = list_single(declarations);
+    return result;
 }
