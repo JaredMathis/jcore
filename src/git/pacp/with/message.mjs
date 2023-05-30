a013c84c8d433ad77df41fbbd70ee15a82d7fb1b
JaredMathis <jared.mathis@native.consulting>
2023-05-29 21:50:05 -0400

    #40

--- src/git/pacp/with/message.mjs
+++ src/git/pacp/with/message.mjs
@@ -3,13 +3,13 @@ import { result_empty } from '../../../result/empty.mjs';
 import { log } from '../../../log.mjs';
 import { command_line_check } from '../../../command/line/check.mjs';
 export async function git_pacp_with_message(commit_message) {
-    let result = result_empty();
     let commands = [
         `git pull`,
         `git add *`,
         `git commit -m "${ commit_message }"`,
         `git push`
     ];
+    let result = result_empty();
     for (let c of commands) {
         let c_result = await command_line_check(c);
         if (!c_result.success) {
