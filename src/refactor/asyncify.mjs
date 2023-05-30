557afb01d05bc08a28888223b30d0adba1fc4a59
JaredMathis <jared.mathis@native.consulting>
2023-05-30 11:13:33 -0400

    #10 git_pacp

--- src/refactor/asyncify.mjs
+++ src/refactor/asyncify.mjs
@@ -3,5 +3,6 @@ import { error } from '../error.mjs';
 export function refactor_asyncify(args) {
     let {parsed} = args;
     let f = js_export_function_single(parsed);
+    object_property_set(f, 'async', true);
     error('todo: refactor_asyncify');
 }
