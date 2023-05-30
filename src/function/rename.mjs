43c843f67d00892bca539d23c38e1b15b6d23093
JaredMathis <jared.mathis@native.consulting>
2023-05-30 14:56:23 -0400

    #49 function_auto function_rename

--- src/function/rename.mjs
+++ src/function/rename.mjs
@@ -1,9 +1,11 @@
+import { error } from '../error.mjs';
 import { file_js_all_map } from '../file/js/all/map.mjs';
 import { refactor_import_fix } from '../refactor/import/fix.mjs';
 import { file_js_all_identifier_rename } from '../file/js/all/identifier/rename.mjs';
 import { function_rename_without_all_refactor } from './rename/without/all/refactor.mjs';
 export async function function_rename(function_name_old, function_name_new) {
     await function_rename_without_all_refactor(function_name_old, function_name_new);
-    await file_js_all_identifier_rename(function_name_old, function_name_new);
+    let file_paths_changed = await file_js_all_identifier_rename(function_name_old, function_name_new);
+    error();
     await file_js_all_map(refactor_import_fix.name);
 }
