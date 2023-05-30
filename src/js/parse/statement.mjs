4854cbf0018412d7271a9dbccb84be428c6a1f17
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:02:05 -0400

    #10 function_auto js_parse_statement

--- src/js/parse/statement.mjs
+++ src/js/parse/statement.mjs
@@ -1,9 +1,13 @@
+import { js_parse_statements } from './statements.mjs';
 import { list_single } from '../../list/single.mjs';
 import { js_body_get } from '../body/get.mjs';
 import { js_parse } from '../parse.mjs';
 export function js_parse_statement(statement_code) {
     let parsed = js_parse(statement_code);
-    let body = js_body_get(parsed);
-    let statement = list_single(body);
+    let code = js_body_get(parsed);
+    if (false) {
+        let code = js_parse_statements(statement_code);
+    }
+    let statement = list_single(code);
     return statement;
 }
--- src/js/parse/statements.mjs
+++ src/js/parse/statements.mjs
@@ -1,4 +1,6 @@
 import { error } from '../../error.mjs';
 export function js_parse_statements(code) {
-    error('todo: js_parse_statements');
+    let parsed = js_parse(code);
+    let body = js_body_get(parsed);
+    return body;
 }
