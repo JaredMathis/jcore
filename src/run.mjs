f6a33a3776bded68b58a97d1079e5a056c8634ec
JaredMathis <jared.mathis@native.consulting>
2023-05-29 22:10:13 -0400

    #41:

--- src/run.mjs
+++ src/run.mjs
@@ -1,4 +1,8 @@
-import { error } from './error.mjs';
-export function run() {
-    error('todo: run');
+import { log } from './log.mjs';
+import { function_run_arguments } from './function/run/arguments.mjs';
+import { command_line_args_skipped } from './command/line/args/skipped.mjs';
+export async function run() {
+    let skipped = command_line_args_skipped();
+    let result = await function_run_arguments(skipped);
+    log(`Run result: ${ result }`);
 }
