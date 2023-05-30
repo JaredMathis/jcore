d4ef2b9bbeb8f0fd4166007b90f60e37f48e2500
JaredMathis <jared.mathis@native.consulting>
2023-05-29 23:06:17 -0400

    closes #10 data_task_next 10

--- src/data/task/next.mjs
+++ src/data/task/next.mjs
@@ -1,6 +1,9 @@
+import { log } from '../../log.mjs';
 import { data_task_id_set_hash } from './id/set/hash.mjs';
 import { data_task_finish } from './finish.mjs';
+import { data_get } from '../get.mjs';
 export async function data_task_next(task_id) {
     await data_task_finish();
+    console.log(await data_get());
     await data_task_id_set_hash(task_id);
 }
