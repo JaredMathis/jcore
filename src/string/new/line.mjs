df773012de6e2abfbcd2e02fc26269d98ed2a724
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:24:24 -0400

    #10 function_auto string_new_line

--- src/string/new/line.mjs
+++ src/string/new/line.mjs
@@ -1,4 +1,5 @@
-import { error } from '../../error.mjs';
+import { comment } from '../../comment.mjs';
+comment(`New line varies by operating system. Making decision for now to be consistent`);
 export function string_new_line() {
-    error('todo: string_new_line');
+    return '\n';
 }
