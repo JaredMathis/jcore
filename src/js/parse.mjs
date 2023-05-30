f95c0e7b785d2f5994bd4321400068e6b067b543
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:24:32 -0400

    #10 git_pacp

--- src/js/parse.mjs
+++ src/js/parse.mjs
@@ -1,17 +1,20 @@
 import { error_caught } from '../error/caught.mjs';
 import { Parser } from 'acorn';
 export function js_parse(unparsed) {
-    try {
-        return lambda();
-    } catch (e) {
-        error_caught(js_parse, e, unparsed);
-    }
-
+    return try_catch(js_parse, lambda);
     function lambda() {
         let parsed = Parser.parse(unparsed, {
             sourceType: 'module',
             ecmaVersion: '2020'
         });
         return parsed;
+    }
+}
+
+function try_catch(fn, lambda) {
+    try {
+        return lambda();
+    } catch (e) {
+        error_caught(fn, e, unparsed);
     }
 }
