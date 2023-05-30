bfb78c6a8022cdab5bb2dabaa19a29c9c41a8515
JaredMathis <jared.mathis@native.consulting>
2023-05-30 13:39:09 -0400

    #10 function_auto js_parse_statements

--- src/js/parse/statements.mjs
+++ src/js/parse/statements.mjs
@@ -1,3 +1,4 @@
+import { js_export_single } from '../export/single.mjs';
 import { string_a } from '../../string/a.mjs';
 import { js_body_get } from '../body/get.mjs';
 import { js_parse } from '../parse.mjs';
@@ -5,6 +6,8 @@ import { js_export_function } from '../export/function.mjs';
 export function js_parse_statements(code) {
     let r = js_export_function(string_a(), code);
     let parsed = js_parse(code);
+    if (false)
+        js_export_single;
     let body = js_body_get(parsed);
     return body;
 }
