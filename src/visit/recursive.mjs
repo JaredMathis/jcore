8a7a4cc3d7d2382eb7af69e487b41ff8a294d2d8
JaredMathis <jared.mathis@native.consulting>
2023-05-30 16:18:54 -0400

    #51 git_pacp

--- src/visit/recursive.mjs
+++ src/visit/recursive.mjs
@@ -1,6 +1,6 @@
-export function visit_recursive(root, children_get, lambda, stack) {
-    lambda(root);
-    let children = children_get(root);
+export function visit_recursive(node, children_get, lambda, stack) {
+    lambda(node);
+    let children = children_get(node);
     for (let c of children) {
         visit_recursive(c, children_get, lambda, stack);
     }
