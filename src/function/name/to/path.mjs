a9d8ec42ff3e92c67b0d623cb310676ece556856
JaredMathis <jared.mathis@native.consulting>
2023-05-29 23:45:02 -0400

    #47 git_pacp

--- src/function/name/to/path.mjs
+++ src/function/name/to/path.mjs
@@ -5,9 +5,10 @@ import { string_split } from '../../../string/split.mjs';
 import { function_extension } from '../../extension.mjs';
 import { directory_current } from '../../../directory/current.mjs';
 import { path_join } from '../../../path/join.mjs';
+import { function_name_separator } from '../separator.mjs';
 export function function_name_to_path(function_name) {
     let directory_source_result = directory_source();
-    let split = string_split(function_name, '_');
+    let split = string_split(function_name, function_name_separator());
     list_add_beginning(split, directory_source_result);
     list_add_beginning(split, directory_current());
     list_last_map(split, last => last + function_extension());
