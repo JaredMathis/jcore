ff03e565c6bfe88915f12a18abc4b247618502e2
JaredMathis <jared.mathis@native.consulting>
2023-05-30 08:12:58 -0400

    #47 function_auto refactor_console_to_function

--- src/refactor/console/to/function.mjs
+++ src/refactor/console/to/function.mjs
@@ -1,3 +1,6 @@
+import { log } from '../../../log.mjs';
+import { js_export_function_single } from '../../../js/export/function/single.mjs';
+import { file_js_path_to_name } from '../../../file/js/path/to/name.mjs';
 import { list_length_is_0 } from '../../../list/length/is/0.mjs';
 import { list_remove_all } from '../../../list/remove/all.mjs';
 import { list_without_all } from '../../../list/without/all.mjs';
@@ -7,7 +10,6 @@ import { js_body_get } from '../../../js/body/get.mjs';
 import { js_exports } from '../../../js/exports.mjs';
 import { assert } from '../../../assert.mjs';
 import { error } from '../../../error.mjs';
-import { file_js_path_all_to_name } from '../../../file/js/path/all/to/name.mjs';
 import { js_parse_statement } from '../../../js/parse/statement.mjs';
 import { list_add } from '../../../list/add.mjs';
 export async function refactor_console_to_function(file_path) {
@@ -19,9 +21,9 @@ export async function refactor_console_to_function(file_path) {
     let function_body = list_without_all(body, imports);
     list_remove_all(body, function_body);
     let function_name = file_js_path_to_name(file_path);
-    let export_statement = js_parse_statement(`export function ${function_name}() { }`);
+    let export_statement = js_parse_statement(`export function ${ function_name }() { }`);
     list_add(body, export_statement);
     let exported_function = js_export_function_single(parsed);
-    console.log({exported_function});
-    error()
+    console.log({ exported_function });
+    error();
 }
