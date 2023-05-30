14e010f100a4695f62d265c3f465f2c0d0d60053
JaredMathis <jared.mathis@native.consulting>
2023-05-29 21:31:39 -0400

    #40

--- src/command/line.mjs
+++ src/command/line.mjs
@@ -7,20 +7,4 @@ export async function command_line(command) {
     return result;
 }
 
-async function command_line_check(command) {
-    return await new Promise(function (resolve, reject) {
-        exec(command, (error, stdout, stderr) => {
-            let success = true;
-            if (error) {
-                success = false;
-            }
-            let result = {
-                success,
-                error,
-                stdout,
-                stderr
-            };
-            resolve(result);
-        });
-    });
-}
+x
