8997a5e6f6b144033280559de417d8c6a65e4549
JaredMathis <jared.mathis@native.consulting>
2023-05-30 14:12:08 -0400

    #10 function_auto file_delete

--- src/file/delete.mjs
+++ src/file/delete.mjs
@@ -1,5 +1,7 @@
 import { file_exists } from './exists.mjs';
 import { assert } from '../assert.mjs';
+import fs from 'fs';
 export async function file_delete(file_path) {
     assert(await file_exists(file_path));
+    await fs.promises.rm(file_path);
 }
