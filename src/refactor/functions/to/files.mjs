a4d0607042c7976dba574c0af98267e367aa97d1
JaredMathis <jared.mathis@native.consulting>
2023-05-30 12:14:21 -0400

    #10 function_auto refactor_functions_to_files

--- src/refactor/functions/to/files.mjs
+++ src/refactor/functions/to/files.mjs
@@ -1,3 +1,4 @@
+import { function_add_with_declaration } from '../../../function/add/with/declaration.mjs';
 import { list_remove_all } from '../../../list/remove/all.mjs';
 import { function_map } from '../../../function/map.mjs';
 import { list_length_is_0 } from '../../../list/length/is/0.mjs';
@@ -8,30 +9,27 @@ import { js_node_is_function_declaration } from '../../../js/node/is/function/de
 import { js_without_imports } from '../../../js/without/imports.mjs';
 import { list_filter } from '../../../list/filter.mjs';
 import { assert } from '../../../assert.mjs';
-import { function_add_with_body } from '../../../function/add/with/body.mjs';
-import { js_function_declaration_to_name } from '../../../js/function/declaration/to/name.mjs';
 import { refactor_import_fix } from '../../import/fix.mjs';
 import { js_body_get } from '../../../js/body/get.mjs';
 export async function refactor_functions_to_files(args) {
     let {parsed} = args;
     let without_imports = js_without_imports(parsed);
-    let functions_to_export = list_filter(without_imports, js_node_is_function_declaration);
-    let function_names_new = js_function_declarations_to_names(functions_to_export);
+    let function_declarations_to_export = list_filter(without_imports, js_node_is_function_declaration);
+    let function_names_new = js_function_declarations_to_names(function_declarations_to_export);
     if (list_length_is_0(function_names_new)) {
         return;
     }
     for (let n of function_names_new) {
         assert(!await function_exists(n));
     }
-    for (let f of functions_to_export) {
-        let n = js_function_declaration_to_name(f);
-        let statements = js_function_delcaration_to_statements(f);
-        await function_add_with_body(n, statements);
+    for (let fd of function_declarations_to_export) {
+        let statements = js_function_delcaration_to_statements(fd);
+        await function_add_with_declaration(fd, statements);
     }
     for (let n of function_names_new) {
         await function_map(refactor_import_fix.name, n);
     }
     let body = js_body_get(parsed);
-    list_remove_all(body, functions_to_export);
+    list_remove_all(body, function_declarations_to_export);
     await refactor_import_fix(args);
 }
