e6f72f886e0f39b0ea67ca53b06bf811b85dafc8
JaredMathis <jared.mathis@native.consulting>
2023-05-31 16:43:14 -0400

    #15 function_auto function_copy

--- src/function/copy.mjs
+++ src/function/copy.mjs
@@ -1,2 +1,4 @@
+import { function_add_with_declaration } from './add/with/declaration.mjs';
 export function function_copy(function_name_to_copy, function_name_of_copy) {
+    function_add_with_declaration();
 }
