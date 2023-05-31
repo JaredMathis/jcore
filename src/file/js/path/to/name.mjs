94ef32e7952548e52d99d976c3f65f65594d16fe
JaredMathis <jared.mathis@native.consulting>
2023-05-31 10:53:13 -0400

    #55 function_dependencies function_dependencies

--- src/file/js/path/to/name.mjs
+++ src/file/js/path/to/name.mjs
@@ -1,5 +1,5 @@
 import { list_single_item } from '../../../../list/single/item.mjs';
 import { file_js_path_all_to_name } from '../all/to/name.mjs';
 export function file_js_path_to_name(file_js_path) {
-    return file_js_path_all_to_name(list_single_item(file_js_path));
+    return list_single_item(file_js_path_all_to_name(list_single_item(file_js_path)));
 }
