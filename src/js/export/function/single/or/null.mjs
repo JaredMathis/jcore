fbc3f49e7ab8353e3ba1ed1dcbc7f3b16b6b3645
JaredMathis <jared.mathis@native.consulting>
2023-05-31 08:30:05 -0400

    #52 function_auto js_export_function_single_or_null

--- src/js/export/function/single/or/null.mjs
+++ src/js/export/function/single/or/null.mjs
@@ -1,5 +1,5 @@
 import { js_export_function_single_generic } from '../generic.mjs';
 export function js_export_function_single_or_null(parsed) {
-    const or_null = false;
+    const or_null = true;
     return js_export_function_single_generic(parsed, or_null);
 }
