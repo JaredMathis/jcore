d518159657df3c0793ed6a00f0c9d120100b3074
JaredMathis <jared.mathis@native.consulting>
2023-05-31 16:33:28 -0400

    #56 function_auto function_unasyncify

--- src/function/unasyncify.mjs
+++ src/function/unasyncify.mjs
@@ -1,6 +1,8 @@
+import { function_name_separator } from './name/separator.mjs';
 import { refactor_unasyncify } from '../refactor/unasyncify.mjs';
 import { function_map } from './map.mjs';
 import { function_name_get } from './name/get.mjs';
 export async function function_unasyncify(function_name) {
+    let a = `${ function_name_separator() }`;
     function_map(function_name_get(refactor_unasyncify), function_name);
 }
--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,4 +1,4 @@
 import { function_name_get } from './function/name/get.mjs';
-export function sandbox() {
+export async function sandbox() {
     function_name_get(a);
 }
