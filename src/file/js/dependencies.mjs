dd1766634364806d7f4e237f5283e418b7ea671f
JaredMathis <jared.mathis@native.consulting>
2023-05-31 10:22:36 -0400

    #55 function_auto file_js_dependencies

--- src/file/js/dependencies.mjs
+++ src/file/js/dependencies.mjs
@@ -1,8 +1,17 @@
+import { js_node_property_value } from '../../js/node/property/value.mjs';
+import { log } from '../../log.mjs';
 import { js_import_all } from '../../js/import/all.mjs';
+import { js_node_property_source } from '../../js/node/property/source.mjs';
+import { list_map } from '../../list/map.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
 import { file_js_parse } from './parse.mjs';
 export async function file_js_dependencies(file_path) {
     let parsed = await file_js_parse(file_path);
     let imports = js_import_all(parsed);
-    
+    list_map(imports, i => {
+        let source = object_property_get(i, js_node_property_source());
+        let raw = object_property_get(source, js_node_property_value());
+        return raw;
+    });
     console.log(imports);
 }
