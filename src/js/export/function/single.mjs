236ac3c24742b66725fb7de69ccd64f372d56cb5
JaredMathis <jared.mathis@native.consulting>
2023-05-31 08:07:14 -0400

    #52 function_rename js_code_export_function_single js_export_function_single

---
+++ src/js/export/function/single.mjs
@@ -0,0 +1,7 @@
+import { js_exports } from '../../exports.mjs';
+import { list_single } from '../../../list/single.mjs';
+export function js_export_function_single(parsed) {
+    let exports_existing = js_exports(parsed);
+    let export_single = list_single(exports_existing);
+    return export_single;
+}
