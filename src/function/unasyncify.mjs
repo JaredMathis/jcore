95f12ae6a57167ebd2859a820f9ec7b3d1a50963
JaredMathis <jared.mathis@native.consulting>
2023-05-31 16:35:36 -0400

    #56 function_auto function_unasyncify

--- src/function/unasyncify.mjs
+++ src/function/unasyncify.mjs
@@ -1,9 +1,12 @@
+import { string_ends_with } from '../string/ends/with.mjs';
 import { function_suffix_async } from './suffix/async.mjs';
 import { function_name_separator } from './name/separator.mjs';
 import { refactor_unasyncify } from '../refactor/unasyncify.mjs';
 import { function_map } from './map.mjs';
 import { function_name_get } from './name/get.mjs';
+import { assert } from '../assert.mjs';
 export async function function_unasyncify(function_name) {
-    let a = `${ function_name_separator() }${ function_suffix_async() }`;
+    let ending = `${ function_name_separator() }${ function_suffix_async() }`;
+    assert(string_ends_with(function_name, ending));
     function_map(function_name_get(refactor_unasyncify), function_name);
 }
