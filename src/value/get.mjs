ca07a43db0975c0f61dd8beb82f0058c3ec89f8b
JaredMathis <jared.mathis@native.consulting>
2023-05-31 08:26:43 -0400

    #52 function_auto value_get

--- src/value/get.mjs
+++ src/value/get.mjs
@@ -1,5 +1,8 @@
+import { value_set_is } from './set/is.mjs';
+import { assert } from '../assert.mjs';
 import { object_property_get } from '../object/property/get.mjs';
 export function value_get(value_object) {
+    assert(value_set_is(value_object));
     let value = object_property_get(value_object, 'value');
     return value;
 }
