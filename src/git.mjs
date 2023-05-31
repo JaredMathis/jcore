ff9394bc142b5b5ef410de0b815a6e64732853cc
JaredMathis <jared.mathis@native.consulting>
2023-05-31 10:46:31 -0400

    #55 function_dependencies function_dependencies

--- src/git.mjs
+++ src/git.mjs
@@ -1,6 +1,7 @@
 import { function_run_arguments } from './function/run/arguments.mjs';
 import { git_pacp } from './git/pacp.mjs';
 export async function git() {
-    await function_run_arguments(arguments);
+    let result = await function_run_arguments(arguments);
     await git_pacp();
+    return result;
 }
