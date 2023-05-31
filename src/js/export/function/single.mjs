b1a796e84d997e21ec11fe0aa78a63a43510e8e5
JaredMathis <jared.mathis@native.consulting>
2023-05-31 08:12:42 -0400

    #52 git_pacp

--- src/js/export/function/single.mjs
+++ src/js/export/function/single.mjs
@@ -1,11 +1,14 @@
 import { js_exports } from '../../exports.mjs';
 import { list_single } from '../../../list/single.mjs';
+import { list_length_is_1 } from '../../../list/length/is/1.mjs';
 export function js_export_function_single(parsed) {
     const rename_me = false;
     let default_value = null;
     let exports_existing = js_exports(parsed);
     if (rename_me) {
-        
+        if (!list_length_is_1(exports_existing)) {
+            return default_value;
+        }
     }
     let export_single = list_single(exports_existing);
     return export_single;
