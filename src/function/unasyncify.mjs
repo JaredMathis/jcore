253b33e10caeab4e448ea089c6d4ca437bc053d8
JaredMathis <jared.mathis@native.consulting>
2023-05-31 16:29:54 -0400

    #56 function_auto refactor_unasyncify

--- src/function/unasyncify.mjs
+++ src/function/unasyncify.mjs
@@ -3,8 +3,4 @@ import { function_map } from './map.mjs';
 import { function_name_get } from './name/get.mjs';
 export async function function_unasyncify(function_name) {
     function_map(function_name_get(refactor_unasyncify), function_name);
-    function test() {
-        a = () => {
-        };
-    }
 }
--- src/js/nodes/count.mjs
+++ src/js/nodes/count.mjs
@@ -1,9 +1,13 @@
+import { log } from '../../log.mjs';
+import { js_unparse } from '../unparse.mjs';
 import { js_visit_nodes_all } from '../visit/nodes/all.mjs';
 export function js_nodes_count(parsed, predicate) {
     let count = 0;
+    log(js_unparse(parsed))
     js_visit_nodes_all(parsed, v => {
         let {node} = v;
         if (predicate(node)) {
+            log(node)
             count++;
         }
     });
