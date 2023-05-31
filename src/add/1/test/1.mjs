1a0f08f49bf7749150fc4d6ce5ea1fb6f78bdc42
JaredMathis <jared.mathis@native.consulting>
2023-05-30 20:05:18 -0400

    #1 function_auto add_1_test_1

--- src/add/1/test/1.mjs
+++ src/add/1/test/1.mjs
@@ -1,6 +1,6 @@
+import { equal } from '../../../equal.mjs';
 import { assert } from '../../../assert.mjs';
-import { error } from '../../../error.mjs';
 import { add_1 } from '../../1.mjs';
 export function add_1_test_1() {
-    assert(equal(add_1(1),2))
+    assert(equal(add_1(1), 2));
 }
