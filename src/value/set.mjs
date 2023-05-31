c2306e39b79b44d18339a84e3e5a43d11c892ad6
JaredMathis <jared.mathis@native.consulting>
2023-05-31 08:20:38 -0400

    #52 function_auto value_set

--- src/value/set.mjs
+++ src/value/set.mjs
@@ -1,4 +1,5 @@
 import { object_property_set } from '../object/property/set.mjs';
 export function value_set(value_object, value) {
     object_property_set(value_object, 'value', value);
+    object_property_set(value_object, 'set', true);
 }
