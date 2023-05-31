7b596f3f328f963cb2df477f6af4244bde8fa9f5
JaredMathis <jared.mathis@native.consulting>
2023-05-31 17:10:10 -0400

    #15 function_auto function_copy

--- src/function/parse.mjs
+++ src/function/parse.mjs
@@ -1,7 +1,7 @@
 import { file_js_parse } from '../file/js/parse.mjs';
 import { function_name_to_path } from './name/to/path.mjs';
-export function function_parse(function_name_to_copy) {
+export async function function_parse(function_name_to_copy) {
     let file_path = function_name_to_path(function_name_to_copy);
-    let parsed = file_js_parse(file_path);
+    let parsed = await file_js_parse(file_path);
     return parsed;
 }
