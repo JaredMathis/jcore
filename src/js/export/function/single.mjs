4adc8737fc7cc04450fd575824e1aadc9dc09dca
JaredMathis <jared.mathis@native.consulting>
2023-05-31 08:27:26 -0400

    #52 function_auto js_export_function_single

--- src/js/export/function/single.mjs
+++ src/js/export/function/single.mjs
@@ -1,19 +1,19 @@
+import { value_set } from '../../../value/set.mjs';
+import { value_new } from '../../../value/new.mjs';
 import { js_exports } from '../../exports.mjs';
 import { list_single } from '../../../list/single.mjs';
 import { list_length_is_1 } from '../../../list/length/is/1.mjs';
 export function js_export_function_single(parsed) {
     const rename_me = false;
-    let default_value = null;
     let result = value_new();
     let exports_existing = js_exports(parsed);
     if (rename_me) {
         if (!list_length_is_1(exports_existing)) {
-            result = default_value;
-            result_set = true;
+            value_set(result, null);
         }
     }
     if (!result_set) {
-        value_set(result, list_single(exports_existing))
+        value_set(result, list_single(exports_existing));
     }
     return result;
 }
