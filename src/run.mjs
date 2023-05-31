e074ff2653bf407fdc53ebfafeee0037124317ef
JaredMathis <jared.mathis@native.consulting>
2023-05-31 10:47:29 -0400

    #55 function_dependencies function_dependencies

--- src/run.mjs
+++ src/run.mjs
@@ -1,8 +1,9 @@
 import { log } from './log.mjs';
 import { function_run_arguments } from './function/run/arguments.mjs';
 import { command_line_args_skipped } from './command/line/args/skipped.mjs';
+import { json_to } from './json/to.mjs';
 export async function run() {
     let skipped = command_line_args_skipped();
     let result = await function_run_arguments(skipped);
-    log(`Run result: ${ result }`);
+    log(`Run result: ${ result ? json_to(result) : result }`);
 }
