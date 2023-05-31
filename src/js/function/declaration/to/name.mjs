456d98f9ee65bd74c491a4156b6280aa9c62cbe0
JaredMathis <jared.mathis@native.consulting>
2023-05-31 09:02:49 -0400

    #52 function_auto js_function_declaration_to_name

--- src/js/function/declaration/to/name.mjs
+++ src/js/function/declaration/to/name.mjs
@@ -1,9 +1,4 @@
-import { js_node_is_identifier } from '../../../node/is/identifier.mjs';
-import { assert } from '../../../../assert.mjs';
-import { object_property_get } from '../../../../object/property/get.mjs';
+import { js_id_name_get } from '../../../id/name/get.mjs';
 export function js_function_declaration_to_name(f) {
-    let id = object_property_get(f, 'id');
-    assert(js_node_is_identifier(id));
-    let name = object_property_get(id, 'name');
-    return name;
+    return js_id_name_get(f);
 }
