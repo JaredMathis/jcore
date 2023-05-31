5e4d1d6854fe1614661c936c13ac661515438140
JaredMathis <jared.mathis@native.consulting>
2023-05-31 17:14:24 -0400

    #15 function_auto function_copy

--- src/function/copy.mjs
+++ src/function/copy.mjs
@@ -6,6 +6,6 @@ import { object_property_change } from '../object/property/change.mjs';
 export async function function_copy(function_name_to_copy, function_name_of_copy) {
     let parsed = await function_parse(function_name_to_copy);
     let fd = js_export_function_single(parsed);
-    object_property_change(fd, js_node_property_name());
+    object_property_change(fd, js_node_property_name(), function_name_of_copy);
     function_add_with_declaration(function_name_of_copy, fd);
 }
