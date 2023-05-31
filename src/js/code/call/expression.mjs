1f701abc60c7ea548617ec8d45f94b3376acbfdb
JaredMathis <jared.mathis@native.consulting>
2023-05-31 19:22:38 -0400

    #60 git_pacp

--- src/js/code/call/expression.mjs
+++ src/js/code/call/expression.mjs
@@ -1,4 +1,8 @@
 export function js_code_call_expression(name) {
     let args = ``;
-    return `${ name }(${ args })`;
+    return js_code_call_expression_with_args(name, args);
+}
+
+function js_code_call_expression_with_args(name, args) {
+    return `${name}(${args})`;
 }
