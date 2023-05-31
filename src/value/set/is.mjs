1491714dad6d987a114229c12d2bae1341ec9f07
JaredMathis <jared.mathis@native.consulting>
2023-05-31 08:22:23 -0400

    #52 function_auto value_set_is

--- src/value/set/is.mjs
+++ src/value/set/is.mjs
@@ -1,4 +1,5 @@
-import { error } from '../../error.mjs';
-export function value_set_is() {
-    error('todo: value_set_is');
+import { object_property_set } from '../../object/property/set.mjs';
+export function value_set_is(value_object) {
+    let set = object_property_set(value_object, 'set');
+    return set;
 }
