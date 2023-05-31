22d6022b651d69fc6d4c28c725ee4f25cf82ae51
JaredMathis <jared.mathis@native.consulting>
2023-05-31 07:55:14 -0400

    #52 git_pacp

--- src/refactor/functions/arguments/assert/add.mjs
+++ src/refactor/functions/arguments/assert/add.mjs
@@ -1,4 +1,8 @@
 import { error } from '../../../../error.mjs';
-export function refactor_functions_arguments_assert_add() {
+import { file_js_all } from '../../../../file/js/all.mjs';
+export async function refactor_functions_arguments_assert_add() {
+
+    await file_js_all();
+
     error('todo: refactor_functions_arguments_assert_add');
 }
