4c8a4bd1659cb776d3fb732855762087068f743c
JaredMathis <jared.mathis@native.consulting>
2023-05-31 16:31:16 -0400

    #56 function_auto refactor_unasyncify_each

--- src/js/nodes/count.mjs
+++ src/js/nodes/count.mjs
@@ -3,11 +3,9 @@ import { js_unparse } from '../unparse.mjs';
 import { js_visit_nodes_all } from '../visit/nodes/all.mjs';
 export function js_nodes_count(parsed, predicate) {
     let count = 0;
-    log(js_unparse(parsed))
     js_visit_nodes_all(parsed, v => {
         let {node} = v;
         if (predicate(node)) {
-            log(node)
             count++;
         }
     });
--- src/refactor/unasyncify.mjs
+++ src/refactor/unasyncify.mjs
@@ -2,8 +2,10 @@ import { refactor_unasyncify_each } from './unasyncify/each.mjs';
 import { js_callable_multiple } from '../js/callable/multiple.mjs';
 import { js_visit_nodes_all } from '../js/visit/nodes/all.mjs';
 import { assert } from '../assert.mjs';
+import { comment } from '../comment.mjs';
 export async function refactor_unasyncify(args) {
     let {parsed} = args;
+    comment(`To remove this assert, fix the code`);
     assert(!js_callable_multiple(parsed));
     js_visit_nodes_all(parsed, refactor_unasyncify_each);
 }
