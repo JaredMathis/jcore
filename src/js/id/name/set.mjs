34797e6fe5bf5d573079f853db1015d9fd87fb98
JaredMathis <jared.mathis@native.consulting>
2023-05-31 17:18:40 -0400

    #15 function_auto js_id_name_set

--- src/js/id/name/set.mjs
+++ src/js/id/name/set.mjs
@@ -1,3 +1,5 @@
+import { object_property_set } from '../../../object/property/set.mjs';
+import { js_id_get } from '../get.mjs';
 export function js_id_name_set(fd, value) {
     let id = js_id_get(fd);
     let name = object_property_set(id, 'name', value);
