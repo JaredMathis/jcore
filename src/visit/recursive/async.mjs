ff640c0e9cd92700186c371b6c6286ea8ff6bbec
JaredMathis <jared.mathis@native.consulting>
2023-05-31 15:34:50 -0400

    #55 function_auto visit_recursive_async

--- src/visit/recursive/async.mjs
+++ src/visit/recursive/async.mjs
@@ -1,4 +1,17 @@
-import { error } from '../../error.mjs';
-export function visit_recursive_async() {
-    error('todo: visit_recursive_async');
+import { list_last_remove_verify } from '../../list/last/remove/verify.mjs';
+import { visit_recursive } from '../recursive.mjs';
+import { list_add_exists_not } from '../../list/add/exists/not.mjs';
+import { list_last_or_null } from '../../list/last/or/null.mjs';
+export async function visit_recursive_async(node, children_get, lambda, stack) {
+    let parent = list_last_or_null(stack);
+    list_add_exists_not(stack, node);
+    await lambda({
+        node,
+        parent
+    });
+    let children = children_get(node);
+    for (let c of children) {
+        visit_recursive(c, children_get, lambda, stack);
+    }
+    list_last_remove_verify(stack, node);
 }
