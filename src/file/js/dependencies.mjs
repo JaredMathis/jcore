6fcf575ec042de6fabb5ab5f8b24fe2355170889
JaredMathis <jared.mathis@native.consulting>
2023-05-31 10:39:54 -0400

    #55 function_dependencies function_dependencies

--- src/file/js/dependencies.mjs
+++ src/file/js/dependencies.mjs
@@ -8,15 +8,16 @@ import { file_js_parse } from './parse.mjs';
 import { path_join } from '../../path/join.mjs';
 import { path_relative_file } from '../../path/relative/file.mjs';
 import { directory_source } from '../../directory/source.mjs';
+import { path_relative } from '../../path/relative.mjs';
 export async function file_js_dependencies(file_path) {
     let parsed = await file_js_parse(file_path);
     let imports = js_import_all(parsed);
     let mapped = list_map(imports, i => {
         let source = object_property_get(i, js_node_property_source());
-        let path_relative = object_property_get(source, js_node_property_value());
-        let path_full = path_join([file_path, path_relative])
+        let relative_path = object_property_get(source, js_node_property_value());
+        let path_full = path_join([file_path, relative_path])
         let resolved = path_resolve(path_full);
-        console.log(path_relative_file(directory_source(), resolved))
+        console.log(path_relative(directory_source(), resolved))
         return resolved;
     });
     console.log(mapped);
