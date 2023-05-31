e01a06c06a618e1c9980f93cf1515b44bcbe6ad6
JaredMathis <jared.mathis@native.consulting>
2023-05-31 09:03:47 -0400

    #52 function_auto js_call_expression_to_name

--- src/js/call/expression/to/name.mjs
+++ src/js/call/expression/to/name.mjs
@@ -1,4 +1,4 @@
-import { error } from '../../../../error.mjs';
-export function js_call_expression_to_name() {
-    error('todo: js_call_expression_to_name');
+import { js_id_name_get } from '../../../id/name/get.mjs';
+export function js_call_expression_to_name(ce) {
+    return js_id_name_get(ce);
 }
