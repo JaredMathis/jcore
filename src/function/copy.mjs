23ba01d0b91472b1f2de462b1a89f933c2c06d28
JaredMathis <jared.mathis@native.consulting>
2023-05-31 17:09:22 -0400

    #15 function_auto function_copy

--- src/function/copy.mjs
+++ src/function/copy.mjs
@@ -1,7 +1,10 @@
+import { file_js_parse } from '../file/js/parse.mjs';
 import { js_export_function_single } from '../js/export/function/single.mjs';
 import { function_add_with_declaration } from './add/with/declaration.mjs';
+import { function_name_to_path } from './name/to/path.mjs';
 export function function_copy(function_name_to_copy, function_name_of_copy) {
-    let parsed = function_parse(function_name_to_copy);
+    let file_path = function_name_to_path(function_name_to_copy);
+    let parsed = file_js_parse(file_path);
     let fd = js_export_function_single(parsed);
     function_add_with_declaration(function_name_of_copy, fd);
 }
