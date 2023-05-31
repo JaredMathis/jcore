908dcdc3aab699ec32d29cafe22d23433b30710e
JaredMathis <jared.mathis@native.consulting>
2023-05-31 10:05:49 -0400

    #55 function_auto function_dependencies

--- src/function/dependencies.mjs
+++ src/function/dependencies.mjs
@@ -1,4 +1,6 @@
-import { error } from '../error.mjs';
-export function function_dependencies() {
-    error('todo: function_dependencies');
+import { file_js_dependencies } from '../file/js/dependencies.mjs';
+import { function_name_to_path } from './name/to/path.mjs';
+export async function function_dependencies(function_name) {
+    let file_path = function_name_to_path(function_name);
+    return await file_js_dependencies(file_path);
 }
