3161ff9d0eb73ed1a9efd4071d74a351440a3671
JaredMathis <jared.mathis@native.consulting>
2023-05-31 18:28:53 -0400

    #60 git_pacp

--- src/js/node/call/expression/on/name/equal.mjs
+++ src/js/node/call/expression/on/name/equal.mjs
@@ -6,7 +6,8 @@ import { node_is_type_call_expression } from '../../../../../../node/is/type/cal
 export function js_node_call_expression_on_name_equal(node, on_name_equal) {
     if (node_is_type_call_expression(node)) {
         let name_actual = js_call_expression_to_name(node);
-        if (equal(name_actual, function_name_get(arguments_assert))) {
+        const name_expected = function_name_get(arguments_assert);
+        if (equal(name_actual, name_expected)) {
             on_name_equal();
         }
     }
