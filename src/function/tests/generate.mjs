416c5171a86081c5c4aed537795d1d6aa5aeb449
JaredMathis <jared.mathis@native.consulting>
2023-05-30 20:15:20 -0400

    #1 function_auto function_tests_generate

--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -1,3 +1,4 @@
+import { log } from '../../log.mjs';
 import { function_name_get } from '../name/get.mjs';
 import { refactor_import_fix } from '../../refactor/import/fix.mjs';
 import { function_map } from '../map.mjs';
@@ -10,11 +11,14 @@ import { list_join } from '../../list/join.mjs';
 import { list_map } from '../../list/map.mjs';
 import { function_all_tests } from '../all/tests.mjs';
 import { function_name_to_path } from '../name/to/path.mjs';
+import { list_add } from '../../list/add.mjs';
+import { list_length } from '../../list/length.mjs';
 export async function function_tests_generate() {
     let function_name = 'tests';
     let file_path = function_name_to_path(function_name);
     let test_names = await function_all_tests();
     let mapped = list_map(test_names, n => `await ${ n }();`);
+    list_add(mapped, `${ function_name_get(log) }(${ list_length(mapped) } + ' tests ran successfully')`);
     let code = list_join(mapped, string_new_line());
     let statements = js_parse_statements(code);
     if (await file_exists(file_path)) {
