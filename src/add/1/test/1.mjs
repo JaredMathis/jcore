ae40f358ca294195f30552d4bed47253a553e339
JaredMathis <jared.mathis@native.consulting>
2023-05-30 20:04:49 -0400

    #1 function_auto add_1

--- src/add/1/test/1.mjs
+++ src/add/1/test/1.mjs
@@ -1,4 +1,6 @@
+import { assert } from '../../../assert.mjs';
 import { error } from '../../../error.mjs';
+import { add_1 } from '../../1.mjs';
 export function add_1_test_1() {
-    error('todo: add_1_test_1');
+    assert(equal(add_1(1),2))
 }
