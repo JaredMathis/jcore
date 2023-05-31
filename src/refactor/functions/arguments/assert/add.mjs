aee3ffb8d8a63f2e5f605d65e46d17900c93f03b
JaredMathis <jared.mathis@native.consulting>
2023-05-31 09:08:47 -0400

    #52 function_auto refactor_functions_arguments_assert_add

--- src/refactor/functions/arguments/assert/add.mjs
+++ src/refactor/functions/arguments/assert/add.mjs
@@ -1,3 +1,4 @@
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { js_call_expression_to_name } from '../../../../js/call/expression/to/name.mjs';
 import { node_is_type_call_expression } from '../../../../node/is/type/call/expression.mjs';
 import { js_function_delcaration_to_statements } from '../../../../js/function/delcaration/to/statements.mjs';
@@ -6,6 +7,8 @@ import { js_export_function_single_or_null } from '../../../../js/export/functio
 import { file_js_all_map_args } from '../../../../file/js/all/map/args.mjs';
 import { error } from '../../../../error.mjs';
 import { list_first } from '../../../../list/first.mjs';
+import { equal } from '../../../../equal.mjs';
+import { function_name_get } from '../../../../function/name/get.mjs';
 export async function refactor_functions_arguments_assert_add() {
     await file_js_all_map_args(function mapper(args) {
         let {parsed, file_path} = args;
@@ -13,10 +16,13 @@ export async function refactor_functions_arguments_assert_add() {
         if (export_single === null) {
             return;
         }
+        let exists = false;
         let statements = js_function_delcaration_to_statements(export_single);
         let statement_first = list_first(statements);
         if (node_is_type_call_expression(statement_first)) {
             let name = js_call_expression_to_name(statement_first);
+            if (equal(name, function_name_get(arguments_assert))) {
+            }
         }
         console.log(statement_first);
         error();
