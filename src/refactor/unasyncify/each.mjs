297fab3641abcaa8bf1ff57ce1c1d9e59015e153
JaredMathis <jared.mathis@native.consulting>
2023-05-31 17:44:27 -0400

    #56 function_unasyncify sandbox_async

--- src/refactor/unasyncify/each.mjs
+++ src/refactor/unasyncify/each.mjs
@@ -7,6 +7,6 @@ export function refactor_unasyncify_each(v) {
     if (js_node_is_await_expression(node)) {
         let arg = object_property_get(node, js_node_property_argument());
         object_replace(node, arg);
-        console.log({node})
     }
+    console.log({node})
 }
