29c7151ca864b659c778c294971253be0c0ac13a
JaredMathis <jared.mathis@native.consulting>
2023-05-31 17:55:52 -0400

    #56 function_auto refactor_unasyncify

--- src/refactor/unasyncify.mjs
+++ src/refactor/unasyncify.mjs
@@ -8,7 +8,7 @@ import { comment } from '../comment.mjs';
 import { js_export_function_single } from '../js/export/function/single.mjs';
 export async function refactor_unasyncify(args) {
     let {parsed} = args;
-    comment(`To remove this assert, fix the code`);
+    comment(`To remove this assert, fix the code - right now it does not handle functions or arrows inside the exported function`);
     assert(!js_callable_multiple(parsed));
     js_visit_nodes_all(parsed, refactor_unasyncify_each);
     let fd = js_export_function_single(parsed);
