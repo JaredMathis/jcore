42c5bb4eeea14b82dae7f4ca46284c42f33d3d2e
JaredMathis <jared.mathis@native.consulting>
2023-05-31 10:07:38 -0400

    #55 function_dependencies function_dependencies

--- src/file/js/dependencies.mjs
+++ src/file/js/dependencies.mjs
@@ -1,6 +1,7 @@
+import { js_import_all } from '../../js/import/all.mjs';
 import { file_js_parse } from './parse.mjs';
 export async function file_js_dependencies(file_path) {
     let parsed = await file_js_parse(file_path);
+    let imports = js_import_all(parsed);
 
-    
 }
