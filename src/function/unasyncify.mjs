d672579dbf942e9ef60f53f9ed0b41c59809e74a
JaredMathis <jared.mathis@native.consulting>
2023-05-31 17:36:29 -0400

    #56 function_auto function_unasyncify

--- src/function/unasyncify.mjs
+++ src/function/unasyncify.mjs
@@ -1,4 +1,4 @@
-import { function_delete } from './delete.mjs';
+import { function_delete_if_exists } from './delete/if/exists.mjs';
 import { function_suffix_async } from './suffix/async.mjs';
 import { function_name_separator } from './name/separator.mjs';
 import { refactor_unasyncify } from '../refactor/unasyncify.mjs';
@@ -6,12 +6,10 @@ import { function_map } from './map.mjs';
 import { function_name_get } from './name/get.mjs';
 import { string_suffix_without } from '../string/suffix/without.mjs';
 import { function_copy } from './copy.mjs';
-import { function_exists } from './exists.mjs';
 export async function function_unasyncify(function_name) {
     let ending = `${ function_name_separator() }${ function_suffix_async() }`;
     let function_name_result = string_suffix_without(function_name, ending);
-    if (function_exists(function_name_result))
-        function_delete(function_name_result);
+    await function_delete_if_exists(function_name_result);
     await function_copy(function_name, function_name_result);
     await function_map(function_name_get(refactor_unasyncify), function_name_result);
 }
