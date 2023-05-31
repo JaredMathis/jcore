fe311341708b3dde99c2026794df0505fe78e268
JaredMathis <jared.mathis@native.consulting>
2023-05-31 08:30:50 -0400

    #52 function_auto refactor_functions_arguments_assert_add

--- src/refactor/functions/arguments/assert/add.mjs
+++ src/refactor/functions/arguments/assert/add.mjs
@@ -3,6 +3,6 @@ import { file_js_all_map_args } from '../../../../file/js/all/map/args.mjs';
 export async function refactor_functions_arguments_assert_add() {
     await file_js_all_map_args(function mapper(args) {
         let {parsed, file_path} = args;
-        js_export_function_single_or_null(parsed);
+        let export_single = js_export_function_single_or_null(parsed);
     });
 }
