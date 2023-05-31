0d4d2d086f41fa24cfa4c6530fbb04706d88927b
JaredMathis <jared.mathis@native.consulting>
2023-05-31 16:28:26 -0400

    #56 function_auto js_callable_multiple

--- src/refactor/unasyncify.mjs
+++ src/refactor/unasyncify.mjs
@@ -4,6 +4,6 @@ import { js_visit_nodes_all } from '../js/visit/nodes/all.mjs';
 import { assert } from '../assert.mjs';
 export async function refactor_unasyncify(args) {
     let {parsed} = args;
-    assert(!js_callable_multiple(args));
+    assert(!js_callable_multiple(parsed));
     js_visit_nodes_all(parsed, refactor_unasyncify_each);
 }
