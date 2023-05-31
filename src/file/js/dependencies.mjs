9199ccc040e7626ef698107d428056ef18b3f3a0
JaredMathis <jared.mathis@native.consulting>
2023-05-31 15:41:07 -0400

    closes #58 data_task_next 57

--- src/file/js/dependencies.mjs
+++ src/file/js/dependencies.mjs
@@ -2,7 +2,6 @@ import { visit } from '../../visit.mjs';
 import { file_js_dependencies_non_recursive } from './dependencies/non/recursive.mjs';
 export async function file_js_dependencies(file_path) {
     visit(file_path, node => {
-        
-    })
+    });
     return await file_js_dependencies_non_recursive(file_path);
 }
