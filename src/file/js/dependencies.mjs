a34b042e3afc0ca5e092b407e67e4d09427f136d
JaredMathis <jared.mathis@native.consulting>
2023-06-01 12:33:47 -0400

    #55 function_auto file_js_dependencies

--- src/file/js/dependencies.mjs
+++ src/file/js/dependencies.mjs
@@ -1,4 +1,5 @@
-import { visit_ignore_duplicates } from '../../visit/ignore/duplicates.mjs';
+import { log } from '../../log.mjs';
+import { visit_ignore_duplicates_async } from '../../visit/ignore/duplicates/async.mjs';
 import { metadata } from '../../metadata.mjs';
 import { file_js_dependencies_non_recursive } from './dependencies/non/recursive.mjs';
 export async function file_js_dependencies(file_path) {
