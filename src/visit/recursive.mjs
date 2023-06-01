496ee61a97305647459a39ca76beb0b00a647f2b
JaredMathis <jared.mathis@native.consulting>
2023-06-01 12:08:56 -0400

    #55 function_unasyncify visit_recursive_async

--- src/visit/recursive.mjs
+++ src/visit/recursive.mjs
@@ -1,19 +1,8 @@
 import { metadata_generated } from '../metadata/generated.mjs';
 import { metadata } from '../metadata.mjs';
-import { list_last_remove_verify } from '../list/last/remove/verify.mjs';
-import { list_add_exists_not } from '../list/add/exists/not.mjs';
-import { list_last_or_null } from '../list/last/or/null.mjs';
-export function visit_recursive(node, children_get, lambda, stack) {
-    let parent = list_last_or_null(stack);
-    list_add_exists_not(stack, node);
-    lambda({
-        node,
-        parent
-    });
-    let children = children_get(node);
-    for (let c of children) {
-        visit_recursive(c, children_get, lambda, stack);
-    }
-    list_last_remove_verify(stack, node);
+import { visit_recursive_ignore_duplicates } from './recursive/ignore/duplicates.mjs';
+export function visit_recursive(node, children_get, lambda, stack, visited) {
+    let ignore_duplicates = false;
+    visit_recursive_ignore_duplicates(node, children_get, lambda, stack, visited, ignore_duplicates);
     metadata([metadata_generated()]);
 }
