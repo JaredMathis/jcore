d5f9a3e14a96fcebea16c44d9d37b7903c1411fd
JaredMathis <jared.mathis@native.consulting>
2023-06-01 07:14:23 -0400

    #56 function_auto file_js_all_map_args_if_function

--- src/file/js/all/map/args/if/function.mjs
+++ src/file/js/all/map/args/if/function.mjs
@@ -1,20 +1,7 @@
+import { js_mapper_with_function_declaration_get } from '../../../../../../js/mapper/with/function/declaration/get.mjs';
 import { metadata } from '../../../../../../metadata.mjs';
-import { js_export_function_single_or_null } from '../../../../../../js/export/function/single/or/null.mjs';
 import { file_js_all_map_args } from '../../args.mjs';
-import { object_merge } from '../../../../../../object/merge.mjs';
 export async function file_js_all_map_args_if_function(logic) {
     await file_js_all_map_args(js_mapper_with_function_declaration_get(logic));
     metadata([]);
-}
-
-function js_mapper_with_function_declaration_get(mapper) {
-    return async function js_mapper_with_function_declaration(args) {
-        let { parsed, file_path } = args;
-        let function_declaration = js_export_function_single_or_null(parsed);
-        if (function_declaration === null) {
-            return;
-        }
-        object_merge({ function_declaration }, args);
-        await mapper(args);
-    };
 }
