7cbb86e42e0cb79324b45e664fc5f8ef16e29067
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:44:40 -0400

    #60 git_pacp

--- src/js/call/expression/to/name.mjs
+++ src/js/call/expression/to/name.mjs
@@ -4,9 +4,11 @@ import { js_node_identifer_by_name_get } from '../../../node/identifer/by/name/g
 import { assert } from '../../../../assert.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { node_is_type_call_expression } from '../../../../node/is/type/call/expression.mjs';
+import { js_node_is_identifier } from '../../../node/is/identifier.mjs';
 export function js_call_expression_to_name(ce) {
     arguments_assert(arguments, [node_is_type_call_expression]);
     let callee = js_node_identifer_by_name_get(ce, 'callee');
+    assert(js_node_is_identifier(callee));
     let name = object_property_get(callee, 'name');
     return name;
     metadata([]);
