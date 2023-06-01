1640e5b8262163e7959e2c3f05ebd808b01bb15c
JaredMathis <jared.mathis@native.consulting>
2023-06-01 19:36:28 -0400

    #68 function_auto function_arguments_assert_extra_next

--- src/function/names/each.mjs
+++ src/function/names/each.mjs
@@ -3,7 +3,7 @@ import { function_name_all } from '../name/all.mjs';
 export async function function_names_each(logic) {
     const all = await function_name_all();
     for (let function_name of all) {
-        let parsed = function_parse(function_name);
+        let parsed = await function_parse(function_name);
         let result = logic({parsed});
         if (result) {
             return;
