b5b48b923395a5679d13d0cb5ba8e7ae16fd7cf3
JaredMathis <jared.mathis@native.consulting>
2023-06-01 10:16:24 -0400

    #55 function_auto function_copy

--- src/function/copy.mjs
+++ src/function/copy.mjs
@@ -1,4 +1,6 @@
+import { file_open_vs_code } from '../file/open/vs/code.mjs';
 import { function_copy_no_open } from './copy/no/open.mjs';
 export async function function_copy(function_name_to_copy, function_name_of_copy) {
     await function_copy_no_open(function_name_to_copy, function_name_of_copy);
+    await file_open_vs_code(function_name_of_copy);
 }
