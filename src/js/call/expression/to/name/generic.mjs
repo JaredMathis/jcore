5799cf3642ec39a608705014a12bfbaea63d3ada
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:55:49 -0400

    #60 git_pacp

--- src/js/call/expression/to/name/generic.mjs
+++ src/js/call/expression/to/name/generic.mjs
@@ -3,12 +3,12 @@ import { assert } from '../../../../../assert.mjs';
 import { js_node_is_identifier } from '../../../../node/is/identifier.mjs';
 import { js_node_identifer_by_name_get } from '../../../../node/identifer/by/name/get.mjs';
 export function js_call_expression_to_name_generic(ce, or_null) {
+    let callee = object_property_get(ce, 'callee');
     if (or_null) {
         if (!js_node_is_identifier(callee)) {
             return null;
         }
     }
-    let callee = js_node_identifer_by_name_get(ce, 'callee');
     assert(js_node_is_identifier(callee));
     let name = object_property_get(callee, 'name');
     return name;
