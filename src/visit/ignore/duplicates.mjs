4659edb19a25a8240e0631174261bea993adff06
JaredMathis <jared.mathis@native.consulting>
2023-06-01 12:19:56 -0400

    #55 function_unasyncify visit_ignore_duplicates_async

--- src/visit/ignore/duplicates.mjs
+++ src/visit/ignore/duplicates.mjs
@@ -1,8 +1,8 @@
 import { metadata_generated } from '../../metadata/generated.mjs';
 import { metadata } from '../../metadata.mjs';
 import { visit_recursive_ignore_duplicates } from '../recursive/ignore/duplicates.mjs';
-export function visit_ignore_duplicates(node, children_get, lambda, stack, visited) {
+export function visit_ignore_duplicates(node, children_get, lambda, stack) {
     let ignore_duplicates = true;
-    visit_recursive_ignore_duplicates(node, children_get, lambda, stack, visited, ignore_duplicates);
+    visit_recursive_ignore_duplicates(node, children_get, lambda, stack, [], ignore_duplicates);
     metadata([metadata_generated()]);
 }
