19b261207722227f9b1f159c59289b08f45584c4
JaredMathis <jared.mathis@native.consulting>
2023-05-31 22:08:19 -0400

    #60 git_pacp

--- src/js/statement/if/metadata.mjs
+++ src/js/statement/if/metadata.mjs
@@ -9,10 +9,10 @@ import { js_node_is_expression_statement } from '../../node/is/expression/statem
 export function js_statement_if_metadata(statement, if_statement_metadata) {
     if (js_node_is_expression_statement(statement)) {
         let last_expression = object_property_get(statement, js_node_property_expression());
-        console.log(last_expression)
-        assert(node_is_type_call_expression(last_expression));
-        js_node_call_expression_if_name_equal(last_expression, function_name_get(metadata), function on_name_equal() {
-            if_statement_metadata(statement, last_expression);
-        });
+        if (node_is_type_call_expression(last_expression)) {
+            js_node_call_expression_if_name_equal(last_expression, function_name_get(metadata), function on_name_equal() {
+                if_statement_metadata(statement, last_expression);
+            });
+        }
     }
 }
