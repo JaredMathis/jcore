b155ffdfdc421e0d3a885839d86d4522ac3c7ca3
JaredMathis <jared.mathis@native.consulting>
2023-06-01 18:05:17 -0400

    #69 function_auto string_identifier_is

--- src/string/letter/digit/or/underscore/is.mjs
+++ src/string/letter/digit/or/underscore/is.mjs
@@ -2,14 +2,14 @@ import { string_underscore_is } from '../../../../underscore/is.mjs';
 import { string_digit_is } from '../../../../digit/is.mjs';
 import { string_letter_is } from '../../../is.mjs';
 export function string_letter_digit_or_underscore_is(c) {
-    if (!string_letter_is(c)) {
-        return false;
+    if (string_letter_is(c)) {
+        return true;
     }
-    if (!string_digit_is(c)) {
-        return false;
+    if (string_digit_is(c)) {
+        return true;
     }
-    if (!string_underscore_is(c)) {
-        return false;
+    if (string_underscore_is(c)) {
+        return true;
     }
-    return true;
+    return false;
 }
