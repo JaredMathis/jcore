bc8fc64a81931da748430df2b5805b0dfe4b9f52
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:48:47 -0400

    #60 function_auto js_call_expression_to_name_or_null

--- src/js/node/call/expression/if/name/equal.mjs
+++ src/js/node/call/expression/if/name/equal.mjs
@@ -4,7 +4,7 @@ import { js_call_expression_to_name } from '../../../../../call/expression/to/na
 import { node_is_type_call_expression } from '../../../../../../node/is/type/call/expression.mjs';
 export function js_node_call_expression_if_name_equal(node, name_expected, on_name_equal) {
     if (node_is_type_call_expression(node)) {
-        let name_actual = js_call_expression_to_name(node);
+        let name_actual = js_call_expression_to_name_or_null(node);
         if (equal(name_actual, name_expected)) {
             on_name_equal();
         }
