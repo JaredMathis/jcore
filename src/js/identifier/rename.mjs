d6b8c3b235d3c8ee2540d7f02b66618d1e56ece4
JaredMathis <jared.mathis@native.consulting>
2023-06-01 08:33:19 -0400

    #56 git_pacp

--- src/js/identifier/rename.mjs
+++ src/js/identifier/rename.mjs
@@ -3,6 +3,17 @@ import { object_property_get } from '../../object/property/get.mjs';
 import { object_property_set } from '../../object/property/set.mjs';
 import { js_visit_nodes_identifier } from '../visit/nodes/identifier.mjs';
 export function js_identifier_rename(args, identifier_name_old, identifier_name_new) {
+    return js_identifier_rename_if(args, should_rename, name_new_get);
+    metadata([]);
+    function should_rename(name) {
+        return name === identifier_name_old
+    }
+    function name_new_get(name) {
+        return identifier_name_new
+    }
+}
+
+function js_identifier_rename_if(args, should_rename, name_new_get) {
     let changed = false;
     let parsed = object_property_get(args, 'parsed');
     js_visit_nodes_identifier(parsed, v => {
@@ -14,11 +25,4 @@ export function js_identifier_rename(args, identifier_name_old, identifier_name_
         }
     });
     return changed;
-    metadata([]);
-    function should_rename(name) {
-        return name === identifier_name_old
-    }
-    function name_new_get(name) {
-        return identifier_name_new
-    }
 }
