2703a4708586d271c386bdc85cff7c61ea8dbb0d
JaredMathis <jared.mathis@native.consulting>
2023-06-01 12:02:50 -0400

    #55 git_pacp

--- src/js/mapper/args/to/metadata/args.mjs
+++ src/js/mapper/args/to/metadata/args.mjs
@@ -7,13 +7,20 @@ import { list_last } from '../../../../../list/last.mjs';
 export async function js_mapper_args_to_metadata_args(args) {
     let {function_declaration} = args;
     let statements = js_function_delcaration_to_statements(function_declaration);
+    let missing = false;
     if (list_length_is_0(statements)) {
+        missing = true;
+    } else {
+        let last_statement = list_last(statements);
+        if (!js_statement_metadata_is(last_statement)) {
+            missing = true;
+        }
+    }
+    if (missing) {
         await refactor_metadata_missing_add(args);
     }
-    let last_statement = list_last(statements);
-    if (!js_statement_metadata_is(last_statement)) {
-        await refactor_metadata_missing_add(args);
-    }
-    let metadata_args = js_statement_metadata_args_get(last_statement);
+    let statements2 = js_function_delcaration_to_statements(function_declaration);
+    let last_statement2 = list_last(statements2);
+    let metadata_args = js_statement_metadata_args_get(last_statement2);
     return metadata_args;
 }
