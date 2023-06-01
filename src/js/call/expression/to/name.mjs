c733700d17492e9067a4179eb311ddedf99e7f90
JaredMathis <jared.mathis@native.consulting>
2023-05-31 20:37:12 -0400

    #60 function_auto js_call_expression_to_name

--- src/js/call/expression/to/name.mjs
+++ src/js/call/expression/to/name.mjs
@@ -1,6 +1,9 @@
+import { object_property_get } from '../../../../object/property/get.mjs';
 import { metadata } from '../../../../metadata.mjs';
-import { js_id_name_get } from '../../../id/name/get.mjs';
+import { js_node_identifer_by_name_get } from '../../../node/identifer/by/name/get.mjs';
 export function js_call_expression_to_name(ce) {
-    return js_id_name_get(ce);
+    let callee = js_node_identifer_by_name_get(ce, 'callee');
+    let name = object_property_get(callee, 'name');
+    return name;
     metadata([]);
 }
