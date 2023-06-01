261779762fe222ee432f47a97e6142010aae95ae
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:37:16 -0400

    #60 git_pacp

--- src/js/node/identifer/by/name/get.mjs
+++ src/js/node/identifer/by/name/get.mjs
@@ -4,7 +4,6 @@ import { assert } from '../../../../../assert.mjs';
 import { object_property_get } from '../../../../../object/property/get.mjs';
 export function js_node_identifer_by_name_get(node, node_identifier_name) {
     let id = object_property_get(node, node_identifier_name);
-    console.log({node,node_identifier_name,id})
     assert(js_node_is_identifier(id));
     return id;
     metadata([]);
