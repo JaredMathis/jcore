6b40fa250858ee02110b9f639a4600c679988bb7
JaredMathis <jared.mathis@native.consulting>
2023-06-01 19:39:49 -0400

    #68 function_arguments_assert_extra_next

--- src/arguments/assert/test/1.mjs
+++ src/arguments/assert/test/1.mjs
@@ -2,7 +2,6 @@ import { metadata } from '../../../metadata.mjs';
 import { integer_is } from '../../../integer/is.mjs';
 import { arguments_assert } from '../../assert.mjs';
 export function arguments_assert_test_1() {
-    arguments_assert(arguments, []);
     arguments_assert([
         1,
         2,
