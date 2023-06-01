47154857eded19ff51660d643686b8b5655531cb
JaredMathis <jared.mathis@native.consulting>
2023-06-01 19:53:36 -0400

    #71 function_auto refactor_metadata_add

--- src/function/metadata/add.mjs
+++ src/function/metadata/add.mjs
@@ -2,4 +2,5 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 export function function_metadata_add(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
+    await function_map(function_name_get(refactor_metadata_add), function_name);
 }
---
+++ src/refactor/metadata/add.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function refactor_metadata_add() {
+    arguments_assert(arguments, []);
+    error('todo: refactor_metadata_add');
+}
