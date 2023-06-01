76f7b33d9596b73b585b0c7bd54e35ded82bcf29
JaredMathis <jared.mathis@native.consulting>
2023-06-01 16:22:27 -0400

    #70 function_auto js_statement_if_metadata

--- src/js/statement/if/metadata.mjs
+++ src/js/statement/if/metadata.mjs
@@ -1,24 +1,15 @@
+import { js_node_call_expression_statement_if_name_equal } from '../../node/call/expression/statement/if/name/equal.mjs';
 import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { function_name_get } from '../../../function/name/get.mjs';
-import { js_node_call_expression_if_name_equal } from '../../node/call/expression/if/name/equal.mjs';
-import { node_is_type_call_expression } from '../../../node/is/type/call/expression.mjs';
-import { js_node_property_expression } from '../../node/property/expression.mjs';
-import { object_property_get } from '../../../object/property/get.mjs';
-import { js_node_is_expression_statement } from '../../node/is/expression/statement.mjs';
 export function js_statement_if_metadata(statement, if_statement_metadata) {
     arguments_assert(arguments, [
         tautology,
         tautology
     ]);
     let name_expected = function_name_get(metadata);
-    if (js_node_is_expression_statement(statement)) {
-        let expression = object_property_get(statement, js_node_property_expression());
-        if (node_is_type_call_expression(expression)) {
-            js_node_call_expression_if_name_equal(expression, name_expected, on_name_equal);
-        }
-    }
+    js_node_call_expression_statement_if_name_equal(statement, name_expected, on_name_equal);
     function on_name_equal() {
         if_statement_metadata(statement, expression);
     }
