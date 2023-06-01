0219b78405ced0747cee351247726c5028200c85
JaredMathis <jared.mathis@native.consulting>
2023-06-01 14:05:20 -0400

    #52 git_pacp

--- src/directory/read.mjs
+++ src/directory/read.mjs
@@ -5,13 +5,19 @@ import { comment } from '../comment.mjs';
 import * as fs from 'fs/promises';
 import path from 'path';
 comment('From: https://gist.github.com/kethinov/6658166');
-export async function directory_read(dir, file_list = []) {
-    if (false) {
-        arguments_assert(arguments, [
-            tautology,
-            tautology
-        ]);
-    }
+export async function directory_read(dir) {
+    arguments_assert(arguments, [
+        tautology
+    ]);
+    return await directory_read_recursive(dir, file_list);
+    metadata([]);
+}
+
+async function directory_read_recursive(dir, file_list) {
+    arguments_assert(arguments, [
+        tautology,
+        tautology
+    ]);
     const files = await fs.readdir(dir);
     for (let file of files) {
         const file_path = path.join(dir, file);
@@ -22,6 +28,5 @@ export async function directory_read(dir, file_list = []) {
             file_list.push(file_path);
         }
     }
-    return file_list;
-    metadata([]);
+    return { __return: file_list, file_list };
 }
