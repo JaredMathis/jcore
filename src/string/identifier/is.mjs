ae7d607be47879fdae77937d02e16c4cd3829b48
JaredMathis <jared.mathis@native.consulting>
2023-06-01 17:16:06 -0400

    #69 function_auto string_identifier_is

--- src/string/identifier/is.mjs
+++ src/string/identifier/is.mjs
@@ -1,11 +1,16 @@
+import { string_length_is_0 } from '../length/is/0.mjs';
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
-import { todo } from '../../todo.mjs';
 import { string_is } from '../is.mjs';
 export function string_identifier_is(value) {
     arguments_assert(arguments, [tautology]);
-    todo(`Make this better (i.e. only letters and underscores and numbers or something)`);
-    return string_is(value);
+    if (!string_is(value)) {
+        return false;
+    }
+    if (string_length_is_0(value)) {
+        return true;
+    }
+    return true;
     metadata([]);
 }
---
+++ src/string/length/is/0.mjs
@@ -0,0 +1,4 @@
+import { string_length } from '../../length.mjs';
+export function string_length_is_0(value) {
+    return string_length(value) === 0;
+}
