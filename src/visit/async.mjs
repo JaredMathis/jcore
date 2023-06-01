163b179cbd7db57094408ee995bb7c235800a7bc
JaredMathis <jared.mathis@native.consulting>
2023-06-01 12:11:23 -0400

    #55 git_pacp

--- src/visit/async.mjs
+++ src/visit/async.mjs
@@ -1,6 +1,6 @@
 import { visit_recursive_async } from './recursive/async.mjs';
 import { metadata } from '../metadata.mjs';
 export async function visit_async(root, children_get, lambda_local) {
-    await visit_recursive_async(root, children_get, lambda_local, []);
+    await visit_recursive_async(root, children_get, lambda_local, [], []);
     metadata([]);
 }
