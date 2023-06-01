efa854d7b85b5973bf5b0c46e816e7d0db1f6f92
JaredMathis <jared.mathis@native.consulting>
2023-05-31 21:30:24 -0400

    #60 function_auto refactor_functions_metadata_add

---
+++ src/js/statement/if/metadata.mjs
@@ -0,0 +1,17 @@
+import { metadata } from '../../../metadata.mjs';
+import { function_name_get } from '../../../function/name/get.mjs';
+import { js_node_call_expression_if_name_equal } from '../../node/call/expression/if/name/equal.mjs';
+import { node_is_type_call_expression } from '../../../node/is/type/call/expression.mjs';
+import { assert } from '../../../assert.mjs';
+import { js_node_property_expression } from '../../node/property/expression.mjs';
+import { object_property_get } from '../../../object/property/get.mjs';
+import { js_node_is_expression_statement } from '../../node/is/expression/statement.mjs';
+export function js_statement_if_metadata(last_statement, if_statement_metadata) {
+    if (js_node_is_expression_statement(last_statement)) {
+        let last_expression = object_property_get(last_statement, js_node_property_expression());
+        assert(node_is_type_call_expression(last_expression));
+        js_node_call_expression_if_name_equal(last_expression, function_name_get(metadata), function on_name_equal() {
+            if_statement_metadata(last_statement, last_expression);
+        });
+    }
+}
--- src/refactor/functions/metadata/add.mjs
+++ src/refactor/functions/metadata/add.mjs
@@ -1,33 +1,22 @@
+import { js_statement_if_metadata } from '../../../js/statement/if/metadata.mjs';
 import { file_js_all_map_args_if_function } from '../../../file/js/all/map/args/if/function.mjs';
-import { js_node_is_expression_statement } from '../../../js/node/is/expression/statement.mjs';
-import { assert } from '../../../assert.mjs';
 import { refactor_import_fix } from '../../import/fix.mjs';
 import { js_parse_statement } from '../../../js/parse/statement.mjs';
 import { js_statement_end } from '../../../js/statement/end.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { function_name_get } from '../../../function/name/get.mjs';
-import { js_node_call_expression_if_name_equal } from '../../../js/node/call/expression/if/name/equal.mjs';
 import { js_function_delcaration_to_statements } from '../../../js/function/delcaration/to/statements.mjs';
 import { list_length_is_0 } from '../../../list/length/is/0.mjs';
 import { list_last } from '../../../list/last.mjs';
 import { list_add } from '../../../list/add.mjs';
 import { js_code_call_expression_with_args } from '../../../js/code/call/expression/with/args.mjs';
-import { js_node_property_expression } from '../../../js/node/property/expression.mjs';
-import { object_property_get } from '../../../object/property/get.mjs';
-import { node_is_type_call_expression } from '../../../node/is/type/call/expression.mjs';
 export async function refactor_functions_metadata_add() {
     await file_js_all_map_args_if_function(async function logic(fd, args) {
         let already_exists = false;
         let statements = js_function_delcaration_to_statements(fd);
         if (!list_length_is_0(statements)) {
             let last_statement = list_last(statements);
-            if (js_node_is_expression_statement(last_statement)) {
-                let last_expression = object_property_get(last_statement, js_node_property_expression());
-                assert(node_is_type_call_expression(last_expression));
-                js_node_call_expression_if_name_equal(last_expression, function_name_get(metadata), function on_name_equal() {
-                    if_statement_metadata(last_statement, last_expression);
-                });
-            }
+            js_statement_if_metadata(last_statement, if_statement_metadata);
             function if_statement_metadata(last_statement, last_expression) {
                 already_exists = true;
             }
