f913fa3306e77e8314c1c3cc4b25551cfd891029
JaredMathis <jared.mathis@native.consulting>
2023-06-01 10:26:59 -0400

    #55 git_pacp

--- src/visit.mjs
+++ src/visit.mjs
@@ -1,3 +1,7 @@
+
+import { metadata } from './metadata.mjs';
+import { metadata_generated } from './metadata/generated.mjs';
+import { visit_recursive } from './visit/recursive.mjs';
 export function visit(root, children_get, lambda_local) {
     visit_recursive(root, children_get, lambda_local, []);
     metadata([metadata_generated()]);
