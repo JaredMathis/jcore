2da125e3ffca7b96b68f9c8ef0b58f6e4dcc6d1b
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:48:31 -0400

    #60 function_auto js_call_expression_to_name_or_null

--- src/js/call/expression/to/name/or/null.mjs
+++ src/js/call/expression/to/name/or/null.mjs
@@ -4,7 +4,7 @@ import { node_is_type_call_expression } from '../../../../../../node/is/type/cal
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
 export function js_call_expression_to_name_or_null() {
     arguments_assert(arguments, [node_is_type_call_expression]);
-    let or_null = false;
+    let or_null = true;
     let name = js_call_expression_to_name_generic(ce, or_null);
     return name;
     metadata([]);
