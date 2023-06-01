ca2164911ba1b89248cfa22c45f7fe040f80c874
JaredMathis <jared.mathis@native.consulting>
2023-06-01 18:14:19 -0400

    #69 function_auto function_search

--- src/function/search.mjs
+++ src/function/search.mjs
@@ -3,10 +3,9 @@ import { list_length_is_1 } from '../list/length/is/1.mjs';
 import { list_filter } from '../list/filter.mjs';
 import { function_name_all } from './name/all.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-import { string_identifier_is } from '../string/identifier/is.mjs';
 import { string_includes } from '../string/includes.mjs';
 export async function function_search(query) {
-    arguments_assert(arguments, [string_identifier_is]);
+    arguments_assert(arguments, [string_identifer_sub_is]);
     const all = await function_name_all();
     let substrings = list_filter(all, a => {
         return string_includes(a, query);
