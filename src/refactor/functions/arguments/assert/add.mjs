5f1dcd44b812e0964cda41e83dc8f9981b9cbb84
JaredMathis <jared.mathis@native.consulting>
2023-06-01 13:14:50 -0400

    #52 git_pacp

--- src/refactor/functions/arguments/assert/add.mjs
+++ src/refactor/functions/arguments/assert/add.mjs
@@ -42,9 +42,11 @@ export async function refactor_functions_arguments_assert_add() {
             let params = object_property_get(function_declaration, 'params');
             let params_length = list_length(params);
             let params_mapped = list_map(range(params_length), i => function_name_get(tautology));
-            list_add_beginning(params_mapped, js_keyword_arguments());
-            let params_code = list_join(params_mapped, ', ');
-            let statement_new = js_parse_statement(`${ function_name_get(arguments_assert) }(${ params_code })`);
+            let params_joined = list_join(params_mapped, ', ');
+            let params_code = `[${params_joined}]`;
+            let params2 = [js_keyword_arguments(), params_code]
+            let params_code2 = list_join(params2, ', ');
+            let statement_new = js_parse_statement(`${ function_name_get(arguments_assert) }(${ params_code2 })`);
             list_add_beginning(statements, statement_new);
             await refactor_import_fix(args);
         }
