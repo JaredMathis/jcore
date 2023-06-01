6e99691313eea47003efe8f016a45efac5be26e9
JaredMathis <jared.mathis@native.consulting>
2023-06-01 12:51:33 -0400

    #55 git_pacp

--- src/file/js/dependencies.mjs
+++ src/file/js/dependencies.mjs
@@ -2,12 +2,15 @@ import { log } from '../../log.mjs';
 import { visit_ignore_duplicates_async } from '../../visit/ignore/duplicates/async.mjs';
 import { metadata } from '../../metadata.mjs';
 import { file_js_dependencies_non_recursive } from './dependencies/non/recursive.mjs';
+import { list_add } from '../../list/add.mjs';
 export async function file_js_dependencies(file_path) {
+    let result = [];
     await visit_ignore_duplicates_async(file_path, async node => {
         return await file_js_dependencies_non_recursive(node);
     }, v => {
         let {node} = v;
-        console.log(node);
+        list_add(result, node);
     });
+    return result;
     metadata([]);
 }
