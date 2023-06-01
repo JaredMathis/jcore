f660613829c080a2dac70ec355cdc0a44ae4581e
JaredMathis <jared.mathis@native.consulting>
2023-05-31 20:43:06 -0400

    #60 git_pacp

--- src/refactor/functions/metadata/add.mjs
+++ src/refactor/functions/metadata/add.mjs
@@ -13,6 +13,7 @@ import { list_add } from '../../../list/add.mjs';
 import { js_code_call_expression_with_args } from '../../../js/code/call/expression/with/args.mjs';
 import { js_node_property_expression } from '../../../js/node/property/expression.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
+import { node_is_type_call_expression } from '../../../node/is/type/call/expression.mjs';
 export async function refactor_functions_metadata_add() {
     await file_js_all_map_args(async function mapper(args) {
         let {parsed} = args;
@@ -20,18 +21,16 @@ export async function refactor_functions_metadata_add() {
         if (fd === null) {
             return;
         }
-        console.log(args.file_path)
         let already_exists = false;
         let statements = js_function_delcaration_to_statements(fd);
         if (!list_length_is_0(statements)) {
             let last_statement = list_last(statements);
             let last_expression = object_property_get(last_statement,js_node_property_expression() );
-            js_node_call_expression_on_name_equal(last_expression, function_name_get(metadata), function on_name_equal() {
-                already_exists = true;
-            });
-        }
-        if (already_exists) {
-            console.log('here')
+            if (node_is_type_call_expression(last_expression)) {
+                js_node_call_expression_on_name_equal(last_expression, function_name_get(metadata), function on_name_equal() {
+                    already_exists = true;
+                });
+            }
         }
         if (!already_exists) {
             let metadata_new_code = `${ js_code_call_expression_with_args(function_name_get(metadata), '[]') }${ js_statement_end() }`;
