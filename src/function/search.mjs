575be60be6fc19baa4ea6dfda0d15123fdf0d08c
JaredMathis <jared.mathis@native.consulting>
2023-06-01 17:10:08 -0400

    #69 function_auto function_search

--- src/function/search.mjs
+++ src/function/search.mjs
@@ -1,7 +1,7 @@
-import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { error } from '../error.mjs';
+import { string_identifier_is } from '../string/identifier/is.mjs';
 export function function_search(query) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_identifier_is]);
     error('todo: function_search');
 }
