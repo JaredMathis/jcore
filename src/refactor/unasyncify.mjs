dba402fed0f4f505715a091758ac6a590db1468d
JaredMathis <jared.mathis@native.consulting>
2023-06-01 07:36:39 -0400

    #56 function_auto refactor_unasyncify

--- src/refactor/unasyncify.mjs
+++ src/refactor/unasyncify.mjs
@@ -1,5 +1,4 @@
-import { js_statement_metadata_args_get } from '../js/statement/metadata/args/get.mjs';
-import { js_function_delcaration_to_statements } from '../js/function/delcaration/to/statements.mjs';
+import { js_function_declaration_to_metadata_args } from '../js/function/declaration/to/metadata/args.mjs';
 import { metadata } from '../metadata.mjs';
 import { js_keyword_async } from '../js/keyword/async.mjs';
 import { object_property_set } from '../object/property/set.mjs';
@@ -8,14 +7,12 @@ import { js_callable_multiple } from '../js/callable/multiple.mjs';
 import { js_visit_nodes_all } from '../js/visit/nodes/all.mjs';
 import { assert } from '../assert.mjs';
 import { comment } from '../comment.mjs';
-import { js_export_function_single } from '../js/export/function/single.mjs';
 export async function refactor_unasyncify(args) {
     let {parsed, function_declaration} = args;
     comment(`To remove this assert, fix the code - right now it does not handle functions or arrows inside the exported function`);
     assert(!js_callable_multiple(parsed));
     js_visit_nodes_all(parsed, refactor_unasyncify_each);
     object_property_set(function_declaration, js_keyword_async(), false);
-    let statements = js_function_delcaration_to_statements(function_declaration);
-    let last_metadata_args = js_statement_metadata_args_get(last_statement);
+    let last_metadata_args = js_function_declaration_to_metadata_args(function_declaration);
     metadata([]);
 }
