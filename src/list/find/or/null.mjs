f79294d05becc5bad7fcb2ea310cfd58a9327186
JaredMathis <jared.mathis@native.consulting>
2023-06-01 09:47:12 -0400

    #56 git_pacp

--- src/list/find/or/null.mjs
+++ src/list/find/or/null.mjs
@@ -1,5 +1,6 @@
 import { list_find_generic } from '../generic.mjs';
 export function list_find_or_null(list, predicate) {
     let or_null = true;
-    let r = list_find_generic(list, predicate, or_null);
+    let result = list_find_generic(list, predicate, or_null);
+    return result
 }
