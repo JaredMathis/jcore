5e81d3763eb59e336f9d83f2255c3f2b68e7b872
JaredMathis <jared.mathis@native.consulting>
2023-06-01 07:25:26 -0400

    #56 git_pacp

--- src/js/mapper/with/function/declaration/get.mjs
+++ src/js/mapper/with/function/declaration/get.mjs
@@ -2,6 +2,7 @@ import { object_merge } from '../../../../../object/merge.mjs';
 import { js_export_function_single_or_null } from '../../../../export/function/single/or/null.mjs';
 export function js_mapper_with_function_declaration_get(mapper) {
     return async function js_mapper_with_function_declaration(args) {
+        let skip_if_no_function_declaration = true;
         let {parsed, file_path} = args;
         let function_declaration = js_export_function_single_or_null(parsed);
         if (true) {
