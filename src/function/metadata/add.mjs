a0ca519dd1ec4ed4fd533483d3b4c012b66e7da9
JaredMathis <jared.mathis@native.consulting>
2023-06-01 19:54:01 -0400

    #71 function_auto function_metadata_add

--- src/function/metadata/add.mjs
+++ src/function/metadata/add.mjs
@@ -1,6 +1,9 @@
+import { refactor_metadata_add } from '../../refactor/metadata/add.mjs';
+import { function_name_get } from '../name/get.mjs';
+import { function_map } from '../map.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
-export function function_metadata_add(function_name) {
+export async function function_metadata_add(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     await function_map(function_name_get(refactor_metadata_add), function_name);
 }
