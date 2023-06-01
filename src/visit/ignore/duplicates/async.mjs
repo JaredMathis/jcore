8276dbe3b9dca22819aaba93828ee5c0689f7ad0
JaredMathis <jared.mathis@native.consulting>
2023-06-01 12:16:21 -0400

    #55 git_pacp

--- src/visit/ignore/duplicates/async.mjs
+++ src/visit/ignore/duplicates/async.mjs
@@ -1,7 +1,7 @@
 import { metadata } from '../../../metadata.mjs';
 import { visit_recursive_ignore_duplicates_async } from '../../recursive/ignore/duplicates/async.mjs';
 export async function visit_ignore_duplicates_async(node, children_get, lambda, stack, visited) {
-    let ignore_duplicates = false;
+    let ignore_duplicates = true;
     await visit_recursive_ignore_duplicates_async(node, children_get, lambda, stack, visited, ignore_duplicates);
     metadata([]);
 }
