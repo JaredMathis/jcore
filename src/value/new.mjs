4ff3be84c2e50b17e9085a44713083ddba800176
JaredMathis <jared.mathis@native.consulting>
2023-06-01 13:56:59 -0400

    #52 git_pacp

--- src/value/new.mjs
+++ src/value/new.mjs
@@ -1,10 +1,10 @@
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
-export function value_new(default_value) {
-    arguments_assert(arguments, [tautology]);
+export function value_new() {
+    arguments_assert(arguments, []);
     return {
-        value: default_value,
+        value: undefined,
         set: false
     };
     metadata([]);
