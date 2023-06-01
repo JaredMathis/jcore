599b89ccec642fa390420d4e3cf73f6ac183fa52
JaredMathis <jared.mathis@native.consulting>
2023-06-01 08:16:15 -0400

    #56 git_pacp

--- src/refactor/unasyncify.mjs
+++ src/refactor/unasyncify.mjs
@@ -10,6 +10,7 @@ import { js_visit_nodes_all } from '../js/visit/nodes/all.mjs';
 import { assert } from '../assert.mjs';
 import { comment } from '../comment.mjs';
 import { function_name_get } from '../function/name/get.mjs';
+import { list_add } from '../list/add.mjs';
 export async function refactor_unasyncify(args) {
     let {parsed, function_declaration} = args;
     comment(`To remove this assert, fix the code - right now it does not handle functions or arrows inside the exported function`);
@@ -19,5 +20,7 @@ export async function refactor_unasyncify(args) {
     let metadata_args = await js_function_declaration_to_metadata_args(function_declaration);
     let metadata_function = metadata_generated;
     let metadata_function_parsed = js_parse_call_expression(function_name_get(metadata_function));
+    list_add(metadata_args, metadata_function_parsed);
+
     metadata([]);
 }
