4451dfaba626d554ceccc4e5a85c1d1c0a7e016c
JaredMathis <jared.mathis@native.consulting>
2023-06-01 09:33:48 -0400

    #56 function_auto js_imports_remove_multiple

--- src/js/imports/remove/multiple.mjs
+++ src/js/imports/remove/multiple.mjs
@@ -3,10 +3,8 @@ import { object_property_get } from '../../../object/property/get.mjs';
 import { list_find } from '../../../list/find.mjs';
 import { list_map } from '../../../list/map.mjs';
 export function js_imports_remove_multiple(parsed, imports, removal_names) {
-    js_imports_remove_generic(parsed, imports, imports_to_remove_get);
-    function imports_to_remove_get(imports) {
-        return list_map(removal_names, removal_name => {
-            return list_find(imports, i => object_property_get(i, 'name') === removal_name);
-        });
-    }
+    let imports_to_remove = list_map(removal_names, removal_name => {
+        return list_find(imports, i => object_property_get(i, 'name') === removal_name);
+    });
+    js_imports_remove_generic(parsed, imports_to_remove);
 }
