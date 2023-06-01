72653e1ca233a30023e954d18601c99068c689ad
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:24:03 -0400

    #60 git_pacp

--- src/js/statement/if/metadata.mjs
+++ src/js/statement/if/metadata.mjs
@@ -7,10 +7,10 @@ import { object_property_get } from '../../../object/property/get.mjs';
 import { js_node_is_expression_statement } from '../../node/is/expression/statement.mjs';
 export function js_statement_if_metadata(statement, if_statement_metadata) {
     if (js_node_is_expression_statement(statement)) {
-        let last_expression = object_property_get(statement, js_node_property_expression());
-        if (node_is_type_call_expression(last_expression)) {
-            js_node_call_expression_if_name_equal(last_expression, function_name_get(metadata), function on_name_equal() {
-                if_statement_metadata(statement, last_expression);
+        let expression = object_property_get(statement, js_node_property_expression());
+        if (node_is_type_call_expression(expression)) {
+            js_node_call_expression_if_name_equal(expression, function_name_get(metadata), function on_name_equal() {
+                if_statement_metadata(statement, expression);
             });
         }
     }
