73194a1982680e00b7d5a97a6151c3d32fb34f11
JaredMathis <jared.mathis@native.consulting>
2023-05-31 22:14:28 -0400

    #60 function_rename refactor_functions_metadata_add refactor_functions_metadata_missing_add

--- src/refactor/functions/metadata/extra/remove.mjs
+++ src/refactor/functions/metadata/extra/remove.mjs
@@ -1,4 +1,4 @@
-import { refactor_functions_metadata_add } from '../add.mjs';
+import { refactor_functions_metadata_missing_add } from '../missing/add.mjs';
 import { js_node_property_expression } from '../../../../js/node/property/expression.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
 import { subtract_1 } from '../../../../subtract/1.mjs';
@@ -10,15 +10,13 @@ import { list_last } from '../../../../list/last.mjs';
 import { list_length_is_0 } from '../../../../list/length/is/0.mjs';
 import { js_function_delcaration_to_statements } from '../../../../js/function/delcaration/to/statements.mjs';
 import { assert } from '../../../../assert.mjs';
-import { comment } from '../../../../comment.mjs';
 import { list_length } from '../../../../list/length.mjs';
 export async function refactor_functions_metadata_extra_remove() {
-    refactor_functions_metadata_add();
+    refactor_functions_metadata_missing_add();
     await file_js_all_map_args_if_function(async function logic(fd, args) {
         let statements = js_function_delcaration_to_statements(fd);
         if (!list_length_is_0(statements)) {
             let last_statement = list_last(statements);
-            comment(`Given we add missing earlier`);
             assert(js_statement_metadata_is(last_statement));
             let remaining = list_take(statements, subtract_1(list_length(statements)));
             for (let s of remaining) {
