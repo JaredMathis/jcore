20fed3179915960f477f50a507dc792469470b1a
JaredMathis <jared.mathis@native.consulting>
2023-06-01 09:43:21 -0400

    #56 function_auto js_imports_remove_multiple

--- src/js/imports/remove/multiple.mjs
+++ src/js/imports/remove/multiple.mjs
@@ -7,7 +7,7 @@ export function js_imports_remove_multiple(parsed, imports, removal_names) {
     let imports_to_remove = list_map(removal_names, removal_name => {
         let result = list_find(imports, i => object_property_get(i, 'name') === removal_name);
         let list, predicate;
-        let or_null = false;
+        let or_null = true;
         let r = list_find_generic(list, predicate, or_null);
         return result;
     });
