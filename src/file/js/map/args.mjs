78bfd7b8cf7a53a34250a4312090d4ca736bff23
JaredMathis <jared.mathis@native.consulting>
2023-06-01 13:34:12 -0400

    #52 function_rename file_js_path_to_args file_js_path_to_mapper_args

--- src/file/js/map/args.mjs
+++ src/file/js/map/args.mjs
@@ -1,9 +1,9 @@
 import { js_mapper_with_function_declaration_generic } from '../../../js/mapper/with/function/declaration/generic.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { file_js_overwrite } from '../overwrite.mjs';
-import { file_js_path_to_args } from '../path/to/args.mjs';
+import { file_js_path_to_mapper_args } from '../path/to/mapper/args.mjs';
 export async function file_js_map_args(file_path, mapper) {
-    let args = await file_js_path_to_args(file_path);
+    let args = await file_js_path_to_mapper_args(file_path);
     await js_mapper_with_function_declaration_generic(args, mapper, false);
     await file_js_overwrite(args);
     metadata([]);
---
+++ src/file/js/path/to/mapper/args.mjs
@@ -0,0 +1,11 @@
+import { metadata } from '../../../../../metadata.mjs';
+import { file_js_parse } from '../../../parse.mjs';
+export async function file_js_path_to_mapper_args(file_path) {
+    let parsed = await file_js_parse(file_path);
+    let args = {
+        parsed,
+        file_path
+    };
+    return args;
+    metadata([]);
+}
