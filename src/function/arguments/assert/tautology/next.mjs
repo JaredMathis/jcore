705dbdc5273f30f8431c4bd732e85d7eedbad6e5
JaredMathis <jared.mathis@native.consulting>
2023-06-01 16:08:20 -0400

    #67 function_auto function_arguments_assert_tautology_next

--- src/function/arguments/assert/tautology/next.mjs
+++ src/function/arguments/assert/tautology/next.mjs
@@ -1,3 +1,6 @@
+import { equal } from '../../../../equal.mjs';
+import { js_call_expression_to_name_or_null } from '../../../../js/call/expression/to/name/or/null.mjs';
+import { node_is_type_call_expression } from '../../../../node/is/type/call/expression.mjs';
 import { js_export_function_single } from '../../../../js/export/function/single.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { function_name_get } from '../../../name/get.mjs';
@@ -16,5 +19,13 @@ export async function function_arguments_assert_tautology_next() {
         js_node_call_expression_if_name_equal(statement_first, function_name_get(arguments_assert), function on_name_equal() {
             exists = true;
         });
+        if (node_is_type_call_expression(node)) {
+            let name_actual = js_call_expression_to_name_or_null(node);
+            if (name_actual != null) {
+                if (equal(name_actual, name_expected)) {
+                    on_name_equal();
+                }
+            }
+        }
     }
 }
