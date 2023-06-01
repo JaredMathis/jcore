95d97aa32601ec130174aff6a5c82ffe285bb9ea
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:52:53 -0400

    #60 git_pacp

--- src/js/call/expression/to/name/or/null.mjs
+++ src/js/call/expression/to/name/or/null.mjs
@@ -2,7 +2,7 @@ import { metadata } from '../../../../../../metadata.mjs';
 import { js_call_expression_to_name_generic } from '../generic.mjs';
 import { node_is_type_call_expression } from '../../../../../../node/is/type/call/expression.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
-export function js_call_expression_to_name_or_null() {
+export function js_call_expression_to_name_or_null(ce) {
     arguments_assert(arguments, [node_is_type_call_expression]);
     let or_null = true;
     let name = js_call_expression_to_name_generic(ce, or_null);
