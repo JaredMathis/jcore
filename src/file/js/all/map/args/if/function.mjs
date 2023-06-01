6a845133fa00cbd9dea34e022d16e290bf30eb4a
JaredMathis <jared.mathis@native.consulting>
2023-06-01 07:07:07 -0400

    #56 git_pacp

--- src/file/js/all/map/args/if/function.mjs
+++ src/file/js/all/map/args/if/function.mjs
@@ -1,6 +1,7 @@
 import { metadata } from '../../../../../../metadata.mjs';
 import { js_export_function_single_or_null } from '../../../../../../js/export/function/single/or/null.mjs';
 import { file_js_all_map_args } from '../../args.mjs';
+import { object_merge } from '../../../../../../object/merge.mjs';
 export async function file_js_all_map_args_if_function(logic) {
     await file_js_all_map_args(async function mapper(args) {
         let {parsed, file_path} = args;
@@ -8,6 +9,7 @@ export async function file_js_all_map_args_if_function(logic) {
         if (function_declaration === null) {
             return;
         }
+        object_merge({function_declaration}, args);
         await logic(function_declaration, args);
     });
     metadata([]);
