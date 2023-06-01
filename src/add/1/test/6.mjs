84f43563a0698c1ae850cb9e5cfcbdcb6fd513ab
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:22:48 -0400

    #60 function_auto refactor_functions_metadata_extra_remove

--- src/add/1/test/6.mjs
+++ src/add/1/test/6.mjs
@@ -4,7 +4,7 @@ import { equal } from '../../../equal.mjs';
 import { assert } from '../../../assert.mjs';
 export function add_1_test_6() {
     assert(equal(add_1(-3), -2));
-    metadata([1]);
+    metadata([2, 3]);
     metadata([
         1,
         1
