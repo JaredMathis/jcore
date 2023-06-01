b94eb99c77bef3152e71424d1cdb34e6955cdcd3
JaredMathis <jared.mathis@native.consulting>
2023-06-01 19:52:13 -0400

    #71 function_auto function_metadata_add

--- src/function/metadata/add.mjs
+++ src/function/metadata/add.mjs
@@ -1,6 +1,5 @@
-import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_metadata_add() {
-    arguments_assert(arguments, []);
-    error('todo: function_metadata_add');
+import { string_identifier_is } from '../../string/identifier/is.mjs';
+export function function_metadata_add(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
 }
