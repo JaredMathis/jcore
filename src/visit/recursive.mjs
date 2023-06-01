85c6017161da2296b67fbdea1961f560e4e71f63
JaredMathis <jared.mathis@native.consulting>
2023-06-01 11:19:16 -0400

    #55 git_pacp

--- src/visit/recursive.mjs
+++ src/visit/recursive.mjs
@@ -2,6 +2,7 @@ import { metadata } from '../metadata.mjs';
 import { list_last_remove_verify } from '../list/last/remove/verify.mjs';
 import { list_add_exists_not } from '../list/add/exists/not.mjs';
 import { list_last_or_null } from '../list/last/or/null.mjs';
+import { metadata_generated } from '../metadata/generated.mjs';
 export function visit_recursive(node, children_get, lambda, stack) {
     let parent = list_last_or_null(stack);
     list_add_exists_not(stack, node);
