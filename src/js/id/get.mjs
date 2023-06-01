60fae809b372bf22f675f798400208a41eeaa44d
JaredMathis <jared.mathis@native.consulting>
2023-05-31 20:34:31 -0400

    #60 git_pacp

--- src/js/id/get.mjs
+++ src/js/id/get.mjs
@@ -3,7 +3,8 @@ import { js_node_is_identifier } from '../node/is/identifier.mjs';
 import { assert } from '../../assert.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 export function js_id_get(fd) {
-    let id = object_property_get(fd, 'id');
+    const node_identifier_name = 'id';
+    let id = object_property_get(fd, node_identifier_name);
     assert(js_node_is_identifier(id));
     return id;
     metadata([]);
--- src/refactor/functions/metadata/add.mjs
+++ src/refactor/functions/metadata/add.mjs
@@ -12,6 +12,7 @@ import { list_last } from '../../../list/last.mjs';
 import { list_add } from '../../../list/add.mjs';
 import { js_code_call_expression_with_args } from '../../../js/code/call/expression/with/args.mjs';
 import { js_node_property_expression } from '../../../js/node/property/expression.mjs';
+import { object_property_get } from '../../../object/property/get.mjs';
 export async function refactor_functions_metadata_add() {
     await file_js_all_map_args(async function mapper(args) {
         let {parsed} = args;
@@ -23,20 +24,21 @@ export async function refactor_functions_metadata_add() {
         let statements = js_function_delcaration_to_statements(fd);
         if (!list_length_is_0(statements)) {
             let last_statement = list_last(statements);
-            let last_expression = js_node_property_expression();
-            console.log(last_expression)
-            js_node_call_expression_on_name_equal(last_statement, function_name_get(metadata), function on_name_equal() {
+            let last_expression = object_property_get(last_statement,js_node_property_expression() );
+            
+            console.log(last_expression);
+            js_node_call_expression_on_name_equal(last_expression, function_name_get(metadata), function on_name_equal() {
                 already_exists = true;
             });
         }
-        console.log(already_exists)
+        console.log(already_exists);
         if (false)
-        if (!already_exists) {
-            let metadata_new_code = `${ js_code_call_expression_with_args(function_name_get(metadata), '[]') }${ js_statement_end() }`;
-            let metadata_new = js_parse_statement(metadata_new_code);
-            list_add(statements, metadata_new);
-            await refactor_import_fix(args);
-        }
+            if (!already_exists) {
+                let metadata_new_code = `${ js_code_call_expression_with_args(function_name_get(metadata), '[]') }${ js_statement_end() }`;
+                let metadata_new = js_parse_statement(metadata_new_code);
+                list_add(statements, metadata_new);
+                await refactor_import_fix(args);
+            }
     });
     metadata([]);
 }
