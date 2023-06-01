6190b0b3b8e5b5465070cad435fb4412854ee3c3
JaredMathis <jared.mathis@native.consulting>
2023-06-01 12:20:39 -0400

    #55 git_pacp

--- src/visit/recursive/.mjs
+++
@@ -1,8 +0,0 @@
-import { metadata_generated } from '../../metadata/generated.mjs';
-import { metadata } from '../../metadata.mjs';
-import { visit_recursive_ignore_duplicates } from './ignore/duplicates.mjs';
-export function visit_recursive_(node, children_get, lambda, stack, visited) {
-    let ignore_duplicates = false;
-    visit_recursive_ignore_duplicates(node, children_get, lambda, stack, visited, ignore_duplicates);
-    metadata([metadata_generated()]);
-}
