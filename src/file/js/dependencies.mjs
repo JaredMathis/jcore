1bfabe098fca908286c2180b3d5d1411d0991ac4
JaredMathis <jared.mathis@native.consulting>
2023-06-01 12:54:17 -0400

    closes #55 data_task_next 52

--- src/file/js/dependencies.mjs
+++ src/file/js/dependencies.mjs
@@ -1,4 +1,3 @@
-import { log } from '../../log.mjs';
 import { visit_ignore_duplicates_async } from '../../visit/ignore/duplicates/async.mjs';
 import { metadata } from '../../metadata.mjs';
 import { file_js_dependencies_non_recursive } from './dependencies/non/recursive.mjs';
