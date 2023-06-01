864bc1cd4e64dc680eac50bc00f4377d6aa0b1c6
JaredMathis <jared.mathis@native.consulting>
2023-05-31 21:44:46 -0400

    #60 function_auto list_last_index

--- src/list/last/index.mjs
+++ src/list/last/index.mjs
@@ -1,6 +1,7 @@
 import { metadata } from '../../metadata.mjs';
 import { list_length } from '../length.mjs';
 export function list_last_index(list) {
-    return list_length(list) - 1;
+    const l = list_length(list);
+    return l - 1;
     metadata([]);
 }
