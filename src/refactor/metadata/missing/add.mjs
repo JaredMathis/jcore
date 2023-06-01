4248344f6f0c5d4c9a55ad6f3eadd4121627931e
JaredMathis <jared.mathis@native.consulting>
2023-06-01 13:31:45 -0400

    #52 git_pacp

--- src/refactor/metadata/missing/add.mjs
+++ src/refactor/metadata/missing/add.mjs
@@ -13,8 +13,9 @@ export async function refactor_metadata_missing_add(args) {
     let {function_declaration} = args;
     let already_exists = false;
     let statements = js_function_delcaration_to_statements(function_declaration);
+    let last_statement;
     if (!list_length_is_0(statements)) {
-        let last_statement = list_last(statements);
+        last_statement = list_last(statements);
         js_statement_if_metadata(last_statement, function if_statement_metadata(last_statement, last_expression) {
             already_exists = true;
         });
