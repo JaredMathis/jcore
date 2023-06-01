1cf1827355690b761bc56c0bf61a193d00ea07ad
JaredMathis <jared.mathis@native.consulting>
2023-06-01 06:59:12 -0400

    #56 git_pacp

--- src/visit/recursive/async.mjs
+++ src/visit/recursive/async.mjs
@@ -1,6 +1,5 @@
 import { metadata } from '../../metadata.mjs';
 import { list_last_remove_verify } from '../../list/last/remove/verify.mjs';
-import { visit_recursive } from '../recursive.mjs';
 import { list_add_exists_not } from '../../list/add/exists/not.mjs';
 import { list_last_or_null } from '../../list/last/or/null.mjs';
 export async function visit_recursive_async(node, children_get, lambda, stack) {
