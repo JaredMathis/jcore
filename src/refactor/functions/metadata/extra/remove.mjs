e2f22e9e7f22e8d11dcebf0f4aef20d5f5c7a771
JaredMathis <jared.mathis@native.consulting>
2023-05-31 21:58:10 -0400

    #60 function_auto refactor_functions_metadata_extra_remove

--- src/refactor/functions/metadata/extra/remove.mjs
+++ src/refactor/functions/metadata/extra/remove.mjs
@@ -1,16 +1,8 @@
-import { js_visit_nodes } from '../../../../js/visit/nodes.mjs';
 import { subtract_1 } from '../../../../subtract/1.mjs';
 import { list_take } from '../../../../list/take.mjs';
 import { js_statement_metadata_is } from '../../../../js/statement/metadata/is.mjs';
-import { js_statement_if_metadata } from '../../../../js/statement/if/metadata.mjs';
 import { file_js_all_map_args_if_function } from '../../../../file/js/all/map/args/if/function.mjs';
-import { refactor_import_fix } from '../../../import/fix.mjs';
-import { list_add } from '../../../../list/add.mjs';
-import { js_parse_statement } from '../../../../js/parse/statement.mjs';
-import { js_statement_end } from '../../../../js/statement/end.mjs';
-import { js_code_call_expression_with_args } from '../../../../js/code/call/expression/with/args.mjs';
 import { metadata } from '../../../../metadata.mjs';
-import { function_name_get } from '../../../../function/name/get.mjs';
 import { list_last } from '../../../../list/last.mjs';
 import { list_length_is_0 } from '../../../../list/length/is/0.mjs';
 import { js_function_delcaration_to_statements } from '../../../../js/function/delcaration/to/statements.mjs';
@@ -19,7 +11,6 @@ import { comment } from '../../../../comment.mjs';
 import { list_length } from '../../../../list/length.mjs';
 export async function refactor_functions_metadata_extra_remove() {
     await file_js_all_map_args_if_function(async function logic(fd, args) {
-        let already_exists = false;
         let statements = js_function_delcaration_to_statements(fd);
         if (!list_length_is_0(statements)) {
             let last_statement = list_last(statements);
@@ -27,20 +18,9 @@ export async function refactor_functions_metadata_extra_remove() {
             assert(js_statement_metadata_is(last_statement));
             let remaining = list_take(subtract_1(list_length(statements)));
             for (let s of statements) {
-                js_visit_nodes(s, node => js_statement_metadata_is(node), v => {
-                    let {node} = v;
-                });
+                if (js_statement_metadata_is(node)) {
+                }
             }
-        }
-        if (false)
-            js_statement_if_metadata(last_statement, function if_statement_metadata(last_statement, last_expression) {
-                already_exists = true;
-            });
-        if (!already_exists) {
-            let metadata_new_code = `${ js_code_call_expression_with_args(function_name_get(metadata), '[]') }${ js_statement_end() }`;
-            let metadata_new = js_parse_statement(metadata_new_code);
-            list_add(statements, metadata_new);
-            await refactor_import_fix(args);
         }
     });
     metadata([]);
