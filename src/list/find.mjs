7996b827d586e93a61308d73807a5f17bc94e9af
JaredMathis <jared.mathis@native.consulting>
2023-06-01 09:42:09 -0400

    #56 git_pacp

--- src/list/find.mjs
+++ src/list/find.mjs
@@ -4,12 +4,16 @@ import { list_length_is_0 } from './length/is/0.mjs';
 import { list_single } from './single.mjs';
 export function list_find(list, predicate) {
     let or_null = false;
+    return list_find_generic(list, predicate, or_null);
+    metadata([]);
+}
+
+function list_find_generic(list, predicate, or_null) {
     let filtered = list_filter(list, predicate);
     if (or_null) {
         if (list_length_is_0(filtered)) {
-            // return null;
+            return null;
         }
     }
     return list_single(filtered);
-    metadata([]);
 }
