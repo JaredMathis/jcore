88419252ead9bb53d6653047a9e98d144bd9f80a
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:48:57 -0400

    #60 function_auto js_node_call_expression_if_name_equal

--- src/js/node/call/expression/if/name/equal.mjs
+++ src/js/node/call/expression/if/name/equal.mjs
@@ -1,6 +1,6 @@
+import { js_call_expression_to_name_or_null } from '../../../../../call/expression/to/name/or/null.mjs';
 import { metadata } from '../../../../../../metadata.mjs';
 import { equal } from '../../../../../../equal.mjs';
-import { js_call_expression_to_name } from '../../../../../call/expression/to/name.mjs';
 import { node_is_type_call_expression } from '../../../../../../node/is/type/call/expression.mjs';
 export function js_node_call_expression_if_name_equal(node, name_expected, on_name_equal) {
     if (node_is_type_call_expression(node)) {
