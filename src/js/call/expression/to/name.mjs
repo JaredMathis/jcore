1af0216f6dd8c2614bbbe246aceb0ca4b9b8d93a
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:47:55 -0400

    #60 function_auto js_call_expression_to_name

--- src/js/call/expression/to/name.mjs
+++ src/js/call/expression/to/name.mjs
@@ -1,26 +1,11 @@
-import { object_property_get } from '../../../../object/property/get.mjs';
+import { js_call_expression_to_name_generic } from './name/generic.mjs';
 import { metadata } from '../../../../metadata.mjs';
-import { js_node_identifer_by_name_get } from '../../../node/identifer/by/name/get.mjs';
-import { assert } from '../../../../assert.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { node_is_type_call_expression } from '../../../../node/is/type/call/expression.mjs';
-import { js_node_is_identifier } from '../../../node/is/identifier.mjs';
 export function js_call_expression_to_name(ce) {
     arguments_assert(arguments, [node_is_type_call_expression]);
-    let  or_null = false;
+    let or_null = false;
     let name = js_call_expression_to_name_generic(ce, or_null);
     return name;
     metadata([]);
-}
-
-function js_call_expression_to_name_generic(ce, or_null) {
-    let callee = js_node_identifer_by_name_get(ce, 'callee');
-    if (or_null) {
-        if (!js_node_is_identifier(callee)) {
-            return null;
-        }
-    }
-    assert(js_node_is_identifier(callee));
-    let name = object_property_get(callee, 'name');
-    return name;
 }
---
+++ src/js/call/expression/to/name/generic.mjs
@@ -0,0 +1,15 @@
+import { object_property_get } from '../../../../../object/property/get.mjs';
+import { assert } from '../../../../../assert.mjs';
+import { js_node_is_identifier } from '../../../../node/is/identifier.mjs';
+import { js_node_identifer_by_name_get } from '../../../../node/identifer/by/name/get.mjs';
+export function js_call_expression_to_name_generic(ce, or_null) {
+    let callee = js_node_identifer_by_name_get(ce, 'callee');
+    if (or_null) {
+        if (!js_node_is_identifier(callee)) {
+            return null;
+        }
+    }
+    assert(js_node_is_identifier(callee));
+    let name = object_property_get(callee, 'name');
+    return name;
+}
