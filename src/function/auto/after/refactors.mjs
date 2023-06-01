2ea35a17e4bf4df629a9ba0be65aeb03409544f9
JaredMathis <jared.mathis@native.consulting>
2023-06-01 17:35:00 -0400

    #69 function_auto function_auto_after_refactors

--- src/function/auto/after/refactors.mjs
+++ src/function/auto/after/refactors.mjs
@@ -3,20 +3,17 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { refactor_functions_to_files } from '../../../refactor/functions/to/files.mjs';
 import { refactor_import_fix } from '../../../refactor/import/fix.mjs';
+import { list_all_combine } from '../../../list/all/combine.mjs';
 export function function_auto_after_refactors() {
     arguments_assert(arguments, []);
     let first = [
         refactor_arguments_assert_add,
-        refactor_import_fix,
-        refactor_functions_to_files
+        refactor_import_fix
     ];
-    if (false)
-        list_combine;
-    let result = [
-        refactor_arguments_assert_add,
-        refactor_import_fix,
-        refactor_functions_to_files
-    ];
-    return result;
+    let second = [refactor_functions_to_files];
+    return list_all_combine([
+        first,
+        second
+    ]);
     metadata([]);
 }
