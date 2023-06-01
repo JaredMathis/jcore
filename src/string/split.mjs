c7ecc6b19bfa588428248ad28f76d8b3f67769f3
JaredMathis <jared.mathis@native.consulting>
2023-06-01 19:56:10 -0400

    #71 git_pacp

--- src/string/split.mjs
+++ src/string/split.mjs
@@ -4,10 +4,6 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { string_is } from './is.mjs';
 export function string_split(input, separator) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
-    ]);
-    arguments_assert(arguments, [
         string_is,
         string_is
     ]);
