9a8a8efadac619b3ce41425658e7b7201f01ef0f
JaredMathis <jared.mathis@native.consulting>
2023-06-01 09:43:14 -0400

    #56 function_auto js_imports_remove_multiple

--- src/js/imports/remove/multiple.mjs
+++ src/js/imports/remove/multiple.mjs
@@ -1,3 +1,4 @@
+import { list_find_generic } from '../../../list/find/generic.mjs';
 import { js_imports_remove_generic } from './generic.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
 import { list_find } from '../../../list/find.mjs';
@@ -5,6 +6,9 @@ import { list_map } from '../../../list/map.mjs';
 export function js_imports_remove_multiple(parsed, imports, removal_names) {
     let imports_to_remove = list_map(removal_names, removal_name => {
         let result = list_find(imports, i => object_property_get(i, 'name') === removal_name);
+        let list, predicate;
+        let or_null = false;
+        let r = list_find_generic(list, predicate, or_null);
         return result;
     });
     js_imports_remove_generic(parsed, imports_to_remove);
