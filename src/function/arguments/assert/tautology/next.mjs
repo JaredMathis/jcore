3ae2b6293ce27d0317454f34d9a7a8080d046dfe
JaredMathis <jared.mathis@native.consulting>
2023-06-01 16:14:52 -0400

    #67 function_auto function_arguments_assert_tautology_next

--- src/function/arguments/assert/tautology/next.mjs
+++ src/function/arguments/assert/tautology/next.mjs
@@ -1,3 +1,4 @@
+import { equal } from '../../../../equal.mjs';
 import { js_call_expression_to_name } from '../../../../js/call/expression/to/name.mjs';
 import { js_node_property_expression } from '../../../../js/node/property/expression.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
@@ -8,6 +9,9 @@ import { list_first } from '../../../../list/first.mjs';
 import { js_function_delcaration_to_statements } from '../../../../js/function/delcaration/to/statements.mjs';
 import { function_parse } from '../../../parse.mjs';
 import { function_name_all } from '../../../name/all.mjs';
+import { function_name_get } from '../../../name/get.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { comment } from '../../../../comment.mjs';
 export async function function_arguments_assert_tautology_next() {
     const all = await function_name_all();
     for (let function_name of all) {
@@ -18,5 +22,7 @@ export async function function_arguments_assert_tautology_next() {
         assert(js_node_is_expression_statement(statement_first));
         let expression = object_property_get(statement_first, js_node_property_expression());
         let name_actual = js_call_expression_to_name(expression);
+        comment(`If this fails then need to add arguments assert to function`);
+        assert(equal(name_actual, function_name_get(arguments_assert)));
     }
 }
