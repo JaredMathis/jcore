1b142f9a20079b8c68c9528bba302b11736a8901
JaredMathis <jared.mathis@native.consulting>
2023-06-01 11:26:46 -0400

    #55 function_unasyncify visit_async

--- src/visit.mjs
+++ src/visit.mjs
@@ -1,5 +1,5 @@
-import { metadata } from './metadata.mjs';
 import { metadata_generated } from './metadata/generated.mjs';
+import { metadata } from './metadata.mjs';
 import { visit_recursive } from './visit/recursive.mjs';
 export function visit(root, children_get, lambda_local) {
     visit_recursive(root, children_get, lambda_local, []);
