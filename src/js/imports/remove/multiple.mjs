7e41e0f7595d259464ed0cd652d868f67d8651f4
JaredMathis <jared.mathis@native.consulting>
2023-06-01 09:25:21 -0400

    #56 git_pacp

--- src/js/imports/remove/multiple.mjs
+++ src/js/imports/remove/multiple.mjs
@@ -4,10 +4,10 @@ import { object_property_get } from '../../../object/property/get.mjs';
 import { list_find } from '../../../list/find.mjs';
 import { list_map } from '../../../list/map.mjs';
 export function js_imports_remove_multiple(parsed, imports, removal_names) {
-    let imports_from_intersection = list_map(removal_names, removal_name => {
+    let imports_to_remove = list_map(removal_names, removal_name => {
         return list_find(imports, i => object_property_get(i, 'name') === removal_name)
     });
-    let import_statements_to_remove = list_map(imports_from_intersection, i => object_property_get(i, 'import'));
+    let import_statements_to_remove = list_map(imports_to_remove, i => object_property_get(i, 'import'));
     let body = js_body_get(parsed);
     list_remove_all(body, import_statements_to_remove);
 }
