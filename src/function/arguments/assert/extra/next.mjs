85684cc569ac982fd40dfce089daf9eb6cf0358d
JaredMathis <jared.mathis@native.consulting>
2023-06-01 19:28:10 -0400

    #68 function_rename node_is_type_call_expression node_is_call_expression

--- src/function/arguments/assert/extra/next.mjs
+++ src/function/arguments/assert/extra/next.mjs
@@ -1,10 +1,11 @@
 import { function_names_each } from '../../../names/each.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { js_visit_nodes } from '../../../../js/visit/nodes.mjs';
+import { node_is_call_expression } from '../../../../node/is/call/expression.mjs';
 export async function function_arguments_assert_extra_next() {
     arguments_assert(arguments, []);
     await function_names_each(logic);
     function logic(parsed) {
-        js_visit_nodes(parsed);
+        js_visit_nodes(parsed, node => node_is_call_expression(node));
     }
 }
--- src/js/call/expression/to/name.mjs
+++ src/js/call/expression/to/name.mjs
@@ -2,10 +2,10 @@ import { tautology } from '../../../../tautology.mjs';
 import { js_call_expression_to_name_generic } from './name/generic.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { node_is_type_call_expression } from '../../../../node/is/type/call/expression.mjs';
+import { node_is_call_expression } from '../../../../node/is/call/expression.mjs';
 export function js_call_expression_to_name(ce) {
     arguments_assert(arguments, [tautology]);
-    arguments_assert(arguments, [node_is_type_call_expression]);
+    arguments_assert(arguments, [node_is_call_expression]);
     let or_null = false;
     let name = js_call_expression_to_name_generic(ce, or_null);
     return name;
