e597d99d696e529666c97b902e30680fb272584e
JaredMathis <jared.mathis@native.consulting>
2023-06-01 17:18:16 -0400

    #69 function_auto string_identifier_is

--- src/string/identifier/is.mjs
+++ src/string/identifier/is.mjs
@@ -3,11 +3,13 @@ import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { string_is } from '../is.mjs';
+import { string_split } from '../split.mjs';
 export function string_identifier_is(value) {
     arguments_assert(arguments, [tautology]);
     if (!string_is(value)) {
         return false;
     }
+    let list = string_split(value, '');
     if (string_length_is_0(value)) {
         return true;
     }
