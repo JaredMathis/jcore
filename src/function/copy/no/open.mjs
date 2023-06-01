44b3a0fdb076db9348573dd08fb1290bdfc1872d
JaredMathis <jared.mathis@native.consulting>
2023-06-01 10:14:02 -0400

    #55 function_rename function_copy function_copy_no_open

---
+++ src/function/copy/no/open.mjs
@@ -0,0 +1,14 @@
+import { metadata } from '../../../metadata.mjs';
+import { function_auto_after } from '../../auto/after.mjs';
+import { js_id_name_set } from '../../../js/id/name/set.mjs';
+import { function_parse } from '../../parse.mjs';
+import { js_export_function_single } from '../../../js/export/function/single.mjs';
+import { function_add_with_declaration } from '../../add/with/declaration.mjs';
+export async function function_copy_no_open(function_name_to_copy, function_name_of_copy) {
+    let parsed = await function_parse(function_name_to_copy);
+    let fd = js_export_function_single(parsed);
+    js_id_name_set(fd, function_name_of_copy);
+    await function_add_with_declaration(function_name_of_copy, fd);
+    await function_auto_after(function_name_of_copy);
+    metadata([]);
+}
