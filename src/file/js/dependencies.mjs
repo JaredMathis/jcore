163b096e6c6e88e0101feebfe177d4fd4741942f
JaredMathis <jared.mathis@native.consulting>
2023-06-01 12:31:21 -0400

    #55 function_auto visit_ignore_duplicates_async

--- src/file/js/dependencies.mjs
+++ src/file/js/dependencies.mjs
@@ -2,7 +2,7 @@ import { visit_ignore_duplicates } from '../../visit/ignore/duplicates.mjs';
 import { metadata } from '../../metadata.mjs';
 import { file_js_dependencies_non_recursive } from './dependencies/non/recursive.mjs';
 export async function file_js_dependencies(file_path) {
-    visit_ignore_duplicates(file_path, async node => {
+    await visit_ignore_duplicates_async(file_path, async node => {
         return await file_js_dependencies_non_recursive(node);
     }, v => {
         let {node} = v;
