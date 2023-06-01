f4b58b72beb5383a1abadcd63d358922dcf0cd52
JaredMathis <jared.mathis@native.consulting>
2023-05-31 20:58:50 -0400

    #60 refactor_functions_metadata_add

--- src/js/node/identifer/by/name/get.mjs
+++ src/js/node/identifer/by/name/get.mjs
@@ -1,3 +1,4 @@
+import { metadata } from '../../../../../metadata.mjs';
 import { js_node_is_identifier } from '../../../is/identifier.mjs';
 import { assert } from '../../../../../assert.mjs';
 import { object_property_get } from '../../../../../object/property/get.mjs';
@@ -5,4 +6,5 @@ export function js_node_identifer_by_name_get(fd, node_identifier_name) {
     let id = object_property_get(fd, node_identifier_name);
     assert(js_node_is_identifier(id));
     return id;
+    metadata([]);
 }
--- src/js/node/is/expression/statement.mjs
+++ src/js/node/is/expression/statement.mjs
@@ -1,4 +1,6 @@
+import { metadata } from '../../../../metadata.mjs';
 import { js_node_is_type } from '../type.mjs';
 export function js_node_is_expression_statement(node) {
     return js_node_is_type(node, 'ExpressionStatement');
+    metadata([]);
 }
