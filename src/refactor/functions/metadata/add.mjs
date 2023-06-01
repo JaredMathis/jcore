652be47a758e4f7bda8d442137092be6c3f7cfb5
JaredMathis <jared.mathis@native.consulting>
2023-05-31 21:22:06 -0400

    #60 function_auto refactor_functions_metadata_add

--- src/refactor/functions/metadata/add.mjs
+++ src/refactor/functions/metadata/add.mjs
@@ -1,3 +1,4 @@
+import { file_js_all_map_args_if_function } from '../../../file/js/all/map/args/if/function.mjs';
 import { js_node_is_expression_statement } from '../../../js/node/is/expression/statement.mjs';
 import { assert } from '../../../assert.mjs';
 import { refactor_import_fix } from '../../import/fix.mjs';
@@ -7,8 +8,6 @@ import { metadata } from '../../../metadata.mjs';
 import { function_name_get } from '../../../function/name/get.mjs';
 import { js_node_call_expression_on_name_equal } from '../../../js/node/call/expression/on/name/equal.mjs';
 import { js_function_delcaration_to_statements } from '../../../js/function/delcaration/to/statements.mjs';
-import { js_export_function_single_or_null } from '../../../js/export/function/single/or/null.mjs';
-import { file_js_all_map_args } from '../../../file/js/all/map/args.mjs';
 import { list_length_is_0 } from '../../../list/length/is/0.mjs';
 import { list_last } from '../../../list/last.mjs';
 import { list_add } from '../../../list/add.mjs';
@@ -17,12 +16,7 @@ import { js_node_property_expression } from '../../../js/node/property/expressio
 import { object_property_get } from '../../../object/property/get.mjs';
 import { node_is_type_call_expression } from '../../../node/is/type/call/expression.mjs';
 export async function refactor_functions_metadata_add() {
-    await file_js_all_map_args(async function mapper(args) {
-        let {parsed} = args;
-        let fd = js_export_function_single_or_null(parsed);
-        if (fd === null) {
-            return;
-        }
+    await file_js_all_map_args_if_function(async function logic(fd, args) {
         let already_exists = false;
         let statements = js_function_delcaration_to_statements(fd);
         if (!list_length_is_0(statements)) {
