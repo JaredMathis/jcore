ebd79129e99be7cb2c81f4dbc15de1bb6c4ed29e
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:18:53 -0400

    #60 function_auto js_statement_metadata_args_get

--- src/js/statement/expression/arguments/get.mjs
+++ src/js/statement/expression/arguments/get.mjs
@@ -1,3 +1,4 @@
+import { metadata } from '../../../../metadata.mjs';
 import { js_node_property_arguments } from '../../../node/property/arguments.mjs';
 import { js_node_property_expression } from '../../../node/property/expression.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
@@ -5,4 +6,5 @@ export function js_statement_expression_arguments_get(s) {
     let expression = object_property_get(s, js_node_property_expression());
     let expression_args = object_property_get(expression, js_node_property_arguments());
     return expression_args;
+    metadata([]);
 }
--- src/js/statement/metadata/args/get.mjs
+++ src/js/statement/metadata/args/get.mjs
@@ -1,10 +1,15 @@
+import { js_node_property_elements } from '../../../node/property/elements.mjs';
+import { metadata } from '../../../../metadata.mjs';
 import { list_single } from '../../../../list/single.mjs';
 import { js_statement_expression_arguments_get } from '../../expression/arguments/get.mjs';
 import { js_statement_metadata_is } from '../is.mjs';
 import { assert } from '../../../../assert.mjs';
+import { object_property_get } from '../../../../object/property/get.mjs';
 export function js_statement_metadata_args_get(statement) {
     assert(js_statement_metadata_is(statement));
     let last_args = js_statement_expression_arguments_get(statement);
-    let metadata_args = list_single(last_args);
+    let array = list_single(last_args);
+    let metadata_args = object_property_get(array, js_node_property_elements());
     return metadata_args;
+    metadata([]);
 }
