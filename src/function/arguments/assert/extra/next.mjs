4d27c0fe0759c7b23e3a1a8629ab480d2f3e370c
JaredMathis <jared.mathis@native.consulting>
2023-06-01 18:32:07 -0400

    #68 git_pacp

---
+++ src/function/arguments/assert/extra/next.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../../error.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function function_arguments_assert_extra_next() {
+    arguments_assert(arguments, []);
+    error('todo: function_arguments_assert_extra_next');
+}
