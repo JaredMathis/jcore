2a66aca40735966a48be548b741f8e1d04375315
JaredMathis <jared.mathis@native.consulting>
2023-06-01 15:56:57 -0400

    #67 function_auto function_arguments_assert_tautology_next

--- src/function/arguments/assert/tautology/next.mjs
+++ src/function/arguments/assert/tautology/next.mjs
@@ -1,4 +1,8 @@
+import { function_parse } from '../../../parse.mjs';
 import { function_name_all } from '../../../name/all.mjs';
 export async function function_arguments_assert_tautology_next() {
     const all = await function_name_all();
+    for (let function_name of all) {
+        let parsed = function_parse(function_name);
+    }
 }
