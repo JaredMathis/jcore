4549902cc51f33a36b3dcd26319dfa6692ea9dc2
JaredMathis <jared.mathis@native.consulting>
2023-06-01 09:19:19 -0400

    #56 function_auto refactor_import_unused

---
+++ src/js/imports/remove/multiple.mjs
@@ -0,0 +1,11 @@
+import { list_remove_all } from '../../../list/remove/all.mjs';
+import { js_body_get } from '../../body/get.mjs';
+import { object_property_get } from '../../../object/property/get.mjs';
+import { list_find } from '../../../list/find.mjs';
+import { list_map } from '../../../list/map.mjs';
+export function js_imports_remove_multiple(imports, removal_names, parsed) {
+    let imports_from_intersection = list_map(removal_names, removal_name => list_find(imports, i => object_property_get(i, 'name') === removal_name));
+    let import_statements_to_remove = list_map(imports_from_intersection, i => object_property_get(i, 'import'));
+    let body = js_body_get(parsed);
+    list_remove_all(body, import_statements_to_remove);
+}
--- src/refactor/import/unused.mjs
+++ src/refactor/import/unused.mjs
@@ -1,13 +1,11 @@
+import { js_imports_remove_multiple } from '../../js/imports/remove/multiple.mjs';
 import { metadata } from '../../metadata.mjs';
 import { js_identifier_counts } from '../../js/identifier/counts.mjs';
 import { object_each_filter } from '../../object/each/filter.mjs';
 import { list_intersection } from '../../list/intersection.mjs';
-import { js_body_get } from '../../js/body/get.mjs';
 import { list_map } from '../../list/map.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 import { js_import_all_with_function_names } from '../../js/import/all/with/function/names.mjs';
-import { list_find } from '../../list/find.mjs';
-import { list_remove_all } from '../../list/remove/all.mjs';
 export async function refactor_import_unused(args) {
     let {parsed} = args;
     let imports = await js_import_all_with_function_names(parsed);
@@ -19,11 +17,4 @@ export async function refactor_import_unused(args) {
     let intersection = list_intersection(import_name_all, twices);
     js_imports_remove_multiple(imports, intersection, parsed);
     metadata([]);
-}
-
-function js_imports_remove_multiple(imports, removal_names, parsed) {
-    let imports_from_intersection = list_map(removal_names, removal_name => list_find(imports, i => object_property_get(i, 'name') === removal_name));
-    let import_statements_to_remove = list_map(imports_from_intersection, i => object_property_get(i, 'import'));
-    let body = js_body_get(parsed);
-    list_remove_all(body, import_statements_to_remove);
 }
