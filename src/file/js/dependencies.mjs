5e3eb747e8735a76dd18991aa6baafe8c7da709d
JaredMathis <jared.mathis@native.consulting>
2023-06-01 11:35:41 -0400

    #55 git_pacp

--- src/file/js/dependencies.mjs
+++ src/file/js/dependencies.mjs
@@ -2,8 +2,12 @@ import { visit_async } from '../../visit/async.mjs';
 import { metadata } from '../../metadata.mjs';
 import { file_js_dependencies_non_recursive } from './dependencies/non/recursive.mjs';
 export async function file_js_dependencies(file_path) {
-    visit_async(file_path, node => {
+    visit_async(file_path, async node => {
+        return await file_js_dependencies_non_recursive(node);
+    }, v => {
+        let {node} = v;
+        
     });
-    return await file_js_dependencies_non_recursive(file_path);
+    
     metadata([]);
 }
--- src/visit/recursive/async.mjs
+++ src/visit/recursive/async.mjs
@@ -2,8 +2,14 @@ import { metadata } from '../../metadata.mjs';
 import { list_last_remove_verify } from '../../list/last/remove/verify.mjs';
 import { list_add_exists_not } from '../../list/add/exists/not.mjs';
 import { list_last_or_null } from '../../list/last/or/null.mjs';
+import { list_contains } from '../../list/contains.mjs';
 export async function visit_recursive_async(node, children_get, lambda, stack) {
     let parent = list_last_or_null(stack);
+    if (false) {
+        // if (list_contains(stack, node)) {
+        //     return;
+        // }
+    }
     list_add_exists_not(stack, node);
     await lambda({
         node,
