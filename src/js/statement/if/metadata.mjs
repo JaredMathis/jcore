ad3823dbb145a5b246c54941ea3b6e85ff06222e
JaredMathis <jared.mathis@native.consulting>
2023-06-01 16:21:34 -0400

    #70 function_auto js_statement_if_metadata

--- src/js/statement/if/metadata.mjs
+++ src/js/statement/if/metadata.mjs
@@ -12,13 +12,15 @@ export function js_statement_if_metadata(statement, if_statement_metadata) {
         tautology,
         tautology
     ]);
+    let name_expected = function_name_get(metadata);
     if (js_node_is_expression_statement(statement)) {
         let expression = object_property_get(statement, js_node_property_expression());
         if (node_is_type_call_expression(expression)) {
-            js_node_call_expression_if_name_equal(expression, function_name_get(metadata), function on_name_equal() {
-                if_statement_metadata(statement, expression);
-            });
+            js_node_call_expression_if_name_equal(expression, name_expected, on_name_equal);
         }
+    }
+    function on_name_equal() {
+        if_statement_metadata(statement, expression);
     }
     metadata([]);
 }
