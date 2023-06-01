851a99ad3a9346044adf7a2b67d92e4c934dac64
JaredMathis <jared.mathis@native.consulting>
2023-06-01 19:38:29 -0400

    #68 function_auto function_open_vs_code

--- src/function/open/vs/code.mjs
+++ src/function/open/vs/code.mjs
@@ -1,3 +1,4 @@
+import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
@@ -5,6 +6,8 @@ import { file_open_vs_code } from '../../../file/open/vs/code.mjs';
 import { function_name_to_path } from '../../name/to/path.mjs';
 export async function function_open_vs_code(function_name) {
     arguments_assert(arguments, [tautology]);
+    if (false)
+        arguments_assert(arguments, [string_identifier_is]);
     let function_path = function_name_to_path(function_name);
     await file_open_vs_code(function_path);
     metadata([]);
