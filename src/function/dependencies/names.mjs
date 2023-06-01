ba9a282fab740ac292258638f2a08222b1cf223b
JaredMathis <jared.mathis@native.consulting>
2023-06-01 13:02:57 -0400

    #52 git_pacp

--- src/function/dependencies/names.mjs
+++ src/function/dependencies/names.mjs
@@ -3,6 +3,6 @@ import { object_property_get } from '../../object/property/get.mjs';
 import { function_dependencies } from '../dependencies.mjs';
 export async function function_dependencies_names(arguments_assert_name) {
     let dependencies = await function_dependencies(arguments_assert_name);
-    let dependencies_names = list_map(dependencies, d => object_property_get('function_name'));
+    let dependencies_names = list_map(dependencies, d => object_property_get(d, 'function_name'));
     return dependencies_names;
 }
