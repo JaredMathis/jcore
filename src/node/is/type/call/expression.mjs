182c1ada51145953d8c188814056a8cb2e6bd107
JaredMathis <jared.mathis@native.consulting>
2023-05-31 23:34:32 -0400

    #60 git_pacp

--- src/node/is/type/call/expression.mjs
+++ src/node/is/type/call/expression.mjs
@@ -1,6 +1,6 @@
 import { metadata } from '../../../../metadata.mjs';
 import { js_node_is_type } from '../../../../js/node/is/type.mjs';
-export function node_is_type_call_expression(statement_first) {
-    return js_node_is_type(statement_first, 'CallExpression');
+export function node_is_type_call_expression(node) {
+    return js_node_is_type(node, 'CallExpression');
     metadata([]);
 }
