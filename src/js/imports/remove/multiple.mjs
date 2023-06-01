598c629683ea1d1860c14c8ffa8e122716c8816d
JaredMathis <jared.mathis@native.consulting>
2023-06-01 09:31:19 -0400

    #56 git_pacp

--- src/js/imports/remove/multiple.mjs
+++ src/js/imports/remove/multiple.mjs
@@ -4,13 +4,17 @@ import { object_property_get } from '../../../object/property/get.mjs';
 import { list_find } from '../../../list/find.mjs';
 import { list_map } from '../../../list/map.mjs';
 export function js_imports_remove_multiple(parsed, imports, removal_names) {
-    let imports_to_remove = imports_to_remove_get(imports);
-    let import_statements_to_remove = list_map(imports_to_remove, i => object_property_get(i, 'import'));
-    let body = js_body_get(parsed);
-    list_remove_all(body, import_statements_to_remove);
+    js_imports_remove_multiple(parsed, imports, imports_to_remove_get);
     function imports_to_remove_get(imports) {
         return list_map(removal_names, removal_name => {
             return list_find(imports, i => object_property_get(i, 'name') === removal_name);
         });
     }
+}
+
+function js_imports_remove_multiple(parsed, imports, imports_to_remove_get) {
+    let imports_to_remove = imports_to_remove_get(imports);
+    let import_statements_to_remove = list_map(imports_to_remove, i => object_property_get(i, 'import'));
+    let body = js_body_get(parsed);
+    list_remove_all(body, import_statements_to_remove);
 }
