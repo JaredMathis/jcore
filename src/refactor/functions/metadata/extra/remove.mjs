fc606ec81b8e09024942681b2a77eecdd25e35e3
JaredMathis <jared.mathis@native.consulting>
2023-06-01 07:39:54 -0400

    #56 function_auto refactor_functions_metadata_extra_remove

--- src/refactor/functions/metadata/extra/remove.mjs
+++ src/refactor/functions/metadata/extra/remove.mjs
@@ -2,7 +2,6 @@ import { refactor_metadata_missing_add } from '../../../metadata/missing/add.mjs
 import { js_function_declaration_to_metadata_args } from '../../../../js/function/declaration/to/metadata/args.mjs';
 import { js_statement_metadata_args_get } from '../../../../js/statement/metadata/args/get.mjs';
 import { list_add_multiple } from '../../../../list/add/multiple.mjs';
-import { refactor_functions_metadata_missing_add } from '../missing/add.mjs';
 import { subtract_1 } from '../../../../subtract/1.mjs';
 import { list_take } from '../../../../list/take.mjs';
 import { js_statement_metadata_is } from '../../../../js/statement/metadata/is.mjs';
@@ -14,22 +13,19 @@ import { list_length } from '../../../../list/length.mjs';
 import { list_filter } from '../../../../list/filter.mjs';
 import { list_remove } from '../../../../list/remove.mjs';
 export async function refactor_functions_metadata_extra_remove() {
-    await refactor_functions_metadata_missing_add();
     await file_js_all_map_args_if_function(async function logic(args) {
         let {function_declaration} = args;
         let statements = js_function_delcaration_to_statements(function_declaration);
         if (list_length_is_0(statements)) {
             await refactor_metadata_missing_add(args);
         }
-        if (!list_length_is_0(statements)) {
-            let last_metadata_args = js_function_declaration_to_metadata_args(statements);
-            let remaining = list_take(statements, subtract_1(list_length(statements)));
-            let removals = list_filter(remaining, js_statement_metadata_is);
-            for (let r of removals) {
-                let expression_args = js_statement_metadata_args_get(r);
-                list_add_multiple(last_metadata_args, expression_args);
-                list_remove(statements, r);
-            }
+        let last_metadata_args = js_function_declaration_to_metadata_args(statements);
+        let remaining = list_take(statements, subtract_1(list_length(statements)));
+        let removals = list_filter(remaining, js_statement_metadata_is);
+        for (let r of removals) {
+            let expression_args = js_statement_metadata_args_get(r);
+            list_add_multiple(last_metadata_args, expression_args);
+            list_remove(statements, r);
         }
     });
     metadata([]);
