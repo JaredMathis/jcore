ce1144a093ffa4dd1e3b3951155df937c436f222
JaredMathis <jared.mathis@native.consulting>
2023-06-01 16:40:31 -0400

    #70 function_delete ttt

--- src/ttt.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from './error.mjs';
-import { arguments_assert } from './arguments/assert.mjs';
-export function ttt() {
-    arguments_assert(arguments, []);
-    error('todo: ttt');
-}
