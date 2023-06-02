af49587aa07bacf5a513ea9d9504f0b7a0789471
JaredMathis <jared.mathis@native.consulting>
2023-06-02 19:30:48 -0400

    #72 function_auto function_dependencies

--- src/function/dependencies.mjs
+++ src/function/dependencies.mjs
@@ -5,10 +5,14 @@ import { file_js_path_to_name } from '../file/js/path/to/name.mjs';
 import { file_js_dependencies } from '../file/js/dependencies.mjs';
 import { list_map } from '../list/map.mjs';
 import { function_name_to_file_path } from './name/to/file/path.mjs';
+import { file_js_dependencies_non_recursive } from '../file/js/dependencies/non/recursive.mjs';
 export async function function_dependencies(function_name) {
     arguments_assert(arguments, [tautology]);
     let file_path = function_name_to_file_path(function_name);
     let lambda = file_js_dependencies;
+    if (false) {
+        lambda = file_js_dependencies_non_recursive;
+    }
     let d = await file_js_dependencies(file_path);
     return list_map(d, file_path => {
         return {
