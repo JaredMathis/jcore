1e4a1ec26f47d9fe03e5221f30d04721746f4550
JaredMathis <jared.mathis@native.consulting>
2023-06-01 22:11:39 -0400

    #68 noop

--- src/function/arguments/assert/extra/next/add/metadata/none.mjs
+++ src/function/arguments/assert/extra/next/add/metadata/none.mjs
@@ -1,7 +1,8 @@
 import { function_arguments_assert_extra_next } from '../../../next.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
+import { function_metadata_add } from '../../../../../../metadata/add.mjs';
 export async function function_arguments_assert_extra_next_add_metadata_none() {
     arguments_assert(arguments, []);
     let next_function_name = await function_arguments_assert_extra_next();
-    console.log(next_function_name);
+    await function_metadata_add(next_function_name, 'arguments_assert_none')
 }
