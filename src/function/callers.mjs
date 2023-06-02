5d5b6d7a6a6d7272da369b85c8d8642065fa7f6b
JaredMathis <jared.mathis@native.consulting>
2023-06-02 08:54:06 -0400

    #73 noop

--- src/function/callers.mjs
+++ src/function/callers.mjs
@@ -1,11 +1,8 @@
-import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { file_js_all } from '../file/js/all.mjs';
 export async function function_callers(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
-
     let all = await file_js_all();
-
-    return all
+    return all;
 }
--- src/js/mapper/with/function/declaration/generic.mjs
+++ src/js/mapper/with/function/declaration/generic.mjs
@@ -10,8 +10,6 @@ export async function js_mapper_with_function_declaration_generic(args, mapper, 
     ]);
     let {parsed, file_path} = args;
     let function_declaration = js_export_function_single_or_null(parsed);
-    if (skip_if_no_function_declaration) {
-    }
     if (function_declaration !== null) {
         object_merge({ function_declaration }, args);
     }
