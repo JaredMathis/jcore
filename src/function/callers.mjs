5b9c664f6c61e463848c5b5093d14e68bc5bae16
JaredMathis <jared.mathis@native.consulting>
2023-06-02 09:05:53 -0400

    #73 function_auto function_callers

--- src/function/callers.mjs
+++ src/function/callers.mjs
@@ -1,8 +1,8 @@
+import { function_name_all } from './name/all.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
-import { file_js_all } from '../file/js/all.mjs';
 export async function function_callers(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    let all = await file_js_all();
+    let all = await function_name_all();
     return all;
 }
