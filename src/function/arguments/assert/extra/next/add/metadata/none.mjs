e03e67862bc0acf81517f37d34fcab1a422ae432
JaredMathis <jared.mathis@native.consulting>
2023-06-01 22:26:29 -0400

    #68 function_auto function_arguments_assert_extra_next_add_metadata_none

--- src/function/arguments/assert/extra/next/add/metadata/none.mjs
+++ src/function/arguments/assert/extra/next/add/metadata/none.mjs
@@ -1,10 +1,14 @@
+import { string_prefix_without } from '../../../../../../../string/prefix/without.mjs';
 import { metadata_arguments_assert_none } from '../../../../../../../metadata/arguments/assert/none.mjs';
 import { function_arguments_assert_extra_next } from '../../../next.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 import { function_metadata_add } from '../../../../../../metadata/add.mjs';
+import { metadata_prefix } from '../../../../../../../metadata/prefix.mjs';
+import { function_name_get } from '../../../../../../name/get.mjs';
 export async function function_arguments_assert_extra_next_add_metadata_none() {
     arguments_assert(arguments, []);
     let next_function_name = await function_arguments_assert_extra_next();
-    let metadata_name = metadata_arguments_assert_none;
-    await function_metadata_add(next_function_name, 'arguments_assert_none');
+    let metadata_name = function_name_get(metadata_arguments_assert_none);
+    let without_prefix = string_prefix_without(metadata_name, metadata_prefix());
+    await function_metadata_add(next_function_name, without_prefix);
 }
