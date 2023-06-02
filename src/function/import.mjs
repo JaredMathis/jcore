b219f8886b157f4ac7539c3fffcb3b97ceb7e2ea
JaredMathis <jared.mathis@native.consulting>
2023-06-01 20:34:04 -0400

    #71 function_auto function_run

---
+++ src/function/import.mjs
@@ -0,0 +1,12 @@
+import { js_import_path_normalize } from '../js/import/path/normalize.mjs';
+import { path_relative_file } from '../path/relative/file.mjs';
+import { function_name_to_file_path } from './name/to/file/path.mjs';
+import { js_import_meta_to_file_path } from '../js/import/meta/to/file/path.mjs';
+export async function function_import(import_meta, function_name) {
+    const __filename = js_import_meta_to_file_path(import_meta);
+    let function_path = function_name_to_file_path(function_name);
+    let concated = path_relative_file(__filename, function_path);
+    let replaced = js_import_path_normalize(concated);
+    let imported = await import(replaced);
+    return imported;
+}
--- src/function/run.mjs
+++ src/function/run.mjs
@@ -1,21 +1,14 @@
-import { js_import_meta_to_file_path } from '../js/import/meta/to/file/path.mjs';
+import { function_import } from './import.mjs';
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
-import { function_name_to_file_path } from './name/to/file/path.mjs';
-import { js_import_path_normalize } from '../js/import/path/normalize.mjs';
-import { path_relative_file } from '../path/relative/file.mjs';
 export async function function_run(function_name, args) {
     arguments_assert(arguments, [
         tautology,
         tautology
     ]);
     let import_meta = import.meta;
-    const __filename = js_import_meta_to_file_path(import_meta);
-    let function_path = function_name_to_file_path(function_name);
-    let concated = path_relative_file(__filename, function_path);
-    let replaced = js_import_path_normalize(concated);
-    let imported = await import(replaced);
+    let imported = await function_import(import_meta, function_name);
     let imported_function = imported[function_name];
     let result = await imported_function(...args);
     return result;
