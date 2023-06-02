f01b9d837af4482e64592bd544876fe520b61f76
JaredMathis <jared.mathis@native.consulting>
2023-06-02 12:02:13 -0400

    #72 noop

--- src/list/each/with/index.mjs
+++ src/list/each/with/index.mjs
@@ -1,8 +1,8 @@
 import { list_get } from '../../get.mjs';
 import { range } from '../../../range.mjs';
-export function list_each_with_index(args, lambda) {
-    const r = range(args);
-    console.log(r);
+import { list_length } from '../../length.mjs';
+export function list_each_with_index(list, lambda) {
+    const r = range(list_length(list));
     for (let index of r) {
         let element = list_get(index);
         lambda(element, index);
