33c4af60e314d5ea9f3bc5e2f3c55e5bc5632cc6
JaredMathis <jared.mathis@native.consulting>
2023-06-01 22:58:51 -0400

    #68 noop

--- src/js/call/expression/to/name/or/null.mjs
+++ src/js/call/expression/to/name/or/null.mjs
@@ -4,7 +4,6 @@ import { js_call_expression_to_name_generic } from '../generic.mjs';
 import { js_node_is_call_expression } from '../../../../../node/is/call/expression.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
 export function js_call_expression_to_name_or_null(ce) {
-    arguments_assert(arguments, [tautology]);
     arguments_assert(arguments, [js_node_is_call_expression]);
     let or_null = true;
     let name = js_call_expression_to_name_generic(ce, or_null);
