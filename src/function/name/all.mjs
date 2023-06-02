b358c333afb147f6e25bb2ea30cdbc0a7974bda1
JaredMathis <jared.mathis@native.consulting>
2023-06-02 09:06:50 -0400

    #73 function_auto function_name_all

--- src/function/name/all.mjs
+++ src/function/name/all.mjs
@@ -1,3 +1,5 @@
+import { file_js_path_all_to_name } from '../../file/js/path/all/to/name.mjs';
+import { file_js_all_path } from '../../file/js/all/path.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { file_js_all } from '../../file/js/all.mjs';
@@ -5,5 +7,10 @@ export async function function_name_all() {
     arguments_assert(arguments, []);
     let all = await file_js_all();
     return all;
+    if (false) {
+        let paths = await file_js_all_path();
+        let names = file_js_path_all_to_name(paths);
+        return names;
+    }
     metadata([]);
 }
