b3d7ad532a8bb91de78d6dce97c16f5453c16457
JaredMathis <jared.mathis@native.consulting>
2023-06-02 14:33:39 -0400

    #72 noop

--- src/js/mapper/args/to/statement/arguments/assert/args/predicate.mjs
+++ src/js/mapper/args/to/statement/arguments/assert/args/predicate.mjs
@@ -2,9 +2,9 @@ import { js_node_property_elements } from '../../../../../../../node/property/el
 import { object_property_get } from '../../../../../../../../object/property/get.mjs';
 import { list_get } from '../../../../../../../../list/get.mjs';
 import { js_mapper_args_to_statement_arguments_assert_args } from '../args.mjs';
-export async function js_mapper_args_to_statement_arguments_assert_args_predicate(c_function_declaration) {
-    const c_arguments_assert_args = await js_mapper_args_to_statement_arguments_assert_args(c_function_declaration);
-    let c_array_expression = list_get(c_arguments_assert_args, 1);
-    let c_args = object_property_get(c_array_expression, js_node_property_elements());
-    return c_args;
+export async function js_mapper_args_to_statement_arguments_assert_args_predicate(function_declaration) {
+    const arguments_assert_args = await js_mapper_args_to_statement_arguments_assert_args(function_declaration);
+    let array_expression = list_get(arguments_assert_args, 1);
+    let args = object_property_get(array_expression, js_node_property_elements());
+    return args;
 }
