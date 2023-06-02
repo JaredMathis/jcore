35c99a2d48ac2a909b0c7f7388e1ff1512c25aac
JaredMathis <jared.mathis@native.consulting>
2023-06-02 09:05:38 -0400

    #73 function_auto function_all_tests

--- src/function/all/tests.mjs
+++ src/function/all/tests.mjs
@@ -1,11 +1,11 @@
+import { function_name_all } from '../name/all.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { string_function_tests_name_is } from '../../string/function/tests/name/is.mjs';
 import { list_filter } from '../../list/filter.mjs';
-import { file_js_all } from '../../file/js/all.mjs';
 export async function function_all_tests() {
     arguments_assert(arguments, []);
-    let all = await file_js_all();
+    let all = await function_name_all();
     return list_filter(all, string_function_tests_name_is);
     metadata([]);
 }
