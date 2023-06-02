a23ac08f3f5c6c1e070ca53cc8ffa6ce317e362e
JaredMathis <jared.mathis@native.consulting>
2023-06-01 20:20:57 -0400

    #71 function_auto object_merge

--- src/object/merge.mjs
+++ src/object/merge.mjs
@@ -1,9 +1,9 @@
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
-import { object_property_set } from './property/set.mjs';
 import { object_property_get } from './property/get.mjs';
 import { object_keys } from './keys.mjs';
+import { object_property_initialize } from './property/initialize.mjs';
 export function object_merge(from, to) {
     arguments_assert(arguments, [
         tautology,
@@ -11,7 +11,7 @@ export function object_merge(from, to) {
     ]);
     for (let property of object_keys(from)) {
         let value = object_property_get(from, property);
-        object_property_set(to, property, value);
+        object_property_initialize(to, property, value);
     }
     metadata([]);
 }
