f7b5ad880a048cdf8e661428e6b03d3d1d4e5c79
JaredMathis <jared.mathis@native.consulting>
2023-06-02 12:00:42 -0400

    #72 function_auto number_is

--- src/number/is.mjs
+++ src/number/is.mjs
@@ -1,7 +1,10 @@
+import { assert_arguments_count } from '../assert/arguments/count.mjs';
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 export function number_is(value) {
+    if (false)
+        assert_arguments_count(arguments, 1);
     arguments_assert(arguments, [tautology]);
     return typeof value === 'number';
     metadata([]);
