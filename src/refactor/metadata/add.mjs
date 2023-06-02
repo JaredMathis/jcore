cdd424d80c2999984fe1ec0d3800d6e30032c9d0
JaredMathis <jared.mathis@native.consulting>
2023-06-01 20:13:03 -0400

    #71 function_auto function_metadata_add

--- src/refactor/metadata/add.mjs
+++ src/refactor/metadata/add.mjs
@@ -1,8 +1,9 @@
 import { refactor_metadata_missing_add } from './missing/add.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { tautology } from '../../tautology.mjs';
+import { metadata } from '../../metadata.mjs';
 export async function refactor_metadata_add(args) {
     arguments_assert(arguments, [tautology]);
     await refactor_metadata_missing_add(args);
-    let {parsed, function_declaration} = args;
+    await refactor_metadata_add_function(args, metadata);
 }
