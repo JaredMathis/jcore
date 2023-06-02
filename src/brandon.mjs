d88079c973d238546ef9d36dc25d537099f5c375
JaredMathis <jared.mathis@native.consulting>
2023-06-01 22:13:16 -0400

    #68 function_auto brandon

---
+++ src/brandon.mjs
@@ -0,0 +1,6 @@
+import { error } from './error.mjs';
+import { arguments_assert } from './arguments/assert.mjs';
+export function brandon() {
+    arguments_assert(arguments, []);
+    error('todo: brandon');
+}
