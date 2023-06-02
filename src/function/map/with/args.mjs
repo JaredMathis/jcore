4e92119042cff890d15a554257fee7a2b3b03860
JaredMathis <jared.mathis@native.consulting>
2023-06-01 20:21:44 -0400

    #71 git_pacp

--- src/function/map/with/args.mjs
+++ src/function/map/with/args.mjs
@@ -1,6 +1,7 @@
 import { file_js_map } from '../../../file/js/map.mjs';
+import { file_js_map_with_args } from '../../../file/js/map/with/args.mjs';
 import { function_name_to_file_path } from '../../name/to/file/path.mjs';
 export async function function_map_with_args(function_name, function_name_mapper, args) {
     let file_path = function_name_to_file_path(function_name);
-    await file_js_map(function_name_mapper, file_path);
+    await file_js_map_with_args(function_name_mapper, file_path, args);
 }
