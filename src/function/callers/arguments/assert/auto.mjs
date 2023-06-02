d3cdfa7b62246886e2df8c9c61ec6f24d9503af4
JaredMathis <jared.mathis@native.consulting>
2023-06-02 11:11:02 -0400

    #72 function_callers_arguments_assert_auto add

--- src/function/callers/arguments/assert/auto.mjs
+++ src/function/callers/arguments/assert/auto.mjs
@@ -5,6 +5,8 @@ import { function_callers } from '../../../callers.mjs';
 import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 import { equal } from '../../../../equal.mjs';
 import { js_mapper_args_to_statement_arguments_assert } from '../../../../js/mapper/args/to/statement/arguments/assert.mjs';
+import { object_property_get } from '../../../../object/property/get.mjs';
+import { js_property_name_params } from '../../../../js/property/name/params.mjs';
 export async function function_callers_arguments_assert_auto(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let callers = await function_callers(function_name);
@@ -14,7 +16,8 @@ export async function function_callers_arguments_assert_auto(function_name) {
         }
         let function_declaration = await function_parse_to_declaration(c);
         let arguments_assert_statement = await js_mapper_args_to_statement_arguments_assert({ function_declaration });
-        console.log({ function_declaration, arguments_assert_statement });
+        let params = object_property_get(function_declaration, js_property_name_params())
+        console.log({ function_declaration, params });
         return;
     }
     console.log(callers);
