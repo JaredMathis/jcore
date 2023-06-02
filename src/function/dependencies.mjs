0debb8a4c17902f85c02d4e081f8a843c0bd342d
JaredMathis <jared.mathis@native.consulting>
2023-06-02 19:31:58 -0400

    #72 function_auto function_dependencies

--- src/function/dependencies.mjs
+++ src/function/dependencies.mjs
@@ -1,25 +1,10 @@
+import { function_dependencies_recursive } from './dependencies/recursive.mjs';
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
-import { file_js_path_to_name } from '../file/js/path/to/name.mjs';
-import { file_js_dependencies } from '../file/js/dependencies.mjs';
-import { list_map } from '../list/map.mjs';
-import { function_name_to_file_path } from './name/to/file/path.mjs';
-import { file_js_dependencies_non_recursive } from '../file/js/dependencies/non/recursive.mjs';
 export async function function_dependencies(function_name) {
     arguments_assert(arguments, [tautology]);
     let recursive = true;
-    let file_path = function_name_to_file_path(function_name);
-    let lambda = file_js_dependencies;
-    if (!recursive) {
-        lambda = file_js_dependencies_non_recursive;
-    }
-    let d = await file_js_dependencies(file_path);
-    return list_map(d, file_path => {
-        return {
-            file_path,
-            function_name: file_js_path_to_name(file_path)
-        };
-    });
+    return await function_dependencies_recursive(function_name, recursive);
     metadata([]);
 }
---
+++ src/function/dependencies/recursive.mjs
@@ -0,0 +1,19 @@
+import { file_js_path_to_name } from '../../file/js/path/to/name.mjs';
+import { list_map } from '../../list/map.mjs';
+import { file_js_dependencies_non_recursive } from '../../file/js/dependencies/non/recursive.mjs';
+import { file_js_dependencies } from '../../file/js/dependencies.mjs';
+import { function_name_to_file_path } from '../name/to/file/path.mjs';
+export async function function_dependencies_recursive(function_name, recursive) {
+    let file_path = function_name_to_file_path(function_name);
+    let lambda = file_js_dependencies;
+    if (!recursive) {
+        lambda = file_js_dependencies_non_recursive;
+    }
+    let d = await file_js_dependencies(file_path);
+    return list_map(d, file_path => {
+        return {
+            file_path,
+            function_name: file_js_path_to_name(file_path)
+        };
+    });
+}
