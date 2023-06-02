908075a7078a2f28f976fb8d812f024abe127543
JaredMathis <jared.mathis@native.consulting>
2023-06-02 11:34:17 -0400

    #72 noop

--- src/function/callers/arguments/assert/auto.mjs
+++ src/function/callers/arguments/assert/auto.mjs
@@ -15,6 +15,7 @@ import { list_length_is_0 } from '../../../../list/length/is/0.mjs';
 import { assert } from '../../../../assert.mjs';
 import { js_node_is_identifier } from '../../../../js/node/is/identifier.mjs';
 import { comment } from '../../../../comment.mjs';
+import { range } from '../../../../range.mjs';
 export async function function_callers_arguments_assert_auto(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let callers = await function_callers(function_name);
@@ -29,12 +30,18 @@ export async function function_callers_arguments_assert_auto(function_name) {
         }
         let arguments_assert_statement = await js_mapper_args_to_statement_arguments_assert({ function_declaration });
         let arguments_assert_call_expression = js_statement_expression_to_expression(arguments_assert_statement);
-        for (let arg of object_property_get(arguments_assert_call_expression, js_node_property_arguments())) {
+
+        const args = object_property_get(arguments_assert_call_expression, js_node_property_arguments());
+        for (let index of range(args)) {
+
+        }
+
+        for (let arg of args) {
             comment(`If this isn't true then this code needs changing`);
             assert(js_node_is_identifier(arg));
             let predicate_name = object_property_get(arg, js_node_property_name());
             if (equal(predicate_name, arguments_assert_predicate_default())) {
-                comment('make sure predicate_name is not assigned in this function')
+                
             }
         }
         let arguments_assert_statement_predicates = [];
