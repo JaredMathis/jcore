14f4cc6fbeeace392521e1197798f2823962d135
JaredMathis <jared.mathis@native.consulting>
2023-06-02 08:46:26 -0400

    #73 noop

--- src/function/map.mjs
+++ src/function/map.mjs
@@ -2,11 +2,11 @@ import { function_map_with_args } from './map/with/args.mjs';
 import { metadata } from '../metadata.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export async function function_map(function_name, function_name_mapper) {
+export async function function_map(function_name_mapper, function_name) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is
     ]);
-    await function_map_with_args(function_name, function_name_mapper, {});
+    await function_map_with_args(function_name_mapper, function_name, {});
     metadata([]);
 }
