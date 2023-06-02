5022c4eb1d0f0438ebfc503fe70b975b658ac82b
JaredMathis <jared.mathis@native.consulting>
2023-06-02 10:14:11 -0400

    #72 function_dependencies add

--- src/function/callers.mjs
+++ src/function/callers.mjs
@@ -5,6 +5,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { object_property_get } from '../object/property/get.mjs';
 import { list_add } from '../list/add.mjs';
+import { equal } from '../equal.mjs';
 export async function function_callers(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let result = [];
@@ -12,7 +13,9 @@ export async function function_callers(function_name) {
     for (let a of all) {
         let dependencies = await function_dependencies(a);
         if (list_any(dependencies, d => object_property_get(d, 'function_name') === function_name)) {
-            list_add(result, a);
+            if (!equal(function_name, a)) {
+                list_add(result, a);
+            }
         }
     }
     return result;
