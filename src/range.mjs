fa7a5bd0e91877b40602e8ec864b251e0477af55
JaredMathis <jared.mathis@native.consulting>
2023-06-02 11:59:29 -0400

    #72 function_auto range

--- src/range.mjs
+++ src/range.mjs
@@ -1,6 +1,13 @@
+import { number_is } from './number/is.mjs';
+import { assert_arguments_count } from './assert/arguments/count.mjs';
 import { metadata } from './metadata.mjs';
 import { list_add } from './list/add.mjs';
+import { assert } from './assert.mjs';
 export function range(count) {
+    if (false) {
+        assert_arguments_count(arguments, 1);
+        assert(number_is(count));
+    }
     let result = [];
     for (let i = 0; i < count; i++) {
         list_add(result, i);
