ac475f18bd95443f53f9780b3394587fb66b1c7b
JaredMathis <jared.mathis@native.consulting>
2023-06-02 09:52:59 -0400

    #72 function_auto function_callers_arguments_assert_auto

--- src/function/callers/arguments/assert/auto.mjs
+++ src/function/callers/arguments/assert/auto.mjs
@@ -1,3 +1,4 @@
+import { function_parse_to_declaration } from '../../../parse/to/declaration.mjs';
 import { log } from '../../../../log.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { function_callers } from '../../../callers.mjs';
@@ -6,7 +7,7 @@ export async function function_callers_arguments_assert_auto(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let callers = await function_callers(function_name);
     for (let c of callers) {
-        
+        let function_declaration = await function_parse_to_declaration(function_name);
     }
     console.log(callers);
 }
