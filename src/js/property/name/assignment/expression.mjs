30a1f89037e52e5c171648b64e933e58f900b2a8
JaredMathis <jared.mathis@native.consulting>
2023-06-02 13:10:11 -0400

    #72 function_delete js_property_name_assignment_expression

--- src/js/property/name/assignment/expression.mjs
+++
@@ -1,5 +0,0 @@
-import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function js_property_name_assignment_expression() {
-    arguments_assert(arguments, []);
-    return 'AssignmentExpression';
-}
