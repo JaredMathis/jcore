074971b938584d4076c0670efd24d277c8ac2d0b
JaredMathis <jared.mathis@native.consulting>
2023-06-01 20:11:33 -0400

    #71 function_auto refactor_unasyncify

---
+++ src/refactor/metadata/add/function.mjs
@@ -0,0 +1,14 @@
+import { list_add } from '../../../list/add.mjs';
+import { list_length_is_0 } from '../../../list/length/is/0.mjs';
+import { assert } from '../../../assert.mjs';
+import { comment } from '../../../comment.mjs';
+import { function_name_get } from '../../../function/name/get.mjs';
+import { js_parse_call_expression } from '../../../js/parse/call/expression.mjs';
+import { js_mapper_args_to_metadata_args } from '../../../js/mapper/args/to/metadata/args.mjs';
+export async function refactor_metadata_add_function(args, metadata_function) {
+    let metadata_args = await js_mapper_args_to_metadata_args(args);
+    let metadata_function_parsed = js_parse_call_expression(function_name_get(metadata_function));
+    comment(`If this fails, the code needs enhancing to handle more complex scenarios`);
+    assert(list_length_is_0(metadata_args));
+    list_add(metadata_args, metadata_function_parsed);
+}
