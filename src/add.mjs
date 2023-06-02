ee6d78ba4b14079ea0a5f9e8c04da62b4213d49a
JaredMathis <jared.mathis@native.consulting>
2023-06-02 08:43:44 -0400

    #73 noop

--- src/add.mjs
+++ src/add.mjs
@@ -1,10 +1,10 @@
-import { tautology } from './tautology.mjs';
+import { number_is } from './number/is.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 import { metadata } from './metadata.mjs';
 export function add(a, b) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        number_is,
+        number_is
     ]);
     return a + b;
     metadata([]);
---
+++ src/function/callers.mjs
@@ -0,0 +1,7 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+import { string_identifier_is } from '../string/identifier/is.mjs';
+export function function_callers(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
+    error('todo: function_callers');
+}
