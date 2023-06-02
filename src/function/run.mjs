a95e6674fea14f3a5acded08f5c199093f1da689
JaredMathis <jared.mathis@native.consulting>
2023-06-01 20:32:10 -0400

    #71 function_auto function_run

--- src/function/run.mjs
+++ src/function/run.mjs
@@ -2,9 +2,7 @@ import { js_import_meta_to_file_path } from '../js/import/meta/to/file/path.mjs'
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
-import { directory_separator } from '../directory/separator.mjs';
 import { function_name_to_file_path } from './name/to/file/path.mjs';
-import { directory_current } from '../directory/current.mjs';
 import { js_import_path_normalize } from '../js/import/path/normalize.mjs';
 import { path_relative_file } from '../path/relative/file.mjs';
 export async function function_run(function_name, args) {
@@ -14,9 +12,8 @@ export async function function_run(function_name, args) {
     ]);
     const __filename = js_import_meta_to_file_path(import.meta);
     let function_path = function_name_to_file_path(function_name);
-    let concated = '../../' + directory_current() + directory_separator() + function_path;
-    let concated2 = path_relative_file(__filename, function_path);
-    let replaced = js_import_path_normalize(concated2);
+    let concated = path_relative_file(__filename, function_path);
+    let replaced = js_import_path_normalize(concated);
     let imported = await import(replaced);
     let imported_function = imported[function_name];
     let result = await imported_function(...args);
