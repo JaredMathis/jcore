2731047d021cf26a621706889c2424b3c6850b53
JaredMathis <jared.mathis@native.consulting>
2023-06-02 09:22:21 -0400

    #73 function_auto data_task_next

--- src/data/task/finish.mjs
+++ src/data/task/finish.mjs
@@ -12,7 +12,9 @@ export async function data_task_finish() {
     arguments_assert(arguments, []);
     todo(`Validate the task id is valid i.e. #123 not asdf`);
     let task_id = await data_task_id_get();
-    await file_js_all_map(function_name_get(refactor_import_fix));
+    if (false) {
+        await file_js_all_map(function_name_get(refactor_import_fix));
+    }
     let result = await git_pacp_with_message(`closes ${ task_id }`);
     assert(result.success);
     await data_task_id_remove();
