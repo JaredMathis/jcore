dea32cf2e05e7700350e9d6389576a565f1b3398
JaredMathis <jared.mathis@native.consulting>
2023-06-02 14:25:36 -0400

    #72 noop

--- src/list/each/with/index/async.mjs
+++ src/list/each/with/index/async.mjs
@@ -1,7 +1,7 @@
 import { list_get } from '../../../get.mjs';
 import { range } from '../../../../range.mjs';
 import { list_length } from '../../../length.mjs';
-export function list_each_with_index_async(list, lambda) {
+export async function list_each_with_index_async(list, lambda) {
     const r = range(list_length(list));
     for (let index of r) {
         let element = list_get(list, index);
