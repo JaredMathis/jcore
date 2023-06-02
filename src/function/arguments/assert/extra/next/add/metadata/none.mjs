ac862559a31e9ee73213d5f0d387508111ad731e
JaredMathis <jared.mathis@native.consulting>
2023-06-01 22:09:56 -0400

    #68 function_arguments_assert_extra_next_add_metadata_none

--- src/function/arguments/assert/extra/next/add/metadata/none.mjs
+++ src/function/arguments/assert/extra/next/add/metadata/none.mjs
@@ -1,6 +1,6 @@
 import { function_arguments_assert_extra_next } from '../../../next.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
-export function function_arguments_assert_extra_next_add_metadata_none() {
+export async function function_arguments_assert_extra_next_add_metadata_none() {
     arguments_assert(arguments, []);
     let next_function_name = await function_arguments_assert_extra_next();
     console.log(next_function_name);
