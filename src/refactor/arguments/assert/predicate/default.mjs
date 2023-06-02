2e7c4dc0d6e0dbfc80a25aa46e08fd3ecd90deb2
JaredMathis <jared.mathis@native.consulting>
2023-06-02 10:22:47 -0400

    #72 function_auto refactor_arguments_assert_predicate_default

--- src/refactor/arguments/assert/predicate/default.mjs
+++ src/refactor/arguments/assert/predicate/default.mjs
@@ -1,6 +1,6 @@
-import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { tautology } from '../../../../tautology.mjs';
 export function refactor_arguments_assert_predicate_default() {
     arguments_assert(arguments, []);
-    error('todo: refactor_arguments_assert_predicate_default');
+    return tautology;
 }
