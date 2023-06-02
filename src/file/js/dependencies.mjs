e715125ee1210486216ee823f74e30d9999286c3
JaredMathis <jared.mathis@native.consulting>
2023-06-02 19:13:09 -0400

    #72 function_auto file_js_dependencies

--- src/file/js/dependencies.mjs
+++ src/file/js/dependencies.mjs
@@ -8,10 +8,7 @@ export async function file_js_dependencies(file_path) {
     arguments_assert(arguments, [tautology]);
     let result = [];
     await visit_ignore_duplicates_async(file_path, async node => {
-        let lambda = file_js_dependencies_non_recursive;
-        if (false) {
-        }
-        return await lambda(node);
+        return await file_js_dependencies_non_recursive(node);
     }, v => {
         let {node} = v;
         list_add(result, node);
