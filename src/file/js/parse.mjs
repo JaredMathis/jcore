fd9aeeafbe98bc0ee66aa841fce7649250cfb62a
JaredMathis <jared.mathis@native.consulting>
2023-06-02 15:51:35 -0400

    #72 noop

--- src/file/js/parse.mjs
+++ src/file/js/parse.mjs
@@ -3,10 +3,23 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { js_parse } from '../../js/parse.mjs';
 import { file_read } from '../read.mjs';
+import { js_node_is } from '../../js/node/is.mjs';
 export async function file_js_parse(file_path) {
     arguments_assert(arguments, [tautology]);
+    let {parsed} = await file_js_parse_with_file_path(file_path);
+    assert(js_node_is(parsed));
+    return parsed;
+    metadata([]);
+}
+
+
+export async function file_js_parse_with_file_path(file_path) {
+    arguments_assert(arguments, [tautology]);
     let unparsed = await file_read(file_path);
-    let result = js_parse(unparsed);
-    return result;
+    let parsed = js_parse(unparsed);
+    return {
+        unparsed,
+        parsed
+    }
     metadata([]);
 }
