cf239100a43925b27773bd5176f533a1ba1febdc
JaredMathis <jared.mathis@native.consulting>
2023-06-02 08:48:02 -0400

    #73 noop

--- src/function/callers.mjs
+++ src/function/callers.mjs
@@ -2,7 +2,7 @@ import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { file_js_all } from '../file/js/all.mjs';
-export function function_callers(function_name) {
+export async function function_callers(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
 
     let all = await file_js_all();
