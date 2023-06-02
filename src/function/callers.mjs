4e978add1dc113ad1da9ec15ee2912b1ec891813
JaredMathis <jared.mathis@native.consulting>
2023-06-02 19:35:35 -0400

    #72 function_auto function_callers

--- src/function/callers.mjs
+++ src/function/callers.mjs
@@ -1,5 +1,5 @@
+import { function_dependencies_non_recursive } from './dependencies/non/recursive.mjs';
 import { list_any } from '../list/any.mjs';
-import { function_dependencies } from './dependencies.mjs';
 import { function_name_all } from './name/all.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
@@ -10,7 +10,7 @@ export async function function_callers(function_name) {
     let result = [];
     let candidates = await function_name_all();
     for (let c of candidates) {
-        let dependencies = await function_dependencies(c);
+        let dependencies = await function_dependencies_non_recursive(c);
         if (list_any(dependencies, d => object_property_get(d, 'function_name') === function_name)) {
             list_add(result, c);
         }
