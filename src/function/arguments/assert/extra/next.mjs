35106b06facad776c009779d25650ca27b066d9b
JaredMathis <jared.mathis@native.consulting>
2023-06-01 22:03:02 -0400

    #68 git_pacp

--- src/function/arguments/assert/extra/next.mjs
+++ src/function/arguments/assert/extra/next.mjs
@@ -9,6 +9,10 @@ import { js_call_expression_to_name } from '../../../../js/call/expression/to/na
 import { list_add } from '../../../../list/add.mjs';
 import { list_length } from '../../../../list/length.mjs';
 import { js_mapper_args_to_metadata_args } from '../../../../js/mapper/args/to/metadata/args.mjs';
+import { json_to } from '../../../../json/to.mjs';
+import { list_map } from '../../../../list/map.mjs';
+import { js_call_expression_to_name_or_null } from '../../../../js/call/expression/to/name/or/null.mjs';
+import { list_contains } from '../../../../list/contains.mjs';
 export async function function_arguments_assert_extra_next() {
     arguments_assert(arguments, []);
     await function_names_each(logic);
@@ -16,7 +20,10 @@ export async function function_arguments_assert_extra_next() {
         let {parsed, function_name} = args;
 
         let metadata_args = await js_mapper_args_to_metadata_args(args);
-        console.log({metadata_args})
+        let mapped = list_map(metadata_args, js_call_expression_to_name_or_null);
+        if (list_contains(mapped, function_name_get(metadata_arguments_assert_none))) {
+            return;
+        }
 
         let result = [];
         js_visit_nodes(parsed, node => node_is_call_expression(node), v => {
--- src/function/map.mjs
+++ src/function/map.mjs
@@ -2,7 +2,7 @@ import { function_map_with_args } from './map/with/args.mjs';
 import { metadata } from '../metadata.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export async function function_map(function_name_mapper, function_name) {
+export async function function_map(function_name, function_name_mapper) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is
