109f8697874affcf8087a9befd1f3c9c7072fad3
JaredMathis <jared.mathis@native.consulting>
2023-06-01 20:19:43 -0400

    #71 git_pacp

--- src/file/js/map/with/args.mjs
+++ src/file/js/map/with/args.mjs
@@ -2,6 +2,7 @@ import { tautology } from '../../../../tautology.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { function_run } from '../../../../function/run.mjs';
 import { file_js_map_args } from '../args.mjs';
+import { object_merge } from '../../../../object/merge.mjs';
 export async function file_js_map_with_args(file_path, function_name_mapper, args) {
     arguments_assert(arguments, [
         tautology,
@@ -10,6 +11,7 @@ export async function file_js_map_with_args(file_path, function_name_mapper, arg
     ]);
     await file_js_map_args(file_path, mapper);
     async function mapper(mapper_args) {
+        object_merge(args, mapper_args)
         await function_run(function_name_mapper, [mapper_args]);
     }
 }
