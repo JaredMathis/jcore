b25fb9982f568386ea6eb9397ecb77ff30b87b8b
JaredMathis <jared.mathis@native.consulting>
2023-06-02 11:56:49 -0400

    #72 function_callers_arguments_assert_auto add

--- src/list/each/with/index.mjs
+++ src/list/each/with/index.mjs
@@ -1,7 +1,9 @@
 import { list_get } from '../../get.mjs';
 import { range } from '../../../range.mjs';
 export function list_each_with_index(args, lambda) {
-    for (let index of range(args)) {
+    const r = range(args);
+    console.log(r)
+    for (let index of r) {
         let element = list_get(index);
         lambda(element, index);
     }
