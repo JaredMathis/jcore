24e6b5afe502c2d92381d028432f812930ac099d
JaredMathis <jared.mathis@native.consulting>
2023-06-01 22:06:21 -0400

    #68 function_rename node_is_call_expression js_node_is_call_expression

--- src/function/arguments/assert/extra/next.mjs
+++ src/function/arguments/assert/extra/next.mjs
@@ -5,7 +5,7 @@ import { function_name_get } from '../../../name/get.mjs';
 import { function_names_each } from '../../../names/each.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { js_visit_nodes } from '../../../../js/visit/nodes.mjs';
-import { node_is_call_expression } from '../../../../node/is/call/expression.mjs';
+import { js_node_is_call_expression } from '../../../../js/node/is/call/expression.mjs';
 import { js_call_expression_to_name } from '../../../../js/call/expression/to/name.mjs';
 import { list_add } from '../../../../list/add.mjs';
 import { list_length } from '../../../../list/length.mjs';
@@ -20,13 +20,13 @@ export async function function_arguments_assert_extra_next() {
     async function logic(args) {
         let {parsed, function_name} = args;
         let metadata_args = await js_mapper_args_to_metadata_args(args);
-        let filtered = list_filter(metadata_args, js_node_is_call_expression);
+        let filtered = list_filter(metadata_args, js_node_is_call_expression1);
         let mapped = list_map(filtered, js_call_expression_to_name_or_null);
         if (list_contains(mapped, function_name_get(metadata_arguments_assert_none))) {
             return;
         }
         let result = [];
-        js_visit_nodes(parsed, node => node_is_call_expression(node), v => {
+        js_visit_nodes(parsed, node => js_node_is_call_expression(node), v => {
             let {node} = v;
             if (equal(js_call_expression_to_name(node), function_name_get(arguments_assert))) {
                 list_add(result, node);
--- src/node/is/call/expression.mjs
+++
@@ -1,9 +0,0 @@
-import { tautology } from '../../../tautology.mjs';
-import { arguments_assert } from '../../../arguments/assert.mjs';
-import { metadata } from '../../../metadata.mjs';
-import { js_node_is_type } from '../../../js/node/is/type.mjs';
-export function node_is_call_expression(node) {
-    arguments_assert(arguments, [tautology]);
-    return js_node_is_type(node, 'CallExpression');
-    metadata([]);
-}
