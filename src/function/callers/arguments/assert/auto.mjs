7da9e60a9f9a69124a505bc24d328861833f586c
JaredMathis <jared.mathis@native.consulting>
2023-06-02 15:16:20 -0400

    #72 function_callers_arguments_assert_auto add

--- src/function/callers/arguments/assert/auto.mjs
+++ src/function/callers/arguments/assert/auto.mjs
@@ -65,7 +65,8 @@ export async function function_callers_arguments_assert_auto(function_name) {
                             let ce_arg_for_arg = list_get(ce_args, index);
                             let ce_arg_for_arg_name = object_property_get(ce_arg_for_arg, 'name');
                             let params_index = list_index_of(c_params_names, ce_arg_for_arg_name);
-                            console.log({ arguments_assert_args });
+                            let arguments_assert_arg = list_get(arguments_assert_args, params_index)
+                            console.log({ arguments_assert_arg });
                             return;
                             let arguments_assert_arg_identifier = list_get(c_params, params_index);
                             let arguments_assert_arg_identifier_name = object_property_get(arguments_assert_arg_identifier, 'name');
