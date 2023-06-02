5fa5c8f513bf5b717ecd86ab941a9019c23ac605
JaredMathis <jared.mathis@native.consulting>
2023-06-01 23:25:04 -0400

    #67 noop

--- src/js/visit.mjs
+++ src/js/visit.mjs
@@ -4,13 +4,19 @@ import { metadata } from '../metadata.mjs';
 import { object_properties } from '../object/properties.mjs';
 import { visit_filter } from '../visit/filter.mjs';
 import { js_body_get } from './body/get.mjs';
+import { js_node_is_type } from './node/is/type.mjs';
 export function js_visit(parsed, filter, lambda) {
     arguments_assert(arguments, [
         tautology,
         tautology,
         tautology
     ]);
-    let body = js_body_get(parsed);
+    let root;
+    if (js_node_is_type(parsed, 'Program')) {
+        root = js_body_get(parsed);
+    } else {
+        root = parsed;
+    }
     visit_filter(body, node => object_properties(node), filter, lambda);
     metadata([]);
 }
