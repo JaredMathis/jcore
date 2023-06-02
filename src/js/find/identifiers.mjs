c6765be7be95aad73a14dfef0899824dc215241b
JaredMathis <jared.mathis@native.consulting>
2023-06-02 08:06:40 -0400

    #67 function_rename js_identifier_all js_find_identifiers

--- src/js/find/identifiers.mjs
+++ src/js/find/identifiers.mjs
@@ -1,9 +1,9 @@
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
-import { js_identifier_counts } from './counts.mjs';
+import { js_identifier_counts } from '../identifier/counts.mjs';
 import { object_keys } from '../../object/keys.mjs';
-export function js_identifier_all(parsed) {
+export function js_find_identifiers(parsed) {
     arguments_assert(arguments, [tautology]);
     let counts = js_identifier_counts(parsed);
     let keys = object_keys(counts);
