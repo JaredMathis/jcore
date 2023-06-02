605b95b20ade7ed6000a514aed4b3ef6b014e563
JaredMathis <jared.mathis@native.consulting>
2023-06-02 13:08:56 -0400

    #72 function_auto js_property_name_assignment_expression

--- src/js/property/name/assignment/expression.mjs
+++ src/js/property/name/assignment/expression.mjs
@@ -1,3 +1,5 @@
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function js_property_name_assignment_expression() {
+    arguments_assert(arguments, []);
     return 'AssignmentExpression';
 }
