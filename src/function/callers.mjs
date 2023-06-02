b1b500a52b8d094fe86b1d07791b5f4d56bb762c
JaredMathis <jared.mathis@native.consulting>
2023-06-02 09:18:41 -0400

    #73 function_auto function_callers

--- src/function/callers.mjs
+++ src/function/callers.mjs
@@ -1,14 +1,18 @@
+import { list_any } from '../list/any.mjs';
 import { log } from '../log.mjs';
 import { function_dependencies } from './dependencies.mjs';
 import { function_name_all } from './name/all.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
+import { object_property_get } from '../object/property/get.mjs';
 export async function function_callers(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let all = await function_name_all();
     for (let function_name of all) {
         let dependencies = await function_dependencies(function_name);
-        console.log(dependencies);
+        if (list_any(dependencies, d => object_property_get(d, 'function_name:') === function_name)) {
+            console.log(function_name);
+        }
     }
     return all;
 }
