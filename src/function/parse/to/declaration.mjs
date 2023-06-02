42a60e9c0d33f0ae73990bf84b5b71940fc1ebe8
JaredMathis <jared.mathis@native.consulting>
2023-06-02 14:28:06 -0400

    #72 noop

--- src/function/parse/to/declaration.mjs
+++ src/function/parse/to/declaration.mjs
@@ -1,7 +1,7 @@
 import { js_export_function_single } from '../../../js/export/function/single.mjs';
 import { function_parse } from '../../parse.mjs';
-export async function function_parse_to_declaration(function_name_to_copy) {
-    let parsed = await function_parse(function_name_to_copy);
+export async function function_parse_to_declaration(function_name) {
+    let parsed = await function_parse(function_name);
     let fd = js_export_function_single(parsed);
     return fd;
 }
