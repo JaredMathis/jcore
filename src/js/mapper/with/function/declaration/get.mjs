6eaffc98ec17d3a96b5661f70db2d74e1551ea2c
JaredMathis <jared.mathis@native.consulting>
2023-06-02 08:55:21 -0400

    #73 noop

--- src/js/mapper/with/function/declaration/get.mjs
+++ src/js/mapper/with/function/declaration/get.mjs
@@ -4,7 +4,6 @@ import { js_mapper_with_function_declaration_generic } from './generic.mjs';
 export function js_mapper_with_function_declaration_get(mapper) {
     arguments_assert(arguments, [tautology]);
     return async function js_mapper_with_function_declaration(args) {
-        let skip_if_no_function_declaration = true;
         await js_mapper_with_function_declaration_generic(args, mapper);
     };
 }
