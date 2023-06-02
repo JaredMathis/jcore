de6e201d0ccc44299729964715bac7b30a8fb785
JaredMathis <jared.mathis@native.consulting>
2023-06-01 22:09:18 -0400

    #68 git_pacp

--- src/function/arguments/assert/extra/next/add/metadata/none.mjs
+++ src/function/arguments/assert/extra/next/add/metadata/none.mjs
@@ -2,5 +2,6 @@ import { function_arguments_assert_extra_next } from '../../../next.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 export function function_arguments_assert_extra_next_add_metadata_none() {
     arguments_assert(arguments, []);
-    function_arguments_assert_extra_next;
+    let next_function_name = function_arguments_assert_extra_next();
+    console.log(next_function_name);
 }
