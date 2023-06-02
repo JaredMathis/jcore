b21ec2960351fe89d7e8e01d701bcb806e7c9df3
JaredMathis <jared.mathis@native.consulting>
2023-06-01 22:07:43 -0400

    #68 function_auto function_arguments_assert_extra_next_add_metadata_none

--- src/function/arguments/assert/extra/next/add/metadata/none.mjs
+++ src/function/arguments/assert/extra/next/add/metadata/none.mjs
@@ -1,6 +1,6 @@
-import { error } from '../../../../../../../error.mjs';
+import { function_arguments_assert_extra_next } from '../../../next.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 export function function_arguments_assert_extra_next_add_metadata_none() {
     arguments_assert(arguments, []);
-    error('todo: function_arguments_assert_extra_next_add_metadata_none');
+    function_arguments_assert_extra_next;
 }
