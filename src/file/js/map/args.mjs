e17e71bd76e6b000f77d9726c0bfb4b0ee00c12d
JaredMathis <jared.mathis@native.consulting>
2023-06-02 09:02:08 -0400

    #73 function_auto file_js_map_args

--- src/file/js/map/args.mjs
+++ src/file/js/map/args.mjs
@@ -1,16 +1,16 @@
 import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { js_mapper_with_function_declaration_generic } from '../../../js/mapper/with/function/declaration/generic.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { file_js_overwrite } from '../overwrite.mjs';
 import { file_js_path_to_mapper_args } from '../path/to/mapper/args.mjs';
+import { js_mapper_with_function_declaration_get } from '../../../js/mapper/with/function/declaration/get.mjs';
 export async function file_js_map_args(file_path, mapper) {
     arguments_assert(arguments, [
         tautology,
         tautology
     ]);
     let args = await file_js_path_to_mapper_args(file_path);
-    await js_mapper_with_function_declaration_generic(args, mapper);
+    await js_mapper_with_function_declaration_get(mapper)(args);
     await file_js_overwrite(args);
     metadata([]);
 }
