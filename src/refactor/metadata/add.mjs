6726705aacd1323b0e5301778acc33a4981f5344
JaredMathis <jared.mathis@native.consulting>
2023-06-01 20:35:24 -0400

    #71 function_auto refactor_metadata_add

--- src/refactor/metadata/add.mjs
+++ src/refactor/metadata/add.mjs
@@ -1,7 +1,7 @@
+import { refactor_metadata_add_function } from './add/function.mjs';
 import { refactor_metadata_missing_add } from './missing/add.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { tautology } from '../../tautology.mjs';
-import { metadata } from '../../metadata.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 export async function refactor_metadata_add(args) {
     arguments_assert(arguments, [tautology]);
