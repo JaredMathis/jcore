7f6bc74a6ff34da2392d0499edf8f9c8146ebd4a
JaredMathis <jared.mathis@native.consulting>
2023-06-03 18:17:15 -0400

    #66 function_search _without

--- src/json/equals.mjs
+++ src/json/equals.mjs
@@ -1,6 +1,10 @@
 import { equal_by } from '../equal/by.mjs';
 import { json_to } from './to.mjs';
 export function json_equals(a, b) {
+    return json_equals_keys_filtered(a, b);
+}
+
+function json_equals_keys_filtered(a, b, filtered) {
     let map = json_to;
     return equal_by(map, a, b);
 }
