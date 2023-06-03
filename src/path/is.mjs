af72812e2e8223c79bbb90d9ded4a5a4f0b07702
JaredMathis <jared.mathis@native.consulting>
2023-06-03 11:53:44 -0400

    #67 function_auto path_is

--- src/path/is.mjs
+++ src/path/is.mjs
@@ -1,6 +1,7 @@
 import { arguments_assert } from '../arguments/assert.mjs';
+import { defined_is } from '../defined/is.mjs';
 import { string_is } from '../string/is.mjs';
 export function path_is(value) {
-    arguments_assert(arguments, [string_is]);
-    return true;
+    arguments_assert(arguments, [defined_is]);
+    return string_is(value);
 }
