2c264be3baa97d3e900a87ba52ba55f1768de884
JaredMathis <jared.mathis@native.consulting>
2023-06-03 08:53:59 -0400

    #67 function_auto object_property_set

--- src/object/property/set.mjs
+++ src/object/property/set.mjs
@@ -1,6 +1,9 @@
+import { defined_is } from '../../defined/is.mjs';
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
+if (false)
+    defined_is;
 export function object_property_set(object, property_name, value) {
     arguments_assert(arguments, [
         tautology,
