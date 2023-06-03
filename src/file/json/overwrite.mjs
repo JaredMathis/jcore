f936d8a66793e53911c6d3f0b09d00a357289e46
JaredMathis <jared.mathis@native.consulting>
2023-06-03 11:51:57 -0400

    #67 noop

--- src/file/json/overwrite.mjs
+++ src/file/json/overwrite.mjs
@@ -3,9 +3,10 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { json_to } from '../../json/to.mjs';
 import { file_overwrite } from '../overwrite.mjs';
+import { object_is } from '../../object/is.mjs';
 export async function file_json_overwrite(data, file_path) {
     arguments_assert(arguments, [
-        tautology,
+        object_is,
         tautology
     ]);
     let json = json_to(data);
