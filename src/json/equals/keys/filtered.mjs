cea252246f3ecd4be0e3523b9c94c194010179c3
JaredMathis <jared.mathis@native.consulting>
2023-06-03 18:21:47 -0400

    #66 function_auto json_equals_keys_filtered

--- src/json/equals/keys/filtered.mjs
+++ src/json/equals/keys/filtered.mjs
@@ -1,9 +1,20 @@
+import { log } from '../../../log.mjs';
+import { tautology } from '../../../tautology.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { json_to } from '../../to.mjs';
 import { equal_by } from '../../../equal/by.mjs';
 import { object_keys_without } from '../../../object/keys/without.mjs';
 export function json_equals_keys_filtered(a, b, filter) {
+    arguments_assert(arguments, [
+        tautology,
+        tautology,
+        tautology
+    ]);
     let filtered_a = object_keys_without(a, filter);
     let filtered_b = object_keys_without(b, filter);
-    console.log({filtered_a,filtered_b})
+    console.log({
+        filtered_a,
+        filtered_b
+    });
     return equal_by(json_to, filtered_a, filtered_b);
 }
