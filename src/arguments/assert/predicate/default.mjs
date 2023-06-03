33e7e7f25071741292bc91e2e8ef1932c0be2d45
JaredMathis <jared.mathis@native.consulting>
2023-06-03 13:27:56 -0400

    #67 noop

--- src/arguments/assert/predicate/default.mjs
+++ src/arguments/assert/predicate/default.mjs
@@ -1,6 +1,12 @@
 import { arguments_assert_todo } from '../todo.mjs';
 import { arguments_assert } from '../../assert.mjs';
+import { comment } from '../../../comment.mjs';
+import { tautology } from '../../../tautology.mjs';
 export function arguments_assert_predicate_default() {
     arguments_assert(arguments, []);
-    return arguments_assert_todo;
+    comment(`After changing tautologies use this moving forward`)
+    if (false) {
+        return arguments_assert_todo;
+    }
+    return tautology;
 }
