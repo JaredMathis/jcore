e253f1b4acbbb9f812bb808a61d35ade448e0606
JaredMathis <jared.mathis@native.consulting>
2023-06-03 13:16:15 -0400

    #67 function_auto integer_parse_try

--- src/integer/parse/try.mjs
+++ src/integer/parse/try.mjs
@@ -1,8 +1,8 @@
-import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 export function integer_parse_try(input) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [arguments_assert_todo]);
     return parseInt(input, 10);
     metadata([]);
 }
