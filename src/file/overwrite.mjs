0e2a93e826ebc73d5c955da9b00a0340a2b44be0
JaredMathis <jared.mathis@native.consulting>
2023-06-03 11:52:29 -0400

    #67 noop

--- src/file/overwrite.mjs
+++ src/file/overwrite.mjs
@@ -3,10 +3,11 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import fs from 'fs';
 import { path_parent_exists_ensure } from '../path/parent/exists/ensure.mjs';
+import { string_is } from '../string/is.mjs';
 export async function file_overwrite(file_path, contents) {
     arguments_assert(arguments, [
         tautology,
-        tautology
+        string_is
     ]);
     await path_parent_exists_ensure(file_path);
     await fs.promises.writeFile(file_path, contents);
