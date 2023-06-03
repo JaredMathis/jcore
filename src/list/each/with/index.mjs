e9d372de530fc22272f8bd48069eb8b4ce3ca836
JaredMathis <jared.mathis@native.consulting>
2023-06-03 12:54:44 -0400

    #67 function_unasyncify list_each_with_index_async

---
+++ src/list/each/with/index.mjs
@@ -0,0 +1,19 @@
+import { metadata_generated } from '../../../metadata/generated.mjs';
+import { metadata } from '../../../metadata.mjs';
+import { list_get } from '../../get.mjs';
+import { list_length } from '../../length.mjs';
+import { range } from '../../../range.mjs';
+import { tautology } from '../../../tautology.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function list_each_with_index(list, lambda) {
+    arguments_assert(arguments, [
+        tautology,
+        tautology
+    ]);
+    const r = range(list_length(list));
+    for (let index of r) {
+        let element = list_get(list, index);
+        lambda(element, index);
+    }
+    metadata([metadata_generated()]);
+}
