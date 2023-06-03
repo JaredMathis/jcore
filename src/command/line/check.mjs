41763de20ac0ca6b670a101cb3b8ab5745efbaf6
JaredMathis <jared.mathis@native.consulting>
2023-06-03 08:40:01 -0400

    #67 function_auto command_line_check

--- src/command/line/check.mjs
+++ src/command/line/check.mjs
@@ -1,8 +1,11 @@
+import { string_empty_not_js } from '../../string/empty/not/js.mjs';
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { error } from '../../error.mjs';
 import { exec } from 'child_process';
+if (false)
+    string_empty_not_js;
 export async function command_line_check(command) {
     arguments_assert(arguments, [tautology]);
     return await new Promise(function (resolve, reject) {
