fac5923648ebff5e47dab8dc582a9264323213b7
JaredMathis <jared.mathis@native.consulting>
2023-06-03 10:59:34 -0400

    #67 noop

--- src/data/key/value/set.mjs
+++ src/data/key/value/set.mjs
@@ -1,12 +1,12 @@
+import { string_is } from '../../../string/is.mjs';
 import { defined_is } from '../../../defined/is.mjs';
-import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { object_property_set } from '../../../object/property/set.mjs';
 import { data_map } from '../../map.mjs';
 export async function data_key_value_set(key, value) {
     arguments_assert(arguments, [
-        tautology,
+        string_is,
         defined_is
     ]);
     await data_map(map);
--- src/function/callers/arguments/assert/auto/generic.mjs
+++ src/function/callers/arguments/assert/auto/generic.mjs
@@ -77,11 +77,13 @@ export async function function_callers_arguments_assert_auto_generic(c_function_
                 if (c_ce_name !== null) {
                     if (equal(c_ce_name, function_name)) {
                         let ce_args = object_property_get(node, js_node_property_arguments());
+                        console.log({ce_args})
                         for (let ce_arg of ce_args) {
                             if (!js_node_is_identifier(ce_arg)) {
                                 continue;
                             }
                             const ce_arg_name = object_property_get(ce_arg, 'name');
+                            console.log({ce_arg_name, c_params_names})
                             if (!list_contains(c_params_names, ce_arg_name)) {
                                 continue;
                             }
