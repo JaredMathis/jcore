94f65a2b888bb180e476df842c1889d48b2a9a48
JaredMathis <jared.mathis@native.consulting>
2023-06-03 19:05:49 -0400

    #66 function_auto function_callers_arguments_assert_auto_recursive

--- src/function/callers/arguments/assert/auto/recursive.mjs
+++ src/function/callers/arguments/assert/auto/recursive.mjs
@@ -1,6 +1,7 @@
-import { error } from '../../../../../error.mjs';
+import { function_callers_arguments_assert_auto_multiple } from './multiple.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export function function_callers_arguments_assert_auto_recursive() {
     arguments_assert(arguments, []);
-    error('todo: function_callers_arguments_assert_auto_recursive');
+    function_callers_arguments_assert_auto_multiple();
+    'js_mapper_with_function_declaration_get';
 }
