d8f7028392775cf6c7bfccfe1ad58f57ce87f2ea
JaredMathis <jared.mathis@native.consulting>
2023-06-03 18:25:14 -0400

    #66 function_auto equal_not

---
+++ src/equal/not.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function equal_not() {
+    arguments_assert(arguments, []);
+    error('todo: equal_not');
+}
