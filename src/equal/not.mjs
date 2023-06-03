c5bb291662071568cbe601787116e49800b332ad
JaredMathis <jared.mathis@native.consulting>
2023-06-03 18:26:54 -0400

    #66 function_auto equal_not

--- src/equal/not.mjs
+++ src/equal/not.mjs
@@ -1,7 +1,10 @@
-import { error } from '../error.mjs';
+import { equal } from '../equal.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { defined_is } from '../defined/is.mjs';
-export function equal_not(a,b) {
-    arguments_assert(arguments, [defined_is, defined_is]);
-    return !equal(a,b);
+export function equal_not(a, b) {
+    arguments_assert(arguments, [
+        defined_is,
+        defined_is
+    ]);
+    return !equal(a, b);
 }
