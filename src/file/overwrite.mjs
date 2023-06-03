24a6b6463393124af34f3ada0daf6060f34f66ee
JaredMathis <jared.mathis@native.consulting>
2023-06-03 11:54:25 -0400

    #67 function_auto file_overwrite

--- src/file/overwrite.mjs
+++ src/file/overwrite.mjs
@@ -1,15 +1,12 @@
 import { path_is } from '../path/is.mjs';
-import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import fs from 'fs';
 import { path_parent_exists_ensure } from '../path/parent/exists/ensure.mjs';
 import { string_is } from '../string/is.mjs';
-if (false)
-    path_is;
 export async function file_overwrite(file_path, contents) {
     arguments_assert(arguments, [
-        tautology,
+        path_is,
         string_is
     ]);
     await path_parent_exists_ensure(file_path);
