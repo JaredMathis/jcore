ff9a380450fbb4a2653b3a65ef088d2a321c6068
JaredMathis <jared.mathis@native.consulting>
2023-06-02 20:48:29 -0400

    #67 function_auto string_empty_not_js

---
+++ src/string/empty/not/js.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function string_empty_not_js() {
+    arguments_assert(arguments, []);
+    error('todo: string_empty_not_js');
+}
