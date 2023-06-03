c6bd51266d73f3a01195e12c409f6fdc0a6bc45a
JaredMathis <jared.mathis@native.consulting>
2023-06-03 08:53:10 -0400

    #67 function_auto defined_is

---
+++ src/defined/is.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function defined_is() {
+    arguments_assert(arguments, []);
+    error('todo: defined_is');
+}
