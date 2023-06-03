19401b304ac2a98d3ed57123de70e62fe0a32360
JaredMathis <jared.mathis@native.consulting>
2023-06-03 14:12:17 -0400

    #67 noop

--- src/file/js/identifiers.mjs
+++ src/file/js/identifiers.mjs
@@ -1,10 +1,10 @@
-import { tautology } from '../../tautology.mjs';
+import { path_is } from '../../path/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { js_identifiers } from '../../js/identifiers.mjs';
 import { file_js_parse } from './parse.mjs';
 export async function file_js_identifiers(file_path) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [path_is]);
     let parsed = await file_js_parse(file_path);
     return js_identifiers(parsed);
     metadata([]);
