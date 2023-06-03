5e289ead1e284088d363993108297ed86937def6
JaredMathis <jared.mathis@native.consulting>
2023-06-03 13:43:54 -0400

    #67 noop

--- src/arguments/assert/todo.mjs
+++ src/arguments/assert/todo.mjs
@@ -2,6 +2,7 @@ import { error } from '../../error.mjs';
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../assert.mjs';
 export function arguments_assert_todo(value) {
+    if (false)
     arguments_assert(arguments, [tautology]);
     return tautology(value);
 }
