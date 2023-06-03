ed033e33f7055ab9a048e8addb05f40bc09eef34
JaredMathis <jared.mathis@native.consulting>
2023-06-03 13:19:36 -0400

    #67 function_auto defined_is

--- src/defined/is.mjs
+++ src/defined/is.mjs
@@ -1,6 +1,9 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { tautology } from '../tautology.mjs';
 export function defined_is(variable) {
     arguments_assert(arguments, [tautology]);
+    if (false)
+        arguments_assert_todo;
     return typeof variable !== 'undefined';
 }
