b2ed2459a218bdafeef962d57c0a871f05d7dfc5
JaredMathis <jared.mathis@native.consulting>
2023-06-03 13:50:57 -0400

    #67 function_copy function_arguments_assert_tautology_next function_arguments_assert_tautology_count

---
+++ src/function/arguments/assert/tautology/count.mjs
@@ -0,0 +1,11 @@
+import { function_open_vs_code } from '../../../open/vs/code.mjs';
+import { function_arguments_assert_tautology_generic } from './generic.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export async function function_arguments_assert_tautology_count() {
+    arguments_assert(arguments, []);
+    await function_arguments_assert_tautology_generic(on_equals);
+    async function on_equals(function_name) {
+        await function_open_vs_code(function_name);
+        return true;
+    }
+}
