55c74e4b837b69555e95cac129c94dc0edafe85f
JaredMathis <jared.mathis@native.consulting>
2023-06-03 13:05:16 -0400

    #67 noop

--- src/function/callers/arguments/assert/auto/generic.mjs
+++ src/function/callers/arguments/assert/auto/generic.mjs
@@ -88,11 +88,6 @@ export async function function_callers_arguments_assert_auto_generic(c_function_
                             if (ce_arg !== null) {
                                 let arguments_assert_arg = list_get(arguments_assert_args, ce_arg_index);
                                 list_set(c_arguments_assert_args, c_arg_index, arguments_assert_arg);
-                                console.log({
-                                    c_arguments_assert_args,
-                                    c_arg_index,
-                                    arguments_assert_arg
-                                });
                                 changed = true;
                             }
                         });
