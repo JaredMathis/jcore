ac0693f0bf34f2426548f74d5f830bd8deb1340d
JaredMathis <jared.mathis@native.consulting>
2023-06-03 09:10:00 -0400

    #67 function_auto list_set

--- src/list/set.mjs
+++ src/list/set.mjs
@@ -1,14 +1,15 @@
 import { assert } from '../assert.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { tautology } from '../tautology.mjs';
-import { object_property_exists } from '../object/property/exists.mjs';
 import { number_is } from '../number/is.mjs';
+import { list_length } from './length.mjs';
 export function list_set(list, index, value) {
     arguments_assert(arguments, [
         tautology,
         number_is,
         tautology
     ]);
-    assert(object_property_exists(list, index));
+    assert(index >= 0);
+    assert(index <= list_length(list, index));
     list[index] = value;
 }
