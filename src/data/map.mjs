a3cd38b0db99ca01e73f7161a68d3bcf3918088e
JaredMathis <jared.mathis@native.consulting>
2023-06-03 11:48:36 -0400

    #67 function_auto data_map

--- src/data/map.mjs
+++ src/data/map.mjs
@@ -1,10 +1,10 @@
-import { tautology } from '../tautology.mjs';
+import { function_is } from '../function/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { data_overwrite } from './overwrite.mjs';
 import { data_get } from './get.mjs';
 export async function data_map(map) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [function_is]);
     let data = await data_get();
     map(data);
     await data_overwrite(data);
