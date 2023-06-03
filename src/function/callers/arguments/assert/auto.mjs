43d3774ef871aae5d250fbc4d0d9fe065a60e985
JaredMathis <jared.mathis@native.consulting>
2023-06-03 09:23:24 -0400

    #67 function_rename function_parse_to_declaration function_to_declaration

--- src/function/callers/arguments/assert/auto.mjs
+++ src/function/callers/arguments/assert/auto.mjs
@@ -1,9 +1,8 @@
 import { list_set } from '../../../../list/set.mjs';
-import { log } from '../../../../log.mjs';
 import { refactor_import_fix } from '../../../../refactor/import/fix.mjs';
 import { js_node_property_left } from '../../../../js/node/property/left.mjs';
 import { js_mapper_args_to_statement_arguments_assert_args_predicate } from '../../../../js/mapper/args/to/statement/arguments/assert/args/predicate.mjs';
-import { function_parse_to_declaration } from '../../../parse/to/declaration.mjs';
+import { function_to_declaration } from '../../../to/declaration.mjs';
 import { list_index_of } from '../../../../list/index/of.mjs';
 import { js_node_is_call_expression } from '../../../../js/node/is/call/expression.mjs';
 import { js_node_is_assignment_expression } from '../../../../js/node/is/assignment/expression.mjs';
@@ -33,72 +32,81 @@ import { list_contains } from '../../../../list/contains.mjs';
 export async function function_callers_arguments_assert_auto(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let result = [];
-    let function_declaration = await function_parse_to_declaration(function_name);
-    let arguments_assert_args = await js_mapper_args_to_statement_arguments_assert_args_predicate(function_declaration);
+    let arguments_assert_args = await function_to_statement_arguments_assert_args_predicate(function_name);
     let callers = await function_callers(function_name);
     for (let c_function_name of callers) {
         if (equal(function_name, c_function_name)) {
             continue;
         }
-        let c_file_path = function_name_to_file_path(c_function_name);
-        await file_js_map_args(c_file_path, async c_args => {
-            let c_parsed = object_property_get(c_args, 'parsed');
-            let c_function_declaration = object_property_get(c_args, 'function_declaration');
-            let c_params = object_property_get(c_function_declaration, js_node_property_params());
-            if (list_length_is_0(c_params)) {
-                return true;
+        await function_callers_arguments_assert_auto_generic(c_function_name, function_name, arguments_assert_args, result);
+    }
+    return result;
+}
+async function function_to_statement_arguments_assert_args_predicate(function_name) {
+    let function_declaration = await function_to_declaration(function_name);
+    let arguments_assert_args = await js_mapper_args_to_statement_arguments_assert_args_predicate(function_declaration);
+    return arguments_assert_args;
+}
+async function function_callers_arguments_assert_auto_single(function_name, c_function_name) {
+}
+async function function_callers_arguments_assert_auto_generic(c_function_name, function_name, arguments_assert_args, result) {
+    let c_file_path = function_name_to_file_path(c_function_name);
+    await file_js_map_args(c_file_path, async c_args => {
+        let c_parsed = object_property_get(c_args, 'parsed');
+        let c_function_declaration = object_property_get(c_args, 'function_declaration');
+        let c_params = object_property_get(c_function_declaration, js_node_property_params());
+        if (list_length_is_0(c_params)) {
+            return true;
+        }
+        let c_params_names = list_map(c_params, p => object_property_get(p, 'name'));
+        let c_arguments_assert_args = await js_mapper_args_to_statement_arguments_assert_args_predicate(c_function_declaration);
+        await list_each_with_index_async(c_arguments_assert_args, async function lambda(c_arg, index) {
+            comment(`If this isn't true then this code needs changing`);
+            assert(js_node_is_identifier(c_arg));
+            let predicate_name = object_property_get(c_arg, js_node_property_name());
+            let default_name = function_name_get(arguments_assert_predicate_default());
+            if (!equal(predicate_name, default_name)) {
+                return false;
             }
-            let c_params_names = list_map(c_params, p => object_property_get(p, 'name'));
-            let c_arguments_assert_args = await js_mapper_args_to_statement_arguments_assert_args_predicate(c_function_declaration);
-            await list_each_with_index_async(c_arguments_assert_args, async function lambda(c_arg, index) {
-                comment(`If this isn't true then this code needs changing`);
-                assert(js_node_is_identifier(c_arg));
-                let predicate_name = object_property_get(c_arg, js_node_property_name());
-                let default_name = function_name_get(arguments_assert_predicate_default());
-                if (!equal(predicate_name, default_name)) {
-                    return false;
-                }
-                let changed = false;
-                let assignment_exists = false;
-                js_visit_nodes(c_parsed, js_node_is_assignment_expression, v => {
-                    let {node} = v;
-                    let left = object_property_get(node, js_node_property_left());
-                    if (js_node_is_identifier(left)) {
-                        if (object_property_get(left, 'name') === predicate_name) {
-                            comment(`Value has been changed - will not assume predicate can be copied`);
-                            assignment_exists = true;
-                        }
+            let changed = false;
+            let assignment_exists = false;
+            js_visit_nodes(c_parsed, js_node_is_assignment_expression, v => {
+                let {node} = v;
+                let left = object_property_get(node, js_node_property_left());
+                if (js_node_is_identifier(left)) {
+                    if (object_property_get(left, 'name') === predicate_name) {
+                        comment(`Value has been changed - will not assume predicate can be copied`);
+                        assignment_exists = true;
                     }
-                });
-                if (assignment_exists === true) {
-                    return changed;
                 }
-                js_visit_nodes(c_parsed, js_node_is_call_expression, v => {
-                    let {node} = v;
-                    let ce_name = js_call_expression_to_name_or_null(node);
-                    if (ce_name !== null) {
-                        if (equal(ce_name, function_name)) {
-                            let ce_args = object_property_get(node, js_node_property_arguments());
-                            let ce_arg_for_arg = list_get(ce_args, index);
-                            if (js_node_is_identifier(ce_arg_for_arg)) {
-                                let ce_arg_for_arg_name = object_property_get(ce_arg_for_arg, 'name');
-                                if (list_contains(c_params_names, ce_arg_for_arg_name)) {
-                                    let params_index = list_index_of(c_params_names, ce_arg_for_arg_name);
-                                    let arguments_assert_arg = list_get(arguments_assert_args, params_index);
-                                    list_set(c_arguments_assert_args, index, arguments_assert_arg);
-                                    changed = true;
-                                }
+            });
+            if (assignment_exists === true) {
+                return changed;
+            }
+            js_visit_nodes(c_parsed, js_node_is_call_expression, v => {
+                let {node} = v;
+                let ce_name = js_call_expression_to_name_or_null(node);
+                if (ce_name !== null) {
+                    if (equal(ce_name, function_name)) {
+                        let ce_args = object_property_get(node, js_node_property_arguments());
+                        let ce_arg_for_arg = list_get(ce_args, index);
+                        if (js_node_is_identifier(ce_arg_for_arg)) {
+                            let ce_arg_for_arg_name = object_property_get(ce_arg_for_arg, 'name');
+                            if (list_contains(c_params_names, ce_arg_for_arg_name)) {
+                                let params_index = list_index_of(c_params_names, ce_arg_for_arg_name);
+                                let arguments_assert_arg = list_get(arguments_assert_args, params_index);
+                                list_set(c_arguments_assert_args, index, arguments_assert_arg);
+                                changed = true;
                             }
                         }
                     }
-                });
-                if (changed) {
-                    await refactor_import_fix(c_args);
-                    list_add(result, c_function_name);
                 }
-                return !changed;
             });
+            if (changed) {
+                await refactor_import_fix(c_args);
+                list_add(result, c_function_name);
+            }
+            return !changed;
         });
-    }
-    return result;
+    });
 }
--- src/function/copy/no/open.mjs
+++ src/function/copy/no/open.mjs
@@ -1,4 +1,4 @@
-import { function_parse_to_declaration } from '../../parse/to/declaration.mjs';
+import { function_to_declaration } from '../../to/declaration.mjs';
 import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
@@ -10,7 +10,7 @@ export async function function_copy_no_open(function_name_to_copy, function_name
         tautology,
         tautology
     ]);
-    let fd = await function_parse_to_declaration(function_name_to_copy);
+    let fd = await function_to_declaration(function_name_to_copy);
     js_id_name_set(fd, function_name_of_copy);
     await function_add_with_declaration(function_name_of_copy, fd);
     await function_auto_after(function_name_of_copy);
