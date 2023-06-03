a03f91de21770ea77d887883ad53f5813928463f
JaredMathis <jared.mathis@native.consulting>
2023-06-03 14:02:11 -0400

    #67 function_auto error_is

--- src/error/caught.mjs
+++ src/error/caught.mjs
@@ -4,10 +4,11 @@ import { metadata } from '../metadata.mjs';
 import { function_name_get } from '../function/name/get.mjs';
 import { error } from '../error.mjs';
 import { log } from '../log.mjs';
+import { function_is } from '../function/is.mjs';
 export function error_caught(fn, exception, error_message) {
     arguments_assert(arguments, [
-        tautology,
-        tautology,
+        function_is,
+        error_is,
         tautology
     ]);
     log(exception);
