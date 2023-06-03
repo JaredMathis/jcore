4921ee46c512cfe2ee4e3982bf152ea68ce9ce31
JaredMathis <jared.mathis@native.consulting>
2023-06-03 19:01:50 -0400

    #66 noop

--- src/js/mapper/with/function/declaration/get.mjs
+++ src/js/mapper/with/function/declaration/get.mjs
@@ -2,8 +2,9 @@ import { object_merge } from '../../../../../object/merge.mjs';
 import { js_export_function_single_or_null } from '../../../../export/function/single/or/null.mjs';
 import { tautology } from '../../../../../tautology.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
+import { function_is } from '../../../../../function/is.mjs';
 export function js_mapper_with_function_declaration_get(mapper) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [function_is]);
     return async function js_mapper_with_function_declaration(args) {
         let {parsed, file_path} = args;
         let function_declaration = js_export_function_single_or_null(parsed);
