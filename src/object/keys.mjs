1ca270e021f00bf9981ad69e5de540aa69fbb351
JaredMathis <jared.mathis@native.consulting>
2023-06-03 14:29:28 -0400

    #67 noop

--- src/object/keys.mjs
+++ src/object/keys.mjs
@@ -1,8 +1,9 @@
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
+import { defined_is } from '../defined/is.mjs';
 export function object_keys(object) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [defined_is]);
     return Object.keys(object);
     metadata([]);
 }
