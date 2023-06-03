77c36f3c677ea64cbcf1ddce804a0f01a037f511
JaredMathis <jared.mathis@native.consulting>
2023-06-03 18:31:26 -0400

    #66 noop

--- src/json/equals/keys/without.mjs
+++ src/json/equals/keys/without.mjs
@@ -4,17 +4,25 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { json_to } from '../../to.mjs';
 import { equal_by } from '../../../equal/by.mjs';
 import { object_keys_without } from '../../../object/keys/without.mjs';
-export function json_equals_keys_without(a, b, filter) {
+import { object_merge_filtered } from '../../../object/merge/filtered.mjs';
+import { list_contains } from '../../../list/contains.mjs';
+export function json_equals_keys_without(a, b, values_without) {
     arguments_assert(arguments, [
         tautology,
         tautology,
         tautology
     ]);
-    let filtered_a = object_keys_without(a, filter);
-    let filtered_b = object_keys_without(b, filter);
+    let filtered_a = object_map_without(a, values_without);
+    let filtered_b = object_map_without(b, values_without);
     console.log({
         filtered_a,
         filtered_b
     });
     return equal_by(json_to, filtered_a, filtered_b);
+}
+
+function object_map_without(a, values_without) {
+    let filtered_a = object_keys_without(a, values_without);
+    object_merge_filtered(a, key => !list_contains(values_without, key), {});
+    return filtered_a;
 }
