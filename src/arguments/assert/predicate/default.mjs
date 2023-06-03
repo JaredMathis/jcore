e5c17d967f887c5910690ee4c41214e5b586c8c6
JaredMathis <jared.mathis@native.consulting>
2023-06-03 18:22:40 -0400

    #66 function_rename json_equals_keys_filtered json_equals_keys_excluded

--- src/arguments/assert/predicate/default.mjs
+++ src/arguments/assert/predicate/default.mjs
@@ -4,7 +4,7 @@ import { comment } from '../../../comment.mjs';
 import { tautology } from '../../../tautology.mjs';
 export function arguments_assert_predicate_default() {
     arguments_assert(arguments, []);
-    comment(`After changing tautologies use this moving forward`)
+    comment(`After changing tautologies use this moving forward`);
     if (false) {
         return arguments_assert_todo;
     }
