f0bece82ce3d53b06c1faa39bdbc9b45b0252e75
JaredMathis <jared.mathis@native.consulting>
2023-06-03 13:45:31 -0400

    #67 noop

--- src/arguments/assert/todo.mjs
+++ src/arguments/assert/todo.mjs
@@ -2,7 +2,6 @@ import { error } from '../../error.mjs';
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../assert.mjs';
 export function arguments_assert_todo(value) {
-    if (false)
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [() => true]);
     return tautology(value);
 }
