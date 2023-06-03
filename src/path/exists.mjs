6c85cb280346b3992d22dbc2abdb9bf9d3adc7d8
JaredMathis <jared.mathis@native.consulting>
2023-06-03 14:08:47 -0400

    #67 function_auto path_exists

--- src/path/exists.mjs
+++ src/path/exists.mjs
@@ -1,11 +1,9 @@
-import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import fs from 'fs';
+import { path_is } from './is.mjs';
 export function path_exists(file_path) {
-    arguments_assert(arguments, [tautology]);
-    if (false)
-        file_path_is;
+    arguments_assert(arguments, [path_is]);
     return fs.promises.access(file_path, fs.constants.F_OK).then(() => true).catch(() => false);
     metadata([]);
 }
