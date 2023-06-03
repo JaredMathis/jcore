dc4bfaab0d4cea57865653f726f4c616a9caf3b1
JaredMathis <jared.mathis@native.consulting>
2023-06-02 20:10:29 -0400

    #67 noop

--- src/function/names/each.mjs
+++ src/function/names/each.mjs
@@ -7,9 +7,11 @@ import { comment } from '../../comment.mjs';
 import { function_name_to_file_path } from '../name/to/file/path.mjs';
 export async function function_names_each(logic) {
     arguments_assert(arguments, [tautology]);
+    let filter;
+    let filter_use = false;
     const all = await function_name_all();
-    if (false){
-        
+    if (filter_use){
+
     }
     for (let function_name of all) {
         let parsed = await function_parse(function_name);
