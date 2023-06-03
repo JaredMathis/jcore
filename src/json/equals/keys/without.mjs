662a2d2d6595eef8c85c6b49345f2703129357f6
JaredMathis <jared.mathis@native.consulting>
2023-06-03 19:14:49 -0400

    #66 function_delete json_equals_keys_without

--- src/json/equals/keys/without.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../../../error.mjs';
-import { arguments_assert } from '../../../arguments/assert.mjs';
-export function json_equals_keys_without() {
-    arguments_assert(arguments, []);
-    error('todo: json_equals_keys_without');
-}
