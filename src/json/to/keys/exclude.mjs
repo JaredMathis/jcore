e24ca2df155143e18891141d43cebd4b1ddd068c
JaredMathis <jared.mathis@native.consulting>
2023-06-03 18:15:09 -0400

    #66 function_auto json_to_keys_exclude

--- src/json/to/keys/exclude.mjs
+++ src/json/to/keys/exclude.mjs
@@ -1,6 +1,11 @@
-import { object_merge } from '../../../object/merge.mjs';
-import { object_keys_without } from '../../../object/keys/without.mjs';
+import { tautology } from '../../../tautology.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+import { object_merge_filtered } from '../../../object/merge/filtered.mjs';
+import { list_contains } from '../../../list/contains.mjs';
 export function json_to_keys_exclude(value, excluded) {
-    let filtered = object_keys_without(value, excluded);
-    object_merge(filtered, {});
+    arguments_assert(arguments, [
+        tautology,
+        tautology
+    ]);
+    object_merge_filtered(filtered, key => !list_contains(excluded, key), {});
 }
