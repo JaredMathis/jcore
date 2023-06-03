dddf21b05e8b56d6aa54acfce9ac0e07679cb8f5
JaredMathis <jared.mathis@native.consulting>
2023-06-03 13:22:27 -0400

    #67 noop

--- src/file/path/split.mjs
+++ src/file/path/split.mjs
@@ -3,8 +3,9 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { directory_separator } from '../../directory/separator.mjs';
 import { string_split } from '../../string/split.mjs';
+import { path_is } from '../../path/is.mjs';
 export function file_path_split(file_path) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [path_is]);
     let ds = directory_separator();
     return string_split(file_path, ds);
     metadata([]);
