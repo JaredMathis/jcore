ab439d164f81bf2d24ef157ae8c8d8bd88db99ff
JaredMathis <jared.mathis@native.consulting>
2023-06-03 19:06:46 -0400

    #66 function_auto function_callers_arguments_assert_auto_multiple

--- src/function/callers/arguments/assert/auto/multiple.mjs
+++ src/function/callers/arguments/assert/auto/multiple.mjs
@@ -1,16 +1,11 @@
-import { function_callers_arguments_assert_auto } from '../auto.mjs';
+import { function_callers_arguments_assert_auto_multiple_list } from './multiple/list.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-import { list_add_multiple } from '../../../../../list/add/multiple.mjs';
 export async function function_callers_arguments_assert_auto_multiple() {
     arguments_assert(arguments, []);
     let result = [];
     let multiple = [
-        "file_js_all_identifier_rename",
-        "file_js_identifier_rename"
-       ]
-    for (let m of multiple) {
-        let r = await function_callers_arguments_assert_auto(m);
-        list_add_multiple(result, r);
-    }
-    return result;
+        'file_js_all_identifier_rename',
+        'file_js_identifier_rename'
+    ];
+    return await function_callers_arguments_assert_auto_multiple_list(multiple, result);
 }
---
+++ src/function/callers/arguments/assert/auto/multiple/list.mjs
@@ -0,0 +1,9 @@
+import { list_add_multiple } from '../../../../../../list/add/multiple.mjs';
+import { function_callers_arguments_assert_auto } from '../../auto.mjs';
+export async function function_callers_arguments_assert_auto_multiple_list(multiple, result) {
+    for (let m of multiple) {
+        let r = await function_callers_arguments_assert_auto(m);
+        list_add_multiple(result, r);
+    }
+    return result;
+}
