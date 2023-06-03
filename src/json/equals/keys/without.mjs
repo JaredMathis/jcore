cdaa295e2178ad1bcaa87b9f02cfc9990018e1fe
JaredMathis <jared.mathis@native.consulting>
2023-06-03 19:12:04 -0400

    #66 function_auto json_equals_keys_without

--- src/json/equals/keys/without.mjs
+++ src/json/equals/keys/without.mjs
@@ -2,8 +2,6 @@ import { json_equals } from '../../equals.mjs';
 import { object_map_without } from '../../../object/map/without.mjs';
 import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { json_to } from '../../to.mjs';
-import { equal_by } from '../../../equal/by.mjs';
 export function json_equals_keys_without(a, b, values_without) {
     arguments_assert(arguments, [
         tautology,
@@ -13,7 +11,4 @@ export function json_equals_keys_without(a, b, values_without) {
     let filtered_a = object_map_without(a, values_without);
     let filtered_b = object_map_without(b, values_without);
     return json_equals(filtered_a, filtered_b);
-}
-function json_equals(filtered_a, filtered_b) {
-    return equal_by(json_to, filtered_a, filtered_b);
 }
