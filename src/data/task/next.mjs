7fbbf8a07e08cb8d470a83e7fe0e7c7566bd4812
JaredMathis <jared.mathis@native.consulting>
2023-06-03 13:10:18 -0400

    #67 function_callers_arguments_assert_auto data_task_id_set_hash

--- src/data/task/next.mjs
+++ src/data/task/next.mjs
@@ -1,10 +1,10 @@
-import { tautology } from '../../tautology.mjs';
+import { string_is } from '../../string/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { data_task_id_set_hash } from './id/set/hash.mjs';
 import { data_task_finish } from './finish.mjs';
 export async function data_task_next(task_id) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_is]);
     await data_task_finish();
     await data_task_id_set_hash(task_id);
     metadata([]);
