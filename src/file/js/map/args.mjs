be4d16eb7bd5172cda5ff7abe6b4ad75f1ccb250
JaredMathis <jared.mathis@native.consulting>
2023-06-03 14:13:30 -0400

    #67 noop

--- src/file/js/map/args.mjs
+++ src/file/js/map/args.mjs
@@ -1,3 +1,4 @@
+import { path_is } from '../../../path/is.mjs';
 import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
@@ -6,7 +7,7 @@ import { file_js_path_to_mapper_args } from '../path/to/mapper/args.mjs';
 import { js_mapper_with_function_declaration_get } from '../../../js/mapper/with/function/declaration/get.mjs';
 export async function file_js_map_args(file_path, mapper) {
     arguments_assert(arguments, [
-        tautology,
+        path_is,
         tautology
     ]);
     let args = await file_js_path_to_mapper_args(file_path);
