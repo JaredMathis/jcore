df736e51303244165bc556ecb2cb319668449e79
JaredMathis <jared.mathis@native.consulting>
2023-06-03 11:50:41 -0400

    #67 function_auto object_is

--- src/object/is.mjs
+++ src/object/is.mjs
@@ -1,6 +1,6 @@
-import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function object_is() {
-    arguments_assert(arguments, []);
-    error('todo: object_is');
+import { tautology } from '../tautology.mjs';
+export function object_is(v) {
+    arguments_assert(arguments, [tautology]);
+    return typeof v === 'object';
 }
