766a6bde894af6b0df8712fd7de482ea5b98a147
JaredMathis <jared.mathis@native.consulting>
2023-06-03 09:26:18 -0400

    #67 noop

--- src/function/callers/arguments/assert/auto/single.mjs
+++ src/function/callers/arguments/assert/auto/single.mjs
@@ -2,5 +2,5 @@ import { function_callers_arguments_assert_auto_generic } from './generic.mjs';
 import { function_to_statement_arguments_assert_args_predicate } from '../../../../to/statement/arguments/assert/args/predicate.mjs';
 export async function function_callers_arguments_assert_auto_single(function_name, c_function_name) {
     let arguments_assert_args = await function_to_statement_arguments_assert_args_predicate(function_name);
-    await function_callers_arguments_assert_auto_generic(c_function_name, function_name, arguments_assert_args, result);
+    await function_callers_arguments_assert_auto_generic(c_function_name, function_name, arguments_assert_args, []);
 }
