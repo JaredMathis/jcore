1dfb878bb6024a66804a9be742b90d437b971a02
JaredMathis <jared.mathis@native.consulting>
2023-06-03 13:47:15 -0400

    #67 noop

--- src/directory/read.mjs
+++ src/directory/read.mjs
@@ -1,5 +1,5 @@
+import { path_is } from '../path/is.mjs';
 import { directory_read_recursive } from './read/recursive.mjs';
-import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { comment } from '../comment.mjs';
@@ -7,7 +7,7 @@ import * as fs from 'fs/promises';
 import path from 'path';
 comment('From: https://gist.github.com/kethinov/6658166');
 export async function directory_read(dir) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [path_is]);
     return await directory_read_recursive(dir, []);
     metadata([]);
 }
