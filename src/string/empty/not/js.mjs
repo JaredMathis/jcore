6aec2bacc684bc0466d1e6143bc6e5e27e60520a
JaredMathis <jared.mathis@native.consulting>
2023-06-02 20:49:34 -0400

    #67 function_auto string_empty_not_js

--- src/string/empty/not/js.mjs
+++ src/string/empty/not/js.mjs
@@ -1,6 +1,6 @@
-import { error } from '../../../error.mjs';
+import { string_length } from '../../length.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export function string_empty_not_js() {
     arguments_assert(arguments, []);
-    error('todo: string_empty_not_js');
+    return string_length(s) >= 1;
 }
