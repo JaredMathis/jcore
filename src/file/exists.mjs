9259565115ab57e530537750f1f9b96d02d56c30
JaredMathis <jared.mathis@native.consulting>
2023-06-03 14:09:55 -0400

    #67 noop

--- src/file/exists.mjs
+++ src/file/exists.mjs
@@ -1,9 +1,9 @@
-import { tautology } from '../tautology.mjs';
+import { path_is } from '../path/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { path_exists } from '../path/exists.mjs';
 export async function file_exists(file_path) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [path_is]);
     return await path_exists(file_path);
     metadata([]);
 }
--- src/file/read.mjs
+++ src/file/read.mjs
@@ -1,11 +1,11 @@
-import { tautology } from '../tautology.mjs';
+import { path_is } from '../path/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { assert } from '../assert.mjs';
 import { path_exists } from '../path/exists.mjs';
 import fs from 'fs';
 export async function file_read(file_path) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [path_is]);
     assert(await path_exists(file_path));
     return await fs.promises.readFile(file_path, 'utf8');
     metadata([]);
