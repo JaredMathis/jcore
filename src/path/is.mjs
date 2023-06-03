6896476245252e2a390421c08e6213a39ab8c78d
JaredMathis <jared.mathis@native.consulting>
2023-06-03 11:52:50 -0400

    #67 function_auto path_is

---
+++ src/path/is.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function path_is() {
+    arguments_assert(arguments, []);
+    error('todo: path_is');
+}
