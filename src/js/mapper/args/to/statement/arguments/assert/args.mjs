309b1659f41cffbd9ea392947127f2639bb01b32
JaredMathis <jared.mathis@native.consulting>
2023-06-04 10:03:49 -0400

    #66 function_auto js_mapper_args_to_statement_arguments_assert_args

--- src/js/mapper/args/to/statement/arguments/assert/args.mjs
+++ src/js/mapper/args/to/statement/arguments/assert/args.mjs
@@ -1,8 +1,8 @@
+import { js_node_is_function_declaration } from '../../../../../../node/is/function/declaration.mjs';
 import { js_node_property_arguments } from '../../../../../../node/property/arguments.mjs';
 import { object_property_get } from '../../../../../../../object/property/get.mjs';
 import { js_statement_expression_to_expression } from '../../../../../../statement/expression/to/expression.mjs';
 import { js_mapper_args_to_statement_arguments_assert } from '../assert.mjs';
-import { tautology } from '../../../../../../../tautology.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 export async function js_mapper_args_to_statement_arguments_assert_args(function_declaration) {
     arguments_assert(arguments, [js_node_is_function_declaration]);
