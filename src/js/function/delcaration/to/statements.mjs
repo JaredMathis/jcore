c683816ddddb961bc9fcfaf18a380b3b4ac47545
JaredMathis <jared.mathis@native.consulting>
2023-06-03 23:57:37 -0400

    #66 noop

--- src/js/function/delcaration/to/statements.mjs
+++ src/js/function/delcaration/to/statements.mjs
@@ -6,8 +6,7 @@ import { assert } from '../../../../assert.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
 import { js_node_is_function_declaration } from '../../../node/is/function/declaration.mjs';
 export function js_function_delcaration_to_statements(fd) {
-    arguments_assert(arguments, [tautology]);
-    assert(js_node_is_function_declaration(fd));
+    arguments_assert(arguments, [js_node_is_function_declaration]);
     let function_body = object_property_get(fd, 'body');
     assert(js_node_is_block_statement(function_body));
     let function_body_statements = object_property_get(function_body, 'body');
