724452e5aab13ad8218b50af31515eae6cd926a5
JaredMathis <jared.mathis@native.consulting>
2023-06-03 22:25:30 -0400

    #66 noop

--- src/file/js/all/map/args.mjs
+++ src/file/js/all/map/args.mjs
@@ -1,10 +1,10 @@
-import { tautology } from '../../../../tautology.mjs';
+import { function_is } from '../../../../function/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { file_js_map_args } from '../../map/args.mjs';
 import { file_js_all_path } from '../path.mjs';
 export async function file_js_all_map_args(mapper) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [function_is]);
     for (let file_path of await file_js_all_path()) {
         await file_js_map_args(file_path, mapper);
     }
--- src/file/js/map/args.mjs
+++ src/file/js/map/args.mjs
@@ -1,5 +1,5 @@
+import { function_is } from '../../../function/is.mjs';
 import { path_is } from '../../../path/is.mjs';
-import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { file_js_overwrite } from '../overwrite.mjs';
@@ -8,7 +8,7 @@ import { js_mapper_with_function_declaration_get } from '../../../js/mapper/with
 export async function file_js_map_args(file_path, mapper) {
     arguments_assert(arguments, [
         path_is,
-        tautology
+        function_is
     ]);
     let args = await file_js_path_to_mapper_args(file_path);
     const get_result = js_mapper_with_function_declaration_get(mapper);
