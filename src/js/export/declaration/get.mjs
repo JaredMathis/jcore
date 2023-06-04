9d5cc8107e988b080be4141a28ecad7570030df0
JaredMathis <jared.mathis@native.consulting>
2023-06-04 08:40:45 -0400

    #66 noop

--- src/js/export/declaration/get.mjs
+++ src/js/export/declaration/get.mjs
@@ -3,8 +3,9 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { js_node_property_declaration } from '../../node/property/declaration.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
+import { js_node_is } from '../../node/is.mjs';
 export function js_export_declaration_get(e) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [js_node_is]);
     let d = object_property_get(e, js_node_property_declaration());
     return d;
     metadata([]);
