4510d82ba7ac1c5a43d4a0988e492a3e15dbddae
JaredMathis <jared.mathis@native.consulting>
2023-06-04 10:10:02 -0400

    #66 noop

--- src/js/node/is/type.mjs
+++ src/js/node/is/type.mjs
@@ -3,10 +3,12 @@ import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
+import { defined_is } from '../../../defined/is.mjs';
+import { string_empty_not_is } from '../../../string/empty/not/is.mjs';
 export function js_node_is_type(node, type_expected) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        defined_is,
+        string_empty_not_is
     ]);
     const type_actual = object_property_get(node, js_node_property_type());
     return type_actual === type_expected;
