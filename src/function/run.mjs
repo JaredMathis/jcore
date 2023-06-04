eb14806129f26f9a39a2c04e78f657f6f30a9685
JaredMathis <jared.mathis@native.consulting>
2023-06-04 01:07:35 -0400

    #66 noop

--- src/function/run.mjs
+++ src/function/run.mjs
@@ -3,14 +3,15 @@ import { function_import } from './import.mjs';
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
-export async function function_run(function_name, args) {
+import { list_is } from '../list/is.mjs';
+export async function function_run(function_name, function_args) {
     arguments_assert(arguments, [
         string_identifier_is,
-        tautology
+        list_is
     ]);
     let imported = await function_import(import.meta, function_name);
     let imported_function = imported[function_name];
-    let result = await imported_function(...args);
+    let result = await imported_function(...function_args);
     return result;
     metadata([]);
 }
