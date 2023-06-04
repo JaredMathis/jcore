cf8175944c5956fedb758deec1a2f245e1dc7db3
JaredMathis <jared.mathis@native.consulting>
2023-06-04 07:50:52 -0400

    #66 noop

--- src/js/code/call/expression/with/args.mjs
+++ src/js/code/call/expression/with/args.mjs
@@ -1,10 +1,11 @@
 import { tautology } from '../../../../../tautology.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { metadata } from '../../../../../metadata.mjs';
+import { string_is } from '../../../../../string/is.mjs';
 export function js_code_call_expression_with_args(name, args) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        string_is,
+        string_is
     ]);
     return `${ name }(${ args })`;
     metadata([]);
