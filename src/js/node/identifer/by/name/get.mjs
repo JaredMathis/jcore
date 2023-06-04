00835e756d9786a7bbd38030a9705c59589afdef
JaredMathis <jared.mathis@native.consulting>
2023-06-04 09:26:43 -0400

    #66 noop

--- src/js/node/identifer/by/name/get.mjs
+++ src/js/node/identifer/by/name/get.mjs
@@ -4,10 +4,12 @@ import { metadata } from '../../../../../metadata.mjs';
 import { js_node_is_identifier } from '../../../is/identifier.mjs';
 import { assert } from '../../../../../assert.mjs';
 import { object_property_get } from '../../../../../object/property/get.mjs';
+import { defined_is } from '../../../../../defined/is.mjs';
+import { string_is } from '../../../../../string/is.mjs';
 export function js_node_identifer_by_name_get(node, node_identifier_name) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        defined_is,
+        string_is
     ]);
     let id = object_property_get(node, node_identifier_name);
     assert(js_node_is_identifier(id));
