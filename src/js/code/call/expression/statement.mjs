c4b1f1b3740a87f6f205755ade2fac3c381f3071
JaredMathis <jared.mathis@native.consulting>
2023-06-04 07:45:51 -0400

    #66 noop

--- src/js/code/call/expression/statement.mjs
+++ src/js/code/call/expression/statement.mjs
@@ -3,8 +3,9 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { js_statement_end } from '../../../statement/end.mjs';
 import { js_code_call_expression } from '../expression.mjs';
+import { js_node_is_call_expression } from '../../../node/is/call/expression.mjs';
 export function js_code_call_expression_statement(callee) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [js_node_is_call_expression]);
     return `${ js_code_call_expression(callee) }${ js_statement_end() }`;
     metadata([]);
 }
