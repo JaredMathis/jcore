2f9a6cfa78c287da41d866039f9ed2fd2624aee9
JaredMathis <jared.mathis@native.consulting>
2023-06-04 00:43:40 -0400

    #66 noop

--- src/function/is.mjs
+++ src/function/is.mjs
@@ -1,6 +1,7 @@
 import { arguments_assert } from '../arguments/assert.mjs';
+import { defined_is } from '../defined/is.mjs';
 import { tautology } from '../tautology.mjs';
 export function function_is(candidate) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [defined_is]);
     return typeof candidate === 'function';
 }
