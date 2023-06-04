58213d0829f2f91176ed1f38ef43cbd39a9e2577
JaredMathis <jared.mathis@native.consulting>
2023-06-03 23:54:41 -0400

    #66 noop

--- src/file/open/vs/code.mjs
+++ src/file/open/vs/code.mjs
@@ -2,8 +2,9 @@ import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { command_line } from '../../../command/line.mjs';
+import { path_is } from '../../../path/is.mjs';
 export async function file_open_vs_code(function_path) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [path_is]);
     await command_line(`code ${ function_path }`);
     metadata([]);
 }
