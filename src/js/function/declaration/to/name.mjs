949c881d785caccefac11697cd8b16ae5419d913
JaredMathis <jared.mathis@native.consulting>
2023-06-04 09:22:30 -0400

    #66 noop

--- src/js/function/declaration/to/name.mjs
+++ src/js/function/declaration/to/name.mjs
@@ -2,8 +2,9 @@ import { tautology } from '../../../../tautology.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { js_id_name_get } from '../../../id/name/get.mjs';
+import { js_node_is_function_declaration } from '../../../node/is/function/declaration.mjs';
 export function js_function_declaration_to_name(fd) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [js_node_is_function_declaration]);
     return js_id_name_get(fd);
     metadata([]);
 }
