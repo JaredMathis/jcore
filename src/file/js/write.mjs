9497e86f73f0bbc1f15ef471f489d35a9165a1db
JaredMathis <jared.mathis@native.consulting>
2023-06-03 23:54:22 -0400

    #66 noop

--- src/file/js/write.mjs
+++ src/file/js/write.mjs
@@ -1,12 +1,12 @@
+import { path_is } from '../../path/is.mjs';
 import { defined_is } from '../../defined/is.mjs';
-import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { file_write } from '../write.mjs';
 import { js_unparse } from '../../js/unparse.mjs';
 export async function file_js_write(function_path, parsed) {
     arguments_assert(arguments, [
-        tautology,
+        path_is,
         defined_is
     ]);
     let unparsed = js_unparse(parsed);
