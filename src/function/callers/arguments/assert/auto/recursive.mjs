629f9ab869564d6345532026b7456cc19f23dbbd
JaredMathis <jared.mathis@native.consulting>
2023-06-03 22:37:48 -0400

    #66 noop

--- src/function/callers/arguments/assert/auto/recursive.mjs
+++ src/function/callers/arguments/assert/auto/recursive.mjs
@@ -1,14 +1,18 @@
+import { log } from '../../../../../log.mjs';
 import { function_callers_arguments_assert_auto_multiple_list } from './multiple/list.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { json_equal } from '../../../../../json/equal.mjs';
-export async function function_callers_arguments_assert_auto_recursive() {
-    arguments_assert(arguments, []);
-    let root = 'js_mapper_with_function_declaration_get';
+import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
+export async function function_callers_arguments_assert_auto_recursive(root) {
+    arguments_assert(arguments, [string_identifier_is]);
     let result = [root];
     let result_previous;
     do {
         result_previous = result;
         result = await function_callers_arguments_assert_auto_multiple_list(result, []);
-        console.log({result_previous, result})
+        console.log({
+            result_previous,
+            result
+        });
     } while (!json_equal(result, result_previous));
 }
--- src/function/name/to/file/path.mjs
+++ src/function/name/to/file/path.mjs
@@ -10,10 +10,8 @@ import { function_extension } from '../../../extension.mjs';
 import { directory_current } from '../../../../directory/current.mjs';
 import { path_join } from '../../../../path/join.mjs';
 import { function_name_separator } from '../../separator.mjs';
-if (false)
-    string_identifier_is;
 export function function_name_to_file_path(function_name) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_identifier_is]);
     let directory_source_result = directory_source();
     let split = string_split(function_name, function_name_separator());
     list_add_beginning(split, directory_source_result);
