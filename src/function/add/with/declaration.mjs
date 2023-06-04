77541f6fb56c6fe98fac4fca84d32c4d267cebc7
JaredMathis <jared.mathis@native.consulting>
2023-06-03 23:56:50 -0400

    #66 noop

--- src/function/add/with/declaration.mjs
+++ src/function/add/with/declaration.mjs
@@ -1,5 +1,5 @@
+import { js_node_is_function_declaration } from '../../../js/node/is/function/declaration.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
-import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { js_code_function_name_to_parsed } from '../../../js/code/function/name/to/parsed.mjs';
@@ -10,7 +10,7 @@ import { function_write } from '../../write.mjs';
 export async function function_add_with_declaration(function_name, fd) {
     arguments_assert(arguments, [
         string_identifier_is,
-        tautology
+        js_node_is_function_declaration
     ]);
     let parsed = js_code_function_name_to_parsed(function_name);
     let export_single = js_export_single(parsed);
