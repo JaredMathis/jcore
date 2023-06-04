d6de2e634fa6436262b62cb11ed01e2bcface61e
JaredMathis <jared.mathis@native.consulting>
2023-06-04 01:48:50 -0400

    #66 noop

--- src/integer/is.mjs
+++ src/integer/is.mjs
@@ -2,8 +2,9 @@ import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { todo } from '../todo.mjs';
+import { defined_is } from '../defined/is.mjs';
 export function integer_is(candidate) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [defined_is]);
     todo(`Maybe revisit after learning about JS number nuance - what about Infinity, NaN, etc.`);
     return Number.isFinite(candidate);
     metadata([]);
