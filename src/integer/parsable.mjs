d7876ee634a1441edc75e610a6904a9744c99688
JaredMathis <jared.mathis@native.consulting>
2023-06-04 02:01:15 -0400

    #66 noop

--- src/integer/parsable.mjs
+++ src/integer/parsable.mjs
@@ -1,10 +1,10 @@
-import { tautology } from '../tautology.mjs';
+import { string_is } from '../string/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { number_nan_is_not } from '../number/nan/is/not.mjs';
 import { integer_parse } from './parse.mjs';
 export function integer_parsable(input) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_is]);
     return number_nan_is_not(integer_parse(input));
     metadata([]);
 }
--- src/integer/parse/try.mjs
+++ src/integer/parse/try.mjs
@@ -1,8 +1,9 @@
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
+import { string_is } from '../../string/is.mjs';
 import { tautology } from '../../tautology.mjs';
 export function integer_parse_try(input) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_is]);
     return parseInt(input, 10);
     metadata([]);
 }
