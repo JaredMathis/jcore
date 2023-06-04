f5cfd370b5416aed9caf16784773959f0d473430
JaredMathis <jared.mathis@native.consulting>
2023-06-03 23:00:15 -0400

    #66 noop

--- src/file/js/dependencies.mjs
+++ src/file/js/dependencies.mjs
@@ -4,8 +4,9 @@ import { visit_ignore_duplicates_async } from '../../visit/ignore/duplicates/asy
 import { metadata } from '../../metadata.mjs';
 import { file_js_dependencies_non_recursive } from './dependencies/non/recursive.mjs';
 import { list_add } from '../../list/add.mjs';
+import { path_is } from '../../path/is.mjs';
 export async function file_js_dependencies(file_path) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [path_is]);
     let result = [];
     await visit_ignore_duplicates_async(file_path, async node => {
         return await file_js_dependencies_non_recursive(node);
