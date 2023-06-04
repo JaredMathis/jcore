65759f657049bea41d195f1b860d3063d4840d60
JaredMathis <jared.mathis@native.consulting>
2023-06-03 23:52:37 -0400

    #66 noop

--- src/file/js/path/to/name.mjs
+++ src/file/js/path/to/name.mjs
@@ -4,8 +4,9 @@ import { metadata } from '../../../../metadata.mjs';
 import { list_single } from '../../../../list/single.mjs';
 import { list_single_item } from '../../../../list/single/item.mjs';
 import { file_js_path_all_to_name } from '../all/to/name.mjs';
+import { path_is } from '../../../../path/is.mjs';
 export function file_js_path_to_name(file_js_path) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [path_is]);
     return list_single(file_js_path_all_to_name(list_single_item(file_js_path)));
     metadata([]);
 }
