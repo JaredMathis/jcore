6fbbb9d29869978414f22e8bc8a07cdef13705dc
JaredMathis <jared.mathis@native.consulting>
2023-06-04 00:03:40 -0400

    #66 noop

--- src/refactor/multiple.mjs
+++ src/refactor/multiple.mjs
@@ -1,10 +1,12 @@
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
+import { defined_is } from '../defined/is.mjs';
+import { list_is } from '../list/is.mjs';
 export async function refactor_multiple(args, refactors) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        defined_is,
+        list_is
     ]);
     for (let r of refactors) {
         await r(args);
