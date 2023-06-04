77d3f6c19145cb0caa7201f70ada54382b84d76b
JaredMathis <jared.mathis@native.consulting>
2023-06-04 07:41:03 -0400

    #66 noop

--- src/js/call/expression/to/name/generic.mjs
+++ src/js/call/expression/to/name/generic.mjs
@@ -4,10 +4,11 @@ import { metadata } from '../../../../../metadata.mjs';
 import { object_property_get } from '../../../../../object/property/get.mjs';
 import { assert } from '../../../../../assert.mjs';
 import { js_node_is_identifier } from '../../../../node/is/identifier.mjs';
+import { boolean_is } from '../../../../../boolean/is.mjs';
 export function js_call_expression_to_name_generic(ce, or_null) {
     arguments_assert(arguments, [
         tautology,
-        tautology
+        boolean_is
     ]);
     let callee = object_property_get(ce, 'callee');
     if (or_null) {
