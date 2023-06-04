352701b9c39276f8310e53fb32697a41c5f5bc47
JaredMathis <jared.mathis@native.consulting>
2023-06-04 01:44:22 -0400

    #66 noop

--- src/identity.mjs
+++ src/identity.mjs
@@ -1,8 +1,9 @@
 import { tautology } from './tautology.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 import { metadata } from './metadata.mjs';
+import { defined_is } from './defined/is.mjs';
 export function identity(value) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [defined_is]);
     return value;
     metadata([]);
 }
