0bb76e0543aa204d6ffbe8d63fff4bc8ad7fad67
JaredMathis <jared.mathis@native.consulting>
2023-06-03 23:19:33 -0400

    #66 noop

--- src/boolean/is.mjs
+++ src/boolean/is.mjs
@@ -1,3 +1,7 @@
+import { tautology } from '../tautology.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+import { defined_is } from '../defined/is.mjs';
 export function boolean_is(b) {
-    return b === true || b === false
+    arguments_assert(arguments, [defined_is]);
+    return b === true || b === false;
 }
--- src/json/equal.mjs
+++ src/json/equal.mjs
@@ -1,5 +1,11 @@
+import { tautology } from '../tautology.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 import { json_to } from './to.mjs';
 import { equal_by } from '../equal/by.mjs';
 export function json_equal(filtered_a, filtered_b) {
+    arguments_assert(arguments, [
+        tautology,
+        tautology
+    ]);
     return equal_by(json_to, filtered_a, filtered_b);
 }
