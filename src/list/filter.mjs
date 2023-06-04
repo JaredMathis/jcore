ad9634f09afb418579790b6fa0f47c776d33dfd6
JaredMathis <jared.mathis@native.consulting>
2023-06-03 23:20:01 -0400

    #66 noop

--- src/list/filter.mjs
+++ src/list/filter.mjs
@@ -1,10 +1,12 @@
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
+import { list_is } from './is.mjs';
+import { function_is } from '../function/is.mjs';
 export function list_filter(list, predicate) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        list_is,
+        function_is
     ]);
     return list.filter(element => predicate(element));
     metadata([]);
