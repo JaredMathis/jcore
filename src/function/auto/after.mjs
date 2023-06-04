da919272b118270723afd87987f702a3e9b3de55
JaredMathis <jared.mathis@native.consulting>
2023-06-04 00:06:01 -0400

    #66 noop

--- src/function/auto/after.mjs
+++ src/function/auto/after.mjs
@@ -1,11 +1,11 @@
-import { tautology } from '../../tautology.mjs';
+import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { function_name_get } from '../name/get.mjs';
 import { function_auto_after_refactors } from './after/refactors.mjs';
 import { function_map } from '../map.mjs';
 export async function function_auto_after(function_name) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_identifier_is]);
     let refactors = function_auto_after_refactors();
     for (let r of refactors) {
         await function_map(function_name_get(r), function_name);
