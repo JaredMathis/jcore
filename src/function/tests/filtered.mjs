dc10ec689cb7c8580c397e6a7423217e8eaffe1d
JaredMathis <jared.mathis@native.consulting>
2023-06-04 01:14:00 -0400

    #66 noop

--- src/function/tests/filtered.mjs
+++ src/function/tests/filtered.mjs
@@ -2,8 +2,9 @@ import { function_all_tests } from '../all/tests.mjs';
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
+import { function_is } from '../is.mjs';
 export async function function_tests_filtered(filter) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [function_is]);
     let filtered = await function_all_tests();
     let filtered2 = filter(filtered);
     return filtered2;
