9e91d278688c2a04e3a1be287b1c7f8c025cdcdc
JaredMathis <jared.mathis@native.consulting>
2023-06-04 01:14:18 -0400

    #66 noop

--- src/function/tests/filtered.mjs
+++ src/function/tests/filtered.mjs
@@ -5,8 +5,8 @@ import { metadata } from '../../metadata.mjs';
 import { function_is } from '../is.mjs';
 export async function function_tests_filtered(filter) {
     arguments_assert(arguments, [function_is]);
-    let filtered = await function_all_tests();
-    let filtered2 = filter(filtered);
-    return filtered2;
+    let all = await function_all_tests();
+    let filtered = filter(all);
+    return filtered;
     metadata([]);
 }
