fe4990d274a3fcf405413c1042a50a11ea1ad245
JaredMathis <jared.mathis@native.consulting>
2023-06-04 02:10:27 -0400

    #66 noop

--- src/string/length.mjs
+++ src/string/length.mjs
@@ -1,8 +1,9 @@
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
+import { string_is } from './is.mjs';
 export function string_length(input) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_is]);
     return input.length;
     metadata([]);
 }
