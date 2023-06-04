6e4ef2bc4cf8cb616dda02608bfa3978d347ba36
JaredMathis <jared.mathis@native.consulting>
2023-06-04 00:44:48 -0400

    #66 noop

--- src/function/names/each.mjs
+++ src/function/names/each.mjs
@@ -1,7 +1,8 @@
+import { function_is } from '../is.mjs';
 import { function_names_each_filter } from './each/filter.mjs';
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function function_names_each(logic) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [function_is]);
     return await function_names_each_filter(logic, tautology);
 }
