903760fdb24c24e142cf3bff8255b3459ad8dd45
JaredMathis <jared.mathis@native.consulting>
2023-06-03 22:37:10 -0400

    #66 function_auto function_name_to_file_path

--- src/function/name/to/file/path.mjs
+++ src/function/name/to/file/path.mjs
@@ -1,3 +1,4 @@
+import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 import { tautology } from '../../../../tautology.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
@@ -9,7 +10,8 @@ import { function_extension } from '../../../extension.mjs';
 import { directory_current } from '../../../../directory/current.mjs';
 import { path_join } from '../../../../path/join.mjs';
 import { function_name_separator } from '../../separator.mjs';
-if (false) string_identifier_is
+if (false)
+    string_identifier_is;
 export function function_name_to_file_path(function_name) {
     arguments_assert(arguments, [tautology]);
     let directory_source_result = directory_source();
