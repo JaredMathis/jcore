0982ba4fab473785370e377fb7ababf606f17832
JaredMathis <jared.mathis@native.consulting>
2023-06-03 22:14:31 -0400

    #66 function_auto function_callers_arguments_assert_auto_recursive

--- src/function/callers/arguments/assert/auto/recursive.mjs
+++ src/function/callers/arguments/assert/auto/recursive.mjs
@@ -1,8 +1,13 @@
 import { function_callers_arguments_assert_auto_multiple_list } from './multiple/list.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
+import { json_equal } from '../../../../../json/equal.mjs';
 export async function function_callers_arguments_assert_auto_recursive() {
     arguments_assert(arguments, []);
     let root = 'js_mapper_with_function_declaration_get';
-    let result = [];
-    let result_next = await function_callers_arguments_assert_auto_multiple_list([root], result);
+    let result;
+    let result_next;
+    do {
+        result = [];
+        result_next = await function_callers_arguments_assert_auto_multiple_list([root], result);
+    } while (!json_equal(result, result_next));
 }
