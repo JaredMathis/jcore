0a3bba3fd9f210d1d04834bad7abc0e178cb4bb4
JaredMathis <jared.mathis@native.consulting>
2023-06-03 23:50:08 -0400

    #66 noop

--- src/list/map.mjs
+++ src/list/map.mjs
@@ -1,9 +1,13 @@
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
+import { list_is } from './is.mjs';
+if (false) {
+    function_is
+}
 export function list_map(list, mapper) {
     arguments_assert(arguments, [
-        tautology,
+        list_is,
         tautology
     ]);
     return list.map(element => mapper(element));
