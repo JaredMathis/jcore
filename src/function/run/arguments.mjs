c4b9a733d1d976be5872e8051ed40f17e7ec4562
JaredMathis <jared.mathis@native.consulting>
2023-06-04 01:10:04 -0400

    #66 noop

--- src/function/run/arguments.mjs
+++ src/function/run/arguments.mjs
@@ -5,8 +5,9 @@ import { list_first } from '../../list/first.mjs';
 import { list_skip_first } from '../../list/skip/first.mjs';
 import { list_to } from '../../list/to.mjs';
 import { function_run } from '../run.mjs';
+import { defined_is } from '../../defined/is.mjs';
 export async function function_run_arguments(input) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [defined_is]);
     let args = list_to(input);
     let function_name = list_first(args);
     let remaining = list_skip_first(args);
--- src/string/function/name/prefix.mjs
+++ src/string/function/name/prefix.mjs
@@ -2,8 +2,9 @@ import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { function_name_separator } from '../../../function/name/separator.mjs';
+import { string_identifier_is } from '../../identifier/is.mjs';
 export function string_function_name_prefix(function_name) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_identifier_is]);
     return function_name + function_name_separator();
     metadata([]);
 }
