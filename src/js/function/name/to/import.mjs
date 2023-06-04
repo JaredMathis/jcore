e207ae3701eea4bb26acae6d86240cd6bed466bd
JaredMathis <jared.mathis@native.consulting>
2023-06-04 09:23:29 -0400

    #66 noop

--- src/js/function/name/to/import.mjs
+++ src/js/function/name/to/import.mjs
@@ -3,8 +3,9 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { js_parse_statement_module } from '../../../parse/statement/module.mjs';
 import { function_extension } from '../../../../function/extension.mjs';
+import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 export function js_function_name_to_import(m) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_identifier_is]);
     const code = `import {${ m }} from './${ m }${ function_extension() }'`;
     let statement = js_parse_statement_module(code);
     return statement;
