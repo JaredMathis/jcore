b8133e0232086a8d6a0508c5e003731893bd4f13
JaredMathis <jared.mathis@native.consulting>
2023-06-04 01:49:25 -0400

    #66 noop

--- src/integer/parse.mjs
+++ src/integer/parse.mjs
@@ -4,8 +4,9 @@ import { metadata } from '../metadata.mjs';
 import { number_nan_is_not } from '../number/nan/is/not.mjs';
 import { integer_parse_try } from './parse/try.mjs';
 import { assert } from '../assert.mjs';
+import { string_is } from '../string/is.mjs';
 export function integer_parse(input) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_is]);
     let result = integer_parse_try(input);
     assert(number_nan_is_not(result));
     return result;
