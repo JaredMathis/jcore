e5efc94a1a80ab41b7a4a2f70d3848f4e3587201
JaredMathis <jared.mathis@native.consulting>
2023-06-04 09:47:40 -0400

    #66 noop

--- src/js/node/identifer/by/name/get.mjs
+++ src/js/node/identifer/by/name/get.mjs
@@ -6,10 +6,12 @@ import { assert } from '../../../../../assert.mjs';
 import { object_property_get } from '../../../../../object/property/get.mjs';
 import { defined_is } from '../../../../../defined/is.mjs';
 import { string_is } from '../../../../../string/is.mjs';
+import { js_node_is } from '../../../is.mjs';
+import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
 export function js_node_identifer_by_name_get(node, node_identifier_name) {
     arguments_assert(arguments, [
-        defined_is,
-        string_is
+        js_node_is,
+        string_identifier_is
     ]);
     let id = object_property_get(node, node_identifier_name);
     assert(js_node_is_identifier(id));
