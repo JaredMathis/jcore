dbb2847f49d1cddbb6e580b5c9813bbc1c434c46
JaredMathis <jared.mathis@native.consulting>
2023-06-04 01:27:41 -0400

    #66 noop

--- src/function/tests/prefix/get.mjs
+++ src/function/tests/prefix/get.mjs
@@ -2,8 +2,9 @@ import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { string_function_tests_sub } from '../../../string/function/tests/sub.mjs';
+import { string_identifier_is } from '../../../string/identifier/is.mjs';
 export function function_tests_prefix_get(function_name) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_identifier_is]);
     return `${ function_name }${ string_function_tests_sub() }`;
     metadata([]);
 }
