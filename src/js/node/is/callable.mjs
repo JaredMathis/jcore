d42eed938dfa10b6297022defdfcced0acefdb9d
JaredMathis <jared.mathis@native.consulting>
2023-06-05 11:30:08 -0400

    #66 noop

--- src/js/node/is/callable.mjs
+++ src/js/node/is/callable.mjs
@@ -1,10 +1,10 @@
-import { tautology } from '../../../tautology.mjs';
+import { js_node_is } from '../is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { js_node_is_function_declaration } from './function/declaration.mjs';
 import { js_node_is_arrow_function_expression } from './arrow/function/expression.mjs';
 export function js_node_is_callable(node) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [js_node_is]);
     return js_node_is_arrow_function_expression(node) || js_node_is_function_declaration(node);
     metadata([]);
 }
