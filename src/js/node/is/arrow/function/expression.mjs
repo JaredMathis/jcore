87cbe2a33669c439424eccbba3379988b74fe5e3
JaredMathis <jared.mathis@native.consulting>
2023-06-05 09:24:34 -0400

    #66 function_auto js_node_is

--- src/js/node/is/arrow/function/expression.mjs
+++ src/js/node/is/arrow/function/expression.mjs
@@ -3,7 +3,7 @@ import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { metadata } from '../../../../../metadata.mjs';
 import { js_node_is_type } from '../../type.mjs';
 export function js_node_is_arrow_function_expression(node) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [js_node_is]);
     return js_node_is_type(node, 'ArrowFunctionExpression');
     metadata([]);
 }
