a50a7d95809c31e4f87b7b24ee7ea1e0fd235109
JaredMathis <jared.mathis@native.consulting>
2023-06-05 16:26:07 -0400

    #66 noop

--- src/js/node/is/import/declaration.mjs
+++ src/js/node/is/import/declaration.mjs
@@ -3,7 +3,7 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { js_node_is_type } from '../type.mjs';
 export function js_node_is_import_declaration(node) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [js_node_is_type]);
     return js_node_is_type(node, 'ImportDeclaration');
     metadata([]);
 }
