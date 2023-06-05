bea5cec7d72fe4cf29f9ec3031cbf28698fc1197
JaredMathis <jared.mathis@native.consulting>
2023-06-05 11:44:48 -0400

    #66 noop

--- src/js/node/is/block/statement.mjs
+++ src/js/node/is/block/statement.mjs
@@ -1,12 +1,9 @@
 import { js_node_is } from '../../is.mjs';
-import { tautology } from '../../../../tautology.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { js_node_is_type } from '../type.mjs';
-if (false)
-    js_node_is;
 export function js_node_is_block_statement(node) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [js_node_is]);
     return js_node_is_type(node, 'BlockStatement');
     metadata([]);
 }
