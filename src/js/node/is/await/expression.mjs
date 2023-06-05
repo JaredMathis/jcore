5b3587e23b030a1bfc74ee58689f1c04e188162b
JaredMathis <jared.mathis@native.consulting>
2023-06-05 11:37:29 -0400

    #66 function_auto js_node_is_await_expression

--- src/js/node/is/await/expression.mjs
+++ src/js/node/is/await/expression.mjs
@@ -1,9 +1,9 @@
-import { tautology } from '../../../../tautology.mjs';
+import { js_node_is } from '../../is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { js_node_is_type } from '../type.mjs';
 export function js_node_is_await_expression(node) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [js_node_is]);
     return js_node_is_type(node, 'AwaitExpression');
     metadata([]);
 }
