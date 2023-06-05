89e17ac67881bc002a490cd978a83f6daccfd4bf
JaredMathis <jared.mathis@native.consulting>
2023-06-05 11:32:48 -0400

    #66 function_auto js_node_is_assignment_expression

--- src/js/node/is/assignment/expression.mjs
+++ src/js/node/is/assignment/expression.mjs
@@ -1,7 +1,6 @@
-import { tautology } from '../../../../tautology.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { js_node_is_type } from '../type.mjs';
 export function js_node_is_assignment_expression(n) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [js_is_node]);
     return js_node_is_type(n, 'AssignmentExpression');
 }
