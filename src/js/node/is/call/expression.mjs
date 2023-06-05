ecfd5e05b939544bd78e59216b032d55692454cb
JaredMathis <jared.mathis@native.consulting>
2023-06-05 11:51:15 -0400

    #66 function_auto js_node_is_call_expression

--- src/js/node/is/call/expression.mjs
+++ src/js/node/is/call/expression.mjs
@@ -1,12 +1,9 @@
 import { js_node_is } from '../../is.mjs';
-import { tautology } from '../../../../tautology.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { js_node_is_type } from '../type.mjs';
 export function js_node_is_call_expression(node) {
-    arguments_assert(arguments, [tautology]);
-    if (false)
-        js_node_is;
+    arguments_assert(arguments, [js_node_is]);
     return js_node_is_type(node, 'CallExpression');
     metadata([]);
 }
