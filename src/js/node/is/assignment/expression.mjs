d8686e770de6a86c572b665878eacc206f4a79d0
JaredMathis <jared.mathis@native.consulting>
2023-06-05 11:33:02 -0400

    #66 function_auto js_node_is_assignment_expression

--- src/js/node/is/assignment/expression.mjs
+++ src/js/node/is/assignment/expression.mjs
@@ -1,6 +1,7 @@
+import { js_node_is } from '../../is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { js_node_is_type } from '../type.mjs';
 export function js_node_is_assignment_expression(n) {
-    arguments_assert(arguments, [js_is_node]);
+    arguments_assert(arguments, [js_node_is]);
     return js_node_is_type(n, 'AssignmentExpression');
 }
