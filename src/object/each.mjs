e0f7fe5c29b8e0930f5a1a71d9cf254db68fed69
JaredMathis <jared.mathis@native.consulting>
2023-06-06 14:50:07 -0400

    #66 noop

--- src/object/each.mjs
+++ src/object/each.mjs
@@ -2,10 +2,12 @@ import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { object_property_get } from './property/get.mjs';
+import { defined_is } from '../defined/is.mjs';
+import { function_is } from '../function/is.mjs';
 export function object_each(object, lambda) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        defined_is,
+        function_is
     ]);
     for (let key in object) {
         let value = object_property_get(object, key);
