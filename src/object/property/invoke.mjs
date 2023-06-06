a917304d66f333c2894c132e2c32fd769e9934ee
JaredMathis <jared.mathis@native.consulting>
2023-06-06 14:56:00 -0400

    #66 noop

--- src/object/property/invoke.mjs
+++ src/object/property/invoke.mjs
@@ -3,11 +3,13 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { object_property_exists } from './exists.mjs';
 import { object_property_get } from './get.mjs';
+import { defined_is } from '../../defined/is.mjs';
+import { string_is } from '../../string/is.mjs';
 export function object_property_invoke(default_value, object, property_name) {
     arguments_assert(arguments, [
-        tautology,
-        tautology,
-        tautology
+        defined_is,
+        defined_is,
+        string_is
     ]);
     let result = default_value;
     if (object_property_exists(object, property_name)) {
