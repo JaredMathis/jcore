02e4e656ad5d4a337728f78bff2ed839eb5df3b2
JaredMathis <jared.mathis@native.consulting>
2023-06-06 11:22:49 -0400

    #66 noop

--- src/list/add/beginning.mjs
+++ src/list/add/beginning.mjs
@@ -1,10 +1,12 @@
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
+import { list_is } from '../is.mjs';
+import { defined_is } from '../../defined/is.mjs';
 export function list_add_beginning(list, element) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        list_is,
+        defined_is
     ]);
     list.unshift(element);
     metadata([]);
--- src/list/add/beginning/multiple.mjs
+++ src/list/add/beginning/multiple.mjs
@@ -1,11 +1,10 @@
-import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { list_add_beginning } from '../beginning.mjs';
 import { list_is } from '../../is.mjs';
 export function list_add_beginning_multiple(list, element_new_all) {
     arguments_assert(arguments, [
-        tautology,
+        list_is,
         list_is
     ]);
     for (let e of element_new_all) {
