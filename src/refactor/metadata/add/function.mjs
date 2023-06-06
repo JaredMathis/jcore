556c1942f2387b5fff3423d71bade5cf799087a8
JaredMathis <jared.mathis@native.consulting>
2023-06-06 15:01:36 -0400

    #66 noop

--- src/refactor/metadata/add/function.mjs
+++ src/refactor/metadata/add/function.mjs
@@ -3,10 +3,11 @@ import { refactor_metadata_add_named } from './named.mjs';
 import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { function_name_get } from '../../../function/name/get.mjs';
+import { function_is } from '../../../function/is.mjs';
 export async function refactor_metadata_add_function(args, metadata_function) {
     arguments_assert(arguments, [
         defined_is,
-        tautology
+        function_is
     ]);
     const metadata_function_name = function_name_get(metadata_function);
     await refactor_metadata_add_named(args, metadata_function_name);
