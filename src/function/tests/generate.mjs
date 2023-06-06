a866e1abddd6373df1f2510c1a1a826b7fd46e23
JaredMathis <jared.mathis@native.consulting>
2023-06-06 15:58:55 -0400

    #65 function_auto function_tests_generate

--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -1,9 +1,14 @@
+import { js_mapper_args_to_statement_arguments_assert_args_predicate } from '../../js/mapper/args/to/statement/arguments/assert/args/predicate.mjs';
+import { function_map } from '../map.mjs';
 import { function_tests_count } from './count.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { assert } from '../../assert.mjs';
 export async function function_tests_generate(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let tests_count = await function_tests_count(function_name);
-    assert(tests_count === 0);
+    if (tests_count === 0) {
+        return;
+    }
+    function_map();
+    js_mapper_args_to_statement_arguments_assert_args_predicate;
 }
