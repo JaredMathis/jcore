4de002e403a34cfe8133f6137b0d60f2336b7ad0
JaredMathis <jared.mathis@native.consulting>
2023-06-06 12:21:05 -0400

    #66 noop

--- src/number/nan/is/not.mjs
+++ src/number/nan/is/not.mjs
@@ -1,8 +1,9 @@
 import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
+import { defined_is } from '../../../defined/is.mjs';
 export function number_nan_is_not(result) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [defined_is]);
     return !isNaN(result);
     metadata([]);
 }
