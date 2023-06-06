498b046d75db7b0e43c16751c3b8d3e8f9950102
JaredMathis <jared.mathis@native.consulting>
2023-06-06 15:06:42 -0400

    #66 noop

--- src/string/letter/is.mjs
+++ src/string/letter/is.mjs
@@ -1,6 +1,7 @@
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { string_is } from '../is.mjs';
 export function string_letter_is(first) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_is]);
     return first.toUpperCase() != first.toLowerCase();
 }
