fa6663cea9263b46d5016147499faa3ae112befd
JaredMathis <jared.mathis@native.consulting>
2023-06-06 15:05:17 -0400

    #66 noop

--- src/string/is.mjs
+++ src/string/is.mjs
@@ -1,8 +1,9 @@
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
+import { defined_is } from '../defined/is.mjs';
 export function string_is(value) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [defined_is]);
     return typeof value === 'string';
     metadata([]);
 }
