64f83f40a0f190b3dd43f208227b99fcf3d27e89
JaredMathis <jared.mathis@native.consulting>
2023-06-06 15:02:07 -0400

    #66 noop

--- src/refactor/unasyncify/each.mjs
+++ src/refactor/unasyncify/each.mjs
@@ -5,8 +5,9 @@ import { object_replace } from '../../object/replace.mjs';
 import { js_node_property_argument } from '../../js/node/property/argument.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 import { js_node_is_await_expression } from '../../js/node/is/await/expression.mjs';
+import { js_mapper_args_is } from '../../js/mapper/args/is.mjs';
 export function refactor_unasyncify_each(v) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [js_mapper_args_is]);
     let {node} = v;
     if (js_node_is_await_expression(node)) {
         let arg = object_property_get(node, js_node_property_argument());
--- src/string/empty/not/is.mjs
+++ src/string/empty/not/is.mjs
@@ -1,7 +1,8 @@
 import { string_length } from '../../length.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { tautology } from '../../../tautology.mjs';
+import { string_is } from '../../is.mjs';
 export function string_empty_not_is(s) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_is]);
     return string_length(s) >= 1;
 }
