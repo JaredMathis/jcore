5af8a5860b98a6622d8add3241ea18277daff7ee
JaredMathis <jared.mathis@native.consulting>
2023-06-06 14:56:35 -0400

    #66 noop

--- src/object/property/remove.mjs
+++ src/object/property/remove.mjs
@@ -1,10 +1,12 @@
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
+import { defined_is } from '../../defined/is.mjs';
+import { string_is } from '../../string/is.mjs';
 export function object_property_remove(object, property_name) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        defined_is,
+        string_is
     ]);
     delete object[property_name];
     metadata([]);
