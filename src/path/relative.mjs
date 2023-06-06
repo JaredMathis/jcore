3203ca228bbc96f439eca551aa625a64ec8f0b67
JaredMathis <jared.mathis@native.consulting>
2023-06-06 14:58:01 -0400

    #66 noop

--- src/path/relative.mjs
+++ src/path/relative.mjs
@@ -2,10 +2,11 @@ import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import path from 'path';
+import { path_is } from './is.mjs';
 export function path_relative(from, to) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        path_is,
+        path_is
     ]);
     return path.relative(from, to);
     metadata([]);
