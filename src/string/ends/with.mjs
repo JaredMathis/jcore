58cedbeba20e1a5979f15dbc6a4a54133df5d2df
JaredMathis <jared.mathis@native.consulting>
2023-06-06 11:06:38 -0400

    #66 noop

--- src/string/ends/with.mjs
+++ src/string/ends/with.mjs
@@ -1,10 +1,11 @@
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
+import { string_is } from '../is.mjs';
 export function string_ends_with(input, suffix) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        string_is,
+        string_is
     ]);
     return input.endsWith(suffix);
     metadata([]);
