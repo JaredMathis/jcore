c7da9fe2d3a22541374694abfb93afc0ffdd544f
JaredMathis <jared.mathis@native.consulting>
2023-06-06 11:50:27 -0400

    #66 noop

--- src/list/find/generic.mjs
+++ src/list/find/generic.mjs
@@ -5,11 +5,12 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_single } from '../single.mjs';
 import { list_length_is_0 } from '../length/is/0.mjs';
 import { list_filter } from '../filter.mjs';
+import { boolean_is } from '../../boolean/is.mjs';
 export function list_find_generic(list, predicate, or_null) {
     arguments_assert(arguments, [
         list_is,
         function_is,
-        tautology
+        boolean_is
     ]);
     let filtered = list_filter(list, predicate);
     if (or_null) {
