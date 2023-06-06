cc970678d102e217db5960e023dcee60e0d68daa
JaredMathis <jared.mathis@native.consulting>
2023-06-06 11:17:17 -0400

    #66 noop

--- src/json/from.mjs
+++ src/json/from.mjs
@@ -1,8 +1,9 @@
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
+import { string_is } from '../string/is.mjs';
 export function json_from(json) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_is]);
     return JSON.parse(json);
     metadata([]);
 }
