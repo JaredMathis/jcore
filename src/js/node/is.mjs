1706063c87041037f1df20304aedbdc3f80bbcef
JaredMathis <jared.mathis@native.consulting>
2023-06-05 22:10:58 -0400

    #66 noop

--- src/js/node/is.mjs
+++ src/js/node/is.mjs
@@ -2,8 +2,9 @@ import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { object_property_exists } from '../../object/property/exists.mjs';
+import { defined_is } from '../../defined/is.mjs';
 export function js_node_is(candidate) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [defined_is]);
     if (!candidate) {
         return false;
     }
