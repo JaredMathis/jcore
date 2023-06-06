73edd62eecb40fed0a83b01f3a0b7b226a234ebc
JaredMathis <jared.mathis@native.consulting>
2023-06-06 11:19:01 -0400

    #66 noop

--- src/json/to/keys/exclude.mjs
+++ src/json/to/keys/exclude.mjs
@@ -1,11 +1,11 @@
+import { defined_is } from '../../../defined/is.mjs';
 import { list_is } from '../../../list/is.mjs';
-import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { object_merge_filtered } from '../../../object/merge/filtered.mjs';
 import { list_contains } from '../../../list/contains.mjs';
 export function json_to_keys_exclude(value, excluded) {
     arguments_assert(arguments, [
-        tautology,
+        defined_is,
         list_is
     ]);
     object_merge_filtered(value, key => !list_contains(excluded, key), {});
