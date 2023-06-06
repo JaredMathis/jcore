874ea162333a0167187cb1337574e89b008d4a92
JaredMathis <jared.mathis@native.consulting>
2023-06-06 16:54:37 -0400

    #65 noop

--- src/refactor/arguments/assert/add/no/check.mjs
+++ src/refactor/arguments/assert/add/no/check.mjs
@@ -38,7 +38,8 @@ export function refactor_arguments_assert_add_no_check(function_declaration, exc
         params_code
     ];
     let params_code2 = list_join(params2, ', ');
-    const statment_code = js_code_call_expression_with_args(function_name_get(arguments_assert), params_code2);
+    const call_expression_function_name = function_name_get(arguments_assert);
+    const statment_code = js_code_call_expression_with_args(call_expression_function_name, params_code2);
     let statement_new = js_parse_statement(statment_code);
     list_add_beginning(statements, statement_new);
 }
