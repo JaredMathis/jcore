a518a6f3a613790a761eecb56821d2bd84b80295
JaredMathis <jared.mathis@native.consulting>
2023-06-06 11:58:27 -0400

    #66 noop

--- src/list/index/of.mjs
+++ src/list/index/of.mjs
@@ -2,10 +2,12 @@ import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { assert } from '../../assert.mjs';
+import { list_is } from '../is.mjs';
+import { defined_is } from '../../defined/is.mjs';
 export function list_index_of(list, element) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        list_is,
+        defined_is
     ]);
     let result = list.indexOf(element);
     assert(result >= 0);
--- src/list/remove.mjs
+++ src/list/remove.mjs
@@ -1,5 +1,5 @@
+import { defined_is } from '../defined/is.mjs';
 import { list_is } from './is.mjs';
-import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { list_index_of } from './index/of.mjs';
@@ -7,7 +7,7 @@ import { list_remove_at } from './remove/at.mjs';
 export function list_remove(list, element) {
     arguments_assert(arguments, [
         list_is,
-        tautology
+        defined_is
     ]);
     let index = list_index_of(list, element);
     list_remove_at(list, index);
