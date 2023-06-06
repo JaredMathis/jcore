69c150c89783f4fa0482b2c31766dbbcd853e85d
JaredMathis <jared.mathis@native.consulting>
2023-06-06 14:50:57 -0400

    #66 noop

--- src/object/each/filter.mjs
+++ src/object/each/filter.mjs
@@ -1,12 +1,14 @@
+import { defined_is } from '../../defined/is.mjs';
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { list_add } from '../../list/add.mjs';
 import { object_each } from '../each.mjs';
+import { function_is } from '../../function/is.mjs';
 export function object_each_filter(object, predicate) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        defined_is,
+        function_is
     ]);
     let result = [];
     let lambda = (value, key) => {
