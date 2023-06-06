8a72484a69e05ca612fca9851b1d3dcd82ce4b2c
JaredMathis <jared.mathis@native.consulting>
2023-06-06 16:10:45 -0400

    #65 noop

--- src/list/any/async.mjs
+++ src/list/any/async.mjs
@@ -2,13 +2,13 @@ import { metadata } from '../../metadata.mjs';
 import { function_is } from '../../function/is.mjs';
 import { list_is } from '../is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function list_any_async(list, predicate) {
+export async function list_any_async(list, predicate) {
     arguments_assert(arguments, [
         list_is,
         function_is
     ]);
     for (let a of list) {
-        if (predicate(a)) {
+        if (await predicate(a)) {
             return true;
         }
     }
