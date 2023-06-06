281c332a3f96d3be14b9d5d6d06c58b6af93624b
JaredMathis <jared.mathis@native.consulting>
2023-06-05 22:22:06 -0400

    #66 noop

--- src/js/statement/if/metadata.mjs
+++ src/js/statement/if/metadata.mjs
@@ -4,10 +4,11 @@ import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { function_name_get } from '../../../function/name/get.mjs';
+import { function_is } from '../../../function/is.mjs';
 export function js_statement_if_metadata(statement, if_statement_metadata) {
     arguments_assert(arguments, [
         js_node_is,
-        tautology
+        function_is
     ]);
     let name_expected = function_name_get(metadata);
     js_node_call_expression_statement_if_name_equal(statement, name_expected, on_name_equal);
