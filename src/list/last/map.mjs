e4be82373bd709f5903bf823409aeb41136f5dc7
JaredMathis <jared.mathis@native.consulting>
2023-06-06 12:05:35 -0400

    #66 noop

--- src/list/last/map.mjs
+++ src/list/last/map.mjs
@@ -3,10 +3,11 @@ import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { list_last_index } from './index.mjs';
+import { function_is } from '../../function/is.mjs';
 export function list_last_map(list, mapper) {
     arguments_assert(arguments, [
         list_is,
-        tautology
+        function_is
     ]);
     list[list_last_index(list)] = mapper(list[list_last_index(list)]);
     metadata([]);
