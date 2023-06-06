e7106c3f83fe38fcc77bed5dca46f46905cff73a
JaredMathis <jared.mathis@native.consulting>
2023-06-06 15:35:55 -0400

    #65 function_auto function_tests_generate

--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -1,6 +1,7 @@
+import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_tests_generate() {
-    arguments_assert(arguments, []);
+export function function_tests_generate(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
     error('todo: function_tests_generate');
 }
