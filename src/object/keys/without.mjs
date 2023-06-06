ea1fd38fba0cd6464225c9b98d0e0bbc94b2459b
JaredMathis <jared.mathis@native.consulting>
2023-06-06 14:53:24 -0400

    #66 noop

--- src/object/keys/without.mjs
+++ src/object/keys/without.mjs
@@ -1,12 +1,14 @@
+import { defined_is } from '../../defined/is.mjs';
 import { equal_not } from '../../equal/not.mjs';
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_filter } from '../../list/filter.mjs';
 import { object_properties } from '../properties.mjs';
+import { list_is } from '../../list/is.mjs';
 export function object_keys_without(value, excluded) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        defined_is,
+        list_is
     ]);
     let all = object_properties(value);
     let filtered = list_filter(all, v => equal_not(v, excluded));
