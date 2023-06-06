d7e87d854266a269b8aa3bd5cbbebb09b7920f5b
JaredMathis <jared.mathis@native.consulting>
2023-06-06 11:27:41 -0400

    #66 noop

--- src/range.mjs
+++ src/range.mjs
@@ -1,14 +1,13 @@
 import { number_is } from './number/is.mjs';
 import { assert_arguments_count } from './assert/arguments/count.mjs';
 import { metadata } from './metadata.mjs';
-import { list_add } from './list/add.mjs';
 import { assert } from './assert.mjs';
 export function range(count) {
     assert_arguments_count(arguments, 1);
     assert(number_is(count));
     let result = [];
     for (let i = 0; i < count; i++) {
-        list_add(result, i);
+        result.push(i);
     }
     return result;
     metadata([]);
