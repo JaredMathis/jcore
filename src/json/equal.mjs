fe3ca3866060e76e45564fc5d13e4393dada21b8
JaredMathis <jared.mathis@native.consulting>
2023-06-06 11:16:58 -0400

    #66 noop

--- src/json/equal.mjs
+++ src/json/equal.mjs
@@ -1,11 +1,11 @@
-import { tautology } from '../tautology.mjs';
+import { defined_is } from '../defined/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { json_to } from './to.mjs';
 import { equal_by } from '../equal/by.mjs';
 export function json_equal(a, b) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        defined_is,
+        defined_is
     ]);
     return equal_by(json_to, a, b);
 }
