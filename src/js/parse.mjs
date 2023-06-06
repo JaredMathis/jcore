18aa4d2bda48d611e5bfc38aedbad2496eb4c5e3
JaredMathis <jared.mathis@native.consulting>
2023-06-05 22:12:46 -0400

    #66 noop

--- src/js/parse.mjs
+++ src/js/parse.mjs
@@ -3,8 +3,9 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { try_catch } from '../try/catch.mjs';
 import { Parser } from 'acorn';
+import { string_is } from '../string/is.mjs';
 export function js_parse(unparsed) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_is]);
     return try_catch(js_parse, lambda, unparsed);
     function lambda() {
         let parsed = Parser.parse(unparsed, {
