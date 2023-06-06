cc035961143fc21e0901162dd1b8cecad5fa8973
JaredMathis <jared.mathis@native.consulting>
2023-06-06 11:51:32 -0400

    #66 noop

--- src/list/first/remove.mjs
+++ src/list/first/remove.mjs
@@ -3,8 +3,9 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { list_remove_at } from '../remove/at.mjs';
 import { list_first_index } from './index.mjs';
+import { list_is } from '../is.mjs';
 export function list_first_remove(list) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [list_is]);
     list_remove_at(list, list_first_index());
     metadata([]);
 }
--- src/list/index/valid.mjs
+++ src/list/index/valid.mjs
@@ -2,10 +2,12 @@ import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { list_last_index } from '../last/index.mjs';
+import { list_is } from '../is.mjs';
+import { integer_is } from '../../integer/is.mjs';
 export function list_index_valid(list, index) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        list_is,
+        integer_is
     ]);
     return index >= 0 && index <= list_last_index(list);
     metadata([]);
