035245df064005a3ad03f82145a6f14f5ea61e9e
JaredMathis <jared.mathis@native.consulting>
2023-06-05 22:12:07 -0400

    #66 noop

--- src/js/nodes/count.mjs
+++ src/js/nodes/count.mjs
@@ -1,12 +1,12 @@
+import { function_is } from '../../function/is.mjs';
 import { js_node_is_program } from '../node/is/program.mjs';
-import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { js_visit_nodes_all } from '../visit/nodes/all.mjs';
 export function js_nodes_count(parsed, predicate) {
     arguments_assert(arguments, [
         js_node_is_program,
-        tautology
+        function_is
     ]);
     let count = 0;
     js_visit_nodes_all(parsed, v => {
