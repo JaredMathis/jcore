4259411bf0f581852c337790a2dbaf02f8906477
JaredMathis <jared.mathis@native.consulting>
2023-06-06 15:36:24 -0400

    #65 function_auto function_tests_generate

--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -1,7 +1,7 @@
+import { function_tests_count } from './count.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
-import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_tests_generate(function_name) {
+export async function function_tests_generate(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    error('todo: function_tests_generate');
+    let tests_count = await function_tests_count(function_name);
 }
