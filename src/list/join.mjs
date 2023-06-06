b939baf34b9cecd06764b7814ab5f19ed8c739cb
JaredMathis <jared.mathis@native.consulting>
2023-06-06 12:01:33 -0400

    #66 noop

--- src/list/join.mjs
+++ src/list/join.mjs
@@ -1,10 +1,12 @@
 import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
+import { list_is } from './is.mjs';
+import { string_is } from '../string/is.mjs';
 export function list_join(list, separator) {
     arguments_assert(arguments, [
-        tautology,
-        tautology
+        list_is,
+        string_is
     ]);
     return list.join(separator);
     metadata([]);
