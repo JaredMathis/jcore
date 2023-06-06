6a32cddf6f18b1715568ffa45bb7bbdba3cc7bda
JaredMathis <jared.mathis@native.consulting>
2023-06-06 12:07:24 -0400

    #66 noop

--- src/list/last/remove.mjs
+++ src/list/last/remove.mjs
@@ -1,8 +1,9 @@
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
+import { list_is } from '../is.mjs';
 export function list_last_remove(list) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [list_is]);
     return list.pop();
     metadata([]);
 }
--- src/list/last/remove/verify.mjs
+++ src/list/last/remove/verify.mjs
@@ -1,5 +1,5 @@
+import { list_is } from '../../is.mjs';
 import { defined_is } from '../../../defined/is.mjs';
-import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { equal } from '../../../equal.mjs';
@@ -7,7 +7,7 @@ import { assert } from '../../../assert.mjs';
 import { list_last_remove } from '../remove.mjs';
 export function list_last_remove_verify(list, last_expected) {
     arguments_assert(arguments, [
-        tautology,
+        list_is,
         defined_is
     ]);
     let last_actual = list_last_remove(list);
