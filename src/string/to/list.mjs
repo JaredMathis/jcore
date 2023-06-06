c42609507d9133e15c1c6498e536dfaa5a6a5b18
JaredMathis <jared.mathis@native.consulting>
2023-06-06 15:12:42 -0400

    #66 noop

--- src/string/to/list.mjs
+++ src/string/to/list.mjs
@@ -1,7 +1,7 @@
-import { tautology } from '../../tautology.mjs';
+import { string_is } from '../is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_split } from '../split.mjs';
 export function string_to_list(value) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [string_is]);
     return string_split(value, '');
 }
