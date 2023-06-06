463ff101ca11d51695fa9170c5cc74e545584e72
JaredMathis <jared.mathis@native.consulting>
2023-06-06 11:20:12 -0400

    #66 noop

--- src/list/add/beginning/multiple.mjs
+++ src/list/add/beginning/multiple.mjs
@@ -2,10 +2,11 @@ import { tautology } from '../../../tautology.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { list_add_beginning } from '../beginning.mjs';
+import { list_is } from '../../is.mjs';
 export function list_add_beginning_multiple(list, element_new_all) {
     arguments_assert(arguments, [
         tautology,
-        tautology
+        list_is
     ]);
     for (let e of element_new_all) {
         list_add_beginning(list, e);
