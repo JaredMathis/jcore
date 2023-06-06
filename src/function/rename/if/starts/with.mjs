8c51e053f9fe9d863d196b547b73f57912bd5b8b
JaredMathis <jared.mathis@native.consulting>
2023-06-06 16:29:59 -0400

    #65 noop

--- src/function/rename/if/starts/with.mjs
+++ src/function/rename/if/starts/with.mjs
@@ -18,16 +18,12 @@ export async function function_rename_if_starts_with(prefix_old, prefix_new) {
     let names = await function_name_all();
     let dictionary = {};
     for (let n_old of names) {
-        await lambda(n_old, prefix_old, prefix_new, dictionary);
-    }
-    async function lambda(n_old, prefix_old, prefix_new, dictionary) {
         if (string_starts_with(n_old, prefix_old)) {
             let n_new = string_prefix_replace(n_old, prefix_old, prefix_new);
             await function_rename_without_all_refactor(n_old, n_new);
             object_property_initialize(dictionary, n_old, n_new);
         }
     }
-    
     await file_js_all_identifier_all_rename(dictionary);
     await file_js_all_map(function_name_get(refactor_import_fix));
     metadata([]);
