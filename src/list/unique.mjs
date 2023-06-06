0294966d692d0aa4909125b1d63cea630aa1a318
JaredMathis <jared.mathis@native.consulting>
2023-06-06 16:28:01 -0400

    #65 function_auto list_unique

--- src/list/unique.mjs
+++ src/list/unique.mjs
@@ -1,9 +1,13 @@
+import { list_add_if_not_exists } from './add/if/not/exists.mjs';
 import { list_is } from './is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
-import { list_intersection } from './intersection.mjs';
 export function list_unique(list) {
     arguments_assert(arguments, [list_is]);
-    return list_intersection(list, list);
+    let result = [];
+    for (let element of list) {
+        list_add_if_not_exists(result, element);
+    }
+    return result;
     metadata([]);
 }
