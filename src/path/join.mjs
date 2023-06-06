24d3dd9f449d8dfaa0bd7e617b1f57c56e9e5b1e
JaredMathis <jared.mathis@native.consulting>
2023-06-06 14:57:16 -0400

    #66 noop

--- src/path/join.mjs
+++ src/path/join.mjs
@@ -2,8 +2,9 @@ import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import path from 'path';
+import { list_is } from '../list/is.mjs';
 export function path_join(paths) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [list_is]);
     return path.join(...paths);
     metadata([]);
 }
