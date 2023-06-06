8c2216ebd475b4daa8389b438c39b8880b6d733f
JaredMathis <jared.mathis@native.consulting>
2023-06-06 11:23:16 -0400

    #66 noop

--- src/list/add/multiple.mjs
+++ src/list/add/multiple.mjs
@@ -2,10 +2,11 @@ import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { list_add } from '../add.mjs';
+import { list_is } from '../is.mjs';
 export function list_add_multiple(list, list_other) {
     arguments_assert(arguments, [
         tautology,
-        tautology
+        list_is
     ]);
     for (let o of list_other) {
         list_add(list, o);
