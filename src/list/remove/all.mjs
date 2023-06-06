59a8645946fbb09bc8cbdff18fb4b0a559d5e143
JaredMathis <jared.mathis@native.consulting>
2023-06-06 12:10:59 -0400

    #66 noop

--- src/list/remove/all.mjs
+++ src/list/remove/all.mjs
@@ -6,7 +6,7 @@ import { list_remove } from '../remove.mjs';
 export function list_remove_all(list, values) {
     arguments_assert(arguments, [
         list_is,
-        tautology
+        list_is
     ]);
     for (let v of values) {
         list_remove(list, v);
