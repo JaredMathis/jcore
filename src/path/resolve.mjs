ccd5bb1459f3f5bee834851ec45e560c5990c7ed
JaredMathis <jared.mathis@native.consulting>
2023-06-06 14:59:05 -0400

    #66 noop

--- src/path/resolve.mjs
+++ src/path/resolve.mjs
@@ -2,8 +2,9 @@ import { tautology } from '../tautology.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import path from 'path';
+import { path_is } from './is.mjs';
 export function path_resolve(file_path) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [path_is]);
     return path.resolve(file_path);
     metadata([]);
 }
