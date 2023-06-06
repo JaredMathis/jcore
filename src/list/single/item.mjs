a3601a8f60f4e9cf0d18186bdfaf06a948820a58
JaredMathis <jared.mathis@native.consulting>
2023-06-06 12:13:10 -0400

    #66 noop

--- src/list/single/item.mjs
+++ src/list/single/item.mjs
@@ -1,8 +1,9 @@
 import { tautology } from '../../tautology.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
+import { defined_is } from '../../defined/is.mjs';
 export function list_single_item(item) {
-    arguments_assert(arguments, [tautology]);
+    arguments_assert(arguments, [defined_is]);
     return [item];
     metadata([]);
 }
