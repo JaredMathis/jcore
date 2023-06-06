846eb46a100e6fd0830fbad4d6cb284c59757411
JaredMathis <jared.mathis@native.consulting>
2023-06-06 16:24:58 -0400

    #65 function_tests_generate string_sub

---
+++ src/integer/is/test/values.mjs
@@ -0,0 +1,11 @@
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function integer_is_test_values() {
+    arguments_assert(arguments, []);
+    return [
+        -2,
+        -1,
+        0,
+        1,
+        2
+    ];
+}
