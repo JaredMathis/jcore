2af7d6d18237736cd3d798cf1d24f83f769b6164
JaredMathis <jared.mathis@native.consulting>
2023-06-07 15:58:59 -0400

    #65 function_auto untested

--- src/untested.mjs
+++ src/untested.mjs
@@ -1,17 +1,14 @@
+import { function_name_all_tests_not } from './function/name/all/tests/not.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 import { metadata } from './metadata.mjs';
-import { string_function_tests_name_is } from './string/function/tests/name/is.mjs';
-import { list_filter } from './list/filter.mjs';
 import { list_filter_async } from './list/filter/async.mjs';
 import { list_take } from './list/take.mjs';
-import { function_name_all } from './function/name/all.mjs';
 import { function_tests_count } from './function/tests/count.mjs';
 import { log } from './log.mjs';
 export async function untested() {
     arguments_assert(arguments, []);
     let maximum_untested_display = 10;
-    const all = await function_name_all();
-    let function_names = list_filter(all, a => !string_function_tests_name_is(a));
+    let function_names = await function_name_all_tests_not();
     let filtered = await list_filter_async(function_names, async f => await function_tests_count(f) === 0);
     let limited = list_take(filtered, maximum_untested_display);
     for (let function_name of limited) {
