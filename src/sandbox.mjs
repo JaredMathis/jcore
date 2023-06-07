90d2f33814334a380bd611b6321bd035f73e8583
JaredMathis <jared.mathis@native.consulting>
2023-06-07 16:59:30 -0400

    #65 function_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,8 +1,15 @@
-import { metadata_generated } from './metadata/generated.mjs';
-import { metadata } from './metadata.mjs';
+import { tests_name_next } from './tests/name/next.mjs';
+import { string_digit_is } from './string/digit/is.mjs';
 import { function_name_get } from './function/name/get.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
-export function sandbox() {
+import { range } from './range.mjs';
+import { tests_add } from './tests/add.mjs';
+export async function sandbox() {
     arguments_assert(arguments, []);
-    function_name_get(a);
+    let function_name = function_name_get(string_digit_is);
+    for (let i of range(10)) {
+        let s = `${ i }`;
+        let test_name = await tests_name_next(function_name);
+        tests_add();
+    }
 }
