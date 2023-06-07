47379d43aaaf5037fb99ad40fc8dc93db5f3f182
JaredMathis <jared.mathis@native.consulting>
2023-06-07 12:02:06 -0400

    #65 noop

--- src/function/auto/after.mjs
+++ src/function/auto/after.mjs
@@ -4,9 +4,11 @@ import { metadata } from '../../metadata.mjs';
 import { function_name_get } from '../name/get.mjs';
 import { function_auto_after_refactors } from './after/refactors.mjs';
 import { function_map } from '../map.mjs';
+import { list_map } from '../../list/map.mjs';
 export async function function_auto_after(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let refactors = function_auto_after_refactors();
+    let refactors_names = list_map(refactors, function_name_get)
     for (let r of refactors) {
         await function_map(function_name_get(r), function_name);
     }
