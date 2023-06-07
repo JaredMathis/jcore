7ce20e6315c15a0dc45d85d7b11f5ef76d765d06
JaredMathis <jared.mathis@native.consulting>
2023-06-07 15:59:18 -0400

    #65 function_auto function_tests_generate_if_starts_with

--- src/function/tests/generate/if/starts/with.mjs
+++ src/function/tests/generate/if/starts/with.mjs
@@ -1,13 +1,13 @@
+import { function_name_all_tests_not } from '../../../../name/all/tests/not.mjs';
 import { function_tests_generate_after } from '../../after.mjs';
 import { function_tests_generate_generic } from '../../generic.mjs';
 import { string_starts_with } from '../../../../../string/starts/with.mjs';
-import { function_name_all } from '../../../../name/all.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { string_identifier_sub_is } from '../../../../../string/identifier/sub/is.mjs';
 import { list_filter } from '../../../../../list/filter.mjs';
 export async function function_tests_generate_if_starts_with(prefix) {
     arguments_assert(arguments, [string_identifier_sub_is]);
-    let names = await function_name_all();
+    let names = await function_name_all_tests_not();
     let starts_with = list_filter(names, n => string_starts_with(n, prefix));
     for (let function_name of starts_with) {
         await function_tests_generate_generic(function_name);
