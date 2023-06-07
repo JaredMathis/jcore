c4d7c3d957a6635f373f43fb66f1f4c91114c92f
JaredMathis <jared.mathis@native.consulting>
2023-06-07 17:06:28 -0400

    #65 noop

--- src/string/digit/is.mjs
+++ src/string/digit/is.mjs
@@ -1,6 +1,7 @@
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_is } from '../is.mjs';
+import { string_length } from '../length.mjs';
 export function string_digit_is(c) {
     arguments_assert(arguments, [string_is]);
-    return c >= '0' && c <= '9';
+    return string_length(c) === 1 && c >= '0' && c <= '9';
 }
