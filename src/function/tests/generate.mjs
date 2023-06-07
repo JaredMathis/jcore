34902cba649b95bbe7be6dacbf093c3fbd8fc54b
JaredMathis <jared.mathis@native.consulting>
2023-06-07 11:26:12 -0400

    #65 noop

--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -73,6 +73,13 @@ export async function function_tests_generate(function_name) {
             }
             let args_code = list_map(args, json_to);
             let ce_function = js_code_call_expression_with_args(function_name, args_code);
+            let statement_assert;
+            if (has_error) {
+                
+                statement_assert = js_code_call_expression_statement_with_args_code(function_name_get(assert), ce_equal);
+                continue;
+            }
+            
             let identifier_expected = 'expected';
             let statement_expected = js_statement_assignment(identifier_expected, json_to(expected));
             let identifier_actual = 'actual';
@@ -81,8 +88,9 @@ export async function function_tests_generate(function_name) {
                 identifier_actual,
                 identifier_expected
             ]);
-            let statement_assert = js_code_call_expression_statement_with_args_code(function_name_get(assert), ce_equal);
+            statement_assert = js_code_call_expression_statement_with_args_code(function_name_get(assert), ce_equal);
             console.log({
+                has_error,
                 statement_expected,
                 statement_function,
                 statement_assert
