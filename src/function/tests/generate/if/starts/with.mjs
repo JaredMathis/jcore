339458473ecc201dac3e07b03aa76ee92738850b
JaredMathis <jared.mathis@native.consulting>
2023-06-07 15:48:56 -0400

    #65 function_auto function_tests_generate_if_starts_with

--- src/function/tests/generate/if/starts/with.mjs
+++ src/function/tests/generate/if/starts/with.mjs
@@ -1,6 +1,10 @@
-import { error } from '../../../../../error.mjs';
+import { string_starts_with } from '../../../../../string/starts/with.mjs';
+import { function_name_all } from '../../../../name/all.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export function function_tests_generate_if_starts_with(prefix) {
-    arguments_assert(arguments, []);
-    error('todo: function_tests_generate_if_starts_with');
+import { string_identifier_sub_is } from '../../../../../string/identifier/sub/is.mjs';
+export async function function_tests_generate_if_starts_with(prefix) {
+    arguments_assert(arguments, [string_identifier_sub_is]);
+    let names = await function_name_all();
+    if (string_starts_with(n_old, prefix_old)) {
+    }
 }
