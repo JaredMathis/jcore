72622d0ded0e44fe816d1bf0e1bab5885ecedabd
JaredMathis <jared.mathis@native.consulting>
2023-06-07 12:03:12 -0400

    #65 function_auto function_auto_after

---
+++ src/function/map/multiple.mjs
@@ -0,0 +1,6 @@
+import { function_map } from '../map.mjs';
+export async function function_map_multiple(refactors_names, function_name) {
+    for (let n of refactors_names) {
+        await function_map(n, function_name);
+    }
+}
