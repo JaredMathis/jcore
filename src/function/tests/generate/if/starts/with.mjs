bb3eaa5bdfa855f4ee23d1ca8412d9b09d64bc50
JaredMathis <jared.mathis@native.consulting>
2023-06-07 15:49:42 -0400

    #65 function_auto function_tests_generate_if_starts_with

--- src/function/tests/generate/if/starts/with.mjs
+++ src/function/tests/generate/if/starts/with.mjs
@@ -2,9 +2,9 @@ import { string_starts_with } from '../../../../../string/starts/with.mjs';
 import { function_name_all } from '../../../../name/all.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { string_identifier_sub_is } from '../../../../../string/identifier/sub/is.mjs';
+import { list_filter } from '../../../../../list/filter.mjs';
 export async function function_tests_generate_if_starts_with(prefix) {
     arguments_assert(arguments, [string_identifier_sub_is]);
     let names = await function_name_all();
-    if (string_starts_with(n_old, prefix_old)) {
-    }
+    let starts_with = list_filter(names, n => string_starts_with(n, prefix));
 }
