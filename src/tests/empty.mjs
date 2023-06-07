711e513a9599cab32b5ff5cf4bc6060c664572c9
JaredMathis <jared.mathis@native.consulting>
2023-06-07 16:22:04 -0400

    #65 function_rename tests_add tests_empty

--- src/tests/empty.mjs
+++ src/tests/empty.mjs
@@ -4,7 +4,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { tests_generate } from './generate.mjs';
 import { function_auto } from '../function/auto.mjs';
-export async function tests_add(function_name) {
+export async function tests_empty(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let test_name = await tests_name_next(function_name);
     await function_auto(test_name);
