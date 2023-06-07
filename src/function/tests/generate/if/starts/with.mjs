64a3cbb62727b296371d52d7a1c51bc45e719913
JaredMathis <jared.mathis@native.consulting>
2023-06-07 15:45:37 -0400

    #65 function_auto string_identifier_is

--- src/function/tests/generate/if/starts/with.mjs
+++ src/function/tests/generate/if/starts/with.mjs
@@ -1,6 +1,6 @@
 import { error } from '../../../../../error.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export function function_tests_generate_if_starts_with() {
+export function function_tests_generate_if_starts_with(prefix) {
     arguments_assert(arguments, []);
     error('todo: function_tests_generate_if_starts_with');
 }
