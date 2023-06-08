eaf96929f0b961d7a4bac64b54ada16fe3ac7340
JaredMathis <jared.mathis@native.consulting>
2023-06-08 08:48:36 -0400

    #65 function_auto function_rename_without_all_refactor

--- src/function/rename/without/all/refactor.mjs
+++ src/function/rename/without/all/refactor.mjs
@@ -1,5 +1,5 @@
+import { list_filter_function_names_starts_with_tests_prefix } from '../../../../list/filter/function/names/starts/with/tests/prefix.mjs';
 import { function_name_all } from '../../../name/all.mjs';
-import { function_tests_prefix_get } from '../../../tests/prefix/get.mjs';
 import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
@@ -8,8 +8,6 @@ import { function_name_to_file_path } from '../../../name/to/file/path.mjs';
 import { file_js_all_identifier_exists } from '../../../../file/js/all/identifier/exists.mjs';
 import { function_exists } from '../../../exists.mjs';
 import { assert } from '../../../../assert.mjs';
-import { list_filter } from '../../../../list/filter.mjs';
-import { string_starts_with } from '../../../../string/starts/with.mjs';
 export async function function_rename_without_all_refactor(function_name_old, function_name_new) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -24,10 +22,4 @@ export async function function_rename_without_all_refactor(function_name_old, fu
     let file_path_new = function_name_to_file_path(function_name_new);
     await file_rename(file_path_old, file_path_new);
     metadata([]);
-}
-
-function list_filter_function_names_starts_with_tests_prefix(function_names, function_name) {
-    let function_name_tests_prefix = function_tests_prefix_get(function_name);
-    let filtered = list_filter(function_names, a => string_starts_with(a, function_name_tests_prefix));
-    return filtered
 }
