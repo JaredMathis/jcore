90171ef95031e2b1de885a1bc6316bc74d489add
JaredMathis <jared.mathis@native.consulting>
2023-06-08 13:41:42 -0400

    #74 function_copy directory_read_recursive directory_read_directories_recursive

---
+++ src/directory/read/directories/recursive.mjs
@@ -0,0 +1,18 @@
+import { list_add } from '../../../list/add.mjs';
+import { noop } from '../../../noop.mjs';
+import { directory_read_recursive_generic } from '../recursive/generic.mjs';
+import { list_is } from '../../../list/is.mjs';
+import { path_is } from '../../../path/is.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export async function directory_read_directories_recursive(dir, path_list) {
+    arguments_assert(arguments, [
+        path_is,
+        list_is
+    ]);
+    return await directory_read_recursive_generic(dir, path_list, noop, on_file);
+    async function on_directory(path_list, file_path) {
+    }
+    async function on_file(path_list, file_path) {
+        list_add(path_list, file_path);
+    }
+}
