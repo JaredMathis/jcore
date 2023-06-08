7659d6a5e2af83c97a101949e0ab1c7c07af6b7c
JaredMathis <jared.mathis@native.consulting>
2023-06-08 13:57:22 -0400

    #74 function_auto directory_all_empty_delete

--- src/directory/all/empty/delete.mjs
+++ src/directory/all/empty/delete.mjs
@@ -4,7 +4,6 @@ import { list_length } from '../../../list/length.mjs';
 import { directory_read_paths } from '../../read/paths.mjs';
 import { directory_current_to_source } from '../../current/to/source.mjs';
 import { directory_read_directories } from '../../read/directories.mjs';
-import { error } from '../../../error.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function directory_all_empty_delete() {
     arguments_assert(arguments, []);
