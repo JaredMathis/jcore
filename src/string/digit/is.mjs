917d020825e15605a95ac3da197676208229fe42
JaredMathis <jared.mathis@native.consulting>
2023-06-08 09:09:34 -0400

    #65 function_auto string_digit_is

--- src/string/digit/is.mjs
+++ src/string/digit/is.mjs
@@ -1,7 +1,7 @@
+import { string_length_is_1 } from '../length/is/1.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_is } from '../is.mjs';
-import { string_length } from '../length.mjs';
 export function string_digit_is(c) {
     arguments_assert(arguments, [string_is]);
-    return string_length(c) === 1 && c >= '0' && c <= '9';
+    return string_length_is_1(c) && c >= '0' && c <= '9';
 }
---
+++ src/string/length/is/1.mjs
@@ -0,0 +1,4 @@
+import { string_length } from '../../length.mjs';
+export function string_length_is_1(c) {
+    return string_length(c) === 1;
+}
