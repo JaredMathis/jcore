4f24a166181c66544b3326ea5b5da2b368d87dfb
JaredMathis <jared.mathis@native.consulting>
2023-06-08 14:06:48 -0400

    #74 function_auto directory_all_empty_delete

--- src/directory/all/empty/delete.mjs
+++ src/directory/all/empty/delete.mjs
@@ -1,6 +1,4 @@
-import { path_parent } from '../../../path/parent.mjs';
-import { directory_empty_delete } from '../../empty/delete.mjs';
-import { directory_empty_is } from '../../empty/is.mjs';
+import { directory_delete_if_empty_recursive } from '../../delete/if/empty/recursive.mjs';
 import { directory_current_to_source } from '../../current/to/source.mjs';
 import { directory_read_directories } from '../../read/directories.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -8,9 +6,6 @@ export async function directory_all_empty_delete() {
     arguments_assert(arguments, []);
     let result = await directory_read_directories(directory_current_to_source());
     for (let directory_path of result) {
-        if (await directory_empty_is(directory_path)) {
-            await directory_empty_delete(directory_path);
-            let parent = path_parent();
-        }
+        await directory_delete_if_empty_recursive(directory_path);
     }
 }
