0e689412182ae7589738f0aac0708e61c26aa4c1
JaredMathis <jared.mathis@native.consulting>
2023-06-08 13:21:52 -0400

    #74 function_auto directory_all_empty_delete

---
+++ src/directory/all/empty/delete.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function directory_all_empty_delete() {
+    arguments_assert(arguments, []);
+    error('todo: directory_all_empty_delete');
+}
