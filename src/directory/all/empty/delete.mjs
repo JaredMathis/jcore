08c0fec15fd37c32024eba989132601acbc7cff0
JaredMathis <jared.mathis@native.consulting>
2023-06-08 13:56:48 -0400

    #74 noop

--- src/directory/all/empty/delete.mjs
+++ src/directory/all/empty/delete.mjs
@@ -8,7 +8,6 @@ import { error } from '../../../error.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function directory_all_empty_delete() {
     arguments_assert(arguments, []);
-    error('todo: directory_all_empty_delete');
     let result = await directory_read_directories(directory_current_to_source());
     for (let directory_path of result) {
         let file_paths = await directory_read_paths(directory_path);
