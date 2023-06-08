37d8c81acd7285ffa7a00988b430276025f635f4
JaredMathis <jared.mathis@native.consulting>
2023-06-08 09:37:46 -0400

    #65 function_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,5 +1,4 @@
-import { string_identifier_sub_is } from './string/identifier/sub/is.mjs';
-import { string_identifier_is } from './string/identifier/is.mjs';
+import { string_letter_digit_or_underscore_is } from './string/letter/digit/or/underscore/is.mjs';
 import { function_tests_generate_after } from './function/tests/generate/after.mjs';
 import { function_tests_generate_generic_each } from './function/tests/generate/generic/each.mjs';
 import { tests_name_next } from './tests/name/next.mjs';
@@ -8,10 +7,7 @@ import { arguments_assert } from './arguments/assert.mjs';
 import { string_split } from './string/split.mjs';
 export async function sandbox() {
     arguments_assert(arguments, []);
-    let functions = [
-        string_identifier_is,
-        string_identifier_sub_is
-    ];
+    let functions = [string_letter_digit_or_underscore_is];
     for (let f of functions) {
         let function_name = function_name_get(f);
         let inputs_string = `_,me,me_myself,me_,_me,1,1me,me1,me_1,my_self_and_i`;
