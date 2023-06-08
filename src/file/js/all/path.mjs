688e8c902c6e1d6f8855704b94b7f1e4b8d9df35
JaredMathis <jared.mathis@native.consulting>
2023-06-08 13:47:51 -0400

    #74 function_auto file_js_all_path

--- src/file/js/all/path.mjs
+++ src/file/js/all/path.mjs
@@ -7,8 +7,7 @@ import { string_ends_with } from '../../../string/ends/with.mjs';
 import { function_extension } from '../../../function/extension.mjs';
 export async function file_js_all_path() {
     arguments_assert(arguments, []);
-    const current_to_source = directory_current_to_source();
-    let result = await directory_read(current_to_source);
+    let result = await directory_read(directory_current_to_source());
     let filtered = list_filter(result, a => string_ends_with(a, function_extension()));
     return filtered;
     metadata([]);
