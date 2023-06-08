09a805e570054eb7f232218bd4cb1c1c425fe0ab
JaredMathis <jared.mathis@native.consulting>
2023-06-08 14:08:12 -0400

    #74 function_auto directory_all_empty_count

---
+++ src/directory/all/empty/count.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function directory_all_empty_count() {
+    arguments_assert(arguments, []);
+    error('todo: directory_all_empty_count');
+}
