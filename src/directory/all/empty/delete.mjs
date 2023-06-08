4a818f9a8fe87f3e1a9e716f18d0320239f998ea
JaredMathis <jared.mathis@native.consulting>
2023-06-08 14:10:21 -0400

    #74 function_auto directory_all_empty_delete

--- src/directory/all/empty/delete.mjs
+++ src/directory/all/empty/delete.mjs
@@ -1,8 +1,11 @@
+import { directory_all_empty_count } from './count.mjs';
+import { equal } from '../../../equal.mjs';
 import { directory_delete_if_empty_recursive } from '../../delete/if/empty/recursive.mjs';
 import { directory_current_to_source } from '../../current/to/source.mjs';
 import { directory_read_directories } from '../../read/directories.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { path_exists } from '../../../path/exists.mjs';
+import { assert } from '../../../assert.mjs';
 export async function directory_all_empty_delete() {
     arguments_assert(arguments, []);
     let result = await directory_read_directories(directory_current_to_source());
@@ -12,4 +15,5 @@ export async function directory_all_empty_delete() {
         }
         await directory_delete_if_empty_recursive(directory_path);
     }
+    assert(equal(directory_all_empty_count(), 0));
 }
