c56bf207b913d1b5462c74692b28574f3a68923d
JaredMathis <jared.mathis@native.consulting>
2023-06-08 09:28:29 -0400

    #65 function_auto function_rename_if_starts_with

--- src/function/rename/if/starts/with.mjs
+++ src/function/rename/if/starts/with.mjs
@@ -9,6 +9,7 @@ import { string_prefix_replace } from '../../../../string/prefix/replace.mjs';
 import { string_starts_with } from '../../../../string/starts/with.mjs';
 import { function_name_all } from '../../../name/all.mjs';
 import { list_filter } from '../../../../list/filter.mjs';
+import { object_merge } from '../../../../object/merge.mjs';
 export async function function_rename_if_starts_with(prefix_old, prefix_new) {
     arguments_assert(arguments, [
         string_identifier_sub_is,
@@ -21,6 +22,7 @@ export async function function_rename_if_starts_with(prefix_old, prefix_new) {
     async function key_to_value(n_old) {
         let n_new = string_prefix_replace(n_old, prefix_old, prefix_new);
         let tests_renames = await function_rename_without_all_refactor(n_old, n_new);
+        object_merge(tests_renames, dictionary_tests);
         return n_new;
     }
     let file_paths_changed = await file_js_all_identifier_multiple_rename(dictionary);
