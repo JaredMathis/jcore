2648e59a557195dffc0e8e51df71e486a5f66ff2
JaredMathis <jared.mathis@native.consulting>
2023-06-08 13:40:21 -0400

    #74 function_auto directory_read_recursive_generic

--- src/directory/read/recursive/generic.mjs
+++ src/directory/read/recursive/generic.mjs
@@ -1,5 +1,14 @@
+import fs from 'fs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { path_join } from '../../../path/join.mjs';
 export async function directory_read_recursive_generic(dir, path_list, on_directory, on_file) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     const files = await fs.promises.readdir(dir);
     for (let file of files) {
         const file_path = path_join([
