ca69b24cab08fcf942cd7dbc86bfa8b3be141926
JaredMathis <jared.mathis@native.consulting>
2023-06-08 13:55:30 -0400

    #74 function_auto directory_all_empty_delete

--- src/directory/all/empty/delete.mjs
+++ src/directory/all/empty/delete.mjs
@@ -1,3 +1,5 @@
+import { list_length } from '../../../list/length.mjs';
+import { directory_read_paths } from '../../read/paths.mjs';
 import { directory_current_to_source } from '../../current/to/source.mjs';
 import { directory_read_directories } from '../../read/directories.mjs';
 import { error } from '../../../error.mjs';
@@ -6,5 +8,8 @@ export async function directory_all_empty_delete() {
     arguments_assert(arguments, []);
     error('todo: directory_all_empty_delete');
     let result = await directory_read_directories(directory_current_to_source());
-    ;
+    for (let directory_path of result) {
+        let file_paths = await directory_read_paths(directory_path);
+        let count = list_length(file_paths);
+    }
 }
