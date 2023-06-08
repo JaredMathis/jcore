dd3148c576c887494fba9f1e0815c6dc2ef72395
JaredMathis <jared.mathis@native.consulting>
2023-06-08 08:53:08 -0400

    #65 function_auto function_rename_without_all_refactor

--- src/function/rename/without/all/refactor.mjs
+++ src/function/rename/without/all/refactor.mjs
@@ -1,10 +1,9 @@
+import { function_rename_file_path } from '../../file/path.mjs';
 import { list_filter_function_names_starts_with_tests_prefix } from '../../../../list/filter/function/names/starts/with/tests/prefix.mjs';
 import { function_name_all } from '../../../name/all.mjs';
 import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
-import { file_rename } from '../../../../file/rename.mjs';
-import { function_name_to_file_path } from '../../../name/to/file/path.mjs';
 import { file_js_all_identifier_exists } from '../../../../file/js/all/identifier/exists.mjs';
 import { function_exists } from '../../../exists.mjs';
 import { assert } from '../../../../assert.mjs';
@@ -16,21 +15,12 @@ export async function function_rename_without_all_refactor(function_name_old, fu
     assert(await function_exists(function_name_old));
     assert(!await function_exists(function_name_new));
     assert(!await file_js_all_identifier_exists(function_name_new));
-
-
     let all = await function_name_all();
     let tests_old = list_filter_function_names_starts_with_tests_prefix(all, function_name_old);
     let tests_new = list_filter_function_names_starts_with_tests_prefix(all, function_name_new);
     for (let t of tests_new) {
         assert(!await function_exists(t));
     }
-
     await function_rename_file_path(function_name_old, function_name_new);
     metadata([]);
-}
-
-async function function_rename_file_path(function_name_old, function_name_new) {
-    let file_path_old = function_name_to_file_path(function_name_old);
-    let file_path_new = function_name_to_file_path(function_name_new);
-    await file_rename(file_path_old, file_path_new);
 }
