9d5abf9f16b0013910ea9284a31e662103be329d
JaredMathis <jared.mathis@native.consulting>
2023-06-08 08:01:18 -0400

    #65 function_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,16 +1,14 @@
-import { string_function_tests_sub } from './string/function/tests/sub.mjs';
+import { string_identifier_is } from './string/identifier/is.mjs';
 import { function_tests_generate_after } from './function/tests/generate/after.mjs';
 import { function_tests_generate_generic_each } from './function/tests/generate/generic/each.mjs';
 import { tests_name_next } from './tests/name/next.mjs';
 import { function_name_get } from './function/name/get.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
-import { string_function_tests_name_is } from './string/function/tests/name/is.mjs';
 import { string_split } from './string/split.mjs';
 export async function sandbox() {
     arguments_assert(arguments, []);
-    let function_name = function_name_get(string_function_tests_name_is);
-    let s = string_function_tests_sub();
-    let inputs_string = `_,a,${ s },a${ s },a${ s }1,${ s }1`;
+    let function_name = function_name_get(string_identifier_is);
+    let inputs_string = `_,me,me_myself,me_,_me,1,1me,me1,me_1,my_self_and_i`;
     let inputs = string_split(inputs_string, ',');
     for (let i of inputs) {
         let test_name = await tests_name_next(function_name);
