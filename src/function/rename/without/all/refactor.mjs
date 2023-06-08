e1cda2146020bc10ea7c2d60e48cbd169f51949e
JaredMathis <jared.mathis@native.consulting>
2023-06-08 08:45:06 -0400

    #65 function_auto function_rename_without_all_refactor

--- src/function/rename/without/all/refactor.mjs
+++ src/function/rename/without/all/refactor.mjs
@@ -1,3 +1,4 @@
+import { function_name_all } from '../../../name/all.mjs';
 import { function_tests_prefix_get } from '../../../tests/prefix/get.mjs';
 import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
@@ -7,12 +8,16 @@ import { function_name_to_file_path } from '../../../name/to/file/path.mjs';
 import { file_js_all_identifier_exists } from '../../../../file/js/all/identifier/exists.mjs';
 import { function_exists } from '../../../exists.mjs';
 import { assert } from '../../../../assert.mjs';
+import { list_filter } from '../../../../list/filter.mjs';
+import { string_starts_with } from '../../../../string/starts/with.mjs';
 export async function function_rename_without_all_refactor(function_name_old, function_name_new) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is
     ]);
     let function_name_old_tests_prefix = function_tests_prefix_get(function_name_old);
+    let all = await function_name_all();
+    let tests_old = list_filter(all, a => string_starts_with(a, function_name_old_tests_prefix));
     assert(await function_exists(function_name_old));
     assert(!await function_exists(function_name_new));
     assert(!await file_js_all_identifier_exists(function_name_new));
