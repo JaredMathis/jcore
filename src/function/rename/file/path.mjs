9ca91fe93aada3c2e99a872ccc731289e6c0865d
JaredMathis <jared.mathis@native.consulting>
2023-06-08 08:52:46 -0400

    #65 function_auto function_rename_file_path

---
+++ src/function/rename/file/path.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function function_rename_file_path() {
+    arguments_assert(arguments, []);
+    error('todo: function_rename_file_path');
+}
