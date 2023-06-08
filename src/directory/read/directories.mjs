636ae732c63819927c9d05b2c2f3a355bcfb3bc1
JaredMathis <jared.mathis@native.consulting>
2023-06-08 13:43:41 -0400

    #74 function_auto directory_read_directories

--- src/directory/read/directories.mjs
+++ src/directory/read/directories.mjs
@@ -1,9 +1,9 @@
+import { directory_read_directories_recursive } from './directories/recursive.mjs';
 import { metadata } from '../../metadata.mjs';
-import { directory_read_recursive } from './recursive.mjs';
 import { path_is } from '../../path/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function directory_read_directories(dir) {
     arguments_assert(arguments, [path_is]);
-    return await directory_read_recursive(dir, []);
+    return await directory_read_directories_recursive(dir, []);
     metadata([]);
 }
