0707488b003483f244e12baaff52ccc4dcdbe4e3
JaredMathis <jared.mathis@native.consulting>
2023-06-08 08:59:46 -0400

    #65 function_auto function_rename_without_all_refactor

--- src/function/rename/without/all/refactor.mjs
+++ src/function/rename/without/all/refactor.mjs
@@ -11,6 +11,7 @@ import { function_exists } from '../../../exists.mjs';
 import { assert } from '../../../../assert.mjs';
 import { list_map } from '../../../../list/map.mjs';
 import { string_prefix_replace } from '../../../../string/prefix/replace.mjs';
+import { object_property_get } from '../../../../object/property/get.mjs';
 export async function function_rename_without_all_refactor(function_name_old, function_name_new) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -23,14 +24,15 @@ export async function function_rename_without_all_refactor(function_name_old, fu
     let tests_prefix_old = function_tests_prefix_get(function_name_old);
     let tests_old = list_filter(all, a => string_starts_with(a, tests_prefix_old));
     let tests_prefix_new = function_tests_prefix_get(function_name_new);
-    let tests_new = list_map(tests_old, t => {
+    let tests_renames = list_map(tests_old, t => {
         return {
             from: t,
             to: string_prefix_replace(t, tests_prefix_old, tests_prefix_new)
         };
     });
-    for (let t of tests_new) {
-        assert(!await function_exists(t));
+    for (let r of tests_renames) {
+        let to = object_property_get(r, 'to');
+        assert(!await function_exists(to));
     }
     await function_rename_file_path(function_name_old, function_name_new);
     metadata([]);
