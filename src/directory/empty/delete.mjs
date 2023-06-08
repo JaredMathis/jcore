116135978e70b9bea621d694b3cd3240eeb8e532
JaredMathis <jared.mathis@native.consulting>
2023-06-08 14:03:24 -0400

    #74 function_auto directory_empty_delete

--- src/directory/empty/delete.mjs
+++ src/directory/empty/delete.mjs
@@ -4,9 +4,10 @@ import { assert } from '../../assert.mjs';
 import { path_is } from '../../path/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import fs from 'fs';
+import { path_exists } from '../../path/exists.mjs';
 export async function directory_empty_delete(directory_path) {
     arguments_assert(arguments, [path_is]);
-    assert(await directory_exists(directory_path));
+    assert(await path_exists(directory_path));
     assert(await directory_empty_is(directory_path));
     await fs.promises.rmdir(directory_path);
     metadata([]);
