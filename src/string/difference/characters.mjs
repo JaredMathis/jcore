eb64164086185ad59d1d9509c0805550ec5f7322
JaredMathis <jared.mathis@native.consulting>
2023-06-08 17:33:53 -0400

    #79 noop

--- src/string/difference/characters.mjs
+++ src/string/difference/characters.mjs
@@ -1,10 +1,11 @@
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_is } from '../is.mjs';
+import {Diff} from 'diff'
 export function string_difference_characters(string_old, string_new) {
     arguments_assert(arguments, [
         string_is,
         string_is
     ]);
-    error('todo: string_difference_characters');
+    Diff.diffChars(string_old, string_new)
 }
