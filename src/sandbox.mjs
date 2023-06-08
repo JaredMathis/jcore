135837d652d54fbdfdb9a85a0cd5092cb95eb508
JaredMathis <jared.mathis@native.consulting>
2023-06-08 07:54:51 -0400

    #65 function_auto sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,13 +1,16 @@
+import { string_function_tests_sub } from './string/function/tests/sub.mjs';
 import { function_tests_generate_after } from './function/tests/generate/after.mjs';
 import { function_tests_generate_generic_each } from './function/tests/generate/generic/each.mjs';
 import { tests_name_next } from './tests/name/next.mjs';
-import { string_digit_is } from './string/digit/is.mjs';
 import { function_name_get } from './function/name/get.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 import { range } from './range.mjs';
+import { string_function_tests_name_is } from './string/function/tests/name/is.mjs';
 export async function sandbox() {
     arguments_assert(arguments, []);
-    let function_name = function_name_get(string_digit_is);
+    let function_name = function_name_get(string_function_tests_name_is);
+    let s = string_function_tests_sub();
+    let inputs = `_,${ s }`;
     for (let i of range(12)) {
         let adjusted = i - 1;
         let s = `${ adjusted }`;
