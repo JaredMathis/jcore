28f7be7622233669f688fbe327f5cce4633e60d8
JaredMathis <jared.mathis@native.consulting>
2023-06-08 14:04:25 -0400

    #74 function_auto directory_all_empty_delete

--- src/directory/all/empty/delete.mjs
+++ src/directory/all/empty/delete.mjs
@@ -1,20 +1,14 @@
+import { directory_empty_delete } from '../../empty/delete.mjs';
 import { directory_empty_is } from '../../empty/is.mjs';
-import { file_delete } from '../../../file/delete.mjs';
 import { directory_current_to_source } from '../../current/to/source.mjs';
 import { directory_read_directories } from '../../read/directories.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function directory_all_empty_delete() {
     arguments_assert(arguments, []);
-    let changed;
-    do {
-        changed = false;
-        let result = await directory_read_directories(directory_current_to_source());
-        for (let directory_path of result) {
-            const is_empty = await directory_empty_is(directory_path);
-            if (is_empty) {
-                await file_delete(directory_path);
-                changed = true;
-            }
+    let result = await directory_read_directories(directory_current_to_source());
+    for (let directory_path of result) {
+        if (await directory_empty_is(directory_path)) {
+            await directory_empty_delete(directory_path);
         }
-    } while (changed);
+    }
 }
