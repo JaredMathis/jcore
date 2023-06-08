aef041c6da4446574b61a1ace2779ac210e64428
JaredMathis <jared.mathis@native.consulting>
2023-06-08 09:01:40 -0400

    #65 noop

--- src/function/rename.mjs
+++ src/function/rename.mjs
@@ -4,6 +4,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { file_js_all_identifier_rename } from '../file/js/all/identifier/rename.mjs';
 import { function_rename_without_all_refactor } from './rename/without/all/refactor.mjs';
+import { tests_generate } from '../tests/generate.mjs';
 export async function function_rename(function_name_old, function_name_new) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -12,5 +13,6 @@ export async function function_rename(function_name_old, function_name_new) {
     await function_rename_without_all_refactor(function_name_old, function_name_new);
     let file_paths_changed = await file_js_all_identifier_rename(function_name_old, function_name_new);
     await file_js_multiple_import_fix(file_paths_changed);
+    await tests_generate();
     metadata([]);
 }
