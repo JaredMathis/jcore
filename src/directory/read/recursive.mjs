6a1badec0c72c78804fcd5d77f1c3f4cd45ef92d
JaredMathis <jared.mathis@native.consulting>
2023-06-08 13:41:04 -0400

    #74 noop

--- src/directory/read/recursive.mjs
+++ src/directory/read/recursive.mjs
@@ -5,12 +5,13 @@ import fs from 'fs';
 import path from 'path';
 import { path_is } from '../../path/is.mjs';
 import { list_is } from '../../list/is.mjs';
+import { noop } from '../../noop.mjs';
 export async function directory_read_recursive(dir, path_list) {
     arguments_assert(arguments, [
         path_is,
         list_is
     ]);
-    return await directory_read_recursive_generic(dir, path_list, on_directory, on_file);
+    return await directory_read_recursive_generic(dir, path_list, noop, on_file);
     async function on_directory(path_list, file_path) {
     }
     async function on_file(path_list, file_path) {
