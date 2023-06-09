0431588d903d3fead921d55437f1eb92c0480753
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:57:09 -0400

    #81 function_auto sandbox2

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -1,4 +1,4 @@
-import { implies } from './implies.mjs';
+import { list_any } from './list/any.mjs';
 import { string_prefix_without } from './string/prefix/without.mjs';
 import { list_map } from './list/map.mjs';
 import { directory_read_directories } from './directory/read/directories.mjs';
@@ -8,7 +8,6 @@ import { version_commit_files_all } from './version/commit/files/all.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 import { directory_current } from './directory/current.mjs';
 import { list_filter } from './list/filter.mjs';
-import { list_all } from './list/all.mjs';
 import { string_starts_with } from './string/starts/with.mjs';
 import { equal } from './equal.mjs';
 export async function sandbox2() {
@@ -19,6 +18,6 @@ export async function sandbox2() {
     let repository_files_path = version_path_files_get(repository_name);
     let paths = await directory_read_directories(repository_files_path);
     let mapped = list_map(paths, p => directory_current() + string_prefix_without(p, repository_files_path));
-    let filtered = list_filter(mapped, m1 => list_all(mapped, m2 => implies(string_starts_with(m2, m1), equal(m2, m1))));
-    console.log(filtered);
+    list_filter(mapped, m1 => !list_any(mapped, m2 => string_starts_with(m1, m2) && equal(m1, m2)));
+    console.log(mapped);
 }
