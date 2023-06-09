414c9170b820d3ac4427b677303dc1fbe8a631f2
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:49:05 -0400

    #81 noop

--- src/file/is.mjs
+++ src/file/is.mjs
@@ -2,7 +2,7 @@ import { path_is } from '../path/is.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { directory_is } from '../directory/is.mjs';
-export function file_is(file_path) {
+export async function file_is(file_path) {
     arguments_assert(arguments, [path_is]);
-    return !directory_is(file_path)
+    return !await directory_is(file_path)
 }
