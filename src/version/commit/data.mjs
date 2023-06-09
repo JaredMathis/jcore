9e54776bf7c9efc3fe03cbe701066dca4e66a09f
JaredMathis <jared.mathis@native.consulting>
2023-06-09 11:10:49 -0400

    #79 function_auto version_commit_data

--- src/version/commit/data.mjs
+++ src/version/commit/data.mjs
@@ -1,11 +1,12 @@
 import { list_to } from '../../list/to.mjs';
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { function_name_get } from '../../function/name/get.mjs';
+import { defined_is } from '../../defined/is.mjs';
+import { function_is } from '../../function/is.mjs';
 export function version_commit_data(fn, args) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        function_is,
+        defined_is
     ]);
     return {
         command: function_name_get(fn),
