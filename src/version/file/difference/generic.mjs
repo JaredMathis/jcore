0673aae6661e181a7480f1be336b02a8f03c4a58
JaredMathis <jared.mathis@native.consulting>
2023-06-09 18:17:48 -0400

    #81 function_auto version_file_difference_generic

--- src/version/file/difference/generic.mjs
+++ src/version/file/difference/generic.mjs
@@ -1,3 +1,4 @@
+import { list_max_or_0 } from '../../../list/max/or/0.mjs';
 import { version_property_hunks } from '../../property/hunks.mjs';
 import { log } from '../../../log.mjs';
 import { version_path_file_directory } from '../../path/file/directory.mjs';
@@ -13,10 +14,10 @@ import { string_prefix_without } from '../../../string/prefix/without.mjs';
 import { string_suffix_without } from '../../../string/suffix/without.mjs';
 import { file_extension_json } from '../../../file/extension/json.mjs';
 import { integer_parse } from '../../../integer/parse.mjs';
-import { list_max_generic } from '../../../list/max/generic.mjs';
-import { object_property_get } from '../../../object/property/get.mjs';
 import { function_is } from '../../../function/is.mjs';
 import { directory_separator } from '../../../directory/separator.mjs';
+import { add_1 } from '../../../add/1.mjs';
+import { version_path_file_get } from '../../path/file/get.mjs';
 export async function version_file_difference_generic(repository_name, file_path, contents_new_get) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -33,13 +34,11 @@ export async function version_file_difference_generic(repository_name, file_path
         let without_suffix = string_suffix_without(without_prefix, file_extension_json());
         console.log({ without_suffix });
         let version = integer_parse(without_suffix);
-        return {
-            file_path,
-            version
-        };
+        return version;
     });
-    let max = list_max_generic(mapped, m => object_property_get(m, 'version'));
-    let version_path = object_property_get(max, 'file_path');
+    let max = list_max_or_0(mapped);
+    let version = add_1(max, 1);
+    let version_path = version_path_file_get(repository_name, file_path, version);
     let property_version_path = version_property_path();
     let property_hunks = version_property_hunks();
     return {
