ed27ab7fc6d6d261a6b084b27837714970ad9d57
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:08:57 -0400

    #80 noop

--- src/version/commit/files/all.mjs
+++ src/version/commit/files/all.mjs
@@ -3,9 +3,11 @@ import { version_commit_data } from '../data.mjs';
 import { version_commit_files } from '../files.mjs';
 import { directory_read } from '../../../directory/read.mjs';
 import { directory_current } from '../../../directory/current.mjs';
+import { git_ignore_lines } from '../../../git/ignore/lines.mjs';
 export async function version_commit_files_all(repository_name) {
     let directory_path = directory_current();
     let file_paths = await directory_read(directory_path);
+    let gil = await git_ignore_lines();
     let filtered
     await version_commit_files(repository_name, file_paths, version_commit_data(todo, arguments));
 }
