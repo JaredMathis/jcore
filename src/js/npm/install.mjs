73ae2a06c45594348777ae10ee32090d5603ec65
JaredMathis <jared.mathis@native.consulting>
2023-06-09 08:48:39 -0400

    #79 function_auto js_npm_install

--- src/js/npm/install.mjs
+++ src/js/npm/install.mjs
@@ -1,7 +1,7 @@
 import { command_line } from '../../command/line.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_is } from '../../string/is.mjs';
-export function js_npm_install(npm_package_name) {
+export async function js_npm_install(npm_package_name) {
     arguments_assert(arguments, [string_is]);
-    command_line;
+    await command_line(`npm i ${ npm_package_name }@latest`);
 }
