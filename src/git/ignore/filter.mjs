0bdaf5b3edd33575230af33b70ead0e372770c73
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:22:31 -0400

    #80 function_auto git_ignore_filter

--- src/git/ignore/filter.mjs
+++ src/git/ignore/filter.mjs
@@ -4,7 +4,6 @@ import { list_single_item } from '../../list/single/item.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_starts_with } from '../../string/starts/with.mjs';
-import { list_any } from '../../list/any.mjs';
 import { directory_separator } from '../../directory/separator.mjs';
 import { list_filter } from '../../list/filter.mjs';
 import { git_ignore_lines } from './lines.mjs';
@@ -17,7 +16,7 @@ export async function git_ignore_filter(file_paths) {
     ]);
     let filtered = list_filter(file_paths, f => {
         let with_slash = f + directory_separator();
-        return list_any(filter, g => string_starts_with(f, with_slash) || equal(f, g));
+        return list_all(filter, g => !string_starts_with(f, with_slash) && !equal(f, g));
     });
     return filtered;
 }
