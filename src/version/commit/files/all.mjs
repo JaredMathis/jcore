f2e7c5ead584ac0268d7c0e250021eb50efc5139
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:12:47 -0400

    #80 function_auto version_commit_files_all

--- src/version/commit/files/all.mjs
+++ src/version/commit/files/all.mjs
@@ -1,7 +1,6 @@
 import { string_starts_with } from '../../../string/starts/with.mjs';
 import { list_any } from '../../../list/any.mjs';
 import { log } from '../../../log.mjs';
-import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { todo } from '../../../todo.mjs';
 import { version_commit_data } from '../data.mjs';
@@ -10,8 +9,9 @@ import { directory_read } from '../../../directory/read.mjs';
 import { directory_current } from '../../../directory/current.mjs';
 import { git_ignore_lines } from '../../../git/ignore/lines.mjs';
 import { list_filter } from '../../../list/filter.mjs';
+import { string_identifier_is } from '../../../string/identifier/is.mjs';
 export async function version_commit_files_all(repository_name) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [string_identifier_is]);
     let directory_path = directory_current();
     let file_paths = await directory_read(directory_path);
     let gil = await git_ignore_lines();
