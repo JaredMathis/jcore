476d3b1ab4d7089961d34b01d909cac874b951d5
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:48:33 -0400

    #81 function_auto file_exists

--- src/file/exists.mjs
+++ src/file/exists.mjs
@@ -1,9 +1,15 @@
+import { file_is } from './is.mjs';
 import { path_is } from '../path/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { path_exists } from '../path/exists.mjs';
+import { assert } from '../assert.mjs';
 export async function file_exists(file_path) {
     arguments_assert(arguments, [path_is]);
-    return await path_exists(file_path);
+    if (!await path_exists(file_path)) {
+        return false;
+    }
+    assert(await file_is(file_path));
+    return true;
     metadata([]);
 }
--- src/file/is.mjs
+++ src/file/is.mjs
@@ -1,7 +1,8 @@
 import { path_is } from '../path/is.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { directory_is } from '../directory/is.mjs';
 export function file_is(file_path) {
     arguments_assert(arguments, [path_is]);
-    error('todo: file_is');
+    return !directory_is(file_path)
 }
