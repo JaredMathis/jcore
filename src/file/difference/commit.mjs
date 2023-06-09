5b8dc6c6759b7797a1aa22d3b41722ba7db8bf7b
JaredMathis <jared.mathis@native.consulting>
2023-06-09 08:55:45 -0400

    #79 function_auto file_difference_commit

---
+++ src/file/difference/commit.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function file_difference_commit() {
+    arguments_assert(arguments, []);
+    error('todo: file_difference_commit');
+}
