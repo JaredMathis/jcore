128c35279d47d5c7389346b5c013833dcf1515fd
JaredMathis <jared.mathis@native.consulting>
2023-06-09 08:59:32 -0400

    #79 function_auto file_difference_commit

--- src/file/difference/commit.mjs
+++ src/file/difference/commit.mjs
@@ -1,6 +1,6 @@
+import { guid_generate } from '../../guid/generate.mjs';
 import { path_is } from '../../path/is.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
-import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { file_difference } from '../difference.mjs';
 export async function file_difference_commit(repository_name, file_path) {
@@ -8,7 +8,7 @@ export async function file_difference_commit(repository_name, file_path) {
         string_identifier_is,
         path_is
     ]);
-    let commit_id = guid_generate()
-    let commit = {commit_id};
+    let commit_id = guid_generate();
+    let commit = { commit_id };
     await file_difference(repository_name, commit_id, file_path);
 }
