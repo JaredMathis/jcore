328b7ff913963a818cbd3f44bbb8e6144e7c1ea7
JaredMathis <jared.mathis@native.consulting>
2023-06-09 08:50:40 -0400

    #79 function_auto guid_generate

---
+++ src/guid/generate.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function guid_generate() {
+    arguments_assert(arguments, []);
+    error('todo: guid_generate');
+}
