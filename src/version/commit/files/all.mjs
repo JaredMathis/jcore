8f8c83c8990c5dfe8fa654ff04349328396d174e
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:12:36 -0400

    #80 function_auto version_commit_files_all

--- src/version/commit/files/all.mjs
+++ src/version/commit/files/all.mjs
@@ -1,13 +1,25 @@
+import { string_starts_with } from '../../../string/starts/with.mjs';
+import { list_any } from '../../../list/any.mjs';
+import { log } from '../../../log.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { todo } from '../../../todo.mjs';
 import { version_commit_data } from '../data.mjs';
 import { version_commit_files } from '../files.mjs';
 import { directory_read } from '../../../directory/read.mjs';
 import { directory_current } from '../../../directory/current.mjs';
 import { git_ignore_lines } from '../../../git/ignore/lines.mjs';
+import { list_filter } from '../../../list/filter.mjs';
 export async function version_commit_files_all(repository_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let directory_path = directory_current();
     let file_paths = await directory_read(directory_path);
     let gil = await git_ignore_lines();
-    let filtered
+    list_filter(file_paths, f => {
+        console.log(f);
+        return list_any(gil, g => string_starts_with(f, g));
+    });
+    return;
+    let filtered;
     await version_commit_files(repository_name, file_paths, version_commit_data(todo, arguments));
 }
