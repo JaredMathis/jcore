b6c5516c19bbc4095e96ad2f2fe279de47dbf82e
JaredMathis <jared.mathis@native.consulting>
2023-06-09 14:59:24 -0400

    #80 function_auto sandbox2

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -1,13 +1,7 @@
-import { todo } from './todo.mjs';
-import { version_commit_data } from './version/commit/data.mjs';
-import { version_commit_files } from './version/commit/files.mjs';
-import { directory_read } from './directory/read.mjs';
+import { version_commit_files_all } from './version/commit/files/all.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
-import { directory_current } from './directory/current.mjs';
 export async function sandbox2() {
     arguments_assert(arguments, []);
     const repository_name = 'a';
-    let directory_path = directory_current();
-    let file_paths = await directory_read(directory_path);
-    await version_commit_files(repository_name, file_paths, version_commit_data(todo, arguments));
+    await version_commit_files_all(repository_name);
 }
---
+++ src/version/commit/files/all.mjs
@@ -0,0 +1,10 @@
+import { todo } from '../../../todo.mjs';
+import { version_commit_data } from '../data.mjs';
+import { version_commit_files } from '../files.mjs';
+import { directory_read } from '../../../directory/read.mjs';
+import { directory_current } from '../../../directory/current.mjs';
+export async function version_commit_files_all(repository_name) {
+    let directory_path = directory_current();
+    let file_paths = await directory_read(directory_path);
+    await version_commit_files(repository_name, file_paths, version_commit_data(todo, arguments));
+}
