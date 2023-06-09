d8d1a6fc6e9454133f417df2d773522ab6b49a66
JaredMathis <jared.mathis@native.consulting>
2023-06-09 08:22:39 -0400

    #79 function_auto file_difference

--- src/file/difference.mjs
+++ src/file/difference.mjs
@@ -1,3 +1,4 @@
+import { list_join } from '../list/join.mjs';
 import { list_add_if_not_exists } from '../list/add/if/not/exists.mjs';
 import { string_trim } from '../string/trim.mjs';
 import { list_map } from '../list/map.mjs';
@@ -11,6 +12,7 @@ import { path_is } from '../path/is.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { string_split } from '../string/split.mjs';
+import { file_overwrite } from './overwrite.mjs';
 export async function file_difference(repository_name, file_path) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -23,9 +25,10 @@ export async function file_difference(repository_name, file_path) {
         await file_write(gitignore_file_path, string_empty());
     }
     let gitignore_contents = await file_read(gitignore_file_path);
-    file_map;
     let gitignore_lines = string_split(gitignore_contents, string_new_line());
     let mapped = list_map(gitignore_lines, string_trim);
     list_add_if_not_exists(mapped, repository_container_folder_name);
+    let gitignore_contents_new = list_join(mapped, string_new_line());
+    file_overwrite(gitignore_file_path);
     return mapped;
 }
