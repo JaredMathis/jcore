3e441b82a957b3f93462eeb9a3a472f89af14b1e
JaredMathis <jared.mathis@native.consulting>
2023-06-09 08:37:04 -0400

    #79 noop

--- src/data/path.mjs
+++ src/data/path.mjs
@@ -2,6 +2,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 export function data_path() {
     arguments_assert(arguments, []);
+    let extension = `.json`
     return `./data.json`;
     metadata([]);
 }
