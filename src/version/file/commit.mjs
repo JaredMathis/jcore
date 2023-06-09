b0fd421be1a27df5db513fb975fb9f42e1cf2796
JaredMathis <jared.mathis@native.consulting>
2023-06-09 10:17:44 -0400

    #79 noop

--- src/version/file/commit.mjs
+++ src/version/file/commit.mjs
@@ -4,6 +4,8 @@ import { path_is } from '../../path/is.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { version_file_difference } from './difference.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
+import { version_property_path } from '../property/path.mjs';
 export async function version_file_commit(repository_name, file_path) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -13,8 +15,15 @@ export async function version_file_commit(repository_name, file_path) {
     let commit_id = guid_generate();
     let parts = [];
     let difference = await version_file_difference(repository_name, file_path);
+    let difference_path = object_property_get(difference, version_property_path());
     let part_id = guid_generate();
-    let difference_overwrite = {};
+    let difference_write = {
+        file_path: difference_path,
+        contents: {
+            part_id,
+            hunks,
+        }
+    };
     list_add(parts, part_id);
     let commit = {
         commit_id,
