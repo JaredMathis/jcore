f0adad0e9f5000fa23d16f46c0ca2f5c88f6c841
JaredMathis <jared.mathis@native.consulting>
2023-06-09 10:09:31 -0400

    #79 function_auto version_file_commit

--- src/version/file/commit.mjs
+++ src/version/file/commit.mjs
@@ -1,23 +1,6 @@
-import { list_add } from '../../list/add.mjs';
-import { guid_generate } from '../../guid/generate.mjs';
-import { path_is } from '../../path/is.mjs';
-import { string_identifier_is } from '../../string/identifier/is.mjs';
+import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { version_file_difference } from './difference.mjs';
-export async function version_file_commit(repository_name, file_path) {
-    arguments_assert(arguments, [
-        string_identifier_is,
-        path_is
-    ]);
-    let when = new Date().getTime();
-    let commit_id = guid_generate();
-    let parts = [];
-    let difference = await version_file_difference(repository_name, file_path);
-    let part_id = guid_generate();
-    list_add(parts, part_id);
-    let commit = {
-        commit_id,
-        when,
-        parts
-    };
+export function version_file_commit() {
+    arguments_assert(arguments, []);
+    error('todo: version_file_commit');
 }
