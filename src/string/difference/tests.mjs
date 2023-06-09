2b812e9447f7ea5bc0aa8356b31d19a7ff5d120b
JaredMathis <jared.mathis@native.consulting>
2023-06-09 07:41:25 -0400

    #79 function_tests_generate string_difference_tests

--- src/string/difference/tests.mjs
+++ src/string/difference/tests.mjs
@@ -1,9 +1,7 @@
-import { equal } from '../../equal.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_is } from '../is.mjs';
 import { string_difference_get } from './get.mjs';
 import { string_difference_apply } from './apply.mjs';
-import { assert } from '../../assert.mjs';
 export function string_difference_tests(string_old, string_new) {
     arguments_assert(arguments, [
         string_is,
@@ -11,5 +9,5 @@ export function string_difference_tests(string_old, string_new) {
     ]);
     let hunks = string_difference_get(string_old, string_new);
     let string_new2 = string_difference_apply(string_old, hunks);
-    assert(equal(string_new2, string_new));
+    return string_new2;
 }
---
+++ src/string/difference/tests/test/generated/6.mjs
@@ -0,0 +1,13 @@
+import { metadata_generated } from '../../../../../metadata/generated.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+import { metadata } from '../../../../../metadata.mjs';
+import { json_equal } from '../../../../../json/equal.mjs';
+import { assert } from '../../../../../assert.mjs';
+import { string_difference_tests } from '../../../tests.mjs';
+export function string_difference_tests_test_generated_6() {
+    arguments_assert(arguments, []);
+    let expected = 'aab';
+    let actual = string_difference_tests('', 'aab');
+    assert(json_equal(actual, expected));
+    metadata([metadata_generated()]);
+}
