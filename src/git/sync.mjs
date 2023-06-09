608563a48d89c3d7f4fd2878da0a3b9e8ed31175
JaredMathis <jared.mathis@native.consulting>
2023-06-09 17:33:31 -0400

    #81 function_auto git_sync

--- src/git/sync.mjs
+++ src/git/sync.mjs
@@ -1,6 +1,10 @@
-import { error } from '../error.mjs';
+import { command_line_all } from '../command/line/all.mjs';
+import { git_commands_sync_add } from './commands/sync/add.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function git_sync() {
+export async function git_sync() {
     arguments_assert(arguments, []);
-    error('todo: git_sync');
+    let commands = [];
+    git_commands_sync_add(commands);
+    let c_result = await command_line_all(commands);
+    return c_result;
 }
