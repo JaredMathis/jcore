aa519551a2a0f43aa0ae7224b8f0e92efdf48990
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:46:27 -0400

    #81 function_auto directory_is

--- src/directory/is.mjs
+++ src/directory/is.mjs
@@ -1,8 +1,8 @@
-import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import fs from 'fs';
+import { path_is } from '../path/is.mjs';
 export async function directory_is(file_path) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [path_is]);
     const stat = await fs.promises.stat(file_path);
     const is_directory = stat.isDirectory();
     return is_directory;
