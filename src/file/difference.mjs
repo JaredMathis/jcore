c701572a8a67455e2492dc884ac375d3dae4cf0c
JaredMathis <jared.mathis@native.consulting>
2023-06-09 08:11:38 -0400

    #79 function_auto file_difference

--- src/file/difference.mjs
+++ src/file/difference.mjs
@@ -1,3 +1,4 @@
+import { file_exists } from './exists.mjs';
 import { directory_exists_ensure } from '../directory/exists/ensure.mjs';
 import { path_is } from '../path/is.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
@@ -10,4 +11,6 @@ export async function file_difference(repository_name, file_path) {
     let repository_container_folder_name = 'tc';
     await directory_exists_ensure(repository_container_folder_name);
     let gitignore_file_path = '.gitignore';
+    if (!await file_exists(gitignore_file_path)) {
+    }
 }
