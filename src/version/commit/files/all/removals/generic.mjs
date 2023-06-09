1584733c4423545666a3040da13265e9fbe5a809
JaredMathis <jared.mathis@native.consulting>
2023-06-09 19:32:46 -0400

    #82 noop

--- src/version/commit/files/all/removals/generic.mjs
+++ src/version/commit/files/all/removals/generic.mjs
@@ -28,7 +28,8 @@ export async function version_commit_files_all_removals_generic(repository_name,
     ]);
     let files_current_filtered = await git_ignore_filter(file_paths);
     let repository_files_path = version_path_files_get(repository_name);
-    let paths = await directory_read_directories(repository_files_path);
+    let paths;
+    paths = await directory_read_directories(repository_files_path);
     let dc = directory_current();
     let mapped = list_map(paths, p => dc + string_prefix_without(p, repository_files_path));
     let files_committed = list_filter(mapped, m1 => list_all(mapped, m2 => implies(string_starts_with(m2, m1), equal(m1, m2))));
