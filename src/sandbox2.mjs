49af44bc5c6a3f99074279a0354c1b2cda48ac18
JaredMathis <jared.mathis@native.consulting>
2023-06-09 16:01:18 -0400

    #81 function_auto sandbox2

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -1,5 +1,5 @@
+import { list_any } from './list/any.mjs';
 import { implies } from './implies.mjs';
-import { list_all } from './list/all.mjs';
 import { string_prefix_without } from './string/prefix/without.mjs';
 import { list_map } from './list/map.mjs';
 import { directory_read_directories } from './directory/read/directories.mjs';
@@ -19,6 +19,7 @@ export async function sandbox2() {
     let repository_files_path = version_path_files_get(repository_name);
     let paths = await directory_read_directories(repository_files_path);
     let mapped = list_map(paths, p => directory_current() + string_prefix_without(p, repository_files_path));
-    let filtered = list_filter(mapped, m1 => list_all(mapped, m2 => implies(string_starts_with(m1, m2), equal(m1, m2))));
+    console.log(mapped);
+    let filtered = list_filter(mapped, m1 => !list_any(mapped, m2 => !implies(string_starts_with(m2, m1), equal(m1, m2))));
     console.log(filtered);
 }
