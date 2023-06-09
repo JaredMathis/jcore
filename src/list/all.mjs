a206fbdccd0e8be0c59084f35b4af01691ea38df
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:23:14 -0400

    #80 function_auto list_all

---
+++ src/list/all.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function list_all() {
+    arguments_assert(arguments, []);
+    error('todo: list_all');
+}
