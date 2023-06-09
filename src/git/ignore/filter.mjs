b27c11b5017933cc0965966ee0a7d5967ea7f7ed
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:17:43 -0400

    #80 function_auto git_ignore_filter

--- src/git/ignore/filter.mjs
+++ src/git/ignore/filter.mjs
@@ -1,10 +1,15 @@
+import { list_single_item } from '../../list/single/item.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_starts_with } from '../../string/starts/with.mjs';
 import { list_any } from '../../list/any.mjs';
 import { directory_separator } from '../../directory/separator.mjs';
 import { list_filter } from '../../list/filter.mjs';
 import { git_ignore_lines } from './lines.mjs';
 export async function git_ignore_filter(file_paths) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let gil = await git_ignore_lines();
+    let filter = list_combine(gil, list_single_item('.git'));
     let filtered = list_filter(file_paths, f => {
         let with_slash = f + directory_separator();
         return list_any(gil, g => string_starts_with(f, with_slash) || equals(f, g));
