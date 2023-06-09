5ea67d2a564f389d75a8947c430416b8050ffe88
JaredMathis <jared.mathis@native.consulting>
2023-06-09 18:36:31 -0400

    #82 function_auto version_commit_files_all

--- src/version/commit/files/all.mjs
+++ src/version/commit/files/all.mjs
@@ -1,14 +1,11 @@
-import { git_ignore_filter } from '../../../git/ignore/filter.mjs';
+import { directory_read_current } from '../../../directory/read/current.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { todo } from '../../../todo.mjs';
 import { version_commit_data } from '../data.mjs';
 import { version_commit_files } from '../files.mjs';
-import { directory_read } from '../../../directory/read.mjs';
-import { directory_current } from '../../../directory/current.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 export async function version_commit_files_all(repository_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    let directory_path = directory_current();
-    let file_paths = await directory_read(directory_path);
+    let file_paths = await directory_read_current();
     await version_commit_files(repository_name, file_paths, version_commit_data(todo, arguments));
 }
