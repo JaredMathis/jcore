c075b281cbf701c04759d0596f2c10f533396ee4
JaredMathis <jared.mathis@native.consulting>
2023-06-09 18:40:38 -0400

    #82 function_auto sandbox2

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -1,4 +1,5 @@
-import { version_commit_files_all_removals } from './version/commit/files/all/removals.mjs';
+import { version_commit_files_all_removals_generic } from './version/commit/files/all/removals/generic.mjs';
+import { version_commit_files_difference } from './version/commit/files/difference.mjs';
 import { version_commit_files_all } from './version/commit/files/all.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 export async function sandbox2() {
@@ -6,8 +7,6 @@ export async function sandbox2() {
     const repository_name = 'a';
     if (false)
         await version_commit_files_all(repository_name);
-
-
     let differences = await version_commit_files_difference(repository_name, file_paths, data);
     let removals = await version_commit_files_all_removals_generic(file_paths, repository_name);
 }
