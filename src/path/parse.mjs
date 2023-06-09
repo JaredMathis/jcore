6d267b3cb2fcbc68eb774220d87795643674b882
JaredMathis <jared.mathis@native.consulting>
2023-06-09 11:26:40 -0400

    #79 function_auto path_parse

---
+++ src/path/parse.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function path_parse() {
+    arguments_assert(arguments, []);
+    error('todo: path_parse');
+}
