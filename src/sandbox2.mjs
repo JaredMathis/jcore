a627241a789b4987c9b6c98e58857c767c13e9a6
JaredMathis <jared.mathis@native.consulting>
2023-06-09 16:05:29 -0400

    #81 function_auto sandbox2

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -1,3 +1,5 @@
+import { log } from './log.mjs';
+import { git_ignore_filter } from './git/ignore/filter.mjs';
 import { implies } from './implies.mjs';
 import { list_all } from './list/all.mjs';
 import { string_prefix_without } from './string/prefix/without.mjs';
@@ -10,6 +12,7 @@ import { directory_current } from './directory/current.mjs';
 import { list_filter } from './list/filter.mjs';
 import { string_starts_with } from './string/starts/with.mjs';
 import { equal } from './equal.mjs';
+import { directory_read } from './directory/read.mjs';
 export async function sandbox2() {
     arguments_assert(arguments, []);
     const repository_name = 'a';
@@ -20,4 +23,7 @@ export async function sandbox2() {
     let dc = directory_current();
     let mapped = list_map(paths, p => dc + string_prefix_without(p, repository_files_path));
     let files_committed = list_filter(mapped, m1 => list_all(mapped, m2 => implies(string_starts_with(m2, m1), equal(m1, m2))));
+    let files_current = directory_read(dc);
+    let files_current_filtered = await git_ignore_filter(files_current);
+    console.log(files_current_filtered);
 }
