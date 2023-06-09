6d69542da18c0ec44d925d214b33f8efe2570b2f
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:44:18 -0400

    #81 function_auto sandbox2

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -1,10 +1,10 @@
+import { string_prefix_without } from './string/prefix/without.mjs';
 import { list_map } from './list/map.mjs';
 import { directory_read_directories } from './directory/read/directories.mjs';
 import { log } from './log.mjs';
 import { version_path_files_get } from './version/path/files/get.mjs';
 import { version_commit_files_all } from './version/commit/files/all.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
-import { string_suffix_without } from './string/suffix/without.mjs';
 export async function sandbox2() {
     arguments_assert(arguments, []);
     const repository_name = 'a';
@@ -12,6 +12,6 @@ export async function sandbox2() {
         await version_commit_files_all(repository_name);
     let repository_files_path = version_path_files_get(repository_name);
     let paths = await directory_read_directories(repository_files_path);
-    let mapped = list_map(paths, p => string_suffix_without(p, repository_files_path));
+    let mapped = list_map(paths, p => string_prefix_without(p, repository_files_path));
     console.log(mapped);
 }
