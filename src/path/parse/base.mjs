fdef6fe64e91ac7dbd4049952d8d8bc01afee1ac
JaredMathis <jared.mathis@native.consulting>
2023-06-09 11:30:27 -0400

    #79 function_rename path_parse_name path_parse_base

--- src/path/parse/base.mjs
+++ src/path/parse/base.mjs
@@ -1,6 +1,6 @@
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function path_parse_name() {
+export function path_parse_base() {
     arguments_assert(arguments, []);
     error('todo: path_parse_name');
 }
