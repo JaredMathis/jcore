e26513b6ae0d8ea2efcd9490cf2af469db345fe1
JaredMathis <jared.mathis@native.consulting>
2023-06-09 16:52:16 -0400

    #81 function_auto git_ac

--- src/git/ac.mjs
+++ src/git/ac.mjs
@@ -1,19 +1,9 @@
+import { git_pacp_generic } from './pacp/generic.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
-import { data_task_id } from '../data/task/id.mjs';
-import { object_property_get } from '../object/property/get.mjs';
-import { data_git_commit_message_initial } from '../data/git/commit/message/initial.mjs';
-import { git_pacp_with_message } from './pacp/with/message.mjs';
-import { data_get } from '../data/get.mjs';
-import { assert } from '../assert.mjs';
 export async function git_ac() {
     arguments_assert(arguments, []);
     let sync = false;
-    let data = await data_get();
-    let task_id = object_property_get(data, data_task_id());
-    let initial = data_git_commit_message_initial();
-    assert(task_id !== initial);
-    let commit_message = task_id;
-    await git_pacp_with_message(commit_message, sync);
+    await git_pacp_generic(sync);
     metadata([]);
 }
---
+++ src/git/pacp/generic.mjs
@@ -0,0 +1,14 @@
+import { git_pacp_with_message } from './with/message.mjs';
+import { assert } from '../../assert.mjs';
+import { data_git_commit_message_initial } from '../../data/git/commit/message/initial.mjs';
+import { data_task_id } from '../../data/task/id.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
+import { data_get } from '../../data/get.mjs';
+export async function git_pacp_generic(sync) {
+    let data = await data_get();
+    let task_id = object_property_get(data, data_task_id());
+    let initial = data_git_commit_message_initial();
+    assert(task_id !== initial);
+    let commit_message = task_id;
+    await git_pacp_with_message(commit_message, sync);
+}
