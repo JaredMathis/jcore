ee68c2cadeb1900fc03b20ca55829b6a2a80d4d5
JaredMathis <jared.mathis@native.consulting>
2023-06-09 11:56:56 -0400

    #80 function_auto version_commit_file

--- src/version/commit/file.mjs
+++ src/version/commit/file.mjs
@@ -1,3 +1,4 @@
+import { list_single_item } from '../../list/single/item.mjs';
 import { version_commit_data } from './data.mjs';
 import { path_is } from '../../path/is.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
@@ -8,5 +9,5 @@ export function version_commit_file(repository_name, file_path) {
         string_identifier_is,
         path_is
     ]);
-    version_commit_files_with_data(repository_name, file_path, version_commit_data(version_commit_file, arguments));
+    version_commit_files_with_data(repository_name, list_single_item(file_path), version_commit_data(version_commit_file, arguments));
 }
