b7798484cc5709c3f2a2514cba0b9cf18a59f759
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:19:39 -0400

    #80 noop

--- src/git/ignore/filter.mjs
+++ src/git/ignore/filter.mjs
@@ -10,7 +10,7 @@ import { git_ignore_lines } from './lines.mjs';
 export async function git_ignore_filter(file_paths) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let gil = await git_ignore_lines();
-    let filter = list_all_combine(gil, list_single_item('.git'));
+    let filter = list_all_combine([gil, list_single_item('.git')]);
     let filtered = list_filter(file_paths, f => {
         let with_slash = f + directory_separator();
         return list_any(filter, g => string_starts_with(f, with_slash) || equals(f, g));
