39054901a996b4a558fd15e4f7888b4e59927ba2
JaredMathis <jared.mathis@native.consulting>
2023-06-09 07:34:16 -0400

    #79 function_auto string_difference_tests

--- src/string/difference/apply/test/1.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../../../../error.mjs';
-import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function string_difference_apply_test_1() {
-    arguments_assert(arguments, []);
-    error('todo: string_difference_apply_test_1');
-}
---
+++ src/string/difference/tests.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function string_difference_tests() {
+    arguments_assert(arguments, []);
+    error('todo: string_difference_tests');
+}
