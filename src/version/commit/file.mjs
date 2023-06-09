183e95dc183bb6f90b2a1b1d22dc7115851d1bfa
JaredMathis <jared.mathis@native.consulting>
2023-06-09 18:38:04 -0400

    #82 function_auto version_commit_file

--- src/version/commit/file.mjs
+++ src/version/commit/file.mjs
@@ -4,10 +4,10 @@ import { path_is } from '../../path/is.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { version_commit_files } from './files.mjs';
-export function version_commit_file(repository_name, file_path) {
+export async function version_commit_file(repository_name, file_path) {
     arguments_assert(arguments, [
         string_identifier_is,
         path_is
     ]);
-    version_commit_files(repository_name, list_single_item(file_path), version_commit_data(version_commit_file, arguments));
+    await version_commit_files(repository_name, list_single_item(file_path), version_commit_data(version_commit_file, arguments));
 }
