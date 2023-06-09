98c9009549d138617c9c86d83a91c8bafd593621
JaredMathis <jared.mathis@native.consulting>
2023-06-09 18:13:09 -0400

    #81 noop

--- src/version/file/difference/generic.mjs
+++ src/version/file/difference/generic.mjs
@@ -15,6 +15,7 @@ import { integer_parse } from '../../../integer/parse.mjs';
 import { list_max_generic } from '../../../list/max/generic.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
 import { function_is } from '../../../function/is.mjs';
+import { directory_separator } from '../../../directory/separator.mjs';
 export async function version_file_difference_generic(repository_name, file_path, contents_new_get) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -28,8 +29,9 @@ export async function version_file_difference_generic(repository_name, file_path
     let repository_file_directory_path = version_path_file_directory(repository_name, file_path);
     let paths = await directory_read(repository_file_directory_path);
     let mapped = list_map(paths, file_path => {
-        let without_prefix = string_prefix_without(file_path, repository_file_directory_path);
+        let without_prefix = string_prefix_without(file_path, repository_file_directory_path + directory_separator());
         let without_suffix = string_suffix_without(without_prefix, file_extension_json());
+        console.log({without_suffix})
         let version = integer_parse(without_suffix);
         return {
             file_path,
