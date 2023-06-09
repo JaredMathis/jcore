a32f8bfcd91ebcd76793bb7cd4bb5d140d0ea121
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:46:45 -0400

    #81 function_auto file_is

--- src/file/is.mjs
+++ src/file/is.mjs
@@ -1,6 +1,7 @@
+import { path_is } from '../path/is.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function file_is() {
-    arguments_assert(arguments, []);
+export function file_is(file_path) {
+    arguments_assert(arguments, [path_is]);
     error('todo: file_is');
 }
