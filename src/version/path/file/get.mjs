edc58f0a22955924d4b4070f60cbcd3a96bde5e0
JaredMathis <jared.mathis@native.consulting>
2023-06-09 10:55:19 -0400

    #79 noop

--- src/version/path/file/get.mjs
+++ src/version/path/file/get.mjs
@@ -12,15 +12,7 @@ export function version_path_file_get(repository_name, file_path, version) {
         integer_is
     ]);
     let repository_files_directory_name = 'files';
-    let repository_container_folder_name = version_path_root();
-    let repository_directory = path_join([
-        repository_container_folder_name,
-        repository_name
-    ]);
-    let repository_sub_path = path_join([
-        repository_directory,
-        repository_files_directory_name,
-    ]);
+    let repository_sub_path = version_path_sub_get(repository_name, repository_files_directory_name);
     let repository_file_path = path_join([
         repository_sub_path,
         file_path
@@ -31,4 +23,17 @@ export function version_path_file_get(repository_name, file_path, version) {
         initial_file_name
     ]);
     return repository_file_path_initial;
+}
+
+function version_path_sub_get(repository_name, repository_files_directory_name) {
+    let repository_container_folder_name = version_path_root();
+    let repository_directory = path_join([
+        repository_container_folder_name,
+        repository_name
+    ]);
+    let repository_sub_path = path_join([
+        repository_directory,
+        repository_files_directory_name,
+    ]);
+    return repository_sub_path;
 }
