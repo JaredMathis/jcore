ce3b177e85d283872a5592f69ba89abbbcb2eb17
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:52:08 -0400

    #81 noop

--- src/implies.mjs
+++ src/implies.mjs
@@ -1,6 +1,7 @@
 import { error } from './error.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
-export function implies() {
-    arguments_assert(arguments, []);
-    error('todo: implies');
+import { boolean_is } from './boolean/is.mjs';
+export function implies(b1, b2) {
+    arguments_assert(arguments, [boolean_is, boolean_is]);
+    return !b1 || b2;
 }
