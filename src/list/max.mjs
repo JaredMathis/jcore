9e1ca16eff9ab8de32a494c589b8ebbf7e91bcf9
JaredMathis <jared.mathis@native.consulting>
2023-06-09 18:09:38 -0400

    #81 noop

--- src/list/max.mjs
+++ src/list/max.mjs
@@ -5,7 +5,6 @@ import { metadata } from '../metadata.mjs';
 import { identity } from '../identity.mjs';
 export function list_max(list) {
     arguments_assert(arguments, [list_is]);
-    let lambda = identity;
-    return list_max_generic(list, lambda);
+    return list_max_generic(list, identity);
     metadata([]);
 }
