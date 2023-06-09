61e676549d10aa6627ae1a26124263b3d319e6e3
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:23:37 -0400

    #80 function_auto list_all

--- src/list/all.mjs
+++ src/list/all.mjs
@@ -1,6 +1,17 @@
+import { function_is } from '../function/is.mjs';
+import { list_is } from './is.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function list_all() {
-    arguments_assert(arguments, []);
+export function list_all(list, predicate) {
+    arguments_assert(arguments, [
+        list_is,
+        function_is
+    ]);
+    for (let a of list) {
+        if (predicate(a)) {
+            return true;
+        }
+    }
+    return false;
     error('todo: list_all');
 }
