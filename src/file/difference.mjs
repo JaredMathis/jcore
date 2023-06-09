6ade3416eac2e94d252bff3025da8dea25da1303
JaredMathis <jared.mathis@native.consulting>
2023-06-09 08:04:29 -0400

    #79 function_auto file_difference

---
+++ src/file/difference.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function file_difference() {
+    arguments_assert(arguments, []);
+    error('todo: file_difference');
+}
