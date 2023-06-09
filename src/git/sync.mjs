a54aa3e4847edf698bb6dedf8a398dcbb2f88786
JaredMathis <jared.mathis@native.consulting>
2023-06-09 17:32:59 -0400

    #81 function_auto git_sync

---
+++ src/git/sync.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function git_sync() {
+    arguments_assert(arguments, []);
+    error('todo: git_sync');
+}
