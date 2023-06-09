b9fe87127127c02ff89e3ab488e947863fb58146
JaredMathis <jared.mathis@native.consulting>
2023-06-09 15:45:58 -0400

    #81 function_auto directory_is

--- src/directory/is.mjs
+++ src/directory/is.mjs
@@ -1,5 +1,8 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 import fs from 'fs';
 export async function directory_is(file_path) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     const stat = await fs.promises.stat(file_path);
     const is_directory = stat.isDirectory();
     return is_directory;
--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -5,6 +5,7 @@ import { log } from './log.mjs';
 import { version_path_files_get } from './version/path/files/get.mjs';
 import { version_commit_files_all } from './version/commit/files/all.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
+import { directory_current } from './directory/current.mjs';
 export async function sandbox2() {
     arguments_assert(arguments, []);
     const repository_name = 'a';
@@ -12,6 +13,6 @@ export async function sandbox2() {
         await version_commit_files_all(repository_name);
     let repository_files_path = version_path_files_get(repository_name);
     let paths = await directory_read_directories(repository_files_path);
-    let mapped = list_map(paths, p => string_prefix_without(p, repository_files_path));
+    let mapped = list_map(paths, p => directory_current() + string_prefix_without(p, repository_files_path));
     console.log(mapped);
 }
