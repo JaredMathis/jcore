e85e31deb85eac7c947be60bdfe874a2a7b3a1e0
JaredMathis <jared.mathis@native.consulting>
2023-06-09 08:48:01 -0400

    #79 function_auto js_npm_install

--- src/js/npm/install.mjs
+++ src/js/npm/install.mjs
@@ -1,6 +1,7 @@
-import { error } from '../../error.mjs';
+import { command_line } from '../../command/line.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { string_is } from '../../string/is.mjs';
 export function js_npm_install(npm_package_name) {
-    arguments_assert(arguments, []);
-    error('todo: npm_install');
+    arguments_assert(arguments, [string_is]);
+    command_line;
 }
