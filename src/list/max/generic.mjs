5b42a1ea8091b69a51318c9ba0a9efef911861a6
JaredMathis <jared.mathis@native.consulting>
2023-06-09 18:14:15 -0400

    #81 noop

--- src/list/max/generic.mjs
+++ src/list/max/generic.mjs
@@ -2,14 +2,15 @@ import { number_is } from '../../number/is.mjs';
 import { assert } from '../../assert.mjs';
 import { list_first } from '../first.mjs';
 export function list_max_generic(list, value_get) {
-    let max = value_get(list_first(list));
+    let result = list_first(list)
+    let max = value_get(result);
     assert(number_is(max));
     for (let element of list) {
         let n = value_get(element);
         assert(number_is(n));
         if (n > max) {
-            max = n;
+            result = element;
         }
     }
-    return max;
+    return result;
 }
