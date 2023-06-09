f1dc30ead9d93c036d066957421ebf035c33fc12
JaredMathis <jared.mathis@native.consulting>
2023-06-09 07:36:27 -0400

    #79 function_auto string_difference_tests

--- src/string/difference/tests.mjs
+++ src/string/difference/tests.mjs
@@ -1,10 +1,15 @@
-import { error } from '../../error.mjs';
+import { equal } from '../../equal.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_is } from '../is.mjs';
+import { string_difference_get } from './get.mjs';
+import { string_difference_apply } from './apply.mjs';
+import { assert } from '../../assert.mjs';
 export function string_difference_tests(string_old, string_new) {
     arguments_assert(arguments, [
         string_is,
         string_is
     ]);
-    error('todo: string_difference_tests');
+    let hunks = string_difference_get(string_old, string_new);
+    let string_new2 = string_difference_apply(string_old, hunks);
+    assert(equal(string_new2, string_new));
 }
