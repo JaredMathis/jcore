fba84bfcb5b3d7e6410e651e7a75f66fed937270
JaredMathis <jared.mathis@native.consulting>
2023-06-09 20:13:53 -0400

    #83 sandbox2

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -13,17 +13,22 @@ export async function sandbox2() {
     let right = 'abbba';
     for (let left_index = 0; left_index < left.length; left_index++) {
         for (let right_index = 0; right_index < right.length; right_index++) {
-            let left_walker = left_index;
-            let right_walker = right_index;
-            let offset;
-            for (offset = 0; left_walker + offset < left.length && right_walker + offset < right.length; offset++) {
-                let left_offset = left[left_walker + offset];
-                let right_offset = right[right_walker + offset];
-                if (left_offset !== right_offset) {
-                    break;
-                }
-            }
+            let offset = a(left, right, left_index, right_index)
             console.log(offset, string_sub(left, left_index, left_index + offset))
         }
     }
+}
+
+function a(left, right, left_index, right_index) {
+    let left_walker = left_index;
+    let right_walker = right_index;
+    let offset;
+    for (offset = 0; left_walker + offset < left.length && right_walker + offset < right.length; offset++) {
+        let left_offset = left[left_walker + offset];
+        let right_offset = right[right_walker + offset];
+        if (left_offset !== right_offset) {
+            break;
+        }
+    }
+    return offset;
 }
