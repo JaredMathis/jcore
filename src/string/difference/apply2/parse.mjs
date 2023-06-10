e3692238fd3ce6899a6ffb24ce841251882dfe33
JaredMathis <jared.mathis@native.consulting>
2023-06-10 09:08:17 -0400

    #83 function_auto string_difference_apply2_parse

--- src/string/difference/apply2/parse.mjs
+++ src/string/difference/apply2/parse.mjs
@@ -1,6 +1,7 @@
-import { error } from '../../../error.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function string_difference_apply2_parse() {
-    arguments_assert(arguments, []);
-    error('todo: string_difference_apply2_parse');
+import { string_is } from '../../is.mjs';
+import { string_index_of_try } from '../../index/of/try.mjs';
+export function string_difference_apply2_parse(hunk) {
+    arguments_assert(arguments, [string_is]);
+    let index_minus = string_index_of_try(hunk, '-');
 }
