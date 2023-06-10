ba109c488624ab06cfaae8354af43d51b7ce5531
JaredMathis <jared.mathis@native.consulting>
2023-06-09 20:34:02 -0400

    #83 function_copy string_empty_not_is string_empty_is

---
+++ src/string/empty/is.mjs
@@ -0,0 +1,7 @@
+import { string_length } from '../length.mjs';
+import { string_is } from '../is.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function string_empty_is(s) {
+    arguments_assert(arguments, [string_is]);
+    return string_length(s) >= 1;
+}
