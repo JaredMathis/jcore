b7af04a1c918425d7922b6ae2e01057ea8f80086
JaredMathis <jared.mathis@native.consulting>
2023-06-09 20:39:06 -0400

    #83 function_auto sandbox2

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -1,7 +1,10 @@
+import { function_tests_generate_generic_each } from './function/tests/generate/generic/each.mjs';
+import { tests_name_next } from './tests/name/next.mjs';
 import { string_difference_get2 } from './string/difference/get2.mjs';
 import { log } from './log.mjs';
 import { version_commit_and_removals } from './version/commit/and/removals.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
+import { function_name_get } from './function/name/get.mjs';
 export async function sandbox2() {
     arguments_assert(arguments, []);
     const repository_name = 'a';
@@ -9,6 +12,15 @@ export async function sandbox2() {
         await version_commit_and_removals(repository_name);
     let left = '';
     let right = 'b';
-    let result = string_difference_get2(left, right);
+    let fn = string_difference_get2;
+    let args = [
+        left,
+        right
+    ];
+    let result = fn(...args);
     console.log({ result });
+    return;
+    let function_name = function_name_get(fn);
+    let test_name = await tests_name_next(function_name);
+    await function_tests_generate_generic_each(function_name, test_name, args);
 }
