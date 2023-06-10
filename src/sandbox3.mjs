d3f4e29ca37e002bd7df06b901c25b7811948d31
JaredMathis <jared.mathis@native.consulting>
2023-06-10 10:19:31 -0400

    #83 sandbox3

--- src/sandbox3.mjs
+++ src/sandbox3.mjs
@@ -5,6 +5,7 @@ import { tests } from './tests.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 export async function sandbox3() {
     arguments_assert(arguments, []);
+    return;
     let values = [
         '2+a',
         '3-2',
