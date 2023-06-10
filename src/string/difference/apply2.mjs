76277b348eeb43e9f68b96978b027ec6a5e193cb
JaredMathis <jared.mathis@native.consulting>
2023-06-10 08:57:17 -0400

    #83 sandbox2

--- src/string/difference/apply2.mjs
+++ src/string/difference/apply2.mjs
@@ -1,9 +1,13 @@
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_is } from '../is.mjs';
 import { list_is } from '../../list/is.mjs';
+import { list_length_is_0 } from '../../list/length/is/0.mjs';
 export function string_difference_apply2(string_old, hunks) {
     arguments_assert(arguments, [
         string_is,
         list_is
     ]);
+    if (list_length_is_0(hunks)) {
+        return string_old
+    }
 }
