8114b657d83fe990fefba91490704872ddc275ae
JaredMathis <jared.mathis@native.consulting>
2023-06-10 09:29:13 -0400

    #83 function_auto sandbox3

--- src/sandbox3.mjs
+++ src/sandbox3.mjs
@@ -1,6 +1,11 @@
-import { error } from './error.mjs';
+import { tests_generate } from './tests/generate.mjs';
+import { string_difference_apply2_parse } from './string/difference/apply2/parse.mjs';
+import { function_tests_generate_next } from './function/tests/generate/next.mjs';
+import { tests } from './tests.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
-export function sandbox3() {
+export async function sandbox3() {
     arguments_assert(arguments, []);
-    error('todo: sandbox3');
+    await tests();
+    await function_tests_generate_next(string_difference_apply2_parse, args1);
+    await tests_generate();
 }
