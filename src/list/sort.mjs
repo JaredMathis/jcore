45b523a0267d97f916bdf591c052349a7721476c
JaredMathis <jared.mathis@native.consulting>
2023-06-10 10:59:58 -0400

    #83 function_auto list_sort

--- src/list/sort.mjs
+++ src/list/sort.mjs
@@ -1,3 +1,7 @@
+import { subtract } from '../subtract.mjs';
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 export function list_sort(removals) {
-    removals.sort((a, b) => a - b);
+    arguments_assert(arguments, [arguments_assert_todo]);
+    removals.sort((a, b) => subtract(a, b));
 }
