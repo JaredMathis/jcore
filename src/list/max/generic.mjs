420820afe93c267acdcdfee859996cde9b8a8628
JaredMathis <jared.mathis@native.consulting>
2023-06-10 09:16:52 -0400

    #83 function_auto list_max_generic

--- src/list/max/generic.mjs
+++ src/list/max/generic.mjs
@@ -1,11 +1,12 @@
 import { list_compare_generic } from '../compare/generic.mjs';
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { value_get } from '../../value/get.mjs';
+import { list_is } from '../is.mjs';
+import { function_is } from '../../function/is.mjs';
 export function list_max_generic(list, value_get) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        list_is,
+        function_is
     ]);
     let compare = (a, b) => a > b;
     return list_compare_generic(list, value_get, compare);
