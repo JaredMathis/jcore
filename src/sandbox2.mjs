fbe648d11749c04293b7e13822536ff6f8ed1cd4
JaredMathis <jared.mathis@native.consulting>
2023-06-09 20:01:57 -0400

    #83 function_auto sandbox2

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -3,6 +3,6 @@ import { arguments_assert } from './arguments/assert.mjs';
 export async function sandbox2() {
     arguments_assert(arguments, []);
     const repository_name = 'a';
-    if(false)
-    await version_commit_and_removals(repository_name);
+    if (false)
+        await version_commit_and_removals(repository_name);
 }
