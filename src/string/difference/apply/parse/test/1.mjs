cae24047d365b039eb527aef96c2092a4a374977
JaredMathis <jared.mathis@native.consulting>
2023-06-10 19:41:42 -0400

    #83 noop

--- src/string/difference/apply/parse/test/1.mjs
+++ src/string/difference/apply/parse/test/1.mjs
@@ -4,7 +4,7 @@ import { metadata } from '../../../../../metadata.mjs';
 import { json_equal } from '../../../../../json/equal.mjs';
 import { assert } from '../../../../../assert.mjs';
 import { string_difference_apply_parse } from '../../parse.mjs';
-export function string_difference_apply2_parse_test_1() {
+export function string_difference_apply_parse_test_1() {
     arguments_assert(arguments, []);
     let expected = {
         'position': 2,
