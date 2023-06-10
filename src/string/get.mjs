b87cd654c7e9db2ee398b3e09a266dae84cab853
JaredMathis <jared.mathis@native.consulting>
2023-06-09 20:05:56 -0400

    #83 function_auto string_get

--- src/string/get.mjs
+++ src/string/get.mjs
@@ -1,6 +1,13 @@
-import { error } from '../error.mjs';
+import { object_property_exists } from '../object/property/exists.mjs';
+import { assert } from '../assert.mjs';
+import { integer_is } from '../integer/is.mjs';
+import { list_is } from '../list/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function string_get() {
-    arguments_assert(arguments, []);
-    error('todo: string_get');
+export function string_get(list, index) {
+    arguments_assert(arguments, [
+        list_is,
+        integer_is
+    ]);
+    assert(object_property_exists(list, index));
+    return list[index];
 }
