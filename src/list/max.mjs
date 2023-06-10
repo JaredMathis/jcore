fe216c3fa21dc35db118946fe25b26b0a559735c
JaredMathis <jared.mathis@native.consulting>
2023-06-10 09:19:35 -0400

    #83 function_auto list_max

--- src/list/max.mjs
+++ src/list/max.mjs
@@ -1,10 +1,11 @@
-import { list_max_generic } from './max/generic.mjs';
+import { list_compare_generic } from './compare/generic.mjs';
 import { list_is } from './is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { identity } from '../identity.mjs';
 export function list_max(list) {
     arguments_assert(arguments, [list_is]);
-    return list_max_generic(list, identity);
+    let compare = (current, found) => current > found;
+    return list_compare_generic(list, identity, compare);
     metadata([]);
 }
