04883ba905787145dcc5e65b6cd0b5c53db26283
JaredMathis <jared.mathis@native.consulting>
2023-06-10 19:46:12 -0400

    #83 sandbox2

--- src/version/commit/and/removals.mjs
+++ src/version/commit/and/removals.mjs
@@ -11,12 +11,10 @@ import { version_commit_data } from '../data.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 export async function version_commit_and_removals(repository_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    console.log('here2')
     const commit_data = version_commit_data(version_commit_and_removals, arguments);
     let file_paths = await directory_read_current();
     let differences = await version_commit_files_difference(repository_name, file_paths, commit_data);
     let removals = await version_commit_files_all_removals_generic(repository_name, file_paths, commit_data);
-    console.log('here')
     await file_json_overwrite('out.txt', {
         removals,
         differences
