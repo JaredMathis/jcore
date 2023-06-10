9a91c469f34146340b56f8a41d3182355e0cd5e7
JaredMathis <jared.mathis@native.consulting>
2023-06-10 11:09:22 -0400

    #83 function_delete list_sorstring_difference_apply2

--- src/list/sorstring/difference/apply2.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../../../error.mjs';
-import { arguments_assert } from '../../../arguments/assert.mjs';
-export function list_sorstring_difference_apply2() {
-    arguments_assert(arguments, []);
-    error('todo: list_sorstring_difference_apply2');
-}
