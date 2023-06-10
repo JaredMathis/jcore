6a991d033e6b0e6a4a617a77e4b0082a9e27fe78
JaredMathis <jared.mathis@native.consulting>
2023-06-10 09:13:30 -0400

    #83 function_auto string_difference_apply2_parse

--- src/string/difference/apply2/parse.mjs
+++ src/string/difference/apply2/parse.mjs
@@ -1,3 +1,4 @@
+import { string_index_of_try_valid } from '../../index/of/try/valid.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_is } from '../../is.mjs';
 import { string_index_of_try } from '../../index/of/try.mjs';
@@ -5,11 +6,16 @@ import { string_difference_removed } from '../removed.mjs';
 import { string_difference_added } from '../added.mjs';
 import { list_all } from '../../../list/all.mjs';
 import { error } from '../../../error.mjs';
+import { list_filter } from '../../../list/filter.mjs';
 export function string_difference_apply2_parse(hunk) {
     arguments_assert(arguments, [string_is]);
     let index_removed = string_index_of_try(hunk, string_difference_removed());
     let index_added = string_index_of_try(hunk, string_difference_added());
-    let indexes = [index_removed, index_added];
+    let indexes = [
+        index_removed,
+        index_added
+    ];
+    let valid = list_filter(indexes, string_index_of_try_valid);
     if (list_all(indexes, i => !string_index_of_try_valid(i))) {
         error();
     }
