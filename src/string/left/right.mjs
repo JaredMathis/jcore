ee556a43a15efe14cc13155ed147856d751706da
JaredMathis <jared.mathis@native.consulting>
2023-06-10 10:05:10 -0400

    #83 function_auto string_left_right

--- src/string/left/right.mjs
+++ src/string/left/right.mjs
@@ -1,14 +1,15 @@
 import { string_take } from '../take.mjs';
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_left_right_property_right } from './right/property/right.mjs';
 import { string_left_right_property_left } from './right/property/left.mjs';
 import { string_skip } from '../skip.mjs';
+import { string_is } from '../is.mjs';
+import { integer_is } from '../../integer/is.mjs';
 export function string_left_right(s, index, offset) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo,
-        arguments_assert_todo
+        string_is,
+        integer_is,
+        integer_is
     ]);
     let left = string_take(s, index);
     let right = string_skip(s, index + offset);
