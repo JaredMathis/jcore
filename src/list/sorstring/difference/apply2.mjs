7e4dca4c8cfedc160cf2f24738d87c8a51057404
JaredMathis <jared.mathis@native.consulting>
2023-06-10 11:09:06 -0400

    #83 function_auto list_sorstring_difference_apply2

---
+++ src/list/sorstring/difference/apply2.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function list_sorstring_difference_apply2() {
+    arguments_assert(arguments, []);
+    error('todo: list_sorstring_difference_apply2');
+}
