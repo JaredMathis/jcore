b4463d3a1495c9686d6954c81d4fafb350b4580c
JaredMathis <jared.mathis@native.consulting>
2023-06-10 10:31:47 -0400

    #83 noop

--- src/tests/name/next.mjs
+++ src/tests/name/next.mjs
@@ -15,9 +15,9 @@ export async function tests_name_next(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let functions_all = await function_name_all();
     const prefix = function_name + string_function_tests_sub();
+    console.log({prefix})
     let tests_all = list_map(functions_all, f => string_starts_with(f, prefix));
     let tests_ids_all = list_map(tests_all, t => {
-        console.log({function_name,t})
         return string_function_name_to_tests_id(function_name, t);
     });
     let tests_ids_all_numeric = list_filter(tests_ids_all, id => {
