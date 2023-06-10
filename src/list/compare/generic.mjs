cb599c65778dd1b8eaef985b7342049a8011ee45
JaredMathis <jared.mathis@native.consulting>
2023-06-10 09:17:37 -0400

    #83 function_auto list_compare_generic

--- src/list/compare/generic.mjs
+++ src/list/compare/generic.mjs
@@ -1,17 +1,24 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { number_is } from '../../number/is.mjs';
 import { assert } from '../../assert.mjs';
 import { list_first } from '../first.mjs';
 import { value_get } from '../../value/get.mjs';
 export function list_compare_generic(list, value_get, compare) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let result = list_first(list);
-    let max = value_get(result);
-    assert(number_is(max));
+    let found = value_get(result);
+    assert(number_is(found));
     for (let element of list) {
-        let n = value_get(element);
-        assert(number_is(n));
-        if (compare(n, max)) {
+        let current = value_get(element);
+        assert(number_is(current));
+        if (compare(current, found)) {
             result = element;
-            max = n;
+            found = current;
         }
     }
     return result;
