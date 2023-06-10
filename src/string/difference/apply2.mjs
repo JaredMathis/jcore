0f9550f939852a43d76bffacb84397d186e46d65
JaredMathis <jared.mathis@native.consulting>
2023-06-10 09:43:56 -0400

    #83 function_auto string_difference_apply2

--- src/string/difference/apply2.mjs
+++ src/string/difference/apply2.mjs
@@ -1,3 +1,4 @@
+import { string_difference_property_operation } from './property/operation.mjs';
 import { string_difference_apply2_parse } from './apply2/parse.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_is } from '../is.mjs';
@@ -6,6 +7,7 @@ import { list_length_is_0 } from '../../list/length/is/0.mjs';
 import { error } from '../../error.mjs';
 import { json_to } from '../../json/to.mjs';
 import { list_map } from '../../list/map.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
 export function string_difference_apply2(string_old, hunks) {
     arguments_assert(arguments, [
         string_is,
@@ -15,9 +17,9 @@ export function string_difference_apply2(string_old, hunks) {
         return string_old;
     }
     let mapped = list_map(hunks, string_difference_apply2_parse);
-    let value = string_old
+    let value = string_old;
     for (let m of mapped) {
-        
+        let operation = object_property_get(m, string_difference_property_operation());
     }
     error(json_to({
         string_old,
