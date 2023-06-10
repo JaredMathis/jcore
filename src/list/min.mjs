a2cf24f4814664b53236d148ad9824b2e975ec39
JaredMathis <jared.mathis@native.consulting>
2023-06-10 09:20:24 -0400

    #83 function_copy list_max list_min

---
+++ src/list/min.mjs
@@ -0,0 +1,11 @@
+import { metadata } from '../metadata.mjs';
+import { identity } from '../identity.mjs';
+import { list_compare_generic } from './compare/generic.mjs';
+import { list_is } from './is.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function list_min(list) {
+    arguments_assert(arguments, [list_is]);
+    let compare = (current, found) => current > found;
+    return list_compare_generic(list, identity, compare);
+    metadata([]);
+}
