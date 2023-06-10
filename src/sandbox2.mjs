62799d8fc9eb5ab58dd3a47a1086dd1f955413aa
JaredMathis <jared.mathis@native.consulting>
2023-06-09 20:03:45 -0400

    #83 noop

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -1,8 +1,18 @@
 import { version_commit_and_removals } from './version/commit/and/removals.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
+import { string_length } from './string/length.mjs';
 export async function sandbox2() {
     arguments_assert(arguments, []);
     const repository_name = 'a';
     if (false)
         await version_commit_and_removals(repository_name);
+
+    let left = "abba";
+    let right = "abbba";
+    for (let i = 0; i < string_length(left); i++) {
+        for (let j = 0; j < string_length(right); j++) {
+            let i2 = i;
+            let j2 = j;
+        }
+    }
 }
