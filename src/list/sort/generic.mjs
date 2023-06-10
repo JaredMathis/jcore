ea72c1ed885ae9bd2222a523e4c8e41b9a82bb60
JaredMathis <jared.mathis@native.consulting>
2023-06-10 11:12:01 -0400

    #83 function_auto list_sort_generic

--- src/list/sort/generic.mjs
+++ src/list/sort/generic.mjs
@@ -1,5 +1,15 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { subtract } from '../../subtract.mjs';
 import { value_get } from '../../value/get.mjs';
-export function list_sort_generic(list, value_get) {
+export function list_sort_generic(list, value_get, reverse) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     list.sort((a, b) => subtract(value_get(a), value_get(b)));
+    if (reverse) {
+        list.reverse();
+    }
 }
