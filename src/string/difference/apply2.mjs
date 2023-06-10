62d3c96996e4a3360ebfc38fd8e1c10c279e1062
JaredMathis <jared.mathis@native.consulting>
2023-06-10 09:06:25 -0400

    #83 function_auto string_difference_apply2_parse

--- src/string/difference/apply2.mjs
+++ src/string/difference/apply2.mjs
@@ -4,6 +4,7 @@ import { list_is } from '../../list/is.mjs';
 import { list_length_is_0 } from '../../list/length/is/0.mjs';
 import { error } from '../../error.mjs';
 import { json_to } from '../../json/to.mjs';
+import { list_map } from '../../list/map.mjs';
 export function string_difference_apply2(string_old, hunks) {
     arguments_assert(arguments, [
         string_is,
