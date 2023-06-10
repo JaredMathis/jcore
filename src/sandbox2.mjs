832791f2d917de4c9ccba14d479a8a4bbc48c6cc
JaredMathis <jared.mathis@native.consulting>
2023-06-09 20:08:34 -0400

    #83 noop

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -2,6 +2,7 @@ import { string_get } from './string/get.mjs';
 import { version_commit_and_removals } from './version/commit/and/removals.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 import { string_length } from './string/length.mjs';
+import { equal } from './equal.mjs';
 export async function sandbox2() {
     arguments_assert(arguments, []);
     const repository_name = 'a';
@@ -9,13 +10,16 @@ export async function sandbox2() {
         await version_commit_and_removals(repository_name);
     let left = 'abba';
     let right = 'abbba';
-    for (let i = 0; i < string_length(left); i++) {
-        for (let j = 0; j < string_length(right); j++) {
+    for (let i = 0; i < left.length; i++) {
+        for (let j = 0; j < right.length; j++) {
             let i2 = i;
             let j2 = j;
-            for (let offset = 0; i2 + offset < string_length(left) && j2 + offset < string_length(right); offset++) {
-                let i2_offset = string_get(i2 + offset);
-                let j2_offset = string_get(j2 + offset);
+            for (let offset = 0; i2 + offset < left.length && j2 + offset < right.length; offset++) {
+                let i2_offset = left[i2 + offset];
+                let j2_offset = right[j2 + offset];
+                if (!i2_offset, j2_offset)) {
+
+                }
             }
         }
     }
