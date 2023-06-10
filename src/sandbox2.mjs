45d78bca2bfdcf98dab61ba0af0e9c6936deaa67
JaredMathis <jared.mathis@native.consulting>
2023-06-10 08:56:25 -0400

    #83 noop

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -7,20 +7,30 @@ import { log } from './log.mjs';
 import { version_commit_and_removals } from './version/commit/and/removals.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 import { function_name_get } from './function/name/get.mjs';
+import { string_difference_apply2 } from './string/difference/apply2.mjs';
+import { assert } from './assert.mjs';
+import { equal } from './equal.mjs';
 export async function sandbox2() {
     arguments_assert(arguments, []);
     const repository_name = 'a';
     if (false)
         await version_commit_and_removals(repository_name);
-    let left = 'aaaa';
-    let right = 'abacada';
+    let left = '';
+    let right = '';
     let fn1 = string_difference_get2;
     let args1 = [
         left,
         right
     ];
-    let result = fn1(...args1);
-    console.log({ result });
+    let result1 = fn1(...args1);
+    console.log({ result1 });
+    let fn2 = string_difference_apply2;
+    let args2 = [
+        left,
+        result1
+    ];
+    let result2 = fn2(...args2);
+    assert(equal(right, result2));
     await tests();
     return;
     let function_name = function_name_get(fn1);
