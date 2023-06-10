062c98b1c372933d222a8540f3a7dc22f558e4b0
JaredMathis <jared.mathis@native.consulting>
2023-06-09 20:42:25 -0400

    #83 sandbox2

--- src/sandbox2.mjs
+++ src/sandbox2.mjs
@@ -10,8 +10,8 @@ export async function sandbox2() {
     const repository_name = 'a';
     if (false)
         await version_commit_and_removals(repository_name);
-    let left = '';
-    let right = '';
+    let left = 'a';
+    let right = 'a';
     let fn = string_difference_get2;
     let args = [
         left,
