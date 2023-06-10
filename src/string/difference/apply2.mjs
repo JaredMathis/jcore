2777f0171152518eded11a58061c9daadd28bd0f
JaredMathis <jared.mathis@native.consulting>
2023-06-10 11:16:02 -0400

    #83 function_auto string_difference_apply2

--- src/string/difference/apply2.mjs
+++ src/string/difference/apply2.mjs
@@ -1,10 +1,10 @@
+import { list_sort_property_generic } from '../../list/sort/property/generic.mjs';
 import { list_filter_property } from '../../list/filter/property.mjs';
 import { string_left_right_property_right } from '../left/right/property/right.mjs';
 import { string_left_right_property_left } from '../left/right/property/left.mjs';
 import { string_left_right } from '../left/right.mjs';
 import { string_difference_added } from './added.mjs';
 import { string_difference_removed } from './removed.mjs';
-import { string_difference_property_added } from './property/added.mjs';
 import { string_difference_property_position } from './property/position.mjs';
 import { string_difference_property_operation } from './property/operation.mjs';
 import { string_difference_apply2_parse } from './apply2/parse.mjs';
@@ -20,7 +20,6 @@ import { string_difference_property_removed } from './property/removed.mjs';
 import { add } from '../../add.mjs';
 import { assert } from '../../assert.mjs';
 import { list_length } from '../../list/length.mjs';
-import { list_sort_generic } from '../../list/sort/generic.mjs';
 export function string_difference_apply2(string_old, hunks) {
     arguments_assert(arguments, [
         string_is,
@@ -38,18 +37,14 @@ export function string_difference_apply2(string_old, hunks) {
     for (let m of removals) {
         let position = object_property_get(m, string_difference_property_position());
         let operation = object_property_get(m, string_difference_property_operation());
-            let removed = object_property_get(m, string_difference_property_removed());
-            let lr = string_left_right(value, position, add(position, removed));
-            let left = object_property_get(lr, string_left_right_property_left());
-            let right = object_property_get(lr, string_left_right_property_right());
-            value = `${ left }${ right }`;
+        let removed = object_property_get(m, string_difference_property_removed());
+        let lr = string_left_right(value, position, add(position, removed));
+        let left = object_property_get(lr, string_left_right_property_left());
+        let right = object_property_get(lr, string_left_right_property_right());
+        value = `${ left }${ right }`;
     }
     for (let m of addeds) {
         error();
     }
     return value;
-}
-
-function list_sort_property_generic(removals, sort_property, reverse) {
-    list_sort_generic(removals, r => object_property_get(r, sort_property), reverse);
 }
