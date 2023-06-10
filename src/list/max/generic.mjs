fa6e4b22d5f982a4a8570745f4d3cb66c6af6b87
JaredMathis <jared.mathis@native.consulting>
2023-06-10 09:15:18 -0400

    #83 noop

--- src/list/max/generic.mjs
+++ src/list/max/generic.mjs
@@ -2,6 +2,7 @@ import { number_is } from '../../number/is.mjs';
 import { assert } from '../../assert.mjs';
 import { list_first } from '../first.mjs';
 export function list_max_generic(list, value_get) {
+    let compare = (a, b) => a > b;
     let result = list_first(list)
     let max = value_get(result);
     assert(number_is(max));
