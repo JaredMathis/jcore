85a8e0556630a6d8c6c1b48e66ae0e4216c1bacf
JaredMathis <jared.mathis@native.consulting>
2023-06-10 09:28:19 -0400

    #83 function_auto sandbox3

---
+++ src/sandbox3.mjs
@@ -0,0 +1,6 @@
+import { error } from './error.mjs';
+import { arguments_assert } from './arguments/assert.mjs';
+export function sandbox3() {
+    arguments_assert(arguments, []);
+    error('todo: sandbox3');
+}
