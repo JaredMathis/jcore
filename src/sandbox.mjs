741dd116efa0051e320c0a57d7170279e7973a2c
JaredMathis <jared.mathis@native.consulting>
2023-06-11 11:51:42 -0400

    #86 sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -23,10 +23,12 @@ import { arguments_assert } from './arguments/assert.mjs';
 import { string_split } from './string/split.mjs';
 import { string_underscore_is } from './string/underscore/is.mjs';
 import { initializeApp } from 'firebase-admin/app';
+import { getFirestore }from 'firebase-admin/firestore';
 export async function sandbox() {
     arguments_assert(arguments, []);
     const repository_name = 'a';
     initializeApp({ projectId: 'truthcode' });
+    const db = getFirestore();
     return;
     let differences = await version_differences(repository_name);
     console.log(differences);
