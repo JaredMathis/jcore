65984c22c391d8382f0903cff92f2b8b5c3562a5
JaredMathis <jared.mathis@native.consulting>
2023-06-11 16:52:08 -0400

    #89 function_auto js_npm_uninstall

--- src/js/npm/uninstall.mjs
+++ src/js/npm/uninstall.mjs
@@ -3,5 +3,5 @@ import { string_is } from '../../string/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function js_npm_uninstall(npm_package_name) {
     arguments_assert(arguments, [string_is]);
-    await command_line(`npm i ${ npm_package_name }@latest`);
+    await command_line(`npm uninstall ${ npm_package_name }`);
 }
