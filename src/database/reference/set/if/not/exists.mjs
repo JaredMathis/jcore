11c89c9caad5b290180963d03ba7d9dc8be55e2f
JaredMathis <jared.mathis@native.consulting>
2023-06-11 15:28:29 -0400

    #89 noop

--- src/database/reference/set/if/not/exists.mjs
+++ src/database/reference/set/if/not/exists.mjs
@@ -2,7 +2,6 @@ import { database_reference_set } from '../../../set.mjs';
 import { database_reference_get } from '../../../get.mjs';
 export async function database_reference_set_if_not_exists(transaction, info_refererence, value) {
     const info = await database_reference_get(transaction, info_refererence);
-    console.log({info})
     if (!info.exists) {
         database_reference_set(info_refererence, value);
     }
