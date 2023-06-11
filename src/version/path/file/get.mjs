dae7b1c5bf347e47c14050a81e36c85638e42b0e
JaredMathis <jared.mathis@native.consulting>
2023-06-11 13:02:31 -0400

    #88 function_auto version_path_file_get

--- src/version/path/file/get.mjs
+++ src/version/path/file/get.mjs
@@ -1,10 +1,10 @@
 import { version_path_file_directory } from './directory.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { file_extension_json } from '../../../file/extension/json.mjs';
 import { path_join } from '../../../path/join.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { path_is } from '../../../path/is.mjs';
 import { integer_is } from '../../../integer/is.mjs';
+import { file_name_json } from '../../../file/name/json.mjs';
 export function version_path_file_get(repository_name, file_path, version) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -12,7 +12,7 @@ export function version_path_file_get(repository_name, file_path, version) {
         integer_is
     ]);
     let repository_file_path = version_path_file_directory(repository_name, file_path);
-    let file_name = `${ version }${ file_extension_json() }`;
+    let file_name = file_name_json(version);
     let repository_file_path_initial = path_join([
         repository_file_path,
         file_name
