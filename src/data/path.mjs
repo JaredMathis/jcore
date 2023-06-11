e808553c991ec9ac78c924ee1fcedf3ae20449e6
JaredMathis <jared.mathis@native.consulting>
2023-06-11 13:03:16 -0400

    #88 function_auto data_path

--- src/data/path.mjs
+++ src/data/path.mjs
@@ -1,8 +1,9 @@
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { file_name_json } from '../file/name/json.mjs';
+import { directory_current } from '../directory/current.mjs';
 export function data_path() {
     arguments_assert(arguments, []);
-    return `./${ file_name_json(`data`) }`;
+    return `${ directory_current() }/${ file_name_json(`data`) }`;
     metadata([]);
 }
