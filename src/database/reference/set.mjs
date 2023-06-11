41f7e2b7a80338cb61085e0e74b77b4ee3b9b297
JaredMathis <jared.mathis@native.consulting>
2023-06-11 15:30:50 -0400

    #89 noop

--- src/database/reference/set.mjs
+++ src/database/reference/set.mjs
@@ -1,3 +1,3 @@
-export function database_reference_set(reference, document_data) {
-    return reference.set(document_data);
+export async function database_reference_set(reference, document_data) {
+    return await reference.set(document_data);
 }
--- src/database/reference/set/if/not/exists.mjs
+++ src/database/reference/set/if/not/exists.mjs
@@ -3,7 +3,7 @@ import { database_reference_get } from '../../../get.mjs';
 export async function database_reference_set_if_not_exists(info_refererence, value) {
     const info = database_reference_get(info_refererence);
     if (!info.exists) {
-        database_reference_set(info_refererence, value);
+        await database_reference_set(info_refererence, value);
     }
     return info;
 }
