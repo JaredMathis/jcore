11739e090877516cb5da9c31ecd993ea21f5df14
JaredMathis <jared.mathis@native.consulting>
2023-06-11 16:51:53 -0400

    #89 function_copy js_npm_install js_npm_uninstall

---
+++ src/js/npm/uninstall.mjs
@@ -0,0 +1,7 @@
+import { command_line } from '../../command/line.mjs';
+import { string_is } from '../../string/is.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export async function js_npm_uninstall(npm_package_name) {
+    arguments_assert(arguments, [string_is]);
+    await command_line(`npm i ${ npm_package_name }@latest`);
+}
