a1e2152432100e33ee0ccee56794aaecdf146d3b
JaredMathis <jared.mathis@native.consulting>
2023-06-11 16:33:25 -0400

    #89 noop

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -57,8 +57,8 @@ export async function sandbox() {
         let repository_files_path = version_path_files_get(repository_name);
         let files = await directory_read_json(repository_files_path);
         let repository_commits_path = version_path_commits_get(repository_name);
-        let contents = await directory_read_json(repository_commits_path);
-        list_each_with_index(contents, (commit, index) => {
+        let commits = await directory_read_json(repository_commits_path);
+        list_each_with_index(commits, (commit, index) => {
             let commit_path = object_property_get(commit, directory_property_file_path());
             let commit_id = version_commits_path_to_integer(list_single_item(commit_path));
             let commit_json = object_property_get(commit, directory_property_json());
@@ -74,7 +74,7 @@ export async function sandbox() {
             let property_commit = 'commit';
             let document_path_commit = `${ property_commit }${ fns }${ commit_id }`;
             database_create(db, transaction, database_collection_name, document_path_commit, {value:commit_files});
-            if (equal(index, list_last_index(contents))) {
+            if (equal(index, list_last_index(commits))) {
                 let property_commit_latest = `${ property_commit }${ fns }latest`;
                 database_reference_update_property(transaction, info_refererence, property_commit_latest, commit_id);
             }
