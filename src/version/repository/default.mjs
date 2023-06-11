e3eeafe0fb8b65b4a8b38f2347a5a3517fcd9429
JaredMathis <jared.mathis@native.consulting>
2023-06-11 12:23:11 -0400

    #86 function_auto version_repository_default

---
+++ src/version/repository/default.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function version_repository_default() {
+    arguments_assert(arguments, []);
+    error('todo: version_repository_default');
+}
