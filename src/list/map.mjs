5dddb9c71782b584f4c0dea06a1f5e8c86186e26
JaredMathis <jared.mathis@native.consulting>
2023-06-10 22:44:41 -0400

    #85 function_unasyncify list_map_async

--- src/list/map.mjs
+++ src/list/map.mjs
@@ -1,12 +1,19 @@
-import { function_is } from '../function/is.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
+import { metadata_generated } from '../metadata/generated.mjs';
 import { metadata } from '../metadata.mjs';
+import { list_add } from './add.mjs';
+import { function_is } from '../function/is.mjs';
 import { list_is } from './is.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 export function list_map(list, mapper) {
     arguments_assert(arguments, [
         list_is,
         function_is
     ]);
-    return list.map(element => mapper(element));
-    metadata([]);
+    let result = [];
+    for (let element of list) {
+        let mapped = mapper(element);
+        list_add(result, mapped);
+    }
+    return result;
+    metadata([metadata_generated()]);
 }
