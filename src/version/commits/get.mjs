568d21af4bc4cbc4893b97421c3172f8b96de500
JaredMathis <jared.mathis@native.consulting>
2023-06-11 14:26:19 -0400

    #89 function_auto version_commits_get

--- src/version/commits/get.mjs
+++ src/version/commits/get.mjs
@@ -1,13 +1,12 @@
-import { integer_parse } from '../../integer/parse.mjs';
-import { file_extension_json } from '../../file/extension/json.mjs';
-import { string_suffix_without } from '../../string/suffix/without.mjs';
-import { path_parse_base } from '../../path/parse/base.mjs';
-import { list_map } from '../../list/map.mjs';
+import { version_commits_path_to_integer } from './path/to/integer.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_length_is_0 } from '../../list/length/is/0.mjs';
 import { directory_read } from '../../directory/read.mjs';
 import { directory_exists_ensure } from '../../directory/exists/ensure.mjs';
 import { version_path_commits_get } from '../path/commits/get.mjs';
 export async function version_commits_get(repository_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let repository_sub_path = version_path_commits_get(repository_name);
     await directory_exists_ensure(repository_sub_path);
     let existing_commits = await directory_read(repository_sub_path);
@@ -15,9 +14,7 @@ export async function version_commits_get(repository_name) {
     if (list_length_is_0(existing_commits)) {
         parsed = [];
     } else {
-        let names = list_map(existing_commits, path_parse_base);
-        let unparsed = list_map(names, c => string_suffix_without(c, file_extension_json()));
-        parsed = list_map(unparsed, integer_parse);
+        parsed = version_commits_path_to_integer(existing_commits);
     }
     return parsed;
 }
---
+++ src/version/commits/path/to/integer.mjs
@@ -0,0 +1,11 @@
+import { integer_parse } from '../../../../integer/parse.mjs';
+import { file_extension_json } from '../../../../file/extension/json.mjs';
+import { string_suffix_without } from '../../../../string/suffix/without.mjs';
+import { path_parse_base } from '../../../../path/parse/base.mjs';
+import { list_map } from '../../../../list/map.mjs';
+export function version_commits_path_to_integer(existing_commits) {
+    let names = list_map(existing_commits, path_parse_base);
+    let unparsed = list_map(names, c => string_suffix_without(c, file_extension_json()));
+    let parsed = list_map(unparsed, integer_parse);
+    return parsed;
+}
