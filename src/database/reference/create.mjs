61284f456daaec686464ed44a0b575d8af7a59b7
JaredMathis <jared.mathis@native.consulting>
2023-06-11 15:14:09 -0400

    #89 function_copy database_reference_set database_reference_create

---
+++ src/database/reference/create.mjs
@@ -0,0 +1,9 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function database_reference_create(reference, document_data) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    return reference.set(document_data);
+}
