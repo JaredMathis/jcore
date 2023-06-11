c6a6dcab602b019e6263b12010300476175f5c6d
JaredMathis <jared.mathis@native.consulting>
2023-06-11 16:50:03 -0400

    #89 function_auto database_transaction

---
+++ src/database/transaction.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function database_transaction() {
+    arguments_assert(arguments, []);
+    error('todo: database_transaction');
+}
