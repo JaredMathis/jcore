7adb87057df596853aba1e2ebcd9ea49ecb6d9fd
JaredMathis <jared.mathis@native.consulting>
2023-06-11 13:12:21 -0400

    #88 function_auto version_repository_file_size_max

--- src/version/repository/file/size/max.mjs
+++ src/version/repository/file/size/max.mjs
@@ -11,13 +11,8 @@ export async function version_repository_file_size_max(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let repository_directory = version_path_repository(repository_name);
     let file_paths = await directory_read(repository_directory);
-    let list_contents = await list_map_async(file_paths, async file_path => {
-        let contents = await file_read(file_path);
-        return contents;
-    });
-    let file_sizes = list_map(list_contents, contents => {
-        return string_byte_size(contents);
-    });
+    let list_contents = await list_map_async(file_paths, file_read);
+    let file_sizes = list_map(list_contents, string_byte_size);
     let file_size_max = list_max(file_sizes);
     return file_size_max;
 }
