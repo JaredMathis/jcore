d5a592a3afcd731542c90b002ea23ff4a9bfb772
JaredMathis <jared.mathis@native.consulting>
2023-06-11 14:43:18 -0400

    #89 function_auto database_set

--- src/database/firestore/get.mjs
+++ src/database/firestore/get.mjs
@@ -1,3 +1,5 @@
+import { initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 export function database_firestore_get() {
     initializeApp({ projectId: 'truthcode' });
     const db = getFirestore();
--- src/database/set.mjs
+++ src/database/set.mjs
@@ -1,8 +1,6 @@
 import { database_firestore_get } from './firestore/get.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-import { initializeApp } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
 export async function database_set(collection_path, document_path, document_data) {
     arguments_assert(arguments, [
         arguments_assert_todo,
