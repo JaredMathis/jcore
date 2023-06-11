a09746f5c68a8824f64d3dd6b308c6f65cb4cca9
JaredMathis <jared.mathis@native.consulting>
2023-06-11 13:15:31 -0400

    #88 function_auto list_add_property

--- src/list/add/property.mjs
+++ src/list/add/property.mjs
@@ -1,6 +1,13 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_add } from '../add.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 export function list_add_property(list, object, property_name) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let property_value = object_property_get(object, property_name);
     list_add(list, property_value);
 }
