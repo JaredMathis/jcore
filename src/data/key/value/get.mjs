e6f1c6a00c68be02a081414cb380c43a50704947
JaredMathis <jared.mathis@native.consulting>
2023-06-11 16:54:44 -0400

    #89 function_auto data_key_value_get

--- src/data/key/value/get.mjs
+++ src/data/key/value/get.mjs
@@ -1,17 +1,15 @@
+import { data_get } from '../../get.mjs';
 import { metadata } from '../../../metadata.mjs';
-import { object_property_set } from '../../../object/property/set.mjs';
-import { data_map } from '../../map.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 import { string_is } from '../../../string/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { object_property_get } from '../../../object/property/get.mjs';
 export async function data_key_value_get(key, value) {
     arguments_assert(arguments, [
         string_is,
         defined_is
     ]);
-    await data_map(map);
-    function map(data) {
-        object_property_set(data, key, value);
-    }
+    let data = await data_get();
+    return object_property_get(data, key);
     metadata([]);
 }
