5599d94edc76cce93845585aa3c4685f8bf436c0
JaredMathis <jared.mathis@native.consulting>
2023-06-10 20:40:19 -0400

    #84 sandbox

--- src/sandbox.mjs
+++ src/sandbox.mjs
@@ -1,3 +1,20 @@
+import { version_file_contents } from './version/file/contents.mjs';
+import { git_ignore_filter } from './git/ignore/filter.mjs';
+import { directory_read_current } from './directory/read/current.mjs';
+import { string_difference_apply_parse } from './string/difference/apply/parse.mjs';
+import { string_multiply } from './string/multiply.mjs';
+import { random_get } from './random/get.mjs';
+import { random_between } from './random/between.mjs';
+import { tests_generate } from './tests/generate.mjs';
+import { function_tests_generate_next } from './function/tests/generate/next.mjs';
+import { list_add } from './list/add.mjs';
+import { equal } from './equal.mjs';
+import { assert } from './assert.mjs';
+import { range } from './range.mjs';
+import { string_difference_apply } from './string/difference/apply.mjs';
+import { string_difference_get } from './string/difference/get.mjs';
+import { tests } from './tests.mjs';
+import { version_commit_and_removals } from './version/commit/and/removals.mjs';
 import { log } from './log.mjs';
 import { string_letter_is } from './string/letter/is.mjs';
 import { function_tests_generate_after } from './function/tests/generate/after.mjs';
@@ -9,6 +26,68 @@ import { string_split } from './string/split.mjs';
 import { string_underscore_is } from './string/underscore/is.mjs';
 export async function sandbox() {
     arguments_assert(arguments, []);
+    const repository_name = 'a';
+    let file_paths = await directory_read_current();
+    let filtered = await git_ignore_filter(file_paths);
+    for (let file_path of filtered) {
+        console.log(file_path)
+        let contents_old = await version_file_contents(repository_name, file_path);
+    }
+    return;
+    await version_commit_and_removals(repository_name);
+    return;
+    await tests();
+    let fn1 = string_difference_get;
+    let fn2 = string_difference_apply;
+    let pairs = [];
+    for (let i of range(10)) {
+        let left = random_input();
+        let right = random_input();
+        let args1 = [
+            left,
+            right
+        ];
+        let result1 = fn1(...args1);
+        console.log({ result1 });
+        let args2 = [
+            left,
+            result1
+        ];
+        let result2 = fn2(...args2);
+        assert(equal(right, result2));
+        list_add(pairs, {
+            args1,
+            args2
+        });
+    }
+    for (let pair of pairs) {
+        let {args1, args2} = pair;
+        await function_tests_generate_next(fn1, args1);
+        await function_tests_generate_next(fn2, args2);
+    }
+    await tests_generate();
+    function random_input() {
+        let result = '';
+        for (let index of range(random_between(8, 16))) {
+            let c = random_get() > 0.5 ? 'a' : 'b';
+            let m = string_multiply(c, random_between(1, 3));
+            result = result + m;
+        }
+        return result;
+    }
+    return;
+    let values = [
+        '2+a',
+        '3-2',
+        '10+def',
+        '123-5'
+    ];
+    await tests();
+    for (let v of values) {
+        await function_tests_generate_next(string_difference_apply_parse, [v]);
+    }
+    await tests_generate();
+    return;
     let functions = [
         string_letter_is,
         string_underscore_is
@@ -24,4 +103,5 @@ export async function sandbox() {
         }
         await function_tests_generate_after();
     }
+    return;
 }
--- src/version/path/file/directory.mjs
+++ src/version/path/file/directory.mjs
@@ -1,11 +1,12 @@
-import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { path_join } from '../../../path/join.mjs';
 import { version_path_files_get } from '../files/get.mjs';
+import { string_identifier_is } from '../../../string/identifier/is.mjs';
+import { path_is } from '../../../path/is.mjs';
 export function version_path_file_directory(repository_name, file_path) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        string_identifier_is,
+        path_is
     ]);
     let repository_files_path = version_path_files_get(repository_name);
     let repository_file_path = path_join([
