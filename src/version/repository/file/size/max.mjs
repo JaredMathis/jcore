74ad394bcec24d67e3288e0ab76af87cb68fe3e0
JaredMathis <jared.mathis@native.consulting>
2023-06-11 13:11:55 -0400

    #88 function_auto version_repository_file_size_max

--- src/version/repository/file/size/max.mjs
+++ src/version/repository/file/size/max.mjs
@@ -1,3 +1,6 @@
+import { list_map } from '../../../../list/map.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { list_max } from '../../../../list/max.mjs';
 import { string_byte_size } from '../../../../string/byte/size.mjs';
 import { file_read } from '../../../../file/read.mjs';
@@ -5,10 +8,14 @@ import { list_map_async } from '../../../../list/map/async.mjs';
 import { directory_read } from '../../../../directory/read.mjs';
 import { version_path_repository } from '../../../path/repository.mjs';
 export async function version_repository_file_size_max(repository_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let repository_directory = version_path_repository(repository_name);
     let file_paths = await directory_read(repository_directory);
-    let file_sizes = await list_map_async(file_paths, async file_path => {
+    let list_contents = await list_map_async(file_paths, async file_path => {
         let contents = await file_read(file_path);
+        return contents;
+    });
+    let file_sizes = list_map(list_contents, contents => {
         return string_byte_size(contents);
     });
     let file_size_max = list_max(file_sizes);
