e39f2e7122bb132d96a8e53b4f4d03e8c17da7f4
JaredMathis <jared.mathis@native.consulting>
2023-06-11 16:30:37 -0400

    #89 noop

--- src/database/reference/create.mjs
+++ src/database/reference/create.mjs
@@ -1,9 +1,10 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function database_reference_create(reference, document_data) {
+export function database_reference_create(transaction, reference, document_data) {
     arguments_assert(arguments, [
+        arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    return reference.create(document_data);
+    return transaction.create(reference, document_data);
 }
