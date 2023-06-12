a234edcee4425613e10d472fead9f2479f50c1ae
JaredMathis <jared.mathis@native.consulting>
2023-06-12 14:57:45 -0400

    #92 function_auto refactor_properties_expand

---
+++ src/refactor/properties/expand.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function refactor_properties_expand() {
+    arguments_assert(arguments, []);
+    error('todo: refactor_properties_expand');
+}
