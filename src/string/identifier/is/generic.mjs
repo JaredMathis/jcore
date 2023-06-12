6d3274a0ad001a20811bf88dc3aefc07b6702862
JaredMathis <jared.mathis@native.consulting>
2023-06-12 19:55:33 -0400

    #92 function_input_add string_identifier_is_generic allow_underscores

--- src/string/identifier/is/generic.mjs
+++ src/string/identifier/is/generic.mjs
@@ -14,7 +14,8 @@ import { defined_is } from '../../../defined/is.mjs';
 export function string_identifier_is_generic(value, enforce_first_and_last) {
     arguments_assert(arguments, [
         defined_is,
-        boolean_is
+        boolean_is,
+        arguments_assert_todo
     ]);
     if (!string_is(value)) {
         return false;
