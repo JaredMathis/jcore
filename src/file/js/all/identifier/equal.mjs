d79073337d0f0acc5b57d5c315449a06a01f6408
JaredMathis <jared.mathis@native.consulting>
2023-06-12 18:21:01 -0400

    #92 function_auto file_js_all_identifier_equal

--- src/file/js/all/identifier/equal.mjs
+++ src/file/js/all/identifier/equal.mjs
@@ -7,9 +7,9 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function file_js_all_identifier_equal(identifier_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let result = [];
-    await file_js_all_identifiers_each(function lambda(identifiers) {
-        for (let i of identifiers) {
-            list_add_if_not_exists(result, i);
+    await file_js_all_identifiers_each(function lambda(identifiers, file_path) {
+        if (list_contains(identifiers, identifier_name)) {
+            list_add_if_not_exists(result, file_path);
         }
     });
     return list_contains(result, identifier_name);
--- src/file/js/all/identifiers/each.mjs
+++ src/file/js/all/identifiers/each.mjs
@@ -3,9 +3,9 @@ import { file_js_parse } from '../../parse.mjs';
 import { file_js_all_path } from '../path.mjs';
 export async function file_js_all_identifiers_each(lambda) {
     let all = await file_js_all_path();
-    for (let a of all) {
-        let parsed = await file_js_parse(a);
+    for (let file_path of all) {
+        let parsed = await file_js_parse(file_path);
         let identifiers = js_identifiers(parsed);
-        lambda(identifiers);
+        lambda(identifiers, file_path);
     }
 }
