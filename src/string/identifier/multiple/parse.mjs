5902c88c7321c367a0643428f5f1df003f574c04
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:15:53 -0400

    #92 function_auto string_identifier_multiple_parse

--- src/string/identifier/multiple/parse.mjs
+++ src/string/identifier/multiple/parse.mjs
@@ -1,12 +1,12 @@
+import { string_split_comma } from '../../split/comma.mjs';
 import { list_all } from '../../../list/all.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { string_split } from '../../split.mjs';
 import { string_is } from '../../is.mjs';
 import { assert } from '../../../assert.mjs';
 import { string_identifier_is } from '../is.mjs';
 export function string_identifier_multiple_parse(values) {
     arguments_assert(arguments, [string_is]);
-    let result = string_split(values, ',');
+    let result = string_split_comma(values);
     assert(list_all(result, string_identifier_is));
     return result;
 }
---
+++ src/string/split/comma.mjs
@@ -0,0 +1,4 @@
+import { string_split } from '../split.mjs';
+export function string_split_comma(values) {
+    return string_split(values, ',');
+}
