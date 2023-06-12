5469b13f83755a7b62808cbaa3a41c87485bf545
JaredMathis <jared.mathis@native.consulting>
2023-06-12 11:59:27 -0400

    #90 noop

--- src/directory/delete.mjs
+++ src/directory/delete.mjs
@@ -3,5 +3,5 @@ import { assert } from '../assert.mjs';
 import fs from 'fs';
 export async function directory_delete(directory_path) {
     assert(await path_exists(directory_path));
-    await fs.promises.rmdir(directory_path);
+    await fs.promises.rmdir(directory_path, { recursive: true, force: true });
 }
