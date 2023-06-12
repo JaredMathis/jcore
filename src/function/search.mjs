c2a5ca52530561b941b6dc408c7fc9176fd2c341
JaredMathis <jared.mathis@native.consulting>
2023-06-12 19:41:23 -0400

    #92 function_auto function_search

--- src/function/search.mjs
+++ src/function/search.mjs
@@ -1,8 +1,5 @@
-import { object_keys_each_filter } from '../object/keys/each/filter.mjs';
 import { string_identifier_sub_multiple_parse } from '../string/identifier/sub/multiple/parse.mjs';
 import { function_name_all_tests_not } from './name/all/tests/not.mjs';
-import { list_single } from '../list/single.mjs';
-import { list_length_is_1 } from '../list/length/is/1.mjs';
 import { list_filter } from '../list/filter.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { string_includes } from '../string/includes.mjs';
@@ -18,9 +15,5 @@ export async function function_search(query) {
         return list_all(parts, p => string_includes(a, p));
     });
     let dictionary = list_to_dictionary(function_names, key => function_name_to_file_path(key));
-    object_keys_each_filter();
-    if (list_length_is_1(function_names)) {
-        return list_single(function_names);
-    }
-    return function_names;
+    return dictionary;
 }
