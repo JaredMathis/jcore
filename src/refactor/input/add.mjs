5f5af6f3e7f1019af6d5adf16377373818a53212
JaredMathis <jared.mathis@native.consulting>
2023-06-12 19:35:09 -0400

    #92 function_search arguments

--- src/refactor/input/add.mjs
+++ src/refactor/input/add.mjs
@@ -2,4 +2,5 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 export function refactor_input_add(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let {input_name} = args;
 }
