274957e6e74e7aaafbe20fb3fd7f5c77b1af37f2
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:38:32 -0400

    #92 function_auto js_visit_nodes_call_expression_name_equal

--- src/js/visit/nodes/call/expression/name/equal.mjs
+++ src/js/visit/nodes/call/expression/name/equal.mjs
@@ -2,7 +2,6 @@ import { js_visit_nodes_filter_node } from '../../../filter/node.mjs';
 import { arguments_assert_todo } from '../../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
 import { js_node_call_expression_name_equal } from '../../../../../node/call/expression/name/equal.mjs';
-import { js_visit_nodes_filter } from '../../../filter.mjs';
 export function js_visit_nodes_call_expression_name_equal(parsed, name_expected, lambda) {
     arguments_assert(arguments, [
         arguments_assert_todo,
