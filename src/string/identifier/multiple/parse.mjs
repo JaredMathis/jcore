5b8474ac1bc9a2fa3a9b55ac36f098762fc6c023
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:15:08 -0400

    #92 function_auto string_identifier_multiple_parse

--- src/string/identifier/multiple/parse.mjs
+++ src/string/identifier/multiple/parse.mjs
@@ -1,4 +1,8 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_split } from '../../split.mjs';
 export function string_identifier_multiple_parse(values) {
-    return string_split(values, ',');
+    arguments_assert(arguments, [arguments_assert_todo]);
+    let result = string_split(values, ',');
+    return result;
 }
