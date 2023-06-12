e3f74c6db375665aa1e5c188790fdaa4e7591016
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:11:36 -0400

    #92 function_auto version_sync_local_preview_generic

--- src/version/sync/local/preview/generic.mjs
+++ src/version/sync/local/preview/generic.mjs
@@ -1,3 +1,5 @@
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { list_add } from '../../../../list/add.mjs';
 import { list_length_is_0 } from '../../../../list/length/is/0.mjs';
 import { string_difference_get } from '../../../../string/difference/get.mjs';
@@ -8,6 +10,7 @@ import { file_read } from '../../../../file/read.mjs';
 import { version_output_generic } from '../../../output/generic.mjs';
 import { directory_read_current } from '../../../../directory/read/current.mjs';
 export async function version_sync_local_preview_generic(repository_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let file_paths = await directory_read_current();
     let differences = [];
     await version_output_generic(repository_name, file_paths, lambda);
