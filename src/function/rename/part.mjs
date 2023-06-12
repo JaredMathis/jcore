aa0883e6cb187c4faf3e26a56629db93fb2fd5ef
JaredMathis <jared.mathis@native.consulting>
2023-06-12 18:11:21 -0400

    #92 function_auto function_rename_part

---
+++ src/function/rename/part.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function function_rename_part() {
+    arguments_assert(arguments, []);
+    error('todo: function_rename_part');
+}
