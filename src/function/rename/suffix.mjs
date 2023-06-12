6def404467cd0f015a9649f293fab33edd3105b0
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:42:38 -0400

    #92 function_auto function_rename_suffix

--- src/function/rename/suffix.mjs
+++ src/function/rename/suffix.mjs
@@ -4,6 +4,7 @@ import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { string_suffix_without } from '../../string/suffix/without.mjs';
 export function function_rename_suffix(function_name_old, suffix_old, suffix_new) {
     arguments_assert(arguments, [string_identifier_is]);
-    let function_name_new = string_suffix_without(function_name_old, suffix_old)
+    let suffix_without = string_suffix_without(function_name_old, suffix_old);
+    let function_;
     error('todo: function_rename_prefix');
 }
