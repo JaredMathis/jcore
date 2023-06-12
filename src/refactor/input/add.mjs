a035862e07219529b776a74d3f5323c311f5637a
JaredMathis <jared.mathis@native.consulting>
2023-06-12 19:57:21 -0400

    #92 function_auto refactor_input_add

--- src/refactor/input/add.mjs
+++ src/refactor/input/add.mjs
@@ -1,3 +1,4 @@
+import { js_function_declaration_to_params } from '../../js/function/declaration/to/params.mjs';
 import { js_parse_expression } from '../../js/parse/expression.mjs';
 import { log } from '../../log.mjs';
 import { js_function_declaration_to_statement_arguments_assert_args_predicate } from '../../js/function/declaration/to/statement/arguments/assert/args/predicate.mjs';
@@ -8,8 +9,10 @@ import { list_add } from '../../list/add.mjs';
 export async function refactor_input_add(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {input_name, function_declaration} = args;
+    js_function_declaration_to_params;
     let arguments_assert_args = await js_function_declaration_to_statement_arguments_assert_args_predicate(function_declaration);
     let type = js_parse_expression(function_name_get(arguments_assert_todo));
-    list_add(arguments_assert_args, type )
+    list_add(arguments_assert_args, type);
     console.log({ arguments_assert_args });
+    return true;
 }
