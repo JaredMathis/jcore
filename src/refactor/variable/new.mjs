0795b8025e0a12ffd100e2a2debfb9d35677595d
JaredMathis <jared.mathis@native.consulting>
2023-06-12 17:32:02 -0400

    #92 function_auto refactor_variable_new

---
+++ src/refactor/variable/new.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function refactor_variable_new() {
+    arguments_assert(arguments, []);
+    error('todo: refactor_variable_new');
+}
