e6d5cb59bd4183177c47d913ee3bd8fa38d7437b
JaredMathis <jared.mathis@native.consulting>
2023-06-12 11:58:37 -0400

    #90 noop

--- src/directory/empty/delete.mjs
+++ src/directory/empty/delete.mjs
@@ -4,7 +4,6 @@ import { metadata } from '../../metadata.mjs';
 import { assert } from '../../assert.mjs';
 import { path_is } from '../../path/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import fs from 'fs';
 export async function directory_empty_delete(directory_path) {
     arguments_assert(arguments, [path_is]);
     assert(await directory_empty_is(directory_path));
