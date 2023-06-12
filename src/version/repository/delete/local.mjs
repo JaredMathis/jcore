deee29bbe2edbf564c6d2377a02fda3d1492cd0a
JaredMathis <jared.mathis@native.consulting>
2023-06-12 11:52:07 -0400

    #90 function_search delete

--- src/version/repository/delete/local.mjs
+++ src/version/repository/delete/local.mjs
@@ -3,5 +3,5 @@ import { error } from '../../../error.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export function version_repository_delete_local(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: version_repository_delete_local');
+    
 }
