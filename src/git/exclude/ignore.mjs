87a0568367f10f6d1208d56e3310c52c53c410d6
JaredMathis <jared.mathis@native.consulting>
2023-06-12 12:36:07 -0400

    #91 function_auto git_exclude_ignore

--- src/git/exclude/ignore.mjs
+++ src/git/exclude/ignore.mjs
@@ -1,3 +1,4 @@
+import { git_exclude } from '../exclude.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { assert } from '../../assert.mjs';
 import { list_length_is_0 } from '../../list/length/is/0.mjs';
@@ -5,7 +6,8 @@ import { git_ignore_lines } from '../ignore/lines.mjs';
 export async function git_exclude_ignore() {
     arguments_assert(arguments, []);
     let {lines} = await git_ignore_lines();
-    assert(!list_length_is_0());
+    assert(!list_length_is_0(lines));
     for (let line of lines) {
+        await git_exclude(line);
     }
 }
