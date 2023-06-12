179733ead4b874131488b32f8030adc1f65eb014
JaredMathis <jared.mathis@native.consulting>
2023-06-12 12:24:59 -0400

    #91 function_delete command_line_try

--- src/command/line/try.mjs
+++
@@ -1,9 +0,0 @@
-import { string_empty_not_is } from '../../string/empty/not/is.mjs';
-import { arguments_assert } from '../../arguments/assert.mjs';
-import { metadata } from '../../metadata.mjs';
-import { command_line } from '../line.mjs';
-export async function command_line_try(command_text) {
-    arguments_assert(arguments, [string_empty_not_is]);
-    await command_line(command_text);
-    metadata([]);
-}
