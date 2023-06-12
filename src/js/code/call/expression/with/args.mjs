cf24acfbc800a302566464724157290a5d8c2c6b
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:38:59 -0400

    #92 function_auto js_code_call_expression_with_args

--- src/js/code/call/expression/with/args.mjs
+++ src/js/code/call/expression/with/args.mjs
@@ -1,7 +1,16 @@
+import { js_code_comma_join } from '../../../comma/join.mjs';
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { js_code_call_expression_with_args_code } from './args/code.mjs';
 import { list_join } from '../../../../../list/join.mjs';
 export function js_code_call_expression_with_args(call_expression_function_name, params) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let params_code2 = list_join(params, ', ');
+    if (false)
+        js_code_comma_join(params);
     const statment_code = js_code_call_expression_with_args_code(call_expression_function_name, params_code2);
     return statment_code;
 }
