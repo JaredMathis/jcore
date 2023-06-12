cd1d1fa305800c9d6c16f47af2dcc920a97e5837
JaredMathis <jared.mathis@native.consulting>
2023-06-12 17:27:15 -0400

    #92 function_auto js_node_is_type

--- src/js/node/is/type.mjs
+++ src/js/node/is/type.mjs
@@ -1,7 +1,6 @@
-import { js_node_property_type } from '../property/type.mjs';
+import { js_node_type_get } from '../type/get.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
-import { object_property_get } from '../../../object/property/get.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 import { string_empty_not_is } from '../../../string/empty/not/is.mjs';
 export function js_node_is_type(node, type_expected) {
@@ -9,7 +8,7 @@ export function js_node_is_type(node, type_expected) {
         defined_is,
         string_empty_not_is
     ]);
-    const type_actual = object_property_get(node, js_node_property_type());
+    const type_actual = js_node_type_get(node);
     return type_actual === type_expected;
     metadata([]);
 }
