9eaadfaae56f5f9979e08fa0f21f007eab8031fc
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:34:54 -0400

    #92 function_auto function_inputs

--- src/function/inputs.mjs
+++ src/function/inputs.mjs
@@ -1,8 +1,12 @@
+import { log } from '../log.mjs';
+import { js_node_property_params } from '../js/node/property/params.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { function_to_declaration } from './to/declaration.mjs';
+import { object_property_get } from '../object/property/get.mjs';
 export async function function_inputs(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let fd = await function_to_declaration(function_name);
-    console.log({fd})
+    let params = object_property_get(fd, js_node_property_params());
+    console.log({ params });
 }
