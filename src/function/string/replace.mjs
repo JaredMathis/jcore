7a24a76952ece60527e34cb8ac28c53b11c622df
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:24:23 -0400

    #92 function_auto function_string_replace

--- src/function/string/replace.mjs
+++ src/function/string/replace.mjs
@@ -1,8 +1,9 @@
-import { error } from '../../error.mjs';
+import { refactor_string_replace } from '../../refactor/string/replace.mjs';
+import { function_name_get } from '../name/get.mjs';
+import { function_map_with_args } from '../map/with/args.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
-export function function_string_replace(function_name) {
+export async function function_string_replace(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    await function_map_with_args(function_name_get(refactor_string_replace), function_name, {
-    });
+    await function_map_with_args(function_name_get(refactor_string_replace), function_name, {});
 }
