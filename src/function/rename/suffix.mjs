b719b524e394d7d3a691034031f9158b0fb1fa73
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:44:45 -0400

    #92 function_auto function_rename_suffix

--- src/function/rename/suffix.mjs
+++ src/function/rename/suffix.mjs
@@ -4,6 +4,6 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 export function function_rename_suffix(function_name_old, suffix_old, suffix_new) {
     arguments_assert(arguments, [string_identifier_is]);
-    string_suffix_replace(function_name_old, suffix_old, suffix_new);
+    let function_name_new = string_suffix_replace(function_name_old, suffix_old, suffix_new);
     error('todo: function_rename_prefix');
 }
