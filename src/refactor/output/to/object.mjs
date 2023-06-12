994251fc368e57aa37d79981b9c7bfcbee188904
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:39:30 -0400

    #92 function_auto refactor_output_to_object

--- src/refactor/output/to/object.mjs
+++ src/refactor/output/to/object.mjs
@@ -1,17 +1,12 @@
+import { js_visit_nodes_filter_node } from '../../../js/visit/nodes/filter/node.mjs';
+import { list_new_then } from '../../../list/new/then.mjs';
 import { js_node_is_return_statment } from '../../../js/node/is/return/statment.mjs';
-import { log } from '../../../log.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { js_visit_nodes_filter } from '../../../js/visit/nodes/filter.mjs';
 export function refactor_output_to_object(args) {
     arguments_assert(arguments, [defined_is]);
     let {parsed, function_declaration} = args;
-    js_visit_nodes_filter(parsed, js_node_is_return_statment, v => {
-        let {node} = v;
-        console.log({ t: node.type });
+    list_new_then(function then(list_new_then_add) {
+        js_visit_nodes_filter_node(parsed, js_node_is_return_statment, list_new_then_add);
     });
-    list_new_then(then);
-    function then(list_new_then_add) {
-        js_visit_nodes_call_expression_name_equal(parsed, name_expected, list_new_then_add);
-    }
 }
