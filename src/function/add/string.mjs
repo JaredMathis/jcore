edfc827c9ebc34df99b0aa8c432b75778877ea50
JaredMathis <jared.mathis@native.consulting>
2023-06-12 14:22:32 -0400

    #92 function_auto function_add_string

--- src/function/add/string.mjs
+++ src/function/add/string.mjs
@@ -2,7 +2,7 @@ import { js_code_expression_string } from '../../js/code/expression/string.mjs';
 import { js_code_return_statement } from '../../js/code/return/statement.mjs';
 import { function_add_with_statement } from './with/statement.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function function_add_string() {
+export async function function_add_string(function_name, string_value) {
     arguments_assert(arguments, []);
     let expression = js_code_expression_string();
     return await function_add_with_statement(function_name, js_code_return_statement(expression));
