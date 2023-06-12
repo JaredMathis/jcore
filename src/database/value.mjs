e7787bf1bf43c104fab7396f2d9e2104cb15e622
JaredMathis <jared.mathis@native.consulting>
2023-06-12 10:45:00 -0400

    #90 function_auto database_value

--- src/database/value.mjs
+++ src/database/value.mjs
@@ -1,7 +1,7 @@
 import { database_property_value } from './property/value.mjs';
-import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function database_value(commit_files) {
-    arguments_assert(arguments, [arguments_assert_todo]);
-    return { [database_property_value()]: commit_files };
+import { defined_is } from '../defined/is.mjs';
+export function database_value(value) {
+    arguments_assert(arguments, [defined_is]);
+    return { [database_property_value()]: value };
 }
