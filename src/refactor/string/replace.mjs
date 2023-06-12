42b6f04827f458e68b3048da08369f5d15d5eb99
JaredMathis <jared.mathis@native.consulting>
2023-06-12 17:28:01 -0400

    #92 function_auto refactor_string_replace

--- src/refactor/string/replace.mjs
+++ src/refactor/string/replace.mjs
@@ -1,11 +1,20 @@
-import { log } from '../../log.mjs';
+import { js_node_type_get } from '../../js/node/type/get.mjs';
 import { js_visit_nodes_all } from '../../js/visit/nodes/all.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
+import { list_new_then } from '../../list/new/then.mjs';
+import { list_contains } from '../../list/contains.mjs';
 export function refactor_string_replace(args) {
     arguments_assert(arguments, [defined_is]);
     let {string_value, replacement_function_name, parsed} = args;
-    js_visit_nodes_all(parsed, v => {
-        console.log({ v });
+    let types = list_new_then(list_then_add => {
+        js_visit_nodes_all(parsed, v => {
+            let {node} = v;
+            let type = js_node_type_get(node);
+            if (!list_contains(type)) {
+                list_then_add(type);
+            }
+        });
     });
+    return types;
 }
