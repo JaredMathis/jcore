a04c073eb65f8e653b924d57fe2122bc5bbbeca3
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:10:59 -0400

    #92 function_auto refactor_output_to_object

--- src/refactor/output/to/object.mjs
+++ src/refactor/output/to/object.mjs
@@ -1,3 +1,4 @@
+import { js_callable_multiple_assert_not } from '../../../js/callable/multiple/assert/not.mjs';
 import { js_node_is_return_statment } from '../../../js/node/is/return/statment.mjs';
 import { log } from '../../../log.mjs';
 import { js_visit_nodes_all } from '../../../js/visit/nodes/all.mjs';
@@ -6,6 +7,7 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 export function refactor_output_to_object(args) {
     arguments_assert(arguments, [defined_is]);
     let {parsed, function_declaration} = args;
+    js_callable_multiple_assert_not(parsed);
     js_visit_nodes_all(parsed, v => {
         let {node} = v;
         js_node_is_return_statment(node);
