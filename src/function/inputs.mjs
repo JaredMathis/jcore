abce6873c8d7c912aab577c0baa92ff5c4523876
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:32:40 -0400

    #92 function_auto function_inputs

--- src/function/inputs.mjs
+++ src/function/inputs.mjs
@@ -1,7 +1,7 @@
 import { string_identifier_is } from '../string/identifier/is.mjs';
-import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function function_inputs(function_name) {
+import { function_to_declaration } from './to/declaration.mjs';
+export async function function_inputs(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    error('todo: function_inputs');
+    await function_to_declaration(function_name);
 }
