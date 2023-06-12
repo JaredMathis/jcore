a622f177e90daccecb17f449acab2925c8e2116d
JaredMathis <jared.mathis@native.consulting>
2023-06-12 18:09:27 -0400

    #92 function_auto object_each_filter

--- src/object/each/filter.mjs
+++ src/object/each/filter.mjs
@@ -1,21 +1,22 @@
 import { defined_is } from '../../defined/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
-import { list_add } from '../../list/add.mjs';
 import { object_each } from '../each.mjs';
 import { function_is } from '../../function/is.mjs';
+import { list_new_then } from '../../list/new/then.mjs';
 export function object_each_filter(object, predicate) {
     arguments_assert(arguments, [
         defined_is,
         function_is
     ]);
-    let result = [];
-    let lambda = (value, key) => {
-        if (predicate(value, key)) {
-            list_add(result, key);
-        }
-    };
-    object_each(object, lambda);
+    let result = list_new_then(list_new_then_add => {
+        let lambda = (value, key) => {
+            if (predicate(value, key)) {
+                list_new_then_add(key);
+            }
+        };
+        object_each(object, lambda);
+    });
     return result;
     metadata([]);
 }
