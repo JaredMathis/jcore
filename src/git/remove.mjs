fe2283165aa3959ab8b231823edae5245f9d5683
JaredMathis <jared.mathis@native.consulting>
2023-06-12 12:27:12 -0400

    #91 function_auto git_remove

--- src/git/remove.mjs
+++ src/git/remove.mjs
@@ -1,7 +1,7 @@
+import { command_line_try } from '../command/line/try.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-import { command_line } from '../command/line.mjs';
 import { path_is } from '../path/is.mjs';
 export async function git_remove(path) {
     arguments_assert(arguments, [path_is]);
-    await command_line(`git rm -r --cached ${ path }`);
+    await command_line_try(`git rm -r --cached ${ path }`);
 }
