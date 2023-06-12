326d19fbe12178f7f5d879b1320a51e43f970825
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:22:07 -0400

    #92 function_auto js_call_expressions_named

--- src/js/call/expressions/named.mjs
+++ src/js/call/expressions/named.mjs
@@ -1,8 +1,8 @@
+import { js_node_call_expression_name_equal } from '../../node/call/expression/name/equal.mjs';
 import { js_node_is_program } from '../../node/is/program.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_add } from '../../../list/add.mjs';
-import { js_call_expression_to_name_or_null } from '../expression/to/name/or/null.mjs';
 import { js_node_is_call_expression } from '../../node/is/call/expression.mjs';
 import { js_visit_nodes_filter } from '../../visit/nodes/filter.mjs';
 export function js_call_expressions_named(parsed, name_expected) {
@@ -13,7 +13,7 @@ export function js_call_expressions_named(parsed, name_expected) {
     let matches = [];
     js_visit_nodes_filter(parsed, node => js_node_is_call_expression(node), v => {
         let {node} = v;
-        if (js_call_expression_to_name_or_null(node, name_expected)) {
+        if (js_node_call_expression_name_equal(node, name_expected)) {
             list_add(matches, node);
         }
     });
