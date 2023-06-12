5b9bda4587123b0fd1ab735286afc08c36795803
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:16:22 -0400

    #92 function_auto function_add_string_multiple

--- src/function/add/string/multiple.mjs
+++ src/function/add/string/multiple.mjs
@@ -9,6 +9,7 @@ import { assert } from '../../../assert.mjs';
 import { list_length } from '../../../list/length.mjs';
 import { range } from '../../../range.mjs';
 import { list_get } from '../../../list/get.mjs';
+import { string_split_comma } from '../../../string/split/comma.mjs';
 export async function function_add_string_multiple(prefix, keys_unparsed, values_unparsed) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -16,6 +17,7 @@ export async function function_add_string_multiple(prefix, keys_unparsed, values
         string_is
     ]);
     let keys = string_identifier_multiple_parse(keys_unparsed);
+    let values = string_split_comma(values_unparsed);
     assert(equal_by(list_length, keys, values));
     for (let i of range(list_length(keys))) {
         let key = list_get(keys, i);
