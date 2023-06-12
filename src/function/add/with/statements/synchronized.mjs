8101d0e9a8efc495067956b7577c6874c59a74eb
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:25:30 -0400

    #92 function_auto function_add_with_statements_synchronized

--- src/function/add/with/statements/synchronized.mjs
+++ src/function/add/with/statements/synchronized.mjs
@@ -1,3 +1,4 @@
+import { js_export_function_single } from '../../../../js/export/function/single.mjs';
 import { boolean_is } from '../../../../boolean/is.mjs';
 import { list_is } from '../../../../list/is.mjs';
 import { string_identifier_is } from '../../../../string/identifier/is.mjs';
@@ -6,7 +7,6 @@ import { metadata } from '../../../../metadata.mjs';
 import { function_write } from '../../../write.mjs';
 import { js_function_declaration_asyncify } from '../../../../js/function/declaration/asyncify.mjs';
 import { js_function_declaration_statements_initialize } from '../../../../js/function/declaration/statements/initialize.mjs';
-import { js_exported_function_declaration_single } from '../../../../js/exported/function/declaration/single.mjs';
 import { js_parse } from '../../../../js/parse.mjs';
 import { js_function_name_to_export } from '../../../../js/function/name/to/export.mjs';
 export async function function_add_with_statements_synchronized(function_name, statements, is_async) {
@@ -17,7 +17,7 @@ export async function function_add_with_statements_synchronized(function_name, s
     ]);
     const code = js_function_name_to_export(function_name);
     let parsed = js_parse(code);
-    let fd = js_exported_function_declaration_single(parsed);
+    let fd = js_export_function_single(parsed);
     js_function_declaration_statements_initialize(fd, statements);
     if (is_async) {
         js_function_declaration_asyncify(fd);
