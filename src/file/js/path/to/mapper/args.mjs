a549fc6783619227ddf17ae0bbef69ccb524c4ff
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:26:56 -0400

    #92 function_auto file_js_path_to_mapper_args

--- src/file/js/path/to/mapper/args.mjs
+++ src/file/js/path/to/mapper/args.mjs
@@ -1,9 +1,12 @@
+import { file_js_parse_with_file_path } from '../../../parse/with/file/path.mjs';
 import { path_is } from '../../../../../path/is.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { metadata } from '../../../../../metadata.mjs';
 import { file_js_parse } from '../../../parse.mjs';
 export async function file_js_path_to_mapper_args(file_path) {
     arguments_assert(arguments, [path_is]);
+    if (false)
+        file_js_parse_with_file_path;
     let parsed = await file_js_parse(file_path);
     let args = {
         parsed,
