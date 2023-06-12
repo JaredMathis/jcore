541ee281ecc0f9d06eeab48faa92052e7f0e3042
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:31:59 -0400

    #92 function_auto file_js_overwrite

--- src/file/js/overwrite.mjs
+++ src/file/js/overwrite.mjs
@@ -3,7 +3,6 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { js_unparse } from '../../js/unparse.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
-import { file_overwrite } from '../overwrite.mjs';
 import { defined_is } from '../../defined/is.mjs';
 export async function file_js_overwrite(args) {
     arguments_assert(arguments, [defined_is]);
@@ -11,9 +10,6 @@ export async function file_js_overwrite(args) {
     let parsed = object_property_get(args, 'parsed');
     let unparsed_old = object_property_get(args, 'unparsed');
     let unparsed_new = js_unparse(parsed);
-    if (false) {
-        file_overwrite_if_changed;
-    }
-    await file_overwrite(file_path, unparsed_new);
+    await file_overwrite_if_changed(file_path, unparsed_new, unparsed_old);
     metadata([]);
 }
