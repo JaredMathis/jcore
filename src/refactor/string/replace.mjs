65d08ffb8b8941adbf1bc4a84f894fc43b8c501a
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:23:58 -0400

    #92 function_auto refactor_string_replace

--- src/refactor/string/replace.mjs
+++ src/refactor/string/replace.mjs
@@ -1,6 +1,7 @@
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function refactor_string_replace() {
-    arguments_assert(arguments, []);
+import { defined_is } from '../../defined/is.mjs';
+export function refactor_string_replace(args) {
+    arguments_assert(arguments, [defined_is]);
     error('todo: refactor_string_replace');
 }
