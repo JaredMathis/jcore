db0e1f3c985cea84e162dfd2e637dbc1e30b643e
JaredMathis <jared.mathis@native.consulting>
2023-06-12 14:56:47 -0400

    #92 function_auto function_properties_expand

--- src/function/properties/expand.mjs
+++ src/function/properties/expand.mjs
@@ -1,13 +1,15 @@
+import { string_identifier_multiple_parse } from '../../string/identifier/multiple/parse.mjs';
 import { function_map_with_args } from '../map/with/args.mjs';
 import { function_name_get } from '../name/get.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_is } from '../../string/is.mjs';
-export async function function_properties_expand(function_name, identifier, properties) {
+export async function function_properties_expand(function_name, identifier, property_names) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is,
         string_is
     ]);
+    let split = string_identifier_multiple_parse(property_names);
     await function_map_with_args(function_name_get(refactor_properties_expand), function_name);
 }
