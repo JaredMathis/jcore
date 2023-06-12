e02bd43e9ddde5f4ca99ea4dc8640a058964820e
JaredMathis <jared.mathis@native.consulting>
2023-06-12 12:23:43 -0400

    #91 noop

--- src/command/line.mjs
+++ src/command/line.mjs
@@ -3,9 +3,13 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { assert } from '../assert.mjs';
 import { command_line_check } from './line/check.mjs';
+import { log } from '../log.mjs';
 export async function command_line(command) {
     arguments_assert(arguments, [string_empty_not_is]);
     let result = await command_line_check(command);
+    if (!result.success) {
+        log(result)
+    }
     assert(result.success);
     return result;
     metadata([]);
