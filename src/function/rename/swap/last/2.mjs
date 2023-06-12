fe2f0c57e82104141cc2ba35cc87d0b20a055670
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:48:43 -0400

    #92 function_auto function_rename_swap_last_2

--- src/function/rename/swap/last/2.mjs
+++ src/function/rename/swap/last/2.mjs
@@ -1,5 +1,7 @@
+import { string_identifier_parts } from '../../../../string/identifier/parts.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 export async function function_rename_swap_last_2(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
+    let parts = string_identifier_parts(function_name);
 }
