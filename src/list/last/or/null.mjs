6baa98c36a4e6a70bdf87ce97d342b2d9b4bc724
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:07:13 -0400

    #92 function_auto list_last_or_null

--- src/list/last/or/null.mjs
+++ src/list/last/or/null.mjs
@@ -1,13 +1,13 @@
+import { list_length_at_least } from '../../length/at/least.mjs';
 import { list_is } from '../../is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { list_last } from '../../last.mjs';
-import { list_length } from '../../length.mjs';
 export function list_last_or_null(list) {
     arguments_assert(arguments, [list_is]);
     let parent = null;
     const value = 1;
-    if (list_length(list) >= value) {
+    if (list_length_at_least(list, value)) {
         parent = list_last(list);
     }
     return parent;
---
+++ src/list/length/at/least.mjs
@@ -0,0 +1,4 @@
+import { list_length } from '../../length.mjs';
+export function list_length_at_least(list, value) {
+    return list_length(list) >= value;
+}
