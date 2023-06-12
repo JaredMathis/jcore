2af844807d99bc18c2c49b1660a68c20f6e40a41
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:26:51 -0400

    #92 function_auto js_exported_function_declaration_single_statements_initialize

--- src/js/exported/function/declaration/single/statements/initialize.mjs
+++ src/js/exported/function/declaration/single/statements/initialize.mjs
@@ -1,15 +1,15 @@
+import { js_export_function_single } from '../../../../../export/function/single.mjs';
 import { js_node_is_program } from '../../../../../node/is/program.mjs';
 import { list_is } from '../../../../../../list/is.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
 import { metadata } from '../../../../../../metadata.mjs';
 import { js_function_declaration_statements_initialize } from '../../../../../function/declaration/statements/initialize.mjs';
-import { js_exported_function_declaration_single } from '../../single.mjs';
 export function js_exported_function_declaration_single_statements_initialize(parsed, function_body_statements_new) {
     arguments_assert(arguments, [
         js_node_is_program,
         list_is
     ]);
-    let fd = js_exported_function_declaration_single(parsed);
+    let fd = js_export_function_single(parsed);
     js_function_declaration_statements_initialize(fd, function_body_statements_new);
     metadata([]);
 }
