5719e97658179b3958bca573ce1f004fa2addf55
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:19:45 -0400

    #92 function_auto function_string_replace

---
+++ src/function/string/replace.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function function_string_replace() {
+    arguments_assert(arguments, []);
+    error('todo: function_string_replace');
+}
