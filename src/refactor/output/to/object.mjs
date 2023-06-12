c9935f10ccb9b323037fc4e1b8f4aeec244a9980
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:05:20 -0400

    #92 function_auto refactor_output_to_object

---
+++ src/refactor/output/to/object.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function refactor_output_to_object() {
+    arguments_assert(arguments, []);
+    error('todo: refactor_output_to_object');
+}
