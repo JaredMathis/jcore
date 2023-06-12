2ccaa100b9a3210e3b8064f126607b9a6df7efc6
JaredMathis <jared.mathis@native.consulting>
2023-06-12 18:00:13 -0400

    #92 function_auto function_search

--- src/function/search.mjs
+++ src/function/search.mjs
@@ -1,3 +1,4 @@
+import { string_identifier_sub_multiple_parse } from '../string/identifier/sub/multiple/parse.mjs';
 import { function_name_all_tests_not } from './name/all/tests/not.mjs';
 import { string_identifier_sub_is } from '../string/identifier/sub/is.mjs';
 import { list_single } from '../list/single.mjs';
@@ -7,6 +8,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { string_includes } from '../string/includes.mjs';
 export async function function_search(query) {
     arguments_assert(arguments, [string_identifier_sub_is]);
+    let parts = string_identifier_sub_multiple_parse(query);
     const all = await function_name_all_tests_not();
     let substrings = list_filter(all, a => {
         return string_includes(a, query);
