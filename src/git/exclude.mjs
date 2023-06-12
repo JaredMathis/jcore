2d887abeb4d3cfa11ac15a3f3178a92129e1be37
JaredMathis <jared.mathis@native.consulting>
2023-06-12 12:29:38 -0400

    #91 git_exclude tc

--- src/git/exclude.mjs
+++ src/git/exclude.mjs
@@ -2,8 +2,11 @@ import { git_ignore_add_if_not_exists } from './ignore/add/if/not/exists.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { path_is } from '../path/is.mjs';
 import { git_remove } from './remove.mjs';
+import { assert } from '../assert.mjs';
+import { path_exists } from '../path/exists.mjs';
 export async function git_exclude(path) {
     arguments_assert(arguments, [path_is]);
+    assert(await path_exists(path));
     await git_remove(path);
     await git_ignore_add_if_not_exists(path);
 }
