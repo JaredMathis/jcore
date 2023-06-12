cf8a764b58f066a571e1684e8b5c4da27b7c9adc
JaredMathis <jared.mathis@native.consulting>
2023-06-12 12:30:11 -0400

    #91 function_auto git_exclude_ignore

---
+++ src/git/exclude/ignore.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function git_exclude_ignore() {
+    arguments_assert(arguments, []);
+    error('todo: git_exclude_ignore');
+}
