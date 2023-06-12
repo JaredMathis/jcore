11159d563aa1492c26ea54fb1f1f8809f00b8092
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:04:14 -0400

    #92 function_search with_prefix

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -3,7 +3,7 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
 export function refactor_properties_expand(args) {
     arguments_assert(arguments, [defined_is]);
-    let {identifier, properties} = args;
+    let {identifier, properties, prefix} = args;
     console.log({
         identifier,
         properties
