ad3c0140b9726a0266e95d94cb5d6fb5c07360f8
JaredMathis <jared.mathis@native.consulting>
2023-06-12 17:42:25 -0400

    #92 function_auto file_write

--- src/file/write.mjs
+++ src/file/write.mjs
@@ -1,16 +1,15 @@
-import { assert } from '../assert.mjs';
+import { file_exists_not_assert } from './exists/not/assert.mjs';
 import { string_is } from '../string/is.mjs';
 import { path_is } from '../path/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { file_overwrite } from './overwrite.mjs';
-import { file_exists } from './exists.mjs';
 export async function file_write(file_path, contents) {
     arguments_assert(arguments, [
         path_is,
         string_is
     ]);
-    assert(!await file_exists(file_path));
+    await file_exists_not_assert(file_path);
     await file_overwrite(file_path, contents);
     metadata([]);
 }
