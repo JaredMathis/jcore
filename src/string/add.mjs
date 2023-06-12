70b7ec07bcd92adf018ba11f8280b143e885c094
JaredMathis <jared.mathis@native.consulting>
2023-06-12 14:30:35 -0400

    #92 function_auto string_add

--- src/string/add.mjs
+++ src/string/add.mjs
@@ -1,3 +1,9 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 export function string_add(result, s) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     return result + s;
 }
