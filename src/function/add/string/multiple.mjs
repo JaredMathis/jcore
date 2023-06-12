b3ee5ff4da8b6121ce61bb684bfbf709b41c38b9
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:18:47 -0400

    #92 function_auto function_add_string_multiple

--- src/function/add/string/multiple.mjs
+++ src/function/add/string/multiple.mjs
@@ -1,20 +1,24 @@
+import { string_identifier_with_prefix } from '../../../string/identifier/with/prefix.mjs';
 import { equal_by } from '../../../equal/by.mjs';
 import { function_add_string_prefix } from './prefix.mjs';
-import { string_identifier_multiple_parse } from '../../../string/identifier/multiple/parse.mjs';
 import { string_is } from '../../../string/is.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { assert } from '../../../assert.mjs';
 import { list_length } from '../../../list/length.mjs';
-export function function_add_string_multiple(prefix, keys, values) {
+import { range } from '../../../range.mjs';
+import { list_get } from '../../../list/get.mjs';
+export async function function_add_string_multiple(prefix, keys, values) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_is,
         string_is
     ]);
     assert(equal_by(list_length, keys, values));
-    let split = string_identifier_multiple_parse(values);
-    for (let value of split) {
-        function_add_string_prefix(prefix, value);
+    for (let i of range(list_length(keys))) {
+        let key = list_get(keys, i);
+        let value = list_get(values, i);
+        let function_name = string_identifier_with_prefix(prefix, key);
+        await function_add_string_prefix(function_name, value);
     }
 }
