e272d9baa5e7b70ff2192958dc69ec6fb750b854
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:19:28 -0400

    #92 function_copy js_node_call_expression_if_name_equal js_node_call_expression_name_equal

---
+++ src/js/node/call/expression/name/equal.mjs
@@ -0,0 +1,24 @@
+import { metadata } from '../../../../../metadata.mjs';
+import { equal } from '../../../../../equal.mjs';
+import { js_call_expression_to_name_or_null } from '../../../../call/expression/to/name/or/null.mjs';
+import { js_node_is_call_expression } from '../../../is/call/expression.mjs';
+import { function_is } from '../../../../../function/is.mjs';
+import { defined_is } from '../../../../../defined/is.mjs';
+import { js_node_is } from '../../../is.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export function js_node_call_expression_name_equal(node, name_expected, on_name_equal) {
+    arguments_assert(arguments, [
+        js_node_is,
+        defined_is,
+        function_is
+    ]);
+    if (js_node_is_call_expression(node)) {
+        let name_actual = js_call_expression_to_name_or_null(node);
+        if (name_actual != null) {
+            if (equal(name_actual, name_expected)) {
+                on_name_equal();
+            }
+        }
+    }
+    metadata([]);
+}
