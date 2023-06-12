4505320d8bfa2206c8428005f61588dadbbb73a8
JaredMathis <jared.mathis@native.consulting>
2023-06-12 18:30:28 -0400

    #92 function_auto function_input_add

--- src/function/input/add.mjs
+++ src/function/input/add.mjs
@@ -1,6 +1,7 @@
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_input_add() {
-    arguments_assert(arguments, []);
+import { string_identifier_is } from '../../string/identifier/is.mjs';
+export function function_input_add(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
     error('todo: function_input_add');
 }
