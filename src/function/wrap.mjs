74154940d7a59489d3043164c1d35c49b655a34a
JaredMathis <jared.mathis@native.consulting>
2023-06-12 17:55:10 -0400

    #92 function_auto function_wrap

--- src/function/wrap.mjs
+++ src/function/wrap.mjs
@@ -1,6 +1,10 @@
+import { string_identifier_is } from '../string/identifier/is.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function function_wrap() {
-    arguments_assert(arguments, []);
+export function function_wrap(function_name_to_wrap, function_name_to_add) {
+    arguments_assert(arguments, [
+        string_identifier_is,
+        string_identifier_is
+    ]);
     error('todo: function_wrap');
 }
