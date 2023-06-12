09cb7a3041512d3ddba19b1f7c337c95bc2d0603
JaredMathis <jared.mathis@native.consulting>
2023-06-12 14:37:52 -0400

    #92 function_auto js_code_return_statement

--- src/js/code/return/statement.mjs
+++ src/js/code/return/statement.mjs
@@ -1,6 +1,8 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { js_statement_end } from '../../statement/end.mjs';
 import { js_keyword_return } from '../../keyword/return.mjs';
-import { js_parse_statement } from '../../parse/statement.mjs';
 export function js_code_return_statement(return_expression) {
-    return js_parse_statement(`${ js_keyword_return() } ${ return_expression }${ js_statement_end() }`);
+    arguments_assert(arguments, [arguments_assert_todo]);
+    return `${ js_keyword_return() } ${ return_expression }${ js_statement_end() }`;
 }
