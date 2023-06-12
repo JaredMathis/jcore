f346f7e2c2bdb3174ed00df489867051ef1970d7
JaredMathis <jared.mathis@native.consulting>
2023-06-12 09:45:50 -0400

    #90 function_auto version_push_latest

--- src/version/push/latest.mjs
+++ src/version/push/latest.mjs
@@ -1,10 +1,14 @@
+import { version_list_file_add } from '../list/file/add.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { version_file_contents_each } from '../file/contents/each.mjs';
 import { version_files_paths } from '../files/paths.mjs';
 export async function version_push_latest(repository_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let latest_files = [];
     let file_paths = await version_files_paths(repository_name);
     await version_file_contents_each(repository_name, file_paths, async (file_path, contents) => {
-        console.log({contents})
+        version_list_file_add(latest_files, file_path, contents);
     });
     return latest_files;
 }
