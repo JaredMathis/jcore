a531b6b9ff4c10abe8035a9c10f875eda9d5ce3d
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:08:29 -0400

    #92 function_auto refactor_output_to_object

--- src/refactor/output/to/object.mjs
+++ src/refactor/output/to/object.mjs
@@ -1,6 +1,12 @@
+import { log } from '../../../log.mjs';
+import { js_visit_nodes_all } from '../../../js/visit/nodes/all.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export function refactor_output_to_object(args) {
     arguments_assert(arguments, [defined_is]);
     let {parsed, function_declaration} = args;
+    js_visit_nodes_all(parsed, v => {
+        let {node} = v;
+        console.log({ node });
+    });
 }
