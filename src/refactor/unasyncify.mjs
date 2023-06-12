4309d94d34fb2e9d06f6323bb02dcc51db089ab0
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:10:48 -0400

    #92 function_auto refactor_unasyncify

--- src/refactor/unasyncify.mjs
+++ src/refactor/unasyncify.mjs
@@ -1,3 +1,4 @@
+import { js_callable_multiple_assert_not } from '../js/callable/multiple/assert/not.mjs';
 import { refactor_metadata_generated_add_function } from './metadata/generated/add/function.mjs';
 import { defined_is } from '../defined/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
@@ -10,18 +11,14 @@ import { metadata } from '../metadata.mjs';
 import { js_keyword_async } from '../js/keyword/async.mjs';
 import { object_property_set } from '../object/property/set.mjs';
 import { refactor_unasyncify_each } from './unasyncify/each.mjs';
-import { js_callable_multiple } from '../js/callable/multiple.mjs';
 import { js_visit_nodes_all } from '../js/visit/nodes/all.mjs';
-import { assert } from '../assert.mjs';
-import { comment } from '../comment.mjs';
 import { string_ends_with } from '../string/ends/with.mjs';
 import { string_suffix_without } from '../string/suffix/without.mjs';
 import { js_function_declaration_to_name } from '../js/function/declaration/to/name.mjs';
 export async function refactor_unasyncify(args) {
     arguments_assert(arguments, [defined_is]);
     let {parsed, function_declaration} = args;
-    comment(`To remove this assert, fix the code - right now it does not handle functions or arrows inside the exported function`);
-    assert(!js_callable_multiple(parsed));
+    js_callable_multiple_assert_not(parsed);
     js_visit_nodes_all(parsed, refactor_unasyncify_each);
     object_property_set(function_declaration, js_keyword_async(), false);
     await refactor_metadata_generated_add_function(args);
