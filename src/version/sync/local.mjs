bdb17ce17d073d681a7a2c8c7da42387b47f00d6
JaredMathis <jared.mathis@native.consulting>
2023-06-12 12:56:07 -0400

    #91 function_rename version_commit_and_removals version_sync_local

---
+++ src/version/sync/local.mjs
@@ -0,0 +1,20 @@
+import { arguments_assert } from '../../arguments/assert.mjs';
+import { version_write_all } from '../write/all.mjs';
+import { list_multiple_combine } from '../../list/multiple/combine.mjs';
+import { version_commit_files_all_removals_generic } from '../commit/files/all/removals/generic.mjs';
+import { version_commit_files_difference } from '../commit/files/difference.mjs';
+import { directory_read_current } from '../../directory/read/current.mjs';
+import { version_commit_data } from '../commit/data.mjs';
+import { string_identifier_is } from '../../string/identifier/is.mjs';
+export async function version_sync_local(repository_name) {
+    arguments_assert(arguments, [string_identifier_is]);
+    const commit_data = version_commit_data(version_sync_local, arguments);
+    let file_paths = await directory_read_current();
+    let differences = await version_commit_files_difference(repository_name, file_paths, commit_data);
+    let removals = await version_commit_files_all_removals_generic(repository_name, file_paths, commit_data);
+    let writes = list_multiple_combine([
+        differences,
+        removals
+    ]);
+    await version_write_all(writes);
+}
