505ad994e8ebff21da7a923da86c8e0174a1bc48
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:20:50 -0400

    #92 function_auto refactor_string_replace

--- src/function/string/replace.mjs
+++ src/function/string/replace.mjs
@@ -1,6 +1,11 @@
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_string_replace() {
-    arguments_assert(arguments, []);
-    error('todo: function_string_replace');
+import { string_identifier_is } from '../../string/identifier/is.mjs';
+export function function_string_replace(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
+    await function_map_with_args(function_name_get(refactor_string_replace), function_name, {
+        identifier,
+        prefix,
+        properties
+    });
 }
---
+++ src/refactor/string/replace.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function refactor_string_replace() {
+    arguments_assert(arguments, []);
+    error('todo: refactor_string_replace');
+}
