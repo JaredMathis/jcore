4c323fc70c1fdbb3f2ce9a7976fdaa4fcac4d3fa
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:02:29 -0400

    #92 function_auto function_output_to_object

---
+++ src/function/output/to/object.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function function_output_to_object() {
+    arguments_assert(arguments, []);
+    error('todo: function_output_to_object');
+}
