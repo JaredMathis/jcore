3c5cb1861083abd089f5a6325260c24ae11032a0
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:14:15 -0400

    #92 function_auto function_add_string_multiple

--- src/function/add/string/multiple.mjs
+++ src/function/add/string/multiple.mjs
@@ -3,9 +3,10 @@ import { string_identifier_multiple_parse } from '../../../string/identifier/mul
 import { string_is } from '../../../string/is.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function function_add_string_multiple(prefix, values) {
+export function function_add_string_multiple(prefix, keys, values) {
     arguments_assert(arguments, [
         string_identifier_is,
+        string_is,
         string_is
     ]);
     let split = string_identifier_multiple_parse(values);
