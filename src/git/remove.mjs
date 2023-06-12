777916ac6a862f8edc13ca08b9975b95a2bb3f98
JaredMathis <jared.mathis@native.consulting>
2023-06-12 12:28:36 -0400

    #91 function_auto git_remove

--- src/git/remove.mjs
+++ src/git/remove.mjs
@@ -1,7 +1,13 @@
 import { command_line_try } from '../command/line/try.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { path_is } from '../path/is.mjs';
+import { assert } from '../assert.mjs';
+import { equal } from '../equal.mjs';
 export async function git_remove(path) {
     arguments_assert(arguments, [path_is]);
     let result = await command_line_try(`git rm -r --cached ${ path }`);
+    if (!result.success) {
+        let expected = `fatal: pathspec '${ path }' did not match any files\n`;
+        assert(equal(result.stderr, expected));
+    }
 }
