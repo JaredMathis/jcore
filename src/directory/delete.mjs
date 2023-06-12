e51700820f5ac4b2a481fccf798bd8bf59c2fb24
JaredMathis <jared.mathis@native.consulting>
2023-06-12 11:58:49 -0400

    #90 noop

--- src/directory/delete.mjs
+++ src/directory/delete.mjs
@@ -1,5 +1,6 @@
 import { path_exists } from '../path/exists.mjs';
 import { assert } from '../assert.mjs';
+import fs from 'fs';
 export async function directory_delete(directory_path) {
     assert(await path_exists(directory_path));
     await fs.promises.rmdir(directory_path);
--- src/version/repository/delete/local.mjs
+++ src/version/repository/delete/local.mjs
@@ -2,7 +2,6 @@ import { directory_delete } from '../../../directory/delete.mjs';
 import { version_path_repository } from '../../path/repository.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import fs from 'fs';
 export async function version_repository_delete_local(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let repository_path = version_path_repository(repository_name);
