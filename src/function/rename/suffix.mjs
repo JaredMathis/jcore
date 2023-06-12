5f4b48c4acad410e923eb5840eddc88de965099e
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:41:53 -0400

    #92 noop

--- src/function/rename/suffix.mjs
+++ src/function/rename/suffix.mjs
@@ -1,7 +1,8 @@
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
-export function function_rename_suffix(function_name, suffix_old) {
+export function function_rename_suffix(function_name_old, suffix_old, suffix_new) {
     arguments_assert(arguments, [string_identifier_is]);
+    let function_name_new = 
     error('todo: function_rename_prefix');
 }
