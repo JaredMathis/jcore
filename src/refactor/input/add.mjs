b4aa0421361489cda05817bd999063a24317027e
JaredMathis <jared.mathis@native.consulting>
2023-06-12 19:52:43 -0400

    #92 function_input_add string_identifier_is_generic allow_underscores

--- src/refactor/input/add.mjs
+++ src/refactor/input/add.mjs
@@ -4,7 +4,7 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 export async function refactor_input_add(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let {input_name} = args;
-    let arguments_assert_args = await js_mapper_args_to_statement_arguments_assert_args_predicate(args);
+    let {input_name, function_declaration} = args;
+    let arguments_assert_args = await js_mapper_args_to_statement_arguments_assert_args_predicate(function_declaration);
     console.log({ arguments_assert_args });
 }
