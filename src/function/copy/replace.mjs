85f258d1587b7d7d98a4db53fbd248509c4601f7
JaredMathis <jared.mathis@native.consulting>
2023-06-12 17:42:46 -0400

    #92 function_auto function_copy_replace

--- src/function/copy/replace.mjs
+++ src/function/copy/replace.mjs
@@ -1,3 +1,5 @@
+import { function_delete } from '../delete.mjs';
+import { file_exists_not_assert } from '../../file/exists/not/assert.mjs';
 import { function_copy } from '../copy.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -6,5 +8,7 @@ export async function function_copy_replace(function_name_to_copy, function_name
         string_identifier_is,
         string_identifier_is
     ]);
+    await file_exists_not_assert(file_path);
+    function_delete;
     await function_copy(function_name_to_copy, function_name_of_copy);
 }
