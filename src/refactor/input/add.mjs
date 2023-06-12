17795707ba0d8c41da37e4159bd816d99a46aaa4
JaredMathis <jared.mathis@native.consulting>
2023-06-12 19:47:50 -0400

    #92 function_auto refactor_input_add

--- src/refactor/input/add.mjs
+++ src/refactor/input/add.mjs
@@ -1,8 +1,10 @@
-import { js_mapper_args_to_statement_arguments_assert_args } from '../../js/mapper/args/to/statement/arguments/assert/args.mjs';
+import { log } from '../../log.mjs';
+import { js_mapper_args_to_statement_arguments_assert_args_predicate } from '../../js/mapper/args/to/statement/arguments/assert/args/predicate.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 export async function refactor_input_add(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {input_name} = args;
-    let arguments_assert_args = await js_mapper_args_to_statement_arguments_assert_args(args);
+    let arguments_assert_args = await js_mapper_args_to_statement_arguments_assert_args_predicate(args);
+    console.log({ arguments_assert_args });
 }
