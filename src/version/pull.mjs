574e1ad698c0283c9edc410ebdf9b0816648679e
JaredMathis <jared.mathis@native.consulting>
2023-06-12 10:13:38 -0400

    #90 function_auto version_pull

--- src/version/pull.mjs
+++ src/version/pull.mjs
@@ -1,6 +1,7 @@
-import { error } from '../error.mjs';
+import { version_collection_repository } from './collection/repository.mjs';
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function version_pull() {
-    arguments_assert(arguments, []);
-    error('todo: version_pull');
+export function version_pull(repository_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    let database_collection_name = version_collection_repository(repository_name);
 }
