516d07326de16eff14414c53d8c5c6aeb331cbf6
JaredMathis <jared.mathis@native.consulting>
2023-06-12 11:47:29 -0400

    #90 function_auto version_repository_delete_local

--- src/version/repository/delete/local.mjs
+++ src/version/repository/delete/local.mjs
@@ -1,6 +1,7 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { error } from '../../../error.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function version_repository_delete_local() {
-    arguments_assert(arguments, []);
+export function version_repository_delete_local(repository_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: version_repository_delete_local');
 }
