6cd4dcecbf8b4a7248a628d31858648c94abbb35
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:21:00 -0400

    #92 function_auto refactor_string_replace

--- src/function/string/replace.mjs
+++ src/function/string/replace.mjs
@@ -4,8 +4,5 @@ import { string_identifier_is } from '../../string/identifier/is.mjs';
 export function function_string_replace(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     await function_map_with_args(function_name_get(refactor_string_replace), function_name, {
-        identifier,
-        prefix,
-        properties
     });
 }
