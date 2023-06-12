76db3578e56fc7923115c36d7910cc3b921dd504
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:15:30 -0400

    #92 function_output_to_object version_sync_local_preview_generic

--- src/js/node/is/return/statment.mjs
+++ src/js/node/is/return/statment.mjs
@@ -1,4 +1,4 @@
 import { js_node_is_type } from '../type.mjs';
 export function js_node_is_return_statment(node) {
-    js_node_is_type(node, 'ReturnStatement');
+    return js_node_is_type(node, 'ReturnStatement');
 }
--- src/refactor/output/to/object.mjs
+++ src/refactor/output/to/object.mjs
@@ -3,12 +3,12 @@ import { log } from '../../../log.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { js_visit_nodes } from '../../../js/visit/nodes.mjs';
+import { tautology } from '../../../tautology.mjs';
 export function refactor_output_to_object(args) {
     arguments_assert(arguments, [defined_is]);
     let {parsed, function_declaration} = args;
     js_visit_nodes(parsed, js_node_is_return_statment, v => {
         let {node} = v;
-        js_node_is_return_statment(node);
-        console.log({ node });
+        console.log({ t:node.type });
     });
 }
