5aeb779be9bd9fe1f220252a958c88fa3b3db903
JaredMathis <jared.mathis@native.consulting>
2023-06-12 14:31:57 -0400

    #92 noop

--- src/function/add/string/multiple.mjs
+++ src/function/add/string/multiple.mjs
@@ -2,6 +2,7 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { string_is } from '../../../string/is.mjs';
 import { string_split } from '../../../string/split.mjs';
+import { function_name_separator } from '../../name/separator.mjs';
 import { function_add_string } from '../string.mjs';
 export function function_add_string_multiple(prefix, values) {
     arguments_assert(arguments, [
@@ -10,6 +11,10 @@ export function function_add_string_multiple(prefix, values) {
     ]);
     let split = string_split(values, ',');
     for (let value of split) {
+        let function_name = '';
+        for (let s of [prefix, function_name_separator(), value]) {
+            function_name = string_add(function_name, s)
+        }
         function_add_string(function_name, value);
     }
 }
