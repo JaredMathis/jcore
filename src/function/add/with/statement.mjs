85f122d287140cd66eb7070a4ade9061ebc9de9b
JaredMathis <jared.mathis@native.consulting>
2023-06-12 14:16:11 -0400

    #92 function_auto function_add_with_statement

--- src/function/add/with/statement.mjs
+++ src/function/add/with/statement.mjs
@@ -1,7 +1,13 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_single_item } from '../../../list/single/item.mjs';
 import { function_add_with_statements } from './statements.mjs';
 import { js_parse_statement } from '../../../js/parse/statement.mjs';
-export async function function_add_with_statement(function_name, error_statement_code) {
-    let error_statement = js_parse_statement(error_statement_code);
-    return await function_add_with_statements(function_name, list_single_item(error_statement));
+export async function function_add_with_statement(function_name, statement_code) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let statement = js_parse_statement(statement_code);
+    return await function_add_with_statements(function_name, list_single_item(statement));
 }
