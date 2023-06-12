1aad992a127deba5738dce876e2742ee30f741aa
JaredMathis <jared.mathis@native.consulting>
2023-06-12 17:31:48 -0400

    #92 function_auto function_variable_new

--- src/function/variable/new.mjs
+++ src/function/variable/new.mjs
@@ -1,6 +1,11 @@
-import { error } from '../../error.mjs';
+import { function_name_get } from '../name/get.mjs';
+import { function_map_with_args } from '../map/with/args.mjs';
+import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_variable_new() {
-    arguments_assert(arguments, []);
-    error('todo: function_variable_new');
+export async function function_variable_new(function_name, identifier) {
+    arguments_assert(arguments, [
+        string_identifier_is,
+        string_identifier_is
+    ]);
+    await function_map_with_args(function_name_get(refactor_variable_new), function_name, { identifier });
 }
