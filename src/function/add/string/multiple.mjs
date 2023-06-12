b047b2c4fe7ed7bcb25ee3d49941ae8a54f68a7e
JaredMathis <jared.mathis@native.consulting>
2023-06-12 14:27:46 -0400

    #92 function_auto function_add_string_multiple

--- src/function/add/string/multiple.mjs
+++ src/function/add/string/multiple.mjs
@@ -1,6 +1,15 @@
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { string_identifier_is } from '../../../string/identifier/is.mjs';
+import { string_is } from '../../../string/is.mjs';
 import { string_split } from '../../../string/split.mjs';
-export function function_add_string_multiple() {
-    arguments_assert(arguments, []);
+import { function_add_string } from '../string.mjs';
+export function function_add_string_multiple(prefix, values) {
+    arguments_assert(arguments, [
+        string_identifier_is,
+        string_is
+    ]);
     let split = string_split(values, ',');
+    for (let value of split) {
+        function_add_string(function_name, value);
+    }
 }
