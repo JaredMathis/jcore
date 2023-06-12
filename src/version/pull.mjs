1b967e7cfae4c4fa5c3193a8b789128e22f2f86d
JaredMathis <jared.mathis@native.consulting>
2023-06-12 10:19:07 -0400

    #90 function_auto version_pull

--- src/version/pull.mjs
+++ src/version/pull.mjs
@@ -1,3 +1,4 @@
+import { version_property_commit_latest } from './property/commit/latest.mjs';
 import { database_transaction } from '../database/transaction.mjs';
 import { version_collection_repository } from './collection/repository.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
@@ -5,6 +6,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 export async function version_pull(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let database_collection_name = version_collection_repository(repository_name);
+    const commit_latest = version_property_commit_latest();
     await database_transaction(db, async transaction => {
     });
 }
