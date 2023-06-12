1fba7d5b742a8401b457efcf19a3343e1667c036
JaredMathis <jared.mathis@native.consulting>
2023-06-12 13:27:48 -0400

    #92 function_auto js_call_expressions_named

--- src/js/call/expressions/named.mjs
+++ src/js/call/expressions/named.mjs
@@ -3,7 +3,6 @@ import { js_node_is_program } from '../../node/is/program.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_add } from '../../../list/add.mjs';
-import { js_node_is_call_expression } from '../../node/is/call/expression.mjs';
 import { js_visit_nodes_filter } from '../../visit/nodes/filter.mjs';
 export function js_call_expressions_named(parsed, name_expected) {
     arguments_assert(arguments, [
@@ -11,11 +10,9 @@ export function js_call_expressions_named(parsed, name_expected) {
         defined_is
     ]);
     let matches = [];
-    js_visit_nodes_filter(parsed, node => js_node_is_call_expression(node), v => {
+    js_visit_nodes_filter(parsed, node => js_node_call_expression_name_equal(node, name_expected), v => {
         let {node} = v;
-        if (js_node_call_expression_name_equal(node, name_expected)) {
-            list_add(matches, node);
-        }
+        list_add(matches, node);
     });
     return matches;
 }
