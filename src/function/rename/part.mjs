51326312437c39030d630247f57f86ddc7a5a655
JaredMathis <jared.mathis@native.consulting>
2023-06-12 18:25:03 -0400

    #92 function_auto function_rename_part

--- src/function/rename/part.mjs
+++ src/function/rename/part.mjs
@@ -1,6 +1,10 @@
-import { error } from '../../error.mjs';
+import { function_rename_generic } from './generic.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_rename_part() {
+export async function function_rename_part() {
     arguments_assert(arguments, []);
-    error('todo: function_rename_part');
+    function predicate_should_rename(n) {
+    }
+    await function_rename_generic(predicate_should_rename, function_name_new_get);
+    function function_name_new_get(n_old) {
+    }
 }
