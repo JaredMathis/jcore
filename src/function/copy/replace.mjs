d654d32e4bf8ca48909dd989dac632b4492966d4
JaredMathis <jared.mathis@native.consulting>
2023-06-12 17:46:01 -0400

    #92 function_auto function_copy_replace

--- src/function/copy/replace.mjs
+++ src/function/copy/replace.mjs
@@ -1,4 +1,3 @@
-import { function_copy_before } from './before.mjs';
 import { function_delete } from '../delete.mjs';
 import { function_copy } from '../copy.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
@@ -8,7 +7,6 @@ export async function function_copy_replace(function_name_to_copy, function_name
         string_identifier_is,
         string_identifier_is
     ]);
-    await function_copy_before(function_name_of_copy);
-    function_delete;
+    await function_delete(function_name_of_copy);
     await function_copy(function_name_to_copy, function_name_of_copy);
 }
