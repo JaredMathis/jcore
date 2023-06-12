da6e6a0417c500d7545beec72232a774d38b1498
JaredMathis <jared.mathis@native.consulting>
2023-06-12 10:49:17 -0400

    #90 function_auto version_pull

--- src/version/pull.mjs
+++ src/version/pull.mjs
@@ -1,7 +1,8 @@
+import { log } from '../log.mjs';
+import { database_reference_get_data } from '../database/reference/get/data.mjs';
 import { database_value_get } from '../database/value/get.mjs';
 import { database_reference } from '../database/reference.mjs';
 import { version_document_path_commit } from './document/path/commit.mjs';
-import { database_reference_get } from '../database/reference/get.mjs';
 import { database_document_info_reference } from '../database/document/info/reference.mjs';
 import { database_firestore_get } from '../database/firestore/get.mjs';
 import { version_property_commit_latest } from './property/commit/latest.mjs';
@@ -19,16 +20,16 @@ export async function version_pull(repository_name) {
     const property_commit_latest = version_property_commit_latest();
     let info_refererence = database_document_info_reference(db, database_collection_name);
     await database_transaction(db, async transaction => {
-        let info_data = await database_reference_get(transaction, info_refererence);
-        console.log({info_data})
+        let info_data = await database_reference_get_data(transaction, info_refererence);
+        console.log({ info_data });
         let commit_latest_value = object_property_get(info_data, property_commit_latest);
         for (let i of range(commit_latest_value)) {
             let commit_id = add_1(i);
             let document_path_commit = version_document_path_commit(commit_id);
             let commit_refererence = database_reference(db, database_collection_name, document_path_commit);
-            let commit_data = await database_reference_get(transaction, commit_refererence);
+            let commit_data = await database_reference_get_data(transaction, commit_refererence);
             let commit_value = database_value_get(commit_data);
-            console.log({commit_value})
+            console.log({ commit_value });
         }
     });
 }
