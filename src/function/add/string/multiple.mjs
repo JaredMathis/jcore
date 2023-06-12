a0fb152b6c4be516bf6b810475d785874f57dfbe
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:13:36 -0400

    #92 function_copy function_add_string_multiple_prefix function_add_string_multiple

---
+++ src/function/add/string/multiple.mjs
@@ -0,0 +1,15 @@
+import { function_add_string_prefix } from './prefix.mjs';
+import { string_identifier_multiple_parse } from '../../../string/identifier/multiple/parse.mjs';
+import { string_is } from '../../../string/is.mjs';
+import { string_identifier_is } from '../../../string/identifier/is.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function function_add_string_multiple(prefix, values) {
+    arguments_assert(arguments, [
+        string_identifier_is,
+        string_is
+    ]);
+    let split = string_identifier_multiple_parse(values);
+    for (let value of split) {
+        function_add_string_prefix(prefix, value);
+    }
+}
