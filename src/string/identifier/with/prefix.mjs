f7cebd91672d9b1b4312029acbb1aadccf7f5cb4
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:04:22 -0400

    #92 function_auto string_identifier_with_prefix

--- src/string/identifier/with/prefix.mjs
+++ src/string/identifier/with/prefix.mjs
@@ -1,6 +1,12 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_add_multiple } from '../../add/multiple.mjs';
 import { function_name_separator } from '../../../function/name/separator.mjs';
 export function string_identifier_with_prefix(prefix, identifier) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     const strings = [
         prefix,
         function_name_separator(),
