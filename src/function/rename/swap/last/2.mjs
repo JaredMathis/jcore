b74380d1f40b58fe917d774d963d5633d1f0afe5
JaredMathis <jared.mathis@native.consulting>
2023-06-12 16:04:54 -0400

    #92 function_auto function_rename_swap_last_2

--- src/function/rename/swap/last/2.mjs
+++ src/function/rename/swap/last/2.mjs
@@ -7,7 +7,6 @@ import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 import { assert } from '../../../../assert.mjs';
 import { list_last_index } from '../../../../list/last/index.mjs';
 import { subtract_1 } from '../../../../subtract/1.mjs';
-import { equal } from '../../../../equal.mjs';
 export async function function_rename_swap_last_2(function_name_old) {
     arguments_assert(arguments, [string_identifier_is]);
     let function_name_new = string_identifier_parts_map(function_name_old, function map(parts) {
@@ -16,5 +15,5 @@ export async function function_rename_swap_last_2(function_name_old) {
         let last_index_second = subtract_1(last_index);
         list_swap(parts, last_index, last_index_second);
     });
-    function_rename;
+    await function_rename(function_name_old, function_name_new);
 }
