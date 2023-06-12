9c0999bb073c27103f1699a8c589e738e66abdd7
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:07:41 -0400

    #92 function_auto refactor_properties_expand

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -3,10 +3,13 @@ import { log } from '../../log.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
 import { list_map } from '../../list/map.mjs';
+import { js_function_declaration_to_statements } from '../../js/function/declaration/to/statements.mjs';
 export function refactor_properties_expand(args) {
     arguments_assert(arguments, [defined_is]);
     let {identifier, properties, prefix} = args;
     let property_gets = list_map(properties, p => string_identifier_with_prefix(prefix, p));
+    let {function_declaration} = args;
+    js_function_declaration_to_statements();
     console.log({
         identifier,
         mapped: property_gets,
