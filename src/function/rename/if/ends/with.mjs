71c89987af1172517870bbee4bb866f3a2f7f929
JaredMathis <jared.mathis@native.consulting>
2023-06-12 17:50:50 -0400

    #92 function_auto function_rename_if_ends_with

--- src/function/rename/if/ends/with.mjs
+++ src/function/rename/if/ends/with.mjs
@@ -1,7 +1,7 @@
+import { string_suffix_replace } from '../../../../string/suffix/replace.mjs';
+import { string_ends_with } from '../../../../string/ends/with.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { function_rename_if_generic } from '../generic.mjs';
-import { string_prefix_replace } from '../../../../string/prefix/replace.mjs';
-import { string_starts_with } from '../../../../string/starts/with.mjs';
 import { string_identifier_sub_is } from '../../../../string/identifier/sub/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function function_rename_if_ends_with(prefix_old, prefix_new) {
@@ -9,8 +9,8 @@ export async function function_rename_if_ends_with(prefix_old, prefix_new) {
         string_identifier_sub_is,
         string_identifier_sub_is
     ]);
-    let predicate = string_starts_with;
-    let function_name_map = string_prefix_replace;
+    let predicate = string_ends_with;
+    let function_name_map = string_suffix_replace;
     await function_rename_if_generic(prefix_old, prefix_new, predicate, function_name_map);
     metadata([]);
 }
