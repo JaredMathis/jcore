be856d239440bfac16f319c3315adcce32559ae2
JaredMathis <jared.mathis@native.consulting>
2023-06-12 17:38:49 -0400

    #92 function_auto function_copy_replace

--- src/function/copy/replace.mjs
+++ src/function/copy/replace.mjs
@@ -1,10 +1,10 @@
+import { function_copy } from '../copy.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
-import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_copy_replace(function_name_to_copy, function_name_of_copy) {
+export async function function_copy_replace(function_name_to_copy, function_name_of_copy) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is
     ]);
-    error('todo: function_copy_replace');
+    await function_copy(function_name_to_copy, function_name_of_copy);
 }
