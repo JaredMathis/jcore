79606c70d5058c415a9298d9a88de3857964f32c
JaredMathis <jared.mathis@native.consulting>
2023-06-12 15:42:17 -0400

    #92 function_search replace

--- src/function/rename/suffix.mjs
+++ src/function/rename/suffix.mjs
@@ -1,8 +1,9 @@
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
+import { string_suffix_without } from '../../string/suffix/without.mjs';
 export function function_rename_suffix(function_name_old, suffix_old, suffix_new) {
     arguments_assert(arguments, [string_identifier_is]);
-    let function_name_new = 
+    let function_name_new = string_suffix_without(function_name_old, suffix_old)
     error('todo: function_rename_prefix');
 }
