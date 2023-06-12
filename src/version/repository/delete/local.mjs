32697d27d1bbf45cbb7c92339321f9699b9f24aa
JaredMathis <jared.mathis@native.consulting>
2023-06-12 11:57:51 -0400

    #90 function_auto version_repository_delete_local

--- src/version/repository/delete/local.mjs
+++ src/version/repository/delete/local.mjs
@@ -2,8 +2,8 @@ import { directory_delete } from '../../../directory/delete.mjs';
 import { version_path_repository } from '../../path/repository.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function version_repository_delete_local(repository_name) {
+export async function version_repository_delete_local(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let repository_path = version_path_repository(repository_name);
-    directory_delete;
+    await directory_delete(repository_path);
 }
