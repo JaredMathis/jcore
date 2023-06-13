3d2ad07a577d53690e72f922c23df527db8dfdc6
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:25:37 -0400

    #92 noop

--- src/refactor/input/add/caller.mjs
+++ src/refactor/input/add/caller.mjs
@@ -2,10 +2,12 @@ import { log } from '../../../log.mjs';
 import { js_visit_nodes_call_expression_name_equal } from '../../../js/visit/nodes/call/expression/name/equal.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { error } from '../../../error.mjs';
 export function refactor_input_add_caller(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {input_name, function_declaration, function_name_called} = args;
     js_visit_nodes_call_expression_name_equal(args, function_name_called, v => {
         console.log({ v });
     });
+    error()
 }
