472595b582a59564fb23bb1329a74f37cf09b390
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:46:44 -0400

    #92 function_copy_replace function_add_string_prefix_camel function_add_js_node_type

--- src/function/add/js/node/type.mjs
+++ src/function/add/js/node/type.mjs
@@ -1,5 +1,4 @@
-import { function_add_string_prefix_generic } from '../../string/prefix/generic.mjs';
-import { string_case_camel_to_snake } from '../../../../string/case/camel/to/snake.mjs';
+import { function_add_string_prefix_camel_generic } from '../../string/prefix/camel/generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function function_add_js_node_type(prefix, value) {
@@ -7,6 +6,5 @@ export async function function_add_js_node_type(prefix, value) {
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let snake = string_case_camel_to_snake(value);
-    await function_add_string_prefix_generic(prefix, snake, value);
+    await function_add_string_prefix_camel_generic(prefix, value);
 }
