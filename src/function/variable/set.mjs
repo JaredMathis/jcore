51b18ae502bcd2cfa4f71f2d0c8abe080e45d6ab
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:12:16 -0400

    #92 function_auto function_variable_set

---
+++ src/function/variable/set.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function function_variable_set() {
+    arguments_assert(arguments, []);
+    error('todo: function_variable_set');
+}
