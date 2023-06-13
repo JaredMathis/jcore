b3b159101fbe585e35eed0b4faf1e3a545fbcfab
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:55:28 -0400

    #92 function_input_add js_node_is_object_pattern node

--- src/js/node/is/object/pattern.mjs
+++ src/js/node/is/object/pattern.mjs
@@ -1,3 +1,6 @@
-export function js_node_is_object_pattern() {
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function js_node_is_object_pattern(node) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     return 'ObjectPattern';
 }
