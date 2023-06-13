13c57871752c6ca954ef4dcd14ba228e5ac81aa9
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:20:02 -0400

    #92 function_input_add function_input_add_type type

--- src/function/input/add/type.mjs
+++ src/function/input/add/type.mjs
@@ -1,19 +1,14 @@
+import { function_input_add_generic } from './generic.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
-import { refactor_input_add } from '../../../refactor/input/add.mjs';
-import { function_name_get } from '../../name/get.mjs';
-import { function_map_with_args } from '../../map/with/args.mjs';
 import { string_is } from '../../../string/is.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_input_add_type(function_name, input_name, input_value_default) {
+export async function function_input_add_type(function_name, input_name, input_value_default, type) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is,
-        string_is
+        string_is,
+        arguments_assert_todo
     ]);
-    await function_map_with_args(function_name_get(refactor_input_add), function_name, {
-        input_name,
-        input_value_default,
-        input_type: arguments_assert_todo
-    });
+    await function_input_add_generic(function_name, input_name, input_value_default, arguments_assert_todo);
 }
