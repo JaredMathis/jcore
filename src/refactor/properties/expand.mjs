5020d7a7a0ed39b554db4a4c1f6705a00420cd21
JaredMathis <jared.mathis@native.consulting>
2023-06-13 19:38:48 -0400

    #92 function_auto refactor_properties_expand

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -1,3 +1,4 @@
+import { js_code_call_expression_statement_with_args_code } from '../../js/code/call/expression/statement/with/args/code.mjs';
 import { js_property_identifier_name } from '../../js/property/identifier/name.mjs';
 import { js_node_property_value } from '../../js/node/property/value.mjs';
 import { js_node_property_key } from '../../js/node/property/key.mjs';
@@ -14,6 +15,7 @@ import { js_node_is_object_pattern } from '../../js/node/is/object/pattern.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
+import { function_name_get } from '../../function/name/get.mjs';
 export function refactor_properties_expand(args) {
     arguments_assert(arguments, [defined_is]);
     let {function_declaration, parsed} = args;
@@ -30,6 +32,7 @@ export function refactor_properties_expand(args) {
                     for (let property of properties) {
                         let key = js_property_identifier_name(property, js_node_property_key());
                         let local = js_property_identifier_name(property, js_node_property_value());
+                        js_code_call_expression_statement_with_args_code(function_name_get(object_property_get));
                         console.log({
                             key,
                             value: local
