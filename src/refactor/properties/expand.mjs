e20e63a3656fbd3ada2e937e558617abf7808c6e
JaredMathis <jared.mathis@native.consulting>
2023-06-13 09:40:55 -0400

    #92 function_auto refactor_properties_expand

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -1,10 +1,10 @@
+import { log_json } from '../../log/json.mjs';
 import { js_nodes_get } from '../../js/nodes/get.mjs';
-import { log } from '../../log.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
 import { js_node_is_type } from '../../js/node/is/type.mjs';
 export function refactor_properties_expand(args) {
     arguments_assert(arguments, [defined_is]);
     let {function_declaration, parsed} = args;
-    log(js_nodes_get(parsed, n => js_node_is_type(n, 'ObjectPattern')));
+    log_json(js_nodes_get(parsed, n => js_node_is_type(n, 'ObjectPattern')));
 }
