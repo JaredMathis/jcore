f73cbd837ebb4919803b92553dee2922c7a01af4
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:11:03 -0400

    #92 function_auto function_wrap

--- src/function/wrap.mjs
+++ src/function/wrap.mjs
@@ -1,5 +1,5 @@
+import { js_function_declaration_to_params_names } from '../js/function/declaration/to/params/names.mjs';
 import { function_add_with_statements_synchronized } from './add/with/statements/synchronized.mjs';
-import { function_inputs } from './inputs.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { error } from '../error.mjs';
@@ -9,8 +9,8 @@ export async function function_wrap(function_name_to_wrap, function_name_to_add)
         string_identifier_is,
         string_identifier_is
     ]);
-    let inputs = await function_inputs(function_name_to_wrap);
     let function_declaration = await function_to_declaration(function_name_to_wrap);
+    let inputs = js_function_declaration_to_params_names(function_declaration);
     let statements = error();
     let is_async = error();
     function_add_with_statements_synchronized(function_name_to_add, statements, is_async);
