fa90d1aed741223320b6388887c744e1e4bcf75a
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:01:07 -0400

    #92 function_input_add string_identifier_is_generic allow_underscores

--- src/string/identifier/is/generic.mjs
+++ src/string/identifier/is/generic.mjs
@@ -1,3 +1,4 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_letter_digit_or_underscore_is } from '../../letter/digit/or/underscore/is.mjs';
 import { string_includes } from '../../includes.mjs';
@@ -11,10 +12,11 @@ import { string_to_list } from '../../to/list.mjs';
 import { string_is } from '../../is.mjs';
 import { boolean_is } from '../../../boolean/is.mjs';
 import { defined_is } from '../../../defined/is.mjs';
-export function string_identifier_is_generic(value, enforce_first_and_last) {
+export function string_identifier_is_generic(value, enforce_first_and_last, allow_underscores) {
     arguments_assert(arguments, [
         defined_is,
         boolean_is,
+        arguments_assert_todo
     ]);
     if (!string_is(value)) {
         return false;
