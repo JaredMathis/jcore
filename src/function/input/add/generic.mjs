bb2174e6c108e140198ecd2d82048b03e837319b
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:36:21 -0400

    #92 function_auto function_input_add_generic

--- src/function/input/add/generic.mjs
+++ src/function/input/add/generic.mjs
@@ -1,4 +1,3 @@
-import { js_parse_expression } from '../../../js/parse/expression.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { refactor_input_add } from '../../../refactor/input/add.mjs';
@@ -13,14 +12,7 @@ export async function function_input_add_generic(function_name, input_name, inpu
     ]);
     await function_map_with_args(function_name_get(refactor_input_add), function_name, {
         input_name,
-        args_additional_get: function args_additional_get() {
-            let input_value_expression = js_parse_expression(input_value_default);
-            const args_additional = {
-                input_value_expression,
-                function_name_called: function_name
-            };
-            return args_additional;
-        },
+        input_value_default,
         input_type
     });
 }
