4ce7b796f8f38cae1f3219589783932af581f101
JaredMathis <jared.mathis@native.consulting>
2023-06-13 08:39:24 -0400

    #92 function_auto list_last_remove

--- src/list/last/remove.mjs
+++ src/list/last/remove.mjs
@@ -1,8 +1,11 @@
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { assert } from '../../assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { list_is } from '../is.mjs';
+import { list_length_is_0 } from '../length/is/0.mjs';
 export function list_last_remove(list) {
     arguments_assert(arguments, [list_is]);
+    assert(!list_length_is_0(list));
     return list.pop();
     metadata([]);
 }
