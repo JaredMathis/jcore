aa2876a8c94e307a972a8ae06a3fbc399a0b07ea
JaredMathis <jared.mathis@native.consulting>
2023-06-13 08:01:39 -0400

    #92 function_auto refactor_string_to_function_call

--- src/refactor/string/to/function/call.mjs
+++ src/refactor/string/to/function/call.mjs
@@ -13,7 +13,7 @@ export function refactor_string_to_function_call(args) {
     js_visit_nodes_filter_node(parsed, js_node_is_literal, n => {
         let current = object_property_get(n, js_node_property_value());
         if (equal(current, string_value)) {
-            object_replace;
+            let ce = object_replace(current, ce);
         }
     });
 }
