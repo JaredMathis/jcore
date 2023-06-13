a9513d7d53e34b67c1be09fb86586b7722cb4294
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:10:08 -0400

    #92 function_auto refactor_input_add_caller

---
+++ src/refactor/input/add/caller.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function refactor_input_add_caller() {
+    arguments_assert(arguments, []);
+    error('todo: refactor_input_add_caller');
+}
