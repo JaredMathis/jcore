75559a31a728ad2fff7582bc879c8fd567e1147a
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:09:28 -0400

    #92 noop

--- src/js/function/declaration/async/is.mjs
+++ src/js/function/declaration/async/is.mjs
@@ -3,8 +3,9 @@ import { js_keyword_async } from '../../../keyword/async.mjs';
 import { object_property_change } from '../../../../object/property/change.mjs';
 import { js_node_is_function_declaration } from '../../../node/is/function/declaration.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { object_property_get } from '../../../../object/property/get.mjs';
 export function js_function_declaration_async_is(fd) {
     arguments_assert(arguments, [js_node_is_function_declaration]);
-    object_property_change(fd, js_keyword_async(), true);
+    return object_property_get(fd, js_keyword_async());
     metadata([]);
 }
