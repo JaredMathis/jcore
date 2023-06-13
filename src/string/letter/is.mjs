7dd684ff1b6ab7afedfea931d1441bfb393ebc58
JaredMathis <jared.mathis@native.consulting>
2023-06-13 11:22:45 -0400

    #92 function_auto string_letter_is

--- src/string/letter/is.mjs
+++ src/string/letter/is.mjs
@@ -1,7 +1,8 @@
+import { string_to_case_lower } from '../to/case/lower.mjs';
 import { string_to_case_upper } from '../to/case/upper.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_is } from '../is.mjs';
 export function string_letter_is(first) {
     arguments_assert(arguments, [string_is]);
-    return string_to_case_upper(first) != first.toLowerCase();
+    return string_to_case_upper(first) != string_to_case_lower(first);
 }
---
+++ src/string/to/case/lower.mjs
@@ -0,0 +1,3 @@
+export function string_to_case_lower(first) {
+    return first.toLowerCase();
+}
