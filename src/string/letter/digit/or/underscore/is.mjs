35b6f9de1590f4e579d6bdea0ec23793cc704560
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:44:34 -0400

    #92 function_auto string_letter_digit_or_underscore_is

--- src/string/letter/digit/or/underscore/is.mjs
+++ src/string/letter/digit/or/underscore/is.mjs
@@ -6,6 +6,7 @@ import { string_letter_is } from '../../../is.mjs';
 import { string_length_is_1 } from '../../../../length/is/1.mjs';
 export function string_letter_digit_or_underscore_is(c) {
     arguments_assert(arguments, [string_is]);
+    let allow_underscores = true;
     if (!string_length_is_1(c)) {
         return false;
     }
