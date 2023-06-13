56c1ce732fdb65e3a7f024b331aaedf30ab7c807
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:33:12 -0400

    #92 function_input_add function_call_replace function_name_called_old

--- src/function/call/replace.mjs
+++ src/function/call/replace.mjs
@@ -3,10 +3,11 @@ import { function_map_with_args } from '../map/with/args.mjs';
 import { function_name_get } from '../name/get.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function function_call_replace(function_name, function_name_called) {
+export async function function_call_replace(function_name, function_name_called_old, function_name_called_old) {
     arguments_assert(arguments, [
+        arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    await function_map_with_args(function_name_get(refactor_input_add), function_name, { function_name_called });
+    await function_map_with_args(function_name_get(refactor_input_add), function_name, { function_name_called_old });
 }
