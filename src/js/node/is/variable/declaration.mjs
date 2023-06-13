92256459484bc2ba82bcb8c45ec516220a4702b4
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:41:50 -0400

    #92 function_auto js_node_is_variable_declaration

--- src/js/node/is/variable/declaration.mjs
+++ src/js/node/is/variable/declaration.mjs
@@ -1,6 +1,7 @@
 import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { js_node_is } from '../../is.mjs';
 import { js_node_is_type } from '../type.mjs';
-export function js_node_is_variable_declaration() {
-    arguments_assert(arguments, []);
+export function js_node_is_variable_declaration(n) {
+    arguments_assert(arguments, [js_node_is]);
     return js_node_is_type(n, 'VariableDeclaration');
 }
