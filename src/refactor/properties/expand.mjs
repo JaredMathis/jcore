196cd49a09ba5c418fbbe3549dcd4c69c3ad4022
JaredMathis <jared.mathis@native.consulting>
2023-06-13 13:13:01 -0400

    #92 function_auto refactor_properties_expand

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -1,3 +1,4 @@
+import { list_filter } from '../../list/filter.mjs';
 import { js_node_is_variable_declaration } from '../../js/node/is/variable/declaration.mjs';
 import { log } from '../../log.mjs';
 import { list_get_end } from '../../list/get/end.mjs';
@@ -6,11 +7,13 @@ import { js_visit_nodes_filter } from '../../js/visit/nodes/filter.mjs';
 import { js_node_is_object_pattern } from '../../js/node/is/object/pattern.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
+import { js_node_is } from '../../js/node/is.mjs';
 export function refactor_properties_expand(args) {
     arguments_assert(arguments, [defined_is]);
     let {function_declaration, parsed} = args;
     js_visit_nodes_filter(parsed, js_node_is_object_pattern, v => {
         let {stack, parent} = v;
+        let stack_nodes = list_filter(stack, js_node_is);
         if (js_node_is_variable_declarator(parent)) {
             let grandparent_great = list_get_end(stack, 3);
             if (js_node_is_variable_declaration(grandparent_great)) {
