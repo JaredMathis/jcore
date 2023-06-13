7d10619a40e0b777ba8adc7167ba4c541153121e
JaredMathis <jared.mathis@native.consulting>
2023-06-12 21:52:57 -0400

    #92 function_auto list_new_then

--- src/list/new/then.mjs
+++ src/list/new/then.mjs
@@ -1,3 +1,5 @@
+import { arguments_assert } from '../../arguments/assert.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { list_add } from '../add.mjs';
 export function list_new_then(then) {
     arguments_assert(arguments, [arguments_assert_todo]);
