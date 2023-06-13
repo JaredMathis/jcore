3c8a2cf9d5673390788c223356fc7c8d88da69c4
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:14:56 -0400

    #92 function_auto js_list_add_identifier

--- src/js/list/add/identifier.mjs
+++ src/js/list/add/identifier.mjs
@@ -1,9 +1,12 @@
-import { assert } from '../../../assert.mjs';
+import { js_parse_identifier } from '../../parse/identifier.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_add } from '../../../list/add.mjs';
-import { js_node_is_identifier } from '../../node/is/identifier.mjs';
-import { js_parse_expression } from '../../parse/expression.mjs';
 export function js_list_add_identifier(list, identifier_name) {
-    let identifier = js_parse_expression(identifier_name);
-    assert(js_node_is_identifier(identifier))
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let identifier = js_parse_identifier(identifier_name);
     list_add(list, identifier);
 }
---
+++ src/js/parse/identifier.mjs
@@ -0,0 +1,8 @@
+import { js_node_is_identifier } from '../node/is/identifier.mjs';
+import { assert } from '../../assert.mjs';
+import { js_parse_expression } from './expression.mjs';
+export function js_parse_identifier(identifier_name) {
+    let identifier = js_parse_expression(identifier_name);
+    assert(js_node_is_identifier(identifier));
+    return identifier;
+}
