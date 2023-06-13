f29916f4d497a9192a9a1d0c8656407ed0390f78
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:42:53 -0400

    #92 function_copy string_identifier_is string_identifier_part_is

---
+++ src/string/identifier/part/is.mjs
@@ -0,0 +1,11 @@
+import { metadata } from '../../../metadata.mjs';
+import { string_identifier_is_generic } from '../is/generic.mjs';
+import { string_is } from '../../is.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function string_identifier_part_is(value) {
+    arguments_assert(arguments, [string_is]);
+    let enforce_first_and_last = true;
+    const allow_underscores = true;
+    return string_identifier_is_generic(value, enforce_first_and_last, allow_underscores);
+    metadata([]);
+}
