d0c7888e7d5719c19d811f204e2dcaea7458be85
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:13:33 -0400

    #92 function_input_last_remove function_variable_set

--- src/function/variable/set.mjs
+++ src/function/variable/set.mjs
@@ -4,11 +4,10 @@ import { function_map_with_args } from '../map/with/args.mjs';
 import { string_is } from '../../string/is.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function function_variable_set(function_name, string_value, replacement_function_name) {
+export async function function_variable_set(function_name, string_value) {
     arguments_assert(arguments, [
         string_identifier_is,
-        string_is,
-        string_identifier_is
+        string_is
     ]);
     await function_map_with_args(function_name_get(refactor_string_replace), function_name, {
         string_value,
