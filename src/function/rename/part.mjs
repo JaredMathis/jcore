92ce131cda6d90cc8d6ddf0189a613f527423aa1
JaredMathis <jared.mathis@native.consulting>
2023-06-12 21:09:19 -0400

    #92 function_auto function_rename_part

--- src/function/rename/part.mjs
+++ src/function/rename/part.mjs
@@ -1,3 +1,4 @@
+import { list_set } from '../../list/set.mjs';
 import { list_index_of_single } from '../../list/index/of/single.mjs';
 import { function_rename_part_generic } from './part/generic.mjs';
 import { string_identifier_part_is } from '../../string/identifier/part/is.mjs';
@@ -7,5 +8,6 @@ export async function function_rename_part(part) {
     await function_rename_part_generic(part, parts_map);
     function parts_map(parts) {
         let index = list_index_of_single(parts, part);
+        list_set;
     }
 }
