d02b1c69fec2172716a46604e8908ae1ba9044a9
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:24:30 -0400

    #92 function_rename function_input_add_last_second function_input_add_last_before

--- src/function/input/add/last/before.mjs
+++ src/function/input/add/last/before.mjs
@@ -3,12 +3,12 @@ import { function_input_swap_last_2 } from '../../swap/last/2.mjs';
 import { function_input_add } from '../../add.mjs';
 import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function function_input_add_last_second(function_name, input_name) {
+export async function function_input_add_last_before(function_name, input_name) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    function_input_add;
-    function_input_swap_last_2;
+    await function_input_add(function_name, input_name);
+    await function_input_swap_last_2(function_name);
     error('todo: function_input_add_last_second');
 }
