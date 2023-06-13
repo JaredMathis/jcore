f1a3ba7382dcbb841f2790737616bcb7c0bb5aa7
JaredMathis <jared.mathis@native.consulting>
2023-06-13 08:27:41 -0400

    #92 function_auto refactor_string_to_function_call

--- src/refactor/string/to/function/call.mjs
+++ src/refactor/string/to/function/call.mjs
@@ -1,4 +1,4 @@
-import { string_a } from '../../../../string/a.mjs';
+import { js_nodes_get } from '../../../../js/nodes/get.mjs';
 import { js_parse_call_expression } from '../../../../js/parse/call/expression.mjs';
 import { log } from '../../../../log.mjs';
 import { object_replace } from '../../../../object/replace.mjs';
@@ -6,19 +6,22 @@ import { js_node_property_value } from '../../../../js/node/property/value.mjs';
 import { js_node_is_literal } from '../../../../js/node/is/literal.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { defined_is } from '../../../../defined/is.mjs';
-import { js_nodes_each } from '../../../../js/nodes/each.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
 import { equal } from '../../../../equal.mjs';
+import { assert } from '../../../../assert.mjs';
+import { list_length_is_0 } from '../../../../list/length/is/0.mjs';
 export function refactor_string_to_function_call(args) {
     arguments_assert(arguments, [defined_is]);
     let {string_value, replacement_function_name, parsed} = args;
-    let t = string_a();
+    let t = 'a';
     let ce = js_parse_call_expression(replacement_function_name);
-    js_nodes_each(parsed, js_node_is_literal, n => {
+    let literals = js_nodes_get(parsed, js_node_is_literal);
+    assert(!list_length_is_0(literals));
+    for (let n of literals) {
         let literal_value = object_property_get(n, js_node_property_value());
         if (equal(literal_value, string_value)) {
             console.log({ ce });
             object_replace(n, ce);
         }
-    });
+    }
 }
