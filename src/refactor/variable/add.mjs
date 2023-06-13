b9fdaaf5b731b49978792d6f8b4fdb2e01333d65
JaredMathis <jared.mathis@native.consulting>
2023-06-12 21:48:09 -0400

    #92 function_auto refactor_variable_add

--- src/refactor/variable/add.mjs
+++ src/refactor/variable/add.mjs
@@ -1,5 +1,5 @@
+import { js_function_declaration_to_statement_arguments_assert } from '../../js/function/declaration/to/statement/arguments/assert.mjs';
 import { list_add_after } from '../../list/add/after.mjs';
-import { js_mapper_args_to_statement_arguments_assert } from '../../js/mapper/args/to/statement/arguments/assert.mjs';
 import { js_keyword_let } from '../../js/keyword/let.mjs';
 import { js_parse_statement } from '../../js/parse/statement.mjs';
 import { js_function_declaration_to_statements } from '../../js/function/declaration/to/statements.mjs';
@@ -10,7 +10,7 @@ export async function refactor_variable_add(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {function_declaration, identifier} = args;
     let statement = js_parse_statement(`${ js_keyword_let() } ${ identifier }`);
-    let statement_arguments_assert = await js_mapper_args_to_statement_arguments_assert(args);
+    let statement_arguments_assert = await js_function_declaration_to_statement_arguments_assert(function_declaration);
     let statements = js_function_declaration_to_statements(function_declaration);
     list_add_after(statements, statement, statement_arguments_assert);
     error('todo: refactor_variable_new');
