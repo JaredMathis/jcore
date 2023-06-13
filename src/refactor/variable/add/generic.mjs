9f8de5f7cad11e80013899697427c78454c23646
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:18:47 -0400

    #92 function_auto refactor_variable_add_generic

--- src/refactor/variable/add/generic.mjs
+++ src/refactor/variable/add/generic.mjs
@@ -1,10 +1,15 @@
+import { js_parse_statement_let } from '../../../js/parse/statement/let.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { refactor_import_fix } from '../../import/fix.mjs';
 import { js_function_declaration_statement_add_after_arguments_assert } from '../../../js/function/declaration/statement/add/after/arguments/assert.mjs';
-import { js_keyword_let } from '../../../js/keyword/let.mjs';
-import { js_parse_statement } from '../../../js/parse/statement.mjs';
 export async function refactor_variable_add_generic(args, after_let_identifier) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let {function_declaration, identifier} = args;
-    let statement = js_parse_statement(`${ js_keyword_let() } ${ identifier }${ after_let_identifier }`);
+    let statement = js_parse_statement_let(identifier, after_let_identifier);
     await js_function_declaration_statement_add_after_arguments_assert(function_declaration, statement);
     await refactor_import_fix(args);
 }
--- src/refactor/variable/set.mjs
+++ src/refactor/variable/set.mjs
@@ -3,9 +3,10 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { log } from '../../log.mjs';
 import { json_to } from '../../json/to.mjs';
+import { js_visit_nodes_filter } from '../../js/visit/nodes/filter.mjs';
 export async function refactor_variable_set(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {parsed} = args;
-    let types = js_visit_nodes_types_get(parsed);
-    log(json_to(parsed))
+    let expecting = 
+    js_visit_nodes_filter(parsed);
 }
