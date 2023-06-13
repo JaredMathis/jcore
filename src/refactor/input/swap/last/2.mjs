0c98b0709a87d0b95a7642670ef08bc535435e37
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:58:42 -0400

    #92 function_variable_add refactor_input_swap_last_2 lambda

--- src/refactor/input/swap/last/2.mjs
+++ src/refactor/input/swap/last/2.mjs
@@ -1,14 +1,10 @@
-import { js_list_add_identifier } from '../../../../js/list/add/identifier.mjs';
-import { list_add } from '../../../../list/add.mjs';
-import { function_name_get } from '../../../../function/name/get.mjs';
-import { js_parse_expression } from '../../../../js/parse/expression.mjs';
 import { refactor_input_generic } from '../../generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { list_swap_last_2 } from '../../../../list/swap/last/2.mjs';
-import { error } from '../../../../error.mjs';
 export async function refactor_input_swap_last_2(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let lambda;
     let {function_declaration} = args;
     await refactor_input_generic(args, function_declaration, args_additional_get, params_change, arguments_assert_args_change);
     function arguments_assert_args_change(arguments_assert_args) {
@@ -18,9 +14,7 @@ export async function refactor_input_swap_last_2(args) {
         list_swap_last_2(params);
     }
     function args_additional_get() {
-        const args_additional = {
-            args_change
-        };
+        const args_additional = { args_change };
         function args_change(args) {
             list_swap_last_2(args);
         }
