2471932ac2fd0461cf4d31e5ac1dd4b69eb09ae2
JaredMathis <jared.mathis@native.consulting>
2023-06-12 23:04:36 -0400

    #92 noop

--- src/refactor/input/last/remove.mjs
+++ src/refactor/input/last/remove.mjs
@@ -2,8 +2,9 @@ import { refactor_input_generic_simple } from '../generic/simple.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_remove } from '../../../list/remove.mjs';
+import { list_last_remove } from '../../../list/last/remove.mjs';
 export async function refactor_input_last_remove(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let lambda = list_remove;
+    let lambda = list_last_remove;
     await refactor_input_generic_simple(args, lambda);
 }
