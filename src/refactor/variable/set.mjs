bf73c1fe242aea135ef8010240c3053ecca6e68c
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:27:25 -0400

    #92 function_auto refactor_variable_set

--- src/refactor/variable/set.mjs
+++ src/refactor/variable/set.mjs
@@ -1,13 +1,12 @@
-import { js_visit_nodes_types_get } from '../../js/visit/nodes/types/get.mjs';
+import { json_equal } from '../../json/equal.mjs';
+import { js_parse_statement_let } from '../../js/parse/statement/let.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { log } from '../../log.mjs';
-import { json_to } from '../../json/to.mjs';
 import { js_visit_nodes_filter } from '../../js/visit/nodes/filter.mjs';
 export async function refactor_variable_set(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let { identifier, value } = args;
+    let {identifier, value} = args;
     let {parsed} = args;
     let expecting = js_parse_statement_let(identifier, ``);
-    js_visit_nodes_filter(parsed);
+    js_visit_nodes_filter(parsed, n => json_equal());
 }
