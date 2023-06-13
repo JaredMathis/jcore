80dea1d760394d7737783f84f4b2a2feb2012b40
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:32:20 -0400

    #92 function_auto function_call_replace

--- src/function/call/replace.mjs
+++ src/function/call/replace.mjs
@@ -1,7 +1,6 @@
-import { function_input_add_generic } from '../input/add/generic.mjs';
-import { error } from '../../error.mjs';
+import { refactor_input_add } from '../../refactor/input/add.mjs';
+import { function_map_with_args } from '../map/with/args.mjs';
 import { function_name_get } from '../name/get.mjs';
-import { js_code_call_expression } from '../../js/code/call/expression.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function function_call_replace(function_name, input_name) {
@@ -9,6 +8,9 @@ export async function function_call_replace(function_name, input_name) {
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let input_value_default = js_code_call_expression(function_name_get(error));
-    await function_input_add_generic(function_name, input_name, function_name_get(arguments_assert_todo), input_value_default);
+    await function_map_with_args(function_name_get(refactor_input_add), function_name, {
+        input_name,
+        input_value_default,
+        input_type
+    });
 }
