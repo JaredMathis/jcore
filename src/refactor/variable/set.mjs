965dee5d3d20029ce81010083efbdc02b638fb1c
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:31:05 -0400

    #92 noop

--- src/refactor/variable/set.mjs
+++ src/refactor/variable/set.mjs
@@ -5,11 +5,12 @@ import { js_parse_statement_let } from '../../js/parse/statement/let.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { js_visit_nodes_filter } from '../../js/visit/nodes/filter.mjs';
+import { js_node_is_type } from '../../js/node/is/type.mjs';
 export async function refactor_variable_set(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {identifier, value} = args;
     let {parsed} = args;
     let expecting = js_parse_statement_let(identifier, ``);
     console.log(json_to({ expecting }));
-    js_visit_nodes_filter(parsed, n => json_equal(n, expecting), n => console.log(n));
+    js_visit_nodes_filter(parsed, n => js_node_is_type(n,"VariableDeclarator"), n => console.log(n));
 }
