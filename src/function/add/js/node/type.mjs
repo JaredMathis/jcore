885343d4d969634209d51ec0c69d9a97ac871e52
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:49:29 -0400

    #92 function_auto function_add_js_node_type

--- src/function/add/js/node/type.mjs
+++ src/function/add/js/node/type.mjs
@@ -3,6 +3,6 @@ import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function function_add_js_node_type(value) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let prefix2 = 'js_node_is';
+    let prefix = 'js_node_is';
     await function_add_string_prefix_camel_generic(prefix, value);
 }
