47702c29a94ae1ea79bef79495b485ae7d98a30a
JaredMathis <jared.mathis@native.consulting>
2023-06-13 09:45:17 -0400

    #92 function_search uppercase

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -1,9 +1,11 @@
 import { js_nodes_type_log } from '../../js/nodes/type/log.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
+import { string_to_list } from '../../string/to/list.mjs';
 export function refactor_properties_expand(args) {
     arguments_assert(arguments, [defined_is]);
     let {function_declaration, parsed} = args;
     const node_type = 'ObjectPattern';
-    js_nodes_type_log(parsed, node_type);
+    let characters = string_to_list(node_type);
+    let indices_capital = 
 }
