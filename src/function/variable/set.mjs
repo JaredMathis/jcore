785adf59aa48b3c1053d23a67211f0e7eaee7dbf
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:14:18 -0400

    #92 function_auto function_variable_set

--- src/function/variable/set.mjs
+++ src/function/variable/set.mjs
@@ -1,16 +1,13 @@
-import { refactor_string_replace } from '../../refactor/string/replace.mjs';
+import { refactor_variable_set } from '../../refactor/variable/set.mjs';
 import { function_name_get } from '../name/get.mjs';
 import { function_map_with_args } from '../map/with/args.mjs';
 import { string_is } from '../../string/is.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function function_variable_set(function_name, string_value) {
+export async function function_variable_set(function_name, value) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_is
     ]);
-    await function_map_with_args(function_name_get(refactor_string_replace), function_name, {
-        string_value,
-        replacement_function_name
-    });
+    await function_map_with_args(function_name_get(refactor_variable_set), function_name, { value });
 }
