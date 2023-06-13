777ebe501ce778a7b3be9db01ea4eebc025852af
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:16:47 -0400

    #92 function_variable_set list_new_then lambda

--- src/refactor/variable/set.mjs
+++ src/refactor/variable/set.mjs
@@ -2,9 +2,10 @@ import { js_visit_nodes_types_get } from '../../js/visit/nodes/types/get.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { log } from '../../log.mjs';
+import { json_to } from '../../json/to.mjs';
 export async function refactor_variable_set(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {parsed} = args;
     let types = js_visit_nodes_types_get(parsed);
-    log(parsed)
+    log(json_to(parsed))
 }
