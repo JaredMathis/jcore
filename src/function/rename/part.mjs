06a83d60128871d496c376d060b51a753d4b23fe
JaredMathis <jared.mathis@native.consulting>
2023-06-12 21:06:23 -0400

    #92 function_auto function_rename_part

--- src/function/rename/part.mjs
+++ src/function/rename/part.mjs
@@ -1,4 +1,4 @@
-import { list_remove } from '../../list/remove.mjs';
+import { list_index_of_single } from '../../list/index/of/single.mjs';
 import { function_rename_part_generic } from './part/generic.mjs';
 import { string_identifier_part_is } from '../../string/identifier/part/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -6,6 +6,6 @@ export async function function_rename_part(part) {
     arguments_assert(arguments, [string_identifier_part_is]);
     await function_rename_part_generic(part, parts_map);
     function parts_map(parts) {
-        list_remove(parts, part);
+        list_index_of_single(parts, part);
     }
 }
---
+++ src/list/index/of/single.mjs
@@ -0,0 +1,10 @@
+import { list_index_of } from '../of.mjs';
+import { list_length_is_1 } from '../../length/is/1.mjs';
+import { assert } from '../../../assert.mjs';
+import { equal } from '../../../equal.mjs';
+import { list_filter } from '../../filter.mjs';
+export function list_index_of_single(parts, part) {
+    let filtered = list_filter(parts, p => equal(p, part));
+    assert(list_length_is_1(filtered));
+    let index = list_index_of(parts, part);
+}
