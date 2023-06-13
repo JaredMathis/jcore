4350f109b395ae3ef0097c5ed4d87b1cd9e3d73f
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:48:23 -0400

    #92 function_auto string_letter_digit_or_underscore_is_generic

--- src/string/letter/digit/or/underscore/is/generic.mjs
+++ src/string/letter/digit/or/underscore/is/generic.mjs
@@ -1,16 +1,15 @@
-import { arguments_assert_todo } from '../../../../../../arguments/assert/todo.mjs';
 import { string_underscore_is } from '../../../../../underscore/is.mjs';
 import { string_digit_is } from '../../../../../digit/is.mjs';
 import { string_letter_is } from '../../../../is.mjs';
 import { string_length_is_1 } from '../../../../../length/is/1.mjs';
 import { string_is } from '../../../../../is.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
+import { boolean_is } from '../../../../../../boolean/is.mjs';
 export function string_letter_digit_or_underscore_is_generic(c, allow_underscores) {
     arguments_assert(arguments, [
         string_is,
-        arguments_assert_todo
+        boolean_is
     ]);
-    let allow_underscores = true;
     if (!string_length_is_1(c)) {
         return false;
     }
