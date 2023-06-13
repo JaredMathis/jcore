50c6ba60057563c4ad692625e237cb7172fd8864
JaredMathis <jared.mathis@native.consulting>
2023-06-13 19:01:05 -0400

    #92 function_auto function_properties_expand

---
+++ src/function/properties/expand.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function function_properties_expand() {
+    arguments_assert(arguments, []);
+    error('todo: function_properties_expand');
+}
