38797c576780700666e487c6fa3adbc4885a5368
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:44:04 -0400

    #92 function_call_replace list_new_then list_add lambda

--- src/list/new/then.mjs
+++ src/list/new/then.mjs
@@ -7,7 +7,7 @@ export function list_new_then(then) {
     let list = [];
     then(list_new_then_add);
     function list_new_then_add(element) {
-        list_add(list, element);
+        lambda(list, element);
     }
     return list;
 }
