997c9482c3f3a2cde4c3021243f6229cd25bf0e9
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:46:59 -0400

    #92 function_auto string_letter_digit_or_underscore_is

--- src/string/letter/digit/or/underscore/is.mjs
+++ src/string/letter/digit/or/underscore/is.mjs
@@ -1,29 +1,7 @@
 import { string_letter_digit_or_underscore_is_generic } from './is/generic.mjs';
 import { string_is } from '../../../../is.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-import { string_underscore_is } from '../../../../underscore/is.mjs';
-import { string_digit_is } from '../../../../digit/is.mjs';
-import { string_letter_is } from '../../../is.mjs';
-import { string_length_is_1 } from '../../../../length/is/1.mjs';
 export function string_letter_digit_or_underscore_is(c) {
     arguments_assert(arguments, [string_is]);
-    if (false) {
-        string_letter_digit_or_underscore_is_generic;
-    }
-    let allow_underscores = true;
-    if (!string_length_is_1(c)) {
-        return false;
-    }
-    if (string_letter_is(c)) {
-        return true;
-    }
-    if (string_digit_is(c)) {
-        return true;
-    }
-    if (allow_underscores) {
-        if (string_underscore_is(c)) {
-            return true;
-        }
-    }
-    return false;
+    return string_letter_digit_or_underscore_is_generic(c);
 }
