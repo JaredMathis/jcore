3d12bd9096fade8c4a1b738d1c08d1f4946dbf54
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:58:14 -0400

    #92 function_auto refactor_string_to_function_call

--- src/refactor/string/to/function/call.mjs
+++ src/refactor/string/to/function/call.mjs
@@ -1,10 +1,11 @@
-import { log_json } from '../../../../log/json.mjs';
-import { js_visit_nodes_types_get } from '../../../../js/visit/nodes/types/get.mjs';
+import { js_node_is_literal } from '../../../../js/node/is/literal.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { defined_is } from '../../../../defined/is.mjs';
+import { js_visit_nodes_filter_node } from '../../../../js/visit/nodes/filter/node.mjs';
 export function refactor_string_to_function_call(args) {
     arguments_assert(arguments, [defined_is]);
     let {string_value, replacement_function_name, parsed} = args;
     let t = 'a';
-    log_json(js_visit_nodes_types_get(parsed));
+    js_visit_nodes_filter_node(parsed, js_node_is_literal, n => {
+    });
 }
