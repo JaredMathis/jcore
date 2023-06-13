b8f5c47704a1a1e0b8f7a805c4b61f2b7649cfd7
JaredMathis <jared.mathis@native.consulting>
2023-06-13 08:15:59 -0400

    #92 function_auto function_arguments_assert_default_count

--- src/function/arguments/assert/default/count.mjs
+++ src/function/arguments/assert/default/count.mjs
@@ -1,14 +1,14 @@
+import { count_async } from '../../../../count/async.mjs';
 import { function_arguments_assert_default_generic } from './generic.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { count } from '../../../../count.mjs';
 export async function function_arguments_assert_default_count() {
     arguments_assert(arguments, []);
     let result = await count_async(async counter => {
         await function_arguments_assert_default_generic(on_equals);
         async function on_equals(function_name) {
-            counter()
+            counter();
             return false;
         }
-    })
+    });
     return result;
 }
