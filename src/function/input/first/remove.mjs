774c3430e300948f1c675c8de1c8ebeea9eb0fc4
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:48:48 -0400

    #92 function_copy function_input_last_remove function_input_first_remove

---
+++ src/function/input/first/remove.mjs
@@ -0,0 +1,9 @@
+import { refactor_input_last_remove } from '../../../refactor/input/last/remove.mjs';
+import { function_name_get } from '../../name/get.mjs';
+import { function_map_with_args } from '../../map/with/args.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export async function function_input_first_remove(function_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    await function_map_with_args(function_name_get(refactor_input_last_remove), function_name, {});
+}
