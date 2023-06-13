91bb9a2ab005bb6b2e9d4086bd3ebab6bc85a012
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:16:01 -0400

    #92 function_auto function_input_add_default

--- src/function/input/add/default.mjs
+++ src/function/input/add/default.mjs
@@ -1,20 +1,14 @@
-import { refactor_input_add } from '../../../refactor/input/add.mjs';
-import { function_name_get } from '../../name/get.mjs';
-import { function_map_with_args } from '../../map/with/args.mjs';
+import { function_input_add } from '../add.mjs';
+import { function_input_add_generic } from './generic.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { string_is } from '../../../string/is.mjs';
-import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 export async function function_input_add_default(function_name, input_name, input_value_default) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is,
         string_is
     ]);
-    let input_value_default = js_code_call_expression()
-    await function_map_with_args(function_name_get(refactor_input_add), function_name, {
-        input_name,
-        input_value_default: input_value_default,
-        input_type: arguments_assert_todo
-    });
+    await function_input_add_generic(function_name, input_name, input_value_default);
+    await function_input_add(function_name, input_name);
 }
---
+++ src/function/input/add/generic.mjs
@@ -0,0 +1,11 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { refactor_input_add } from '../../../refactor/input/add.mjs';
+import { function_name_get } from '../../name/get.mjs';
+import { function_map_with_args } from '../../map/with/args.mjs';
+export async function function_input_add_generic(function_name, input_name, input_value_default) {
+    await function_map_with_args(function_name_get(refactor_input_add), function_name, {
+        input_name,
+        input_value_default: input_value_default,
+        input_type: arguments_assert_todo
+    });
+}
