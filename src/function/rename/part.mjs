eb6cfa0ad6e7e402b369842ba143e75ecf475e0e
JaredMathis <jared.mathis@native.consulting>
2023-06-12 21:08:20 -0400

    #92 function_auto function_rename_part

--- src/function/rename/part.mjs
+++ src/function/rename/part.mjs
@@ -6,6 +6,6 @@ export async function function_rename_part(part) {
     arguments_assert(arguments, [string_identifier_part_is]);
     await function_rename_part_generic(part, parts_map);
     function parts_map(parts) {
-        list_index_of_single(parts, part);
+        let index = list_index_of_single(parts, part);
     }
 }
