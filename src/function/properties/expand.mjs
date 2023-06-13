1aaa9c64d0312799aa12ac17bbf2227ecf7ab0dd
JaredMathis <jared.mathis@native.consulting>
2023-06-13 09:32:28 -0400

    #92 function_input_last_3_remove function_properties_expand

--- src/function/properties/expand.mjs
+++ src/function/properties/expand.mjs
@@ -4,14 +4,8 @@ import { function_map_with_args } from '../map/with/args.mjs';
 import { function_name_get } from '../name/get.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { string_is } from '../../string/is.mjs';
-export async function function_properties_expand(function_name, identifier, prefix, properties_unparsed) {
-    arguments_assert(arguments, [
-        string_identifier_is,
-        string_identifier_is,
-        string_identifier_is,
-        string_is
-    ]);
+export async function function_properties_expand(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
     let properties = string_identifier_multiple_parse(properties_unparsed);
     await function_map_with_args(function_name_get(refactor_properties_expand), function_name, {
         identifier,
