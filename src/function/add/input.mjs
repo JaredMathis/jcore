22f998c0b8385e8587fb7a32b53e943bb098b1ce
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:16:15 -0400

    #92 function_delete function_add_input

--- src/function/add/input.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../../error.mjs';
-import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_add_input() {
-    arguments_assert(arguments, []);
-    error('todo: function_add_input');
-}
