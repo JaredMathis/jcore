43ef48df5e34b576e38fe400e73a80fa71323887
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:40:01 -0400

    #92 function_auto function_add_string_prefix

--- src/function/add/string/prefix.mjs
+++ src/function/add/string/prefix.mjs
@@ -2,11 +2,11 @@ import { string_identifier_with_prefix } from '../../../string/identifier/with/p
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { function_add_string } from '../string.mjs';
-export async function function_add_string_prefix(prefix, value) {
+export async function function_add_string_prefix(prefix, suffix) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let function_name = string_identifier_with_prefix(prefix, value);
-    await function_add_string(function_name, value);
+    let function_name = string_identifier_with_prefix(prefix, suffix);
+    await function_add_string(function_name, suffix);
 }
