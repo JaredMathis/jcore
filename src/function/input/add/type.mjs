9c6b2bd7165f5fa8c0f481b44ee50ad32d58b241
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:56:10 -0400

    #92 noop

--- src/function/input/add/type.mjs
+++ src/function/input/add/type.mjs
@@ -3,12 +3,12 @@ import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { string_is } from '../../../string/is.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_input_add_type(function_name, input_name, input_value_default, type) {
+export async function function_input_add_type(function_name, input_name, input_value_default, input_type) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is,
         string_is,
         string_identifier_is
     ]);
-    await function_input_add_generic(function_name, input_name, input_value_default, type);
+    await function_input_add_generic(function_name, input_name, input_value_default, input_type);
 }
