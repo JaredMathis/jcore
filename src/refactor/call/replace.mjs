d8429695388c2c7b65560bc9c779697e8554f07b
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:38:42 -0400

    #92 function_auto refactor_call_replace

--- src/refactor/call/replace.mjs
+++ src/refactor/call/replace.mjs
@@ -1,3 +1,5 @@
+import { log } from '../../log.mjs';
+import { js_visit_nodes_call_expression_name_equal } from '../../js/visit/nodes/call/expression/name/equal.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -6,7 +8,7 @@ export function refactor_call_replace(args) {
     let {function_name_called_old, function_name_called_new} = args;
     let {parsed} = args;
     js_visit_nodes_call_expression_name_equal(parsed, function_name_called_old, n => {
-        console.log({n})
-    })
+        console.log({ n });
+    });
     error('todo: refactor_call_replace');
 }
