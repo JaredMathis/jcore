e71bc3036e6f63e1d00aae505e9a380ffe93df68
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:34:25 -0400

    #92 function_input_add refactor_call_replace args

--- src/refactor/call/replace.mjs
+++ src/refactor/call/replace.mjs
@@ -1,6 +1,7 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function refactor_call_replace() {
-    arguments_assert(arguments, []);
+export function refactor_call_replace(args) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: refactor_call_replace');
 }
