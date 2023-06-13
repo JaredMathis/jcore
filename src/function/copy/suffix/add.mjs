0414aa36b68d8ef9ce2300cf2b628a46263fbfc1
JaredMathis <jared.mathis@native.consulting>
2023-06-13 05:46:13 -0400

    #92 function_auto function_copy_suffix_add

--- src/function/copy/suffix/add.mjs
+++ src/function/copy/suffix/add.mjs
@@ -2,12 +2,14 @@ import { string_identifier_multiple_parse } from '../../../string/identifier/mul
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { function_copy_suffix } from '../suffix.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { list_last_remove } from '../../../list/last/remove.mjs';
 export async function function_copy_suffix_add(function_name_old, function_name_suffix) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
     let function_name_old_parts = string_identifier_multiple_parse(function_name_old);
+    let last = list_last_remove(function_name_old_parts);
     let function_name_suffix_parts = string_identifier_multiple_parse(function_name_suffix);
     function_copy_suffix(function_name_prefix, function_name_suffix_from, function_name_suffix_to);
 }
