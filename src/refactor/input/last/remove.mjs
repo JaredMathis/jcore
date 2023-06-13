cac73b062a40bfdcf9c03dcb39a0050b545a5ae3
JaredMathis <jared.mathis@native.consulting>
2023-06-12 23:01:09 -0400

    #92 function_auto refactor_input_last_remove

--- src/refactor/input/last/remove.mjs
+++ src/refactor/input/last/remove.mjs
@@ -1,9 +1,9 @@
 import { refactor_input_generic_simple } from '../generic/simple.mjs';
-import { list_swap_last_2 } from '../../../list/swap/last/2.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { list_remove } from '../../../list/remove.mjs';
 export async function refactor_input_last_remove(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let lambda = list_swap_last_2;
+    let lambda = list_remove;
     await refactor_input_generic_simple(args, lambda);
 }
