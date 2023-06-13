d8026806ace5abcb5de55c7a423f6237eddcbe4a
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:00:04 -0400

    #92 function_auto refactor_input_add

--- src/refactor/input/add.mjs
+++ src/refactor/input/add.mjs
@@ -1,6 +1,5 @@
 import { js_function_declaration_to_params } from '../../js/function/declaration/to/params.mjs';
 import { js_parse_expression } from '../../js/parse/expression.mjs';
-import { log } from '../../log.mjs';
 import { js_function_declaration_to_statement_arguments_assert_args_predicate } from '../../js/function/declaration/to/statement/arguments/assert/args/predicate.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
@@ -12,12 +11,9 @@ export async function refactor_input_add(args) {
     let {input_name, function_declaration} = args;
     let params = js_function_declaration_to_params(function_declaration);
     let input_new = js_parse_expression(input_name);
+    list_add(arguments_assert_args, input_new);
     let arguments_assert_args = await js_function_declaration_to_statement_arguments_assert_args_predicate(function_declaration);
     let type = js_parse_expression(function_name_get(arguments_assert_todo));
     list_add(arguments_assert_args, type);
-    console.log({
-        arguments_assert_args,
-        params
-    });
     error();
 }
