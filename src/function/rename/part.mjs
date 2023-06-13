f5fac38fe2581524a8076da5018ac874f9dee248
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:53:58 -0400

    #92 function_auto function_rename_part

--- src/function/rename/part.mjs
+++ src/function/rename/part.mjs
@@ -5,7 +5,6 @@ import { function_rename_generic } from './generic.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_contains } from '../../list/contains.mjs';
 import { list_remove } from '../../list/remove.mjs';
-import { string_identifier_parts } from '../../string/identifier/parts.mjs';
 export async function function_rename_part(part) {
     arguments_assert(arguments, [string_identifier_part_is]);
     function predicate_should_rename(n) {
