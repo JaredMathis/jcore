bdf2ad12543c026168ca0f31ddfae93c7a468b9a
JaredMathis <jared.mathis@native.consulting>
2023-06-12 23:04:54 -0400

    #92 function_input_last_remove function_input_swap_last_2

--- src/function/input/swap/last/2.mjs
+++ src/function/input/swap/last/2.mjs
@@ -3,9 +3,8 @@ import { function_name_get } from '../../../name/get.mjs';
 import { function_map_with_args } from '../../../map/with/args.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function function_input_swap_last_2(function_name, input_name, input_value_default, input_type) {
+export async function function_input_swap_last_2(function_name, input_name, input_value_default) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
