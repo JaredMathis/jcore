fe539075f4f57c7bdf1a018797ad8b59296539fe
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:12:49 -0400

    #92 noop

--- src/function/input/add/default.mjs
+++ src/function/input/add/default.mjs
@@ -13,7 +13,7 @@ export async function function_input_add_default(function_name, input_name, inpu
     ]);
     await function_map_with_args(function_name_get(refactor_input_add), function_name, {
         input_name,
-        input_value_default,
+        input_value_default: input_value_default,
         input_type: arguments_assert_todo
     });
 }
