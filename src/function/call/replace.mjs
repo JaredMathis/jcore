ad420782e4415e3e081a75329dc0b65e93daa9b9
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:31:01 -0400

    #92 function_copy function_input_add function_call_replace

---
+++ src/function/call/replace.mjs
@@ -0,0 +1,14 @@
+import { function_input_add_generic } from '../input/add/generic.mjs';
+import { error } from '../../error.mjs';
+import { function_name_get } from '../name/get.mjs';
+import { js_code_call_expression } from '../../js/code/call/expression.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export async function function_call_replace(function_name, input_name) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let input_value_default = js_code_call_expression(function_name_get(error));
+    await function_input_add_generic(function_name, input_name, function_name_get(arguments_assert_todo), input_value_default);
+}
