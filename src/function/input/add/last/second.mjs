40d5f0caa70f2a7c22551d661928f12e901b01cb
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:21:38 -0400

    #92 function_auto function_input_add_last_second

--- src/function/input/add/last/second.mjs
+++ src/function/input/add/last/second.mjs
@@ -1,6 +1,10 @@
+import { function_input_swap_last_2 } from '../../swap/last/2.mjs';
+import { function_input_add } from '../../add.mjs';
 import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function function_input_add_last_second() {
+export async function function_input_add_last_second() {
     arguments_assert(arguments, []);
+    function_input_add;
+    function_input_swap_last_2;
     error('todo: function_input_add_last_second');
 }
