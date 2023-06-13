62d4df974c1aae2e0affb56895da9018fd053561
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:43:07 -0400

    #92 function_copy_replace function_add_string_prefix function_add_string_prefix_camel

--- src/function/add/string/prefix/camel.mjs
+++ src/function/add/string/prefix/camel.mjs
@@ -1,11 +1,11 @@
-import { function_add_string_prefix } from '../prefix.mjs';
+import { function_add_string_prefix_generic } from './generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function function_add_string_prefix_camel(prefix, value) {
+export async function function_add_string_prefix_camel(prefix, suffix) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let result = await function_add_string_prefix(prefix, value);
-    return result;
+    let value = suffix;
+    await function_add_string_prefix_generic(prefix, suffix, value);
 }
