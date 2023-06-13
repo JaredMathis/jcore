db00026a94e22cb463481cbc73028ecee257cac8
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:56:34 -0400

    #92 function_variable_set list_new_then lambda list_add

--- src/list/new/then.mjs
+++ src/list/new/then.mjs
@@ -3,7 +3,7 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { list_add } from '../add.mjs';
 export function list_new_then(then) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let lambda;
+    let lambda = list_add;
     let list = [];
     then(list_new_then_add);
     function list_new_then_add(element) {
