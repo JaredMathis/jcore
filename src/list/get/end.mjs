1a0b053df38f89eb5a0b38cf00f97c1e6289968e
JaredMathis <jared.mathis@native.consulting>
2023-06-13 13:05:48 -0400

    #92 function_auto refactor_properties_expand

---
+++ src/list/get/end.mjs
@@ -0,0 +1,10 @@
+import { list_get } from '../get.mjs';
+import { subtract } from '../../subtract.mjs';
+import { list_last_index } from '../last/index.mjs';
+import { assert } from '../../assert.mjs';
+export function list_get_end(list, index_end) {
+    assert(index_end >= 0);
+    let index_last = list_last_index(list);
+    let index = subtract(index_last, index_end);
+    return list_get(list, index);
+}
--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -1,5 +1,5 @@
+import { list_get_end } from '../../list/get/end.mjs';
 import { js_node_is_variable_declarator } from '../../js/node/is/variable/declarator.mjs';
-import { log } from '../../log.mjs';
 import { js_visit_nodes_filter } from '../../js/visit/nodes/filter.mjs';
 import { js_node_is_object_pattern } from '../../js/node/is/object/pattern.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -8,8 +8,9 @@ export function refactor_properties_expand(args) {
     arguments_assert(arguments, [defined_is]);
     let {function_declaration, parsed} = args;
     js_visit_nodes_filter(parsed, js_node_is_object_pattern, v => {
-        log({ v });
-        if (js_node_is_variable_declarator(v)) {
+        let {stack, parent} = v;
+        let grandpart = list_get_end(stack, 2);
+        if (js_node_is_variable_declarator(parent)) {
         }
     });
 }
