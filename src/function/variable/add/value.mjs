3df89981b74351a81dddab4c6dcb7e01b78c0777
JaredMathis <jared.mathis@native.consulting>
2023-06-13 05:56:59 -0400

    #92 function_input_add function_variable_add_value identifier_value

--- src/function/variable/add/value.mjs
+++ src/function/variable/add/value.mjs
@@ -1,12 +1,14 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { refactor_variable_add } from '../../../refactor/variable/add.mjs';
 import { function_name_get } from '../../name/get.mjs';
 import { function_map_with_args } from '../../map/with/args.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_variable_add_value(function_name, identifier) {
+export async function function_variable_add_value(function_name, identifier, identifier_value) {
     arguments_assert(arguments, [
         string_identifier_is,
-        string_identifier_is
+        string_identifier_is,
+        arguments_assert_todo
     ]);
     await function_map_with_args(function_name_get(refactor_variable_add), function_name, { identifier });
 }
