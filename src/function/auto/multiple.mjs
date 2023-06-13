9d5ec32900d7b90b8a419d12c281fa0007d66f28
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:05:10 -0400

    #92 function_input_add function_auto_multiple function_names error()

--- src/function/auto/multiple.mjs
+++ src/function/auto/multiple.mjs
@@ -1,6 +1,7 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_auto_multiple() {
-    arguments_assert(arguments, []);
+export function function_auto_multiple(function_names) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: function_auto_multiple');
 }
