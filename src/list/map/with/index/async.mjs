d70e1239b9ba1f5cc63fab59af264e2ac5bf3666
JaredMathis <jared.mathis@native.consulting>
2023-06-13 10:15:24 -0400

    #92 function_copy list_map_async list_map_with_index_async

---
+++ src/list/map/with/index/async.mjs
@@ -0,0 +1,14 @@
+import { metadata } from '../../../../metadata.mjs';
+import { list_map_generic_async } from '../../generic/async.mjs';
+import { function_is } from '../../../../function/is.mjs';
+import { list_is } from '../../../is.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export async function list_map_with_index_async(list, mapper) {
+    arguments_assert(arguments, [
+        list_is,
+        function_is
+    ]);
+    let lambda = async (element, index) => await mapper(element);
+    return await list_map_generic_async(list, lambda);
+    metadata([]);
+}
