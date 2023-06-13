b523b01657296a3174f1f0fef62d109948078f5f
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:57:19 -0400

    #92 function_auto refactor_string_to_function_call

--- src/refactor/string/to/function/call.mjs
+++ src/refactor/string/to/function/call.mjs
@@ -1,3 +1,4 @@
+import { log_json } from '../../../../log/json.mjs';
 import { js_visit_nodes_types_get } from '../../../../js/visit/nodes/types/get.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { defined_is } from '../../../../defined/is.mjs';
@@ -5,5 +6,5 @@ export function refactor_string_to_function_call(args) {
     arguments_assert(arguments, [defined_is]);
     let {string_value, replacement_function_name, parsed} = args;
     let t = 'a';
-    return js_visit_nodes_types_get(parsed);
+    log_json(js_visit_nodes_types_get(parsed));
 }
