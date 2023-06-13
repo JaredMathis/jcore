f935701ca3910ca1162eb1850732a4e83e5fc1ad
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:32:48 -0400

    #92 function_auto function_call_replace

--- src/function/call/replace.mjs
+++ src/function/call/replace.mjs
@@ -3,14 +3,10 @@ import { function_map_with_args } from '../map/with/args.mjs';
 import { function_name_get } from '../name/get.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function function_call_replace(function_name, input_name) {
+export async function function_call_replace(function_name, function_name_called) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    await function_map_with_args(function_name_get(refactor_input_add), function_name, {
-        input_name,
-        input_value_default,
-        input_type
-    });
+    await function_map_with_args(function_name_get(refactor_input_add), function_name, { function_name_called });
 }
