7e872a2699003e0f093ce1d4c373622ff29c6c03
JaredMathis <jared.mathis@native.consulting>
2023-06-12 21:40:46 -0400

    #92 function_auto refactor_variable_add

--- src/refactor/variable/add.mjs
+++ src/refactor/variable/add.mjs
@@ -10,10 +10,10 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 export async function refactor_variable_add(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {js_function_declaration, identifier} = args;
-    let statements = js_function_declaration_to_statements(js_function_declaration);
     let statement = js_parse_statement(`${ js_keyword_let() } ${ identifier }`);
     let statement_arguments_assert = await js_mapper_args_to_statement_arguments_assert(args);
     let after_index = list_index_of(statements, statement_arguments_assert);
+    let statements = js_function_declaration_to_statements(js_function_declaration);
     list_add_at(statements, statement, after_index);
     error('todo: refactor_variable_new');
 }
