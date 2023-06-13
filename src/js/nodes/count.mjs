06eeae5b8893c05b3bf506d94a3e34eb5ce59fd3
JaredMathis <jared.mathis@native.consulting>
2023-06-13 08:19:30 -0400

    #92 function_auto js_nodes_count

--- src/js/nodes/count.mjs
+++ src/js/nodes/count.mjs
@@ -3,18 +3,19 @@ import { js_node_is_program } from '../node/is/program.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { js_visit_nodes_all } from '../visit/nodes/all.mjs';
+import { count } from '../../count.mjs';
 export function js_nodes_count(parsed, predicate) {
     arguments_assert(arguments, [
         js_node_is_program,
         function_is
     ]);
-    let count = 0;
-    js_visit_nodes_all(parsed, v => {
-        let {node} = v;
-        if (predicate(node)) {
-            count++;
-        }
+    return count(counter => {
+        js_visit_nodes_all(parsed, v => {
+            let {node} = v;
+            if (predicate(node)) {
+                counter();
+            }
+        });
     });
-    return count;
     metadata([]);
 }
