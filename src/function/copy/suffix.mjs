513a033a14e31ef5ad8836e7446c641164979e30
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:52:35 -0400

    #92 function_input_add_type function_copy_suffix function_name_suffix_to error() string_identifier_sub_is

--- src/function/copy/suffix.mjs
+++ src/function/copy/suffix.mjs
@@ -1,10 +1,12 @@
+import { string_identifier_sub_is } from '../../string/identifier/sub/is.mjs';
 import { function_copy } from '../copy.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function function_copy_suffix(function_name_prefix, function_name_suffix_from) {
+export async function function_copy_suffix(function_name_prefix, function_name_suffix_from, function_name_suffix_to) {
     arguments_assert(arguments, [
         string_identifier_is,
-        string_identifier_is
+        string_identifier_is,
+        string_identifier_sub_is
     ]);
     await function_copy(function_name_to_copy, function_name_of_copy);
 }
