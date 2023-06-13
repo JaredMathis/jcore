5beefd913f37e615e3bf86b15f119bb467b07e85
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:56:57 -0400

    #92 function_string_to_function_call refactor_string_to_function_call a string_a

--- src/refactor/string/to/function/call.mjs
+++ src/refactor/string/to/function/call.mjs
@@ -4,5 +4,6 @@ import { defined_is } from '../../../../defined/is.mjs';
 export function refactor_string_to_function_call(args) {
     arguments_assert(arguments, [defined_is]);
     let {string_value, replacement_function_name, parsed} = args;
+    let t = 'a';
     return js_visit_nodes_types_get(parsed);
 }
