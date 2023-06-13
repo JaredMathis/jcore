d762ad27c5d0633ba9595c81bc092cc85e8b339c
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:50:45 -0400

    #92 function_auto string_case_camel_to_snake

--- src/string/case/camel/to/snake.mjs
+++ src/string/case/camel/to/snake.mjs
@@ -1,7 +1,11 @@
+import { list_join } from '../../../../list/join.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { function_name_separator } from '../../../../function/name/separator.mjs';
 import { string_case_camel_parse } from '../parse.mjs';
 export function string_case_camel_to_snake(input) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let parsed = string_case_camel_parse(input);
-    let snake = string_join(parsed, function_name_separator());
+    let snake = list_join(parsed, function_name_separator());
     return snake;
 }
