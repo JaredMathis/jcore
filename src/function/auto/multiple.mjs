32676f33589532e4b2c5f545d27c7793037f81eb
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:06:20 -0400

    #92 function_auto function_auto_multiple

--- src/function/auto/multiple.mjs
+++ src/function/auto/multiple.mjs
@@ -1,7 +1,12 @@
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
-import { error } from '../../error.mjs';
+import { function_auto } from '../auto.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_auto_multiple(function_names) {
-    arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: function_auto_multiple');
+import { assert } from '../../assert.mjs';
+import { list_length_is_0 } from '../../list/length/is/0.mjs';
+import { list_is } from '../../list/is.mjs';
+export async function function_auto_multiple(function_names) {
+    arguments_assert(arguments, [list_is]);
+    assert(!list_length_is_0(function_names));
+    for (let f of function_names) {
+        await function_auto(f);
+    }
 }
