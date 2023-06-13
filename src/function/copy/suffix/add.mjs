9ad4a35a52df03a6441d0acc12cb56c74049d64a
JaredMathis <jared.mathis@native.consulting>
2023-06-13 05:43:13 -0400

    #92 function_input_add function_copy_suffix_add function_name_suffix

--- src/function/copy/suffix/add.mjs
+++ src/function/copy/suffix/add.mjs
@@ -1,7 +1,10 @@
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { function_copy_suffix } from '../suffix.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_copy_suffix_add(function_name_prefix) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+export async function function_copy_suffix_add(function_name_prefix, function_name_suffix) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     function_copy_suffix();
 }
