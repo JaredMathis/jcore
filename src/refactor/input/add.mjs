9c91680415cead10607287b3d2f27b20bbbe13db
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:52:19 -0400

    #92 noop

--- src/refactor/input/add.mjs
+++ src/refactor/input/add.mjs
@@ -11,7 +11,7 @@ export async function refactor_input_add(args) {
     let {function_declaration, input_value_default} = args;
     await refactor_input_generic(args, function_declaration, args_additional_get, params_change, arguments_assert_args_change);
     function arguments_assert_args_change(arguments_assert_args) {
-        let type = js_parse_expression(function_name_get(input_type));
+        let type = js_parse_expression((input_type));
         list_add(arguments_assert_args, type);
     }
     function params_change(params) {
