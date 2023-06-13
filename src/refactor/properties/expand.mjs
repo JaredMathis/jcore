76ba398e409768f994009cf22dff0dd49b5bed4a
JaredMathis <jared.mathis@native.consulting>
2023-06-13 09:47:18 -0400

    #92 function_auto refactor_properties_expand

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -1,11 +1,16 @@
-import { js_nodes_type_log } from '../../js/nodes/type/log.mjs';
+import { equal } from '../../equal.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
 import { string_to_list } from '../../string/to/list.mjs';
+import { list_map } from '../../list/map.mjs';
+import { string_letter_is } from '../../string/letter/is.mjs';
+import { string_to_case_upper } from '../../string/to/case/upper.mjs';
 export function refactor_properties_expand(args) {
     arguments_assert(arguments, [defined_is]);
     let {function_declaration, parsed} = args;
     const node_type = 'ObjectPattern';
     let characters = string_to_list(node_type);
-    let indices_capital = 
+    let indices_capital = list_map(characters, c => {
+        return { is_capital: string_letter_is(c) && equal(string_to_case_upper(c), c) };
+    });
 }
