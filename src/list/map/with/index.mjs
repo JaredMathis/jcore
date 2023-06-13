87c326e76f58cf6c42740d1199b0b148a264cd8e
JaredMathis <jared.mathis@native.consulting>
2023-06-13 10:17:37 -0400

    #92 function_unasyncify list_map_with_index_async

---
+++ src/list/map/with/index.mjs
@@ -0,0 +1,14 @@
+import { metadata_generated } from '../../../metadata/generated.mjs';
+import { metadata } from '../../../metadata.mjs';
+import { list_map_generic } from '../generic.mjs';
+import { function_is } from '../../../function/is.mjs';
+import { list_is } from '../../is.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function list_map_with_index(list, mapper) {
+    arguments_assert(arguments, [
+        list_is,
+        function_is
+    ]);
+    return list_map_generic(list, mapper);
+    metadata([metadata_generated()]);
+}
