24f4e35f96f2443ff694edb45e9495018d820892
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:51:56 -0400

    #92 noop

--- src/function/add/string/prefix/generic.mjs
+++ src/function/add/string/prefix/generic.mjs
@@ -3,4 +3,5 @@ import { string_identifier_with_prefix } from '../../../../string/identifier/wit
 export async function function_add_string_prefix_generic(prefix, suffix, value) {
     let function_name = string_identifier_with_prefix(prefix, suffix);
     await function_add_string(function_name, value);
+    return function_name;
 }
