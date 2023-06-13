583c83f87dcae345411462275b8ea24a93745c37
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:09:01 -0400

    #92 function_copy js_function_declaration_asyncify js_function_declaration_async_is

---
+++ src/js/function/declaration/async/is.mjs
@@ -0,0 +1,10 @@
+import { metadata } from '../../../../metadata.mjs';
+import { js_keyword_async } from '../../../keyword/async.mjs';
+import { object_property_change } from '../../../../object/property/change.mjs';
+import { js_node_is_function_declaration } from '../../../node/is/function/declaration.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function js_function_declaration_async_is(fd) {
+    arguments_assert(arguments, [js_node_is_function_declaration]);
+    object_property_change(fd, js_keyword_async(), true);
+    metadata([]);
+}
