5efa00d97a18228d8a4d26c85ce0da5e1b66241a
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:42:18 -0400

    #92 function_auto refactor_call_replace

--- src/refactor/call/replace.mjs
+++ src/refactor/call/replace.mjs
@@ -5,16 +5,17 @@ import { js_visit_nodes_call_expression_name_equal } from '../../js/visit/nodes/
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { object_property_set } from '../../object/property/set.mjs';
 export function refactor_call_replace(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {function_name_called_old, function_name_called_new} = args;
     let {parsed} = args;
     js_visit_nodes_call_expression_name_equal(parsed, function_name_called_old, n => {
-        let callee = object_property_get(ce, js_node_property_callee());
+        let callee = object_property_get(n, js_node_property_callee());
         if (!js_node_is_identifier(callee)) {
             return;
         }
-        let name = object_property_get(callee, 'name');
+        object_property_set(callee, function_name_called_new);
     });
     error('todo: refactor_call_replace');
 }
