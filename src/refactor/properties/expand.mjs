2f842b58e0979cf58a0d8f912a3e6c0c48a87e0a
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:57:39 -0400

    #92 noop

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -3,10 +3,13 @@ import { js_visit_nodes_filter } from '../../js/visit/nodes/filter.mjs';
 import { js_node_is_object_pattern } from '../../js/node/is/object/pattern.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
+import { assert } from '../../assert.mjs';
 export function refactor_properties_expand(args) {
     arguments_assert(arguments, [defined_is]);
     let {function_declaration, parsed} = args;
     js_visit_nodes_filter(parsed, js_node_is_object_pattern, v => {
-        console.log({ v });
+        if (js_node_is_variable_declarator(v)) {
+            
+        }
     });
 }
