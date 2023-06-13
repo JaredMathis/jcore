aeb0b82b97904953b58824d8486725afe816caa9
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:24:54 -0400

    #92 function_delete function_add_string_prefix_camel

--- src/function/add/string/prefix/camel.mjs
+++
@@ -1,7 +0,0 @@
-import { function_add_string_prefix } from '../prefix.mjs';
-import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function function_add_string_prefix_camel() {
-    arguments_assert(arguments, []);
-    let result = await function_add_string_prefix(prefix, value);
-    return result;
-}
