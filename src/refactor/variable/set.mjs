449972d757e9bc5e8082008eff281ddf1880416e
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:39:29 -0400

    #92 function_auto refactor_variable_set

--- src/refactor/variable/set.mjs
+++ src/refactor/variable/set.mjs
@@ -1,3 +1,4 @@
+import { js_property_name } from '../../js/property/name.mjs';
 import { js_node_is_variable_declaration } from '../../js/node/is/variable/declaration.mjs';
 import { js_node_property_declarations } from '../../js/node/property/declarations.mjs';
 import { js_node_is_variable_declarator } from '../../js/node/is/variable/declarator.mjs';
@@ -10,6 +11,8 @@ import { js_visit_nodes_filter } from '../../js/visit/nodes/filter.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 import { list_length_is_1 } from '../../list/length/is/1.mjs';
 import { list_single } from '../../list/single.mjs';
+import { js_node_is_identifier } from '../../js/node/is/identifier.mjs';
+import { equal } from '../../equal.mjs';
 export async function refactor_variable_set(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {identifier, value} = args;
@@ -29,5 +32,9 @@ export async function refactor_variable_set(args) {
             return false;
         }
         let id = object_property_get(declaration, js_node_property_id());
+        if (!js_node_is_identifier(id)) {
+            return false;
+        }
+        return equal(object_property_get(id, js_property_name()), identifier);
     }, n => console.log(n));
 }
