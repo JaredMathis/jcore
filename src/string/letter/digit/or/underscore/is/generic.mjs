efb748edab62f4041278266fd0f4486130f1f848
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:45:19 -0400

    #92 function_copy string_letter_digit_or_underscore_is string_letter_digit_or_underscore_is_generic

---
+++ src/string/letter/digit/or/underscore/is/generic.mjs
@@ -0,0 +1,25 @@
+import { string_underscore_is } from '../../../../../underscore/is.mjs';
+import { string_digit_is } from '../../../../../digit/is.mjs';
+import { string_letter_is } from '../../../../is.mjs';
+import { string_length_is_1 } from '../../../../../length/is/1.mjs';
+import { string_is } from '../../../../../is.mjs';
+import { arguments_assert } from '../../../../../../arguments/assert.mjs';
+export function string_letter_digit_or_underscore_is_generic(c) {
+    arguments_assert(arguments, [string_is]);
+    let allow_underscores = true;
+    if (!string_length_is_1(c)) {
+        return false;
+    }
+    if (string_letter_is(c)) {
+        return true;
+    }
+    if (string_digit_is(c)) {
+        return true;
+    }
+    if (allow_underscores) {
+        if (string_underscore_is(c)) {
+            return true;
+        }
+    }
+    return false;
+}
