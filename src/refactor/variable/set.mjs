da8014b6bc0100348f9088f184c7809a5a34f0d6
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:34:50 -0400

    #92 function_auto refactor_variable_set

--- src/refactor/variable/set.mjs
+++ src/refactor/variable/set.mjs
@@ -6,11 +6,14 @@ import { js_parse_statement_let } from '../../js/parse/statement/let.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { js_visit_nodes_filter } from '../../js/visit/nodes/filter.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
 export async function refactor_variable_set(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {identifier, value} = args;
     let {parsed} = args;
     let expecting = js_parse_statement_let(identifier, ``);
     console.log(json_to({ expecting }));
-    js_visit_nodes_filter(parsed, n => js_node_is_variable_declarator(n) && js_node_property_declarations(), n => console.log(n));
+    js_visit_nodes_filter(parsed, n => {
+        return js_node_is_variable_declarator(n) && object_property_get(n, js_node_property_declarations());
+    }, n => console.log(n));
 }
