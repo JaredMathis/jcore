bbec21d04d8f9280251b8b4ce1ff03eacad9538c
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:57:00 -0400

    #92 function_auto function_rename

--- src/function/rename.mjs
+++ src/function/rename.mjs
@@ -1,8 +1,10 @@
+import { function_rename_no_open } from './rename/no/open.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function function_rename(function_name_old, function_name_new) {
+export async function function_rename(function_name_old, function_name_new) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is
     ]);
+    await function_rename_no_open(function_name_old, function_name_new);
 }
