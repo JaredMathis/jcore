43ac737af4ecdb43ccffadbd1239b9d3f6f25075
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:35:57 -0400

    #92 function_auto function_input_add_generic

--- src/function/input/add/generic.mjs
+++ src/function/input/add/generic.mjs
@@ -1,3 +1,4 @@
+import { js_parse_expression } from '../../../js/parse/expression.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { refactor_input_add } from '../../../refactor/input/add.mjs';
@@ -12,7 +13,14 @@ export async function function_input_add_generic(function_name, input_name, inpu
     ]);
     await function_map_with_args(function_name_get(refactor_input_add), function_name, {
         input_name,
-        input_value_default,
+        args_additional_get: function args_additional_get() {
+            let input_value_expression = js_parse_expression(input_value_default);
+            const args_additional = {
+                input_value_expression,
+                function_name_called: function_name
+            };
+            return args_additional;
+        },
         input_type
     });
 }
--- src/refactor/input/generic.mjs
+++ src/refactor/input/generic.mjs
@@ -9,7 +9,7 @@ import { refactor_import_fix } from '../import/fix.mjs';
 import { js_function_declaration_to_statement_arguments_assert_args_predicate } from '../../js/function/declaration/to/statement/arguments/assert/args/predicate.mjs';
 import { js_function_declaration_to_params } from '../../js/function/declaration/to/params.mjs';
 import { js_parse_expression } from '../../js/parse/expression.mjs';
-export async function refactor_input_generic(args, function_declaration, input_value_default, params_change, arguments_assert_args_change) {
+export async function refactor_input_generic(args, function_declaration, args_additional_get, params_change, arguments_assert_args_change) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo,
@@ -29,12 +29,5 @@ export async function refactor_input_generic(args, function_declaration, input_v
         await function_map_with_args(function_name_get(refactor_input_add_caller), caller, args_additional);
     }
 
-    function args_additional_get() {
-        let input_value_expression = js_parse_expression(input_value_default);
-        const args_additional = {
-            input_value_expression,
-            function_name_called: function_name
-        };
-        return args_additional;
-    }
+    
 }
