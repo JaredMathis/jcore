cc328a2f44e6355fc4c19812c6b061115e1f8042
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:55:12 -0400

    #92 function_auto function_copy_suffix

--- src/function/copy/suffix.mjs
+++ src/function/copy/suffix.mjs
@@ -1,3 +1,4 @@
+import { string_identifier_with_prefix } from '../../string/identifier/with/prefix.mjs';
 import { string_identifier_sub_is } from '../../string/identifier/sub/is.mjs';
 import { function_copy } from '../copy.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
@@ -8,5 +9,7 @@ export async function function_copy_suffix(function_name_prefix, function_name_s
         string_identifier_is,
         string_identifier_sub_is
     ]);
+    let function_name_to_copy = string_identifier_with_prefix(function_name_prefix, function_name_suffix_from);
+    let function_name_of_copy = string_identifier_with_prefix(function_name_prefix, function_name_suffix_to);
     await function_copy(function_name_to_copy, function_name_of_copy);
 }
