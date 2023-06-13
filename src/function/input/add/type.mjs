fb4a1e7875aca684ca70b7f8fad7d3a559a9917c
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:58:00 -0400

    #92 noop

--- src/function/input/add/type.mjs
+++ src/function/input/add/type.mjs
@@ -1,5 +1,4 @@
 import { function_input_add_generic } from './generic.mjs';
-import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { string_is } from '../../../string/is.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
