d607e0bea8aab76236154eced32b46cb9530b7c3
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:14:35 -0400

    #92 noop

--- src/js/list/add/identifier.mjs
+++ src/js/list/add/identifier.mjs
@@ -1,6 +1,9 @@
+import { assert } from '../../../assert.mjs';
 import { list_add } from '../../../list/add.mjs';
+import { js_node_is_identifier } from '../../node/is/identifier.mjs';
 import { js_parse_expression } from '../../parse/expression.mjs';
 export function js_list_add_identifier(list, identifier_name) {
     let identifier = js_parse_expression(identifier_name);
+    assert(js_node_is_identifier(identifier))
     list_add(list, identifier);
 }
