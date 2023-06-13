0c155ad49128ada76c24824b06956518d56d1648
JaredMathis <jared.mathis@native.consulting>
2023-06-12 21:12:00 -0400

    #92 function_rename_if_ends_with _variable_new _variable_add

--- src/refactor/variable/add.mjs
+++ src/refactor/variable/add.mjs
@@ -1,6 +1,6 @@
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function refactor_variable_new() {
+export function refactor_variable_add() {
     arguments_assert(arguments, []);
     error('todo: refactor_variable_new');
 }
