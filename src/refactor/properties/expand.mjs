e16012ecd3db3b85eacae58c6effb4e0e8279340
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:52:34 -0400

    #92 function_auto refactor_properties_expand

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -1,7 +1,8 @@
-import { string_case_camel_to_snake } from '../../string/case/camel/to/snake.mjs';
+import { js_node_is_object_pattern } from '../../js/node/is/object/pattern.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
 export function refactor_properties_expand(args) {
     arguments_assert(arguments, [defined_is]);
     let {function_declaration, parsed} = args;
+    js_node_is_object_pattern;
 }
