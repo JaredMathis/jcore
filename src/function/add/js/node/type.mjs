0b44d7c35431fd3ce40e1be94e67113f63e082c8
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:49:16 -0400

    #92 function_input_first_remove function_add_js_node_type

--- src/function/add/js/node/type.mjs
+++ src/function/add/js/node/type.mjs
@@ -1,11 +1,8 @@
 import { function_add_string_prefix_camel_generic } from '../../string/prefix/camel/generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function function_add_js_node_type(prefix, value) {
-    arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
-    ]);
+export async function function_add_js_node_type(value) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let prefix2 = 'js_node_is';
     await function_add_string_prefix_camel_generic(prefix, value);
 }
