6089cadf3b67bf57c75a52f75c45a6cf3634deb7
JaredMathis <jared.mathis@native.consulting>
2023-06-13 19:00:23 -0400

    #92 function_delete function_properties_expand

--- src/function/properties/expand.mjs
+++
@@ -1,9 +0,0 @@
-import { refactor_properties_expand } from '../../refactor/properties/expand.mjs';
-import { function_map_with_args } from '../map/with/args.mjs';
-import { function_name_get } from '../name/get.mjs';
-import { string_identifier_is } from '../../string/identifier/is.mjs';
-import { arguments_assert } from '../../arguments/assert.mjs';
-export async function function_properties_expand(function_name) {
-    arguments_assert(arguments, [string_identifier_is]);
-    await function_map_with_args(function_name_get(refactor_properties_expand), function_name, {});
-}
