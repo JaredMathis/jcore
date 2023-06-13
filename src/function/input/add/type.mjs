32665b91891f40fd4a6ac57d3d12e418b94408eb
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:23:28 -0400

    #92 function_auto function_input_add_type

--- src/function/input/add/type.mjs
+++ src/function/input/add/type.mjs
@@ -1,3 +1,4 @@
+import { function_input_value_default } from '../value/default.mjs';
 import { function_input_add_generic } from './generic.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -7,5 +8,6 @@ export async function function_input_add_type(function_name, input_name, input_t
         string_identifier_is,
         string_identifier_is
     ]);
+    let input_value_default = function_input_value_default();
     await function_input_add_generic(function_name, input_name, input_type, input_value_default);
 }
