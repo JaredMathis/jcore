ccd6050f0d7f4de04b6ba86becd04771d717ca20
JaredMathis <jared.mathis@native.consulting>
2023-06-12 20:46:40 -0400

    #92 function_auto string_letter_digit_or_underscore_is

--- src/string/letter/digit/or/underscore/is.mjs
+++ src/string/letter/digit/or/underscore/is.mjs
@@ -1,3 +1,4 @@
+import { string_letter_digit_or_underscore_is_generic } from './is/generic.mjs';
 import { string_is } from '../../../../is.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { string_underscore_is } from '../../../../underscore/is.mjs';
@@ -6,6 +7,9 @@ import { string_letter_is } from '../../../is.mjs';
 import { string_length_is_1 } from '../../../../length/is/1.mjs';
 export function string_letter_digit_or_underscore_is(c) {
     arguments_assert(arguments, [string_is]);
+    if (false) {
+        string_letter_digit_or_underscore_is_generic;
+    }
     let allow_underscores = true;
     if (!string_length_is_1(c)) {
         return false;
