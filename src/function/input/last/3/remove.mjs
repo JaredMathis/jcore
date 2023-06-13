4b61a38c251bbe56ff4edb92b188811ca0a6c880
JaredMathis <jared.mathis@native.consulting>
2023-06-13 08:37:30 -0400

    #92 function_auto function_input_last_3_remove

--- src/function/input/last/3/remove.mjs
+++ src/function/input/last/3/remove.mjs
@@ -1,7 +1,10 @@
+import { function_input_last_remove } from '../remove.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function function_input_last_3_remove(function_name) {
+import { range } from '../../../../range.mjs';
+export async function function_input_last_3_remove(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: function_input_last_3_remove');
+    for (let i of range(3)) {
+        await function_input_last_remove(function_name);
+    }
 }
