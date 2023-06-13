ffa3d9ee9c0e6d6d5d1454747c72995602998a0c
JaredMathis <jared.mathis@native.consulting>
2023-06-12 21:54:29 -0400

    #92 function_auto refactor_variable_add

--- src/refactor/variable/add.mjs
+++ src/refactor/variable/add.mjs
@@ -1,3 +1,4 @@
+import { refactor_import_fix } from '../import/fix.mjs';
 import { js_function_declaration_statement_add_after_arguments_assert } from '../../js/function/declaration/statement/add/after/arguments/assert.mjs';
 import { js_keyword_let } from '../../js/keyword/let.mjs';
 import { js_parse_statement } from '../../js/parse/statement.mjs';
@@ -8,4 +9,5 @@ export async function refactor_variable_add(args) {
     let {function_declaration, identifier} = args;
     let statement = js_parse_statement(`${ js_keyword_let() } ${ identifier }`);
     await js_function_declaration_statement_add_after_arguments_assert(function_declaration, statement);
+    refactor_import_fix;
 }
