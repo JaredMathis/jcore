a230ad580f8d2c898593e83ee67175bda4b714af
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:10:12 -0400

    #92 function_auto_multiple refactor_input_add_caller

--- src/refactor/input/add/caller.mjs
+++ src/refactor/input/add/caller.mjs
@@ -3,11 +3,13 @@ import { js_call_expression_arguments } from '../../../js/call/expression/argume
 import { js_visit_nodes_call_expression_name_equal } from '../../../js/visit/nodes/call/expression/name/equal.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function refactor_input_add_caller(args) {
+import { refactor_import_fix } from '../../import/fix.mjs';
+export async function refactor_input_add_caller(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {input_value_expression, function_declaration, function_name_called, parsed} = args;
     js_visit_nodes_call_expression_name_equal(parsed, function_name_called, node => {
         let args = js_call_expression_arguments(node);
         list_add(args, input_value_expression);
     });
+    await refactor_import_fix(args);
 }
