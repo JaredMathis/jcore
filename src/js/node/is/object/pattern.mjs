9fd9f4377dbddb1edc6d59ed6014d676bb157869
JaredMathis <jared.mathis@native.consulting>
2023-06-13 12:55:49 -0400

    #92 noop

--- src/js/node/is/object/pattern.mjs
+++ src/js/node/is/object/pattern.mjs
@@ -1,6 +1,7 @@
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { object_property_get } from '../../../../object/property/get.mjs';
 export function js_node_is_object_pattern(node) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    return 'ObjectPattern';
+    return object_property_get(node, 'ObjectPattern');
 }
