d40f2afb58af7fca4e08d4627f15fa09b3404c7f
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:24:42 -0400

    #92 function_auto function_input_swap_last_2

---
+++ src/function/input/swap/last/2.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../../error.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function function_input_swap_last_2() {
+    arguments_assert(arguments, []);
+    error('todo: function_input_swap_last_2');
+}
