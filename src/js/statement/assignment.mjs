916b01a847925bb6f61362143ae55a7a12fb4381
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:07:43 -0400

    #92 function_auto js_statement_assignment

--- src/js/statement/assignment.mjs
+++ src/js/statement/assignment.mjs
@@ -1,7 +1,14 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { js_statement_end } from './end.mjs';
 import { js_keyword_let } from '../keyword/let.mjs';
+import { js_token_equal } from '../token/equal.mjs';
 export function js_statement_assignment(identifier, value) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let keyword_let = js_keyword_let();
-    let statement = `${ keyword_let } ${ identifier } = ${ value }${ js_statement_end() }`;
+    let statement = `${ keyword_let } ${ identifier } ${ js_token_equal() } ${ value }${ js_statement_end() }`;
     return statement;
 }
