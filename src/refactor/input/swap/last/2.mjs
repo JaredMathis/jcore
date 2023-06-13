67ec52676f38757fae281d20e931c0c36ef7cd3f
JaredMathis <jared.mathis@native.consulting>
2023-06-12 22:59:19 -0400

    #92 noop

--- src/refactor/input/swap/last/2.mjs
+++ src/refactor/input/swap/last/2.mjs
@@ -4,7 +4,7 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { list_swap_last_2 } from '../../../../list/swap/last/2.mjs';
 export async function refactor_input_swap_last_2(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let lambda;
+    let lambda = list_swap_last_2;
     let {function_declaration} = args;
     await refactor_input_generic(args, function_declaration, args_additional_get, params_change, arguments_assert_args_change);
     function arguments_assert_args_change(arguments_assert_args) {
