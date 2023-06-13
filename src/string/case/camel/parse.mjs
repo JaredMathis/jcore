9dfdc0de5bb21ba114fdc66aaa39a6bc63d71632
JaredMathis <jared.mathis@native.consulting>
2023-06-13 11:20:38 -0400

    #92 string_case_camel_parse ObjectPattern

--- src/string/case/camel/parse.mjs
+++ src/string/case/camel/parse.mjs
@@ -5,8 +5,12 @@ import { list_filter_property } from '../../../list/filter/property.mjs';
 import { string_case_upper_is } from '../upper/is.mjs';
 import { list_map_with_index } from '../../../list/map/with/index.mjs';
 import { string_to_list } from '../../to/list.mjs';
-export function string_case_camel_parse(node_type) {
-    let characters = string_to_list(node_type);
+import { equal } from '../../../equal.mjs';
+import { list_add } from '../../../list/add.mjs';
+import { list_last_index } from '../../../list/last/index.mjs';
+import { list_length } from '../../../list/length.mjs';
+export function string_case_camel_parse(input) {
+    let characters = string_to_list(input);
     const property_is_capital = 'is_capital';
     const property_index = 'index';
     let mapped = list_map_with_index(characters, (c, index) => {
@@ -17,10 +21,14 @@ export function string_case_camel_parse(node_type) {
     });
     let filtered = list_filter_property(mapped, property_is_capital, true);
     let indices = list_map_property(filtered, property_index);
+    list_add(indices, list_length(characters))
     return list_new_then(list_new_then_add => {
         let previous = 0;
         for (let index of indices) {
-            let part = string_sub(node_type, previous, index);
+            if (equal(index, 0)) {
+                continue;
+            }
+            let part = string_sub(input, previous, index);
             list_new_then_add(part);
             previous = index;
         }
