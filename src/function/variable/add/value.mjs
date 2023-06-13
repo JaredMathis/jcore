78a0ccf9f92fdb87f5fa9a829d84149a01865da8
JaredMathis <jared.mathis@native.consulting>
2023-06-13 05:57:17 -0400

    #92 function_auto function_variable_add_value

--- src/function/variable/add/value.mjs
+++ src/function/variable/add/value.mjs
@@ -1,5 +1,5 @@
+import { refactor_variable_add_value } from '../../../refactor/variable/add/value.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
-import { refactor_variable_add } from '../../../refactor/variable/add.mjs';
 import { function_name_get } from '../../name/get.mjs';
 import { function_map_with_args } from '../../map/with/args.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
@@ -10,5 +10,8 @@ export async function function_variable_add_value(function_name, identifier, ide
         string_identifier_is,
         arguments_assert_todo
     ]);
-    await function_map_with_args(function_name_get(refactor_variable_add), function_name, { identifier });
+    await function_map_with_args(function_name_get(refactor_variable_add_value), function_name, {
+        identifier,
+        identifier_value
+    });
 }
