90480a389b4664668d6610a62d4ec64369d5c24a
JaredMathis <jared.mathis@native.consulting>
2023-06-13 06:39:44 -0400

    #92 function_auto refactor_variable_set

--- src/refactor/variable/set.mjs
+++ src/refactor/variable/set.mjs
@@ -2,9 +2,7 @@ import { js_property_name } from '../../js/property/name.mjs';
 import { js_node_is_variable_declaration } from '../../js/node/is/variable/declaration.mjs';
 import { js_node_property_declarations } from '../../js/node/property/declarations.mjs';
 import { js_node_is_variable_declarator } from '../../js/node/is/variable/declarator.mjs';
-import { json_to } from '../../json/to.mjs';
 import { log } from '../../log.mjs';
-import { js_parse_statement_let } from '../../js/parse/statement/let.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { js_visit_nodes_filter } from '../../js/visit/nodes/filter.mjs';
@@ -17,8 +15,6 @@ export async function refactor_variable_set(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {identifier, value} = args;
     let {parsed} = args;
-    let expecting = js_parse_statement_let(identifier, ``);
-    console.log(json_to({ expecting }));
     js_visit_nodes_filter(parsed, n => {
         if (!js_node_is_variable_declaration(n)) {
             return false;
