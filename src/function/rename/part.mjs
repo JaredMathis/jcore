89f5b964c4a71eb5c3bc6f9ba80afc585174f89a
JaredMathis <jared.mathis@native.consulting>
2023-06-12 21:09:59 -0400

    #92 function_auto function_rename_part

--- src/function/rename/part.mjs
+++ src/function/rename/part.mjs
@@ -3,11 +3,17 @@ import { list_index_of_single } from '../../list/index/of/single.mjs';
 import { function_rename_part_generic } from './part/generic.mjs';
 import { string_identifier_part_is } from '../../string/identifier/part/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function function_rename_part(part) {
-    arguments_assert(arguments, [string_identifier_part_is]);
-    await function_rename_part_generic(part, parts_map);
+import { assert } from '../../assert.mjs';
+import { equal } from '../../equal.mjs';
+export async function function_rename_part(part_old) {
+    arguments_assert(arguments, [
+        string_identifier_part_is,
+        string_identifier_part_is
+    ]);
+    assert(!equal(part_old, part_new));
+    await function_rename_part_generic(part_old, parts_map);
     function parts_map(parts) {
-        let index = list_index_of_single(parts, part);
-        list_set;
+        let index = list_index_of_single(parts, part_old);
+        list_set(parts, index, part_new);
     }
 }
