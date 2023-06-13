45078b33fd214d40a71b492e7e41f8c2c0ffe4e6
JaredMathis <jared.mathis@native.consulting>
2023-06-12 23:01:35 -0400

    #92 function_auto function_input_last_remove

--- src/function/input/last/remove.mjs
+++ src/function/input/last/remove.mjs
@@ -1,4 +1,4 @@
-import { refactor_input_swap_last_2 } from '../../../refactor/input/swap/last/2.mjs';
+import { refactor_input_last_remove } from '../../../refactor/input/last/remove.mjs';
 import { function_name_get } from '../../name/get.mjs';
 import { function_map_with_args } from '../../map/with/args.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
@@ -10,7 +10,7 @@ export async function function_input_last_remove(function_name, input_name, inpu
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    await function_map_with_args(function_name_get(refactor_input_swap_last_2), function_name, {
+    await function_map_with_args(function_name_get(refactor_input_last_remove), function_name, {
         input_name,
         input_value_default,
         input_type
