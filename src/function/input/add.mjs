0302071c3c9484d11e800a3aa90ab64e527f6410
JaredMathis <jared.mathis@native.consulting>
2023-06-13 05:42:21 -0400

    #92 noop

--- src/function/input/add.mjs
+++ src/function/input/add.mjs
@@ -10,5 +10,5 @@ export async function function_input_add(function_name, input_name) {
         arguments_assert_todo
     ]);
     let input_value_default = js_code_call_expression(function_name_get(error));
-    await function_input_add_generic(function_name, input_name, arguments_assert_todo, input_value_default);
+    await function_input_add_generic(function_name, input_name, function_name_get(arguments_assert_todo), input_value_default);
 }
--- src/function/input/add/default.mjs
+++ src/function/input/add/default.mjs
@@ -3,11 +3,12 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { string_is } from '../../../string/is.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { function_name_get } from '../../name/get.mjs';
 export async function function_input_add_default(function_name, input_name, input_value_default) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is,
         string_is
     ]);
-    await function_input_add_generic(function_name, input_name, arguments_assert_todo, input_value_default);
+    await function_input_add_generic(function_name, input_name, function_name_get(arguments_assert_todo), input_value_default);
 }
