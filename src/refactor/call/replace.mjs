064bd99b1f43544aead25747d93e80e7a54e844d
JaredMathis <jared.mathis@native.consulting>
2023-06-13 07:35:04 -0400

    #92 function_auto refactor_call_replace

--- src/refactor/call/replace.mjs
+++ src/refactor/call/replace.mjs
@@ -3,5 +3,6 @@ import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function refactor_call_replace(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let {function_name_called_old, function_name_called_new} = args;
     error('todo: refactor_call_replace');
 }
