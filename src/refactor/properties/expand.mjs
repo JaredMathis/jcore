0bc4f034dde68ce4bd1141f7748cde09eabd12b9
JaredMathis <jared.mathis@native.consulting>
2023-06-13 13:17:02 -0400

    #92 function_auto refactor_properties_expand

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -1,6 +1,6 @@
+import { js_node_is_block_statement } from '../../js/node/is/block/statement.mjs';
 import { js_visit_node_grandparent } from '../../js/visit/node/grandparent.mjs';
 import { js_node_is_variable_declaration } from '../../js/node/is/variable/declaration.mjs';
-import { log } from '../../log.mjs';
 import { js_node_is_variable_declarator } from '../../js/node/is/variable/declarator.mjs';
 import { js_visit_nodes_filter } from '../../js/visit/nodes/filter.mjs';
 import { js_node_is_object_pattern } from '../../js/node/is/object/pattern.mjs';
@@ -15,7 +15,8 @@ export function refactor_properties_expand(args) {
             let grandparent = js_visit_node_grandparent(stack, 0);
             if (js_node_is_variable_declaration(grandparent)) {
                 let grandparent_great = js_visit_node_grandparent(stack, 1);
-                console.log({ grandparent_great });
+                if (js_node_is_block_statement(grandparent)) {
+                }
             }
         }
     });
