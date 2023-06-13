794f7c19cda11f69945ab7bc9e594a77f04bc005
JaredMathis <jared.mathis@native.consulting>
2023-06-12 21:56:56 -0400

    #92 function_variable_add refactor_variable_add after_let_identifier

--- src/refactor/variable/add.mjs
+++ src/refactor/variable/add.mjs
@@ -6,6 +6,7 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 export async function refactor_variable_add(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let after_let_identifier;
     let {function_declaration, identifier} = args;
     let statement = js_parse_statement(`${ js_keyword_let() } ${ identifier }`);
     await js_function_declaration_statement_add_after_arguments_assert(function_declaration, statement);
