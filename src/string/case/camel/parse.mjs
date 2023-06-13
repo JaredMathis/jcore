9fb6ea31cda673e1d82bcb6520cd36b70a6b0a1b
JaredMathis <jared.mathis@native.consulting>
2023-06-13 11:23:06 -0400

    #92 function_auto string_case_camel_parse

--- src/string/case/camel/parse.mjs
+++ src/string/case/camel/parse.mjs
@@ -1,3 +1,6 @@
+import { string_to_case_lower } from '../../to/case/lower.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_sub } from '../../sub.mjs';
 import { list_new_then } from '../../../list/new/then.mjs';
 import { list_map_property } from '../../../list/map/property.mjs';
@@ -7,12 +10,11 @@ import { list_map_with_index } from '../../../list/map/with/index.mjs';
 import { string_to_list } from '../../to/list.mjs';
 import { equal } from '../../../equal.mjs';
 import { list_add } from '../../../list/add.mjs';
-import { list_last_index } from '../../../list/last/index.mjs';
 import { list_length } from '../../../list/length.mjs';
 import { string_empty } from '../../empty.mjs';
 import { list_map } from '../../../list/map.mjs';
-import { string_to_case_upper } from '../../to/case/upper.mjs';
 export function string_case_camel_parse(input) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let characters = string_to_list(input);
     const property_is_capital = 'is_capital';
     const property_index = 'index';
@@ -24,7 +26,7 @@ export function string_case_camel_parse(input) {
     });
     let filtered = list_filter_property(mapped, property_is_capital, true);
     let indices = list_map_property(filtered, property_index);
-    list_add(indices, list_length(characters))
+    list_add(indices, list_length(characters));
     let parts = list_new_then(list_new_then_add => {
         let previous = 0;
         for (let index of indices) {
@@ -38,6 +40,6 @@ export function string_case_camel_parse(input) {
             previous = index;
         }
     });
-    list_map(parts, string_to_case_lower)
+    list_map(parts, string_to_case_lower);
     return parts;
 }
