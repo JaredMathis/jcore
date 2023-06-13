4c43aad70a6e439210d3ddae9fccce38c967e3c9
JaredMathis <jared.mathis@native.consulting>
2023-06-13 08:14:54 -0400

    #92 function_input_last_remove count_async

--- src/count/async.mjs
+++ src/count/async.mjs
@@ -1,10 +1,7 @@
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function count_async(then, lambda) {
-    arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
-    ]);
+export function count_async(then) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let result = 0;
     then(counter);
     function counter() {
--- src/function/arguments/assert/default/count.mjs
+++ src/function/arguments/assert/default/count.mjs
@@ -3,7 +3,7 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { count } from '../../../../count.mjs';
 export async function function_arguments_assert_default_count() {
     arguments_assert(arguments, []);
-    let result = count(counter => {
+    let result = await count_async(async counter => {
         await function_arguments_assert_default_generic(on_equals);
         async function on_equals(function_name) {
             counter()
