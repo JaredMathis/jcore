775fb394a9167d0cb3cb4be0fb8c624ec3bc49cb
JaredMathis <jared.mathis@native.consulting>
2023-06-14 12:22:07 -0400

    #93 function_asyncify list_new_then_generic

---
+++ src/list/new/then/generic/async.mjs
@@ -0,0 +1,16 @@
+import { defined_is } from '../../../../defined/is.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export async function list_new_then_generic_async(then, lambda) {
+    await arguments_assert_async(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let list = [];
+    then(list_new_then_add);
+    function list_new_then_add(element) {
+        await arguments_assert_async(arguments, [defined_is]);
+        lambda(list, element);
+    }
+    return list;
+}
