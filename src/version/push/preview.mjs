d38edc7d57c1323f0a39df626a9c648a5b4ef7df
JaredMathis <jared.mathis@native.consulting>
2023-06-14 12:37:22 -0400

    #93 noop

--- src/version/push/preview.mjs
+++ src/version/push/preview.mjs
@@ -3,6 +3,6 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function version_push_preview(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let preview = false;
+    let preview = true;
     return await version_push_generic(repository_name, preview);
 }
