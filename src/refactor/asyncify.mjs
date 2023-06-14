db992221134b06760dd89a6f741bf2b3f32a5866
JaredMathis <jared.mathis@native.consulting>
2023-06-14 11:01:41 -0400

    #92 function_auto refactor_asyncify

--- src/refactor/asyncify.mjs
+++ src/refactor/asyncify.mjs
@@ -1,7 +1,10 @@
+import { js_node_is_call_expression } from '../js/node/is/call/expression.mjs';
+import { js_nodes_get } from '../js/nodes/get.mjs';
+import { refactor_async_add } from './async/add.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
-import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 export function refactor_asyncify(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: refactor_asyncify');
+    refactor_async_add(args);
+    js_nodes_get(parsed, js_node_is_call_expression);
 }
