c4842f10301e080d5b5d2a716f88d3f2e12dbcdc
JaredMathis <jared.mathis@native.consulting>
2023-06-14 09:37:02 -0400

    #92 function_auto refactor_output_to_object

--- src/refactor/output/to/object.mjs
+++ src/refactor/output/to/object.mjs
@@ -4,8 +4,6 @@ import { js_code_return_statement } from '../../../js/code/return/statement.mjs'
 import { js_node_property_properties } from '../../../js/node/property/properties.mjs';
 import { js_function_declaration_to_statements } from '../../../js/function/declaration/to/statements.mjs';
 import { comment } from '../../../comment.mjs';
-import { js_nodes_each } from '../../../js/nodes/each.mjs';
-import { list_new_then } from '../../../list/new/then.mjs';
 import { js_node_is_return_statment } from '../../../js/node/is/return/statment.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -20,10 +18,7 @@ import { js_node_is_identifier } from '../../../js/node/is/identifier.mjs';
 export function refactor_output_to_object(args) {
     arguments_assert(arguments, [defined_is]);
     let {parsed, function_declaration} = args;
-    let returns = list_new_then(function then(list_new_then_add) {
-        js_nodes_each(parsed, js_node_is_return_statment, list_new_then_add);
-    });
-    js_nodes_get;
+    let returns = js_nodes_get(parsed, js_node_is_return_statment);
     comment(`If this fails code needs changing`);
     assert(list_length_is_1(returns));
     let return_single = list_single(returns);
