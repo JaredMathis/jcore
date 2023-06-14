fa7e54b7320ac674e8db5de1a54e855d5ecb49be
JaredMathis <jared.mathis@native.consulting>
2023-06-13 20:25:52 -0400

    #92 noop

--- src/string/identifier/part/is.mjs
+++ src/string/identifier/part/is.mjs
@@ -4,7 +4,7 @@ import { string_is } from '../../is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export function string_identifier_part_is(value) {
     arguments_assert(arguments, [string_is]);
-    let enforce_first = true;
+    let enforce_first = false;
     let enforce_last = true;
     const allow_underscores = false;
     return string_identifier_is_generic(value, enforce_first, enforce_last, allow_underscores);
