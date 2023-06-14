b4c9721c6dc30d0975dab28a7f6b738c7ec61627
JaredMathis <jared.mathis@native.consulting>
2023-06-14 10:11:32 -0400

    #92 version_sync_local_preview default

--- src/version/sync/local/summary.mjs
+++ src/version/sync/local/summary.mjs
@@ -1,8 +1,10 @@
 import { version_sync_local_preview } from './preview.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { list_length } from '../../../list/length.mjs';
 export async function version_sync_local_summary(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let result = await version_sync_local_preview(repository_name);
-    return result;
+    let file_path_count = list_length(result)
+    return {file_path_count};
 }
