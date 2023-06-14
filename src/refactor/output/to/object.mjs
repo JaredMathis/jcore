fa81f24a4c256a0496d3b3ba8b1c639dd69436b4
JaredMathis <jared.mathis@native.consulting>
2023-06-14 09:43:43 -0400

    #92 function_auto refactor_output_to_object

--- src/refactor/output/to/object.mjs
+++ src/refactor/output/to/object.mjs
@@ -1,5 +1,5 @@
+import { list_last } from '../../../list/last.mjs';
 import { js_unparse } from '../../../js/unparse.mjs';
-import { js_block_statement_body } from '../../../js/block/statement/body.mjs';
 import { js_nodes_get } from '../../../js/nodes/get.mjs';
 import { js_return_statement_argument } from '../../../js/return/statement/argument.mjs';
 import { js_code_return_statement } from '../../../js/code/return/statement.mjs';
@@ -28,13 +28,16 @@ export function refactor_output_to_object(args) {
     comment(`Needs to be identifier - maybe convert to identifier in the future if not`);
     assert(js_node_is_identifier(return_single_argument));
     let statements = js_function_declaration_to_statements(function_declaration);
-    let statements_body = js_block_statement_body(statements);
+    console.log({
+        l: list_last(statements),
+        return_single
+    });
     comment(`If this fails code needs changing - needs to be 1 return statement per function`);
-    assert(list_contains(statements_body, return_single));
+    assert(list_contains(statements, return_single));
     const return_expression = `{ ${ string_a() } }`;
     let object = js_code_return_statement(return_expression);
     let arg = js_return_statement_argument(object);
     let properties = object_property_get(arg, js_node_property_properties());
-    log(js_unparse(properties));
+    log(js_unparse(object));
     return args;
 }
