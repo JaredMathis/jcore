2dc57f7fe9b3b6d82827147091e18b4deafae748
JaredMathis <jared.mathis@native.consulting>
2023-06-14 10:01:52 -0400

    #92 function_delete temp_a

--- src/temp/a.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../error.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
-export function temp_a() {
-    arguments_assert(arguments, []);
-    error('todo: temp_a');
-}
