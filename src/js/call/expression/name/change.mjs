71e5f177a40c2e5088ea2e5c92f0095651908c5f
JaredMathis <jared.mathis@native.consulting>
2023-06-14 12:05:35 -0400

    #93 function_auto js_call_expression_name_change

--- src/js/call/expression/name/change.mjs
+++ src/js/call/expression/name/change.mjs
@@ -1,3 +1,4 @@
+import { js_node_property_callee } from '../../../node/property/callee.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { js_identifier_name_change } from '../../../identifier/name/change.mjs';
 import { js_if_identifier_or_null_generic } from '../../../if/identifier/or/null/generic.mjs';
@@ -10,6 +11,7 @@ export function js_call_expression_name_change(ce, name_new) {
         arguments_assert_todo
     ]);
     let or_null = false;
+    const property_name = js_node_property_callee();
     return js_if_identifier_or_null_generic(ce, property_name, or_null, lambda);
     function lambda(property) {
         let name = js_identifier_name_change(property, name_new);
