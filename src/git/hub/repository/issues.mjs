6308377a37232db59cbe2790c17e2dbcc9a4ab23
JaredMathis <jared.mathis@native.consulting>
2023-06-14 19:54:04 -0400

    #93 noop

--- src/git/hub/repository/issues.mjs
+++ src/git/hub/repository/issues.mjs
@@ -6,10 +6,18 @@ import { file_json_read } from '../../../file/json/read.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
 import { git_ignore_add_if_not_exists } from '../../ignore/add/if/not/exists.mjs';
 import { error } from '../../../error.mjs';
+import { file_exists } from '../../../file/exists.mjs';
+import { path_join } from '../../../path/join.mjs';
+import { file_extension_json } from '../../../file/extension/json.mjs';
 export async function git_hub_repository_issues() {
     arguments_assert(arguments, []);
     await git_ignore_add_if_not_exists(git_ignore_cache());
     let key = function_name_get(git_hub_repository_issues);
+    let file_name = string_base64_to(key);
+    let file_path = path_join([git_ignore_cache(), string_add(file_name, file_extension_json())]);
+    if (await file_exists(file_path)) {
+
+    }
     let cache = await file_json_read(error());
     let p = await file_json_read('../private.json');
     let token = object_property_get(p, 'git_hub_api_token');
