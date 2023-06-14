4275d9f23b0edc0d1a0f053241b1c61115849674
JaredMathis <jared.mathis@native.consulting>
2023-06-14 11:21:14 -0400

    #92 function_input_add js_if_identifier_or_null_generic lambda

--- src/js/if/identifier/or/null/generic.mjs
+++ src/js/if/identifier/or/null/generic.mjs
@@ -1,10 +1,10 @@
-import { js_identifier_name_get } from '../../../../identifier/name/get.mjs';
 import { js_node_is_identifier } from '../../../../node/is/identifier.mjs';
 import { object_property_get } from '../../../../../object/property/get.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export function js_if_identifier_or_null_generic(node, property_name, or_null) {
+export function js_if_identifier_or_null_generic(node, property_name, or_null, lambda) {
     arguments_assert(arguments, [
+        arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
