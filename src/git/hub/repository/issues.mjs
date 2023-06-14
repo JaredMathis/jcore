6ce7507a9d52847784c0039c000a0c04255617ed
JaredMathis <jared.mathis@native.consulting>
2023-06-14 19:48:17 -0400

    #93 noop

--- src/git/hub/repository/issues.mjs
+++ src/git/hub/repository/issues.mjs
@@ -2,8 +2,10 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { Octokit } from 'octokit';
 import { file_json_read } from '../../../file/json/read.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
+import { git_ignore_add_if_not_exists } from '../../ignore/add/if/not/exists.mjs';
 export async function git_hub_repository_issues() {
     arguments_assert(arguments, []);
+    await git_ignore_add_if_not_exists('cache')
     let p = await file_json_read('../private.json');
     let token = object_property_get(p, 'git_hub_api_token');
     const octokit = new Octokit({ auth: token });
