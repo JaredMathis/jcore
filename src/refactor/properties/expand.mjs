2f1b40c6248439bd7868a1d6186f625a705d5491
JaredMathis <jared.mathis@native.consulting>
2023-06-13 20:27:07 -0400

    #92 function_properties_expand refactor_properties_expand

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -54,7 +54,7 @@ export function refactor_properties_expand(args) {
                                 js_parse_statement_let(identifier_next);
                             let args = [];
                             let args_code = js_code_join_comma(args);
-                            js_code_call_expression_statement_with_args_code(function_name_get(object_property_get), args);
+                            js_code_call_expression_statement_with_args_code(function_name_get(object_property_get), args_code);
                             console.log({
                                 key,
                                 value: local
