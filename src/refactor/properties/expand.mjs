f5a698a83539a557bd096609727562d5feecfb65
JaredMathis <jared.mathis@native.consulting>
2023-06-13 21:23:44 -0400

    #92 noop

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -43,8 +43,8 @@ export function refactor_properties_expand(args) {
                         let function_body_statements = js_block_statement_body(grandparent_great);
                         let previous = grandparent;
                         let v = js_identifier_name_next(parsed);
-                        object_replace(node, js_parse_expression(v));
                         let properties = object_property_get(node, js_node_property_properties());
+                        object_replace(node, js_parse_expression(v));
                         for (let property of properties) {
                             let key = js_property_identifier_name(property, js_node_property_key());
                             let local = js_property_identifier_name(property, js_node_property_value());
