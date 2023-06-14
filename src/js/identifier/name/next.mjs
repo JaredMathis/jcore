27c3a5dab0d7a67316c703e5bca59279d5850060
JaredMathis <jared.mathis@native.consulting>
2023-06-13 20:41:09 -0400

    #92 function_rename js_identifier_next js_identifier_name_next

--- src/js/identifier/name/next.mjs
+++ src/js/identifier/name/next.mjs
@@ -1,8 +1,8 @@
-import { string_to } from '../../string/to.mjs';
-import { string_identifier_with_prefix } from '../../string/identifier/with/prefix.mjs';
-import { list_contains } from '../../list/contains.mjs';
-import { js_identifiers } from '../identifiers.mjs';
-export function js_identifier_next(parsed) {
+import { string_to } from '../../../string/to.mjs';
+import { string_identifier_with_prefix } from '../../../string/identifier/with/prefix.mjs';
+import { list_contains } from '../../../list/contains.mjs';
+import { js_identifiers } from '../../identifiers.mjs';
+export function js_identifier_name_next(parsed) {
     let identifier_next_prefix = 'v';
     let identifier_next = identifier_next_prefix;
     let identifiers = js_identifiers(parsed);
