549fb56cb50d3248dec477f6cfbd1e937f621a35
JaredMathis <jared.mathis@native.consulting>
2023-06-14 19:56:20 -0400

    #93 function_auto git_hub_repository_issues

--- src/git/hub/repository/issues.mjs
+++ src/git/hub/repository/issues.mjs
@@ -1,3 +1,6 @@
+import { file_json_write } from '../../../file/json/write.mjs';
+import { string_add } from '../../../string/add.mjs';
+import { string_base64_to } from '../../../string/base64/to.mjs';
 import { function_name_get } from '../../../function/name/get.mjs';
 import { git_ignore_cache } from '../../ignore/cache.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -5,19 +8,20 @@ import { Octokit } from 'octokit';
 import { file_json_read } from '../../../file/json/read.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
 import { git_ignore_add_if_not_exists } from '../../ignore/add/if/not/exists.mjs';
-import { error } from '../../../error.mjs';
 import { file_exists } from '../../../file/exists.mjs';
 import { path_join } from '../../../path/join.mjs';
 import { file_extension_json } from '../../../file/extension/json.mjs';
 import { json_from } from '../../../json/from.mjs';
 import { json_to } from '../../../json/to.mjs';
-import { list_map } from '../../../list/map.mjs';
 export async function git_hub_repository_issues() {
     arguments_assert(arguments, []);
     await git_ignore_add_if_not_exists(git_ignore_cache());
     let key = function_name_get(git_hub_repository_issues);
     let file_name = string_base64_to(key);
-    let file_path = path_join([git_ignore_cache(), string_add(file_name, file_extension_json())]);
+    let file_path = path_join([
+        git_ignore_cache(),
+        string_add(file_name, file_extension_json())
+    ]);
     if (await file_exists(file_path)) {
         let json = await file_json_read(file_path);
         let result = json_from(json);
