26e4e56caa73e1f6329dc734e9ca20fbe3b6f90f
JaredMathis <jared.mathis@native.consulting>
2023-06-14 10:41:39 -0400

    #92 function_auto function_auto

--- src/function/auto.mjs
+++ src/function/auto.mjs
@@ -10,7 +10,6 @@ export async function function_auto(function_name) {
     if (!await function_exists(function_name)) {
         await function_add(function_name);
     }
-    await function_auto_after(function_name);
     await function_open_vs_code(function_name);
     metadata([]);
 }
