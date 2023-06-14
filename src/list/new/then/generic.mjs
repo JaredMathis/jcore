cddc8294db542b272dbcf9a40b0f51c5a2f11763
JaredMathis <jared.mathis@native.consulting>
2023-06-14 12:24:02 -0400

    #93 function_unasyncify list_new_then_generic_async

--- src/list/new/then/generic.mjs
+++ src/list/new/then/generic.mjs
@@ -1,6 +1,13 @@
-import { arguments_assert } from '../../../arguments/assert.mjs';
+import { metadata_generated } from '../../../metadata/generated.mjs';
+import { metadata } from '../../../metadata.mjs';
 import { defined_is } from '../../../defined/is.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 export function list_new_then_generic(then, lambda) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let list = [];
     then(list_new_then_add);
     function list_new_then_add(element) {
@@ -8,4 +15,5 @@ export function list_new_then_generic(then, lambda) {
         lambda(list, element);
     }
     return list;
+    metadata([metadata_generated()]);
 }
