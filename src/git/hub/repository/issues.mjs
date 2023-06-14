12df40c839f1dbf10df418142d3fead8c465f815
JaredMathis <jared.mathis@native.consulting>
2023-06-14 19:49:43 -0400

    #93 function_auto git_hub_repository_issues

--- src/git/hub/repository/issues.mjs
+++ src/git/hub/repository/issues.mjs
@@ -6,7 +6,7 @@ import { object_property_get } from '../../../object/property/get.mjs';
 import { git_ignore_add_if_not_exists } from '../../ignore/add/if/not/exists.mjs';
 export async function git_hub_repository_issues() {
     arguments_assert(arguments, []);
-    await git_ignore_add_if_not_exists(git_ignore_cache);
+    await git_ignore_add_if_not_exists(git_ignore_cache());
     let p = await file_json_read('../private.json');
     let token = object_property_get(p, 'git_hub_api_token');
     const octokit = new Octokit({ auth: token });
