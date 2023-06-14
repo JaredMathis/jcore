113dac134a22ef593c006010949541c42e105f12
JaredMathis <jared.mathis@native.consulting>
2023-06-14 11:28:57 -0400

    #93 function_rename js_call_expression_name_change_try js_call_expression_name_change

---
+++ src/js/call/expression/name/change.mjs
@@ -0,0 +1,15 @@
+import { js_identifier_name_get } from '../../../identifier/name/get.mjs';
+import { js_if_identifier_or_null_generic } from '../../../if/identifier/or/null/generic.mjs';
+import { metadata } from '../../../../metadata.mjs';
+import { js_node_is_call_expression } from '../../../node/is/call/expression.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function js_call_expression_name_change(ce) {
+    arguments_assert(arguments, [js_node_is_call_expression]);
+    let or_null = true;
+    return js_if_identifier_or_null_generic(node, property_name, or_null, lambda);
+    function lambda(property) {
+        let name = js_identifier_name_get(property);
+        return name;
+    }
+    metadata([]);
+}
