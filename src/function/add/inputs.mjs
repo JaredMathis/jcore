da8a5a9e80c4707de619230dbc45aa95336b6fa0
JaredMathis <jared.mathis@native.consulting>
2023-06-14 10:37:32 -0400

    #92 function_rename function_auto_inputs function_add_inputs

--- src/function/add/inputs.mjs
+++ src/function/add/inputs.mjs
@@ -1,6 +1,6 @@
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_auto_inputs() {
+export function function_add_inputs() {
     arguments_assert(arguments, []);
     error('todo: function_auto_inputs');
 }
