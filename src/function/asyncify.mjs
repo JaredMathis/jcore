6b8f63eddd8e4a48ec0e03d6f5c655e53926758e
JaredMathis <jared.mathis@native.consulting>
2023-06-14 10:21:10 -0400

    #92 function_auto function_asyncify

---
+++ src/function/asyncify.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function function_asyncify() {
+    arguments_assert(arguments, []);
+    error('todo: function_asyncify');
+}
