17c8fa4ed55276752172532343578679092ca767
JaredMathis <jared.mathis@native.consulting>
2023-06-14 09:30:21 -0400

    #92 noop

--- src/test/mom.mjs
+++
@@ -1,6 +0,0 @@
-import { string_is } from '../string/is.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
-export function test_mom(input) {
-    arguments_assert(arguments, [string_is]);
-    return 'hello ' + input;
-}
