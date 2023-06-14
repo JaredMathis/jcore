ce76a8a32376310097992ebf8dbb46032cab0b7c
JaredMathis <jared.mathis@native.consulting>
2023-06-14 12:37:05 -0400

    #93 function_copy version_push version_push_preview

---
+++ src/version/push/preview.mjs
@@ -0,0 +1,8 @@
+import { version_push_generic } from './generic.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export async function version_push_preview(repository_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    let preview = false;
+    return await version_push_generic(repository_name, preview);
+}
