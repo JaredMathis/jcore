83d33298729be38819996086be0bfa4ad92f188d
JaredMathis <jared.mathis@native.consulting>
2023-06-14 12:07:09 -0400

    #93 noop

--- src/function/dependencies/names.mjs
+++ src/function/dependencies/names.mjs
@@ -3,9 +3,9 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_map } from '../../list/map.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 import { function_dependencies } from '../dependencies.mjs';
-export async function function_dependencies_names(arguments_assert_name) {
+export async function function_dependencies_names(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    let dependencies = await function_dependencies(arguments_assert_name);
+    let dependencies = await function_dependencies(function_name);
     let dependencies_names = list_map(dependencies, d => object_property_get(d, 'function_name'));
     return dependencies_names;
 }
