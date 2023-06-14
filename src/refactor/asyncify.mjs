3f9b05338426cd780a774bbda1400d537ac0de7f
JaredMathis <jared.mathis@native.consulting>
2023-06-14 11:04:38 -0400

    #92 function_search call,exp,eq

--- src/refactor/asyncify.mjs
+++ src/refactor/asyncify.mjs
@@ -3,8 +3,10 @@ import { js_nodes_get } from '../js/nodes/get.mjs';
 import { refactor_async_add } from './async/add.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { function_name_all } from '../function/name/all.mjs';
 export function refactor_asyncify(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     refactor_async_add(args);
+    let function_names = await function_name_all();
     let calls = js_nodes_get(parsed, js_node_is_call_expression);
 }
