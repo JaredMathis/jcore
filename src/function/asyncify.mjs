81a97e01a9052ee7a7fc48a80e98f09628ec2175
JaredMathis <jared.mathis@native.consulting>
2023-06-14 10:33:29 -0400

    #92 function_auto refactor_asyncify

--- src/function/asyncify.mjs
+++ src/function/asyncify.mjs
@@ -5,9 +5,11 @@ import { function_naming_suffix_async } from './naming/suffix/async.mjs';
 import { string_add } from '../string/add.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { function_parse } from './parse.mjs';
 export async function function_asyncify(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let function_name_new = string_add(function_name, function_naming_suffix_async());
     await function_copy(function_name, function_name_new);
+    function_parse(function_name_new)
     js_nodes_get(parsed, js_node_is_call_expression);
 }
