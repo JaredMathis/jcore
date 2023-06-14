325a089411783d6c71b02818417b1da9574f3d2d
JaredMathis <jared.mathis@native.consulting>
2023-06-14 11:30:09 -0400

    #93 function_input_add js_call_expression_name_change name_new

--- src/js/call/expression/name/change.mjs
+++ src/js/call/expression/name/change.mjs
@@ -1,12 +1,16 @@
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { js_identifier_name_change } from '../../../identifier/name/change.mjs';
 import { js_if_identifier_or_null_generic } from '../../../if/identifier/or/null/generic.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { js_node_is_call_expression } from '../../../node/is/call/expression.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function js_call_expression_name_change(ce) {
-    arguments_assert(arguments, [js_node_is_call_expression]);
+export function js_call_expression_name_change(ce, name_new) {
+    arguments_assert(arguments, [
+        js_node_is_call_expression,
+        arguments_assert_todo
+    ]);
     let or_null = false;
-    return js_if_identifier_or_null_generic(node, property_name, or_null, lambda);
+    return js_if_identifier_or_null_generic(ce, property_name, or_null, lambda);
     function lambda(property) {
         let name = js_identifier_name_change(property);
         return name;
