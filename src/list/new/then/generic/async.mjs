48dcd43341218532aa53c4bf17a2746201d79a95
JaredMathis <jared.mathis@native.consulting>
2023-06-14 12:23:34 -0400

    #93 noop

--- src/list/new/then/generic/async.mjs
+++ src/list/new/then/generic/async.mjs
@@ -2,14 +2,14 @@ import { defined_is } from '../../../../defined/is.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function list_new_then_generic_async(then, lambda) {
-    await arguments_assert(arguments, [
+    arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
     let list = [];
-    then(list_new_then_add);
+    await then(list_new_then_add);
     function list_new_then_add(element) {
-        await arguments_assert(arguments, [defined_is]);
+        arguments_assert(arguments, [defined_is]);
         lambda(list, element);
     }
     return list;
