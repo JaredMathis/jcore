2db81edd8107bda9a75cc80a39548e570ca1463e
JaredMathis <jared.mathis@native.consulting>
2023-06-14 09:54:39 -0400

    #92 function_input_add js_return_statement_argument_change value

--- src/js/return/statement/argument/change.mjs
+++ src/js/return/statement/argument/change.mjs
@@ -4,8 +4,11 @@ import { assert } from '../../../../assert.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { object_property_change } from '../../../../object/property/change.mjs';
-export function js_return_statement_argument_change(object) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+export function js_return_statement_argument_change(object, value) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     assert(js_node_is_return_statment(object));
     let arg = object_property_change(object, js_node_property_argument());
     return arg;
