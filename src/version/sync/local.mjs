37aec43c8800beba9fcd0db7892854e2999888e7
JaredMathis <jared.mathis@native.consulting>
2023-06-14 12:50:27 -0400

    #93 function_auto version_sync_local

--- src/version/sync/local.mjs
+++ src/version/sync/local.mjs
@@ -10,6 +10,7 @@ import { version_commit_data } from '../commit/data.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { list_length } from '../../list/length.mjs';
 import { object_property_initialize } from '../../object/property/initialize.mjs';
+import { function_name_get } from '../../function/name/get.mjs';
 export async function version_sync_local(repository_name) {
     arguments_assert(arguments, [string_identifier_is]);
     const commit_data = version_commit_data(version_sync_local, arguments);
@@ -29,6 +30,7 @@ export async function version_sync_local(repository_name) {
     object_keys_each(lists, (list, list_name) => {
         let prefix = 'list_';
         let fn = list_length;
+        let fn_name = function_name_get(fn);
         let length = fn(list);
         object_property_initialize(result, string_add(list_name, '_length'), length);
     });
