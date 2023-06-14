de673d17dbbf72fcb349d1bac16347122f455eb7
JaredMathis <jared.mathis@native.consulting>
2023-06-14 19:59:47 -0400

    #93 function_auto git_hub_repository_issues_length

---
+++ src/git/hub/repository/issues/length.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../../error.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function git_hub_repository_issues_length() {
+    arguments_assert(arguments, []);
+    error('todo: git_hub_repository_issues_length');
+}
