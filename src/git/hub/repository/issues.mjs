9dce7775262a6d6fb1fd1b83f466477c27b9a6c9
JaredMathis <jared.mathis@native.consulting>
2023-06-14 13:36:22 -0400

    #93 function_auto git_hub_repository_issues

--- src/git/hub/repository/issues.mjs
+++ src/git/hub/repository/issues.mjs
@@ -1,7 +1,9 @@
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { Octokit } from 'octokit';
+import { file_json_read } from '../../../file/json/read.mjs';
 export async function git_hub_repository_issues() {
     arguments_assert(arguments, []);
+    let p = await file_json_read('../private.json');
     const octokit = new Octokit({ auth: 'YOUR-TOKEN' });
     await octokit.request('GET /repos/{owner}/{repo}/issues', {
         owner: 'github',
