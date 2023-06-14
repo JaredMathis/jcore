1845dc15d26ae2cec690a77b2b08afaf77f593d4
JaredMathis <jared.mathis@native.consulting>
2023-06-14 19:52:28 -0400

    #93 function_copy string_base64_to string_base64_from

--- src/string/base64/from.mjs
+++ src/string/base64/from.mjs
@@ -1,7 +1,6 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
-import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function string_base64_to(input) {
+export function string_base64_from(input) {
     arguments_assert(arguments, [arguments_assert_todo]);
     return atob(input);
 }
