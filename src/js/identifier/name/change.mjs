f5294a9b4e24dfdcd213b1e920727401b6ae08fe
JaredMathis <jared.mathis@native.consulting>
2023-06-14 11:16:04 -0400

    #92 function_input_add js_identifier_name_change value

--- src/js/identifier/name/change.mjs
+++ src/js/identifier/name/change.mjs
@@ -1,11 +1,14 @@
-import { object_property_get } from '../../../object/property/get.mjs';
 import { js_node_is_identifier } from '../../node/is/identifier.mjs';
 import { assert } from '../../../assert.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function js_identifier_name_change(identifier) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+import { object_property_change } from '../../../object/property/change.mjs';
+export function js_identifier_name_change(identifier, value) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     assert(js_node_is_identifier(identifier));
-    let name = object_property_get(identifier, 'name');
+    let name = object_property_change(identifier, 'name');
     return name;
 }
