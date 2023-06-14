8587341095d4a6a9c1a1bc6fe73f46a0b48feb19
JaredMathis <jared.mathis@native.consulting>
2023-06-14 10:49:26 -0400

    #92 function_auto function_add_inputs

--- src/function/add/inputs.mjs
+++ src/function/add/inputs.mjs
@@ -1,10 +1,12 @@
+import { function_add } from '../add.mjs';
 import { string_identifier_multiple_parse } from '../../string/identifier/multiple/parse.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_add_inputs(function_name, inputs_string) {
+export async function function_add_inputs(function_name, inputs_string) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let function_names = string_identifier_multiple_parse(function_names_string);
+    let inputs = string_identifier_multiple_parse(inputs_string);
+    function_add();
 }
