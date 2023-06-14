28822a3585cb0f15177be5f21699a59e77ca7882
JaredMathis <jared.mathis@native.consulting>
2023-06-14 10:01:09 -0400

    #92 function_auto temp_b

---
+++ src/temp/b.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function temp_b() {
+    arguments_assert(arguments, []);
+    error('todo: temp_b');
+}
