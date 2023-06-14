359d401cdbc5ef5b91d1cc0133e5f8daa400d5c0
JaredMathis <jared.mathis@native.consulting>
2023-06-14 10:41:06 -0400

    #92 function_auto_multiple function_add_with_statements_synchronized,function_auto

--- src/function/add/with/statements/synchronized.mjs
+++ src/function/add/with/statements/synchronized.mjs
@@ -1,3 +1,4 @@
+import { function_auto_after } from '../../../auto/after.mjs';
 import { js_export_function_single } from '../../../../js/export/function/single.mjs';
 import { boolean_is } from '../../../../boolean/is.mjs';
 import { list_is } from '../../../../list/is.mjs';
@@ -22,6 +23,7 @@ export async function function_add_with_statements_synchronized(function_name, s
     if (is_async) {
         js_function_declaration_async_add(fd);
     }
+    await function_auto_after(function_name);
     return await function_write(function_name, parsed);
     metadata([]);
 }
