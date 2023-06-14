92b6a281e850c1addf5b4fe1a1233c1c23706cd8
JaredMathis <jared.mathis@native.consulting>
2023-06-14 19:52:39 -0400

    #93 noop

--- src/string/base64/from.mjs
+++ src/string/base64/from.mjs
@@ -2,5 +2,5 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function string_base64_from(input) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    return atob(input);
+    return btoa(input);
 }
