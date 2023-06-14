367bfded502dcd625b66f42b4d8467fb0d85bce6
JaredMathis <jared.mathis@native.consulting>
2023-06-14 10:59:33 -0400

    #92 function_delete refactor_asyncify

--- src/refactor/asyncify.mjs
+++
@@ -1,7 +0,0 @@
-import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
-import { error } from '../error.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
-export function refactor_asyncify(function_name) {
-    arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: refactor_asyncify');
-}
