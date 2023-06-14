ffd2bc77832f2dc4e8e4d203eb278ccde687ddd3
JaredMathis <jared.mathis@native.consulting>
2023-06-14 11:24:01 -0400

    #92 function_rename_if_starts_with js_call_expression_to_name_ js_call_expression_name_

--- src/js/call/expression/name/or/null.mjs
+++ src/js/call/expression/name/or/null.mjs
@@ -1,10 +1,10 @@
-import { js_call_expression_name_generic } from '../../../../call/expression/name/generic.mjs';
 import { metadata } from '../../../../../metadata.mjs';
-import { arguments_assert } from '../../../../../arguments/assert.mjs';
+import { js_call_expression_name_generic } from '../generic.mjs';
 import { js_node_is_call_expression } from '../../../../node/is/call/expression.mjs';
-export function js_find_call_expressions_to_name(ce) {
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export function js_call_expression_name_or_null(ce) {
     arguments_assert(arguments, [js_node_is_call_expression]);
-    let or_null = false;
+    let or_null = true;
     let name = js_call_expression_name_generic(ce, or_null);
     return name;
     metadata([]);
