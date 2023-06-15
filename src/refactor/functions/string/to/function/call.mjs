302cac6203e442e9a8f8c81cfe133756375b4732
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:01:00 -0400

    #101 function_auto refactor_functions_string_to_function_call

--- src/refactor/functions/string/to/function/call.mjs
+++ src/refactor/functions/string/to/function/call.mjs
@@ -1,4 +1,4 @@
-import { log } from '../../../../../log.mjs';
+import { changed } from '../../../../../changed.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { object_replace } from '../../../../../object/replace.mjs';
@@ -15,13 +15,18 @@ export async function refactor_functions_string_to_function_call(function_name, 
         arguments_assert_todo
     ]);
     await file_js_all_map_args_if_function(async function logic(args) {
-        let {parsed} = args;
-        js_nodes_each(parsed, js_node_is_literal, n => {
-            let value = object_property_get(n, js_node_property_value());
-            if (equal(value, string_value)) {
-                let replacement = js_parse_call_expression(function_name);
-                object_replace(n, replacement);
-            }
+        let c = changed(change => {
+            let {parsed} = args;
+            js_nodes_each(parsed, js_node_is_literal, n => {
+                let value = object_property_get(n, js_node_property_value());
+                if (equal(value, string_value)) {
+                    let replacement = js_parse_call_expression(function_name);
+                    object_replace(n, replacement);
+                    change();
+                }
+            });
         });
+        if (c) {
+        }
     });
 }
