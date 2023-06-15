cd38431cd388b5f8bb198c94f0cc8c800f8b2ac8
JaredMathis <jared.mathis@native.consulting>
2023-06-15 14:01:37 -0400

    #93 noop

--- src/refactor/unlambdaify.mjs
+++ src/refactor/unlambdaify.mjs
@@ -1,3 +1,4 @@
+import { js_identifier_name_next } from '../js/identifier/name/next.mjs';
 import { js_arrow_function_expression_functionify } from '../js/arrow/function/expression/functionify.mjs';
 import { js_node_is_arrow_function_expression } from '../js/node/is/arrow/function/expression.mjs';
 import { js_nodes_each } from '../js/nodes/each.mjs';
@@ -6,5 +7,5 @@ import { arguments_assert } from '../arguments/assert.mjs';
 export function refactor_unlambdaify(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {parsed} = args;
-    js_nodes_each(parsed, js_node_is_arrow_function_expression, js_arrow_function_expression_functionify);
+    js_nodes_each(parsed, js_node_is_arrow_function_expression, n => js_arrow_function_expression_functionify(n, js_identifier_name_next(parsed)));
 }
