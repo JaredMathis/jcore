9d0e0102b21f8af63d29a001e6acfcd6008fe949
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:19:50 -0400

    #93 function_auto git_hub_issues_pull

--- src/git/hub/issues/pull.mjs
+++ src/git/hub/issues/pull.mjs
@@ -1,8 +1,9 @@
-import { error } from '../../../error.mjs';
+import { list_length } from '../../../list/length.mjs';
+import { git_hub_repository_issues_all } from '../repository/issues/all.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { assert } from '../../../assert.mjs';
 export function git_hub_issues_pull() {
     arguments_assert(arguments, []);
-    let issues = git_hub_repository_issues_all()
+    let issues = git_hub_repository_issues_all();
     assert(list_length(issues) <= 100);
 }
