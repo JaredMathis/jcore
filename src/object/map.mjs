37b2e92c5256c941d929977ef7a8cb432b45fb8b
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:25:30 -0400

    #93 function_copy object_map_without object_map

---
+++ src/object/map.mjs
@@ -0,0 +1,12 @@
+import { list_contains } from '../list/contains.mjs';
+import { object_merge_filtered } from './merge/filtered.mjs';
+import { list_is } from '../list/is.mjs';
+import { defined_is } from '../defined/is.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function object_map(object, values_without) {
+    arguments_assert(arguments, [
+        defined_is,
+        list_is
+    ]);
+    return object_merge_filtered(object, key => !list_contains(values_without, key), {});
+}
