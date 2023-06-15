55c0c5c2be93343b89b112aea6151e34d39429ed
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:43:23 -0400

    #93 function_auto git_hub_repository_issues_generic

--- src/git/hub/repository/issues/generic.mjs
+++ src/git/hub/repository/issues/generic.mjs
@@ -1,19 +1,15 @@
-import { git_hub_cache_file_path } from '../../cache/file/path.mjs';
+import { git_hub_cached } from '../../cached.mjs';
 import { git_hub_page_size } from '../../page/size.mjs';
-import { file_json_write } from '../../../../file/json/write.mjs';
 import { object_merge } from '../../../../object/merge.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
 import { file_json_read } from '../../../../file/json/read.mjs';
-import { file_exists } from '../../../../file/exists.mjs';
 import { git_hub_repository_issues_open } from './open.mjs';
-import { git_ignore_cache } from '../../../ignore/cache.mjs';
-import { git_ignore_add_if_not_exists } from '../../../ignore/add/if/not/exists.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { Octokit } from 'octokit';
 import { defined_is } from '../../../../defined/is.mjs';
 export async function git_hub_repository_issues_generic(api_args_to_merge) {
     arguments_assert(arguments, [defined_is]);
-    return await git_hub_cached(git_hub_repository_issues_open, arguments, async function lambda(api_args_to_merge) {
+    return await git_hub_cached(git_hub_repository_issues_open, arguments, async function lambda() {
         let p = await file_json_read('../private.json');
         let token = object_property_get(p, 'git_hub_api_token');
         const octokit = new Octokit({ auth: token });
