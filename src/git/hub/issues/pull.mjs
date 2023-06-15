2151ffacc4c845c3ec65e302abab7a81007eb80f
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:56:02 -0400

    #93 noop

--- src/git/hub/issues/pull.mjs
+++ src/git/hub/issues/pull.mjs
@@ -9,6 +9,10 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { assert } from '../../../assert.mjs';
 import { comment } from '../../../comment.mjs';
 import { list_map } from '../../../list/map.mjs';
+import { file_json_write } from '../../../file/json/write.mjs';
+import { path_join } from '../../../path/join.mjs';
+import { string_add } from '../../../string/add.mjs';
+import { file_extension_json } from '../../../file/extension/json.mjs';
 export async function git_hub_issues_pull(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let issues_response = await git_hub_repository_issues_all();
@@ -19,5 +23,7 @@ export async function git_hub_issues_pull(repository_name) {
         'number',
         'title'
     ]));
-    let repository_file_directory_path = version_path_tasks_get(repository_name);
+    let tasks_path = version_path_tasks_get(repository_name);
+    let tasks_all_path = path_join([tasks_path, string_add('all', file_extension_json())])
+    await file_json_write()
 }
