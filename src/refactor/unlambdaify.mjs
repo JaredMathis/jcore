577a53e75377f0a3ff5e5828d7cf67747215743f
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:21:30 -0400

    #93 function_add_mapper unlambdaify

---
+++ src/refactor/unlambdaify.mjs
@@ -0,0 +1,7 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function refactor_unlambdaify(args) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    error('todo: refactor_unlambdaify');
+}
