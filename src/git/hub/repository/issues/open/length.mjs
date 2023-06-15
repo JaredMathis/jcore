7d9bf8fe0d9f8e61c1691f8ed4eebcbd1e769a3b
JaredMathis <jared.mathis@native.consulting>
2023-06-14 20:02:27 -0400

    #93 function_rename_if_starts_with git_hub_repository_issues_ git_hub_repository_issues_open_

---
+++ src/git/hub/repository/issues/open/length.mjs
@@ -0,0 +1,11 @@
+import { object_property_get } from '../../../../../object/property/get.mjs';
+import { git_hub_repository_issues } from '../../issues.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+import { list_length } from '../../../../../list/length.mjs';
+export async function git_hub_repository_issues_open_length() {
+    arguments_assert(arguments, []);
+    let issues = await git_hub_repository_issues();
+    let v = issues;
+    let data = object_property_get(v, 'data');
+    return list_length(data);
+}
