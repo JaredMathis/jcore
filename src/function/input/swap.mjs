7415be22d2a0a60d5444fca2dcbddbcbd15c850c
JaredMathis <jared.mathis@native.consulting>
2023-06-15 14:57:34 -0400

    #93 function_input_add_multiple_prefix function_input_swap index from,to

--- src/function/input/swap.mjs
+++ src/function/input/swap.mjs
@@ -3,7 +3,11 @@ import { function_name_get } from '../name/get.mjs';
 import { function_map_with_args } from '../map/with/args.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function function_input_swap(function_name) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+export async function function_input_swap(function_name, index_from, index_to) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     await function_map_with_args(function_name_get(refactor_input_swap), function_name, {});
 }
