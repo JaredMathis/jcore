125635f7f3142d65946ebe74223c4855e4fab7bb
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:10:06 -0400

    #93 function_auto refactor_input_swap_last_to

--- src/refactor/input/swap/last/to.mjs
+++ src/refactor/input/swap/last/to.mjs
@@ -1,7 +1,11 @@
+import { refactor_input_swap } from '../../swap.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { range } from '../../../../range.mjs';
+import { list_length } from '../../../../list/length.mjs';
 export function refactor_input_swap_last_to(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: refactor_input_swap_last_to');
+    let {index} = args;
+    for (let i of range(0, list_length()));
+    refactor_input_swap();
 }
