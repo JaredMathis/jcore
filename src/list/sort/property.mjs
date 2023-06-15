2dd60721149e67eb5553a982b93eea04f3a4cd1a
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:34:27 -0400

    #105 function_wrap list_sort_property_generic list_sort_property

---
+++ src/list/sort/property.mjs
@@ -0,0 +1,12 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { list_sort_property_generic } from './property/generic.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function list_sort_property(removals, sort_property, reverse) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let result = list_sort_property_generic(removals, sort_property, reverse);
+    return result;
+}
