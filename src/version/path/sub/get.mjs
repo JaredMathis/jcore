11a65af9a5ff84bcd191d4501403b9ece1c2b3e4
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:47:01 -0400

    #93 function_auto version_path_sub_get

--- src/version/path/sub/get.mjs
+++ src/version/path/sub/get.mjs
@@ -2,7 +2,7 @@ import { version_path_repository } from '../repository.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { path_join } from '../../../path/join.mjs';
-export function version_path_sub_get(repository_name, repository_files_directory_name) {
+export function version_path_sub_get(repository_name, sub_name) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
@@ -10,7 +10,7 @@ export function version_path_sub_get(repository_name, repository_files_directory
     let repository_directory = version_path_repository(repository_name);
     let repository_sub_path = path_join([
         repository_directory,
-        repository_files_directory_name
+        sub_name
     ]);
     return repository_sub_path;
 }
