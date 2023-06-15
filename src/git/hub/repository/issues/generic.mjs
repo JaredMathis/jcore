17f7e0cab1a4503d2937146ad29af5da82ae6db4
JaredMathis <jared.mathis@native.consulting>
2023-06-15 01:03:59 -0400

    #93 function_auto git_hub_repository_issues_get

---
+++ src/git/hub/repository/issues/generic.mjs
@@ -0,0 +1,21 @@
+import { object_merge } from '../../../../object/merge.mjs';
+import { object_property_get } from '../../../../object/property/get.mjs';
+import { file_json_read } from '../../../../file/json/read.mjs';
+import { git_hub_repository_issues_open } from './open.mjs';
+import { git_hub_cached } from '../../cached.mjs';
+export async function git_hub_repository_issues_generic(api_args_to_merge, verb) {
+    return await git_hub_cached(git_hub_repository_issues_open, arguments, async function lambda() {
+        let p = await file_json_read('../private.json');
+        let token = object_property_get(p, 'git_hub_api_token');
+        const octokit = new Octokit({ auth: token });
+        let owner = 'JaredMathis';
+        let repository = 'jcore';
+        const api_args = {
+            owner: owner,
+            repo: repository
+        };
+        object_merge(api_args_to_merge, api_args);
+        let issues = await octokit.request(`${ verb } /repos/{owner}/{repo}/issues`, api_args);
+        return issues;
+    });
+}
--- src/git/hub/repository/issues/get.mjs
+++ src/git/hub/repository/issues/get.mjs
@@ -1,30 +1,12 @@
-import { git_hub_cached } from '../../cached.mjs';
+import { git_hub_repository_issues_generic } from './generic.mjs';
 import { git_hub_page_size } from '../../page/size.mjs';
 import { object_merge } from '../../../../object/merge.mjs';
-import { object_property_get } from '../../../../object/property/get.mjs';
-import { file_json_read } from '../../../../file/json/read.mjs';
-import { git_hub_repository_issues_open } from './open.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { Octokit } from 'octokit';
 import { defined_is } from '../../../../defined/is.mjs';
 export async function git_hub_repository_issues_get(api_args_to_merge) {
     arguments_assert(arguments, [defined_is]);
     let verb = 'GET';
-    object_merge({
-        per_page: git_hub_page_size()
-    }, api_args_to_merge);
-    return await git_hub_cached(git_hub_repository_issues_open, arguments, async function lambda() {
-        let p = await file_json_read('../private.json');
-        let token = object_property_get(p, 'git_hub_api_token');
-        const octokit = new Octokit({ auth: token });
-        let owner = 'JaredMathis';
-        let repository = 'jcore';
-        const api_args = {
-            owner: owner,
-            repo: repository,
-        };
-        object_merge(api_args_to_merge, api_args);
-        let issues = await octokit.request(`${ verb } /repos/{owner}/{repo}/issues`, api_args);
-        return issues;
-    });
+    object_merge({ per_page: git_hub_page_size() }, api_args_to_merge);
+    return await git_hub_repository_issues_generic(api_args_to_merge, verb);
 }
