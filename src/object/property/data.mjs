5f6cb34db71155136f401f4603f89bc8788f9a35
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:22:38 -0400

    #93 function_auto object_property_data_list_length

---
+++ src/object/property/data.mjs
@@ -0,0 +1,4 @@
+import { object_property_get } from './get.mjs';
+export function object_property_data(issues) {
+    return object_property_get(issues, 'data');
+}
--- src/object/property/data/list/length.mjs
+++ src/object/property/data/list/length.mjs
@@ -1,6 +1,9 @@
+import { object_property_data } from '../../data.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { list_length } from '../../../../list/length.mjs';
-import { object_property_get } from '../../get.mjs';
 export function object_property_data_list_length(issues) {
-    let data = object_property_get(issues, 'data');
+    arguments_assert(arguments, [arguments_assert_todo]);
+    let data = object_property_data(issues);
     return list_length(data);
 }
