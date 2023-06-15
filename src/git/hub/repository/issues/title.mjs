0d3abf22f605cfd533829044d84f916f64dff86b
JaredMathis <jared.mathis@native.consulting>
2023-06-15 07:24:15 -0400

    #93 function_input_first_remove git_hub_repository_issues_title

--- src/git/hub/repository/issues/title.mjs
+++ src/git/hub/repository/issues/title.mjs
@@ -1,15 +1,12 @@
 import { git_hub_repository_issues_patch } from './patch.mjs';
-import { object_merge } from '../../../../object/merge.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { defined_is } from '../../../../defined/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { task_property_title } from '../../../../task/property/title.mjs';
-export async function git_hub_repository_issues_title(api_args_to_merge, issue_number, title) {
+export async function git_hub_repository_issues_title(issue_number, title) {
     arguments_assert(arguments, [
-        defined_is,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let api_args_to_merge = {[task_property_title()]: title }
+    let api_args_to_merges = { [task_property_title()]: title };
     return git_hub_repository_issues_patch(issue_number, api_args_to_merge);
 }
