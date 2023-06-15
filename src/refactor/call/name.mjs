6c7b743d47bd6b32839bde7eb1ed7a34457479b9
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:49:55 -0400

    #93 function_input_add function_call_name name_new

--- src/refactor/call/name.mjs
+++ src/refactor/call/name.mjs
@@ -6,10 +6,11 @@ import { list_get } from '../../list/get.mjs';
 import { list_index_after } from '../../list/index/after.mjs';
 export async function refactor_call_name(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let {name_new} = args;
     let {function_declaration} = args;
     let {statements, statement_arguments_assert} = await js_function_declaration_to_statements_and_arguments_assert_statement(function_declaration);
     let index_after = list_index_after(statements, statement_arguments_assert);
     let statement_after = list_get(index_after);
     let expression = js_statement_expression_to_expression(statement_after);
-    js_call_expression_name_change
+    js_call_expression_name_change(expression, name_new);
 }
