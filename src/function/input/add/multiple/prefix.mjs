b94f530217ff33223c6f978bf943d036b1cdb05f
JaredMathis <jared.mathis@native.consulting>
2023-06-15 09:58:24 -0400

    #93 function_auto function_input_add_multiple_prefix

--- src/function/input/add/multiple/prefix.mjs
+++ src/function/input/add/multiple/prefix.mjs
@@ -1,8 +1,9 @@
+import { string_identifier_parts_from } from '../../../../string/identifier/parts/from.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { function_input_add_multiple } from '../multiple.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { function_input_add_multiple_generic } from './generic.mjs';
 import { list_map } from '../../../../list/map.mjs';
+import { string_identifier_multiple_parse } from '../../../../string/identifier/multiple/parse.mjs';
 export async function function_input_add_multiple_prefix(function_name, prefix, inputs_string) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -10,6 +11,7 @@ export async function function_input_add_multiple_prefix(function_name, prefix, 
         arguments_assert_todo
     ]);
     let inputs = string_identifier_multiple_parse(inputs_string);
+    let mapped = list_map(inputs, string_identifier_parts_from);
     let result = await function_input_add_multiple_generic(function_name, inputs);
     return result;
 }
