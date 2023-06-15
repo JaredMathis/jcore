a0627817c2bbe3023768e937dd6704c034fd09b6
JaredMathis <jared.mathis@native.consulting>
2023-06-15 07:39:38 -0400

    #93 function_search string,mult

--- src/function/wrap/suffix.mjs
+++ src/function/wrap/suffix.mjs
@@ -1,7 +1,7 @@
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { function_wrap } from '../wrap.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function function_wrap_suffix(function_name_to_wrap, function_name_to_add) {
+export async function function_wrap_suffix(function_name_to_wrap, suffix) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is
