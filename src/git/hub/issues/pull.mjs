6dddd3c77a03a3e24b624973da84f0d26d2d0c4f
JaredMathis <jared.mathis@native.consulting>
2023-06-14 20:17:30 -0400

    #93 function_auto git_hub_issues_pull

---
+++ src/git/hub/issues/pull.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function git_hub_issues_pull() {
+    arguments_assert(arguments, []);
+    error('todo: git_hub_issues_pull');
+}
