9fdf68cab07e91d13d8d76207e42f03e33757247
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:13:11 -0400

    #93 function_auto range_from

---
+++ src/range/from.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function range_from() {
+    arguments_assert(arguments, []);
+    error('todo: range_from');
+}
