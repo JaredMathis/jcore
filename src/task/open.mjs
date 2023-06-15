b1dafcb48ead95e1998da444f07d55a265ab461d
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:24:35 -0400

    #104 function_auto task_open

--- src/task/open.mjs
+++ src/task/open.mjs
@@ -1,6 +1,6 @@
 import { task_all } from './all.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function task_open() {
+export async function task_open() {
     arguments_assert(arguments, []);
-    let all = task_all();
+    let all = await task_all();
 }
