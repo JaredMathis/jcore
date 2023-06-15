4ddea17145dfd1ef7f861071920d578eb4f5d6fe
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:21:27 -0400

    #93 function_auto git_hub_issues_pull

--- src/git/hub/issues/pull.mjs
+++ src/git/hub/issues/pull.mjs
@@ -1,3 +1,4 @@
+import { list_first } from '../../../list/first.mjs';
 import { git_hub_page_size } from '../page/size.mjs';
 import { list_length } from '../../../list/length.mjs';
 import { git_hub_repository_issues_all } from '../repository/issues/all.mjs';
@@ -9,4 +10,5 @@ export function git_hub_issues_pull() {
     let issues = git_hub_repository_issues_all();
     comment(`If there's more than 1 page of issues code needs adjusting`);
     assert(list_length(issues) <= git_hub_page_size());
+    let issue_first = list_first(issues);
 }
