3927dcf17547e1f53f1a2e7c667bb623d9e18a16
JaredMathis <jared.mathis@native.consulting>
2023-06-15 13:16:11 -0400

    #93 function_auto refactor_unlambdaify

--- src/refactor/unlambdaify.mjs
+++ src/refactor/unlambdaify.mjs
@@ -1,4 +1,4 @@
-import { log } from '../log.mjs';
+import { js_arrow_function_expression_functionify } from '../js/arrow/function/expression/functionify.mjs';
 import { js_node_is_arrow_function_expression } from '../js/node/is/arrow/function/expression.mjs';
 import { js_nodes_each } from '../js/nodes/each.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
