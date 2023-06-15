9b692241676b5a591e6569069974c485dac31c24
JaredMathis <jared.mathis@native.consulting>
2023-06-15 13:57:46 -0400

    #93 noop

--- src/js/arrow/function/expression/functionify.mjs
+++ src/js/arrow/function/expression/functionify.mjs
@@ -6,10 +6,11 @@ import { js_node_is_arrow_function_expression } from '../../../node/is/arrow/fun
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { js_parse } from '../../../parse.mjs';
 import { string_a } from '../../../../string/a.mjs';
+import { js_parse_expression } from '../../../parse/expression.mjs';
 export function js_arrow_function_expression_functionify(node) {
     arguments_assert(arguments, [js_node_is_arrow_function_expression]);
-    let function_code = `${ js_keyword_function() } ${string_a()}${ js_code_parenthesis_surround(``) }${ js_brace_left_right() }`;
-    let function_expression = js_parse(function_code);
+    let function_code = `${ js_keyword_function() } ${ js_code_parenthesis_surround(``) }${ js_brace_left_right() }`;
+    let function_expression = js_parse_expression(function_code);
     console.log({
         node,
         function_expression
