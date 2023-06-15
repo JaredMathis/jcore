5cb97ba9ef6e32251923116bacccbe15bfde0b26
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:17:02 -0400

    #102 function_auto task_finish

--- src/task/finish.mjs
+++ src/task/finish.mjs
@@ -6,9 +6,13 @@ import { metadata } from '../metadata.mjs';
 import { task_id_remove } from './id/remove.mjs';
 import { task_id_get_or_null } from './id/get/or/null.mjs';
 import { task_close } from './close.mjs';
+import { equal } from '../equal.mjs';
 export async function task_finish() {
     arguments_assert(arguments, []);
     let task_number_hashed = await task_id_get_or_null();
+    if (equal(task_number_hashed, null)) {
+        return 'no current task';
+    }
     let task_number_string = string_prefix_without(task_number_hashed, task_symbol_hash());
     let task_number = integer_parse(task_number_string);
     await task_close(task_number);
