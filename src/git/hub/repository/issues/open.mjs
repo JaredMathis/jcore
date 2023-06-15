c4f7e1f6f8d40d6c7f9647fae7308bb471ce43b2
JaredMathis <jared.mathis@native.consulting>
2023-06-14 20:05:47 -0400

    #93 noop

--- src/git/hub/repository/issues/open.mjs
+++ src/git/hub/repository/issues/open.mjs
@@ -13,32 +13,10 @@ import { path_join } from '../../../../path/join.mjs';
 import { file_extension_json } from '../../../../file/extension/json.mjs';
 export async function git_hub_repository_issues_open() {
     arguments_assert(arguments, []);
-    await git_ignore_add_if_not_exists(git_ignore_cache());
-    let key = function_name_get(git_hub_repository_issues_open);
-    let file_name = string_base64_to(key);
-    let file_path = path_join([
-        git_ignore_cache(),
-        string_add(file_name, file_extension_json())
-    ]);
-    if (await file_exists(file_path)) {
-        let result = await file_json_read(file_path);
-        return result;
-    }
-    let p = await file_json_read('../private.json');
-    let token = object_property_get(p, 'git_hub_api_token');
-    const octokit = new Octokit({ auth: token });
-    let owner = 'JaredMathis';
-    let repository = 'jcore';
-    const args = {
-        owner: owner,
-        repo: repository
-    };
-    let issues = await octokit.request('GET /repos/{owner}/{repo}/issues', args);
-    await file_json_write(file_path, issues);
-    return issues;
+    return await git_hub_repository_issues_generic({});
 }
 
-async function git_hub_repository_issues_generic() {
+async function git_hub_repository_issues_generic(args_to_merge) {
     arguments_assert(arguments, []);
     await git_ignore_add_if_not_exists(git_ignore_cache());
     let key = function_name_get(git_hub_repository_issues_open);
@@ -60,6 +38,7 @@ async function git_hub_repository_issues_generic() {
         owner: owner,
         repo: repository
     };
+    object_merge(args_to_merge, args);
     let issues = await octokit.request('GET /repos/{owner}/{repo}/issues', args);
     await file_json_write(file_path, issues);
     return issues;
