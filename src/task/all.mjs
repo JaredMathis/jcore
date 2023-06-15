7f0b61c670396272ad46f81c69a98c092686a7f5
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:23:29 -0400

    #94 function_auto task_all

--- src/task/all.mjs
+++ src/task/all.mjs
@@ -3,6 +3,6 @@ import { arguments_assert } from '../arguments/assert.mjs';
 export async function task_all() {
     arguments_assert(arguments, []);
     let result;
-    await task_map(tasks_all => result = task_all);
+    await task_map(tasks_all => result = tasks_all);
     return result;
 }
