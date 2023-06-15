700f8a01635acfc3e8975fd7707999488e01ddc5
JaredMathis <jared.mathis@native.consulting>
2023-06-15 06:41:38 -0400

    #93 function_auto task_add

---
+++ src/file/json/map.mjs
@@ -0,0 +1,11 @@
+import { file_json_read } from './read.mjs';
+import { file_json_overwrite } from './overwrite.mjs';
+import { file_exists } from '../exists.mjs';
+export async function file_json_map(tasks_all_path, initial_value, map) {
+    if (!await file_exists(tasks_all_path)) {
+        await file_json_overwrite(tasks_all_path, initial_value);
+    }
+    let tasks_all = await file_json_read(tasks_all_path);
+    map(tasks_all);
+    await file_json_overwrite(tasks_all_path, tasks_all);
+}
--- src/task/add.mjs
+++ src/task/add.mjs
@@ -1,3 +1,4 @@
+import { file_json_map } from '../file/json/map.mjs';
 import { task_id_set_hash } from './id/set/hash.mjs';
 import { task_from_git_hub_issue } from './from/git/hub/issue.mjs';
 import { object_property_data } from '../object/property/data.mjs';
@@ -5,9 +6,6 @@ import { git_hub_repository_issues_post } from '../git/hub/repository/issues/pos
 import { version_path_tasks_all_get } from '../version/path/tasks/all/get.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-import { file_json_read } from '../file/json/read.mjs';
-import { file_json_overwrite } from '../file/json/overwrite.mjs';
-import { file_exists } from '../file/exists.mjs';
 import { list_add } from '../list/add.mjs';
 import { object_property_get } from '../object/property/get.mjs';
 import { task_property_number } from './property/number.mjs';
@@ -18,15 +16,9 @@ export async function task_add(title) {
     let task = task_from_git_hub_issue(data);
     let tasks_all_path = version_path_tasks_all_get(repository_name);
     const initial_value = [];
-    if (!await file_exists(tasks_all_path)) {
-        await file_json_overwrite(tasks_all_path, initial_value);
-    }
-    let tasks_all = await file_json_read(tasks_all_path);
-    map(tasks_all);
-    await file_json_overwrite(tasks_all_path, tasks_all);
+    await file_json_map(tasks_all_path, initial_value, map);
     let task_number = object_property_get(task, task_property_number());
     await task_id_set_hash(task_number);
-
     function map(tasks_all) {
         list_add(tasks_all, task);
     }
