890506d731816c309d1952ce97e1d1beed0d3677
JaredMathis <jared.mathis@native.consulting>
2023-06-15 17:22:41 -0400

    #93 function_auto git_hub_repository_issues_patch

--- src/git/hub/repository/issues/patch.mjs
+++ src/git/hub/repository/issues/patch.mjs
@@ -1,7 +1,13 @@
-import { git_hub_repository_issues_generic } from './generic.mjs';
+import { git_hub_repository_issue_generic } from '../issue/generic.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { object_merge } from '../../../../object/merge.mjs';
 export async function git_hub_repository_issues_patch(issue_number, api_args_to_merge) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     object_merge({ issue_number }, api_args_to_merge);
     let verb = 'PATCH';
-    return await git_hub_repository_issues_generic(api_args_to_merge, verb);
+    return await git_hub_repository_issue_generic(api_args_to_merge, verb);
 }
