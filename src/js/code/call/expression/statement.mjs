6e368b5919f1f3935a73ce1b7990bc3e25d96250
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:32:11 -0400

    #93 function_search async

--- src/js/code/call/expression/statement.mjs
+++ src/js/code/call/expression/statement.mjs
@@ -3,8 +3,8 @@ import { metadata } from '../../../../metadata.mjs';
 import { js_code_call_expression } from '../expression.mjs';
 import { string_is } from '../../../../string/is.mjs';
 import { js_code_statement } from '../../statement.mjs';
-export function js_code_call_expression_statement(callee) {
+export function js_code_call_expression_statement(name) {
     arguments_assert(arguments, [string_is]);
-    return js_code_statement(js_code_call_expression(callee));
+    return js_code_statement(js_code_call_expression(name));
     metadata([]);
 }
--- src/refactor/call/add.mjs
+++ src/refactor/call/add.mjs
@@ -2,10 +2,13 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { js_parse_statement } from '../../js/parse/statement.mjs';
-export function refactor_call_add(args) {
+import { function_name_get } from '../../function/name/get.mjs';
+import { error_is } from '../../error/is.mjs';
+export async function refactor_call_add(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {function_declaration} = args;
-    let statement = js_parse_statement(identifier, after_let_identifier);
+    let statement_code = js_code_call_expression_statement(function_name_get(error))
+    let statement = js_parse_statement(statement_code);
     await js_function_declaration_statement_add_after_arguments_assert(function_declaration, statement);
     await refactor_import_fix(args);
     error('todo: refactor_call_add');
