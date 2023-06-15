a8058238e58f74963cdfc12a2f30042074672c5b
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:56:32 -0400

    #93 function_auto refactor_input_swap_first_to

--- src/refactor/input/swap/first/to.mjs
+++ src/refactor/input/swap/first/to.mjs
@@ -1,7 +1,19 @@
+import { range_from } from '../../../../range/from.mjs';
+import { list_last_index } from '../../../../list/last/index.mjs';
+import { add_1 } from '../../../../add/1.mjs';
+import { refactor_input_swap_generic_to } from '../generic/to.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function refactor_input_swap_first_to(args) {
+export async function refactor_input_swap_first_to(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: refactor_input_swap_first_to');
+    await refactor_input_swap_generic_to(args, predicate, indices_get);
+    function indices_get(index, params) {
+        let index_next = add_1(index);
+        let index_last = list_last_index(params);
+        return range_from(index_next, index_last);
+    }
+    function predicate(index, params) {
+        let index_last = list_last_index(params);
+        return index < index_last;
+    }
 }
