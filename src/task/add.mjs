a68dbbd4a288452aec301887e3421e29bc02917b
JaredMathis <jared.mathis@native.consulting>
2023-06-15 06:52:32 -0400

    #93 function_auto task_add

--- src/task/add.mjs
+++ src/task/add.mjs
@@ -11,10 +11,9 @@ export async function task_add(title) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let data = await git_hub_repository_issues_post(title);
     let task = task_from_git_hub_issue(data);
-    await task_map(map);
+    await task_map(function map(tasks_all) {
+        list_add(tasks_all, task);
+    });
     let task_number = object_property_get(task, task_property_number());
     await task_id_set_hash(task_number);
-    function map(tasks_all) {
-        list_add(tasks_all, task);
-    }
 }
