463adef414af965a195c39253362cbdafc676f9d
JaredMathis <jared.mathis@native.consulting>
2023-06-14 20:10:59 -0400

    #93 function_auto git_hub_repository_issues_all

--- src/git/hub/repository/issues/all.mjs
+++ src/git/hub/repository/issues/all.mjs
@@ -2,5 +2,5 @@ import { git_hub_repository_issues_generic } from './generic.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function git_hub_repository_issues_all() {
     arguments_assert(arguments, []);
-    return await git_hub_repository_issues_generic({ state: 'closed' });
+    return await git_hub_repository_issues_generic({ state: 'all' });
 }
