4a088be5512851087094887503c6ec77e7aa588b
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:00:17 -0400

    #101 function_auto changed

--- src/changed.mjs
+++ src/changed.mjs
@@ -1,7 +1,10 @@
 import { arguments_assert_todo } from './arguments/assert/todo.mjs';
-import { error } from './error.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 export function changed(lambda) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: changed');
+    let result = false;
+    lambda(() => {
+        result = true;
+    });
+    return result;
 }
