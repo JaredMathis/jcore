185fec3b6396812bb654e692b39a84386dbdf94a
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:01:07 -0400

    #93 function_auto function_add_mapper

--- src/function/add/mapper.mjs
+++ src/function/add/mapper.mjs
@@ -1,14 +1,13 @@
+import { js_expression_awaitify } from '../../js/expression/awaitify.mjs';
 import { function_add_inputs } from './inputs.mjs';
 import { function_input_add_type } from '../input/add/type.mjs';
 import { function_map } from '../map.mjs';
 import { function_name_get } from '../name/get.mjs';
 import { js_code_call_expression_with_args } from '../../js/code/call/expression/with/args.mjs';
-import { js_call_expression_awaitify } from '../../js/call/expression/awaitify.mjs';
 import { js_code_statement } from '../../js/code/statement.mjs';
 import { function_add_with_statement } from './with/statement.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { error } from '../../error.mjs';
 import { comment } from '../../comment.mjs';
 import { string_identifier_parts_from } from '../../string/identifier/parts/from.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
@@ -28,7 +27,7 @@ export async function function_add_mapper(function_name_suffix) {
         js_code_call_expression_with_args(function_name_get(function_name_get), [function_name_refactor]),
         input
     ]);
-    await function_add_with_statement(function_name, js_code_statement(js_call_expression_awaitify(expression)));
+    await function_add_with_statement(function_name, js_code_statement(js_expression_awaitify(expression)));
     await function_input_add_type(function_name, input, function_name_get(string_identifier_is));
     await function_add_inputs(function_name_refactor, 'args');
 }
