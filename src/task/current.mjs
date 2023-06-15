03acf3209caa460b57d38d7b2f50c2bd92200d2c
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:47:36 -0400

    #108 noop

--- src/task/current.mjs
+++ src/task/current.mjs
@@ -6,6 +6,7 @@ import { task_all } from './all.mjs';
 import { list_filter_property } from '../list/filter/property.mjs';
 import { task_property_number } from './property/number.mjs';
 import { integer_parse } from '../integer/parse.mjs';
+import { list_single } from '../list/single.mjs';
 export async function task_current() {
     arguments_assert(arguments, []);
     let task_number_hashed = await task_id_get_or_null();
@@ -16,5 +17,7 @@ export async function task_current() {
     let task_number = integer_parse(task_number_string);
     let all = await task_all();
     let filtered = list_filter_property(all, task_property_number(), task_number);
+    let result = list_single(filtered);
+    return result;
     metadata([]);
 }
