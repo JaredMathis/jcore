2356c90d4a32be881a0ff0c3a7ed06ad309ef4bb
JaredMathis <jared.mathis@native.consulting>
2023-06-15 18:21:34 -0400

    #94 noop

--- src/git/hub/repository/issues/post.mjs
+++ src/git/hub/repository/issues/post.mjs
@@ -3,11 +3,11 @@ import { git_hub_repository_issues_generic } from './generic.mjs';
 import { object_merge } from '../../../../object/merge.mjs';
 import { defined_is } from '../../../../defined/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function git_hub_repository_issues_post(api_args_to_merge, title) {
+export async function git_hub_repository_issues_post(title) {
     arguments_assert(arguments, [
-        defined_is,
         arguments_assert_todo
     ]);
+    let api_args_to_merge = {};
     let verb = 'POST';
     object_merge({ title }, api_args_to_merge);
     return await git_hub_repository_issues_generic(api_args_to_merge, verb);
