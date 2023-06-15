a2c35756261c2d8923635b8ce4a7bbf4a1307fd5
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:58:55 -0400

    #93 function_auto refactor_input_swap_first_to

--- src/refactor/input/swap/first/to.mjs
+++ src/refactor/input/swap/first/to.mjs
@@ -1,6 +1,5 @@
+import { list_reverse } from '../../../../list/reverse.mjs';
 import { range_from } from '../../../../range/from.mjs';
-import { list_last_index } from '../../../../list/last/index.mjs';
-import { add_1 } from '../../../../add/1.mjs';
 import { refactor_input_swap_generic_to } from '../generic/to.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
@@ -8,9 +7,9 @@ export async function refactor_input_swap_first_to(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     await refactor_input_swap_generic_to(args, predicate, indices_get);
     function indices_get(index, params) {
-        let index_next = add_1(index);
-        let index_last = list_last_index(params);
-        let ranged = range_from(index_next, index_last);
+        let ranged = range_from(1, index);
+        let reversed = list_reverse(ranged);
+        return reversed;
     }
     function predicate(index, params) {
         return index >= 1;
