9c3f42b38fb5b578bbdef1bb8dfc4575415cdd8b
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:13:30 -0400

    #93 function_input_add_multiple range_from from,to

--- src/range/from.mjs
+++ src/range/from.mjs
@@ -1,6 +1,10 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function range_from() {
-    arguments_assert(arguments, []);
+export function range_from(from, to) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     error('todo: range_from');
 }
