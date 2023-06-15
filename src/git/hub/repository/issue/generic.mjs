507064efc046e5e6c34195cc5271b0073a0711a2
JaredMathis <jared.mathis@native.consulting>
2023-06-15 17:22:00 -0400

    #93 function_copy git_hub_repository_issues_generic git_hub_repository_issue_generic

---
+++ src/git/hub/repository/issue/generic.mjs
@@ -0,0 +1,11 @@
+import { git_hub_api } from '../../api.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export async function git_hub_repository_issue_generic(fn, args, api_args_to_merge, verb) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let api_path = `/repos/{owner}/{repo}/issues`;
+    return await git_hub_api(fn, args, verb, api_path, api_args_to_merge);
+}
