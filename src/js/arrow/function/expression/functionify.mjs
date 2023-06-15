406002b5c93820cc67de8608475c7d3ea247db19
JaredMathis <jared.mathis@native.consulting>
2023-06-15 14:08:30 -0400

    #93 function_auto js_arrow_function_expression_functionify

--- src/js/arrow/function/expression/functionify.mjs
+++ src/js/arrow/function/expression/functionify.mjs
@@ -1,3 +1,4 @@
+import { js_code_return_statement } from '../../../code/return/statement.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { log } from '../../../../log.mjs';
 import { js_brace_left_right } from '../../../brace/left/right.mjs';
@@ -5,7 +6,6 @@ import { js_code_parenthesis_surround } from '../../../code/parenthesis/surround
 import { js_keyword_function } from '../../../keyword/function.mjs';
 import { js_node_is_arrow_function_expression } from '../../../node/is/arrow/function/expression.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { string_a } from '../../../../string/a.mjs';
 import { js_parse_expression } from '../../../parse/expression.mjs';
 export function js_arrow_function_expression_functionify(node, name) {
     arguments_assert(arguments, [
@@ -14,6 +14,7 @@ export function js_arrow_function_expression_functionify(node, name) {
     ]);
     let function_code = `${ js_keyword_function() } ${ name }${ js_code_parenthesis_surround(``) }${ js_brace_left_right() }`;
     let function_expression = js_parse_expression(function_code);
+    js_code_return_statement;
     console.log({
         node,
         function_expression
