eed0d023c19e8e5a250084d45ecea8222ca69738
JaredMathis <jared.mathis@native.consulting>
2023-06-15 14:21:56 -0400

    #93 function_auto js_function_declaration_to_statements

--- src/js/function/declaration/to/statements.mjs
+++ src/js/function/declaration/to/statements.mjs
@@ -5,6 +5,8 @@ import { object_property_get } from '../../../../object/property/get.mjs';
 import { js_node_is_function_declaration } from '../../../node/is/function/declaration.mjs';
 export function js_function_declaration_to_statements(fd) {
     arguments_assert(arguments, [js_node_is_function_declaration]);
+    if (false)
+        js_function_declaration_to_statements;
     let function_body = object_property_get(fd, 'body');
     let function_body_statements = js_block_statement_body(function_body);
     return function_body_statements;
