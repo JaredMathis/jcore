b5f216669ade7e29c56837e4092fcf173ab19a41
JaredMathis <jared.mathis@native.consulting>
2023-06-15 06:51:13 -0400

    #93 function_auto task_add

--- src/task/add.mjs
+++ src/task/add.mjs
@@ -1,7 +1,6 @@
 import { task_map } from './map.mjs';
 import { task_id_set_hash } from './id/set/hash.mjs';
 import { task_from_git_hub_issue } from './from/git/hub/issue.mjs';
-import { object_property_data } from '../object/property/data.mjs';
 import { git_hub_repository_issues_post } from '../git/hub/repository/issues/post.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
@@ -10,8 +9,7 @@ import { object_property_get } from '../object/property/get.mjs';
 import { task_property_number } from './property/number.mjs';
 export async function task_add(title) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let response = await git_hub_repository_issues_post(title);
-    let data = object_property_data(response);
+    let data = await git_hub_repository_issues_post(title);
     let task = task_from_git_hub_issue(data);
     await task_map(map);
     let task_number = object_property_get(task, task_property_number());
