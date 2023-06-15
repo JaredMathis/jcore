aefd09fe191bb42fc2e4aa4731aa79dd6e0a8bd5
JaredMathis <jared.mathis@native.consulting>
2023-06-14 20:13:42 -0400

    #93 function_auto git_hub_repository_issues_all_length

--- src/git/hub/repository/issues/all/length.mjs
+++ src/git/hub/repository/issues/all/length.mjs
@@ -1,11 +1,8 @@
-import { list_length } from '../../../../../list/length.mjs';
-import { object_property_get } from '../../../../../object/property/get.mjs';
+import { object_property_data_list_length } from '../../../../../object/property/data/list/length.mjs';
 import { git_hub_repository_issues_open } from '../open.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export async function git_hub_repository_issues_all_length() {
     arguments_assert(arguments, []);
     let issues = await git_hub_repository_issues_open();
-    let v = issues;
-    let data = object_property_get(v, 'data');
-    return list_length(data);
+    return object_property_data_list_length(issues);
 }
