76b0972c4b7695036f05250837c5fb7449ecbd09
JaredMathis <jared.mathis@native.consulting>
2023-06-15 01:11:41 -0400

    #93 function_auto task_from_git_hub_issue

--- src/task/from/git/hub/issue.mjs
+++ src/task/from/git/hub/issue.mjs
@@ -1,8 +1,12 @@
+import { task_property_number } from '../../../property/number.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { task_property_title } from '../../../property/title.mjs';
 import { object_map } from '../../../../object/map.mjs';
 export function task_from_git_hub_issue(issue) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     return object_map(issue, [
-        'number',
+        task_property_number(),
         task_property_title()
     ]);
 }
