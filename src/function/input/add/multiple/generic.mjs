3395ff97d5d066d2c005cfed5513a088c0ab5a30
JaredMathis <jared.mathis@native.consulting>
2023-06-15 14:53:39 -0400

    #93 function_auto function_input_add_multiple_generic

--- src/function/input/add/multiple/generic.mjs
+++ src/function/input/add/multiple/generic.mjs
@@ -1,12 +1,16 @@
+import { list_map } from '../../../../list/map.mjs';
+import { string_identifier_multiple_parse } from '../../../../string/identifier/multiple/parse.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { function_input_add } from '../../add.mjs';
-export async function function_input_add_multiple_generic(function_name, inputs, mapper) {
+export async function function_input_add_multiple_generic(function_name, inputs_string, mapper) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
+    let inputs = string_identifier_multiple_parse(inputs_string);
+    let mapped = list_map(inputs, mapper);
     for (let i of inputs) {
         await function_input_add(function_name, i);
     }
