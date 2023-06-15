d79ce9d9a8bd541abf90924120e364dcd1a3195e
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:08:55 -0400

    #93 noop

--- src/function/input/swap/last/to.mjs
+++ src/function/input/swap/last/to.mjs
@@ -9,5 +9,5 @@ export async function function_input_swap_last_to(function_name, index) {
         string_identifier_is,
         arguments_assert_todo
     ]);
-    await function_map_with_args(function_name_get(refactor_input_swap_last_to), function_name, {});
+    await function_map_with_args(function_name_get(refactor_input_swap_last_to), function_name, {index});
 }
