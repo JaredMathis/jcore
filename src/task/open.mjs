359af6d805c80de208a67cd13a15bc4898d1b7ca
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:36:09 -0400

    #105 task_open

--- src/task/open.mjs
+++ src/task/open.mjs
@@ -12,7 +12,7 @@ export async function task_open() {
     arguments_assert(arguments, []);
     let all = await task_all();
     let open = list_filter_property(all, task_property_state(), task_property_open());
-    let sorted = list_sort_property(open, task_property_number());
-    let summaries = list_map(sorted, o => `${ object_property_get(o, task_property_number()) } ${ object_property_get(o, task_property_title()) }`);
+    list_sort_property(open, task_property_number());
+    let summaries = list_map(open, o => `${ object_property_get(o, task_property_number()) } ${ object_property_get(o, task_property_title()) }`);
     return summaries;
 }
