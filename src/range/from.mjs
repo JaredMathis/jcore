a35cc7b446159e927c3bab3fe6a83596ab705f7e
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:14:23 -0400

    #93 function_auto range_from

--- src/range/from.mjs
+++ src/range/from.mjs
@@ -1,10 +1,14 @@
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
-import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { range } from '../range.mjs';
+import { subtract } from '../subtract.mjs';
+import { assert } from '../assert.mjs';
 export function range_from(from, to) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    error('todo: range_from');
+    assert(from <= to);
+    let difference = subtract(from, to);
+    return range();
 }
