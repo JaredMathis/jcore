a3db130dab92fb88117d78d08ba7b666b2c62f96
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:32:13 -0400

    #104 noop

--- src/task/open.mjs
+++ src/task/open.mjs
@@ -3,8 +3,13 @@ import { list_filter_property } from '../list/filter/property.mjs';
 import { task_all } from './all.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { task_property_state } from './property/state.mjs';
+import { list_map } from '../list/map.mjs';
+import { object_property_get } from '../object/property/get.mjs';
+import { task_property_number } from './property/number.mjs';
 export async function task_open() {
     arguments_assert(arguments, []);
     let all = await task_all();
-    return list_filter_property(all, task_property_state(), task_property_open());
+    let open = list_filter_property(all, task_property_state(), task_property_open());
+    let summaries = list_map(open, o => `${object_property_get(o, task_property_number())} ${object_property_get(o, task_property_state())}`)
+    return summaries
 }
