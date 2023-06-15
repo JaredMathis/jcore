f75931ef17a6cbbdeeb30b729cb61686c286cf0f
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:57:01 -0400

    #93 function_add_mapper call_inputs

---
+++ src/function/call/inputs.mjs
@@ -0,0 +1,9 @@
+import { string_identifier_is } from '../../string/identifier/is.mjs';
+import { refactor_call_inputs } from '../../refactor/call/inputs.mjs';
+import { function_name_get } from '../name/get.mjs';
+import { function_map_with_args } from '../map/with/args.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export async function function_call_inputs(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
+    await function_map_with_args(function_name_get(refactor_call_inputs), function_name, {});
+}
