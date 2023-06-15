376dfa499d65cf4ba1aa4e3f8982c67d9de4241f
JaredMathis <jared.mathis@native.consulting>
2023-06-15 18:57:56 -0400

    #101 function_input_swap_last_2 refactor_functions_string_to_function_call

--- src/refactor/functions/string/to/function/call.mjs
+++ src/refactor/functions/string/to/function/call.mjs
@@ -1,3 +1,6 @@
+import { log } from '../../../../../log.mjs';
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { object_replace } from '../../../../../object/replace.mjs';
 import { js_parse_call_expression } from '../../../../../js/parse/call/expression.mjs';
 import { equal } from '../../../../../equal.mjs';
@@ -6,7 +9,11 @@ import { object_property_get } from '../../../../../object/property/get.mjs';
 import { js_node_is_literal } from '../../../../../js/node/is/literal.mjs';
 import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { file_js_all_map_args_if_function } from '../../../../../file/js/all/map/args/if/function.mjs';
-export async function refactor_functions_string_to_function_call(string_value, function_name) {
+export async function refactor_functions_string_to_function_call(function_name, string_value) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     await file_js_all_map_args_if_function(async function logic(args) {
         let {parsed} = args;
         js_nodes_each(parsed, js_node_is_literal, n => {
