19209ad76e951da85ad69b1e9ba699d931fca0dd
JaredMathis <jared.mathis@native.consulting>
2023-06-15 10:25:51 -0400

    #93 function_auto function_add_mapper

--- src/function/add/mapper.mjs
+++ src/function/add/mapper.mjs
@@ -1,3 +1,4 @@
+import { js_code_call_expression_with_args } from '../../js/code/call/expression/with/args.mjs';
 import { js_call_expression_awaitify } from '../../js/call/expression/awaitify.mjs';
 import { js_code_statement } from '../../js/code/statement.mjs';
 import { function_add_with_statement } from './with/statement.mjs';
@@ -7,5 +8,6 @@ import { error } from '../../error.mjs';
 export async function function_add_mapper(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let expression = error();
+    js_code_call_expression_with_args();
     return await function_add_with_statement(function_name, js_code_statement(js_call_expression_awaitify(expression)));
 }
