2ca7a83f3f9e8a8b21b3b1790d6353c9421bc74c
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:03:47 -0400

    #101 function_auto refactor_functions_function_add_string_and_replace

--- src/refactor/functions/function/add/string/and/replace.mjs
+++ src/refactor/functions/function/add/string/and/replace.mjs
@@ -1,4 +1,13 @@
-import { refactor_functions_string_to_function_call } from '../../../../string/to/function/call.mjs';
+import { refactor_import_fix } from '../../../../../import/fix.mjs';
+import { object_replace } from '../../../../../../object/replace.mjs';
+import { js_parse_call_expression } from '../../../../../../js/parse/call/expression.mjs';
+import { equal } from '../../../../../../equal.mjs';
+import { js_node_property_value } from '../../../../../../js/node/property/value.mjs';
+import { object_property_get } from '../../../../../../object/property/get.mjs';
+import { js_node_is_literal } from '../../../../../../js/node/is/literal.mjs';
+import { js_nodes_each } from '../../../../../../js/nodes/each.mjs';
+import { changed } from '../../../../../../changed.mjs';
+import { file_js_all_map_args_if_function } from '../../../../../../file/js/all/map/args/if/function.mjs';
 import { function_add_string } from '../../../../../../function/add/string.mjs';
 import { metadata } from '../../../../../../metadata.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
@@ -10,6 +19,21 @@ export async function refactor_functions_function_add_string_and_replace(functio
         string_is
     ]);
     await function_add_string(function_name, string_value);
-    await refactor_functions_string_to_function_call(function_name, string_value);
+    await file_js_all_map_args_if_function(async function logic(args) {
+        let c = changed(change => {
+            let {parsed} = args;
+            js_nodes_each(parsed, js_node_is_literal, n => {
+                let value = object_property_get(n, js_node_property_value());
+                if (equal(value, string_value)) {
+                    let replacement = js_parse_call_expression(function_name);
+                    object_replace(n, replacement);
+                    change();
+                }
+            });
+        });
+        if (c) {
+            await refactor_import_fix(args);
+        }
+    });
     metadata([]);
 }
