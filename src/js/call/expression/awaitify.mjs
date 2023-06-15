306b94a1754657fc55af0f3bac96237955c00404
JaredMathis <jared.mathis@native.consulting>
2023-06-15 10:16:20 -0400

    #93 function_auto js_call_expression_awaitify

--- src/js/call/expression/awaitify.mjs
+++ src/js/call/expression/awaitify.mjs
@@ -1,3 +1,5 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { object_replace } from '../../../object/replace.mjs';
 import { js_await_expression_argument_change } from '../../await/expression/argument/change.mjs';
 import { object_copy_shallow } from '../../../object/copy/shallow.mjs';
@@ -7,12 +9,16 @@ import { js_parse_expression } from '../../parse/expression.mjs';
 import { js_call_expression_name_change } from './name/change.mjs';
 import { string_add } from '../../../string/add.mjs';
 import { js_call_expression_name_get_or_null } from './name/get/or/null.mjs';
-export function js_call_expression_awaitify(f, suffix) {
-    let name_old = js_call_expression_name_get_or_null(f);
+export function js_call_expression_awaitify(node, suffix) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let name_old = js_call_expression_name_get_or_null(node);
     let name_new = string_add(name_old, suffix);
-    js_call_expression_name_change(f, name_new);
+    js_call_expression_name_change(node, name_new);
     let awaited = js_parse_expression(js_code_await(string_a()));
-    let result = object_copy_shallow(f);
+    let result = object_copy_shallow(node);
     js_await_expression_argument_change(awaited, result);
-    object_replace(f, awaited);
+    object_replace(node, awaited);
 }
