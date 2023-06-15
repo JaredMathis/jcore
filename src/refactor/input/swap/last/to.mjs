d2f4eed83d948105dbc0f40648ebf3918e5a56fa
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:43:30 -0400

    #93 noop

--- src/refactor/input/swap/last/to.mjs
+++ src/refactor/input/swap/last/to.mjs
@@ -6,13 +6,14 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { add_1 } from '../../../../add/1.mjs';
 import { subtract_1 } from '../../../../subtract/1.mjs';
 import { assert } from '../../../../assert.mjs';
+import { list_last_index } from '../../../../list/last/index.mjs';
 export async function refactor_input_swap_last_to(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {index} = args;
     assert(index >= 0);
     let {function_declaration} = args;
     let params = js_function_declaration_to_params(function_declaration);
-    let index_last = list_index_last(params);
+    let index_last = list_last_index(params);
     assert(index < index_last);
     let index_next = add_1(index);
     for (let i of range_from(index_next, index_last)) {
