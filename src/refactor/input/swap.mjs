9b2c53b317dfea3e37ece55a991eec500f49710c
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:04:27 -0400

    #93 function_auto refactor_input_swap

--- src/refactor/input/swap.mjs
+++ src/refactor/input/swap.mjs
@@ -1,3 +1,4 @@
+import { integer_parse } from '../../integer/parse.mjs';
 import { list_swap } from '../../list/swap.mjs';
 import { refactor_input_generic_simple } from './generic/simple.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
@@ -5,6 +6,8 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 export async function refactor_input_swap(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {index_from, index_to} = args;
+    let index_from_integer = integer_parse(index_from);
+    let index_to_integer = integer_parse(index_to);
     let lambda = list => list_swap(list, index_from, index_to);
     await refactor_input_generic_simple(args, lambda);
 }
