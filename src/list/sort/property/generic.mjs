549ae9db1c134d8583ed42166f8929da73c08203
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:35:01 -0400

    #105 noop

--- src/list/sort/property/generic.mjs
+++ src/list/sort/property/generic.mjs
@@ -1,5 +1,5 @@
 import { object_property_get } from '../../../object/property/get.mjs';
 import { list_sort_generic } from '../generic.mjs';
-export function list_sort_property_generic(removals, sort_property, reverse) {
-    list_sort_generic(removals, r => object_property_get(r, sort_property), reverse);
+export function list_sort_property_generic(list, sort_property, reverse) {
+    list_sort_generic(list, r => object_property_get(r, sort_property), reverse);
 }
