80e213ad7c06416f729252ab05681904ef6af52e
JaredMathis <jared.mathis@native.consulting>
2023-06-15 07:34:21 -0400

    #93 noop

--- src/list/swap.mjs
+++ src/list/swap.mjs
@@ -1,8 +1,8 @@
 import { list_set } from './set.mjs';
 import { list_get } from './get.mjs';
-export function list_swap(list, index_a, index_b) {
-    let a = list_get(list, index_a);
-    let b = list_get(list, index_b);
-    list_set(list, index_b, a);
-    list_set(list, index_a, b);
+export function list_swap(list, index_from, index_to) {
+    let from = list_get(list, index_from);
+    let to = list_get(list, index_to);
+    list_set(list, index_to, from);
+    list_set(list, index_from, to);
 }
