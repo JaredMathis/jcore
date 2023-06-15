4cf2ce063f01826fec25dbaade218f6947d029f3
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:33:33 -0400

    #93 function_call_add refactor_unlambdaify

--- src/refactor/unlambdaify.mjs
+++ src/refactor/unlambdaify.mjs
@@ -3,5 +3,6 @@ import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 export function refactor_unlambdaify(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    error();
     error('todo: refactor_unlambdaify');
 }
