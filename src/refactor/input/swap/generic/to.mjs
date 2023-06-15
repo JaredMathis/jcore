4ab88183b8c8e44345e23f8246a95a749d8a10f8
JaredMathis <jared.mathis@native.consulting>
2023-06-15 16:01:08 -0400

    #93 noop

--- src/refactor/input/swap/generic/to.mjs
+++ src/refactor/input/swap/generic/to.mjs
@@ -5,9 +5,9 @@ import { list_index_valid } from '../../../../list/index/valid.mjs';
 import { assert } from '../../../../assert.mjs';
 export async function refactor_input_swap_generic_to(args, predicate, indices_get) {
     let {index} = args;
-    assert(list_index_valid(index));
     let {function_declaration} = args;
     let params = js_function_declaration_to_params(function_declaration);
+    assert(list_index_valid(params, index));
     assert(predicate(index, params));
     for (let i of indices_get(index, params)) {
         let index_previous = subtract_1(i);
