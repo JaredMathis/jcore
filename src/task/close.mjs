5e29476154c04279088f065524ac21eaa5966fb4
JaredMathis <jared.mathis@native.consulting>
2023-06-15 06:53:16 -0400

    #93 function_copy_replace task_add task_close

--- src/task/close.mjs
+++ src/task/close.mjs
@@ -1,7 +1,19 @@
+import { task_id_set_hash } from './id/set/hash.mjs';
+import { task_property_number } from './property/number.mjs';
+import { object_property_get } from '../object/property/get.mjs';
+import { list_add } from '../list/add.mjs';
+import { task_map } from './map.mjs';
+import { task_from_git_hub_issue } from './from/git/hub/issue.mjs';
+import { git_hub_repository_issues_post } from '../git/hub/repository/issues/post.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
-import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function task_close(task_number) {
+export async function task_close(title) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: task_close');
+    let data = await git_hub_repository_issues_post(title);
+    let task = task_from_git_hub_issue(data);
+    await task_map(function map(tasks_all) {
+        list_add(tasks_all, task);
+    });
+    let task_number = object_property_get(task, task_property_number());
+    await task_id_set_hash(task_number);
 }
