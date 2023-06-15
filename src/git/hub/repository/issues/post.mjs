87d339a9d3da7160f9d8a9cd62c52a5da1a28e25
JaredMathis <jared.mathis@native.consulting>
2023-06-15 01:04:53 -0400

    #93 function_input_add git_hub_repository_issues_post title

--- src/git/hub/repository/issues/post.mjs
+++ src/git/hub/repository/issues/post.mjs
@@ -1,9 +1,13 @@
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { git_hub_repository_issues_generic } from './generic.mjs';
 import { object_merge } from '../../../../object/merge.mjs';
 import { defined_is } from '../../../../defined/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function git_hub_repository_issues_post(api_args_to_merge) {
-    arguments_assert(arguments, [defined_is]);
+export async function git_hub_repository_issues_post(api_args_to_merge, title) {
+    arguments_assert(arguments, [
+        defined_is,
+        arguments_assert_todo
+    ]);
     let verb = 'POST';
     object_merge({ title }, api_args_to_merge);
     return await git_hub_repository_issues_generic(api_args_to_merge, verb);
