ad891501533992b7787aeabcc8debbdbea507243
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:26:13 -0400

    #93 function_wrap list_contains list_contains_not

---
+++ src/list/contains/not.mjs
@@ -0,0 +1,12 @@
+import { defined_is } from '../../defined/is.mjs';
+import { list_is } from '../is.mjs';
+import { list_contains } from '../contains.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function list_contains_not(list, element) {
+    arguments_assert(arguments, [
+        list_is,
+        defined_is
+    ]);
+    let result = list_contains(list, element);
+    return result;
+}
