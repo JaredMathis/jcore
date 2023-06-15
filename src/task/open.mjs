043155c964ed3ad42b6b51be82315de9ca957db2
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:30:29 -0400

    #104 function_add_string_prefix task_property open

--- src/task/open.mjs
+++ src/task/open.mjs
@@ -1,8 +1,9 @@
 import { list_filter_property } from '../list/filter/property.mjs';
 import { task_all } from './all.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { task_property_state } from './property/state.mjs';
 export async function task_open() {
     arguments_assert(arguments, []);
     let all = await task_all();
-    list_filter_property;
+    list_filter_property(all, task_property_state(), task_property_open());
 }
---
+++ src/task/property/open.mjs
@@ -0,0 +1,5 @@
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function task_property_open() {
+    arguments_assert(arguments, []);
+    return 'open';
+}
