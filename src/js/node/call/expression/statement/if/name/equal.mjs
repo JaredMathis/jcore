5139e2f690ab74b3603eb2b4256072fcd2a311f9
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:46:25 -0400

    #93 function_auto js_node_call_expression_statement_if_name_equal

--- src/js/node/call/expression/statement/if/name/equal.mjs
+++ src/js/node/call/expression/statement/if/name/equal.mjs
@@ -1,3 +1,4 @@
+import { js_statement_expression_to_expression } from '../../../../../../statement/expression/to/expression.mjs';
 import { function_is } from '../../../../../../../function/is.mjs';
 import { defined_is } from '../../../../../../../defined/is.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
@@ -14,7 +15,7 @@ export function js_node_call_expression_statement_if_name_equal(statement, name_
     ]);
     if (js_node_is_expression_statement(statement)) {
         if (false)
-            js_expression_statement_to_expression;
+            js_statement_expression_to_expression;
         let expression = object_property_get(statement, js_node_property_expression());
         js_node_call_expression_if_name_equal(expression, name_expected, on_name_equal);
     }
