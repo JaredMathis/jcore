afcd64fb0688a38c795f60b0368b191476cac111
JaredMathis <jared.mathis@native.consulting>
2023-06-15 01:04:10 -0400

    #93 function_auto git_hub_repository_issues_generic

--- src/git/hub/repository/issues/generic.mjs
+++ src/git/hub/repository/issues/generic.mjs
@@ -1,9 +1,16 @@
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { object_merge } from '../../../../object/merge.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
 import { file_json_read } from '../../../../file/json/read.mjs';
 import { git_hub_repository_issues_open } from './open.mjs';
 import { git_hub_cached } from '../../cached.mjs';
+import { Octokit } from 'octokit';
 export async function git_hub_repository_issues_generic(api_args_to_merge, verb) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     return await git_hub_cached(git_hub_repository_issues_open, arguments, async function lambda() {
         let p = await file_json_read('../private.json');
         let token = object_property_get(p, 'git_hub_api_token');
--- src/git/hub/repository/issues/get.mjs
+++ src/git/hub/repository/issues/get.mjs
@@ -2,7 +2,6 @@ import { git_hub_repository_issues_generic } from './generic.mjs';
 import { git_hub_page_size } from '../../page/size.mjs';
 import { object_merge } from '../../../../object/merge.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { Octokit } from 'octokit';
 import { defined_is } from '../../../../defined/is.mjs';
 export async function git_hub_repository_issues_get(api_args_to_merge) {
     arguments_assert(arguments, [defined_is]);
