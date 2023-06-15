f8885df7bf6ed47374788dffe295440425d120dc
JaredMathis <jared.mathis@native.consulting>
2023-06-15 07:16:25 -0400

    #93 function_auto task_next

--- src/task/next.mjs
+++ src/task/next.mjs
@@ -1,11 +1,11 @@
+import { task_add } from './add.mjs';
 import { string_is } from '../string/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
-import { task_id_set_hash } from './id/set/hash.mjs';
 import { task_finish } from './finish.mjs';
 export async function task_next(task_id) {
     arguments_assert(arguments, [string_is]);
     await task_finish();
-    await task_id_set_hash(task_id);
+    task_add;
     metadata([]);
 }
