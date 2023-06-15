b04e6feffc10eaa731e5c42094e02f9a02ecca15
JaredMathis <jared.mathis@native.consulting>
2023-06-15 14:57:21 -0400

    #93 noop

--- src/string/identifier/parts/from.mjs
+++ src/string/identifier/parts/from.mjs
@@ -1,5 +1,5 @@
 import { function_name_separator } from '../../../function/name/separator.mjs';
 import { list_join } from '../../../list/join.mjs';
-export function string_identifier_parts_from(parts) {
-    return list_join(parts, function_name_separator());
+export function string_identifier_parts_from(list_parts) {
+    return list_join(list_parts, function_name_separator());
 }
