b40c94ba0ee9dad5bd5d5d67ea79e6916b615203
JaredMathis <jared.mathis@native.consulting>
2023-06-15 18:59:54 -0400

    #101 function_input_add changed lambda

--- src/changed.mjs
+++ src/changed.mjs
@@ -1,6 +1,7 @@
+import { arguments_assert_todo } from './arguments/assert/todo.mjs';
 import { error } from './error.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
-export function changed() {
-    arguments_assert(arguments, []);
+export function changed(lambda) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: changed');
 }
