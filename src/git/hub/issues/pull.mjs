827ae48b8f241d3b7ad121d614a6a7f043009f5c
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:56:31 -0400

    #93 noop

--- src/git/hub/issues/pull.mjs
+++ src/git/hub/issues/pull.mjs
@@ -13,6 +13,7 @@ import { file_json_write } from '../../../file/json/write.mjs';
 import { path_join } from '../../../path/join.mjs';
 import { string_add } from '../../../string/add.mjs';
 import { file_extension_json } from '../../../file/extension/json.mjs';
+import { file_json_overwrite } from '../../../file/json/overwrite.mjs';
 export async function git_hub_issues_pull(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let issues_response = await git_hub_repository_issues_all();
@@ -25,5 +26,5 @@ export async function git_hub_issues_pull(repository_name) {
     ]));
     let tasks_path = version_path_tasks_get(repository_name);
     let tasks_all_path = path_join([tasks_path, string_add('all', file_extension_json())])
-    await file_json_write()
+    await file_json_overwrite(tasks_all_path, mapped);
 }
