1b1eee1906f9b2f77fe9da550edee6e14c7f986d
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:28:28 -0400

    #104 function_auto task_open

--- src/task/open.mjs
+++ src/task/open.mjs
@@ -1,6 +1,8 @@
+import { list_filter_property } from '../list/filter/property.mjs';
 import { task_all } from './all.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 export async function task_open() {
     arguments_assert(arguments, []);
     let all = await task_all();
+    list_filter_property;
 }
