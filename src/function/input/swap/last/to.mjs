67ce8890d4d9d82125b5996cbf5890cac8e303fd
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:08:00 -0400

    #93 function_add_mapper input_swap_last_to

---
+++ src/function/input/swap/last/to.mjs
@@ -0,0 +1,9 @@
+import { string_identifier_is } from '../../../../string/identifier/is.mjs';
+import { refactor_input_swap_last_to } from '../../../../refactor/input/swap/last/to.mjs';
+import { function_name_get } from '../../../name/get.mjs';
+import { function_map_with_args } from '../../../map/with/args.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export async function function_input_swap_last_to(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
+    await function_map_with_args(function_name_get(refactor_input_swap_last_to), function_name, {});
+}
