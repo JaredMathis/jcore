12b953de0853535e55dd83501dd9327b848b627a
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:42:56 -0400

    #107 function_auto task_resume

--- src/task/resume.mjs
+++ src/task/resume.mjs
@@ -1,11 +1,13 @@
 import { metadata } from '../metadata.mjs';
-import { task_add } from './add.mjs';
 import { task_finish } from './finish.mjs';
 import { string_is } from '../string/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export async function task_resume(title) {
+import { integer_parse } from '../integer/parse.mjs';
+import { task_set } from './set.mjs';
+export async function task_resume(task_number_string) {
     arguments_assert(arguments, [string_is]);
+    integer_parse(task_number_string);
     await task_finish();
-    await task_add(title);
+    await task_set(task_number_string);
     metadata([]);
 }
