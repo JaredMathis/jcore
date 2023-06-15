1db91ea141915a0fca9892cf6a155c4127529f46
JaredMathis <jared.mathis@native.consulting>
2023-06-15 14:54:35 -0400

    #93 function_auto function_input_add_multiple_prefix

--- src/function/input/add/multiple/prefix.mjs
+++ src/function/input/add/multiple/prefix.mjs
@@ -1,19 +1,14 @@
-import { error } from '../../../../error.mjs';
 import { string_identifier_parts_from } from '../../../../string/identifier/parts/from.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { function_input_add_multiple_generic } from './generic.mjs';
-import { list_map } from '../../../../list/map.mjs';
-import { string_identifier_multiple_parse } from '../../../../string/identifier/multiple/parse.mjs';
 export async function function_input_add_multiple_prefix(function_name, prefix, inputs_string) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let inputs = string_identifier_multiple_parse(inputs_string);
-    let mapped = list_map(inputs, map);
-    let result = await function_input_add_multiple_generic(function_name, mapped, error());
+    let result = await function_input_add_multiple_generic(function_name, mapped, map);
     return result;
     function map(i) {
         return string_identifier_parts_from(prefix, i);
