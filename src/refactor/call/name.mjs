e3a5a7e9017b24ea2c2a3df64c862b62eb0a30d6
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:36:52 -0400

    #93 function_auto refactor_call_name

--- src/refactor/call/name.mjs
+++ src/refactor/call/name.mjs
@@ -3,5 +3,6 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function refactor_call_name(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let {function_declaration} = args;
     let statement_arguments_assert = await js_function_declaration_to_statement_arguments_assert(function_declaration);
 }
