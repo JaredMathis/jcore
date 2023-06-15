75dbef111aaa170da17cceb3d769bec58f880619
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:19:40 -0400

    #93 noop

--- src/git/hub/issues/pull.mjs
+++ src/git/hub/issues/pull.mjs
@@ -1,6 +1,8 @@
 import { error } from '../../../error.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { assert } from '../../../assert.mjs';
 export function git_hub_issues_pull() {
     arguments_assert(arguments, []);
-    error('todo: git_hub_issues_pull');
+    let issues = git_hub_repository_issues_all()
+    assert(list_length(issues) <= 100);
 }
