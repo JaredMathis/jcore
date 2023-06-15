d7e587f8035c29fbdbffa4c35707123c097ca4a7
JaredMathis <jared.mathis@native.consulting>
2023-06-15 18:28:37 -0400

    #99 function_auto task_finish

--- src/task/finish.mjs
+++ src/task/finish.mjs
@@ -1,3 +1,4 @@
+import { string_prefix_without } from '../string/prefix/without.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { task_id_remove } from './id/remove.mjs';
@@ -6,7 +7,7 @@ import { task_close } from './close.mjs';
 export async function task_finish() {
     arguments_assert(arguments, []);
     let task_number_hashed = await task_id_get();
-    let task_number = string_prefix_remove(task_number_hashed, '#');
+    let task_number = string_prefix_without(task_number_hashed, '#');
     await task_close(task_number);
     await task_id_remove();
     metadata([]);
