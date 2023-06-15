0f55ac1dea7e7f8ffc8eb941527317b87746c635
JaredMathis <jared.mathis@native.consulting>
2023-06-15 14:09:31 -0400

    #93 noop

--- src/js/arrow/function/expression/functionify.mjs
+++ src/js/arrow/function/expression/functionify.mjs
@@ -15,7 +15,7 @@ export function js_arrow_function_expression_functionify(node, name) {
     ]);
     let function_code = `${ js_keyword_function() } ${ name }${ js_code_parenthesis_surround(``) }${ js_brace_left_right() }`;
     let function_expression = js_parse_expression(function_code);
-    js_code_return_statement(string_a());
+    let return_statement_code = js_code_return_statement(string_a());
     console.log({
         node,
         function_expression
