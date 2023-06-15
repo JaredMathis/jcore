a131869ef64e1ebdbdada2557be9b98b1e28873b
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:56:49 -0400

    #93 function_rename git_hub_issues_pull version_git_hub_issues_pull

---
+++ src/version/git/hub/issues/pull.mjs
@@ -0,0 +1,32 @@
+import { version_path_tasks_get } from '../../../path/tasks/get.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { object_map } from '../../../../object/map.mjs';
+import { object_property_data } from '../../../../object/property/data.mjs';
+import { git_hub_page_size } from '../../../../git/hub/page/size.mjs';
+import { list_length } from '../../../../list/length.mjs';
+import { git_hub_repository_issues_all } from '../../../../git/hub/repository/issues/all.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { assert } from '../../../../assert.mjs';
+import { comment } from '../../../../comment.mjs';
+import { list_map } from '../../../../list/map.mjs';
+import { path_join } from '../../../../path/join.mjs';
+import { string_add } from '../../../../string/add.mjs';
+import { file_extension_json } from '../../../../file/extension/json.mjs';
+import { file_json_overwrite } from '../../../../file/json/overwrite.mjs';
+export async function version_git_hub_issues_pull(repository_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    let issues_response = await git_hub_repository_issues_all();
+    let issues = object_property_data(issues_response);
+    comment(`If there's more than 1 page of issues code needs adjusting`);
+    assert(list_length(issues) <= git_hub_page_size());
+    let mapped = list_map(issues, issue => object_map(issue, [
+        'number',
+        'title'
+    ]));
+    let tasks_path = version_path_tasks_get(repository_name);
+    let tasks_all_path = path_join([
+        tasks_path,
+        string_add('all', file_extension_json())
+    ]);
+    await file_json_overwrite(tasks_all_path, mapped);
+}
