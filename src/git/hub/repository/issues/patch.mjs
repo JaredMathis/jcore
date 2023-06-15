669db731080878ee98809dae7d371a49451a6d0f
JaredMathis <jared.mathis@native.consulting>
2023-06-15 17:25:02 -0400

    #93 noop

--- src/git/hub/repository/issues/patch.mjs
+++ src/git/hub/repository/issues/patch.mjs
@@ -2,12 +2,14 @@ import { git_hub_repository_issue_generic } from '../issue/generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { object_merge } from '../../../../object/merge.mjs';
-export async function git_hub_repository_issues_patch(issue_number, api_args_to_merge) {
+export async function git_hub_repository_issues_patch(fn, args, issue_number, api_args_to_merge) {
     arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
     object_merge({ issue_number }, api_args_to_merge);
     let verb = 'PATCH';
-    return await git_hub_repository_issue_generic(api_args_to_merge, verb);
+    return await git_hub_repository_issue_generic(fn, args, api_args_to_merge, verb);
 }
