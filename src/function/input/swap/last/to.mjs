e080c0a2a7da5db9c88390ee2400adc1c0339249
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:44:20 -0400

    #93 function_auto function_input_swap_last_to

--- src/function/input/swap/last/to.mjs
+++ src/function/input/swap/last/to.mjs
@@ -4,10 +4,12 @@ import { refactor_input_swap_last_to } from '../../../../refactor/input/swap/las
 import { function_name_get } from '../../../name/get.mjs';
 import { function_map_with_args } from '../../../map/with/args.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function function_input_swap_last_to(function_name, index) {
+import { integer_parse } from '../../../../integer/parse.mjs';
+export async function function_input_swap_last_to(function_name, index_string) {
     arguments_assert(arguments, [
         string_identifier_is,
         arguments_assert_todo
     ]);
-    await function_map_with_args(function_name_get(refactor_input_swap_last_to), function_name, {index});
+    let index = integer_parse(index_string);
+    await function_map_with_args(function_name_get(refactor_input_swap_last_to), function_name, { index });
 }
