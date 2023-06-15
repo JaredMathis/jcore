c4a525875110fd9b3e060d2a078fbfb2d7ed99e8
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:29:55 -0400

    #93 function_auto git_hub_issues_pull

--- src/git/hub/issues/pull.mjs
+++ src/git/hub/issues/pull.mjs
@@ -1,22 +1,21 @@
 import { object_map } from '../../../object/map.mjs';
 import { object_property_data } from '../../../object/property/data.mjs';
-import { list_first } from '../../../list/first.mjs';
 import { git_hub_page_size } from '../page/size.mjs';
 import { list_length } from '../../../list/length.mjs';
 import { git_hub_repository_issues_all } from '../repository/issues/all.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { assert } from '../../../assert.mjs';
 import { comment } from '../../../comment.mjs';
+import { list_map } from '../../../list/map.mjs';
 export async function git_hub_issues_pull() {
     arguments_assert(arguments, []);
     let issues_response = await git_hub_repository_issues_all();
     let issues = object_property_data(issues_response);
     comment(`If there's more than 1 page of issues code needs adjusting`);
     assert(list_length(issues) <= git_hub_page_size());
-    let issue_first = list_first(issues);
-    let mapped = object_map(issue_first, [
+    let mapped = list_map(issues, issue => object_map(issue, [
         'number',
         'title'
-    ]);
+    ]));
     return mapped;
 }
