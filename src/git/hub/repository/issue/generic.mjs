ecb43d8d3c70b10b6ba9865667cc48bdc2b4dd2f
JaredMathis <jared.mathis@native.consulting>
2023-06-15 17:29:03 -0400

    #93 function_auto git_hub_repository_issue_generic

--- src/git/hub/repository/issue/generic.mjs
+++ src/git/hub/repository/issue/generic.mjs
@@ -8,6 +8,6 @@ export async function git_hub_repository_issue_generic(fn, args, api_args_to_mer
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let api_path = `/repos/{owner}/{repo}/issue/{issue_number}`;
+    let api_path = `/repos/{owner}/{repo}/issues/{issue_number}`;
     return await git_hub_api(fn, args, verb, api_path, api_args_to_merge);
 }
