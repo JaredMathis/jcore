8aac3a14b0b5c4a7d0fbb16dd5f3e3f2ba2d90ed
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:54:02 -0400

    #93 function_input_add git_hub_issues_pull repository_name

--- src/git/hub/issues/pull.mjs
+++ src/git/hub/issues/pull.mjs
@@ -1,3 +1,5 @@
+import { version_path_tasks_get } from '../../../version/path/tasks/get.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { object_map } from '../../../object/map.mjs';
 import { object_property_data } from '../../../object/property/data.mjs';
 import { git_hub_page_size } from '../page/size.mjs';
@@ -7,8 +9,8 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { assert } from '../../../assert.mjs';
 import { comment } from '../../../comment.mjs';
 import { list_map } from '../../../list/map.mjs';
-export async function git_hub_issues_pull() {
-    arguments_assert(arguments, []);
+export async function git_hub_issues_pull(repository_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let issues_response = await git_hub_repository_issues_all();
     let issues = object_property_data(issues_response);
     comment(`If there's more than 1 page of issues code needs adjusting`);
@@ -17,5 +19,5 @@ export async function git_hub_issues_pull() {
         'number',
         'title'
     ]));
-    
+    let repository_file_directory_path = version_path_tasks_get(repository_name);
 }
