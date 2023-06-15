5979a5ceee4d5832f92eb46b38d907f2dd56d558
JaredMathis <jared.mathis@native.consulting>
2023-06-15 07:01:24 -0400

    #93 function_auto task_close

--- src/task/close.mjs
+++ src/task/close.mjs
@@ -1,17 +1,17 @@
+import { task_property_closed } from './property/closed.mjs';
+import { task_property_state } from './property/state.mjs';
+import { object_property_change } from '../object/property/change.mjs';
+import { list_find_property } from '../list/find/property.mjs';
 import { git_hub_repository_issues_close } from '../git/hub/repository/issues/close.mjs';
-import { list_add } from '../list/add.mjs';
 import { task_map } from './map.mjs';
-import { task_from_git_hub_issue } from './from/git/hub/issue.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-import { list_filter } from '../list/filter.mjs';
-import { equal } from '../equal.mjs';
 import { task_property_number } from './property/number.mjs';
 export async function task_close(task_number) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let data = await git_hub_repository_issues_close(task_number);
     await task_map(function map(tasks_all) {
         let task = list_find_property(tasks_all, task_property_number(), task_number);
-        object_property_change(task, task_property_state(), task_property_closed())
+        object_property_change(task, task_property_state(), task_property_closed());
     });
 }
