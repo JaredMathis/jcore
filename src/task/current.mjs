216d91471f2b6caf61eb95f0b6ec257ef674d40c
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:47:13 -0400

    #108 noop

--- src/task/current.mjs
+++ src/task/current.mjs
@@ -3,6 +3,9 @@ import { metadata } from '../metadata.mjs';
 import { task_id_get_or_null } from './id/get/or/null.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { task_all } from './all.mjs';
+import { list_filter_property } from '../list/filter/property.mjs';
+import { task_property_number } from './property/number.mjs';
+import { integer_parse } from '../integer/parse.mjs';
 export async function task_current() {
     arguments_assert(arguments, []);
     let task_number_hashed = await task_id_get_or_null();
@@ -10,6 +13,8 @@ export async function task_current() {
         return 'no current task';
     }
     let task_number_string = task_id_unhash(task_number_hashed);
+    let task_number = integer_parse(task_number_string);
     let all = await task_all();
+    let filtered = list_filter_property(all, task_property_number(), task_number);
     metadata([]);
 }
