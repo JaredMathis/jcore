fbfd4e36d720b42b757b84c6c6a145df09f18365
JaredMathis <jared.mathis@native.consulting>
2023-06-15 12:50:28 -0400

    #93 function_auto js_arrow_function_expression_functionify

--- src/js/arrow/function/expression/functionify.mjs
+++ src/js/arrow/function/expression/functionify.mjs
@@ -1,7 +1,7 @@
-import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { js_node_is_arrow_function_expression } from '../../../node/is/arrow/function/expression.mjs';
 import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function js_arrow_function_expression_functionify(node) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [js_node_is_arrow_function_expression]);
     error('todo: js_arrow_function_expression_functionify');
 }
