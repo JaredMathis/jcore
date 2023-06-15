5c98a0dbcc6aabfe6028f3b1f505a4552749e125
JaredMathis <jared.mathis@native.consulting>
2023-06-15 07:22:55 -0400

    #93 function_auto git_hub_repository_issues_close

--- src/git/hub/repository/issues/close.mjs
+++ src/git/hub/repository/issues/close.mjs
@@ -1,15 +1,10 @@
 import { git_hub_repository_issues_patch } from './patch.mjs';
 import { task_property_closed } from '../../../../task/property/closed.mjs';
 import { task_property_state } from '../../../../task/property/state.mjs';
-import { object_merge } from '../../../../object/merge.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { defined_is } from '../../../../defined/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function git_hub_repository_issues_close(api_args_to_merge, issue_number) {
-    arguments_assert(arguments, [
-        defined_is,
-        arguments_assert_todo
-    ]);
-    object_merge({ [task_property_state()]: task_property_closed() }, api_args_to_merge);
+export async function git_hub_repository_issues_close(issue_number) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    let api_args_to_merge = { [task_property_state()]: task_property_closed() };
     return git_hub_repository_issues_patch(issue_number, api_args_to_merge);
 }
