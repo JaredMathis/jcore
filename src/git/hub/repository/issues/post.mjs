082142fe8e8f9e44324d332e390b81c16a27f9f1
JaredMathis <jared.mathis@native.consulting>
2023-06-15 18:22:44 -0400

    #94 noop

--- src/git/hub/repository/issues/post.mjs
+++ src/git/hub/repository/issues/post.mjs
@@ -10,5 +10,5 @@ export async function git_hub_repository_issues_post(title) {
     let api_args_to_merge = {};
     let verb = 'POST';
     object_merge({ title }, api_args_to_merge);
-    return await git_hub_repository_issues_generic(api_args_to_merge, verb);
+    return await git_hub_repository_issues_generic(git_hub_repository_issues_post, arguments, api_args_to_merge, verb);
 }
