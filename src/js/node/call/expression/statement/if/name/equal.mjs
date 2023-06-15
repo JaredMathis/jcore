0926a43c6c654877a5a7bda71ff74dd8ec36f7e8
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:46:34 -0400

    #93 function_auto js_node_call_expression_statement_if_name_equal

--- src/js/node/call/expression/statement/if/name/equal.mjs
+++ src/js/node/call/expression/statement/if/name/equal.mjs
@@ -3,8 +3,6 @@ import { function_is } from '../../../../../../../function/is.mjs';
 import { defined_is } from '../../../../../../../defined/is.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 import { js_node_call_expression_if_name_equal } from '../../../if/name/equal.mjs';
-import { js_node_property_expression } from '../../../../../property/expression.mjs';
-import { object_property_get } from '../../../../../../../object/property/get.mjs';
 import { js_node_is_expression_statement } from '../../../../../is/expression/statement.mjs';
 import { js_node_is } from '../../../../../is.mjs';
 export function js_node_call_expression_statement_if_name_equal(statement, name_expected, on_name_equal) {
@@ -14,9 +12,7 @@ export function js_node_call_expression_statement_if_name_equal(statement, name_
         function_is
     ]);
     if (js_node_is_expression_statement(statement)) {
-        if (false)
-            js_statement_expression_to_expression;
-        let expression = object_property_get(statement, js_node_property_expression());
+        let expression = js_statement_expression_to_expression(statement);
         js_node_call_expression_if_name_equal(expression, name_expected, on_name_equal);
     }
 }
