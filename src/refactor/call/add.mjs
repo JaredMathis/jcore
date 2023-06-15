1f6fb9d048e171d872bed32486c67433bd6b270a
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:29:38 -0400

    #93 function_search statement,call

--- src/refactor/call/add.mjs
+++ src/refactor/call/add.mjs
@@ -1,7 +1,12 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { js_parse_statement } from '../../js/parse/statement.mjs';
 export function refactor_call_add(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let {function_declaration} = args;
+    let statement = js_parse_statement(identifier, after_let_identifier);
+    await js_function_declaration_statement_add_after_arguments_assert(function_declaration, statement);
+    await refactor_import_fix(args);
     error('todo: refactor_call_add');
 }
