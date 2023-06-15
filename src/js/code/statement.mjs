99ad13e0875413048a42c534ac0c3bdd87a3b602
JaredMathis <jared.mathis@native.consulting>
2023-06-15 10:10:02 -0400

    #93 function_auto js_code_statement

---
+++ src/js/code/statement.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function js_code_statement() {
+    arguments_assert(arguments, []);
+    error('todo: js_code_statement');
+}
