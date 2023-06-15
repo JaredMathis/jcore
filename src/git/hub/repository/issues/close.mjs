d1215809a1d511e86ce924ecf5d421980433e384
JaredMathis <jared.mathis@native.consulting>
2023-06-15 06:47:54 -0400

    #93 function_auto git_hub_repository_issues_close

--- src/git/hub/repository/issues/close.mjs
+++ src/git/hub/repository/issues/close.mjs
@@ -3,12 +3,12 @@ import { object_merge } from '../../../../object/merge.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { defined_is } from '../../../../defined/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function git_hub_repository_issues_close(api_args_to_merge, title) {
+export async function git_hub_repository_issues_close(api_args_to_merge, issue_number) {
     arguments_assert(arguments, [
         defined_is,
         arguments_assert_todo
     ]);
-    let verb = 'POST';
-    object_merge({ title }, api_args_to_merge);
+    let verb = 'PATCH';
+    object_merge({ issue_number }, api_args_to_merge);
     return await git_hub_repository_issues_generic(api_args_to_merge, verb);
 }
