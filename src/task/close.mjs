46be4bb23586eeb9ff18b6394ad238add7b65732
JaredMathis <jared.mathis@native.consulting>
2023-06-15 06:45:35 -0400

    #93 function_input_add task_close task_number

--- src/task/close.mjs
+++ src/task/close.mjs
@@ -1,6 +1,7 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function task_close() {
-    arguments_assert(arguments, []);
+export function task_close(task_number) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: task_close');
 }
