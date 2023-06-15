5f12399600d7a1882685c653c02668f0acb1900d
JaredMathis <jared.mathis@native.consulting>
2023-06-14 20:05:13 -0400

    #93 noop

--- src/git/hub/repository/issues/open.mjs
+++ src/git/hub/repository/issues/open.mjs
@@ -37,3 +37,30 @@ export async function git_hub_repository_issues_open() {
     await file_json_write(file_path, issues);
     return issues;
 }
+
+async function git_hub_repository_issues_generic() {
+    arguments_assert(arguments, []);
+    await git_ignore_add_if_not_exists(git_ignore_cache());
+    let key = function_name_get(git_hub_repository_issues_open);
+    let file_name = string_base64_to(key);
+    let file_path = path_join([
+        git_ignore_cache(),
+        string_add(file_name, file_extension_json())
+    ]);
+    if (await file_exists(file_path)) {
+        let result = await file_json_read(file_path);
+        return result;
+    }
+    let p = await file_json_read('../private.json');
+    let token = object_property_get(p, 'git_hub_api_token');
+    const octokit = new Octokit({ auth: token });
+    let owner = 'JaredMathis';
+    let repository = 'jcore';
+    const args = {
+        owner: owner,
+        repo: repository
+    };
+    let issues = await octokit.request('GET /repos/{owner}/{repo}/issues', args);
+    await file_json_write(file_path, issues);
+    return issues;
+}
