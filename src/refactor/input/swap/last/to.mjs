dc686f02b6e72948c7cc8f1a9c4157cc0c626cc8
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:37:27 -0400

    #93 function_auto refactor_input_swap_last_to

--- src/refactor/input/swap/last/to.mjs
+++ src/refactor/input/swap/last/to.mjs
@@ -1,16 +1,17 @@
+import { range_from } from '../../../../range/from.mjs';
 import { js_function_declaration_to_params } from '../../../../js/function/declaration/to/params.mjs';
 import { refactor_input_swap_generic } from '../generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { range } from '../../../../range.mjs';
-import { list_length } from '../../../../list/length.mjs';
+import { add_1 } from '../../../../add/1.mjs';
 export function refactor_input_swap_last_to(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {index} = args;
     let {function_declaration} = args;
     let params = js_function_declaration_to_params(function_declaration);
-    let params_length = list_length(params);
-    let limit = params_length - index;
-    for (let i of range(0, list_length()));
+    let index_next = add_1(index);
+    let index_last = list_index_last(params);
+    for (let i of range_from(index_next, index_last)) {
+    }
     refactor_input_swap_generic();
 }
