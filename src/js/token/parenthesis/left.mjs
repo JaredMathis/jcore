c720650324c90f3aba9981dc28a03da4eec4f8ba
JaredMathis <jared.mathis@native.consulting>
2023-06-15 12:54:48 -0400

    #93 function_add_string_multiple js_token_parenthesis left,right (,)

---
+++ src/js/token/parenthesis/left.mjs
@@ -0,0 +1,5 @@
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function js_token_parenthesis_left() {
+    arguments_assert(arguments, []);
+    return '(';
+}
---
+++ src/js/token/parenthesis/right.mjs
@@ -0,0 +1,5 @@
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function js_token_parenthesis_right() {
+    arguments_assert(arguments, []);
+    return ')';
+}
