131bf0fbe014d03cecf04779e092bda736977691
JaredMathis <jared.mathis@native.consulting>
2023-06-15 00:54:40 -0400

    #93 function_auto task_add

--- src/task/add.mjs
+++ src/task/add.mjs
@@ -4,6 +4,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { file_json_read } from '../file/json/read.mjs';
 import { file_json_overwrite } from '../file/json/overwrite.mjs';
 import { file_exists } from '../file/exists.mjs';
+import { list_add } from '../list/add.mjs';
 export async function task_add(title) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let tasks_all_path = version_path_tasks_all_get(repository_name);
@@ -11,5 +12,6 @@ export async function task_add(title) {
         await file_json_overwrite(tasks_all_path, []);
     }
     let tasks_all = await file_json_read(tasks_all_path);
+    list_add(tasks_all, {});
     await file_json_overwrite(tasks_all);
 }
