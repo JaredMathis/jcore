15950af1ae7c58bc6f20f09e1aea7548f88a6966
JaredMathis <jared.mathis@native.consulting>
2023-06-15 14:23:16 -0400

    #93 function_copy js_block_statement_body js_arrow_function_expression_body

---
+++ src/js/arrow/function/expression/body.mjs
@@ -0,0 +1,12 @@
+import { js_node_property_body } from '../../../node/property/body.mjs';
+import { object_property_get } from '../../../../object/property/get.mjs';
+import { js_node_is_block_statement } from '../../../node/is/block/statement.mjs';
+import { assert } from '../../../../assert.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function js_arrow_function_expression_body(function_body) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    assert(js_node_is_block_statement(function_body));
+    let function_body_statements = object_property_get(function_body, js_node_property_body());
+    return function_body_statements;
+}
