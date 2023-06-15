7dd9107c28e94bd789176fc50b75e26d9f847075
JaredMathis <jared.mathis@native.consulting>
2023-06-14 23:23:26 -0400

    #93 git_hub_issues_pull

--- src/git/hub/issues/pull.mjs
+++ src/git/hub/issues/pull.mjs
@@ -6,9 +6,9 @@ import { git_hub_repository_issues_all } from '../repository/issues/all.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { assert } from '../../../assert.mjs';
 import { comment } from '../../../comment.mjs';
-export function git_hub_issues_pull() {
+export async function git_hub_issues_pull() {
     arguments_assert(arguments, []);
-    let issues_response = git_hub_repository_issues_all();
+    let issues_response = await git_hub_repository_issues_all();
     let issues = object_property_data(issues_response);
     comment(`If there's more than 1 page of issues code needs adjusting`);
     assert(list_length(issues) <= git_hub_page_size());
