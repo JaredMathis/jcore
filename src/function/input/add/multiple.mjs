50a67bd8a4ed01890765c5dd6cf6554057736664
JaredMathis <jared.mathis@native.consulting>
2023-06-15 14:54:11 -0400

    #93 function_auto function_input_add_multiple

--- src/function/input/add/multiple.mjs
+++ src/function/input/add/multiple.mjs
@@ -1,13 +1,11 @@
-import { error } from '../../../error.mjs';
+import { identity } from '../../../identity.mjs';
 import { function_input_add_multiple_generic } from './multiple/generic.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { string_identifier_multiple_parse } from '../../../string/identifier/multiple/parse.mjs';
 export async function function_input_add_multiple(function_name, inputs_string) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let inputs = string_identifier_multiple_parse(inputs_string);
-    await function_input_add_multiple_generic(function_name, inputs, error());
+    await function_input_add_multiple_generic(function_name, inputs_string, identity);
 }
