3217230911fa26a2f045f451352a0f968e70e78e
JaredMathis <jared.mathis@native.consulting>
2023-06-15 12:50:51 -0400

    #93 function_auto js_arrow_function_expression_functionify

--- src/js/arrow/function/expression/functionify.mjs
+++ src/js/arrow/function/expression/functionify.mjs
@@ -1,7 +1,7 @@
+import { js_keyword_function } from '../../../keyword/function.mjs';
 import { js_node_is_arrow_function_expression } from '../../../node/is/arrow/function/expression.mjs';
-import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function js_arrow_function_expression_functionify(node) {
     arguments_assert(arguments, [js_node_is_arrow_function_expression]);
-    error('todo: js_arrow_function_expression_functionify');
+    let function_code = `${ js_keyword_function() }`;
 }
