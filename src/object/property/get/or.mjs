43a29e41df9df20797ae83b41482f1b7ab3415a6
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:15:54 -0400

    #102 function_auto object_property_get_or

--- src/object/property/get/or.mjs
+++ src/object/property/get/or.mjs
@@ -1,14 +1,17 @@
 import { metadata } from '../../../metadata.mjs';
 import { object_property_exists } from '../exists.mjs';
-import { assert } from '../../../assert.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function object_property_get_or(object, property_name) {
+import { object_property_get } from '../get.mjs';
+export function object_property_get_or(object, property_name, or_value) {
     arguments_assert(arguments, [
+        arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    assert(object_property_exists(object, property_name));
-    return object[property_name];
+    if (object_property_exists(object, property_name)) {
+        return object_property_get(object, property_name);
+    }
+    return or_value;
     metadata([]);
 }
