3de579c5e551c79d577e0da431d88937197954d5
JaredMathis <jared.mathis@native.consulting>
2023-06-14 20:08:36 -0400

    #93 function_copy json_to json_to_minimized

---
+++ src/json/to/minimized.mjs
@@ -0,0 +1,10 @@
+import { metadata } from '../../metadata.mjs';
+import { json_to_generic } from './generic.mjs';
+import { defined_is } from '../../defined/is.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function json_to_minimized(object) {
+    arguments_assert(arguments, [defined_is]);
+    const space = ' ';
+    return json_to_generic(object, space);
+    metadata([]);
+}
