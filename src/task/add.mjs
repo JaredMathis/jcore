eccea92b1f1393741d0713d2d7046d41ae8a5d7f
JaredMathis <jared.mathis@native.consulting>
2023-06-15 00:49:37 -0400

    #93 function_input_add task_add title

--- src/task/add.mjs
+++ src/task/add.mjs
@@ -1,6 +1,7 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function task_add() {
-    arguments_assert(arguments, []);
+export function task_add(title) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: task_add');
 }
