1b87fd54c8a38bc52d96b84e9d2324804bb72790
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:12:24 -0400

    #93 function_auto refactor_input_swap_last_to

--- src/refactor/input/swap/last/to.mjs
+++ src/refactor/input/swap/last/to.mjs
@@ -1,3 +1,4 @@
+import { js_function_declaration_to_params } from '../../../../js/function/declaration/to/params.mjs';
 import { refactor_input_swap_generic } from '../generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
@@ -6,6 +7,9 @@ import { list_length } from '../../../../list/length.mjs';
 export function refactor_input_swap_last_to(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {index} = args;
+    let {function_declaration} = args;
+    let params = js_function_declaration_to_params(function_declaration);
+    let params_length = list_length(params);
     for (let i of range(0, list_length()));
     refactor_input_swap_generic();
 }
