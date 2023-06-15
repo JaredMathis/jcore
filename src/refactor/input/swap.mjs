72b14ba373cfd47ab5a381a1e166ff183ef34d48
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:04:41 -0400

    #93 noop

--- src/refactor/input/swap.mjs
+++ src/refactor/input/swap.mjs
@@ -8,6 +8,6 @@ export async function refactor_input_swap(args) {
     let {index_from, index_to} = args;
     let index_from_integer = integer_parse(index_from);
     let index_to_integer = integer_parse(index_to);
-    let lambda = list => list_swap(list, index_from, index_to);
+    let lambda = list => list_swap(list, index_from_integer, index_to_integer);
     await refactor_input_generic_simple(args, lambda);
 }
