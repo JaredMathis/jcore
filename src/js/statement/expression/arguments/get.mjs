5aa2b5402977f6fe2c554c1f0e87a0e5d0da8cf2
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:43:07 -0400

    #93 function_auto js_statement_expression_arguments_get

--- src/js/statement/expression/arguments/get.mjs
+++ src/js/statement/expression/arguments/get.mjs
@@ -1,12 +1,12 @@
+import { js_statement_expression_to_expression } from '../to/expression.mjs';
 import { js_node_is_expression_statement } from '../../../node/is/expression/statement.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { js_node_property_arguments } from '../../../node/property/arguments.mjs';
-import { js_node_property_expression } from '../../../node/property/expression.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
 export function js_statement_expression_arguments_get(s) {
     arguments_assert(arguments, [js_node_is_expression_statement]);
-    let expression = object_property_get(s, js_node_property_expression());
+    let expression = js_statement_expression_to_expression(s);
     let expression_args = object_property_get(expression, js_node_property_arguments());
     return expression_args;
     metadata([]);
