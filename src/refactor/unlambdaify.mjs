5393d61bd70aabac8d0d48a6365eaee52701ee83
JaredMathis <jared.mathis@native.consulting>
2023-06-15 12:14:38 -0400

    #93 function_auto refactor_unlambdaify

--- src/refactor/unlambdaify.mjs
+++ src/refactor/unlambdaify.mjs
@@ -1,10 +1,12 @@
+import { log } from '../log.mjs';
 import { js_node_is_arrow_function_expression } from '../js/node/is/arrow/function/expression.mjs';
 import { js_nodes_each } from '../js/nodes/each.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
-import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 export function refactor_unlambdaify(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    js_nodes_each(parsed, js_node_is_arrow_function_expression, lambda);
-    error('todo: refactor_unlambdaify');
+    let {parsed} = args;
+    js_nodes_each(parsed, js_node_is_arrow_function_expression, n => {
+        console.log({ n });
+    });
 }
