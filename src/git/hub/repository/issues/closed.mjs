36467525e3a8c064ce90831d5612a2841fb7e63d
JaredMathis <jared.mathis@native.consulting>
2023-06-14 20:10:31 -0400

    #93 noop

--- src/git/hub/repository/issues/closed.mjs
+++ src/git/hub/repository/issues/closed.mjs
@@ -2,5 +2,5 @@ import { git_hub_repository_issues_generic } from './generic.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function git_hub_repository_issues_closed() {
     arguments_assert(arguments, []);
-    return await git_hub_repository_issues_generic({});
+    return await git_hub_repository_issues_generic({state:'closed'});
 }
