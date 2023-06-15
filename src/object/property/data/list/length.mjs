0d320ae8f51d1dd17420055f22aaa9c10ff1ee0b
JaredMathis <jared.mathis@native.consulting>
2023-06-15 06:50:42 -0400

    #93 function_delete object_property_data_list_length

--- src/object/property/data/list/length.mjs
+++
@@ -1,9 +0,0 @@
-import { object_property_data } from '../../data.mjs';
-import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { list_length } from '../../../../list/length.mjs';
-export function object_property_data_list_length(issues) {
-    arguments_assert(arguments, [arguments_assert_todo]);
-    let data = object_property_data(issues);
-    return list_length(data);
-}
