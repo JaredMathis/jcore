14ebeaeedc1310fc53d005f7264d508b6f7eaa95
JaredMathis <jared.mathis@native.consulting>
2023-06-15 19:02:38 -0400

    #101 noop

--- src/task/symbol/hash.mjs
+++ src/task/symbol/hash.mjs
@@ -1,5 +1,5 @@
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function task_symbol_hash() {
     arguments_assert(arguments, []);
-    return task_symbol_hash();
+    return '#';
 }
