8d8bc42e0374a793fb0360df230313e38b0a8809
JaredMathis <jared.mathis@native.consulting>
2023-06-15 11:39:23 -0400

    #93 function_search after

--- src/refactor/call/name.mjs
+++ src/refactor/call/name.mjs
@@ -1,11 +1,13 @@
 import { js_function_declaration_to_statements_and_arguments_assert_statement } from '../../js/function/declaration/to/statements/and/arguments/assert/statement.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { add_1 } from '../../add/1.mjs';
+import { list_get } from '../../list/get.mjs';
 export async function refactor_call_name(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {function_declaration} = args;
     let {statements, statement_arguments_assert} = await js_function_declaration_to_statements_and_arguments_assert_statement(function_declaration);
     let index = list_index_of(statements, statement_arguments_assert);
-    let index_after = 
-    let statement_after = 
+    let index_after = add_1(index);
+    let statement_after = list_get(index_after);
 }
