46c9150f295add4cbef3652b529064bf4a9c880d
JaredMathis <jared.mathis@native.consulting>
2023-06-15 12:56:51 -0400

    #93 function_auto js_code_call_expression_with_args_code

--- src/js/code/call/expression/with/args/code.mjs
+++ src/js/code/call/expression/with/args/code.mjs
@@ -1,3 +1,5 @@
+import { js_token_parenthesis_right } from '../../../../../token/parenthesis/right.mjs';
+import { js_token_parenthesis_left } from '../../../../../token/parenthesis/left.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
 import { metadata } from '../../../../../../metadata.mjs';
 import { string_is } from '../../../../../../string/is.mjs';
@@ -6,6 +8,6 @@ export function js_code_call_expression_with_args_code(name, args) {
         string_is,
         string_is
     ]);
-    return `${ name }(${ args })`;
+    return `${ name }${ js_token_parenthesis_left() }${ args }${ js_token_parenthesis_right() }`;
     metadata([]);
 }
