8777c1ebeb24ea32e5ef265ed37b7812b3d3fb09
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:55:23 -0400

    #93 function_auto refactor_input_swap_last_to

---
+++ src/refactor/input/swap/generic/to.mjs
@@ -0,0 +1,16 @@
+import { refactor_input_swap_generic } from '../generic.mjs';
+import { subtract_1 } from '../../../../subtract/1.mjs';
+import { js_function_declaration_to_params } from '../../../../js/function/declaration/to/params.mjs';
+import { list_index_valid } from '../../../../list/index/valid.mjs';
+import { assert } from '../../../../assert.mjs';
+export async function refactor_input_swap_generic_to(args, predicate, indices_get) {
+    let {index} = args;
+    assert(list_index_valid(index));
+    let {function_declaration} = args;
+    let params = js_function_declaration_to_params(function_declaration);
+    assert(predicate(index, params));
+    for (let i of indices_get(index, params)) {
+        let index_previous = subtract_1(i);
+        await refactor_input_swap_generic(index_previous, i, args);
+    }
+}
--- src/refactor/input/swap/last/to.mjs
+++ src/refactor/input/swap/last/to.mjs
@@ -1,30 +1,17 @@
-import { list_index_valid } from '../../../../list/index/valid.mjs';
+import { refactor_input_swap_generic_to } from '../generic/to.mjs';
 import { range_from } from '../../../../range/from.mjs';
-import { js_function_declaration_to_params } from '../../../../js/function/declaration/to/params.mjs';
-import { refactor_input_swap_generic } from '../generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { add_1 } from '../../../../add/1.mjs';
-import { subtract_1 } from '../../../../subtract/1.mjs';
-import { assert } from '../../../../assert.mjs';
 import { list_last_index } from '../../../../list/last/index.mjs';
 export async function refactor_input_swap_last_to(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let {index} = args;
-    assert(list_index_valid(index));
-    let {function_declaration} = args;
-    let params = js_function_declaration_to_params(function_declaration);
-    assert(predicate(index, params));
-    for (let i of indices_get(index, params)) {
-        let index_previous = subtract_1(i);
-        await refactor_input_swap_generic(index_previous, i, args);
-    }
+    await refactor_input_swap_generic_to(args, predicate, indices_get);
     function indices_get(index, params) {
         let index_next = add_1(index);
         let index_last = list_last_index(params);
         return range_from(index_next, index_last);
     }
-
     function predicate(index, params) {
         let index_last = list_last_index(params);
         return index < index_last;
