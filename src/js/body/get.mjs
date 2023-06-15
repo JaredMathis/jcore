7a740fe43a1c0d509cf8a6bcbe35d4f28abf1b43
JaredMathis <jared.mathis@native.consulting>
2023-06-15 14:21:03 -0400

    #93 function_auto js_body_get

--- src/js/body/get.mjs
+++ src/js/body/get.mjs
@@ -1,9 +1,12 @@
+import { js_node_property_body } from '../node/property/body.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 import { js_node_is_program } from '../node/is/program.mjs';
 export function js_body_get(parsed) {
     arguments_assert(arguments, [js_node_is_program]);
+    if (false)
+        js_node_property_body;
     return object_property_get(parsed, 'body');
     metadata([]);
 }
