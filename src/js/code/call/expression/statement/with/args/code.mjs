8065094cc4d9969fe743e98ff6a537bada6f8523
JaredMathis <jared.mathis@native.consulting>
2023-06-15 10:12:03 -0400

    #93 function_auto js_code_call_expression_statement_with_args_code

--- src/js/code/call/expression/statement/with/args/code.mjs
+++ src/js/code/call/expression/statement/with/args/code.mjs
@@ -1,5 +1,5 @@
+import { js_code_statement } from '../../../../../statement.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
-import { js_statement_end } from '../../../../../../statement/end.mjs';
 import { js_code_call_expression_with_args_code } from '../../../with/args/code.mjs';
 import { string_is } from '../../../../../../../string/is.mjs';
 export function js_code_call_expression_statement_with_args_code(name, call_args) {
@@ -7,5 +7,5 @@ export function js_code_call_expression_statement_with_args_code(name, call_args
         string_is,
         string_is
     ]);
-    return `${ js_code_call_expression_with_args_code(name, call_args) }${ js_statement_end() }`;
+    return js_code_statement(js_code_call_expression_with_args_code(name, call_args));
 }
