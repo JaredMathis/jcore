9d6da36b410d2d9e41a0d784392d44590210fdc3
JaredMathis <jared.mathis@native.consulting>
2023-06-15 01:04:41 -0400

    #93 function_auto git_hub_repository_issues_post

--- src/git/hub/repository/issues/post.mjs
+++ src/git/hub/repository/issues/post.mjs
@@ -1,11 +1,10 @@
 import { git_hub_repository_issues_generic } from './generic.mjs';
-import { git_hub_page_size } from '../../page/size.mjs';
 import { object_merge } from '../../../../object/merge.mjs';
 import { defined_is } from '../../../../defined/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function git_hub_repository_issues_post(api_args_to_merge) {
     arguments_assert(arguments, [defined_is]);
-    let verb = 'GET';
-    object_merge({ per_page: git_hub_page_size() }, api_args_to_merge);
+    let verb = 'POST';
+    object_merge({ title }, api_args_to_merge);
     return await git_hub_repository_issues_generic(api_args_to_merge, verb);
 }
