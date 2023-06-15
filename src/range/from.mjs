2fc752546990d64f1af2a4256ef15cb6d538dc4d
JaredMathis <jared.mathis@native.consulting>
2023-06-15 15:28:02 -0400

    #93 function_auto range_from

--- src/range/from.mjs
+++ src/range/from.mjs
@@ -1,15 +1,15 @@
 import { add } from '../add.mjs';
-import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { range } from '../range.mjs';
 import { subtract } from '../subtract.mjs';
 import { assert } from '../assert.mjs';
 import { add_1 } from '../add/1.mjs';
 import { list_map } from '../list/map.mjs';
+import { integer_is } from '../integer/is.mjs';
 export function range_from(from, to) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        integer_is,
+        integer_is
     ]);
     assert(from <= to);
     let difference = subtract(from, to);
