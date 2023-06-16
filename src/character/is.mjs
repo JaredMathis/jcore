fe1b934bd669d0be85f5eb155f8ddddefad0d860
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:47:38 -0400

    #149 noop

--- src/character/is.mjs
+++ src/character/is.mjs
@@ -1,6 +1,7 @@
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function character_is() {
-    arguments_assert(arguments, []);
+import { defined_is } from '../defined/is.mjs';
+export function character_is(value) {
+    arguments_assert(arguments, [defined_is]);
     error('todo: character_is');
 }
