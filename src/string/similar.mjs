e5a7665c7e257fd058b5a42b974b0b7cce845bbf
JaredMathis <jared.mathis@native.consulting>
2023-06-16 15:00:01 -0400

    #149 function_rename string_similar_directed string_similar_remove_single

--- src/string/similar.mjs
+++ src/string/similar.mjs
@@ -1,6 +1,6 @@
 import { true_is } from '../true/is.mjs';
 import { string_is } from './is.mjs';
-import { string_similar_directed } from './similar/directed.mjs';
+import { string_similar_remove_single } from './similar/remove/single.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { list_any } from '../list/any.mjs';
 export function string_similar(a, b) {
@@ -9,8 +9,8 @@ export function string_similar(a, b) {
         string_is
     ]);
     let result = [
-        string_similar_directed(a, b),
-        string_similar_directed(b, a)
+        string_similar_remove_single(a, b),
+        string_similar_remove_single(b, a)
     ];
     return list_any(result, true_is);
 }
---
+++ src/string/similar/remove/single.mjs
@@ -0,0 +1,18 @@
+import { string_remove_at } from '../../remove/at.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+import { range } from '../../../range.mjs';
+import { equal } from '../../../equal.mjs';
+import { string_is } from '../../is.mjs';
+export function string_similar_remove_single(a, b) {
+    arguments_assert(arguments, [
+        string_is,
+        string_is
+    ]);
+    for (let i of range(a_length)) {
+        let other = string_remove_at(a, i);
+        if (equal(other, b)) {
+            return true;
+        }
+    }
+    return false;
+}
