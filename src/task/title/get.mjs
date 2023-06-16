56c55898824c746d799c6fa04eda16ea6506192b
JaredMathis <jared.mathis@native.consulting>
2023-06-16 18:48:21 -0400

    #153 noop

--- src/task/title/get.mjs
+++ src/task/title/get.mjs
@@ -1,8 +1,10 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { task_get } from '../get.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
+import { task_property_title } from '../property/title.mjs';
 export async function task_title_get(task_number_string) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let result = await task_get(task_number_string);
-    return result;
+    let task = await task_get(task_number_string);
+    return object_property_get(task, task_property_title());
 }
