f84014869c2fbbe2a59a1167d4c66b45cd29f31a
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:23:25 -0400

    #111 task_close 17

--- src/task/close.mjs
+++ src/task/close.mjs
@@ -8,12 +8,14 @@ import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { task_property_number } from './property/number.mjs';
 import { integer_parse } from '../integer/parse.mjs';
+import { task_open } from './open.mjs';
 export async function task_close(task_number_string) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let task_number = integer_parse(task_number_string);
-    let data = await git_hub_repository_issues_close(task_number);
+    await git_hub_repository_issues_close(task_number);
     await task_map(function map(tasks_all) {
         let task = list_find_property(tasks_all, task_property_number(), task_number);
         object_property_change(task, task_property_state(), task_property_closed());
     });
+    return await task_open();
 }
