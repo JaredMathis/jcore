0d576eceed2ddaa430885bfb0340df4f6e81087d
JaredMathis <jared.mathis@native.consulting>
2023-06-16 10:54:38 -0400

    #141 function_auto task_requires_get

---
+++ src/task/requires/get.mjs
@@ -0,0 +1,10 @@
+import { task_body_requires } from '../body/requires.mjs';
+import { task_body_parse } from '../body/parse.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+import { task_is } from '../is.mjs';
+export function task_requires_get(task) {
+    arguments_assert(arguments, [task_is]);
+    let body_parsed = task_body_parse(task);
+    let requires = task_body_requires(body_parsed);
+    return body_parsed;
+}
