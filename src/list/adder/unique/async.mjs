f570726e57b498638d03b3d14c77a357ae54d8b1
JaredMathis <jared.mathis@native.consulting>
2023-06-16 12:32:12 -0400

    #142 function_asyncify list_adder_unique

---
+++ src/list/adder/unique/async.mjs
@@ -0,0 +1,9 @@
+import { list_adder_generic } from '../generic.mjs';
+import { list_add_if_not_exists } from '../../add/if/not/exists.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export async function list_adder_unique_async(then) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    let lambda = list_add_if_not_exists;
+    return list_adder_generic(then, lambda);
+}
