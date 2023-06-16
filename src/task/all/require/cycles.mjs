a551424065f91715c8392c9b1e3b3a584f0fb0f1
JaredMathis <jared.mathis@native.consulting>
2023-06-16 12:03:31 -0400

    #142 function_auto task_all_require_cycles

--- src/task/all/require/cycles.mjs
+++ src/task/all/require/cycles.mjs
@@ -1,6 +1,8 @@
+import { visit_throws_not } from '../../../visit/throws/not.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { task_all_unsummarized } from '../unsummarized.mjs';
 export async function task_all_require_cycles() {
     arguments_assert(arguments, []);
     let all = await task_all_unsummarized();
+    visit_throws_not;
 }
