2a750e1378c286e83a1f8b955e0901702097ad87
JaredMathis <jared.mathis@native.consulting>
2023-06-16 15:01:06 -0400

    #149 function_auto function_callers

--- src/function/callers.mjs
+++ src/function/callers.mjs
@@ -1,3 +1,4 @@
+import { list_to_dictionary } from '../list/to/dictionary.mjs';
 import { function_dependencies_non_recursive } from './dependencies/non/recursive.mjs';
 import { list_any } from '../list/any.mjs';
 import { function_name_all } from './name/all.mjs';
@@ -5,6 +6,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { object_property_get } from '../object/property/get.mjs';
 import { list_add } from '../list/add.mjs';
+import { function_name_to_file_path } from './name/to/file/path.mjs';
 export async function function_callers(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let result = [];
@@ -15,5 +17,5 @@ export async function function_callers(function_name) {
             list_add(result, c);
         }
     }
-    return result;
+    return list_to_dictionary(result, function_name_to_file_path);
 }
