e84abb6326feaf4177595cd8b549af13e2f85470
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:06:30 -0400

    #98 function_auto task_later

--- src/task/later.mjs
+++ src/task/later.mjs
@@ -1,11 +1,17 @@
+import { list_find_property } from '../list/find/property.mjs';
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 import { list_add } from '../list/add.mjs';
 import { task_map } from './map.mjs';
 import { task_from_git_hub_issue } from './from/git/hub/issue.mjs';
 import { git_hub_repository_issues_post } from '../git/hub/repository/issues/post.mjs';
+import { assert } from '../assert.mjs';
 export async function task_later(title) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let data = await git_hub_repository_issues_post(title);
     let task = task_from_git_hub_issue(data);
     await task_map(function map(tasks_all) {
+        assert(list_find_property());
         list_add(tasks_all, task);
     });
     return task;
