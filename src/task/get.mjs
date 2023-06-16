728555290ccd4822c1ff9598fb5afa7c47ad926e
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:55:55 -0400

    #122 function_auto task_get

--- src/task/get.mjs
+++ src/task/get.mjs
@@ -1,8 +1,11 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 import { list_find_property } from '../list/find/property.mjs';
 import { task_property_number } from './property/number.mjs';
 import { task_all } from './all.mjs';
 import { integer_parse } from '../integer/parse.mjs';
 export async function task_get(task_number_string) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let task_number = integer_parse(task_number_string);
     let all = await task_all();
     const property_name = task_property_number();
