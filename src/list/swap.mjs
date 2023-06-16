1dbbd97097d211d7e8bed0c17729659926740f30
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:36:31 -0400

    #149 function_auto list_swap

--- src/list/swap.mjs
+++ src/list/swap.mjs
@@ -1,6 +1,15 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 import { list_set } from './set.mjs';
 import { list_get } from './get.mjs';
 export function list_swap(list, index_from, index_to) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let generic_get;
+    let generic_set;
     let from = list_get(list, index_from);
     let to = list_get(list, index_to);
     list_set(list, index_to, from);
