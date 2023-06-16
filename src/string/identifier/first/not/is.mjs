a6563448027eabdb66888130d907abda93b9e7a2
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:59:03 -0400

    #123 function_auto string_identifier_first_not_is

---
+++ src/string/identifier/first/not/is.mjs
@@ -0,0 +1,12 @@
+import { metadata } from '../../../../metadata.mjs';
+import { string_identifier_is_generic } from '../../is/generic.mjs';
+import { string_is } from '../../../is.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function string_identifier_first_not_is(value) {
+    arguments_assert(arguments, [string_is]);
+    let enforce_first = false;
+    let enforce_last = true;
+    const allow_underscores = true;
+    return string_identifier_is_generic(value, enforce_first, enforce_last, allow_underscores);
+    metadata([]);
+}
