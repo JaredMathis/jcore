eef8cfcf8f73f9a3c205d07a84b449fa89699084
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:57:13 -0400

    #122 function_auto task_get

--- src/task/get.mjs
+++ src/task/get.mjs
@@ -1,15 +1,9 @@
+import { task_get_generic } from './get/generic.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { list_find_property } from '../list/find/property.mjs';
-import { task_property_number } from './property/number.mjs';
-import { task_all } from './all.mjs';
-import { integer_parse } from '../integer/parse.mjs';
 export async function task_get(task_number_string) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let lambda = list_find_property;
-    let task_number = integer_parse(task_number_string);
-    let all = await task_all();
-    const property_name = task_property_number();
-    let result = lambda(all, property_name, task_number);
-    return result;
+    return await task_get_generic(task_number_string, lambda);
 }
---
+++ src/task/get/generic.mjs
@@ -0,0 +1,10 @@
+import { task_property_number } from '../property/number.mjs';
+import { task_all } from '../all.mjs';
+import { integer_parse } from '../../integer/parse.mjs';
+export async function task_get_generic(task_number_string, lambda) {
+    let task_number = integer_parse(task_number_string);
+    let all = await task_all();
+    const property_name = task_property_number();
+    let result = lambda(all, property_name, task_number);
+    return result;
+}
