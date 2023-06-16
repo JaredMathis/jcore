a5eaeea5cb3612d9ba228b3a345f894e268fe3e4
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:35:42 -0400

    #124 function_auto task_open_description_to_json_if_unparsable

--- src/task/open/description/to/json/if/unparsable.mjs
+++ src/task/open/description/to/json/if/unparsable.mjs
@@ -1,7 +1,9 @@
+import { task_body_map_json } from '../../../../../body/map/json.mjs';
+import { task_body_map_generic } from '../../../../../body/map/generic.mjs';
+import { log } from '../../../../../../log.mjs';
 import { json_invalid } from '../../../../../../json/invalid.mjs';
 import { string_to } from '../../../../../../string/to.mjs';
 import { list_new_then_async } from '../../../../../../list/new/then/async.mjs';
-import { task_description } from '../../../../../description.mjs';
 import { task_property_body } from '../../../../../property/body.mjs';
 import { object_property_get } from '../../../../../../object/property/get.mjs';
 import { task_open_get } from '../../../../get.mjs';
@@ -12,13 +14,15 @@ export async function task_open_description_to_json_if_unparsable() {
     arguments_assert(arguments, []);
     let open = await task_open_get();
     let result = await list_new_then_async(async list_add_then => {
-        for (let t of open) {
-            let task_body_value = object_property_get(t, task_property_body());
+        for (let task of open) {
+            let task_body_value = object_property_get(task, task_property_body());
             if (null_not_is(task_body_value)) {
                 if (json_invalid(task_body_value)) {
-                    let t_number = object_property_get(t, task_property_number());
+                    let t_number = object_property_get(task, task_property_number());
                     let t_number_string = string_to(t_number);
-                    await task_description(t_number_string, task_body_value);
+                    console.log({ task });
+                    await task_body_map_generic(t_number_string, current => null);
+                    await task_body_map_json(t_number_string, task_body_value);
                     list_add_then(t_number);
                 }
             }
