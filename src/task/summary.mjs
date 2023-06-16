bec9ecf10e11a7f5a215a97c6d03e838b7b13699
JaredMathis <jared.mathis@native.consulting>
2023-06-16 10:43:54 -0400

    #141 function_auto task_summary

--- src/task/summary.mjs
+++ src/task/summary.mjs
@@ -1,6 +1,13 @@
+import { string_comma } from '../string/comma.mjs';
+import { string_add } from '../string/add.mjs';
+import { js_code_parenthesis_surround } from '../js/code/parenthesis/surround.mjs';
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 import { task_property_title } from './property/title.mjs';
 import { task_property_number } from './property/number.mjs';
 import { object_property_get } from '../object/property/get.mjs';
+import { list_join } from '../list/join.mjs';
 export function task_summary(task) {
-    return `${ object_property_get(task, task_property_number()) } ${ object_property_get(task, task_property_title()) }`;
+    arguments_assert(arguments, [arguments_assert_todo]);
+    return `${ object_property_get(task, task_property_number()) } ${ js_code_parenthesis_surround(string_add(`requires: `, list_join(requies, string_comma()))) } ${ object_property_get(task, task_property_title()) }`;
 }
