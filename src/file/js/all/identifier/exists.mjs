fec0d78f3731a7f83252ec0b4fdf8c8fead4d272
JaredMathis <jared.mathis@native.consulting>
2023-06-16 12:34:29 -0400

    #142 function_auto file_js_all_identifier_exists

--- src/file/js/all/identifier/exists.mjs
+++ src/file/js/all/identifier/exists.mjs
@@ -2,15 +2,16 @@ import { file_js_all_identifiers_each } from '../identifiers/each.mjs';
 import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
-import { list_add_if_not_exists } from '../../../../list/add/if/not/exists.mjs';
 import { list_contains } from '../../../../list/contains.mjs';
+import { list_adder_unique_async } from '../../../../list/adder/unique/async.mjs';
 export async function file_js_all_identifier_exists(identifier_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    let result = [];
-    await file_js_all_identifiers_each(function lambda(identifiers) {
-        for (let i of identifiers) {
-            list_add_if_not_exists(result, i);
-        }
+    let result = list_adder_unique_async(async la => {
+        await file_js_all_identifiers_each(function lambda(identifiers) {
+            for (let i of identifiers) {
+                la(result, i);
+            }
+        });
     });
     return list_contains(result, identifier_name);
     metadata([]);
