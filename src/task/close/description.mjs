c40b82255466b98f06cd8d345127f42530c4e4d8
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:36:44 -0400

    #120 function_wrap_suffix_add task_close description

---
+++ src/task/close/description.mjs
@@ -0,0 +1,8 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { task_close } from '../close.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export async function task_close_description(task_number_string) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    let result = await task_close(task_number_string);
+    return result;
+}
