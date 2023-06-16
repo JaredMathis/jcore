5bbbdf119e98d178ca8c37edc25198e006a3d849
JaredMathis <jared.mathis@native.consulting>
2023-06-16 07:54:25 -0400

    #124 function_rename function_rename_suffix function_rename_suffix_change

--- src/assert.mjs
+++ src/assert.mjs
@@ -3,6 +3,6 @@ import { assert_arguments_count } from './assert/arguments/count.mjs';
 import { assert_message } from './assert/message.mjs';
 export function assert(value) {
     assert_arguments_count(arguments, 1);
-    assert_message(value, 'Assertion failed')
+    assert_message(value, 'Assertion failed');
     metadata([]);
 }
