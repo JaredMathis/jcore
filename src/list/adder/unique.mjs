5c6a5fa9cedfad5ba6be0f8cfe66c0e8165f2645
JaredMathis <jared.mathis@native.consulting>
2023-06-16 12:29:10 -0400

    #142 function_delete list_adder_unique

--- src/list/adder/unique.mjs
+++
@@ -1,8 +0,0 @@
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
-import { list_adder } from '../adder.mjs';
-import { arguments_assert } from '../../arguments/assert.mjs';
-export function list_adder_unique(then) {
-    arguments_assert(arguments, [arguments_assert_todo]);
-    let result = list_adder(then);
-    return result;
-}
