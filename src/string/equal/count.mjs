cb478319c7ba3af79abc929714eb410c4248885d
JaredMathis <jared.mathis@native.consulting>
2023-06-16 16:01:53 -0400

    #150 function_auto string_equal_count

---
+++ src/string/equal/count.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function string_equal_count() {
+    arguments_assert(arguments, []);
+    error('todo: string_equal_count');
+}
