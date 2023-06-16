2d7748ab4f7bab51c743e584a8292962232a74c9
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:41:36 -0400

    #149 function_auto list_swap_first_2

--- src/list/swap/first/2.mjs
+++ src/list/swap/first/2.mjs
@@ -1,11 +1,10 @@
 import { list_swap } from '../../swap.mjs';
-import { subtract_1 } from '../../../subtract/1.mjs';
-import { list_last_index } from '../../last/index.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { add_1 } from '../../../add/1.mjs';
 export function list_swap_first_2(list) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let last_index = list_last_index(list);
-    let last_index_second = subtract_1(last_index);
-    list_swap(list, last_index, last_index_second);
+    let first_index = 0;
+    let second_index = add_1(first_index);
+    list_swap(list, first_index, second_index);
 }
