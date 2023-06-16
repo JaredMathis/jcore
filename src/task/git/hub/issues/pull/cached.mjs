2d8506e2ed20bb90135e9576ba8ab4ab71365fcc
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:48:27 -0400

    #114 function_copy task_git_hub_issues_pull task_git_hub_issues_pull_cached

---
+++ src/task/git/hub/issues/pull/cached.mjs
@@ -0,0 +1,7 @@
+import { task_git_hub_issues_pull_generic } from './generic.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export async function task_git_hub_issues_pull_cached() {
+    arguments_assert(arguments, []);
+    const no_cache = true;
+    await task_git_hub_issues_pull_generic(no_cache);
+}
