57356ec65f83ad4e6d703d95a6f3e3222c4f74b2
JaredMathis <jared.mathis@native.consulting>
2023-06-16 16:14:44 -0400

    #151 function_auto function_wrap_replace

--- src/function/wrap/replace.mjs
+++ src/function/wrap/replace.mjs
@@ -1,3 +1,4 @@
+import { function_delete } from '../delete.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { function_wrap } from '../wrap.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -6,6 +7,7 @@ export async function function_wrap_replace(function_name_to_wrap, function_name
         string_identifier_is,
         string_identifier_is
     ]);
+    await function_delete(function_name_to_add);
     let result = await function_wrap(function_name_to_wrap, function_name_to_add);
     return result;
 }
