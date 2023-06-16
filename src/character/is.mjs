11d37e5ee1ebda92ceac5de842108d092160c519
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:47:25 -0400

    #149 function_auto character_is

---
+++ src/character/is.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function character_is() {
+    arguments_assert(arguments, []);
+    error('todo: character_is');
+}
