dfb2e6536288e4ea865ef6ccbac9e6132de98aaf
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:13:00 -0400

    #120 function_auto task_body

--- src/task/body.mjs
+++ src/task/body.mjs
@@ -1,14 +1,14 @@
 import { task_property_body } from './property/body.mjs';
 import { task_property_set_local } from './property/set/local.mjs';
-import { git_hub_repository_issue_title } from '../git/hub/repository/issue/title.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { git_hub_repository_issue_body } from '../git/hub/repository/issue/body.mjs';
 export async function task_body(issue_number, body) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let result = await git_hub_repository_issue_title(issue_number, body);
+    let result = await git_hub_repository_issue_body(issue_number, body);
     const property_name = task_property_body();
     await task_property_set_local(issue_number, property_name, body);
     return result;
