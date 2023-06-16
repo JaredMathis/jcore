6dff8908f05b71dbf7b67cf7bd5f2ac7aa94b1b6
JaredMathis <jared.mathis@native.consulting>
2023-06-16 09:05:39 -0400

    #123 function_auto task_open

--- src/task/open.mjs
+++ src/task/open.mjs
@@ -1,16 +1,12 @@
-import { task_open_get } from './open/get.mjs';
+import { task_open_generic } from './open/generic.mjs';
 import { list_find_property_exists } from '../list/find/property/exists.mjs';
 import { list_any } from '../list/any.mjs';
 import { task_body_property_requires } from './body/property/requires.mjs';
 import { task_property_body } from './property/body.mjs';
 import { null_is } from '../null/is.mjs';
-import { list_sort_property } from '../list/sort/property.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-import { list_map } from '../list/map.mjs';
 import { object_property_get } from '../object/property/get.mjs';
 import { task_property_number } from './property/number.mjs';
-import { task_property_title } from './property/title.mjs';
-import { list_filter } from '../list/filter.mjs';
 import { json_from } from '../json/from.mjs';
 import { object_property_exists } from '../object/property/exists.mjs';
 export async function task_open() {
@@ -34,14 +30,4 @@ export async function task_open() {
             return true;
         };
     }
-}
-
-async function task_open_generic(filter_get) {
-    let open = await task_open_get();
-    let filtered = list_filter(open, filter_get(open));
-    list_sort_property(filtered, task_property_number());
-    let summaries = list_map(filtered, function v_3(o) {
-        return `${object_property_get(o, task_property_number())} ${object_property_get(o, task_property_title())}`;
-    });
-    return summaries;
 }
---
+++ src/task/open/generic.mjs
@@ -0,0 +1,16 @@
+import { task_property_title } from '../property/title.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
+import { list_map } from '../../list/map.mjs';
+import { task_property_number } from '../property/number.mjs';
+import { list_sort_property } from '../../list/sort/property.mjs';
+import { list_filter } from '../../list/filter.mjs';
+import { task_open_get } from './get.mjs';
+export async function task_open_generic(filter_get) {
+    let open = await task_open_get();
+    let filtered = list_filter(open, filter_get(open));
+    list_sort_property(filtered, task_property_number());
+    let summaries = list_map(filtered, function v_3(o) {
+        return `${ object_property_get(o, task_property_number()) } ${ object_property_get(o, task_property_title()) }`;
+    });
+    return summaries;
+}
