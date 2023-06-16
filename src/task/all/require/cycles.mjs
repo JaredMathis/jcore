5cc2ec97eea5669b3381e1e81d3751c28ee9e2da
JaredMathis <jared.mathis@native.consulting>
2023-06-16 13:06:27 -0400

    #142 function_auto task_all_require_cycles

--- src/task/all/require/cycles.mjs
+++ src/task/all/require/cycles.mjs
@@ -1,7 +1,6 @@
+import { visit_lambda_none } from '../../../visit/lambda/none.mjs';
 import { log } from '../../../log.mjs';
-import { list_intersection } from '../../../list/intersection.mjs';
 import { list_map_property } from '../../../list/map/property.mjs';
-import { visit } from '../../../visit.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { task_all_unsummarized } from '../unsummarized.mjs';
 import { task_requires_get } from '../../requires/get.mjs';
@@ -13,7 +12,7 @@ export async function task_all_require_cycles() {
     let all = await task_all_unsummarized();
     let task_numbers = list_map_property(all, task_property_number());
     for (let t of task_numbers) {
-        visit(t, task_number => {
+        visit_lambda_none(t, task_number => {
             let task = list_find_property(all, task_property_number(), task_number);
             let task_numbers = task_requires_get(task);
             if (!list_length_is_0(task_numbers)) {
@@ -23,8 +22,6 @@ export async function task_all_require_cycles() {
                 });
             }
             return task_numbers;
-        }, v => {
-            let {node} = v;
         });
     }
 }
