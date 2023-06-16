99484b97839d9025782aa5546d4693483258eecf
JaredMathis <jared.mathis@native.consulting>
2023-06-16 12:58:38 -0400

    #142 function_auto task_all_require_cycles

--- src/task/all/require/cycles.mjs
+++ src/task/all/require/cycles.mjs
@@ -1,6 +1,5 @@
 import { list_map_property } from '../../../list/map/property.mjs';
 import { list_remove_until_empty } from '../../../list/remove/until/empty.mjs';
-import { task_current } from '../../current.mjs';
 import { visit } from '../../../visit.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { task_all_unsummarized } from '../unsummarized.mjs';
@@ -8,15 +7,17 @@ import { task_requires_get } from '../../requires/get.mjs';
 import { list_adder } from '../../../list/adder.mjs';
 import { list_first } from '../../../list/first.mjs';
 import { task_property_number } from '../../property/number.mjs';
+import { list_find_property } from '../../../list/find/property.mjs';
 export async function task_all_require_cycles() {
     arguments_assert(arguments, []);
     let all = await task_all_unsummarized();
     let remaining = list_map_property(all, task_property_number());
     list_remove_until_empty(remaining, removals_get);
     function removals_get() {
-        let task_current = list_first(remaining);
+        let task_number_current = list_first(remaining);
         let visited = list_adder(list_new_then_add => {
-            visit(task_current, task => {
+            visit(task_number_current, task_number => {
+                let task = list_find_property(all, task_property_number(), task_number);
                 let task_numbers = task_requires_get(task);
                 return task_numbers;
             }, v => {
