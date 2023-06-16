493aab82a55794c3654b5781b50c7e2b78134ecc
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:54:17 -0400

    #122 function_copy task_description task_requires

---
+++ src/task/requires.mjs
@@ -0,0 +1,17 @@
+import { task_body_property_description } from './body/property/description.mjs';
+import { object_property_set } from '../object/property/set.mjs';
+import { task_body_map } from './body/map.mjs';
+import { string_is } from '../string/is.mjs';
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export async function task_requires(issue_number, description) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        string_is
+    ]);
+    let result = await task_body_map(issue_number, map);
+    return result;
+    function map(body_parsed) {
+        object_property_set(body_parsed, task_body_property_description(), description);
+    }
+}
