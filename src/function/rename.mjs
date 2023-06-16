95aa84fe1a39cec973cc6453db9446592e6a9957
JaredMathis <jared.mathis@native.consulting>
2023-06-16 13:56:11 -0400

    #147 function_auto function_rename

--- src/function/rename.mjs
+++ src/function/rename.mjs
@@ -1,3 +1,4 @@
+import { function_name_starts_with } from './name/starts/with.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { function_rename_single } from './rename/single.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
@@ -6,6 +7,7 @@ export async function function_rename(function_name_old, function_name_new) {
         string_identifier_is,
         string_identifier_is
     ]);
+    let starting_withs = await function_name_starts_with(function_name_old);
     let result = await function_rename_single(function_name_old, function_name_new);
     return result;
 }
