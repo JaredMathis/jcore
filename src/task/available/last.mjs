9b9c09eed797677f8e6f832cca095ac74223ee6f
JaredMathis <jared.mathis@native.consulting>
2023-06-16 10:18:16 -0400

    #138 function_auto task_unsub

---
+++ src/task/available/last.mjs
@@ -0,0 +1,8 @@
+import { list_last } from '../../list/last.mjs';
+import { identity } from '../../identity.mjs';
+import { task_available_generic } from './generic.mjs';
+export async function task_available_last() {
+    let available = await task_available_generic(identity);
+    let last = list_last(available);
+    return last;
+}
--- src/task/unsub.mjs
+++ src/task/unsub.mjs
@@ -1,15 +1,12 @@
-import { task_available_generic } from './available/generic.mjs';
-import { list_last } from '../list/last.mjs';
+import { task_available_last } from './available/last.mjs';
 import { task_finish } from './finish.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { task_set } from './set.mjs';
 import { task_number_get_string } from './number/get/string.mjs';
-import { identity } from '../identity.mjs';
 export async function task_unsub() {
     arguments_assert(arguments, []);
     await task_finish();
-    let available = await task_available_generic(identity);
-    let last = list_last(available);
+    let last = await task_available_last();
     await task_set(task_number_get_string(last));
     return last;
 }
