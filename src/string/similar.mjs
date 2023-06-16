90cae6f8b31f23554da7e1e1fa1a4c97f1d978f7
JaredMathis <jared.mathis@native.consulting>
2023-06-16 15:05:18 -0400

    #149 function_auto string_similar

--- src/string/similar.mjs
+++ src/string/similar.mjs
@@ -3,15 +3,18 @@ import { string_is } from './is.mjs';
 import { string_similar_remove_single } from './similar/remove/single.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { list_any } from '../list/any.mjs';
+import { list_single_item } from '../list/single/item.mjs';
 export function string_similar(a, b) {
     arguments_assert(arguments, [
         string_is,
         string_is
     ]);
-    let fn = string_similar_remove_single
-    let result = [
-        fn(a, b),
-        fn(b, a)
-    ];
-    return list_any(result, true_is);
+    let fn = string_similar_remove_single;
+    for (let f of list_single_item(f)) {
+        let result = [
+            fn(a, b),
+            fn(b, a)
+        ];
+        return list_any(result, true_is);
+    }
 }
