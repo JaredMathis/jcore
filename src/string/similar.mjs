3ed10d2351ffc8ae0a542a07534700d9040acdb4
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:02:42 -0400

    #133 function_auto string_similar

--- src/string/similar.mjs
+++ src/string/similar.mjs
@@ -1,10 +1,14 @@
+import { string_remove_at } from './remove/at.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
-import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { range } from '../range.mjs';
+import { string_length } from './length.mjs';
 export function string_similar(a, b) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    error('todo: string_similar');
+    for (let i of range(string_length(a))) {
+        let other = string_remove_at(a, i);
+    }
 }
