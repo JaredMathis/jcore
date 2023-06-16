dfeb10a5bccc431eaab9caf9050564edd2f9b487
JaredMathis <jared.mathis@native.consulting>
2023-06-16 09:32:17 -0400

    #127 noop

--- src/json/map/empty/if/null.mjs
+++ src/json/map/empty/if/null.mjs
@@ -5,6 +5,8 @@ export function json_map_empty_if_null(task_body_value, map) {
     let body_before;
     if (null_is(task_body_value)) {
         body_before = js_brace_left_right();
+    } else {
+        body_before = task_body_value
     }
     let body_after = json_map(body_before, map);
     return body_after;
