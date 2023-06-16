a4acaa8645876c5106793b63aad5fc288c3b8f30
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:38:02 -0400

    #120 task_later when an async function is called, there should be a command that automatically adds await and makes the function async if needed and this should be part of auto as well

--- src/task/close/description.mjs
+++ src/task/close/description.mjs
@@ -1,8 +1,10 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { task_close } from '../close.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { task_description } from '../description.mjs';
 export async function task_close_description(task_number_string) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let result = await task_close(task_number_string);
+    await task_description(task_number_string, description)
     return result;
 }
