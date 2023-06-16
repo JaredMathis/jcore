99c00a1555a5c1d4518b3629dd37f553d74582d3
JaredMathis <jared.mathis@native.consulting>
2023-06-16 10:11:11 -0400

    #138 function_auto task_open_generic

--- src/task/open/generic.mjs
+++ src/task/open/generic.mjs
@@ -1,3 +1,5 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { task_property_title } from '../property/title.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 import { list_map } from '../../list/map.mjs';
@@ -6,11 +8,13 @@ import { list_sort_property } from '../../list/sort/property.mjs';
 import { list_filter } from '../../list/filter.mjs';
 import { task_open_get } from './get.mjs';
 export async function task_open_generic(filter_get) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let open = await task_open_get();
     let filtered = list_filter(open, filter_get(open));
     list_sort_property(filtered, task_property_number());
-    let summaries = list_map(filtered, function map(o) {
-        return `${ object_property_get(o, task_property_number()) } ${ object_property_get(o, task_property_title()) }`;
-    });
+    let summaries = list_map(filtered, map);
     return summaries;
+    function map(o) {
+        return `${ object_property_get(o, task_property_number()) } ${ object_property_get(o, task_property_title()) }`;
+    }
 }
