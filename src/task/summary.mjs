785f033e5be8a15bda6511a0c12de275e263e00e
JaredMathis <jared.mathis@native.consulting>
2023-06-16 10:42:02 -0400

    #141 noop

--- src/task/summary.mjs
+++ src/task/summary.mjs
@@ -1,6 +1,6 @@
 import { task_property_title } from './property/title.mjs';
 import { task_property_number } from './property/number.mjs';
 import { object_property_get } from '../object/property/get.mjs';
-export function task_summary(o) {
-    return `${ object_property_get(o, task_property_number()) } ${ object_property_get(o, task_property_title()) }`;
+export function task_summary(task) {
+    return `${ object_property_get(task, task_property_number()) } ${ object_property_get(task, task_property_title()) }`;
 }
