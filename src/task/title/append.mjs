22c871cabebbcb5b941b5129b5ae0dca091273ef
JaredMathis <jared.mathis@native.consulting>
2023-06-16 18:49:48 -0400

    #153 function_auto task_title_append

--- src/task/title/append.mjs
+++ src/task/title/append.mjs
@@ -8,7 +8,7 @@ export async function task_title_append(issue_number, title_additional) {
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let title_before = task_title_get(issue_number);
+    let title_before = await task_title_get(issue_number);
     let title_after = string_add(title_before, title_additional);
     let result = await task_title(issue_number, title_after);
     return result;
