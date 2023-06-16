ebb0029f8dcc7a00b3018ffb5a4fb7713e22aeac
JaredMathis <jared.mathis@native.consulting>
2023-06-16 07:41:50 -0400

    #124 noop

--- src/assert.mjs
+++ src/assert.mjs
@@ -1,11 +1,8 @@
 import { metadata } from './metadata.mjs';
-import { error } from './error.mjs';
 import { assert_arguments_count } from './assert/arguments/count.mjs';
+import { assert_message } from './assert/message.mjs';
 export function assert(value) {
     assert_arguments_count(arguments, 1);
-    if (value === true) {
-        return;
-    }
-    error('Assertion failed');
+    assert_message(value, 'Assertion failed')
     metadata([]);
 }
