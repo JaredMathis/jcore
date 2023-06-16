848a2f764362ed9de4cdefdd150b9b129476df83
JaredMathis <jared.mathis@native.consulting>
2023-06-16 10:10:51 -0400

    #138 noop

--- src/task/open/generic.mjs
+++ src/task/open/generic.mjs
@@ -9,7 +9,7 @@ export async function task_open_generic(filter_get) {
     let open = await task_open_get();
     let filtered = list_filter(open, filter_get(open));
     list_sort_property(filtered, task_property_number());
-    let summaries = list_map(filtered, function v_3(o) {
+    let summaries = list_map(filtered, function map(o) {
         return `${ object_property_get(o, task_property_number()) } ${ object_property_get(o, task_property_title()) }`;
     });
     return summaries;
--- src/task/unsub.mjs
+++ src/task/unsub.mjs
@@ -3,10 +3,12 @@ import { task_finish } from './finish.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { task_set } from './set.mjs';
 import { task_number_get_string } from './number/get/string.mjs';
+import { task_available } from './available.mjs';
 export async function task_unsub() {
     arguments_assert(arguments, []);
-    let available = await task_finish();
+    await task_finish();
+    let available = await task_available();
     let last = list_last(available);
     await task_set(task_number_get_string(last));
-    return available;
+    return last;
 }
