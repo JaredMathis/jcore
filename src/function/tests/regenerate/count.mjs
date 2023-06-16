33db941fde5a6aad0d52f7f8a7d8e028fbbdf9b1
JaredMathis <jared.mathis@native.consulting>
2023-06-16 16:38:14 -0400

    #151 function_auto function_tests_regenerate_count

--- src/function/tests/regenerate/count.mjs
+++ src/function/tests/regenerate/count.mjs
@@ -1,10 +1,11 @@
-import { function_tests_generate } from '../generate.mjs';
+import { count } from '../../../count.mjs';
 import { function_tests_generated_delete } from '../generated/delete.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_tests_regenerate_count(function_name) {
+import { function_tests_generate_count } from '../generate/count.mjs';
+export async function function_tests_regenerate_count(function_name, count) {
     arguments_assert(arguments, [string_identifier_is]);
     await function_tests_generated_delete(function_name);
-    let result = await function_tests_generate(function_name);
+    let result = await function_tests_generate_count(function_name, count);
     return result;
 }
