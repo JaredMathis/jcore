69dff5fce5c1751f5b0c0a7fb27b8bc9749ffd62
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:45:55 -0400

    #149 function_input_add string_set s

--- src/string/set.mjs
+++ src/string/set.mjs
@@ -1,6 +1,7 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function string_set() {
-    arguments_assert(arguments, []);
+export function string_set(s) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: string_set');
 }
