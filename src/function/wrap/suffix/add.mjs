d84f497be79be81196a2c59d8921c34428ab2b74
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:20:54 -0400

    #120 function_auto function_wrap_suffix_add

--- src/function/wrap/suffix/add.mjs
+++ src/function/wrap/suffix/add.mjs
@@ -2,14 +2,14 @@ import { string_identifier_parts_from } from '../../../string/identifier/parts/f
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { function_wrap } from '../../wrap.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_wrap_suffix_add(function_name_to_wrap, suffix) {
+export async function function_wrap_suffix_add(function_name_to_wrap, suffix_to_add) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is
     ]);
     let function_name_to_add = string_identifier_parts_from([
         function_name_to_wrap,
-        suffix
+        suffix_to_add
     ]);
     let result = await function_wrap(function_name_to_wrap, function_name_to_add);
     return result;
