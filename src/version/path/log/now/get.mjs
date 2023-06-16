ad8477e473b3196d0e0d61c436792c8df12a93f2
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:11:52 -0400

    #124 function_auto version_path_log_now_get

--- src/version/path/log/now/get.mjs
+++ src/version/path/log/now/get.mjs
@@ -1,3 +1,5 @@
+import { log } from '../../../../log.mjs';
+import { date_now_iso } from '../../../../date/now/iso.mjs';
 import { file_path_json } from '../../../../file/path/json.mjs';
 import { version_path_log_get } from '../get.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
@@ -5,5 +7,7 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function version_path_log_now_get(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let log_path = version_path_log_get(repository_name);
+    let iso = date_now_iso();
+    console.log({ iso });
     return file_path_json(log_path, 'all');
 }
