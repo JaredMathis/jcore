952eda9f6a9cd9a5144bfe474dc94f302999de73
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:38:49 -0400

    #120 function_input_add task_close_description description

--- src/task/close/description.mjs
+++ src/task/close/description.mjs
@@ -2,9 +2,12 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { task_close } from '../close.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { task_description } from '../description.mjs';
-export async function task_close_description(task_number_string) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+export async function task_close_description(task_number_string, description) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let result = await task_close(task_number_string);
-    await task_description(task_number_string, description)
+    await task_description(task_number_string, description);
     return result;
 }
