d06c7fe356dac1097b61cdc6ca267dc382c5bc11
JaredMathis <jared.mathis@native.consulting>
2023-06-16 07:40:50 -0400

    #124 function_rename assert_generic assert_message

--- src/assert/message.mjs
+++ src/assert/message.mjs
@@ -2,7 +2,7 @@ import { metadata } from '../metadata.mjs';
 import { error } from '../error.mjs';
 import { assert_arguments_count } from './arguments/count.mjs';
 import { string_is } from '../string/is.mjs';
-export function assert_generic(value, message) {
+export function assert_message(value, message) {
     assert_arguments_count(arguments, 2);
     if (!string_is(message)) {
         error('invalid message');
