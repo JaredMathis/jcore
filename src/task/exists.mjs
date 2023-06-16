53b411271995b5780d677a537844a3c63cb6ec91
JaredMathis <jared.mathis@native.consulting>
2023-06-16 07:00:18 -0400

    #122 function_auto task_exists

--- src/task/exists.mjs
+++ src/task/exists.mjs
@@ -1,9 +1,9 @@
+import { list_find_property_exists } from '../list/find/property/exists.mjs';
 import { task_get_generic } from './get/generic.mjs';
-import { list_find_property } from '../list/find/property.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 export async function task_exists(task_number_string) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let lambda = list_find_property;
+    let lambda = list_find_property_exists;
     return await task_get_generic(task_number_string, lambda);
 }
