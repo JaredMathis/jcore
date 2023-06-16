97ab6369ddcb67f128820e9e538f4ea4fdd384a3
JaredMathis <jared.mathis@native.consulting>
2023-06-16 07:47:13 -0400

    #124 function_auto throws

--- src/throws.mjs
+++ src/throws.mjs
@@ -1,4 +1,7 @@
+import { arguments_assert_todo } from './arguments/assert/todo.mjs';
+import { arguments_assert } from './arguments/assert.mjs';
 export function throws(lambda) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let errored = false;
     try {
         lambda();
