5035f8220be4c96a7cc3009690738b7213713c6d
JaredMathis <jared.mathis@native.consulting>
2023-06-15 21:01:22 -0400

    #115 function_auto task_finish

--- src/git/hub/repository/issues/close.mjs
+++ src/git/hub/repository/issues/close.mjs
@@ -6,5 +6,6 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function git_hub_repository_issues_close(issue_number) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let api_args_to_merge = { [task_property_state()]: task_property_closed() };
+    let no_cache = true;
     return git_hub_repository_issues_patch(git_hub_repository_issues_close, arguments, issue_number, api_args_to_merge, no_cache);
 }
--- src/git/hub/repository/issues/post.mjs
+++ src/git/hub/repository/issues/post.mjs
@@ -6,6 +6,7 @@ export async function git_hub_repository_issues_post(title) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let api_args_to_merge = {};
     let verb = 'POST';
+    let no_cache = true;
     object_merge({ title }, api_args_to_merge);
     return await git_hub_repository_issues_generic(git_hub_repository_issues_post, arguments, api_args_to_merge, verb, no_cache);
 }
