843bb1bae535f1fa0dcbea4ae381c90c67eabb6c
JaredMathis <jared.mathis@native.consulting>
2023-06-16 11:41:27 -0400

    #141 function_auto task_open_generic

--- src/task/open/generic.mjs
+++ src/task/open/generic.mjs
@@ -1,16 +1,16 @@
+import { task_open_all_unsummarized } from './all/unsummarized.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_map } from '../../list/map.mjs';
 import { task_property_number } from '../property/number.mjs';
 import { list_sort_property } from '../../list/sort/property.mjs';
 import { list_filter } from '../../list/filter.mjs';
-import { task_open_unsummarized } from './unsummarized.mjs';
 import { function_is } from '../../function/is.mjs';
 export async function task_open_generic(filter_get, map_with_all_unsummarized) {
     arguments_assert(arguments, [
         function_is,
         function_is
     ]);
-    let open = await task_open_unsummarized();
+    let {open, all} = await task_open_all_unsummarized();
     let filtered = list_filter(open, filter_get(open));
     list_sort_property(filtered, task_property_number());
     let summaries = list_map(filtered, map_with_all_unsummarized);
