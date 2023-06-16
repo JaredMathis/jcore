cbf61b635de3cf20f0b309d514a47f5829a04bb7
JaredMathis <jared.mathis@native.consulting>
2023-06-16 15:07:34 -0400

    #149 function_copy string_similar_remove_single string_similar_swap_adjascent

---
+++ src/string/similar/swap/adjascent.mjs
@@ -0,0 +1,19 @@
+import { equal } from '../../../equal.mjs';
+import { string_remove_at } from '../../remove/at.mjs';
+import { string_length } from '../../length.mjs';
+import { range } from '../../../range.mjs';
+import { string_is } from '../../is.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function string_similar_swap_adjascent(a, b) {
+    arguments_assert(arguments, [
+        string_is,
+        string_is
+    ]);
+    for (let i of range(string_length(a))) {
+        let other = string_remove_at(a, i);
+        if (equal(other, b)) {
+            return true;
+        }
+    }
+    return false;
+}
