8f59653f928f8e6f5c9014d66fa51c2b02421490
JaredMathis <jared.mathis@native.consulting>
2023-06-16 16:06:49 -0400

    #150 function_auto string_equal_count

--- src/string/equal/count.mjs
+++ src/string/equal/count.mjs
@@ -1,15 +1,15 @@
 import { count } from '../../count.mjs';
 import { number_min } from '../../number/min.mjs';
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { range } from '../../range.mjs';
 import { string_length } from '../length.mjs';
 import { string_get } from '../get.mjs';
 import { equal } from '../../equal.mjs';
+import { string_is } from '../is.mjs';
 export function string_equal_count(a, b) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        string_is,
+        string_is
     ]);
     return count(c => {
         let a_length = string_length(a);
