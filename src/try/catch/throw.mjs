c876d2789b02419f13ae3254d48281f28f211a19
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:17:41 -0400

    #98 function_unasyncify try_catch_throw_async

--- src/try/catch/throw.mjs
+++ src/try/catch/throw.mjs
@@ -1,14 +1,17 @@
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { metadata_generated } from '../../metadata/generated.mjs';
+import { metadata } from '../../metadata.mjs';
+import { function_is } from '../../function/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function try_catch_throw(lambda_try, lambda_catch) {
+export function try_catch_throw(lambda_try, lambda_catch) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        function_is,
+        function_is
     ]);
     try {
-        await lambda_try();
+        lambda_try();
     } catch (e) {
-        await lambda_catch(e);
+        lambda_catch(e);
         throw e;
     }
+    metadata([metadata_generated()]);
 }
