14ad26b3c423d80bd259e232fc07f8a4a3bbb098
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:07:35 -0400

    #98 function_auto list_find_property

--- src/list/find/property.mjs
+++ src/list/find/property.mjs
@@ -1,5 +1,17 @@
+import { list_find_generic } from './generic.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 import { list_find } from '../find.mjs';
 export function list_find_property(imports, property_name, removal_name) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    if (false) {
+        let or_null = false;
+        return list_find_generic(list, i => object_property_get(i, property_name) === removal_name, or_null);
+    }
     return list_find(imports, i => object_property_get(i, property_name) === removal_name);
 }
