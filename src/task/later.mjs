bed5ec7d74ae2e8ba128a9cbb07d99833600ede7
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:19:28 -0400

    #98 function_auto task_later

--- src/task/later.mjs
+++ src/task/later.mjs
@@ -1,3 +1,4 @@
+import { try_catch_throw } from '../try/catch/throw.mjs';
 import { list_find_property_exists } from '../list/find/property/exists.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
@@ -7,27 +8,19 @@ import { task_from_git_hub_issue } from './from/git/hub/issue.mjs';
 import { git_hub_repository_issues_post } from '../git/hub/repository/issues/post.mjs';
 import { assert } from '../assert.mjs';
 import { task_property_title } from './property/title.mjs';
+import { log } from '../log.mjs';
 export async function task_later(title) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let data = await git_hub_repository_issues_post(title);
     let task = task_from_git_hub_issue(data);
     await task_map(function map(tasks_all) {
-        
-        await try_catch_throw(async function lambda_try() {
-            for (let w of writes) {
-                const file_path = object_property_get(w, property_file_path);
-                const contents = object_property_get(w, property_contents);
-                await file_json_overwrite(file_path, contents);
-            }
-        }, async function lambda_catch(e) {
-            for (let w of writes) {
-                const file_path = object_property_get(w, property_file_path);
-                if (await file_exists(file_path)) {
-                    await file_delete(file_path);
-                }
-            }
+        const condition = !list_find_property_exists(tasks_all, task_property_title(), title);
+        let message = `duplicate task title`;
+        try_catch_throw(() => {
+            assert(condition);
+        }, () => {
+            log(message);
         });
-        assert(!list_find_property_exists(tasks_all, task_property_title(), title));
         list_add(tasks_all, task);
     });
     return task;
