49ec9a4df0958f8d21b7e614919c467c9521ce97
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:49:40 -0400

    #114 function_auto task_git_hub_issues_pull_cached_length

--- src/task/git/hub/issues/pull/cached/length.mjs
+++ src/task/git/hub/issues/pull/cached/length.mjs
@@ -1,7 +1,8 @@
 import { task_git_hub_issues_pull_cached } from '../cached.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
+import { list_length } from '../../../../../../list/length.mjs';
 export async function task_git_hub_issues_pull_cached_length() {
     arguments_assert(arguments, []);
     let result = await task_git_hub_issues_pull_cached();
-    return result;
+    return list_length(result);
 }
