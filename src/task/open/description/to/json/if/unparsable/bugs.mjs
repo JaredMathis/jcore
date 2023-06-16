adb348d38e83195295bf9abbc3e59473064c4a62
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:44:33 -0400

    #124 function_auto task_open_description_to_json_if_unparsable_bugs

--- src/task/open/description/to/json/if/unparsable/bugs.mjs
+++ src/task/open/description/to/json/if/unparsable/bugs.mjs
@@ -2,13 +2,19 @@ import { task_open_description_to_json_if_unparsable_generic } from './generic.m
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 export async function task_open_description_to_json_if_unparsable_bugs() {
     arguments_assert(arguments, []);
-    let bugs = [{
-            task: {
-                number: 62,
-                title: 'Maybe enforce naming convention of _async',
-                state: 'open',
-                body: 'If file ends in _async\r\n' + 'There must be a non-async version?\r\n' + '\r\n' + ' node run.mjs git function_unasyncify \r\n' + '\r\n' + 'Therefore  function_unasyncify  can safely remove _async suffix'
-            }
-        }];
+    let bugs = [
+        {
+            number: 62,
+            title: 'Maybe enforce naming convention of _async',
+            state: 'open',
+            body: 'If file ends in _async\r\n' + 'There must be a non-async version?\r\n' + '\r\n' + ' node run.mjs git function_unasyncify \r\n' + '\r\n' + 'Therefore  function_unasyncify  can safely remove _async suffix'
+        },
+        {
+            number: 76,
+            title: 'release function',
+            state: 'open',
+            body: 'release should run all tests, enforce all rules'
+        }
+    ];
     return await task_open_description_to_json_if_unparsable_generic(bugs);
 }
