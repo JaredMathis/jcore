d05490968adac95c23e27a2e01d7dcd1dc2e9593
JaredMathis <jared.mathis@native.consulting>
2023-06-16 07:07:59 -0400

    #122 function_auto task_requires

--- src/task/requires.mjs
+++ src/task/requires.mjs
@@ -1,3 +1,4 @@
+import { list_add_assert_exists_not } from '../list/add/assert/exists/not.mjs';
 import { integer_parse } from '../integer/parse.mjs';
 import { object_property_get } from '../object/property/get.mjs';
 import { task_body_property_requires } from './body/property/requires.mjs';
@@ -20,6 +21,6 @@ export async function task_requires(task_number_string, task_number_required_str
         object_property_initialize_if_unset(body_parsed, task_body_property_requires(), []);
         let requires = object_property_get(body_parsed, task_body_property_requires());
         let task_number_required = integer_parse(task_number_required_string);
-        list_add_assert_not_exists(requires, task_number_required);
+        list_add_assert_exists_not(requires, task_number_required);
     }
 }
