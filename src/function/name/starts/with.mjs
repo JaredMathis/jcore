497703632743a3e1f9d9efd662dc9885bc28f599
JaredMathis <jared.mathis@native.consulting>
2023-06-16 13:54:30 -0400

    #148 function_input_add_type function_name_starts_with prefix string_is

--- src/function/name/starts/with.mjs
+++ src/function/name/starts/with.mjs
@@ -1,7 +1,10 @@
+import { string_is } from '../../../string/is.mjs';
 import { function_name_all } from '../all.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_name_starts_with() {
-    arguments_assert(arguments, []);
+import { list_filter } from '../../../list/filter.mjs';
+import { string_starts_with } from '../../../string/starts/with.mjs';
+export async function function_name_starts_with(prefix) {
+    arguments_assert(arguments, [string_is]);
     let result = await function_name_all();
-    return result;
+    return list_filter(result, r => string_starts_with(prefix));
 }
