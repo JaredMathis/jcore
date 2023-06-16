ae7ac23e0194fc9b35bc84e6755335c8160f4399
JaredMathis <jared.mathis@native.consulting>
2023-06-16 13:36:04 -0400

    #145 function_auto function_tests_generated_delete

--- src/function/tests/generated/delete.mjs
+++ src/function/tests/generated/delete.mjs
@@ -4,7 +4,9 @@ import { function_delete } from '../../delete.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function function_tests_generated_delete(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    let generateds = function_tests_generated;
-    let result = await function_delete(function_name);
-    return result;
+    let generateds = function_tests_generated(function_name);
+    for (let g of generateds) {
+        await function_delete(g);
+    }
+    return generateds;
 }
