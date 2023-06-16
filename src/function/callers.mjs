ce1d9a621ae180d8585a5a9835fa68b3d5c5be71
JaredMathis <jared.mathis@native.consulting>
2023-06-16 15:01:40 -0400

    #149 function_auto function_callers

--- src/function/callers.mjs
+++ src/function/callers.mjs
@@ -1,21 +1,10 @@
+import { function_callers_names } from './callers/names.mjs';
 import { list_to_dictionary } from '../list/to/dictionary.mjs';
-import { function_dependencies_non_recursive } from './dependencies/non/recursive.mjs';
-import { list_any } from '../list/any.mjs';
-import { function_name_all } from './name/all.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
-import { object_property_get } from '../object/property/get.mjs';
-import { list_add } from '../list/add.mjs';
 import { function_name_to_file_path } from './name/to/file/path.mjs';
 export async function function_callers(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    let result = [];
-    let candidates = await function_name_all();
-    for (let c of candidates) {
-        let dependencies = await function_dependencies_non_recursive(c);
-        if (list_any(dependencies, d => object_property_get(d, 'function_name') === function_name)) {
-            list_add(result, c);
-        }
-    }
+    let result = await function_callers_names(function_name);
     return list_to_dictionary(result, function_name_to_file_path);
 }
---
+++ src/function/callers/names.mjs
@@ -0,0 +1,16 @@
+import { list_add } from '../../list/add.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
+import { list_any } from '../../list/any.mjs';
+import { function_dependencies_non_recursive } from '../dependencies/non/recursive.mjs';
+import { function_name_all } from '../name/all.mjs';
+export async function function_callers_names(function_name) {
+    let result = [];
+    let candidates = await function_name_all();
+    for (let c of candidates) {
+        let dependencies = await function_dependencies_non_recursive(c);
+        if (list_any(dependencies, d => object_property_get(d, 'function_name') === function_name)) {
+            list_add(result, c);
+        }
+    }
+    return result;
+}
