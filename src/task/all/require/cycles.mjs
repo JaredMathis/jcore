084c172c5e42400fad8e50f6927faf4c722e64ed
JaredMathis <jared.mathis@native.consulting>
2023-06-16 12:48:35 -0400

    #142 function_auto task_all_require_cycles

--- src/task/all/require/cycles.mjs
+++ src/task/all/require/cycles.mjs
@@ -1,3 +1,4 @@
+import { log } from '../../../log.mjs';
 import { task_current } from '../../current.mjs';
 import { visit } from '../../../visit.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -8,12 +9,14 @@ import { list_first } from '../../../list/first.mjs';
 export async function task_all_require_cycles() {
     arguments_assert(arguments, []);
     let all = await task_all_unsummarized();
-    let task_current = list_first(all);
+    let remaining = all;
+    let task_current = list_first(remaining);
     let visited = list_adder(list_new_then_add => {
         visit(task_current, task => task_requires_get(task), v => {
             let {node} = v;
             list_new_then_add(node);
         });
     });
-    console.log({visited})
+    remaining = list_without(all, visited);
+    console.log({ visited });
 }
