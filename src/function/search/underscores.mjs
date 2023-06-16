038dc879b17195f39d1390b16ddc95e030d20602
JaredMathis <jared.mathis@native.consulting>
2023-06-16 09:58:44 -0400

    #136 function_auto function_search_underscores

--- src/function/search/underscores.mjs
+++ src/function/search/underscores.mjs
@@ -1,13 +1,13 @@
+import { function_search_delimeter } from './delimeter.mjs';
 import { function_name_separator } from '../name/separator.mjs';
 import { string_is } from '../../string/is.mjs';
 import { function_search } from '../search.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_replace } from '../../string/replace.mjs';
-import { string_comma } from '../../string/comma.mjs';
 export async function function_search_underscores(query) {
     arguments_assert(arguments, [string_is]);
     let underscore = function_name_separator();
-    string_replace(query, underscore, string_comma());
+    string_replace(query, underscore, function_search_delimeter());
     let result = await function_search(query);
     return result;
 }
