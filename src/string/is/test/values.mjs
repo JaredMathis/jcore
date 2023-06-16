78498b7f8828e2edb8a257aace6e203a18de0105
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:53:24 -0400

    #149 function_auto string_is_test_values

--- src/string/is/test/values.mjs
+++ src/string/is/test/values.mjs
@@ -1,14 +1,16 @@
+import { character_is_test_values } from '../../../character/is/test/values.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_empty } from '../../empty.mjs';
 import { string_a } from '../../a.mjs';
 export function string_is_test_values() {
     arguments_assert(arguments, []);
     let aa = `${ string_a() }${ string_a() }`;
-    return [
-        string_empty(),
-        string_a(),
-        `b`,
-        aa,
-        `${ aa }b`
-    ];
+    return list_combine([
+        character_is_test_values(),
+        [
+            string_empty(),
+            aa,
+            `${ aa }b`
+        ]
+    ]);
 }
