cd3d335aed235a74358a132ab8826a362f4bb4bc
JaredMathis <jared.mathis@native.consulting>
2023-06-16 15:02:04 -0400

    #149 function_auto_multiple function_callers_arguments_assert_auto,refactor_input_generic

--- src/function/callers/arguments/assert/auto.mjs
+++ src/function/callers/arguments/assert/auto.mjs
@@ -1,13 +1,13 @@
+import { function_callers_names } from '../../names.mjs';
 import { function_callers_arguments_assert_auto_generic } from './auto/generic.mjs';
 import { function_to_statement_arguments_assert_args_predicate } from '../../../to/statement/arguments/assert/args/predicate.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { function_callers } from '../../../callers.mjs';
 import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 export async function function_callers_arguments_assert_auto(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let result = [];
     let arguments_assert_args = await function_to_statement_arguments_assert_args_predicate(function_name);
-    let callers = await function_callers(function_name);
+    let callers = await function_callers_names(function_name);
     for (let c_function_name of callers) {
         await function_callers_arguments_assert_auto_generic(c_function_name, function_name, arguments_assert_args, result);
     }
