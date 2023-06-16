3bd5011f240ae93a13d4264d5c7e50c24f422701
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:20:36 -0400

    #120 function_auto function_wrap_suffix_change

--- src/function/wrap/suffix/change.mjs
+++ src/function/wrap/suffix/change.mjs
@@ -1,14 +1,15 @@
+import { string_suffix_replace } from '../../../string/suffix/replace.mjs';
+import { string_identifier_part_is } from '../../../string/identifier/part/is.mjs';
 import { function_wrap } from '../../wrap.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { string_suffix_without } from '../../../string/suffix/without.mjs';
 export async function function_wrap_suffix_change(function_name_to_wrap, suffix_before, suffix_after) {
     arguments_assert(arguments, [
         string_identifier_is,
-        string_identifier_is,
-        string_identifier_is
+        string_identifier_part_is,
+        string_identifier_part_is
     ]);
-    string_suffix_without();
+    let function_name_to_add = string_suffix_replace(function_name_to_wrap, suffix_before, suffix_after);
     let result = await function_wrap(function_name_to_wrap, function_name_to_add);
     return result;
 }
