f10c9da46818059ef334381b5d25a901cfdbd88c
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:46:10 -0400

    #149 function_input_add string_set value

--- src/string/set.mjs
+++ src/string/set.mjs
@@ -1,8 +1,9 @@
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function string_set(s, index) {
+export function string_set(s, index, value) {
     arguments_assert(arguments, [
+        arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
