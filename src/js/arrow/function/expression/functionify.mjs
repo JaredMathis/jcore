6a483fcdf64a9b7788edcdfb9344f1a894a656b0
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:55:33 -0400

    #123 function_auto js_arrow_function_expression_functionify

--- src/js/arrow/function/expression/functionify.mjs
+++ src/js/arrow/function/expression/functionify.mjs
@@ -1,4 +1,4 @@
-import { js_node_property_arguments } from '../../../node/property/arguments.mjs';
+import { js_node_property_body } from '../../../node/property/body.mjs';
 import { object_replace } from '../../../../object/replace.mjs';
 import { js_arrow_function_expression_body } from './body.mjs';
 import { js_return_statement_argument_change } from '../../../return/statement/argument/change.mjs';
@@ -20,6 +20,7 @@ import { list_add } from '../../../../list/add.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
 import { object_property_change } from '../../../../object/property/change.mjs';
 import { js_node_property_params } from '../../../node/property/params.mjs';
+import { js_node_is_block_statement } from '../../../node/is/block/statement.mjs';
 export function js_arrow_function_expression_functionify(node, name) {
     arguments_assert(arguments, [
         js_node_is_arrow_function_expression,
@@ -30,11 +31,15 @@ export function js_arrow_function_expression_functionify(node, name) {
     let function_expression = js_parse_expression(function_code);
     let return_statement_code = js_code_return_statement(string_a());
     let return_statement = js_parse_statement(return_statement_code);
-    comment(`If this fails, code needs to handle`);
-    assert(js_node_is_call_expression(body));
-    js_return_statement_argument_change(return_statement, body);
-    let statements = js_function_declaration_to_statements(function_expression);
-    list_add(statements, return_statement);
+    if (js_node_is_block_statement(body)) {
+        object_property_change(function_expression, js_node_property_body(), body);
+    } else {
+        comment(`If this fails, code needs to handle`);
+        assert(js_node_is_call_expression(body));
+        js_return_statement_argument_change(return_statement, body);
+        let statements = js_function_declaration_to_statements(function_expression);
+        list_add(statements, return_statement);
+    }
     let args = object_property_get(node, js_node_property_params());
     object_property_change(function_expression, js_node_property_params(), args);
     object_replace(node, function_expression);
