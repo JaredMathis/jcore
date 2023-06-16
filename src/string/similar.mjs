5052f86e5884f9ed0b493c60948a33211ded812d
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:00:53 -0400

    #133 function_auto string_similar

---
+++ src/string/similar.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function string_similar() {
+    arguments_assert(arguments, []);
+    error('todo: string_similar');
+}
