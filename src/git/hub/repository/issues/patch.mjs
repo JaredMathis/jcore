5f0e452cda73125f50497c2a871db4b140555f20
JaredMathis <jared.mathis@native.consulting>
2023-06-15 21:00:02 -0400

    #114 function_input_add_type_default_identifier git_hub_repository_issues_patch no_cache boolean_is

--- src/git/hub/repository/issues/patch.mjs
+++ src/git/hub/repository/issues/patch.mjs
@@ -1,13 +1,15 @@
+import { boolean_is } from '../../../../boolean/is.mjs';
 import { git_hub_repository_issue_generic } from '../issue/generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { object_merge } from '../../../../object/merge.mjs';
-export async function git_hub_repository_issues_patch(fn, args, issue_number, api_args_to_merge) {
+export async function git_hub_repository_issues_patch(fn, args, issue_number, api_args_to_merge, no_cache) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo,
-        arguments_assert_todo
+        arguments_assert_todo,
+        boolean_is
     ]);
     object_merge({ issue_number }, api_args_to_merge);
     let verb = 'PATCH';
