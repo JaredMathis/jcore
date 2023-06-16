940c78eebf48290395087def95bdb82651c803a5
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:41:45 -0400

    #149 function_auto refactor_input_swap_first_2

--- src/refactor/input/swap/first/2.mjs
+++ src/refactor/input/swap/first/2.mjs
@@ -1,5 +1,5 @@
+import { list_swap_first_2 } from '../../../../list/swap/first/2.mjs';
 import { refactor_input_generic_simple } from '../../generic/simple.mjs';
-import { list_swap_last_2 } from '../../../../list/swap/last/2.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function refactor_input_swap_first_2(args) {
