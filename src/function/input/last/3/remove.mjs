0141df8d322a4ae693f37b079f3fca11d3ca60ff
JaredMathis <jared.mathis@native.consulting>
2023-06-16 12:43:42 -0400

    #142 function_auto function_input_last_3_remove

--- src/function/input/last/3/remove.mjs
+++ src/function/input/last/3/remove.mjs
@@ -1,3 +1,4 @@
+import { function_input_remove_last } from '../../remove/last.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { function_input_last_remove } from '../remove.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
@@ -5,6 +6,6 @@ import { range } from '../../../../range.mjs';
 export async function function_input_last_3_remove(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     for (let i of range(3)) {
-        await function_input_last_remove(function_name);
+        await function_input_remove_last(function_name);
     }
 }
--- src/function/input/remove/last.mjs
+++ src/function/input/remove/last.mjs
@@ -3,7 +3,7 @@ import { function_name_get } from '../../name/get.mjs';
 import { function_map_with_args } from '../../map/with/args.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_input_last_remove(function_name) {
+export async function function_input_remove_last(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     await function_map_with_args(function_name_get(refactor_input_last_remove), function_name, {});
 }
