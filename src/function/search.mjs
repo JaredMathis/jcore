28e99b7f0e69b35b793ae3ce3f077069cf0040d2
JaredMathis <jared.mathis@native.consulting>
2023-06-16 09:58:09 -0400

    #136 function_auto function_search

--- src/function/search.mjs
+++ src/function/search.mjs
@@ -1,4 +1,4 @@
-import { string_comma } from '../string/comma.mjs';
+import { function_search_delimeter } from './search/delimeter.mjs';
 import { string_identifier_sub_multiple_parse } from '../string/identifier/sub/multiple/parse.mjs';
 import { function_name_all_tests_not } from './name/all/tests/not.mjs';
 import { list_filter } from '../list/filter.mjs';
@@ -10,7 +10,7 @@ import { list_to_dictionary } from '../list/to/dictionary.mjs';
 import { function_name_to_file_path } from './name/to/file/path.mjs';
 export async function function_search(query) {
     arguments_assert(arguments, [string_is]);
-    let parts = string_identifier_sub_multiple_parse(query, string_comma());
+    let parts = string_identifier_sub_multiple_parse(query, function_search_delimeter());
     const all = await function_name_all_tests_not();
     let function_names = list_filter(all, a => {
         return list_all(parts, p => string_includes(a, p));
---
+++ src/function/search/delimeter.mjs
@@ -0,0 +1,4 @@
+import { string_comma } from '../../string/comma.mjs';
+export function function_search_delimeter() {
+    return string_comma();
+}
