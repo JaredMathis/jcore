c843f44686ffb0f9ca74ab1fff49388157961620
JaredMathis <jared.mathis@native.consulting>
2023-06-16 10:03:35 -0400

    #137 function_auto task_unsub

--- src/task/unsub.mjs
+++ src/task/unsub.mjs
@@ -1,9 +1,9 @@
+import { list_last } from '../list/last.mjs';
 import { task_finish } from './finish.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-import { task_current } from './current.mjs';
 export async function task_unsub() {
     arguments_assert(arguments, []);
-    let current = await task_current();
     let available = await task_finish();
+    let last = list_last(available);
     return available;
 }
