42de0ffd88bc1574458e56fa0d507c82e187f8b4
JaredMathis <jared.mathis@native.consulting>
2023-06-16 10:56:35 -0400

    #141 noop

--- src/task/requires/get.mjs
+++ src/task/requires/get.mjs
@@ -6,5 +6,5 @@ export function task_requires_get(task) {
     arguments_assert(arguments, [task_is]);
     let body_parsed = task_body_parse(task);
     let requires = task_body_requires(body_parsed);
-    return body_parsed;
+    return requires;
 }
