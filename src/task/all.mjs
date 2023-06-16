9114ce0354e6e86f68d8be5a8486da3a496485c1
JaredMathis <jared.mathis@native.consulting>
2023-06-16 11:25:51 -0400

    #141 function_auto task_all

--- src/task/all.mjs
+++ src/task/all.mjs
@@ -4,7 +4,7 @@ import { task_all_unsummarized } from './all/unsummarized.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 export async function task_all() {
     arguments_assert(arguments, []);
-    let result = await task_all_unsummarized();
-    let summaries = list_map(filtered, task_summary);
+    let all = await task_all_unsummarized();
+    let summaries = list_map(all, task_summary);
     return summaries;
 }
