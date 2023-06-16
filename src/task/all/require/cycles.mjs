305e4ad3925cf0c62661e56bf68455791f4c4865
JaredMathis <jared.mathis@native.consulting>
2023-06-16 12:01:28 -0400

    #142 function_auto task_all_require_cycles

---
+++ src/task/all/require/cycles.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function task_all_require_cycles() {
+    arguments_assert(arguments, []);
+    error('todo: task_all_require_cycles');
+}
