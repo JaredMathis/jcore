8506dcd57a18827458070748f831b1439e54f229
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:44:21 -0400

    #124 function_auto task_open_description_to_json_if_unparsable_bugs

--- src/task/open/description/to/json/if/unparsable/bugs.mjs
+++ src/task/open/description/to/json/if/unparsable/bugs.mjs
@@ -1,8 +1,14 @@
 import { task_open_description_to_json_if_unparsable_generic } from './generic.mjs';
-import { task_open_get } from '../../../../../get.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 export async function task_open_description_to_json_if_unparsable_bugs() {
     arguments_assert(arguments, []);
-    let open = await task_open_get();
-    return await task_open_description_to_json_if_unparsable_generic(open);
+    let bugs = [{
+            task: {
+                number: 62,
+                title: 'Maybe enforce naming convention of _async',
+                state: 'open',
+                body: 'If file ends in _async\r\n' + 'There must be a non-async version?\r\n' + '\r\n' + ' node run.mjs git function_unasyncify \r\n' + '\r\n' + 'Therefore  function_unasyncify  can safely remove _async suffix'
+            }
+        }];
+    return await task_open_description_to_json_if_unparsable_generic(bugs);
 }
