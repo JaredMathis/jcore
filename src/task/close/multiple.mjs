5288b4351207f8c968ae5bcb4bf245b32c2da46d
JaredMathis <jared.mathis@native.consulting>
2023-06-15 22:29:13 -0400

    #118 task_close_multiple 109,100

--- src/task/close/multiple.mjs
+++ src/task/close/multiple.mjs
@@ -1,8 +1,9 @@
 import { string_split_comma } from '../../string/split/comma.mjs';
 import { task_close } from '../close.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { string_is } from '../../string/is.mjs';
 export async function task_close_multiple(task_number_list) {
-    arguments_assert(arguments, []);
+    arguments_assert(arguments, [string_is]);
     let task_numbers = string_split_comma(task_number_list);
     for (let task_number of task_numbers) {
         await task_close(task_number);
