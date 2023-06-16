5733691e6ce5b4ae3436435b59fa3c1532ecedd3
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:43:03 -0400

    #124 function_copy_suffix_add task_open_description_to_json_if_unparsable bugs

---
+++ src/task/open/description/to/json/if/unparsable/bugs.mjs
@@ -0,0 +1,8 @@
+import { task_open_description_to_json_if_unparsable_generic } from './generic.mjs';
+import { task_open_get } from '../../../../../get.mjs';
+import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
+export async function task_open_description_to_json_if_unparsable_bugs() {
+    arguments_assert(arguments, []);
+    let open = await task_open_get();
+    return await task_open_description_to_json_if_unparsable_generic(open);
+}
