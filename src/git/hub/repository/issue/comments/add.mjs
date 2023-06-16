3070e5fec758dcb3adf734b4759ab596a28db144
JaredMathis <jared.mathis@native.consulting>
2023-06-15 22:53:17 -0400

    #119 function_auto git_hub_repository_issue_comments_add

--- src/git/hub/repository/issue/comments/add.mjs
+++ src/git/hub/repository/issue/comments/add.mjs
@@ -2,11 +2,11 @@ import { git_hub_repository_issues_generic } from '../../issues/generic.mjs';
 import { object_merge } from '../../../../../object/merge.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export async function git_hub_repository_issue_comments_add(title) {
+export async function git_hub_repository_issue_comments_add(body) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let api_args_to_merge = {};
     let verb = 'POST';
     let no_cache = true;
-    object_merge({ title }, api_args_to_merge);
+    object_merge({ title: body }, api_args_to_merge);
     return await git_hub_repository_issues_generic(git_hub_repository_issue_comments_add, arguments, api_args_to_merge, verb, no_cache);
 }
