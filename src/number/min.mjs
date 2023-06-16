d6146ff94e151b97ed046571ecdd10bb8b061dcc
JaredMathis <jared.mathis@native.consulting>
2023-06-16 16:04:22 -0400

    #150 function_auto number_min

--- src/number/min.mjs
+++ src/number/min.mjs
@@ -1,10 +1,9 @@
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
-import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 export function number_min(a, b) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    error('todo: number_min');
+    return Math.min(a, b);
 }
