c55f69ea61f19e8cc10251480c497f74c02c2e3d
JaredMathis <jared.mathis@native.consulting>
2023-06-16 09:56:39 -0400

    #136 function_auto string_identifier_sub_multiple_parse

--- src/string/identifier/sub/multiple/parse.mjs
+++ src/string/identifier/sub/multiple/parse.mjs
@@ -1,12 +1,13 @@
+import { string_comma } from '../../../comma.mjs';
+import { string_split } from '../../../split.mjs';
 import { string_identifier_sub_is } from '../is.mjs';
 import { list_all } from '../../../../list/all.mjs';
 import { assert } from '../../../../assert.mjs';
-import { string_split_comma } from '../../../split/comma.mjs';
 import { string_is } from '../../../is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function string_identifier_sub_multiple_parse(values) {
     arguments_assert(arguments, [string_is]);
-    let result = string_split_comma(values);
+    let result = string_split(values, string_comma());
     assert(list_all(result, string_identifier_sub_is));
     return result;
 }
