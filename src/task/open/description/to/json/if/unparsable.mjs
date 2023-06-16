7ece7fbc431e11c69c6106a65aa1b77759ebbbf9
JaredMathis <jared.mathis@native.consulting>
2023-06-16 07:27:11 -0400

    #124 function_auto task_open_description_to_json_if_unparsable

--- src/task/open/description/to/json/if/unparsable.mjs
+++ src/task/open/description/to/json/if/unparsable.mjs
@@ -2,6 +2,8 @@ import { task_open_get } from '../../../../get.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
 export async function task_open_description_to_json_if_unparsable() {
     arguments_assert(arguments, []);
-    let result = await task_open_get();
-    return result;
+    let open = await task_open_get();
+    for (let o of open) {
+    }
+    return open;
 }
