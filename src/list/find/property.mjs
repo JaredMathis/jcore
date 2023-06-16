c0a079bbc21d6f76864023c6e062f7640c25baa1
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:08:48 -0400

    #98 function_auto list_find_property

--- src/list/find/property.mjs
+++ src/list/find/property.mjs
@@ -1,7 +1,6 @@
-import { list_find_generic } from './generic.mjs';
+import { list_find_property_generic } from './property/generic.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { object_property_get } from '../../object/property/get.mjs';
 export function list_find_property(list, property_name, property_value) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -9,5 +8,5 @@ export function list_find_property(list, property_name, property_value) {
         arguments_assert_todo
     ]);
     let or_null = false;
-    return list_find_generic(list, i => object_property_get(i, property_name) === property_value, or_null);
+    return list_find_property_generic(list, property_name, property_value, or_null);
 }
---
+++ src/list/find/property/generic.mjs
@@ -0,0 +1,5 @@
+import { object_property_get } from '../../../object/property/get.mjs';
+import { list_find_generic } from '../generic.mjs';
+export function list_find_property_generic(list, property_name, property_value, or_null) {
+    return list_find_generic(list, i => object_property_get(i, property_name) === property_value, or_null);
+}
