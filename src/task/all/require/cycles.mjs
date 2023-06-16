ab6a7e5b01610d7b19932fd29f84367f4c965703
JaredMathis <jared.mathis@native.consulting>
2023-06-16 12:57:53 -0400

    #142 function_auto task_all_require_cycles

--- src/task/all/require/cycles.mjs
+++ src/task/all/require/cycles.mjs
@@ -1,3 +1,4 @@
+import { list_map_property } from '../../../list/map/property.mjs';
 import { list_remove_until_empty } from '../../../list/remove/until/empty.mjs';
 import { task_current } from '../../current.mjs';
 import { visit } from '../../../visit.mjs';
@@ -6,9 +7,11 @@ import { task_all_unsummarized } from '../unsummarized.mjs';
 import { task_requires_get } from '../../requires/get.mjs';
 import { list_adder } from '../../../list/adder.mjs';
 import { list_first } from '../../../list/first.mjs';
+import { task_property_number } from '../../property/number.mjs';
 export async function task_all_require_cycles() {
     arguments_assert(arguments, []);
-    let remaining = await task_all_unsummarized();
+    let all = await task_all_unsummarized();
+    let remaining = list_map_property(all, task_property_number());
     list_remove_until_empty(remaining, removals_get);
     function removals_get() {
         let task_current = list_first(remaining);
