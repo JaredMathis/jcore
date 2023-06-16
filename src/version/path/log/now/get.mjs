cec97b69f06d84ec8b702ab9d8fef0d30662add5
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:07:34 -0400

    #124 function_auto version_path_log_now_get

--- src/version/path/log/now/get.mjs
+++ src/version/path/log/now/get.mjs
@@ -1,6 +1,5 @@
+import { file_name_json } from '../../../../file/name/json.mjs';
 import { version_path_log_get } from '../get.mjs';
-import { file_extension_json } from '../../../../file/extension/json.mjs';
-import { string_add } from '../../../../string/add.mjs';
 import { path_join } from '../../../../path/join.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
@@ -9,7 +8,7 @@ export function version_path_log_now_get(repository_name) {
     let log_path = version_path_log_get(repository_name);
     let tasks_all_path = path_join([
         log_path,
-        string_add('all', file_extension_json())
+        file_name_json('all')
     ]);
     return tasks_all_path;
 }
