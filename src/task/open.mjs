ea94596afa25cfd83a61a33c1c651ecd2bfe8673
JaredMathis <jared.mathis@native.consulting>
2023-06-16 10:15:28 -0400

    #138 function_auto task_open

--- src/task/open.mjs
+++ src/task/open.mjs
@@ -1,8 +1,9 @@
+import { task_summary } from './summary.mjs';
 import { lambda_get } from '../lambda/get.mjs';
 import { task_open_generic } from './open/generic.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { tautology } from '../tautology.mjs';
 export async function task_open() {
     arguments_assert(arguments, []);
-    return await task_open_generic(lambda_get(tautology), map);
+    return await task_open_generic(lambda_get(tautology), task_summary);
 }
