3e925edafa0db551d1f3da9882aa9fc4e2c7c69b
JaredMathis <jared.mathis@native.consulting>
2023-06-16 16:06:25 -0400

    #150 function_auto string_equal_count

--- src/string/equal/count.mjs
+++ src/string/equal/count.mjs
@@ -5,17 +5,21 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { range } from '../../range.mjs';
 import { string_length } from '../length.mjs';
 import { string_get } from '../get.mjs';
+import { equal } from '../../equal.mjs';
 export function string_equal_count(a, b) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    count(c => {
+    return count(c => {
+        let a_length = string_length(a);
+        let b_length = string_length(b);
+        for (let i of range(number_min(a_length, b_length))) {
+            let a_i = string_get(a, i);
+            let b_i = string_get(b, i);
+            if (equal(a_i), equal(b_i)) {
+                c();
+            }
+        }
     });
-    let a_length = string_length(a);
-    let b_length = string_length(b);
-    for (let i of range(number_min(a_length, b_length))) {
-        let a_i = string_get(a, i);
-        let b_i = string_get(b, i);
-    }
 }
