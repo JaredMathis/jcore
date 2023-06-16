94c8606e37e6f679e3b7432d538af5f8244d1f33
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:09:06 -0400

    #98 function_copy list_find_property list_find_property_or_null

---
+++ src/list/find/property/or/null.mjs
@@ -0,0 +1,12 @@
+import { list_find_property_generic } from '../generic.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function list_find_property_or_null(list, property_name, property_value) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let or_null = false;
+    return list_find_property_generic(list, property_name, property_value, or_null);
+}
