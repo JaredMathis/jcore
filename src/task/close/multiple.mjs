484f0ec85f2c1863c2376e7a81c61b836b1ae10c
JaredMathis <jared.mathis@native.consulting>
2023-06-15 22:27:16 -0400

    #118 function_search string,from

--- src/task/close/multiple.mjs
+++ src/task/close/multiple.mjs
@@ -1,6 +1,6 @@
 import { task_close } from '../close.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function task_close_multiple() {
+export function task_close_multiple(task_number_list) {
     arguments_assert(arguments, []);
-    task_close;
+    task_close(task_number)
 }
