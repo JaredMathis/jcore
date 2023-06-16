52c3c6ed9adf6b924758ad1e1ace1318aa0fedde
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:14:53 -0400

    #120 function_auto task_description

--- src/task/body/description.mjs
+++ src/task/body/description.mjs
@@ -1,7 +1,7 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { task_body } from '../body.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function task_body_description(issue_number, body) {
+export async function task_body_description(issue_number, description) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
