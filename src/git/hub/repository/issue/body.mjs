b74088ca4d1fa0152306a15d3d6a056dabf19fbd
JaredMathis <jared.mathis@native.consulting>
2023-06-15 21:29:31 -0400

    #117 function_add_string_prefix task_property body

--- src/git/hub/repository/issue/body.mjs
+++ src/git/hub/repository/issue/body.mjs
@@ -3,12 +3,12 @@ import { git_hub_repository_issues_patch } from '../issues/patch.mjs';
 import { task_property_title } from '../../../../task/property/title.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function git_hub_repository_issue_body(issue_number, title) {
+export async function git_hub_repository_issue_body(issue_number, body) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let api_args_to_merge = { [task_property_title()]: title };
+    let api_args_to_merge = { [task_property_title()]: body };
     let no_cache = true;
-    return git_hub_repository_issues_patch(git_hub_repository_issue_title, arguments, issue_number, api_args_to_merge, no_cache);
+    return git_hub_repository_issues_patch(git_hub_repository_issue_body, arguments, issue_number, api_args_to_merge, no_cache);
 }
---
+++ src/task/property/body.mjs
@@ -0,0 +1,5 @@
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function task_property_body() {
+    arguments_assert(arguments, []);
+    return 'body';
+}
