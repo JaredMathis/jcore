158abe5691fd64857a8b22f3be461f11d2bda76c
JaredMathis <jared.mathis@native.consulting>
2023-06-16 13:37:30 -0400

    #144 function_auto function_tests_regenerate

--- src/function/tests/regenerate.mjs
+++ src/function/tests/regenerate.mjs
@@ -1,8 +1,10 @@
+import { function_tests_generated_delete } from './generated/delete.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { function_tests_generate } from './generate.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function function_tests_regenerate(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
+    await function_tests_generated_delete(function_name);
     let result = await function_tests_generate(function_name);
     return result;
 }
