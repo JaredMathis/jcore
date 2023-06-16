cc02bd27e70a5cfca93d8f033a60bd61934fb2b0
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:17:30 -0400

    #98 function_auto try_catch_throw_async

--- src/try/catch/throw/async.mjs
+++ src/try/catch/throw/async.mjs
@@ -1,9 +1,9 @@
-import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { function_is } from '../../../function/is.mjs';
 export async function try_catch_throw_async(lambda_try, lambda_catch) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        function_is,
+        function_is
     ]);
     try {
         await lambda_try();
