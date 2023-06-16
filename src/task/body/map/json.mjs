3834715224d94579cbe9d56b7e7bd60d71ca0e0e
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:36:40 -0400

    #124 function_auto task_body_map_json

--- src/task/body/map/json.mjs
+++ src/task/body/map/json.mjs
@@ -1,11 +1,12 @@
 import { task_body_map_generic } from './generic.mjs';
-import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { json_map_empty_if_null } from '../../../json/map/empty/if/null.mjs';
+import { function_is } from '../../../function/is.mjs';
+import { string_is } from '../../../string/is.mjs';
 export async function task_body_map_json(issue_number, map) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        string_is,
+        function_is
     ]);
     let lambda = task_body_value => json_map_empty_if_null(task_body_value, map);
     let result = await task_body_map_generic(issue_number, lambda);
