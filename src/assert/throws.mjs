7685c22ac1667dc368b6acb6620b9a4be5e909da
JaredMathis <jared.mathis@native.consulting>
2023-06-16 07:44:38 -0400

    #124 function_auto assert_throws

--- src/assert/throws.mjs
+++ src/assert/throws.mjs
@@ -2,15 +2,13 @@ import { throws } from '../throws.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { assert_arguments_count } from './arguments/count.mjs';
-import { error } from '../error.mjs';
 import { function_is } from '../function/is.mjs';
+import { assert_message } from './message.mjs';
 export function assert_throws(lambda) {
     arguments_assert(arguments, [function_is]);
     assert_arguments_count(arguments, 1);
     let errored = throws(lambda);
-    if (!errored) {
-        error(`Expecting throw`);
-    }
+    assert_message(!errored, `Expecting throw`);
     return errored;
     metadata([]);
 }
