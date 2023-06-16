b7d66cae24113d60feb18a1c5bba5f1aa7f6b882
JaredMathis <jared.mathis@native.consulting>
2023-06-16 11:32:34 -0400

    #141 function_rename task_open_get task_open_unsummarized

--- src/task/description/to/json/if/unparsable.mjs
+++ src/task/description/to/json/if/unparsable.mjs
@@ -1,8 +1,8 @@
 import { task_description_to_json_if_unparsable_generic } from './unparsable/generic.mjs';
-import { task_open_get } from '../../../../open/get.mjs';
+import { task_open_unsummarized } from '../../../../open/unsummarized.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export async function task_description_to_json_if_unparsable() {
     arguments_assert(arguments, []);
-    let open = await task_open_get();
+    let open = await task_open_unsummarized();
     return await task_description_to_json_if_unparsable_generic(open);
 }
--- src/task/open/generic.mjs
+++ src/task/open/generic.mjs
@@ -3,14 +3,14 @@ import { list_map } from '../../list/map.mjs';
 import { task_property_number } from '../property/number.mjs';
 import { list_sort_property } from '../../list/sort/property.mjs';
 import { list_filter } from '../../list/filter.mjs';
-import { task_open_get } from './get.mjs';
+import { task_open_unsummarized } from './unsummarized.mjs';
 import { function_is } from '../../function/is.mjs';
 export async function task_open_generic(filter_get, map) {
     arguments_assert(arguments, [
         function_is,
         function_is
     ]);
-    let open = await task_open_get();
+    let open = await task_open_unsummarized();
     let filtered = list_filter(open, filter_get(open));
     list_sort_property(filtered, task_property_number());
     let summaries = list_map(filtered, map);
