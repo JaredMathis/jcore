5ec59e565d2b73b0790b8c4855b44c607e678ea4
JaredMathis <jared.mathis@native.consulting>
2023-06-16 15:00:18 -0400

    #149 function_auto string_similar_remove_single

--- src/string/similar/remove/single.mjs
+++ src/string/similar/remove/single.mjs
@@ -3,12 +3,13 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { range } from '../../../range.mjs';
 import { equal } from '../../../equal.mjs';
 import { string_is } from '../../is.mjs';
+import { string_length } from '../../length.mjs';
 export function string_similar_remove_single(a, b) {
     arguments_assert(arguments, [
         string_is,
         string_is
     ]);
-    for (let i of range(a_length)) {
+    for (let i of range(string_length(a))) {
         let other = string_remove_at(a, i);
         if (equal(other, b)) {
             return true;
