c68c34f39f283ec0ea7a9a2d1444159345974ea6
JaredMathis <jared.mathis@native.consulting>
2023-06-16 07:16:40 -0400

    #123 function_auto task_open

--- src/task/open.mjs
+++ src/task/open.mjs
@@ -1,3 +1,4 @@
+import { log } from '../log.mjs';
 import { list_sort_property } from '../list/sort/property.mjs';
 import { task_property_open } from './property/open.mjs';
 import { list_filter_property } from '../list/filter/property.mjs';
@@ -8,10 +9,15 @@ import { list_map } from '../list/map.mjs';
 import { object_property_get } from '../object/property/get.mjs';
 import { task_property_number } from './property/number.mjs';
 import { task_property_title } from './property/title.mjs';
+import { list_filter } from '../list/filter.mjs';
 export async function task_open() {
     arguments_assert(arguments, []);
     let all = await task_all();
     let open = list_filter_property(all, task_property_state(), task_property_open());
+    let filtered = list_filter(open, o => {
+        console.log({ o });
+        return true;
+    });
     list_sort_property(open, task_property_number());
     let summaries = list_map(open, o => `${ object_property_get(o, task_property_number()) } ${ object_property_get(o, task_property_title()) }`);
     return summaries;
