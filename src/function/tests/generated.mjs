31c80f4598eaf470ca0870122c07f54f2c618191
JaredMathis <jared.mathis@native.consulting>
2023-06-16 13:40:05 -0400

    #144 function_auto function_tests_generated

--- src/function/tests/generated.mjs
+++ src/function/tests/generated.mjs
@@ -1,16 +1,12 @@
-import { function_tests_generated_string } from './generated/string.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { function_tests_all } from './all.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_filter } from '../../list/filter.mjs';
-import { string_identifier_parts_to } from '../../string/identifier/parts/to.mjs';
-import { list_contains } from '../../list/contains.mjs';
+import { string_includes } from '../../string/includes.mjs';
+import { function_tests_generated_string_sub } from './generated/string/sub.mjs';
 export async function function_tests_generated(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let result = await function_tests_all(function_name);
-    let generated = list_filter(result, r => {
-        let parts = string_identifier_parts_to(r);
-        return list_contains(parts, function_tests_generated_string());
-    });
+    let generated = list_filter(result, r => string_includes(r, function_tests_generated_string_sub()));
     return generated;
 }
