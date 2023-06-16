00eb362d7be9806a3894ffa43c8caedb4366bae6
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:53:35 -0400

    #114 function_auto task_git_hub_issues_pull_generic

--- src/task/git/hub/issues/pull/generic.mjs
+++ src/task/git/hub/issues/pull/generic.mjs
@@ -1,3 +1,6 @@
+import { list_new_then_async } from '../../../../../list/new/then/async.mjs';
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { file_json_overwrite } from '../../../../../file/json/overwrite.mjs';
 import { version_path_tasks_all_get } from '../../../../../version/path/tasks/all/get.mjs';
 import { task_from_git_hub_issue } from '../../../../from/git/hub/issue.mjs';
@@ -9,7 +12,9 @@ import { comment } from '../../../../../comment.mjs';
 import { git_hub_repository_issues_all } from '../../../../../git/hub/repository/issues/all.mjs';
 import { version_repository_default } from '../../../../../version/repository/default.mjs';
 export async function task_git_hub_issues_pull_generic(no_cache) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let repository_name = version_repository_default();
+    list_new_then_async;
     let page = 1;
     let issues = await git_hub_repository_issues_all(no_cache, page);
     comment(`If there's more than 1 page of issues code needs adjusting`);
