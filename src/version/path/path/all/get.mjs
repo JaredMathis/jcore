ea730b4c6649cc40ca49b4a38da59c1373f61fee
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:06:43 -0400

    #124 function_auto version_path_path_all_get

---
+++ src/version/path/path/all/get.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../../error.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function version_path_path_all_get() {
+    arguments_assert(arguments, []);
+    error('todo: version_path_path_all_get');
+}
