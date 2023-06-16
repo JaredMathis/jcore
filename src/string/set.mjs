dbbf47324cc6582848397c3c0a0ea2d3669f18b7
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:49:48 -0400

    #149 function_auto string_set

--- src/string/set.mjs
+++ src/string/set.mjs
@@ -3,11 +3,17 @@ import { character_is } from '../character/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { string_is } from './is.mjs';
 import { integer_is } from '../integer/is.mjs';
-export function string_set(s, index, value) {
+import { string_take } from './take.mjs';
+import { string_skip } from './skip.mjs';
+export function string_set(input, index, value) {
     arguments_assert(arguments, [
         string_is,
         integer_is,
         character_is
     ]);
-    return string_add_multiple([]);
+    return string_add_multiple([
+        string_take(input, index),
+        value,
+        string_skip(input, index + 1)
+    ]);
 }
