28958f987a2d47fecc112229084ef1ce4a7e3c3a
JaredMathis <jared.mathis@native.consulting>
2023-06-16 13:41:33 -0400

    #144 function_auto function_tests_generated_delete

--- src/function/tests/generated/delete.mjs
+++ src/function/tests/generated/delete.mjs
@@ -3,6 +3,7 @@ import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { function_delete } from '../../delete.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_length_is_0 } from '../../../list/length/is/0.mjs';
+import { tests_generate } from '../../../tests/generate.mjs';
 export async function function_tests_generated_delete(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let generateds = await function_tests_generated(function_name);
@@ -10,6 +11,7 @@ export async function function_tests_generated_delete(function_name) {
         for (let g of generateds) {
             await function_delete(g);
         }
+        await tests_generate();
     }
     return generateds;
 }
