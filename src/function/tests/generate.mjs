5e3b205d5f12e3d08b11012e2b0d972c3d0db197
JaredMathis <jared.mathis@native.consulting>
2023-06-16 16:34:05 -0400

    #151 noop

--- src/function/tests/generate.mjs
+++ src/function/tests/generate.mjs
@@ -5,6 +5,7 @@ import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function function_tests_generate(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    await function_tests_generate_generic(function_name, function_tests_generate_count_default());
+    const count = function_tests_generate_count_default();
+    await function_tests_generate_generic(function_name, count);
     await function_tests_generate_after();
 }
