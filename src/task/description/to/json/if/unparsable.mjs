c1fe932df80faccf09a5bb7d8df2f456a32089b1
JaredMathis <jared.mathis@native.consulting>
2023-06-16 11:32:57 -0400

    #141 function_auto task_description_to_json_if_unparsable

--- src/task/description/to/json/if/unparsable.mjs
+++ src/task/description/to/json/if/unparsable.mjs
@@ -1,8 +1,8 @@
+import { task_all_unsummarized } from '../../../../all/unsummarized.mjs';
 import { task_description_to_json_if_unparsable_generic } from './unparsable/generic.mjs';
-import { task_open_unsummarized } from '../../../../open/unsummarized.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export async function task_description_to_json_if_unparsable() {
     arguments_assert(arguments, []);
-    let open = await task_open_unsummarized();
-    return await task_description_to_json_if_unparsable_generic(open);
+    let all = await task_all_unsummarized();
+    return await task_description_to_json_if_unparsable_generic(all);
 }
