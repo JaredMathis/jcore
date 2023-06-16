9e2f42e75f5a1e85fa2e5a6fab16eab88c33367e
JaredMathis <jared.mathis@native.consulting>
2023-06-16 18:46:39 -0400

    #153 noop

--- src/function/wrap/suffix/add.mjs
+++ src/function/wrap/suffix/add.mjs
@@ -1,9 +1,9 @@
+import { function_wrap } from '../../wrap.mjs';
 import { string_identifier_first_not_is } from '../../../string/identifier/first/not/is.mjs';
-import { function_wrap_suffix_change } from './change.mjs';
 import { string_identifier_parts_from } from '../../../string/identifier/parts/from.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_wrap_suffix_add(function_name_to_wrap, suffix_to_add) {
+export async function function_wrap_suffix_add(function_name_to_wrap_without_suffix, suffix_to_add) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_first_not_is
@@ -12,6 +12,6 @@ export async function function_wrap_suffix_add(function_name_to_wrap, suffix_to_
         function_name_to_wrap_without_suffix,
         suffix_to_add
     ]);
-    let result = await function_wrap_suffix_change(function_name_to_wrap, function_name_to_add);
+    let result = await function_wrap(function_name_to_wrap_without_suffix, function_name_to_add);
     return result;
 }
---
+++ src/task/title/append.mjs
@@ -0,0 +1,14 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { task_title } from '../title.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+import { string_add } from '../../string/add.mjs';
+export async function task_title_append(issue_number, title_additional) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    task_title_get;
+    let title_after = string_add(title_before, title_additional);
+    let result = await task_title(issue_number, title_after);
+    return result;
+}
