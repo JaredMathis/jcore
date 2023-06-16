6a1b0dafc208be5d57e3e58e96c13305f10b8169
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:45:21 -0400

    #114 function_input_add_type_default_identifier git_hub_repository_issues_get no_cache boolean_is

--- src/git/hub/repository/issues/all.mjs
+++ src/git/hub/repository/issues/all.mjs
@@ -2,5 +2,5 @@ import { git_hub_repository_issues_get } from './get.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function git_hub_repository_issues_all() {
     arguments_assert(arguments, []);
-    return await git_hub_repository_issues_get({ state: 'all' });
+    return await git_hub_repository_issues_get({ state: 'all' }, no_cache);
 }
--- src/git/hub/repository/issues/closed.mjs
+++ src/git/hub/repository/issues/closed.mjs
@@ -3,5 +3,5 @@ import { git_hub_repository_issues_get } from './get.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function git_hub_repository_issues_closed() {
     arguments_assert(arguments, []);
-    return await git_hub_repository_issues_get({ state: task_property_closed() });
+    return await git_hub_repository_issues_get({ state: task_property_closed() }, no_cache);
 }
