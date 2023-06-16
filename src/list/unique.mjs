36da6dedef53c1711bfc548f133b763fdd76d9fe
JaredMathis <jared.mathis@native.consulting>
2023-06-16 12:31:13 -0400

    #142 function_auto list_unique

--- src/list/unique.mjs
+++ src/list/unique.mjs
@@ -1,13 +1,13 @@
-import { list_add_if_not_exists } from './add/if/not/exists.mjs';
+import { list_adder_unique } from './adder/unique.mjs';
 import { list_is } from './is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 export function list_unique(list) {
     arguments_assert(arguments, [list_is]);
-    let result = [];
-    for (let element of list) {
-        list_add_if_not_exists(result, element);
-    }
-    return result;
+    return list_adder_unique(la => {
+        for (let element of list) {
+            la(element);
+        }
+    });
     metadata([]);
 }
