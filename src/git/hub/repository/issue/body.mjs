b0e6066a1a411ad494d8af6b12ebbe6eea8d21a7
JaredMathis <jared.mathis@native.consulting>
2023-06-15 21:29:44 -0400

    #117 function_auto git_hub_repository_issue_body

--- src/git/hub/repository/issue/body.mjs
+++ src/git/hub/repository/issue/body.mjs
@@ -1,6 +1,5 @@
-import { git_hub_repository_issue_title } from './title.mjs';
+import { task_property_body } from '../../../../task/property/body.mjs';
 import { git_hub_repository_issues_patch } from '../issues/patch.mjs';
-import { task_property_title } from '../../../../task/property/title.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function git_hub_repository_issue_body(issue_number, body) {
@@ -8,7 +7,7 @@ export async function git_hub_repository_issue_body(issue_number, body) {
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let api_args_to_merge = { [task_property_title()]: body };
+    let api_args_to_merge = { [task_property_body()]: body };
     let no_cache = true;
     return git_hub_repository_issues_patch(git_hub_repository_issue_body, arguments, issue_number, api_args_to_merge, no_cache);
 }
