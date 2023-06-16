835c8ae437e9081d14327ee4ec365991fb84a8e6
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:59:45 -0400

    #123 function_auto string_identifier_combine

--- src/string/identifier/combine.mjs
+++ src/string/identifier/combine.mjs
@@ -3,15 +3,15 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_add_multiple } from '../add/multiple.mjs';
 import { function_name_separator } from '../../function/name/separator.mjs';
 import { string_identifier_is } from './is.mjs';
-export function string_identifier_combine(prefix, identifier) {
+export function string_identifier_combine(first, second) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_first_not_is
     ]);
     const strings = [
-        prefix,
+        first,
         function_name_separator(),
-        identifier
+        second
     ];
     let result = string_add_multiple(strings);
     return result;
