574ce594ac7b71b47f915880eccae748d3a98069
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:45:43 -0400

    #114 function_input_add_type_default_identifier git_hub_repository_issues_all no_cache boolean_is

--- src/git/hub/repository/issues/all.mjs
+++ src/git/hub/repository/issues/all.mjs
@@ -1,6 +1,7 @@
+import { boolean_is } from '../../../../boolean/is.mjs';
 import { git_hub_repository_issues_get } from './get.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function git_hub_repository_issues_all() {
-    arguments_assert(arguments, []);
+export async function git_hub_repository_issues_all(no_cache) {
+    arguments_assert(arguments, [boolean_is]);
     return await git_hub_repository_issues_get({ state: 'all' }, no_cache);
 }
