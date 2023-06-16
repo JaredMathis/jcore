676f02834687f7ac2f1ba51babbdfd8be8aa021e
JaredMathis <jared.mathis@native.consulting>
2023-06-16 16:28:35 -0400

    #151 function_auto string_similar

--- src/string/similar.mjs
+++ src/string/similar.mjs
@@ -14,7 +14,7 @@ export function string_similar(a, b) {
         return true;
     }
     const fns = [string_similar_swap_adjascent];
-    if (string_similar_swap_adjascent(a, b)) {
+    if (list_each_function_results_any(fns, results_get)) {
         return true;
     }
     return false;
