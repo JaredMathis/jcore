47423fa9b5fd0ec2ddb6b9eb8701cb7d74bb3c91
JaredMathis <jared.mathis@native.consulting>
2023-06-15 22:26:33 -0400

    #118 function_auto task_close_multiple

--- src/task/close/multiple.mjs
+++ src/task/close/multiple.mjs
@@ -1,6 +1,6 @@
-import { error } from '../../error.mjs';
+import { task_close } from '../close.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function task_close_multiple() {
     arguments_assert(arguments, []);
-    error('todo: task_close_multiple');
+    task_close;
 }
