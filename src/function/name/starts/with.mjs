e435ee25a8d75a7158989bbeec927e7ab96b5d11
JaredMathis <jared.mathis@native.consulting>
2023-06-16 13:55:09 -0400

    #148 function_name_starts_with string_

--- src/function/name/starts/with.mjs
+++ src/function/name/starts/with.mjs
@@ -6,5 +6,5 @@ import { string_starts_with } from '../../../string/starts/with.mjs';
 export async function function_name_starts_with(prefix) {
     arguments_assert(arguments, [string_is]);
     let result = await function_name_all();
-    return list_filter(result, r => string_starts_with(prefix));
+    return list_filter(result, r => string_starts_with(r, prefix));
 }
