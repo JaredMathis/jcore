2176708fdc81925ae10eb94d44ab8e64cf6c5062
JaredMathis <jared.mathis@native.consulting>
2023-06-16 07:04:58 -0400

    #122 function_auto task_requires

--- src/task/requires.mjs
+++ src/task/requires.mjs
@@ -1,8 +1,7 @@
+import { object_property_get } from '../object/property/get.mjs';
 import { task_body_property_requires } from './body/property/requires.mjs';
 import { object_property_initialize_if_unset } from '../object/property/initialize/if/unset.mjs';
 import { task_exists } from './exists.mjs';
-import { task_body_property_description } from './body/property/description.mjs';
-import { object_property_set } from '../object/property/set.mjs';
 import { task_body_map } from './body/map.mjs';
 import { string_is } from '../string/is.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
@@ -18,6 +17,6 @@ export async function task_requires(issue_number, issue_number_required) {
     return result;
     function map(body_parsed) {
         object_property_initialize_if_unset(body_parsed, task_body_property_requires(), []);
-        object_property_set(body_parsed, task_body_property_description(), issue_number_required);
+        let requires = object_property_get(body_parsed, task_body_property_requires());
     }
 }
