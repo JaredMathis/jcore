346409bfd3d7452271990a8b372c2feaecb5df99
JaredMathis <jared.mathis@native.consulting>
2023-06-16 09:54:40 -0400

    #136 function_auto string_split_comma

--- src/string/split/comma.mjs
+++ src/string/split/comma.mjs
@@ -1,4 +1,8 @@
+import { string_comma } from '../comma.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_split } from '../split.mjs';
 export function string_split_comma(values) {
-    return string_split(values, ',');
+    arguments_assert(arguments, [arguments_assert_todo]);
+    return string_split(values, string_comma());
 }
