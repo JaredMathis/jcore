33f9a466bd82d593c10ac97ded928c5ee79ce34a
JaredMathis <jared.mathis@native.consulting>
2023-06-16 10:52:52 -0400

    #141 function_search task,body

--- src/task/requires/get.mjs
+++ src/task/requires/get.mjs
@@ -3,5 +3,11 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { task_is } from '../is.mjs';
 export function task_requires_get(task) {
     arguments_assert(arguments, [task_is]);
-    error('todo: task_requires_get');
+    let body = object_property_get(task, task_property_body());
+    if (null_is(body)) {
+        return body;
+    }
+    let body_parsed = json_from(body);
+    let requires = task_body_requires(body_parsed);
+    return requires;
 }
