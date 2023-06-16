bdd9b728df111654f442e357c4d03c1b881e04e7
JaredMathis <jared.mathis@native.consulting>
2023-06-15 22:28:06 -0400

    #118 function_auto task_close_multiple

--- src/task/close/multiple.mjs
+++ src/task/close/multiple.mjs
@@ -1,6 +1,8 @@
+import { string_split_comma } from '../../string/split/comma.mjs';
 import { task_close } from '../close.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function task_close_multiple(task_number_list) {
     arguments_assert(arguments, []);
-    task_close(task_number)
+    let parts = string_split_comma(task_number_list);
+    task_close(task_number);
 }
