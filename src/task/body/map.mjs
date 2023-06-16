0f9d0546e4d5f6ffbb6eb624569043d1d6272a74
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:26:35 -0400

    #124 function_auto task_body_map

--- src/task/body/map.mjs
+++ src/task/body/map.mjs
@@ -1,6 +1,3 @@
-import { version_log } from '../../version/log.mjs';
-import { version_repository_default } from '../../version/repository/default.mjs';
-import { json_invalid } from '../../json/invalid.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { task_body } from '../body.mjs';
@@ -8,25 +5,13 @@ import { json_map_empty_if_null } from '../../json/map/empty/if/null.mjs';
 import { task_property_body } from '../property/body.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 import { task_get } from '../get.mjs';
-import { string_is } from '../../string/is.mjs';
-import { object_property_set } from '../../object/property/set.mjs';
 export async function task_body_map(issue_number, map) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let repository_name = version_repository_default();
     let task = await task_get(issue_number);
     let task_body_value = object_property_get(task, task_property_body());
-    if (string_is(task_body_value)) {
-        if (json_invalid(task_body_value)) {
-            let fn = task_body_map;
-            const message = `Invaild JSON`;
-            const data = { task };
-            await version_log(repository_name, fn, arguments, message, data);
-            object_property_set(task, task_property_body(), null);
-        }
-    }
     let body_after = json_map_empty_if_null(task_body_value, map);
     let result = await task_body(issue_number, body_after);
     return result;
