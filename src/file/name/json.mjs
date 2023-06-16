c9ccb727809ae47f2cc860da05d1304f9e723a7c
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:08:43 -0400

    #124 function_auto file_name_json

--- src/file/name/json.mjs
+++ src/file/name/json.mjs
@@ -1,7 +1,7 @@
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { file_extension_json } from '../extension/json.mjs';
 export function file_name_json(file_name) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [string_identifier_is]);
     return `${ file_name }${ file_extension_json() }`;
 }
