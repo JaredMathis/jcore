25f4973aadc8ffe25cf687434f2d8479b15216b5
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:32:33 -0400

    #120 function_auto task_description

--- src/task/description.mjs
+++ src/task/description.mjs
@@ -10,10 +10,11 @@ import { task_property_body } from './property/body.mjs';
 import { json_from } from '../json/from.mjs';
 import { json_to } from '../json/to.mjs';
 import { object_property_set } from '../object/property/set.mjs';
+import { string_is } from '../string/is.mjs';
 export async function task_description(issue_number, description) {
     arguments_assert(arguments, [
         arguments_assert_todo,
-        arguments_assert_todo
+        string_is
     ]);
     let t = await task_get(issue_number);
     let body_before = object_property_get(t, task_property_body());
@@ -21,7 +22,7 @@ export async function task_description(issue_number, description) {
         body_before = js_brace_left_right();
     }
     let body_parsed = json_from(body_before);
-    object_property_set(body_parsed, task_body_property_description());
+    object_property_set(body_parsed, task_body_property_description(), description);
     let body_after = json_to(body_parsed);
     let result = await task_body(issue_number, body_after);
     return result;
