4fdfd2f45254550f5838f61f18e13f9de3b63e4c
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:42:32 -0400

    #149 function_copy list_swap string_swap

---
+++ src/string/swap.mjs
@@ -0,0 +1,15 @@
+import { swap_generic } from '../swap/generic.mjs';
+import { list_set } from '../list/set.mjs';
+import { list_get } from '../list/get.mjs';
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function string_swap(list, index_from, index_to) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let generic_get = list_get;
+    let generic_set = list_set;
+    swap_generic(generic_get, generic_set, list, index_from, index_to);
+}
