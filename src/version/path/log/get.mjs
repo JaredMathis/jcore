0c111cfa0386e56e7bf9a20ce42a8eb162c04dad
JaredMathis <jared.mathis@native.consulting>
2023-06-16 08:05:45 -0400

    #124 function_auto version_path_log_get

--- src/version/path/log/get.mjs
+++ src/version/path/log/get.mjs
@@ -1,10 +1,9 @@
 import { version_path_sub_get } from '../sub/get.mjs';
-import { version_path_tasks } from '../tasks.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export function version_path_log_get(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let sub_name = version_path_tasks();
+    let sub_name = version_path_log_get();
     let repository_sub_path = version_path_sub_get(repository_name, sub_name);
     return repository_sub_path;
 }
