0353af0d9913587a065972aea6f5f1ace8687e93
JaredMathis <jared.mathis@native.consulting>
2023-06-16 16:15:01 -0400

    #151 function_wrap_replace string_equal_count string_equal_except_1

--- src/string/equal/except/1.mjs
+++ src/string/equal/except/1.mjs
@@ -1,6 +1,11 @@
-import { error } from '../../../error.mjs';
+import { string_is } from '../../is.mjs';
+import { string_equal_count } from '../count.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function string_equal_except_1() {
-    arguments_assert(arguments, []);
-    error('todo: string_equal_except_1');
+export function string_equal_except_1(a, b) {
+    arguments_assert(arguments, [
+        string_is,
+        string_is
+    ]);
+    let result = string_equal_count(a, b);
+    return result;
 }
