17acb5cc80e45ed0493ea4162dde5f6e780d207e
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:44:43 -0400

    #114 function_input_add_type_default_identifier git_hub_repository_issues_generic no_cache boolean_is

--- src/git/hub/repository/issues/get.mjs
+++ src/git/hub/repository/issues/get.mjs
@@ -7,5 +7,5 @@ export async function git_hub_repository_issues_get(api_args_to_merge) {
     arguments_assert(arguments, [defined_is]);
     let verb = 'GET';
     object_merge({ per_page: git_hub_page_size() }, api_args_to_merge);
-    return await git_hub_repository_issues_generic(git_hub_repository_issues_get, arguments, api_args_to_merge, verb);
+    return await git_hub_repository_issues_generic(git_hub_repository_issues_get, arguments, api_args_to_merge, verb, no_cache);
 }
--- src/git/hub/repository/issues/post.mjs
+++ src/git/hub/repository/issues/post.mjs
@@ -1,12 +1,11 @@
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { git_hub_repository_issues_generic } from './generic.mjs';
 import { object_merge } from '../../../../object/merge.mjs';
-import { defined_is } from '../../../../defined/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function git_hub_repository_issues_post(title) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let api_args_to_merge = {};
     let verb = 'POST';
     object_merge({ title }, api_args_to_merge);
-    return await git_hub_repository_issues_generic(git_hub_repository_issues_post, arguments, api_args_to_merge, verb);
+    return await git_hub_repository_issues_generic(git_hub_repository_issues_post, arguments, api_args_to_merge, verb, no_cache);
 }
