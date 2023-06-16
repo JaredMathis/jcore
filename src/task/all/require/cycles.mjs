8211ce7535d4e80f8c1e89e3c8715d99801fbd40
JaredMathis <jared.mathis@native.consulting>
2023-06-16 12:22:13 -0400

    #142 function_auto task_all_require_cycles

--- src/task/all/require/cycles.mjs
+++ src/task/all/require/cycles.mjs
@@ -1,9 +1,9 @@
-import { visit_lambda_none } from '../../../visit/lambda/none.mjs';
+import { visit } from '../../../visit.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { task_all_unsummarized } from '../unsummarized.mjs';
 import { task_requires_get } from '../../requires/get.mjs';
 export async function task_all_require_cycles() {
     arguments_assert(arguments, []);
     let all = await task_all_unsummarized();
-    visit_lambda_none(t, task => task_requires_get(task));
+    visit(t, task => task_requires_get(task));
 }
