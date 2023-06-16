6fdf856a543db7ed3871792b673799ee314aa6d2
JaredMathis <jared.mathis@native.consulting>
2023-06-15 20:06:00 -0400

    #98 function_auto task_current

--- src/task/current.mjs
+++ src/task/current.mjs
@@ -1,13 +1,12 @@
+import { list_find_property } from '../list/find/property.mjs';
 import { task_id_unhash } from './id/unhash.mjs';
 import { equal } from '../equal.mjs';
 import { metadata } from '../metadata.mjs';
 import { task_id_get_or_null } from './id/get/or/null.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { task_all } from './all.mjs';
-import { list_filter_property } from '../list/filter/property.mjs';
 import { task_property_number } from './property/number.mjs';
 import { integer_parse } from '../integer/parse.mjs';
-import { list_single } from '../list/single.mjs';
 export async function task_current() {
     arguments_assert(arguments, []);
     let task_number_hashed = await task_id_get_or_null();
@@ -18,8 +17,7 @@ export async function task_current() {
     let task_number = integer_parse(task_number_string);
     let all = await task_all();
     const property_name = task_property_number();
-    let filtered = list_filter_property(all, property_name, task_number);
-    let result = list_single(filtered);
+    let result = list_find_property(all, property_name, task_number);
     return result;
     metadata([]);
 }
