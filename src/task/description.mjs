191856b40601a568351ce8b8af2640099432802a
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:15:22 -0400

    #120 function_delete task_description

--- src/task/description.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../error.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
-export function task_description() {
-    arguments_assert(arguments, []);
-    error('todo: task_description');
-}
