b30f5ef3d7f961a2cf754231abe0ec9b7a00b6a4
JaredMathis <jared.mathis@native.consulting>
2023-06-16 09:57:26 -0400

    #136 function_input_add_type_default string_identifier_sub_multiple_parse separator string_is string_comma()

--- src/string/identifier/sub/multiple/parse.mjs
+++ src/string/identifier/sub/multiple/parse.mjs
@@ -5,8 +5,11 @@ import { list_all } from '../../../../list/all.mjs';
 import { assert } from '../../../../assert.mjs';
 import { string_is } from '../../../is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function string_identifier_sub_multiple_parse(values) {
-    arguments_assert(arguments, [string_is]);
+export function string_identifier_sub_multiple_parse(values, separator) {
+    arguments_assert(arguments, [
+        string_is,
+        string_is
+    ]);
     let result = string_split(values, string_comma());
     assert(list_all(result, string_identifier_sub_is));
     return result;
