5d5328d5440e9ac305f2ba737a1529393f7f0fb9
JaredMathis <jared.mathis@native.consulting>
2023-06-16 14:39:59 -0400

    #149 function_auto function_input_swap_first_2

--- src/function/input/swap/first/2.mjs
+++ src/function/input/swap/first/2.mjs
@@ -1,9 +1,9 @@
-import { refactor_input_swap_last_2 } from '../../../../refactor/input/swap/last/2.mjs';
+import { refactor_input_swap_first_2 } from '../../../../refactor/input/swap/first/2.mjs';
 import { function_name_get } from '../../../name/get.mjs';
 import { function_map_with_args } from '../../../map/with/args.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function function_input_swap_first_2(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    await function_map_with_args(function_name_get(refactor_input_swap_last_2), function_name, {});
+    await function_map_with_args(function_name_get(refactor_input_swap_first_2), function_name, {});
 }
