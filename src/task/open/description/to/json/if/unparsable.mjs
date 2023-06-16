64a49bf5c6a4f64787ae1c1bafebbdeba6ea4852
JaredMathis <jared.mathis@native.consulting>
2023-06-16 07:32:11 -0400

    #124 function_auto task_open_description_to_json_if_unparsable

--- src/task/open/description/to/json/if/unparsable.mjs
+++ src/task/open/description/to/json/if/unparsable.mjs
@@ -1,3 +1,4 @@
+import { list_new_then_async } from '../../../../../../list/new/then/async.mjs';
 import { task_description } from '../../../../../description.mjs';
 import { throws } from '../../../../../../throws.mjs';
 import { task_property_body } from '../../../../../property/body.mjs';
@@ -8,6 +9,7 @@ import { json_to } from '../../../../../../json/to.mjs';
 export async function task_open_description_to_json_if_unparsable() {
     arguments_assert(arguments, []);
     let open = await task_open_get();
+    list_new_then_async;
     for (let t of open) {
         let task_body_value = object_property_get(t, task_property_body());
         if (throws(() => json_to(task_body_value))) {
