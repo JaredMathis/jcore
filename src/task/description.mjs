6e0cc9f4ffa6160ff15d9e7755c8a81a8387e20d
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:29:56 -0400

    #120 function_auto task_description

--- src/task/description.mjs
+++ src/task/description.mjs
@@ -1,3 +1,4 @@
+import { js_brace_left_right } from '../js/brace/left/right.mjs';
 import { null_is } from '../null/is.mjs';
 import { task_get } from './get.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
@@ -15,7 +16,7 @@ export async function task_description(issue_number, description) {
     let t = await task_get(issue_number);
     let body_before = object_property_get(t, task_property_body());
     if (null_is(body_before)) {
-        
+        body_before = js_brace_left_right();
     }
     let body_parsed = json_from(body_before);
     let body_after = json_to(body_parsed);
