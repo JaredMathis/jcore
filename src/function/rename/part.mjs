b1ac636de4752c59a1a6d648a82900055cfe4ec8
JaredMathis <jared.mathis@native.consulting>
2023-06-15 22:44:37 -0400

    #119 function_auto function_rename_part

--- src/function/rename/part.mjs
+++ src/function/rename/part.mjs
@@ -1,3 +1,5 @@
+import { log } from '../../log.mjs';
+import { list_map } from '../../list/map.mjs';
 import { string_identifier_parts_map } from '../../string/identifier/parts/map.mjs';
 import { string_identifier_part_is } from '../../string/identifier/part/is.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
@@ -10,12 +12,17 @@ export async function function_rename_part(function_name_old, part_old, part_new
         string_identifier_part_is,
         string_identifier_part_is
     ]);
-    let function_name_new = string_identifier_parts_map(function_name_old, p => {
+    let function_name_new = string_identifier_parts_map(function_name_old, parts => list_map(parts, p => {
+        console.log({
+            p,
+            part_old,
+            part_new
+        });
         if (equal(p, part_old)) {
             return part_new;
         }
         return p;
-    });
+    }));
     let result = await function_rename(function_name_old, function_name_new);
     return result;
 }
