b7ccf8f1c30efcefbd832a8095b2a983a7367460
JaredMathis <jared.mathis@native.consulting>
2023-06-16 16:18:39 -0400

    #151 function_auto string_equal_count

--- src/string/equal/count.mjs
+++ src/string/equal/count.mjs
@@ -1,10 +1,8 @@
 import { list_min } from '../../list/min.mjs';
 import { list_string_lengths } from '../../list/string/lengths.mjs';
 import { count } from '../../count.mjs';
-import { number_min } from '../../number/min.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { range } from '../../range.mjs';
-import { string_length } from '../length.mjs';
 import { string_get } from '../get.mjs';
 import { equal } from '../../equal.mjs';
 import { string_is } from '../is.mjs';
@@ -18,10 +16,7 @@ export function string_equal_count(a, b) {
             a,
             b
         ]);
-        list_min(lengths);
-        let a_length = string_length(a);
-        let b_length = string_length(b);
-        for (let i of range(number_min(a_length, b_length))) {
+        for (let i of range(list_min(lengths))) {
             let a_i = string_get(a, i);
             let b_i = string_get(b, i);
             if (equal(a_i, b_i)) {
