88480316d29c758bf94b1677482655f1511bbd49
JaredMathis <jared.mathis@native.consulting>
2023-06-16 13:34:22 -0400

    #145 noop

--- src/task/unsub.mjs
+++ src/task/unsub.mjs
@@ -4,11 +4,13 @@ import { task_finish } from './finish.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { list_last } from '../list/last.mjs';
 import { task_current } from './current.mjs';
+import { string_to } from '../string/to.mjs';
 export async function task_unsub() {
     arguments_assert(arguments, []);
     let required_bys = await task_current_required_bys();
     let last = await list_last(required_bys);
     await task_finish();
-    await task_set(last);
+    let last_string = string_to(last);
+    await task_set(last_string);
     return await task_current();
 }
