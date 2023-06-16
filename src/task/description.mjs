c3d541aa7fd6a1ef3e08684235ec544540842241
JaredMathis <jared.mathis@native.consulting>
2023-06-16 06:25:37 -0400

    #120 function_auto task_description

--- src/task/description.mjs
+++ src/task/description.mjs
@@ -4,6 +4,8 @@ import { task_body } from './body.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { object_property_get } from '../object/property/get.mjs';
 import { task_property_body } from './property/body.mjs';
+import { json_from } from '../json/from.mjs';
+import { json_to } from '../json/to.mjs';
 export async function task_description(issue_number, description) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -11,6 +13,8 @@ export async function task_description(issue_number, description) {
     ]);
     let t = await task_get(issue_number);
     let body_before = object_property_get(t, task_property_body());
-    let result = await task_body(issue_number, body);
+    let body_parsed = json_from(body_before);
+    let body_after = json_to(body_parsed);
+    let result = await task_body(issue_number, body_after);
     return result;
 }
