9d3304fc40fa04cb03fd103baf51908c373e5af2
JaredMathis <jared.mathis@native.consulting>
2023-06-17 12:26:03 -0400

    #174 function_unasyncify list_map_generic_async

--- src/list/map/generic.mjs
+++ src/list/map/generic.mjs
@@ -1,19 +1,20 @@
 import { metadata_generated } from '../../metadata/generated.mjs';
 import { metadata } from '../../metadata.mjs';
-import { list_add } from '../add.mjs';
 import { list_each_with_index } from '../each/with/index.mjs';
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { list_adder } from '../adder.mjs';
+import { function_is } from '../../function/is.mjs';
+import { list_is } from '../is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function list_map_generic(list, lambda) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        list_is,
+        function_is
     ]);
-    let result = [];
-    list_each_with_index(list, (element, index) => {
-        let mapped = lambda(element, index);
-        list_add(result, mapped);
+    return list_adder(la => {
+        list_each_with_index(list, (element, index) => {
+            let mapped = lambda(element, index);
+            la(result, mapped);
+        });
     });
-    return result;
     metadata([metadata_generated()]);
 }
