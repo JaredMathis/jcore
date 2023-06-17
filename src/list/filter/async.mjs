8157b7f8359cf86c2b819c9fec4ebd11cbe3a6dd
JaredMathis <jared.mathis@native.consulting>
2023-06-17 10:36:52 -0400

    #169 function_auto list_filter_async

--- src/list/filter/async.mjs
+++ src/list/filter/async.mjs
@@ -1,3 +1,4 @@
+import { list_each_with_index_async } from '../each/with/index/async.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { list_add } from '../add.mjs';
@@ -13,6 +14,13 @@ export async function list_filter_async(array, filter) {
         if (await filter(a)) {
             list_add(result, a);
         }
+    }
+    if (false) {
+        await list_each_with_index_async(array, async a => {
+            if (await filter(a)) {
+                list_add(result, a);
+            }
+        });
     }
     return result;
     metadata([]);
