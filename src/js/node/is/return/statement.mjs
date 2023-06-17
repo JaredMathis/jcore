69f18476072f46cfab9945bdbdebb64a95c2e785
JaredMathis <jared.mathis@native.consulting>
2023-06-17 19:03:44 -0400

    #180 function_auto js_node_is_return_statement

--- src/js/node/is/return/statement.mjs
+++ src/js/node/is/return/statement.mjs
@@ -1,5 +1,7 @@
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function js_node_is_return_statement() {
-    arguments_assert(arguments, []);
-    return 'ReturnStatement';
+import { object_property_get } from '../../../../object/property/get.mjs';
+import { js_node_is } from '../../is.mjs';
+export function js_node_is_return_statement(node) {
+    arguments_assert(arguments, [js_node_is]);
+    return object_property_get(node, 'ReturnStatement');
 }
