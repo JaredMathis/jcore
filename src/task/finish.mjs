5ddc3bc7f518343032c8d29337f9d595ec08e447
JaredMathis <jared.mathis@native.consulting>
2023-06-17 09:30:17 -0400

    #169 noop

--- src/task/finish.mjs
+++ src/task/finish.mjs
@@ -1,7 +1,7 @@
 import { task_number_unhash } from './number/unhash.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
-import { task_id_remove } from './id/remove.mjs';
+import { task_unset } from './unset.mjs';
 import { task_id_get_or_null } from './id/get/or/null.mjs';
 import { task_close } from './close.mjs';
 import { task_available } from './available.mjs';
@@ -10,7 +10,7 @@ export async function task_finish() {
     let task_number_hashed = await task_id_get_or_null();
     let task_number_string = task_number_unhash(task_number_hashed);
     await task_close(task_number_string);
-    await task_id_remove();
+    await task_unset();
     return await task_available();
     metadata([]);
 }
