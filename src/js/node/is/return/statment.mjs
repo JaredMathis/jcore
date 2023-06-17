41b872618b63d1b078b0f0360e8c6253c6a54d1f
JaredMathis <jared.mathis@native.consulting>
2023-06-17 19:06:03 -0400

    #180 function_auto js_node_is_return_statment

---
+++ src/js/node/is/return/statment.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../../error.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function js_node_is_return_statment() {
+    arguments_assert(arguments, []);
+    error('todo: js_node_is_return_statment');
+}
