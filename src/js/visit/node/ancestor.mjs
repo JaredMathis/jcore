bb663a872aafcc50e6cf209c9c1bff6ae7a192bf
JaredMathis <jared.mathis@native.consulting>
2023-06-17 10:20:09 -0400

    #169 noop

--- src/js/visit/node/ancestor.mjs
+++ src/js/visit/node/ancestor.mjs
@@ -1,12 +1,12 @@
 import { js_visit_node_ancestor_generic } from './ancestor/generic.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function js_visit_node_ancestor(stack, grandparent_index) {
+export function js_visit_node_ancestor(stack, ancestor_index) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    const offset = 2;
-    let grandparent = js_visit_node_ancestor_generic(stack, grandparent_index, offset);
-    return grandparent;
+    const offset = 1;
+    let ancestor = js_visit_node_ancestor_generic(stack, ancestor_index, offset);
+    return ancestor;
 }
