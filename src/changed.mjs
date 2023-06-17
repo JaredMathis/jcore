75c21f3e2834699fc5fff1d79ebd0eddd2fe6c9c
JaredMathis <jared.mathis@native.consulting>
2023-06-17 19:16:38 -0400

    #180 function_unasyncify changed_async

--- src/changed.mjs
+++ src/changed.mjs
@@ -1,10 +1,13 @@
-import { arguments_assert_todo } from './arguments/assert/todo.mjs';
+import { metadata_generated } from './metadata/generated.mjs';
+import { metadata } from './metadata.mjs';
+import { function_is } from './function/is.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 export function changed(lambda) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [function_is]);
     let result = false;
     lambda(() => {
         result = true;
     });
     return result;
+    metadata([metadata_generated()]);
 }
