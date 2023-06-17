a7e22e88cbcaf1108bbf2382e5ed4c68d9c44cd2
JaredMathis <jared.mathis@native.consulting>
2023-06-17 12:23:16 -0400

    #174 function_auto throws_async

--- src/throws/async.mjs
+++ src/throws/async.mjs
@@ -1,12 +1,11 @@
+import { result_property_success_get } from '../result/property/success/get.mjs';
+import { not } from '../not.mjs';
 import { function_is } from '../function/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { throws_generic_async } from './generic/async.mjs';
 export async function throws_async(lambda) {
     arguments_assert(arguments, [function_is]);
-    let errored = false;
-    try {
-        await lambda();
-    } catch (e) {
-        errored = true;
-    }
+    let result = await throws_generic_async(lambda);
+    let errored = not(result_property_success_get(result));
     return errored;
 }
