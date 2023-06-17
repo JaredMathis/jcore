48d5c53445a6e560c1d42dc47eb3dc6810a8aaaf
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:24:57 -0400

    #159 noop

--- src/task/requires.mjs
+++ src/task/requires.mjs
@@ -8,10 +8,23 @@ import { string_is } from '../string/is.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { assert } from '../assert.mjs';
+import { function_is } from '../function/is.mjs';
 export async function task_requires(task_number_string, task_number_required_string) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         string_is
+    ]);
+    await task_requires_generic(task_number_string, task_number_required_string, requires_map)
+    function requires_map(requires, task_number_required) {
+        list_add_assert_exists_not(requires, task_number_required);
+    }
+}
+
+async function task_requires_generic(task_number_string, task_number_required_string, requires_map) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        string_is,
+        function_is
     ]);
     let task_number_required = integer_parse(task_number_required_string);
     assert(await task_exists(task_number_required_string));
@@ -19,10 +32,6 @@ export async function task_requires(task_number_string, task_number_required_str
     return await task_available();
     function map(body_parsed) {
         let requires = task_body_requires(body_parsed);
-        requires_map(requires);
-    }
-
-    function requires_map(requires) {
-        list_add_assert_exists_not(requires, task_number_required);
+        requires_map(requires, task_number_required);
     }
 }
