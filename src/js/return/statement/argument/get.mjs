b810487f4fbdbe05c8d4241b04afcdd3d0532ad9
JaredMathis <jared.mathis@native.consulting>
2023-06-17 19:08:13 -0400

    #180 function_auto js_return_statement_argument_get

--- src/js/return/statement/argument/get.mjs
+++ src/js/return/statement/argument/get.mjs
@@ -1,11 +1,8 @@
-import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { js_node_is_return_statement } from '../../../node/is/return/statement.mjs';
-import { assert } from '../../../../assert.mjs';
 import { js_node_property_argument_get } from '../../../node/property/argument/get.mjs';
 export function js_return_statement_argument_get(object) {
-    arguments_assert(arguments, [arguments_assert_todo]);
-    assert(js_node_is_return_statement(object));
+    arguments_assert(arguments, [js_node_is_return_statement]);
     let arg = js_node_property_argument_get(object);
     return arg;
 }
