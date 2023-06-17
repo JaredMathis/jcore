8b1c6097cdd977c6db12f69ab9519f39546b1f71
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:46:04 -0400

    #162 function_auto refactor_functions_exclamation_to_not

--- src/refactor/functions/exclamation/to/not.mjs
+++ src/refactor/functions/exclamation/to/not.mjs
@@ -1,31 +1,12 @@
+import { js_visit_nodes_types_log } from '../../../../js/visit/nodes/types/log.mjs';
 import { metadata } from '../../../../metadata.mjs';
-import { refactor_import_fix } from '../../../import/fix.mjs';
-import { refactor_arguments_assert_add_no_check } from '../../../arguments/assert/add/no/check.mjs';
-import { refactor_functions_arguments_assert_missing_add_excludes } from '../../arguments/assert/missing/add/excludes.mjs';
-import { function_name_get } from '../../../../function/name/get.mjs';
-import { js_node_call_expression_statement_if_name_equal } from '../../../../js/node/call/expression/statement/if/name/equal.mjs';
-import { list_first } from '../../../../list/first.mjs';
-import { list_empty } from '../../../../list/empty.mjs';
-import { js_function_declaration_to_statements } from '../../../../js/function/declaration/to/statements.mjs';
 import { file_js_all_map_args_if_function } from '../../../../file/js/all/map/args/if/function.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function refactor_functions_exclamation_to_not() {
     arguments_assert(arguments, []);
     await file_js_all_map_args_if_function(async function logic(args) {
         let {function_declaration} = args;
-        let exists = false;
-        let statements = js_function_declaration_to_statements(function_declaration);
-        if (!list_empty(statements)) {
-            let statement_first = list_first(statements);
-            js_node_call_expression_statement_if_name_equal(statement_first, function_name_get(arguments_assert), function on_name_equal() {
-                exists = true;
-            });
-        }
-        if (!exists) {
-            let excludes = await refactor_functions_arguments_assert_missing_add_excludes();
-            refactor_arguments_assert_add_no_check(function_declaration, excludes);
-            await refactor_import_fix(args);
-        }
+        js_visit_nodes_types_log();
     });
     metadata([]);
 }
