bd7382c10c469f9b831fd010e92c66fa8354a218
JaredMathis <jared.mathis@native.consulting>
2023-06-17 15:22:15 -0400

    #169 function_auto js_node_property_right_get

--- src/js/node/property/right/get.mjs
+++ src/js/node/property/right/get.mjs
@@ -1,7 +1,8 @@
+import { js_node_property_right } from '../right.mjs';
 import { defined_is } from '../../../../defined/is.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function js_node_property_right_get(js_node) {
     arguments_assert(arguments, [defined_is]);
-    return object_property_get(js_node, js_node_property_right1());
+    return object_property_get(js_node, js_node_property_right());
 }
