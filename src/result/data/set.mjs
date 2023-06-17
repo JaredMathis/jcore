b547813bdcccc88b8691c5162c3985c12b34a68e
JaredMathis <jared.mathis@native.consulting>
2023-06-16 21:51:46 -0400

    #154 function_auto result_data_set

--- src/result/data/set.mjs
+++ src/result/data/set.mjs
@@ -1,11 +1,11 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { object_merge } from '../../object/merge.mjs';
+import { object_property_set } from '../../object/property/set.mjs';
 export function result_data_set(result, data) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    object_merge({ data }, result);
+    object_property_set(result, 'data', data);
     return result;
 }
