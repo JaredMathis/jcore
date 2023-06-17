6e73d6a2207d12b3f11f6d347ebc053961ad6ca1
JaredMathis <jared.mathis@native.consulting>
2023-06-16 22:23:07 -0400

    #154 function_auto result_property_success_get

--- src/result/property/success/get.mjs
+++ src/result/property/success/get.mjs
@@ -1,7 +1,8 @@
 import { result_property_success } from '../success.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function result_property_success_get() {
-    arguments_assert(arguments, []);
+import { defined_is } from '../../../defined/is.mjs';
+export function result_property_success_get(result) {
+    arguments_assert(arguments, [defined_is]);
     object_property_get(result, result_property_success());
 }
