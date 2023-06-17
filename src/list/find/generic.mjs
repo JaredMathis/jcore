90ed0081ded570dbf3a895109841a260975233a5
JaredMathis <jared.mathis@native.consulting>
2023-06-17 09:56:02 -0400

    #169 function_auto list_find_generic

--- src/list/find/generic.mjs
+++ src/list/find/generic.mjs
@@ -4,7 +4,6 @@ import { assert } from '../../assert.mjs';
 import { function_is } from '../../function/is.mjs';
 import { list_is } from '../is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { list_single } from '../single.mjs';
 import { list_empty } from '../empty.mjs';
 import { list_filter } from '../filter.mjs';
 import { boolean_is } from '../../boolean/is.mjs';
@@ -20,7 +19,6 @@ export function list_find_generic(list, predicate, or_null) {
             return null;
         }
     }
-    return list_single(filtered);
     assert(list_length_is_1(filtered));
     let result = list_first(filtered);
     return result;
