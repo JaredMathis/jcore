a568b41b95c2324ef714a30becc9c8dc8a803769
JaredMathis <jared.mathis@native.consulting>
2023-06-17 10:42:13 -0400

    #169 function_unasyncify list_filter_async

--- src/list/filter.mjs
+++ src/list/filter.mjs
@@ -1,12 +1,17 @@
-import { arguments_assert } from '../arguments/assert.mjs';
+import { metadata_generated } from '../metadata/generated.mjs';
 import { metadata } from '../metadata.mjs';
-import { list_is } from './is.mjs';
+import { list_filter_generic } from './filter/generic.mjs';
 import { function_is } from '../function/is.mjs';
-export function list_filter(list, predicate) {
+import { list_is } from './is.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function list_filter(array, filter) {
     arguments_assert(arguments, [
         list_is,
         function_is
     ]);
-    return list.filter(element => predicate(element));
-    metadata([]);
+    function added_get(element, index) {
+        return element;
+    }
+    return list_filter_generic(array, filter, added_get);
+    metadata([metadata_generated()]);
 }
