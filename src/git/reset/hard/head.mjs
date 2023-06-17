9441fd52087a2f229031b49ff011fc7fd178aa8e
JaredMathis <jared.mathis@native.consulting>
2023-06-17 14:33:42 -0400

    #169 function_input_remove_last git_reset_hard_head

--- src/git/reset/hard/head.mjs
+++ src/git/reset/hard/head.mjs
@@ -1,8 +1,7 @@
 import { git_command_name } from '../../command/name.mjs';
 import { command_line_try } from '../../../command/line/try.mjs';
-import { path_is } from '../../../path/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function git_reset_hard_head(path) {
-    arguments_assert(arguments, [path_is]);
+export async function git_reset_hard_head() {
+    arguments_assert(arguments, []);
     await command_line_try(`${ git_command_name() } reset --hard HEAD`);
 }
