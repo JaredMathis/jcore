bf4f76d161bcb17ae768224c01d960b8b9d23eae
JaredMathis <jared.mathis@native.consulting>
2023-06-17 14:02:06 -0400

    #169 function_auto list_index_before

--- src/list/index/before.mjs
+++ src/list/index/before.mjs
@@ -1,13 +1,13 @@
-import { add_1 } from '../../add/1.mjs';
 import { list_index_of } from './of.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { subtract_1 } from '../../subtract/1.mjs';
 export function list_index_before(list, element_after) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
     let after_index = list_index_of(list, element_after);
-    let add_index = add_1(after_index);
-    return add_index;
+    let before_index = subtract_1(after_index);
+    return before_index;
 }
