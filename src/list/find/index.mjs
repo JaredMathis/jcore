c142628f52ad7533e88f20a253fc9e196f47b5ee
JaredMathis <jared.mathis@native.consulting>
2023-06-17 10:53:18 -0400

    #169 function_copy_suffix_add list_find index

---
+++ src/list/find/index.mjs
@@ -0,0 +1,15 @@
+import { metadata } from '../../metadata.mjs';
+import { list_filter } from '../filter.mjs';
+import { list_find_generic } from './generic.mjs';
+import { function_is } from '../../function/is.mjs';
+import { list_is } from '../is.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function list_find_index(list, predicate) {
+    arguments_assert(arguments, [
+        list_is,
+        function_is
+    ]);
+    let or_null = false;
+    return list_find_generic(list, predicate, or_null, true, list_filter);
+    metadata([]);
+}
