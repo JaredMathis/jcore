13bc2fd553c0633725301e1157aefbca09399040
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:48:51 -0400

    #162 function_auto refactor_functions_exclamation_to_not

--- src/refactor/functions/exclamation/to/not.mjs
+++ src/refactor/functions/exclamation/to/not.mjs
@@ -1,16 +1,18 @@
-import { js_visit_nodes_types_log } from '../../../../js/visit/nodes/types/log.mjs';
+import { list_adder_unique_async } from '../../../../list/adder/unique/async.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { file_js_all_map_args_if_function } from '../../../../file/js/all/map/args/if/function.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { list_adder_unique } from '../../../../list/adder/unique.mjs';
+import { js_visit_nodes_types_get } from '../../../../js/visit/nodes/types/get.mjs';
 export async function refactor_functions_exclamation_to_not() {
     arguments_assert(arguments, []);
-    list_adder_unique(la => {
-        
-    })
-    await file_js_all_map_args_if_function(async function logic(args) {
-        let {parsed} = args;
-        js_visit_nodes_types_log(parsed);
+    return await list_adder_unique_async(async la => {
+        await file_js_all_map_args_if_function(async function logic(args) {
+            let {parsed} = args;
+            let types = js_visit_nodes_types_get(parsed);
+            for (let t of types) {
+                la(t);
+            }
+        });
     });
     metadata([]);
 }
