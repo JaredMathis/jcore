0e4cbb71124bdde3c0f32146529eddbde3022abb
JaredMathis <jared.mathis@native.consulting>
2023-06-17 12:44:16 -0400

    #178 function_unasyncify list_map_generic_async

--- src/list/map/generic.mjs
+++ src/list/map/generic.mjs
@@ -1,18 +1,43 @@
 import { metadata_generated } from '../../metadata/generated.mjs';
 import { metadata } from '../../metadata.mjs';
+import { result_property_success_get } from '../../result/property/success/get.mjs';
+import { assert } from '../../assert.mjs';
+import { result_unsuccess_is } from '../../result/unsuccess/is.mjs';
+import { changed } from '../../changed.mjs';
+import { not } from '../../not.mjs';
+import { throws_generic } from '../../throws/generic.mjs';
 import { list_each_with_index } from '../each/with/index.mjs';
 import { list_adder } from '../adder.mjs';
+import { defined_is } from '../../defined/is.mjs';
+import { boolean_is } from '../../boolean/is.mjs';
 import { function_is } from '../../function/is.mjs';
 import { list_is } from '../is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function list_map_generic(list, lambda) {
+export function list_map_generic(list, lambda, allow_error_mapping, value_on_error_mapping) {
     arguments_assert(arguments, [
         list_is,
-        function_is
+        function_is,
+        boolean_is,
+        defined_is
     ]);
     return list_adder(la => {
         list_each_with_index(list, (element, index) => {
-            let mapped = lambda(element, index);
+            let result = throws_generic(() => {
+                return lambda(element, index);
+            });
+            let mapped;
+            if (not(changed(c => {
+                    if (allow_error_mapping) {
+                        if (result_unsuccess_is(result)) {
+                            mapped = value_on_error_mapping;
+                            c();
+                        }
+                    } else {
+                        assert(result_property_success_get(result));
+                    }
+                }))) {
+                mapped = result_property_data_get(result);
+            }
             la(result, mapped);
         });
     });
