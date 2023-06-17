65d17dea682ffae7f6a2d43581ae5863936edbfb
JaredMathis <jared.mathis@native.consulting>
2023-06-17 09:37:54 -0400

    #169 function_auto refactor_functions_call_arguments_to_assignments

--- src/refactor/functions/call/arguments/to/assignments.mjs
+++ src/refactor/functions/call/arguments/to/assignments.mjs
@@ -1,15 +1,5 @@
+import { js_node_is_expression_statement } from '../../../../../js/node/is/expression/statement.mjs';
 import { metadata } from '../../../../../metadata.mjs';
-import { object_replace } from '../../../../../object/replace.mjs';
-import { list_add } from '../../../../../list/add.mjs';
-import { js_node_property_arguments_get } from '../../../../../js/node/property/arguments/get.mjs';
-import { not } from '../../../../../not.mjs';
-import { function_name_get } from '../../../../../function/name/get.mjs';
-import { js_parse_call_expression } from '../../../../../js/parse/call/expression.mjs';
-import { js_node_property_argument_get } from '../../../../../js/node/property/argument/get.mjs';
-import { js_token_exclamation } from '../../../../../js/token/exclamation.mjs';
-import { equal } from '../../../../../equal.mjs';
-import { js_node_property_operator_get } from '../../../../../js/node/property/operator/get.mjs';
-import { js_node_is_unary_expression } from '../../../../../js/node/is/unary/expression.mjs';
 import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { refactor_import_fix_if_changed } from '../../../../import/fix/if/changed.mjs';
 import { file_js_all_map_args_if_function } from '../../../../../file/js/all/map/args/if/function.mjs';
@@ -19,14 +9,8 @@ export async function refactor_functions_call_arguments_to_assignments() {
     await file_js_all_map_args_if_function(async function logic(args) {
         await refactor_import_fix_if_changed(args, change => {
             let {parsed} = args;
-            js_nodes_each(parsed, js_node_is_unary_expression, n => {
-                let operator = js_node_property_operator_get(n);
-                if (equal(operator, js_token_exclamation())) {
-                    let argument = js_node_property_argument_get(n);
-                    let call_expression = js_parse_call_expression(function_name_get(not));
-                    let args = js_node_property_arguments_get(call_expression);
-                    list_add(args, argument);
-                    object_replace(n, call_expression);
+            js_nodes_each(parsed, js_node_is_expression_statement, n => {
+                if (false) {
                     change();
                 }
             });
