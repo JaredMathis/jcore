be45635ad56bd1e894659fe2addb1edc09262d08
JaredMathis <jared.mathis@native.consulting>
2023-06-17 12:22:19 -0400

    #174 function_unasyncify throws_generic_async

--- src/throws/generic.mjs
+++ src/throws/generic.mjs
@@ -1,10 +1,12 @@
-import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
+import { metadata_generated } from '../metadata/generated.mjs';
+import { metadata } from '../metadata.mjs';
 import { result_data_set } from '../result/data/set.mjs';
 import { result_unsuccess } from '../result/unsuccess.mjs';
 import { result_empty } from '../result/empty.mjs';
+import { function_is } from '../function/is.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 export function throws_generic(lambda) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [function_is]);
     let result = result_empty();
     let lambda_result;
     try {
@@ -14,4 +16,5 @@ export function throws_generic(lambda) {
     }
     result_data_set(result, lambda_result);
     return result;
+    metadata([metadata_generated()]);
 }
