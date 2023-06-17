4fdad0b2dd8332024f9e537cce1277321b7dbb60
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:02:55 -0400

    #162 function_rename file_js_all_nodes_type_first file_js_all_node_type_first

--- src/file/js/all/node/type/first.mjs
+++ src/file/js/all/node/type/first.mjs
@@ -3,7 +3,7 @@ import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs'
 import { metadata } from '../../../../../metadata.mjs';
 import { file_js_all_map_args_if_function } from '../../map/args/if/function.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export async function file_js_all_nodes_type_first(node_type) {
+export async function file_js_all_node_type_first(node_type) {
     arguments_assert(arguments, [arguments_assert_todo]);
     await file_js_all_map_args_if_function(async function logic(args) {
         let {parsed} = args;
