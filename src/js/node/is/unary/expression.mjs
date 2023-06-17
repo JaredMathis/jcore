b6ff83076801ff931b7bc7db01928ff60339675e
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:30:52 -0400

    #162 noop

--- src/js/node/is/unary/expression.mjs
+++ src/js/node/is/unary/expression.mjs
@@ -1,5 +1,8 @@
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function js_node_is_unary_expression() {
-    arguments_assert(arguments, []);
-    return 'UnaryExpression';
+import { equal } from '../../../../equal.mjs';
+import { js_node_is } from '../../is.mjs';
+import { js_node_type_get } from '../../type/get.mjs';
+export function js_node_is_unary_expression(node) {
+    arguments_assert(arguments, [js_node_is]);
+    return equal(js_node_type_get(node), 'UnaryExpression');
 }
