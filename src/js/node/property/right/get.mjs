034f3d833716452c922c1dea32f4b682f8108029
JaredMathis <jared.mathis@native.consulting>
2023-06-17 15:21:43 -0400

    #169 function_rename js_node_right js_node_property_right

--- src/js/node/property/right/get.mjs
+++ src/js/node/property/right/get.mjs
@@ -3,5 +3,5 @@ import { object_property_get } from '../../../../object/property/get.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function js_node_property_right_get(js_node) {
     arguments_assert(arguments, [defined_is]);
-    return object_property_get(js_node, js_node_property_right());
+    return object_property_get(js_node, js_node_property_right1());
 }
--- src/js/node/right.mjs
+++
@@ -1,5 +0,0 @@
-import { arguments_assert } from '../../arguments/assert.mjs';
-export function js_node_right() {
-    arguments_assert(arguments, []);
-    return 'right';
-}
