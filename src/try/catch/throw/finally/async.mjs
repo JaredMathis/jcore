6c52f257a64cb16532cd89c3c74757fff4564381
JaredMathis <jared.mathis@native.consulting>
2023-06-17 15:10:33 -0400

    #169 function_input_add_type try_catch_throw_finally_async lambda_finally function_is

--- src/try/catch/throw/finally/async.mjs
+++ src/try/catch/throw/finally/async.mjs
@@ -1,8 +1,9 @@
 import { noop } from '../../../../noop.mjs';
 import { function_is } from '../../../../function/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function try_catch_throw_finally_async(lambda_try, lambda_catch) {
+export async function try_catch_throw_finally_async(lambda_try, lambda_catch, lambda_finally) {
     arguments_assert(arguments, [
+        function_is,
         function_is,
         function_is
     ]);
