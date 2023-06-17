49811635b3e418843b1870d408c73336177d304a
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:49:51 -0400

    #162 function_copy refactor_functions_exclamation_to_not file_js_all_nodes_types_get

---
+++ src/file/js/all/nodes/types/get.mjs
@@ -0,0 +1,18 @@
+import { metadata } from '../../../../../metadata.mjs';
+import { js_visit_nodes_types_get } from '../../../../../js/visit/nodes/types/get.mjs';
+import { file_js_all_map_args_if_function } from '../../map/args/if/function.mjs';
+import { list_adder_unique_async } from '../../../../../list/adder/unique/async.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export async function file_js_all_nodes_types_get() {
+    arguments_assert(arguments, []);
+    return await list_adder_unique_async(async la => {
+        await file_js_all_map_args_if_function(async function logic(args) {
+            let {parsed} = args;
+            let types = js_visit_nodes_types_get(parsed);
+            for (let t of types) {
+                la(t);
+            }
+        });
+    });
+    metadata([]);
+}
