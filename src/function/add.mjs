f1e7c2a69634a9ce8f77ffbf239b984f71ea431d
JaredMathis <jared.mathis@native.consulting>
2023-06-17 18:13:24 -0400

    #169 function_auto function_add

--- src/function/add.mjs
+++ src/function/add.mjs
@@ -3,9 +3,11 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { function_add_with_body_error } from './add/with/body/error.mjs';
 import { file_open_vs_code } from '../file/open/vs/code.mjs';
+import { object_property_get } from '../object/property/get.mjs';
 export async function function_add(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    let function_path = await function_add_with_body_error(function_name);
+    let result = await function_add_with_body_error(function_name);
+    let function_path = object_property_get(result, 'file_path');
     await file_open_vs_code(function_path);
     metadata([]);
 }
