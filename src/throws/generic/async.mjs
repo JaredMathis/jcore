8b1d4c8e574e3997cdbf2a7804b54eb30e908497
JaredMathis <jared.mathis@native.consulting>
2023-06-17 12:21:13 -0400

    #174 function_auto throws_generic_async

--- src/throws/generic/async.mjs
+++ src/throws/generic/async.mjs
@@ -1,10 +1,10 @@
 import { result_data_set } from '../../result/data/set.mjs';
 import { result_unsuccess } from '../../result/unsuccess.mjs';
 import { result_empty } from '../../result/empty.mjs';
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { function_is } from '../../function/is.mjs';
 export async function throws_generic_async(lambda) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [function_is]);
     let result = result_empty();
     let lambda_result;
     try {
