4815346e74c6433c0c5d7cb54eb40c8234830ae5
JaredMathis <jared.mathis@native.consulting>
2023-06-17 10:41:29 -0400

    #169 function_auto list_filter_async

--- src/list/filter/async.mjs
+++ src/list/filter/async.mjs
@@ -1,5 +1,4 @@
-import { list_adder_async } from '../adder/async.mjs';
-import { list_each_with_index_async } from '../each/with/index/async.mjs';
+import { list_filter_generic_async } from './generic/async.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { list_is } from '../is.mjs';
@@ -12,13 +11,6 @@ export async function list_filter_async(array, filter) {
     function added_get(element, index) {
         return element;
     }
-    return await list_adder_async(async la => {
-        await list_each_with_index_async(array, async (element, index) => {
-            if (await filter(element)) {
-                let added = added_get(element, index);
-                la(result, added);
-            }
-        });
-    });
+    return await list_filter_generic_async(array, filter, added_get);
     metadata([]);
 }
---
+++ src/list/filter/generic/async.mjs
@@ -0,0 +1,12 @@
+import { list_each_with_index_async } from '../../each/with/index/async.mjs';
+import { list_adder_async } from '../../adder/async.mjs';
+export async function list_filter_generic_async(array, filter, added_get) {
+    return await list_adder_async(async la => {
+        await list_each_with_index_async(array, async (element, index) => {
+            if (await filter(element)) {
+                let added = added_get(element, index);
+                la(result, added);
+            }
+        });
+    });
+}
