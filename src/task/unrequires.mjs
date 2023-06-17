be7f40d5e2292012504e012fe90ca039e7dc9036
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:25:47 -0400

    #159 function_auto task_unrequires

--- src/task/unrequires.mjs
+++ src/task/unrequires.mjs
@@ -1,3 +1,4 @@
+import { list_remove } from '../list/remove.mjs';
 import { list_add_assert_exists_not } from '../list/add/assert/exists/not.mjs';
 import { task_requires_generic } from './requires/generic.mjs';
 import { string_is } from '../string/is.mjs';
@@ -10,6 +11,7 @@ export async function task_unrequires(task_number_string, task_number_required_s
     ]);
     await task_requires_generic(task_number_string, task_number_required_string, requires_map);
     function requires_map(requires, task_number_required) {
+        list_remove();
         list_add_assert_exists_not(requires, task_number_required);
     }
 }
