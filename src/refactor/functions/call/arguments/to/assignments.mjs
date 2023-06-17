a74f5cb5286d09815ff34615a661801cf8a48c3c
JaredMathis <jared.mathis@native.consulting>
2023-06-17 11:02:58 -0400

    #169 noop

--- src/refactor/functions/call/arguments/to/assignments.mjs
+++ src/refactor/functions/call/arguments/to/assignments.mjs
@@ -14,8 +14,8 @@ import { list_is } from '../../../../../list/is.mjs';
 import { list_get } from '../../../../../list/get.mjs';
 import { assert } from '../../../../../assert.mjs';
 import { js_node_is_block_statement } from '../../../../../js/node/is/block/statement.mjs';
-import { subtract_1 } from '../../../../../subtract/1.mjs';
 import { add_1 } from '../../../../../add/1.mjs';
+import { js_node_is_program } from '../../../../../js/node/is/program.mjs';
 export async function refactor_functions_call_arguments_to_assignments() {
     arguments_assert(arguments, []);
     await file_js_all_map_args_if_function(async function logic(args) {
@@ -36,6 +36,9 @@ export async function refactor_functions_call_arguments_to_assignments() {
                     let parent_list_index = list_find_first_index(stack_reversed, list_is);
                     let parent_list_index_next = add_1(parent_list_index);
                     let parent_list_next = list_get(stack_reversed, parent_list_index_next);
+                    if(js_node_is_program(parent_list_index)) {
+                        return;
+                    }
                     log({ parent_list_next });
                     assert(js_node_is_block_statement(parent_list_next));
                     for (let arg of args_reversed) {
