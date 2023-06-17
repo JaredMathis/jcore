392d22b5ef0be94cae659a1b19020c05f1de044b
JaredMathis <jared.mathis@native.consulting>
2023-06-17 10:39:21 -0400

    #169 function_auto list_filter_async

--- src/list/filter/async.mjs
+++ src/list/filter/async.mjs
@@ -2,7 +2,6 @@ import { list_adder_async } from '../adder/async.mjs';
 import { list_each_with_index_async } from '../each/with/index/async.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
-import { list_add } from '../add.mjs';
 import { list_is } from '../is.mjs';
 import { function_is } from '../../function/is.mjs';
 export async function list_filter_async(array, filter) {
@@ -10,13 +9,12 @@ export async function list_filter_async(array, filter) {
         list_is,
         function_is
     ]);
-    let result = [];
-    list_adder_async();
-    await list_each_with_index_async(array, async (element, index) => {
-        if (await filter(element)) {
-            list_add(result, element);
-        }
+    return await list_adder_async(async la => {
+        await list_each_with_index_async(array, async (element, index) => {
+            if (await filter(element)) {
+                la(result, element);
+            }
+        });
     });
-    return result;
     metadata([]);
 }
