e823e9910dc6eb0fa50875ace413e4d6ee6b496b
JaredMathis <jared.mathis@native.consulting>
2023-06-16 21:56:21 -0400

    #154 function_auto task_sub

--- src/task/sub.mjs
+++ src/task/sub.mjs
@@ -1,3 +1,4 @@
+import { result_data_get } from '../result/data/get.mjs';
 import { task_number_get_string } from './number/get/string.mjs';
 import { task_current } from './current.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
@@ -7,6 +8,7 @@ import { task_requires } from './requires.mjs';
 export async function task_sub(title) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let current_result = await task_current();
+    let current = result_data_get(current_result);
     let current_number_string = task_number_get_string(current);
     let sub = await task_add(title);
     let sub_number_string = task_number_get_string(sub);
