4b8ee379047ea7c8dee15a4a66f0d183d0cd0284
JaredMathis <jared.mathis@native.consulting>
2023-06-16 21:55:46 -0400

    #154 function_input_remove_last result_data_get

--- src/result/data/get.mjs
+++ src/result/data/get.mjs
@@ -2,11 +2,8 @@ import { result_property_data } from '../property/data.mjs';
 import { object_property_set } from '../../object/property/set.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function result_data_get(result, data) {
-    arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
-    ]);
+export function result_data_get(result) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let data = object_property_set(result, result_property_data());
     return data;
 }
