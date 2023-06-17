1998ec8e7ec53c8933df4149637c5be495f2ee31
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:12:42 -0400

    #162 function_add_js_node_type UnaryExpression

---
+++ src/js/node/is/unary/expression.mjs
@@ -0,0 +1,5 @@
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function js_node_is_unary_expression() {
+    arguments_assert(arguments, []);
+    return 'UnaryExpression';
+}
