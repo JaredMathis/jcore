38f668f5371a42ffbe823e9a90f2f77e3b115b9b
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:23:40 -0400

    #162 function_search refactor,string

--- src/refactor/functions/exclamation/to/not.mjs
+++ src/refactor/functions/exclamation/to/not.mjs
@@ -4,12 +4,15 @@ import { js_nodes_each } from '../../../../js/nodes/each.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { file_js_all_map_args_if_function } from '../../../../file/js/all/map/args/if/function.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { equal } from '../../../../equal.mjs';
 export async function refactor_functions_exclamation_to_not() {
     arguments_assert(arguments, []);
     await file_js_all_map_args_if_function(async function logic(args) {
         let {parsed} = args;
         js_nodes_each(parsed, js_node_is_unary_expression, n => {
-            if (js_node_property_operator_get()) {
+            let operator = js_node_property_operator_get(n);
+            if (equal(operator, '!')) {
+
             }
         });
     });
