6e5a0d0cf0795a4d695d2c67adc2a6abf41e423a
JaredMathis <jared.mathis@native.consulting>
2023-06-17 10:13:20 -0400

    #169 noop

--- src/js/visit/node/grandparent.mjs
+++ src/js/visit/node/grandparent.mjs
@@ -3,8 +3,8 @@ import { list_get_end } from '../../../list/get/end.mjs';
 import { js_node_is } from '../../node/is.mjs';
 import { list_filter } from '../../../list/filter.mjs';
 export function js_visit_node_grandparent(stack, grandparent_index) {
-    let stack_nodes = list_filter(stack, js_node_is);
     const offset = 2;
+    let stack_nodes = list_filter(stack, js_node_is);
     let grandparent = list_get_end(stack_nodes, add(grandparent_index, offset));
     return grandparent;
 }
