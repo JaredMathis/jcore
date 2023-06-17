96a46b9762a4149c154fd52dc8c1557d7c5b58a8
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:54:22 -0400

    #162 function_copy_suffix_add list_sort string

---
+++ src/list/sort/string.mjs
@@ -0,0 +1,9 @@
+import { subtract } from '../../subtract.mjs';
+import { identity } from '../../identity.mjs';
+import { list_sort_generic } from './generic.mjs';
+import { list_is } from '../is.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function list_sort_string(list) {
+    arguments_assert(arguments, [list_is]);
+    list_sort_generic(list, identity, false, subtract);
+}
