6fbe28dbe68b6ccb2d76cf1a71fe473d416a146a
JaredMathis <jared.mathis@native.consulting>
2023-06-17 09:18:51 -0400

    #162 noop

--- src/boolean/is.mjs
+++ src/boolean/is.mjs
@@ -1,6 +1,9 @@
 import { arguments_assert } from '../arguments/assert.mjs';
+import { assert } from '../assert.mjs';
+import { assert_arguments_count } from '../assert/arguments/count.mjs';
 import { defined_is } from '../defined/is.mjs';
 export function boolean_is(b) {
-    arguments_assert(arguments, [defined_is]);
+    assert_arguments_count(arguments, 1);
+    assert(defined_is(b));
     return b === true || b === false;
 }
--- src/not.mjs
+++ src/not.mjs
@@ -1,7 +1,9 @@
 import { boolean_is } from './boolean/is.mjs';
 import { error } from './error.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
+import { assert_arguments_count } from './assert/arguments/count.mjs';
+import { assert } from './assert.mjs';
 export function not(b) {
-    arguments_assert(arguments, [boolean_is]);
-    error('todo: not');
+    assert_arguments_count(arguments, 1);
+    return !b;
 }
