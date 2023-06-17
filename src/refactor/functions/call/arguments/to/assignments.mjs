71373ff0270f5a20172a8392f917d6ca5998f76d
JaredMathis <jared.mathis@native.consulting>
2023-06-17 18:15:05 -0400

    #169 function_auto refactor_functions_call_arguments_to_assignments

--- src/refactor/functions/call/arguments/to/assignments.mjs
+++ src/refactor/functions/call/arguments/to/assignments.mjs
@@ -1,12 +1,9 @@
 import { refactor_call_arguments_to_assignments } from '../../../../call/arguments/to/assignments.mjs';
-import { list_adder_unique_async } from '../../../../../list/adder/unique/async.mjs';
 import { metadata } from '../../../../../metadata.mjs';
 import { file_js_all_map_args_if_function } from '../../../../../file/js/all/map/args/if/function.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export async function refactor_functions_call_arguments_to_assignments() {
     arguments_assert(arguments, []);
-    return await list_adder_unique_async(async la => {
-        await file_js_all_map_args_if_function(refactor_call_arguments_to_assignments);
-    });
+    await file_js_all_map_args_if_function(refactor_call_arguments_to_assignments);
     metadata([]);
 }
