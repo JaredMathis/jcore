690d276c99954fd5b87485f7ebcf535561a5f002
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:28:21 -0400

    #158 noop

---
+++ src/task/unrequires/self.mjs
@@ -0,0 +1,12 @@
+import { string_is } from '../../string/is.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { task_unrequires } from '../unrequires.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export async function task_unrequires_self(task_number_string, task_number_required_string) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        string_is
+    ]);
+    let result = await task_unrequires(task_number_string, task_number_string);
+    return result;
+}
