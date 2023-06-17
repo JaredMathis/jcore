8737d8bec7854af9a0ec4006186d0b09b47c61d1
JaredMathis <jared.mathis@native.consulting>
2023-06-16 22:07:05 -0400

    #154 function_auto function_add_property_get

--- src/function/add/property/get.mjs
+++ src/function/add/property/get.mjs
@@ -2,13 +2,14 @@ import { js_code_call_expression_object_property_get } from '../../../js/code/ca
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { function_add_with_statement_code } from '../with/statement/code.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_add_property_get(function_name, property_prefix, property_name) {
+import { js_code_call_expression } from '../../../js/code/call/expression.mjs';
+export async function function_add_property_get(function_name, property_prefix, property_name_fn_get) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let expression_code = js_code_call_expression_object_property_get();
+    let expression_code = js_code_call_expression_object_property_get(property_prefix, js_code_call_expression(property_name_fn_get));
     let result = await function_add_with_statement_code(function_name, statement_code);
     return result;
 }
