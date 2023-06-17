a4deb0ab967e890083106bac131323828aab68fd
JaredMathis <jared.mathis@native.consulting>
2023-06-16 21:56:06 -0400

    #154 function_auto result_data_get

--- src/result/data/get.mjs
+++ src/result/data/get.mjs
@@ -1,9 +1,9 @@
 import { result_property_data } from '../property/data.mjs';
-import { object_property_set } from '../../object/property/set.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
 export function result_data_get(result) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let data = object_property_set(result, result_property_data());
+    let data = object_property_get(result, result_property_data());
     return data;
 }
