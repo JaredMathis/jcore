4c9bbbfb4ef60d693557bc320fc15f87c5b15c60
JaredMathis <jared.mathis@native.consulting>
2023-06-17 10:48:43 -0400

    #169 function_unasyncify list_filter_async

--- src/list/filter.mjs
+++ src/list/filter.mjs
@@ -1,5 +1,6 @@
 import { metadata_generated } from '../metadata/generated.mjs';
 import { metadata } from '../metadata.mjs';
+import { argument_binary_first } from '../argument/binary/first.mjs';
 import { list_filter_generic } from './filter/generic.mjs';
 import { function_is } from '../function/is.mjs';
 import { list_is } from './is.mjs';
@@ -9,9 +10,6 @@ export function list_filter(array, filter) {
         list_is,
         function_is
     ]);
-    function added_get(element, index) {
-        return element;
-    }
-    return list_filter_generic(array, filter, added_get);
+    return list_filter_generic(array, filter, argument_binary_first);
     metadata([metadata_generated()]);
 }
