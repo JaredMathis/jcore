f66347b075330f03b7b25aa2fbae579a4aba67ff
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:54:03 -0400

    #162 noop

--- src/list/sort.mjs
+++ src/list/sort.mjs
@@ -6,6 +6,5 @@ import { list_is } from './is.mjs';
 import { identity } from '../identity.mjs';
 export function list_sort(list) {
     arguments_assert(arguments, [list_is]);
-    let value_get = identity;
-    list_sort_generic(list, value_get, false, subtract);
+    list_sort_generic(list, identity, false, subtract);
 }
