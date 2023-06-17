514be5280234262e33d95839b34e90aa23dbef95
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:01:38 -0400

    #162 function_copy file_js_all_nodes_types_get file_js_all_nodes_type_first

---
+++ src/file/js/all/nodes/type/first.mjs
@@ -0,0 +1,21 @@
+import { metadata } from '../../../../../metadata.mjs';
+import { list_sort_string } from '../../../../../list/sort/string.mjs';
+import { js_visit_nodes_types_get } from '../../../../../js/visit/nodes/types/get.mjs';
+import { file_js_all_map_args_if_function } from '../../map/args/if/function.mjs';
+import { list_adder_unique_async } from '../../../../../list/adder/unique/async.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export async function file_js_all_nodes_type_first() {
+    arguments_assert(arguments, []);
+    let result = await list_adder_unique_async(async la => {
+        await file_js_all_map_args_if_function(async function logic(args) {
+            let {parsed} = args;
+            let types = js_visit_nodes_types_get(parsed);
+            for (let t of types) {
+                la(t);
+            }
+        });
+    });
+    list_sort_string(result);
+    return result;
+    metadata([]);
+}
