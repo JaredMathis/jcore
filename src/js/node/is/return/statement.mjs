9b5868acfec1eee7ea3937f1ca397306af887d60
JaredMathis <jared.mathis@native.consulting>
2023-06-17 19:03:09 -0400

    #180 function_rename js_node_type_return_statement js_node_is_return_statement

--- src/js/node/is/return/statement.mjs
+++ src/js/node/is/return/statement.mjs
@@ -1,5 +1,5 @@
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function js_node_type_return_statement() {
+export function js_node_is_return_statement() {
     arguments_assert(arguments, []);
     return 'ReturnStatement';
 }
