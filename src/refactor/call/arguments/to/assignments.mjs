feb7d25c1338c745433774b5a710aec1cd8e6842
JaredMathis <jared.mathis@native.consulting>
2023-06-17 18:13:39 -0400

    #169 function_add_mapper call_arguments_to_assignments

---
+++ src/refactor/call/arguments/to/assignments.mjs
@@ -0,0 +1,7 @@
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { error } from '../../../../error.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function refactor_call_arguments_to_assignments(args) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    error('todo: refactor_call_arguments_to_assignments');
+}
