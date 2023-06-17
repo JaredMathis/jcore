1d9e6a927925e27b7ff7dd1439bcad55dbea6bc2
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:08:34 -0400

    #162 noop

--- src/refactor/functions/exclamation/to/not.mjs
+++ src/refactor/functions/exclamation/to/not.mjs
@@ -5,14 +5,12 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { js_visit_nodes_types_get } from '../../../../js/visit/nodes/types/get.mjs';
 export async function refactor_functions_exclamation_to_not() {
     arguments_assert(arguments, []);
-    return await list_adder_unique_async(async la => {
-        await file_js_all_map_args_if_function(async function logic(args) {
-            let {parsed} = args;
-            let types = js_visit_nodes_types_get(parsed);
-            for (let t of types) {
-                la(t);
-            }
-        });
+    await file_js_all_map_args_if_function(async function logic(args) {
+        let {parsed} = args;
+        let types = js_visit_nodes_types_get(parsed);
+        for (let t of types) {
+            la(t);
+        }
     });
     metadata([]);
 }
