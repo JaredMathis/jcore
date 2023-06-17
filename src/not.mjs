eac17f21b85b75c56d643b3939f01ab0c8bea292
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:44:26 -0400

    #162 function_input_add_type not b boolean_is

--- src/not.mjs
+++ src/not.mjs
@@ -1,6 +1,7 @@
+import { boolean_is } from './boolean/is.mjs';
 import { error } from './error.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
-export function not() {
-    arguments_assert(arguments, []);
+export function not(b) {
+    arguments_assert(arguments, [boolean_is]);
     error('todo: not');
 }
