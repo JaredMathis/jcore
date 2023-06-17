1d7c9ceaa0b98a4c7f3718ed710721cb6d0218e3
JaredMathis <jared.mathis@native.consulting>
2023-06-17 14:09:06 -0400

    #169 noop

--- src/list/add/before.mjs
+++ src/list/add/before.mjs
@@ -2,12 +2,12 @@ import { list_index_before } from '../index/before.mjs';
 import { list_add_at } from './at.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function list_add_before(list, element_to_add, element_after) {
+export function list_add_before(list, element_to_add, element_before) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let before_index = list_index_before(list, element_after);
+    let before_index = list_index_before(list, element_before);
     list_add_at(list, element_to_add, before_index);
 }
