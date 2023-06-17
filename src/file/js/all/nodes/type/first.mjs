2b84c5e12fc67c2f0a85efa3dc23db19326bda97
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:02:17 -0400

    #162 function_auto file_js_all_nodes_type_first

--- src/file/js/all/nodes/type/first.mjs
+++ src/file/js/all/nodes/type/first.mjs
@@ -1,7 +1,7 @@
+import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { metadata } from '../../../../../metadata.mjs';
 import { list_sort_string } from '../../../../../list/sort/string.mjs';
-import { js_visit_nodes_types_get } from '../../../../../js/visit/nodes/types/get.mjs';
 import { file_js_all_map_args_if_function } from '../../map/args/if/function.mjs';
 import { list_adder_unique_async } from '../../../../../list/adder/unique/async.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
@@ -10,7 +10,7 @@ export async function file_js_all_nodes_type_first(node_type) {
     let result = await list_adder_unique_async(async la => {
         await file_js_all_map_args_if_function(async function logic(args) {
             let {parsed} = args;
-            let types = js_visit_nodes_types_get(parsed);
+            let types = js_nodes_each(parsed);
             for (let t of types) {
                 la(t);
             }
