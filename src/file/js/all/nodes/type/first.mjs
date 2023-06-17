f19805428487d2ba2710cd7a6343d960cfa414ca
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:01:57 -0400

    #162 function_input_add file_js_all_nodes_type_first node_type

--- src/file/js/all/nodes/type/first.mjs
+++ src/file/js/all/nodes/type/first.mjs
@@ -1,11 +1,12 @@
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { metadata } from '../../../../../metadata.mjs';
 import { list_sort_string } from '../../../../../list/sort/string.mjs';
 import { js_visit_nodes_types_get } from '../../../../../js/visit/nodes/types/get.mjs';
 import { file_js_all_map_args_if_function } from '../../map/args/if/function.mjs';
 import { list_adder_unique_async } from '../../../../../list/adder/unique/async.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export async function file_js_all_nodes_type_first() {
-    arguments_assert(arguments, []);
+export async function file_js_all_nodes_type_first(node_type) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let result = await list_adder_unique_async(async la => {
         await file_js_all_map_args_if_function(async function logic(args) {
             let {parsed} = args;
