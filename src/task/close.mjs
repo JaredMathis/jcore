27ce11c88606967f9fc90be202bee17fa73c030d
JaredMathis <jared.mathis@native.consulting>
2023-06-17 13:01:12 -0400

    #169 noop

--- src/task/close.mjs
+++ src/task/close.mjs
@@ -8,10 +8,10 @@ import { integer_parse } from '../integer/parse.mjs';
 import { task_available } from './available.mjs';
 export async function task_close(task_number_string) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    const property_value = task_property_closed();
     let task_number = integer_parse(task_number_string);
     await git_hub_repository_issue_close(task_number);
     const property_name = task_property_state();
-    const property_value = task_property_closed();
     await task_property_set_local(task_number_string, property_name, property_value);
     return await task_available();
 }
