599a06a3df4f08aa10677be26c75e1546599dade
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:18:41 -0400

    #158 function_auto function_add_property_get

--- src/function/add/property/get.mjs
+++ src/function/add/property/get.mjs
@@ -1,9 +1,9 @@
+import { js_code_return_statement } from '../../../js/code/return/statement.mjs';
 import { function_input_add_type } from '../../input/add/type.mjs';
 import { js_code_call_expression_object_property_get } from '../../../js/code/call/expression/object/property/get.mjs';
 import { function_add_with_statement_code } from '../with/statement/code.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { js_code_call_expression } from '../../../js/code/call/expression.mjs';
-import { js_code_statement } from '../../../js/code/statement.mjs';
 import { list_join } from '../../../list/join.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { function_name_separator } from '../../name/separator.mjs';
@@ -20,7 +20,7 @@ export async function function_add_property_get(property_prefix, property_name) 
         property_name
     ], function_name_separator());
     let expression_code = js_code_call_expression_object_property_get(property_prefix, js_code_call_expression(property_name_fn_get));
-    let statement_code = js_code_statement(expression_code);
+    let statement_code = js_code_return_statement(expression_code);
     let function_name = list_join([
         property_name_fn_get,
         'get'
