438c9dcdc434819f1fd6ffc2492706fc49e44e92
JaredMathis <jared.mathis@native.consulting>
2023-06-17 12:25:42 -0400

    #174 function_auto list_map_generic_async

--- src/list/map/generic/async.mjs
+++ src/list/map/generic/async.mjs
@@ -1,11 +1,12 @@
 import { list_adder_async } from '../../adder/async.mjs';
-import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_each_with_index_async } from '../../each/with/index/async.mjs';
+import { list_is } from '../../is.mjs';
+import { function_is } from '../../../function/is.mjs';
 export async function list_map_generic_async(list, lambda) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        list_is,
+        function_is
     ]);
     await list_adder_async(async la => {
         await list_each_with_index_async(list, async (element, index) => {
