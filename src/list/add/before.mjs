6b3b5b9f6449c35dcfe12cb835a9f7d7c1e8a141
JaredMathis <jared.mathis@native.consulting>
2023-06-17 14:01:13 -0400

    #169 function_copy_suffix_change list_add after before

---
+++ src/list/add/before.mjs
@@ -0,0 +1,13 @@
+import { list_add_at } from './at.mjs';
+import { list_index_after } from '../index/after.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function list_add_before(list, element_to_add, element_after) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let add_index = list_index_after(list, element_after);
+    list_add_at(list, element_to_add, add_index);
+}
