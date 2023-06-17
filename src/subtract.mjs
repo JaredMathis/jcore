95031f1c5777bdf6c26da83d239e976d88a70789
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:52:00 -0400

    #162 function_auto subtract

--- src/subtract.mjs
+++ src/subtract.mjs
@@ -1,9 +1,9 @@
 import { arguments_assert } from './arguments/assert.mjs';
-import { string_is } from './string/is.mjs';
+import { number_is } from './number/is.mjs';
 export function subtract(a, b) {
     arguments_assert(arguments, [
-        string_is,
-        string_is
+        number_is,
+        number_is
     ]);
     return a - b;
 }
