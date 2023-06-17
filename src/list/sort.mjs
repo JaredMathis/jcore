6c1e1501a1c5ff790b888ecc5bb7860da9810779
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:53:01 -0400

    #162 function_input_add_type_default list_sort_generic compare function_is subtract

--- src/list/sort.mjs
+++ src/list/sort.mjs
@@ -1,3 +1,4 @@
+import { subtract } from '../subtract.mjs';
 import { list_sort_generic } from './sort/generic.mjs';
 import { value_get } from '../value/get.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
@@ -6,5 +7,5 @@ import { identity } from '../identity.mjs';
 export function list_sort(list) {
     arguments_assert(arguments, [list_is]);
     let value_get = identity;
-    list_sort_generic(list, value_get, false);
+    list_sort_generic(list, value_get, false, subtract);
 }
--- src/list/sort/generic.mjs
+++ src/list/sort/generic.mjs
@@ -5,11 +5,12 @@ import { value_get } from '../../value/get.mjs';
 import { list_is } from '../is.mjs';
 import { function_is } from '../../function/is.mjs';
 import { boolean_is } from '../../boolean/is.mjs';
-export function list_sort_generic(list, value_get, reverse) {
+export function list_sort_generic(list, value_get, reverse, compare) {
     arguments_assert(arguments, [
         list_is,
         function_is,
-        boolean_is
+        boolean_is,
+        function_is
     ]);
     list.sort((a, b) => subtract(value_get(a), value_get(b)));
     if (reverse) {
