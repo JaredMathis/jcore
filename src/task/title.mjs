c35b189ebc51ca8dec502f0d5d65f13a3c83471c
JaredMathis <jared.mathis@native.consulting>
2023-06-16 22:15:17 -0400

    #154 function_auto task_title

--- src/task/title.mjs
+++ src/task/title.mjs
@@ -9,7 +9,7 @@ export async function task_title(issue_number, title) {
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let result = await git_hub_repository_issue_title(issue_number, title);
+    await git_hub_repository_issue_title(issue_number, title);
     const property_name = task_property_title();
     await task_property_set_local(issue_number, property_name, title);
     return await task_available();
