b856b9417f3f88ac7a23ee6d3c285dfae7ac05a3
JaredMathis <jared.mathis@native.consulting>
2023-06-16 22:02:02 -0400

    #154 function_input_add function_add_property_get property_name

--- src/function/add/property/get.mjs
+++ src/function/add/property/get.mjs
@@ -1,8 +1,9 @@
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { function_add_with_statement_code } from '../with/statement/code.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_add_property_get(function_name, statement_code) {
+export async function function_add_property_get(function_name, property_prefix, property_name) {
     arguments_assert(arguments, [
+        arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
