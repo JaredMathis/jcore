6dc7c148e1cce0df5288c1ca635654609e681b2e
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:18:33 -0400

    #167 function_auto function_add_with_statements_synchronized

--- src/function/add/with/statements/synchronized.mjs
+++ src/function/add/with/statements/synchronized.mjs
@@ -23,8 +23,11 @@ export async function function_add_with_statements_synchronized(function_name, s
     if (is_async) {
         js_function_declaration_async_add(fd);
     }
-    let result = await function_write(function_name, parsed);
+    let file_path = await function_write(function_name, parsed);
     await function_auto_after(function_name);
-    return result;
+    return {
+        function_name,
+        file_path
+    };
     metadata([]);
 }
