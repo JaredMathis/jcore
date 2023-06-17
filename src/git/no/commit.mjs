91854c662eab076f21c08ef31492ed65286a58d6
JaredMathis <jared.mathis@native.consulting>
2023-06-17 15:13:14 -0400

    #169 function_auto git_no_commit

--- src/git/no/commit.mjs
+++ src/git/no/commit.mjs
@@ -1,15 +1,14 @@
+import { try_catch_finally_async } from '../../try/catch/finally/async.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { git_rhh } from '../rhh.mjs';
 import { metadata } from '../../metadata.mjs';
 import { function_run_arguments } from '../../function/run/arguments.mjs';
-import { try_catch_throw_async } from '../../try/catch/throw/async.mjs';
 export async function git_no_commit() {
     arguments_assert(arguments, []);
-    let args= arguments
-    let result = await try_catch_throw_async(async () => {
+    let args = arguments;
+    let result = await try_catch_finally_async(async () => {
         await function_run_arguments(args);
-    })
-    await git_rhh();
+    }, git_rhh);
     return result;
     metadata([]);
 }
