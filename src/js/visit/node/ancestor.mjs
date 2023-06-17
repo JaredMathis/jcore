eddb37a7764d116b601d1aac778212a47783bf58
JaredMathis <jared.mathis@native.consulting>
2023-06-17 10:19:36 -0400

    #169 function_rename_suffix_change js_visit_node parent ancestor

--- src/js/visit/node/ancestor.mjs
+++ src/js/visit/node/ancestor.mjs
@@ -1,7 +1,7 @@
 import { js_visit_node_ancestor_generic } from './ancestor/generic.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function js_visit_node_parent(stack, grandparent_index) {
+export function js_visit_node_ancestor(stack, grandparent_index) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
