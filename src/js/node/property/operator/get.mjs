627bd9f4d3003e2b93df0fc6b13263503d37c181
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:14:27 -0400

    #162 function_add_property_get js_node operator

---
+++ src/js/node/property/operator/get.mjs
@@ -0,0 +1,7 @@
+import { defined_is } from '../../../../defined/is.mjs';
+import { object_property_get } from '../../../../object/property/get.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function js_node_property_operator_get(js_node) {
+    arguments_assert(arguments, [defined_is]);
+    return object_property_get(js_node, js_node_property_operator());
+}
