fec82446424fee29d0fc6c78ee38f0df09485231
JaredMathis <jared.mathis@native.consulting>
2023-06-17 10:02:10 -0400

    #169 function_auto_run refactor_functions_call_arguments_to_assignments

--- src/refactor/functions/call/arguments/to/assignments.mjs
+++ src/refactor/functions/call/arguments/to/assignments.mjs
@@ -5,19 +5,18 @@ import { js_node_property_expression_get } from '../../../../../js/node/property
 import { log } from '../../../../../log.mjs';
 import { js_node_is_expression_statement } from '../../../../../js/node/is/expression/statement.mjs';
 import { metadata } from '../../../../../metadata.mjs';
-import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { refactor_import_fix_if_changed } from '../../../../import/fix/if/changed.mjs';
 import { file_js_all_map_args_if_function } from '../../../../../file/js/all/map/args/if/function.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
+import { js_visit_nodes_filter } from '../../../../../js/visit/nodes/filter.mjs';
 export async function refactor_functions_call_arguments_to_assignments() {
     arguments_assert(arguments, []);
     await file_js_all_map_args_if_function(async function logic(args) {
         await refactor_import_fix_if_changed(args, change => {
-            let {parsed, file_path, parent} = args;
-            log({ args });
-            log(file_path);
-            js_nodes_each(parsed, js_node_is_expression_statement, n => {
-                let expression = js_node_property_expression_get(n);
+            let {parsed, parent} = args;
+            js_visit_nodes_filter(parsed, js_node_is_expression_statement, v => {
+                let {node} = v;
+                let expression = js_node_property_expression_get(node);
                 if (js_node_is_call_expression(expression)) {
                     let args = js_node_property_arguments_get(expression);
                     let reversed = list_reversed_get(args);
