98d0c655b061c7cdc211cf5d67afc2f7ea315a0b
JaredMathis <jared.mathis@native.consulting>
2023-06-16 21:54:03 -0400

    #154 noop

--- src/external/octokit/property/data.mjs
+++ src/external/octokit/property/data.mjs
@@ -1,4 +1,4 @@
 import { object_property_get } from '../../../object/property/get.mjs';
-export function external_octokit_property_data(issues) {
-    return object_property_get(issues, 'data');
+export function external_octokit_property_data(response) {
+    return object_property_get(response, 'data');
 }
