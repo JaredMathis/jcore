b7cc10284e713446f7a1a7f88b62e059ab43d8bd
JaredMathis <jared.mathis@native.consulting>
2023-06-17 12:38:35 -0400

    #178 function_auto list_map_generic_async

--- src/list/map/generic/async.mjs
+++ src/list/map/generic/async.mjs
@@ -1,3 +1,4 @@
+import { result_property_success_get } from '../../../result/property/success/get.mjs';
 import { boolean_is } from '../../../boolean/is.mjs';
 import { result_unsuccess_is } from '../../../result/unsuccess/is.mjs';
 import { throws_generic } from '../../../throws/generic.mjs';
@@ -6,6 +7,8 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_each_with_index_async } from '../../each/with/index/async.mjs';
 import { list_is } from '../../is.mjs';
 import { function_is } from '../../../function/is.mjs';
+import { not } from '../../../not.mjs';
+import { assert } from '../../../assert.mjs';
 export async function list_map_generic_async(list, lambda, allow_error_mapping) {
     arguments_assert(arguments, [
         list_is,
@@ -17,7 +20,11 @@ export async function list_map_generic_async(list, lambda, allow_error_mapping) 
             let result = throws_generic(async () => {
                 return await lambda(element, index);
             });
-            if (result_unsuccess_is(result)) {
+            if (not(allow_error_mapping)) {
+                assert(result_property_success_get(allow_error_mapping));
+            } else {
+                if (result_unsuccess_is(result)) {
+                }
             }
             let mapped = result_property_data_get(result);
             la(result, mapped);
