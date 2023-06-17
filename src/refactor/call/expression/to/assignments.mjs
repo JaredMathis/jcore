4d136769f5aa4f5dc70ec685c7c3ddef1dac9cdd
JaredMathis <jared.mathis@native.consulting>
2023-06-17 18:28:54 -0400

    #169 function_delete refactor_call_expression_to_assignments

--- src/refactor/call/expression/to/assignments.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../../../../error.mjs';
-import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function refactor_call_expression_to_assignments() {
-    arguments_assert(arguments, []);
-    error('todo: refactor_call_expression_to_assignments');
-}
