bb84b8071fd4ce20f26b7ba76770c366ad437e3e
JaredMathis <jared.mathis@native.consulting>
2023-06-17 09:22:28 -0400

    #162 noop

--- src/not.mjs
+++ src/not.mjs
@@ -3,7 +3,11 @@ import { error } from './error.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 import { assert_arguments_count } from './assert/arguments/count.mjs';
 import { assert } from './assert.mjs';
+import { defined_is } from './defined/is.mjs';
 export function not(b) {
     assert_arguments_count(arguments, 1);
+    if (!defined_is(b)) {
+        error('invalid b');
+    }
     return !b;
 }
