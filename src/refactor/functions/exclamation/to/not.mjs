5810dcf1341d99f56c3066cc3dba9532530f6079
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:47:53 -0400

    #162 function_search list_adder_unique

--- src/refactor/functions/exclamation/to/not.mjs
+++ src/refactor/functions/exclamation/to/not.mjs
@@ -2,8 +2,12 @@ import { js_visit_nodes_types_log } from '../../../../js/visit/nodes/types/log.m
 import { metadata } from '../../../../metadata.mjs';
 import { file_js_all_map_args_if_function } from '../../../../file/js/all/map/args/if/function.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { list_adder_unique } from '../../../../list/adder/unique.mjs';
 export async function refactor_functions_exclamation_to_not() {
     arguments_assert(arguments, []);
+    list_adder_unique(la => {
+        
+    })
     await file_js_all_map_args_if_function(async function logic(args) {
         let {parsed} = args;
         js_visit_nodes_types_log(parsed);
