0dbd6f4a772498d3435ef1eb0611b4a35198b70c
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:12:58 -0400

    #157 noop

--- src/task/later/before.mjs
+++ src/task/later/before.mjs
@@ -1,8 +1,10 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { task_later } from '../later.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { task_requires } from '../requires.mjs';
 export async function task_later_before(title) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let result = await task_later(title);
+    task_requires()
     return result;
 }
