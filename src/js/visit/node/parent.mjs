4568f0488d4044dd7ace2ea7a6e0370eacacfb1c
JaredMathis <jared.mathis@native.consulting>
2023-06-17 10:18:06 -0400

    #169 function_copy_suffix_change js_visit_node grandparent parent

---
+++ src/js/visit/node/parent.mjs
@@ -0,0 +1,12 @@
+import { js_visit_node_ancestor_generic } from './ancestor/generic.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function js_visit_node_parent(stack, grandparent_index) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    const offset = 2;
+    let grandparent = js_visit_node_ancestor_generic(stack, grandparent_index, offset);
+    return grandparent;
+}
