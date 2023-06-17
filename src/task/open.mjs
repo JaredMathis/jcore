efc72958d7cf5937ed42b585447c2e08c2a7a4b9
JaredMathis <jared.mathis@native.consulting>
2023-06-17 13:04:19 -0400

    #179 function_auto task_open

--- src/task/open.mjs
+++ src/task/open.mjs
@@ -1,9 +1,8 @@
 import { task_state_change } from './state/change.mjs';
-import { task_property_closed } from './property/closed.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { task_property_open } from './property/open.mjs';
 export async function task_open(task_number_string) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    const property_value = task_property_closed();
-    return await task_state_change(task_number_string, property_value);
+    return await task_state_change(task_number_string, task_property_open());
 }
