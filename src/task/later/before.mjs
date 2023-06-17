73d3321e83f9386249cd39b92499feaee602465a
JaredMathis <jared.mathis@native.consulting>
2023-06-17 07:14:29 -0400

    #157 function_input_swap_first_to task_later_before 1

--- src/task/later/before.mjs
+++ src/task/later/before.mjs
@@ -2,7 +2,7 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { task_later } from '../later.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { task_requires } from '../requires.mjs';
-export async function task_later_before(title, task_number_string) {
+export async function task_later_before(task_number_string, title) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
