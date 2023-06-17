29886a968e6d96afc70eff23306376c9dab5f479
JaredMathis <jared.mathis@native.consulting>
2023-06-17 19:05:23 -0400

    #180 function_delete js_node_is_return_statement

--- src/js/node/is/return/statement.mjs
+++
@@ -1,7 +0,0 @@
-import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { object_property_get } from '../../../../object/property/get.mjs';
-import { js_node_is } from '../../is.mjs';
-export function js_node_is_return_statement(node) {
-    arguments_assert(arguments, [js_node_is]);
-    return object_property_get(node, 'ReturnStatement');
-}
