fcc1e762f40a5ae6524562e9d0b17ec2ce79c7a0
JaredMathis <jared.mathis@native.consulting>
2023-06-17 08:29:33 -0400

    #162 noop

--- src/function/add/js/node/type.mjs
+++ src/function/add/js/node/type.mjs
@@ -1,8 +1,10 @@
 import { function_add_string_prefix_camel_generic } from '../../string/prefix/camel/generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { comment } from '../../../../comment.mjs';
 export async function function_add_js_node_type(value) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let prefix = 'js_node_is';
+    comment('this needs to add js_node_is_* as well')
+    let prefix = 'js_node_type';
     return await function_add_string_prefix_camel_generic(prefix, value);
 }
