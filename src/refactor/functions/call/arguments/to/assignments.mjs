485460f74e82ed8aa388a78f2776d2a57db71fbf
JaredMathis <jared.mathis@native.consulting>
2023-06-17 14:14:51 -0400

    #169 noop

--- src/refactor/functions/call/arguments/to/assignments.mjs
+++ src/refactor/functions/call/arguments/to/assignments.mjs
@@ -15,7 +15,6 @@ import { js_node_is_call_expression } from '../../../../../js/node/is/call/expre
 import { js_node_property_expression_get } from '../../../../../js/node/property/expression/get.mjs';
 import { js_node_is_expression_statement } from '../../../../../js/node/is/expression/statement.mjs';
 import { metadata } from '../../../../../metadata.mjs';
-import { refactor_import_fix_if_changed } from '../../../../import/fix/if/changed.mjs';
 import { file_js_all_map_args_if_function } from '../../../../../file/js/all/map/args/if/function.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { js_visit_nodes_filter } from '../../../../../js/visit/nodes/filter.mjs';
@@ -30,57 +29,58 @@ import { list_single } from '../../../../../list/single.mjs';
 import { object_replace } from '../../../../../object/replace.mjs';
 import { assert_message } from '../../../../../assert/message.mjs';
 import { js_parse_expression } from '../../../../../js/parse/expression.mjs';
-import { list_add_after } from '../../../../../list/add/after.mjs';
 import { list_add_before } from '../../../../../list/add/before.mjs';
 export async function refactor_functions_call_arguments_to_assignments() {
     arguments_assert(arguments, []);
     await file_js_all_map_args_if_function(async function logic(args) {
-        await refactor_import_fix_if_changed(args, change => {
-            let parsed = object_property_get(args, 'parsed');
-            let unparsed = object_property_get(args, 'unparsed');
-            js_visit_nodes_filter(parsed, js_node_is_expression_statement, v => {
-                let node = object_property_get(v, 'node');
-                let stack = object_property_get(v, 'stack');
-                let expression = js_node_property_expression_get(node);
-                if (js_node_is_call_expression(expression)) {
-                    let stack_reversed = list_reversed_get(stack);
-                    let index_starting_at = 0;
-                    let list_find_first_after_result = list_find_first_after(stack_reversed, index_starting_at);
-                    let parent_list_next = object_property_get(list_find_first_after_result, 'next');
-                    let parent_list_index = object_property_get(list_find_first_after_result, 'index');
-                    if (js_node_is_program(parent_list_next)) {
-                        return;
-                    }
-                    if (js_node_is_variable_declaration(parent_list_next)) {
-                        list_find_first_after_result = list_find_first_after(stack_reversed, parent_list_index);
-                        parent_list_next = object_property_get(list_find_first_after_result, 'next');
-                        parent_list_index = object_property_get(list_find_first_after_result, 'index');
-                        return;
-                    }
-                    assert_message(js_node_is_block_statement(parent_list_next), json_to({
-                        s: list_map_try(stack_reversed, js_node_property_type_get),
-                        n: js_node_property_type_get(parent_list_next)
-                    }));
-                    let parent_list = list_get(stack_reversed, parent_list_index);
-                    let args = js_node_property_arguments_get(expression);
-                    let args_reversed = list_reversed_get(args);
-                    for (let arg of args_reversed) {
-                        if (js_node_is_call_expression(arg)) {
-                            let id = js_identifier_name_next(parsed);
-                            let assignment_code = js_code_statement_assignment(id, string_a());
-                            let assignment = js_parse_statement(assignment_code);
-                            let declarations = js_node_property_declarations_get(assignment);
-                            let declaration = list_single(declarations);
-                            js_variable_declarator_init_change(declaration, object_copy_shallow(arg));
-                            object_replace(arg, js_parse_expression(id));
-                            list_add_before(parent_list, assignment, node);
-                            if (false) {
-                                change();
-                            }
-                        }
+        let parsed = object_property_get(args, 'parsed');
+        let unparsed = object_property_get(args, 'unparsed');
+        js_visit_nodes_filter(parsed, js_node_is_expression_statement, v => {
+            let node = object_property_get(v, 'node');
+            let stack = object_property_get(v, 'stack');
+            let expression = js_node_property_expression_get(node);
+            if (js_node_is_call_expression(expression)) {
+                refactor_call_expression_to_assignments();
+            }
+            function refactor_call_expression_to_assignments() {
+                let stack_reversed = list_reversed_get(stack);
+                let index_starting_at = 0;
+                let list_find_first_after_result = list_find_first_after(stack_reversed, index_starting_at);
+                let parent_list_next = object_property_get(list_find_first_after_result, 'next');
+                let parent_list_index = object_property_get(list_find_first_after_result, 'index');
+                if (js_node_is_program(parent_list_next)) {
+                    return;
+                }
+                if (js_node_is_variable_declaration(parent_list_next)) {
+                    list_find_first_after_result = list_find_first_after(stack_reversed, parent_list_index);
+                    parent_list_next = object_property_get(list_find_first_after_result, 'next');
+                    parent_list_index = object_property_get(list_find_first_after_result, 'index');
+                    return;
+                }
+                let v_2 = json_to({
+                    s: list_map_try(stack_reversed, js_node_property_type_get),
+                    n: js_node_property_type_get(parent_list_next)
+                });
+                let v_3 = js_node_is_block_statement(parent_list_next);
+                assert_message(v_3, v_2);
+                let parent_list = list_get(stack_reversed, parent_list_index);
+                let args = js_node_property_arguments_get(expression);
+                let args_reversed = list_reversed_get(args);
+                for (let arg of args_reversed) {
+                    if (js_node_is_call_expression(arg)) {
+                        let id = js_identifier_name_next(parsed);
+                        let assignment_code = js_code_statement_assignment(id, string_a());
+                        let assignment = js_parse_statement(assignment_code);
+                        let declarations = js_node_property_declarations_get(assignment);
+                        let declaration = list_single(declarations);
+                        let v_4 = object_copy_shallow(arg);
+                        js_variable_declarator_init_change(declaration, v_4);
+                        let v_5 = js_parse_expression(id);
+                        object_replace(arg, v_5);
+                        list_add_before(parent_list, assignment, node);
                     }
                 }
-            });
+            }
         });
         function list_find_first_after(stack_reversed, index_starting_at) {
             let index = list_find_first_index_after(stack_reversed, list_is, index_starting_at);
