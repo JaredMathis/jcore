87e8307ed2d9d943f311177ced93d9e65f3f1235
JaredMathis <jared.mathis@native.consulting>
2023-06-17 12:35:19 -0400

    #178 function_auto list_map_async

--- src/list/map/async.mjs
+++ src/list/map/async.mjs
@@ -1,5 +1,3 @@
-import { result_unsuccess_is } from '../../result/unsuccess/is.mjs';
-import { throws_generic } from '../../throws/generic.mjs';
 import { list_map_generic_async } from './generic/async.mjs';
 import { metadata } from '../../metadata.mjs';
 import { function_is } from '../../function/is.mjs';
@@ -11,12 +9,7 @@ export async function list_map_async(list, mapper) {
         function_is
     ]);
     let lambda = async (element, index) => {
-        let result = throws_generic(async () => {
-            return await mapper(element);
-        });
-        if (result_unsuccess_is(result)) {
-        }
-        return result;
+        return await mapper(element);
     };
     return await list_map_generic_async(list, lambda);
     metadata([]);
