b840eff275fcc0191a5644ab33151e7a99dcebdb
JaredMathis <jared.mathis@native.consulting>
2023-06-17 15:12:20 -0400

    #169 function_copy try_catch_throw_async try_catch_finally_async

---
+++ src/try/catch/finally/async.mjs
@@ -0,0 +1,11 @@
+import { noop } from '../../../noop.mjs';
+import { try_catch_throw_finally_async } from '../throw/finally/async.mjs';
+import { function_is } from '../../../function/is.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export async function try_catch_finally_async(lambda_try, lambda_catch) {
+    arguments_assert(arguments, [
+        function_is,
+        function_is
+    ]);
+    return await try_catch_throw_finally_async(lambda_try, lambda_catch, noop);
+}
