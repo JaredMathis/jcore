106bce336fd09bfe23e97fe077beb666a41037cf
JaredMathis <jared.mathis@native.consulting>
2023-06-17 14:10:12 -0400

    #169 function_auto list_add_before

--- src/list/add/before.mjs
+++ src/list/add/before.mjs
@@ -1,13 +1,13 @@
-import { list_index_before } from '../index/before.mjs';
 import { list_add_at } from './at.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { list_index_of } from '../index/of.mjs';
 export function list_add_before(list, element_to_add, element_before) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let before_index = list_index_before(list, element_before);
+    let before_index = list_index_of(list, element_before);
     list_add_at(list, element_to_add, before_index);
 }
--- src/list/index/before.mjs
+++ src/list/index/before.mjs
@@ -2,12 +2,12 @@ import { list_index_of } from './of.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { subtract_1 } from '../../subtract/1.mjs';
-export function list_index_before(list, element_after) {
+export function list_index_before(list, element_before) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let after_index = list_index_of(list, element_after);
+    let after_index = list_index_of(list, element_before);
     let before_index = subtract_1(after_index);
     return before_index;
 }
