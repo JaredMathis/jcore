85d2066726b4b01684d780dc7dcea15ec592d940
JaredMathis <jared.mathis@native.consulting>
2023-06-18 08:58:54 -0400

    #183 function_copy function_wrap function_wrap_with

---
+++ src/function/wrap/with.mjs
@@ -0,0 +1,11 @@
+import { noop } from '../../noop.mjs';
+import { function_wrap_generic } from './generic.mjs';
+import { string_identifier_is } from '../../string/identifier/is.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export async function function_wrap_with(function_name_to_wrap, function_name_to_add) {
+    arguments_assert(arguments, [
+        string_identifier_is,
+        string_identifier_is
+    ]);
+    await function_wrap_generic(function_name_to_wrap, function_name_to_add, noop);
+}
