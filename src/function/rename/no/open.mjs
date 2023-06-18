9d6d50376737e68ec1c770d5fc9d3b23051b5337
JaredMathis <jared.mathis@native.consulting>
2023-06-18 11:21:41 -0400

    #204 function_auto function_rename_no_open

--- src/function/rename/no/open.mjs
+++ src/function/rename/no/open.mjs
@@ -1,3 +1,4 @@
+import { file_js_all_identifier_multiple } from '../../../file/js/all/identifier/multiple.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { function_rename_after } from '../after.mjs';
 import { file_js_all_identifier_rename } from '../../../file/js/all/identifier/rename.mjs';
@@ -10,6 +11,7 @@ export async function function_rename_no_open(function_name_old, function_name_n
         string_identifier_is
     ]);
     let tests_renames = await function_rename_without_all_refactor(function_name_old, function_name_new);
+    let file_paths_tests_changed = await file_js_all_identifier_multiple(tests_renames);
     let file_paths_changed = await file_js_all_identifier_rename(function_name_old, function_name_new);
     await function_rename_after(file_paths_changed);
     metadata([]);
