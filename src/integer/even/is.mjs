f13a538c1d751a3fb038d20268053d84cf6c6f8e
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:03:23 -0400

    #193 function_input_remove_last integer_even_is

--- src/integer/even/is.mjs
+++ src/integer/even/is.mjs
@@ -1,11 +1,8 @@
 import { integer_is } from '../is.mjs';
 import { integer_mod } from '../mod.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function integer_even_is(a, b) {
-    arguments_assert(arguments, [
-        integer_is,
-        integer_is
-    ]);
+export function integer_even_is(a) {
+    arguments_assert(arguments, [integer_is]);
     let result = integer_mod(a, 2);
     return result;
 }
