d9aaa0573151f3b5d89aab3eacd012efb4702cfa
JaredMathis <jared.mathis@native.consulting>
2023-06-18 11:30:01 -0400

    #211 function_auto task_sub_undo

---
+++ src/task/sub/undo.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function task_sub_undo() {
+    arguments_assert(arguments, []);
+    error('todo: task_sub_undo');
+}
