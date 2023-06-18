cf5757348590e45b4d6f9452819770c2aba23ae5
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:28:50 -0400

    #197 function_rename_suffix_add integer_divide evenly

---
+++ src/integer/divide/evenly/test/generated/5.mjs
@@ -0,0 +1,11 @@
+import { metadata_generated } from '../../../../../metadata/generated.mjs';
+import { metadata } from '../../../../../metadata.mjs';
+import { integer_divide_evenly } from '../../../evenly.mjs';
+import { assert_throws } from '../../../../../assert/throws.mjs';
+import { assert } from '../../../../../assert.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export function integer_divide_test_generated_5() {
+    arguments_assert(arguments, []);
+    assert(assert_throws(() => integer_divide_evenly(-2, 3)));
+    metadata([metadata_generated()]);
+}
