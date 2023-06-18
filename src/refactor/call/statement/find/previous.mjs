80ff203f4d410811d1c31f9c4a2ef93ae33908c2
JaredMathis <jared.mathis@native.consulting>
2023-06-18 18:12:54 -0400

    #230 function_auto refactor_call_statement_find_previous

--- src/refactor/call/statement/find/previous.mjs
+++ src/refactor/call/statement/find/previous.mjs
@@ -1,3 +1,4 @@
+import { log_multiple_map } from '../../../../log/multiple/map.mjs';
 import { js_node_call_expression_if_name_equal } from '../../../../js/node/call/expression/if/name/equal.mjs';
 import { list_index_of } from '../../../../list/index/of.mjs';
 import { js_unparse } from '../../../../js/unparse.mjs';
@@ -9,20 +10,25 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { subtract_1 } from '../../../../subtract/1.mjs';
 import { list_get } from '../../../../list/get.mjs';
 import { error } from '../../../../error.mjs';
-import { log_multiple } from '../../../../log/multiple.mjs';
 export function refactor_call_statement_find_previous(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {function_name_find} = args;
     let function_name = js_mapper_args_to_function_name(args);
     js_visit_call_statements(args, (stack_reversed, node, expression, parent_list) => {
         js_node_call_expression_if_name_equal(expression, function_name_find, () => {
-            log({function_name,function_name_find});
+            log({
+                function_name,
+                function_name_find
+            });
             let node_string = js_unparse(expression);
             let index = list_index_of(parent_list, node);
             let index_previous = subtract_1(index);
             let previous = list_get(parent_list, index_previous);
-            let statements = [previous, node];
-            log_multiple(statements);
+            let statements = [
+                previous,
+                node
+            ];
+            log_multiple_map(statements, js_unparse);
             log('');
             error();
         });
