8f1bd06ccb2e84808214f3eec5f165d51834ab18
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:07:08 -0400

    #192 function_auto string_repeat_is

--- src/string/repeat/is.mjs
+++ src/string/repeat/is.mjs
@@ -1,8 +1,11 @@
+import { integer_even_is } from '../../integer/even/is.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
-import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_length } from '../length.mjs';
+import { not } from '../../not.mjs';
 export function string_repeat_is(input) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let length = string_length(input);
+    if (not(integer_even_is(length))) {
+    }
 }
