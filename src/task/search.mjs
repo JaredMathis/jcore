2477f5d1c20407b12b126da8f01eb0c71a0a27f1
JaredMathis <jared.mathis@native.consulting>
2023-06-18 17:19:42 -0400

    #229 function_auto task_search

--- src/task/search.mjs
+++ src/task/search.mjs
@@ -1,4 +1,4 @@
-import { identity } from '../identity.mjs';
+import { task_property_title_get } from './property/title/get.mjs';
 import { search_generic } from '../search/generic.mjs';
 import { string_is } from '../string/is.mjs';
 import { task_all } from './all.mjs';
@@ -6,5 +6,5 @@ import { arguments_assert } from '../arguments/assert.mjs';
 export async function task_search(query) {
     arguments_assert(arguments, [string_is]);
     let candidates = await task_all();
-    return search_generic(candidates, query, identity);
+    return search_generic(candidates, query, task_property_title_get);
 }
