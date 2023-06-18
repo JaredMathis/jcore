c2b2633d9a16e2c7d7684b8f02e84e7fd02acccc
JaredMathis <jared.mathis@native.consulting>
2023-06-18 17:07:55 -0400

    #228 function_auto refactor_call_statement_find

--- src/refactor/call/statement/find.mjs
+++ src/refactor/call/statement/find.mjs
@@ -1,9 +1,13 @@
+import { js_visit_call_statements } from '../../../js/visit/call/statements.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
-import { error } from '../../../error.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { log } from '../../../log.mjs';
 export function refactor_call_statement_find(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     js_visit_call_statements(args, (stack_reversed, node, expression, parent_list) => {
-
+        log({
+            node,
+            expression
+        });
     });
 }
