d476481095a343189a76b67f63f755133fa8c23e
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:29:56 -0400

    #197 function_auto number_zero_is

--- src/number/zero/is.mjs
+++ src/number/zero/is.mjs
@@ -1,7 +1,7 @@
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { equal } from '../../equal.mjs';
-import { integer_is } from '../../integer/is.mjs';
+import { number_is } from '../is.mjs';
 export function number_zero_is(a) {
-    arguments_assert(arguments, [integer_is]);
+    arguments_assert(arguments, [number_is]);
     return equal(a, 0);
 }
