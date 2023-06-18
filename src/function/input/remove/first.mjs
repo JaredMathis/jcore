1f1fd22f6a14bcee88b76601baf83b19bc9be32b
JaredMathis <jared.mathis@native.consulting>
2023-06-18 13:33:08 -0400

    #218 function_rename refactor_input_first_remove refactor_input_remove_first

--- src/function/input/remove/first.mjs
+++ src/function/input/remove/first.mjs
@@ -1,10 +1,10 @@
-import { refactor_input_first_remove } from '../../../refactor/input/first/remove.mjs';
+import { refactor_input_remove_first } from '../../../refactor/input/remove/first.mjs';
 import { function_name_get } from '../../name/get.mjs';
 import { function_map_with_args } from '../../map/with/args.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function function_input_remove_first(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let v = function_name_get(refactor_input_first_remove);
+    let v = function_name_get(refactor_input_remove_first);
     await function_map_with_args(v, function_name, {});
 }
--- src/refactor/input/remove/first.mjs
+++ src/refactor/input/remove/first.mjs
@@ -2,7 +2,7 @@ import { refactor_input_generic_simple } from '../generic/simple.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_first_remove } from '../../../list/first/remove.mjs';
-export async function refactor_input_first_remove(args) {
+export async function refactor_input_remove_first(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let lambda = list_first_remove;
     await refactor_input_generic_simple(args, lambda);
