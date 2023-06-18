a9d3740c6decbbc7041889f91c129c9b5388e45b
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:12:10 -0400

    #192 function_auto integer_divide

---
+++ src/integer/divide.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function integer_divide() {
+    arguments_assert(arguments, []);
+    error('todo: integer_divide');
+}
