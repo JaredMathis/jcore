9efbcc395fc35be048be43fb5a09aff23421be36
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:04:35 -0400

    #193 function_auto integer_even_is

--- src/integer/even/is.mjs
+++ src/integer/even/is.mjs
@@ -1,8 +1,9 @@
 import { integer_is } from '../is.mjs';
 import { integer_mod } from '../mod.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { equal } from '../../equal.mjs';
 export function integer_even_is(a) {
     arguments_assert(arguments, [integer_is]);
-    let result = integer_mod(a, 2);
-    return result;
+    let result_mod = integer_mod(a, 2);
+    return equal(result_mod, 0);
 }
