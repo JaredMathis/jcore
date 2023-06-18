0581c47c00c3418224dc056860dcfa3fbb32f6a4
JaredMathis <jared.mathis@native.consulting>
2023-06-18 15:38:20 -0400

    #222 function_auto function_input_swap_named

--- src/function/input/swap/named.mjs
+++ src/function/input/swap/named.mjs
@@ -1,12 +1,15 @@
+import { function_inputs_index_of_string } from '../../inputs/index/of/string.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { function_input_swap } from '../swap.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_input_swap_named(function_name, index_from, index_to) {
+export async function function_input_swap_named(function_name, input_name_from, input_name_to) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo,
         arguments_assert_todo
     ]);
+    let index_from = function_inputs_index_of_string(input_name_from);
+    let index_to = function_inputs_index_of_string(input_name_to);
     let result = await function_input_swap(function_name, index_from, index_to);
     return result;
 }
