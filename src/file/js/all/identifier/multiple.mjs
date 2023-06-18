3bdec793328df72c0be7c88c9eab46e0a76979bd
JaredMathis <jared.mathis@native.consulting>
2023-06-18 11:20:45 -0400

    #208 function_auto file_js_all_identifier_rename

---
+++ src/file/js/all/identifier/multiple.mjs
@@ -0,0 +1,19 @@
+import { list_add_property_generic } from '../../../../list/add/property/generic.mjs';
+import { js_identifier_rename } from '../../../../js/identifier/rename.mjs';
+import { changed } from '../../../../changed.mjs';
+import { object_keys_each } from '../../../../object/keys/each.mjs';
+import { file_js_all_map_args } from '../map/args.mjs';
+import { list_adder_async } from '../../../../list/adder/async.mjs';
+export async function file_js_all_identifier_multiple(renames) {
+    let file_paths_changed = await list_adder_async(async la => {
+        await file_js_all_map_args(function mapper(args) {
+            object_keys_each(renames, (identifier_name_new, identifier_name_old) => {
+                let changed = js_identifier_rename(args, identifier_name_old, identifier_name_new);
+                if (changed) {
+                    list_add_property_generic(la, args, 'file_path');
+                }
+            });
+        });
+    });
+    return file_paths_changed;
+}
--- src/file/js/all/identifier/rename.mjs
+++ src/file/js/all/identifier/rename.mjs
@@ -1,28 +1,13 @@
-import { object_keys_each } from '../../../../object/keys/each.mjs';
-import { list_adder_async } from '../../../../list/adder/async.mjs';
-import { list_add_property_generic } from '../../../../list/add/property/generic.mjs';
-import { changed } from '../../../../changed.mjs';
+import { file_js_all_identifier_multiple } from './multiple.mjs';
 import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
-import { js_identifier_rename } from '../../../../js/identifier/rename.mjs';
-import { file_js_all_map_args } from '../map/args.mjs';
 export async function file_js_all_identifier_rename(identifier_name_old, identifier_name_new) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is
     ]);
     let renames = { [identifier_name_old]: identifier_name_new };
-    let file_paths_changed = await list_adder_async(async la => {
-        await file_js_all_map_args(function mapper(args) {
-            object_keys_each(renames, (identifier_name_new, identifier_name_old) => {
-                let changed = js_identifier_rename(args, identifier_name_old, identifier_name_new);
-                if (changed) {
-                    list_add_property_generic(la, args, 'file_path');
-                }
-            });
-        });
-    });
-    return file_paths_changed;
+    return await file_js_all_identifier_multiple(renames);
     metadata([]);
 }
