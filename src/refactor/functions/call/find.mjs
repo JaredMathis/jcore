c78089fa30a6c62f63bfac8af04a8cb9936260a1
JaredMathis <jared.mathis@native.consulting>
2023-06-18 16:26:38 -0400

    #227 function_auto refactor_functions_call_find

--- src/refactor/functions/call/find.mjs
+++ src/refactor/functions/call/find.mjs
@@ -4,7 +4,5 @@ import { refactor_call_find } from '../../call/find.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function refactor_functions_call_find(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let refactor_fn = refactor_call_find;
-    let args_additional = {};
-    await file_js_all_map_args_if_function_args(refactor_fn, args_additional);
+    await file_js_all_map_args_if_function_args(refactor_call_find, {});
 }
