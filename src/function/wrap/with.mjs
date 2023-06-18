4c3f939e9c94a4d6eccf9f43b7f498697ac82e34
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:02:34 -0400

    #183 function_auto function_wrap_with

--- src/function/wrap/with.mjs
+++ src/function/wrap/with.mjs
@@ -1,3 +1,4 @@
+import { function_inputs } from '../inputs.mjs';
 import { function_wrap_generic } from './generic.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -7,6 +8,7 @@ export async function function_wrap_with(function_name_to_wrap, function_name_to
         string_identifier_is,
         string_identifier_is
     ]);
+    let function_name_map_inputs = await function_inputs(function_name_map);
     await function_wrap_generic(function_name_to_wrap, function_name_to_add, function map(args) {
         let {identifier, statements_code} = args;
         let index = 1;
