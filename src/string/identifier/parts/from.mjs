ecd65c3b1ab6d45053b1b4e0eb77564d656a0973
JaredMathis <jared.mathis@native.consulting>
2023-06-18 08:52:34 -0400

    #183 function_auto string_identifier_parts_from

--- src/string/identifier/parts/from.mjs
+++ src/string/identifier/parts/from.mjs
@@ -1,6 +1,9 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { function_name_separator } from '../../../function/name/separator.mjs';
 import { list_join } from '../../../list/join.mjs';
 export function string_identifier_parts_from(list_parts) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let v = function_name_separator();
     let v_2 = list_join(list_parts, v);
     return v_2;
