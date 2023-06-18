c30faa737d8e5bdfa5a4426357502040e464a1cc
JaredMathis <jared.mathis@native.consulting>
2023-06-18 11:51:54 -0400

    #197 function_auto function_tests_generate_after

--- src/function/tests/generate/after.mjs
+++ src/function/tests/generate/after.mjs
@@ -1,14 +1,15 @@
+import { tests_name } from '../../../tests/name.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { log } from '../../../log.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
-import { function_name_get } from '../../name/get.mjs';
 import { command_line } from '../../../command/line.mjs';
-import { tests } from '../../../tests.mjs';
 import { comment } from '../../../comment.mjs';
 import { tests_generate } from '../../../tests/generate.mjs';
 export async function function_tests_generate_after() {
+    arguments_assert(arguments, []);
     await tests_generate();
-    comment(`NodeJS will not re-import ${ tests } after we re-generate it so we must run through command line`);
-    let cl_result = await command_line(`node run.mjs ${ function_name_get(tests) }`);
+    comment(`NodeJS will not re-import ${ tests_name() } after we re-generate it so we must run through command line`);
+    let cl_result = await command_line(`node run.mjs ${ tests_name() }`);
     let stdout = object_property_get(cl_result, 'stdout');
     log(stdout);
 }
