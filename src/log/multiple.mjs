74f3cec39d9159f1cdde9ff9052f25719196e8fd
JaredMathis <jared.mathis@native.consulting>
2023-06-18 18:11:51 -0400

    #230 function_auto log_multiple

--- src/log/multiple.mjs
+++ src/log/multiple.mjs
@@ -2,10 +2,10 @@ import { identity } from '../identity.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { log } from '../log.mjs';
-export function log_multiple(logs) {
+export function log_multiple(list) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let mapper = identity;
-    for (let message of logs) {
+    for (let message of list) {
         log(message);
     }
 }
