d6f34d5b51586f4f55871749288e1afa2243d013
JaredMathis <jared.mathis@native.consulting>
2023-06-18 17:52:58 -0400

    #230 function_auto refactor_call_statement_find_previous

--- src/refactor/call/statement/find/previous.mjs
+++ src/refactor/call/statement/find/previous.mjs
@@ -1,7 +1,16 @@
+import { js_unparse } from '../../../../js/unparse.mjs';
+import { js_visit_call_statements } from '../../../../js/visit/call/statements.mjs';
+import { log } from '../../../../log.mjs';
+import { js_mapper_args_to_function_name } from '../../../../js/mapper/args/to/function/name.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function refactor_call_statement_find_previous(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: refactor_call_statement_find_previous');
+    let function_name = js_mapper_args_to_function_name(args);
+    log(function_name);
+    js_visit_call_statements(args, (stack_reversed, node, expression, parent_list) => {
+        let node_string = js_unparse(node);
+        log(node_string);
+    });
+    log('');
 }
