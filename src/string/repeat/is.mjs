b02385c767577475f51a33cce65cd7b2c103ff4c
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:52:41 -0400

    #193 task_sub integer_even_is

--- src/string/repeat/is.mjs
+++ src/string/repeat/is.mjs
@@ -1,7 +1,8 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { string_length } from '../length.mjs';
 export function string_repeat_is(input) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: string_repeat_is');
+    let length = string_length(input);
 }
