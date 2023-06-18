b49b2fccca71155e8ca95b4ea20a324908dadb50
JaredMathis <jared.mathis@native.consulting>
2023-06-18 07:48:09 -0400

    #180 noop

--- src/list/copy/shallow/add/multiple.mjs
+++ src/list/copy/shallow/add/multiple.mjs
@@ -3,7 +3,7 @@ import { object_copy_shallow } from '../../../../object/copy/shallow.mjs';
 export function list_copy_shallow_add_multiple(refactor_stack, multiple) {
     let refactor_stack_child = object_copy_shallow(refactor_stack);
     for (let m of multiple) {
-        list_add(refactor_stack_child, child);
+        list_add(refactor_stack_child, m);
     }
     return refactor_stack_child;
 }
