f5e2597f8ad8338f6017bbd29651fcd08b09063e
JaredMathis <jared.mathis@native.consulting>
2023-06-18 14:52:53 -0400

    #222 noop

--- src/refactor/function/name/from/parts.mjs
+++ src/refactor/function/name/from/parts.mjs
@@ -1,8 +1,9 @@
 import { string_identifier_parts_from_combine } from '../../../../string/identifier/parts/from/combine.mjs';
 import { refactor_prefix } from '../../../prefix.mjs';
 export function refactor_function_name_from_parts(function_name_suffix_parts) {
+    const prefix = refactor_prefix();
     const list_with_multiple_lists = [
-        [refactor_prefix()],
+        [prefix],
         function_name_suffix_parts
     ];
     let function_name_refactor = string_identifier_parts_from_combine(list_with_multiple_lists);
