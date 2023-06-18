7267c70ff7d308688e32c464d79b7c5e76f3a006
JaredMathis <jared.mathis@native.consulting>
2023-06-18 18:04:15 -0400

    #230 function_input_remove_last js_call_expression_name_get

--- src/js/call/expression/name/get.mjs
+++ src/js/call/expression/name/get.mjs
@@ -1,12 +1,8 @@
-import { boolean_is } from '../../../../boolean/is.mjs';
 import { js_node_is_call_expression } from '../../../node/is/call/expression.mjs';
 import { js_call_expression_name_generic } from './generic.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function js_call_expression_name_get(ce, or_null) {
-    arguments_assert(arguments, [
-        js_node_is_call_expression,
-        boolean_is
-    ]);
+export function js_call_expression_name_get(ce) {
+    arguments_assert(arguments, [js_node_is_call_expression]);
     let result = js_call_expression_name_generic(ce, false);
     return result;
 }
