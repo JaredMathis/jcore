0c6a68dd9f52816f56cb5d64f1ad9b88f9c7046f
JaredMathis <jared.mathis@native.consulting>
2023-06-18 11:11:55 -0400

    #204 function_delete function_rename_identifiers_and_after

--- src/function/rename/identifiers/and/after.mjs
+++
@@ -1,6 +0,0 @@
-import { function_rename_after } from '../../after.mjs';
-import { file_js_all_identifier_rename } from '../../../../file/js/all/identifier/rename.mjs';
-export async function function_rename_identifiers_and_after(function_name_old, function_name_new) {
-    let file_paths_changed = await file_js_all_identifier_rename(function_name_old, function_name_new);
-    await function_rename_after(file_paths_changed);
-}
