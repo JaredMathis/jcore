676af79d739dc1c9419db9ef0c5e67ea44ce5f4c
JaredMathis <jared.mathis@native.consulting>
2023-06-18 14:35:58 -0400

    #222 function_input_swap_first_to_named function_add_with_call_expression_synchronized function_name

--- src/function/add/mapper.mjs
+++ src/function/add/mapper.mjs
@@ -48,7 +48,7 @@ export async function function_add_mapper(function_name_suffix) {
         js_brace_left_right()
     ];
     let function_name_to_call = function_name_get(fn);
-    await function_add_with_call_expression_synchronized(function_name_to_call, expression_code_args, function_name);
+    await function_add_with_call_expression_synchronized(function_name, function_name_to_call, expression_code_args);
     let v_2 = function_name_get(string_identifier_is);
     await function_input_add_type(function_name, input, v_2);
     await function_open_vs_code(function_name);
--- src/function/add/with/call/expression/synchronized.mjs
+++ src/function/add/with/call/expression/synchronized.mjs
@@ -1,8 +1,15 @@
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_add_with_statement_synchronized } from '../../statement/synchronized.mjs';
 import { js_code_statement } from '../../../../../js/code/statement.mjs';
 import { js_code_await } from '../../../../../js/code/await.mjs';
 import { js_code_call_expression_with_args } from '../../../../../js/code/call/expression/with/args.mjs';
-export async function function_add_with_call_expression_synchronized(function_name_to_call, expression_code_args, function_name) {
+export async function function_add_with_call_expression_synchronized(function_name, function_name_to_call, expression_code_args) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let expression_code = js_code_call_expression_with_args(function_name_to_call, expression_code_args);
     let awaited = js_code_await(expression_code);
     let statement_code = js_code_statement(awaited);
