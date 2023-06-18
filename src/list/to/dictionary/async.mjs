4b4c9e2491a5745395486851818710b532144b17
JaredMathis <jared.mathis@native.consulting>
2023-06-18 17:27:20 -0400

    #61 function_asyncify list_to_dictionary

---
+++ src/list/to/dictionary/async.mjs
@@ -0,0 +1,17 @@
+import { object_property_initialize } from '../../../object/property/initialize.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export async function list_to_dictionary_async(list, element_to_key, element_to_value) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let dictionary = {};
+    for (let element of list) {
+        let key = element_to_key(element);
+        let value = element_to_value(element);
+        object_property_initialize(dictionary, key, value);
+    }
+    return dictionary;
+}
