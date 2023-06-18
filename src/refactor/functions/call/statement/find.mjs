3bf8d1349c256927e93d92001d3295d10d34e370
JaredMathis <jared.mathis@native.consulting>
2023-06-18 17:36:35 -0400

    #61 function_search function_name_to_declaration

--- src/refactor/functions/call/statement/find.mjs
+++ src/refactor/functions/call/statement/find.mjs
@@ -4,5 +4,6 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 export async function refactor_functions_call_statement_find(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
+    let function_declaration = await function_name_to_declaration
     await file_js_all_map_args_if_function_args(refactor_call_statement_find, { function_name });
 }
