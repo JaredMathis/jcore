d394d67362a63f4917b9c728947b4ad5a1188987
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:33:46 -0400

    #200 noop

--- src/function/rename/prefix/change.mjs
+++ src/function/rename/prefix/change.mjs
@@ -5,7 +5,7 @@ import { function_name_separator } from '../../name/separator.mjs';
 import { string_identifier_sub_is } from '../../../string/identifier/sub/is.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_rename_prefix_change(function_name_old_without_suffix, suffix_old, suffix_new) {
+export async function function_rename_prefix_change(function_name_old_without_prefix, prefix_old, prefix_new) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_sub_is,
@@ -13,9 +13,9 @@ export async function function_rename_prefix_change(function_name_old_without_su
     ]);
     let v = function_name_separator();
     let function_name_old = list_join([
-        function_name_old_without_suffix,
-        suffix_old
+        function_name_old_without_prefix,
+        prefix_old
     ], v);
-    let function_name_new = string_suffix_replace(function_name_old, suffix_old, suffix_new);
+    let function_name_new = string_suffix_replace(function_name_old, prefix_old, prefix_new);
     await function_rename_single(function_name_old, function_name_new);
 }
