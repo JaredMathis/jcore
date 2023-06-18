43eeb0b80c2e520d9c53f46a44a4e68343e68b85
JaredMathis <jared.mathis@native.consulting>
2023-06-18 07:48:57 -0400

    #180 function_auto list_copy_shallow_add_multiple

---
+++ src/list/copy.mjs
@@ -0,0 +1,4 @@
+import { list_skip } from './skip.mjs';
+export function list_copy(refactor_stack) {
+    return list_skip(refactor_stack, 0);
+}
--- src/list/copy/shallow/add/multiple.mjs
+++ src/list/copy/shallow/add/multiple.mjs
@@ -1,7 +1,13 @@
+import { list_copy } from '../../../copy.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { list_add } from '../../../add.mjs';
-import { object_copy_shallow } from '../../../../object/copy/shallow.mjs';
 export function list_copy_shallow_add_multiple(refactor_stack, multiple) {
-    let refactor_stack_child = object_copy_shallow(refactor_stack);
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let refactor_stack_child = list_copy(refactor_stack);
     for (let m of multiple) {
         list_add(refactor_stack_child, m);
     }
