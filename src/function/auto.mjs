e4e1105f74750de9bad2838977ac3831a5c86ce7
JaredMathis <jared.mathis@native.consulting>
2023-06-18 12:56:59 -0400

    #191 function_auto function_auto

--- src/function/auto.mjs
+++ src/function/auto.mjs
@@ -1,3 +1,4 @@
+import { string_repeat_is } from '../string/repeat/is.mjs';
 import { not } from '../not.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
@@ -6,8 +7,11 @@ import { function_auto_after } from './auto/after.mjs';
 import { function_exists } from './exists.mjs';
 import { function_add } from './add.mjs';
 import { function_open_vs_code } from './open/vs/code.mjs';
+import { assert } from '../assert.mjs';
 export async function function_auto(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
+    if (false)
+        assert(not(string_repeat_is(function_name)));
     if (not(await function_exists(function_name))) {
         await function_add(function_name);
     } else {
