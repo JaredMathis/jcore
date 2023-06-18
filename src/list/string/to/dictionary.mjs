4db37a46d98534eb0b64b85e993088f5fb66d0e1
JaredMathis <jared.mathis@native.consulting>
2023-06-18 17:30:13 -0400

    #61 function_unasyncify list_string_to_dictionary_async

--- src/list/string/to/dictionary.mjs
+++ src/list/string/to/dictionary.mjs
@@ -1,18 +1,14 @@
 import { metadata_generated } from '../../../metadata/generated.mjs';
 import { metadata } from '../../../metadata.mjs';
-import { object_property_initialize } from '../../../object/property/initialize.mjs';
+import { identity } from '../../../identity.mjs';
+import { list_to_dictionary } from '../../to/dictionary.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function list_string_to_dictionary(keys, key_to_value) {
+export function list_string_to_dictionary(list, element_to_value) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let dictionary = {};
-    for (let key of keys) {
-        let value = key_to_value(key);
-        object_property_initialize(dictionary, key, value);
-    }
-    return dictionary;
+    return list_to_dictionary(list, identity, element_to_value);
     metadata([metadata_generated()]);
 }
