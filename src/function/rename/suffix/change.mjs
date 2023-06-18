83b50c09f9fd07df5fed1fddfce3d833c1e9afe9
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:36:56 -0400

    #200 function_auto function_rename_suffix_change

--- src/function/rename/suffix/change.mjs
+++ src/function/rename/suffix/change.mjs
@@ -1,10 +1,8 @@
-import { list_join } from '../../../list/join.mjs';
+import { function_rename_suffix_generic } from './generic.mjs';
 import { string_identifier_sub_is } from '../../../string/identifier/sub/is.mjs';
-import { function_rename_single } from '../single.mjs';
 import { string_suffix_replace } from '../../../string/suffix/replace.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
-import { function_name_separator } from '../../name/separator.mjs';
 export async function function_rename_suffix_change(function_name_old_without_suffix, suffix_old, suffix_new) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -15,9 +13,6 @@ export async function function_rename_suffix_change(function_name_old_without_su
         function_name_old_without_suffix,
         suffix_old
     ];
-    let function_name_old_to_new = function_name_old => string_suffix_replace(function_name_old, suffix_old, suffix_new)
-    let v = function_name_separator();
-    let function_name_old = list_join(function_name_old_parts, v);
-    let function_name_new = function_name_old_to_new(function_name_old);
-    await function_rename_single(function_name_old, function_name_new);
+    let function_name_old_to_new = function_name_old => string_suffix_replace(function_name_old, suffix_old, suffix_new);
+    await function_rename_suffix_generic(function_name_old_parts, function_name_old_to_new);
 }
---
+++ src/function/rename/suffix/generic.mjs
@@ -0,0 +1,9 @@
+import { function_rename_single } from '../single.mjs';
+import { list_join } from '../../../list/join.mjs';
+import { function_name_separator } from '../../name/separator.mjs';
+export async function function_rename_suffix_generic(function_name_old_parts, function_name_old_to_new) {
+    let v = function_name_separator();
+    let function_name_old = list_join(function_name_old_parts, v);
+    let function_name_new = function_name_old_to_new(function_name_old);
+    await function_rename_single(function_name_old, function_name_new);
+}
