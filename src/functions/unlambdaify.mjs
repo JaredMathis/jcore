dea573d440dbd448866258a8c18fc166891de477
JaredMathis <jared.mathis@native.consulting>
2023-06-18 15:49:00 -0400

    #221 function_rename functions_unlambdaify refactor_functions_unlambdaify

--- src/functions/unlambdaify.mjs
+++
@@ -1,7 +0,0 @@
-import { refactor_unlambdaify } from '../refactor/unlambdaify.mjs';
-import { file_js_all_map_args_if_function } from '../file/js/all/map/args/if/function.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
-export async function functions_unlambdaify() {
-    arguments_assert(arguments, []);
-    await file_js_all_map_args_if_function(refactor_unlambdaify);
-}
