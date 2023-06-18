6f89fb15fd6c686a3fc1cc53e5a061a3f54cdb0a
JaredMathis <jared.mathis@native.consulting>
2023-06-18 13:33:57 -0400

    #218 noop

--- src/function/input/remove/at.mjs
+++ src/function/input/remove/at.mjs
@@ -3,8 +3,9 @@ import { refactor_input_remove_first } from '../../../refactor/input/remove/firs
 import { function_name_get } from '../../name/get.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { refactor_input_remove_at } from '../../../refactor/input/remove/at.mjs';
 export async function function_input_remove_at(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let v = function_name_get(refactor_input_remove_first);
+    let v = function_name_get(refactor_input_remove_at);
     await function_map_with_args(v, function_name, {});
 }
