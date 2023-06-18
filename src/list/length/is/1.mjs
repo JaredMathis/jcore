2819ce517c644521a7001c385491dca33c9a1383
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:17:03 -0400

    #183 noop

--- src/list/length/is/1.mjs
+++ src/list/length/is/1.mjs
@@ -2,9 +2,9 @@ import { list_is } from '../../is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { list_length_is_value } from './value.mjs';
-export function list_length_is_1(specifiers) {
+export function list_length_is_1(list) {
     arguments_assert(arguments, [list_is]);
-    let v = list_length_is_value(specifiers, 1);
+    let v = list_length_is_value(list, 1);
     return v;
     metadata([]);
 }
