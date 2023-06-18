6cc67d1320f8090841c2ab33331580ccf0e8f65c
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:02:49 -0400

    #183 function_auto function_wrap_with

--- src/function/wrap/with.mjs
+++ src/function/wrap/with.mjs
@@ -2,6 +2,8 @@ import { function_inputs } from '../inputs.mjs';
 import { function_wrap_generic } from './generic.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { assert } from '../../assert.mjs';
+import { list_length_is_1 } from '../../list/length/is/1.mjs';
 export async function function_wrap_with(function_name_to_wrap, function_name_to_add, function_name_map) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -9,6 +11,7 @@ export async function function_wrap_with(function_name_to_wrap, function_name_to
         string_identifier_is
     ]);
     let function_name_map_inputs = await function_inputs(function_name_map);
+    assert(list_length_is_1(function_name_map_inputs));
     await function_wrap_generic(function_name_to_wrap, function_name_to_add, function map(args) {
         let {identifier, statements_code} = args;
         let index = 1;
