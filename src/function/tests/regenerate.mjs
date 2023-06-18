c975d2412731aca480211e07ef3dffe8ab4a113d
JaredMathis <jared.mathis@native.consulting>
2023-06-18 11:54:37 -0400

    #196 function_tests_regenerate integer_divide_evenly

--- src/function/tests/regenerate.mjs
+++ src/function/tests/regenerate.mjs
@@ -4,7 +4,7 @@ import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function function_tests_regenerate(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
-    await function_tests_generated_delete_no_after(function_name);
+    let result = await function_tests_generated_delete_no_after(function_name);
     await function_tests_generate_after();
     return result;
 }
