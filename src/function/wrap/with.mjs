bff032f2c05a883483fb187d767aad838839c781
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:48:03 -0400

    #182 function_auto function_wrap_with

--- src/function/wrap/with.mjs
+++ src/function/wrap/with.mjs
@@ -1,9 +1,8 @@
+import { list_single_item } from '../../list/single/item.mjs';
 import { list_replace } from '../../list/replace.mjs';
 import { function_wrap_generic_identifier_result } from './generic/identifier/result.mjs';
 import { js_code_return_statement } from '../../js/code/return/statement.mjs';
 import { js_code_function_declaration_to_statement_assignment_wrapped } from '../../js/code/function/declaration/to/statement/assignment/wrapped.mjs';
-import { list_length_is_value } from '../../list/length/is/value.mjs';
-import { list_single } from '../../list/single.mjs';
 import { js_function_declaration_to_params_names } from '../../js/function/declaration/to/params/names.mjs';
 import { function_to_declaration } from '../to/declaration.mjs';
 import { js_code_function_declaration_to_statement_assignment } from '../../js/code/function/declaration/to/statement/assignment.mjs';
@@ -13,8 +12,6 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { assert } from '../../assert.mjs';
 import { list_length_is_1 } from '../../list/length/is/1.mjs';
 import { comment } from '../../comment.mjs';
-import { list_last_index } from '../../list/last/index.mjs';
-import { list_last_remove } from '../../list/last/remove.mjs';
 export async function function_wrap_with(function_name_to_wrap, function_name_to_add, function_name_map) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -31,7 +28,7 @@ export async function function_wrap_with(function_name_to_wrap, function_name_to
         const identifier_mapped = 'result_to_be_mapped';
         let statements_code_new = [
             js_code_function_declaration_to_statement_assignment_wrapped(function_declaration, identifier_mapped),
-            js_code_function_declaration_to_statement_assignment(function_declaration_map, list_single(identifier_mapped), identifier_result),
+            js_code_function_declaration_to_statement_assignment(function_declaration_map, list_single_item(identifier_mapped), identifier_result),
             js_code_return_statement(identifier_result)
         ];
         list_replace(statements_code, statements_code_new);
