ca865c5aa3046bea83520a2fab57e380781b26d5
JaredMathis <jared.mathis@native.consulting>
2023-06-18 18:44:19 -0400

    #232 function_auto js_statements_refactorable

---
+++ src/js/statements/refactorable.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function js_statements_refactorable() {
+    arguments_assert(arguments, []);
+    error('todo: js_statements_refactorable');
+}
