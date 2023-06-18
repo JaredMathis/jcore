1909ada66870d4dcaaf31faddd9051e5aa5706d8
JaredMathis <jared.mathis@native.consulting>
2023-06-18 13:34:46 -0400

    #218 function_input_add function_input_remove_at index_string

--- src/function/input/remove/at.mjs
+++ src/function/input/remove/at.mjs
@@ -1,11 +1,13 @@
 import { function_map_with_args } from '../../map/with/args.mjs';
-import { refactor_input_remove_first } from '../../../refactor/input/remove/first.mjs';
 import { function_name_get } from '../../name/get.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { refactor_input_remove_at } from '../../../refactor/input/remove/at.mjs';
-export async function function_input_remove_at(function_name) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+export async function function_input_remove_at(index_string, function_name) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let v = function_name_get(refactor_input_remove_at);
     await function_map_with_args(v, function_name, {});
 }
