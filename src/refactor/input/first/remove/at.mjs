95aafeddc1637e233dd5c0c6758beb8f68d82fa1
JaredMathis <jared.mathis@native.consulting>
2023-06-18 13:30:02 -0400

    #218 function_auto refactor_input_first_remove_at

--- src/refactor/input/first/remove/at.mjs
+++ src/refactor/input/first/remove/at.mjs
@@ -4,6 +4,7 @@ import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function refactor_input_first_remove_at(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let {input_index} = args;
     let lambda = list => list_remove_at(list, input_index);
     await refactor_input_generic_simple(args, lambda);
 }
