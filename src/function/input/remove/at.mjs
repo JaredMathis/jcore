45967c747b347528b5e1b81985ac52d370b38772
JaredMathis <jared.mathis@native.consulting>
2023-06-18 13:38:28 -0400

    #218 function_input_swap_first_2 function_input_remove_at

--- src/function/input/remove/at.mjs
+++ src/function/input/remove/at.mjs
@@ -3,7 +3,7 @@ import { function_name_get } from '../../name/get.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { refactor_input_remove_at } from '../../../refactor/input/remove/at.mjs';
-export async function function_input_remove_at(index_string, function_name) {
+export async function function_input_remove_at(function_name, index_string) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
