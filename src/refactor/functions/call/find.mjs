09b1bbc7972a17462544691a97ef58047aad80d1
JaredMathis <jared.mathis@native.consulting>
2023-06-18 16:24:23 -0400

    #227 noop

--- src/refactor/functions/call/find.mjs
+++ src/refactor/functions/call/find.mjs
@@ -4,5 +4,5 @@ import { file_js_all_map_args_if_function } from '../../../file/js/all/map/args/
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function refactor_functions_call_find(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    await file_js_all_map_args_if_function(refactor_call_find);
+    await file_js_all_map_args_if_function(args => refactor_call_find(args));
 }
