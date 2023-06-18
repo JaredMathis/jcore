c7d74cf5fdf7ea6f453b516edae4c67dd3e5bdea
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:59:45 -0400

    #194 function_tests_generate integer_mod

---
+++ src/integer/mod/test/generated/4.mjs
@@ -0,0 +1,13 @@
+import { metadata_generated } from '../../../../metadata/generated.mjs';
+import { metadata } from '../../../../metadata.mjs';
+import { json_equal } from '../../../../json/equal.mjs';
+import { assert } from '../../../../assert.mjs';
+import { integer_mod } from '../../../mod.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function integer_mod_test_generated_4() {
+    arguments_assert(arguments, []);
+    let expected = 0;
+    let actual = integer_mod(3, -1);
+    assert(json_equal(actual, expected));
+    metadata([metadata_generated()]);
+}
