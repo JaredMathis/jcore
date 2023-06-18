b549f441ade7dc37c91fe1f3efc97c31a0b138aa
JaredMathis <jared.mathis@native.consulting>
2023-06-18 17:50:50 -0400

    #230 function_auto refactor_functions_call_statement_find_previous

--- src/refactor/functions/call/statement/find/previous.mjs
+++ src/refactor/functions/call/statement/find/previous.mjs
@@ -1,7 +1,8 @@
 import { refactor_call_statement_find_previous } from '../../../../call/statement/find/previous.mjs';
 import { file_js_all_map_args_if_function_args } from '../../../../../file/js/all/map/args/if/function/args.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export async function refactor_functions_call_statement_find_previous() {
-    arguments_assert(arguments, []);
-    await file_js_all_map_args_if_function_args(refactor_call_statement_find_previous, {});
+import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
+export async function refactor_functions_call_statement_find_previous(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
+    await file_js_all_map_args_if_function_args(refactor_call_statement_find_previous, { function_name });
 }
