2cbeb2a4c56dbadd9aa923f4b6f6c51616553bed
JaredMathis <jared.mathis@native.consulting>
2023-06-18 11:22:01 -0400

    #204 function_auto function_rename_no_open

--- src/function/rename/no/open.mjs
+++ src/function/rename/no/open.mjs
@@ -5,6 +5,7 @@ import { file_js_all_identifier_rename } from '../../../file/js/all/identifier/r
 import { function_rename_without_all_refactor } from '../without/all/refactor.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { list_multiple_combine } from '../../../list/multiple/combine.mjs';
 export async function function_rename_no_open(function_name_old, function_name_new) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -13,6 +14,9 @@ export async function function_rename_no_open(function_name_old, function_name_n
     let tests_renames = await function_rename_without_all_refactor(function_name_old, function_name_new);
     let file_paths_tests_changed = await file_js_all_identifier_multiple(tests_renames);
     let file_paths_changed = await file_js_all_identifier_rename(function_name_old, function_name_new);
-    await function_rename_after(file_paths_changed);
+    await function_rename_after(list_multiple_combine([
+        file_paths_tests_changed,
+        file_paths_changed
+    ]));
     metadata([]);
 }
