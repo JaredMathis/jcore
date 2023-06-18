3c403f75b084727f1bd47c22508cb9ae59912a40
JaredMathis <jared.mathis@native.consulting>
2023-06-18 13:24:15 -0400

    #217 function_auto function_input_add_last_before

--- src/function/input/add/last/before.mjs
+++ src/function/input/add/last/before.mjs
@@ -1,14 +1,14 @@
 import { function_inputs } from '../../../inputs.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { function_input_swap_last_2 } from '../../swap/last/2.mjs';
-import { function_input_add } from '../../add.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { list_last_index } from '../../../../list/last/index.mjs';
+import { function_input_add_at } from '../at.mjs';
 export async function function_input_add_last_before(function_name, input_name) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
     let inputs = await function_inputs(input_name);
-    await function_input_add(function_name, input_name);
-    await function_input_swap_last_2(function_name);
+    let last_index = list_last_index(inputs);
+    await function_input_add_at(function_name, input_name, last_index);
 }
