af0b292b8ec14ebdac719bcbf195a08d62a1a999
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:59:38 -0400

    #201 function_auto task_unsub

--- src/task/unsub.mjs
+++ src/task/unsub.mjs
@@ -1,26 +1,18 @@
-import { list_last_string_to } from '../list/last/string/to.mjs';
-import { if_else_async } from '../if/else/async.mjs';
+import { task_unsub_generic } from './unsub/generic.mjs';
 import { result_property_success_get } from '../result/property/success/get.mjs';
 import { result_property_data_get } from '../result/property/data/get.mjs';
 import { result_unsuccess } from '../result/unsuccess.mjs';
 import { result_property_data_set } from '../result/property/data/set.mjs';
 import { result_empty } from '../result/empty.mjs';
 import { task_set } from './set.mjs';
-import { task_current_required_bys } from './current/required/bys.mjs';
 import { task_finish } from './finish.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { task_current } from './current.mjs';
-import { list_empty } from '../list/empty.mjs';
 import { assert } from '../assert.mjs';
 export async function task_unsub() {
     arguments_assert(arguments, []);
     let r = result_empty();
-    let required_bys = await task_current_required_bys();
-    const if_check = list_empty(required_bys);
-    return await if_else_async(if_check, on_if, async () => {
-        let last_string = list_last_string_to(required_bys);
-        return await on_else(last_string);
-    });
+    return await task_unsub_generic(on_if, on_else);
     async function on_else(last_string) {
         await task_finish();
         await task_set(last_string);
