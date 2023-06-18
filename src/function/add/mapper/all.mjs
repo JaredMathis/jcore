88708c91d0ed086215baa748c3304b304f1b681f
JaredMathis <jared.mathis@native.consulting>
2023-06-18 15:40:42 -0400

    #222 function_auto function_add_mapper_all

--- src/function/add/mapper/all.mjs
+++ src/function/add/mapper/all.mjs
@@ -1,11 +1,11 @@
 import { function_add_mapper_generic } from './generic.mjs';
 import { function_map_with_args } from '../../map/with/args.mjs';
-import { function_input_add_type } from '../../input/add/type.mjs';
 import { js_brace_left_right } from '../../../js/brace/left/right.mjs';
 import { function_name_get } from '../../name/get.mjs';
 import { js_code_call_expression_with_args } from '../../../js/code/call/expression/with/args.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { noop } from '../../../noop.mjs';
 export async function function_add_mapper_all(function_name_suffix) {
     arguments_assert(arguments, [string_identifier_is]);
     function expression_code_args_get(function_name_refactor) {
@@ -16,14 +16,8 @@ export async function function_add_mapper_all(function_name_suffix) {
         ];
         return expression_code_args;
     }
-    async function add_after(function_name_function) {
-        let input = 'function_name';
-        let v_2 = function_name_get(string_identifier_is);
-        await function_input_add_type(function_name_function, input, v_2);
-        return input;
-    }
     let fn = function_map_with_args;
     let function_name_to_call = function_name_get(fn);
     const prefix_function = 'function';
-    let input = await function_add_mapper_generic(prefix_function, function_name_suffix, function_name_to_call, expression_code_args_get, add_after);
+    let input = await function_add_mapper_generic(prefix_function, function_name_suffix, function_name_to_call, expression_code_args_get, noop);
 }
