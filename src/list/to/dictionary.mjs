dd9a633be5e4837f269b9a9a81ff50db09369d99
JaredMathis <jared.mathis@native.consulting>
2023-06-18 17:28:53 -0400

    #61 function_unasyncify list_to_dictionary_async

--- src/list/to/dictionary.mjs
+++ src/list/to/dictionary.mjs
@@ -1,5 +1,14 @@
+import { metadata_generated } from '../../metadata/generated.mjs';
+import { metadata } from '../../metadata.mjs';
 import { object_property_initialize } from '../../object/property/initialize.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 export function list_to_dictionary(list, element_to_key, element_to_value) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let dictionary = {};
     for (let element of list) {
         let key = element_to_key(element);
@@ -7,4 +16,5 @@ export function list_to_dictionary(list, element_to_key, element_to_value) {
         object_property_initialize(dictionary, key, value);
     }
     return dictionary;
+    metadata([metadata_generated()]);
 }
