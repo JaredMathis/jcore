57de2054606ef34ef719098f44213fca64dfebb2
JaredMathis <jared.mathis@native.consulting>
2023-06-18 18:44:58 -0400

    #232 function_input_add_multiple js_statements_refactorable left,right

--- src/js/statements/refactorable.mjs
+++ src/js/statements/refactorable.mjs
@@ -1,6 +1,10 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function js_statements_refactorable() {
-    arguments_assert(arguments, []);
+export function js_statements_refactorable(left, right) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     error('todo: js_statements_refactorable');
 }
