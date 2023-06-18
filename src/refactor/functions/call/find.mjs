8d05bc1e09c0dbb0b1f593af47b8e2b27a44f1d5
JaredMathis <jared.mathis@native.consulting>
2023-06-18 16:23:32 -0400

    #227 function_input_add refactor_functions_call_find function_name

--- src/refactor/functions/call/find.mjs
+++ src/refactor/functions/call/find.mjs
@@ -1,7 +1,8 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { refactor_call_find } from '../../call/find.mjs';
 import { file_js_all_map_args_if_function } from '../../../file/js/all/map/args/if/function.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function refactor_functions_call_find() {
-    arguments_assert(arguments, []);
+export async function refactor_functions_call_find(function_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     await file_js_all_map_args_if_function(refactor_call_find);
 }
