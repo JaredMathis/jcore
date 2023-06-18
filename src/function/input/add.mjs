58e033e7680ac9e27f057626e67ffc8e4c093a5d
JaredMathis <jared.mathis@native.consulting>
2023-06-18 14:28:32 -0400

    #222 function_auto function_input_add

--- src/function/input/add.mjs
+++ src/function/input/add.mjs
@@ -1,3 +1,4 @@
+import { function_inputs } from '../inputs.mjs';
 import { function_input_add_at } from './add/at.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -7,7 +8,7 @@ export async function function_input_add(function_name, input_name) {
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let inputs = await function_intpus(function_name);
+    let inputs = await function_inputs(function_name);
     const input_index = list_last_index(inputs);
     await function_input_add_at(function_name, input_name, input_index);
 }
