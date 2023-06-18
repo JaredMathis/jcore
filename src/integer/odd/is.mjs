ffb06fba65e9faa6fb2b74d38761712d28223df4
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:08:36 -0400

    #192 function_copy integer_even_is integer_odd_is

---
+++ src/integer/odd/is.mjs
@@ -0,0 +1,9 @@
+import { equal } from '../../equal.mjs';
+import { integer_mod } from '../mod.mjs';
+import { integer_is } from '../is.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function integer_odd_is(a) {
+    arguments_assert(arguments, [integer_is]);
+    let result_mod = integer_mod(a, 2);
+    return equal(result_mod, 0);
+}
