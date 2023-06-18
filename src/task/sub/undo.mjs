53e0da44d6c973f9f1304e3c7b1d1f2e286874da
JaredMathis <jared.mathis@native.consulting>
2023-06-18 11:31:51 -0400

    #211 function_auto task_sub_undo

--- src/task/sub/undo.mjs
+++ src/task/sub/undo.mjs
@@ -1,3 +1,4 @@
+import { task_number_get_string } from '../number/get/string.mjs';
 import { task_unrequires } from '../unrequires.mjs';
 import { task_unsub_preview } from '../unsub/preview.mjs';
 import { task_current } from '../current.mjs';
@@ -6,5 +7,5 @@ export async function task_sub_undo() {
     arguments_assert(arguments, []);
     let current = await task_current();
     let previous = await task_unsub_preview();
-    await task_unrequires();
+    await task_unrequires(task_number_get_string(current), task_number_get_string(previous));
 }
