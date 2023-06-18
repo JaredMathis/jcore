684ce4fbc1db97405c0a3fc26c252743d01642f5
JaredMathis <jared.mathis@native.consulting>
2023-06-18 17:29:54 -0400

    #61 function_auto list_string_to_dictionary_async

--- src/list/string/to/dictionary/async.mjs
+++ src/list/string/to/dictionary/async.mjs
@@ -1,15 +1,11 @@
+import { identity } from '../../../../identity.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { object_property_initialize } from '../../../../object/property/initialize.mjs';
-export async function list_string_to_dictionary_async(keys, key_to_value) {
+import { list_to_dictionary_async } from '../../../to/dictionary/async.mjs';
+export async function list_string_to_dictionary_async(list, element_to_value) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let dictionary = {};
-    for (let key of keys) {
-        let value = await key_to_value(key);
-        object_property_initialize(dictionary, key, value);
-    }
-    return dictionary;
+    return list_to_dictionary_async(keys, identity, element_to_value);
 }
