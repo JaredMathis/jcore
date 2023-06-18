f56c82136afe328c18c39f01f5fbc0769deba4fa
JaredMathis <jared.mathis@native.consulting>
2023-06-18 11:53:26 -0400

    #197 function_tests_generated_delete integer_divide_evenly

--- src/function/tests/generated/delete/no/after.mjs
+++ src/function/tests/generated/delete/no/after.mjs
@@ -5,8 +5,11 @@ import { function_delete } from '../../../../delete.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { list_empty } from '../../../../../list/empty.mjs';
 import { tests_generate } from '../../../../../tests/generate.mjs';
+import { assert } from '../../../../../assert.mjs';
+import { function_exists } from '../../../../exists.mjs';
 export async function function_tests_generated_delete_no_after(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
+    assert(await function_exists(function_name))
     let generateds = await function_tests_generated(function_name);
     if (not(list_empty(generateds))) {
         for (let g of generateds) {
