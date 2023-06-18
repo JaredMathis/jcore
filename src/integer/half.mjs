b13d083c8e464b576870c474615107f867da3331
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:14:37 -0400

    #196 function_input_remove_last integer_half

--- src/integer/half.mjs
+++ src/integer/half.mjs
@@ -1,11 +1,8 @@
 import { integer_is } from './is.mjs';
 import { integer_divide } from './divide.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function integer_half(a, b) {
-    arguments_assert(arguments, [
-        integer_is,
-        integer_is
-    ]);
+export function integer_half(a) {
+    arguments_assert(arguments, [integer_is]);
     let result = integer_divide(a, 2);
     return result;
 }
