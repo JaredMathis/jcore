d68a8cc64f5ce02fe18f6f264f8451611fb8c5c1
JaredMathis <jared.mathis@native.consulting>
2023-06-18 14:32:29 -0400

    #222 noop

--- src/function/input/swap/first/to/named.mjs
+++ src/function/input/swap/first/to/named.mjs
@@ -2,7 +2,7 @@ import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs'
 import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
 import { function_input_swap_first_to } from '../to.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export async function function_input_swap_first_to_named(index_string, function_name) {
+export async function function_input_swap_first_to_named(function_name, index_string) {
     arguments_assert(arguments, [
         string_identifier_is,
         arguments_assert_todo
