995dc9460ff1bb79dad0a4f7c849c645eaa8e057
JaredMathis <jared.mathis@native.consulting>
2023-06-18 16:28:36 -0400

    #227 function_auto function_add_mapper_all

--- src/function/add/mapper/all.mjs
+++ src/function/add/mapper/all.mjs
@@ -1,5 +1,6 @@
+import { js_brace_left_right } from '../../../js/brace/left/right.mjs';
+import { file_js_all_map_args_if_function_args } from '../../../file/js/all/map/args/if/function/args.mjs';
 import { js_keyword_function } from '../../../js/keyword/function.mjs';
-import { file_js_all_map_args_if_function } from '../../../file/js/all/map/args/if/function.mjs';
 import { function_add_mapper_generic } from './generic.mjs';
 import { function_name_get } from '../../name/get.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
@@ -9,10 +10,13 @@ import { function_name_separator } from '../../name/separator.mjs';
 import { refactor_prefix } from '../../../refactor/prefix.mjs';
 export async function function_add_mapper_all(function_name_suffix) {
     arguments_assert(arguments, [string_identifier_is]);
-    let fn = file_js_all_map_args_if_function;
+    let fn = file_js_all_map_args_if_function_args;
     let function_name_to_call = function_name_get(fn);
     function expression_code_args_get(function_name_refactor) {
-        const expression_code_args = [function_name_refactor];
+        const expression_code_args = [
+            function_name_refactor,
+            js_brace_left_right()
+        ];
         return expression_code_args;
     }
     const prefix_function = `${ refactor_prefix() }${ function_name_separator() }${ js_keyword_function() }s`;
