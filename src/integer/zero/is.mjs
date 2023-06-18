b4ca3cbe8cefce6d826f0fab23dacc8842f6ea76
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:21:42 -0400

    #197 task_later maybe function_auto should automatically figure out type information if add arguments_assert if possible

--- src/integer/zero/is.mjs
+++ src/integer/zero/is.mjs
@@ -1,7 +1,8 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { equal } from '../../equal.mjs';
+import { integer_is } from '../is.mjs';
 export function integer_zero_is(a) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [integer_is]);
     return equal(a, 0);
 }
