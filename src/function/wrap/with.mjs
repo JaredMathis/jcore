c45ccd7ab6be3801181c565330020d0f4210299e
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:01:42 -0400

    #183 function_input_add_type function_wrap_with function_name_map string_identifier_is

--- src/function/wrap/with.mjs
+++ src/function/wrap/with.mjs
@@ -1,17 +1,14 @@
-import { noop } from '../../noop.mjs';
 import { function_wrap_generic } from './generic.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function function_wrap_with(function_name_to_wrap, function_name_to_add) {
+export async function function_wrap_with(function_name_to_wrap, function_name_to_add, function_name_map) {
     arguments_assert(arguments, [
+        string_identifier_is,
         string_identifier_is,
         string_identifier_is
     ]);
     await function_wrap_generic(function_name_to_wrap, function_name_to_add, function map(args) {
-        let {
-            identifier,
-            statements_code
-        } = args;
+        let {identifier, statements_code} = args;
         let index = 1;
     });
 }
