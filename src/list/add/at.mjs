9eb96470f8d362647099d6ccfd33a66bca47c227
JaredMathis <jared.mathis@native.consulting>
2023-06-18 14:43:51 -0400

    #222 noop

--- src/list/add/at.mjs
+++ src/list/add/at.mjs
@@ -6,13 +6,14 @@ import { list_is } from '../is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
 import { equal } from '../../equal.mjs';
+import { list_length } from '../length.mjs';
 export function list_add_at(list, value, index) {
     arguments_assert(arguments, [
         list_is,
         defined_is,
         integer_is
     ]);
-    let v = list_index_valid(list, index) || equal(index, 0);
+    let v = list_index_valid(list, index) || equal(index, list_length(list));
     assert(v);
     const delete_count = 0;
     list.splice(index, delete_count, value);
