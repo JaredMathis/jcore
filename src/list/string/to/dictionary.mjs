fb0557d6af8947425c957254b0b8d8c7749f919f
JaredMathis <jared.mathis@native.consulting>
2023-06-18 17:25:00 -0400

    #61 noop

--- src/list/string/to/dictionary.mjs
+++ src/list/string/to/dictionary.mjs
@@ -3,15 +3,15 @@ import { metadata } from '../../../metadata.mjs';
 import { object_property_initialize } from '../../../object/property/initialize.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function list_string_to_dictionary(keys, key_to_value) {
+export function list_string_to_dictionary(list, key_to_value) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
     let dictionary = {};
-    for (let key of keys) {
-        let value = key_to_value(key);
-        object_property_initialize(dictionary, key, value);
+    for (let element of list) {
+        let value = key_to_value(element);
+        object_property_initialize(dictionary, element, value);
     }
     return dictionary;
     metadata([metadata_generated()]);
