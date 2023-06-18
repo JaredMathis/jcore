ed2cb2505b6bc9e97cdb49ad69313a864dc8dfcf
JaredMathis <jared.mathis@native.consulting>
2023-06-18 11:45:47 -0400

    #213 function_rename_suffix_add function_tests_generated_delete no_after

--- src/function/tests/regenerate/count.mjs
+++ src/function/tests/regenerate/count.mjs
@@ -1,5 +1,5 @@
 import { count } from '../../../count.mjs';
-import { function_tests_generated_delete } from '../generated/delete.mjs';
+import { function_tests_generated_delete_no_after } from '../generated/delete/no/after.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { function_tests_generate_count } from '../generate/count.mjs';
@@ -9,7 +9,7 @@ export async function function_tests_regenerate_count(function_name, count) {
         string_identifier_is,
         string_is
     ]);
-    await function_tests_generated_delete(function_name);
+    await function_tests_generated_delete_no_after(function_name);
     let result = await function_tests_generate_count(function_name, count);
     return result;
 }
