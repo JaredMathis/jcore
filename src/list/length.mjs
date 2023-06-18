f12f5805852e00f9d5faada9ec6eda4d0ccff723
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:37:48 -0400

    #183 function_auto list_length

--- src/list/length.mjs
+++ src/list/length.mjs
@@ -1,9 +1,10 @@
+import { list_property_length } from './property/length.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { object_property_get } from '../object/property/get.mjs';
 import { list_is } from './is.mjs';
 export function list_length(list) {
     arguments_assert(arguments, [list_is]);
-    return object_property_get(list, 'length');
+    return object_property_get(list, list_property_length());
     metadata([]);
 }
---
+++ src/list/property/length.mjs
@@ -0,0 +1,3 @@
+export function list_property_length() {
+    return 'length';
+}
