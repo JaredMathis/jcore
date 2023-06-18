2c3c20c2fbd3f8f8b6347a2b4d6cc41268400108
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:53:55 -0400

    #194 function_auto integer_mod

---
+++ src/integer/mod.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function integer_mod() {
+    arguments_assert(arguments, []);
+    error('todo: integer_mod');
+}
