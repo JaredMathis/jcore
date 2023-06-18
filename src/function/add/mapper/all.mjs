8b33469bd7e8fd3b1d5dae9dea68983ff2771411
JaredMathis <jared.mathis@native.consulting>
2023-06-18 15:51:21 -0400

    #221 function_auto function_add_mapper_all

--- src/function/add/mapper/all.mjs
+++ src/function/add/mapper/all.mjs
@@ -5,6 +5,8 @@ import { function_name_get } from '../../name/get.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { noop } from '../../../noop.mjs';
+import { function_name_separator } from '../../name/separator.mjs';
+import { refactor_prefix } from '../../../refactor/prefix.mjs';
 export async function function_add_mapper_all(function_name_suffix) {
     arguments_assert(arguments, [string_identifier_is]);
     let fn = file_js_all_map_args_if_function;
@@ -13,6 +15,6 @@ export async function function_add_mapper_all(function_name_suffix) {
         const expression_code_args = [function_name_refactor];
         return expression_code_args;
     }
-    const prefix_function = `${ js_keyword_function() }s`;
+    const prefix_function = `${ refactor_prefix() }${ function_name_separator() }${ js_keyword_function() }s`;
     await function_add_mapper_generic(prefix_function, function_name_suffix, function_name_to_call, expression_code_args_get, noop);
 }
