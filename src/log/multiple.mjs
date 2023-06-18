59d4de1e6fa567b472cdaa45015d18960feea406
JaredMathis <jared.mathis@native.consulting>
2023-06-18 18:12:00 -0400

    #230 noop

--- src/log/multiple.mjs
+++ src/log/multiple.mjs
@@ -5,7 +5,7 @@ import { log } from '../log.mjs';
 export function log_multiple(list) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let mapper = identity;
-    for (let message of list) {
-        log(message);
+    for (let element of list) {
+        log(element);
     }
 }
