4265320ef83e5e3567e54eb91408ddd5f0d1d0d0
JaredMathis <jared.mathis@native.consulting>
2023-06-18 16:33:24 -0400

    #227 function_auto refactor_call_find

--- src/refactor/call/find.mjs
+++ src/refactor/call/find.mjs
@@ -1,7 +1,15 @@
+import { log } from '../../log.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
+import { js_node_property_callee } from '../../js/node/property/callee.mjs';
+import { js_visit_nodes_call_expression_name_equal } from '../../js/visit/nodes/call/expression/name/equal.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
-import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function refactor_call_find(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {function_name} = args;
+    js_visit_nodes_call_expression_name_equal(parsed, function_name, function v_3(n) {
+        let v = js_node_property_callee();
+        let callee = object_property_get(n, v);
+        console.log(callee);
+    });
 }
