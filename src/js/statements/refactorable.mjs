546831f923add1cbbaada6cf69849f13af404790
JaredMathis <jared.mathis@native.consulting>
2023-06-18 18:45:22 -0400

    #232 noop

--- src/js/statements/refactorable.mjs
+++ src/js/statements/refactorable.mjs
@@ -1,10 +1,11 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { list_is } from '../../list/is.mjs';
 export function js_statements_refactorable(left, right) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        list_is,
+        list_is
     ]);
     error('todo: js_statements_refactorable');
 }
