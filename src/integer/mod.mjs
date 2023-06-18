14239ea9738a2069253631b45f3b561b1d32d769
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:54:24 -0400

    #194 function_input_add_multiple integer_mod a,b

--- src/integer/mod.mjs
+++ src/integer/mod.mjs
@@ -1,6 +1,10 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function integer_mod() {
-    arguments_assert(arguments, []);
+export function integer_mod(a, b) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     error('todo: integer_mod');
 }
