5b1896d4d011e63edfdf1557532dbc195c88e0ef
JaredMathis <jared.mathis@native.consulting>
2023-06-18 14:35:47 -0400

    #222 function_auto function_inputs_index_of_string

--- src/function/inputs/index/of/string.mjs
+++ src/function/inputs/index/of/string.mjs
@@ -1,9 +1,16 @@
+import { equal } from '../../../../equal.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { string_to } from '../../../../string/to.mjs';
 import { list_find_index } from '../../../../list/find/index.mjs';
 import { function_inputs } from '../../../inputs.mjs';
 export async function function_inputs_index_of_string(function_name, input_name) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let inputs = await function_inputs(function_name);
-    let index = list_find_index(inputs, input_name);
+    let index = list_find_index(inputs, i => equal(i, input_name));
     let index_string = string_to(index);
     return index_string;
 }
