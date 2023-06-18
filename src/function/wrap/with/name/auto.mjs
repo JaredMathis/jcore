969eec624021ebe104b2e90841f0f370fff0e215
JaredMathis <jared.mathis@native.consulting>
2023-06-18 13:40:11 -0400

    #218 function_input_remove_at function_wrap_with_name_auto 1

--- src/function/wrap/with/name/auto.mjs
+++ src/function/wrap/with/name/auto.mjs
@@ -1,9 +1,8 @@
 import { string_identifier_is } from '../../../../string/identifier/is.mjs';
 import { function_wrap_with } from '../../with.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function function_wrap_with_name_auto(function_name_to_wrap, function_name_to_add, function_name_map) {
+export async function function_wrap_with_name_auto(function_name_to_wrap, function_name_map) {
     arguments_assert(arguments, [
-        string_identifier_is,
         string_identifier_is,
         string_identifier_is
     ]);
