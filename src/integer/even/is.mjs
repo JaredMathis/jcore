0c6b471b7e6ae83c29bd3c137fc4f03e77990be6
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:01:02 -0400

    #193 function_wrap integer_mod integer_even_is

---
+++ src/integer/even/is.mjs
@@ -0,0 +1,11 @@
+import { integer_is } from '../is.mjs';
+import { integer_mod } from '../mod.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function integer_even_is(a, b) {
+    arguments_assert(arguments, [
+        integer_is,
+        integer_is
+    ]);
+    let result = integer_mod(a, b);
+    return result;
+}
