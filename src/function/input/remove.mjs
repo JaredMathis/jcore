6e98470e760d33872b76e3b7e49be66deac69824
JaredMathis <jared.mathis@native.consulting>
2023-06-18 13:25:35 -0400

    #218 function_delete function_input_remove

--- src/function/input/remove.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../../error.mjs';
-import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_input_remove() {
-    arguments_assert(arguments, []);
-    error('todo: function_input_remove');
-}
