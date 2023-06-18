1013db94811bf878b890a09d54e85f3b64b840a3
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:24:29 -0400

    #183 function_auto function_wrap_with

--- src/function/wrap/with.mjs
+++ src/function/wrap/with.mjs
@@ -1,3 +1,6 @@
+import { js_code_return_statement } from '../../js/code/return/statement.mjs';
+import { js_code_function_declaration_to_statement_assignment_wrapped } from '../../js/code/function/declaration/to/statement/assignment/wrapped.mjs';
+import { list_length_is_value } from '../../list/length/is/value.mjs';
 import { list_single } from '../../list/single.mjs';
 import { js_function_declaration_to_params_names } from '../../js/function/declaration/to/params/names.mjs';
 import { function_to_declaration } from '../to/declaration.mjs';
@@ -8,8 +11,6 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { assert } from '../../assert.mjs';
 import { list_length_is_1 } from '../../list/length/is/1.mjs';
 import { comment } from '../../comment.mjs';
-import { list_get } from '../../list/get.mjs';
-import { list_last } from '../../list/last.mjs';
 import { list_last_index } from '../../list/last/index.mjs';
 import { list_last_remove } from '../../list/last/remove.mjs';
 export async function function_wrap_with(function_name_to_wrap, function_name_to_add, function_name_map) {
@@ -24,10 +25,14 @@ export async function function_wrap_with(function_name_to_wrap, function_name_to
     assert(list_length_is_1(function_name_map_inputs));
     await function_wrap_generic(function_name_to_wrap, function_name_to_add, function map(args) {
         let {identifier, statements_code} = args;
-        assert(list_length_is_value(statements_code, 2))
+        assert(list_length_is_value(statements_code, 2));
         comment(`The index we'll add at`);
         let index = list_last_index(statements_code);
-        list_last_remove(statements_code)
+        list_last_remove(statements_code);
         let mapped = js_code_function_declaration_to_statement_assignment(function_declaration_map, list_single(identifier), 'mapped');
+        let statements_code_new = [
+            js_code_function_declaration_to_statement_assignment_wrapped(function_declaration, identifier),
+            js_code_return_statement(identifier)
+        ];
     });
 }
