31a54f9bf5293b7d6699a52be4a0f5a862c7a3fc
JaredMathis <jared.mathis@native.consulting>
2023-06-18 08:52:50 -0400

    #183 noop

--- src/string/identifier/parts/from.mjs
+++ src/string/identifier/parts/from.mjs
@@ -2,8 +2,9 @@ import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { function_name_separator } from '../../../function/name/separator.mjs';
 import { list_join } from '../../../list/join.mjs';
+import { list_is } from '../../../list/is.mjs';
 export function string_identifier_parts_from(list_parts) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [list_is]);
     let v = function_name_separator();
     let v_2 = list_join(list_parts, v);
     return v_2;
