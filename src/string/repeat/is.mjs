c2ee8b630bfed3e861b6bf719b571ce6da38f987
JaredMathis <jared.mathis@native.consulting>
2023-06-18 12:50:01 -0400

    #192 function_auto string_repeat_is

--- src/string/repeat/is.mjs
+++ src/string/repeat/is.mjs
@@ -1,12 +1,12 @@
 import { list_all_equal } from '../../list/all/equal.mjs';
 import { integer_divide_evenly } from '../../integer/divide/evenly.mjs';
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_length } from '../length.mjs';
 import { string_split_at } from '../split/at.mjs';
 import { object_properties } from '../../object/properties.mjs';
+import { string_is } from '../is.mjs';
 export function string_repeat_is(input) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [string_is]);
     let length = string_length(input);
     let half = integer_divide_evenly(length, 2);
     let split = string_split_at(half);
