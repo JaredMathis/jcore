bb4a2964638e3aa5e9c9097ea0e1d4b1c21fbba7
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:49:47 -0400

    #192 function_auto string_repeat_is

---
+++ src/string/repeat/is.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function string_repeat_is() {
+    arguments_assert(arguments, []);
+    error('todo: string_repeat_is');
+}
