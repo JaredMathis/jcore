a9972f765d814e68a829d8c24d3411ff8d8a8bf9
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:09:48 -0400

    #192 function_auto integer_even_is

--- src/integer/even/is.mjs
+++ src/integer/even/is.mjs
@@ -1,10 +1,8 @@
+import { integer_even_is_generic } from './is/generic.mjs';
 import { integer_is } from '../is.mjs';
-import { integer_mod } from '../mod.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { equal } from '../../equal.mjs';
 export function integer_even_is(a) {
     arguments_assert(arguments, [integer_is]);
     const expected = 0;
-    let result_mod = integer_mod(a, 2);
-    return equal(result_mod, expected);
+    return integer_even_is_generic(a, expected);
 }
