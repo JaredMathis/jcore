05347e3f13ee424989b257d938d2eb76c5f65196
JaredMathis <jared.mathis@native.consulting>
2023-06-18 11:58:25 -0400

    #196 function_tests_regenerate integer_half

--- src/integer/half/test/generated/2.mjs
+++ src/integer/half/test/generated/2.mjs
@@ -1,13 +1,11 @@
 import { metadata_generated } from '../../../../metadata/generated.mjs';
 import { metadata } from '../../../../metadata.mjs';
-import { json_equal } from '../../../../json/equal.mjs';
-import { assert } from '../../../../assert.mjs';
 import { integer_half } from '../../../half.mjs';
+import { assert_throws } from '../../../../assert/throws.mjs';
+import { assert } from '../../../../assert.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function integer_half_test_generated_2() {
     arguments_assert(arguments, []);
-    let expected = 0;
-    let actual = integer_half(0);
-    assert(json_equal(actual, expected));
+    assert(assert_throws(() => integer_half(-1)));
     metadata([metadata_generated()]);
 }
