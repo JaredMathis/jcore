0b39a800c132e218ef394dff9cb725196a112aaa
JaredMathis <jared.mathis@native.consulting>
2023-06-18 09:52:15 -0400

    #192 function_input_add string_repeat_is input

--- src/string/repeat/is.mjs
+++ src/string/repeat/is.mjs
@@ -1,6 +1,7 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function string_repeat_is() {
-    arguments_assert(arguments, []);
+export function string_repeat_is(input) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: string_repeat_is');
 }
