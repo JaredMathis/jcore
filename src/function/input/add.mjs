6683feaffd9b5d548a3ffc79bdb4cee8fb5c3f23
JaredMathis <jared.mathis@native.consulting>
2023-06-18 13:22:34 -0400

    #217 function_auto function_input_add

--- src/function/input/add.mjs
+++ src/function/input/add.mjs
@@ -1,15 +1,11 @@
-import { function_input_value_default } from './value/default.mjs';
+import { function_input_add_at } from './add/at.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { function_input_add_generic } from './add/generic.mjs';
-import { function_name_get } from '../name/get.mjs';
 export async function function_input_add(function_name, input_name) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
     const input_index = 0;
-    let input_value_default = function_input_value_default();
-    let v = function_name_get(arguments_assert_todo);
-    await function_input_add_generic(function_name, input_name, v, input_value_default, input_index);
+    await function_input_add_at(function_name, input_name, input_index);
 }
---
+++ src/function/input/add/at.mjs
@@ -0,0 +1,9 @@
+import { function_input_add_generic } from './generic.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { function_name_get } from '../../name/get.mjs';
+import { function_input_value_default } from '../value/default.mjs';
+export async function function_input_add_at(function_name, input_name, input_index) {
+    let input_value_default = function_input_value_default();
+    let v = function_name_get(arguments_assert_todo);
+    await function_input_add_generic(function_name, input_name, v, input_value_default, input_index);
+}
