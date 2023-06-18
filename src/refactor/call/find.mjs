d59a5118f233435a5e321ac6c78f7b65d5904ec7
JaredMathis <jared.mathis@native.consulting>
2023-06-18 16:40:12 -0400

    #227 function_auto refactor_call_find

--- src/refactor/call/find.mjs
+++ src/refactor/call/find.mjs
@@ -1,6 +1,5 @@
+import { js_function_declaration_to_name } from '../../js/function/declaration/to/name.mjs';
 import { log } from '../../log.mjs';
-import { object_property_get } from '../../object/property/get.mjs';
-import { js_node_property_callee } from '../../js/node/property/callee.mjs';
 import { js_visit_nodes_call_expression_name_equal } from '../../js/visit/nodes/call/expression/name/equal.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -10,10 +9,12 @@ export function refactor_call_find(args) {
     let {function_name} = args;
     let {parsed} = args;
     if (changed(c => {
-        js_visit_nodes_call_expression_name_equal(parsed, function_name, function v_3(n) {
-            c();
-        });
-    })) {
-        let function_name_caller = 
+            js_visit_nodes_call_expression_name_equal(parsed, function_name, function v_3(n) {
+                c();
+            });
+        })) {
+        let {function_declaration} = args;
+        let function_name_caller = js_function_declaration_to_name(function_declaration);
+        log(function_name_caller);
     }
 }
