53386a52e8fc48904e0f5fe1a22635fb84f2d63d
JaredMathis <jared.mathis@native.consulting>
2023-06-18 13:23:26 -0400

    #217 function_auto function_input_add_last_before

--- src/function/input/add/last/before.mjs
+++ src/function/input/add/last/before.mjs
@@ -1,13 +1,14 @@
+import { function_inputs } from '../../../inputs.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { function_input_swap_last_2 } from '../../swap/last/2.mjs';
 import { function_input_add } from '../../add.mjs';
-import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function function_input_add_last_before(function_name, input_name) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
+    let inputs = await function_inputs(input_name);
     await function_input_add(function_name, input_name);
     await function_input_swap_last_2(function_name);
 }
