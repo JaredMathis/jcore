c2246b66973698dc839991310b88dcc4061f7775
JaredMathis <jared.mathis@native.consulting>
2023-06-18 17:09:03 -0400

    #228 refactor_functions_call_statement_find list_empty

--- src/refactor/call/statement/find.mjs
+++ src/refactor/call/statement/find.mjs
@@ -2,9 +2,11 @@ import { js_visit_call_statements } from '../../../js/visit/call/statements.mjs'
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { log } from '../../../log.mjs';
+import { js_unparse } from '../../../js/unparse.mjs';
 export function refactor_call_statement_find(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     js_visit_call_statements(args, (stack_reversed, node, expression, parent_list) => {
+        let node_string = js_unparse(node);
         log({
             node,
             expression
