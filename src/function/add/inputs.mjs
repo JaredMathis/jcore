004d2b838d6a5db6fb9448e075d90c308c11b1c9
JaredMathis <jared.mathis@native.consulting>
2023-06-18 08:51:23 -0400

    #183 noop

--- src/function/add/inputs.mjs
+++ src/function/add/inputs.mjs
@@ -2,10 +2,11 @@ import { function_input_add_multiple } from '../input/add/multiple.mjs';
 import { function_add } from '../add.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { string_identifier_is } from '../../string/identifier/is.mjs';
 export async function function_add_inputs(function_name, inputs_string) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        string_identifier_is,
+        string_identifier_is
     ]);
     await function_add(function_name);
     await function_input_add_multiple(function_name, inputs_string);
