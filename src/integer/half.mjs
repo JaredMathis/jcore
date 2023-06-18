303b71e1b904b02ae951a19256b845788d4cb5e4
JaredMathis <jared.mathis@native.consulting>
2023-06-18 10:15:30 -0400

    #196 function_auto integer_half

--- src/integer/half.mjs
+++ src/integer/half.mjs
@@ -1,8 +1,11 @@
+import { integer_even_is } from './even/is.mjs';
 import { integer_is } from './is.mjs';
 import { integer_divide } from './divide.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { assert } from '../assert.mjs';
 export function integer_half(a) {
     arguments_assert(arguments, [integer_is]);
+    assert(integer_even_is(a));
     let result = integer_divide(a, 2);
     return result;
 }
