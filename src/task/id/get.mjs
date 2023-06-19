0188a895658576c31bb61422633ee342312bebaa
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:06:13 -0400

    #255 function_auto task_id_get

--- src/task/id/get.mjs
+++ src/task/id/get.mjs
@@ -1,10 +1,10 @@
+import { object_property_get } from '../../object/property/get.mjs';
 import { metadata } from '../../metadata.mjs';
 import { task_id_get_generic } from './get/generic.mjs';
-import { object_property_get_or } from '../../object/property/get/or.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function task_id_get() {
     arguments_assert(arguments, []);
-    let lambda = object_property_get_or;
+    let lambda = object_property_get;
     return await task_id_get_generic(lambda);
     metadata([]);
 }
