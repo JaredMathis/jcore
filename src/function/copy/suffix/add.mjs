3d02ad9a65865f647038087c810e7610d1e21b2e
JaredMathis <jared.mathis@native.consulting>
2023-06-19 17:50:22 -0400

    #255 function_auto function_copy_suffix_add

--- src/function/copy/suffix/add.mjs
+++ src/function/copy/suffix/add.mjs
@@ -1,14 +1,10 @@
-import { list_remove_last_verify_multiple } from '../../../list/remove/last/verify/multiple.mjs';
-import { string_identifier_parts_from } from '../../../string/identifier/parts/from.mjs';
+import { function_copy } from '../../copy.mjs';
+import { error } from '../../../error.mjs';
+import { string_identifier_parts_from_combine } from '../../../string/identifier/parts/from/combine.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
-import { function_copy_suffix_change } from './change.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { list_add_beginning } from '../../../list/add/beginning.mjs';
 import { string_identifier_parts_to } from '../../../string/identifier/parts/to.mjs';
 import { json_to } from '../../../json/to.mjs';
-import { list_join } from '../../../list/join.mjs';
-import { list_multiple_combine } from '../../../list/multiple/combine.mjs';
-import { function_name_separator } from '../../name/separator.mjs';
 export async function function_copy_suffix_add(function_name_old, function_name_suffix) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -16,7 +12,14 @@ export async function function_copy_suffix_add(function_name_old, function_name_
     ]);
     let function_name_suffix_parts = string_identifier_parts_to(function_name_suffix);
     let function_name_old_parts = string_identifier_parts_to(function_name_old);
-    let function_name_new = string_identifier_parts_from_combine([function_name_old_parts, function_name_suffix_parts])
-    error(json_to({function_name_old, function_name_suffix_from, function_name_suffix_to}))
-    await function_copy_suffix_change(function_name_old, function_name_suffix_from, function_name_suffix_to);
+    let function_name_new = string_identifier_parts_from_combine([
+        function_name_old_parts,
+        function_name_suffix_parts
+    ]);
+    error(json_to({
+        function_name_old,
+        function_name_suffix_from,
+        function_name_suffix_to
+    }));
+    await function_copy(function_name_old, function_name_suffix_from, function_name_suffix_to);
 }
