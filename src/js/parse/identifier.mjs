c568e51d252dfd36c84c2791e1e5580b148733ef
JaredMathis <jared.mathis@native.consulting>
2023-06-19 09:44:49 -0400

    #232 function_auto js_parse_identifier

--- src/js/parse/identifier.mjs
+++ src/js/parse/identifier.mjs
@@ -1,10 +1,10 @@
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { js_node_is_identifier } from '../node/is/identifier.mjs';
 import { assert } from '../../assert.mjs';
 import { js_parse_expression } from './expression.mjs';
+import { string_identifier_is } from '../../string/identifier/is.mjs';
 export function js_parse_identifier(identifier_name) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [string_identifier_is]);
     let identifier = js_parse_expression(identifier_name);
     let v = js_node_is_identifier(identifier);
     assert(v);
