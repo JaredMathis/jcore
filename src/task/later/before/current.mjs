fe2d877cbdc6768b6e21bd96ebb8072e5261cd53
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:24:26 -0400

    #254 function_auto task_number_string

--- src/task/later/before/current.mjs
+++ src/task/later/before/current.mjs
@@ -4,7 +4,7 @@ import { task_later_before } from '../before.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function task_later_before_current(title) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let task_number_strings = await task_id_get_string();
+    let task_number_string = await task_id_get_string();
     let result = await task_later_before(task_number_string, title);
     return result;
 }
---
+++ src/task/number/string.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function task_number_string() {
+    arguments_assert(arguments, []);
+    error('todo: task_number_string');
+}
