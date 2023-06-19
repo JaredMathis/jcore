30092763c5ded27d2bea86ab60324473d6a66549
JaredMathis <jared.mathis@native.consulting>
2023-06-19 13:17:43 -0400

    #242 function_auto task_current_open

--- src/task/current/open.mjs
+++ src/task/current/open.mjs
@@ -1,7 +1,9 @@
+import { task_id_get_or_null } from '../id/get/or/null.mjs';
 import { task_open } from '../open.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function task_current_open() {
     arguments_assert(arguments, []);
+    let current = await task_id_get_or_null();
     let result = await task_open(task_number_string);
     return result;
 }
