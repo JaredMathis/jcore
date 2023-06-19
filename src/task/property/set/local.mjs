abf371a9ab4d6e30a1f8686a0d8698c07df7941c
JaredMathis <jared.mathis@native.consulting>
2023-06-19 16:56:36 -0400

    #242 noop

--- src/task/property/set/local.mjs
+++ src/task/property/set/local.mjs
@@ -5,9 +5,13 @@ import { tasks_all_map } from '../../../tasks/all/map.mjs';
 import { integer_parse } from '../../../integer/parse.mjs';
 export async function task_property_set_local(task_number_string, property_name, property_value) {
     let task_number = integer_parse(task_number_string);
-    await tasks_all_map(function map(tasks_all) {
+    await tasks_all_map(function map_all(tasks_all) {
         let v = task_property_number();
         let task = list_find_property(tasks_all, v, task_number);
-        object_property_change(task, property_name, property_value);
+        newFunction(task);
     });
+
+    function newFunction(task) {
+        object_property_change(task, property_name, property_value);
+    }
 }
