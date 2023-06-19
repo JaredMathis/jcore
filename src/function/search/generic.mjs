d549b1f0401722c5d95c5368f91e709b8dfc5812
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:52:39 -0400

    #257 function_input_swap_first_to function_search_generic 2

--- src/function/search/generic.mjs
+++ src/function/search/generic.mjs
@@ -1,9 +1,16 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { function_name_to_file_path } from '../name/to/file/path.mjs';
 import { list_string_to_dictionary } from '../../list/string/to/dictionary.mjs';
 import { identity } from '../../identity.mjs';
 import { search_generic } from '../../search/generic.mjs';
 import { function_name_all_tests_not } from '../name/all/tests/not.mjs';
-export async function function_search_generic(candidate_as_string_matches, query, query_map) {
+export async function function_search_generic(query_map, candidate_as_string_matches, query) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     const candidates = await function_name_all_tests_not();
     let candidates_matching = search_generic(candidates, identity, candidate_as_string_matches, query, query_map);
     return list_string_to_dictionary(candidates_matching, function_name_to_file_path);
