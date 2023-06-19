12b4f99c6473a4026aeaa47f55e347c58b50bc82
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:57:47 -0400

    #232 function_auto js_node_identifiers_replace_if_parent_not_call_expression

--- src/js/node/identifiers/replace/if/parent/not/call/expression.mjs
+++ src/js/node/identifiers/replace/if/parent/not/call/expression.mjs
@@ -1,15 +1,10 @@
-import { js_identifier_name_get } from '../../../../../../../identifier/name/get.mjs';
+import { js_visit_nodes_filter } from '../../../../../../../visit/nodes/filter.mjs';
 import { js_node_is_identifier } from '../../../../../../is/identifier.mjs';
-import { js_nodes_each } from '../../../../../../../nodes/each.mjs';
-import { list_adder } from '../../../../../../../../list/adder.mjs';
 import { arguments_assert_todo } from '../../../../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
 export function js_node_identifiers_replace_if_parent_not_call_expression(node) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    return list_adder(la => {
-        js_nodes_each(node, js_node_is_identifier, n => {
-            let name = js_identifier_name_get(n);
-            la(name);
-        });
+    js_visit_nodes_filter(node, js_node_is_identifier, v => {
+        let {node} = v;
     });
 }
