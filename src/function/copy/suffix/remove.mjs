2da0fb6a626ec58912ff82b6d30049636240480a
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:03:33 -0400

    #255 function_auto function_copy_suffix_remove

--- src/function/copy/suffix/remove.mjs
+++ src/function/copy/suffix/remove.mjs
@@ -1,4 +1,4 @@
-import { string_identifier_suffix_remove } from '../../../string/identifier/suffix/remove.mjs';
+import { string_identifier_parts_from } from '../../../string/identifier/parts/from.mjs';
 import { function_copy } from '../../copy.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -7,6 +7,9 @@ export async function function_copy_suffix_remove(function_name_old, function_na
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let function_name_new = string_identifier_suffix_remove(function_name_old, function_name_suffix);
+    let function_name_new = string_identifier_parts_from([
+        function_name_old,
+        function_name_suffix
+    ]);
     await function_copy(function_name_old, function_name_new);
 }
