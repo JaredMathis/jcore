0ae85b44c06dd74fa56b49426780e423ea8f7e15
JaredMathis <jared.mathis@native.consulting>
2023-06-19 08:02:37 -0400

    #232 function_wrap_suffix_add function_input_swap_last_to named

---
+++ src/function/input/swap/last/to/named.mjs
@@ -0,0 +1,12 @@
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
+import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
+import { function_input_swap_last_to } from '../to.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export async function function_input_swap_last_to_named(function_name, index_string) {
+    arguments_assert(arguments, [
+        string_identifier_is,
+        arguments_assert_todo
+    ]);
+    let result = await function_input_swap_last_to(function_name, index_string);
+    return result;
+}
