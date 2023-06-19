77db74527aa507e2a51ca71cc6b2718003c88612
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:24:17 -0400

    #254 function_input_remove_first task_later_before_current

--- src/task/later/before/current.mjs
+++ src/task/later/before/current.mjs
@@ -2,11 +2,8 @@ import { task_id_get_string } from '../../id/get/string.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { task_later_before } from '../before.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function task_later_before_current(task_number_string, title) {
-    arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
-    ]);
+export async function task_later_before_current(title) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let task_number_strings = await task_id_get_string();
     let result = await task_later_before(task_number_string, title);
     return result;
