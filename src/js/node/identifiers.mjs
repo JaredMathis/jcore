fb3657c76496a48e85f9dc0ee3daf8dfb82cbb13
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:56:19 -0400

    #232 noop

--- src/js/node/identifiers.mjs
+++ src/js/node/identifiers.mjs
@@ -2,9 +2,9 @@ import { js_identifier_name_get } from '../identifier/name/get.mjs';
 import { js_node_is_identifier } from './is/identifier.mjs';
 import { js_nodes_each } from '../nodes/each.mjs';
 import { list_adder } from '../../list/adder.mjs';
-export function js_node_identifiers(left_i) {
+export function js_node_identifiers(node) {
     return list_adder(la => {
-        js_nodes_each(left_i, js_node_is_identifier, node => {
+        js_nodes_each(node, js_node_is_identifier, node => {
             let name = js_identifier_name_get(node);
             la(name);
         });
