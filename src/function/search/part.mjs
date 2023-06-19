c7ef2b8329be494192b678d4cbf5c77cdfeb3293
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:56:19 -0400

    #257 function_delete function_search_part

--- src/function/search/part.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../../error.mjs';
-import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_search_part() {
-    arguments_assert(arguments, []);
-    error('todo: function_search_part');
-}
