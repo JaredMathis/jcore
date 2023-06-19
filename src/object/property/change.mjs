0274e5bf12ffc368b31c96a065637c87a59aa5ff
JaredMathis <jared.mathis@native.consulting>
2023-06-19 09:53:26 -0400

    #232 refactor_functions_call_statement_find_previous equal_not

--- src/object/property/change.mjs
+++ src/object/property/change.mjs
@@ -1,3 +1,4 @@
+import { equal_not } from '../../equal/not.mjs';
 import { string_is } from '../../string/is.mjs';
 import { defined_is } from '../../defined/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -5,8 +6,6 @@ import { metadata } from '../../metadata.mjs';
 import { object_property_set } from './set.mjs';
 import { object_property_get } from './get.mjs';
 import { assert } from '../../assert.mjs';
-import { not } from '../../not.mjs';
-import { equal } from '../../equal.mjs';
 export function object_property_change(f, property_name, value) {
     arguments_assert(arguments, [
         defined_is,
@@ -14,8 +13,7 @@ export function object_property_change(f, property_name, value) {
         defined_is
     ]);
     let before = object_property_get(f, property_name);
-    let v_2 = equal(before, value);
-    let v = not(v_2);
+    let v = equal_not(v);
     assert(v);
     object_property_set(f, property_name, value);
     metadata([]);
