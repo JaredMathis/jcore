f4de8a8e7c4bd247f4aa85435849de503808d970
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:02:15 -0400

    #232 function_auto js_node_identifiers_replace_if_parent_not_call_expression

--- src/js/node/identifiers/replace/if/parent/not/call/expression.mjs
+++ src/js/node/identifiers/replace/if/parent/not/call/expression.mjs
@@ -1,20 +1,18 @@
+import { list_consume } from '../../../../../../../../list/consume.mjs';
 import { list_is } from '../../../../../../../../list/is.mjs';
 import { js_visit_nodes_filter } from '../../../../../../../visit/nodes/filter.mjs';
 import { js_node_is_identifier } from '../../../../../../is/identifier.mjs';
 import { arguments_assert_todo } from '../../../../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
-import { assert } from '../../../../../../../../assert.mjs';
-import { list_length } from '../../../../../../../../list/length.mjs';
-import { equal } from '../../../../../../../../equal.mjs';
-import { list_empty } from '../../../../../../../../list/empty.mjs';
 export function js_node_identifiers_replace_if_parent_not_call_expression(node, replacements) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         list_is
     ]);
-    js_visit_nodes_filter(node, js_node_is_identifier, v => {
-        let {node} = v;
-        let next = list_first_remove(replacements);
+    list_consume(next => {
+        js_visit_nodes_filter(node, js_node_is_identifier, v => {
+            let {node} = v;
+            let n = next();
+        });
     });
-    assert(list_empty(replacements));
 }
