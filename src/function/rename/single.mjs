6e308683976c91f7a0edc1e07da647d240c6b942
JaredMathis <jared.mathis@native.consulting>
2023-06-19 13:23:48 -0400

    #242 function_auto function_rename_single

--- src/function/rename/single.mjs
+++ src/function/rename/single.mjs
@@ -1,3 +1,4 @@
+import { log } from '../../log.mjs';
 import { function_open_vs_code } from '../open/vs/code.mjs';
 import { function_rename_no_open } from './no/open.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
@@ -12,5 +13,4 @@ export async function function_rename_single(function_name_old, function_name_ne
     log('a');
     await function_open_vs_code(function_name_new);
     log('a');
-
 }
