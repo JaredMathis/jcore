8934edbe95fc9a9eaa19cec162ab0c73b8d00ee0
JaredMathis <jared.mathis@native.consulting>
2023-06-19 11:08:32 -0400

    #242 function_auto refactor_properties_expand

--- src/refactor/properties/expand.mjs
+++ src/refactor/properties/expand.mjs
@@ -1,6 +1,6 @@
+import { refactor_import_fix_if_changed } from '../import/fix/if/changed.mjs';
 import { js_code_call_expression_object_property_get } from '../../js/code/call/expression/object/property/get.mjs';
 import { occurs } from '../../occurs.mjs';
-import { refactor_import_fix } from '../import/fix.mjs';
 import { list_add_after } from '../../list/add/after.mjs';
 import { object_replace } from '../../object/replace.mjs';
 import { js_code_statement_assignment } from '../../js/code/statement/assignment.mjs';
@@ -26,44 +26,42 @@ import { js_parse_statement } from '../../js/parse/statement.mjs';
 import { js_parse_expression } from '../../js/parse/expression.mjs';
 export async function refactor_properties_expand(args) {
     arguments_assert(arguments, [defined_is]);
-    let occurs = false;
-    let {function_declaration, parsed} = args;
-    js_visit_nodes_filter(parsed, js_node_is_object_pattern, function v_8(v) {
-        let {stack, parent, node} = v;
-        if (js_node_is_variable_declarator(parent)) {
-            let grandparent = js_visit_node_grandparent(stack, 0);
-            if (js_node_is_variable_declaration(grandparent)) {
-                let v_2 = js_node_property_declarations();
-                let declarations = object_property_get(grandparent, v_2);
-                if (list_length_is_1(declarations)) {
-                    let grandparent_great = js_visit_node_grandparent(stack, 1);
-                    if (js_node_is_block_statement(grandparent_great)) {
-                        let function_body_statements = js_block_statement_body(grandparent_great);
-                        let previous = grandparent;
-                        let v = js_identifier_name_next(parsed);
-                        let v_3 = js_node_property_properties();
-                        let properties = object_property_get(node, v_3);
-                        let v_4 = js_parse_expression(v);
-                        object_replace(node, v_4);
-                        for (let property of properties) {
-                            let v_5 = js_node_property_key();
-                            let property_name = js_property_identifier_name(property, v_5);
-                            let v_6 = js_node_property_value();
-                            let local = js_property_identifier_name(property, v_6);
-                            let v_7 = js_code_expression_string(property_name);
-                            let after_let = js_code_call_expression_object_property_get(v, v_7);
-                            let statement_code = js_code_statement_assignment(local, after_let);
-                            let statement = js_parse_statement(statement_code);
-                            list_add_after(function_body_statements, statement, previous);
-                            previous = statement;
-                            occurs = true;
+    refactor_import_fix_if_changed(args, o => {
+        let {function_declaration, parsed} = args;
+        js_visit_nodes_filter(parsed, js_node_is_object_pattern, function v_8(v) {
+            let {stack, parent, node} = v;
+            if (js_node_is_variable_declarator(parent)) {
+                let grandparent = js_visit_node_grandparent(stack, 0);
+                if (js_node_is_variable_declaration(grandparent)) {
+                    let v_2 = js_node_property_declarations();
+                    let declarations = object_property_get(grandparent, v_2);
+                    if (list_length_is_1(declarations)) {
+                        let grandparent_great = js_visit_node_grandparent(stack, 1);
+                        if (js_node_is_block_statement(grandparent_great)) {
+                            let function_body_statements = js_block_statement_body(grandparent_great);
+                            let previous = grandparent;
+                            let v = js_identifier_name_next(parsed);
+                            let v_3 = js_node_property_properties();
+                            let properties = object_property_get(node, v_3);
+                            let v_4 = js_parse_expression(v);
+                            object_replace(node, v_4);
+                            for (let property of properties) {
+                                let v_5 = js_node_property_key();
+                                let property_name = js_property_identifier_name(property, v_5);
+                                let v_6 = js_node_property_value();
+                                let local = js_property_identifier_name(property, v_6);
+                                let v_7 = js_code_expression_string(property_name);
+                                let after_let = js_code_call_expression_object_property_get(v, v_7);
+                                let statement_code = js_code_statement_assignment(local, after_let);
+                                let statement = js_parse_statement(statement_code);
+                                list_add_after(function_body_statements, statement, previous);
+                                previous = statement;
+                                occurs = true;
+                            }
                         }
                     }
                 }
             }
-        }
+        });
     });
-    if (occurs) {
-        await refactor_import_fix(args);
-    }
 }
