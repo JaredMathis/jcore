0c23c7099ee1fcde10a7138889cdc85e0757da72
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:59:54 -0400

    #232 function_input_add_type list_consume list list_is

--- src/list/consume.mjs
+++ src/list/consume.mjs
@@ -1,6 +1,7 @@
+import { list_is } from './is.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function list_consume() {
-    arguments_assert(arguments, []);
+export function list_consume(list) {
+    arguments_assert(arguments, [list_is]);
     error('todo: list_consume');
 }
