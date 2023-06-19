0a64a457f35f6e5732db80de94503561ee0724ab
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:46:05 -0400

    #232 function_auto list_elements_all_equal

--- src/list/elements/all/equal.mjs
+++ src/list/elements/all/equal.mjs
@@ -1,12 +1,9 @@
+import { list_elements_all_equal_generic } from './equal/generic.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { equal } from '../../../equal.mjs';
-import { list_all } from '../../all.mjs';
-import { list_first } from '../../first.mjs';
 export function list_elements_all_equal(values) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let lambda = equal;
-    let first = list_first(values);
-    let list_all_equal = list_all(values, function list_all_each(v) {
-        let e = lambda(v, first);
-        return e;
-    });
-    return list_all_equal;
+    return list_elements_all_equal_generic(values, lambda);
 }
---
+++ src/list/elements/all/equal/generic.mjs
@@ -0,0 +1,10 @@
+import { list_all } from '../../../all.mjs';
+import { list_first } from '../../../first.mjs';
+export function list_elements_all_equal_generic(values, lambda) {
+    let first = list_first(values);
+    let list_all_equal = list_all(values, function list_all_each(v) {
+        let e = lambda(v, first);
+        return e;
+    });
+    return list_all_equal;
+}
