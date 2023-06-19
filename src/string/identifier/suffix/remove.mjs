487b9ee93177849d98a4658b7c14a5224e50e44d
JaredMathis <jared.mathis@native.consulting>
2023-06-19 17:58:24 -0400

    #255 function_auto string_identifier_suffix_remove

--- src/string/identifier/suffix/remove.mjs
+++ src/string/identifier/suffix/remove.mjs
@@ -1,10 +1,16 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_identifier_parts_from } from '../parts/from.mjs';
 import { list_remove_last_verify_multiple } from '../../../list/remove/last/verify/multiple.mjs';
 import { string_identifier_parts_to } from '../parts/to.mjs';
 export function string_identifier_suffix_remove(suffix, before) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let suffix_parts = string_identifier_parts_to(suffix);
     let before_parts = string_identifier_parts_to(before);
     list_remove_last_verify_multiple(before_parts, suffix_parts);
-    let function_name_new = string_identifier_parts_from(before_parts);
-    return function_name_new;
+    let after = string_identifier_parts_from(before_parts);
+    return after;
 }
