53dc46502598615c8fb35efbd6a5f17d7eea3546
JaredMathis <jared.mathis@native.consulting>
2023-06-19 13:15:19 -0400

    #242 function_input_remove_last task_current_open

--- src/task/current/open.mjs
+++ src/task/current/open.mjs
@@ -1,8 +1,7 @@
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { task_open } from '../open.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function task_current_open(task_number_string) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+export async function task_current_open() {
+    arguments_assert(arguments, []);
     let result = await task_open(task_number_string);
     return result;
 }
