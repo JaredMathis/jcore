0e966f34d7b569fbca3f43f8bf4abcbb72970cb5
JaredMathis <jared.mathis@native.consulting>
2023-06-19 11:33:06 -0400

    #243 function_auto refactor_returnify

--- src/refactor/returnify.mjs
+++ src/refactor/returnify.mjs
@@ -6,12 +6,14 @@ import { js_node_is_return_statement } from '../js/node/is/return/statement.mjs'
 import { assert } from '../assert.mjs';
 import { result_property_data_get } from '../result/property/data/get.mjs';
 import { result_property_success_get } from '../result/property/success/get.mjs';
+import { list_reversed_get } from '../list/reversed/get.mjs';
 export function refactor_returnify(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {parsed} = args;
     js_visit_nodes_filter(parsed, js_node_is_return_statement, v => {
         let {node, stack} = v;
-        let r = js_visit_stack_to_ancestor_list(refactor_stack, lambda);
+        let stack_reversed = list_reversed_get(stack);
+        let r = js_visit_stack_to_ancestor_list(stack_reversed, lambda);
         assert(result_property_success_get(r));
         let ancestor_list = result_property_data_get(r);
     });
