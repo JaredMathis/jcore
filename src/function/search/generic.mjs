b0be885305e2483690e32240b00b64b21affad20
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:59:09 -0400

    #257 noop

--- src/function/search/generic.mjs
+++ src/function/search/generic.mjs
@@ -11,7 +11,8 @@ export async function function_search_generic(query, query_map, candidate_as_str
         arguments_assert_todo,
         arguments_assert_todo
     ]);
+    let candiate_to_string = identity
     const candidates = await function_name_all_tests_not();
-    let candidates_matching = search_generic(candidates, identity, candidate_as_string_matches, query, query_map);
+    let candidates_matching = search_generic(candidates, candiate_to_string, candidate_as_string_matches, query, query_map);
     return list_string_to_dictionary(candidates_matching, function_name_to_file_path);
 }
--- src/function/search/part.mjs
+++ src/function/search/part.mjs
@@ -5,7 +5,7 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { identity } from '../../identity.mjs';
 export async function function_search_part(query) {
     arguments_assert(arguments, [string_is]);
-    let candidate_as_string_matches = search_candidate_as_string_matches_default;
+    let candidate_as_string_matches = candidate;
     let query_map = identity;
     return await function_search_generic(query, query_map, candidate_as_string_matches);
 }
