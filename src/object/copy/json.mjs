a2e5894dcde71af76408896fd49ac679626a72e2
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:41:01 -0400

    #232 function_add_inputs object_copy_json value

---
+++ src/object/copy/json.mjs
@@ -0,0 +1,7 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function object_copy_json(value) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    error('todo: object_copy_json');
+}
