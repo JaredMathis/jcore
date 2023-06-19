ee8cf792694c0a49f2fea2bc2d7f71ed24f29d12
JaredMathis <jared.mathis@native.consulting>
2023-06-19 10:01:38 -0400

    Revert "#232 refactor_functions_call_statement_find_previous equal_not"
    This reverts commit 0274e5bf12ffc368b31c96a065637c87a59aa5ff.

--- src/object/property/change.mjs
+++ src/object/property/change.mjs
@@ -1,4 +1,3 @@
-import { equal_not } from '../../equal/not.mjs';
 import { string_is } from '../../string/is.mjs';
 import { defined_is } from '../../defined/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -6,6 +5,8 @@ import { metadata } from '../../metadata.mjs';
 import { object_property_set } from './set.mjs';
 import { object_property_get } from './get.mjs';
 import { assert } from '../../assert.mjs';
+import { not } from '../../not.mjs';
+import { equal } from '../../equal.mjs';
 export function object_property_change(f, property_name, value) {
     arguments_assert(arguments, [
         defined_is,
@@ -13,7 +14,8 @@ export function object_property_change(f, property_name, value) {
         defined_is
     ]);
     let before = object_property_get(f, property_name);
-    let v = equal_not(v);
+    let v_2 = equal(before, value);
+    let v = not(v_2);
     assert(v);
     object_property_set(f, property_name, value);
     metadata([]);
