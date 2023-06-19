eeac66b0e0c36fd039f0fc72e61907dcd838ddf9
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:13:44 -0400

    #254 function_auto task_current_title_append

--- src/task/current/title/append.mjs
+++ src/task/current/title/append.mjs
@@ -4,6 +4,6 @@ import { task_title_append } from '../../title/append.mjs';
 export async function task_current_title_append() {
     arguments_assert(arguments, []);
     let task_number = await task_id_get();
-    task_title_append(task_number);
+    await task_title_append(task_number);
     return result;
 }
