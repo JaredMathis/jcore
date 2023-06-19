5f58ea32ef3160e2e64ae4de6b05f4483c962c9e
JaredMathis <jared.mathis@native.consulting>
2023-06-19 10:42:20 -0400

    #12 function_auto task_close_reason

---
+++ src/task/close/reason.mjs
@@ -0,0 +1,14 @@
+import { task_title_append } from '../title/append.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { task_close } from '../close.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+import { string_add } from '../../string/add.mjs';
+export async function task_close_reason(task_number_string, reason) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let result = await task_close(task_number_string);
+    await task_title_append(task_number_string, string_add(': ', reason));
+    return result;
+}
