e2a5d67343594680e9865ed101a23004441ecc3b
JaredMathis <jared.mathis@native.consulting>
2023-06-19 17:40:39 -0400

    #255 function_auto function_copy_suffix_add

--- src/function/copy/suffix/add.mjs
+++ src/function/copy/suffix/add.mjs
@@ -1,8 +1,8 @@
+import { list_remove_last_verify } from '../../../list/remove/last/verify.mjs';
 import { string_identifier_parts_from } from '../../../string/identifier/parts/from.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { function_copy_suffix_change } from './change.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { list_remove_last } from '../../../list/remove/last.mjs';
 import { list_add_beginning } from '../../../list/add/beginning.mjs';
 import { string_identifier_parts_to } from '../../../string/identifier/parts/to.mjs';
 export async function function_copy_suffix_add(function_name_old, function_name_suffix) {
@@ -13,7 +13,7 @@ export async function function_copy_suffix_add(function_name_old, function_name_
     let function_name_suffix_parts = string_identifier_parts_to(function_name_suffix);
     let function_name_old_parts = string_identifier_parts_to(function_name_old);
     for (let part of function_name_suffix_parts) {
-        list_remove_last(function_name_old_parts)
+        list_remove_last_verify(function_name_old_parts);
     }
     let function_name_prefix = string_identifier_parts_from(function_name_old_parts);
     list_add_beginning(function_name_suffix_parts, function_name_suffix_from);
