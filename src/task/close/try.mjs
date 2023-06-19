452dd029fbd5f5349b65a595364c1b6f344121df
JaredMathis <jared.mathis@native.consulting>
2023-06-19 17:02:13 -0400

    #242 function_auto task_close_try

--- src/task/close/try.mjs
+++ src/task/close/try.mjs
@@ -5,5 +5,5 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { task_property_closed } from '../property/closed.mjs';
 export async function task_close_try(task_number_string) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    return await task_map_local(lambda, t => task_property_state_set(t, task_property_closed()));
+    return await task_map_local(task_number_string, t => task_property_state_set(t, task_property_closed()));
 }
