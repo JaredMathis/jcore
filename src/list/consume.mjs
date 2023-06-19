81b8d68c1614ea4da3a4ec104386b297e91f3786
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:00:29 -0400

    #232 function_input_add_type list_consume lambda function_is

--- src/list/consume.mjs
+++ src/list/consume.mjs
@@ -1,7 +1,11 @@
+import { function_is } from '../function/is.mjs';
 import { list_is } from './is.mjs';
 import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function list_consume(list) {
-    arguments_assert(arguments, [list_is]);
+export function list_consume(list, lambda) {
+    arguments_assert(arguments, [
+        list_is,
+        function_is
+    ]);
     error('todo: list_consume');
 }
