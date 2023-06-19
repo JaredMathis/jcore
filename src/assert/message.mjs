2b1e8f2a5ecbc7f4b8d4d5cf59701388a260898b
JaredMathis <jared.mathis@native.consulting>
2023-06-19 14:25:47 -0400

    #247 noop

--- src/assert/message.mjs
+++ src/assert/message.mjs
@@ -1,8 +1,4 @@
-import { not } from '../not.mjs';
 import { metadata } from '../metadata.mjs';
-import { error } from '../error.mjs';
-import { assert_arguments_count } from './arguments/count.mjs';
-import { string_is } from '../string/is.mjs';
 import { assert_message_lazy } from './message/lazy.mjs';
 export function assert_message(value, message) {
     assert_message_lazy(value, () => message);
--- src/error.mjs
+++ src/error.mjs
@@ -3,7 +3,7 @@ import { function_name_get } from './function/name/get.mjs';
 import { assert_arguments_count } from './assert/arguments/count.mjs';
 import { comment } from './comment.mjs';
 export function error(message) {
-    comment(function_name_get(assert_arguments_count) + ' calls error, so do not check args length here');
     throw new Error(message);
+    comment(function_name_get(assert_arguments_count) + ' calls error, so do not check args length here');
     metadata([]);
 }
