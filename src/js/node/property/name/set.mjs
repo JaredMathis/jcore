fa46ba0da66d965a008894b59fd6bc7676b615ce
JaredMathis <jared.mathis@native.consulting>
2023-06-19 09:11:54 -0400

    #232 function_delete js_node_property_name_set

--- src/js/node/property/name/set.mjs
+++
@@ -1,8 +0,0 @@
-import { defined_is } from '../../../../defined/is.mjs';
-import { js_node_property_name } from '../name.mjs';
-import { object_property_set } from '../../../../object/property/set.mjs';
-import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function js_node_property_name_set(js_node) {
-    arguments_assert(arguments, [defined_is]);
-    return object_property_set(js_node, js_node_property_name(), value);
-}
