8aedfe9f8a90ab230f3fc9a5c6470925f1bbc0ca
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:50:14 -0400

    #232 function_copy_suffix_change function_input_add at first

---
+++ src/function/input/add/first.mjs
@@ -0,0 +1,15 @@
+import { function_input_add_generic } from './generic.mjs';
+import { function_name_get } from '../../name/get.mjs';
+import { function_input_value_default } from '../value/default.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export async function function_input_add_first(function_name, input_name, input_index) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let input_value_default = function_input_value_default();
+    let v = function_name_get(arguments_assert_todo);
+    await function_input_add_generic(function_name, input_name, v, input_value_default, input_index);
+}
