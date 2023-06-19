c5af7c18e25bf71743a408e8ef0bbe35da361baf
JaredMathis <jared.mathis@native.consulting>
2023-06-19 13:28:45 -0400

    #242 function_auto file_js_all_identifiers_each

--- src/file/js/all/identifiers/each.mjs
+++ src/file/js/all/identifiers/each.mjs
@@ -1,9 +1,14 @@
+import { log } from '../../../../log.mjs';
+import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { js_identifiers } from '../../../../js/identifiers.mjs';
 import { file_js_parse } from '../../parse.mjs';
 import { file_js_all_path } from '../path.mjs';
 export async function file_js_all_identifiers_each(lambda) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let all = await file_js_all_path();
     for (let file_path of all) {
+        log(file_path);
         let parsed = await file_js_parse(file_path);
         let identifiers = js_identifiers(parsed);
         lambda(identifiers, file_path);
