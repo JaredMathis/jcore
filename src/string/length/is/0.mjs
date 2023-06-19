6062bcf4e6be84db3c38408428cd45ef9d731db0
JaredMathis <jared.mathis@native.consulting>
2023-06-18 20:37:18 -0400

    #232 noop

--- src/string/length/is/0.mjs
+++ src/string/length/is/0.mjs
@@ -1,7 +1,8 @@
 import { string_is } from '../../is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_length } from '../../length.mjs';
+import { string_empty_is } from '../../empty/is.mjs';
 export function string_length_is_0(value) {
     arguments_assert(arguments, [string_is]);
-    return string_length(value) === 0;
+    return string_empty_is(value);
 }
