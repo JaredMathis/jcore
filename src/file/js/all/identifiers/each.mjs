c10722bcb0c3eaba0fccf17b6e86b95f22b91803
JaredMathis <jared.mathis@native.consulting>
2023-06-19 13:59:30 -0400

    #247 function_auto file_js_all_identifiers_each

--- src/file/js/all/identifiers/each.mjs
+++ src/file/js/all/identifiers/each.mjs
@@ -1,3 +1,4 @@
+import { speed_property_differences_get } from '../../../../speed/property/differences/get.mjs';
 import { speed_property_previous_get } from '../../../../speed/property/previous/get.mjs';
 import { speed_start } from '../../../../speed/start.mjs';
 import { log } from '../../../../log.mjs';
@@ -17,6 +18,7 @@ export async function file_js_all_identifiers_each(lambda) {
         let previous = speed_property_previous_get(s);
         let now = speed_now();
         let difference = subtract(now, previous);
+        let differences = speed_property_differences_get(s);
         const t1 = performance.now() - previous;
         let identifiers = js_identifiers(parsed);
         const t2 = performance.now() - t1;
