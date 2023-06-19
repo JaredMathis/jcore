96cf0a78d32e46ab33aba5c98550bba32ed8dd42
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:22:04 -0400

    #232 function_auto try_catch_log

---
+++ src/try/catch.mjs
@@ -0,0 +1,9 @@
+export function try_catch(lambda, on_error) {
+    let result;
+    try {
+        result = lambda();
+    } catch (e) {
+        result = on_error(e);
+    }
+    return result;
+}
--- src/try/catch/log.mjs
+++ src/try/catch/log.mjs
@@ -1,3 +1,4 @@
+import { try_catch } from '../catch.mjs';
 import { string_empty_not_is } from '../../string/empty/not/is.mjs';
 import { function_is } from '../../function/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -12,18 +13,7 @@ export function try_catch_log(fn, lambda, error_message) {
     let result = try_catch(lambda, on_error);
     return result;
     metadata([]);
-
     function on_error(e) {
         error_caught(fn, e, error_message);
     }
-}
-
-function try_catch(lambda, on_error) {
-    let result;
-    try {
-        result = lambda();
-    } catch (e) {
-        result = on_error(e);
-    }
-    return result;
 }
