8f12cfa0227c7c12c5f1f4e657a618ed2fdf9d22
JaredMathis <jared.mathis@native.consulting>
2023-06-19 14:43:24 -0400

    #242 function_auto speed_start

--- src/speed/next.mjs
+++ src/speed/next.mjs
@@ -8,8 +8,8 @@ import { speed_now } from './now.mjs';
 import { speed_property_previous_get } from './property/previous/get.mjs';
 export function speed_next(s) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let previous = speed_property_previous_get(s);
     let now = speed_now();
+    let previous = speed_property_previous_get(s);
     let difference = subtract(now, previous);
     let differences = speed_property_differences_get(s);
     list_add(differences, difference);
