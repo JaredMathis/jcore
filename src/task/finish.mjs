ab5444d31fb5a17420788ee3d9f9bece671eed32
JaredMathis <jared.mathis@native.consulting>
2023-06-19 16:45:37 -0400

    #242 function_search open

--- src/task/finish.mjs
+++ src/task/finish.mjs
@@ -11,6 +11,9 @@ export async function task_finish() {
     let task_number_hashed = await task_id_get_or_null();
     let task_number_string = task_number_unhash(task_number_hashed);
     let task = await task_get(task_number_string);
+    if (equal(task_property_state_get(task), )) {
+
+    }
     await task_close(task_number_string);
     await task_unset();
     let v = await task_available();
