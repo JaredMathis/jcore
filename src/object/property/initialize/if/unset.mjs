59d6a697a9c5b15af06358b0ea2ac0f5d68ebd80
JaredMathis <jared.mathis@native.consulting>
2023-06-19 08:01:00 -0400

    #232 noop

--- src/object/property/initialize/if/unset.mjs
+++ src/object/property/initialize/if/unset.mjs
@@ -4,6 +4,9 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { object_property_exists } from '../../exists.mjs';
 import { object_property_initialize } from '../../initialize.mjs';
+import { assert } from '../../../../assert.mjs';
+import { equal } from '../../../../equal.mjs';
+import { object_property_get } from '../../get.mjs';
 export function object_property_initialize_if_unset(object, property_name, initial_value) {
     arguments_assert(arguments, [
         defined_is,
@@ -13,6 +16,8 @@ export function object_property_initialize_if_unset(object, property_name, initi
     let ensure = false;
     if (object_property_exists(object, property_name)) {
         if (ensure) {
+            let existing = object_property_get(object, property_name)
+            assert(equal(existing, initial_value))
         }
     } else {
         object_property_initialize(object, property_name, initial_value);
