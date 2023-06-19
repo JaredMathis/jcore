2aa70f18c8b849c8600fa3be7cc4cc3e3c9e3298
JaredMathis <jared.mathis@native.consulting>
2023-06-18 21:25:47 -0400

    #232 function_auto js_statements_refactorable

---
+++ src/js/node/identifiers.mjs
@@ -0,0 +1,12 @@
+import { js_identifier_name_get } from '../identifier/name/get.mjs';
+import { js_node_is_identifier } from './is/identifier.mjs';
+import { js_nodes_each } from '../nodes/each.mjs';
+import { list_adder } from '../../list/adder.mjs';
+export function js_node_identifiers(left_i) {
+    return list_adder(la => {
+        js_nodes_each(left_i, js_node_is_identifier, node => {
+            let name = js_identifier_name_get(node);
+            la(name);
+        });
+    });
+}
--- src/js/statements/refactorable.mjs
+++ src/js/statements/refactorable.mjs
@@ -1,7 +1,5 @@
-import { js_nodes_each } from '../nodes/each.mjs';
-import { list_adder } from '../../list/adder.mjs';
+import { js_node_identifiers } from '../node/identifiers.mjs';
 import { log } from '../../log.mjs';
-import { js_identifier_name_get } from '../identifier/name/get.mjs';
 import { list_length } from '../../list/length.mjs';
 import { equal_by } from '../../equal/by.mjs';
 import { not } from '../../not.mjs';
@@ -9,7 +7,6 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_is } from '../../list/is.mjs';
 import { range } from '../../range.mjs';
 import { list_get } from '../../list/get.mjs';
-import { js_node_is_identifier } from '../node/is/identifier.mjs';
 export function js_statements_refactorable(left, right) {
     arguments_assert(arguments, [
         list_is,
@@ -22,12 +19,7 @@ export function js_statements_refactorable(left, right) {
     for (let index of range(list_length(left))) {
         let left_i = list_get(left, index);
         let right_i = list_get(right, index);
-        let left_identifiers = list_adder(la => {
-            js_nodes_each(left_i, js_node_is_identifier, node => {
-                let name = js_identifier_name_get(node);
-                la(name);
-            });
-        });
+        let left_identifiers = js_node_identifiers(left_i);
         log({ left_identifiers });
     }
 }
