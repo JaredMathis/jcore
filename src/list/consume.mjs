e00c13b410460b298e549b9e31d6a6446193d2e0
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:01:28 -0400

    #232 function_auto list_consume

--- src/list/consume.mjs
+++ src/list/consume.mjs
@@ -1,11 +1,13 @@
+import { list_empty } from './empty.mjs';
 import { function_is } from '../function/is.mjs';
 import { list_is } from './is.mjs';
-import { error } from '../error.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { list_first_remove } from './first/remove.mjs';
 export function list_consume(list, lambda) {
     arguments_assert(arguments, [
         list_is,
         function_is
     ]);
-    error('todo: list_consume');
+    lambda(() => list_first_remove(list));
+    list_empty(replacements);
 }
