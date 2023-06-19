96dae131ad7d678cd411feb017d4d6d91ecebef4
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:09:32 -0400

    #232 noop

--- src/refactor/call/statement/find/previous.mjs
+++ src/refactor/call/statement/find/previous.mjs
@@ -42,7 +42,9 @@ export async function refactor_call_statement_find_previous(args) {
                 previous,
                 node
             ];
-            js_statements_refactorable(statements, function_name_find_statements);
+            if (!js_statements_refactorable(statements, function_name_find_statements)) {
+                return;
+            }
             log({
                 function_name,
                 function_name_find
