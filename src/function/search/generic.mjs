6b09ee809651627d84f798740c1f6033608ca1f0
JaredMathis <jared.mathis@native.consulting>
2023-06-19 19:01:11 -0400

    #257 file_js_all_identifier_rename candiate_to_string candidate_mapper

--- src/function/search/generic.mjs
+++ src/function/search/generic.mjs
@@ -6,7 +6,7 @@ import { list_string_to_dictionary } from '../../list/string/to/dictionary.mjs';
 import { identity } from '../../identity.mjs';
 import { search_generic } from '../../search/generic.mjs';
 import { function_name_all_tests_not } from '../name/all/tests/not.mjs';
-export async function function_search_generic(query, query_map, candidate_as_string_matches, candiate_to_string) {
+export async function function_search_generic(query, query_map, candidate_as_string_matches, candidate_mapper) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo,
@@ -14,6 +14,6 @@ export async function function_search_generic(query, query_map, candidate_as_str
         function_is
     ]);
     const candidates = await function_name_all_tests_not();
-    let candidates_matching = search_generic(candidates, candiate_to_string, candidate_as_string_matches, query, query_map);
+    let candidates_matching = search_generic(candidates, candidate_mapper, candidate_as_string_matches, query, query_map);
     return list_string_to_dictionary(candidates_matching, function_name_to_file_path);
 }
