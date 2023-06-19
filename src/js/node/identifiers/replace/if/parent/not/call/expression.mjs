7593d8c94b61d8f12be88f0a4648775626b66ef5
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:58:07 -0400

    #232 function_input_add_type js_node_identifiers_replace_if_parent_not_call_expression replacements list_is

--- src/js/node/identifiers/replace/if/parent/not/call/expression.mjs
+++ src/js/node/identifiers/replace/if/parent/not/call/expression.mjs
@@ -1,9 +1,13 @@
+import { list_is } from '../../../../../../../../list/is.mjs';
 import { js_visit_nodes_filter } from '../../../../../../../visit/nodes/filter.mjs';
 import { js_node_is_identifier } from '../../../../../../is/identifier.mjs';
 import { arguments_assert_todo } from '../../../../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
-export function js_node_identifiers_replace_if_parent_not_call_expression(node) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+export function js_node_identifiers_replace_if_parent_not_call_expression(node, replacements) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        list_is
+    ]);
     js_visit_nodes_filter(node, js_node_is_identifier, v => {
         let {node} = v;
     });
