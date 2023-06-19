99563c071a5009db73658a6d29d751075442ca08
JaredMathis <jared.mathis@native.consulting>
2023-06-19 11:15:23 -0400

    #243 function_add_mapper returnify

---
+++ src/function/returnify.mjs
@@ -0,0 +1,9 @@
+import { string_identifier_is } from '../string/identifier/is.mjs';
+import { refactor_returnify } from '../refactor/returnify.mjs';
+import { function_name_get } from './name/get.mjs';
+import { function_map_with_args } from './map/with/args.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export async function function_returnify(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
+    await function_map_with_args(function_name_get(refactor_returnify), function_name, {});
+}
---
+++ src/refactor/returnify.mjs
@@ -0,0 +1,7 @@
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function refactor_returnify(args) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    error('todo: refactor_returnify');
+}
