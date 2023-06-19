f8489582e1cad31ee4d16dcfeccdbdac74c16e1a
JaredMathis <jared.mathis@native.consulting>
2023-06-19 10:10:08 -0400

    #232 noop

--- src/js/statements/refactorable.mjs
+++ src/js/statements/refactorable.mjs
@@ -32,8 +32,8 @@ export function js_statements_refactorable(left, right) {
     for (let index of range(list_length(left))) {
         let left_i = list_get(left, index);
         let right_i = list_get(right, index);
-        let right_identifiers = js_node_identifiers(right_i);
         let left_identifiers = js_node_identifiers(left_i);
+        let right_identifiers = js_node_identifiers(right_i);
         if (not(equal_by(list_length, right_identifiers, left_identifiers))) {
             result_unsuccess(result);
             return result;
