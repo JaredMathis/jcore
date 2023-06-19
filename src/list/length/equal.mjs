aa830bebcd3972e9465cb9ac8f1a1886eb900fbc
JaredMathis <jared.mathis@native.consulting>
2023-06-18 21:28:17 -0400

    #232 function_auto list_length_equal

--- src/list/length/equal.mjs
+++ src/list/length/equal.mjs
@@ -2,13 +2,12 @@ import { list_is } from '../is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { list_length } from '../length.mjs';
+import { equal_by } from '../../equal/by.mjs';
 export function list_length_equal(a, b) {
     arguments_assert(arguments, [
         list_is,
         list_is
     ]);
-    let a_length = list_length(a);
-    let b_length = list_length(b);
-    return a_length === b_length;
+    return equal_by(list_length, a, b);
     metadata([]);
 }
