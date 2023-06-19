5ba95b37c1beba631c3761993a706a45191d506f
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:57:36 -0400

    #257 function_auto function_search_part

--- src/function/search/part.mjs
+++ src/function/search/part.mjs
@@ -1,11 +1,11 @@
 import { function_search_generic } from './generic.mjs';
-import { search_query_map_default } from '../../search/query/map/default.mjs';
 import { search_candidate_as_string_matches_default } from '../../search/candidate/as/string/matches/default.mjs';
 import { string_is } from '../../string/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { identity } from '../../identity.mjs';
 export async function function_search_part(query) {
     arguments_assert(arguments, [string_is]);
     let candidate_as_string_matches = search_candidate_as_string_matches_default;
-    let query_map = search_query_map_default;
+    let query_map = identity;
     return await function_search_generic(query, query_map, candidate_as_string_matches);
 }
