c7d04297d8cf3fdd08ae85f33255fc56be0586f7
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:22:16 -0400

    #232 function_auto try_catch_log

--- src/try/catch/log.mjs
+++ src/try/catch/log.mjs
@@ -10,10 +10,10 @@ export function try_catch_log(fn, lambda, error_message) {
         function_is,
         string_empty_not_is
     ]);
-    let result = try_catch(lambda, on_error);
-    return result;
-    metadata([]);
     function on_error(e) {
         error_caught(fn, e, error_message);
     }
+    let result = try_catch(lambda, on_error);
+    return result;
+    metadata([]);
 }
