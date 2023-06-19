ac912c143eb7b646617154c87af686f89ac9407c
JaredMathis <jared.mathis@native.consulting>
2023-06-19 16:47:49 -0400

    #242 noop

--- src/task/close.mjs
+++ src/task/close.mjs
@@ -5,10 +5,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 export async function task_close(task_number_string) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let lambda = task_state_change;
-    return await task_close_generic(task_number_string);
-}
-async function task_close_generic(task_number_string) {
     const property_value = task_property_closed();
-    let v = await task_state_change(task_number_string, property_value);
+    let v = await lambda(task_number_string, property_value);
     return v;
 }
