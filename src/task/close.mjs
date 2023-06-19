ddbaf25bfb18ca182b0777b13f51ad6fa5d7f719
JaredMathis <jared.mathis@native.consulting>
2023-06-19 16:46:40 -0400

    #242 function_auto task_close

--- src/task/close.mjs
+++ src/task/close.mjs
@@ -1,10 +1,9 @@
+import { task_close_generic } from './close/generic.mjs';
 import { task_state_change } from './state/change.mjs';
-import { task_property_closed } from './property/closed.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 export async function task_close(task_number_string) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    const property_value = task_property_closed();
-    let v = await task_state_change(task_number_string, property_value);
-    return v;
+    let lambda = task_state_change;
+    return await task_close_generic(task_number_string);
 }
