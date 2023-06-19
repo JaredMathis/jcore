e8443531e410b86ced7f86d6c50dc705e94d4ccb
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:51:41 -0400

    #232 function_input_add_first list_elements_all_equal_by map

--- src/list/elements/all/equal/by.mjs
+++ src/list/elements/all/equal/by.mjs
@@ -1,10 +1,12 @@
 import { list_elements_all_equal_generic } from './generic.mjs';
-import { equal } from '../../../../equal.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { equal_by } from '../../../../equal/by.mjs';
-export function list_elements_all_equal_by(values) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+export function list_elements_all_equal_by(map, values) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let lambda = (a, b) => equal_by(map, a, b);
     return list_elements_all_equal_generic(lambda, values);
 }
