5c0cd004e0d92050564c43c3a2b8bccc1dec5491
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:50:11 -0400

    #257 function_auto function_search

--- src/function/search.mjs
+++ src/function/search.mjs
@@ -1,17 +1,11 @@
+import { function_search_generic } from './search/generic.mjs';
 import { search_query_map_default } from '../search/query/map/default.mjs';
 import { search_candidate_as_string_matches_default } from '../search/candidate/as/string/matches/default.mjs';
-import { function_name_to_file_path } from './name/to/file/path.mjs';
-import { identity } from '../identity.mjs';
-import { search_generic } from '../search/generic.mjs';
-import { function_name_all_tests_not } from './name/all/tests/not.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { string_is } from '../string/is.mjs';
-import { list_string_to_dictionary } from '../list/string/to/dictionary.mjs';
 export async function function_search(query) {
     arguments_assert(arguments, [string_is]);
     let candidate_as_string_matches = search_candidate_as_string_matches_default;
     let query_map = search_query_map_default;
-    const candidates = await function_name_all_tests_not();
-    let candidates_matching = search_generic(candidates, identity, candidate_as_string_matches, query, query_map);
-    return list_string_to_dictionary(candidates_matching, function_name_to_file_path);
+    return await function_search_generic(candidate_as_string_matches, query, query_map);
 }
---
+++ src/function/search/generic.mjs
@@ -0,0 +1,10 @@
+import { function_name_to_file_path } from '../name/to/file/path.mjs';
+import { list_string_to_dictionary } from '../../list/string/to/dictionary.mjs';
+import { identity } from '../../identity.mjs';
+import { search_generic } from '../../search/generic.mjs';
+import { function_name_all_tests_not } from '../name/all/tests/not.mjs';
+export async function function_search_generic(candidate_as_string_matches, query, query_map) {
+    const candidates = await function_name_all_tests_not();
+    let candidates_matching = search_generic(candidates, identity, candidate_as_string_matches, query, query_map);
+    return list_string_to_dictionary(candidates_matching, function_name_to_file_path);
+}
