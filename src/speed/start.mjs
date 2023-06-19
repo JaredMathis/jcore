338a23adcdf6a6104b8c1f47c06902f84e8be6cd
JaredMathis <jared.mathis@native.consulting>
2023-06-19 13:52:14 -0400

    #247 function_auto speed_start

--- src/speed/start.mjs
+++ src/speed/start.mjs
@@ -1,8 +1,8 @@
-import { error } from '../error.mjs';
+import { speed_property_previous_set } from './property/previous/set.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 export function speed_start() {
     arguments_assert(arguments, []);
-    let result = {
-        
-    };
+    let result = {};
+    speed_property_previous_set(result, performance.now());
+    return result;
 }
