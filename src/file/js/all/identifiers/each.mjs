611ff93ab0ff180a3fd0dd5be26e0332221e8bbe
JaredMathis <jared.mathis@native.consulting>
2023-06-19 13:53:21 -0400

    #247 function_auto file_js_all_identifiers_each

--- src/file/js/all/identifiers/each.mjs
+++ src/file/js/all/identifiers/each.mjs
@@ -1,3 +1,5 @@
+import { speed_property_previous_get } from '../../../../speed/property/previous/get.mjs';
+import { speed_start } from '../../../../speed/start.mjs';
 import { log } from '../../../../log.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
@@ -8,8 +10,9 @@ export async function file_js_all_identifiers_each(lambda) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let all = await file_js_all_path();
     for (let file_path of all) {
-        const t0 = performance.now();
+        let s = speed_start();
         let parsed = await file_js_parse(file_path);
+        let previous = speed_property_previous_get(s);
         const t1 = performance.now() - t0;
         let identifiers = js_identifiers(parsed);
         const t2 = performance.now() - t1;
