b4884c827eeb8c4f233d3b35172e143552d877df
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:56:02 -0400

    #232 function_copy_suffix_add js_node_identifiers replace_if_parent_not_call_expression

---
+++ src/js/node/identifiers/replace/if/parent/not/call/expression.mjs
@@ -0,0 +1,15 @@
+import { js_identifier_name_get } from '../../../../../../../identifier/name/get.mjs';
+import { js_node_is_identifier } from '../../../../../../is/identifier.mjs';
+import { js_nodes_each } from '../../../../../../../nodes/each.mjs';
+import { list_adder } from '../../../../../../../../list/adder.mjs';
+import { arguments_assert_todo } from '../../../../../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
+export function js_node_identifiers_replace_if_parent_not_call_expression(left_i) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    return list_adder(la => {
+        js_nodes_each(left_i, js_node_is_identifier, node => {
+            let name = js_identifier_name_get(node);
+            la(name);
+        });
+    });
+}
