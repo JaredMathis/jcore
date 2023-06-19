f05a08d5cd3fe2c1747fcb232464f0d6897387d4
JaredMathis <jared.mathis@native.consulting>
2023-06-18 20:37:54 -0400

    #232 function_delete string_empty_is

--- src/string/empty/is.mjs
+++
@@ -1,7 +0,0 @@
-import { string_length } from '../length.mjs';
-import { string_is } from '../is.mjs';
-import { arguments_assert } from '../../arguments/assert.mjs';
-export function string_empty_is(s) {
-    arguments_assert(arguments, [string_is]);
-    return string_length(s) === 0;
-}
