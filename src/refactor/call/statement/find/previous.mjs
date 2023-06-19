83f8ff5389355bf6dc055b54faa730646a6da93d
JaredMathis <jared.mathis@native.consulting>
2023-06-18 20:33:16 -0400

    #232 function_auto refactor_call_statement_find_previous

--- src/refactor/call/statement/find/previous.mjs
+++ src/refactor/call/statement/find/previous.mjs
@@ -39,7 +39,7 @@ export async function refactor_call_statement_find_previous(args) {
         });
     });
     let function_name_find_statements_last_name = list_single(function_name_find_statements_last_names);
-    log({function_name_find_statements_last_name})
+    log({ function_name_find_statements_last_name });
     let function_name = js_mapper_args_to_function_name(args);
     js_visit_call_statements(args, (stack_reversed, node, expression, parent_list) => {
         js_node_call_expression_if_name_equal(expression, function_name_find, () => {
