69e5e8a43c8c1214550dd697729776194e4ffc96
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:23:03 -0400

    #232 function_auto list_consume_try

--- src/list/consume/try.mjs
+++ src/list/consume/try.mjs
@@ -2,11 +2,13 @@ import { function_is } from '../../function/is.mjs';
 import { list_is } from '../is.mjs';
 import { list_consume } from '../consume.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { try_catch } from '../../try/catch.mjs';
 export function list_consume_try(list, lambda) {
     arguments_assert(arguments, [
         list_is,
         function_is
     ]);
-    let result = list_consume(list, lambda);
-    return result;
+    let success = true;
+    try_catch(() => list_consume(list, lambda), e => success = false);
+    return success;
 }
