f73b054027bedd940b889b578fa378c2889d2d4a
JaredMathis <jared.mathis@native.consulting>
2023-06-19 16:46:56 -0400

    #242 function_delete task_close_generic

--- src/task/close/generic.mjs
+++
@@ -1,7 +0,0 @@
-import { task_state_change } from '../state/change.mjs';
-import { task_property_closed } from '../property/closed.mjs';
-export async function task_close_generic(task_number_string) {
-    const property_value = task_property_closed();
-    let v = await task_state_change(task_number_string, property_value);
-    return v;
-}
