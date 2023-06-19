089f7011f2b27b0bb184247e48615dbff5517b59
JaredMathis <jared.mathis@native.consulting>
2023-06-19 08:29:21 -0400

    #232 function_auto js_node_identifiers_replaceify

--- src/js/node/identifiers/replaceify.mjs
+++ src/js/node/identifiers/replaceify.mjs
@@ -1,15 +1,12 @@
+import { js_visit_identifiers_not_call_expressions } from '../../visit/identifiers/not/call/expressions.mjs';
 import { log } from '../../../log.mjs';
 import { result_property_data_set } from '../../../result/property/data/set.mjs';
 import { object_property_ensure } from '../../../object/property/ensure.mjs';
 import { list_consume_try } from '../../../list/consume/try.mjs';
-import { js_node_is } from '../is.mjs';
 import { js_identifier_name_change } from '../../identifier/name/change.mjs';
 import { list_is } from '../../../list/is.mjs';
-import { js_visit_nodes_filter } from '../../visit/nodes/filter.mjs';
-import { js_node_is_identifier } from '../is/identifier.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { js_node_is_call_expression } from '../is/call/expression.mjs';
 import { js_identifier_name_get } from '../../identifier/name/get.mjs';
 import { result_unsuccess } from '../../../result/unsuccess.mjs';
 import { result_empty } from '../../../result/empty.mjs';
@@ -21,15 +18,7 @@ export function js_node_identifiers_replaceify(node, replacements) {
     let result = result_empty();
     let dictionary = {};
     if (list_consume_try(replacements, next => {
-            js_visit_nodes_filter(node, js_node_is_identifier, v => {
-                let {parent} = v;
-                if (js_node_is(parent) && js_node_is_call_expression(parent)) {
-                    return;
-                }
-                let {node} = v;
-                lambda(node);
-            });
-
+            js_visit_identifiers_not_call_expressions(node, lambda);
             function lambda(node) {
                 let existing = js_identifier_name_get(node);
                 let replacement = next();
---
+++ src/js/visit/identifiers/not/call/expressions.mjs
@@ -0,0 +1,14 @@
+import { js_node_is_call_expression } from '../../../../node/is/call/expression.mjs';
+import { js_node_is } from '../../../../node/is.mjs';
+import { js_node_is_identifier } from '../../../../node/is/identifier.mjs';
+import { js_visit_nodes_filter } from '../../../nodes/filter.mjs';
+export function js_visit_identifiers_not_call_expressions(node, lambda) {
+    js_visit_nodes_filter(node, js_node_is_identifier, v => {
+        let {parent} = v;
+        if (js_node_is(parent) && js_node_is_call_expression(parent)) {
+            return;
+        }
+        let {node} = v;
+        lambda(node);
+    });
+}
