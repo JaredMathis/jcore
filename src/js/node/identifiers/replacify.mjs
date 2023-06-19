dbc5bb2505cff23e64c643cad223366d6cf9fe46
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:03:28 -0400

    #232 noop

--- src/js/node/identifiers/replacify.mjs
+++ src/js/node/identifiers/replacify.mjs
@@ -4,6 +4,7 @@ import { js_visit_nodes_filter } from '../../visit/nodes/filter.mjs';
 import { js_node_is_identifier } from '../is/identifier.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { js_node_is_call_expression } from '../is/call/expression.mjs';
 export function js_node_identifiers_replacify(node, replacements) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -11,6 +12,10 @@ export function js_node_identifiers_replacify(node, replacements) {
     ]);
     list_consume(next => {
         js_visit_nodes_filter(node, js_node_is_identifier, v => {
+            let {parent} = v;
+            if (js_node_is_call_expression(parent)) {
+                return;
+            }
             let {node} = v;
             let n = next();
         });
