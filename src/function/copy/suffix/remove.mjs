275a4ea5afeefe3bf5aab8f88e700686d68565e3
JaredMathis <jared.mathis@native.consulting>
2023-06-19 17:56:15 -0400

    #255 function_auto function_copy_suffix_remove

--- src/function/copy/suffix/remove.mjs
+++ src/function/copy/suffix/remove.mjs
@@ -1,7 +1,5 @@
-import { string_identifier_parts_from } from '../../../string/identifier/parts/from.mjs';
-import { list_remove_last_verify_multiple } from '../../../list/remove/last/verify/multiple.mjs';
+import { string_identifier_suffix_remove } from '../../../string/identifier/suffix/remove.mjs';
 import { function_copy } from '../../copy.mjs';
-import { string_identifier_parts_to } from '../../../string/identifier/parts/to.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function function_copy_suffix_remove(function_name_old, function_name_suffix) {
@@ -9,9 +7,6 @@ export async function function_copy_suffix_remove(function_name_old, function_na
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let function_name_suffix_parts = string_identifier_parts_to(function_name_suffix);
-    let function_name_old_parts = string_identifier_parts_to(function_name_old);
-    list_remove_last_verify_multiple(function_name_old_parts, function_name_suffix_parts);
-    let function_name_new = string_identifier_parts_from(function_name_old_parts);
+    let function_name_new = string_identifier_suffix_remove(function_name_suffix, function_name_old);
     await function_copy(function_name_old, function_name_new);
 }
---
+++ src/string/identifier/suffix/remove.mjs
@@ -0,0 +1,10 @@
+import { string_identifier_parts_from } from '../parts/from.mjs';
+import { list_remove_last_verify_multiple } from '../../../list/remove/last/verify/multiple.mjs';
+import { string_identifier_parts_to } from '../parts/to.mjs';
+export function string_identifier_suffix_remove(function_name_suffix, function_name_old) {
+    let function_name_suffix_parts = string_identifier_parts_to(function_name_suffix);
+    let function_name_old_parts = string_identifier_parts_to(function_name_old);
+    list_remove_last_verify_multiple(function_name_old_parts, function_name_suffix_parts);
+    let function_name_new = string_identifier_parts_from(function_name_old_parts);
+    return function_name_new;
+}
