1be8a43aa35e543b3a339b814657a2c3783b45e1
JaredMathis <jared.mathis@native.consulting>
2023-06-19 11:25:43 -0400

    #243 function_auto js_visit_call_statements

--- src/js/visit/call/statements.mjs
+++ src/js/visit/call/statements.mjs
@@ -1,11 +1,5 @@
-import { add_1 } from '../../../add/1.mjs';
-import { list_is } from '../../../list/is.mjs';
-import { list_find_first_index_starting_at } from '../../../list/find/first/index/starting/at.mjs';
-import { list_get } from '../../../list/get.mjs';
-import { assert } from '../../../assert.mjs';
-import { js_node_is_block_statement } from '../../node/is/block/statement.mjs';
-import { js_node_is_program } from '../../node/is/program.mjs';
-import { list_reversed_get } from '../../../list/reversed/get.mjs';
+import { js_visit_stack_to_ancestor_list } from '../stack/to/ancestor/list.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { js_node_is_call_expression } from '../../node/is/call/expression.mjs';
 import { not } from '../../../not.mjs';
 import { js_return_statement_argument_get } from '../../return/statement/argument/get.mjs';
@@ -28,6 +22,10 @@ import { js_node_is_expression_statement } from '../../node/is/expression/statem
 import { js_visit_nodes_filter } from '../nodes/filter.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
 export function js_visit_call_statements(args, call_each) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let parsed = object_property_get(args, 'parsed');
     js_visit_nodes_filter(parsed, function v_18(n) {
         return js_node_is_expression_statement(n) || js_node_is_variable_declaration(n) || js_node_is_return_statement(n);
@@ -89,32 +87,4 @@ export function js_visit_call_statements(args, call_each) {
             let stack_reversed = js_visit_stack_to_ancestor_list(refactor_stack, lambda);
         }
     });
-}
-function js_visit_stack_to_ancestor_list(refactor_stack, lambda) {
-    let stack_reversed = list_reversed_get(refactor_stack);
-    let index_starting_at = 0;
-    let list_find_first_after_result = list_find_first_start_at(stack_reversed, index_starting_at);
-    let parent_list_next = object_property_get(list_find_first_after_result, 'next');
-    let parent_list_index = object_property_get(list_find_first_after_result, 'index');
-    if (!js_node_is_program(parent_list_next)) {
-        if (js_node_is_variable_declaration(parent_list_next)) {
-            list_find_first_after_result = list_find_first_start_at(stack_reversed, parent_list_index);
-            parent_list_next = object_property_get(list_find_first_after_result, 'next');
-            parent_list_index = object_property_get(list_find_first_after_result, 'index');
-        }
-        let v_14 = js_node_is_block_statement(parent_list_next);
-        assert(v_14);
-        let ancestor_list = list_get(stack_reversed, parent_list_index);
-        lambda(ancestor_list);
-    }
-    function list_find_first_start_at(stack_reversed, index_starting_at) {
-        let index = list_find_first_index_starting_at(stack_reversed, list_is, index_starting_at);
-        let index_next = add_1(index);
-        let next = list_get(stack_reversed, index_next);
-        return {
-            next,
-            index
-        };
-    }
-    return stack_reversed;
 }
---
+++ src/js/visit/stack/to/ancestor/list.mjs
@@ -0,0 +1,38 @@
+import { add_1 } from '../../../../../add/1.mjs';
+import { list_is } from '../../../../../list/is.mjs';
+import { list_find_first_index_starting_at } from '../../../../../list/find/first/index/starting/at.mjs';
+import { list_get } from '../../../../../list/get.mjs';
+import { assert } from '../../../../../assert.mjs';
+import { js_node_is_block_statement } from '../../../../node/is/block/statement.mjs';
+import { js_node_is_variable_declaration } from '../../../../node/is/variable/declaration.mjs';
+import { js_node_is_program } from '../../../../node/is/program.mjs';
+import { object_property_get } from '../../../../../object/property/get.mjs';
+import { list_reversed_get } from '../../../../../list/reversed/get.mjs';
+export function js_visit_stack_to_ancestor_list(refactor_stack, lambda) {
+    let stack_reversed = list_reversed_get(refactor_stack);
+    let index_starting_at = 0;
+    let list_find_first_after_result = list_find_first_start_at(stack_reversed, index_starting_at);
+    let parent_list_next = object_property_get(list_find_first_after_result, 'next');
+    let parent_list_index = object_property_get(list_find_first_after_result, 'index');
+    if (!js_node_is_program(parent_list_next)) {
+        if (js_node_is_variable_declaration(parent_list_next)) {
+            list_find_first_after_result = list_find_first_start_at(stack_reversed, parent_list_index);
+            parent_list_next = object_property_get(list_find_first_after_result, 'next');
+            parent_list_index = object_property_get(list_find_first_after_result, 'index');
+        }
+        let v_14 = js_node_is_block_statement(parent_list_next);
+        assert(v_14);
+        let ancestor_list = list_get(stack_reversed, parent_list_index);
+        lambda(ancestor_list);
+    }
+    function list_find_first_start_at(stack_reversed, index_starting_at) {
+        let index = list_find_first_index_starting_at(stack_reversed, list_is, index_starting_at);
+        let index_next = add_1(index);
+        let next = list_get(stack_reversed, index_next);
+        return {
+            next,
+            index
+        };
+    }
+    return stack_reversed;
+}
