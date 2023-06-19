0c5875759aec6f69d604d71a8f0f952ed0712d09
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:02:38 -0400

    #232 function_rename js_node_identifiers_replace_if_parent_not_call_expression js_node_identifiers_replacify

---
+++ src/js/node/identifiers/replacify.mjs
@@ -0,0 +1,18 @@
+import { list_consume } from '../../../list/consume.mjs';
+import { list_is } from '../../../list/is.mjs';
+import { js_visit_nodes_filter } from '../../visit/nodes/filter.mjs';
+import { js_node_is_identifier } from '../is/identifier.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function js_node_identifiers_replacify(node, replacements) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        list_is
+    ]);
+    list_consume(next => {
+        js_visit_nodes_filter(node, js_node_is_identifier, v => {
+            let {node} = v;
+            let n = next();
+        });
+    });
+}
