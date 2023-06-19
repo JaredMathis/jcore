fe6644661e032875c1ad5b9196785430865cb0e1
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:59:39 -0400

    #232 function_auto list_consume

---
+++ src/list/consume.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function list_consume() {
+    arguments_assert(arguments, []);
+    error('todo: list_consume');
+}
