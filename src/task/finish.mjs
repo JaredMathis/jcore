269fd1076e82936af13e885b63fe7de734c7e35b
JaredMathis <jared.mathis@native.consulting>
2023-06-19 16:48:59 -0400

    #242 function_auto task_finish

--- src/task/finish.mjs
+++ src/task/finish.mjs
@@ -1,17 +1,15 @@
-import { task_get } from './get.mjs';
+import { task_close_try } from './close/try.mjs';
 import { task_number_unhash } from './number/unhash.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { task_unset } from './unset.mjs';
 import { task_id_get_or_null } from './id/get/or/null.mjs';
-import { task_close } from './close.mjs';
 import { task_available } from './available.mjs';
 export async function task_finish() {
     arguments_assert(arguments, []);
     let task_number_hashed = await task_id_get_or_null();
     let task_number_string = task_number_unhash(task_number_hashed);
-    let task = await task_get(task_number_string);
-    await task_close(task_number_string);
+    await task_close_try(task_number_string);
     await task_unset();
     let v = await task_available();
     return v;
