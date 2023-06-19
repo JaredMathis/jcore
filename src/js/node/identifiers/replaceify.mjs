39b497f0da7c95c0cfe2fcfeccb865395fb12877
JaredMathis <jared.mathis@native.consulting>
2023-06-19 08:06:00 -0400

    #232 function_auto js_node_identifiers_replaceify

--- src/js/node/identifiers/replaceify.mjs
+++ src/js/node/identifiers/replaceify.mjs
@@ -1,3 +1,4 @@
+import { object_property_ensure } from '../../../object/property/ensure.mjs';
 import { list_consume_try } from '../../../list/consume/try.mjs';
 import { js_node_is } from '../is.mjs';
 import { js_identifier_name_change } from '../../identifier/name/change.mjs';
@@ -7,6 +8,7 @@ import { js_node_is_identifier } from '../is/identifier.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { js_node_is_call_expression } from '../is/call/expression.mjs';
+import { js_identifier_name_get } from '../../identifier/name/get.mjs';
 export function js_node_identifiers_replaceify(node, replacements) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -20,8 +22,10 @@ export function js_node_identifiers_replaceify(node, replacements) {
                 return;
             }
             let {node} = v;
-            let n = next();
-            js_identifier_name_change(node, n);
+            let existing = js_identifier_name_get(node);
+            let replacement = next();
+            object_property_ensure(dictionary, existing, replacement);
+            js_identifier_name_change(node, replacement);
         });
     });
     return false;
