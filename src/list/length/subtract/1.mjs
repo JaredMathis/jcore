df51e9d7990c7363a986c30964bddd9e8558e1b5
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:48:25 -0400

    #232 function_wrap_with_name_auto list_length subtract_1

---
+++ src/list/length/subtract/1.mjs
@@ -0,0 +1,10 @@
+import { list_is } from '../../is.mjs';
+import { subtract_1 } from '../../../subtract/1.mjs';
+import { list_length } from '../../length.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function list_length_subtract_1(list) {
+    arguments_assert(arguments, [list_is]);
+    let result_to_be_mapped = list_length(list);
+    let result = subtract_1(result_to_be_mapped);
+    return result;
+}
