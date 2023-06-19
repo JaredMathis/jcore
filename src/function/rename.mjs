db40562fa464ec6275b5805a27447f56e6cb4aa1
JaredMathis <jared.mathis@native.consulting>
2023-06-19 13:22:11 -0400

    #242 function_auto function_rename

--- src/function/rename.mjs
+++ src/function/rename.mjs
@@ -1,3 +1,4 @@
+import { log } from '../log.mjs';
 import { function_rename_if_starts_with } from './rename/if/starts/with.mjs';
 import { function_name_all_tests_not_starts_with } from './name/all/tests/not/starts/with.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
@@ -11,6 +12,7 @@ export async function function_rename(function_name_old, function_name_new) {
         string_identifier_is,
         string_identifier_is
     ]);
+    log('a');
     let starting_withs = await function_name_all_tests_not_starts_with(function_name_old);
     let v = list_length_is_1(starting_withs);
     assert_message(v, `Multiple functions exist. Use ${ function_name_get(function_rename_single) } if you're sure or consider ${ function_name_get(function_rename_if_starts_with) }: ` + starting_withs);
