5a6578b721d7556cb269b9a6526ff9001992bcb5
JaredMathis <jared.mathis@native.consulting>
2023-06-19 17:55:53 -0400

    #255 function_auto function_copy_suffix_remove

--- src/function/copy/suffix/remove.mjs
+++ src/function/copy/suffix/remove.mjs
@@ -1,6 +1,6 @@
+import { string_identifier_parts_from } from '../../../string/identifier/parts/from.mjs';
 import { list_remove_last_verify_multiple } from '../../../list/remove/last/verify/multiple.mjs';
 import { function_copy } from '../../copy.mjs';
-import { string_identifier_parts_from_combine } from '../../../string/identifier/parts/from/combine.mjs';
 import { string_identifier_parts_to } from '../../../string/identifier/parts/to.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -11,10 +11,7 @@ export async function function_copy_suffix_remove(function_name_old, function_na
     ]);
     let function_name_suffix_parts = string_identifier_parts_to(function_name_suffix);
     let function_name_old_parts = string_identifier_parts_to(function_name_old);
-    list_remove_last_verify_multiple;
-    let function_name_new = string_identifier_parts_from_combine([
-        function_name_old_parts,
-        function_name_suffix_parts
-    ]);
+    list_remove_last_verify_multiple(function_name_old_parts, function_name_suffix_parts);
+    let function_name_new = string_identifier_parts_from(function_name_old_parts);
     await function_copy(function_name_old, function_name_new);
 }
