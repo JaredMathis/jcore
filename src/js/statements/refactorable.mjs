c1ced81f4b2e49a0a3185cb8c327ce292aeb554c
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:55:49 -0400

    #232 function_auto js_statements_refactorable

--- src/js/statements/refactorable.mjs
+++ src/js/statements/refactorable.mjs
@@ -1,3 +1,4 @@
+import { result_unsuccess } from '../../result/unsuccess.mjs';
 import { js_node_identifiers_replaceify } from '../node/identifiers/replaceify.mjs';
 import { object_copy_json } from '../../object/copy/json.mjs';
 import { js_node_identifiers } from '../node/identifiers.mjs';
@@ -10,13 +11,16 @@ import { range } from '../../range.mjs';
 import { list_get } from '../../list/get.mjs';
 import { js_unparse } from '../unparse.mjs';
 import { log_multiple } from '../../log/multiple.mjs';
+import { result_empty } from '../../result/empty.mjs';
 export function js_statements_refactorable(left, right) {
     arguments_assert(arguments, [
         list_is,
         list_is
     ]);
+    let result = result_empty();
     if (not(equal_by(list_length, left, right))) {
-        return false;
+        result_unsuccess(result);
+        return result;
     }
     for (let index of range(list_length(left))) {
         let left_i = list_get(left, index);
@@ -28,15 +32,18 @@ export function js_statements_refactorable(left, right) {
             right_identifiers
         ]);
         if (not(equal_by(list_length, left_identifiers, right_identifiers))) {
-            return false;
+            result_unsuccess(result);
+            return result;
         }
         let left_i_copy = object_copy_json(left_i);
         if (!js_node_identifiers_replaceify(left_i_copy, right_identifiers)) {
-            return false;
+            result_unsuccess(result);
+            return result;
         }
         if (!equal_by(js_unparse, left_i_copy, right_i)) {
-            return false;
+            result_unsuccess(result);
+            return result;
         }
     }
-    return true;
+    return result;
 }
