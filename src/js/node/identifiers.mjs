e7fbf5f93c9a97e7adba815a6578cdbe7966ac15
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:56:40 -0400

    #232 function_auto js_node_identifiers

--- src/js/node/identifiers.mjs
+++ src/js/node/identifiers.mjs
@@ -1,8 +1,11 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { js_identifier_name_get } from '../identifier/name/get.mjs';
 import { js_node_is_identifier } from './is/identifier.mjs';
 import { js_nodes_each } from '../nodes/each.mjs';
 import { list_adder } from '../../list/adder.mjs';
 export function js_node_identifiers(node) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     return list_adder(la => {
         js_nodes_each(node, js_node_is_identifier, node => {
             let name = js_identifier_name_get(node);
--- src/js/node/identifiers/replace/if/parent/not/call/expression.mjs
+++ src/js/node/identifiers/replace/if/parent/not/call/expression.mjs
@@ -4,10 +4,10 @@ import { js_nodes_each } from '../../../../../../../nodes/each.mjs';
 import { list_adder } from '../../../../../../../../list/adder.mjs';
 import { arguments_assert_todo } from '../../../../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
-export function js_node_identifiers_replace_if_parent_not_call_expression(left_i) {
+export function js_node_identifiers_replace_if_parent_not_call_expression(noop) {
     arguments_assert(arguments, [arguments_assert_todo]);
     return list_adder(la => {
-        js_nodes_each(left_i, js_node_is_identifier, node => {
+        js_nodes_each(noop, js_node_is_identifier, node => {
             let name = js_identifier_name_get(node);
             la(name);
         });
