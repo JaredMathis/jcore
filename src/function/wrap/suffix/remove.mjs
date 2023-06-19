2b1a858ca9550fb193ff18c42ba7a58b3d0aea9c
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:03:08 -0400

    #255 function_auto function_wrap_suffix_remove

--- src/function/wrap/suffix/remove.mjs
+++ src/function/wrap/suffix/remove.mjs
@@ -1,14 +1,17 @@
-import { string_identifier_suffix_remove } from '../../../string/identifier/suffix/remove.mjs';
 import { function_wrap } from '../../wrap.mjs';
 import { string_identifier_first_not_is } from '../../../string/identifier/first/not/is.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { string_identifier_parts_from } from '../../../string/identifier/parts/from.mjs';
 export async function function_wrap_suffix_remove(function_name_to_wrap_without_suffix, suffix_to_remove) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_first_not_is
     ]);
-    let function_name_to_wrap = string_identifier_suffix_remove(function_name_to_wrap_without_suffix, suffix_to_remove);
+    let function_name_to_wrap = string_identifier_parts_from([
+        function_name_to_wrap_without_suffix,
+        suffix_to_remove
+    ]);
     let result = await function_wrap(function_name_to_wrap, function_name_to_wrap_without_suffix);
     return result;
 }
