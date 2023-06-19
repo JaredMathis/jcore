f56aace876dc76176887f0b686510fb8396270f9
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:41:15 -0400

    #232 function_auto object_copy_json

--- src/object/copy/json.mjs
+++ src/object/copy/json.mjs
@@ -1,7 +1,7 @@
-import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { defined_is } from '../../defined/is.mjs';
 export function object_copy_json(value) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [defined_is]);
     error('todo: object_copy_json');
 }
