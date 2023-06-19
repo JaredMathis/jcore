f17fb59a2255b8eeeb4aa636ff8d71380c981d55
JaredMathis <jared.mathis@native.consulting>
2023-06-19 13:12:23 -0400

    #242 function_auto git_pacp_generic

--- src/git/pacp/generic.mjs
+++ src/git/pacp/generic.mjs
@@ -1,3 +1,4 @@
+import { date_now_iso_underscores } from '../../date/now/iso/underscores.mjs';
 import { task_now } from '../../task/now.mjs';
 import { null_is } from '../../null/is.mjs';
 import { task_id_get_or_null } from '../../task/id/get/or/null.mjs';
@@ -10,7 +11,8 @@ export async function git_pacp_generic(sync) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let task_id = await task_id_get_or_null();
     if (null_is(task_id)) {
-        await task_now();
+        let now = date_now_iso_underscores();
+        await task_now(now);
     }
     let initial = data_git_commit_message_initial();
     assert(task_id !== initial);
