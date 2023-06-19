ce5ed2f1641fdab39fcfce3e0dbf5a94bbec7c44
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:29:26 -0400

    #232 noop

--- src/refactor/call/statement/find/previous.mjs
+++ src/refactor/call/statement/find/previous.mjs
@@ -32,7 +32,7 @@ export async function refactor_call_statement_find_previous(args) {
     let function_name_find_statements_last = list_last(function_name_find_statements);
     let function_name_find_statements_last_name = js_call_statement_name(function_name_find_statements_last);
     if (null_not_is(return_statement)) {
-        
+        console.log({function_name_find_statements_last})
     }
     let function_name = js_mapper_args_to_function_name(args);
     js_visit_call_statements(args, (stack_reversed, node, expression, parent_list) => {
@@ -45,6 +45,7 @@ export async function refactor_call_statement_find_previous(args) {
                 previous,
                 node
             ];
+            return;
             log({
                 function_name,
                 function_name_find
