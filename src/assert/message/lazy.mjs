24a5f2beb93babcda979faca05a22ba71e2710af
JaredMathis <jared.mathis@native.consulting>
2023-06-19 14:20:07 -0400

    #247 noop

--- src/assert/message/lazy.mjs
+++ src/assert/message/lazy.mjs
@@ -4,13 +4,14 @@ import { not } from '../../not.mjs';
 import { assert_arguments_count } from '../arguments/count.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { function_is } from '../../function/is.mjs';
 export function assert_message_lazy(value, message_get) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
     assert_arguments_count(arguments, 2);
-    if (not(string_is(message_get))) {
+    if (not(function_is(message_get))) {
         error('invalid message');
     }
     if (value === true) {
