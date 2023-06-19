b1e7823da8ecccd19a2e982498dd8c45d6ca43b6
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:50:35 -0400

    #232 function_auto refactor_functions_metadata_extra_remove

--- src/refactor/functions/metadata/extra/remove.mjs
+++ src/refactor/functions/metadata/extra/remove.mjs
@@ -1,21 +1,19 @@
+import { list_length_subtract_1 } from '../../../../list/length/subtract/1.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { js_mapper_args_to_metadata_args } from '../../../../js/mapper/args/to/metadata/args.mjs';
 import { js_statement_metadata_args_get } from '../../../../js/statement/metadata/args/get.mjs';
 import { list_add_multiple } from '../../../../list/add/multiple.mjs';
-import { subtract_1 } from '../../../../subtract/1.mjs';
 import { list_take } from '../../../../list/take.mjs';
 import { js_statement_metadata_is } from '../../../../js/statement/metadata/is.mjs';
 import { file_js_all_map_args_if_function } from '../../../../file/js/all/map/args/if/function.mjs';
 import { metadata } from '../../../../metadata.mjs';
-import { list_length } from '../../../../list/length.mjs';
 import { list_filter } from '../../../../list/filter.mjs';
 import { list_remove } from '../../../../list/remove.mjs';
 export async function refactor_functions_metadata_extra_remove() {
     arguments_assert(arguments, []);
     await file_js_all_map_args_if_function(async function logic(args) {
         let last_metadata_args = js_mapper_args_to_metadata_args(args);
-        let v_2 = list_length(statements);
-        let v = subtract_1(v_2);
+        let v = list_length_subtract_1(statements);
         let remaining = list_take(statements, v);
         let removals = list_filter(remaining, js_statement_metadata_is);
         for (let r of removals) {
