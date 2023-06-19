c1cdcbc3ce5e0c783e7fe255df34cd08989f9a83
JaredMathis <jared.mathis@native.consulting>
2023-06-19 17:33:03 -0400

    #254 noop

--- src/task/id/get/or/null.mjs
+++ src/task/id/get/or/null.mjs
@@ -5,9 +5,10 @@ import { task_property_id } from '../../../property/id.mjs';
 import { data_get } from '../../../../data/get.mjs';
 export async function task_id_get_or_null() {
     arguments_assert(arguments, []);
+    let lambda = object_property_get_or;
     let data = await data_get();
     let v = task_property_id();
-    let v_2 = object_property_get_or(data, v, null);
+    let v_2 = lambda(data, v, null);
     return v_2;
     metadata([]);
 }
