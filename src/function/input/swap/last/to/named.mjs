ba26f6c8b19c3e3808b58dd5d54c308e8cf35b83
JaredMathis <jared.mathis@native.consulting>
2023-06-19 08:02:57 -0400

    #232 function_auto function_input_swap_last_to_named

--- src/function/input/swap/last/to/named.mjs
+++ src/function/input/swap/last/to/named.mjs
@@ -1,12 +1,13 @@
-import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
+import { function_inputs_index_of_string } from '../../../../inputs/index/of/string.mjs';
 import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
 import { function_input_swap_last_to } from '../to.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export async function function_input_swap_last_to_named(function_name, index_string) {
+export async function function_input_swap_last_to_named(function_name, input_name) {
     arguments_assert(arguments, [
         string_identifier_is,
-        arguments_assert_todo
+        string_identifier_is
     ]);
+    let index_string = await function_inputs_index_of_string(function_name, input_name);
     let result = await function_input_swap_last_to(function_name, index_string);
     return result;
 }
