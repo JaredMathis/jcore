274eb4c97f28cfefb5b1226036111909ba1f102f
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:57:05 -0400

    #232 noop

--- src/js/node/identifiers/replace/if/parent/not/call/expression.mjs
+++ src/js/node/identifiers/replace/if/parent/not/call/expression.mjs
@@ -4,10 +4,10 @@ import { js_nodes_each } from '../../../../../../../nodes/each.mjs';
 import { list_adder } from '../../../../../../../../list/adder.mjs';
 import { arguments_assert_todo } from '../../../../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
-export function js_node_identifiers_replace_if_parent_not_call_expression(left_i) {
+export function js_node_identifiers_replace_if_parent_not_call_expression(node) {
     arguments_assert(arguments, [arguments_assert_todo]);
     return list_adder(la => {
-        js_nodes_each(left_i, js_node_is_identifier, n => {
+        js_nodes_each(node, js_node_is_identifier, n => {
             let name = js_identifier_name_get(n);
             la(name);
         });
