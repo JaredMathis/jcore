3fd1184375893ca6af65641a9e16bbcd73ec6b6c
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:41:54 -0400

    #232 function_auto object_copy_json

--- src/object/copy/json.mjs
+++ src/object/copy/json.mjs
@@ -1,7 +1,10 @@
 import { json_to_minimized } from '../../json/to/minimized.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { defined_is } from '../../defined/is.mjs';
+import { json_from } from '../../json/from.mjs';
 export function object_copy_json(value) {
     arguments_assert(arguments, [defined_is]);
     let json = json_to_minimized(value);
+    let copy = json_from(json);
+    return copy;
 }
