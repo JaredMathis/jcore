8bbc3b4ce93ec53908fc87d33b7124a30b1cc0e6
JaredMathis <jared.mathis@native.consulting>
2023-06-19 09:48:26 -0400

    #232 function_search import_fix

--- src/refactor/call/statement/find/replaceify.mjs
+++ src/refactor/call/statement/find/replaceify.mjs
@@ -63,35 +63,39 @@ export async function refactor_call_statement_find_replaceify(args) {
         }
     }
     let function_find_inputs = js_function_declaration_to_params_names(function_declaration_find);
-    occurs();
-    js_visit_call_statements(args, (stack_reversed, node, expression, parent_list) => {
-        js_node_call_expression_if_name_equal(expression, function_name_find_statements_last_name, () => {
-            log(function_name);
-            let index = list_index_of(parent_list, node);
-            let index_previous = subtract_1(index);
-            let previous = list_get(parent_list, index_previous);
-            let statements = [
-                previous,
-                node
-            ];
-            let refactorable = js_statements_refactorable(statements, function_name_find_statements);
-            if (!result_property_success_get(refactorable)) {
-                return;
-            }
-            let refactorable_data = result_property_data_get(refactorable);
-            let statements_to_remove = list_take_without_last(statements);
-            for (let s of statements_to_remove) {
-                list_remove(parent_list, s);
-            }
-            js_call_expression_name_change(expression, function_name_find);
-            log({ expression });
-            let args = js_node_property_arguments_get(expression);
-            list_replace(args, list_map(args, a => {
-                let name_before = js_identifier_name_get(a);
-                let name_after = object_property_get(refactorable_data, name_before);
-                let p_after = js_parse_identifier(name_after);
-                return p_after;
-            }));
+    if (occurs(o => {
+        js_visit_call_statements(args, (stack_reversed, node, expression, parent_list) => {
+            js_node_call_expression_if_name_equal(expression, function_name_find_statements_last_name, () => {
+                log(function_name);
+                let index = list_index_of(parent_list, node);
+                let index_previous = subtract_1(index);
+                let previous = list_get(parent_list, index_previous);
+                let statements = [
+                    previous,
+                    node
+                ];
+                let refactorable = js_statements_refactorable(statements, function_name_find_statements);
+                if (!result_property_success_get(refactorable)) {
+                    return;
+                }
+                let refactorable_data = result_property_data_get(refactorable);
+                let statements_to_remove = list_take_without_last(statements);
+                for (let s of statements_to_remove) {
+                    list_remove(parent_list, s);
+                }
+                js_call_expression_name_change(expression, function_name_find);
+                log({ expression });
+                let args = js_node_property_arguments_get(expression);
+                list_replace(args, list_map(args, a => {
+                    let name_before = js_identifier_name_get(a);
+                    let name_after = object_property_get(refactorable_data, name_before);
+                    let p_after = js_parse_identifier(name_after);
+                    return p_after;
+                }));
+                o();
+            });
         });
-    });
+    })) {
+        await refactor_import_fix()
+    }
 }
