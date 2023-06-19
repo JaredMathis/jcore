d9ab2284213bf22b069322d2fdeef531a07bb28b
JaredMathis <jared.mathis@native.consulting>
2023-06-19 07:23:58 -0400

    #232 function_auto js_node_identifiers_replaceify

--- src/js/node/identifiers/replaceify.mjs
+++ src/js/node/identifiers/replaceify.mjs
@@ -1,6 +1,6 @@
+import { list_consume_try } from '../../../list/consume/try.mjs';
 import { js_node_is } from '../is.mjs';
 import { js_identifier_name_change } from '../../identifier/name/change.mjs';
-import { list_consume } from '../../../list/consume.mjs';
 import { list_is } from '../../../list/is.mjs';
 import { js_visit_nodes_filter } from '../../visit/nodes/filter.mjs';
 import { js_node_is_identifier } from '../is/identifier.mjs';
@@ -12,7 +12,7 @@ export function js_node_identifiers_replaceify(node, replacements) {
         arguments_assert_todo,
         list_is
     ]);
-    list_consume(replacements, next => {
+    return list_consume_try(replacements, next => {
         js_visit_nodes_filter(node, js_node_is_identifier, v => {
             let {parent} = v;
             if (js_node_is(parent) && js_node_is_call_expression(parent)) {
