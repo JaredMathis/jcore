efc5e8ac5726791e3d280a0c91912e4792cc8013
JaredMathis <jared.mathis@native.consulting>
2023-06-19 16:52:49 -0400

    #242 function_auto task_close_try

--- src/task/close/try.mjs
+++ src/task/close/try.mjs
@@ -1,3 +1,4 @@
+import { task_property_state_set } from '../property/state/set.mjs';
 import { task_close_generic } from './generic.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
