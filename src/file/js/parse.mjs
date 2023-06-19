ff5bb78f9279c529a060c838bdad777118ec8f5f
JaredMathis <jared.mathis@native.consulting>
2023-06-19 13:33:36 -0400

    #242 function_auto file_js_parse

--- src/file/js/parse.mjs
+++ src/file/js/parse.mjs
@@ -7,11 +7,11 @@ import { assert } from '../../assert.mjs';
 import { log } from '../../log.mjs';
 export async function file_js_parse(file_path) {
     arguments_assert(arguments, [path_is]);
-    log('beforee')
+    log('beforee');
     let {parsed} = await file_js_parse_with_unparsed(file_path);
     let v = js_node_is(parsed);
     assert(v);
-    log('after')
+    log('after');
     return parsed;
     metadata([]);
 }
