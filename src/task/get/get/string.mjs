e08500fc557cd087f3fc027b81765fc4dd834443
JaredMathis <jared.mathis@native.consulting>
2023-06-19 18:20:34 -0400

    #254 function_auto task_get_get_string

--- src/task/get/get/string.mjs
+++ src/task/get/get/string.mjs
@@ -1,7 +1,11 @@
+import { task_number_unhash } from '../../number/unhash.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_to } from '../../../string/to.mjs';
 import { task_id_get } from '../../id/get.mjs';
 export async function task_get_get_string() {
+    arguments_assert(arguments, []);
     let task_number = await task_id_get();
-    let task_number_string = string_to(task_number);
-    return task_number_string;
+    let task_number_string_hashed = string_to(task_number);
+    let unhashed = task_number_unhash(task_number_string);
+    return unhashed;
 }
