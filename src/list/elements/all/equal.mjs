43e3f6086bffe6cc9da4e197bba9ab15a9834c73
JaredMathis <jared.mathis@native.consulting>
2023-06-19 06:45:25 -0400

    #232 noop

--- src/list/elements/all/equal.mjs
+++ src/list/elements/all/equal.mjs
@@ -2,6 +2,7 @@ import { equal } from '../../../equal.mjs';
 import { list_all } from '../../all.mjs';
 import { list_first } from '../../first.mjs';
 export function list_elements_all_equal(values) {
+    let lambda = equal;
     let first = list_first(values);
     let list_all_equal = list_all(values, function list_all_each(v) {
         let e = equal(v, first);
