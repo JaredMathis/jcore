2defd5296fdcbf3f19d5f18be8df014612f8a57c
JaredMathis <jared.mathis@native.consulting>
2023-06-20 09:30:53 -0400

    #64 function_names_metadata_arguments_assert_none

--- src/function/names/metadata/arguments/assert/none.mjs
+++ src/function/names/metadata/arguments/assert/none.mjs
@@ -12,6 +12,7 @@ import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
 import { assert } from '../../../../../assert.mjs';
 import { function_names_each } from '../../../each.mjs';
 import { list_adder_async } from '../../../../../list/adder/async.mjs';
+import { list_empty_not } from '../../../../../list/empty/not.mjs';
 export async function function_names_metadata_arguments_assert_none() {
     arguments_assert(arguments, []);
     return await list_adder_async(async la => {
@@ -20,6 +21,9 @@ export async function function_names_metadata_arguments_assert_none() {
             assert(string_identifier_is(function_name));
             let metadata_args = await js_mapper_args_to_metadata_args_no_add_missing(args);
             let filtered = list_filter(metadata_args, js_node_is_call_expression);
+            if (list_empty_not(filtered)){
+                console.log({filtered})
+            }
             let filtered2 = list_filter(filtered, node => js_node_call_expression_name_equal(node, function_name_get(metadata_arguments_assert_none)));
             if (list_empty(filtered2)) {
                 return;
