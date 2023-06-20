ee62eace3425da09384e6ad3a2e06170c3c3052c
JaredMathis <jared.mathis@native.consulting>
2023-06-20 09:33:55 -0400

    #64 function_auto function_names_metadata_arguments_assert_none

--- src/function/names/metadata/arguments/assert/none.mjs
+++ src/function/names/metadata/arguments/assert/none.mjs
@@ -1,5 +1,5 @@
+import { js_node_property_argument_get } from '../../../../../js/node/property/argument/get.mjs';
 import { js_mapper_args_to_metadata_args_no_add_missing } from '../../../../../js/mapper/args/to/metadata/args/no/add/missing.mjs';
-import { log } from '../../../../../log.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { list_length_is_1 } from '../../../../../list/length/is/1.mjs';
 import { list_empty } from '../../../../../list/empty.mjs';
@@ -12,7 +12,7 @@ import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
 import { assert } from '../../../../../assert.mjs';
 import { function_names_each } from '../../../each.mjs';
 import { list_adder_async } from '../../../../../list/adder/async.mjs';
-import { list_empty_not } from '../../../../../list/empty/not.mjs';
+import { list_map } from '../../../../../list/map.mjs';
 export async function function_names_metadata_arguments_assert_none() {
     arguments_assert(arguments, []);
     return await list_adder_async(async la => {
@@ -21,9 +21,8 @@ export async function function_names_metadata_arguments_assert_none() {
             assert(string_identifier_is(function_name));
             let metadata_args = await js_mapper_args_to_metadata_args_no_add_missing(args);
             let filtered = list_filter(metadata_args, js_node_is_call_expression);
-            if (list_empty_not(filtered)) {
-                console.log({ metadata_args });
-            }
+            return;
+            let mapped = list_map(metadata_args, js_node_property_argument_get);
             let filtered2 = list_filter(filtered, node => js_node_call_expression_name_equal(node, function_name_get(metadata_arguments_assert_none)));
             if (list_empty(filtered2)) {
                 return;
