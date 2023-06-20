d2ce37b7ef76b2305a115b180515469e34f46d51
JaredMathis <jared.mathis@native.consulting>
2023-06-20 08:36:43 -0400

    #64 function_auto metadata_arguments_assert_none

---
+++ src/metadata/arguments/assert/none.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function metadata_arguments_assert_none() {
+    arguments_assert(arguments, []);
+    error('todo: metadata_arguments_assert_none');
+}
