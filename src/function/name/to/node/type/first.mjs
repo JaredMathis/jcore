0ca57ea2cf44e56d75a1eacf0d71974f93b9157d
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:14:57 -0400

    #272 function_copy function_name_to_node_types function_name_to_node_type_first

---
+++ src/function/name/to/node/type/first.mjs
@@ -0,0 +1,10 @@
+import { file_js_nodes_types_get } from '../../../../../file/js/nodes/types/get.mjs';
+import { function_name_to_file_path } from '../../file/path.mjs';
+import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export async function function_name_to_node_type_first(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
+    let file_path = function_name_to_file_path(function_name);
+    let result = await file_js_nodes_types_get(file_path);
+    return result;
+}
