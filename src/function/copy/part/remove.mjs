cdb96a808248cf3e2795b37de1e9619176a441d1
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:03:37 -0400

    #270 function_auto function_copy_part_remove

--- src/function/copy/part/remove.mjs
+++ src/function/copy/part/remove.mjs
@@ -1,3 +1,4 @@
+import { string_identifier_parts_from } from '../../../string/identifier/parts/from.mjs';
 import { string_identifier_parts_to } from '../../../string/identifier/parts/to.mjs';
 import { function_copy } from '../../copy.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
@@ -10,5 +11,6 @@ export async function function_copy_part_remove(function_name_prefix, part_to_re
     ]);
     let function_name_prefix_parts = string_identifier_parts_to(function_name_prefix);
     list_remove(function_name_prefix_parts, part_to_remove);
+    let function_name_of_copy = string_identifier_parts_from(function_name_prefix_parts);
     await function_copy(function_name_prefix, function_name_of_copy);
 }
