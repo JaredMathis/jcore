cea16f199fc67e32df1fa2d29e23951aef1f05d5
JaredMathis <jared.mathis@native.consulting>
2023-06-20 01:16:02 -0400

    #261 function_auto rules

--- src/rules.mjs
+++ src/rules.mjs
@@ -1,6 +1,8 @@
+import { tests } from './tests.mjs';
 import { rule_function_async_and_suffix_async_implies_suffix_asynk } from './rule/function/async/and/suffix/async/implies/suffix/asynk.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 export async function rules() {
     arguments_assert(arguments, []);
     await rule_function_async_and_suffix_async_implies_suffix_asynk();
+    await tests();
 }
