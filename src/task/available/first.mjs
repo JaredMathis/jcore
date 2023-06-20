5cb44fa60752e2b919942c6270f57370d94dd151
JaredMathis <jared.mathis@native.consulting>
2023-06-20 10:02:22 -0400

    #266 function_auto noop

--- src/task/available/first.mjs
+++ src/task/available/first.mjs
@@ -2,9 +2,10 @@ import { identity } from '../../identity.mjs';
 import { task_available_generic } from './generic.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_first } from '../../list/first.mjs';
+import { argument_binary_first } from '../../argument/binary/first.mjs';
 export async function task_available_first() {
     arguments_assert(arguments, []);
-    let available = await task_available_generic(identity);
+    let available = await task_available_generic(argument_binary_first);
     let last = list_first(available);
     return last;
 }
