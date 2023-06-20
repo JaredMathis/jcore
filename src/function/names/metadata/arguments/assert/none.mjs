86204b184082b8dd01be66b330f0ea249d4d0c73
JaredMathis <jared.mathis@native.consulting>
2023-06-20 09:40:14 -0400

    #64 function_names_metadata_arguments_assert_none

--- src/function/names/metadata/arguments/assert/none.mjs
+++ src/function/names/metadata/arguments/assert/none.mjs
@@ -21,10 +21,8 @@ export async function function_names_metadata_arguments_assert_none() {
             let {function_name} = args;
             assert(string_identifier_is(function_name));
             let metadata_args = await js_mapper_args_to_metadata_args_no_add_missing(args);
-            let mapped = list_map(metadata_args, js_node_property_arguments_get);
-            let mapped2 = list_map(mapped, list_single);
-            let filtered = list_filter(mapped2, js_node_is_call_expression);
-            let filtered2 = list_filter(filtered, node => js_node_call_expression_name_equal(node, function_name_get(metadata_arguments_assert_none)));
+            console.log({metadata_args})
+            let filtered2 = list_filter(metadata_args, node => js_node_call_expression_name_equal(node, function_name_get(metadata_arguments_assert_none)));
             if (list_empty(filtered2)) {
                 return;
             }
