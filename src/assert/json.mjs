344c9f55f8f74d9e22d2d28c064b984dae40401d
JaredMathis <jared.mathis@native.consulting>
2023-06-20 10:48:08 -0400

    #113 noop

--- src/assert/json.mjs
+++ src/assert/json.mjs
@@ -1,5 +1,6 @@
 import { json_to } from '../json/to.mjs';
 import { assert_message } from './message.mjs';
 export function assert_json(v, info) {
-    assert_message(v, json_to(info));
+    let v_2 = json_to(info);
+    assert_message(v, v_2);
 }
