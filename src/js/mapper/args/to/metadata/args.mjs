afa910a9e29173c8209726aff8f0798237305ebf
JaredMathis <jared.mathis@native.consulting>
2023-06-20 09:18:33 -0400

    #64 function_auto js_mapper_args_to_metadata_args

--- src/js/mapper/args/to/metadata/args.mjs
+++ src/js/mapper/args/to/metadata/args.mjs
@@ -1,33 +1,8 @@
-import { not } from '../../../../../not.mjs';
+import { js_mapper_args_to_metadata_args_generic } from './args/generic.mjs';
 import { defined_is } from '../../../../../defined/is.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-import { js_statement_metadata_is } from '../../../../statement/metadata/is.mjs';
-import { refactor_metadata_missing_add } from '../../../../../refactor/metadata/missing/add.mjs';
-import { list_empty } from '../../../../../list/empty.mjs';
-import { js_function_declaration_to_statements } from '../../../../function/declaration/to/statements.mjs';
-import { js_statement_metadata_args_get } from '../../../../statement/metadata/args/get.mjs';
-import { list_last } from '../../../../../list/last.mjs';
 export async function js_mapper_args_to_metadata_args(args) {
     arguments_assert(arguments, [defined_is]);
     let add_missing = true;
-    let {function_declaration} = args;
-    let statements = js_function_declaration_to_statements(function_declaration);
-    let missing = false;
-    if (list_empty(statements)) {
-        missing = true;
-    } else {
-        let last_statement = list_last(statements);
-        if (not(js_statement_metadata_is(last_statement))) {
-            missing = true;
-        }
-    }
-    if (add_missing) {
-        if (missing) {
-            await refactor_metadata_missing_add(args);
-        }
-    }
-    let statements2 = js_function_declaration_to_statements(function_declaration);
-    let last_statement2 = list_last(statements2);
-    let metadata_args = js_statement_metadata_args_get(last_statement2);
-    return metadata_args;
+    return await js_mapper_args_to_metadata_args_generic(args, add_missing);
 }
---
+++ src/js/mapper/args/to/metadata/args/generic.mjs
@@ -0,0 +1,29 @@
+import { js_statement_metadata_args_get } from '../../../../../statement/metadata/args/get.mjs';
+import { refactor_metadata_missing_add } from '../../../../../../refactor/metadata/missing/add.mjs';
+import { js_statement_metadata_is } from '../../../../../statement/metadata/is.mjs';
+import { not } from '../../../../../../not.mjs';
+import { list_last } from '../../../../../../list/last.mjs';
+import { list_empty } from '../../../../../../list/empty.mjs';
+import { js_function_declaration_to_statements } from '../../../../../function/declaration/to/statements.mjs';
+export async function js_mapper_args_to_metadata_args_generic(args, add_missing) {
+    let {function_declaration} = args;
+    let statements = js_function_declaration_to_statements(function_declaration);
+    let missing = false;
+    if (list_empty(statements)) {
+        missing = true;
+    } else {
+        let last_statement = list_last(statements);
+        if (not(js_statement_metadata_is(last_statement))) {
+            missing = true;
+        }
+    }
+    if (add_missing) {
+        if (missing) {
+            await refactor_metadata_missing_add(args);
+        }
+    }
+    let statements2 = js_function_declaration_to_statements(function_declaration);
+    let last_statement2 = list_last(statements2);
+    let metadata_args = js_statement_metadata_args_get(last_statement2);
+    return metadata_args;
+}
