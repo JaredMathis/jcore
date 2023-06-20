f4e8ec2f82e824cec763e5273286b78a6cb23aa4
JaredMathis <jared.mathis@native.consulting>
2023-06-20 08:07:36 -0400

    #263 function_delete refactor_functions_arguments_assert_add

--- src/refactor/functions/arguments/assert/add.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../../../../error.mjs';
-import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function refactor_functions_arguments_assert_add() {
-    arguments_assert(arguments, []);
-    error('todo: refactor_functions_arguments_assert_add');
-}
