2e7860e716c761a41b52939fe40aed99687f1908
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:33:00 -0400

    #62 function_auto function_rename_single

--- src/function/rename/single.mjs
+++ src/function/rename/single.mjs
@@ -1,4 +1,9 @@
-import { function_rename_no_open } from './no/open.mjs';
+import { metadata } from '../../metadata.mjs';
+import { function_rename_after } from './after.mjs';
+import { list_multiple_combine } from '../../list/multiple/combine.mjs';
+import { file_js_all_identifier_rename } from '../../file/js/all/identifier/rename.mjs';
+import { file_js_all_identifier_multiple } from '../../file/js/all/identifier/multiple.mjs';
+import { function_rename_without_all_refactor } from './without/all/refactor.mjs';
 import { string_identifier_is } from '../../string/identifier/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function function_rename_single(function_name_old, function_name_new) {
@@ -6,5 +11,13 @@ export async function function_rename_single(function_name_old, function_name_ne
         string_identifier_is,
         string_identifier_is
     ]);
-    await function_rename_no_open(function_name_old, function_name_new);
+    let tests_renames = await function_rename_without_all_refactor(function_name_old, function_name_new);
+    let file_paths_tests_changed = await file_js_all_identifier_multiple(tests_renames);
+    let file_paths_changed = await file_js_all_identifier_rename(function_name_old, function_name_new);
+    let v = list_multiple_combine([
+        file_paths_tests_changed,
+        file_paths_changed
+    ]);
+    await function_rename_after(v);
+    metadata([]);
 }
