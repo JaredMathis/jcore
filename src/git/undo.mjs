4cb17a5865628bd5212bb1f4862f15b0b38486ab
JaredMathis <jared.mathis@native.consulting>
2023-06-20 08:28:43 -0400

    #263 noop

--- src/git/undo.mjs
+++ src/git/undo.mjs
@@ -1,8 +1,9 @@
 import { command_line } from '../command/line.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { git_command_name } from './command/name.mjs';
+import { git_rhh } from './rhh.mjs';
 export async function git_undo() {
     arguments_assert(arguments, []);
-    let result = await command_line(`${ git_command_name() } reset --soft HEAD~1`);
+    let result = await command_line(`${ git_command_name() } reset --hard HEAD~1`);
     return result;
 }
