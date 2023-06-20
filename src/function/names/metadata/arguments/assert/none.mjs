f9116d3ae80ba88e304b8658433ee720d583b6c7
JaredMathis <jared.mathis@native.consulting>
2023-06-20 09:16:34 -0400

    #64 function_auto refactor_functions_arguments_assert_missing_add_excludes

---
+++ src/function/names/metadata/arguments/assert/none.mjs
@@ -0,0 +1,28 @@
+import { list_length_is_1 } from '../../../../../list/length/is/1.mjs';
+import { list_empty } from '../../../../../list/empty.mjs';
+import { metadata_arguments_assert_none } from '../../../../../metadata/arguments/assert/none.mjs';
+import { function_name_get } from '../../../../name/get.mjs';
+import { js_node_call_expression_name_equal } from '../../../../../js/node/call/expression/name/equal.mjs';
+import { js_node_is_call_expression } from '../../../../../js/node/is/call/expression.mjs';
+import { list_filter } from '../../../../../list/filter.mjs';
+import { js_mapper_args_to_metadata_args } from '../../../../../js/mapper/args/to/metadata/args.mjs';
+import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
+import { assert } from '../../../../../assert.mjs';
+import { function_names_each } from '../../../each.mjs';
+import { list_adder_async } from '../../../../../list/adder/async.mjs';
+export async function function_names_metadata_arguments_assert_none() {
+    return await list_adder_async(async la => {
+        await function_names_each(async args => {
+            let {function_name} = args;
+            assert(string_identifier_is(function_name));
+            let metadata_args = await js_mapper_args_to_metadata_args(args);
+            let filtered = list_filter(metadata_args, js_node_is_call_expression);
+            let filtered2 = list_filter(filtered, node => js_node_call_expression_name_equal(node, function_name_get(metadata_arguments_assert_none)));
+            if (list_empty(filtered2)) {
+                return;
+            }
+            assert(list_length_is_1(filtered2));
+            la(function_name);
+        });
+    });
+}
--- src/refactor/functions/arguments/assert/missing/add/excludes.mjs
+++ src/refactor/functions/arguments/assert/missing/add/excludes.mjs
@@ -1,35 +1,15 @@
-import { list_adder_async } from '../../../../../../list/adder/async.mjs';
-import { metadata_arguments_assert_none } from '../../../../../../metadata/arguments/assert/none.mjs';
-import { js_node_call_expression_name_equal } from '../../../../../../js/node/call/expression/name/equal.mjs';
-import { function_names_each } from '../../../../../../function/names/each.mjs';
-import { js_mapper_args_to_metadata_args } from '../../../../../../js/mapper/args/to/metadata/args.mjs';
+import { function_names_metadata_arguments_assert_none } from '../../../../../../function/names/metadata/arguments/assert/none.mjs';
 import { refactor_functions_arguments_assert_missing_add_excludes_additional } from './excludes/additional.mjs';
 import { function_dependencies_names_arguments_assert } from '../../../../../../function/dependencies/names/arguments/assert.mjs';
 import { list_add_multiple } from '../../../../../../list/add/multiple.mjs';
 import { comment } from '../../../../../../comment.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
 import { error } from '../../../../../../error.mjs';
-import { list_filter } from '../../../../../../list/filter.mjs';
-import { js_node_is_call_expression } from '../../../../../../js/node/is/call/expression.mjs';
-import { list_empty } from '../../../../../../list/empty.mjs';
-import { assert } from '../../../../../../assert.mjs';
-import { list_length_is_1 } from '../../../../../../list/length/is/1.mjs';
-import { function_name_get } from '../../../../../../function/name/get.mjs';
 export async function refactor_functions_arguments_assert_missing_add_excludes() {
     arguments_assert(arguments, []);
     let excludes = await function_dependencies_names_arguments_assert();
     comment(`Eventually this should be refactored maybe to read files and detect an attribute`);
-    let excludes_additional = await list_adder_async(async la => {
-        await function_names_each(async args => {
-            let metadata_args = await js_mapper_args_to_metadata_args(args);
-            let filtered = list_filter(metadata_args, js_node_is_call_expression);
-            let filtered2 = list_filter(filtered, node => js_node_call_expression_name_equal(node, function_name_get(metadata_arguments_assert_none)));
-            if (list_empty(filtered2)) {
-                return;
-            }
-            assert(list_length_is_1(filtered2));
-        });
-    });
+    let excludes_additional = await function_names_metadata_arguments_assert_none();
     error();
     excludes_additional = refactor_functions_arguments_assert_missing_add_excludes_additional();
     list_add_multiple(excludes, excludes_additional);
