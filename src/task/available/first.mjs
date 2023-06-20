99449ebdd2a5d4a98c901941d018b170303e22d6
JaredMathis <jared.mathis@native.consulting>
2023-06-20 09:48:31 -0400

    #267 function_auto task_available_first

--- src/task/available/first.mjs
+++ src/task/available/first.mjs
@@ -1,10 +1,10 @@
-import { list_last } from '../../list/last.mjs';
 import { identity } from '../../identity.mjs';
 import { task_available_generic } from './generic.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { list_first } from '../../list/first.mjs';
 export async function task_available_first() {
     arguments_assert(arguments, []);
     let available = await task_available_generic(identity);
-    let last = list_last(available);
+    let last = list_first(available);
     return last;
 }
