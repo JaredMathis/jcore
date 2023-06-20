891d687d956435866c95ad90d7eda9777dc6f904
JaredMathis <jared.mathis@native.consulting>
2023-06-19 23:50:09 -0400

    #257 noop

--- src/function/search/part.mjs
+++ src/function/search/part.mjs
@@ -5,7 +5,7 @@ import { identity } from '../../identity.mjs';
 import { string_identifier_parts_to } from '../../string/identifier/parts/to.mjs';
 export async function function_search_part(query) {
     arguments_assert(arguments, [string_is]);
-    let candidate_mapped_matches = (candidate_mapped, query_mapped) => false;
+    let candidate_mapped_matches = list_contains;
     let query_map = identity;
     let candidate_mapper = string_identifier_parts_to;
     return await function_search_generic(query, query_map, candidate_mapped_matches, candidate_mapper);
