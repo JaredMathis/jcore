783ca604d830cb46aa602d07df587e12926e2d0a
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:10:19 -0400

    #270 function_auto function_name_to_node_types

--- src/function/name/to/node/types.mjs
+++ src/function/name/to/node/types.mjs
@@ -1,8 +1,8 @@
-import { path_is } from '../../../../path/is.mjs';
 import { file_js_nodes_types_get } from '../../../../file/js/nodes/types/get.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function function_name_to_node_types(file_path) {
-    arguments_assert(arguments, [path_is]);
+import { string_identifier_is } from '../../../../string/identifier/is.mjs';
+export async function function_name_to_node_types(function_name) {
+    arguments_assert(arguments, [string_identifier_is]);
     let result = await file_js_nodes_types_get(file_path);
     return result;
 }
