9f4854434a2ffd3f17fa6d62acac6023275d0afc
JaredMathis <jared.mathis@native.consulting>
2023-06-20 08:12:46 -0400

    #263 noop

--- src/function/unasyncify.mjs
+++ src/function/unasyncify.mjs
@@ -10,6 +10,9 @@ import { arguments_assert } from '../arguments/assert.mjs';
 export async function function_unasyncify(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let ending = function_naming_suffix_asynk();
+    if (false) {
+        
+    }
     let function_name_result = string_suffix_without(function_name, ending);
     await function_delete_if_exists(function_name_result);
     await function_copy(function_name, function_name_result);
