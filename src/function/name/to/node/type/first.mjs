d83c5ee6453c1f44584d2256fce3e29bbf37c60b
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:25:03 -0400

    #272 function_auto function_name_to_node_type_first

--- src/function/name/to/node/type/first.mjs
+++ src/function/name/to/node/type/first.mjs
@@ -1,15 +1,18 @@
-import { js_node_is } from '../../../../../js/node/is.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_parse } from '../../../../parse.mjs';
-import { js_visit_nodes_filter } from '../../../../../js/visit/nodes/filter.mjs';
+import { js_node_is_type } from '../../../../../js/node/is/type.mjs';
+import { list_adder } from '../../../../../list/adder.mjs';
+import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 export async function function_name_to_node_type_first(function_name, node_type_name) {
     arguments_assert(arguments, [
         string_identifier_is,
         arguments_assert_todo
     ]);
     let parsed = function_parse(function_name);
-    js_visit_nodes_filter(parsed, js_node_is);
+    list_adder(la => {
+        js_nodes_each(parsed, n => js_node_is_type(n, node_type_name), n => la(n));
+    });
     return result;
 }
