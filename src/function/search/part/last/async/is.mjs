3009b492f32c75e92ca0b3d04f3d51f0948451e4
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:15:25 -0400

    #62 function_auto function_search_part_last_async_is

--- src/function/search/part/last/async/is.mjs
+++ src/function/search/part/last/async/is.mjs
@@ -1,6 +1,5 @@
+import { function_name_async_is } from '../../../../name/async/is.mjs';
 import { string_identifier_parts_from } from '../../../../../string/identifier/parts/from.mjs';
-import { js_node_property_async_get } from '../../../../../js/node/property/async/get.mjs';
-import { function_name_to_declaration } from '../../../../name/to/declaration.mjs';
 import { function_search_part_generic } from '../../generic.mjs';
 import { list_last } from '../../../../../list/last.mjs';
 import { equal } from '../../../../../equal.mjs';
@@ -10,8 +9,7 @@ export async function function_search_part_last_async_is(query) {
     arguments_assert(arguments, [string_is]);
     let candidate_mapped_matches = async function v(candidate_mapped, query_mapped) {
         let function_name = string_identifier_parts_from(candidate_mapped);
-        let fd = await function_name_to_declaration(function_name);
-        let async_is = js_node_property_async_get(fd);
+        let async_is = await function_name_async_is(function_name);
         if (!async_is) {
             return false;
         }
