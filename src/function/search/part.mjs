c5ae29245e99d4e9eaeeb536944517f38e6a4159
JaredMathis <jared.mathis@native.consulting>
2023-06-19 23:53:01 -0400

    #254 noop

--- src/function/search/part.mjs
+++ src/function/search/part.mjs
@@ -1,13 +1,9 @@
+import { function_search_part_generic } from './part/generic.mjs';
 import { list_contains } from '../../list/contains.mjs';
-import { function_search_generic } from './generic.mjs';
 import { string_is } from '../../string/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { identity } from '../../identity.mjs';
-import { string_identifier_parts_to } from '../../string/identifier/parts/to.mjs';
 export async function function_search_part(query) {
     arguments_assert(arguments, [string_is]);
-    let candidate_mapped_matches = list_contains;
-    let query_map = identity;
-    let candidate_mapper = string_identifier_parts_to;
-    return await function_search_generic(query, query_map, candidate_mapped_matches, candidate_mapper);
+    let candidate_mapped_matches = (candidate_mapped, query_mapped) => list_contains(candidate_mapped, query_mapped);
+    return function_search_part_generic(query, candidate_mapped_matches);
 }
---
+++ src/function/search/part/generic.mjs
@@ -0,0 +1,8 @@
+import { function_search_generic } from '../generic.mjs';
+import { string_identifier_parts_to } from '../../../string/identifier/parts/to.mjs';
+import { identity } from '../../../identity.mjs';
+export async function function_search_part_generic(query, candidate_mapped_matches) {
+    let query_map = identity;
+    let candidate_mapper = string_identifier_parts_to;
+    return await function_search_generic(query, query_map, candidate_mapped_matches, candidate_mapper);
+}
