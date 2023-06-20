9ea00386d3da0036b6d56667b41bbe4cf7e4dd2b
JaredMathis <jared.mathis@native.consulting>
2023-06-20 08:40:19 -0400

    #64 function_auto metadata_arguments_assert_none_initialize

--- src/metadata/arguments/assert/none/initialize.mjs
+++ src/metadata/arguments/assert/none/initialize.mjs
@@ -1,6 +1,6 @@
-import { error } from '../../../../error.mjs';
+import { refactor_functions_arguments_assert_missing_add_excludes_additional } from '../../../../refactor/functions/arguments/assert/missing/add/excludes/additional.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function metadata_arguments_assert_none_initialize() {
     arguments_assert(arguments, []);
-    error('todo: metadata_arguments_assert_none_initialize');
+    const excludes_additional = refactor_functions_arguments_assert_missing_add_excludes_additional();
 }
