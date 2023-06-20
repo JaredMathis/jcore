9e281b8e15811e8b291f2f1b8b7495b098ac91d3
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:23:31 -0400

    #272 function_input_add function_name_to_node_type_first node_type_name

--- src/function/name/to/node/type/first.mjs
+++ src/function/name/to/node/type/first.mjs
@@ -1,8 +1,15 @@
+import { js_node_is } from '../../../../../js/node/is.mjs';
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_parse } from '../../../../parse.mjs';
-export async function function_name_to_node_type_first(function_name) {
-    arguments_assert(arguments, [string_identifier_is]);
+import { js_visit_nodes_filter } from '../../../../../js/visit/nodes/filter.mjs';
+export async function function_name_to_node_type_first(function_name, node_type_name) {
+    arguments_assert(arguments, [
+        string_identifier_is,
+        arguments_assert_todo
+    ]);
     let parsed = function_parse(function_name);
+    js_visit_nodes_filter(parsed, js_node_is);
     return result;
 }
