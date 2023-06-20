d6678ee683a17b7dce37659d0b6ba113636027a5
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:14:23 -0400

    #272 function_auto function_name_to_node_type_first

---
+++ src/function/name/to/node/type/first.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../../../error.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export function function_name_to_node_type_first() {
+    arguments_assert(arguments, []);
+    error('todo: function_name_to_node_type_first');
+}
