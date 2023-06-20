49228da4408f7db22978fc4eea7c261c9ec517d9
JaredMathis <jared.mathis@native.consulting>
2023-06-20 09:15:11 -0400

    #64 function_auto refactor_functions_arguments_assert_missing_add_excludes

--- src/refactor/functions/arguments/assert/missing/add/excludes.mjs
+++ src/refactor/functions/arguments/assert/missing/add/excludes.mjs
@@ -14,6 +14,7 @@ import { js_node_is_call_expression } from '../../../../../../js/node/is/call/ex
 import { list_empty } from '../../../../../../list/empty.mjs';
 import { assert } from '../../../../../../assert.mjs';
 import { list_length_is_1 } from '../../../../../../list/length/is/1.mjs';
+import { function_name_get } from '../../../../../../function/name/get.mjs';
 export async function refactor_functions_arguments_assert_missing_add_excludes() {
     arguments_assert(arguments, []);
     let excludes = await function_dependencies_names_arguments_assert();
@@ -22,7 +23,7 @@ export async function refactor_functions_arguments_assert_missing_add_excludes()
         await function_names_each(async args => {
             let metadata_args = await js_mapper_args_to_metadata_args(args);
             let filtered = list_filter(metadata_args, js_node_is_call_expression);
-            let filtered2 = list_filter(filtered, node => js_node_call_expression_name_equal(node, metadata_arguments_assert_none()));
+            let filtered2 = list_filter(filtered, node => js_node_call_expression_name_equal(node, function_name_get(metadata_arguments_assert_none)));
             if (list_empty(filtered2)) {
                 return;
             }
