07916494caa63baa3d5328cb96a1fb33b4199399
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:45:05 -0400

    #62 function_auto search_generic_async

--- src/search/generic/async.mjs
+++ src/search/generic/async.mjs
@@ -1,16 +1,16 @@
 import { list_filter_async } from '../../list/filter/async.mjs';
 import { string_is } from '../../string/is.mjs';
 import { function_is } from '../../function/is.mjs';
-import { list_is } from '../../list/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function search_generic_async(candidates, candidate_mapper, candidate_mapped_matches, query, query_map) {
+export async function search_generic_async(candidates_get, candidate_mapper, candidate_mapped_matches, query, query_map) {
     arguments_assert(arguments, [
-        list_is,
+        function_is,
         function_is,
         function_is,
         string_is,
         function_is
     ]);
+    let candidates = await candidates_get();
     let query_mapped = await query_map(query);
     let candidates_matching = await list_filter_async(candidates, async function list_filter_predicate(candidate) {
         let candidate_mapped = await candidate_mapper(candidate);
