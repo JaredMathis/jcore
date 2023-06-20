8c2218b13a74b7cf6810175c6f14a62583bb010c
JaredMathis <jared.mathis@native.consulting>
2023-06-20 19:57:25 -0400

    #274 noop

--- src/js/code/call/expression/with/args.mjs
+++ src/js/code/call/expression/with/args.mjs
@@ -2,10 +2,11 @@ import { js_code_join_comma } from '../../../join/comma.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { js_code_call_expression_with_args_code } from './args/code.mjs';
+import { list_is } from '../../../../../list/is.mjs';
 export function js_code_call_expression_with_args(call_expression_function_name, params) {
     arguments_assert(arguments, [
         arguments_assert_todo,
-        arguments_assert_todo
+        list_is
     ]);
     let params_code2 = js_code_join_comma(params);
     const statment_code = js_code_call_expression_with_args_code(call_expression_function_name, params_code2);
