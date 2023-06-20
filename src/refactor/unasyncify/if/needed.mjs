965f569d8eb710009f5b67df437a06e7a66fb381
JaredMathis <jared.mathis@native.consulting>
2023-06-20 08:02:31 -0400

    #63 function_auto refactor_unasyncify_if_needed

--- src/refactor/unasyncify/if/needed.mjs
+++ src/refactor/unasyncify/if/needed.mjs
@@ -1,8 +1,10 @@
+import { js_mapper_args_to_function_name } from '../../../js/mapper/args/to/function/name.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 import { refactor_unasyncify } from '../../unasyncify.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function refactor_unasyncify_if_needed(args) {
     arguments_assert(arguments, [defined_is]);
+    let function_name = js_mapper_args_to_function_name(args);
     let result = await refactor_unasyncify(args);
     return result;
 }
