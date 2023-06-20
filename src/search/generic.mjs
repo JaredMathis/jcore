519f642ba73e4e97fd710e96584133b3428ff318
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:45:15 -0400

    #62 function_unasyncify search_generic_async

--- src/search/generic.mjs
+++ src/search/generic.mjs
@@ -3,9 +3,8 @@ import { metadata } from '../metadata.mjs';
 import { list_filter } from '../list/filter.mjs';
 import { string_is } from '../string/is.mjs';
 import { function_is } from '../function/is.mjs';
-import { list_is } from '../list/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function search_generic(candidates, candidate_mapper, candidate_mapped_matches, query, query_map) {
+export function search_generic(candidates_get, candidate_mapper, candidate_mapped_matches, query, query_map) {
     arguments_assert(arguments, [
         function_is,
         function_is,
