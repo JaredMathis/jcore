8ccf60fa6090fe69a127249cf2bae92ff16e2101
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:14:30 -0400

    #272 function_delete function_name_to_node_type_first

--- src/function/name/to/node/type/first.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../../../../../error.mjs';
-import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export function function_name_to_node_type_first() {
-    arguments_assert(arguments, []);
-    error('todo: function_name_to_node_type_first');
-}
