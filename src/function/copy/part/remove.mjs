40db40af01623de628dae42c3a6ab431ebff0f4c
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:01:07 -0400

    #270 noop

--- src/function/copy/part/remove.mjs
+++ src/function/copy/part/remove.mjs
@@ -7,7 +7,6 @@ export async function function_copy_part_remove(function_name_prefix, part_to_re
         string_identifier_is,
         string_identifier_is
     ]);
-    let function_name_to_copy = ;
     let function_name_of_copy = ;
-    await function_copy(function_name_to_copy, function_name_of_copy);
+    await function_copy(function_name_prefix, function_name_of_copy);
 }
