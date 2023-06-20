c769ba29ae2366d1b603ad4e14d224bff08f6bca
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:02:03 -0400

    #62 function_auto function_search_part_last

--- src/function/search/part/last.mjs
+++ src/function/search/part/last.mjs
@@ -1,5 +1,4 @@
 import { function_search_part_generic } from './generic.mjs';
-import { list_contains } from '../../../list/contains.mjs';
 import { list_last } from '../../../list/last.mjs';
 import { equal } from '../../../equal.mjs';
 import { string_is } from '../../../string/is.mjs';
@@ -7,10 +6,7 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function function_search_part_last(query) {
     arguments_assert(arguments, [string_is]);
     let candidate_mapped_matches = function v(candidate_mapped, query_mapped) {
-        if (equal(list_last(candidate_mapped), query_mapped)) {
-            return false;
-        }
-        return list_contains(candidate_mapped, query_mapped);
+        return equal(list_last(candidate_mapped), query_mapped);
     };
     return function_search_part_generic(query, candidate_mapped_matches);
 }
