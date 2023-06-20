3ad40f4f3e2ac66feddb16e68445d3796e85b305
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:08:52 -0400

    #62 function_unasyncify search_generic_async

--- src/search/generic.mjs
+++ src/search/generic.mjs
@@ -1,10 +1,10 @@
-import { search_candidate_as_string_matches_default } from './candidate/as/string/matches/default.mjs';
-import { search_query_map_default } from './query/map/default.mjs';
-import { function_is } from '../function/is.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
+import { metadata_generated } from '../metadata/generated.mjs';
+import { metadata } from '../metadata.mjs';
 import { list_filter } from '../list/filter.mjs';
-import { list_is } from '../list/is.mjs';
 import { string_is } from '../string/is.mjs';
+import { function_is } from '../function/is.mjs';
+import { list_is } from '../list/is.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 export function search_generic(candidates, candidate_mapper, candidate_mapped_matches, query, query_map) {
     arguments_assert(arguments, [
         list_is,
@@ -14,9 +14,10 @@ export function search_generic(candidates, candidate_mapper, candidate_mapped_ma
         function_is
     ]);
     let query_mapped = query_map(query);
-    let candidates_matching = list_filter(candidates, function list_filter_predicate(candidate) {
+    let candidates_matching = list_filter(candidates, async function list_filter_predicate(candidate) {
         let candidate_mapped = candidate_mapper(candidate);
         return candidate_mapped_matches(candidate_mapped, query_mapped);
     });
     return candidates_matching;
+    metadata([metadata_generated()]);
 }
