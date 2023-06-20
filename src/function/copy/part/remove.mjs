5648734b4a2bb091a20472ce4ba41b02b62ea3a5
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:00:24 -0400

    #270 function_input_remove_last function_copy_part_remove

--- src/function/copy/part/remove.mjs
+++ src/function/copy/part/remove.mjs
@@ -1,13 +1,11 @@
 import { function_copy } from '../../copy.mjs';
 import { string_identifier_combine } from '../../../string/identifier/combine.mjs';
-import { string_identifier_sub_is } from '../../../string/identifier/sub/is.mjs';
 import { string_identifier_is } from '../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_copy_part_remove(function_name_prefix, function_name_suffix_from, function_name_suffix_to) {
+export async function function_copy_part_remove(function_name_prefix, function_name_suffix_from) {
     arguments_assert(arguments, [
         string_identifier_is,
-        string_identifier_is,
-        string_identifier_sub_is
+        string_identifier_is
     ]);
     let function_name_to_copy = string_identifier_combine(function_name_prefix, function_name_suffix_from);
     let function_name_of_copy = string_identifier_combine(function_name_prefix, function_name_suffix_to);
