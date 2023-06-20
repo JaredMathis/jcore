e2e87a8d72b84d1457d30c2ae75e036dc955f1b3
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:46:33 -0400

    #62 noop

--- src/function/search/generic.mjs
+++ src/function/search/generic.mjs
@@ -12,7 +12,6 @@ export async function function_search_generic(query, query_map, candidate_mapped
         arguments_assert_todo,
         function_is
     ]);
-    const candidates = await function_name_all_tests_not();
-    let candidates_matching = await search_generic_async(candidates, candidate_mapper, candidate_mapped_matches, query, query_map);
+    let candidates_matching = await search_generic_async(function_name_all_tests_not, candidate_mapper, candidate_mapped_matches, query, query_map);
     return list_string_to_dictionary(candidates_matching, function_name_to_file_path);
 }
