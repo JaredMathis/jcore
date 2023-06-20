7afe98f8babe41c2d71a4542ba4fd0bee436d0f0
JaredMathis <jared.mathis@native.consulting>
2023-06-20 08:55:01 -0400

    #64 function_auto function_dependencies_names_arguments_assert

--- src/function/dependencies/names/arguments/assert.mjs
+++ src/function/dependencies/names/arguments/assert.mjs
@@ -1,8 +1,9 @@
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { function_name_get } from '../../../name/get.mjs';
 import { function_dependencies_names } from '../../names.mjs';
-export function function_dependencies_names_arguments_assert() {
+export async function function_dependencies_names_arguments_assert() {
+    arguments_assert(arguments, []);
     let v = function_name_get(arguments_assert);
-    let v_2 = function_dependencies_names(v);
+    let v_2 = await function_dependencies_names(v);
     return v_2;
 }
