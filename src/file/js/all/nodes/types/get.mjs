31918d2878110be3acdc3a12e2a9cb29d48baf4c
JaredMathis <jared.mathis@native.consulting>
2023-06-20 10:58:48 -0400

    #270 function_auto file_js_all_nodes_types_get

--- src/file/js/all/nodes/types/get.mjs
+++ src/file/js/all/nodes/types/get.mjs
@@ -1,22 +1,10 @@
-import { list_sort_string } from '../../../../../list/sort/string.mjs';
+import { file_js_all_nodes_types_get_generic } from './get/generic.mjs';
 import { metadata } from '../../../../../metadata.mjs';
-import { js_visit_nodes_types_get } from '../../../../../js/visit/nodes/types/get.mjs';
 import { file_js_all_map_args_if_function } from '../../map/args/if/function.mjs';
-import { list_adder_unique_async } from '../../../../../list/adder/unique/async.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export async function file_js_all_nodes_types_get() {
     arguments_assert(arguments, []);
     let lambda = file_js_all_map_args_if_function;
-    let result = await list_adder_unique_async(async function v(la) {
-        await lambda(async function logic(args) {
-            let {parsed} = args;
-            let types = js_visit_nodes_types_get(parsed);
-            for (let t of types) {
-                la(t);
-            }
-        });
-    });
-    list_sort_string(result);
-    return result;
+    return await file_js_all_nodes_types_get_generic(lambda);
     metadata([]);
 }
---
+++ src/file/js/all/nodes/types/get/generic.mjs
@@ -0,0 +1,16 @@
+import { list_sort_string } from '../../../../../../list/sort/string.mjs';
+import { js_visit_nodes_types_get } from '../../../../../../js/visit/nodes/types/get.mjs';
+import { list_adder_unique_async } from '../../../../../../list/adder/unique/async.mjs';
+export async function file_js_all_nodes_types_get_generic(lambda) {
+    let result = await list_adder_unique_async(async function v(la) {
+        await lambda(async function logic(args) {
+            let {parsed} = args;
+            let types = js_visit_nodes_types_get(parsed);
+            for (let t of types) {
+                la(t);
+            }
+        });
+    });
+    list_sort_string(result);
+    return result;
+}
