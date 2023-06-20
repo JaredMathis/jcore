e09b61facbadaa064ee851a29d744f180273fac9
JaredMathis <jared.mathis@native.consulting>
2023-06-20 01:12:42 -0400

    #62 function_auto rules

--- src/rules.mjs
+++ src/rules.mjs
@@ -2,10 +2,10 @@ import { function_suffix_async } from './function/suffix/async.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 import { function_search_part_last_async_not_is } from './function/search/part/last/async/not/is.mjs';
 import { object_keys } from './object/keys.mjs';
-export function rules() {
+export async function rules() {
     arguments_assert(arguments, []);
     const suffix = function_suffix_async();
-    let needs_renaming = function_search_part_last_async_not_is(suffix);
+    let needs_renaming = await function_search_part_last_async_not_is(suffix);
     for (let function_name of object_keys(needs_renaming)) {
         let replacement_suffix = 'asynk';
     }
