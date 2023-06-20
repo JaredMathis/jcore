2c82d858b601ffef1e3f1db8bb508b1bf5e8b7ab
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:16:35 -0400

    #272 noop

--- src/function/name/to/node/type/first.mjs
+++ src/function/name/to/node/type/first.mjs
@@ -6,5 +6,12 @@ export async function function_name_to_node_type_first(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let file_path = function_name_to_file_path(function_name);
     let result = await file_js_nodes_types_get(file_path);
+    async function mapper(args) {
+        let {parsed} = args;
+        let types = js_visit_nodes_types_get(parsed);
+        for (let t of types) {
+            la(t);
+        }
+    }
     return result;
 }
