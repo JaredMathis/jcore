e6f06973fe2969b4f98ecd381d180379899cadf9
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:25:47 -0400

    #272 noop

--- src/function/name/to/node/type/first.mjs
+++ src/function/name/to/node/type/first.mjs
@@ -5,6 +5,8 @@ import { function_parse } from '../../../../parse.mjs';
 import { js_node_is_type } from '../../../../../js/node/is/type.mjs';
 import { list_adder } from '../../../../../list/adder.mjs';
 import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
+import { list_filter } from '../../../../../list/filter.mjs';
+import { list_first } from '../../../../../list/first.mjs';
 export async function function_name_to_node_type_first(function_name, node_type_name, node_type_name) {
     arguments_assert(arguments, [
         string_identifier_is,
@@ -12,7 +14,8 @@ export async function function_name_to_node_type_first(function_name, node_type_
         arguments_assert_todo
     ]);
     let parsed = function_parse(function_name);
-    return list_adder(la => {
+    let filtered = list_adder(la => {
         js_nodes_each(parsed, n => js_node_is_type(n, node_type_name), n => la(n));
     });
+    return list_first(filtered);
 }
