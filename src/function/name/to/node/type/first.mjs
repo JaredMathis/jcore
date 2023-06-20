37c800f216618309f1b06744de85c8c725449f7e
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:25:23 -0400

    #272 function_input_add function_name_to_node_type_first node_type_name

--- src/function/name/to/node/type/first.mjs
+++ src/function/name/to/node/type/first.mjs
@@ -5,14 +5,14 @@ import { function_parse } from '../../../../parse.mjs';
 import { js_node_is_type } from '../../../../../js/node/is/type.mjs';
 import { list_adder } from '../../../../../list/adder.mjs';
 import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
-export async function function_name_to_node_type_first(function_name, node_type_name) {
+export async function function_name_to_node_type_first(function_name, node_type_name, node_type_name) {
     arguments_assert(arguments, [
         string_identifier_is,
+        arguments_assert_todo,
         arguments_assert_todo
     ]);
     let parsed = function_parse(function_name);
-    list_adder(la => {
+    return list_adder(la => {
         js_nodes_each(parsed, n => js_node_is_type(n, node_type_name), n => la(n));
     });
-    return result;
 }
