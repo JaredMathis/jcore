8c013620a67e749310ca6499e1cdcc7e1058845a
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:05:16 -0400

    #62 function_auto function_search_part_last_async_is

--- src/function/search/part/last/async/is.mjs
+++ src/function/search/part/last/async/is.mjs
@@ -1,3 +1,4 @@
+import { function_name_to_declaration } from '../../../../name/to/declaration.mjs';
 import { function_search_part_generic } from '../../generic.mjs';
 import { list_last } from '../../../../../list/last.mjs';
 import { equal } from '../../../../../equal.mjs';
@@ -6,7 +7,7 @@ import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export async function function_search_part_last_async_is(query) {
     arguments_assert(arguments, [string_is]);
     let candidate_mapped_matches = function v(candidate_mapped, query_mapped) {
-        let fd = function_name_to_declaration
+        let fd = function_name_to_declaration();
         return equal(list_last(candidate_mapped), query_mapped);
     };
     return function_search_part_generic(query, candidate_mapped_matches);
