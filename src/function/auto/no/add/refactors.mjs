587f7436c4f78339ebe75b5f42e62c5899b7ab81
JaredMathis <jared.mathis@native.consulting>
2023-06-20 08:16:44 -0400

    #263 function_auto function_auto_no_add_refactors

--- src/function/auto/no/add/refactors.mjs
+++ src/function/auto/no/add/refactors.mjs
@@ -1,4 +1,3 @@
-import { refactor_unasyncify_if_needed } from '../../../../refactor/unasyncify/if/needed.mjs';
 import { function_auto_no_add_refactors_first } from './refactors/first.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
@@ -7,10 +6,7 @@ import { list_multiple_combine } from '../../../../list/multiple/combine.mjs';
 export function function_auto_no_add_refactors() {
     arguments_assert(arguments, []);
     let first = function_auto_no_add_refactors_first();
-    let second = [
-        refactor_functions_to_files,
-        refactor_unasyncify_if_needed
-    ];
+    let second = [refactor_functions_to_files];
     let v = list_multiple_combine([
         first,
         second
