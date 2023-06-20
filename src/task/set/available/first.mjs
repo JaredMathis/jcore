5949f92c6b7ab81d672d6c117729bdfd5355e05a
JaredMathis <jared.mathis@native.consulting>
2023-06-20 10:04:06 -0400

    #266 function_auto task_set_available_first

--- src/task/set/available/first.mjs
+++ src/task/set/available/first.mjs
@@ -1,6 +1,10 @@
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { task_set } from '../../set.mjs';
 import { task_available_first } from '../../available/first.mjs';
+import { task_number_get_string } from '../../number/get/string.mjs';
 export async function task_set_available_first() {
-    let available = await task_available_first();
-    task_set(available);
+    arguments_assert(arguments, []);
+    let first = await task_available_first();
+    let first_number = task_number_get_string(first);
+    task_set(first_number);
 }
