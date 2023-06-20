22d046e366b0034759654cfcadcb3727eeea2833
JaredMathis <jared.mathis@native.consulting>
2023-06-20 01:02:15 -0400

    #62 function_auto task_search_summarized

--- src/task/search/summarized.mjs
+++ src/task/search/summarized.mjs
@@ -1,3 +1,4 @@
+import { task_all } from '../all.mjs';
 import { search_query_map_default } from '../../search/query/map/default.mjs';
 import { search_candidate_mapped_matches_default } from '../../search/candidate/mapped/matches/default.mjs';
 import { search_generic_async } from '../../search/generic/async.mjs';
@@ -6,7 +7,7 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { identity } from '../../identity.mjs';
 export async function task_search_summarized(query) {
     arguments_assert(arguments, [string_is]);
-    let candidates_get = task_all_summarized;
+    let candidates_get = task_all;
     let candidate_mapper = identity;
     return search_generic_async(candidates_get, candidate_mapper, search_candidate_mapped_matches_default, query, search_query_map_default);
 }
