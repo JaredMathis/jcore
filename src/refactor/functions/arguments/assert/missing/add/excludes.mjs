331c4db2bda8193bc9776756fe6353ede27bc9fa
JaredMathis <jared.mathis@native.consulting>
2023-06-20 09:00:43 -0400

    #64 function_auto refactor_functions_arguments_assert_missing_add_excludes

--- src/refactor/functions/arguments/assert/missing/add/excludes.mjs
+++ src/refactor/functions/arguments/assert/missing/add/excludes.mjs
@@ -1,3 +1,4 @@
+import { function_names_each_filter } from '../../../../../../function/names/each/filter.mjs';
 import { refactor_functions_arguments_assert_missing_add_excludes_additional } from './excludes/additional.mjs';
 import { function_dependencies_names_arguments_assert } from '../../../../../../function/dependencies/names/arguments/assert.mjs';
 import { list_add_multiple } from '../../../../../../list/add/multiple.mjs';
@@ -7,6 +8,7 @@ export async function refactor_functions_arguments_assert_missing_add_excludes()
     arguments_assert(arguments, []);
     let excludes = await function_dependencies_names_arguments_assert();
     comment(`Eventually this should be refactored maybe to read files and detect an attribute`);
+    function_names_each_filter();
     const excludes_additional = refactor_functions_arguments_assert_missing_add_excludes_additional();
     list_add_multiple(excludes, excludes_additional);
     return excludes;
