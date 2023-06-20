5e3b7b72bd93427acd0aa4a7340a3e8163eefc19
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:19:24 -0400

    #272 function_auto function_name_to_node_type_first

--- src/function/name/to/node/type/first.mjs
+++ src/function/name/to/node/type/first.mjs
@@ -6,7 +6,7 @@ import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export async function function_name_to_node_type_first(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let file_path = function_name_to_file_path(function_name);
-    let result = await file_js_map_args(file_path);
+    let result = await file_js_map_args(file_path, mapper);
     async function mapper(args) {
         let {parsed} = args;
         let types = js_visit_nodes_types_get(parsed);
