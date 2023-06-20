a22092c1838e18585603ad8ffb6aa680b1dcbe68
JaredMathis <jared.mathis@native.consulting>
2023-06-20 01:19:37 -0400

    #62 function_auto rule_function_async_and_suffix_async_implies_suffix_asynk

--- src/rule/function/async/and/suffix/async/implies/suffix/asynk.mjs
+++ src/rule/function/async/and/suffix/async/implies/suffix/asynk.mjs
@@ -1,3 +1,5 @@
+import { list_take_without_last } from '../../../../../../../../list/take/without/last.mjs';
+import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
 import { function_rename_suffix_change } from '../../../../../../../../function/rename/suffix/change.mjs';
 import { object_keys } from '../../../../../../../../object/keys.mjs';
 import { function_search_part_last_async_not_is } from '../../../../../../../../function/search/part/last/async/not/is.mjs';
@@ -5,6 +7,7 @@ import { function_suffix_async } from '../../../../../../../../function/suffix/a
 import { string_identifier_parts_from } from '../../../../../../../../string/identifier/parts/from.mjs';
 import { string_identifier_parts_to } from '../../../../../../../../string/identifier/parts/to.mjs';
 export async function rule_function_async_and_suffix_async_implies_suffix_asynk() {
+    arguments_assert(arguments, []);
     const suffix = function_suffix_async();
     let needs_renaming = await function_search_part_last_async_not_is(suffix);
     for (let function_name of object_keys(needs_renaming)) {
