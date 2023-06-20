18d61026c89dc39cebf498e0f3a78eb46c77fa32
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:09:31 -0400

    #62 function_auto function_search_generic

--- src/function/search/generic.mjs
+++ src/function/search/generic.mjs
@@ -1,10 +1,9 @@
+import { search_generic_async } from '../../search/generic/async.mjs';
 import { function_is } from '../is.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { function_name_to_file_path } from '../name/to/file/path.mjs';
 import { list_string_to_dictionary } from '../../list/string/to/dictionary.mjs';
-import { identity } from '../../identity.mjs';
-import { search_generic } from '../../search/generic.mjs';
 import { function_name_all_tests_not } from '../name/all/tests/not.mjs';
 export async function function_search_generic(query, query_map, candidate_mapped_matches, candidate_mapper) {
     arguments_assert(arguments, [
@@ -14,6 +13,6 @@ export async function function_search_generic(query, query_map, candidate_mapped
         function_is
     ]);
     const candidates = await function_name_all_tests_not();
-    let candidates_matching = search_generic(candidates, candidate_mapper, candidate_mapped_matches, query, query_map);
+    let candidates_matching = await search_generic_async(candidates, candidate_mapper, candidate_mapped_matches, query, query_map);
     return list_string_to_dictionary(candidates_matching, function_name_to_file_path);
 }
