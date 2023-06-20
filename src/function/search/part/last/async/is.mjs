7eb96b647b5bd4642821aa000db7aa19c28bfd21
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:11:26 -0400

    #62 function_auto function_search_part_last_async_is

--- src/function/search/part/last/async/is.mjs
+++ src/function/search/part/last/async/is.mjs
@@ -1,3 +1,4 @@
+import { js_node_property_async_get } from '../../../../../js/node/property/async/get.mjs';
 import { function_name_to_declaration } from '../../../../name/to/declaration.mjs';
 import { function_search_part_generic } from '../../generic.mjs';
 import { list_last } from '../../../../../list/last.mjs';
@@ -8,6 +9,7 @@ export async function function_search_part_last_async_is(query) {
     arguments_assert(arguments, [string_is]);
     let candidate_mapped_matches = async function v(candidate_mapped, query_mapped) {
         let fd = await function_name_to_declaration(candidate_mapped);
+        js_node_property_async_get;
         return equal(list_last(candidate_mapped), query_mapped);
     };
     return await function_search_part_generic(query, candidate_mapped_matches);
