e41e4d16d60c9920235cb7a5332831c9a4660260
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:08:14 -0400

    #270 function_auto file_js_nodes_types_get

--- src/file/js/nodes/types/get.mjs
+++ src/file/js/nodes/types/get.mjs
@@ -1,11 +1,11 @@
+import { file_js_map_args } from '../../map/args.mjs';
 import { path_is } from '../../../../path/is.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { file_js_all_nodes_types_get_generic } from '../../all/nodes/types/get/generic.mjs';
-import { file_js_all_map_args_if_function } from '../../all/map/args/if/function.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function file_js_nodes_types_get(file_path) {
     arguments_assert(arguments, [path_is]);
-    let lambda = file_js_all_map_args_if_function;
+    let lambda = args => file_js_map_args();
     return await file_js_all_nodes_types_get_generic(lambda);
     metadata([]);
 }
