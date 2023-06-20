41ed80f75c31bf6dc5c93603d7da248b524f8217
JaredMathis <jared.mathis@native.consulting>
2023-06-20 08:10:39 -0400

    #263 noop

--- src/search/generic/async.mjs
+++ src/search/generic/async.mjs
@@ -1,10 +1,8 @@
-import { metadata_generated } from '../../metadata/generated.mjs';
-import { metadata } from '../../metadata.mjs';
-import { list_filter } from '../../list/filter.mjs';
+import { list_filter_async } from '../../list/filter/async.mjs';
 import { string_is } from '../../string/is.mjs';
 import { function_is } from '../../function/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function search_generic(candidates_get, candidate_mapper, candidate_mapped_matches, query, query_map) {
+export async function search_generic_async(candidates_get, candidate_mapper, candidate_mapped_matches, query, query_map) {
     arguments_assert(arguments, [
         function_is,
         function_is,
@@ -12,12 +10,11 @@ export function search_generic(candidates_get, candidate_mapper, candidate_mappe
         string_is,
         function_is
     ]);
-    let candidates = candidates_get();
-    let query_mapped = query_map(query);
-    let candidates_matching = list_filter(candidates, async function list_filter_predicate(candidate) {
-        let candidate_mapped = candidate_mapper(candidate);
-        return candidate_mapped_matches(candidate_mapped, query_mapped);
+    let candidates = await candidates_get();
+    let query_mapped = await query_map(query);
+    let candidates_matching = await list_filter_async(candidates, async function list_filter_predicate(candidate) {
+        let candidate_mapped = await candidate_mapper(candidate);
+        return await candidate_mapped_matches(candidate_mapped, query_mapped);
     });
     return candidates_matching;
-    metadata([metadata_generated()]);
 }
