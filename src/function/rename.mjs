0772afd4b23785a09858b22b91d532e79202e1a0
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:33:35 -0400

    #62 function_auto function_rename

--- src/function/rename.mjs
+++ src/function/rename.mjs
@@ -1,21 +1,13 @@
+import { function_rename_no_open } from './rename/no/open.mjs';
 import { function_open_vs_code } from './open/vs/code.mjs';
-import { function_rename_if_starts_with } from './rename/if/starts/with.mjs';
-import { function_name_all_tests_not_starts_with } from './name/all/tests/not/starts/with.mjs';
 import { string_identifier_is } from '../string/identifier/is.mjs';
-import { function_rename_single } from './rename/single.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-import { list_length_is_1 } from '../list/length/is/1.mjs';
-import { assert_message } from '../assert/message.mjs';
-import { function_name_get } from './name/get.mjs';
 export async function function_rename(function_name_old, function_name_new) {
     arguments_assert(arguments, [
         string_identifier_is,
         string_identifier_is
     ]);
-    let starting_withs = await function_name_all_tests_not_starts_with(function_name_old);
-    let v = list_length_is_1(starting_withs);
-    assert_message(v, `Multiple functions exist. Use ${ function_name_get(function_rename_single) } if you're sure or consider ${ function_name_get(function_rename_if_starts_with) }: ` + starting_withs);
-    let result = await function_rename_single(function_name_old, function_name_new);
+    let result = await function_rename_no_open(function_name_old, function_name_new);
     await function_open_vs_code(function_name_new);
     return result;
 }
---
+++ src/function/rename/no/open.mjs
@@ -0,0 +1,13 @@
+import { function_rename_if_starts_with } from '../if/starts/with.mjs';
+import { function_rename_single } from '../single.mjs';
+import { function_name_get } from '../../name/get.mjs';
+import { assert_message } from '../../../assert/message.mjs';
+import { list_length_is_1 } from '../../../list/length/is/1.mjs';
+import { function_name_all_tests_not_starts_with } from '../../name/all/tests/not/starts/with.mjs';
+export async function function_rename_no_open(function_name_old, function_name_new) {
+    let starting_withs = await function_name_all_tests_not_starts_with(function_name_old);
+    let v = list_length_is_1(starting_withs);
+    assert_message(v, `Multiple functions exist. Use ${ function_name_get(function_rename_single) } if you're sure or consider ${ function_name_get(function_rename_if_starts_with) }: ` + starting_withs);
+    let result = await function_rename_single(function_name_old, function_name_new);
+    return result;
+}
