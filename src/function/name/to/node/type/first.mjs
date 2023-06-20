4d71f4acb9badb9a6b8ca991212bccc6fc143e6c
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:18:05 -0400

    #272 function_auto function_name_to_node_type_first

--- src/function/name/to/node/type/first.mjs
+++ src/function/name/to/node/type/first.mjs
@@ -1,11 +1,12 @@
-import { file_js_nodes_types_get } from '../../../../../file/js/nodes/types/get.mjs';
+import { js_visit_nodes_types_get } from '../../../../../js/visit/nodes/types/get.mjs';
+import { file_js_map_args } from '../../../../../file/js/map/args.mjs';
 import { function_name_to_file_path } from '../../file/path.mjs';
 import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export async function function_name_to_node_type_first(function_name) {
     arguments_assert(arguments, [string_identifier_is]);
     let file_path = function_name_to_file_path(function_name);
-    let result = await file_js_nodes_types_get(file_path);
+    let result = await file_js_map_args(file_path);
     async function mapper(args) {
         let {parsed} = args;
         let types = js_visit_nodes_types_get(parsed);
