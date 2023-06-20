fcd81d92705d08d68a1e811c0cee42927fbbb822
JaredMathis <jared.mathis@native.consulting>
2023-06-20 01:10:06 -0400

    #62 function_auto rules

--- src/rules.mjs
+++ src/rules.mjs
@@ -3,5 +3,5 @@ import { arguments_assert } from './arguments/assert.mjs';
 import { function_search_part_last_async_not_is } from './function/search/part/last/async/not/is.mjs';
 export function rules() {
     arguments_assert(arguments, []);
-    function_search_part_last_async_not_is(function_suffix_async());
+    let needs_renaming = function_search_part_last_async_not_is(function_suffix_async());
 }
