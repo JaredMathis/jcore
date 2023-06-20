bc11a01d360052c73d6d043573af3c394b1d2d77
JaredMathis <jared.mathis@native.consulting>
2023-06-20 08:07:30 -0400

    #263 function_auto refactor_functions_arguments_assert_add

---
+++ src/refactor/functions/arguments/assert/add.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../../error.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function refactor_functions_arguments_assert_add() {
+    arguments_assert(arguments, []);
+    error('todo: refactor_functions_arguments_assert_add');
+}
