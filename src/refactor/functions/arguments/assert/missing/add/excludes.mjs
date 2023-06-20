29abf2f9a45dea471d4e161b3cb60bc310e01634
JaredMathis <jared.mathis@native.consulting>
2023-06-20 09:14:35 -0400

    #64 function_auto refactor_functions_arguments_assert_missing_add_excludes

--- src/refactor/functions/arguments/assert/missing/add/excludes.mjs
+++ src/refactor/functions/arguments/assert/missing/add/excludes.mjs
@@ -18,15 +18,16 @@ export async function refactor_functions_arguments_assert_missing_add_excludes()
     arguments_assert(arguments, []);
     let excludes = await function_dependencies_names_arguments_assert();
     comment(`Eventually this should be refactored maybe to read files and detect an attribute`);
-    await list_adder_async;
-    await function_names_each(async args => {
-        let metadata_args = await js_mapper_args_to_metadata_args(args);
-        let filtered = list_filter(metadata_args, js_node_is_call_expression);
-        let filtered2 = list_filter(filtered, node => js_node_call_expression_name_equal(node, metadata_arguments_assert_none()));
-        if (list_empty(filtered2)) {
-            return;
-        }
-        assert(list_length_is_1(filtered2));
+    await list_adder_async(async la => {
+        await function_names_each(async args => {
+            let metadata_args = await js_mapper_args_to_metadata_args(args);
+            let filtered = list_filter(metadata_args, js_node_is_call_expression);
+            let filtered2 = list_filter(filtered, node => js_node_call_expression_name_equal(node, metadata_arguments_assert_none()));
+            if (list_empty(filtered2)) {
+                return;
+            }
+            assert(list_length_is_1(filtered2));
+        });
     });
     error();
     const excludes_additional = refactor_functions_arguments_assert_missing_add_excludes_additional();
