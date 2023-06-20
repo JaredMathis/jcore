4bc3c599ff98a7517cf34e35bace6e35f06b1809
JaredMathis <jared.mathis@native.consulting>
2023-06-20 01:19:22 -0400

    #62 noop

--- src/rule/function/async/and/suffix/async/implies/suffix/asynk.mjs
+++ src/rule/function/async/and/suffix/async/implies/suffix/asynk.mjs
@@ -2,11 +2,16 @@ import { function_rename_suffix_change } from '../../../../../../../../function/
 import { object_keys } from '../../../../../../../../object/keys.mjs';
 import { function_search_part_last_async_not_is } from '../../../../../../../../function/search/part/last/async/not/is.mjs';
 import { function_suffix_async } from '../../../../../../../../function/suffix/async.mjs';
+import { string_identifier_parts_from } from '../../../../../../../../string/identifier/parts/from.mjs';
+import { string_identifier_parts_to } from '../../../../../../../../string/identifier/parts/to.mjs';
 export async function rule_function_async_and_suffix_async_implies_suffix_asynk() {
     const suffix = function_suffix_async();
     let needs_renaming = await function_search_part_last_async_not_is(suffix);
     for (let function_name of object_keys(needs_renaming)) {
+        let parts = string_identifier_parts_to(function_name);
+        let without_last = list_take_without_last(parts);
+        let function_name_no_suffix = string_identifier_parts_from(without_last);
         let replacement_suffix = 'asynk';
-        await function_rename_suffix_change(function_name, suffix, replacement_suffix);
+        await function_rename_suffix_change(function_name_no_suffix, suffix, replacement_suffix);
     }
 }
