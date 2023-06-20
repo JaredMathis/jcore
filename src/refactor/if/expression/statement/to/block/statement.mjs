0de98dab34e42fe304aceffe50b700b0942a1256
JaredMathis <jared.mathis@native.consulting>
2023-06-20 11:39:55 -0400

    #272 function_add_mapper_all if_expression_statement_to_block_statement

---
+++ src/refactor/if/expression/statement/to/block/statement.mjs
@@ -0,0 +1,7 @@
+import { arguments_assert_todo } from '../../../../../../arguments/assert/todo.mjs';
+import { error } from '../../../../../../error.mjs';
+import { arguments_assert } from '../../../../../../arguments/assert.mjs';
+export function refactor_if_expression_statement_to_block_statement(args) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    error('todo: refactor_if_expression_statement_to_block_statement');
+}
