c946b537d744a8da9fe832c86660694a5703024b
JaredMathis <jared.mathis@native.consulting>
2023-06-20 09:28:51 -0400

    #64 function_auto function_names_metadata_arguments_assert_none

--- src/function/names/metadata/arguments/assert/none.mjs
+++ src/function/names/metadata/arguments/assert/none.mjs
@@ -8,7 +8,6 @@ import { function_name_get } from '../../../../name/get.mjs';
 import { js_node_call_expression_name_equal } from '../../../../../js/node/call/expression/name/equal.mjs';
 import { js_node_is_call_expression } from '../../../../../js/node/is/call/expression.mjs';
 import { list_filter } from '../../../../../list/filter.mjs';
-import { js_mapper_args_to_metadata_args } from '../../../../../js/mapper/args/to/metadata/args.mjs';
 import { string_identifier_is } from '../../../../../string/identifier/is.mjs';
 import { assert } from '../../../../../assert.mjs';
 import { function_names_each } from '../../../each.mjs';
@@ -20,10 +19,7 @@ export async function function_names_metadata_arguments_assert_none() {
             let {function_name} = args;
             console.log({ function_name });
             assert(string_identifier_is(function_name));
-            if (false)
-                js_mapper_args_to_metadata_args_no_add_missing;
-            return;
-            let metadata_args = await js_mapper_args_to_metadata_args(args);
+            let metadata_args = await js_mapper_args_to_metadata_args_no_add_missing(args);
             let filtered = list_filter(metadata_args, js_node_is_call_expression);
             let filtered2 = list_filter(filtered, node => js_node_call_expression_name_equal(node, function_name_get(metadata_arguments_assert_none)));
             if (list_empty(filtered2)) {
