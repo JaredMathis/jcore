4ba595a3b27527604230c962a14a6bf80b9b47cc
JaredMathis <jared.mathis@native.consulting>
2023-06-19 23:54:26 -0400

    #254 function_copy_suffix_add function_search_part not_last

---
+++ src/function/search/part/not/last.mjs
@@ -0,0 +1,16 @@
+import { function_search_part_generic } from '../generic.mjs';
+import { list_contains } from '../../../../list/contains.mjs';
+import { list_last } from '../../../../list/last.mjs';
+import { equal } from '../../../../equal.mjs';
+import { string_is } from '../../../../string/is.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export async function function_search_part_not_last(query) {
+    arguments_assert(arguments, [string_is]);
+    let candidate_mapped_matches = function v(candidate_mapped, query_mapped) {
+        if (equal(list_last(candidate_mapped), query_mapped)) {
+            return false;
+        }
+        return list_contains(candidate_mapped, query_mapped);
+    };
+    return function_search_part_generic(query, candidate_mapped_matches);
+}
