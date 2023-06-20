1d35030133b868d78849326afa641ad800df6c53
JaredMathis <jared.mathis@native.consulting>
2023-06-20 09:04:29 -0400

    #64 function_auto refactor_functions_arguments_assert_missing_add_excludes

--- src/refactor/functions/arguments/assert/missing/add/excludes.mjs
+++ src/refactor/functions/arguments/assert/missing/add/excludes.mjs
@@ -1,22 +1,24 @@
+import { function_names_each } from '../../../../../../function/names/each.mjs';
 import { log } from '../../../../../../log.mjs';
 import { js_mapper_args_to_metadata_args } from '../../../../../../js/mapper/args/to/metadata/args.mjs';
-import { function_names_each_filter } from '../../../../../../function/names/each/filter.mjs';
 import { refactor_functions_arguments_assert_missing_add_excludes_additional } from './excludes/additional.mjs';
 import { function_dependencies_names_arguments_assert } from '../../../../../../function/dependencies/names/arguments/assert.mjs';
 import { list_add_multiple } from '../../../../../../list/add/multiple.mjs';
 import { comment } from '../../../../../../comment.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
 import { list_empty_not } from '../../../../../../list/empty/not.mjs';
+import { error } from '../../../../../../error.mjs';
 export async function refactor_functions_arguments_assert_missing_add_excludes() {
     arguments_assert(arguments, []);
     let excludes = await function_dependencies_names_arguments_assert();
     comment(`Eventually this should be refactored maybe to read files and detect an attribute`);
-    await function_names_each_filter(args => {
+    await function_names_each(args => {
         let metadata_args = js_mapper_args_to_metadata_args(args);
         if (list_empty_not(metadata_args)) {
             console.log(metadata_args);
         }
     });
+    error();
     const excludes_additional = refactor_functions_arguments_assert_missing_add_excludes_additional();
     list_add_multiple(excludes, excludes_additional);
     return excludes;
