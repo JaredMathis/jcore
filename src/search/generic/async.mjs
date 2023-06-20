06130e070cebcb1fc60e08ab5e9f510be683fe18
JaredMathis <jared.mathis@native.consulting>
2023-06-20 00:08:26 -0400

    #62 noop

--- src/search/generic/async.mjs
+++ src/search/generic/async.mjs
@@ -11,7 +11,7 @@ export async function search_generic_async(candidates, candidate_mapper, candida
         string_is,
         function_is
     ]);
-    let query_mapped = query_map(query);
+    let query_mapped = await query_map(query);
     let candidates_matching = await list_filter_async(candidates, async function list_filter_predicate(candidate) {
         let candidate_mapped = await candidate_mapper(candidate);
         return await candidate_mapped_matches(candidate_mapped, query_mapped);
