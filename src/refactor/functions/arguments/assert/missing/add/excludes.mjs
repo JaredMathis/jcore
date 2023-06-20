47e1d074cce92e6ef3a435595e36195781ecf832
JaredMathis <jared.mathis@native.consulting>
2023-06-20 08:37:42 -0400

    #64 task_later maybe rename additional to extra globally identifier

--- src/refactor/functions/arguments/assert/missing/add/excludes.mjs
+++ src/refactor/functions/arguments/assert/missing/add/excludes.mjs
@@ -11,11 +11,12 @@ export async function refactor_functions_arguments_assert_missing_add_excludes()
     arguments_assert(arguments, []);
     let excludes = await function_dependencies_names_arguments_assert();
     comment(`Eventually this should be refactored maybe to read files and detect an attribute`);
-    list_add_multiple(excludes, [
+    const excludes_additional = [
         function_name_get(metadata_arguments_assert_extra_allow),
         function_name_get(arguments_assert_predicate_default()),
         function_name_get(git),
         function_name_get(git_no_commit)
-    ]);
+    ];
+    list_add_multiple(excludes, excludes_additional);
     return excludes;
 }
