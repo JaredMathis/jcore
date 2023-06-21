641b30569b758aedfdf6b8bbc25cdee12c294f4c
JaredMathis <jared.mathis@native.consulting>
2023-06-21 09:42:10 -0400

    #276 function_auto function_add_js_node_type_auto

---
+++ src/function/add/js/node/type/auto.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../../../error.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export function function_add_js_node_type_auto() {
+    arguments_assert(arguments, []);
+    error('todo: function_add_js_node_type_auto');
+}
