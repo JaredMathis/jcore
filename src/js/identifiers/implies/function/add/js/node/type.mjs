c80cfe94a8eace81618d62a16156acdc89cdb496
JaredMathis <jared.mathis@native.consulting>
2023-06-21 09:56:23 -0400

    #276 function_rename js_identifier_implies_function_add_js_node_type js_identifiers_implies_function_add_js_node_type

--- src/js/identifiers/implies/function/add/js/node/type.mjs
+++ src/js/identifiers/implies/function/add/js/node/type.mjs
@@ -1,11 +1,11 @@
 import { js_prefix_node_is } from '../../../../../../prefix/node/is.mjs';
 import { js_prefix_node_type } from '../../../../../../prefix/node/type.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
-import { js_identifier_is } from '../../../../../is.mjs';
+import { js_identifier_is } from '../../../../../../identifier/is.mjs';
 import { string_combine } from '../../../../../../../string/combine.mjs';
 import { function_name_separator } from '../../../../../../../function/name/separator.mjs';
 import { string_starts_with } from '../../../../../../../string/starts/with.mjs';
-export function js_identifier_implies_function_add_js_node_type(identifier_name) {
+export function js_identifiers_implies_function_add_js_node_type(identifier_name) {
     arguments_assert(arguments, [js_identifier_is]);
     let prefix_1 = js_prefix_node_is();
     let prefix_2 = js_prefix_node_type();
