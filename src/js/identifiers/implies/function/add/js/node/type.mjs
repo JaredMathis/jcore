c96dedbb899469d938e30e0249a53e3b0eb9b55e
JaredMathis <jared.mathis@native.consulting>
2023-06-21 10:07:37 -0400

    #276 function_auto js_identifiers_implies_function_add_js_node_type

--- src/js/identifiers/implies/function/add/js/node/type.mjs
+++ src/js/identifiers/implies/function/add/js/node/type.mjs
@@ -1,22 +1,26 @@
 import { js_prefix_node_is } from '../../../../../../prefix/node/is.mjs';
 import { js_prefix_node_type } from '../../../../../../prefix/node/type.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
-import { js_identifier_is } from '../../../../../../identifier/is.mjs';
 import { string_combine } from '../../../../../../../string/combine.mjs';
 import { function_name_separator } from '../../../../../../../function/name/separator.mjs';
 import { string_starts_with } from '../../../../../../../string/starts/with.mjs';
-export function js_identifiers_implies_function_add_js_node_type(identifier_name) {
-    arguments_assert(arguments, [js_identifier_is]);
+import { list_is } from '../../../../../../../list/is.mjs';
+import { list_adder } from '../../../../../../../list/adder.mjs';
+export function js_identifiers_implies_function_add_js_node_type(identifier_names) {
+    arguments_assert(arguments, [list_is]);
     let prefix_1 = js_prefix_node_is();
     let prefix_2 = js_prefix_node_type();
-    for (let p of [
-            prefix_1,
-            prefix_2
-        ]) {
-        let p_with_underscore_trailing = string_combine(p, function_name_separator());
-        if (string_starts_with(identifier_name, p_with_underscore_trailing)) {
-            return true;
+    return list_adder(la => {
+        for (let p of [
+                prefix_1,
+                prefix_2
+            ]) {
+            let p_with_underscore_trailing = string_combine(p, function_name_separator());
+            for (let identifier_name of identifier_names) {
+                if (string_starts_with(identifier_name, p_with_underscore_trailing)) {
+                    la(identifier_name);
+                }
+            }
         }
-    }
-    return false;
+    });
 }
