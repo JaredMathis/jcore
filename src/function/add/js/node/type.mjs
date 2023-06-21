d1d0742fd7f4462662d636ce9253ef2da07c2b79
JaredMathis <jared.mathis@native.consulting>
2023-06-21 08:28:59 -0400

    #274 noop

--- src/function/add/js/node/type.mjs
+++ src/function/add/js/node/type.mjs
@@ -10,7 +10,7 @@ export async function function_add_js_node_type(camel_case) {
     let snake = string_case_camel_to_snake(camel_case);
     let prefix_1 = 'js_node_is_';
     let expression = js_code_expression_string(string_value);
-    let after_let = js_code_call_expression_with_args(v, args);
+    let after_let = js_code_call_expression_with_args(function_name_get(object_property_get), args);
     let code_return_statment = js_code_return_statement(expression);
     let js_node_is_function_name = `${ prefix_1 }${ snake }`;
     let v_2 = await function_add_with_statement_code(js_node_is_function_name, code_return_statment);
