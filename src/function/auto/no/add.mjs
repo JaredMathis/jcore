c3e9157edb4aabcc1f6b5f211f5ccf9c8b8c3e1a
JaredMathis <jared.mathis@native.consulting>
2023-06-21 10:19:26 -0400

    #276 function_auto function_auto_no_add

--- src/function/auto/no/add.mjs
+++ src/function/auto/no/add.mjs
@@ -1,3 +1,4 @@
+import { function_add_js_node_type_auto } from '../../add/js/node/type/auto.mjs';
 import { function_map_multiple } from '../../map/multiple.mjs';
 import { js_identifier_is } from '../../../js/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -12,6 +13,8 @@ export async function function_auto_no_add(function_name) {
     let refactors_names = list_map(refactors, function_name_get);
     await function_map_multiple(refactors_names, function_name);
     const only_if_needed = true;
+    if (false)
+        await function_add_js_node_type_auto(function_name);
     await function_unasyncify_generic(function_name, only_if_needed);
     metadata([]);
 }
