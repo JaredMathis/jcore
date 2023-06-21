5c35078e91b1d5207f133f5bc9a594ccee777a80
JaredMathis <jared.mathis@native.consulting>
2023-06-21 09:27:36 -0400

    #276 noop

--- src/js/identifier/implies/function/add/js/node/type.mjs
+++ src/js/identifier/implies/function/add/js/node/type.mjs
@@ -3,11 +3,12 @@ import { js_prefix_node_type } from '../../../../../../prefix/node/type.mjs';
 import { js_identifier_combine } from '../../../../../combine.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 import { js_identifier_is } from '../../../../../is.mjs';
+import { string_add } from '../../../../../../../string/add.mjs';
 export function js_identifier_implies_function_add_js_node_type(identifier_name) {
     arguments_assert(arguments, [js_identifier_is]);
     let prefix_1 = js_prefix_node_is();
     let prefix_2 = js_prefix_node_type();
     for (let p of [prefix_1, prefix_2]) {
-
+        let p_with_underscore_trailing = string_add()
     }
 }
