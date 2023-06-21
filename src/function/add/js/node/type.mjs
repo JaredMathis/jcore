64956326985b0e1231e1169634d8c39bed4efc5a
JaredMathis <jared.mathis@native.consulting>
2023-06-20 20:49:28 -0400

    #274 noop

--- src/function/add/js/node/type.mjs
+++ src/function/add/js/node/type.mjs
@@ -1,12 +1,17 @@
+import { function_add_with_statement_code } from '../../with/statement/code.mjs';
+import { js_code_return_statement } from '../../../../js/code/return/statement.mjs';
+import { js_code_expression_string } from '../../../../js/code/expression/string.mjs';
 import { function_add_string_prefix_camel_generic } from '../../string/prefix/camel/generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function function_add_js_node_type(camel_case) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let prefix_1 = 'js_node_is_';
     let expression = js_code_expression_string(string_value);
     let code_return_statment = js_code_return_statement(expression);
-    let v_2 = await function_add_with_statement_code(function_name, v);
-    let prefix = 'js_node_type';
-    let v = await function_add_string_prefix_camel_generic(prefix, camel_case);
+    let js_node_is_function_name = `js_node_is_`;
+    let v_2 = await function_add_with_statement_code(js_node_is_function_name, v);
+    let prefix_2 = 'js_node_type';
+    let v = await function_add_string_prefix_camel_generic(prefix_2, camel_case);
     return v;
 }
--- src/js/code/call/expression/arguments/2.mjs
+++ src/js/code/call/expression/arguments/2.mjs
@@ -1,7 +1,6 @@
 import { js_code_call_expression_with_args } from '../with/args.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-import { js_code_join_comma } from '../../../join/comma.mjs';
 import { list_add_multiple } from '../../../../../list/add/multiple.mjs';
 export function js_code_call_expression_arguments_2(code_argument_1, code_argument_2, args_additional, v) {
     arguments_assert(arguments, [
