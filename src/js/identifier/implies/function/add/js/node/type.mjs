e91d432eb3750e26e1615c227ce49bd9f94f6141
JaredMathis <jared.mathis@native.consulting>
2023-06-21 09:29:30 -0400

    #276 function_auto js_identifier_implies_function_add_js_node_type

--- src/js/identifier/implies/function/add/js/node/type.mjs
+++ src/js/identifier/implies/function/add/js/node/type.mjs
@@ -4,6 +4,7 @@ import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 import { js_identifier_is } from '../../../../../is.mjs';
 import { string_combine } from '../../../../../../../string/combine.mjs';
 import { function_name_separator } from '../../../../../../../function/name/separator.mjs';
+import { string_starts_with } from '../../../../../../../string/starts/with.mjs';
 export function js_identifier_implies_function_add_js_node_type(identifier_name) {
     arguments_assert(arguments, [js_identifier_is]);
     let prefix_1 = js_prefix_node_is();
@@ -13,5 +14,9 @@ export function js_identifier_implies_function_add_js_node_type(identifier_name)
             prefix_2
         ]) {
         let p_with_underscore_trailing = string_combine(p, function_name_separator());
+        if (string_starts_with(identifier_name, p_with_underscore_trailing)) {
+            return true;
+        }
     }
+    return false;
 }
