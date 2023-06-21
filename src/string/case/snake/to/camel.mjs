3b79d225b982a35620cf387d57fc5bc7d338cacf
JaredMathis <jared.mathis@native.consulting>
2023-06-21 10:54:36 -0400

    #269 task_later refactor '' etc  to string_empty()

--- src/string/case/snake/to/camel.mjs
+++ src/string/case/snake/to/camel.mjs
@@ -3,10 +3,11 @@ import { list_join } from '../../../../list/join.mjs';
 import { function_name_separator } from '../../../../function/name/separator.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { string_empty } from '../../../empty.mjs';
 export function string_case_snake_to_camel(input) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let parsed = js_identifier_parts_to(input);
     let v = function_name_separator();
-    let snake = list_join(parsed, v);
+    let snake = list_join(parsed, string_empty());
     return snake;
 }
