57c89588caf79209a98a097c0b6837d834173a7c
JaredMathis <jared.mathis@native.consulting>
2023-06-21 11:02:12 -0400

    #280 noop

--- src/string/case/upper/character/first.mjs
+++ src/string/case/upper/character/first.mjs
@@ -2,9 +2,9 @@ import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { string_to_case_upper } from '../../../to/case/upper.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { string_to_list } from '../../../to/list.mjs';
-export function string_case_upper_character_first(first) {
+export function string_case_upper_character_first(value) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    string_to_list(first);
-    let result = string_to_case_upper(first);
+    string_to_list(value);
+    let result = string_to_case_upper(value);
     return result;
 }
