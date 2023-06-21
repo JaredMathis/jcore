22948aae476b9ca7abecac8308361dee69a7e50e
JaredMathis <jared.mathis@native.consulting>
2023-06-21 09:25:27 -0400

    #276 function_auto js_identifier_implies_function_add_js_node_type

--- src/js/identifier/implies/function/add/js/node/type.mjs
+++ src/js/identifier/implies/function/add/js/node/type.mjs
@@ -1,7 +1,7 @@
-import { error } from '../../../../../../../error.mjs';
+import { js_identifier_combine } from '../../../../../combine.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 import { js_identifier_is } from '../../../../../is.mjs';
 export function js_identifier_implies_function_add_js_node_type(identifier_name) {
     arguments_assert(arguments, [js_identifier_is]);
-    error('todo: js_identifier_implies_function_add_js_node_type');
+    let function_name = js_identifier_combine(prefix, suffix);
 }
