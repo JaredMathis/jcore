3fccb6542aa48075813afe9e9660f60059b10342
JaredMathis <jared.mathis@native.consulting>
2023-06-21 11:06:49 -0400

    #280 function_auto string_case_upper_character_first

--- src/string/case/upper/character/first.mjs
+++ src/string/case/upper/character/first.mjs
@@ -1,12 +1,12 @@
+import { string_take } from '../../../take.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { string_get } from '../../../get.mjs';
-import { string_sub } from '../../../sub.mjs';
 import { string_combine } from '../../../combine.mjs';
 export function string_case_upper_character_first(value) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let first = string_get(value, 0);
-    let remaining = string_sub(value, 1);
+    let remaining = string_take(value, 1);
     let result = string_combine(first, remaining);
     return result;
 }
