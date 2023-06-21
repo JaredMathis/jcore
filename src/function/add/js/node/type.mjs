b4a5e2699b9454e9b35ac4936969166245fac170
JaredMathis <jared.mathis@native.consulting>
2023-06-20 20:50:01 -0400

    #274 function_auto function_add_js_node_type

--- src/function/add/js/node/type.mjs
+++ src/function/add/js/node/type.mjs
@@ -1,3 +1,4 @@
+import { string_case_camel_to_snake } from '../../../../string/case/camel/to/snake.mjs';
 import { function_add_with_statement_code } from '../../with/statement/code.mjs';
 import { js_code_return_statement } from '../../../../js/code/return/statement.mjs';
 import { js_code_expression_string } from '../../../../js/code/expression/string.mjs';
@@ -6,6 +7,7 @@ import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function function_add_js_node_type(camel_case) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let snake = string_case_camel_to_snake(camel_cased);
     let prefix_1 = 'js_node_is_';
     let expression = js_code_expression_string(string_value);
     let code_return_statment = js_code_return_statement(expression);
