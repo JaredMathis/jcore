978d6e248dc759c0625dc78fcdf50e52ad26173c
JaredMathis <jared.mathis@native.consulting>
2023-06-20 20:52:02 -0400

    #274 noop

--- src/function/add/js/node/type.mjs
+++ src/function/add/js/node/type.mjs
@@ -7,7 +7,7 @@ import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function function_add_js_node_type(camel_case) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let snake = string_case_camel_to_snake(camel_cased);
+    let snake = string_case_camel_to_snake(camel_case);
     let prefix_1 = 'js_node_is_';
     let expression = js_code_expression_string(string_value);
     let code_return_statment = js_code_return_statement(expression);
