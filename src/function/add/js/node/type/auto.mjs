a5021bb6fdf5a2cd3c93b12d937c657b829167a8
JaredMathis <jared.mathis@native.consulting>
2023-06-21 10:18:13 -0400

    #276 function_auto function_add_js_node_type_auto

--- src/function/add/js/node/type/auto.mjs
+++ src/function/add/js/node/type/auto.mjs
@@ -1,15 +1,15 @@
-import { function_add_js_node_type } from '../type.mjs';
 import { js_identifiers_filter_to_node_types } from '../../../../../js/identifiers/filter/to/node/types.mjs';
 import { js_identifiers } from '../../../../../js/identifiers.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_parse } from '../../../../parse.mjs';
+import { function_add_js_node_type_snake } from './snake.mjs';
 export async function function_add_js_node_type_auto(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let parsed = await function_parse(function_name);
     let identifiers = js_identifiers(parsed);
     let needed = js_identifiers_filter_to_node_types(identifiers);
     for (let n of needed) {
-        function_add_js_node_type;
+        await function_add_js_node_type_snake(n);
     }
 }
