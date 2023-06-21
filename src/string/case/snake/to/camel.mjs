3bf1540e17d7d955f362e96ff0a588fa35ea5ef9
JaredMathis <jared.mathis@native.consulting>
2023-06-21 11:05:10 -0400

    #280 noop

--- src/string/case/snake/to/camel.mjs
+++ src/string/case/snake/to/camel.mjs
@@ -5,8 +5,9 @@ import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { string_empty } from '../../../empty.mjs';
 import { list_map } from '../../../../list/map.mjs';
+import { js_identifier_is } from '../../../../js/identifier/is.mjs';
 export function string_case_snake_to_camel(input) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [js_identifier_is]);
     let parsed = js_identifier_parts_to(input);
     let mapped = list_map(parsed, string_case_upper_character_first);
     let snake = list_join(mapped, string_empty());
