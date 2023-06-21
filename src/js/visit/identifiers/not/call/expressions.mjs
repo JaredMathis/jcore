f90b107e6748090140cb2cbf1b4f3b280d0eb1c8
JaredMathis <jared.mathis@native.consulting>
2023-06-21 09:49:01 -0400

    #276 function_auto js_visit_identifiers_not_call_expressions

--- src/js/visit/identifiers/not/call/expressions.mjs
+++ src/js/visit/identifiers/not/call/expressions.mjs
@@ -1,9 +1,14 @@
+import { js_visit_identifiers } from '../../../identifiers.mjs';
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { js_node_is_call_expression } from '../../../../node/is/call/expression.mjs';
 import { js_node_is } from '../../../../node/is.mjs';
-import { js_node_is_identifier } from '../../../../node/is/identifier.mjs';
-import { js_visit_nodes_filter } from '../../../nodes/filter.mjs';
 export function js_visit_identifiers_not_call_expressions(node, lambda) {
-    js_visit_nodes_filter(node, js_node_is_identifier, v_2);
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    js_visit_identifiers(node, v_2);
     function v_2(v) {
         let {parent} = v;
         if (js_node_is(parent) && js_node_is_call_expression(parent)) {
