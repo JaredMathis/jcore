110cb87a7ceb564fc93d1acca6220eb4e45d0664
JaredMathis <jared.mathis@native.consulting>
2023-06-21 10:08:34 -0400

    #276 function_auto function_add_js_node_type_auto

--- src/function/add/js/node/type/auto.mjs
+++ src/function/add/js/node/type/auto.mjs
@@ -1,3 +1,4 @@
+import { js_identifiers_implies_function_add_js_node_type } from '../../../../../js/identifiers/implies/function/add/js/node/type.mjs';
 import { js_identifiers } from '../../../../../js/identifiers.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
@@ -6,4 +7,5 @@ export async function function_add_js_node_type_auto(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let parsed = await function_parse(function_name);
     let identifiers = js_identifiers(parsed);
+    await js_identifiers_implies_function_add_js_node_type(identifiers);
 }
