a7ea0d3e52e093f7d5c83bcb755d304554f822a6
JaredMathis <jared.mathis@native.consulting>
2023-06-21 11:03:15 -0400

    #280 function_auto string_case_upper_character_first

--- src/string/case/upper/character/first.mjs
+++ src/string/case/upper/character/first.mjs
@@ -1,12 +1,12 @@
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { string_to_case_upper } from '../../../to/case/upper.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { string_to_list } from '../../../to/list.mjs';
 import { string_get } from '../../../get.mjs';
+import { string_sub } from '../../../sub.mjs';
+import { string_combine } from '../../../combine.mjs';
 export function string_case_upper_character_first(value) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let list = string_to_list(value);
     let first = string_get(value, 0);
-    let result = string_to_case_upper(value);
+    let remaining = string_sub(value, 1);
+    let result = string_combine(first, remaining);
     return result;
 }
