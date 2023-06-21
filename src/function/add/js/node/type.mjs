0b2bf86ff8043ce0bfb26da3bb845b7a9ac7ef00
JaredMathis <jared.mathis@native.consulting>
2023-06-21 08:42:24 -0400

    #274 function_auto function_add_js_node_type

--- src/function/add/js/node/type.mjs
+++ src/function/add/js/node/type.mjs
@@ -7,7 +7,6 @@ import { js_code_identifier_value } from '../../../../js/code/identifier/value.m
 import { string_case_camel_to_snake } from '../../../../string/case/camel/to/snake.mjs';
 import { function_add_with_statement_code } from '../../with/statement/code.mjs';
 import { js_code_return_statement } from '../../../../js/code/return/statement.mjs';
-import { js_code_expression_string } from '../../../../js/code/expression/string.mjs';
 import { function_add_string_prefix_camel_generic } from '../../string/prefix/camel/generic.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
@@ -26,5 +25,8 @@ export async function function_add_js_node_type(camel_case) {
     let code_return_statment = js_code_return_statement(expression);
     let v_2 = await function_add_with_statement_code(js_node_is_function_name, code_return_statment);
     await function_input_named_value(js_node_is_function_name);
-    return [v, v_2];
+    return [
+        v,
+        v_2
+    ];
 }
