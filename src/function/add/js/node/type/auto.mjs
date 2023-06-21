03e9cb851ada0deea21492722560f98b991fef10
JaredMathis <jared.mathis@native.consulting>
2023-06-21 09:48:24 -0400

    #276 noop

--- src/function/add/js/node/type/auto.mjs
+++ src/function/add/js/node/type/auto.mjs
@@ -3,8 +3,8 @@ import { error } from '../../../../../error.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { function_parse } from '../../../../parse.mjs';
-export function function_add_js_node_type_auto(function_name) {
+export async function function_add_js_node_type_auto(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let parsed = function_parse(function_name);
+    let parsed = await function_parse(function_name);
     js_nodes_each(parsed, )
 }
