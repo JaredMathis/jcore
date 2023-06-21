31316c599bc7f8b7fc9f346068559a5b481bd79a
JaredMathis <jared.mathis@native.consulting>
2023-06-21 10:17:00 -0400

    #276 function_rename js_identifiers_implies_function_add_js_node_type js_identifiers_filter_to_node_types

--- src/function/add/js/node/type/auto.mjs
+++ src/function/add/js/node/type/auto.mjs
@@ -1,5 +1,5 @@
 import { function_add_js_node_type } from '../type.mjs';
-import { js_identifiers_implies_function_add_js_node_type } from '../../../../../js/identifiers/implies/function/add/js/node/type.mjs';
+import { js_identifiers_filter_to_node_types } from '../../../../../js/identifiers/filter/to/node/types.mjs';
 import { js_identifiers } from '../../../../../js/identifiers.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
@@ -8,7 +8,7 @@ export async function function_add_js_node_type_auto(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let parsed = await function_parse(function_name);
     let identifiers = js_identifiers(parsed);
-    let needed = js_identifiers_implies_function_add_js_node_type(identifiers);
+    let needed = js_identifiers_filter_to_node_types(identifiers);
     for (let n of needed) {
         function_add_js_node_type;
     }
