8477a59297fdbd3836401590f3c09b84910dc7ab
JaredMathis <jared.mathis@native.consulting>
2023-06-21 09:51:44 -0400

    #276 noop

--- src/function/add/js/node/type/auto.mjs
+++ src/function/add/js/node/type/auto.mjs
@@ -5,5 +5,5 @@ import { function_parse } from '../../../../parse.mjs';
 export async function function_add_js_node_type_auto(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let parsed = await function_parse(function_name);
-    let identifiers = js_identifiers();
+    let identifiers = js_identifiers(parsed);
 }
