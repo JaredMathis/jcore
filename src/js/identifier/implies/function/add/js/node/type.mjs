407a0e5ca6f5973fef2b88fe02f866b815d63bdf
JaredMathis <jared.mathis@native.consulting>
2023-06-21 09:28:55 -0400

    #276 noop

--- src/js/identifier/implies/function/add/js/node/type.mjs
+++ src/js/identifier/implies/function/add/js/node/type.mjs
@@ -3,6 +3,7 @@ import { js_prefix_node_type } from '../../../../../../prefix/node/type.mjs';
 import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 import { js_identifier_is } from '../../../../../is.mjs';
 import { string_combine } from '../../../../../../../string/combine.mjs';
+import { function_name_separator } from '../../../../../../../function/name/separator.mjs';
 export function js_identifier_implies_function_add_js_node_type(identifier_name) {
     arguments_assert(arguments, [js_identifier_is]);
     let prefix_1 = js_prefix_node_is();
@@ -11,6 +12,6 @@ export function js_identifier_implies_function_add_js_node_type(identifier_name)
             prefix_1,
             prefix_2
         ]) {
-        let p_with_underscore_trailing = string_combine();
+        let p_with_underscore_trailing = string_combine(p, function_name_separator());
     }
 }
