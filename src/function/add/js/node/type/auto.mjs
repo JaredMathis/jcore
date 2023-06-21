e26f3598894a513adbbd16cf4396621d27936103
JaredMathis <jared.mathis@native.consulting>
2023-06-21 09:42:24 -0400

    #276 function_input_add function_add_js_node_type_auto function_name

--- src/function/add/js/node/type/auto.mjs
+++ src/function/add/js/node/type/auto.mjs
@@ -1,6 +1,7 @@
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { error } from '../../../../../error.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export function function_add_js_node_type_auto() {
-    arguments_assert(arguments, []);
+export function function_add_js_node_type_auto(function_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: function_add_js_node_type_auto');
 }
