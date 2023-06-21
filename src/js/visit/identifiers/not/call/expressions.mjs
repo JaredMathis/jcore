d2495c89d1f577c639c57d0977b6ed6e670bec30
JaredMathis <jared.mathis@native.consulting>
2023-06-21 09:47:23 -0400

    #276 task_later refactor f(function x(){}) to f(x);function x(){}

--- src/js/visit/identifiers/not/call/expressions.mjs
+++ src/js/visit/identifiers/not/call/expressions.mjs
@@ -3,12 +3,13 @@ import { js_node_is } from '../../../../node/is.mjs';
 import { js_node_is_identifier } from '../../../../node/is/identifier.mjs';
 import { js_visit_nodes_filter } from '../../../nodes/filter.mjs';
 export function js_visit_identifiers_not_call_expressions(node, lambda) {
-    js_visit_nodes_filter(node, js_node_is_identifier, function v_2(v) {
+    js_visit_nodes_filter(node, js_node_is_identifier, v_2);
+    function v_2(v) {
         let {parent} = v;
         if (js_node_is(parent) && js_node_is_call_expression(parent)) {
             return;
         }
         let {node} = v;
         lambda(node);
-    });
+    }
 }
