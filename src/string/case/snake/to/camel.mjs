9fb443503d0d8d87f6d5f57e87825b23d1af5d2e
JaredMathis <jared.mathis@native.consulting>
2023-06-21 11:03:24 -0400

    #280 function_auto string_case_snake_to_camel

--- src/string/case/snake/to/camel.mjs
+++ src/string/case/snake/to/camel.mjs
@@ -1,6 +1,6 @@
+import { string_case_upper_character_first } from '../../upper/character/first.mjs';
 import { js_identifier_parts_to } from '../../../../js/identifier/parts/to.mjs';
 import { list_join } from '../../../../list/join.mjs';
-import { function_name_separator } from '../../../../function/name/separator.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { string_empty } from '../../../empty.mjs';
@@ -8,7 +8,7 @@ import { list_map } from '../../../../list/map.mjs';
 export function string_case_snake_to_camel(input) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let parsed = js_identifier_parts_to(input);
-    let mapped = list_map(parsed, string_case_upper_character_first)
+    let mapped = list_map(parsed, string_case_upper_character_first);
     let snake = list_join(mapped, string_empty());
     return snake;
 }
