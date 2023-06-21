3e6ee38cc6802a7fff7b6c8cc38857613272b620
JaredMathis <jared.mathis@native.consulting>
2023-06-21 08:40:24 -0400

    #274 noop

--- src/function/add/js/node/type.mjs
+++ src/function/add/js/node/type.mjs
@@ -13,6 +13,8 @@ import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function function_add_js_node_type(camel_case) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let prefix_2 = 'js_node_type';
+    let v = await function_add_string_prefix_camel_generic(prefix_2, camel_case);
     let value = js_code_identifier_value();
     let snake = string_case_camel_to_snake(camel_case);
     let prefix_1 = 'js_node_is_';
@@ -24,7 +26,5 @@ export async function function_add_js_node_type(camel_case) {
     let js_node_is_function_name = `${ prefix_1 }${ snake }`;
     let v_2 = await function_add_with_statement_code(js_node_is_function_name, code_return_statment);
     await function_input_named_value(js_node_is_function_name);
-    let prefix_2 = 'js_node_type';
-    let v = await function_add_string_prefix_camel_generic(prefix_2, camel_case);
     return v;
 }
