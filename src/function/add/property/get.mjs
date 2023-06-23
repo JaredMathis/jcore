ee163ad9fd64e95f5a6013c18ccdf1afc37a02c0
JaredMathis <jared.mathis@native.consulting>
2023-06-23 12:45:25 -0400

    #284 function_auto function_add_property_get

--- src/function/add/property/get.mjs
+++ src/function/add/property/get.mjs
@@ -1,3 +1,4 @@
+import { js_suffix_get } from '../../../js/suffix/get.mjs';
 import { function_add_property_generic } from './generic.mjs';
 import { js_code_call_expression_object_property_get } from '../../../js/code/call/expression/object/property/get.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -7,7 +8,7 @@ export async function function_add_property_get(property_prefix, property_name) 
         js_identifier_is,
         js_identifier_is
     ]);
-    const method = 'get';
+    const method = js_suffix_get();
     let lambda = (property_prefix, property_name_get) => js_code_call_expression_object_property_get(property_prefix, property_name_get);
     let v = await function_add_property_generic(property_prefix, property_name, lambda, method, false);
     return v;
