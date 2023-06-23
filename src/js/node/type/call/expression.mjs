85f20a8373dbdeb836a986b7e3768e63f9143066
JaredMathis <jared.mathis@native.consulting>
2023-06-23 14:16:25 -0400

    #286 function_auto rule_function_call_arguments_are_identifiers

---
+++ src/js/node/type/call/expression.mjs
@@ -0,0 +1,5 @@
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function js_node_type_call_expression() {
+    arguments_assert(arguments, []);
+    return 'CallExpression';
+}
