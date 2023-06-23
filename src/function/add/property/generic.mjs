057b6138d01e8aca8637995373818bdd2245505a
JaredMathis <jared.mathis@native.consulting>
2023-06-23 13:09:58 -0400

    #284 function_auto function_add_property_generic

--- src/function/add/property/generic.mjs
+++ src/function/add/property/generic.mjs
@@ -8,8 +8,6 @@ import { function_input_add_type } from '../../input/add/type.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 import { function_name_get } from '../../name/get.mjs';
 import { function_add_with_statement_code } from '../with/statement/code.mjs';
-import { list_join } from '../../../list/join.mjs';
-import { function_name_separator } from '../../name/separator.mjs';
 import { js_code_return_statement } from '../../../js/code/return/statement.mjs';
 import { js_code_call_expression } from '../../../js/code/call/expression.mjs';
 import { function_add_string_prefix } from '../string/prefix.mjs';
@@ -31,11 +29,7 @@ export async function function_add_property_generic(property_prefix, property_na
     }
     let property_name_get = js_code_call_expression(property_name_fn_get);
     const value_name = js_code_identifier_value();
-    let v_2 = function_name_separator();
-    let function_name = list_join([
-        property_name_fn_get,
-        method
-    ], v_2);
+    let function_name = js_identifier_combine(property_name_fn_get, method);
     let expression_code = lambda(property_prefix, property_name_get, value_name);
     let statement_code = js_code_return_statement(expression_code);
     let result = await function_add_with_statement_code(function_name, statement_code);
