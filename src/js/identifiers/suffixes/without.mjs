a3ee4540c6f0b026d70d38e6ba424e38eb120d03
JaredMathis <jared.mathis@native.consulting>
2023-06-23 12:34:09 -0400

    #284 function_copy_part_change js_identifiers_prefixes_without prefixes suffixes

---
+++ src/js/identifiers/suffixes/without.mjs
@@ -0,0 +1,13 @@
+import { list_string_prefix_without_multiple } from '../../../list/string/prefix/without/multiple.mjs';
+import { function_name_separator_combine } from '../../../function/name/separator/combine.mjs';
+import { list_map } from '../../../list/map.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function js_identifiers_suffixes_without(prefixes, identifier_names) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let mapped = list_map(prefixes, function_name_separator_combine);
+    return list_string_prefix_without_multiple(identifier_names, mapped);
+}
