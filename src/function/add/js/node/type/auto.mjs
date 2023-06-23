f1c6bf5fcf056384ef3aad3e635f49095c646a14
JaredMathis <jared.mathis@native.consulting>
2023-06-23 13:12:43 -0400

    #284 function_auto function_add_js_node_type_auto

--- src/function/add/js/node/type/auto.mjs
+++ src/function/add/js/node/type/auto.mjs
@@ -13,16 +13,14 @@ import { function_exists } from '../../../../exists.mjs';
 export async function function_add_js_node_type_auto(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let identifiers = await function_identifiers(function_name);
-    if (false) {
-        let property_names = js_identifiers_filter_to_node_properties_get(identifiers);
-        let prefix = js_prefix_node_property();
-        for (let property_name of property_names) {
-            let property_name_fn_get_prefix = js_identifier_prefix_property(prefix);
-            let property_name_fn_get = js_identifier_combine(property_name_fn_get_prefix, property_name);
-            let function_name = js_identifier_combine(property_name_fn_get, method);
-            if (!await function_exists(function_name)) {
-                await function_add_property_get(prefix, property_name);
-            }
+    let property_names = js_identifiers_filter_to_node_properties_get(identifiers);
+    let prefix = js_prefix_node_property();
+    for (let property_name of property_names) {
+        let property_name_fn_get_prefix = js_identifier_prefix_property(prefix);
+        let property_name_fn_get = js_identifier_combine(property_name_fn_get_prefix, property_name);
+        let function_name = js_identifier_combine(property_name_fn_get, method);
+        if (!await function_exists(function_name)) {
+            await function_add_property_get(prefix, property_name);
         }
     }
     await functions_new_if_not_exists(js_identifiers_filter_to_node_types, function_add_js_node_type_snake, identifiers);
