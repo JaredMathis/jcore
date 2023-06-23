bac67e7125a5c5039e96efb7e53f0ca17d29796b
JaredMathis <jared.mathis@native.consulting>
2023-06-23 12:20:50 -0400

    #284 function_auto list_string_suffix_without_multiple

--- src/list/string/suffix/without/multiple.mjs
+++ src/list/string/suffix/without/multiple.mjs
@@ -1,6 +1,6 @@
+import { string_suffix_without } from '../../../../string/suffix/without.mjs';
+import { string_ends_with } from '../../../../string/ends/with.mjs';
 import { list_string_without_multiple_generic } from '../../without/multiple/generic.mjs';
-import { string_prefix_without } from '../../../../string/prefix/without.mjs';
-import { string_starts_with } from '../../../../string/starts/with.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function list_string_suffix_without_multiple(strings, prefixes) {
@@ -8,7 +8,7 @@ export function list_string_suffix_without_multiple(strings, prefixes) {
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let predicate = string_starts_with;
-    let mapper = string_prefix_without;
+    let predicate = string_ends_with;
+    let mapper = string_suffix_without;
     return list_string_without_multiple_generic(prefixes, strings, predicate, mapper);
 }
