073bad1abe3815d1a4ff49f6aeb8aea279a40426
JaredMathis <jared.mathis@native.consulting>
2023-06-23 12:42:17 -0400

    #284 function_input_swap_last_2 functions_new_if_not_exists

--- src/function/add/js/node/type/auto.mjs
+++ src/function/add/js/node/type/auto.mjs
@@ -11,5 +11,5 @@ export async function function_add_js_node_type_auto(function_name) {
     let needed2 = js_identifiers_filter_to_node_properties(identifiers);
     let lambda_function_new = function_add_js_node_type_snake;
     let lambda_function_new_get = js_identifiers_filter_to_node_types;
-    await functions_new_if_not_exists(lambda_function_new_get, identifiers, lambda_function_new);
+    await functions_new_if_not_exists(lambda_function_new_get, lambda_function_new, identifiers);
 }
