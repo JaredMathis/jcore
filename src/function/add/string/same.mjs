accccd34943fafc72c3eaef9879951093a7ad538
JaredMathis <jared.mathis@native.consulting>
2023-06-23 12:08:43 -0400

    #284 noop

--- src/function/add/string/same.mjs
+++ src/function/add/string/same.mjs
@@ -3,6 +3,6 @@ import { function_add_string } from '../string.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function function_add_string_same(function_name) {
     arguments_assert(arguments, [js_identifier_is]);
-    let result = await function_add_string(function_name, string_value);
+    let result = await function_add_string(function_name, function_name);
     return result;
 }
