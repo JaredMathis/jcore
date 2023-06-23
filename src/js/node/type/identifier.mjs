ec56bc4a57958ffc93ccc99cfc27fbd7ebe7570c
JaredMathis <jared.mathis@native.consulting>
2023-06-23 14:22:08 -0400

    #286 function_auto rule_function_call_arguments_are_identifiers

---
+++ src/js/node/type/identifier.mjs
@@ -0,0 +1,5 @@
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function js_node_type_identifier() {
+    arguments_assert(arguments, []);
+    return 'Identifier';
+}
--- src/rule/function/call/arguments/are/identifiers.mjs
+++ src/rule/function/call/arguments/are/identifiers.mjs
@@ -3,16 +3,16 @@ import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_names_each } from '../../../../../function/names/each.mjs';
 import { js_node_is_call_expression } from '../../../../../js/node/is/call/expression.mjs';
-import { log } from '../../../../../log.mjs';
-import { error } from '../../../../../error.mjs';
+import { assert } from '../../../../../assert.mjs';
+import { list_all } from '../../../../../list/all.mjs';
+import { js_node_is_identifier } from '../../../../../js/node/is/identifier.mjs';
 export async function rule_function_call_arguments_are_identifiers() {
     arguments_assert(arguments, []);
     await function_names_each(args => {
         let {parsed} = args;
         js_nodes_each(parsed, js_node_is_call_expression, node => {
             let args = js_node_property_arguments_get(node);
-            log({ node });
-            error();
+            assert(list_all(args, js_node_is_identifier));
         });
     });
 }
