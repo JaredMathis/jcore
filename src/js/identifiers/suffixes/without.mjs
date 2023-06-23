769b7bd4f16ead2456cace804cd8820c3d5f82b6
JaredMathis <jared.mathis@native.consulting>
2023-06-23 13:14:44 -0400

    #284 noop

--- src/js/identifiers/suffixes/without.mjs
+++ src/js/identifiers/suffixes/without.mjs
@@ -3,11 +3,11 @@ import { function_name_separator_combine } from '../../../function/name/separato
 import { list_map } from '../../../list/map.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function js_identifiers_suffixes_without(prefixes, identifier_names) {
+export function js_identifiers_suffixes_without(suffixes, identifier_names) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let mapped = list_map(prefixes, function_name_separator_combine);
+    let mapped = list_map(suffixes, function_name_separator_combine);
     return list_string_suffix_without_multiple(identifier_names, mapped);
 }
