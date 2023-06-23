10871d6ebf2b4a73422ea6ddacd3e8e2e2ecda6e
JaredMathis <jared.mathis@native.consulting>
2023-06-23 14:18:34 -0400

    #286 function_auto rule_function_call_arguments_are_identifiers

--- src/rule/function/call/arguments/are/identifiers.mjs
+++ src/rule/function/call/arguments/are/identifiers.mjs
@@ -2,10 +2,15 @@ import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_names_each } from '../../../../../function/names/each.mjs';
 import { js_node_is_call_expression } from '../../../../../js/node/is/call/expression.mjs';
+import { log } from '../../../../../log.mjs';
+import { error } from '../../../../../error.mjs';
 export async function rule_function_call_arguments_are_identifiers() {
     arguments_assert(arguments, []);
     await function_names_each(args => {
         let {parsed} = args;
-        js_nodes_each(parsed, js_node_is_call_expression, lambda);
+        js_nodes_each(parsed, js_node_is_call_expression, node => {
+            log({ node });
+            error();
+        });
     });
 }
