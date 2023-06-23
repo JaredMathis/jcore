baa751e19de611e1a68bb1caf1387a0c09395cca
JaredMathis <jared.mathis@native.consulting>
2023-06-23 12:18:45 -0400

    #284 function_auto function_copy_part_change

---
+++ src/js/identifier/part/change.mjs
@@ -0,0 +1,11 @@
+import { js_identifier_parts_from } from '../parts/from.mjs';
+import { list_set } from '../../../list/set.mjs';
+import { list_index_of_single } from '../../../list/index/of/single.mjs';
+import { js_identifier_parts_to } from '../parts/to.mjs';
+export function js_identifier_part_change(function_name_prefix, part_to_change_from, part_to_change_to) {
+    let function_name_prefix_parts = js_identifier_parts_to(function_name_prefix);
+    let index = list_index_of_single(function_name_prefix_parts, part_to_change_from);
+    list_set(function_name_prefix_parts, index, part_to_change_to);
+    let function_name_of_copy = js_identifier_parts_from(function_name_prefix_parts);
+    return function_name_of_copy;
+}
