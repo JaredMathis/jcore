e37a074606fbd435cca91ac30ca811de7ec45489
JaredMathis <jared.mathis@native.consulting>
2023-06-23 15:27:39 -0400

    #288 function_asyncify js_visit

---
+++ src/js/visit/async.mjs
@@ -0,0 +1,18 @@
+import { metadata } from '../../metadata.mjs';
+import { object_properties } from '../../object/properties.mjs';
+import { visit_filter } from '../../visit/filter.mjs';
+import { function_is } from '../../function/is.mjs';
+import { js_node_is } from '../node/is.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export async function js_visit_async(parsed, filter, lambda) {
+    arguments_assert(arguments, [
+        js_node_is,
+        function_is,
+        function_is
+    ]);
+    visit_filter(parsed, function v(node) {
+        let v_2 = object_properties(node);
+        return v_2;
+    }, filter, lambda);
+    metadata([]);
+}
