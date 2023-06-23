60a0f44c18dfd64fc5a75c0dcaaa2a8ea972f393
JaredMathis <jared.mathis@native.consulting>
2023-06-23 12:10:49 -0400

    #284 function_auto js_identifiers_filter_to_node_properties

--- src/js/identifiers/filter/to/node/properties.mjs
+++ src/js/identifiers/filter/to/node/properties.mjs
@@ -1,12 +1,9 @@
+import { js_prefix_node_property } from '../../../../prefix/node/property.mjs';
 import { js_identifiers_prefixes_without } from '../../../prefixes/without.mjs';
-import { js_prefix_node_type } from '../../../../prefix/node/type.mjs';
-import { js_prefix_node_is } from '../../../../prefix/node/is.mjs';
 import { list_is } from '../../../../../list/is.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export function js_identifiers_filter_to_node_properties(identifier_names) {
     arguments_assert(arguments, [list_is]);
-    const prefixes = [
-        js_prefix_node_property(),
-    ];
+    const prefixes = [js_prefix_node_property()];
     return js_identifiers_prefixes_without(prefixes, identifier_names);
 }
