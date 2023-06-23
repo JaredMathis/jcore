a9d33cad3d4edc82e90a9a092823715a4571d2b0
JaredMathis <jared.mathis@native.consulting>
2023-06-23 13:44:38 -0400

    #284 function_search block_statement

--- src/refactor/if/expression/statement/to/block/statement.mjs
+++ src/refactor/if/expression/statement/to/block/statement.mjs
@@ -3,10 +3,14 @@ import { js_node_is_if_statement } from '../../../../../../js/node/is/if/stateme
 import { arguments_assert_todo } from '../../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
 import { js_nodes_each } from '../../../../../../js/nodes/each.mjs';
+import { js_node_is_expression_statement } from '../../../../../../js/node/is/expression/statement.mjs';
 export function refactor_if_expression_statement_to_block_statement(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {parsed} = args;
     js_nodes_each(parsed, js_node_is_if_statement, node => {
         let consequent = js_node_property_consequent_get(node);
+        if (js_node_is_expression_statement(consequent)) {
+            
+        }
     });
 }
