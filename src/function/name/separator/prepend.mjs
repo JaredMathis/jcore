33953bbb8e7af68596094c77cf586ce356a7d3ea
JaredMathis <jared.mathis@native.consulting>
2023-06-23 13:16:15 -0400

    #284 function_copy_suffix_change function_name_separator append prepend

---
+++ src/function/name/separator/prepend.mjs
@@ -0,0 +1,8 @@
+import { function_name_separator } from '../separator.mjs';
+import { string_combine } from '../../../string/combine.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function function_name_separator_prepend(p) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    return string_combine(p, function_name_separator());
+}
