bd9b1167a45a7d357d8ad85090ad8756a55a820f
JaredMathis <jared.mathis@native.consulting>
2023-06-23 11:52:11 -0400

    #269 function_auto functions_to_node_type_first

--- src/functions/to/node/type/first.mjs
+++ src/functions/to/node/type/first.mjs
@@ -1,11 +1,19 @@
+import { js_node_is_type } from '../../../../js/node/is/type.mjs';
+import { js_nodes_get } from '../../../../js/nodes/get.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { function_name_to_node_type_first } from '../../../../function/name/to/node/type/first.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { function_name_all } from '../../../../function/name/all.mjs';
+import { list_empty_not } from '../../../../list/empty/not.mjs';
+import { list_first } from '../../../../list/first.mjs';
+import { function_parse } from '../../../../function/parse.mjs';
 export async function functions_to_node_type_first(node_type_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     for (let function_name of await function_name_all()) {
-        let result = await function_name_to_node_type_first(function_name, node_type_name);
-        return result;
+        let parsed = function_parse(function_name);
+        let filtered = js_nodes_get(parsed, n => js_node_is_type(n, node_type_name));
+        if (list_empty_not(filtered)) {
+            return list_first(filtered);
+        }
     }
+    return null;
 }
