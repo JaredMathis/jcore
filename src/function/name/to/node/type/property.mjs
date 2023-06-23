77efaa97c62ff49eef061348ef2319ede91bc80c
JaredMathis <jared.mathis@native.consulting>
2023-06-23 14:50:39 -0400

    #288 function_input_add function_name_to_node_type_property property_name

--- src/function/name/to/node/type/property.mjs
+++ src/function/name/to/node/type/property.mjs
@@ -3,9 +3,10 @@ import { function_name_to_node_type_all } from './all.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { js_identifier_is } from '../../../../../js/identifier/is.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export async function function_name_to_node_type_property(function_name, node_type_name) {
+export async function function_name_to_node_type_property(function_name, node_type_name, property_name) {
     arguments_assert(arguments, [
         js_identifier_is,
+        arguments_assert_todo,
         arguments_assert_todo
     ]);
     let filtered = await function_name_to_node_type_all(function_name, node_type_name);
