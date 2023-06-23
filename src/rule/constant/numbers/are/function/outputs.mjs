49533fa107d77adacc2c72e075fb8a160242723b
JaredMathis <jared.mathis@native.consulting>
2023-06-23 14:52:53 -0400

    #288 noop

--- src/rule/constant/numbers/are/function/outputs.mjs
+++ src/rule/constant/numbers/are/function/outputs.mjs
@@ -11,12 +11,13 @@ import { js_node_is_call_expression } from '../../../../../js/node/is/call/expre
 import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { function_names_each } from '../../../../../function/names/each.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
+import { js_node_is_literal } from '../../../../../js/node/is/literal.mjs';
 export async function rule_constant_numbers_are_function_outputs() {
     arguments_assert(arguments, []);
     await function_names_each(args => {
         let {parsed} = args;
         let {file_path} = args;
-        js_nodes_each(parsed, js_node_is_call_expression, node => {
+        js_nodes_each(parsed, js_node_is_literal, node => {
             let name = js_call_expression_name_get(node);
             const rule_exceptions = [
                 function_name_get(arguments_assert),
