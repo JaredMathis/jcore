df0c646989b4d1771c12db367d7ab0080a03da01
JaredMathis <jared.mathis@native.consulting>
2023-06-23 14:12:55 -0400

    #286 function_auto rule_function_call_arguments_are_identifiers

--- src/rule/function/call/arguments/are/identifiers.mjs
+++ src/rule/function/call/arguments/are/identifiers.mjs
@@ -1,7 +1,9 @@
+import { log } from '../../../../../log.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_names_each } from '../../../../../function/names/each.mjs';
 export async function rule_function_call_arguments_are_identifiers() {
     arguments_assert(arguments, []);
     await function_names_each(args => {
+        console.log({ args });
     });
 }
