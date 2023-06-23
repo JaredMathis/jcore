7b6835f9de75e339920c247bac9b6e0ebf080dea
JaredMathis <jared.mathis@native.consulting>
2023-06-23 14:25:06 -0400

    #286 function_auto rule_function_call_arguments_are_identifiers

--- src/rule/function/call/arguments/are/identifiers.mjs
+++ src/rule/function/call/arguments/are/identifiers.mjs
@@ -1,13 +1,12 @@
+import { js_call_expression_name_get } from '../../../../../js/call/expression/name/get.mjs';
 import { js_node_property_arguments_get } from '../../../../../js/node/property/arguments/get.mjs';
 import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_names_each } from '../../../../../function/names/each.mjs';
 import { js_node_is_call_expression } from '../../../../../js/node/is/call/expression.mjs';
-import { assert } from '../../../../../assert.mjs';
 import { list_all } from '../../../../../list/all.mjs';
 import { js_node_is_identifier } from '../../../../../js/node/is/identifier.mjs';
 import { assert_message } from '../../../../../assert/message.mjs';
-import { error } from '../../../../../error.mjs';
 import { equal } from '../../../../../equal.mjs';
 import { function_name_get } from '../../../../../function/name/get.mjs';
 export async function rule_function_call_arguments_are_identifiers() {
