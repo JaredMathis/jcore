36f94dcb2c9c1aebfda25c9e5461ab37f2079d6e
JaredMathis <jared.mathis@native.consulting>
2023-06-23 12:17:14 -0400

    #284 noop

--- src/function/copy/part/change.mjs
+++ src/function/copy/part/change.mjs
@@ -4,13 +4,14 @@ import { list_remove } from '../../../list/remove.mjs';
 import { js_identifier_parts_to } from '../../../js/identifier/parts/to.mjs';
 import { js_identifier_is } from '../../../js/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_copy_part_change(function_name_prefix, part_to_remove) {
+import { list_index_of_single } from '../../../list/index/of/single.mjs';
+export async function function_copy_part_change(function_name_prefix, part_to_change) {
     arguments_assert(arguments, [
         js_identifier_is,
         js_identifier_is
     ]);
     let function_name_prefix_parts = js_identifier_parts_to(function_name_prefix);
-    list_remove(function_name_prefix_parts, part_to_remove);
+    list_index_of_single(function_name_prefix_parts, part_to_change)
     let function_name_of_copy = js_identifier_parts_from(function_name_prefix_parts);
     await function_copy(function_name_prefix, function_name_of_copy);
 }
