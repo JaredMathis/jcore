9227d03c4d25de653dce78c7960eb2be216c671f
JaredMathis <jared.mathis@native.consulting>
2023-06-23 14:00:12 -0400

    #269 function_auto function_auto_no_add_refactors

--- src/function/auto/no/add/refactors.mjs
+++ src/function/auto/no/add/refactors.mjs
@@ -3,17 +3,16 @@ import { js_mapper_args_to_statement_arguments_assert } from '../../../../js/map
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { refactor_functions_to_files } from '../../../../refactor/functions/to/files.mjs';
-import { list_multiple_combine } from '../../../../list/multiple/combine.mjs';
 export function function_auto_no_add_refactors() {
     arguments_assert(arguments, []);
-    let first = [js_mapper_args_to_statement_arguments_assert];
-    let second = [refactor_functions_to_files];
+    let first = [];
+    let second = [];
     if (false)
         [refactor_if_expression_statement_to_block_statement];
-    let v = list_multiple_combine([
-        first,
-        second
-    ]);
+    let v = [
+        js_mapper_args_to_statement_arguments_assert,
+        refactor_functions_to_files
+    ];
     return v;
     metadata([]);
 }
