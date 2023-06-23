6e3f7ad9a2e2ddc517e6eeac215717e260c321c7
JaredMathis <jared.mathis@native.consulting>
2023-06-23 13:49:03 -0400

    #284 function_input_add_type js_node_is_if_statement value js_node_is

--- src/js/node/is/if/statement.mjs
+++ src/js/node/is/if/statement.mjs
@@ -1,7 +1,8 @@
+import { js_node_is } from '../../is.mjs';
 import { js_node_type_if_statement } from '../../type/if/statement.mjs';
 import { js_node_is_type } from '../type.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function js_node_is_if_statement() {
-    arguments_assert(arguments, []);
+export function js_node_is_if_statement(value) {
+    arguments_assert(arguments, [js_node_is]);
     return js_node_is_type(value, js_node_type_if_statement());
 }
