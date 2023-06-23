fcf2910bed8b6685516e267eb2c7accd582843a6
JaredMathis <jared.mathis@native.consulting>
2023-06-23 13:47:45 -0400

    #284 function_auto refactor_if_expression_statement_to_block_statement

--- src/refactor/if/expression/statement/to/block/statement.mjs
+++ src/refactor/if/expression/statement/to/block/statement.mjs
@@ -8,13 +8,14 @@ import { js_nodes_each } from '../../../../../../js/nodes/each.mjs';
 import { js_node_is_expression_statement } from '../../../../../../js/node/is/expression/statement.mjs';
 import { assert } from '../../../../../../assert.mjs';
 import { js_node_is_block_statement } from '../../../../../../js/node/is/block/statement.mjs';
+import { string_a } from '../../../../../../string/a.mjs';
 export function refactor_if_expression_statement_to_block_statement(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {parsed} = args;
     js_nodes_each(parsed, js_node_is_if_statement, node => {
         let consequent = js_node_property_consequent_get(node);
         if (js_node_is_expression_statement(consequent)) {
-            let function_expression = js_function_declaration_named(name);
+            let function_expression = js_function_declaration_named(string_a());
             let body = js_node_property_body_get(function_expression);
             assert(js_node_is_block_statement(body));
         }
