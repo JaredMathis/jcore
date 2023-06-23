67d5986adbacc3ac8f801ba60947f3bee47c5065
JaredMathis <jared.mathis@native.consulting>
2023-06-23 15:02:35 -0400

    #288 function_auto function_add_return

--- src/function/add/return.mjs
+++ src/function/add/return.mjs
@@ -1,7 +1,13 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { function_add_with_statement_code } from './with/statement/code.mjs';
 import { js_code_return_statement } from '../../js/code/return/statement.mjs';
-export async function function_add_return(expression_code, function_name) {
-    let v = js_code_return_statement(expression_code);
+export async function function_add_return(return_expression_code, function_name) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let v = js_code_return_statement(return_expression_code);
     let v_2 = await function_add_with_statement_code(function_name, v);
     return v_2;
 }
