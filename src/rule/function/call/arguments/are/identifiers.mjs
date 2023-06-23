72d8fb32db07721e45deb7f4171175f685620c69
JaredMathis <jared.mathis@native.consulting>
2023-06-23 14:11:33 -0400

    #286 function_auto rules

--- src/rule/function/call/arguments/are/identifiers.mjs
+++ src/rule/function/call/arguments/are/identifiers.mjs
@@ -1,6 +1,6 @@
 import { error } from '../../../../../error.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-export function rule_function_call_arguments_are_identifiers() {
+export async function rule_function_call_arguments_are_identifiers() {
     arguments_assert(arguments, []);
     error('todo: rule_function_call_arguments_are_identifiers');
 }
