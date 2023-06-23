54078f22d2757186c3a0af9626cbd754437cc984
JaredMathis <jared.mathis@native.consulting>
2023-06-23 12:08:36 -0400

    #284 function_input_remove_last function_add_string_same

--- src/function/add/string/same.mjs
+++ src/function/add/string/same.mjs
@@ -1,12 +1,8 @@
-import { string_is } from '../../../string/is.mjs';
 import { js_identifier_is } from '../../../js/identifier/is.mjs';
 import { function_add_string } from '../string.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function function_add_string_same(function_name, string_value) {
-    arguments_assert(arguments, [
-        js_identifier_is,
-        string_is
-    ]);
+export async function function_add_string_same(function_name) {
+    arguments_assert(arguments, [js_identifier_is]);
     let result = await function_add_string(function_name, string_value);
     return result;
 }
