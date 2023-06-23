b57b8c39fcdeed130c898a26fc2492d9bd1bd68a
JaredMathis <jared.mathis@native.consulting>
2023-06-23 13:27:12 -0400

    #284 noop

--- src/js/prefix/node/property.mjs
+++ src/js/prefix/node/property.mjs
@@ -1,5 +1,5 @@
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export function js_prefix_node_property() {
     arguments_assert(arguments, []);
-    return `js_node_property`;
+    return `${`js_node`}_property`;
 }
