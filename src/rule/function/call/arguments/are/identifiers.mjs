4cabd09e10b55a82ced3a8bd8b5a678d23e328a1
JaredMathis <jared.mathis@native.consulting>
2023-06-23 14:19:20 -0400

    #286 function_auto rule_function_call_arguments_are_identifiers

--- src/rule/function/call/arguments/are/identifiers.mjs
+++ src/rule/function/call/arguments/are/identifiers.mjs
@@ -1,3 +1,4 @@
+import { js_node_property_arguments_get } from '../../../../../js/node/property/arguments/get.mjs';
 import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_names_each } from '../../../../../function/names/each.mjs';
@@ -9,6 +10,7 @@ export async function rule_function_call_arguments_are_identifiers() {
     await function_names_each(args => {
         let {parsed} = args;
         js_nodes_each(parsed, js_node_is_call_expression, node => {
+            let args = js_node_property_arguments_get(node);
             log({ node });
             error();
         });
