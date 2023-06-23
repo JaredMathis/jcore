febdb74c8c258554d40fa3ecbed91ff0eb2a04cd
JaredMathis <jared.mathis@native.consulting>
2023-06-23 11:51:21 -0400

    #269 function_auto function_name_to_node_type_first

--- src/function/name/to/node/type/first.mjs
+++ src/function/name/to/node/type/first.mjs
@@ -1,10 +1,9 @@
+import { js_nodes_get } from '../../../../../js/nodes/get.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { js_identifier_is } from '../../../../../js/identifier/is.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_parse } from '../../../../parse.mjs';
 import { js_node_is_type } from '../../../../../js/node/is/type.mjs';
-import { list_adder } from '../../../../../list/adder.mjs';
-import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { list_first } from '../../../../../list/first.mjs';
 export async function function_name_to_node_type_first(function_name, node_type_name) {
     arguments_assert(arguments, [
@@ -12,8 +11,6 @@ export async function function_name_to_node_type_first(function_name, node_type_
         arguments_assert_todo
     ]);
     let parsed = await function_parse(function_name);
-    let filtered = list_adder(la => {
-        js_nodes_each(parsed, n => js_node_is_type(n, node_type_name), n => la(n));
-    });
+    let filtered = js_nodes_get(parsed, n => js_node_is_type(n, node_type_name));
     return list_first(filtered);
 }
