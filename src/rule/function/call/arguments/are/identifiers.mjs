abe56adde4318bd6fb2716d4a6b5f81e9666c6c0
JaredMathis <jared.mathis@native.consulting>
2023-06-23 14:24:41 -0400

    #286 noop

--- src/rule/function/call/arguments/are/identifiers.mjs
+++ src/rule/function/call/arguments/are/identifiers.mjs
@@ -8,14 +8,18 @@ import { list_all } from '../../../../../list/all.mjs';
 import { js_node_is_identifier } from '../../../../../js/node/is/identifier.mjs';
 import { assert_message } from '../../../../../assert/message.mjs';
 import { error } from '../../../../../error.mjs';
+import { equal } from '../../../../../equal.mjs';
+import { function_name_get } from '../../../../../function/name/get.mjs';
 export async function rule_function_call_arguments_are_identifiers() {
     arguments_assert(arguments, []);
     await function_names_each(args => {
         let {parsed} = args;
         let {file_path} = args;
         js_nodes_each(parsed, js_node_is_call_expression, node => {
-            console.log(node);
-            error()
+            let name = js_call_expression_name_get(node);
+            if (equal(name, function_name_get(arguments_assert))) {
+                return;
+            }
             let args = js_node_property_arguments_get(node);
             assert_message(list_all(args, js_node_is_identifier), file_path);
         });
