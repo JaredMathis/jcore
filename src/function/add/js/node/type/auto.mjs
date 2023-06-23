295e0ad6750a0b69a0c386f5a29e04b282666fc5
JaredMathis <jared.mathis@native.consulting>
2023-06-23 13:28:17 -0400

    #284 function_auto function_add_js_node_type_auto

--- src/function/add/js/node/type/auto.mjs
+++ src/function/add/js/node/type/auto.mjs
@@ -1,7 +1,5 @@
 import { js_suffix_get } from '../../../../../js/suffix/get.mjs';
-import { log } from '../../../../../log.mjs';
 import { js_identifier_combine } from '../../../../../js/identifier/combine.mjs';
-import { js_identifier_prefix_property } from '../../../../../js/identifier/prefix/property.mjs';
 import { js_prefix_node_property } from '../../../../../js/prefix/node/property.mjs';
 import { function_add_property_get } from '../../../property/get.mjs';
 import { functions_new_if_not_exists } from '../../../../../functions/new/if/not/exists.mjs';
@@ -12,6 +10,7 @@ import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs'
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_add_js_node_type_snake } from './snake.mjs';
 import { function_exists } from '../../../../exists.mjs';
+import { js_prefix_node } from '../../../../../js/prefix/node.mjs';
 export async function function_add_js_node_type_auto(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let identifiers = await function_identifiers(function_name);
@@ -22,9 +21,8 @@ export async function function_add_js_node_type_auto(function_name) {
         let property_name_fn_get = js_identifier_combine(prefix, property_name);
         let function_name_new = js_identifier_combine(property_name_fn_get, method);
         if (!await function_exists(function_name_new)) {
-            console.log({prefix,property_name})
             return;
-            await function_add_property_get(prefix, property_name);
+            await function_add_property_get(js_prefix_node(), property_name);
         }
     }
     await functions_new_if_not_exists(js_identifiers_filter_to_node_types, function_add_js_node_type_snake, identifiers);
