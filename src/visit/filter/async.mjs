0f9bbd96452190d4584b20eaaad1640ee715546c
JaredMathis <jared.mathis@native.consulting>
2023-06-23 15:31:08 -0400

    #288 function_asyncify visit_filter

---
+++ src/visit/filter/async.mjs
@@ -0,0 +1,23 @@
+import { metadata } from '../../metadata.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
+import { visit } from '../../visit.mjs';
+import { function_is } from '../../function/is.mjs';
+import { defined_is } from '../../defined/is.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export async function visit_filter_async(root, children_get, filter, lambda) {
+    arguments_assert(arguments, [
+        defined_is,
+        function_is,
+        function_is,
+        function_is
+    ]);
+    visit(root, children_get, lambda_local);
+    function lambda_local(v) {
+        let node = object_property_get(v, 'node');
+        if (filter(node)) {
+            let v_2 = lambda(v);
+            return v_2;
+        }
+    }
+    metadata([]);
+}
