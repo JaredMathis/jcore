f423b9028957286e02ede9f3bb8b6de14654ce4c
JaredMathis <jared.mathis@native.consulting>
2023-06-23 15:27:14 -0400

    #288 function_auto function_auto_no_add

--- src/function/auto/no/add.mjs
+++ src/function/auto/no/add.mjs
@@ -1,17 +1,11 @@
-import { function_map_multiple_references } from '../../map/multiple/references.mjs';
-import { function_add_js_node_type_auto } from '../../add/js/node/type/auto.mjs';
+import { function_auto_no_add_unasyncify } from './add/unasyncify.mjs';
 import { js_identifier_is } from '../../../js/identifier/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
-import { function_auto_no_add_refactors } from './add/refactors.mjs';
 import { function_unasyncify_generic } from '../../unasyncify/generic.mjs';
-import { refactor_import_fix } from '../../../refactor/import/fix.mjs';
 export async function function_auto_no_add(function_name) {
     arguments_assert(arguments, [js_identifier_is]);
-    let refactors = function_auto_no_add_refactors();
-    await function_map_multiple_references(refactors, function_name);
-    await function_add_js_node_type_auto(function_name);
-    await function_map_multiple_references([refactor_import_fix], function_name);
+    await function_auto_no_add_unasyncify(function_name);
     const only_if_needed = true;
     await function_unasyncify_generic(function_name, only_if_needed);
     metadata([]);
---
+++ src/function/auto/no/add/unasyncify.mjs
@@ -0,0 +1,10 @@
+import { refactor_import_fix } from '../../../../refactor/import/fix.mjs';
+import { function_add_js_node_type_auto } from '../../../add/js/node/type/auto.mjs';
+import { function_map_multiple_references } from '../../../map/multiple/references.mjs';
+import { function_auto_no_add_refactors } from './refactors.mjs';
+export async function function_auto_no_add_unasyncify(function_name) {
+    let refactors = function_auto_no_add_refactors();
+    await function_map_multiple_references(refactors, function_name);
+    await function_add_js_node_type_auto(function_name);
+    await function_map_multiple_references([refactor_import_fix], function_name);
+}
