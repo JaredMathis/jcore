dcc8b059033e4d92d1bb4244f60946f32bacb8e8
JaredMathis <jared.mathis@native.consulting>
2023-06-24 12:10:21 -0400

    #288 function_auto rule_constant_numbers_are_function_outputs

--- src/rule/constant/numbers/are/function/outputs.mjs
+++ src/rule/constant/numbers/are/function/outputs.mjs
@@ -2,23 +2,16 @@ import { js_code_call_expression } from '../../../../../js/code/call/expression.
 import { js_visit_nodes_filter_async } from '../../../../../js/visit/nodes/filter/async.mjs';
 import { function_add_return } from '../../../../../function/add/return.mjs';
 import { js_node_property_value_get } from '../../../../../js/node/property/value/get.mjs';
-import { js_node_is_identifier } from '../../../../../js/node/is/identifier.mjs';
-import { list_all } from '../../../../../list/all.mjs';
 import { assert_message } from '../../../../../assert/message.mjs';
-import { js_node_property_arguments_get } from '../../../../../js/node/property/arguments/get.mjs';
-import { equal } from '../../../../../equal.mjs';
-import { list_any } from '../../../../../list/any.mjs';
-import { metadata } from '../../../../../metadata.mjs';
-import { function_name_get } from '../../../../../function/name/get.mjs';
-import { js_call_expression_name_get } from '../../../../../js/call/expression/name/get.mjs';
 import { function_names_each } from '../../../../../function/names/each.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { js_node_is_literal } from '../../../../../js/node/is/literal.mjs';
 import { number_is } from '../../../../../number/is.mjs';
 import { integer_is } from '../../../../../integer/is.mjs';
-import { list_map } from '../../../../../list/map.mjs';
 import { string_to } from '../../../../../string/to.mjs';
 import { function_exists } from '../../../../../function/exists.mjs';
+import { object_replace } from '../../../../../object/replace.mjs';
+import { refactor_import_fix } from '../../../../../refactor/import/fix.mjs';
 export async function rule_constant_numbers_are_function_outputs() {
     arguments_assert(arguments, []);
     await function_names_each(async args => {
@@ -36,22 +29,8 @@ export async function rule_constant_numbers_are_function_outputs() {
                 await function_add_return(function_name_new, string_to(value));
             }
             let ce = js_code_call_expression(function_name_new);
-            let name = js_call_expression_name_get(node);
-            const rule_exceptions = [
-                arguments_assert,
-                metadata
-            ];
-            let rule_exceptions_names = list_map(rule_exceptions, function_name_get);
-            if (list_any(rule_exceptions_names, e => equal(e, name))) {
-                return;
-            }
-            let args = js_node_property_arguments_get(node);
-            for (let a of args) {
-                if (js_node_is_identifier(a)) {
-                    continue;
-                }
-            }
-            assert_message(list_all(args, js_node_is_identifier), file_path);
+            object_replace(node, ce);
+            refactor_import_fix(args);
         });
     });
 }
