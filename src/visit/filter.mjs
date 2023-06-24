cfab84bb084e32774e2a6d51e86c6d718595658a
JaredMathis <jared.mathis@native.consulting>
2023-06-24 10:42:21 -0400

    #288 noop

--- src/visit/filter.mjs
+++ src/visit/filter.mjs
@@ -1,21 +1,21 @@
 import { metadata } from '../metadata.mjs';
 import { object_property_get } from '../object/property/get.mjs';
-import { visit_async } from './async.mjs';
+import { visit } from '../visit.mjs';
 import { function_is } from '../function/is.mjs';
 import { defined_is } from '../defined/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export async function visit_filter(root, children_get, filter, lambda) {
+export function visit_filter(root, children_get, filter, lambda) {
     arguments_assert(arguments, [
         defined_is,
         function_is,
         function_is,
         function_is
     ]);
-    await visit_async(root, children_get, lambda_local);
+    visit(root, children_get, lambda_local);
     async function lambda_local(v) {
         let node = object_property_get(v, 'node');
         if (filter(node)) {
-            let v_2 = await lambda(v);
+            let v_2 = lambda(v);
             return v_2;
         }
     }
