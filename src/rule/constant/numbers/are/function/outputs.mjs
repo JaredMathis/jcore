75e7bea19ec568ac810e95f58daff1f194331320
JaredMathis <jared.mathis@native.consulting>
2023-06-24 12:14:54 -0400

    #288 function_auto rule_constant_numbers_are_function_outputs

--- src/rule/constant/numbers/are/function/outputs.mjs
+++ src/rule/constant/numbers/are/function/outputs.mjs
@@ -1,6 +1,5 @@
 import { log } from '../../../../../log.mjs';
 import { file_js_all_map_args_if_function } from '../../../../../file/js/all/map/args/if/function.mjs';
-import { js_code_call_expression } from '../../../../../js/code/call/expression.mjs';
 import { js_visit_nodes_filter_async } from '../../../../../js/visit/nodes/filter/async.mjs';
 import { function_add_return } from '../../../../../function/add/return.mjs';
 import { js_node_property_value_get } from '../../../../../js/node/property/value/get.mjs';
@@ -13,6 +12,7 @@ import { string_to } from '../../../../../string/to.mjs';
 import { function_exists } from '../../../../../function/exists.mjs';
 import { object_replace } from '../../../../../object/replace.mjs';
 import { refactor_import_fix } from '../../../../../refactor/import/fix.mjs';
+import { js_parse_call_expression } from '../../../../../js/parse/call/expression.mjs';
 export async function rule_constant_numbers_are_function_outputs() {
     arguments_assert(arguments, []);
     await file_js_all_map_args_if_function(async args => {
@@ -31,7 +31,7 @@ export async function rule_constant_numbers_are_function_outputs() {
             if (!await function_exists(function_name_new)) {
                 await function_add_return(function_name_new, string_to(value));
             }
-            let ce = js_code_call_expression(function_name_new);
+            let ce = js_parse_call_expression(function_name_new);
             object_replace(node, ce);
             refactor_import_fix(args);
         });
