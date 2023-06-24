837eaedaa71376175b21a3db88a1d807846a7386
JaredMathis <jared.mathis@native.consulting>
2023-06-24 10:51:00 -0400

    #288 noop

--- src/rule/constant/numbers/are/function/outputs.mjs
+++ src/rule/constant/numbers/are/function/outputs.mjs
@@ -15,6 +15,7 @@ import { js_node_is_literal } from '../../../../../js/node/is/literal.mjs';
 import { number_is } from '../../../../../number/is.mjs';
 import { integer_is } from '../../../../../integer/is.mjs';
 import { list_map } from '../../../../../list/map.mjs';
+import { string_to } from '../../../../../string/to.mjs';
 export async function rule_constant_numbers_are_function_outputs() {
     arguments_assert(arguments, []);
     await function_names_each(args => {
@@ -28,7 +29,7 @@ export async function rule_constant_numbers_are_function_outputs() {
             assert_message(integer_is(value), 'need to handle non-integers maybe');
             assert_message(value >= 0, 'need to handle negatives maybe');
             let function_name_new = `integer_value_${ value }`;
-            await function_add_return(function_name_new, integer_to_string(value));
+            await function_add_return(function_name_new, string_to(value));
             let name = js_call_expression_name_get(node);
             const rule_exceptions = [
                 arguments_assert,
