c700c09869ad5cc29268abd9403a49e48d1230a8
JaredMathis <jared.mathis@native.consulting>
2023-06-24 23:06:05 -0400

    #286 function_auto list_intersection

--- src/list/intersection.mjs
+++ src/list/intersection.mjs
@@ -1,17 +1,21 @@
 import { list_is } from './is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
-import { list_contains } from './contains.mjs';
-import { list_filter } from './filter.mjs';
+import { equal } from '../equal.mjs';
+import { list_adder_unique } from './adder/unique.mjs';
 export function list_intersection(left, right) {
     arguments_assert(arguments, [
         list_is,
         list_is
     ]);
-    let v = list_filter(left, function v_2(l) {
-        let v_3 = list_contains(right, l);
-        return v_3;
+    return list_adder_unique(la => {
+        for (let l of left) {
+            for (let r of right) {
+                if (equal(l, r)) {
+                    la(l);
+                }
+            }
+        }
     });
-    return v;
     metadata([]);
 }
