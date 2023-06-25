c08eccf5a649ea747b4583a3aeb7a07b4e21b73a
JaredMathis <jared.mathis@native.consulting>
2023-06-25 11:57:12 -0400

    #289 function_auto rule_member_expression_none

--- src/rule/member/expression/none.mjs
+++ src/rule/member/expression/none.mjs
@@ -1,13 +1,15 @@
+import { log } from '../../../log.mjs';
 import { js_node_is_member_expression } from '../../../js/node/is/member/expression.mjs';
 import { function_names_each_map } from '../../../function/names/each/map.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { js_nodes_each } from '../../../js/nodes/each.mjs';
-import { noop } from '../../../noop.mjs';
+import { error } from '../../../error.mjs';
 export async function rule_member_expression_none() {
     arguments_assert(arguments, []);
     await function_names_each_map(args => {
         js_nodes_each(args, js_node_is_member_expression, node => {
-            
+            console.log({ node });
+            error();
         });
     });
 }
