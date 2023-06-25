8049f6e83d2b221d53ee5cb24de872358dd927e4
JaredMathis <jared.mathis@native.consulting>
2023-06-25 18:39:10 -0400

    #290 function_auto js_node_property_test_get

--- src/js/node/property/test/get.mjs
+++ src/js/node/property/test/get.mjs
@@ -3,8 +3,9 @@ import { js_node_property_test } from '../test.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
+import { metadata_tests_none } from '../../../../metadata/tests/none.mjs';
 export function js_node_property_test_get(js_node) {
     arguments_assert(arguments, [defined_is]);
     return object_property_get(js_node, js_node_property_test());
-    metadata([]);
+    metadata([metadata_tests_none]);
 }
