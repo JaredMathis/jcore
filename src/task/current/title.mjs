554c3dea9ee20702e2caf3fbf9fdadde7e13014f
JaredMathis <jared.mathis@native.consulting>
2023-06-25 18:44:02 -0400

    #289 function_auto task_current_title

--- src/task/current/title.mjs
+++ src/task/current/title.mjs
@@ -5,6 +5,6 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 export async function task_current_title(title_new) {
     arguments_assert(arguments, [string_is]);
     let task_number_string = await task_id_get_string();
-    let result = await task_title(task_number_string, title_additional);
+    let result = await task_title(task_number_string, title_new);
     return result;
 }
