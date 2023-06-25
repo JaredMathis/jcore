c14eb25f8935f485fbe4ba586ba97fd7b42d25b3
JaredMathis <jared.mathis@native.consulting>
2023-06-25 18:15:18 -0400

    #290 function_auto rule_if_statement_arguments_are_identifiers

--- src/rule/if/statement/arguments/are/identifiers.mjs
+++ src/rule/if/statement/arguments/are/identifiers.mjs
@@ -1,14 +1,14 @@
+import { js_node_property_test_get } from '../../../../../js/node/property/test/get.mjs';
 import { function_names_each_map } from '../../../../../function/names/each/map.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { js_node_is_if_statement } from '../../../../../js/node/is/if/statement.mjs';
-import { error } from '../../../../../error.mjs';
 export async function rule_if_statement_arguments_are_identifiers() {
     arguments_assert(arguments, []);
     await function_names_each_map(args => {
         let {parsed} = args;
         js_nodes_each(parsed, js_node_is_if_statement, node => {
-            error({ node });
+            let test = js_node_property_test_get(node);
         });
     });
 }
