2acd82707a473953f6925e9f664e716b84c9265d
JaredMathis <jared.mathis@native.consulting>
2023-06-25 11:38:15 -0400

    #286 noop

--- src/rule/function/call/arguments/are/identifiers.mjs
+++ src/rule/function/call/arguments/are/identifiers.mjs
@@ -7,7 +7,7 @@ import { function_names_each } from '../../../../../function/names/each.mjs';
 import { list_contains } from '../../../../../list/contains.mjs';
 export async function rule_function_call_arguments_are_identifiers() {
     arguments_assert(arguments, []);
-    let excludes = refactor_functions_arguments_assert_missing_add_excludes(args);
+    let excludes = refactor_functions_arguments_assert_missing_add_excludes();
     await function_names_each(args => {
         refactor_call_arguments_to_assignments(args);
         let function_name = js_mapper_args_to_function_name(args);
