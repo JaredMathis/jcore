c47bb225bbae46d7bfedbf49b0ddab1d10415d75
JaredMathis <jared.mathis@native.consulting>
2023-06-24 23:06:51 -0400

    #286 noop

--- src/rule/constant/numbers/are/function/outputs.mjs
+++ src/rule/constant/numbers/are/function/outputs.mjs
@@ -36,8 +36,12 @@ export async function rule_constant_numbers_are_function_outputs() {
                 }
                 let ces = list_filter(stack, s => js_node_is(s) && js_node_is_call_expression(s));
                 let ces_names = list_map(ces, js_call_expression_name_get);
+                console.log({
+                    ces_names,
+                    excludes,
+                    file_path
+                });
                 if (list_empty_not(list_intersection(ces_names, excludes))) {
-                    console.log({ces_names,file_path})
                     return;
                 }
                 assert_message(integer_is(value), 'need to handle non-integers maybe');
