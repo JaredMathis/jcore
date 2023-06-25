11a98511dcd963eebcbb32b2c9dbf1e84727012c
JaredMathis <jared.mathis@native.consulting>
2023-06-25 18:19:05 -0400

    #290 function_auto rule_if_statement_arguments_are_identifiers

--- src/rule/if/statement/arguments/are/identifiers.mjs
+++ src/rule/if/statement/arguments/are/identifiers.mjs
@@ -1,18 +1,24 @@
+import { js_node_assign_and_replace } from '../../../../../js/node/assign/and/replace.mjs';
+import { js_visit_stack_reversed_to_ancestor_list } from '../../../../../js/visit/stack/reversed/to/ancestor/list.mjs';
+import { list_reversed_get } from '../../../../../list/reversed/get.mjs';
 import { js_node_property_test_get } from '../../../../../js/node/property/test/get.mjs';
 import { function_names_each_map } from '../../../../../function/names/each/map.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { js_node_is_if_statement } from '../../../../../js/node/is/if/statement.mjs';
 import { js_node_is_identifier } from '../../../../../js/node/is/identifier.mjs';
+import { js_visit_nodes_filter } from '../../../../../js/visit/nodes/filter.mjs';
 export async function rule_if_statement_arguments_are_identifiers() {
     arguments_assert(arguments, []);
     await function_names_each_map(args => {
         let {parsed} = args;
-        js_nodes_each(parsed, js_node_is_if_statement, node => {
+        js_visit_nodes_filter(parsed, js_node_is_if_statement, v => {
+            let {node, stack} = v;
             let test = js_node_property_test_get(node);
             if (js_node_is_identifier(test)) {
                 return;
             }
+            let stack_reversed = list_reversed_get(refactor_stack);
+            let r = js_visit_stack_reversed_to_ancestor_list(stack_reversed);
             js_node_assign_and_replace(parsed, expression_to_replace, ancestor_list, add_assignment_before_node);
         });
     });
