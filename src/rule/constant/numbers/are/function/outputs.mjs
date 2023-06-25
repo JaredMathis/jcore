069194445c89107a446a5c94d656651f07171751
JaredMathis <jared.mathis@native.consulting>
2023-06-24 23:12:46 -0400

    #286 function_auto rule_constant_numbers_are_function_outputs

--- src/rule/constant/numbers/are/function/outputs.mjs
+++ src/rule/constant/numbers/are/function/outputs.mjs
@@ -1,3 +1,4 @@
+import { js_mapper_args_to_function_name } from '../../../../../js/mapper/args/to/function/name.mjs';
 import { refactor_import_fix_if_changed } from '../../../../../refactor/import/fix/if/changed.mjs';
 import { list_empty_not } from '../../../../../list/empty/not.mjs';
 import { refactor_functions_arguments_assert_missing_add_excludes } from '../../../../../refactor/functions/arguments/assert/missing/add/excludes.mjs';
@@ -21,14 +22,13 @@ import { list_map } from '../../../../../list/map.mjs';
 import { js_call_expression_name_get } from '../../../../../js/call/expression/name/get.mjs';
 import { list_intersection } from '../../../../../list/intersection.mjs';
 import { js_node_is } from '../../../../../js/node/is.mjs';
-import { js_unparse } from '../../../../../js/unparse.mjs';
 import { error } from '../../../../../error.mjs';
 export async function rule_constant_numbers_are_function_outputs() {
     arguments_assert(arguments, []);
     let excludes = await refactor_functions_arguments_assert_missing_add_excludes();
     await file_js_all_map_args_if_function(async args => {
         let {parsed} = args;
-        let {file_path} = args;
+        let function_name = js_mapper_args_to_function_name(args);
         await refactor_import_fix_if_changed(args, async changed => {
             await js_visit_nodes_filter_async(parsed, js_node_is_literal, async v => {
                 let {node, stack} = v;
@@ -49,10 +49,11 @@ export async function rule_constant_numbers_are_function_outputs() {
                 }
                 let ce = js_parse_call_expression(function_name_new);
                 object_replace(node, ce);
+                console.log({ node });
                 changed();
             });
         });
-        console.log(js_unparse(parsed))
-        error()
+        console.log({ args });
+        error();
     });
 }
