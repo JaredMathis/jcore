2b3a7fb03ee703cc2769265b940c93264df4e7f7
JaredMathis <jared.mathis@native.consulting>
2023-06-25 11:57:43 -0400

    #289 noop

--- src/rule/member/expression/none.mjs
+++ src/rule/member/expression/none.mjs
@@ -7,7 +7,8 @@ import { error } from '../../../error.mjs';
 export async function rule_member_expression_none() {
     arguments_assert(arguments, []);
     await function_names_each_map(args => {
-        js_nodes_each(args, js_node_is_member_expression, node => {
+        let {parsed}=args;
+        js_nodes_each(parsed, js_node_is_member_expression, node => {
             console.log({ node });
             error();
         });
