deb36822a0f73647b1c059a7f38133a5488242df
JaredMathis <jared.mathis@native.consulting>
2023-06-25 18:16:03 -0400

    #290 function_auto rule_if_statement_arguments_are_identifiers

--- src/rule/if/statement/arguments/are/identifiers.mjs
+++ src/rule/if/statement/arguments/are/identifiers.mjs
@@ -3,12 +3,16 @@ import { function_names_each_map } from '../../../../../function/names/each/map.
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { js_nodes_each } from '../../../../../js/nodes/each.mjs';
 import { js_node_is_if_statement } from '../../../../../js/node/is/if/statement.mjs';
+import { js_node_is_identifier } from '../../../../../js/node/is/identifier.mjs';
 export async function rule_if_statement_arguments_are_identifiers() {
     arguments_assert(arguments, []);
     await function_names_each_map(args => {
         let {parsed} = args;
         js_nodes_each(parsed, js_node_is_if_statement, node => {
             let test = js_node_property_test_get(node);
+            if (js_node_is_identifier(test)) {
+                return;
+            }
         });
     });
 }
