08f01d614ab21679bbe2024e7b3305b7c08814ab
JaredMathis <jared.mathis@native.consulting>
2023-06-25 13:13:37 -0400

    #289 noop

--- src/object/property/get.mjs
+++ src/object/property/get.mjs
@@ -3,7 +3,7 @@ import { metadata } from '../../metadata.mjs';
 import { object_property_exists } from './exists.mjs';
 export function object_property_get(object, property_name) {
     let lambda = object_property_exists;
-    let v = object_property_exists(object, property_name);
+    let v = lambda(object, property_name);
     assert(v);
     return object[property_name];
     metadata([]);
