8fa229fe6751b42abeaa3a12b542c83b461b56d3
JaredMathis <jared.mathis@native.consulting>
2023-06-25 19:58:29 -0400

    #289 function_auto refactor_call_arguments_to_assignments_skip

--- src/refactor/call/arguments/to/assignments/skip.mjs
+++ src/refactor/call/arguments/to/assignments/skip.mjs
@@ -1,14 +1,18 @@
+import { js_call_expression_name_get_or_null } from '../../../../../js/call/expression/name/get/or/null.mjs';
 import { equal } from '../../../../../equal.mjs';
 import { list_any } from '../../../../../list/any.mjs';
 import { function_name_get } from '../../../../../function/name/get.mjs';
 import { list_map } from '../../../../../list/map.mjs';
 import { metadata } from '../../../../../metadata.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-import { js_call_expression_name_get } from '../../../../../js/call/expression/name/get.mjs';
 import { js_node_is_call_expression } from '../../../../../js/node/is/call/expression.mjs';
+import { null_is } from '../../../../../null/is.mjs';
 export function refactor_call_arguments_to_assignments_skip(node) {
     arguments_assert(arguments, [js_node_is_call_expression]);
-    let name = js_call_expression_name_get(node);
+    let name = js_call_expression_name_get_or_null(node);
+    if (null_is(name)) {
+        return false;
+    }
     const rule_exceptions = [
         arguments_assert,
         metadata
