8fb233ee9b0971ccde9ce20caecf4acb37e55ccd
JaredMathis <jared.mathis@native.consulting>
2023-06-25 11:37:42 -0400

    #286 function_auto rule_function_call_arguments_are_identifiers

--- src/rule/function/call/arguments/are/identifiers.mjs
+++ src/rule/function/call/arguments/are/identifiers.mjs
@@ -4,12 +4,16 @@ import { refactor_call_arguments_to_assignments } from '../../../../../refactor/
 import { rule_function_call_arguments_are_identifiers_each } from './identifiers/each.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_names_each } from '../../../../../function/names/each.mjs';
+import { list_contains } from '../../../../../list/contains.mjs';
 export async function rule_function_call_arguments_are_identifiers() {
     arguments_assert(arguments, []);
     let excludes = refactor_functions_arguments_assert_missing_add_excludes(args);
     await function_names_each(args => {
         refactor_call_arguments_to_assignments(args);
         let function_name = js_mapper_args_to_function_name(args);
+        if (list_contains(excludes, function_name)) {
+            return;
+        }
         rule_function_call_arguments_are_identifiers_each(args);
     });
 }
