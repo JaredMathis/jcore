f4fbcfbdd6efd7ad104c9d315f1e6b54a42cc869
JaredMathis <jared.mathis@native.consulting>
2023-06-25 16:05:14 -0400

    #289 function_auto rule_function_call_arguments_are_identifiers_each

---
+++ src/refactor/call/arguments/to/assignments/skip.mjs
@@ -0,0 +1,17 @@
+import { equal } from '../../../../../equal.mjs';
+import { list_any } from '../../../../../list/any.mjs';
+import { function_name_get } from '../../../../../function/name/get.mjs';
+import { list_map } from '../../../../../list/map.mjs';
+import { metadata } from '../../../../../metadata.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+import { js_call_expression_name_get } from '../../../../../js/call/expression/name/get.mjs';
+export function refactor_call_arguments_to_assignments_skip(node) {
+    let name = js_call_expression_name_get(node);
+    const rule_exceptions = [
+        arguments_assert,
+        metadata
+    ];
+    let rule_exceptions_names = list_map(rule_exceptions, function_name_get);
+    const skip = list_any(rule_exceptions_names, e => equal(e, name));
+    return skip;
+}
--- src/rule/function/call/arguments/are/identifiers/each.mjs
+++ src/rule/function/call/arguments/are/identifiers/each.mjs
@@ -1,27 +1,18 @@
+import { refactor_call_arguments_to_assignments_skip } from '../../../../../../refactor/call/arguments/to/assignments/skip.mjs';
+import { arguments_assert_todo } from '../../../../../../arguments/assert/todo.mjs';
 import { js_node_is_identifier } from '../../../../../../js/node/is/identifier.mjs';
 import { list_all } from '../../../../../../list/all.mjs';
 import { assert_message } from '../../../../../../assert/message.mjs';
 import { js_node_property_arguments_get } from '../../../../../../js/node/property/arguments/get.mjs';
-import { equal } from '../../../../../../equal.mjs';
-import { list_any } from '../../../../../../list/any.mjs';
-import { function_name_get } from '../../../../../../function/name/get.mjs';
-import { list_map } from '../../../../../../list/map.mjs';
-import { metadata } from '../../../../../../metadata.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
-import { js_call_expression_name_get } from '../../../../../../js/call/expression/name/get.mjs';
 import { js_node_is_call_expression } from '../../../../../../js/node/is/call/expression.mjs';
 import { js_nodes_each } from '../../../../../../js/nodes/each.mjs';
 export function rule_function_call_arguments_are_identifiers_each(args) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let {parsed} = args;
     let {file_path} = args;
     js_nodes_each(parsed, js_node_is_call_expression, node => {
-        let name = js_call_expression_name_get(node);
-        const rule_exceptions = [
-            arguments_assert,
-            metadata
-        ];
-        let rule_exceptions_names = list_map(rule_exceptions, function_name_get);
-        const skip = list_any(rule_exceptions_names, e => equal(e, name));
+        const skip = refactor_call_arguments_to_assignments_skip(node);
         if (skip) {
             return;
         }
