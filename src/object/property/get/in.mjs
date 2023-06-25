caa5af1299d596d99591a86be782c523449207df
JaredMathis <jared.mathis@native.consulting>
2023-06-25 13:16:55 -0400

    #289 function_auto object_property_get_in

--- src/object/property/get/in.mjs
+++ src/object/property/get/in.mjs
@@ -1,6 +1,6 @@
+import { object_property_in } from '../in.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { object_property_get_generic } from './generic.mjs';
-import { object_property_exists } from '../exists.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export function object_property_get_in(object, property_name) {
@@ -8,7 +8,7 @@ export function object_property_get_in(object, property_name) {
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let lambda = object_property_exists;
+    let lambda = object_property_in;
     return object_property_get_generic(lambda, object, property_name);
     metadata([]);
 }
