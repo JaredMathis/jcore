7c915602290e80c28ee5f1871fb011d8f0b34ef3
JaredMathis <jared.mathis@native.consulting>
2023-06-25 16:00:32 -0400

    #289 function_auto rule_member_expression_none

--- src/rule/member/expression/none.mjs
+++ src/rule/member/expression/none.mjs
@@ -1,23 +1,7 @@
+import { refactor_member_expression_none } from '../../../refactor/member/expression/none.mjs';
 import { function_dependencies_names } from '../../../function/dependencies/names.mjs';
-import { js_node_property_computed_get } from '../../../js/node/property/computed/get.mjs';
-import { assert_message } from '../../../assert/message.mjs';
-import { js_code_expression_string } from '../../../js/code/expression/string.mjs';
-import { js_node_property_name_get } from '../../../js/node/property/name/get.mjs';
-import { refactor_import_fix_if_changed } from '../../../refactor/import/fix/if/changed.mjs';
-import { js_node_property_arguments_get } from '../../../js/node/property/arguments/get.mjs';
-import { list_set } from '../../../list/set.mjs';
-import { js_node_property_property_get } from '../../../js/node/property/property/get.mjs';
-import { js_node_property_object_get } from '../../../js/node/property/object/get.mjs';
-import { js_code_call_expression_object_property_get } from '../../../js/code/call/expression/object/property/get.mjs';
-import { js_node_is_member_expression } from '../../../js/node/is/member/expression.mjs';
 import { function_names_each_map } from '../../../function/names/each/map.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { js_nodes_each } from '../../../js/nodes/each.mjs';
-import { string_a } from '../../../string/a.mjs';
-import { js_parse_expression } from '../../../js/parse/expression.mjs';
-import { object_replace } from '../../../object/replace.mjs';
-import { js_node_is_identifier } from '../../../js/node/is/identifier.mjs';
-import { json_to } from '../../../json/to.mjs';
 import { object_property_get } from '../../../object/property/get.mjs';
 import { function_name_get } from '../../../function/name/get.mjs';
 import { js_mapper_args_to_function_name } from '../../../js/mapper/args/to/function/name.mjs';
@@ -30,6 +14,6 @@ export async function rule_member_expression_none() {
         if (list_contains(excludes, function_name)) {
             return;
         }
-        
+        await refactor_member_expression_none(args);
     });
 }
