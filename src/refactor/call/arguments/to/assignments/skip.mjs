58df7f5afcbe9446b96e5caa769e4b4f04934b8e
JaredMathis <jared.mathis@native.consulting>
2023-06-25 19:56:00 -0400

    #289 function_auto refactor_call_arguments_to_assignments_skip

--- src/refactor/call/arguments/to/assignments/skip.mjs
+++ src/refactor/call/arguments/to/assignments/skip.mjs
@@ -1,4 +1,3 @@
-import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { equal } from '../../../../../equal.mjs';
 import { list_any } from '../../../../../list/any.mjs';
 import { function_name_get } from '../../../../../function/name/get.mjs';
@@ -6,8 +5,9 @@ import { list_map } from '../../../../../list/map.mjs';
 import { metadata } from '../../../../../metadata.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { js_call_expression_name_get } from '../../../../../js/call/expression/name/get.mjs';
+import { js_node_is_call_expression } from '../../../../../js/node/is/call/expression.mjs';
 export function refactor_call_arguments_to_assignments_skip(node) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+    arguments_assert(arguments, [js_node_is_call_expression]);
     let name = js_call_expression_name_get(node);
     const rule_exceptions = [
         arguments_assert,
