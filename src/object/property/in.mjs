f6565c4d349caec95c9408b25af11d9c66bd382f
JaredMathis <jared.mathis@native.consulting>
2023-06-25 13:15:46 -0400

    #289 function_copy_suffix_change object_property exists in

---
+++ src/object/property/in.mjs
@@ -0,0 +1,12 @@
+import { metadata } from '../../metadata.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function object_property_in(object, property_name) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    let v = object.hasOwnProperty(property_name);
+    return v;
+    metadata([]);
+}
