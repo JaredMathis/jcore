db6d73e6af76ca41e475d34ea8a923b79f8ef2fe
JaredMathis <jared.mathis@native.consulting>
2023-06-25 18:27:46 -0400

    #290 function_auto rule_if_statement_arguments_are_identifiers

--- src/rule/if/statement/arguments/are/identifiers.mjs
+++ src/rule/if/statement/arguments/are/identifiers.mjs
@@ -1,4 +1,3 @@
-import { log } from '../../../../../log.mjs';
 import { js_node_assign_and_replace } from '../../../../../js/node/assign/and/replace.mjs';
 import { js_visit_stack_reversed_to_ancestor_list } from '../../../../../js/visit/stack/reversed/to/ancestor/list.mjs';
 import { list_reversed_get } from '../../../../../list/reversed/get.mjs';
@@ -10,8 +9,6 @@ import { js_node_is_identifier } from '../../../../../js/node/is/identifier.mjs'
 import { js_visit_nodes_filter } from '../../../../../js/visit/nodes/filter.mjs';
 import { list_index_before } from '../../../../../list/index/before.mjs';
 import { list_get } from '../../../../../list/get.mjs';
-import { js_unparse } from '../../../../../js/unparse.mjs';
-import { error } from '../../../../../error.mjs';
 import { assert } from '../../../../../assert.mjs';
 import { result_property_success_get } from '../../../../../result/property/success/get.mjs';
 import { result_property_data_get } from '../../../../../result/property/data/get.mjs';
@@ -19,7 +16,6 @@ export async function rule_if_statement_arguments_are_identifiers() {
     arguments_assert(arguments, []);
     await function_names_each_map(args => {
         let {parsed, file_path} = args;
-        let changed = false;
         js_visit_nodes_filter(parsed, js_node_is_if_statement, v => {
             let {node, stack} = v;
             let test = js_node_property_test_get(node);
@@ -33,11 +29,6 @@ export async function rule_if_statement_arguments_are_identifiers() {
             let add_assignment_before_node_index = list_index_before(stack_reversed, ancestor_list);
             let add_assignment_before_node = list_get(stack_reversed, add_assignment_before_node_index);
             js_node_assign_and_replace(parsed, test, ancestor_list, add_assignment_before_node);
-            changed = true;
         });
-        if (changed) {
-            console.log(js_unparse(parsed));
-            error(file_path);
-        }
     });
 }
