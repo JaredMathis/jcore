763232478614d90db391997b68730091927679d1
JaredMathis <jared.mathis@native.consulting>
2023-06-26 15:23:39 -0400

    #292 function_auto metadata_rule_operator_to_function_call_none

---
+++ src/metadata/rule/operator/to/function/call/none.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../../../../error.mjs';
+import { arguments_assert } from '../../../../../../arguments/assert.mjs';
+export function metadata_rule_operator_to_function_call_none() {
+    arguments_assert(arguments, []);
+    error('todo: metadata_rule_operator_to_function_call_none');
+}
