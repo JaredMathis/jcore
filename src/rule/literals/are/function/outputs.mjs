100a6316eb732d7de033ce7e7b2363f474018fb2
JaredMathis <jared.mathis@native.consulting>
2023-06-26 17:42:42 -0400

    #289 function_auto rule_literals_are_function_outputs

--- src/rule/literals/are/function/outputs.mjs
+++ src/rule/literals/are/function/outputs.mjs
@@ -3,8 +3,10 @@ import { metadata } from '../../../../metadata.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { number_is } from '../../../../number/is.mjs';
 import { integer_is } from '../../../../integer/is.mjs';
+import { boolean_is } from '../../../../boolean/is.mjs';
 export async function rule_literals_are_function_outputs() {
     arguments_assert(arguments, []);
     await rule_literals_are_function_outputs_generic(`integer`, number_is, integer_is);
+    await rule_literals_are_function_outputs_generic(`boolean`, boolean_is, boolean_is);
     metadata([]);
 }
