3b44f9e8532e431a09c421c14a336344c3e332c4
JaredMathis <jared.mathis@native.consulting>
2023-06-25 21:28:55 -0400

    #291 function_auto rules

--- src/rules.mjs
+++ src/rules.mjs
@@ -1,3 +1,4 @@
+import { rule_equals_operator_to_function_call } from './rule/equals/operator/to/function/call.mjs';
 import { rule_constant_numbers_are_function_outputs } from './rule/constant/numbers/are/function/outputs.mjs';
 import { rule_function_call_arguments_are_identifiers } from './rule/function/call/arguments/are/identifiers.mjs';
 import { rule_tests_pass } from './rule/tests/pass.mjs';
@@ -7,6 +8,7 @@ export async function rules() {
     arguments_assert(arguments, []);
     await rule_function_async_and_suffix_async_implies_suffix_asynk();
     await rule_constant_numbers_are_function_outputs();
+    await rule_equals_operator_to_function_call();
     await rule_function_call_arguments_are_identifiers();
     await rule_tests_pass();
 }
