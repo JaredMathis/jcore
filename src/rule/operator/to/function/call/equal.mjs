596f41ca454b3ab8aea7752c854621574bc0858a
JaredMathis <jared.mathis@native.consulting>
2023-06-26 10:18:38 -0400

    #289 function_auto rule_operator_to_function_call_equal

--- src/rule/operator/to/function/call/equal.mjs
+++ src/rule/operator/to/function/call/equal.mjs
@@ -1,8 +1,9 @@
+import { and } from '../../../../../and.mjs';
 import { rule_operator_to_function_call_generic } from './generic.mjs';
 import { equal } from '../../../../../equal.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
-import { function_name_get } from '../../../../../function/name/get.mjs';
 export async function rule_operator_to_function_call_equal() {
     arguments_assert(arguments, []);
     await rule_operator_to_function_call_generic(equal, '===');
+    await rule_operator_to_function_call_generic(and, '&&');
 }
