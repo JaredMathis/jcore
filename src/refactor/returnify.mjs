7fd05cb0326a94d5682737aded81180f6f7dd13f
JaredMathis <jared.mathis@native.consulting>
2023-06-26 18:42:12 -0400

    #289 function_auto refactor_returnify

--- src/refactor/returnify.mjs
+++ src/refactor/returnify.mjs
@@ -1,12 +1,13 @@
+import { list_single_item } from '../list/single/item.mjs';
 import { refactor_node_assign_and_replace } from './node/assign/and/replace.mjs';
 import { metadata } from '../metadata.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { js_node_is_return_statement } from '../js/node/is/return/statement.mjs';
-import { js_node_assign_and_replace } from '../js/node/assign/and/replace.mjs';
+import { js_node_property_argument_get } from '../js/node/property/argument/get.mjs';
 export function refactor_returnify(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let lambda_child_get = js_node_assign_and_replace;
+    let lambda_child_get = n => list_single_item(js_node_property_argument_get(n));
     let lambda_node_is_type = js_node_is_return_statement;
     refactor_node_assign_and_replace(args, lambda_node_is_type, lambda_child_get);
     metadata([]);
