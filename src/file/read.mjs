c25f6aa6028fc8a0c5c4861447919dfa0b3a74dd
JaredMathis <jared.mathis@native.consulting>
2023-06-26 14:49:05 -0400

    #289 noop

--- src/file/read.mjs
+++ src/file/read.mjs
@@ -5,10 +5,11 @@ import { assert } from '../assert.mjs';
 import { path_exists } from '../path/exists.mjs';
 import fs from 'fs';
 import { assert_message } from '../assert/message.mjs';
+import { string_combine } from '../string/combine.mjs';
 export async function file_read(file_path) {
     arguments_assert(arguments, [path_is]);
     let v = await path_exists(file_path);
-    let v_2 = `Path does not exist: ` + file_path;
+    let v_2 = string_combine(`Path does not exist: `, file_path);
     assert_message(v, v_2);
     let v_3 = 'utf8';
     let result = await fs.promises.readFile(file_path, v_3);
