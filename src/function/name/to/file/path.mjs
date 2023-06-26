06a8a9ff8c9ca9c177065dda6e22c0460a23ecac
JaredMathis <jared.mathis@native.consulting>
2023-06-26 14:49:47 -0400

    #289 noop

--- src/function/name/to/file/path.mjs
+++ src/function/name/to/file/path.mjs
@@ -8,6 +8,7 @@ import { directory_source } from '../../../../directory/source.mjs';
 import { function_extension } from '../../../extension.mjs';
 import { directory_current } from '../../../../directory/current.mjs';
 import { path_join } from '../../../../path/join.mjs';
+import { string_combine } from '../../../../string/combine.mjs';
 export function function_name_to_file_path(function_name) {
     arguments_assert(arguments, [js_identifier_is]);
     let directory_source_result = directory_source();
@@ -16,7 +17,7 @@ export function function_name_to_file_path(function_name) {
     let v = directory_current();
     list_add_beginning(parts, v);
     let v_3 = function v_2(last) {
-        return last + function_extension();
+        return string_combine(last, function_extension());
     };
     list_last_map(parts, v_3);
     let joined = path_join(parts);
