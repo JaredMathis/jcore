01302ca16b3da282a4be26c2ceaa04b7ad33e1d7
JaredMathis <jared.mathis@native.consulting>
2023-06-26 18:39:59 -0400

    #289 function_add_mapper_all object_expression_value_assign

---
+++ src/refactor/functions/object/expression/value/assign.mjs
@@ -0,0 +1,7 @@
+import { refactor_object_expression_value_assign } from '../../../../object/expression/value/assign.mjs';
+import { function_names_each_map_args } from '../../../../../function/names/each/map/args.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export async function refactor_functions_object_expression_value_assign() {
+    arguments_assert(arguments, []);
+    await function_names_each_map_args(refactor_object_expression_value_assign, {});
+}
