891e104ed74b07a0aa2a8bff336ba75d8fb47ef7
JaredMathis <jared.mathis@native.consulting>
2023-06-26 09:54:47 -0400

    #289 function_call_arguments_to_assignments function_input_remove_last_3

--- src/function/input/remove/last/3.mjs
+++ src/function/input/remove/last/3.mjs
@@ -6,7 +6,8 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { range } from '../../../../range.mjs';
 export async function function_input_remove_last_3(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    for (let i of range(integer_value_3())) {
+    let v = integer_value_3();
+    for (let i of range(v)) {
         await function_input_remove_last(function_name);
     }
 }
