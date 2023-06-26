0fc2a8fb225c34df4d5bc19dc633d547066a183a
JaredMathis <jared.mathis@native.consulting>
2023-06-26 18:34:41 -0400

    #289 function_add_mapper_all returnify

---
+++ src/refactor/functions/returnify.mjs
@@ -0,0 +1,7 @@
+import { refactor_returnify } from '../returnify.mjs';
+import { function_names_each_map_args } from '../../function/names/each/map/args.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export async function refactor_functions_returnify() {
+    arguments_assert(arguments, []);
+    await function_names_each_map_args(refactor_returnify, {});
+}
