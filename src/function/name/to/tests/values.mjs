9bb89d2da54f383aa71f47135334180b4a2a880e
JaredMathis <jared.mathis@native.consulting>
2023-06-26 14:50:04 -0400

    #289 noop

--- src/function/name/to/tests/values.mjs
+++ src/function/name/to/tests/values.mjs
@@ -1,6 +1,7 @@
 import { metadata } from '../../../../metadata.mjs';
+import { string_combine } from '../../../../string/combine.mjs';
 export function function_name_to_tests_values(n) {
     let ending = '_tests_values';
-    return n + ending;
+    return string_combine(n, ending);
     metadata([]);
 }
