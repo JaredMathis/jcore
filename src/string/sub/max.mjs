f92931ac4df975ce97d08b54e59784d4c156d9b8
JaredMathis <jared.mathis@native.consulting>
2023-06-26 18:21:00 -0400

    #289 function_auto string_sub_max

--- src/string/sub/max.mjs
+++ src/string/sub/max.mjs
@@ -1,3 +1,6 @@
+import { string_length } from '../length.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { less_than } from '../../less/than.mjs';
 import { integer_value_0 } from '../../integer/value/0.mjs';
@@ -6,13 +9,19 @@ import { string_sub_max_property_left_index } from './max/property/left/index.mj
 import { string_sub_max_property_offset } from './max/property/offset.mjs';
 import { string_sub_max_starting_at } from './max/starting/at.mjs';
 export function string_sub_max(left, right) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let offset_max = integer_value_0();
     let left_index_max = integer_value_0();
     let right_index_max = integer_value_0();
-    let left_index = integer_value_0()
-    while (less_than(left_index, left.length) ) {
-        let right_index = integer_value_0()
-        while (less_than(right_index, right.length) ) {
+    let left_index = integer_value_0();
+    const left_length = string_length(left);
+    const right_length = string_length(right);
+    while (less_than(left_index, left_length)) {
+        let right_index = integer_value_0();
+        while (less_than(right_index, right_length)) {
             let offset = string_sub_max_starting_at(left, right, left_index, right_index);
             let v = offset > offset_max;
             if (v) {
@@ -20,9 +29,9 @@ export function string_sub_max(left, right) {
                 left_index_max = left_index;
                 right_index_max = right_index;
             }
-            right_index++
+            right_index++;
         }
-        left_index++
+        left_index++;
     }
     let result = {
         [string_sub_max_property_offset()]: offset_max,
