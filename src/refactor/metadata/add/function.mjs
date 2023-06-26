ca2eb2e95fbbdbdc9150aebfd2b36e41359a083b
JaredMathis <jared.mathis@native.consulting>
2023-06-26 15:36:47 -0400

    #292 function_auto refactor_metadata_add_function

--- src/refactor/metadata/add/function.mjs
+++ src/refactor/metadata/add/function.mjs
@@ -1,4 +1,4 @@
-import { defined_is } from '../../../defined/is.mjs';
+import { metadata } from '../../../metadata.mjs';
 import { refactor_metadata_add_named } from './named.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { function_name_get } from '../../../function/name/get.mjs';
