6eca02e468e398d57125c284d619f324f0dbff46
JaredMathis <jared.mathis@native.consulting>
2023-06-26 12:48:55 -0400

    #289 function_auto list_remove_until_empty

--- src/list/remove/until/empty.mjs
+++ src/list/remove/until/empty.mjs
@@ -1,11 +1,15 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_empty_not } from '../../empty/not.mjs';
-import { not } from '../../../not.mjs';
 import { list_remove_multiple } from '../multiple.mjs';
 import { assert } from '../../../assert.mjs';
-import { list_empty } from '../../empty.mjs';
 import { comment } from '../../../comment.mjs';
 export function list_remove_until_empty(remaining, removals_get) {
-    while (not(list_empty(remaining))) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
+    while (list_empty_not(remaining)) {
         let removals = removals_get();
         comment(`This ensures no infinte loops - we make progress each time`);
         let v = list_empty_not(removals);
