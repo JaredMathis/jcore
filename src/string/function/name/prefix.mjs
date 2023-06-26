83f448e891d58479bcf8e603c31cb61f11c13218
JaredMathis <jared.mathis@native.consulting>
2023-06-26 15:01:23 -0400

    #289 noop

--- src/string/function/name/prefix.mjs
+++ src/string/function/name/prefix.mjs
@@ -2,8 +2,9 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { function_name_separator } from '../../../function/name/separator.mjs';
 import { js_identifier_is } from '../../../js/identifier/is.mjs';
+import { string_combine } from '../../combine.mjs';
 export function string_function_name_prefix(function_name) {
     arguments_assert(arguments, [js_identifier_is]);
-    return function_name + function_name_separator();
+    return string_combine(function_name, function_name_separator());
     metadata([]);
 }
