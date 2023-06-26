f74d901de031d87c89b14bc6387ed777b01d948d
JaredMathis <jared.mathis@native.consulting>
2023-06-26 18:16:39 -0400

    #289 noop

--- src/string/sub/max/starting/at.mjs
+++ src/string/sub/max/starting/at.mjs
@@ -8,13 +8,14 @@ export function string_sub_max_starting_at(left, right, left_index, right_index)
     let left_walker = left_index;
     let right_walker = right_index;
     let offset= integer_value_0();
-    for ( ; and(less_than(add(left_walker, offset), left.length), less_than(add(right_walker, offset), right.length)); offset++) {
+    for ( ; and(less_than(add(left_walker, offset), left.length), less_than(add(right_walker, offset), right.length)); ) {
         let left_offset = left[add(left_walker, offset)];
         let right_offset = right[add(right_walker, offset)];
         let v = equal_not(left_offset, right_offset);
         if (v) {
             break;
         }
+        offset++
     }
     return offset;
     metadata([]);
