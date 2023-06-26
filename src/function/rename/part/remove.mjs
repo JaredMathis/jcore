5cc1d01031e70fc1d1104fde66a9e9c113fab367
JaredMathis <jared.mathis@native.consulting>
2023-06-26 17:41:29 -0400

    #289 function_auto function_rename_part_remove

--- src/function/rename/part/remove.mjs
+++ src/function/rename/part/remove.mjs
@@ -1,10 +1,10 @@
 import { metadata } from '../../../metadata.mjs';
 import { function_rename_part_generic } from './generic.mjs';
+import { js_identifier_part_is } from '../../../js/identifier/part/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_remove } from '../../../list/remove.mjs';
-import { js_identifier_is } from '../../../js/identifier/is.mjs';
 export async function function_rename_part_remove(part) {
-    arguments_assert(arguments, [js_identifier_is]);
+    arguments_assert(arguments, [js_identifier_part_is]);
     await function_rename_part_generic(part, parts_map);
     function parts_map(parts) {
         list_remove(parts, part);
