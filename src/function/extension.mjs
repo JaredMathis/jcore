42c52d8db6adefa98681c6b8d8dd25589d2bf45a
JaredMathis <jared.mathis@native.consulting>
2023-06-26 14:49:29 -0400

    #289 function_auto function_extension

--- src/function/extension.mjs
+++ src/function/extension.mjs
@@ -1,8 +1,9 @@
 import { arguments_assert } from '../arguments/assert.mjs';
 import { metadata } from '../metadata.mjs';
 import { file_extension_separator } from '../file/extension/separator.mjs';
+import { string_combine } from '../string/combine.mjs';
 export function function_extension() {
     arguments_assert(arguments, []);
-    return file_extension_separator() + 'mjs';
+    return string_combine(file_extension_separator(), 'mjs');
     metadata([]);
 }
