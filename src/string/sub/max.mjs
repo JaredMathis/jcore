09fc8781c5fb66f73db824afe3e8a6fe262eb90a
JaredMathis <jared.mathis@native.consulting>
2023-06-26 18:19:02 -0400

    #289 noop

--- src/string/sub/max.mjs
+++ src/string/sub/max.mjs
@@ -11,7 +11,8 @@ export function string_sub_max(left, right) {
     let right_index_max = integer_value_0();
     let left_index = integer_value_0()
     for (; less_than(left_index, left.length); left_index++) {
-        for (let right_index = integer_value_0(); less_than(right_index, right.length); right_index++) {
+        let right_index = integer_value_0()
+        for (; less_than(right_index, right.length); right_index++) {
             let offset = string_sub_max_starting_at(left, right, left_index, right_index);
             let v = offset > offset_max;
             if (v) {
