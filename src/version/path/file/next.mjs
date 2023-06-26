ed776d235b0cda2a9308d594dc88da370e2b22a8
JaredMathis <jared.mathis@native.consulting>
2023-06-26 15:04:40 -0400

    #289 function_auto version_path_file_next

--- src/version/path/file/next.mjs
+++ src/version/path/file/next.mjs
@@ -1,3 +1,6 @@
+import { string_combine } from '../../../string/combine.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { version_path_file_get } from './get.mjs';
 import { add_1 } from '../../../add/1.mjs';
 import { list_max_or_0 } from '../../../list/max/or/0.mjs';
@@ -11,13 +14,17 @@ import { directory_read } from '../../../directory/read.mjs';
 import { path_exists } from '../../../path/exists.mjs';
 import { version_path_file_directory } from './directory.mjs';
 export async function version_path_file_next(repository_name, file_path) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let repository_file_directory_path = version_path_file_directory(repository_name, file_path);
     let mapped;
     let v_3 = await path_exists(repository_file_directory_path);
     if (v_3) {
         let paths = await directory_read(repository_file_directory_path);
         mapped = list_map(paths, function v_2(file_path) {
-            let without_prefix = string_prefix_without(file_path, repository_file_directory_path + directory_separator());
+            let without_prefix = string_prefix_without(file_path, string_combine(repository_file_directory_path, directory_separator()));
             let v = file_extension_json();
             let without_suffix = string_suffix_without(without_prefix, v);
             let version = integer_parse(without_suffix);
