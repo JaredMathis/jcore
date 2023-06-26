df5defdd63d9a21a151e96c3556141de1d484c18
JaredMathis <jared.mathis@native.consulting>
2023-06-25 21:12:10 -0400

    #291 function_auto rule_equals_operator_to_function_call

--- src/rule/equals/operator/to/function/call.mjs
+++ src/rule/equals/operator/to/function/call.mjs
@@ -1,3 +1,4 @@
+import { js_node_is_binary_expression } from '../../../../../js/node/is/binary/expression.mjs';
 import { object_replace } from '../../../../../object/replace.mjs';
 import { js_parse_call_expression } from '../../../../../js/parse/call/expression.mjs';
 import { string_to } from '../../../../../string/to.mjs';
@@ -7,21 +8,16 @@ import { integer_is } from '../../../../../integer/is.mjs';
 import { assert_message } from '../../../../../assert/message.mjs';
 import { number_is } from '../../../../../number/is.mjs';
 import { js_node_property_value_get } from '../../../../../js/node/property/value/get.mjs';
-import { js_node_is_literal } from '../../../../../js/node/is/literal.mjs';
 import { js_visit_nodes_filter_async } from '../../../../../js/visit/nodes/filter/async.mjs';
 import { refactor_import_fix_if_changed } from '../../../../../refactor/import/fix/if/changed.mjs';
-import { list_contains } from '../../../../../list/contains.mjs';
-import { string_starts_with } from '../../../../../string/starts/with.mjs';
-import { js_mapper_args_to_function_name } from '../../../../../js/mapper/args/to/function/name.mjs';
 import { function_names_each_map } from '../../../../../function/names/each/map.mjs';
-import { refactor_functions_arguments_assert_missing_add_excludes } from '../../../../../refactor/functions/arguments/assert/missing/add/excludes.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export async function rule_equals_operator_to_function_call() {
     arguments_assert(arguments, []);
     await function_names_each_map(async args => {
         let {parsed} = args;
         await refactor_import_fix_if_changed(args, async changed => {
-            await js_visit_nodes_filter_async(parsed, js_node_is_literal, async v => {
+            await js_visit_nodes_filter_async(parsed, js_node_is_binary_expression, async v => {
                 let {node} = v;
                 let value = js_node_property_value_get(node);
                 let v_4 = !number_is(value);
