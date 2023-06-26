9f51f0c9c8e7b3bf9dae11369ca1e3240e241c21
JaredMathis <jared.mathis@native.consulting>
2023-06-26 15:03:21 -0400

    #289 noop

--- src/task/description/to/json/if/unparsable/bugs.mjs
+++ src/task/description/to/json/if/unparsable/bugs.mjs
@@ -2,6 +2,7 @@ import { integer_value_76 } from '../../../../../../integer/value/76.mjs';
 import { integer_value_62 } from '../../../../../../integer/value/62.mjs';
 import { task_description_to_json_if_unparsable_generic } from './generic.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
+import { string_combine_multiple } from '../../../../../../string/combine/multiple.mjs';
 export async function task_description_to_json_if_unparsable_bugs() {
     arguments_assert(arguments, []);
     let bugs = [
@@ -9,7 +10,7 @@ export async function task_description_to_json_if_unparsable_bugs() {
             number: integer_value_62(),
             title: 'Maybe enforce naming convention of _async',
             state: 'open',
-            body: 'If file ends in _async\r\n' + 'There must be a non-async version?\r\n' + '\r\n' + ' node run.mjs git function_unasyncify \r\n' + '\r\n' + 'Therefore  function_unasyncify  can safely remove _async suffix'
+            body:  string_combine_multiple(['If file ends in _async\r\n', 'There must be a non-async version?\r\n', '\r\n', ' node run.mjs git function_unasyncify \r\n', '\r\n', 'Therefore  function_unasyncify  can safely remove _async suffix' ])
         },
         {
             number: integer_value_76(),
