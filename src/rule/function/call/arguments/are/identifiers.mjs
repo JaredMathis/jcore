11ddc3d7becc3c5b22d543f159f73d13f26a53e0
JaredMathis <jared.mathis@native.consulting>
2023-06-26 18:13:41 -0400

    #289 function_auto rule_function_call_arguments_are_identifiers

--- src/rule/function/call/arguments/are/identifiers.mjs
+++ src/rule/function/call/arguments/are/identifiers.mjs
@@ -1,3 +1,4 @@
+import { boolean_is } from '../../../../../boolean/is.mjs';
 import { metadata } from '../../../../../metadata.mjs';
 import { function_names_each_map } from '../../../../../function/names/each/map.mjs';
 import { js_mapper_args_to_function_name } from '../../../../../js/mapper/args/to/function/name.mjs';
@@ -6,6 +7,7 @@ import { refactor_call_arguments_to_assignments } from '../../../../../refactor/
 import { rule_function_call_arguments_are_identifiers_each } from './identifiers/each.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { list_contains } from '../../../../../list/contains.mjs';
+import { function_name_get } from '../../../../../function/name/get.mjs';
 export async function rule_function_call_arguments_are_identifiers() {
     arguments_assert(arguments, []);
     let excludes = await refactor_functions_arguments_assert_missing_add_excludes();
@@ -13,6 +15,10 @@ export async function rule_function_call_arguments_are_identifiers() {
         let function_name = js_mapper_args_to_function_name(args);
         let v = list_contains(excludes, function_name);
         if (v) {
+            return;
+        }
+        let v_2 = list_contains([function_name_get(boolean_is)], function_name);
+        if (v_2) {
             return;
         }
         refactor_call_arguments_to_assignments(args);
