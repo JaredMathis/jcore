63b93c7c04cc17e60cd4f1f8855e73bde6fa7be5
JaredMathis <jared.mathis@native.consulting>
2023-06-26 12:46:02 -0400

    #289 function_auto rule_operator_to_function_call_multiple

--- src/rule/operator/to/function/call/multiple.mjs
+++ src/rule/operator/to/function/call/multiple.mjs
@@ -5,9 +5,11 @@ import { and } from '../../../../../and.mjs';
 import { rule_operator_to_function_call_generic } from './generic.mjs';
 import { equal } from '../../../../../equal.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
+import { equal_not } from '../../../../../equal/not.mjs';
 export async function rule_operator_to_function_call_multiple() {
     arguments_assert(arguments, []);
     await rule_operator_to_function_call_generic(equal, '===', js_node_is_binary_expression);
+    await rule_operator_to_function_call_generic(equal_not, '!==', js_node_is_binary_expression);
     await rule_operator_to_function_call_generic(and, '&&', js_node_is_logical_expression);
     await rule_operator_to_function_call_generic(or, '||', js_node_is_logical_expression);
 }
