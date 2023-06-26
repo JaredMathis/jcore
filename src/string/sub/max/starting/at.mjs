d63ec5835363359f5ced36f859eec53e2d0ab8fd
JaredMathis <jared.mathis@native.consulting>
2023-06-26 18:16:27 -0400

    #289 noop

--- src/string/sub/max/starting/at.mjs
+++ src/string/sub/max/starting/at.mjs
@@ -7,8 +7,8 @@ import { integer_value_0 } from '../../../../integer/value/0.mjs';
 export function string_sub_max_starting_at(left, right, left_index, right_index) {
     let left_walker = left_index;
     let right_walker = right_index;
-    let offset;
-    for (offset = integer_value_0(); and(less_than(add(left_walker, offset), left.length), less_than(add(right_walker, offset), right.length)); offset++) {
+    let offset= integer_value_0();
+    for ( ; and(less_than(add(left_walker, offset), left.length), less_than(add(right_walker, offset), right.length)); offset++) {
         let left_offset = left[add(left_walker, offset)];
         let right_offset = right[add(right_walker, offset)];
         let v = equal_not(left_offset, right_offset);
