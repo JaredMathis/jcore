3107867b73f7610c6ea8ef98562f387da716f66f
JaredMathis <jared.mathis@native.consulting>
2023-06-26 15:40:20 -0400

    #292 tests_generate

--- src/tests.mjs
+++ src/tests.mjs
@@ -1,3 +1,4 @@
+import { metadata } from './metadata.mjs';
 import { string_combine } from './string/combine.mjs';
 import { log } from './log.mjs';
 import { string_underscore_is_test_generated_5 } from './string/underscore/is/test/generated/5.mjs';
@@ -826,4 +827,5 @@ export async function tests() {
     await string_underscore_is_test_generated_4();
     await string_underscore_is_test_generated_5();
     log(string_combine('411', ' tests ran successfully'));
+    metadata([]);
 }
