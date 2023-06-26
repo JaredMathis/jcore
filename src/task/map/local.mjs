dd2108b1795d8f89db3fbae0b86fb31de648d682
JaredMathis <jared.mathis@native.consulting>
2023-06-26 15:46:00 -0400

    #292 function_auto task_map_local

--- src/task/map/local.mjs
+++ src/task/map/local.mjs
@@ -1,8 +1,15 @@
+import { metadata } from '../../metadata.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_find_property } from '../../list/find/property.mjs';
 import { task_property_number } from '../property/number.mjs';
 import { tasks_all_map } from '../../tasks/all/map.mjs';
 import { integer_parse } from '../../integer/parse.mjs';
 export async function task_map_local(task_number_string, map) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let task_number = integer_parse(task_number_string);
     await tasks_all_map(function map_all(tasks_all) {
         let v = task_property_number();
