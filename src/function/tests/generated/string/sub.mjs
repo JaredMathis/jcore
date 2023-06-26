f7516b18ada4d6544ec898f39a574586546cecdc
JaredMathis <jared.mathis@native.consulting>
2023-06-26 14:57:02 -0400

    #289 function_auto function_tests_generated_string_sub

--- src/function/tests/generated/string/sub.mjs
+++ src/function/tests/generated/string/sub.mjs
@@ -1,8 +1,15 @@
+import { string_combine_multiple } from '../../../../string/combine/multiple.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { metadata } from '../../../../metadata.mjs';
 import { function_name_separator } from '../../../name/separator.mjs';
 import { function_tests_generated_string } from '../string.mjs';
 import { string_function_tests_sub } from '../../../../string/function/tests/sub.mjs';
 export function function_tests_generated_string_sub() {
-    return string_function_tests_sub() + function_tests_generated_string() + function_name_separator();
+    arguments_assert(arguments, []);
+    return string_combine_multiple([
+        string_function_tests_sub(),
+        function_tests_generated_string(),
+        function_name_separator()
+    ]);
     metadata([]);
 }
