8ffffb91b3f494b1d2c0b42cbf6491a5884a1c3d
JaredMathis <jared.mathis@native.consulting>
2023-06-26 15:01:38 -0400

    #289 noop

--- src/string/prefix/replace.mjs
+++ src/string/prefix/replace.mjs
@@ -2,6 +2,7 @@ import { string_is } from '../is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { string_prefix_without } from './without.mjs';
+import { string_combine } from '../combine.mjs';
 export function string_prefix_replace(input, prefix_old, prefix_new) {
     arguments_assert(arguments, [
         string_is,
@@ -9,6 +10,6 @@ export function string_prefix_replace(input, prefix_old, prefix_new) {
         string_is
     ]);
     let prefix_without = string_prefix_without(input, prefix_old);
-    return prefix_new + prefix_without;
+    return string_combine(prefix_new, prefix_without);
     metadata([]);
 }
