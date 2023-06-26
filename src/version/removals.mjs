2b5b723ef570654f65577573db0b354e39fc54c6
JaredMathis <jared.mathis@native.consulting>
2023-06-26 15:03:57 -0400

    #289 noop

--- src/version/removals.mjs
+++ src/version/removals.mjs
@@ -5,6 +5,7 @@ import { list_without_multiple } from '../list/without/multiple.mjs';
 import { directory_current_with_separator } from '../directory/current/with/separator.mjs';
 import { list_map } from '../list/map.mjs';
 import { git_ignore_filter } from '../git/ignore/filter.mjs';
+import { string_combine } from '../string/combine.mjs';
 export async function version_removals(repository_name, file_paths) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -13,7 +14,7 @@ export async function version_removals(repository_name, file_paths) {
     let files_current_filtered = await git_ignore_filter(file_paths);
     let files_committed = await version_files_paths(repository_name);
     let mapped2 = list_map(files_current_filtered, function v(f) {
-        return directory_current_with_separator() + f;
+        return string_combine(directory_current_with_separator(), f);
     });
     let removals = list_without_multiple(files_committed, mapped2);
     return removals;
