4d01461ed2b1e84185ff492c20d42e347e569eed
JaredMathis <jared.mathis@native.consulting>
2023-06-26 10:18:12 -0400

    #289 noop

--- src/and.mjs
+++ src/and.mjs
@@ -3,6 +3,7 @@ import { defined_is } from './defined/is.mjs';
 import { error } from './error.mjs';
 import { arguments_assert_todo } from './arguments/assert/todo.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
+import { boolean_is } from './boolean/is.mjs';
 export function and(left, right) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -11,12 +12,12 @@ export function and(left, right) {
     if (arguments.length !== 2) {
         error();
     }
-    if (!defined_is(left)) {
+    if (!boolean_is(left)) {
         error();
     }
-    if (!defined_is(right)) {
+    if (!boolean_is(right)) {
         error();
     }
-    return left === right;
+    return left && right;
     metadata([]);
 }
