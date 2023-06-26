8a3850407a3beee05d9a8619360d76946d93e074
JaredMathis <jared.mathis@native.consulting>
2023-06-26 15:45:37 -0400

    #292 function_auto tasks_all_map

--- src/tasks/all/map.mjs
+++ src/tasks/all/map.mjs
@@ -1,3 +1,4 @@
+import { metadata } from '../../metadata.mjs';
 import { version_repository_default } from '../../version/repository/default.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
