ed3f3b800f251e28c49eec11ada9700a788d0011
JaredMathis <jared.mathis@native.consulting>
2023-06-26 13:05:00 -0400

    #289 function_auto metadata_function_call_arguments_are_identifiers_none

---
+++ src/metadata/function/call/arguments/are/identifiers/none.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../../../../error.mjs';
+import { arguments_assert } from '../../../../../../arguments/assert.mjs';
+export function metadata_function_call_arguments_are_identifiers_none() {
+    arguments_assert(arguments, []);
+    error('todo: metadata_function_call_arguments_are_identifiers_none');
+}
