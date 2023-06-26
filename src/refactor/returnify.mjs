efcba3a742604e6060eb3526d302cba2edb700dc
JaredMathis <jared.mathis@native.consulting>
2023-06-26 18:42:27 -0400

    #289 noop

--- src/refactor/returnify.mjs
+++ src/refactor/returnify.mjs
@@ -7,8 +7,8 @@ import { js_node_is_return_statement } from '../js/node/is/return/statement.mjs'
 import { js_node_property_argument_get } from '../js/node/property/argument/get.mjs';
 export function refactor_returnify(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let lambda_child_get = n => list_single_item(js_node_property_argument_get(n));
+    let lambda_children_get = n => list_single_item(js_node_property_argument_get(n));
     let lambda_node_is_type = js_node_is_return_statement;
-    refactor_node_assign_and_replace(args, lambda_node_is_type, lambda_child_get);
+    refactor_node_assign_and_replace(args, lambda_node_is_type, lambda_children_get);
     metadata([]);
 }
