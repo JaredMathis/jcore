5d85eb849456850801743061f7d88a507227c306
JaredMathis <jared.mathis@native.consulting>
2023-06-26 17:42:12 -0400

    #289 function_rename rule_literals_are_function_outputs_integer rule_literals_are_function_outputs

---
+++ src/rule/literals/are/function/outputs.mjs
@@ -0,0 +1,10 @@
+import { rule_literals_are_function_outputs_generic } from './outputs/generic.mjs';
+import { metadata } from '../../../../metadata.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { number_is } from '../../../../number/is.mjs';
+import { integer_is } from '../../../../integer/is.mjs';
+export async function rule_literals_are_function_outputs() {
+    arguments_assert(arguments, []);
+    await rule_literals_are_function_outputs_generic(`integer`, number_is, integer_is);
+    metadata([]);
+}
