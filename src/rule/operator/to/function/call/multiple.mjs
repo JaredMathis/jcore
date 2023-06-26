d013e9a33db791d6c9c52ff920bcaf4e08750cca
JaredMathis <jared.mathis@native.consulting>
2023-06-26 10:26:11 -0400

    #289 function_auto rule_operator_to_function_call_multiple

--- src/rule/operator/to/function/call/multiple.mjs
+++ src/rule/operator/to/function/call/multiple.mjs
@@ -1,10 +1,10 @@
-import { error } from '../../../../../error.mjs';
+import { js_node_is_binary_expression } from '../../../../../js/node/is/binary/expression.mjs';
 import { and } from '../../../../../and.mjs';
 import { rule_operator_to_function_call_generic } from './generic.mjs';
 import { equal } from '../../../../../equal.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export async function rule_operator_to_function_call_multiple() {
     arguments_assert(arguments, []);
-    await rule_operator_to_function_call_generic(equal, '===', error());
-    await rule_operator_to_function_call_generic(and, '&&', error());
+    await rule_operator_to_function_call_generic(equal, '===', js_node_is_binary_expression);
+    await rule_operator_to_function_call_generic(and, '&&', js_node_is_binary_expression);
 }
