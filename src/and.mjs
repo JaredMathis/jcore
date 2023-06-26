4432366efdbb558b0922b4d5e9ef9f8ff89c543c
JaredMathis <jared.mathis@native.consulting>
2023-06-26 12:30:56 -0400

    #289 function_auto and

--- src/and.mjs
+++ src/and.mjs
@@ -1,23 +1,11 @@
 import { metadata } from './metadata.mjs';
-import { defined_is } from './defined/is.mjs';
-import { error } from './error.mjs';
-import { arguments_assert_todo } from './arguments/assert/todo.mjs';
 import { arguments_assert } from './arguments/assert.mjs';
 import { boolean_is } from './boolean/is.mjs';
 export function and(left, right) {
     arguments_assert(arguments, [
-        arguments_assert_todo,
-        arguments_assert_todo
+        boolean_is,
+        boolean_is
     ]);
-    if (arguments.length !== 2) {
-        error();
-    }
-    if (!boolean_is(left)) {
-        error();
-    }
-    if (!boolean_is(right)) {
-        error();
-    }
     return left && right;
     metadata([]);
 }
