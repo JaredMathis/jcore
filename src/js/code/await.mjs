f114d2d0591bb794563dc4d44637259a1673b65c
JaredMathis <jared.mathis@native.consulting>
2023-06-27 12:13:08 -0400

    #121 function_auto js_code_await

--- src/js/code/await.mjs
+++ src/js/code/await.mjs
@@ -1,5 +1,9 @@
+import { js_keyword_await } from '../keyword/await.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 export function js_code_await(awaited) {
-    return `await ${ awaited }`;
+    arguments_assert(arguments, [arguments_assert_todo]);
+    return `${ js_keyword_await() } ${ awaited }`;
     metadata([]);
 }
