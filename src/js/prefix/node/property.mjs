cfde3dca89ac123813010e57d1fdc4f83787294d
JaredMathis <jared.mathis@native.consulting>
2023-06-27 15:56:09 -0400

    #297 function_auto js_prefix_node_property

--- src/js/prefix/node/property.mjs
+++ src/js/prefix/node/property.mjs
@@ -5,7 +5,6 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 export function js_prefix_node_property() {
     arguments_assert(arguments, []);
     let v = js_prefix_node();
-    return `${ v }_property`;
-    js_identifier_prefix_property;
+    return js_identifier_prefix_property(v);
     metadata([]);
 }
