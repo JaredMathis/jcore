1bd7bfacfe752e459edac21703d4927a9bfcf00c
JaredMathis <jared.mathis@native.consulting>
2023-06-27 12:21:09 -0400

    #121 function_awaitify refactor_awaitify

--- src/refactor/awaitify.mjs
+++ src/refactor/awaitify.mjs
@@ -50,7 +50,7 @@ export async function refactor_awaitify(args) {
         js_function_declaration_asyncify_try(ancestor_function_declaration);
         js_expression_awaitify(node);
     });
-    function test() {
-        js_visit_nodes_filter_async();
+    async function test() {
+        await js_visit_nodes_filter_async();
     }
 }
