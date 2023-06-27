3076d43b5330efdd5fcef5421588940d14b11c21
JaredMathis <jared.mathis@native.consulting>
2023-06-26 20:20:21 -0400

    #289 function_auto refactor_object_expression_value_assign

--- src/refactor/object/expression/value/assign.mjs
+++ src/refactor/object/expression/value/assign.mjs
@@ -6,11 +6,14 @@ import { refactor_node_assign_and_replace } from '../../../node/assign/and/repla
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { list_map } from '../../../../list/map.mjs';
+import { list_filter } from '../../../../list/filter.mjs';
+import { js_node_is_identifier } from '../../../../js/node/is/identifier.mjs';
 export function refactor_object_expression_value_assign(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let lambda_children_get = n => {
         let properties = js_node_property_properties_get(n);
-        return list_map(properties, js_node_property_value_get);
+        let mapped = list_map(properties, js_node_property_value_get);
+        return list_filter(mapped, n => !js_node_is_identifier(n));
     };
     let lambda_node_is_type = js_node_is_object_expression;
     refactor_node_assign_and_replace(args, lambda_node_is_type, lambda_children_get);
