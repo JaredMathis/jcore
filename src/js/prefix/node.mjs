d4efcd3a5d222cdbe81d428c18f3f2554105b466
JaredMathis <jared.mathis@native.consulting>
2023-06-27 15:57:33 -0400

    #297 function_auto js_prefix_node

--- src/js/prefix/node.mjs
+++ src/js/prefix/node.mjs
@@ -3,7 +3,6 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 export function js_prefix_node() {
     arguments_assert(arguments, []);
-    return `js_node`;
-    js_identifier_combine;
+    return js_identifier_combine('js', 'node');
     metadata([]);
 }
