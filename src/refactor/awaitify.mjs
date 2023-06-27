9751233b45e9c680e1fa990c3e5fc6288b642bda
JaredMathis <jared.mathis@native.consulting>
2023-06-27 11:58:34 -0400

    #121 function_auto refactor_awaitify

--- src/refactor/awaitify.mjs
+++ src/refactor/awaitify.mjs
@@ -1,11 +1,11 @@
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-import { js_visit_nodes_filter } from '../js/visit/nodes/filter.mjs';
 import { js_node_is_call_expression } from '../js/node/is/call/expression.mjs';
-export function refactor_awaitify(args) {
+import { js_visit_nodes_filter_async } from '../js/visit/nodes/filter/async.mjs';
+export async function refactor_awaitify(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {parsed} = args;
-    js_visit_nodes_filter(parsed, js_node_is_call_expression, v => {
+    await js_visit_nodes_filter_async(parsed, js_node_is_call_expression, async v => {
         let {node, stack, parent} = v;
     });
 }
