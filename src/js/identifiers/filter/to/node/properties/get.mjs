0f9b48be6b71df035be1f884b512b25787c953b2
JaredMathis <jared.mathis@native.consulting>
2023-06-27 16:07:04 -0400

    #297 function_search list_to_dictionary

--- src/js/identifiers/filter/to/node/properties/get.mjs
+++ src/js/identifiers/filter/to/node/properties/get.mjs
@@ -6,18 +6,19 @@ import { js_prefix_node_property } from '../../../../../prefix/node/property.mjs
 import { js_identifiers_prefixes_without } from '../../../../prefixes/without.mjs';
 import { list_is } from '../../../../../../list/is.mjs';
 import { arguments_assert } from '../../../../../../arguments/assert.mjs';
+import { list_to_dictionary } from '../../../../../../list/to/dictionary.mjs';
 export function js_identifiers_filter_to_node_properties_get(identifier_names) {
     arguments_assert(arguments, [list_is]);
     const prefixes = [
         js_prefix_node_property(),
         js_prefix_mapper_args_property()
     ];
-    for (let prefix of prefixes) {
+    return list_to_dictionary(prefixes, function v_3(prefix) {
         let filtered = js_identifiers_prefixes_without([prefix], identifier_names);
         let suffix = js_suffix_get();
         let v = [suffix];
         let v_2 = js_identifiers_suffixes_without(v, filtered);
         return v_2;
-    }
+    });
     metadata([]);
 }
