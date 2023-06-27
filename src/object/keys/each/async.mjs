94c4d7f8c0ae60bb4b654bf51b7a5879968547c6
JaredMathis <jared.mathis@native.consulting>
2023-06-27 16:10:28 -0400

    #297 function_asyncify object_keys_each

---
+++ src/object/keys/each/async.mjs
@@ -0,0 +1,17 @@
+import { metadata } from '../../../metadata.mjs';
+import { object_property_get } from '../../property/get.mjs';
+import { object_keys } from '../../keys.mjs';
+import { function_is } from '../../../function/is.mjs';
+import { defined_is } from '../../../defined/is.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export async function object_keys_each_async(object, lambda_value_key) {
+    arguments_assert(arguments, [
+        defined_is,
+        function_is
+    ]);
+    for (let key of object_keys(object)) {
+        let value = object_property_get(object, key);
+        lambda_value_key(value, key);
+    }
+    metadata([]);
+}
