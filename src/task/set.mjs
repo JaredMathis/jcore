7fcf03d75db3e0a4486da20bdec38c750ea3a9a7
JaredMathis <jared.mathis@native.consulting>
2023-06-26 20:30:54 -0400

    #121 noop

--- src/task/set.mjs
+++ src/task/set.mjs
@@ -4,10 +4,12 @@ import { string_is } from '../string/is.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { task_id_set } from './id/set.mjs';
 import { string_combine } from '../string/combine.mjs';
+import { task_current } from './current.mjs';
 export async function task_set(task_id_unhashed) {
     arguments_assert(arguments, [string_is]);
     let v = task_symbol_hash();
     let hashed = string_combine(v, task_id_unhashed);
     await task_id_set(hashed);
+    return await task_current();
     metadata([]);
 }
