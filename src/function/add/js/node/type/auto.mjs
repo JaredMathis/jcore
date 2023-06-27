61023aa08325a567e34c9aa1b696091a8b447d54
JaredMathis <jared.mathis@native.consulting>
2023-06-27 16:15:47 -0400

    #297 function_auto function_add_js_node_type_auto

--- src/function/add/js/node/type/auto.mjs
+++ src/function/add/js/node/type/auto.mjs
@@ -11,13 +11,14 @@ import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs'
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { function_add_js_node_type_snake } from './snake.mjs';
 import { function_exists } from '../../../../exists.mjs';
-import { js_prefix_node } from '../../../../../js/prefix/node.mjs';
+import { js_identifier_parts_to } from '../../../../../js/identifier/parts/to.mjs';
+import { list_remove_last_verify } from '../../../../../list/remove/last/verify.mjs';
+import { string_value_property } from '../../../../../string/value/property.mjs';
+import { js_identifier_parts_from } from '../../../../../js/identifier/parts/from.mjs';
 export async function function_add_js_node_type_auto(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let identifiers = await function_identifiers(function_name);
     let property_names_dictionary = js_identifiers_filter_to_node_properties_get(identifiers);
-    console.log({property_names_dictionary})
-    return;
     await object_keys_each_async(property_names_dictionary, async function v_3(property_names, prefix) {
         for (let property_name of property_names) {
             let method = js_suffix_get();
@@ -25,8 +26,10 @@ export async function function_add_js_node_type_auto(function_name) {
             let function_name_new = js_identifier_combine(property_name_fn_get, method);
             let v = !await function_exists(function_name_new);
             if (v) {
-                let v_2 = js_prefix_node();
-                await function_add_property_get(v_2, property_name);
+                let prefix_parts = js_identifier_parts_to(string_value_property);
+                list_remove_last_verify(prefix_parts, string_value_property());
+                let without = js_identifier_parts_from(prefix_parts);
+                await function_add_property_get(without, property_name);
             }
         }
     });
