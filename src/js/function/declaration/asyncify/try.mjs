74c81b8589143fd22219807bebf9858cd56c4a98
JaredMathis <jared.mathis@native.consulting>
2023-06-27 12:10:42 -0400

    #121 function_auto js_function_declaration_asyncify_try

--- src/js/function/declaration/asyncify/try.mjs
+++ src/js/function/declaration/asyncify/try.mjs
@@ -1,10 +1,10 @@
+import { object_property_set } from '../../../../object/property/set.mjs';
 import { metadata } from '../../../../metadata.mjs';
-import { object_property_change } from '../../../../object/property/change.mjs';
 import { js_function_declaration_asyncify_generic } from './generic.mjs';
 import { js_node_is_function_declaration } from '../../../node/is/function/declaration.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function js_function_declaration_asyncify_try(fd) {
     arguments_assert(arguments, [js_node_is_function_declaration]);
-    js_function_declaration_asyncify_generic(fd, object_property_change);
+    js_function_declaration_asyncify_generic(fd, object_property_set);
     metadata([]);
 }
