9ccc6bdd5b88d033769259c8ab72b97eccd9187e
JaredMathis <jared.mathis@native.consulting>
2023-06-27 15:59:12 -0400

    #297 noop

--- src/js/prefix/node.mjs
+++ src/js/prefix/node.mjs
@@ -4,6 +4,7 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 export function js_prefix_node() {
     arguments_assert(arguments, []);
-    return js_identifier_combine(js_prefix(), 'node');
+    const suffix = 'node';
+    return js_identifier_combine(js_prefix(), suffix);
     metadata([]);
 }
