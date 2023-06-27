c5f905f4d4c18d4ea62a7fe85177e424f1206dce
JaredMathis <jared.mathis@native.consulting>
2023-06-27 15:56:31 -0400

    #297 function_copy js_prefix_node_property js_prefix_mapper_args_property

---
+++ src/js/prefix/mapper/args/property.mjs
@@ -0,0 +1,10 @@
+import { metadata } from '../../../../metadata.mjs';
+import { js_identifier_prefix_property } from '../../../identifier/prefix/property.mjs';
+import { js_prefix_node } from '../../node.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function js_prefix_mapper_args_property() {
+    arguments_assert(arguments, []);
+    let v = js_prefix_node();
+    return js_identifier_prefix_property(v);
+    metadata([]);
+}
