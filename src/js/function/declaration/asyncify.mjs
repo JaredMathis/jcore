7b2c3f7042ef9eed65d4b6d9403c0d337244fa38
JaredMathis <jared.mathis@native.consulting>
2023-06-27 12:10:12 -0400

    #121 noop

--- src/js/function/declaration/asyncify.mjs
+++ src/js/function/declaration/asyncify.mjs
@@ -5,7 +5,6 @@ import { object_property_change } from '../../../object/property/change.mjs';
 import { js_node_is_function_declaration } from '../../node/is/function/declaration.mjs';
 export function js_function_declaration_asyncify(fd) {
     arguments_assert(arguments, [js_node_is_function_declaration]);
-    let lambda_object_property = object_property_change;
-    js_function_declaration_asyncify_generic(fd, lambda_object_property);
+    js_function_declaration_asyncify_generic(fd, object_property_change);
     metadata([]);
 }
