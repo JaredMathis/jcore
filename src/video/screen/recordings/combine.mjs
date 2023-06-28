3bc03a3d6783850606a41ae78dcab2886bfb1338
JaredMathis <jared.mathis@native.consulting>
2023-06-28 12:44:28 -0400

    #298 function_auto video_screen_recordings_combine

--- src/video/screen/recordings/combine.mjs
+++ src/video/screen/recordings/combine.mjs
@@ -1,3 +1,4 @@
+import { guid_generate } from '../../../guid/generate.mjs';
 import { directory_read } from '../../../directory/read.mjs';
 import { path_video_screen_recordings } from '../../../path/video/screen/recordings.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -6,16 +7,18 @@ export async function video_screen_recordings_combine() {
     arguments_assert(arguments, []);
     let ish_video_1 = 'ish_video_1';
     let path_base = path_video_screen_recordings();
-    let path_combined = path_join([path_base, ish_video_1])
+    let path_combined = path_join([
+        path_base,
+        ish_video_1
+    ]);
     let paths = await directory_read(path_combined);
-    let file_path  = `all_videos.txt`
-    let cmd = `ffmpeg -f concat -i ${file_path} -c copy merged_video.mp4`
-    `
+    let file_path = `${ guid_generate() }.txt`;
+    let cmd = `ffmpeg -f concat -i ${ file_path } -c copy merged_video.mp4``
 
 Exec(cmd, function(err, stdout, stderr) {
   if(err) console.log(err)
   else console.log("Done!")
 })
-    `
+    `;
     return paths;
 }
