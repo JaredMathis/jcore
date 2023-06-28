2c7bbd7e5a0758d062d838de05b3468a0fc149db
JaredMathis <jared.mathis@native.consulting>
2023-06-28 12:37:58 -0400

    #298 function_auto video_screen_recordings_combine

--- src/video/screen/recordings/combine.mjs
+++ src/video/screen/recordings/combine.mjs
@@ -1,7 +1,9 @@
+import { directory_read } from '../../../directory/read.mjs';
 import { path_video_screen_recordings } from '../../../path/video/screen/recordings.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function video_screen_recordings_combine() {
+export async function video_screen_recordings_combine() {
     arguments_assert(arguments, []);
     let result = path_video_screen_recordings();
+    let paths = await directory_read(result);
     return result;
 }
