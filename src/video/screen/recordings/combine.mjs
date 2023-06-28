dc77fc8921eaf54a849e888ce88e432a26906215
JaredMathis <jared.mathis@native.consulting>
2023-06-28 12:52:18 -0400

    #298 function_search git_ignore

--- src/video/screen/recordings/combine.mjs
+++ src/video/screen/recordings/combine.mjs
@@ -22,8 +22,8 @@ export async function video_screen_recordings_combine() {
     await try_catch_finally_async(async function v() {
         let file_path = `${ guid_generate() }.txt`;
         await file_write(file_path, list_join(paths, string_new_line()));
-        let cmd = `ffmpeg -f concat -i ${ file_path } -c copy merged_video.mp4`;
-        await command_line();
+        let command = `ffmpeg -f concat -i ${ file_path } -c copy merged_video.mp4`;
+        await command_line(command);
     }, async function v_2() {
         if (await file_exists(file_path)) {
             await file_delete(file_path);
