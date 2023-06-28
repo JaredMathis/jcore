dea53d3b9fd95b6f270552dbc4694ae9392d774e
JaredMathis <jared.mathis@native.consulting>
2023-06-27 22:52:47 -0400

    #139 function_auto refactor_arguments_assert_call_expression_to_function_is_type

--- src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
+++ src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
@@ -1,11 +1,12 @@
+import { js_function_declaration_to_statement_arguments_assert_args_predicate } from '../../../../../../../../js/function/declaration/to/statement/arguments/assert/args/predicate.mjs';
 import { arguments_assert_todo } from '../../../../../../../../arguments/assert/todo.mjs';
 import { error } from '../../../../../../../../error.mjs';
 import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
 import { log } from '../../../../../../../../log.mjs';
-import { js_function_declaration_to_name } from '../../../../../../../../js/function/declaration/to/name.mjs';
-export function refactor_arguments_assert_call_expression_to_function_is_type(args) {
+export async function refactor_arguments_assert_call_expression_to_function_is_type(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let {function_declaration} = args
-    let function_name = js_function_declaration_to_name(function_declaration)
+    let {function_declaration} = args;
+    let arguments_assert_args = await js_function_declaration_to_statement_arguments_assert_args_predicate(function_declaration);
+    log(arguments_assert_args);
     error('todo: refactor_arguments_assert_call_expression_to_function_is_type');
 }
