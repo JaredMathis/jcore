963f5a3b5354b97be405f11bbd8d7296df8edc27
JaredMathis <jared.mathis@native.consulting>
2023-06-27 22:08:14 -0400

    #139 noop

--- src/js/visit/assignment/exists.mjs
+++ src/js/visit/assignment/exists.mjs
@@ -7,14 +7,14 @@ import { js_node_property_name_get } from '../../node/property/name/get.mjs';
 import { js_node_is_identifier } from '../../node/is/identifier.mjs';
 import { js_node_property_left_get } from '../../node/property/left/get.mjs';
 import { occurs } from '../../../occurs.mjs';
+import { js_nodes_each } from '../../nodes/each.mjs';
 export function js_visit_assignment_exists(parsed, identifier_name) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
     return occurs(function v_3(o) {
-        let v_26 = function v_9(v) {
-            let {node} = v;
+        let v_26 = function v_9(node) {
             let left = js_node_property_left_get(node);
             let v_15 = js_node_is_identifier(left);
             if (v_15) {
@@ -25,6 +25,6 @@ export function js_visit_assignment_exists(parsed, identifier_name) {
                 }
             }
         };
-        js_visit_nodes_filter(parsed, js_node_is_assignment_expression, v_26);
+        js_nodes_each(parsed, js_node_is_assignment_expression, v_26);
     });
 }
