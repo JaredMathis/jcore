fb684c8c9c14a010d464eff6991552d806f1c31e
JaredMathis <jared.mathis@native.consulting>
2023-06-27 22:46:36 -0400

    #139 function_add_mapper_all arguments_assert_call_expression_to_function_is_type

---
+++ src/refactor/functions/arguments/assert/call/expression/to/function/is/type.mjs
@@ -0,0 +1,7 @@
+import { refactor_arguments_assert_call_expression_to_function_is_type } from '../../../../../../../../arguments/assert/call/expression/to/function/is/type.mjs';
+import { function_names_each_map_args } from '../../../../../../../../../function/names/each/map/args.mjs';
+import { arguments_assert } from '../../../../../../../../../arguments/assert.mjs';
+export async function refactor_functions_arguments_assert_call_expression_to_function_is_type() {
+    arguments_assert(arguments, []);
+    await function_names_each_map_args(refactor_arguments_assert_call_expression_to_function_is_type, {});
+}
