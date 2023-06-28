f09ba3ddf7e166cea3eb2d5567f7bcd08a5dbb6c
JaredMathis <jared.mathis@native.consulting>
2023-06-28 15:59:14 -0400

    #300 function_auto video_screen_recordings_combine_1

---
+++ src/video/screen/recordings/combine.mjs
@@ -0,0 +1,38 @@
+import { file_temporary } from '../../../file/temporary.mjs';
+import { command_line } from '../../../command/line.mjs';
+import { string_new_line } from '../../../string/new/line.mjs';
+import { list_join } from '../../../list/join.mjs';
+import { list_add_beginning } from '../../../list/add/beginning.mjs';
+import { list_map } from '../../../list/map.mjs';
+import { directory_read } from '../../../directory/read.mjs';
+import { git_exclude } from '../../../git/exclude.mjs';
+import { directory_exists_ensure } from '../../../directory/exists/ensure.mjs';
+import { path_join } from '../../../path/join.mjs';
+import { video_screen_recordings_path } from './path.mjs';
+export async function video_screen_recordings_combine(ish_video_1) {
+    let path_base = video_screen_recordings_path();
+    let path_combined = path_join([
+        path_base,
+        ish_video_1
+    ]);
+    let path_output = 'videos';
+    await directory_exists_ensure(path_output);
+    await git_exclude(path_output);
+    let paths = await directory_read(path_combined);
+    let mapped = list_map(paths, function v_3(p) {
+        return `file \'${ p }\'`;
+    });
+    list_add_beginning(mapped, `ffconcat version 1.0`);
+    let contents = list_join(mapped, string_new_line());
+    async function logic(file_path_temporary) {
+        let file_path_output_name = `${ ish_video_1 }.mp4`;
+        let file_path_output = path_join([
+            path_output,
+            file_path_output_name
+        ]);
+        let command = `ffmpeg -f concat -safe 0 -i ${ file_path_temporary } -c copy ${ file_path_output }`;
+        await command_line(command);
+    }
+    await file_temporary(contents, logic);
+    return paths;
+}
--- src/video/screen/recordings/combine/1.mjs
+++ src/video/screen/recordings/combine/1.mjs
@@ -1,44 +1,10 @@
+import { video_screen_recordings_combine } from '../combine.mjs';
 import { js_identifier_combine } from '../../../../js/identifier/combine.mjs';
 import { video_screen_recordings_prefix } from '../prefix.mjs';
-import { file_temporary } from '../../../../file/temporary.mjs';
-import { list_map } from '../../../../list/map.mjs';
-import { list_add_beginning } from '../../../../list/add/beginning.mjs';
-import { git_exclude } from '../../../../git/exclude.mjs';
-import { command_line } from '../../../../command/line.mjs';
-import { string_new_line } from '../../../../string/new/line.mjs';
-import { list_join } from '../../../../list/join.mjs';
-import { directory_read } from '../../../../directory/read.mjs';
-import { video_screen_recordings_path } from '../path.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { path_join } from '../../../../path/join.mjs';
-import { directory_exists_ensure } from '../../../../directory/exists/ensure.mjs';
 export async function video_screen_recordings_combine_1() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
     let ish_video_1 = js_identifier_combine(ish_video_prefix, `1`);
-    let path_base = video_screen_recordings_path();
-    let path_combined = path_join([
-        path_base,
-        ish_video_1
-    ]);
-    let path_output = 'videos';
-    await directory_exists_ensure(path_output);
-    await git_exclude(path_output);
-    let paths = await directory_read(path_combined);
-    let mapped = list_map(paths, function v_3(p) {
-        return `file \'${ p }\'`;
-    });
-    list_add_beginning(mapped, `ffconcat version 1.0`);
-    let contents = list_join(mapped, string_new_line());
-    async function logic(file_path_temporary) {
-        let file_path_output_name = `${ ish_video_1 }.mp4`;
-        let file_path_output = path_join([
-            path_output,
-            file_path_output_name
-        ]);
-        let command = `ffmpeg -f concat -safe 0 -i ${ file_path_temporary } -c copy ${ file_path_output }`;
-        await command_line(command);
-    }
-    await file_temporary(contents, logic);
-    return paths;
+    return await video_screen_recordings_combine(ish_video_1);
 }
