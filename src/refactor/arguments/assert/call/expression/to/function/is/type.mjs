c590bd1b3aeb2df9088921db3c54b7ecf9e7974d
JaredMathis <jared.mathis@native.consulting>
2023-06-27 22:55:53 -0400

    #139 function_auto refactor_arguments_assert_call_expression_to_function_is_type

--- src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
+++ src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
@@ -3,10 +3,13 @@ import { arguments_assert_todo } from '../../../../../../../../arguments/assert/
 import { error } from '../../../../../../../../error.mjs';
 import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
 import { log } from '../../../../../../../../log.mjs';
+import { js_function_declaration_to_name } from '../../../../../../../../js/function/declaration/to/name.mjs';
 export async function refactor_arguments_assert_call_expression_to_function_is_type(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {function_declaration} = args;
+    let function_name = js_function_declaration_to_name(function_declaration);
     let arguments_assert_args = await js_function_declaration_to_statement_arguments_assert_args_predicate(function_declaration);
+    log(function_name);
     log(arguments_assert_args);
     error('todo: refactor_arguments_assert_call_expression_to_function_is_type');
 }
