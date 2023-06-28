def1b892cfb06b472fd7c795cfde05b0afb83871
JaredMathis <jared.mathis@native.consulting>
2023-06-28 09:42:18 -0400

    #139 noop

--- src/js/function/declaration/to/statement/arguments/assert/args/predicate.mjs
+++ src/js/function/declaration/to/statement/arguments/assert/args/predicate.mjs
@@ -4,7 +4,7 @@ import { metadata } from '../../../../../../../../metadata.mjs';
 import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
 export async function js_function_declaration_to_statement_arguments_assert_args_predicate(function_declaration) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let {args} = await js_function_declaration_to_statement_arguments_assert_args_predicate_added(function_declaration);
-    return args;
+    let {arguments_assert_args} = await js_function_declaration_to_statement_arguments_assert_args_predicate_added(function_declaration);
+    return arguments_assert_args;
     metadata([]);
 }
