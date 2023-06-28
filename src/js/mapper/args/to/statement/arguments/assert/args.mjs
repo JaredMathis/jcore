5c6ce739be7bc2985fe56776de4353e7bde6f37a
JaredMathis <jared.mathis@native.consulting>
2023-06-28 08:59:48 -0400

    #139 noop

--- src/js/mapper/args/to/statement/arguments/assert/args.mjs
+++ src/js/mapper/args/to/statement/arguments/assert/args.mjs
@@ -8,7 +8,8 @@ import { arguments_assert } from '../../../../../../../arguments/assert.mjs';
 export async function js_mapper_args_to_statement_arguments_assert_args(function_declaration) {
     arguments_assert(arguments, [js_node_is_function_declaration]);
     let v_2 = { function_declaration };
-    let arguments_assert_statement = await js_mapper_args_to_statement_arguments_assert(v_2);
+    let result = await js_mapper_args_to_statement_arguments_assert_added(v_2);
+    let arguments_assert_statement = result.statement;
     let arguments_assert_call_expression = js_statement_expression_to_expression(arguments_assert_statement);
     let v = js_node_property_arguments();
     const arguments_assert_args = object_property_get(arguments_assert_call_expression, v);
