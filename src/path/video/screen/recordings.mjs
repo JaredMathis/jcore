6154667b6da9c09777f37a9868caf478745d92c5
JaredMathis <jared.mathis@native.consulting>
2023-06-28 12:33:37 -0400

    #298 function_add_string path_video_screen_recordings C:\Users\jared\Videos

---
+++ src/path/video/screen/recordings.mjs
@@ -0,0 +1,5 @@
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function path_video_screen_recordings() {
+    arguments_assert(arguments, []);
+    return 'C:UsersjaredVideos';
+}
