48edf274fdc950815f6b308263739ebe67288475
JaredMathis <jared.mathis@native.consulting>
2023-06-27 23:10:05 -0400

    #139 function_auto refactor_arguments_assert_call_expression_to_function_is_type

--- src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
+++ src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
@@ -1,3 +1,4 @@
+import { arguments_assert_predicate_default_name_equal } from '../../../../../../../../arguments/assert/predicate/default/name/equal.mjs';
 import { list_each_with_index_async } from '../../../../../../../../list/each/with/index/async.mjs';
 import { js_function_declaration_to_statement_arguments_assert_args_predicate } from '../../../../../../../../js/function/declaration/to/statement/arguments/assert/args/predicate.mjs';
 import { arguments_assert_todo } from '../../../../../../../../arguments/assert/todo.mjs';
@@ -5,6 +6,7 @@ import { error } from '../../../../../../../../error.mjs';
 import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
 import { log } from '../../../../../../../../log.mjs';
 import { js_function_declaration_to_name } from '../../../../../../../../js/function/declaration/to/name.mjs';
+import { js_node_property_name_get } from '../../../../../../../../js/node/property/name/get.mjs';
 export async function refactor_arguments_assert_call_expression_to_function_is_type(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {function_declaration, file_path} = args;
@@ -15,7 +17,9 @@ export async function refactor_arguments_assert_call_expression_to_function_is_t
         file_path
     });
     await list_each_with_index_async(arguments_assert_args, function v(arg, arg_index) {
-        log(arg);
+        let arg_name = js_node_property_name_get(arg);
+        let is_equal = arguments_assert_predicate_default_name_equal(arg_name);
+        log(is_equal);
     });
     error('todo: refactor_arguments_assert_call_expression_to_function_is_type');
 }
