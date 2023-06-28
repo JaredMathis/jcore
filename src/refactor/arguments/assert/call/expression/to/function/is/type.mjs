fb9cc94854079692135ed0a4d22c62121c91d530
JaredMathis <jared.mathis@native.consulting>
2023-06-27 22:56:11 -0400

    #139 function_auto refactor_arguments_assert_call_expression_to_function_is_type

--- src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
+++ src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
@@ -6,10 +6,13 @@ import { log } from '../../../../../../../../log.mjs';
 import { js_function_declaration_to_name } from '../../../../../../../../js/function/declaration/to/name.mjs';
 export async function refactor_arguments_assert_call_expression_to_function_is_type(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let {function_declaration} = args;
+    let {function_declaration, file_path} = args;
     let function_name = js_function_declaration_to_name(function_declaration);
     let arguments_assert_args = await js_function_declaration_to_statement_arguments_assert_args_predicate(function_declaration);
-    log(function_name);
+    log({
+        function_name,
+        file_path
+    });
     log(arguments_assert_args);
     error('todo: refactor_arguments_assert_call_expression_to_function_is_type');
 }
