0b0562e8f2a91817afa3b4f7b1aa07b88ab91297
JaredMathis <jared.mathis@native.consulting>
2023-06-28 12:49:30 -0400

    #298 function_auto video_screen_recordings_combine

--- src/video/screen/recordings/combine.mjs
+++ src/video/screen/recordings/combine.mjs
@@ -1,3 +1,5 @@
+import { file_delete } from '../../../file/delete.mjs';
+import { try_catch_finally_async } from '../../../try/catch/finally/async.mjs';
 import { try_catch_finally } from '../../../try/catch/finally.mjs';
 import { string_new_line } from '../../../string/new/line.mjs';
 import { list_join } from '../../../list/join.mjs';
@@ -7,6 +9,7 @@ import { directory_read } from '../../../directory/read.mjs';
 import { path_video_screen_recordings } from '../../../path/video/screen/recordings.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { path_join } from '../../../path/join.mjs';
+import { file_exists } from '../../../file/exists.mjs';
 export async function video_screen_recordings_combine() {
     arguments_assert(arguments, []);
     let ish_video_1 = 'ish_video_1';
@@ -19,6 +22,12 @@ export async function video_screen_recordings_combine() {
     let file_path = `${ guid_generate() }.txt`;
     try_catch_finally;
     await file_write(file_path, list_join(paths, string_new_line()));
+    await try_catch_finally_async(function v() {
+    }, async function v_2() {
+        if (await file_exists(file_path)) {
+            await file_delete(file_path);
+        }
+    });
     let cmd = `ffmpeg -f concat -i ${ file_path } -c copy merged_video.mp4``
 
 Exec(cmd, function(err, stdout, stderr) {
