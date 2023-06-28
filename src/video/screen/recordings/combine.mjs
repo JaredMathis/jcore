c33c2f41a527d54de5f2a18b805c5c4f23fe3f08
JaredMathis <jared.mathis@native.consulting>
2023-06-28 13:08:00 -0400

    #298 function_auto video_screen_recordings_combine

--- src/video/screen/recordings/combine.mjs
+++ src/video/screen/recordings/combine.mjs
@@ -1,3 +1,4 @@
+import { string_combine_multiple } from '../../../string/combine/multiple.mjs';
 import { string_replace } from '../../../string/replace.mjs';
 import { git_exclude } from '../../../git/exclude.mjs';
 import { command_line } from '../../../command/line.mjs';
@@ -27,7 +28,11 @@ export async function video_screen_recordings_combine() {
     await directory_exists_ensure(path_output);
     await git_exclude(path_output);
     let paths = await directory_read(path_combined);
-    let c = string_combine_multiple(["\"concat:", list_join(paths, '|'), "\""])
+    let c = string_combine_multiple([
+        '"concat:',
+        list_join(paths, '|'),
+        '"'
+    ]);
     let file_path = `${ guid_generate() }.txt`;
     await try_catch_finally_async(async function v() {
         let contents = list_join(paths, string_new_line());
