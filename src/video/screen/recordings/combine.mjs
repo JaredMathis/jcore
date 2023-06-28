deb07ac78b0d179c7253a0ffd7130333a8c17d72
JaredMathis <jared.mathis@native.consulting>
2023-06-28 12:39:05 -0400

    #298 noop

--- src/video/screen/recordings/combine.mjs
+++ src/video/screen/recordings/combine.mjs
@@ -1,9 +1,12 @@
 import { directory_read } from '../../../directory/read.mjs';
 import { path_video_screen_recordings } from '../../../path/video/screen/recordings.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { path_join } from '../../../path/join.mjs';
 export async function video_screen_recordings_combine() {
     arguments_assert(arguments, []);
-    let result = path_video_screen_recordings();
-    let paths = await directory_read(result);
+    let ish_video_1 = 'ish_video_1';
+    let path_base = path_video_screen_recordings();
+    let path_combined = path_join([path_base, ish_video_1])
+    let paths = await directory_read(path_combined);
     return paths;
 }
