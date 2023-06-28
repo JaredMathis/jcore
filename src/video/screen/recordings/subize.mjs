030cdecd0c93a2a315a8878b3f59de75887bd785
JaredMathis <jared.mathis@native.consulting>
2023-06-28 15:11:18 -0400

    #300 function_auto video_screen_recordings_subize

--- src/video/screen/recordings/subize.mjs
+++ src/video/screen/recordings/subize.mjs
@@ -1,3 +1,5 @@
+import { string_ends_with } from '../../../string/ends/with.mjs';
+import { list_filter } from '../../../list/filter.mjs';
 import { video_screen_recordings_path } from './path.mjs';
 import { video_screen_recordings_prefix } from './prefix.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -7,5 +9,8 @@ export async function video_screen_recordings_subize() {
     let ish_video_prefix = video_screen_recordings_prefix();
     let path_base = video_screen_recordings_path();
     let paths = await directory_read_non_recursive(path_base);
+    let filtered = list_filter(paths, function v(p) {
+        return string_ends_with(p, '.mkv');
+    });
     return paths;
 }
