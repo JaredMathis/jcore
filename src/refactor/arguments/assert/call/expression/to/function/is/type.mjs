57a520d7182890df3da5d368bfa57d1d0071f7f8
JaredMathis <jared.mathis@native.consulting>
2023-06-27 23:31:22 -0400

    #139 noop

--- src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
+++ src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
@@ -13,6 +13,7 @@ import { list_get } from '../../../../../../../../list/get.mjs';
 import { occurs } from '../../../../../../../../occurs.mjs';
 import { function_name_get } from '../../../../../../../../function/name/get.mjs';
 import { function_is } from '../../../../../../../../function/is.mjs';
+import { js_node_is_identifier } from '../../../../../../../../js/node/is/identifier.mjs';
 export async function refactor_arguments_assert_call_expression_to_function_is_type(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {function_declaration, file_path, parsed} = args;
@@ -20,6 +21,9 @@ export async function refactor_arguments_assert_call_expression_to_function_is_t
     let arguments_assert_args = await js_function_declaration_to_statement_arguments_assert_args_predicate(function_declaration);
     await refactor_import_fix_if_changed(args, async function v_3(c) {
         await list_each_with_index_async(arguments_assert_args, function v(arg, arg_index) {
+            if (!js_node_is_identifier(arg)) {
+                return;
+            }
             let arg_name = js_node_property_name_get(arg);
             let is_equal = arguments_assert_predicate_default_name_equal(arg_name);
             if (!is_equal) {
