ea93360729dafe5534cedc812d4a655459e40a5d
JaredMathis <jared.mathis@native.consulting>
2023-06-27 22:48:23 -0400

    #139 noop

--- src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
+++ src/refactor/arguments/assert/call/expression/to/function/is/type.mjs
@@ -1,6 +1,7 @@
 import { arguments_assert_todo } from '../../../../../../../../arguments/assert/todo.mjs';
 import { error } from '../../../../../../../../error.mjs';
 import { arguments_assert } from '../../../../../../../../arguments/assert.mjs';
+import { log } from '../../../../../../../../log.mjs';
 export function refactor_arguments_assert_call_expression_to_function_is_type(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: refactor_arguments_assert_call_expression_to_function_is_type');
