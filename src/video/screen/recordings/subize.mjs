4c2baacb1e6b1b45d1daa89a96fbb065deb8debf
JaredMathis <jared.mathis@native.consulting>
2023-06-28 15:04:02 -0400

    #300 function_auto video_screen_recordings_subize

--- src/video/screen/recordings/subize.mjs
+++ src/video/screen/recordings/subize.mjs
@@ -1,3 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 export function video_screen_recordings_subize() {
+    arguments_assert(arguments, []);
     error('todo: video_screen_recordings_subize');
 }
