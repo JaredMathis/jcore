631776e5ad684606817974a84422a8bc12448e0c
JaredMathis <jared.mathis@native.consulting>
2023-06-28 15:08:05 -0400

    #300 function_auto video_screen_recordings_subize

--- src/video/screen/recordings/subize.mjs
+++ src/video/screen/recordings/subize.mjs
@@ -1,6 +1,10 @@
+import { video_screen_recordings_path } from './path.mjs';
 import { video_screen_recordings_prefix } from './prefix.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export function video_screen_recordings_subize() {
+import { directory_read_non_recursive } from '../../../directory/read/non/recursive.mjs';
+export async function video_screen_recordings_subize() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
+    let path_base = video_screen_recordings_path();
+    let paths = await directory_read_non_recursive(path_base);
 }
