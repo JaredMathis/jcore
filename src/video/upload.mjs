9a99e4ff8b56f0b6346ba64a940bbc4922f67cf2
JaredMathis <jared.mathis@native.consulting>
2023-06-29 18:30:12 -0400

    #301 function_auto_run video_upload

--- src/video/upload.mjs
+++ src/video/upload.mjs
@@ -2,10 +2,11 @@ import { video_screen_recordings_prefix } from './screen/recordings/prefix.mjs';
 import { video_path_get } from './path/get.mjs';
 import { js_identifier_combine } from '../js/identifier/combine.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
+import { google } from 'googleapis';
 export function video_upload() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
     let ish_video_1 = js_identifier_combine(ish_video_prefix, `1`);
     let ish_video_1_path = video_path_get(ish_video_1);
-    return ish_video_1_path;
+    return google;
 }
