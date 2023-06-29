4f8d5b33a914dc3bd31c9349e93d879748f9db88
JaredMathis <jared.mathis@native.consulting>
2023-06-29 18:21:03 -0400

    #301 function_auto video_upload

---
+++ src/video/upload.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function video_upload() {
+    arguments_assert(arguments, []);
+    error('todo: video_upload');
+}
