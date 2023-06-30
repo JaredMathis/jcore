685d1a8c4785fd8d5c63d9e421f27028e38a1126
JaredMathis <jared.mathis@native.consulting>
2023-06-30 14:07:00 -0400

    #301 function_rename video_compress_1 video_segment_1

--- src/video/segment/1.mjs
+++ src/video/segment/1.mjs
@@ -2,7 +2,7 @@ import { video_compress } from '../compress.mjs';
 import { js_identifier_combine } from '../../js/identifier/combine.mjs';
 import { video_screen_recordings_prefix } from '../screen/recordings/prefix.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function video_compress_1() {
+export async function video_segment_1() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
     let video_key = js_identifier_combine(ish_video_prefix, `1`);
