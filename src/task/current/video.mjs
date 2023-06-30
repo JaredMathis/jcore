5294fbdbc9d3e50d590dea9afaa17f961a58a2b3
JaredMathis <jared.mathis@native.consulting>
2023-06-30 15:48:56 -0400

    #301 function_auto task_current_video

--- src/task/current/video.mjs
+++ src/task/current/video.mjs
@@ -1,8 +1,8 @@
 import { task_comment_video } from '../comment/video.mjs';
 import { video_screen_recordings_subize } from '../../video/screen/recordings/subize.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function task_current_video() {
+export async function task_current_video() {
     arguments_assert(arguments, []);
-    video_screen_recordings_subize;
+    let subized = await video_screen_recordings_subize();
     task_comment_video;
 }
