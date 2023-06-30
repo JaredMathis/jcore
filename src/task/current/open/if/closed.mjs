bab1be3f65391311534edc40405aa323825c8a02
JaredMathis <jared.mathis@native.consulting>
2023-06-30 18:17:08 -0400

    #300 function_auto task_current_open_if_closed

--- src/task/current/open/if/closed.mjs
+++ src/task/current/open/if/closed.mjs
@@ -1,11 +1,12 @@
+import { task_current_data } from '../../data.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { task_current_open } from '../../open.mjs';
 import { task_property_closed } from '../../../property/closed.mjs';
 import { task_property_state_get } from '../../../property/state/get.mjs';
 import { equal } from '../../../../equal.mjs';
-import { task_current } from '../../../current.mjs';
 export async function task_current_open_if_closed() {
-    let before = await task_current();
-    console.log({before})
+    arguments_assert(arguments, []);
+    let before = await task_current_data();
     if (equal(task_property_state_get(before), task_property_closed())) {
         await task_current_open();
     }
