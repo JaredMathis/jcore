fb8f8cb31fc35293e1f251f171da4ce3ffcd1f3e
JaredMathis <jared.mathis@native.consulting>
2023-06-30 15:35:34 -0400

    #301 noop

--- src/task/comment/video.mjs
+++ src/task/comment/video.mjs
@@ -15,5 +15,6 @@ export async function task_comment_video(issue_number, video_key) {
     await video_segment(video_key);
     let uploads = await video_upload(video_key);
     let url_streams = list_map_property(uploads, 'url_stream');
+    let notify = ['ismael-texidor']
     await git_hub_repository_issue_comments_add(issue_number, list_join(url_streams, string_new_line()));
 }
