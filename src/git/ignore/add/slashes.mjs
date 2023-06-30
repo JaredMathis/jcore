435a98149529772599ead202a7bd2d402e44264a
JaredMathis <jared.mathis@native.consulting>
2023-06-30 18:37:08 -0400

    #308 function_input_remove_last git_ignore_add_slashes

--- src/git/ignore/add/slashes.mjs
+++ src/git/ignore/add/slashes.mjs
@@ -1,16 +1,18 @@
+import { list_set } from '../../../list/set.mjs';
 import { list_each_with_index } from '../../../list/each/with/index.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { git_ignore_map } from '../map.mjs';
 import { list_add_if_not_exists } from '../../../list/add/if/not/exists.mjs';
 import { git_ignore_line_slash_add } from '../line/slash/add.mjs';
-import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-export async function git_ignore_add_slashes(gitignore_line_to_add) {
-    arguments_assert(arguments, [arguments_assert_todo]);
+export async function git_ignore_add_slashes() {
+    arguments_assert(arguments, []);
     function lambda_map(gitignore_lines) {
-        let slashed = git_ignore_line_slash_add(gitignore_line_to_add);
         list_add_if_not_exists(gitignore_lines, slashed);
-        list_each_with_index(gitignore_lines, (value, index));
+        list_each_with_index(gitignore_lines, (element, index) => {
+            let element_new = git_ignore_line_slash_add(element);
+            list_set(gitignore_lines, index, element_new);
+        });
     }
     await git_ignore_map(lambda_map);
     metadata([]);
