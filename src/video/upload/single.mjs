17447f76ad658e2984705c5e2110152d86425c9a
JaredMathis <jared.mathis@native.consulting>
2023-06-30 14:40:52 -0400

    #301 noop

--- src/video/upload/single.mjs
+++ src/video/upload/single.mjs
@@ -2,13 +2,17 @@ import { file_read_bytes } from '../../file/read/bytes.mjs';
 import { private_get } from '../../private/get.mjs';
 import { publitioApi } from 'publitio_js_sdk';
 export async function video_upload_single(first, video_key) {
-    const publitio = publitioApi(await private_get(`publit_api_key`), await private_get(`publit_api_secret`));
-    const file = await file_read_bytes(first);
-    return await publitio.uploadFile(file, 'file', {
-        title: video_key,
-        description: video_key,
-        tags: '',
-        privacy: '1',
-        option_download: '0'
-    });
+    return await lambda();
+
+    async function lambda() {
+        const publitio = publitioApi(await private_get(`publit_api_key`), await private_get(`publit_api_secret`));
+        const file = await file_read_bytes(first);
+        return await publitio.uploadFile(file, 'file', {
+            title: video_key,
+            description: video_key,
+            tags: '',
+            privacy: '1',
+            option_download: '0'
+        });
+    }
 }
