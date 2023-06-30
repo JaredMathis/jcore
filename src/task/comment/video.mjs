11aa4994ab96f3be3826a5f84b301365f76af662
JaredMathis <jared.mathis@native.consulting>
2023-06-30 15:57:20 -0400

    #301 noop

--- src/task/comment/video.mjs
+++ src/task/comment/video.mjs
@@ -9,6 +9,8 @@ import { list_join } from '../../list/join.mjs';
 import { string_new_line } from '../../string/new/line.mjs';
 import { list_map } from '../../list/map.mjs';
 import { string_combine } from '../../string/combine.mjs';
+import { assert } from '../../assert.mjs';
+import { list_empty_not } from '../../list/empty/not.mjs';
 export async function task_comment_video(issue_number, video_key) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -17,6 +19,7 @@ export async function task_comment_video(issue_number, video_key) {
     await video_segment(video_key);
     let uploads = await video_upload(video_key);
     let url_streams = list_map_property(uploads, 'url_stream');
+    assert(list_empty_not(url_streams));
     let notify = ['ismael-texidor'];
     let mapped = list_map(notify, function v(n) {
         return string_combine('@', n);
