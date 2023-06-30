5c3faf79e07e60abb2038ba377a59a36aea652b6
JaredMathis <jared.mathis@native.consulting>
2023-06-30 12:27:15 -0400

    #301 noop

--- src/command/line/ffmpeg.mjs
+++ src/command/line/ffmpeg.mjs
@@ -1,7 +1,7 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { command_line } from '../line.mjs';
-export async function command_line_ffmpeg(file_path_temporary, file_path_output, before, middle) {
+export async function command_line_ffmpeg(file_path_input, file_path_output, before, middle) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo,
@@ -9,6 +9,6 @@ export async function command_line_ffmpeg(file_path_temporary, file_path_output,
         arguments_assert_todo
     ]);
     let ffmpeg = `ffmpeg`;
-    let command = `${ ffmpeg } ${ before } -i ${ file_path_temporary } ${ middle } ${ file_path_output }`;
+    let command = `${ ffmpeg } ${ before } -i ${ file_path_input } ${ middle } ${ file_path_output }`;
     await command_line(command);
 }
