b980cb4708be86a9c9bc9d3613084b8209af3f56
JaredMathis <jared.mathis@native.consulting>
2023-06-30 12:44:36 -0400

    #301 function_auto video_path_get

---
+++ src/video/name/get.mjs
@@ -0,0 +1,3 @@
+export function video_name_get(video_key) {
+    return `${ video_key }.mp4`;
+}
--- src/video/path/get.mjs
+++ src/video/path/get.mjs
@@ -1,7 +1,11 @@
+import { video_name_get } from '../name/get.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { video_path_output } from './output.mjs';
 import { path_join } from '../../path/join.mjs';
 export function video_path_get(video_key) {
-    let file_path_output_name = `${ video_key }.mp4`;
+    arguments_assert(arguments, [arguments_assert_todo]);
+    let file_path_output_name = video_name_get(video_key);
     let file_path_output = path_join([
         video_path_output(),
         file_path_output_name
