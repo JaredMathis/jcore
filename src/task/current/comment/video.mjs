515fe2b46746adfb84a97a01245248d4766a5350
JaredMathis <jared.mathis@native.consulting>
2023-06-30 15:23:45 -0400

    #301 function_auto task_current_comment_video

--- src/task/current/comment/video.mjs
+++ src/task/current/comment/video.mjs
@@ -1,10 +1,10 @@
-import { task_id_get_or_null } from '../../id/get/or/null.mjs';
+import { task_id_get_string } from '../../id/get/string.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { task_comment_video } from '../../comment/video.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function task_current_comment_video(video_key) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let issue_number = await task_id_get_or_null();
+    let issue_number = await task_id_get_string();
     return issue_number;
     let result = await task_comment_video(issue_number, video_key);
     return result;
