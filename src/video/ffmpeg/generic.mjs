b4d5079c9abf208795d3c510f213762aedc281f6
JaredMathis <jared.mathis@native.consulting>
2023-06-30 14:08:38 -0400

    #301 function_auto video_ffmpeg_generic

--- src/video/ffmpeg/generic.mjs
+++ src/video/ffmpeg/generic.mjs
@@ -1,16 +1,30 @@
-import { js_identifier_combine } from '../../js/identifier/combine.mjs';
+import { string_combine_multiple } from '../../string/combine/multiple.mjs';
+import { function_name_separator } from '../../function/name/separator.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { command_line_ffmpeg } from '../../command/line/ffmpeg.mjs';
 import { video_name_map } from '../name/map.mjs';
 import { path_exists } from '../../path/exists.mjs';
 import { assert } from '../../assert.mjs';
 import { video_path_get } from '../path/get.mjs';
 export async function video_ffmpeg_generic(video_key, middle, suffix) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let file_path_input = video_path_get(video_key);
     assert(await path_exists(file_path_input));
     let file_path_output = video_name_map(file_path_input, lambda_map);
     assert(!await path_exists(file_path_output));
     await command_line_ffmpeg(file_path_input, file_path_output, ``, middle);
     function lambda_map(name_before) {
-        return js_identifier_combine(name_before, suffix);
+        const strings = [
+            name_before,
+            function_name_separator(),
+            suffix
+        ];
+        let result = string_combine_multiple(strings);
+        return result;
     }
 }
--- src/video/segment/1.mjs
+++ src/video/segment/1.mjs
@@ -1,4 +1,4 @@
-import { video_compress } from '../compress.mjs';
+import { video_segment } from '../segment.mjs';
 import { js_identifier_combine } from '../../js/identifier/combine.mjs';
 import { video_screen_recordings_prefix } from '../screen/recordings/prefix.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -6,5 +6,5 @@ export async function video_segment_1() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
     let video_key = js_identifier_combine(ish_video_prefix, `1`);
-    await video_compress(video_key);
+    await video_segment(video_key);
 }
