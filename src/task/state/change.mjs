203363bfdaaf05ca9933943985e88237c5c6fbb3
JaredMathis <jared.mathis@native.consulting>
2023-06-30 18:05:01 -0400

    #305 function_auto task_state_change

--- src/task/state/change.mjs
+++ src/task/state/change.mjs
@@ -1,10 +1,10 @@
+import { git_hub_repository_issue_state_generic } from '../../git/hub/repository/issue/state/generic.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { task_available } from '../available.mjs';
 import { task_property_change_local } from '../property/change/local.mjs';
 import { task_property_state } from '../property/state.mjs';
-import { git_hub_repository_issue_close } from '../../git/hub/repository/issue/close.mjs';
 import { integer_parse } from '../../integer/parse.mjs';
 export async function task_state_change(task_number_string, property_value) {
     arguments_assert(arguments, [
@@ -12,7 +12,7 @@ export async function task_state_change(task_number_string, property_value) {
         arguments_assert_todo
     ]);
     let task_number = integer_parse(task_number_string);
-    await git_hub_repository_issue_close(task_number);
+    await git_hub_repository_issue_state_generic(task_number, property_value);
     const property_name = task_property_state();
     await task_property_change_local(task_number_string, property_name, property_value);
     let v = await task_available();
