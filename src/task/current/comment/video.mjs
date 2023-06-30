742d42119977bc575808c5d1829529d7d70f54ac
JaredMathis <jared.mathis@native.consulting>
2023-06-30 15:24:16 -0400

    #301 task_current_comment_video ish_video_1

--- src/task/current/comment/video.mjs
+++ src/task/current/comment/video.mjs
@@ -5,7 +5,6 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function task_current_comment_video(video_key) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let issue_number = await task_id_get_string();
-    return issue_number;
     let result = await task_comment_video(issue_number, video_key);
     return result;
 }
