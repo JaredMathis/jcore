9d7f98331396fb19dbd1213a22ad0f164704429d
JaredMathis <jared.mathis@native.consulting>
2023-06-30 10:31:02 -0400

    #301 function_auto firebase_initialize

--- src/firebase/initialize.mjs
+++ src/firebase/initialize.mjs
@@ -1,6 +1,9 @@
+import { arguments_assert } from '../arguments/assert.mjs';
 import { boolean_value_true } from '../boolean/value/true.mjs';
 import { not } from '../not.mjs';
+import { initializeApp } from 'firebase/app';
 export function firebase_initialize() {
+    arguments_assert(arguments, []);
     let v = not(initialized);
     if (v) {
         let v_3 = 'truthcode';
