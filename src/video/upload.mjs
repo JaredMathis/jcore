1ecedc3e21c197790fc747b0457b545c875f73d2
JaredMathis <jared.mathis@native.consulting>
2023-06-30 10:20:02 -0400

    #301 noop

--- src/video/upload.mjs
+++ src/video/upload.mjs
@@ -32,11 +32,14 @@ export async function video_upload() {
         'https://www.googleapis.com/auth/youtube.upload',
         'https://www.googleapis.com/auth/youtube'
     ];
-    const auth_url = oauth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: scopes
-    });
-    return auth_url;
+    if (false) {
+
+        const auth_url = oauth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: scopes
+        });
+        return auth_url;
+    }
     oauth2Client.setCredentials({ access_token });
     google.options({ auth: oauth2Client });
     const res = await youtube.videos.insert({
