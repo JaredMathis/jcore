e960e9aba719fb105cd2269c352055c118340a2b
JaredMathis <jared.mathis@native.consulting>
2023-06-30 15:17:30 -0400

    #301 function_auto task_comment_video

---
+++ src/task/comment/video.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function task_comment_video() {
+    arguments_assert(arguments, []);
+    error('todo: task_comment_video');
+}
