fdae07a53267c98bcd8b9e2daabcb1f6a5d827c1
JaredMathis <jared.mathis@native.consulting>
2023-06-30 15:37:22 -0400

    #301 function_auto task_comment_video

--- src/task/comment/video.mjs
+++ src/task/comment/video.mjs
@@ -1,4 +1,4 @@
-import { error } from '../../error.mjs';
+import { list_multiple_combine } from '../../list/multiple/combine.mjs';
 import { git_hub_repository_issue_comments_add } from '../../git/hub/repository/issue/comments/add.mjs';
 import { list_map_property } from '../../list/map/property.mjs';
 import { video_upload } from '../../video/upload.mjs';
@@ -7,6 +7,8 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_join } from '../../list/join.mjs';
 import { string_new_line } from '../../string/new/line.mjs';
+import { list_map } from '../../list/map.mjs';
+import { string_combine } from '../../string/combine.mjs';
 export async function task_comment_video(issue_number, video_key) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -15,6 +17,11 @@ export async function task_comment_video(issue_number, video_key) {
     await video_segment(video_key);
     let uploads = await video_upload(video_key);
     let url_streams = list_map_property(uploads, 'url_stream');
-    let notify = ['ismael-texidor']
-    await git_hub_repository_issue_comments_add(issue_number, list_join(url_streams, string_new_line()));
+    let notify = ['ismael-texidor'];
+    notify = ['JaredMathis'];
+    let mapped = list_map(notify, function v(n) {
+        return string_combine('@', n);
+    });
+    let comment_lines = list_multiple_combine(mapped, url_streams);
+    await git_hub_repository_issue_comments_add(issue_number, list_join(comment_lines, string_new_line()));
 }
