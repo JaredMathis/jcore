ceafec2a5bfd42af77c64b353a2b966d1696cb2e
JaredMathis <jared.mathis@native.consulting>
2023-06-30 11:52:42 -0400

    #301 video_upload

--- src/video/upload.mjs
+++ src/video/upload.mjs
@@ -2,7 +2,7 @@ import { video_screen_recordings_prefix } from './screen/recordings/prefix.mjs';
 import { video_path_get } from './path/get.mjs';
 import { js_identifier_combine } from '../js/identifier/combine.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-import PublitioAPI from 'publitio_js_sdk';
+import * as PublitioAPI from 'publitio_js_sdk';
 import { private_get } from '../private/get.mjs';
 export async function video_upload() {
     arguments_assert(arguments, []);
