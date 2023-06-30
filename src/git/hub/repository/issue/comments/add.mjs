a46671d35b3ebadcd010e664eae5613eefcb8ca9
JaredMathis <jared.mathis@native.consulting>
2023-06-30 15:16:07 -0400

    #301 function_auto git_hub_repository_issue_comments_add

--- src/git/hub/repository/issue/comments/add.mjs
+++ src/git/hub/repository/issue/comments/add.mjs
@@ -1,6 +1,6 @@
+import { git_hub_repository_issue_comments_generic } from './generic.mjs';
 import { boolean_value_true } from '../../../../../boolean/value/true.mjs';
 import { metadata } from '../../../../../metadata.mjs';
-import { git_hub_repository_issues_generic } from '../../issues/generic.mjs';
 import { object_merge } from '../../../../../object/merge.mjs';
 import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
@@ -11,7 +11,7 @@ export async function git_hub_repository_issue_comments_add(body) {
     let no_cache = boolean_value_true();
     let v_2 = { body };
     object_merge(v_2, api_args_to_merge);
-    let v = await git_hub_repository_issues_generic(git_hub_repository_issue_comments_add, arguments, api_args_to_merge, verb, no_cache);
+    let v = await git_hub_repository_issue_comments_generic(git_hub_repository_issue_comments_add, arguments, api_args_to_merge, verb, no_cache);
     return v;
     metadata([]);
 }
