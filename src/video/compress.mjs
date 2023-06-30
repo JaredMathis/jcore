3c4e1214ceb9f68a8077c52d2978ffbf77cb2912
JaredMathis <jared.mathis@native.consulting>
2023-06-30 12:40:02 -0400

    #301 function_auto video_compress

--- src/video/compress.mjs
+++ src/video/compress.mjs
@@ -1,3 +1,4 @@
+import { path_parse_name } from '../path/parse/name.mjs';
 import { video_path_get } from './path/get.mjs';
 import { js_identifier_combine } from '../js/identifier/combine.mjs';
 import { video_screen_recordings_prefix } from './screen/recordings/prefix.mjs';
@@ -8,5 +9,6 @@ export async function video_compress() {
     let ish_video_prefix = video_screen_recordings_prefix();
     let video_key = js_identifier_combine(ish_video_prefix, `1`);
     let file_path_output = video_path_get(video_key);
+    let name = path_parse_name(file_path_output);
     await command_line_ffmpeg(file_path_input, file_path_output, ``, `-vcodec libx265 -crf 28`);
 }
