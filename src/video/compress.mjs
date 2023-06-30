1de5c4309c84500608c1fe483f612ef99af6b74a
JaredMathis <jared.mathis@native.consulting>
2023-06-30 14:04:14 -0400

    #301 noop

--- src/video/compress.mjs
+++ src/video/compress.mjs
@@ -4,8 +4,6 @@ import { arguments_assert } from '../arguments/assert.mjs';
 export async function video_compress(video_key) {
     arguments_assert(arguments, [arguments_assert_todo]);
     const suffix = 'compressed';
-    `ffmpeg -i input.mp4 -c copy -map 0 -segment_time 00:20:00 -f segment output%03d.mp4
-    `;
     const middle = `-vcodec libx265 -crf 28`;
     await video_ffmpeg_generic(video_key, middle, suffix);
 }
