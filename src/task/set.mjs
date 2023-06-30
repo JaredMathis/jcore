666cef6290c20171069a223a2535ba2b8a04621f
JaredMathis <jared.mathis@native.consulting>
2023-06-30 18:07:52 -0400

    #305 function_auto task_set

--- src/task/set.mjs
+++ src/task/set.mjs
@@ -1,3 +1,4 @@
+import { task_property_state_get } from './property/state/get.mjs';
 import { task_current_open } from './current/open.mjs';
 import { task_symbol_hash } from './symbol/hash.mjs';
 import { metadata } from '../metadata.mjs';
@@ -11,7 +12,8 @@ export async function task_set(task_id_unhashed) {
     let v = task_symbol_hash();
     let hashed = string_combine(v, task_id_unhashed);
     await task_id_set(hashed);
-    if (false) {
+    let before = await task_current();
+    if (task_property_state_get()) {
         await task_current_open();
     }
     return await task_current();
