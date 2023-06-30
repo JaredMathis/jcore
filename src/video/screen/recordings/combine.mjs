721017ff6274be62be863e2f5bb7081b452861cb
JaredMathis <jared.mathis@native.consulting>
2023-06-30 12:30:14 -0400

    #301 function_auto video_screen_recordings_combine

--- src/video/screen/recordings/combine.mjs
+++ src/video/screen/recordings/combine.mjs
@@ -33,7 +33,11 @@ export async function video_screen_recordings_combine(ish_video_1) {
         let file_path_output = video_path_get(ish_video_1);
         let ffmpeg = `ffmpeg`;
         let before1 = ``;
-        let middle1 = `-vcodec libx265 -crf 28`;`${ ffmpeg } ${ before1 } -i ${ file_path_temporary } ${ middle1 } ${ file_path_output }`;
+        let middle1 = `-vcodec libx265 -crf 28`;
+        `${ ffmpeg } ${ before1 } -i ${ file_path_temporary } ${ middle1 } ${ file_path_output }`;
+        if (false) {
+            await command_line_ffmpeg(file_path_temporary, file_path_output, `-f concat -safe 0`, `-c copy`);
+        }
         await command_line_ffmpeg(file_path_temporary, file_path_output, `-f concat -safe 0`, `-c copy`);
     }
     await file_temporary(contents, logic);
