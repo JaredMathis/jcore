a26b244be3254633ba531e16e162e66fd803d0f2
JaredMathis <jared.mathis@native.consulting>
2023-06-30 16:06:56 -0400

    #303 function_auto task_current_open

--- src/task/current/open.mjs
+++ src/task/current/open.mjs
@@ -1,11 +1,10 @@
 import { metadata } from '../../metadata.mjs';
-import { task_id_get_or_null } from '../id/get/or/null.mjs';
 import { task_open } from '../open.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { task_id_get_string } from '../id/get/string.mjs';
 export async function task_current_open() {
     arguments_assert(arguments, []);
-    let current = await task_id_get_string();
+    let task_number_string = await task_id_get_string();
     let result = await task_open(task_number_string);
     return result;
     metadata([]);
