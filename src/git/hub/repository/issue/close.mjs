808f08f2026d9e4bbde0d2eaa48c11a1996c5e00
JaredMathis <jared.mathis@native.consulting>
2023-06-30 18:04:09 -0400

    #305 function_input_swap_first_2 git_hub_repository_issue_state_generic

--- src/git/hub/repository/issue/close.mjs
+++ src/git/hub/repository/issue/close.mjs
@@ -6,6 +6,6 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function git_hub_repository_issue_close(issue_number) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let v_2 = task_property_closed();
-    return git_hub_repository_issue_state_generic(v_2, issue_number);
+    return git_hub_repository_issue_state_generic(issue_number, v_2);
     metadata([]);
 }
--- src/git/hub/repository/issue/state/generic.mjs
+++ src/git/hub/repository/issue/state/generic.mjs
@@ -1,8 +1,14 @@
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { git_hub_repository_issue_close } from '../close.mjs';
 import { git_hub_repository_issue_patch } from '../patch.mjs';
 import { boolean_value_true } from '../../../../../boolean/value/true.mjs';
 import { task_property_state } from '../../../../../task/property/state.mjs';
-export function git_hub_repository_issue_state_generic(v_2, issue_number) {
+export function git_hub_repository_issue_state_generic(issue_number, v_2) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let api_args_to_merge = { [task_property_state()]: v_2 };
     let no_cache = boolean_value_true();
     let v = git_hub_repository_issue_patch(git_hub_repository_issue_close, arguments, issue_number, api_args_to_merge, no_cache);
