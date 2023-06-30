df0411701c28e37405735b6bb40fba1640b4c644
JaredMathis <jared.mathis@native.consulting>
2023-06-30 12:39:25 -0400

    #301 function_copy_suffix_change path_parse base name

---
+++ src/path/parse/name.mjs
@@ -0,0 +1,11 @@
+import { metadata } from '../../metadata.mjs';
+import { path_parse_generic } from './generic.mjs';
+import { path_is } from '../is.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function path_parse_name(file_path) {
+    arguments_assert(arguments, [path_is]);
+    let v_2 = 'base';
+    let v = path_parse_generic(file_path, v_2);
+    return v;
+    metadata([]);
+}
