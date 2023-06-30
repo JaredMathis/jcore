62884a6a43124a759140072486cb3a13d7872c99
JaredMathis <jared.mathis@native.consulting>
2023-06-29 20:15:18 -0400

    #301 function_auto video_upload

--- src/video/upload.mjs
+++ src/video/upload.mjs
@@ -1,23 +1,23 @@
+import { directory_current } from '../directory/current.mjs';
 import { video_screen_recordings_prefix } from './screen/recordings/prefix.mjs';
 import { video_path_get } from './path/get.mjs';
 import { js_identifier_combine } from '../js/identifier/combine.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { google } from 'googleapis';
 import { file_extension_json } from '../file/extension/json.mjs';
-export function video_upload() {
+export async function video_upload() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
     let ish_video_1 = js_identifier_combine(ish_video_prefix, `1`);
     let ish_video_1_path = video_path_get(ish_video_1);
     const youtube = google.youtube('v3');
-      // Obtain user credentials to use for the request
-  const auth = await authenticate({
-    keyfilePath: `../oauth2.keys${file_extension_json()}`,
-    scopes: [
-      'https://www.googleapis.com/auth/youtube.upload',
-      'https://www.googleapis.com/auth/youtube',
-    ],
-  });
-  google.options({auth});
+    const auth = await authenticate({
+        keyfilePath: `${ directory_current() }./oauth2.keys${ file_extension_json() }`,
+        scopes: [
+            'https://www.googleapis.com/auth/youtube.upload',
+            'https://www.googleapis.com/auth/youtube'
+        ]
+    });
+    google.options({ auth });
     return youtube;
 }
