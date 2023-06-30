6c78fe0d8bcd8e187c66922f88e8f25f21835d2d
JaredMathis <jared.mathis@native.consulting>
2023-06-30 12:25:53 -0400

    #301 function_input_swap_first_2 command_line_ffmpeg

--- src/command/line/ffmpeg.mjs
+++ src/command/line/ffmpeg.mjs
@@ -1,5 +1,13 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { command_line } from '../line.mjs';
-export async function command_line_ffmpeg(before, file_path_temporary, middle, file_path_output) {
+export async function command_line_ffmpeg(file_path_temporary, before, middle, file_path_output) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     let ffmpeg = `ffmpeg`;
     let command = `${ ffmpeg } ${ before } -i ${ file_path_temporary } ${ middle } ${ file_path_output }`;
     await command_line(command);
