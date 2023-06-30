c45b4de9302f1d2579cc00d062025d445c29a511
JaredMathis <jared.mathis@native.consulting>
2023-06-30 12:06:17 -0400

    #301 function_auto video_upload

--- src/video/upload.mjs
+++ src/video/upload.mjs
@@ -1,3 +1,6 @@
+import { error } from '../error.mjs';
+import { log } from '../log.mjs';
+import { file_read_bytes } from '../file/read/bytes.mjs';
 import { video_screen_recordings_prefix } from './screen/recordings/prefix.mjs';
 import { video_path_get } from './path/get.mjs';
 import { js_identifier_combine } from '../js/identifier/combine.mjs';
@@ -10,22 +13,22 @@ export async function video_upload() {
     let ish_video_1 = js_identifier_combine(ish_video_prefix, `1`);
     let ish_video_1_path = video_path_get(ish_video_1);
     const publitio = publitioApi(await private_get(`publit_api_key`), await private_get(`publit_api_secret`));
-
-    const file = readFileSync('image.png')
-
-    publitio.uploadFile(file, 'file')
-      .then(data => { console.log(data) })
-      .catch(error => { console.log(error) })
-    
-    // A more realistic example
+    const file = await file_read_bytes(ish_video_1_path);
+    publitio.uploadFile(file, 'file').then(function v(data) {
+        console.log(data);
+    }).catch(function v_2(error) {
+        console.log(error);
+    });
     publitio.uploadFile(file, 'file', {
-      title: '<file title>',
-      description: '<file description>',
-      tags: '<file tags separated by spaces or commas>',
-      privacy: '<0 - private, 1 - public>',
-      option_download: '<0 - disable downloads, 1 - enable downloads>'
-    }).then(data => { console.log(data) })
-      .catch(error => { console.log(error) })
-
+        title: '<file title>',
+        description: '<file description>',
+        tags: '<file tags separated by spaces or commas>',
+        privacy: '<0 - private, 1 - public>',
+        option_download: '<0 - disable downloads, 1 - enable downloads>'
+    }).then(function v_3(data) {
+        console.log(data);
+    }).catch(function v_4(error) {
+        console.log(error);
+    });
     return publitio;
 }
