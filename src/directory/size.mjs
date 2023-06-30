6aaead156df478bf3686b97ebcb4e468a0746c43
JaredMathis <jared.mathis@native.consulting>
2023-06-29 21:26:55 -0400

    #301 function_auto directory_size

--- src/directory/size.mjs
+++ src/directory/size.mjs
@@ -5,7 +5,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 export async function directory_size(file_path) {
     arguments_assert(arguments, [path_is]);
     const stat = await directory_stat(file_path);
-    const is_directory = stat.isDirectory();
-    return is_directory;
+    const size = stat.size;
+    return size;
     metadata([]);
 }
