9bf67f46a4ea5d66bad721baf3be7961441fb990
JaredMathis <jared.mathis@native.consulting>
2023-06-30 14:03:34 -0400

    #301 function_auto video_compress

--- src/video/compress.mjs
+++ src/video/compress.mjs
@@ -1,20 +1,11 @@
-import { js_identifier_combine } from '../js/identifier/combine.mjs';
-import { command_line_ffmpeg } from '../command/line/ffmpeg.mjs';
-import { video_name_map } from './name/map.mjs';
-import { path_exists } from '../path/exists.mjs';
-import { assert } from '../assert.mjs';
-import { video_path_get } from './path/get.mjs';
+import { video_ffmpeg_generic } from './ffmpeg/generic.mjs';
+import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
 export async function video_compress(video_key) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     const suffix = 'compressed';
     `ffmpeg -i input.mp4 -c copy -map 0 -segment_time 00:20:00 -f segment output%03d.mp4
     `;
     const middle = `-vcodec libx265 -crf 28`;
-    let file_path_input = video_path_get(video_key);
-    assert(await path_exists(file_path_input));
-    let file_path_output = video_name_map(file_path_input, lambda_map);
-    assert(!await path_exists(file_path_output));
-    await command_line_ffmpeg(file_path_input, file_path_output, ``, middle);
-    function lambda_map(name_before) {
-        return js_identifier_combine(name_before, suffix);
-    }
+    await video_ffmpeg_generic(video_key, middle, suffix);
 }
