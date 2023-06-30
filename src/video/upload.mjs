ea4acdef25d25a4d6c73523247aeaf42d4deb58e
JaredMathis <jared.mathis@native.consulting>
2023-06-30 09:08:58 -0400

    #301 function_auto_run video_upload

--- src/video/upload.mjs
+++ src/video/upload.mjs
@@ -1,3 +1,4 @@
+import { file_json_read } from '../file/json/read.mjs';
 import { directory_parent } from '../directory/parent.mjs';
 import { video_screen_recordings_prefix } from './screen/recordings/prefix.mjs';
 import { video_path_get } from './path/get.mjs';
@@ -6,9 +7,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { google } from 'googleapis';
 import { file_extension_json } from '../file/extension/json.mjs';
 import { path_join } from '../path/join.mjs';
-import { directory_size } from '../directory/size.mjs';
 import fs from 'fs';
-import { private_get } from '../private/get.mjs';
 export async function video_upload() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
@@ -20,13 +19,8 @@ export async function video_upload() {
         directory_parent(),
         `oauth2.keys${ file_extension_json() }`
     ]);
-    const auth = new google.auth.GoogleAuth({
-        keyFile: key_path,
-        scopes: [
-            'https://www.googleapis.com/auth/youtube.upload',
-            'https://www.googleapis.com/auth/youtube'
-        ]
-    });
+    let key = await file_json_read(key_path);
+    return key;
     const res = await youtube.videos.insert({
         part: 'id,snippet,status',
         notifySubscribers: false,
