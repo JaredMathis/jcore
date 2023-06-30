d6aecb81343ec8a8c522650da079440c6d070ee1
JaredMathis <jared.mathis@native.consulting>
2023-06-30 14:39:17 -0400

    #301 noop

--- src/video/upload/1.mjs
+++ src/video/upload/1.mjs
@@ -2,7 +2,6 @@ import { video_upload } from '../upload.mjs';
 import { video_screen_recordings_prefix } from '../screen/recordings/prefix.mjs';
 import { js_identifier_combine } from '../../js/identifier/combine.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { publitioApi } from 'publitio_js_sdk';
 export async function video_upload_1() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
--- src/video/upload/single.mjs
+++ src/video/upload/single.mjs
@@ -1,5 +1,6 @@
 import { file_read_bytes } from '../../file/read/bytes.mjs';
 import { private_get } from '../../private/get.mjs';
+import { publitioApi } from 'publitio_js_sdk';
 export async function video_upload_single(first, video_key) {
     const publitio = publitioApi(await private_get(`publit_api_key`), await private_get(`publit_api_secret`));
     const file = await file_read_bytes(first);
