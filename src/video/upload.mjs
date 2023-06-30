79a6c94d1a49bb18e7adccab8c7f260d648b122a
JaredMathis <jared.mathis@native.consulting>
2023-06-30 14:13:36 -0400

    #301 function_auto video_upload

--- src/video/upload.mjs
+++ src/video/upload.mjs
@@ -1,8 +1,6 @@
-import { error } from '../error.mjs';
-import { log } from '../log.mjs';
+import { video_path_prefix_get } from './path/prefix/get.mjs';
 import { file_read_bytes } from '../file/read/bytes.mjs';
 import { video_screen_recordings_prefix } from './screen/recordings/prefix.mjs';
-import { video_path_get } from './path/get.mjs';
 import { js_identifier_combine } from '../js/identifier/combine.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { publitioApi } from 'publitio_js_sdk';
@@ -11,7 +9,7 @@ export async function video_upload() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
     let ish_video_1 = js_identifier_combine(ish_video_prefix, `1`);
-    let ish_video_1_path = video_path_get(ish_video_1);
+    let ish_video_1_path_prefix = video_path_prefix_get(ish_video_1);
     const publitio = publitioApi(await private_get(`publit_api_key`), await private_get(`publit_api_secret`));
     const file = await file_read_bytes(ish_video_1_path);
     return await publitio.uploadFile(file, 'file', {
