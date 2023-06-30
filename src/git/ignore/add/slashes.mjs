ef81866275967ccb15a3b77c1e31b5cedc21a1bd
JaredMathis <jared.mathis@native.consulting>
2023-06-30 18:34:41 -0400

    #308 function_copy git_ignore_add_if_not_exists git_ignore_add_slashes

---
+++ src/git/ignore/add/slashes.mjs
@@ -0,0 +1,15 @@
+import { metadata } from '../../../metadata.mjs';
+import { git_ignore_map } from '../map.mjs';
+import { list_add_if_not_exists } from '../../../list/add/if/not/exists.mjs';
+import { git_ignore_line_slash_add } from '../line/slash/add.mjs';
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export async function git_ignore_add_slashes(gitignore_line_to_add) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    function lambda_map(gitignore_lines) {
+        let slashed = git_ignore_line_slash_add(gitignore_line_to_add);
+        list_add_if_not_exists(gitignore_lines, slashed);
+    }
+    await git_ignore_map(lambda_map);
+    metadata([]);
+}
