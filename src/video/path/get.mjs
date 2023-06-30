897e4da8e839ac9228808c04ab3476efdd9a7599
JaredMathis <jared.mathis@native.consulting>
2023-06-30 14:12:23 -0400

    #301 noop

--- src/video/path/get.mjs
+++ src/video/path/get.mjs
@@ -5,10 +5,10 @@ import { video_path_output } from './output.mjs';
 import { path_join } from '../../path/join.mjs';
 export function video_path_get(video_key) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let file_path_output_name = video_name_get(video_key);
-    let file_path_output = path_join([
+    let video_key_path = path_join([
         video_path_output(),
-        file_path_output_name
+        video_key
     ]);
-    return file_path_output;
+    let file_path_output_name = video_name_get(video_key_path);
+    return file_path_output_name;
 }
