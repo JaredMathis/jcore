cb9a17fa0f22cccdebefb0870a36bc0fcb470320
JaredMathis <jared.mathis@native.consulting>
2023-06-30 15:18:19 -0400

    #301 function_input_add task_comment_video issue_number

--- src/task/comment/video.mjs
+++ src/task/comment/video.mjs
@@ -1,6 +1,7 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function task_comment_video() {
-    arguments_assert(arguments, []);
+export function task_comment_video(issue_number) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: task_comment_video');
 }
