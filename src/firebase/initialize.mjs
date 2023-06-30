e59fe5e102a9740f7a4dc78164d0b25580f853e6
JaredMathis <jared.mathis@native.consulting>
2023-06-30 11:02:06 -0400

    #301 noop

--- src/firebase/initialize.mjs
+++ src/firebase/initialize.mjs
@@ -3,7 +3,7 @@ import { arguments_assert } from '../arguments/assert.mjs';
 import { boolean_value_true } from '../boolean/value/true.mjs';
 import { not } from '../not.mjs';
 import { initializeApp } from 'firebase-admin/app';
-import { cert } from 'firebase-admin/credential';
+import admin from 'firebase-admin/';
 import { file_json_read } from '../file/json/read.mjs';
 let initialized = boolean_value_false();
 export async function firebase_initialize() {
@@ -14,7 +14,7 @@ export async function firebase_initialize() {
         let serviceAccount = await file_json_read(`truthcode-firebase-adminsdk-hcmrs-5fe0e2478e`);
         let v_2 = {
             projectId: v_3,
-            credential: cert(serviceAccount)
+            credential: admin.credential.cert(serviceAccount)
         };
         initializeApp(v_2);
         initialized = boolean_value_true();
