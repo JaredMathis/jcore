2020172b34a058b78dda038a9295c1c3a3969ec3
JaredMathis <jared.mathis@native.consulting>
2023-06-30 10:59:11 -0400

    #301 function_auto database_firestore_get

--- src/database/firestore/get.mjs
+++ src/database/firestore/get.mjs
@@ -1,11 +1,10 @@
 import { firebase_initialize } from '../../firebase/initialize.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { boolean_value_false } from '../../boolean/value/false.mjs';
 import { metadata } from '../../metadata.mjs';
 import { getFirestore } from 'firebase-admin/firestore';
-export function database_firestore_get() {
+export async function database_firestore_get() {
     arguments_assert(arguments, []);
-    firebase_initialize();
+    await firebase_initialize();
     const db = getFirestore();
     return db;
     metadata([]);
