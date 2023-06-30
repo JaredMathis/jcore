0fb1bd534288e6a07e3fde2b9cd4c8497398349a
JaredMathis <jared.mathis@native.consulting>
2023-06-30 14:37:10 -0400

    #301 function_rename_suffix_add video_upload 1

--- src/video/upload/1.mjs
+++ src/video/upload/1.mjs
@@ -1,18 +1,18 @@
-import { string_starts_with } from '../string/starts/with.mjs';
-import { list_filter } from '../list/filter.mjs';
-import { video_path_output } from './path/output.mjs';
-import { video_path_prefix_get } from './path/prefix/get.mjs';
-import { file_read_bytes } from '../file/read/bytes.mjs';
-import { video_screen_recordings_prefix } from './screen/recordings/prefix.mjs';
-import { js_identifier_combine } from '../js/identifier/combine.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
+import { string_starts_with } from '../../string/starts/with.mjs';
+import { list_filter } from '../../list/filter.mjs';
+import { video_path_output } from '../path/output.mjs';
+import { video_path_prefix_get } from '../path/prefix/get.mjs';
+import { file_read_bytes } from '../../file/read/bytes.mjs';
+import { video_screen_recordings_prefix } from '../screen/recordings/prefix.mjs';
+import { js_identifier_combine } from '../../js/identifier/combine.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
 import { publitioApi } from 'publitio_js_sdk';
-import { private_get } from '../private/get.mjs';
-import { directory_read } from '../directory/read.mjs';
-import { string_combine } from '../string/combine.mjs';
-import { function_name_separator } from '../function/name/separator.mjs';
-import { list_first } from '../list/first.mjs';
-export async function video_upload() {
+import { private_get } from '../../private/get.mjs';
+import { directory_read } from '../../directory/read.mjs';
+import { string_combine } from '../../string/combine.mjs';
+import { function_name_separator } from '../../function/name/separator.mjs';
+import { list_first } from '../../list/first.mjs';
+export async function video_upload_1() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
     let video_key = js_identifier_combine(ish_video_prefix, `1`);
