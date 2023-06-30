9fee6b5e7b60ec8f564e529e6ab7840a3de97229
JaredMathis <jared.mathis@native.consulting>
2023-06-30 14:53:08 -0400

    #301 function_auto video_upload

--- src/video/upload.mjs
+++ src/video/upload.mjs
@@ -2,7 +2,6 @@ import { list_map_async } from '../list/map/async.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { video_upload_single } from './upload/single.mjs';
-import { list_first } from '../list/first.mjs';
 import { string_starts_with } from '../string/starts/with.mjs';
 import { list_filter } from '../list/filter.mjs';
 import { function_name_separator } from '../function/name/separator.mjs';
@@ -17,7 +16,5 @@ export async function video_upload(video_key) {
     let matches = list_filter(video_path_output_files, function v(f) {
         return string_starts_with(f, ish_video_1_path_prefix);
     });
-    list_map_async;
-    let first = list_first(matches);
-    return await video_upload_single(first);
+    return await list_map_async(matches, video_upload_single);
 }
