66a98a4d7a4f232b5607907f507751d8758841bc
JaredMathis <jared.mathis@native.consulting>
2023-06-30 12:02:59 -0400

    #301 noop

--- src/video/upload.mjs
+++ src/video/upload.mjs
@@ -10,5 +10,22 @@ export async function video_upload() {
     let ish_video_1 = js_identifier_combine(ish_video_prefix, `1`);
     let ish_video_1_path = video_path_get(ish_video_1);
     const publitio = publitioApi(await private_get(`publit_api_key`), await private_get(`publit_api_secret`));
+
+    const file = readFileSync('image.png')
+
+    publitio.uploadFile(file, 'file')
+      .then(data => { console.log(data) })
+      .catch(error => { console.log(error) })
+    
+    // A more realistic example
+    publitio.uploadFile(file, 'file', {
+      title: '<file title>',
+      description: '<file description>',
+      tags: '<file tags separated by spaces or commas>',
+      privacy: '<0 - private, 1 - public>',
+      option_download: '<0 - disable downloads, 1 - enable downloads>'
+    }).then(data => { console.log(data) })
+      .catch(error => { console.log(error) })
+
     return publitio;
 }
