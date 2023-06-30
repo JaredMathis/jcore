65715e8bce6f3c0357c21b7d89cb4fd86cf65e3c
JaredMathis <jared.mathis@native.consulting>
2023-06-30 12:38:40 -0400

    #301 noop

--- src/path/parse/base.mjs
+++ src/path/parse/base.mjs
@@ -5,8 +5,8 @@ import { path_parse } from '../parse.mjs';
 import { object_property_get } from '../../object/property/get.mjs';
 export function path_parse_base(file_path) {
     arguments_assert(arguments, [path_is]);
-    let parsed = path_parse(file_path);
     let v_2 = 'base';
+    let parsed = path_parse(file_path);
     let v = object_property_get(parsed, v_2);
     return v;
     metadata([]);
