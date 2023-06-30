17f45566984013ce4c21d2a6ed89d768dcef1571
JaredMathis <jared.mathis@native.consulting>
2023-06-30 18:30:55 -0400

    #308 noop

--- src/git/ignore/add/if/not/exists.mjs
+++ src/git/ignore/add/if/not/exists.mjs
@@ -12,16 +12,21 @@ import { string_new_line } from '../../../../../string/new/line.mjs';
 import { string_combine } from '../../../../../string/combine.mjs';
 export async function git_ignore_add_if_not_exists(gitignore_line_to_add) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let slashed = string_combine('/', gitignore_line_to_add);
+    function lambda_map(gitignore_lines_before) {
+        let slashed = string_combine('/', gitignore_line_to_add);
+        let gitignore_lines_after = list_map(gitignore_lines_before, string_trim);
+        list_add_if_not_exists(gitignore_lines_after, slashed);
+        return gitignore_lines_after;
+    }
     let {
         lines: gitignore_lines_before,
         contents: gitignore_contents_old
     } = await git_ignore_lines();
     let gitignore_file_path = git_ignore_path();
-    let gitignore_lines_after = list_map(gitignore_lines_before, string_trim);
-    list_add_if_not_exists(gitignore_lines_after, slashed);
+    let gitignore_lines_after = lambda_map(gitignore_lines_before);
     let v = string_new_line();
     let gitignore_contents_new = list_join(gitignore_lines_after, v);
     await file_overwrite_if_changed(gitignore_file_path, gitignore_contents_new, gitignore_contents_old);
     metadata([]);
 }
+
