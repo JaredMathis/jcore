48c84ee1e7c915b7b44e82750e79de62bc508d5a
JaredMathis <jared.mathis@native.consulting>
2023-06-30 11:05:35 -0400

    #301 noop

--- src/file/name/json.mjs
+++ src/file/name/json.mjs
@@ -2,8 +2,9 @@ import { metadata } from '../../metadata.mjs';
 import { js_identifier_sub_is } from '../../js/identifier/sub/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { file_extension_json } from '../extension/json.mjs';
+import { string_is } from '../../string/is.mjs';
 export function file_name_json(file_name) {
-    arguments_assert(arguments, [js_identifier_sub_is]);
+    arguments_assert(arguments, [string_is]);
     let v = file_extension_json();
     return `${ file_name }${ v }`;
     metadata([]);
