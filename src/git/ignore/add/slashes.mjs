069351ce18659d901699c0d5148cbb26ad532dc6
JaredMathis <jared.mathis@native.consulting>
2023-06-30 18:35:54 -0400

    #308 function_auto git_ignore_add_slashes

--- src/git/ignore/add/slashes.mjs
+++ src/git/ignore/add/slashes.mjs
@@ -1,3 +1,4 @@
+import { list_each_with_index } from '../../../list/each/with/index.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { git_ignore_map } from '../map.mjs';
 import { list_add_if_not_exists } from '../../../list/add/if/not/exists.mjs';
@@ -9,6 +10,7 @@ export async function git_ignore_add_slashes(gitignore_line_to_add) {
     function lambda_map(gitignore_lines) {
         let slashed = git_ignore_line_slash_add(gitignore_line_to_add);
         list_add_if_not_exists(gitignore_lines, slashed);
+        list_each_with_index(gitignore_lines, (value, index));
     }
     await git_ignore_map(lambda_map);
     metadata([]);
