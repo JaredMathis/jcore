ca42e1337b632351e391d277ae92614f6b8f9b15
JaredMathis <jared.mathis@native.consulting>
2023-06-29 20:13:27 -0400

    #301 function_search parent

--- src/video/upload.mjs
+++ src/video/upload.mjs
@@ -3,6 +3,7 @@ import { video_path_get } from './path/get.mjs';
 import { js_identifier_combine } from '../js/identifier/combine.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 import { google } from 'googleapis';
+import { file_extension_json } from '../file/extension/json.mjs';
 export function video_upload() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
@@ -11,7 +12,7 @@ export function video_upload() {
     const youtube = google.youtube('v3');
       // Obtain user credentials to use for the request
   const auth = await authenticate({
-    keyfilePath: `../oauth2.keys.json`,
+    keyfilePath: `../oauth2.keys${file_extension_json()}`,
     scopes: [
       'https://www.googleapis.com/auth/youtube.upload',
       'https://www.googleapis.com/auth/youtube',
