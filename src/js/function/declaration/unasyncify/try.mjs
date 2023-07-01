8d202ff7a137b1fce12240b986940e4c09908603
JaredMathis <jared.mathis@native.consulting>
2023-07-01 10:06:24 -0400

    #310 function_delete js_function_declaration_unasyncify_try

--- src/js/function/declaration/unasyncify/try.mjs
+++
@@ -1,5 +0,0 @@
-export function js_function_declaration_unasyncify_try(fd) {
-    arguments_assert(arguments, [js_node_is_function_declaration]);
-    js_function_declaration_asyncify_generic(fd, object_property_set, true);
-    metadata([]);
-}
