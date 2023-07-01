09f1988ebb5a8d3d153162032759844e3c47c6ef
JaredMathis <jared.mathis@native.consulting>
2023-07-01 10:06:32 -0400

    #310 function_copy js_function_declaration_asyncify_try js_function_declaration_unasyncify_try

---
+++ src/js/function/declaration/unasyncify/try.mjs
@@ -0,0 +1,10 @@
+import { metadata } from '../../../../metadata.mjs';
+import { object_property_set } from '../../../../object/property/set.mjs';
+import { js_function_declaration_asyncify_generic } from '../asyncify/generic.mjs';
+import { js_node_is_function_declaration } from '../../../node/is/function/declaration.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function js_function_declaration_unasyncify_try(fd) {
+    arguments_assert(arguments, [js_node_is_function_declaration]);
+    js_function_declaration_asyncify_generic(fd, object_property_set, true);
+    metadata([]);
+}
