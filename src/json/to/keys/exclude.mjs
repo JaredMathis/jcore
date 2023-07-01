3e45e7de045842f3b429cd784170bf3d27b4bcd3
JaredMathis <jared.mathis@native.consulting>
2023-07-01 10:31:18 -0400

    #310 noop

--- src/json/to/keys/exclude.mjs
+++ src/json/to/keys/exclude.mjs
@@ -5,15 +5,14 @@ import { defined_is } from '../../../defined/is.mjs';
 import { list_is } from '../../../list/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { list_contains } from '../../../list/contains.mjs';
-export function json_to_keys_exclude(value, excluded) {
+export function json_to_keys_exclude(value, include) {
     arguments_assert(arguments, [
         defined_is,
         list_is
     ]);
     let v_4 = function v(key) {
-        let v_2 = list_contains(excluded, key);
-        let v_3 = not(v_2);
-        return v_3;
+        let v_2 = list_contains(include, key);
+        return v_2;
     };
     object_map_generic(value, v_4);
     metadata([]);
