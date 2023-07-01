6e7b5b874811e63c4ad50408794139f820878cd1
JaredMathis <jared.mathis@native.consulting>
2023-07-01 10:14:58 -0400

    #310 noop

--- src/js/function/declaration/unasyncify/try.mjs
+++ src/js/function/declaration/unasyncify/try.mjs
@@ -3,8 +3,9 @@ import { object_property_set } from '../../../../object/property/set.mjs';
 import { js_function_declaration_asyncify_generic } from '../asyncify/generic.mjs';
 import { js_node_is_function_declaration } from '../../../node/is/function/declaration.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
+import { js_node_is } from '../../../node/is.mjs';
 export function js_function_declaration_unasyncify_try(fd) {
-    arguments_assert(arguments, [js_node_is_function_declaration]);
+    arguments_assert(arguments, [js_node_is]);
     js_function_declaration_asyncify_generic(fd, object_property_set, false);
     metadata([]);
 }
--- src/list/map/generic.mjs
+++ src/list/map/generic.mjs
@@ -1,33 +1,32 @@
-import { result_property_data_get } from '../../result/property/data/get.mjs';
-import { defined_is } from '../../defined/is.mjs';
-import { metadata_generated } from '../../metadata/generated.mjs';
 import { metadata } from '../../metadata.mjs';
-import { result_property_success_get } from '../../result/property/success/get.mjs';
-import { assert } from '../../assert.mjs';
-import { result_unsuccess_is } from '../../result/unsuccess/is.mjs';
-import { occurs } from '../../occurs.mjs';
+import { list_adder_async } from '../adder/async.mjs';
+import { list_each_with_index_async } from '../each/with/index/async.mjs';
+import { result_property_data_get } from '../../result/property/data/get.mjs';
 import { not } from '../../not.mjs';
-import { throws_generic } from '../../throws/generic.mjs';
-import { list_each_with_index } from '../each/with/index.mjs';
-import { list_adder } from '../adder.mjs';
+import { occurs } from '../../occurs.mjs';
+import { assert } from '../../assert.mjs';
+import { result_property_success_get } from '../../result/property/success/get.mjs';
+import { result_unsuccess_is } from '../../result/unsuccess/is.mjs';
+import { throws_generic_async } from '../../throws/generic/async.mjs';
+import { defined_is } from '../../defined/is.mjs';
 import { boolean_is } from '../../boolean/is.mjs';
 import { function_is } from '../../function/is.mjs';
 import { list_is } from '../is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function list_map_generic(list, lambda, allow_error_mapping, value_on_error_mapping) {
+export async function list_map_generic(list, lambda, allow_error_mapping, value_on_error_mapping) {
     arguments_assert(arguments, [
         list_is,
         function_is,
         boolean_is,
         defined_is
     ]);
-    let v_10 = function v_4(la) {
-        let v_11 = function v_5(element, index) {
-            let v_12 = function v_6() {
-                let v_2 = lambda(element, index);
+    let v_10 = async function v_4(la) {
+        let v_11 = async function v_5(element, index) {
+            let v_12 = async function v_6() {
+                let v_2 = await lambda(element, index);
                 return v_2;
             };
-            let result = throws_generic(v_12);
+            let result = await throws_generic_async(v_12);
             let mapped;
             let v_14 = function v_7(c) {
                 if (allow_error_mapping) {
@@ -48,9 +47,9 @@ export function list_map_generic(list, lambda, allow_error_mapping, value_on_err
             }
             la(mapped);
         };
-        list_each_with_index(list, v_11);
+        await list_each_with_index_async(list, v_11);
     };
-    let v = list_adder(v_10);
+    let v = await list_adder_async(v_10);
     return v;
-    metadata([metadata_generated()]);
+    metadata([]);
 }
