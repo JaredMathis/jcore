feaa3c77943f9112e3ae138d69a201a5458b6dce
JaredMathis <jared.mathis@native.consulting>
2023-07-01 09:57:07 -0400

    #310 noop

--- src/object/keys/camel/to/snake.mjs
+++ src/object/keys/camel/to/snake.mjs
@@ -4,6 +4,6 @@ import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function object_keys_camel_to_snake(options) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let lambda_map_key = string_case_snake_to_camel;
+    let lambda_map_key = string_case_camel_to_snake;
     return object_keys_to_generic(options, lambda_map_key);
 }
