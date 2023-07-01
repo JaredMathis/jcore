95a37d62938a5a3a10d8fd0cf380f301fd348ce1
JaredMathis <jared.mathis@native.consulting>
2023-07-01 09:44:10 -0400

    #310 database_storage_upload test.txt

--- src/database/storage/upload.mjs
+++ src/database/storage/upload.mjs
@@ -5,6 +5,7 @@ import { object_keys_to_camel } from '../../object/keys/to/camel.mjs';
 import { b2_get } from '../../b2/get.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { object_keys } from '../../object/keys.mjs';
+import { object_property_get } from '../../object/property/get.mjs';
 export async function database_storage_upload(file_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     `
@@ -15,7 +16,8 @@ b2_finish_large_file
     `;
     const b2 = await b2_get();
     let result = await b2.getBucket(object_keys_to_camel(database_storage_bucket_name_object()));
-    return object_keys(result);
+    let data = object_property_get(result, 'data');
+    return data;
     let mapped = object_keys_to_camel({
         bucket_id: database_storage_bucket_name(),
         file_name
