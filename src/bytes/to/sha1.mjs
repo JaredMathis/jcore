19101fc3964cc6814592721fffea5a4d1b72c078
JaredMathis <jared.mathis@native.consulting>
2023-07-01 11:48:22 -0400

    #310 function_auto bytes_to_sha1

--- src/bytes/to/sha1.mjs
+++ src/bytes/to/sha1.mjs
@@ -1,6 +1,10 @@
-import { error } from '../../error.mjs';
+import crypto from 'crypto';
+import fs from 'fs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function bytes_to_sha1() {
     arguments_assert(arguments, []);
-    error('todo: bytes_to_sha1');
+    const fileBuffer = fs.readFileSync('myfile.js');
+    const hashSum = crypto.createHash('sha256');
+    hashSum.update(fileBuffer);
+    const hex = hashSum.digest('hex');
 }
