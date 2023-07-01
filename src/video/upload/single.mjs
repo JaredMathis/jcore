0e9b310620ca229de3a86f7e0ca2874aad70e544
JaredMathis <jared.mathis@native.consulting>
2023-07-01 13:19:20 -0400

    #310 function_copy video_upload_publit_single video_upload_single

---
+++ src/video/upload/single.mjs
@@ -0,0 +1,23 @@
+import { file_read_bytes } from '../../file/read/bytes.mjs';
+import { private_get } from '../../private/get.mjs';
+import { path_parse_name } from '../../path/parse/name.mjs';
+import { video_upload_publit_single } from './publit/single.mjs';
+import { git_hub_cached } from '../../git/hub/cached.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export async function video_upload_single(video_path) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    return await git_hub_cached(video_upload_publit_single, [video_path], lambda);
+    async function lambda() {
+        let name = path_parse_name(video_path);
+        const publitio = publitioApi(await private_get(`publit_api_key`), await private_get(`publit_api_secret`));
+        const file = await file_read_bytes(video_path);
+        return await publitio.uploadFile(file, 'file', {
+            title: name,
+            description: name,
+            tags: '',
+            privacy: '1',
+            option_download: '0'
+        });
+    }
+}
