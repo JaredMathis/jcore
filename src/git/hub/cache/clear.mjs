798ba9ade527ede3e352c782d1d8550f78a57b8f
JaredMathis <jared.mathis@native.consulting>
2023-07-01 14:51:23 -0400

    #310 function_auto git_hub_cache_clear

--- src/git/hub/cache/clear.mjs
+++ src/git/hub/cache/clear.mjs
@@ -1,3 +1,4 @@
+import { file_paths_to_name_base64 } from '../../../file/paths/to/name/base64.mjs';
 import { git_ignore_cache_file_paths } from '../../ignore/cache/file/paths.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { git_hub_cache_filter } from './filter.mjs';
@@ -6,5 +7,6 @@ export async function git_hub_cache_clear(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let result = await git_hub_cache_filter(function_name);
     let file_paths = await git_ignore_cache_file_paths();
+    file_paths_to_name_base64;
     return result;
 }
