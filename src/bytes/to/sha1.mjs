7301b2f8b9fd7b8568051e3c282c0bc116870bdd
JaredMathis <jared.mathis@native.consulting>
2023-07-01 11:47:41 -0400

    #310 function_auto bytes_to_sha1

---
+++ src/bytes/to/sha1.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function bytes_to_sha1() {
+    arguments_assert(arguments, []);
+    error('todo: bytes_to_sha1');
+}
