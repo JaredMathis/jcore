0fe00a9d9fa1cd73d10c86e10d8f15e79314b871
JaredMathis <jared.mathis@native.consulting>
2023-07-01 09:26:02 -0400

    #310 function_auto_run database_storage_upload

--- src/database/storage/upload.mjs
+++ src/database/storage/upload.mjs
@@ -1,6 +1,5 @@
 import { object_merge } from '../../object/merge.mjs';
 import { database_storage_bucket_name_object } from './bucket/name/object.mjs';
-import { database_storage_bucket_name } from './bucket/name.mjs';
 import { object_keys_to_camel } from '../../object/keys/to/camel.mjs';
 import { b2_get } from '../../b2/get.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -13,12 +12,9 @@ b2_upload_part or b2_copy_part (for each part of the file)
 b2_finish_large_file
     `;
     const b2 = await b2_get();
-    b2.startLargeFile(database_storage_bucket_name_object());
-    const options = {
-        bucket_name: database_storage_bucket_name(),
-        bucket_type: 'allPublic'
-    };
-    object_merge;
+    const options = object_merge(database_storage_bucket_name_object(), { bucket_type: 'allPublic' });
     let mapped = object_keys_to_camel(options);
+    return mapped;
+    b2.startLargeFile(database_storage_bucket_name_object());
     return await b2.createBucket(mapped);
 }
