265c0615a28037721c138dedd5d4c430870fe439
JaredMathis <jared.mathis@native.consulting>
2023-07-01 15:17:00 -0400

    #310 function_auto video_upload_1

--- src/video/upload/1.mjs
+++ src/video/upload/1.mjs
@@ -1,3 +1,4 @@
+import { string_to } from '../../string/to.mjs';
 import { video_upload_publit } from './publit.mjs';
 import { video_segment } from '../segment.mjs';
 import { js_identifier_combine } from '../../js/identifier/combine.mjs';
@@ -6,7 +7,7 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 export async function video_upload_1() {
     arguments_assert(arguments, []);
     let ish_video_prefix = video_screen_recordings_prefix();
-    let video_key = js_identifier_combine(ish_video_prefix, `1`);
+    let video_key = js_identifier_combine(ish_video_prefix, string_to(1));
     await video_segment(video_key);
     return await video_upload_publit(video_key);
 }
