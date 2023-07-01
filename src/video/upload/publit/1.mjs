7d5a7e9c50b88617df329b55c89ee3d4917c4b22
JaredMathis <jared.mathis@native.consulting>
2023-07-01 12:30:32 -0400

    #310 function_rename_single video_upload_1 video_upload_publit_1

---
+++ src/video/upload/publit/1.mjs
@@ -0,0 +1,12 @@
+import { video_segment } from '../../segment.mjs';
+import { video_upload_publit } from '../publit.mjs';
+import { video_screen_recordings_prefix } from '../../screen/recordings/prefix.mjs';
+import { js_identifier_combine } from '../../../js/identifier/combine.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export async function video_upload_publit_1() {
+    arguments_assert(arguments, []);
+    let ish_video_prefix = video_screen_recordings_prefix();
+    let video_key = js_identifier_combine(ish_video_prefix, `2`);
+    await video_segment(video_key);
+    return await video_upload_publit(video_key);
+}
