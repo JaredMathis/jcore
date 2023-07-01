7f1a87f0c35b27dbc733c3ba45815f23a61289dc
JaredMathis <jared.mathis@native.consulting>
2023-07-01 13:23:29 -0400

    #310 noop

--- src/video/upload/single.mjs
+++ src/video/upload/single.mjs
@@ -4,7 +4,6 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export async function video_upload_single(video_path) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    return video_path;
     return await git_hub_cached(video_upload_single, [video_path], lambda);
     async function lambda() {
         await database_storage_upload(video_path, video_path);
