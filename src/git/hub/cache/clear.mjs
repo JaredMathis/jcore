4e9618f9bf7e483aa5db58587f181ec695844e07
JaredMathis <jared.mathis@native.consulting>
2023-07-01 14:35:14 -0400

    #310 git_hub_cache_clear

--- src/git/hub/cache/clear.mjs
+++ src/git/hub/cache/clear.mjs
@@ -1,7 +1,10 @@
 import { git_hub_cache_get } from './get.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { list_map } from '../../../list/map.mjs';
+import { json_from } from '../../../json/from.mjs';
 export async function git_hub_cache_clear() {
     arguments_assert(arguments, []);
     let result = await git_hub_cache_get();
-    return result;
+    let mapped = list_map(result, json_from);
+    return mapped;
 }
