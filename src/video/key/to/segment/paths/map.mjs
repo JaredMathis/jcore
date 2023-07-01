fc2d64afb3dfc11298fd9484813e32247bdf4cd2
JaredMathis <jared.mathis@native.consulting>
2023-07-01 13:02:01 -0400

    #310 function_delete video_key_to_segment_paths_map

--- src/video/key/to/segment/paths/map.mjs
+++
@@ -1,7 +0,0 @@
-import { video_upload_publit_single } from '../../../../upload/publit/single.mjs';
-import { list_map_async } from '../../../../../list/map/async.mjs';
-import { video_key_to_segment_paths } from '../paths.mjs';
-export async function video_key_to_segment_paths_map(video_key) {
-    let matches = await video_key_to_segment_paths(video_key);
-    return await list_map_async(matches, video_upload_publit_single);
-}
--- src/video/upload/publit.mjs
+++ src/video/upload/publit.mjs
@@ -1,7 +1,13 @@
-import { video_key_to_segment_paths_map } from '../key/to/segment/paths/map.mjs';
+import { video_key_to_segment_paths } from '../key/to/segment/paths.mjs';
+import { list_map_async } from '../../list/map/async.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { video_upload_publit_single } from './publit/single.mjs';
 export async function video_upload_publit(video_key) {
     arguments_assert(arguments, [arguments_assert_todo]);
     return await video_key_to_segment_paths_map(video_key);
+}
+async function video_key_to_segment_paths_map(video_key) {
+    let matches = await video_key_to_segment_paths(video_key);
+    return await list_map_async(matches, video_upload_publit_single);
 }
