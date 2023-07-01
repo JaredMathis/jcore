ede91014504769896da705378872e6e4dfaad4fd
JaredMathis <jared.mathis@native.consulting>
2023-07-01 14:50:59 -0400

    #310 function_rename file_path_to_name_base64 file_paths_to_name_base64

--- src/file/paths/to/name/base64.mjs
+++ src/file/paths/to/name/base64.mjs
@@ -1,7 +1,7 @@
 import { string_base64_from } from '../../../../string/base64/from.mjs';
 import { path_parse_name } from '../../../../path/parse/name.mjs';
 import { list_map } from '../../../../list/map.mjs';
-export function file_path_to_name_base64(file_paths) {
+export function file_paths_to_name_base64(file_paths) {
     let mapped = list_map(file_paths, path_parse_name);
     let mapped2 = list_map(mapped, string_base64_from);
     return mapped2;
