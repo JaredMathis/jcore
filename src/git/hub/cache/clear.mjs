6f3d557d0be80a68a1852bef7534f24d45be6a77
JaredMathis <jared.mathis@native.consulting>
2023-07-01 14:56:38 -0400

    #310 noop

--- src/git/hub/cache/clear.mjs
+++ src/git/hub/cache/clear.mjs
@@ -10,7 +10,6 @@ import { string_base64_from } from '../../../string/base64/from.mjs';
 export async function git_hub_cache_clear(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let filtered = await git_hub_cache_filter(function_name);
-    console.log({filtered})
     let file_paths = await git_ignore_cache_file_paths();
     let file_paths_filtered = list_filter(file_paths, function v(f) {
         let b64 = list_single(file_paths_to_name_base64([f]));
