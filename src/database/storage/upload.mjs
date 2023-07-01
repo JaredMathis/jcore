a6bd8c2e7aafa039ce4e4705e72e7e8136767ded
JaredMathis <jared.mathis@native.consulting>
2023-07-01 09:39:18 -0400

    #310 function_input_add database_storage_upload file_name

--- src/database/storage/upload.mjs
+++ src/database/storage/upload.mjs
@@ -1,10 +1,10 @@
-import { object_merge } from '../../object/merge.mjs';
-import { database_storage_bucket_name_object } from './bucket/name/object.mjs';
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
+import { database_storage_bucket_name } from './bucket/name.mjs';
 import { object_keys_to_camel } from '../../object/keys/to/camel.mjs';
 import { b2_get } from '../../b2/get.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export async function database_storage_upload() {
-    arguments_assert(arguments, []);
+export async function database_storage_upload(file_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     `
     b2_start_large_file
 b2_get_upload_part_url (for each thread that are are uploading)
@@ -12,9 +12,6 @@ b2_upload_part or b2_copy_part (for each part of the file)
 b2_finish_large_file
     `;
     const b2 = await b2_get();
-    const options = object_merge(database_storage_bucket_name_object(), { bucket_type: 'allPublic' });
-    let mapped = object_keys_to_camel(options);
-    return mapped;
-    b2.startLargeFile(database_storage_bucket_name_object());
-    return await b2.createBucket(mapped);
+    let mapped = object_keys_to_camel({ bucket_id: database_storage_bucket_name() });
+    return await b2.startLargeFile(mapped);
 }
