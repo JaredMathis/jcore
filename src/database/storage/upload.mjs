64334023911bf610873b44aeaf13e7e270b89756
JaredMathis <jared.mathis@native.consulting>
2023-07-01 09:18:58 -0400

    #310 function_auto database_storage_upload

--- src/database/storage/upload.mjs
+++ src/database/storage/upload.mjs
@@ -1,11 +1,19 @@
-import { error } from '../../error.mjs';
+import { object_keys_to_camel } from '../../object/keys/to/camel.mjs';
+import { b2_get } from '../../b2/get.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function database_storage_upload() {
+export async function database_storage_upload() {
     arguments_assert(arguments, []);
     `
     b2_start_large_file
 b2_get_upload_part_url (for each thread that are are uploading)
 b2_upload_part or b2_copy_part (for each part of the file)
 b2_finish_large_file
-    `
+    `;
+    const b2 = await b2_get();
+    const options = {
+        bucket_name: 'truthcode',
+        bucket_type: 'allPublic'
+    };
+    let mapped = object_keys_to_camel(options);
+    return await b2.createBucket(mapped);
 }
