51a60ed313686d6a021e39ce01120c0428bdc8c7
JaredMathis <jared.mathis@native.consulting>
2023-07-01 10:29:53 -0400

    #310 function_auto json_to_keys_exclude

--- src/json/to/keys/exclude.mjs
+++ src/json/to/keys/exclude.mjs
@@ -1,9 +1,9 @@
+import { object_map_generic } from '../../../object/map/generic.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { not } from '../../../not.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 import { list_is } from '../../../list/is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { object_merge_filtered } from '../../../object/merge/filtered.mjs';
 import { list_contains } from '../../../list/contains.mjs';
 export function json_to_keys_exclude(value, excluded) {
     arguments_assert(arguments, [
@@ -15,7 +15,6 @@ export function json_to_keys_exclude(value, excluded) {
         let v_3 = not(v_2);
         return v_3;
     };
-    let v_5 = {};
-    object_merge_filtered(value, v_4, v_5);
+    object_map_generic(value, v_4);
     metadata([]);
 }
