fb07d3969e37283e90d3db90db62050c440e56c5
JaredMathis <jared.mathis@native.consulting>
2023-07-01 08:05:42 -0400

    #311 function_auto database_storage_bucket_create

---
+++ src/database/storage/bucket/create.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../error.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
+export function database_storage_bucket_create() {
+    arguments_assert(arguments, []);
+    error('todo: database_storage_bucket_create');
+}
