a4e48c30ef55bd634008bc0b0a37a4bc8eded081
JaredMathis <jared.mathis@native.consulting>
2023-07-01 09:43:20 -0400

    #310 function_auto database_storage_upload

--- src/database/storage/upload.mjs
+++ src/database/storage/upload.mjs
@@ -13,7 +13,7 @@ b2_upload_part or b2_copy_part (for each part of the file)
 b2_finish_large_file
     `;
     const b2 = await b2_get();
-    return b2.getBucket(object_keys_to_camel(database_storage_bucket_name_object()));
+    return await b2.getBucket(object_keys_to_camel(database_storage_bucket_name_object()));
     let mapped = object_keys_to_camel({
         bucket_id: database_storage_bucket_name(),
         file_name
