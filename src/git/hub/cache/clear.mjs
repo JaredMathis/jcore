881ba7b513192f1b03e8a23fb10d28362803d6ae
JaredMathis <jared.mathis@native.consulting>
2023-07-01 14:37:15 -0400

    #310 noop

--- src/git/hub/cache/clear.mjs
+++ src/git/hub/cache/clear.mjs
@@ -4,12 +4,14 @@ import { arguments_assert } from '../../../arguments/assert.mjs';
 import { json_from } from '../../../json/from.mjs';
 import { list_filter } from '../../../list/filter.mjs';
 import { list_first } from '../../../list/first.mjs';
+import { equal } from '../../../equal.mjs';
 export async function git_hub_cache_clear(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let result = await git_hub_cache_get();
     let filtered = list_filter(result, r => {
         let jf = json_from(r);
-        let function_name = list_first(jf);
+        let function_name_actual = list_first(jf);
+        return equal(function_name_actual, function_name);
     });
     return filtered;
 }
