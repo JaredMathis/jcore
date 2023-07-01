e10238917bea5f02400cae58adbd04c95d1da2ff
JaredMathis <jared.mathis@native.consulting>
2023-07-01 12:22:41 -0400

    #310 database_storage_upload goals.txt goals.txt

--- src/database/storage/upload.mjs
+++ src/database/storage/upload.mjs
@@ -15,7 +15,7 @@ export async function database_storage_upload(file_name, file_path) {
         arguments_assert_todo
     ]);
     const data = await file_read_bytes(file_path);
-    return data;
+    return data.length;
     let hash = bytes_to_sha1(data);
     `
     b2_start_large_file
