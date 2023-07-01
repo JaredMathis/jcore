0bc3f33590cb04ce83d954660a1f6c4d4f743fc9
JaredMathis <jared.mathis@native.consulting>
2023-07-01 10:19:41 -0400

    #310 function_auto object_keys_camel_to_snake

--- src/object/keys/camel/to/snake.mjs
+++ src/object/keys/camel/to/snake.mjs
@@ -1,9 +1,9 @@
+import { string_case_pascal_to_snake } from '../../../../string/case/pascal/to/snake.mjs';
 import { object_keys_to_generic } from '../../to/generic.mjs';
-import { string_case_snake_to_camel } from '../../../../string/case/snake/to/camel.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function object_keys_camel_to_snake(options) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let lambda_map_key = string_case_camel_to_snake;
+    let lambda_map_key = string_case_pascal_to_snake;
     return object_keys_to_generic(options, lambda_map_key);
 }
