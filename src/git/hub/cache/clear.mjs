2db26c6dd628dadbebf4b232d823698288a3683f
JaredMathis <jared.mathis@native.consulting>
2023-07-01 14:36:48 -0400

    #310 function_input_add git_hub_cache_clear function_name

--- src/git/hub/cache/clear.mjs
+++ src/git/hub/cache/clear.mjs
@@ -1,13 +1,15 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { git_hub_cache_get } from './get.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { list_map } from '../../../list/map.mjs';
 import { json_from } from '../../../json/from.mjs';
 import { list_filter } from '../../../list/filter.mjs';
-export async function git_hub_cache_clear() {
-    arguments_assert(arguments, []);
+import { list_first } from '../../../list/first.mjs';
+export async function git_hub_cache_clear(function_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let result = await git_hub_cache_get();
     let filtered = list_filter(result, r => {
         let jf = json_from(r);
+        let function_name = list_first(jf);
     });
     return filtered;
 }
