8595ec0a8cd027d9cbfa265d7578fe85e14fef79
JaredMathis <jared.mathis@native.consulting>
2023-07-01 14:53:06 -0400

    #310 function_auto git_hub_cache_clear

--- src/git/hub/cache/clear.mjs
+++ src/git/hub/cache/clear.mjs
@@ -3,10 +3,16 @@ import { git_ignore_cache_file_paths } from '../../ignore/cache/file/paths.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { git_hub_cache_filter } from './filter.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { list_filter } from '../../../list/filter.mjs';
+import { list_single } from '../../../list/single.mjs';
+import { list_contains } from '../../../list/contains.mjs';
 export async function git_hub_cache_clear(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let result = await git_hub_cache_filter(function_name);
+    let filtered = await git_hub_cache_filter(function_name);
     let file_paths = await git_ignore_cache_file_paths();
-    file_paths_to_name_base64;
-    return result;
+    let file_paths_filtered = list_filter(file_paths, function v(f) {
+        let b64 = list_single(file_paths_to_name_base64([f]));
+        list_contains(filtered, b64);
+    });
+    return file_paths_filtered;
 }
