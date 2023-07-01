2dc5f8dc7c76f688c264cd1e660ff4d9f1d4b652
JaredMathis <jared.mathis@native.consulting>
2023-07-01 09:18:01 -0400

    #310 function_auto database_storage_bucket_create

---
+++ src/b2/get.mjs
@@ -0,0 +1,9 @@
+import { private_get } from '../private/get.mjs';
+export async function b2_get() {
+    const b2 = new B2({
+        applicationKeyId: await private_get('b2_key_id'),
+        applicationKey: await private_get('b2_application_key')
+    });
+    await b2.authorize();
+    return b2;
+}
--- src/database/storage/bucket/create.mjs
+++ src/database/storage/bucket/create.mjs
@@ -1,15 +1,10 @@
+import { b2_get } from '../../../b2/get.mjs';
 import { object_keys_to_camel } from '../../../object/keys/to/camel.mjs';
-import { log } from '../../../log.mjs';
-import { private_get } from '../../../private/get.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import B2 from 'backblaze-b2';
 export async function database_storage_bucket_create() {
     arguments_assert(arguments, []);
-    const b2 = new B2({
-        applicationKeyId: await private_get('b2_key_id'),
-        applicationKey: await private_get('b2_application_key')
-    });
-    await b2.authorize();
+    const b2 = await b2_get();
     const options = {
         bucket_name: 'truthcode',
         bucket_type: 'allPublic'
