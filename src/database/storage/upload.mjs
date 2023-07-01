dc2e66da12c306b0f48f7d902c3b46f0d9317401
JaredMathis <jared.mathis@native.consulting>
2023-07-01 09:39:52 -0400

    #310 function_auto database_storage_upload

--- src/database/storage/upload.mjs
+++ src/database/storage/upload.mjs
@@ -12,6 +12,9 @@ b2_upload_part or b2_copy_part (for each part of the file)
 b2_finish_large_file
     `;
     const b2 = await b2_get();
-    let mapped = object_keys_to_camel({ bucket_id: database_storage_bucket_name() });
+    let mapped = object_keys_to_camel({
+        bucket_id: database_storage_bucket_name(),
+        file_name
+    });
     return await b2.startLargeFile(mapped);
 }
