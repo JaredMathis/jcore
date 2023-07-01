470f9a3d76cc17e02d0beea0932ea0a62913113b
JaredMathis <jared.mathis@native.consulting>
2023-07-01 08:38:53 -0400

    #311 function_copy string_case_upper_character_first string_case_lower_character_first

---
+++ src/string/case/lower/character/first.mjs
@@ -0,0 +1,11 @@
+import { metadata } from '../../../../metadata.mjs';
+import { string_character_first_generic } from '../../../character/first/generic.mjs';
+import { string_to_case_upper } from '../../../to/case/upper.mjs';
+import { string_is } from '../../../is.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function string_case_lower_character_first(value) {
+    arguments_assert(arguments, [string_is]);
+    let lambda_map_first = string_to_case_upper;
+    return string_character_first_generic(value, lambda_map_first);
+    metadata([]);
+}
