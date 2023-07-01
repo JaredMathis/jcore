95f02550ab8d93abb03722784d47098b22e30de5
JaredMathis <jared.mathis@native.consulting>
2023-07-01 12:04:47 -0400

    #310 function_auto bytes_to_sha1

--- src/bytes/to/sha1.mjs
+++ src/bytes/to/sha1.mjs
@@ -1,6 +1,5 @@
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import crypto from 'crypto';
-import fs from 'fs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function bytes_to_sha1(buffer) {
     arguments_assert(arguments, [arguments_assert_todo]);
