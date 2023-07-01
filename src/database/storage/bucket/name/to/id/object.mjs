483578c0517c9310c6985091e644d75973937b22
JaredMathis <jared.mathis@native.consulting>
2023-07-01 10:37:40 -0400

    #310 function_auto database_storage_bucket_name_to_id_object

--- src/database/storage/bucket/name/to/id/object.mjs
+++ src/database/storage/bucket/name/to/id/object.mjs
@@ -1,3 +1,6 @@
+import { b2_property_data_get } from '../../../../../../b2/property/data/get.mjs';
+import { arguments_assert_todo } from '../../../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../../../arguments/assert.mjs';
 import { object_keys_include } from '../../../../../../object/keys/include.mjs';
 import { object_keys_camel_to_snake } from '../../../../../../object/keys/camel/to/snake.mjs';
 import { list_single } from '../../../../../../list/single.mjs';
@@ -5,8 +8,9 @@ import { object_property_get } from '../../../../../../object/property/get.mjs';
 import { database_storage_bucket_name_object } from '../../object.mjs';
 import { object_keys_to_camel } from '../../../../../../object/keys/to/camel.mjs';
 export async function database_storage_bucket_name_to_id_object(b2) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     let result = await b2.getBucket(object_keys_to_camel(database_storage_bucket_name_object()));
-    let data = object_property_get(result, 'data');
+    let data = b2_property_data_get(result);
     let buckets = object_property_get(data, 'buckets');
     let bucket = list_single(buckets);
     let mapped2 = object_keys_camel_to_snake(bucket);
