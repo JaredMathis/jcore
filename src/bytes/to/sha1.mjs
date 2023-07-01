3c2ea0c533103718b08645b8a4008421f17ef7de
JaredMathis <jared.mathis@native.consulting>
2023-07-01 11:48:43 -0400

    #310 function_input_add bytes_to_sha1 buffer

--- src/bytes/to/sha1.mjs
+++ src/bytes/to/sha1.mjs
@@ -1,8 +1,9 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import crypto from 'crypto';
 import fs from 'fs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function bytes_to_sha1() {
-    arguments_assert(arguments, []);
+export function bytes_to_sha1(buffer) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     const fileBuffer = fs.readFileSync('myfile.js');
     const hashSum = crypto.createHash('sha256');
     hashSum.update(fileBuffer);
