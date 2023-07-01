92d18cfa6e70d16953fa526ecbbf0523e248f517
JaredMathis <jared.mathis@native.consulting>
2023-07-01 09:49:20 -0400

    #310 function_auto object_keys_to_camel

--- src/object/keys/to/camel.mjs
+++ src/object/keys/to/camel.mjs
@@ -1,10 +1,14 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { object_property_initialize } from '../../property/initialize.mjs';
 import { string_case_snake_to_camel } from '../../../string/case/snake/to/camel.mjs';
 import { object_keys_each } from '../each.mjs';
 export function object_keys_to_camel(options) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    let lambda_map_key = string_case_snake_to_camel;
     let mapped = {};
     object_keys_each(options, function v(value, key) {
-        let key_mapped = string_case_snake_to_camel(key);
+        let key_mapped = lambda_map_key(key);
         object_property_initialize(mapped, key_mapped, value);
     });
     return mapped;
