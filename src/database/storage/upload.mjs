e043188ef56cf55c4b75f24f23462dfdf68a5bf9
JaredMathis <jared.mathis@native.consulting>
2023-07-01 10:38:18 -0400

    #310 function_auto database_storage_upload

--- src/database/storage/upload.mjs
+++ src/database/storage/upload.mjs
@@ -1,7 +1,7 @@
+import { b2_property_data_get } from '../../b2/property/data/get.mjs';
 import { object_merge } from '../../object/merge.mjs';
 import { database_storage_bucket_name_to_id_object } from './bucket/name/to/id/object.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
-import { database_storage_bucket_name } from './bucket/name.mjs';
 import { object_keys_to_camel } from '../../object/keys/to/camel.mjs';
 import { b2_get } from '../../b2/get.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
@@ -15,9 +15,7 @@ b2_finish_large_file
     `;
     const b2 = await b2_get();
     let mapped = await database_storage_bucket_name_to_id_object(b2);
-    const options = {
-        file_name
-    };
+    const options = { file_name };
     let camel = object_keys_to_camel(object_merge(mapped, options));
     let result = await b2.startLargeFile(camel);
     let data = b2_property_data_get(result);
