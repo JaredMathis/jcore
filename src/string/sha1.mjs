254985b630785bf695a07a2ec74c8f128418f431
JaredMathis <jared.mathis@native.consulting>
2023-07-01 11:47:22 -0400

    #310 function_delete string_sha1

--- src/string/sha1.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../error.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
-export function string_sha1() {
-    arguments_assert(arguments, []);
-    error('todo: string_sha1');
-}
