a9f8b06ba57f0aad13c99b9bf31b879a43d31413
JaredMathis <jared.mathis@native.consulting>
2023-07-01 14:49:54 -0400

    #310 function_auto git_hub_cache_get

--- src/git/hub/cache/get.mjs
+++ src/git/hub/cache/get.mjs
@@ -1,12 +1,9 @@
+import { file_path_to_name_base64 } from '../../../file/path/to/name/base64.mjs';
 import { git_ignore_cache_file_paths } from '../../ignore/cache/file/paths.mjs';
-import { string_base64_from } from '../../../string/base64/from.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
-import { list_map } from '../../../list/map.mjs';
-import { path_parse_name } from '../../../path/parse/name.mjs';
 export async function git_hub_cache_get() {
     arguments_assert(arguments, []);
     let file_paths = await git_ignore_cache_file_paths();
-    let mapped = list_map(file_paths, path_parse_name);
-    let mapped2 = list_map(mapped, string_base64_from);
+    let mapped2 = file_path_to_name_base64(file_paths);
     return mapped2;
 }
