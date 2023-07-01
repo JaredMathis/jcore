a863e5b326831236f92fc4e397cb5aa1832f7df2
JaredMathis <jared.mathis@native.consulting>
2023-07-01 13:01:39 -0400

    #310 function_auto video_upload_publit

--- src/video/upload/publit.mjs
+++ src/video/upload/publit.mjs
@@ -1,10 +1,7 @@
-import { video_key_to_segment_paths } from '../key/to/segment/paths.mjs';
-import { list_map_async } from '../../list/map/async.mjs';
+import { video_key_to_segment_paths_map } from '../key/to/segment/paths/map.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { video_upload_publit_single } from './publit/single.mjs';
 export async function video_upload_publit(video_key) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let matches = await video_key_to_segment_paths(video_key);
-    return await list_map_async(matches, video_upload_publit_single);
+    return await video_key_to_segment_paths_map(video_key);
 }
