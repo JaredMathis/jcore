6c5125261a3d73f9f627562e8a78e944f7ab295a
JaredMathis <jared.mathis@native.consulting>
2023-07-02 15:25:21 -0400

    #312 noop

--- src/task/git/hub/issues/compare.mjs
+++ src/task/git/hub/issues/compare.mjs
@@ -18,21 +18,13 @@ export async function task_git_hub_issues_compare() {
     let all = await file_json_read(all_path);
     let all_2 = await file_json_read(all_2_path);
     const number = 'number';
-    let all_2_numbers = list_map_property(all_2, number);
-    list_sort(all_2_numbers);
     for (let a of all) {
         const number_value = object_property_get(a, number);
         let a_2 = list_find_property(all_2, number, number_value);
         const property_name = task_property_state();
         if (!equal_by_property(property_name, a, a_2)) {
             let property_value = object_property_get(a, property_name);
-            log({
-                number_value,
-                property_value
-            });
-            if (false) {
-                await git_hub_repository_issue_state_generic(number_value, property_value);
-            }
+            await git_hub_repository_issue_state_generic(number_value, property_value);
         }
     }
 }
