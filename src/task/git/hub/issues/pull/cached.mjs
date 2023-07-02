141b2ec04749063e35c5565d9e07213d1c68ecaa
JaredMathis <jared.mathis@native.consulting>
2023-07-02 10:56:18 -0400

    #312 noop

--- src/task/git/hub/issues/pull/cached.mjs
+++ src/task/git/hub/issues/pull/cached.mjs
@@ -6,8 +6,9 @@ import { task_git_hub_issues_pull_generic } from './generic.mjs';
 import { arguments_assert } from '../../../../../arguments/assert.mjs';
 export async function task_git_hub_issues_pull_cached() {
     arguments_assert(arguments, []);
+    let lambda = version_path_tasks_all_get;
     const no_cache = boolean_value_false();
-    let v = await task_git_hub_issues_pull_generic(no_cache, version_path_tasks_all_get(version_repository_default()));
+    let v = await task_git_hub_issues_pull_generic(no_cache, lambda(version_repository_default()));
     return v;
     metadata([]);
 }
