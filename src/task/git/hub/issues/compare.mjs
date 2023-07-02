ed9dd24021adf901c8d599044c2c0bd2110f8cdb
JaredMathis <jared.mathis@native.consulting>
2023-07-02 12:20:51 -0400

    #312 task_git_hub_issues_compare

--- src/task/git/hub/issues/compare.mjs
+++ src/task/git/hub/issues/compare.mjs
@@ -3,10 +3,14 @@ import { version_repository_default } from '../../../../version/repository/defau
 import { version_path_tasks_all_2_get } from '../../../../version/path/tasks/all/2/get.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { file_json_read } from '../../../../file/json/read.mjs';
+import { log } from '../../../../log.mjs';
 export async function task_git_hub_issues_compare() {
     arguments_assert(arguments, []);
     let all_2_path = version_path_tasks_all_2_get(version_repository_default());
     let all_path = version_path_tasks_all_get(version_repository_default());
     let all = await file_json_read(all_path);
     let all_2 = await file_json_read(all_2_path);
+    for (let a of all) {
+        log(a);
+    }
 }
