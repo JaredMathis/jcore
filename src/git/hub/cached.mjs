3dd341604aa7645242843963117d74d496ff7598
JaredMathis <jared.mathis@native.consulting>
2023-07-02 12:42:43 -0400

    #312 function_auto git_hub_cached

--- src/git/hub/cached.mjs
+++ src/git/hub/cached.mjs
@@ -1,10 +1,5 @@
+import { git_hub_cached_generic } from './cached/generic.mjs';
 import { metadata } from '../../metadata.mjs';
-import { file_json_write } from '../../file/json/write.mjs';
-import { file_json_read } from '../../file/json/read.mjs';
-import { file_exists } from '../../file/exists.mjs';
-import { git_hub_cache_file_path } from './cache/file/path.mjs';
-import { git_ignore_cache } from '../ignore/cache.mjs';
-import { git_ignore_add_if_not_exists } from '../ignore/add/if/not/exists.mjs';
 import { defined_is } from '../../defined/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { function_is } from '../../function/is.mjs';
@@ -14,16 +9,7 @@ export async function git_hub_cached(fn, args, lambda) {
         defined_is,
         function_is
     ]);
-    let v = git_ignore_cache();
-    await git_ignore_add_if_not_exists(v);
-    let file_path = git_hub_cache_file_path(fn, args);
-    let v_2 = await file_exists(file_path);
-    if (v_2) {
-        let result = await file_json_read(file_path);
-        return result;
-    }
-    let result = await lambda();
-    await file_json_write(file_path, result);
-    return result;
+    let no_cache = false;
+    return await git_hub_cached_generic(no_cache, fn, args, lambda);
     metadata([]);
 }
