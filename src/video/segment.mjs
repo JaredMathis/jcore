af9645e5fdcca5690cf025d7b83fbc24e006851d
JaredMathis <jared.mathis@native.consulting>
2023-07-01 23:53:00 -0400

    #310 function_auto video_segment

--- src/video/segment.mjs
+++ src/video/segment.mjs
@@ -1,12 +1,12 @@
+import { list_empty_not } from '../list/empty/not.mjs';
 import { video_key_to_segment_paths } from './key/to/segment/paths.mjs';
 import { video_ffmpeg_generic } from './ffmpeg/generic.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-import { list_empty } from '../list/empty.mjs';
 export async function video_segment(video_key) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let matches = await video_key_to_segment_paths(video_key);
-    if (list_empty(matches)) {
+    if (list_empty_not(matches)) {
         return;
     }
     const suffix = '%03d';
