3ebd27c86b4deaa12066e897b1f7eecf39426358
JaredMathis <jared.mathis@native.consulting>
2023-07-02 12:19:14 -0400

    #312 function_auto task_git_hub_issues_compare

---
+++ src/task/git/hub/issues/compare.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../../../error.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function task_git_hub_issues_compare() {
+    arguments_assert(arguments, []);
+    error('todo: task_git_hub_issues_compare');
+}
