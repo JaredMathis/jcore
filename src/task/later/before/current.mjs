2495fbc84e93f5eeea6d44ece65fe3029946f0e7
JaredMathis <jared.mathis@native.consulting>
2023-07-02 18:06:00 -0400

    #315 function_auto task_later_before_current

--- src/task/later/before/current.mjs
+++ src/task/later/before/current.mjs
@@ -1,3 +1,5 @@
+import { task_set } from '../../set.mjs';
+import { task_property_number_get } from '../../property/number/get.mjs';
 import { task_available_first } from '../../available/first.mjs';
 import { task_unset } from '../../unset.mjs';
 import { metadata } from '../../../metadata.mjs';
@@ -5,12 +7,15 @@ import { task_id_get_string } from '../../id/get/string.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { task_later_before } from '../before.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { string_to } from '../../../string/to.mjs';
 export async function task_later_before_current(title) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let task_number_string = await task_id_get_string();
     let result = await task_later_before(task_number_string, title);
     await task_unset();
     let taf = await task_available_first();
-    return result;
+    let taf_number = task_property_number_get(taf);
+    let taf_number_string = string_to(taf_number);
+    return await task_set(taf_number_string);
     metadata([]);
 }
