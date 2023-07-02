73d5d319be693279a1e0330f55c87302cc246248
JaredMathis <jared.mathis@native.consulting>
2023-07-02 16:08:37 -0400

    #156 function_delete task_before

--- src/task/before.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../error.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
-export function task_before() {
-    arguments_assert(arguments, []);
-    error('todo: task_before');
-}
