d4a476cb5b6a238bc176738a701edbe91f302f08
JaredMathis <jared.mathis@native.consulting>
2023-07-02 10:54:09 -0400

    #312 function_copy version_path_tasks_all_get version_path_tasks_all_2_get

---
+++ src/version/path/tasks/all/2/get.mjs
@@ -0,0 +1,13 @@
+import { metadata } from '../../../../../metadata.mjs';
+import { file_path_json } from '../../../../../file/path/json.mjs';
+import { version_path_tasks_get } from '../../get.mjs';
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export function version_path_tasks_all_2_get(repository_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
+    let tasks_path = version_path_tasks_get(repository_name);
+    let v_2 = 'all';
+    let v = file_path_json(tasks_path, v_2);
+    return v;
+    metadata([]);
+}
