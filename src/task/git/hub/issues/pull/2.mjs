2fe576a865ae15b9bdc5bf8c8a4f14b89ea2db94
JaredMathis <jared.mathis@native.consulting>
2023-07-02 11:02:23 -0400

    #312 function_copy task_git_hub_issues_pull task_git_hub_issues_pull_2

---
+++ src/task/git/hub/issues/pull/2.mjs
@@ -0,0 +1,13 @@
+import { metadata } from '../../../../../metadata.mjs';
+import { version_repository_default } from '../../../../../version/repository/default.mjs';
+import { version_path_tasks_all_get } from '../../../../../version/path/tasks/all/get.mjs';
+import { task_git_hub_issues_pull_generic } from './generic.mjs';
+import { boolean_value_true } from '../../../../../boolean/value/true.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+export async function task_git_hub_issues_pull_2() {
+    arguments_assert(arguments, []);
+    const no_cache = boolean_value_true();
+    let v = await task_git_hub_issues_pull_generic(no_cache, version_path_tasks_all_get(version_repository_default()));
+    return v;
+    metadata([]);
+}
