594d4bc353c46809d22837c15ed94e8c34fb591a
JaredMathis <jared.mathis@native.consulting>
2023-07-02 00:10:56 -0400

    #310 function_input_swap_first_2 task_comment_video_segment

--- src/task/comment/video/segment.mjs
+++ src/task/comment/video/segment.mjs
@@ -1,6 +1,12 @@
+import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
+import { arguments_assert } from '../../../arguments/assert.mjs';
 import { task_comment_video } from '../video.mjs';
 import { video_segment } from '../../../video/segment.mjs';
-export async function task_comment_video_segment(video_key, issue_number) {
+export async function task_comment_video_segment(issue_number, video_key) {
+    arguments_assert(arguments, [
+        arguments_assert_todo,
+        arguments_assert_todo
+    ]);
     await video_segment(video_key);
     await task_comment_video(issue_number, video_key);
 }
--- src/task/current/video.mjs
+++ src/task/current/video.mjs
@@ -8,5 +8,5 @@ export async function task_current_video() {
     let issue_number = await task_id_get_string();
     let subized = await video_screen_recordings_subize();
     let video_key = object_property_get(subized, 'video_key');
-    await task_comment_video_segment(video_key, issue_number);
+    await task_comment_video_segment(issue_number, video_key);
 }
