16572b23798ea77031f55bbaca38b9d86ebf1aba
JaredMathis <jared.mathis@native.consulting>
2023-07-03 14:42:57 -0400

    #160 function_auto string_sub_max_2

--- src/string/sub/max/2.mjs
+++ src/string/sub/max/2.mjs
@@ -1,3 +1,4 @@
+import { list_without_multiple } from '../../../list/without/multiple.mjs';
 import { object_keys_recursive } from '../../../object/keys/recursive.mjs';
 import { string_to_tree } from '../../to/tree.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
@@ -11,8 +12,10 @@ export function string_sub_max_2(left, right) {
     let tree_left_keys = object_keys_recursive(tree_left);
     let tree_right = string_to_tree(right);
     let tree_right_keys = object_keys_recursive(tree_right);
+    let r = list_without_multiple(tree_left_keys, tree_right_keys);
     return {
         tree_left_keys,
-        tree_right_keys
+        tree_right_keys,
+        r
     };
 }
