ba751e15479e8442446f47d3925d341e79f887c0
JaredMathis <jared.mathis@native.consulting>
2023-07-03 14:57:57 -0400

    #160 noop

--- src/object/keys/recursive.mjs
+++ src/object/keys/recursive.mjs
@@ -8,13 +8,17 @@ import { list_adder_unique } from '../../list/adder/unique.mjs';
 export function object_keys_recursive(result) {
     arguments_assert(arguments, [arguments_assert_todo]);
     return list_adder_unique(function v_5(la) {
-        object_recursive(result, lambda);
-        function lambda(v) {
+        object_recursive(result, lambda_wrapper);
+        function lambda_wrapper(v) {
             let {node} = v;
             let property_key = object_recursive_property_key();
             if (!object_property_exists(node, property_key)) {
                 return;
             }
+            lambda(node);
+        }
+
+        function lambda(node) {
             let key = object_property_get(node, object_recursive_property_key());
             la(key);
         }
