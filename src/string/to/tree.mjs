bebb3102119c961099f67aedb77fa823a8c7ccc8
JaredMathis <jared.mathis@native.consulting>
2023-07-03 12:25:57 -0400

    #160 noop

--- src/string/to/tree.mjs
+++ src/string/to/tree.mjs
@@ -4,6 +4,7 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function string_to_tree(s) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    return;
     let offset = 0;
     let list_of_characters = string_to_list(s);
     return string_to_tree_recursive(list_of_characters, offset);
