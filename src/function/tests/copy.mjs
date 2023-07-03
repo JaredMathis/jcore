2a12ed84d77b71d2c81459a22db30ffdf99efc02
JaredMathis <jared.mathis@native.consulting>
2023-07-03 19:57:54 -0400

    #160 function_auto function_tests_copy

--- src/function/tests/copy.mjs
+++ src/function/tests/copy.mjs
@@ -1,3 +1,4 @@
+import { function_copy_no_open } from '../copy/no/open.mjs';
 import { log } from '../../log.mjs';
 import { file_js_identifier_rename } from '../../file/js/identifier/rename.mjs';
 import { function_name_to_file_path } from '../name/to/file/path.mjs';
@@ -8,7 +9,6 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { assert } from '../../assert.mjs';
 import { list_empty_not } from '../../list/empty/not.mjs';
-import { function_copy } from '../copy.mjs';
 export async function function_tests_copy(function_name_from, function_name_to) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -20,7 +20,7 @@ export async function function_tests_copy(function_name_from, function_name_to) 
     assert(list_empty(result_to));
     for (let r of result_from) {
         let r_mapped = string_prefix_replace(r, function_name_from, function_name_to);
-        await function_copy(r, r_mapped);
+        await function_copy_no_open(r, r_mapped);
         let file_path = function_name_to_file_path(r_mapped);
         log({
             file_path,
