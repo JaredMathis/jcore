8c9996c64907de46ee08a497c0053520f455e9e6
JaredMathis <jared.mathis@native.consulting>
2023-07-03 18:49:11 -0400

    #160 function_auto function_tests_copy

--- src/function/tests/copy.mjs
+++ src/function/tests/copy.mjs
@@ -1,7 +1,10 @@
+import { function_tests_all } from './all.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
-import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_tests_copy(function_name) {
+import { assert } from '../../assert.mjs';
+import { list_empty_not } from '../../list/empty/not.mjs';
+export async function function_tests_copy(function_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: function_tests_copy');
+    let result = await function_tests_all(function_name);
+    assert(list_empty_not(result));
 }
