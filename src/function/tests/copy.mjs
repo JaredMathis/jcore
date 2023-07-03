a5bad3e84eabdb560602756c46f41c1f9f8d03a0
JaredMathis <jared.mathis@native.consulting>
2023-07-03 18:44:17 -0400

    #160 function_auto function_tests_copy

---
+++ src/function/tests/copy.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function function_tests_copy() {
+    arguments_assert(arguments, []);
+    error('todo: function_tests_copy');
+}
