3f096d5038ad700433a30bc60049fbe4685849aa
JaredMathis <jared.mathis@native.consulting>
2023-07-03 15:11:35 -0400

    #160 noop

--- src/object/property/remove.mjs
+++ src/object/property/remove.mjs
@@ -2,11 +2,14 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 import { metadata } from '../../metadata.mjs';
 import { defined_is } from '../../defined/is.mjs';
 import { string_is } from '../../string/is.mjs';
+import { assert } from '../../assert.mjs';
+import { object_property_exists } from './exists.mjs';
 export function object_property_remove(object, property_name) {
     arguments_assert(arguments, [
         defined_is,
         string_is
     ]);
+    assert(object_property_exists(object, property_name))
     delete object[property_name];
     metadata([]);
 }
