d2a75e83ad64d79df381dcbbcf711d49c5e26aab
JaredMathis <jared.mathis@native.consulting>
2023-07-03 12:15:50 -0400

    #160 string_to_tree a

--- src/string/to/tree/recursive.mjs
+++ src/string/to/tree/recursive.mjs
@@ -3,6 +3,8 @@ import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { object_property_initialize_if_unset } from '../../../object/property/initialize/if/unset.mjs';
 import { list_each_with_index } from '../../../list/each/with/index.mjs';
+import { object_property_get } from '../../../object/property/get.mjs';
+import { list_add } from '../../../list/add.mjs';
 export function string_to_tree_recursive(list_of_characters, offset) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -14,7 +16,9 @@ export function string_to_tree_recursive(list_of_characters, offset) {
             return;
         }
         object_property_initialize_if_unset(result, element, []);
-        string_to_tree_recursive(list_of_characters, add_1(index));
+        let list = object_property_get(result, element);
+        let value = string_to_tree_recursive(list_of_characters, add_1(index));
+        list_add(list, value);
     });
     return result;
 }
