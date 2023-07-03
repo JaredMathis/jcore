0c8010a4e148413cb9acef99a0cba1c7698000c9
JaredMathis <jared.mathis@native.consulting>
2023-07-03 14:59:23 -0400

    #160 function_auto object_keys_recursive

--- src/object/keys/recursive.mjs
+++ src/object/keys/recursive.mjs
@@ -1,23 +1,13 @@
+import { object_recursive_skip_root } from '../recursive/skip/root.mjs';
 import { object_recursive_property_key } from '../recursive/property/key.mjs';
-import { object_recursive } from '../recursive.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-import { object_property_exists } from '../property/exists.mjs';
 import { object_property_get } from '../property/get.mjs';
 import { list_adder_unique } from '../../list/adder/unique.mjs';
 export function object_keys_recursive(result) {
     arguments_assert(arguments, [arguments_assert_todo]);
     return list_adder_unique(function v_5(la) {
-        object_recursive(result, lambda_wrapper);
-        function lambda_wrapper(v) {
-            let {node} = v;
-            let property_key = object_recursive_property_key();
-            if (!object_property_exists(node, property_key)) {
-                return;
-            }
-            lambda(node);
-        }
-
+        object_recursive_skip_root(result, lambda);
         function lambda(node) {
             let key = object_property_get(node, object_recursive_property_key());
             la(key);
---
+++ src/object/recursive/skip/root.mjs
@@ -0,0 +1,14 @@
+import { object_property_exists } from '../../property/exists.mjs';
+import { object_recursive_property_key } from '../property/key.mjs';
+import { object_recursive } from '../../recursive.mjs';
+export function object_recursive_skip_root(result, lambda) {
+    object_recursive(result, lambda_wrapper);
+    function lambda_wrapper(v) {
+        let {node} = v;
+        let property_key = object_recursive_property_key();
+        if (!object_property_exists(node, property_key)) {
+            return;
+        }
+        lambda(node);
+    }
+}
