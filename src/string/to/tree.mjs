ac3722098bbb84d8325231469719520d89391369
JaredMathis <jared.mathis@native.consulting>
2023-07-03 12:06:38 -0400

    #160 function_auto string_to_tree

--- src/string/to/tree.mjs
+++ src/string/to/tree.mjs
@@ -1,8 +1,9 @@
+import { string_to_list } from './list.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
-import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function string_to_tree(s) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let s_list = string_to_list(s);
     let result = {};
     return result;
 }
