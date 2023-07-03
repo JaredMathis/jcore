23646814af67cb861885bba38d4433c15c99dde1
JaredMathis <jared.mathis@native.consulting>
2023-07-03 18:06:21 -0400

    #160 function_search copy

--- src/object/recursive/skip/root/depth/keys.mjs
+++ src/object/recursive/skip/root/depth/keys.mjs
@@ -4,13 +4,16 @@ import { arguments_assert } from '../../../../../arguments/assert.mjs';
 import { object_recursive_property_key } from '../../../property/key.mjs';
 import { list_map_property } from '../../../../../list/map/property.mjs';
 import { object_recursive_skip_root_depth } from '../depth.mjs';
+import { list_map } from '../../../../../list/map.mjs';
 export function object_recursive_skip_root_depth_keys(tree_right, length_max) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let tree_right_found = object_recursive_skip_root_depth(tree_right, length_max);
-    log({ tree_right_found });
-    let tree_right_found_keys = list_map_property(tree_right_found, object_recursive_property_key());
+    let stacks = object_recursive_skip_root_depth(tree_right, length_max);
+    list_map(stacks, stack => {
+        log({ stack});
+    })
+    let tree_right_found_keys = list_map_property(stacks, object_recursive_property_key());
     return tree_right_found_keys;
 }
