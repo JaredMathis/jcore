06252eb9aaf25c5f1a0b88f6454a3598dc403d5c
JaredMathis <jared.mathis@native.consulting>
2023-07-02 22:28:46 -0400

    #317 function_auto task_git_hub_issues_compare

--- src/task/git/hub/issues/compare.mjs
+++ src/task/git/hub/issues/compare.mjs
@@ -1,8 +1,7 @@
-import { log } from '../../../../log.mjs';
+import { task_git_hub_issues_pull_2 } from './pull/2.mjs';
 import { git_hub_repository_issue_state_generic } from '../../../../git/hub/repository/issue/state/generic.mjs';
 import { equal_by_property } from '../../../../equal/by/property.mjs';
 import { task_property_state } from '../../../property/state.mjs';
-import { list_sort } from '../../../../list/sort.mjs';
 import { list_find_property } from '../../../../list/find/property.mjs';
 import { version_path_tasks_all_get } from '../../../../version/path/tasks/all/get.mjs';
 import { version_repository_default } from '../../../../version/repository/default.mjs';
@@ -10,13 +9,17 @@ import { version_path_tasks_all_2_get } from '../../../../version/path/tasks/all
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 import { file_json_read } from '../../../../file/json/read.mjs';
 import { object_property_get } from '../../../../object/property/get.mjs';
-import { list_map_property } from '../../../../list/map/property.mjs';
+import { equal_by } from '../../../../equal/by.mjs';
+import { list_length } from '../../../../list/length.mjs';
 export async function task_git_hub_issues_compare() {
     arguments_assert(arguments, []);
     let all_2_path = version_path_tasks_all_2_get(version_repository_default());
     let all_path = version_path_tasks_all_get(version_repository_default());
     let all = await file_json_read(all_path);
     let all_2 = await file_json_read(all_2_path);
+    if (!equal_by(list_length, all, all_2)) {
+        await task_git_hub_issues_pull_2();
+    }
     const number = 'number';
     for (let a of all) {
         const number_value = object_property_get(a, number);
