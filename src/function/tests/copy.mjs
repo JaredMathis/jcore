db6189ff23b3370affba9a11606e5dc5ce9decbd
JaredMathis <jared.mathis@native.consulting>
2023-07-03 18:57:21 -0400

    #160 function_auto function_tests_copy

--- src/function/tests/copy.mjs
+++ src/function/tests/copy.mjs
@@ -1,9 +1,11 @@
+import { string_prefix_replace } from '../../string/prefix/replace.mjs';
 import { list_empty } from '../../list/empty.mjs';
 import { function_tests_all } from './all.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { assert } from '../../assert.mjs';
 import { list_empty_not } from '../../list/empty/not.mjs';
+import { function_copy } from '../copy.mjs';
 export async function function_tests_copy(function_name_from, function_name_to) {
     arguments_assert(arguments, [
         arguments_assert_todo,
@@ -14,10 +16,11 @@ export async function function_tests_copy(function_name_from, function_name_to) 
     let result_to = await function_tests_all(function_name_to);
     assert(list_empty(result_to));
     for (let r of result_from) {
-        let r_mapped = 
+        let r_mapped = string_prefix_replace(r, function_name_from, function_name_to);
+        await function_copy(r, r_mapped);
     }
     return {
         result_from,
-        result_to,
-    }
+        result_to
+    };
 }
