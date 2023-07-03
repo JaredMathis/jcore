26187b2dcf079408d189fccc62aee3c5f8b5c881
JaredMathis <jared.mathis@native.consulting>
2023-07-03 12:36:36 -0400

    #160 string_to_tree abcd

--- src/string/to/tree.mjs
+++ src/string/to/tree.mjs
@@ -8,6 +8,7 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_length } from '../length.mjs';
 import { add_1 } from '../../add/1.mjs';
+import { object_property_set } from '../../object/property/set.mjs';
 export function string_to_tree(s) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let result = {};
@@ -15,12 +16,12 @@ export function string_to_tree(s) {
         let s_index = string_get(s, index);
         let s_index_next = string_get(s, add_1(index));
         for (let s_i of [s_index, s_index_next]) {
-            object_property_initialize_if_unset(result, s_i, []);
+            object_property_initialize_if_unset(result, s_i, {});
 
         }
-        let list = object_property_get(result, s_index);
-        const newLocal = object_property_get(result, s_index_next);
-        list_add(list, newLocal);
+        let sub_result = object_property_get(result, s_index);
+        const result_s_index_next = object_property_get(result, s_index_next);
+        object_property_set(sub_result, s_index_next, result_s_index_next);
     }
     return result;
     let offset = 0;
