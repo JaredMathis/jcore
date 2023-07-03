4b18a5d442448ea13855e900d7ab828b3f03ce80
JaredMathis <jared.mathis@native.consulting>
2023-07-03 18:47:28 -0400

    #160 function_input_add function_tests_copy function_name

--- src/function/tests/copy.mjs
+++ src/function/tests/copy.mjs
@@ -1,6 +1,7 @@
+import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
-export function function_tests_copy() {
-    arguments_assert(arguments, []);
+export function function_tests_copy(function_name) {
+    arguments_assert(arguments, [arguments_assert_todo]);
     error('todo: function_tests_copy');
 }
