5743ecf0445fece95bed1ba43aaabca050e46c1c
JaredMathis <jared.mathis@native.consulting>
2023-07-03 12:04:47 -0400

    #160 function_search index

--- src/string/to/tree.mjs
+++ src/string/to/tree.mjs
@@ -3,5 +3,6 @@ import { error } from '../../error.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function string_to_tree(s) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: string_to_tree');
+    let result = {};
+    return result;
 }
