adfd26c349969a076e72d4332010212b320265bc
JaredMathis <jared.mathis@native.consulting>
2023-07-03 12:11:25 -0400

    #160 noop

--- src/string/to/tree.mjs
+++ src/string/to/tree.mjs
@@ -5,10 +5,15 @@ import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function string_to_tree(s) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let offset = 0;
     let s_list = string_to_list(s);
     let result = {};
     list_each_with_index(s_list, function v(element, index) {
+        if (index < offset) {
+            return;
+        }
         object_property_initialize_if_unset(result, element, []);
+
     });
     return result;
 }
