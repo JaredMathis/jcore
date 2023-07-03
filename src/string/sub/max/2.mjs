75473d6745d2b8cd93f3a8bfdde06db0730e4d75
JaredMathis <jared.mathis@native.consulting>
2023-07-03 13:36:21 -0400

    #160 function_auto string_sub_max_2

--- src/string/sub/max/2.mjs
+++ src/string/sub/max/2.mjs
@@ -1,11 +1,12 @@
+import { string_to_tree } from '../../to/tree.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
-import { string_sub_max } from '../max.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export function string_sub_max_2(left, right) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let result = string_sub_max(left, right);
+    let tree_left = string_to_tree(left);
+    let tree_right = string_to_tree(right);
     return result;
 }
