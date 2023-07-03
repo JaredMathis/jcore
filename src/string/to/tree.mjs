19acbe51f6f53d27b312562c61d5dfe9e2abb2db
JaredMathis <jared.mathis@native.consulting>
2023-07-03 12:28:34 -0400

    #160 string_to_tree abcd

--- src/string/to/tree.mjs
+++ src/string/to/tree.mjs
@@ -4,13 +4,16 @@ import { string_to_list } from './list.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { string_length } from '../length.mjs';
+import { add_1 } from '../../add/1.mjs';
 export function string_to_tree(s) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    let result = {};
     for (let index = string_length(s) - 2; index >= 0; index--) {
         let s_index = string_get(s, index);
-        console.log({s_index})
+        let s_index_next = string_get(s, add_1(index));
+        console.log({s_index, s_index_next})
     }
-    return;
+    return result;
     let offset = 0;
     let list_of_characters = string_to_list(s);
     return string_to_tree_recursive(list_of_characters, offset);
