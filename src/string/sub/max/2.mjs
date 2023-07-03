7b1adf352794f86f5fb471ff64fa5b8d197d3f2f
JaredMathis <jared.mathis@native.consulting>
2023-07-03 14:40:31 -0400

    #160 function_auto string_sub_max_2

--- src/string/sub/max/2.mjs
+++ src/string/sub/max/2.mjs
@@ -1,3 +1,4 @@
+import { object_keys_recursive } from '../../../object/keys/recursive.mjs';
 import { string_to_tree } from '../../to/tree.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -7,6 +8,11 @@ export function string_sub_max_2(left, right) {
         arguments_assert_todo
     ]);
     let tree_left = string_to_tree(left);
+    let tree_left_keys = object_keys_recursive(tree_left);
     let tree_right = string_to_tree(right);
-    return {tree_left,tree_right};
+    let tree_right_keys = object_keys_recursive(tree_right);
+    return {
+        tree_left_keys,
+        tree_right_keys
+    };
 }
