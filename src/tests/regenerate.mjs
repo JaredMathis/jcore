23dff38e197fd12442c24550e578277e8c787c74
JaredMathis <jared.mathis@native.consulting>
2023-07-03 18:30:19 -0400

    #160 function_auto tests_regenerate

---
+++ src/tests/regenerate.mjs
@@ -0,0 +1,6 @@
+import { error } from '../error.mjs';
+import { arguments_assert } from '../arguments/assert.mjs';
+export function tests_regenerate() {
+    arguments_assert(arguments, []);
+    error('todo: tests_regenerate');
+}
