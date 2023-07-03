cb6b37a8786b68a40549b96e501bb6a5dcf19675
JaredMathis <jared.mathis@native.consulting>
2023-07-03 12:08:43 -0400

    #160 function_auto string_to_tree

--- src/string/to/tree.mjs
+++ src/string/to/tree.mjs
@@ -1,3 +1,4 @@
+import { object_property_initialize_if_unset } from '../../object/property/initialize/if/unset.mjs';
 import { list_each_with_index } from '../../list/each/with/index.mjs';
 import { string_to_list } from './list.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
@@ -7,6 +8,7 @@ export function string_to_tree(s) {
     let s_list = string_to_list(s);
     let result = {};
     list_each_with_index(s_list, function v(element, index) {
+        object_property_initialize_if_unset(result, element, []);
     });
     return result;
 }
