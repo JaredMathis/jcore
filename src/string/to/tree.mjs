d016b1379f3eb2f0adf7bfab882601c04cf81e60
JaredMathis <jared.mathis@native.consulting>
2023-07-03 12:07:04 -0400

    #160 function_auto string_to_tree

--- src/string/to/tree.mjs
+++ src/string/to/tree.mjs
@@ -1,9 +1,12 @@
+import { list_each_with_index } from '../../list/each/with/index.mjs';
 import { string_to_list } from './list.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function string_to_tree(s) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let s_list = string_to_list(s);
+    list_each_with_index(s_list, function v(element, index) {
+    });
     let result = {};
     return result;
 }
