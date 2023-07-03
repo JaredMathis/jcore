37cdc2d4d78b614004843e3b47c1f8da152e3346
JaredMathis <jared.mathis@native.consulting>
2023-07-03 18:30:32 -0400

    #160 function_delete tests_regenerate

--- src/tests/regenerate.mjs
+++
@@ -1,6 +0,0 @@
-import { error } from '../error.mjs';
-import { arguments_assert } from '../arguments/assert.mjs';
-export function tests_regenerate() {
-    arguments_assert(arguments, []);
-    error('todo: tests_regenerate');
-}
