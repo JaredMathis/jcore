30cd11822bd0a1578d38185afd270895d99fb685
JaredMathis <jared.mathis@native.consulting>
2023-07-03 19:49:05 -0400

    #160 noop

--- src/function/tests/copy.mjs
+++ src/function/tests/copy.mjs
@@ -21,6 +21,7 @@ export async function function_tests_copy(function_name_from, function_name_to) 
         let r_mapped = string_prefix_replace(r, function_name_from, function_name_to);
         await function_copy(r, r_mapped);
         let file_path = function_name_to_file_path(r_mapped);
+        log({file_path, function_name_from, function_name_to})
         await file_js_identifier_rename(file_path, function_name_from, function_name_to);
         break;
     }
