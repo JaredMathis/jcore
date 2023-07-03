92355dfe111155d6a84739b6289a83e7c64315e2
JaredMathis <jared.mathis@native.consulting>
2023-07-03 18:50:50 -0400

    #160 function_auto function_tests_copy

--- src/function/tests/copy.mjs
+++ src/function/tests/copy.mjs
@@ -1,13 +1,16 @@
+import { list_empty } from '../../list/empty.mjs';
 import { function_tests_all } from './all.mjs';
 import { arguments_assert_todo } from '../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { assert } from '../../assert.mjs';
 import { list_empty_not } from '../../list/empty/not.mjs';
-export async function function_tests_copy(function_name, function_name_to) {
+export async function function_tests_copy(function_name_from, function_name_to) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let result = await function_tests_all(function_name);
-    assert(list_empty_not(result));
+    let result_from = await function_tests_all(function_name_from);
+    assert(list_empty_not(result_from));
+    let result_to = await function_tests_all(function_name_to);
+    assert(list_empty(result_from));
 }
