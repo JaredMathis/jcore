bf0592c4f2535e346ab6cbfb2df321fc33ba62fc
JaredMathis <jared.mathis@native.consulting>
2023-07-04 17:04:36 -0400

    #319 function_auto string_sub_max_3

--- src/string/sub/max/3.mjs
+++ src/string/sub/max/3.mjs
@@ -1,7 +1,7 @@
 import { string_is } from '../../is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_length } from '../../length.mjs';
-import { equal } from '../../../equal.mjs';
+import { string_get } from '../../get.mjs';
 export function string_sub_max_3(left, right) {
     arguments_assert(arguments, [
         string_is,
@@ -10,7 +10,8 @@ export function string_sub_max_3(left, right) {
     let offset = 0;
     for (let i = 0; i < string_length(left); i++) {
         let i_offset = number_add(i, offset);
-        if (equal()) {
+        let i_offset_value = string_get(left, i_offset);
+        if (string_contains()) {
         }
     }
     return result;
