dfa011936abaa788a6ca505d34ca401538afdf7f
JaredMathis <jared.mathis@native.consulting>
2023-07-04 17:14:08 -0400

    #319 noop

--- src/list/adder/generic/async.mjs
+++ src/list/adder/generic/async.mjs
@@ -7,11 +7,20 @@ export async function list_adder_generic_async(then, lambda) {
         function_is,
         function_is
     ]);
-    let list = [];
+    let list = intialize();
     await then(list_new_then_add);
+    function intialize() {
+        return [];
+    }
+
     function list_new_then_add(element) {
         arguments_assert(arguments, [defined_is]);
+        list = each(list);
+    }
+
+    function each(list) {
         lambda(list, element);
+        return list;
     }
     return list;
     metadata([]);
