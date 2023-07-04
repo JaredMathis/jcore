82d98e33a7afbba0e05cac4a5235367214a4f0c0
JaredMathis <jared.mathis@native.consulting>
2023-07-03 20:32:22 -0400

    #318 noop

--- src/refactor/console/log/delete.mjs
+++ src/refactor/console/log/delete.mjs
@@ -1,7 +1,12 @@
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { error } from '../../../error.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { js_visit_nodes_filter } from '../../../js/visit/nodes/filter.mjs';
+import { js_node_is_expression_statement } from '../../../js/node/is/expression/statement.mjs';
 export function refactor_console_log_delete(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: refactor_console_log_delete');
+    let {parsed} =args
+    js_visit_nodes_filter(parsed, js_node_is_expression_statement, v => {
+        
+    })
 }
