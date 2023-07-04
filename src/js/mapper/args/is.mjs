85d73f5f0a0ff16349d449a8f24acab2d56b4994
JaredMathis <jared.mathis@native.consulting>
2023-07-04 18:01:58 -0400

    #319 function_auto js_mapper_args_is

--- src/js/mapper/args/is.mjs
+++ src/js/mapper/args/is.mjs
@@ -1,10 +1,10 @@
-import { boolean_value_true } from '../../../boolean/value/true.mjs';
+import { object_is } from '../../../object/is.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { defined_is } from '../../../defined/is.mjs';
 export function js_mapper_args_is(value) {
     arguments_assert(arguments, [defined_is]);
-    let v = boolean_value_true();
-    return v;
+    return true;
+    return object_is(value);
     metadata([]);
 }
