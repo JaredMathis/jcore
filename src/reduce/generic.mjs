b3b623b04faddb997f89a97628fb040abfaef5f4
JaredMathis <jared.mathis@native.consulting>
2023-07-04 17:42:42 -0400

    #319 function_auto reduce_generic_async

--- src/reduce/generic.mjs
+++ src/reduce/generic.mjs
@@ -3,7 +3,7 @@ import { metadata } from '../metadata.mjs';
 import { defined_is } from '../defined/is.mjs';
 import { arguments_assert_todo } from '../arguments/assert/todo.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
-export function reduce_generic(intialize, then, each_inner) {
+export function reduce_generic(intialize, each_inner, then) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo,
