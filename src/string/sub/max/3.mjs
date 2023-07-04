6c8238e905d5bdddc48cd00e3f6a8492223834da
JaredMathis <jared.mathis@native.consulting>
2023-07-04 18:42:34 -0400

    #319 function_auto string_sub_max_3

--- src/string/sub/max/3.mjs
+++ src/string/sub/max/3.mjs
@@ -1,7 +1,7 @@
+import { reduce_generic } from '../../../reduce/generic.mjs';
 import { string_is } from '../../is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_length } from '../../length.mjs';
-import { string_get } from '../../get.mjs';
 import { string_sub } from '../../sub.mjs';
 import { string_index_of_try } from '../../index/of/try.mjs';
 export function string_sub_max_3(left, right) {
@@ -9,6 +9,14 @@ export function string_sub_max_3(left, right) {
         string_is,
         string_is
     ]);
+    reduce_generic(function v() {
+        return '';
+    }, function v_2(max, value) {
+        if (string_length(max) > string_length(value)) {
+            max;
+        }
+        return value;
+    }, then);
     let offset = 0;
     for (let i = 0; i < string_length(left); i++) {
         let i_offset = number_add(i, offset);
