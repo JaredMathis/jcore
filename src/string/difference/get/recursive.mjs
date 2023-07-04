0340253fe85aea17efe3a0bf71d5cf45c29582ec
JaredMathis <jared.mathis@native.consulting>
2023-07-04 12:49:16 -0400

    #160 function_auto string_difference_get_recursive

--- src/string/difference/get/recursive.mjs
+++ src/string/difference/get/recursive.mjs
@@ -1,3 +1,4 @@
+import { string_sub_max_2 } from '../../sub/max/2.mjs';
 import { speed_log } from '../../../speed/log.mjs';
 import { speed_next } from '../../../speed/next.mjs';
 import { speed_start } from '../../../speed/start.mjs';
@@ -38,6 +39,8 @@ export function string_difference_get_recursive(left, right, left_offset, right_
     }
     let s = speed_start();
     let max = string_sub_max(left, right);
+    speed_next(s);
+    let max2 = string_sub_max_2(left, right);
     speed_next(s);
     speed_log(s);
     let v = string_sub_max_property_offset();
