50176e5a1bab9e769bfb9464d0c9d239aa94c1c7
JaredMathis <jared.mathis@native.consulting>
2023-07-03 20:47:01 -0400

    #318 function_auto refactor_console_log_delete

--- src/refactor/console/log/delete.mjs
+++ src/refactor/console/log/delete.mjs
@@ -1,17 +1,19 @@
+import { js_node_is_member_expression } from '../../../js/node/is/member/expression.mjs';
+import { js_node_is_call_expression } from '../../../js/node/is/call/expression.mjs';
+import { js_node_property_expression_get } from '../../../js/node/property/expression/get.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
-import { error } from '../../../error.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { js_visit_nodes_filter } from '../../../js/visit/nodes/filter.mjs';
 import { js_node_is_expression_statement } from '../../../js/node/is/expression/statement.mjs';
-import { json_to } from '../../../json/to.mjs';
 export function refactor_console_log_delete(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let {parsed} =args
-    js_visit_nodes_filter(parsed, js_node_is_expression_statement, v => {
-        let {node} =v;
-        let expression= js_node_property_expression_get(node)
-        if (js_node_is_member_expression(expression)) {
-            
+    let {parsed} = args;
+    js_visit_nodes_filter(parsed, js_node_is_expression_statement, function v_2(v) {
+        let {node} = v;
+        let expression = js_node_property_expression_get(node);
+        if (js_node_is_call_expression(expression)) {
+            if (js_node_is_member_expression(expression)) {
+            }
         }
-    })
+    });
 }
