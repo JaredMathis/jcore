756e24c24a7701e9e63320daaeb8bb414cd3003a
JaredMathis <jared.mathis@native.consulting>
2023-07-03 21:46:00 -0400

    #160 function_auto function_tests_all

--- src/string/index/of.mjs
+++ src/string/index/of.mjs
@@ -1,11 +1,14 @@
 import { string_is } from '../is.mjs';
 import { string_index_of_try } from './of/try.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
+import { assert } from '../../assert.mjs';
 export function string_index_of(input, search) {
     arguments_assert(arguments, [
         string_is,
         string_is
     ]);
     let result = string_index_of_try(input, search);
+    let v = string_index_of_try_valid(result);
+    assert(v);
     return result;
 }
