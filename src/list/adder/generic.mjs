5134a99defa748bdfa6c4cb63fd9d00c7bd57b95
JaredMathis <jared.mathis@native.consulting>
2023-07-04 17:40:46 -0400

    #319 function_auto list_adder_generic_async

--- src/list/adder/generic.mjs
+++ src/list/adder/generic.mjs
@@ -1,6 +1,6 @@
 import { metadata_generated } from '../../metadata/generated.mjs';
 import { metadata } from '../../metadata.mjs';
-import { defined_is } from '../../defined/is.mjs';
+import { reduce_generic } from '../../reduce/generic.mjs';
 import { function_is } from '../../function/is.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 export function list_adder_generic(then, lambda) {
@@ -15,12 +15,6 @@ export function list_adder_generic(then, lambda) {
         lambda(list, element);
         return list;
     }
-    let current = intialize();
-    then(each);
-    function each(element) {
-        arguments_assert(arguments, [defined_is]);
-        current = each_inner(current, element);
-    }
-    return current;
+    return reduce_generic(intialize, then, each_inner);
     metadata([metadata_generated()]);
 }
