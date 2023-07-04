67b6bb8362d7ef0b1fbbfdbc98408a9364b5be71
JaredMathis <jared.mathis@native.consulting>
2023-07-04 18:16:26 -0400

    #319 noop

--- src/reduce/max.mjs
+++ src/reduce/max.mjs
@@ -3,6 +3,10 @@ import { reduce_generic } from './generic.mjs';
 import { arguments_assert } from '../arguments/assert.mjs';
 export function reduce_max(then) {
     arguments_assert(arguments, [arguments_assert_todo]);
+    function intialize(){
+        return Number.NEGATIVE_INFINITY;
+    }
+    function each_inner(){}
     let result = reduce_generic(intialize, each_inner, then);
     return result;
 }
