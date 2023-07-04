5c658c5165f7b4bd289f0cc5c03e3b8fb76e9de2
JaredMathis <jared.mathis@native.consulting>
2023-07-04 17:57:22 -0400

    #319 function_auto refactor_input_remove_first_2

--- src/refactor/input/remove/first/2.mjs
+++ src/refactor/input/remove/first/2.mjs
@@ -1,7 +1,8 @@
+import { refactor_input_remove_first } from '../first.mjs';
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export function refactor_input_remove_first_2(args) {
+export async function refactor_input_remove_first_2(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    error('todo: refactor_input_remove_first_2');
+    await refactor_input_remove_first(args);
+    await refactor_input_remove_first(args);
 }
