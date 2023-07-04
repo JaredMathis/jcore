90e1827051f20461647fe4e254be11cff409768e
JaredMathis <jared.mathis@native.consulting>
2023-07-04 17:06:38 -0400

    #319 function_search max

--- src/string/sub/max/3.mjs
+++ src/string/sub/max/3.mjs
@@ -2,6 +2,8 @@ import { string_is } from '../../is.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { string_length } from '../../length.mjs';
 import { string_get } from '../../get.mjs';
+import { string_sub } from '../../sub.mjs';
+import { string_index_of_try } from '../../index/of/try.mjs';
 export function string_sub_max_3(left, right) {
     arguments_assert(arguments, [
         string_is,
@@ -10,8 +12,9 @@ export function string_sub_max_3(left, right) {
     let offset = 0;
     for (let i = 0; i < string_length(left); i++) {
         let i_offset = number_add(i, offset);
-        let i_offset_value = string_get(left, i_offset);
-        if (string_contains()) {
+        let i_offset_value = string_sub(left, i, i_offset);
+        const index = string_index_of_try(right, i_offset_value);
+        if (index >= 0) {
         }
     }
     return result;
