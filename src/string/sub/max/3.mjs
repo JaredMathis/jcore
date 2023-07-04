0f6d8d84f8d79205688c02de0ce8e563c24c607a
JaredMathis <jared.mathis@native.consulting>
2023-07-04 16:59:54 -0400

    #319 noop

--- src/string/sub/max/3.mjs
+++ src/string/sub/max/3.mjs
@@ -1,11 +1,15 @@
 import { string_is } from '../../is.mjs';
 import { string_sub_max } from '../max.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
+import { string_length } from '../../length.mjs';
 export function string_sub_max_3(left, right) {
     arguments_assert(arguments, [
         string_is,
         string_is
     ]);
-    let result = string_sub_max(left, right);
+    let offset = 0;
+    for (let i = 0; i < string_length(left); i++) {
+
+    }
     return result;
 }
