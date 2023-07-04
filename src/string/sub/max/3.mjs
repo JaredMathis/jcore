11e0dc5b8ffce167a5a2074d7223f4b97b4556b9
JaredMathis <jared.mathis@native.consulting>
2023-07-04 18:49:58 -0400

    #319 function_auto string_sub_max_3

--- src/string/sub/max/3.mjs
+++ src/string/sub/max/3.mjs
@@ -1,3 +1,4 @@
+import { add } from '../../../add.mjs';
 import { string_includes } from '../../includes.mjs';
 import { reduce_generic } from '../../../reduce/generic.mjs';
 import { string_is } from '../../is.mjs';
@@ -19,7 +20,7 @@ export function string_sub_max_3(left, right) {
     }, function v_3(max_candidate) {
         let offset = 0;
         for (let i = 0; i < string_length(left); i++) {
-            let i_offset = number_add(i, offset);
+            let i_offset = add(i, offset);
             let i_offset_value = string_sub(left, i, i_offset);
             const contains = string_includes(right, i_offset_value);
             if (contains) {
