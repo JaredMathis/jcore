769f6f9d672f3a5a1e542cbfafa6932af0de2f10
JaredMathis <jared.mathis@native.consulting>
2023-07-03 20:53:37 -0400

    #318 function_auto refactor_console_log_delete

--- src/refactor/console/log/delete.mjs
+++ src/refactor/console/log/delete.mjs
@@ -1,3 +1,5 @@
+import { js_identifier_name_get } from '../../../js/identifier/name/get.mjs';
+import { js_identifier_is } from '../../../js/identifier/is.mjs';
 import { js_node_property_callee_get } from '../../../js/node/property/callee/get.mjs';
 import { js_node_is_member_expression } from '../../../js/node/is/member/expression.mjs';
 import { js_node_is_call_expression } from '../../../js/node/is/call/expression.mjs';
@@ -6,6 +8,8 @@ import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 import { js_visit_nodes_filter } from '../../../js/visit/nodes/filter.mjs';
 import { js_node_is_expression_statement } from '../../../js/node/is/expression/statement.mjs';
+import { error } from '../../../error.mjs';
+import { json_to } from '../../../json/to.mjs';
 export function refactor_console_log_delete(args) {
     arguments_assert(arguments, [arguments_assert_todo]);
     let {parsed} = args;
@@ -15,6 +19,9 @@ export function refactor_console_log_delete(args) {
         if (js_node_is_call_expression(expression)) {
             let callee = js_node_property_callee_get(node);
             if (js_node_is_member_expression(callee)) {
+                error(json_to({ callee }));
+                js_identifier_is;
+                js_identifier_name_get;
             }
         }
     });
