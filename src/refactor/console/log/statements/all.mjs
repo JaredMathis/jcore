ab83923addb6ecc2d121a8811d93ab3aa7688b34
JaredMathis <jared.mathis@native.consulting>
2023-07-04 20:19:08 -0400

    #321 task_later maybe each part of each identifier must be an approved word - that way tyops would be caught

--- src/refactor/console/log/statements/all.mjs
+++ src/refactor/console/log/statements/all.mjs
@@ -3,6 +3,7 @@ import { js_mapper_args_is } from '../../../../js/mapper/args/is.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function refactor_console_log_statements_all(args) {
     arguments_assert(arguments, [js_mapper_args_is]);
-    let {function_delcaration} = args;
-    let statements = js_function_declaration_to_statements(function_delcaration);
+    let {function_declaration} = args;
+    let statements = js_function_declaration_to_statements(function_declaration);
+    return statements;
 }
