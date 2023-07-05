30032470edad6a751eece24d2c72660e9f4cf7db
JaredMathis <jared.mathis@native.consulting>
2023-07-04 21:35:46 -0400

    #321 function_auto string_sub_max_3

--- src/string/sub/max/3.mjs
+++ src/string/sub/max/3.mjs
@@ -1,3 +1,4 @@
+import { string_length_max } from '../../length/max.mjs';
 import { string_is } from '../../is.mjs';
 import { string_sub_max_asymmetrical } from './asymmetrical.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
@@ -8,5 +9,5 @@ export function string_sub_max_3(left, right) {
     ]);
     let result1 = string_sub_max_asymmetrical(left, right);
     let result2 = string_sub_max_asymmetrical(right, left);
-    return string_length_max(result1,result2);
+    return string_length_max(result1, result2);
 }
