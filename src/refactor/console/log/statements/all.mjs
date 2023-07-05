2d780af7d84795ea765e2b4552cec6277309bae3
JaredMathis <jared.mathis@native.consulting>
2023-07-04 20:16:47 -0400

    #321 function_auto refactor_console_log_statements_all

--- src/refactor/console/log/statements/all.mjs
+++ src/refactor/console/log/statements/all.mjs
@@ -1,7 +1,8 @@
+import { log } from '../../../../log.mjs';
 import { js_mapper_args_is } from '../../../../js/mapper/args/is.mjs';
-import { error } from '../../../../error.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
 export function refactor_console_log_statements_all(args) {
     arguments_assert(arguments, [js_mapper_args_is]);
-    error('todo: refactor_console_log_statements_all');
+    let {parsed} = args;
+    log({ parsed });
 }
