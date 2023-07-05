ed4f97be81e0155e180b43775aa201749df10b66
JaredMathis <jared.mathis@native.consulting>
2023-07-05 06:38:40 -0400

    #321 noop

--- src/version/sync/local/preview/single.mjs
+++ src/version/sync/local/preview/single.mjs
@@ -1,11 +1,12 @@
 import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
 import { version_sync_local_preview_generic } from './generic.mjs';
 import { arguments_assert } from '../../../../arguments/assert.mjs';
-export async function version_sync_local_preview_single(repository_name, file_paths) {
+import { list_single_item } from '../../../../list/single/item.mjs';
+export async function version_sync_local_preview_single(repository_name, file_path) {
     arguments_assert(arguments, [
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    let result = await version_sync_local_preview_generic(repository_name, file_paths);
+    let result = await version_sync_local_preview_generic(repository_name, list_single_item(file_path));
     return result;
 }
