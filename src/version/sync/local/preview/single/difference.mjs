63a1988579ffc4af348c6fec1c2b0b1d4342381e
JaredMathis <jared.mathis@native.consulting>
2023-07-05 07:14:34 -0400

    #321 function_auto version_sync_local_preview_single_difference

--- src/version/sync/local/preview/single/difference.mjs
+++ src/version/sync/local/preview/single/difference.mjs
@@ -1,7 +1,7 @@
-import { arguments_assert_todo } from '../../../../arguments/assert/todo.mjs';
-import { version_sync_local_preview_generic } from './generic.mjs';
-import { arguments_assert } from '../../../../arguments/assert.mjs';
-import { list_single_item } from '../../../../list/single/item.mjs';
+import { arguments_assert_todo } from '../../../../../arguments/assert/todo.mjs';
+import { version_sync_local_preview_generic } from '../generic.mjs';
+import { arguments_assert } from '../../../../../arguments/assert.mjs';
+import { list_single_item } from '../../../../../list/single/item.mjs';
 export async function version_sync_local_preview_single_difference(repository_name, file_path) {
     arguments_assert(arguments, [
         arguments_assert_todo,
