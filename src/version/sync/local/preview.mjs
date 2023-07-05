4b452ea490a0ea5166366e9d3f785a06dbb39b32
JaredMathis <jared.mathis@native.consulting>
2023-07-05 06:34:49 -0400

    #321 function_input_add version_sync_local_preview_generic file_path

--- src/version/sync/local/preview.mjs
+++ src/version/sync/local/preview.mjs
@@ -1,10 +1,11 @@
+import { error } from '../../../error.mjs';
 import { metadata } from '../../../metadata.mjs';
 import { version_sync_local_preview_generic } from './preview/generic.mjs';
 import { arguments_assert_todo } from '../../../arguments/assert/todo.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function version_sync_local_preview(repository_name) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    let differences = await version_sync_local_preview_generic(repository_name);
+    let differences = await version_sync_local_preview_generic(repository_name, error());
     return differences;
     metadata([]);
 }
