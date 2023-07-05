2c0657d211256ec4622c5ac25cd3834146d7fb77
JaredMathis <jared.mathis@native.consulting>
2023-07-04 20:14:53 -0400

    #321 function_add_mapper console_log_statements_all

---
+++ src/refactor/console/log/statements/all.mjs
@@ -0,0 +1,7 @@
+import { js_mapper_args_is } from '../../../../js/mapper/args/is.mjs';
+import { error } from '../../../../error.mjs';
+import { arguments_assert } from '../../../../arguments/assert.mjs';
+export function refactor_console_log_statements_all(args) {
+    arguments_assert(arguments, [js_mapper_args_is]);
+    error('todo: refactor_console_log_statements_all');
+}
