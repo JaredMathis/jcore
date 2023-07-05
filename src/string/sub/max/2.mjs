a88ab3ce59fa47fc8ebc281d024286744ebf5ab0
JaredMathis <jared.mathis@native.consulting>
2023-07-05 06:11:34 -0400

    #321 function_auto string_sub_max_2

--- src/string/sub/max/2.mjs
+++ src/string/sub/max/2.mjs
@@ -16,47 +16,42 @@ import { list_contains } from '../../../list/contains.mjs';
 import { list_empty } from '../../../list/empty.mjs';
 import { string_length } from '../../length.mjs';
 import { string_is } from '../../is.mjs';
-import { speed_group } from '../../../speed/group.mjs';
 export function string_sub_max_2(left, right) {
     arguments_assert(arguments, [
         string_is,
         string_is
     ]);
-    return speed_group(function v_3(c) {
-        let tree_left = string_to_tree(left);
-        let tree_left_keys = object_keys_recursive(tree_left);
-        let tree_right = string_to_tree(right);
-        let tree_right_keys = object_keys_recursive(tree_right);
-        let r = list_without_multiple(tree_right_keys, tree_left_keys);
-        c();
-        object_recursive_skip_root(tree_right, function v_2(v) {
-            let {node, parent} = v;
-            let key = object_property_get(node, object_recursive_property_key());
-            if (list_contains(r, key)) {
-                let parent_value = object_property_get(parent, object_recursive_property_value());
-                object_property_remove(parent_value, key);
-            }
-        });
-        c();
-        let intersection;
-        let intersection_previous = [];
-        const max = number_min(string_length(left), string_length(right));
-        let length_max;
-        for (length_max = 1; length_max <= max; length_max++) {
-            let tree_right_found_keys = object_recursive_skip_root_depth_keys(tree_right, length_max);
-            let tree_left_found_keys = object_recursive_skip_root_depth_keys(tree_left, length_max);
-            intersection = list_intersection(tree_right_found_keys, tree_left_found_keys);
-            if (list_empty(intersection)) {
-                break;
-            }
-            intersection_previous = intersection;
+    let tree_left = string_to_tree(left);
+    let tree_left_keys = object_keys_recursive(tree_left);
+    let tree_right = string_to_tree(right);
+    let tree_right_keys = object_keys_recursive(tree_right);
+    let r = list_without_multiple(tree_right_keys, tree_left_keys);
+    object_recursive_skip_root(tree_right, function v_2(v) {
+        let {node, parent} = v;
+        let key = object_property_get(node, object_recursive_property_key());
+        if (list_contains(r, key)) {
+            let parent_value = object_property_get(parent, object_recursive_property_value());
+            object_property_remove(parent_value, key);
         }
-        let first;
-        if (list_empty(intersection_previous)) {
-            first = '';
-        } else {
-            first = list_first(intersection_previous);
-        }
-        return string_sub_max_to_result(first, left, right);
     });
+    let intersection;
+    let intersection_previous = [];
+    const max = number_min(string_length(left), string_length(right));
+    let length_max;
+    for (length_max = 1; length_max <= max; length_max++) {
+        let tree_right_found_keys = object_recursive_skip_root_depth_keys(tree_right, length_max);
+        let tree_left_found_keys = object_recursive_skip_root_depth_keys(tree_left, length_max);
+        intersection = list_intersection(tree_right_found_keys, tree_left_found_keys);
+        if (list_empty(intersection)) {
+            break;
+        }
+        intersection_previous = intersection;
+    }
+    let first;
+    if (list_empty(intersection_previous)) {
+        first = '';
+    } else {
+        first = list_first(intersection_previous);
+    }
+    return string_sub_max_to_result(first, left, right);
 }
