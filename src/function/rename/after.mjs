bf2e0ddce7f72d618019136634e986b95bda287e
JaredMathis <jared.mathis@native.consulting>
2023-07-05 06:55:39 -0400

    #321 refactor_functions_console_log_delete

--- src/function/rename/after.mjs
+++ src/function/rename/after.mjs
@@ -7,13 +7,8 @@ import { function_name_get } from '../name/get.mjs';
 import { file_js_multiple_map } from '../../file/js/multiple/map.mjs';
 export async function function_rename_after(file_paths_changed) {
     arguments_assert(arguments, [arguments_assert_todo]);
-    console.log(0);
     let v = function_name_get(refactor_import_fix);
-    console.log(1);
     await file_js_multiple_map(v, file_paths_changed);
-    console.log(2);
     await tests_generate();
-    console.log(3);
     metadata([]);
-    console.log(4);
 }
--- src/version/sync/local/preview/generic.mjs
+++ src/version/sync/local/preview/generic.mjs
@@ -17,26 +17,18 @@ export async function version_sync_local_preview_generic(repository_name, file_p
         arguments_assert_todo,
         arguments_assert_todo
     ]);
-    console.log(0);
     let differences = [];
-    console.log(1);
     await version_output_generic(repository_name, file_paths, lambda);
-    console.log(2);
     async function lambda(file_path, contents) {
         let existing = await file_read(file_path);
         todo(contents, existing, file_path);
     }
-    console.log(3);
     let removals = await version_removals(repository_name, file_paths);
-    console.log({removals});
-    console.log(4);
     let v_4 = async function v_2(file_path, contents) {
         let v_8 = '';
         todo(contents, v_8, file_path);
     };
-    console.log(5);
     await version_file_contents_each(repository_name, removals, v_4);
-    console.log(6);
     function todo(contents, existing, file_path) {
         let hunks = string_difference_get(contents, existing);
         let v_5 = list_empty(hunks);
@@ -49,18 +41,12 @@ export async function version_sync_local_preview_generic(repository_name, file_p
             list_add(differences, v_6);
         }
     }
-    console.log(7);
     let v_7 = {
         differences,
         removals
     };
-    console.log(8);
     let v = list_multiple_summary(v_7);
-    console.log(9);
     return v;
-    console.log(10);
     ;
-    console.log(11);
     metadata([]);
-    console.log(12);
 }
