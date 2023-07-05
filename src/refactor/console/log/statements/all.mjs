7d247ea0b0093171769bf0715dce44b2df44e536
JaredMathis <jared.mathis@native.consulting>
2023-07-04 20:25:22 -0400

    #321 function_auto refactor_console_log_statements_all

--- src/refactor/console/log/statements/all.mjs
+++ src/refactor/console/log/statements/all.mjs
@@ -7,7 +7,7 @@ import { arguments_assert } from '../../../../arguments/assert.mjs';
 export async function refactor_console_log_statements_all(args) {
     arguments_assert(arguments, [js_mapper_args_is]);
     let {function_declaration} = args;
-    await refactor_import_fix_if_changed(async function v(c) {
+    await refactor_import_fix_if_changed(args, async function v(c) {
         let {added} = await js_function_declaration_to_statement_arguments_assert_added(function_declaration);
         if (added) {
             c();
