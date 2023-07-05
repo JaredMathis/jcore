a636d7308decf77dd9ac28b94aecaef7611a9e5d
JaredMathis <jared.mathis@native.consulting>
2023-07-05 15:20:01 -0400

    #321 function_auto list_last_before

--- src/list/last/before.mjs
+++ src/list/last/before.mjs
@@ -1,3 +1,4 @@
+import { subtract_1 } from '../../subtract/1.mjs';
 import { metadata } from '../../metadata.mjs';
 import { list_get } from '../get.mjs';
 import { list_last_index } from './index.mjs';
@@ -6,7 +7,7 @@ import { arguments_assert } from '../../arguments/assert.mjs';
 export function list_last_before(list) {
     arguments_assert(arguments, [list_is]);
     let v = list_last_index(list);
-    let v_2 = list_get(list, v);
+    let v_2 = list_get(list, subtract_1(v));
     return v_2;
     metadata([]);
 }
