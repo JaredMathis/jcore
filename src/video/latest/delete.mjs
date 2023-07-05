a70dead237b338f3950569d47cf65552b6e9b7db
JaredMathis <jared.mathis@native.consulting>
2023-07-05 14:06:22 -0400

    #321 function_auto video_latest_delete

---
+++ src/video/latest/delete.mjs
@@ -0,0 +1,6 @@
+import { error } from '../../error.mjs';
+import { arguments_assert } from '../../arguments/assert.mjs';
+export function video_latest_delete() {
+    arguments_assert(arguments, []);
+    error('todo: video_latest_delete');
+}
