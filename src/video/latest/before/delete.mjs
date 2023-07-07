d71a7f2e214b65739842278eae94356a1ccae91c
JaredMathis <jared.mathis@native.consulting>
2023-07-07 16:13:23 -0400

    #321 function_auto video_latest_before_delete

--- src/video/latest/before/delete.mjs
+++ src/video/latest/before/delete.mjs
@@ -1,8 +1,8 @@
+import { list_last_before } from '../../../list/last/before.mjs';
 import { video_latest_delete_generic } from '../delete/generic.mjs';
-import { list_last } from '../../../list/last.mjs';
 import { arguments_assert } from '../../../arguments/assert.mjs';
 export async function video_latest_before_delete() {
     arguments_assert(arguments, []);
-    let lambda = list_last;
+    let lambda = list_last_before;
     await video_latest_delete_generic(lambda);
 }
