334e6c87e60df178a9b69a14e24317d6825404a4
JaredMathis <jared.mathis@native.consulting>
2023-07-07 10:05:47 -0400

    #321 noop

--- src/video/latest/delete.mjs
+++ src/video/latest/delete.mjs
@@ -4,10 +4,11 @@ import { video_paths_filter } from '../paths/filter.mjs';
 import { video_screen_recordings_path_read } from '../screen/recordings/path/read.mjs';
 import { arguments_assert } from '../../arguments/assert.mjs';
 import { list_last } from '../../list/last.mjs';
+import { list_last_before } from '../../list/last/before.mjs';
 export async function video_latest_delete() {
     arguments_assert(arguments, []);
     let paths = await video_screen_recordings_path_read();
     let filtered = video_paths_filter(paths);
-    let last = list_last(filtered);
+    let last = list_last_before(filtered);
     await file_move_to_subdirectory(last, string_delete_soft_path());
 }
